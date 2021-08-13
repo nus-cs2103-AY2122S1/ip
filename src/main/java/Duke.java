de1c16e0643da94049e9727661f41331e4e85d2e
Rohit <rajeshbhatrohit@gmail.com>
2021-08-13 22:16:49 +0800

    added basic greet, echo, and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Duke implements Runnable {
+    private final String BORDER = "--------------------------------------------";
+    private final String GREETING = "Hello! I'm Duke, what can I do for you?";
+    private final String FAREWELL = "Bye. Hope to see you again soon!";
+
+    private void printDuke(String str) {
+        System.out.printf(
+            String.format("%s\n%s\n%s\n", BORDER, str, BORDER)
+                .replaceAll("(?m)^", "\t")
+        );
+    }
+
+    private void execute(String input) {
+        printDuke(input);
+    }
+
+    public void run() {
+        printDuke(GREETING);
+
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        while (!(input = sc.nextLine().trim()).equals("bye")) {
+            execute(input);
+        }
+
+        printDuke(FAREWELL);
+        sc.close();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+       new Duke().run();
     }
 }
