06d915320cb3c5b0518b997c90b95f4176e0fc41
SkyBlaise <zsc990124@gmail.com>
2021-08-13 20:07:12 +0800

    Level 2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,12 @@ public class Duke {
 	private final String MESSAGE_WELCOME = "Hi! I'm Sora. How can I help you?";
 	private final String MESSAGE_EXIT = "Have a nice day! Good bye XD";
 	
+	private final String[] listTasks = new String[100];
+	private int numTasks = 0;
+	
+	/**
+	 * Main body of the bot.
+	 */
 	public void run() {
 		// Print welcome message
 		printMessage(MESSAGE_WELCOME);
@@ -13,13 +19,44 @@ public class Duke {
 		Scanner sc = new Scanner(System.in);
 		String input;
 		
-		while (!(input = sc.nextLine().trim()).equals("bye")) printMessage(input);
+		// Main body of chat box, each if else handles one type of command
+		while (!(input = sc.nextLine().trim()).equals("bye")) {
+			if (input.equals("list")) printFullList();
+			else addTask(input);
+		}
 		
 		// Close off scanner
 		sc.close();
 		
 		// Print good bye message
 		printMessage(MESSAGE_EXIT);
+	}
+	
+	/**
+	 * Adds a new task to the list of tasks.
+	 *
+	 * @param description new task to be added
+	 */
+	private void addTask(String description) {
+		// Add the newly created task into list of tasks
+		listTasks[numTasks++] = description;
+		
+		// Display message
+		printMessage("added: " + description);
+	}
+	
+	/**
+	 * Prints all the tasks in the list.
+	 */
+	private void printFullList() {
+		// Reformat the list of tasks
+		StringBuilder msg = new StringBuilder();
+		
+		for (int i = 0; i < numTasks - 1; i++) msg.append(i + 1).append(". ").append(listTasks[i]).append("\n");
+		msg.append(numTasks).append(". ").append(listTasks[numTasks - 1]);
+		
+		// Display message
+		printMessage(msg.toString());
 	}
 	
 	/**
