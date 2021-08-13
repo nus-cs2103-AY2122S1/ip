734f94b8364850aaba093de71637b869df2cf93d
TzeJit <51875344+tzejit@users.noreply.github.com>
2021-08-13 18:05:06 +0800

    Add Level-1 functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String command;
+        String line = "    ____________________________________________________________\n";
+        System.out.println(line + "     Hello! I'm Duke\n" + "     What can I do for you?\n" + line);
+        command = sc.next();
+        while (!command.equals("bye")) {
+            System.out.println(line + "     " + command + "\n" + line);
+            command = sc.next();
+        }
+        System.out.println(line + "     Bye. Hope to see you again soon!\n" + line);
     }
 }
