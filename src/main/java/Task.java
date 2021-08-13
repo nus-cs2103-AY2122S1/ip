de4b765c0eee0f2c95caeab121a399d14b3be56b
TzeJit <51875344+tzejit@users.noreply.github.com>
2021-08-13 21:21:57 +0800

    Add DukeException throwing

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,7 +2,10 @@ public class Task {
     private final String description;
     private boolean isDone;
 
-    public Task(String description) {
+    public Task(String description) throws DukeException {
+        if (description.equals("")) {
+            throw new DukeException("empty description");
+        }
         this.description = description;
         this.isDone = false;
     }
