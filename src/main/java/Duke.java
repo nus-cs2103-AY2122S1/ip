f8ac26f7bc5f100cf830c36d19366f15b934ef4e
wpinrui <wongpinrui@gmail.com>
2021-08-13 19:53:34 +0800

    Implement skeletal version of Good Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String intro = "Hey there! I'm Good Duke. How many I help you today?";
+        String outro = "That was an excellent chat - I look forward to seeing you again soon!";
+        Scanner sc = new Scanner(System.in);
+        System.out.println(intro);
+        while (true) {
+            String userEntry = sc.nextLine();
+            if (!userEntry.equals("bye")) {
+                System.out.println(userEntry);
+            } else {
+                System.out.println(outro);
+                break;
+            }
+        }
     }
 }
