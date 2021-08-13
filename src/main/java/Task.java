875a891fdcd35b213a5d8e5b4ab120031d0ccf87
Imran <imranr2@msn.com>
2021-08-13 20:37:31 +0800

    feat: Add Task class to represent tasks

---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String name;
+    private boolean isDone;
+
+    Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public void setDone(boolean done) {
+        this.isDone = done;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        if (isDone) {
+            return "[✓] " + this.name;
+        } else {
+            return "[✗] " + this.name;
+        }
+    }
+}
