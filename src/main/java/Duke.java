85ebb7745a90ee2d237bf377a660fbb55828d18f
Jae Ho <zld.ejho@gmail.com>
2021-08-14 01:02:06 +0800

    Added Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<String> taskList;
+
     public static void main(String[] args) {
         /* // in case we might need the logo later
         String logo = " ____        _        \n"
@@ -14,11 +17,16 @@ public class Duke {
         String greeting = "\t Hello! I'm Duke\n" +
                 "\t What can I do for you?\n" ;
         System.out.println(reply(greeting));
+        taskList = new ArrayList<>();
         // listen to user input
         Scanner scan = new Scanner(System.in);
         String command = scan.nextLine();
         while (!command.equals("bye")) {
-            System.out.println(reply(echo(command)));
+            if (command.equals("list")) {
+                System.out.println(reply(list()));
+            } else { // add the task
+                System.out.println(reply(add(command)));
+            }
             command = scan.nextLine();
         }
         // farewell the user
@@ -35,6 +43,23 @@ public class Duke {
     private static String echo(String input) {
         // echo back user commands
         return String.format("\t %s\n", input);
+    }
+
+    private static String add(String input) {
+        // add task to the taskList
+        taskList.add(input);
+        return "\t added: " + input + "\n";
+    }
+
+    private static String list() {
+        // list out all tasks
+        int count = 1;
+        StringBuilder reply = new StringBuilder();
+        for (String task : taskList) {
+            reply.append(String.format("\t %d. %s\n", count, task));
+            count++;
+        }
+        return reply.toString();
     }
 }
 
