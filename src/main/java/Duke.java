1766940b8ba8057b8b7d54aced20dc422a1b7623
TzeJit <51875344+tzejit@users.noreply.github.com>
2021-08-13 20:14:24 +0800

    Add Level-4 functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,6 +10,7 @@ public class Duke {
         System.out.println(line + "     Hello! I'm Duke\n" + "     What can I do for you?\n" + line);
         command = sc.next();
         while (!command.equals("bye")) {
+            Task task;
             switch(command) {
             case "list":
                 System.out.println(line + list.returnItems() + line);
@@ -17,12 +18,30 @@ public class Duke {
             case "done":
                 int index = sc.nextInt();
                 System.out.println(line + "     Nice! I've marked this task as done:\n"
-                        + list.markDone(index) + line);
+                        + "       " + list.markDone(index) + "\n" + line);
                 break;
-            default:
-                    command += sc.nextLine();
-                list.addItem(command);
-                System.out.println(line + "     added: " + command + "\n" + line);
+            case "todo":
+                command = sc.nextLine();
+                task = new Todo(command);
+                list.addItem(task);
+                System.out.println(line + "     Got it. I've added this task:\n"
+                        + "       " + task + "\n" + list.returnItemCount() + "\n" + line);
+                break;
+            case "deadline":
+                command = sc.nextLine();
+                String[] deadlineEntry = command.split(" /by ");
+                task = new Deadline(deadlineEntry[0], deadlineEntry[1]);
+                list.addItem(task);
+                System.out.println(line + "     Got it. I've added this task:\n"
+                        + "       " + task + "\n" + list.returnItemCount() + "\n" + line);
+                break;
+            case "event":
+                command = sc.nextLine();
+                String[] eventEntry = command.split(" /at ");
+                task = new Event(eventEntry[0], eventEntry[1]);
+                list.addItem(task);
+                System.out.println(line + "     Got it. I've added this task:\n"
+                        + "       " + task + "\n" + list.returnItemCount() + "\n" + line);
                 break;
             }
             command = sc.next();
