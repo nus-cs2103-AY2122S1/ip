3b25bd8665de0c364527a3104a74031287818d3b
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-08-13 21:45:55 +0800

    Add Task class Javadocs

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,20 +1,43 @@
+/**
+ * This class encapsulates a Task.
+ * Contains the boolean logic to store if a task has been completed, as well as the task description.
+ */
+
 public class Task {
+    /** The description of the task. */
     protected String description;
+    /** The status of the task if it has been completed*/
     protected boolean isDone;
 
+    /**
+     * Basic constructor to create a task.
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Gets the string representation of the status.
+     * @return X or empty string depending on the state of isDone
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
 
+    /**
+     * Setter for isDone to true.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+
+    /**
+     * Overriding method to print a Task object.
+     * @return String representation of the task instance.
+     */
     @Override
     public String toString() {
         return String.format("[%s] %s", this.getStatusIcon(), this.description);
