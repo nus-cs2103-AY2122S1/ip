29d4cb806dd8220427b1c38dc711b401bf438f43
wpinrui <wongpinrui@gmail.com>
2021-08-13 21:38:27 +0800

    Add support for ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,47 +1,83 @@
 import java.util.Scanner;
-import java.util.function.Function;
-import java.util.function.UnaryOperator;
 
 public class Duke {
     private static String introString = "Hey there! I'm Good Duke. How many I help you today?";
     private static String outroString = "That was an excellent chat - I look forward to seeing you again soon!";
-    private static UnaryOperator<String> addedString = item ->
-            String.format("Alright, I've added \"%s\" to my list :)", item);
-    private static Function<Task, String> setDoneString = task ->
-            String.format("Certainly, I've marked this task as done: \n\t%s", task);
+
     private static Scanner sc = new Scanner(System.in);
     private static Task[] taskList = new Task[100];
     private static int taskIndex = 0;
 
-    public static void printList() {
+    public static String taskListString() {
+        String output = "";
         for (int i = 0; i < taskIndex; i++) {
-            String output = String.format("%d. %s", i + 1, taskList[i]);
-            System.out.println(output);
+            output += String.format("%d. %s\n", i + 1, taskList[i]);
         }
+        return output;
+    }
+
+    public static String addedString(Task task) {
+        return String.format("Alright, I've added this task: \n\t%s\nNow, you have %d tasks in the list.\n", task, taskIndex);
+    }
+
+    public static String doneString(Task task) {
+        return String.format("Certainly, I've marked this task as done: \n\t%s\n", task);
+    }
+
+    public static void print(String str) {
+        String horizontalLine = "________________________________________________________________________________";
+        System.out.println(horizontalLine);
+        System.out.println(str);
+        System.out.println(horizontalLine);
+
     }
 
     public static void main(String[] args) {
-        System.out.println(introString);
+        print(introString);
         label:
         while (true) {
             String userEntry = sc.nextLine();
             String[] commands = userEntry.split(" ");
+            String description;
+            String descriptionAndTime;
+            String time;
+            Task task;
             switch (commands[0]) {
                 case "bye":
-                    System.out.println(outroString);
+                    print(outroString);
                     break label;
                 case "list":
-                    printList();
+                    print(taskListString());
                     break;
                 case "done":
-                    Task task = taskList[Integer.parseInt(commands[1]) - 1];
+                    task = taskList[Integer.parseInt(commands[1]) - 1];
                     task.setDone(true);
-                    System.out.println(setDoneString.apply(task));
+                    print(doneString(task));
                     break;
-                default:
-                    taskList[taskIndex] = new Task(userEntry);
+                case "todo":
+                    description = userEntry.substring(5);
+                    task = new ToDo(description);
+                    taskList[taskIndex] = task;
                     taskIndex++;
-                    System.out.println(addedString.apply(userEntry));
+                    print(addedString(task));
+                    break;
+                case "deadline":
+                    descriptionAndTime = userEntry.substring(9);
+                    description = descriptionAndTime.split("/by ")[0];
+                    time = descriptionAndTime.split("/by ")[1];
+                    task = new Deadline(description, time);
+                    taskList[taskIndex] = task;
+                    taskIndex++;
+                    print(addedString(task));
+                    break;
+                case "event":
+                    descriptionAndTime = userEntry.substring(6);
+                    description = descriptionAndTime.split("/at ")[0];
+                    time = descriptionAndTime.split("/at ")[1];
+                    task = new Event(description, time);
+                    taskList[taskIndex] = task;
+                    taskIndex++;
+                    print(addedString(task));
                     break;
             }
         }
