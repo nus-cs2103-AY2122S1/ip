31848599afbb51ada954a2f644497bbe3410ca11
luminousleek <68138671+luminousleek@users.noreply.github.com>
2021-08-13 18:22:25 +0800

    Implement Greet, Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
+
+//    private static String logo = " ____        _        \n"
+//            + "|  _ \\ _   _| | _____ \n"
+//            + "| | | | | | | |/ / _ \\\n"
+//            + "| |_| | |_| |   <  __/\n"
+//            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private static String line = "____________________________________________________________";
+
+    /**
+     * Wraps a given message above and below with lines and prints it
+     *
+     * @param msg
+     */
+    private static void wrap(String msg) {
+        System.out.println(line);
+        System.out.println(msg);
+        System.out.println(line);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // initialise scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Greet
+        wrap("Hello! I'm Bob\nWhat can I do for you?");
+
+        // Echo
+        String input = sc.nextLine();
+        while (!input.equals("bye")){
+            wrap(input);
+            input = sc.nextLine();
+        }
+
+        // Exit
+        wrap("Bye. Hope to see you again soon!");
     }
 }
