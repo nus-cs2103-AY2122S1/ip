3d5a362f393da2ecaedd8b999cfa213b6549e524
junghon3709 <junghonyip@gmail.com>
2021-08-13 21:25:14 +0800

    Add greeting and bye feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+
+        String greetingMessage = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(greetingMessage);
+        boolean userExit = false;
+        while (!userExit) {
+            String userInput = scanner.nextLine();
+            switch (userInput) {
+                case "bye":
+                    userExit = true;
+                    break;
+                default:
+                    System.out.println(userInput);
+            }
+        }
+        if (userExit) {
+            String goodbyeMessage = "Bye. Hope to see you again soon!";
+            System.out.println(goodbyeMessage);
+        }
     }
 }
