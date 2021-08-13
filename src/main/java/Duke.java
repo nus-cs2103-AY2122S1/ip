3f68127d4005051ada2e0403ad3ce95545c6bc03
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-08-13 21:43:18 +0800

    Add Task class with mark complete feature
    Features basic error handling for handling errenous user input.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,20 @@
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Duke {
     /** Simple string array to store inputs **/
-    String[] list;
+    private Task[] list;
     /** Simple index to keep track of the current element in the list**/
-    int index;
+    private int index;
+    /** Enum for handling user command**/
+    public enum Commands {
+        BYE, LIST, DONE
+    }
 
     /** Basic constructor to initialise the list **/
     public Duke() {
-        this.list = new String[100];
+        this.list = new Task[100];
         this.index = 0;
     }
 
@@ -18,7 +23,7 @@ public class Duke {
      * @param text The text to be appended.
      */
     private void append(String text) {
-        this.list[index] = text;
+        this.list[index] = new Task(text);
         this.index ++;
     }
 
@@ -30,13 +35,60 @@ public class Duke {
         for (int i = 0; i < this.index; i++) {
             // i + 1 is used to start indexing from 1
             output.append(i + 1).append(". ");
-            output.append(this.list[i]);
+            output.append(this.list[i].toString());
             // ensure the newline is not added on the last element
             if (i != this.index - 1) {
                 output.append("\n");
             }
         }
         this.printMsg(output.toString());
+    }
+
+
+    /**
+     * Method to be called when the first argument to the input is "done".
+     * Parses the second argument as an integer and checks if it is a valid index.
+     * If valid, marks the task that is currently stored in memory to be complete.
+     *
+     * @param text Command line argument string
+     */
+    private void done(String text) {
+        // check for the existence of the second argument and that it can be parsed to an int for lookup
+        String[] splitted = text.split(" ");
+        // TODO: Ensure that index 1 is valid. Throw an error if no secondary argument is provided on "done"
+        if (splitted.length < 2) {
+            printMsg("Command is missing an argument 'index'.");
+            return;
+        }
+
+        String dirtyInt = splitted[1];
+
+
+        // ensure that it is defined
+        if (dirtyInt != null) {
+            // try to parse it into an int
+            try {
+                int lookup = Integer.parseInt(dirtyInt);
+                if (lookup < 1) {
+                    this.printMsg(String.format("The specified task number %d does not exist. Enter a number that is at least 1.", lookup));
+                }
+                else if (lookup < this.index + 1) {
+                    Task task = this.list[lookup - 1];
+                    task.markAsDone();
+                    this.printMsg(String.format("Nice! I've marked this task as done:\n  %s", task));
+                } else {
+                    this.printMsg(String.format("The task number %s is invalid, please enter a valid " +
+                            "number.", lookup));
+                }
+            } catch(NumberFormatException e) {
+                printMsg(String.format("The specified task number %s, is not a valid integer, please " +
+                        "enter a valid numeric task number.", dirtyInt));
+            }
+        } else {
+            System.out.println(Arrays.toString(splitted));
+        }
+
+
     }
 
 
@@ -60,7 +112,9 @@ public class Duke {
         boolean terminate = false;
         while (!terminate) {
             String text = scanner.nextLine().toLowerCase();
-            switch (text.toLowerCase()) {
+            // get the first word of the text
+            String command = text.split(" ")[0];
+            switch (command.toLowerCase()) {
                 // break out of the while loop
                 case ("bye") -> {
                     this.printMsg("Bye. Hope to see you again soon!");
@@ -69,6 +123,10 @@ public class Duke {
                 }
                 // list out the items in the list if "list" is called
                 case ("list") -> this.list();
+
+                case("done") -> {
+                    this.done(text);
+                }
 
                 default -> {
                     this.append(text);
