176f8cf9cc293449bb5bcff5d6b75b3fcf912f61
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-08-13 20:15:58 +0800

    Add list storage functionality
    Make Duke to be instance based and removed all static methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,14 +2,49 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Duke {
+    /** Simple string array to store inputs **/
+    String[] list;
+    /** Simple index to keep track of the current element in the list**/
+    int index;
 
+    /** Basic constructor to initialise the list **/
+    public Duke() {
+        this.list = new String[100];
+        this.index = 0;
+    }
+
+    /**
+     * Private method to append items to the back of the list.
+     * @param text The text to be appended.
+     */
+    private void append(String text) {
+        this.list[index] = text;
+        this.index ++;
+    }
+
+    /**
+     * Simple method that iterates through the task list and prints out all the entries
+     */
+    public void list() {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < this.index; i++) {
+            // i + 1 is used to start indexing from 1
+            output.append(i + 1).append(". ");
+            output.append(this.list[i]);
+            // ensure the newline is not added on the last element
+            if (i != this.index - 1) {
+                output.append("\n");
+            }
+        }
+        this.printMsg(output.toString());
+    }
 
 
     /**
      * Simple formatting tool to be used when printing commands
      * @param text The text to be printed.
      */
-    public static void printMsg(String text) {
+    private void printMsg(String text) {
         String bar = "===============================================";
         System.out.printf("%s\n%s\n%s\n", bar, text, bar);
     }
@@ -19,18 +54,26 @@ public class Duke {
      * Method that listens and scans for user input.
      * Program will exit when the command "bye" is given to the scanner.
      */
-    public static void listen() {
+    public void listen() {
         Scanner scanner = new Scanner(System.in);
-        Duke.printMsg("Hello! I'm Duke\nWhat can I do for you?");
-        while (true) {
+        this.printMsg("Hello! I'm Duke\nWhat can I do for you?");
+        boolean terminate = false;
+        while (!terminate) {
             String text = scanner.nextLine().toLowerCase();
-            if (text.equals("bye")) {
-                Duke.printMsg("Bye. Hope to see you again soon!");
-                scanner.close();
-                break;
-            }
-            else {
-                Duke.printMsg(text);
+            switch (text.toLowerCase()) {
+                // break out of the while loop
+                case ("bye") -> {
+                    this.printMsg("Bye. Hope to see you again soon!");
+                    scanner.close();
+                    terminate = true;
+                }
+                // list out the items in the list if "list" is called
+                case ("list") -> this.list();
+
+                default -> {
+                    this.append(text);
+                    this.printMsg("added: " + text);
+                }
             }
         }
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // Start listening to user input
-        Duke.listen();
+        // Start listening to user input through a duke instance
+        Duke duke = new Duke();
+        duke.listen();
     }
 }
