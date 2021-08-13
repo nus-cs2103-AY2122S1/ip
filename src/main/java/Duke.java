e1c40872e1813ba21e83fc5f931ed4eff3bcccc3
TzeJit <51875344+tzejit@users.noreply.github.com>
2021-08-13 18:53:29 +0800

    Add Level-2 functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,13 +4,19 @@ public class Duke {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List list = new List();
         String command;
         String line = "    ____________________________________________________________\n";
         System.out.println(line + "     Hello! I'm Duke\n" + "     What can I do for you?\n" + line);
-        command = sc.next();
+        command = sc.nextLine();
         while (!command.equals("bye")) {
-            System.out.println(line + "     " + command + "\n" + line);
-            command = sc.next();
+            if (command.equals("list")) {
+                System.out.println(line + list.returnItems() + line);
+            } else {
+                list.addItem(command);
+                System.out.println(line + "     added: " + command + "\n" + line);
+            }
+            command = sc.nextLine();
         }
         System.out.println(line + "     Bye. Hope to see you again soon!\n" + line);
     }
