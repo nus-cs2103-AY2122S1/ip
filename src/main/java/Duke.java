4f178b024cdacbada409ee3eef219425c15ed414
twothicc <chenenliuu@gmail.com>
2021-08-14 00:15:39 +0800

    Added Greet, Echo, Exit functionality to chatbot with formatting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.*;
+
 public class Duke {
+    static final String lineSeparator = "_____________________________________________________________";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println(formatResponse("Hello! I'm Duke\nWhat can I do for you?"));
+        String echoInput = sc.nextLine();
+        while (!echoInput.equals("bye")) {
+            System.out.println(formatResponse(echoInput));
+            echoInput = sc.nextLine();
+        }
+        System.out.println(formatResponse("Bye. Hope to see you again soon!"));
+    }
+
+    private static String formatResponse(String response) {
+        return String.format("%s\n    %s\n%s", lineSeparator, response, lineSeparator);
     }
 }
