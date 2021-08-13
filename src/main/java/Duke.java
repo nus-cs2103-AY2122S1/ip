9b0926f6f92b2e371ad6f588e881e72721f9068b
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-08-13 19:50:14 +0800

    Add echo functionality
    Seperated the Duke instance to a Main class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+
+
+    /**
+     * Simple formatting tool to be used when printing commands
+     * @param text The text to be printed.
+     */
+    public static void printMsg(String text) {
+        String bar = "===============================================";
+        System.out.printf("%s\n%s\n%s\n", bar, text, bar);
     }
+
+
+    /**
+     * Method that listens and scans for user input.
+     * Program will exit when the command "bye" is given to the scanner.
+     */
+    public static void listen() {
+        Scanner scanner = new Scanner(System.in);
+        Duke.printMsg("Hello! I'm Duke\nWhat can I do for you?");
+        while (true) {
+            String text = scanner.nextLine().toLowerCase();
+            if (text.equals("bye")) {
+                Duke.printMsg("Bye. Hope to see you again soon!");
+                scanner.close();
+                break;
+            }
+            else {
+                Duke.printMsg(text);
+            }
+        }
+    }
+
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Start listening to user input
+        Duke.listen();
+    }
+}
