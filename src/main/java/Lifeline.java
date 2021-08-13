9d78b87fcea24815f363f4ee62eaf75fe41920c8
Imran <imranr2@msn.com>
2021-08-13 20:25:08 +0800

    feat: Add Greet, Echo, Exit to Lifeline

--- src/main/java/Lifeline.java
+++ src/main/java/Lifeline.java
@@ -1,13 +1,37 @@
-public class Lifeline {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        String logo = " _      _____ ______ ______ _      _____ _   _ ______\n"
+public class Lifeline {
+
+    private void greet() {
+        String lifeline = " _      _____ ______ ______ _      _____ _   _ ______\n"
                 + "| |    |_   _|  ____|  ____| |    |_   _| \\ | |  ____|\n"
                 + "| |      | | | |__  | |__  | |      | | |  \\| | |__\n"
                 + "| |      | | |  __| |  __| | |      | | | . ` |  __|\n"
                 + "| |____ _| |_| |    | |____| |____ _| |_| |\\  | |____\n"
                 + "|______|_____|_|    |______|______|_____|_| \\_|______|\n";
+        System.out.println("Hello! I am\n" + lifeline);
+        System.out.println("What can I help you with today?\n");
+    }
 
-        System.out.println("Hello from\n" + logo);
+    private void echo(String input) {
+        System.out.println("You have said " + "\"" + input + "\".");
+        System.out.println("Anything else?\n");
+    }
+
+    private void exit() {
+        System.out.println("Goodbye! Thanks for chatting with me!\n");
+    }
+
+    public static void main(String[] args) {
+        Lifeline lifeline = new Lifeline();
+        lifeline.greet();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println();
+            lifeline.echo(input);
+            input = sc.nextLine();
+        }
+        lifeline.exit();
     }
 }
