2afd6c3c9dc7db159da3174e882cc98030c58c99
Jae Ho <zld.ejho@gmail.com>
2021-08-14 01:25:11 +0800

    Added Task.java and Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static ArrayList<String> taskList;
+    private static ArrayList<Task> taskList;
 
     public static void main(String[] args) {
         /* // in case we might need the logo later
@@ -24,7 +24,11 @@ public class Duke {
         while (!command.equals("bye")) {
             if (command.equals("list")) {
                 System.out.println(reply(list()));
+            } else if (command.startsWith("done ")) {
+                int index = command.toCharArray()[command.length()-1] - '0' - 1;
+                System.out.println(reply(done(index)));
             } else { // add the task
+                Task t = new Task(command);
                 System.out.println(reply(add(command)));
             }
             command = scan.nextLine();
@@ -47,7 +51,7 @@ public class Duke {
 
     private static String add(String input) {
         // add task to the taskList
-        taskList.add(input);
+        taskList.add(new Task(input));
         return "\t added: " + input + "\n";
     }
 
@@ -55,11 +59,21 @@ public class Duke {
         // list out all tasks
         int count = 1;
         StringBuilder reply = new StringBuilder();
-        for (String task : taskList) {
-            reply.append(String.format("\t %d. %s\n", count, task));
+        for (Task task : taskList) {
+            reply.append(String.format("\t %d.[%s] %s\n", count, task.getStatusIcon(), task));
             count++;
         }
         return reply.toString();
+    }
+
+    private static String done(int index) {
+        // retrieve task from TaskList
+        Task t = taskList.get(index);
+        // mark task as done
+        t.markAsDone();
+        // reply
+        return String.format("\t Nice! I've marked this task as done: \n" +
+                "\t   [X] %s\n", t);
     }
 }
 
