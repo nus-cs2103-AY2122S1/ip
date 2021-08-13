9a96cc6c6f8e68a233ee1f068cf1a0fef0e431d5
skcheongbrian <skcheongbrian@gmail.com>
2021-08-14 03:03:31 +0800

    Implemented Level-1: starts by greeting the user, simply echos commands entered by the user, and exits when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                   + "|  _ \\ _   _| | _____ \n"
+                   + "| | | | | | | |/ / _ \\\n"
+                   + "| |_| | |_| |   <  __/\n"
+                   + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        lineSeparator();
+        System.out.println("I'm Duke");
+        System.out.println("What can I do for you?");
+        lineSeparator();
+        while (true) {
+            String userInput = sc.next();
+            if (userInput.equals("bye")) {
+                lineSeparator();
+                System.out.println("Bye. Hope to see you soon!");
+                lineSeparator();
+                break;
+            } else {
+                lineSeparator();
+                System.out.println(userInput);
+                lineSeparator();
+            }
+
+        }
+    }
+
+    private static void lineSeparator() {
+        System.out.println("____________________________________________________________");
     }
 }
