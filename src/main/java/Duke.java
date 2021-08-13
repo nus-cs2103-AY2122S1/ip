fc66d7146890c513db231accfbd7c03931ef2577
twothicc <chenenliuu@gmail.com>
2021-08-14 01:05:36 +0800

    Add chatbot functionality that allows users to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,16 +4,29 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        String[] storedMessages = new String[100];
+        Task[] storedTasks = new Task[100];
         int idx = 0;
         String echoInput = sc.nextLine();
         while (!echoInput.equals("bye")) {
             if (echoInput.equals("list")) {
                 for (int i = 0; i < idx; i++) {
-                    System.out.println(i + ": " + storedMessages[i]);
+                    System.out.println(i + "." + storedTasks[i].getStatusIcon() +
+                            " " + storedTasks[i].description);
+                }
+            } else if (echoInput.startsWith("done")) {
+                try {
+                    int num = Integer.parseInt(echoInput.substring(5));
+                    storedTasks[num - 1].markAsDone();
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println(storedTasks[num - 1].getStatusIcon()
+                            + " " + storedTasks[num - 1].description);
+                } catch (NumberFormatException nfe) {
+                    System.out.println("done came with an incorrect input");
+                } catch (ArrayIndexOutOfBoundsException ae) {
+                    System.out.println("number provided with done must be from 1 to 100 inclusive");
                 }
             } else {
-                storedMessages[idx] = echoInput;
+                storedTasks[idx] = new Task(echoInput);
                 System.out.println("added: " + echoInput);
                 idx++;
             }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -8,11 +8,11 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
     }
 
     public void markAsDone() {
         this.isDone = true;
     }
-    
+
 }
