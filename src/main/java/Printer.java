834a0f9e914b884d7278cc42cf97e70328247f23
Siew Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2021-08-13 18:50:22 +0800

    Level 1 (#1)
    * Update .gitignore to not track Java class files
    
    * Add Printer class
    
    * Modify divider line
    
    * Add basic IO logic

---
+++ src/main/java/Printer.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Printer {
+    /**
+     * Parses and formats the output.
+     *
+     * @param output variable length arguments, where each argument corresponds to a new line to be printed
+     * @return a formatted string
+     */
+    private static String outputParser(String ...output) {
+        String divider = "\t************************************************************\n";
+        return String.format(
+                "%s%s%s",
+                divider,
+                Arrays.stream(output)
+                        .map(str -> String.format("\t%s\n", str))
+                        .reduce((res, curr) -> res + curr)
+                        .orElse(""),
+                divider);
+    }
+
+    /**
+     * Prints the formatted output on the screen.
+     *
+     * @param output variable length arguments, where each argument corresponds to a new line to be printed
+     */
+    public static void print(String ...output) {
+        System.out.println(outputParser(output));
+    }
+}
