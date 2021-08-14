36d0c63b72e983ef33b647813001c21603a174fb
pbthang <bathang02@gmail.com>
2021-08-14 18:00:51 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
 
 public class Duke {
+    private final String[] tasks = new String[100];
+    private int count = 0;
 
     private void printMessage(String[] msgLines) {
+        if (msgLines.length == 0) {
+            return;
+        }
         System.out.println("\t_______________________________________________________");
         for (String msg : msgLines) {
             System.out.println("\t  " + msg);
@@ -11,7 +16,26 @@ public class Duke {
     }
 
     private void printMessage(String msg) {
+        if (msg.length() == 0) {
+            return;
+        }
         printMessage(new String[] {msg});
+    }
+
+    public void addTask(String task) {
+        this.tasks[count++] = task;
+    }
+
+    public void listTasks() {
+        if (this.count == 0) {
+            printMessage("No task added yet.");
+            return;
+        }
+        String[] messages = new String[this.count];
+        for (int i = 0; i < this.count; i++) {
+            messages[i] = String.format("%d - %s", i + 1, this.tasks[i]);
+        }
+        printMessage(messages);
     }
 
     public void run() {
@@ -20,7 +44,12 @@ public class Duke {
        Scanner scanner = new Scanner(System.in);
        String commandString = scanner.nextLine();
        while (!commandString.equals("bye")) {
-           printMessage(commandString);
+           if (commandString.equals("list")) {
+               listTasks();
+           } else {
+               addTask(commandString);
+               printMessage("Added: " + commandString);
+           }
            commandString = scanner.nextLine();
        }
        printMessage("Bye, see you later.");
