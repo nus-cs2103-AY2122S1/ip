9cff32d9c913973030cc7a56a5937372bca9b9d5
auhc99 <auhc99@gmail.com>
2021-08-14 17:19:48 +0800

    Level 3. Mark as Done
    Add the ability to mark tasks as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private static ArrayList<String> list = new ArrayList<>(100);
+    private static ArrayList<Task> list = new ArrayList<>(100);
 
     private static void chat(String content) {
         System.out.println(
@@ -14,13 +14,14 @@ public class Duke {
     }
 
     private static void addItem(String item) {
-        list.add(item);
+        list.add(new Task(item));
     }
 
     private static void displayList() {
-        StringBuilder listString = new StringBuilder();
+        StringBuilder listString = new StringBuilder("Here are the tasks in your list:\n");
         for (int i = 0; i < list.size(); i++) {
-            listString.append(String.valueOf(i + 1) + ". " + list.get(i));
+            Task task = list.get(i);
+            listString.append(String.valueOf(i + 1) + ".[" + task.getStatusIcon() + "] " + task.description);
             if (i != list.size() - 1) {
                 listString.append("\n");
             }
@@ -38,16 +39,25 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
+            String[] arguments = input.split(" ");
             if (input.equals("bye")) {
-                chat("Bye. Hope to see you again soon!");
                 break;
             } else if (input.equals("list")) {
                 displayList();
+            } else if (arguments[0].equals("done")) {
+                Task task = list.get(Integer.valueOf(arguments[1]) - 1);
+                task.markDone();
+                chat("Nice! I've marked this task as done: \n"
+                        + "  ["
+                        + task.getStatusIcon()
+                        + "] "
+                        + task.description);
             } else {
                 addItem(input);
                 chat("added: " + input);
             }
         }
+        chat("Bye. Hope to see you again soon!");
     }
 
 
