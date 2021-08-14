70272eb1036dba32b119619507cadad85d758195
Jeff Sieu <jeffsy00@gmail.com>
2021-08-14 16:49:48 +0800

    Skeletal version: Greet, Echo, Exit
    - Duke greets the user
    - All commands are echoed
    - "bye" causes the Duke to exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +10,34 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        DukeCommandFormatter commandFormatter = new DukeCommandFormatter(System.in, System.out);
+        String command;
+        while (!(command = commandFormatter.nextCommand()).equals("bye")){
+            processCommand(commandFormatter, command);
+        }
+    }
+
+    public static void processCommand(DukeCommandFormatter commandFormatter, String command) {
+        commandFormatter.printOutputLine(command);
+    }
+
+    private static class DukeCommandFormatter {
+        private Scanner scanner;
+        private PrintStream outputStream;
+
+        DukeCommandFormatter(InputStream inputStream, PrintStream outputStream) {
+            this.scanner = new Scanner(inputStream);
+            this.outputStream = outputStream;
+        }
+
+        String nextCommand() {
+            outputStream.print("Duke> ");
+            return scanner.nextLine();
+        }
+
+        void printOutputLine(String output) {
+            outputStream.println(output);
+        }
     }
 }
