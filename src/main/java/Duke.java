ba9355fc48593d47b418d9a91c8536f87271438d
Xiaoyunnn <xiaoyun.wu00@gmail.com>
2021-08-14 16:45:43 +0800

    Add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,32 +2,42 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                    + "|  _ \\ _   _| | _____ \n"
-                    + "| | | | | | | |/ / _ \\\n"
-                    + "| |_| | |_| |   <  __/\n"
-                    + "|____/ \\__,_|_|\\_\\___|\n";
-        String logo2 = " ______      ____      __  \n"
+        String[] items = new String[100];
+        int index = 0;
+
+        String logo = " ______      ____      __  \n"
                      + "|   _  \\    /    \\    |  | \n"
                      + "|  |_|  /  /  /\\  \\   |  | \n"
                      + "|  |_|  \\ /  ____  \\  |  |\n"
                      + "|______/ /__/    \\__\\ |__|\n";
-        System.out.println(logo2);
+        System.out.println(logo);
         System.out.println("Hello! I'm Bai. \n" +
                             "What can I do for you?\n" +
                             "----------------------------------------------------");
 
         Scanner sc = new Scanner(System.in);
+
         while (true) {
             String userCommand = sc.nextLine();
+
             if (userCommand.equalsIgnoreCase("bye")) {
                 System.out.println("====================================================\n" +
                         "Goodbai. Hope to see you again soon! （ ● ___ ●.）" +
                         "\n====================================================");
                 break;
+            } else if (userCommand.equalsIgnoreCase("list")) {
+                for (int i = 0; i < items.length; i++) {
+                    if (items[i] != null) {
+                        System.out.printf("%d. %s\n", i + 1, items[i]);
+                    }
+                }
+                System.out.println("\n----------------------------------------------------");
             } else {
+                items[index] = userCommand;
+                index++;
+
                 System.out.println("----------------------------------------------------\n"
-                        + userCommand +
+                        + "added: " + userCommand +
                         "\n----------------------------------------------------");
             }
 
