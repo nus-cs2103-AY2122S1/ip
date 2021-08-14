00b1d720a96d43583ff5c8f66dca3e0dbaa6af03
Xiaoyunnn <xiaoyun.wu00@gmail.com>
2021-08-14 22:04:47 +0800

    Add Level 5

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -30,9 +30,11 @@ dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 diff ACTUAL.TXT EXPECTED-UNIX.TXT
 if [ $? -eq 0 ]
 then
-    echo "Test result: PASSED"
+    RED='\033[0;41m' #ANSI color code 31m for red text, 32m for green text
+    GREEN='\033[0;42m'
+    echo -e "Test result: ${GREEN}PASSED"
     exit 0
 else
-    echo "Test result: FAILED"
+    echo -e "Test result: ${RED}FAILED"
     exit 1
 fi
