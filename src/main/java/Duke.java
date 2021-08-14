1daad22ef24e6a07f7c3797f39d8f67a7e44f3e6
marcuspeh <hkmpeh@gmail.com>
2021-08-14 22:01:58 +0800

    Add greeting, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,79 @@
-public class Duke {
-    private void chat() {
+import java.util.Scanner;
 
+/**
+ * Main file for chatbot.
+ *
+ * @author marcuspeh
+ * @version 1.0
+ * @since 14 Aug 2021
+ */
+
+public class Duke {
+    private final static String EXIT = "bye";
+    private final static String LINEBREAK = "\t____________________________________________________________";
+
+    private Scanner sc;
+
+    /**
+     * Constructor for Duke.
+     */
+    Duke() {
+        sc = new Scanner(System.in);
     }
 
+    /**
+     * Start the chatbot and get it to chat with the user.
+     */
+    private void chat() {
+        GreetMessage();
+
+        String message;
+        while (true) {
+            message = sc.nextLine().toLowerCase();
+            if (message.equals(EXIT)) break;
+            EchoMessage(message);
+        }
+        ExitMessage();
+    }
+
+    /**
+     * Print out the greeting message used when the chat started.
+     */
+    private void GreetMessage() {
+        PrintMessage("Good day th're! I'm DUKE\n", "What can I doth f'r thee?");
+    }
+
+    /**
+     * Echos the message the user sends for level-1.
+     * @param s Message user sent.
+     */
+    private void EchoMessage(String s) {
+        PrintMessage(s);
+    }
+
+    /**
+     * Print out the exit message when chat ends.
+     */
+    private void ExitMessage() {
+        PrintMessage("Farewell! Desire to seeth thee again.");
+    }
+
+    /**
+     * Formats the sentences that will be printed out by the chatbot
+     * @param strings Arbitrary number of strings to be printed out
+     */
+    private void PrintMessage(String... strings) {
+        System.out.println(LINEBREAK);
+        for (String str: strings)
+            System.out.println("\t" + str);
+        System.out.println(LINEBREAK);
+    }
+
+    /**
+     * Main function to run the chatbot
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        duke.chat();
     }
 }
