9da982c6c232932f2e837af6afd50c791032339e
knotstoks <70320812+knotstoks@users.noreply.github.com>
2021-08-14 18:26:01 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,31 @@
 import java.util.Scanner;
 
 public class Duke {
+    private enum Prompt {
+
+    }
+
+    /*
+    Reads the input from the user and triggers the logic for the command.
+     */
+    private static void NextCommand() {
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        if (command.equals("bye")) {
+            EndDuke();
+        } else {
+            System.out.println(command);
+            NextCommand();
+        }
+    }
+
+    /*
+    Closes and exits Duke.
+     */
+    private static void EndDuke() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,6 +35,6 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke!\n" + "What can I do for you?");
 
-        System.out.println("Bye. Hope to see you again soon!");
+        NextCommand();
     }
 }
