c10e3a11811b703d70f6c111ad8e3f2f6dc7bc5e
Xiaoyunnn <xiaoyun.wu00@gmail.com>
2021-08-14 23:35:57 +0800

    Add javadoc for DukeException

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,11 +1,26 @@
+/**
+ * A subclass of Exception that deals with Duke-specific exceptions.
+ */
 public class DukeException extends Exception {
+    /**
+     * Message to be displayed.
+     */
     private final String message;
 
+    /**
+     * Constructor to instantiate DukeException.
+     *
+     * @param message Error message to be displayed.
+     */
     public DukeException(String message) {
         super(message);
         this.message = message;
     }
 
+    /**
+     * A method to return the string representation of this Exception.
+     * @return Error message specified.
+     */
     @Override
     public String toString() {
         return "OOPS!!! " + this.message;
