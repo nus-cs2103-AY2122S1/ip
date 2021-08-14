febf90eabb626f868f7c88630f8b3bfe6a6748b2
Xiaoyunnn <xiaoyun.wu00@gmail.com>
2021-08-14 19:22:33 +0800

    Add Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        List<Task> items = new ArrayList<>();
+
+        Task[] items = new Task[100];
+        int index = 0;
 
         String logo = " ______      ____      __  \n"
                      + "|   _  \\    /    \\    |  | \n"
@@ -21,47 +23,106 @@ public class Duke {
         while (true) {
             String userCommand = sc.nextLine();
 
-            if (userCommand.equalsIgnoreCase("bye")) {
+            // Solution below adapted from https://stackoverflow.com/questions/5455794/removing-whitespace-from-strings-in-java
+            if (userCommand.replaceAll("\\s+","").equalsIgnoreCase("bye")) {
                 System.out.println("====================================================\n" +
                         "Goodbai. Hope to see you again soon! （ ● ___ ●.）" +
                         "\n====================================================");
                 break;
-            } else if (userCommand.equalsIgnoreCase("list")) {
+            } else if (userCommand.replaceAll("\\s+","").equalsIgnoreCase("list")) {
                 System.out.println("----------------------------------------------------");
                 System.out.println("Here are the tasks in your list: ");
 
-                for (int i = 0; i <  items.size(); i++) {
-                    System.out.printf("%d. " + items.get(i) + "\n", i + 1);
+                for (int i = 0; i <  index; i++) {
+                    System.out.printf("%d. " + items[i] + "\n", i + 1);
                 }
 
                 System.out.println("----------------------------------------------------");
-            } else if (userCommand.length() > 1) {
+            } else {
                 String[] words = userCommand.split(" ");
 
-                if (words[0].equalsIgnoreCase("done")) {
-                    int idx = Integer.parseInt(words[1]) - 1;
-                    items.get(idx).markAsDone();
+                try {
+                    switch (words[0].toLowerCase()) {
+                        case "done":
+                            try {
+                                int idx = Integer.parseInt(words[1]) - 1;
+                                items[idx].markAsDone();
 
+                                System.out.println("----------------------------------------------------\n"
+                                        + "Nice! I've marked this task as done: ");
+                                System.out.println(items[idx]);
+                                System.out.println("----------------------------------------------------");
 
+                            } catch (NullPointerException e) {
+                                System.out.println("Uh oh! No such task exists.");
+                                System.out.println("----------------------------------------------------");
+                            }
+
+                            break;
+                        case "todo":
+                            Todo newTodo = new Todo(userCommand);
+                            items[index] = newTodo;
+                            index++;
+
+                            System.out.println("----------------------------------------------------\n"
+                                    + "Got it. I've added this task: \n" + newTodo);
+                            System.out.printf("\nNow you have %d tasks in the list", index);
+                            System.out.println("\n----------------------------------------------------");
+                            break;
+
+                        case "deadline":
+                            try {
+                                int byIndex = userCommand.indexOf("/by");
+                                String by = userCommand.substring(byIndex + 4);
+                                Deadline newDeadline = new Deadline(userCommand.substring(0, byIndex - 1), by);
+                                items[index] = newDeadline;
+                                index++;
+
+                                System.out.println("----------------------------------------------------\n"
+                                        + "Got it. I've added this task: \n" + newDeadline);
+                                System.out.printf("\nNow you have %d tasks in the list", index);
+                                System.out.println("\n----------------------------------------------------");
+                            } catch (StringIndexOutOfBoundsException e) {
+                                System.out.println("----------------------------------------------------\n"
+                                        + "Please specify the item name and/or deadline!" +
+                                        "\n----------------------------------------------------");
+                            }
+
+                            break;
+
+                        case "event":
+                            try {
+                                int atIndex = userCommand.indexOf("/at");
+                                String at = userCommand.substring(atIndex + 4);
+                                Event newEvent = new Event(userCommand.substring(0, atIndex - 1), at);
+                                items[index] = newEvent;
+                                index++;
+
+                                System.out.println("----------------------------------------------------\n"
+                                        + "Got it. I've added this task: \n" + newEvent);
+                                System.out.printf("\nNow you have %d tasks in the list", index);
+                                System.out.println("\n----------------------------------------------------");
+
+                            } catch (StringIndexOutOfBoundsException e) {
+                                System.out.println("----------------------------------------------------\n"
+                                        + "Please specify the item name and/or event date!" +
+                                        "\n----------------------------------------------------");
+                            }
+
+                            break;
+
+                        default:
+                            System.out.println("----------------------------------------------------\n"
+                                    + "No such command! D:" +
+                                    "\n----------------------------------------------------");
+                            break;
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
                     System.out.println("----------------------------------------------------\n"
-                            + "Nice! I've marked this task as done: ");
-
-                    System.out.println(items.get(idx));
-                    System.out.println("----------------------------------------------------");
-                } else {
-                    items.add(new Task(userCommand));
-
-                    System.out.println("----------------------------------------------------\n"
-                            + "added: " + userCommand +
+                            + "Invalid input! D:" +
                             "\n----------------------------------------------------");
                 }
             }
-
-
-
         }
-
-
-
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -3,11 +3,18 @@ import java.util.Objects;
 public class Task {
     protected String description;
     protected boolean isDone;
+//    protected char tag;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
+
+//    public Task(String description, char tag) {
+//        this.description = description;
+//        this.isDone = false;
+//        this.tag = tag;
+//    }
 
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
@@ -17,12 +24,8 @@ public class Task {
         isDone = true;
     }
 
-    public String getDescription() {
-        return this.description;
-    }
-
     @Override
     public String toString() {
-        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+        return "[" + this.getStatusIcon() + "] " + this.description;
     }
 }
