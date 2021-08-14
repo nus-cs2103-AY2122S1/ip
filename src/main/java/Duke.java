b9d9cd45de332179c39123b63626dbca5945ad68
XXJJXJ <yexinjian1506@gmail.com>
2021-08-14 13:00:41 +0800

    Complete Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+    /**
+     * This is the main point of interaction of user and Duke.
+     */
+    public void run() {
+        System.out.println("Hello I'm BugGenerator");
+        System.out.println("What can I do for you?");
+
+        Boolean end = false;
+        Scanner s = new Scanner(System.in);
+        while(!end) {
+            String input = s.next(); //or nextLine?
+            switch (input) {
+                case "bye":
+                    System.out.println("\t_______________________________");
+                    System.out.println("\tBye. Hope to see you again soon!");
+                    System.out.println("\t_______________________________");
+                    end = true;
+                    break;
+                default:
+                    System.out.println("\t_______________________________");
+                    System.out.printf("\t%s\n", input);
+                    System.out.println("\t_______________________________");
+            }
+        }
+        s.close();
     }
+
+    public static void main(String[] args) {
+        new Duke().run();
+    }
+
 }
