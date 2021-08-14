6ec00e5caff727edfa8c4c6de27d28f6bedbd96d
marcuspeh <hkmpeh@gmail.com>
2021-08-14 22:11:26 +0800

    update methods to camelCase notation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,44 +25,44 @@ public class Duke {
      * Start the chatbot and get it to chat with the user.
      */
     private void chat() {
-        GreetMessage();
+        greetMessage();
 
         String message;
         while (true) {
             message = sc.nextLine().toLowerCase();
             if (message.equals(EXIT)) break;
-            EchoMessage(message);
+            echoMessage(message);
         }
-        ExitMessage();
+        exitMessage();
     }
 
     /**
      * Print out the greeting message used when the chat started.
      */
-    private void GreetMessage() {
-        PrintMessage("Good day th're! I'm DUKE\n", "What can I doth f'r thee?");
+    private void greetMessage() {
+        printMessage("Good day th're! I'm DUKE\n", "What can I doth f'r thee?");
     }
 
     /**
      * Echos the message the user sends for level-1.
      * @param s Message user sent.
      */
-    private void EchoMessage(String s) {
-        PrintMessage(s);
+    private void echoMessage(String s) {
+        printMessage(s);
     }
 
     /**
      * Print out the exit message when chat ends.
      */
-    private void ExitMessage() {
-        PrintMessage("Farewell! Desire to seeth thee again.");
+    private void exitMessage() {
+        printMessage("Farewell! Desire to seeth thee again.");
     }
 
     /**
      * Formats the sentences that will be printed out by the chatbot
      * @param strings Arbitrary number of strings to be printed out
      */
-    private void PrintMessage(String... strings) {
+    private void printMessage(String... strings) {
         System.out.println(LINEBREAK);
         for (String str: strings)
             System.out.println("\t" + str);
