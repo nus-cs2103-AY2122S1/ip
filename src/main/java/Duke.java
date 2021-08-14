d917824831e4a1720b9829f0b4b25980e801416c
Wayne Tee <waynetee37@gmail.com>
2021-08-14 15:39:16 +0800

    Reformat code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,39 +4,39 @@ public class Duke {
 
     public static void main(String[] args) {
         greet();
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 print(bye());
                 return;
-            } else{
+            } else {
                 print(input);
             }
         }
     }
 
-    static void greet(){
-        print("Hello! I'm Duke","What can I do for you?");
+    static void greet() {
+        print("Hello! I'm Duke", "What can I do for you?");
     }
 
-    static String[] bye(){
+    static String[] bye() {
         return new String[]{"Bye. Hope to see you again soon!"};
     }
 
-    static void print(String ...lines){
+    static void print(String... lines) {
         printSeparator();
-        for(String line: lines)
+        for (String line : lines)
             println(line);
         printSeparator();
     }
 
-    static void printSeparator(){
+    static void printSeparator() {
         System.out.println("\t____________________________________");
     }
 
-    static void println(String out){
-        System.out.println("\t "+out);
+    static void println(String out) {
+        System.out.println("\t " + out);
     }
 
 }
