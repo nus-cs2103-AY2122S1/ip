86e774956d2834d23dc9e58d318ac4322f3afae8
lwlshawn <37958530+lwlshawn@users.noreply.github.com>
2021-08-14 10:46:41 +0800

    Implemented level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,14 +2,29 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] store = new String[100];
+        int index = 0;
+
         System.out.println("Hello! I'm Duke");
         System.out.println("What can i do for you?");
 
         Scanner sc = new Scanner(System.in);
         String in = sc.nextLine();
         while(!in.equals("bye")) {
-            System.out.println(in);
-            in = sc.nextLine();
+            switch (in) {
+                case "list":
+                    for (int i = 0; i < index; i++) {
+                        System.out.println((i + 1) + ". " + store[i]);
+                    }
+                    in = sc.nextLine();
+                    break;
+                default:
+                    System.out.println("added: " + in);
+                    store[index] = in;
+                    index++;
+                    in = sc.nextLine();
+                    break;
+            }
         }
 
         System.out.println("Bye. Hope to see you again soon!");
