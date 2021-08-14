b2c50c04da37e7cc0d1d2ebf9cd211c6c09d1613
skcheongbrian <skcheongbrian@gmail.com>
2021-08-14 14:35:38 +0800

    added ability to add items and list them out. As well as fixed input bug

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,33 +1,83 @@
 import java.util.Scanner;
 
+class Node {
+    String text;
+    Node next;
+
+    public Node(String text) {
+        this.text = text;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node n) {
+        this.next = n;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+}
+
 public class Duke {
+    private static int taskCount = 0;
+    private static Node dummyHead = new Node("");
+    private static Node lastNode = dummyHead;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String logo = " ____        _        \n"
-                   + "|  _ \\ _   _| | _____ \n"
-                   + "| | | | | | | |/ / _ \\\n"
-                   + "| |_| | |_| |   <  __/\n"
-                   + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "┏━━┓╋╋╋┏━━┓╋╋╋┏┓\n" +
+                      "┃┏┓┃╋╋╋┃┏┓┃╋╋┏┛┗┓\n" +
+                      "┃┗┛┗┳━┳┫┗┛┗┳━┻┓┏┛\n" +
+                      "┃┏━┓┃┏╋┫┏━┓┃┏┓┃┃\n" +
+                      "┃┗━┛┃┃┃┃┗━┛┃┗┛┃┗┓\n" +
+                      "┗━━━┻┛┗┻━━━┻━━┻━┛";
+
         System.out.println("Hello from\n" + logo);
         lineSeparator();
-        System.out.println("I'm Duke");
+        System.out.println("I'm BriBot");
         System.out.println("What can I do for you?");
         lineSeparator();
         while (true) {
-            String userInput = sc.next();
+            String userInput = sc.nextLine();
             if (userInput.equals("bye")) {
-                lineSeparator();
-                System.out.println("Bye. Hope to see you soon!");
-                lineSeparator();
+                sayGoodBye();
                 break;
+            } else if (userInput.equals("list")) {
+                printList();
             } else {
-                lineSeparator();
-                System.out.println(userInput);
-                lineSeparator();
+                addInput(userInput);
             }
 
         }
+    }
+
+    private static void addInput(String userInput) {
+        lineSeparator();
+        System.out.println("added: " + userInput);
+        lineSeparator();
+        taskCount++;
+        lastNode.setNext(new Node(userInput));
+        lastNode = lastNode.getNext();
+    }
+
+    private static void printList() {
+        Node pointer = dummyHead;
+        lineSeparator();
+        for (int i = 0; i < taskCount; i++) {
+            pointer = pointer.getNext();
+            System.out.println((i+1) + ". " + pointer.toString());
+        }
+        lineSeparator();
+    }
+
+    private static void sayGoodBye() {
+        lineSeparator();
+        System.out.println("Bye. Hope to see you soon!");
+        lineSeparator();
     }
 
     private static void lineSeparator() {
