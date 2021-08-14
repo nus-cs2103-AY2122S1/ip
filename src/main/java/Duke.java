daf1e19387f10563fd4e9ec9546ca9efe1424dcf
wyrchris <wongyunruichris@gmail.com>
2021-08-14 14:16:34 +0800

    Add level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,12 +13,25 @@ public class Duke {
 
         DukeList dukeList = new DukeList();
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        String input = sc.nextLine();
 
         while (!input.equals("bye")) {
-            String reply = input.equals("list") ? dukeList.displayList() : dukeList.addToList(input);
+            String[] inputArray = input.split(" ");
+            String reply = "";
+            switch (inputArray[0]) {
+                case "list":
+                    reply = dukeList.displayTask();
+                    break;
+                case "done":
+                    reply = dukeList.markTask(Integer.parseInt(inputArray[1]));
+                    break;
+                default:
+                    reply = dukeList.addTask(input);
+                    break;
+            }
+
             System.out.println(reply);
-            input = sc.next();
+            input = sc.nextLine();
         }
 
         System.out.println("\tBye. Hope to see you again soon!");
--- src/main/java/DukeList.java
+++ src/main/java/DukeList.java
@@ -1,21 +1,25 @@
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class DukeList {
-    private LinkedList<String> list = new LinkedList<>();
+    private ArrayList<Task> list = new ArrayList<>();
 
-    public String addToList(String input) {
-        this.list.push(input);
+    public String addTask(String input) {
+        this.list.add(new Task(input));
         return "\tadded: " + input + "\n";
     }
 
-    public String displayList() {
-        StringBuilder output = new StringBuilder();
+    public String displayTask() {
+        StringBuilder output = new StringBuilder("\tHere are the tasks in your list:\n");
         int i = 1;
-        for (String s: list) {
-            String temp = "\t" + i + ". " + s + "\n";
+        for (Task task: list) {
+            String temp = "\t" + i + ". " + task.toString() + "\n";
             output.append(temp);
             i++;
         }
         return output.toString();
+    }
+
+    public String markTask(int index) {
+        return "\tNice! I've marked this task as done:\n\t\t" + list.get(index - 1).markDone();
     }
 }
