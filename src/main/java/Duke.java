0743bedc8f25f110fbf8f7e42bf51281f36c868e
xlzior <wenjun.lye@gmail.com>
2021-08-14 17:47:55 +0800

    Level 2: Implement add and list
    * add
    * list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String ENDING_COMMAND = "bye";
 
+    private static List<String> taskList = new ArrayList<>();
+
     private static void say(String message) {
-        System.out.println("Iris: " + message);
+        System.out.println(String.format("Iris: %s", message));
+    }
+
+    private static void say(String message, boolean isFirst) {
+        String name = isFirst ? "Iris:": "     ";
+        System.out.println(String.format("%s %s", name, message));
     }
 
     private static String prompt() {
@@ -17,11 +26,28 @@ public class Duke {
         say(message);
     }
 
+    private static void add(String item) {
+        say(String.format("added: %s", item));
+        taskList.add(item);
+    }
+
+    private static void listTasks() {
+        for (int i = 0; i < taskList.size(); i++) {
+            say(String.format("%d. %s", i + 1, taskList.get(i)), i == 0);
+        }
+    }
+
     public static void main(String[] args) {
         say("Hello! I'm Iris. What can I do for you?");
         String command = prompt();
         while (!command.equals(ENDING_COMMAND)) {
-            echo(command);
+            switch(command) {
+                case "list":
+                    listTasks();
+                    break;
+                default:
+                    add(command);
+            }
             command = prompt();
         }
 
