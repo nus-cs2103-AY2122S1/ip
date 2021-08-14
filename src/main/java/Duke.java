1889abec10dd1b1c4cdbe50dc0b774822829a1b2
Shruthi0310 <e0564271@u.nus.edu>
2021-08-14 23:22:07 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,43 @@
 import java.util.Scanner;
 
 public class Duke {
-    //level 2
-    public static String[] taskList = new String[100];
-    public static int counter = 0;
 
-    public static String getUserInput(Scanner scanner){
-        return scanner.nextLine();
+    private static Task[] taskList = new Task[100];
+    private static int counter = 0;
+    public Duke(){}
+
+    public static String getFirstWord(Scanner scanner){
+        return scanner.next();
     }
 
     public static void dukeAction(Scanner scanner){
-        String inp = getUserInput(scanner);
+        String inp = getFirstWord(scanner);
+
         while(!inp.equals("bye")) {
             switch (inp) {
                 case "list":
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < taskList.length; i++) {
                         if (taskList[i] == null) break;
-                        System.out.printf("%s" + "." + taskList[i] + "%n", i + 1);
+                        System.out.printf("\t%s." + taskList[i].toString() + "%n", i + 1);
                     }
-                    inp = getUserInput(scanner);
+                    inp = getFirstWord(scanner);
                     break;
+
+                case "done":
+                    int doneNumber = Integer.parseInt(scanner.next()) - 1;
+                    taskList[doneNumber].setDone();
+                    System.out.println("Nice! I've marked this task as done:\n"
+                                        + "%t" + taskList[doneNumber].toString());
+                    inp = getFirstWord(scanner);
+                    break;
+
                 default:
-                    taskList[counter++] = inp;
+                    inp += scanner.nextLine();
+                    Task task = new Task(inp);
+                    taskList[counter++] = task;
                     System.out.println("added: " + inp);
-                    inp = getUserInput(scanner);
+                    inp = getFirstWord(scanner);
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
