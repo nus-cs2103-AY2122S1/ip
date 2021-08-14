a4ff68b0b4fbd96b7c7c74dd59de4d448d07a886
Xiaoyunnn <xiaoyun.wu00@gmail.com>
2021-08-14 17:20:44 +0800

    Add Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String[] items = new String[100];
-        int index = 0;
+        List<Task> items = new ArrayList<>();
 
         String logo = " ______      ____      __  \n"
                      + "|   _  \\    /    \\    |  | \n"
@@ -26,20 +27,37 @@ public class Duke {
                         "\n====================================================");
                 break;
             } else if (userCommand.equalsIgnoreCase("list")) {
-                for (int i = 0; i < items.length; i++) {
-                    if (items[i] != null) {
-                        System.out.printf("%d. %s\n", i + 1, items[i]);
-                    }
-                }
-                System.out.println("\n----------------------------------------------------");
-            } else {
-                items[index] = userCommand;
-                index++;
+                System.out.println("----------------------------------------------------");
+                System.out.println("Here are the tasks in your list: ");
 
-                System.out.println("----------------------------------------------------\n"
-                        + "added: " + userCommand +
-                        "\n----------------------------------------------------");
+                for (int i = 0; i <  items.size(); i++) {
+                    System.out.printf("%d. " + items.get(i) + "\n", i + 1);
+                }
+
+                System.out.println("----------------------------------------------------");
+            } else if (userCommand.length() > 1) {
+                String[] words = userCommand.split(" ");
+
+                if (words[0].equalsIgnoreCase("done")) {
+                    int idx = Integer.parseInt(words[1]) - 1;
+                    items.get(idx).markAsDone();
+
+
+                    System.out.println("----------------------------------------------------\n"
+                            + "Nice! I've marked this task as done: ");
+
+                    System.out.println(items.get(idx));
+                    System.out.println("----------------------------------------------------");
+                } else {
+                    items.add(new Task(userCommand));
+
+                    System.out.println("----------------------------------------------------\n"
+                            + "added: " + userCommand +
+                            "\n----------------------------------------------------");
+                }
             }
+
+
 
         }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+import java.util.Objects;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+    }
+}
