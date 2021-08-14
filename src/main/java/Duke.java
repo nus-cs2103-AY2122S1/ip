2031d517c44ea03bb1d45e7f136f13ebcecd2f27
Chrus Chong <chrus1000@gmail.com>
2021-08-14 12:11:36 +0800

    Complete basic level 1 functionalities (to add aesthetic features later on) and omit class files in .gitignore

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What can I do for you today?");
+
+        while (true) {
+            String cmd = sc.nextLine();
+            if (cmd.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(cmd);
+
+        }
+
     }
 }
