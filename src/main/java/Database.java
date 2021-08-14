154ca81261f5cbe37359c93468d74d0042dc8061
trxe <peixianwong@gmail.com>
2021-08-14 20:56:06 +0800

    A-TestUiTesting

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -22,7 +22,7 @@ public class Database {
         if (db.size() == 0) return " You have no tasks!";
         for (int i = 1; i <= db.size(); i++) {
             sb.append("\n ");
-            sb.append(i + ". " + db.get(i-1));
+            sb.append(i + "." + db.get(i-1));
         }
         sb.deleteCharAt(0);
         return sb.toString();
