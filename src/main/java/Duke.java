eb3435a9ebab59765c36cd2f3257984bef234025
darrenhoon <darrenhoon.ad@gmail.com>
2021-08-14 15:31:16 +0800

    Level-1 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     private final String welcomeMessage = "Hello! I'm Duke\n" + "What can I do for you?";
-    private final String borderLine = "____________________________________________________________";
+    private final String borderLine = "\n____________________________________________________________\n";
     private final String goodbyeMessage = "Bye. Hope to see you again soon!";
 
     public static void main(String[] args) {
@@ -10,7 +10,32 @@ public class Duke {
     }
 
     public void runProgram() {
-        System.out.println(borderLine + welcomeMessage + borderLine);
+
+        //displays the welcome message
+        this.displayText(welcomeMessage);
+
+        //initialises the scanner
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = sc.nextLine().trim();
+            if (input.equals("bye")) {
+                break;
+            }
+
+            //echos the text input by the user
+            this.displayText(input);
+        }
+
+        //terminates scanner
+        sc.close();
+
+        //displays goodbye message
+        this.displayText(goodbyeMessage);
     }
 
+    public void displayText(String input) {
+        System.out.println(borderLine + input + borderLine);
+    }
 }
