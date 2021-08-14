032e245ede3bacd94a979819553b243c216055e9
KT27Learn <keithtan20@gmail.com>
2021-08-15 01:21:49 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String mode = "greeting";
+        String horizontalLine = "____________________________________________________________";
+        String greeting = horizontalLine
+                + "\nHello! I'm Duke"
+                + "\n What can I do for you?"
+                + "\n" + horizontalLine;
+        Scanner commandScanner = new Scanner(System.in);
+        System.out.println(greeting);
+        while (!mode.equals("bye")) {
+            String userNextCommand = commandScanner.nextLine();
+            if (userNextCommand.equals("bye")) {
+
+                mode = "bye";
+                String byeString = horizontalLine
+                        + "\nBye. Hope to see you again soon!\n"
+                        + horizontalLine;
+                System.out.println(byeString);
+
+            } else {
+
+                mode = "echo";
+                String echoString = horizontalLine
+                        + "\n"
+                        + userNextCommand
+                        + "\n"
+                        + horizontalLine;
+                System.out.println(echoString);
+
+            }
+
+        }
     }
 }
