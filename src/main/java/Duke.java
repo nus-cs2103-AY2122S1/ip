84d60a089a8018eb3ebb4eda88093a5c568523f0
Ruofan1023 <e0563986@u.nus.edu>
2021-08-14 14:32:28 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        Scanner sc = new Scanner(System.in);
+        duke.greet();
+        String command = sc.nextLine();
+        while(!command.equals("bye")) {
+            duke.echo(command);
+            command = sc.nextLine();
+        }
+        sc.close();
+        duke.bye();
+    }
+
+    /**
+     * Greet the user.
+     */
+    public void greet() {
+        String greet = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(greet);
+    }
+
+    /**
+     * Echo the command entered.
+     *
+     * @param command a command said by the user
+     */
+    public void echo(String command) {
+        System.out.println(command);
+    }
+
+    /**
+     * exit behavior.
+     */
+    public void bye() {
+        String byeCommand = "Bye. Hope to see you again soon!";
+        System.out.println(byeCommand);
     }
 }
