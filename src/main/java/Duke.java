f6de9e8d46fb68765dddc9c3bfe052568d8d6d26
KishendranVendarKon <kishen.vendarkon@gmail.com>
2021-08-14 14:16:38 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String dottedLines = "----------------------------------------------------------------------------------------";
+        System.out.println(dottedLines);
+        System.out.println("Hello I'm LOTTERY-A");
+        System.out.println("Also known as the List Of Tasks That Eventually Require Your Attention");
+        System.out.println("What can I do for you?");
+        System.out.println(dottedLines);
+
+        Scanner keyboard = new Scanner(System.in);
+        while(true) {
+            String userInput = keyboard.next();
+            if(userInput.equals("bye")) {
+                System.out.println(dottedLines);
+                System.out.println("Bye. Don't forget, these tasks will still require your attention when you return!");
+                System.out.println(dottedLines);
+                break;
+            } else {
+                System.out.println(dottedLines);
+                System.out.println(userInput);
+                System.out.println(dottedLines);
+            }
+        }
     }
 }
