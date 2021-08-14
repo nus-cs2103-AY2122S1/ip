437674413cf9148030ac8f1f8600e5e497e0a448
qinguorui2001 <qinguorui666@gmail.com>
2021-08-14 16:56:39 +0800

    Level 1, Change output format and add annotations

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,24 +8,51 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+
+        //Start Prompt.
+        System.out.println(pattern("Hello! I'm Duke\n   What can I do for you?"));
+
+        //User input.
         Scanner sc = new Scanner(System.in);
         boolean flag = true;
+
+        //Continue the loop until user says bye.
         while (flag) {
-            String response = sc.next();
+            String response = sc.nextLine();
             flag = echo(response);
         }
     }
 
+    /**
+     * Returns a boolean to react to a response.
+     *
+     * @param response User inputs.
+     * @return continue or not.
+     */
     public static boolean echo(String response) {
         switch (response) {
             case "bye":
                 System.out.println("Bye, see you soon.");
                 return false;
             default:
-                System.out.println(response);
+                System.out.println(pattern(response));
                 return true;
         }
+    }
+
+    /**
+     * Returns formatted string output.
+     *
+     * @param r The input string.
+     * @return formatted string.
+     */
+    public static String pattern(String r) {
+        int lens = r.length();
+        StringBuffer result = new StringBuffer();
+        StringBuffer curr = new StringBuffer();
+        String empty = "   ";
+        curr.append("*".repeat(50));
+        result.append(empty + curr + "\n" + empty + r + "\n" + empty + curr + "\n");
+        return result.toString();
     }
 }
