c078f154eded1452d1c252c4389e8b096d484aa5
Marcus Choo <marcuschoo@outlook.com>
2021-08-14 23:54:42 +0800

    Add greet, echo, exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LINE = "____________________________________________________________\n";
+    private static final String WELCOME = "Hello, I'm Duke\nWhat can I do for you?\n";
+    private static final String BYE = "Bye. Hope to see you again soon!\n";
+
+    /**
+     * Echoes the user input by printing user input between two lines.
+     * @param input String that user inputs.
+     */
+    public static void echo(String input) {
+        System.out.println(LINE + input + "\n" + LINE);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(LINE + WELCOME + LINE);
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String s = sc.nextLine();
+            if (s.equals("bye")) {
+                System.out.println(LINE + BYE + LINE);
+                break;
+            } else {
+                echo(s);
+            }
+        }
     }
 }
