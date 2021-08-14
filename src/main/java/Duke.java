f37fd3bb6c970cb03eee26298178bd1c2bb41a29
xlzior <wenjun.lye@gmail.com>
2021-08-14 17:28:06 +0800

    Level 1: Implement skeletal version
    * greet
    * echo
    * exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String ENDING_COMMAND = "bye";
+
+    private static void say(String message) {
+        System.out.println("Iris: " + message);
+    }
+
+    private static String prompt() {
+        System.out.print("me: ");
+        return scanner.nextLine();
+    }
+
+    private static void echo(String message) {
+        say(message);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        say("Hello! I'm Iris. What can I do for you?");
+        String command = prompt();
+        while (!command.equals(ENDING_COMMAND)) {
+            echo(command);
+            command = prompt();
+        }
+
+        say("Bye. Hope to see you again soon!");
     }
 }
