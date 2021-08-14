36e37ccf3dc6be9f14477452f59b73a56f9eef1b
jackgugz <jackgugz@users.noreply.github.com>
2021-08-14 20:27:12 +0800

    echo added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________\n"
+        + "Heyllo! Jack here\n"
+        + "What can I do for you?\n"
+        + "____________________________________________________________\n");
+
+        boolean bye = false;
+        Scanner myObj = new Scanner(System.in);
+        System.out.println();
+
+        while (!bye) {
+            String temp = myObj.nextLine();
+            if (temp.equals("bye")) {
+                System.out.println("____________________________________________________________\n"
+                        + "Bye bye. Love you\n"
+                        + "____________________________________________________________\n");
+                bye = true;
+            } else {
+                System.out.println("____________________________________________________________\n"
+                        + temp
+                        + "\n"
+                        + "____________________________________________________________\n");
+            }
+
+        }
     }
 }
