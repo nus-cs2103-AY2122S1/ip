a7e9f4eaac887d569c4ddc1c2cff53556e787b84
yongxiangng <yongxiangng@gmail.com>
2021-08-14 21:27:02 +0800

    Handle errors for incorrect user inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,17 +9,22 @@ public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input;
-        Duke.greet();
+        Duke.start();
         while (!(input = scanner.nextLine().trim()).equals("bye")) {
-            handleUserInput(input);
+            try {
+                handleUserInput(input);
+            } catch (DukeException e) {
+                Duke.print(e.getMessage());
+            }
         }
         Duke.exit();
+        scanner.close();
     }
 
     /**
      * Handles user input conditionally based on the command.
      */
-    private static void handleUserInput(String input) {
+    private static void handleUserInput(String input) throws DukeException {
         String[] userInput = input.split(" ", 2);
         String command = userInput[0];
         switch(command) {
@@ -27,29 +32,26 @@ public class Duke {
                 Duke.printTaskList();
                 break;
             case "done":
-                Duke.setTaskDone(Integer.parseInt(input.split(" ")[1]));
+                Duke.setTaskDone(userInput);
                 break;
             case "todo":
-                String arg = userInput[1];
-                Duke.addTask(new Todo(arg));
+                Duke.addTodo(userInput);
                 break;
             case "deadline":
-                String deadlineDescription = userInput[1].split(" /by ")[0];
-                String by = userInput[1].split(" /by ")[1];
-                Duke.addTask(new Deadline(deadlineDescription, by));
+                Duke.addDeadline(userInput);
                 break;
             case "event":
-                String eventDescription = userInput[1].split(" /at ")[0];
-                String at = userInput[1].split(" /at ")[1];
-                Duke.addTask(new Event(eventDescription, at));
+                Duke.addEvent(userInput);
                 break;
+            default:
+                throw new DukeException("I'm sorry, but I don't know what that means :-(");
         }
     }
 
     /**
      * Message to be printed by Duke when Duke is first started.
      */
-    private static void greet() {
+    private static void start() {
         Duke.print("Hello! I'm Duke\nWhat can I do for you?");
     }
 
@@ -82,12 +84,62 @@ public class Duke {
     }
 
     /**
-     * Set i-th task to be done and prints confirmation message.
+     * Adds a todo task to the list of tasks.
+     *
+     * @param userInput given by user.
      */
-    private static void setTaskDone(int i) {
-        Task task = taskList.get(i - 1);
-        task.setDone();
-        print(String.format("Nice! I've marked this task as done:\n  %s", task.toString()));
+    public static void addTodo(String[] userInput) throws DukeException {
+        try {
+            Duke.addTask(new Todo(userInput[1]));
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("Todo description cannot be empty");
+        }
+    }
+
+    /**
+     * Adds a deadline task to the list of tasks.
+     *
+     * @param userInput given by user.
+     */
+    public static void addDeadline(String[] userInput) throws DukeException {
+        try {
+            String deadlineDescription = userInput[1].split(" /by ")[0];
+            String by = userInput[1].split(" /by ")[1];
+            Duke.addTask(new Deadline(deadlineDescription, by));
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("Deadline description and time by cannot be empty");
+        }
+    }
+
+    /**
+     * Adds a event task to the list of tasks.
+     *
+     * @param userInput given by user.
+     */
+    public static void addEvent(String[] userInput) throws DukeException {
+        try {
+            String eventDescription = userInput[1].split(" /at ")[0];
+            String by = userInput[1].split(" /at ")[1];
+            Duke.addTask(new Event(eventDescription, by));
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("Event description and time at cannot be empty");
+        }
+    }
+
+    /**
+     * Set i-th task to be done and prints confirmation message.
+     *
+     * @param userInput given by user.
+     */
+    private static void setTaskDone(String[] userInput) throws DukeException {
+        try {
+            int i = Integer.parseInt(userInput[1]);
+            Task task = taskList.get(i - 1);
+            task.setDone();
+            print(String.format("Nice! I've marked this task as done:\n  %s", task.toString()));
+        } catch (IndexOutOfBoundsException | NumberFormatException e) {
+            throw new DukeException("Please give a valid number!");
+        }
     }
 
     /**
