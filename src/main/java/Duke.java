3ca5cadff661b23e5abfa68efbc4fba3fa997805
XXJJXJ <yexinjian1506@gmail.com>
2021-08-14 14:07:08 +0800

    Complete Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,55 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
-    private String[] tasks = new String[100];
-    private int count = 0;
+    private final ArrayList<Task> tasks = new ArrayList<>();
+
+    /**
+     * This method marks the task at the specified index and prints out a response
+     * to the "done" command.
+     *
+     * @param indexToMark  An int that represents the task to be marked
+     */
+    private void doneTask(int indexToMark) {
+        tasks.get(indexToMark).markAsDone();
+        System.out.println("\t_______________________________");
+        System.out.println("\tNice! I've marked this task as done:");
+        System.out.printf("\t%s\n", tasks.get(indexToMark));
+        System.out.println("\t_______________________________");
+    }
+
+    /**
+     * This method list out all the task so far upon "list" command.
+     */
+    private void listOutTasks() {
+        System.out.println("\t_______________________________");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.printf("\t%d. %s\n", i+1, tasks.get(i));
+        }
+        System.out.println("\t_______________________________");
+    }
+
+    /**
+     * This method prints the bye message upon "bye" command
+     */
+    private void endDuke() {
+        System.out.println("\t_______________________________");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t_______________________________");
+    }
+
+    /**
+     * This method registers and add the tasks to the list.
+     *
+     * @param description  A String that describes the task to be added.
+     */
+    private void addTasks(String description) {
+        tasks.add(new Task(description));
+        System.out.println("\t_______________________________");
+        System.out.printf("\tadded: %s\n", description);
+        System.out.println("\t_______________________________");
+    }
 
     /**
      * This is the main point of interaction of user and Duke.
@@ -11,32 +57,28 @@ public class Duke {
     public void run() {
         System.out.println("Hello I'm BugGenerator");
         System.out.println("What can I do for you?");
-
         Boolean end = false;
         Scanner s = new Scanner(System.in);
+
         while(!end) {
-            String input = s.nextLine();
+            //First word is command
+            String input = s.next();
+            //This chunk might have to change depending on the types of command
             switch (input) {
-                case "":
+                case "done":
+                    int indexToMark = s.nextInt() - 1;
+                    this.doneTask(indexToMark);
                     break;
                 case "list":
-                    System.out.println("\t_______________________________");
-                    for (int i = 0; i < count; i++) {
-                        System.out.printf("\t%d. %s\n", i+1, tasks[i]);
-                    }
-                    System.out.println("\t_______________________________");
+                    this.listOutTasks();
                     break;
                 case "bye":
-                    System.out.println("\t_______________________________");
-                    System.out.println("\tBye. Hope to see you again soon!");
-                    System.out.println("\t_______________________________");
+                    this.endDuke();
                     end = true;
                     break;
                 default:
-                    tasks[count++] = input;
-                    System.out.println("\t_______________________________");
-                    System.out.printf("\tadded: %s\n", input);
-                    System.out.println("\t_______________________________");
+                    String fullLine = input + s.nextLine();
+                    this.addTasks(fullLine);
             }
         }
         s.close();
