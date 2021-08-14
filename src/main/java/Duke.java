503e992e5455156b6e4d8df90be9eb2b285e4536
knotstoks <70320812+knotstoks@users.noreply.github.com>
2021-08-14 18:03:00 +0800

    Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke!\n" + "What can I do for you?");
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
