f02155d61c1f02638f59d8212e6f5486153c5045
darrenhoon <darrenhoon.ad@gmail.com>
2021-08-14 15:56:46 +0800

    Level-2 Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,14 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Duke {
     private final String welcomeMessage = "Hello! I'm Duke\n" + "What can I do for you?";
     private final String borderLine = "\n____________________________________________________________\n";
     private final String goodbyeMessage = "Bye. Hope to see you again soon!";
+
+    private List<String> inputList = new ArrayList<String>();
 
     public static void main(String[] args) {
         new Duke().runProgram();
@@ -22,10 +27,15 @@ public class Duke {
             input = sc.nextLine().trim();
             if (input.equals("bye")) {
                 break;
-            }
+            } else if (input.equals("list")) {
+                this.displayListItems();
+            } else {
+                //echos the text input by the user
+                this.displayText(input);
 
-            //echos the text input by the user
-            this.displayText(input);
+                //adds item input by the user into the inputList
+                this.addItem(input);
+            }
         }
 
         //terminates scanner
@@ -35,7 +45,33 @@ public class Duke {
         this.displayText(goodbyeMessage);
     }
 
+    /**
+     * Displays the message input given by the user with horizontal borderlines on both the top and bottom.
+     * @param input String message to be displayed to user
+     */
     public void displayText(String input) {
         System.out.println(borderLine + input + borderLine);
+    }
+
+    /**
+     *
+     */
+    public void displayListItems() {
+        System.out.println(borderLine);
+
+        for (int i = 0; i < this.inputList.size(); i++) {
+            String inputMessage = String.format("%d. %s", i+1, this.inputList.get(i));
+            System.out.println(inputMessage);
+        }
+
+        System.out.println(borderLine);
+    }
+
+    /**
+     *
+     * @param input String message input by the user to be kept in the inputList
+     */
+    public void addItem(String input) {
+        this.inputList.add(input);
     }
 }
