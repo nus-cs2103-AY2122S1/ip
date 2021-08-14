dbb8d0783a3481f57177ed6f8918b7da906b9c74
VimuthM <vimuthmendis@gmail.com>
2021-08-14 15:37:06 +0800

    Add greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String wrapOutput(String s) {
+        return "\n\t@Herb:~$ " + s + "\n";
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcomeMessage = "\n\tHi! I'm Herbert, you can call me Herb  ٩(˘◡˘)۶\n"
+                + "\tHow can I help you?\n\n"
+                + "\tSay `bye` to end this chat.\n";
+        String endMessage = "\n\tSad to see you go :(\n\t...shutting down...";
+
+        System.out.println(welcomeMessage);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("--> ");
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(endMessage);
+                break;
+            } else {
+                System.out.println(wrapOutput(input));
+            }
+        }
     }
 }
