6abaf7416f97ebf983f15baa12cecc56caef5a14
Yukun99 <yukunxu123@gmail.com>
2021-08-14 21:00:55 +0800

    HelpBot: Add simple reply and exit

--- src/main/java/HelpBot.java
+++ src/main/java/HelpBot.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class HelpBot {
-	// Name of the HelpBot.
-	private final String NAME;
+	// Static fields
+	private static final String EXIT = "bye";
+
+	// Constants
+	private final String name;
+	private final Scanner scanner = new Scanner(System.in);
 
 	/**
 	 * Constructor for a HelpBot instance.
@@ -8,17 +14,50 @@ public class HelpBot {
 	 * @param name Name of the bot to be created.
 	 */
 	public HelpBot(String name) {
-		this.NAME = name;
-		this.start();
+		this.name = name;
+		this.onEnable();
 	}
 
 	/**
-	 * Starts the operation of the bot.
+	 * Method called when bot is instantiated.
 	 */
-	private void start() {
+	private void onEnable() {
+		String message = "Hello! I'm \n" + name
+				+ "\nHow may I serve your incredibly needy highness today?";
+		reply(message);
+		listen();
+	}
+
+	/**
+	 * Method to listen for user inputs.
+	 */
+	private void listen() {
+		while (scanner.hasNext()) {
+			// Current message sent by the user
+			String current = scanner.nextLine();
+			if (current.equals(EXIT)) {
+				break;
+			}
+			reply(current);
+		}
+		onDisable();
+	}
+
+	/**
+	 * Method called when player issues exit command on bot.
+	 */
+	private void onDisable() {
+		reply("Good riddance...");
+	}
+
+	/**
+	 * Method to print reply message correctly.
+	 *
+	 * @param message Message to be formatted.
+	 */
+	private void reply(String message) {
 		System.out.println("____________________________________________________________");
-		System.out.println("Hello! I'm \n" + NAME);
-		System.out.println("What can I do for you?");
+		System.out.println(message);
 		System.out.println("____________________________________________________________");
 	}
 }
