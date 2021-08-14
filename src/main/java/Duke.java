0872c9681a072a99eac8cf1b8a317045b90ffccb
itsyme <e0524723@u.nus.edu>
2021-08-14 11:45:15 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Duke {
     public static void main(String[] args) {
+        List<String> savedTasks = new ArrayList<>();
         Scanner scn = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,8 +19,15 @@ public class Duke {
                 System.out.println("Bye! Hope to see you again soon!");
                 break;
             }
+            else if (userInput.toLowerCase().equals("list")) {
+                for (int i=0; i< savedTasks.size(); i++) {
+                    int index = i+1;
+                    System.out.println(index + ". " + savedTasks.get(i));
+                }
+            }
             else {
-                System.out.println(userInput);
+                System.out.println("added: " + userInput);
+                savedTasks.add(userInput);
             }
         }
     }
