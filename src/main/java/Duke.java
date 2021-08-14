dba8b7ceedc69b1ecf167cc66cdb4fe6c1abbd9f
Joel-Sung <joelsung123@gmail.com>
2021-08-14 17:39:15 +0800

    Add greeting, echo, exit features to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String line = "----------------------------------------------";
+        System.out.println(
+                line + "\n\n" +
+                "\tHello! I'm Duke\n\n" +
+                "\tWhat can I do for you?\n\n" +
+                line + "\n"
+        );
+
+        Scanner scanner = new Scanner(System.in);
+        try {
+            String text;
+            boolean leave = false;
+            while (!leave) {
+                text = scanner.nextLine();
+                if (text.equals("bye")) {
+                    text = "Bye. Hope to see you again soon!";
+                    leave = true;
+                }
+                System.out.println(
+                        "\n" + line + "\n\n" +
+                        "\t" + text + "\n\n" +
+                        line + "\n"
+                );
+            }
+        } catch(IllegalStateException | NoSuchElementException e) {
+            // System.in has been closed
+            System.out.println("System.in was closed; exiting");
+        }
     }
 }
