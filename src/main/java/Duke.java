0a2ed823987604a3215253563c04dbd8b65a224a
En Rong <53928333+chownces@users.noreply.github.com>
2021-08-14 18:07:12 +0800

    Complete level 1. Added Greet, Echo and Exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        // Print welcome text
+        String line = "--------------------------------\n";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(line + logo + "\nHello! I'm Duke :)\nWhat can I do for you?\n" + line);
+
+        // Echos input back to user. Program exits when user inputs "bye"
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line + "Bye. Hope to see you again soon!\n" + line);
+                System.exit(0);
+            } else {
+                System.out.println(line + input + "\n" + line);
+            }
+        }
     }
 }
