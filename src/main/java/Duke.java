49ce501ce220b08eadff2f5796667842ce0c5fe2
KishendranVendarKon <kishen.vendarkon@gmail.com>
2021-08-14 14:57:02 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    static List<String> toDoList = new ArrayList<>();
+
     public static void main(String[] args) {
         String dottedLines = "----------------------------------------------------------------------------------------";
         System.out.println(dottedLines);
@@ -11,15 +15,20 @@ public class Duke {
 
         Scanner keyboard = new Scanner(System.in);
         while(true) {
-            String userInput = keyboard.next();
+            String userInput = keyboard.nextLine();
             if(userInput.equals("bye")) {
                 System.out.println(dottedLines);
                 System.out.println("Bye. Don't forget, these tasks will still require your attention when you return!");
                 System.out.println(dottedLines);
                 break;
+            } else if(userInput.equals("list")) {
+                for(int i = 0; i < toDoList.size(); i++) {
+                    System.out.println(i+1 + ". " + toDoList.get(i));
+                }
             } else {
+                toDoList.add(userInput);
                 System.out.println(dottedLines);
-                System.out.println(userInput);
+                System.out.println("added: " + userInput);
                 System.out.println(dottedLines);
             }
         }
