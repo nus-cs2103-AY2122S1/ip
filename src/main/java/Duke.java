c4d0ff05f5e1f19ecfc4176668b3cd3548e6ac3f
VimuthM <vimuthmendis@gmail.com>
2021-08-14 16:50:13 +0800

    Add listing functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,47 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
     private static String wrapOutput(String s) {
-        return "\n\t@Herb:~$ " + s + "\n";
+        // Align list items properly
+        // Adapted regex from https://stackoverflow.com/questions/15888934/how-to-indent-a-multi-line-paragraph-being-written-to-the-console-in-java
+
+        String mstr = s.replaceAll("(?m)^", "\t\t\t ");
+        return "\n\t@Herb:~$" + mstr.substring(3) + "\n";
     }
 
     public static void main(String[] args) {
         String welcomeMessage = "\n\tHi! I'm Herbert, you can call me Herb  ٩(˘◡˘)۶\n"
                 + "\tHow can I help you?\n\n"
-                + "\tSay `bye` to end this chat.\n";
+                + "\tYou can type:\n"
+                + "\t\t `list` to get a list of input texts\n"
+                + "\t\t `bye` to end this chat.\n";
         String endMessage = "\n\tSad to see you go :(\n\t...shutting down...";
+
+        ArrayList<String> texts = new ArrayList<>();
 
         System.out.println(welcomeMessage);
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.print("--> ");
             String input = sc.nextLine();
-            if (input.equals("bye")) {
+            if (input.equals("list")) {
+                if  (texts.size() == 0) {
+                    System.out.println(wrapOutput("No texts added yet!"));
+                    continue;
+                }
+                String res = "";
+                for (int i = 0; i < texts.size(); i++) {
+                    res += (i + 1) + ". " + texts.get(i) + "\n";
+                }
+                System.out.println(wrapOutput(res.substring(0, res.length() - 1)));
+            } else if (input.equals("bye")) {
                 System.out.println(endMessage);
                 break;
             } else {
-                System.out.println(wrapOutput(input));
+                texts.add(input);
+                System.out.println(wrapOutput("Added: " + input));
             }
         }
     }
