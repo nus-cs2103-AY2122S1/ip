765f99a016ed884335f5e80eb6048f4ce1fb7c52
Imran <imranr2@msn.com>
2021-08-14 20:44:54 +0800

    feat: Add level-8 features (Dates and Times) * Event now has LocalDate to represent date of event, LocalTime to represent start time and end time. * Date now has LocalDateTime to represent deadline date and time

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,23 @@
-public class Deadline extends Task {
-    private String by;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    Deadline(String name, String by) {
+public class Deadline extends Task {
+    private LocalDateTime by;
+
+    Deadline(String name, LocalDateTime by) {
         super(name);
         this.by = by;
     }
 
-    Deadline(String name, String by, boolean isDone) {
+    Deadline(String name, LocalDateTime by, boolean isDone) {
         super(name, isDone);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM d yyyy h:m a")) +
+                ")";
     }
+
 }
