30e9c88dd5f002c5562c15c72b06c783b794e4e4
Yeo Zhi Long <44795294+yzhilong@users.noreply.github.com>
2021-08-14 15:34:06 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static final String SEPARATOR = "_".repeat(50);
+    private static final String WELCOME_MESSAGE = "Hello! I'm Duke\nWhat can I do for you?";
+    private static final String BYE_MESSAGE = "Bye. Hope to see you again soon!";
+
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        printOut(WELCOME_MESSAGE);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            printOut(input);
+            input = scanner.nextLine();
+        }
+        printOut(BYE_MESSAGE);
+    }
+
+    private static void printOut(String message) {
+        System.out.println(String.format("%s\n%s\n%s", SEPARATOR, message, SEPARATOR));
     }
 }
