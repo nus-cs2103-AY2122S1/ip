b224a8d0d329a5d8596607f70f1ad5bbc6a9c315
Yuichiro Fukushima <e0407814@u.nus.edu>
2021-08-14 18:00:05 +0900

    level1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        run();
+
     }
+
+    public static void run(){
+        System.out.println(great());
+    }
+
+
+    public static String great(){
+        String greatSent = "\n" +
+        "____________________________________________________________\n" +
+        "Hello! I'm Duke\n" +
+        "What can I do for you? \n" +
+        "____________________________________________________________";
+
+        return greatSent;
+    }    
 }
