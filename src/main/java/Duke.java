d375c8317c8a4a2fe1a3ba8fa68d7e6dc5513ed0
knotstoks <70320812+knotstoks@users.noreply.github.com>
2021-08-14 19:08:29 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,60 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private enum Prompt {
+    private static ArrayList<String> itemList = new ArrayList<String>(0);
 
-    }
-
-    /*
-    Reads the input from the user and triggers the logic for the command.
+    /**
+     * Does the logic for registering prompts.
+     * @param command The command to be registered.
      */
-    private static void NextCommand() {
-        Scanner sc = new Scanner(System.in);
-        String command = sc.nextLine();
-        if (command.equals("bye")) {
-            EndDuke();
-        } else {
-            System.out.println(command);
-            NextCommand();
+    private static void EnterCommand(String command) {
+        switch (command) {
+            case "list":
+                DisplayList();
+                NextCommand();
+                break;
+            case "bye":
+                ExitDuke();
+                break;
+            default:
+                System.out.println("added: " + command);
+                itemList.add(command);
+                NextCommand();
         }
     }
 
-    /*
-    Closes and exits Duke.
+    /**
+     * Displays the itemList.
      */
-    private static void EndDuke() {
+    private static void DisplayList() {
+        for (int i = 1; i < itemList.size() + 1; i++) {
+            System.out.println(i + ". " + itemList.get(i - 1) + "\n");
+        }
+        NextCommand();
+    }
+
+    /**
+     * Adds the string item to the itemList.
+     * @param item The item to be added to the itemList.
+     */
+    private static void AddToList(String item) {
+
+    }
+
+    /**
+     * Reads the input from the user and triggers the logic for the command.
+     */
+    private static void NextCommand() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        EnterCommand(input);
+    }
+
+    /**
+     * Closes and exits Duke.
+     */
+    private static void ExitDuke() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
