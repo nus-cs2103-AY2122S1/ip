1d5d84c36878d65142c5b4e1c6eeb6b4eedce9c1
Wayne Tee <waynetee37@gmail.com>
2021-08-14 15:57:30 +0800

    Add add and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class Duke {
+    static ArrayList<String> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         greet();
@@ -8,20 +11,22 @@ public class Duke {
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
-                print(bye());
+                print("Bye. Hope to see you again soon!");
                 return;
+            } else if (input.equals("list")) {
+                print(IntStream
+                        .range(0, tasks.size())
+                        .mapToObj(x -> (x + 1) + ". " + tasks.get(x))
+                        .toArray(String[]::new));
             } else {
-                print(input);
+                tasks.add(input);
+                print("added: " + input);
             }
         }
     }
 
     static void greet() {
         print("Hello! I'm Duke", "What can I do for you?");
-    }
-
-    static String[] bye() {
-        return new String[]{"Bye. Hope to see you again soon!"};
     }
 
     static void print(String... lines) {
