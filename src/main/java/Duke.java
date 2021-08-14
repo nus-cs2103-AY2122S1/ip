917e71dd553452efefb62a1516db9759be6eb370
Wayne Tee <waynetee37@gmail.com>
2021-08-14 15:30:07 +0800

    Add echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
 
     public static void main(String[] args) {
         greet();
+        Scanner scanner=new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                print(bye());
+                return;
+            } else{
+                print(input);
+            }
+        }
     }
 
     static void greet(){
+        print("Hello! I'm Duke","What can I do for you?");
+    }
+
+    static String[] bye(){
+        return new String[]{"Bye. Hope to see you again soon!"};
+    }
+
+    static void print(String ...lines){
         printSeparator();
-        println("Hello! I'm Duke");
-        println("What can I do for you?");
+        for(String line: lines)
+            println(line);
         printSeparator();
     }
 
