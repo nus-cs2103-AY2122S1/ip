5a96064a22a1e26bf07ca96dce5e753dd8cbf088
qinguorui2001 <qinguorui666@gmail.com>
2021-08-14 10:51:07 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        while (flag) {
+            String response = sc.next();
+            flag = echo(response);
+        }
+    }
+
+    public static boolean echo(String response) {
+        switch (response) {
+            case "bye":
+                System.out.println("Bye, see you soon.");
+                return false;
+            default:
+                System.out.println(response);
+                return true;
+        }
     }
 }
