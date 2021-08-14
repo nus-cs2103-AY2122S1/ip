aa35b90d63666c36b5cbeb777ae6d98a287df25a
skythefire <minganntan@gmail.com>
2021-08-15 01:49:36 +0800

    Added Echo and Exit functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String line = "\n----------------------------------------------";
+
         // Credits to https://textart4u.blogspot.com/2014/04/frog-leap-unicode-copy-paste-text-art.html
         // for the frog ASCII text art!
         String frog =
@@ -9,9 +13,21 @@ public class Duke {
                 "╱  ╱╲╲╱╱         ╰━━━━╱\n" +
                 "╲╱╱▔▔╲╱╲         ╱▔▔▔▔▔\n" +
                 " ╱  ╱╲╲╱╱▔▔╲    ╰━━━╮━━━╮\n" +
-                " ╲ ╱  ╲╱    ▔▔▔▔▔▔▔╰╯▔▔╰╯";
-        System.out.println(frog);
+                " ╲ ╱  ╲╱    ▔▔▔▔▔▔▔╰╯▔▔╰╯\n";
+        String greeting = "I am Jo and I love frogs! RIBBIT! \n";
+        System.out.println(frog + greeting + "How may I help you?" + line);
 
-        String greeting = "My name is Jo and I love frogs! RIBBIT! ";
+        String input = "";
+        do {
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("See you again in my frog hole! RIBBIT!");
+            } else {
+                System.out.println(input + line);
+            }
+
+        } while (!input.equals("bye"));
     }
 }
