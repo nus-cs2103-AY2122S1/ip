da8b48122787bc0118da110ea924078d375e2a09
Yukun99 <yukunxu123@gmail.com>
2021-08-14 21:29:12 +0800

    HelpBot: add ability to add/list task
    Test files have also been updated to reflect the actual code.

--- src/main/java/HelpBot.java
+++ src/main/java/HelpBot.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class HelpBot {
-	// Static fields
+	// Command list
 	private static final String EXIT = "bye";
+	private static final String LIST = "list";
 
 	// Constants
 	private final String name;
 	private final Scanner scanner = new Scanner(System.in);
+
+	// Data storage
+	private final List<String> tasks = new ArrayList<>();
 
 	/**
 	 * Constructor for a HelpBot instance.
@@ -29,6 +35,13 @@ public class HelpBot {
 	}
 
 	/**
+	 * Method called when player issues exit command on bot.
+	 */
+	private void onDisable() {
+		reply("Good riddance...");
+	}
+
+	/**
 	 * Method to listen for user inputs.
 	 */
 	private void listen() {
@@ -38,16 +51,34 @@ public class HelpBot {
 			if (current.equals(EXIT)) {
 				break;
 			}
-			reply(current);
+			if (current.equals(LIST)) {
+				listAllTasks();
+				continue;
+			}
+			addTask(current);
 		}
 		onDisable();
 	}
 
 	/**
-	 * Method called when player issues exit command on bot.
+	 * Method to add a new task to the task list.
+	 *
+	 * @param task Task to be added to the task list.
 	 */
-	private void onDisable() {
-		reply("Good riddance...");
+	private void addTask(String task) {
+		tasks.add(task);
+		reply("Urgh, fine. Task '" + task + "' has been added.");
+	}
+
+	/**
+	 * Method to list all added tasks in the task list.
+	 */
+	private void listAllTasks() {
+		StringBuilder message = new StringBuilder("Oh. My. God. Ok... Here are your tasks");
+		for (int i = 0; i < tasks.size(); ++i) {
+			message.append("\n").append(i + 1).append(". ").append(tasks.get(i)).append(".");
+		}
+		reply(message.toString());
 	}
 
 	/**
