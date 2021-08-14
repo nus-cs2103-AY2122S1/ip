1a534eb130ef7f20054ff7ab58b9b92ce8598047
idgrr <77221700+idgrr@users.noreply.github.com>
2021-08-14 20:55:19 +0800

    Added basic response functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,57 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Duke {
+
+    // Duke Response Template
+    private static String duke_speak(String[] in) {
+        String top_border = "          ___________________________________________________\n";
+        String bot_border = "          | ________________________________________________|\n" + "          |/\n";
+        String emoticon = emoticon_rand();
+        String msges = "";
+        for( String x : in){
+            msges += message_format(x, 48);
+        }
+        return top_border + msges + bot_border+ emoticon;
+    }
+    private static String duke_speak(String in) {
+        String[] temp = {in};
+        return duke_speak(temp);
+    }
+    private static String message_format(String in, int longest) {
+        String spaces = "          | ";
+        String msg = spaces + String.format("%-"+longest+"s", in) +"|\n";
+        return msg;
+    }
+
+    private static String emoticon_rand() {
+        String[] emoticon = {"(ﾍﾟ◇ﾟ)」" ,"( ＾◡＾)っ", "(*°▽°)ノ", "(o^ω^)" , "(✿˘▽˘)"};
+        int rando = new Random().nextInt(emoticon.length-1);
+        return emoticon[rando];
+    }
+
+    private static String welcome() {
+        String[] duke_welcome = {"Hello! I'm Duke!" , "What can I do for you"};
+        return duke_speak(duke_welcome);
+    }
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(welcome());
+        Scanner sc = new Scanner(System.in);
+
+        String user_input = "";
+
+        while (true) {
+            System.out.print("Say something to Duke: ");
+            user_input = sc.next();
+            if (user_input.equals("bye")) {
+                break;
+            }
+            System.out.println(duke_speak(user_input));
+        }
+        System.out.println( duke_speak("Bye. Hope to see you again soon!" ));
     }
 }
