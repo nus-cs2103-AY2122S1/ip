a13d3ac91dc3daba2db1f59e0d600cf229801666
auhc99 <auhc99@gmail.com>
2021-08-14 16:10:09 +0800

    Level 2. Add, List
    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    private static ArrayList<String> list = new ArrayList<>(100);
+
+    private static void chat(String content) {
+        System.out.println(
+                "____________________________________________________________\n"
+                + content
+                + "\n____________________________________________________________\n"
+        );
+    }
+
+    private static void addItem(String item) {
+        list.add(item);
+    }
+
+    private static void displayList() {
+        StringBuilder listString = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            listString.append(String.valueOf(i + 1) + ". " + list.get(i));
+            if (i != list.size() - 1) {
+                listString.append("\n");
+            }
+        }
+        chat(listString.toString());
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,21 +37,18 @@ public class Duke {
         chat("Hello I'm\n" + logo + "What can I do for you?");
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            String input = scanner.next();
+            String input = scanner.nextLine();
             if (input.equals("bye")) {
-                chat(" Bye. Hope to see you again soon!");
+                chat("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                displayList();
             } else {
-                chat(input);
+                addItem(input);
+                chat("added: " + input);
             }
         }
     }
 
-    private static void chat(String content) {
-        System.out.println(
-                "____________________________________________________________\n"
-                + content
-                + "\n____________________________________________________________\n"
-        );
-    }
+
 }
