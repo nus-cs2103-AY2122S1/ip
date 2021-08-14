1b4e32367b1828a3358ef83f8490be0c2da5aea5
Shezad <shezadhassan22@gmail.com>
2021-08-14 10:49:13 +0800

    Level 1 - Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +8,27 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "\nWhat can I do for you?");
+        drawLine();
+
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String userInput = sc.nextLine();
+            if (userInput.toLowerCase(Locale.ROOT).equals("bye")) {
+                drawLine();
+                System.out.println("    " + "Bye. Hope to see you again soon!");
+                drawLine();
+                break;
+            }
+            drawLine();
+            System.out.println("    " + userInput);
+            drawLine();
+        }
     }
+
+    //Draws vertical line to seperate users speech from duke
+    public static void drawLine() {
+        System.out.println("------------------------");
+    }
+
 }
