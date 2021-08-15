6bb9f82d1853cf25b64b113e4280bfccc1619244
EssWhyy <swenyitang99@gmail.com>
2021-08-15 16:46:09 +0800

    Implement level-1 of individual project
    Added the following functionalities to the main() function:
    * Logo & introduction to app
    * Echo/print out user input
    * Exit app when user types "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner reader = new Scanner(System.in);
+        String logo = "                                        _       \n"
+                + "  _ __  _ _   _ __  ___ ___ ___ ___ ___| |__ ___\n"
+                + " | '  \\| '_| | '  \\/ -_) -_|_-</ -_) -_) / /(_-<\n"
+                + " |_|_|_|_|   |_|_|_\\___\\___/__/\\___\\___|_\\_\\/__/\n"
+                + "\n";
+        System.out.println(logo);
+        System.out.println("I'm Mr Meeseeks look at me!");
+        System.out.println("Type in anything and I will repeat it to you!");
+        System.out.println("Type \"bye\" to exit");
+
+
+        while (true){
+            String input = reader.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. *Poof!*");
+                break;
+            }
+
+            System.out.println(input);
+        }
     }
 }
