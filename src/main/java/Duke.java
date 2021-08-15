1ab729d46f66a97a9f83d3af1738de50cf51a586
xnajasho <iifourxd@gmail.com>
2021-08-15 22:51:15 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,8 +18,8 @@ public class Duke {
         System.out.println(openingMessage);
 
         Scanner sc = new Scanner(System.in);
-
-        while (sc.hasNextLine()) {
+        boolean flag = true;
+        while (sc.hasNextLine() && flag) {
             String input = sc.nextLine();
             String firstWord;
             String remainingWords = "";
@@ -30,27 +30,68 @@ public class Duke {
                 firstWord = input;
             }
 
-            String output;
-
             switch (firstWord) {
                 case "list":
-                    output = bot.printList();
+                    System.out.println(bot.printList());
                     break;
 
                 case "bye":
-                    output = bot.sayBye();
+                    System.out.println(bot.sayBye());
+                    flag = false;
                     break;
 
                 case "done":
                     int index = Integer.parseInt(remainingWords);
-                    output = bot.completeTask(index);
+                    try {
+                        System.out.println(bot.completeTask(index));
+                    } catch (DukeException e) {
+                        String errorMessage = "   -------------------------------------------- \n"
+                                            + "      OOPS!!! Invalid task number given \n"
+                                            + "   -------------------------------------------- \n";
+                        System.out.println(errorMessage);
+                    }
+                    break;
+
+                case "todo":
+                    try {
+                        System.out.println(bot.addTodo(remainingWords));
+                    } catch (DukeException e) {
+                        String errorMessage = "   -------------------------------------------- \n"
+                                            + "      OOPS!!! The description of a todo cannot be empty. \n"
+                                            + "   -------------------------------------------- \n";
+                        System.out.println(errorMessage);
+                    }
+                    break;
+
+                case "deadline":
+                    try {
+                        System.out.println(bot.addDeadline(remainingWords));
+                    } catch (DukeException e) {
+                        String errorMessage = "   -------------------------------------------- \n"
+                                            + "      OOPS!!! The description of a deadline cannot be empty. \n"
+                                            + "   -------------------------------------------- \n";
+                        System.out.println(errorMessage);
+                    }
+                    break;
+
+                case "event":
+                    try {
+                        System.out.println(bot.addEvent(remainingWords));
+                    } catch (DukeException e) {
+                        String errorMessage = "   -------------------------------------------- \n"
+                                            + "      OOPS!!! The description of an event cannot be empty. \n"
+                                            + "   -------------------------------------------- \n";
+                        System.out.println(errorMessage);
+                    }
                     break;
 
                 default:
-                    output = bot.addTask(input);
+                    String errorMessage = "   -------------------------------------------- \n"
+                                        + "      OOPS!!! I have no idea what that means :-( \n"
+                                        + "   -------------------------------------------- \n";
+                    System.out.println(errorMessage);
 
             }
-            System.out.println(output);
         }
         sc.close();
     }
@@ -81,108 +122,115 @@ public class Duke {
     public String sayBye() {
         return "   -------------------------------------------- \n"
                 + "   Bye! Hope to see you again soon! \n"
-                + "   -------------------------------------------- \n";
+                + "   --------------------------------------------";
     }
 
-    public String completeTask(int index) {
-        String taskClass;
-        if (this.taskList.get(index - 1) instanceof Todo) {
-            taskClass = "Todo";
-        } else if (this.taskList.get(index - 1) instanceof Deadline) {
-            taskClass = "Deadline";
-        } else if (this.taskList.get(index - 1) instanceof Event) {
-            taskClass = "Event";
+    public String completeTask(int index) throws DukeException {
+        if (!(index >= 0 && index - 1 <= taskList.size() && !taskList.isEmpty()) || index > taskList.size()) {
+            String errorMessage = "\n"
+                    + "   -------------------------------------------- \n"
+                    + "      OOPS!!! Invalid task number given \n"
+                    + "   -------------------------------------------- \n";
+            throw new DukeException(errorMessage);
         } else {
-            taskClass = "Task";
-        }
+            String taskClass;
+            if (this.taskList.get(index - 1) instanceof Todo) {
+                taskClass = "Todo";
+            } else if (this.taskList.get(index - 1) instanceof Deadline) {
+                taskClass = "Deadline";
+            } else if (this.taskList.get(index - 1) instanceof Event) {
+                taskClass = "Event";
+            } else {
+                taskClass = "Task";
+            }
 
-        String output = "   -------------------------------------------- \n"
+            String output = "   -------------------------------------------- \n"
                     + "   Nice! I've marked this task as done: \n"
                     + "      ";
 
-        switch (taskClass) {
-            case "Todo":
-                Todo completedTask = ((Todo) this.taskList.get(index - 1)).markAsDone();
-                this.taskList.set(index - 1, completedTask);
-                output += completedTask.toString() + "\n";
-                break;
+            switch (taskClass) {
+                case "Todo":
+                    Todo completedTask = ((Todo) this.taskList.get(index - 1)).markAsDone();
+                    this.taskList.set(index - 1, completedTask);
+                    output += completedTask.toString() + "\n";
+                    break;
 
-            case "Deadline":
-                Deadline completedTask2 = ((Deadline) this.taskList.get(index - 1)).markAsDone();
-                this.taskList.set(index - 1, completedTask2);
-                output += completedTask2.toString() + "\n";
-                break;
+                case "Deadline":
+                    Deadline completedTask2 = ((Deadline) this.taskList.get(index - 1)).markAsDone();
+                    this.taskList.set(index - 1, completedTask2);
+                    output += completedTask2.toString() + "\n";
+                    break;
 
-            case "Event":
-                Event completedTask3 = ((Event) this.taskList.get(index - 1)).markAsDone();
-                this.taskList.set(index - 1, completedTask3);
-                output += completedTask3.toString() + "\n";
-                break;
+                case "Event":
+                    Event completedTask3 = ((Event) this.taskList.get(index - 1)).markAsDone();
+                    this.taskList.set(index - 1, completedTask3);
+                    output += completedTask3.toString() + "\n";
+                    break;
 
-            default:
-                Task completedTask4 = this.taskList.get(index - 1).markAsDone();
-                this.taskList.set(index - 1, completedTask4);
-                output += completedTask4.toString() + "\n";
+                default:
+                    Task completedTask4 = this.taskList.get(index - 1).markAsDone();
+                    this.taskList.set(index - 1, completedTask4);
+                    output += completedTask4.toString() + "\n";
+            }
+            output += "   -------------------------------------------- \n";
+            return output;
         }
-        output += "   -------------------------------------------- \n";
-        return output;
     }
 
-    public String addTask(String input) {
-        String firstWord;
-        String remainingWords = "";
-        if (input.contains(" ")) {
-            firstWord = this.getFirstWord(input);
-            remainingWords = this.getRestOfWords(input);
+    public String addTodo(String input) throws DukeException {
+        if (!input.equals("")) {
+            Todo newTodo = new Todo(input);
+            this.taskList.add(newTodo);
+            return printTaskMessage(newTodo.toString(), this.taskList.size());
         } else {
-            firstWord = input;
+            String errorMessage = "\n"
+                    + "   -------------------------------------------- \n"
+                    + "      OOPS!!! The description of a todo cannot be empty. \n"
+                    + "   -------------------------------------------- \n";
+            throw new DukeException(errorMessage);
         }
-
-        String output;
-
-        switch (firstWord) {
-            case "todo":
-                Todo newTodo = new Todo(remainingWords);
-                this.taskList.add(newTodo);
-                output = "   -------------------------------------------- \n"
-                        + "   Got it. I've added this task: \n"
-                        + "      " + newTodo + "\n";
-                break;
-
-            case "deadline":
-                String[] arr = remainingWords.split("/by", 2);
-                String day = arr[1];
-                Deadline newDeadline = new Deadline(arr[0].trim(), day.trim());
-                this.taskList.add(newDeadline);
-                output = "   -------------------------------------------- \n"
-                        + "   Got it. I've added this task: \n"
-                        + "      " + newDeadline + "\n";
-                break;
-
-            case "event":
-                String[] arr2 = remainingWords.split("/at", 2);
-                String time = arr2[1];
-                Event newEvent = new Event(arr2[0].trim(), time.trim());
-                this.taskList.add(newEvent);
-                output = "   -------------------------------------------- \n"
-                        + "   Got it. I've added this task: \n"
-                        + "      " + newEvent + "\n";
-                break;
-
-            default:
-                Task newTask = new Task(input);
-                this.taskList.add(newTask);
-                output = "   -------------------------------------------- \n"
-                        + "   Got it. I've added this task: \n"
-                        + "      " + newTask + "\n";
-
-        }
-
-        int numTasks = this.taskList.size();
-        output += String.format("   Now you have %d tasks in the list.", numTasks) + "\n"
-                + "   -------------------------------------------- \n";
-        return output;
     }
+
+    public String addDeadline(String input) throws DukeException {
+        if (!input.equals("")) {
+            String[] arr = input.split("/by", 2);
+            String day = arr[1];
+            Deadline newDeadline = new Deadline(arr[0].trim(), day.trim());
+            this.taskList.add(newDeadline);
+            return printTaskMessage(newDeadline.toString(), this.taskList.size());
+        } else {
+            String errorMessage = "\n"
+                    + "   -------------------------------------------- \n"
+                    + "      OOPS!!! The description of a deadline cannot be empty. \n"
+                    + "   -------------------------------------------- \n";
+            throw new DukeException(errorMessage);
+        }
+    }
+
+    public String addEvent(String input) throws DukeException {
+        if (!input.equals("")) {
+            String[] arr = input.split("/at", 2);
+            String time = arr[1];
+            Event newEvent = new Event(arr[0].trim(), time.trim());
+            this.taskList.add(newEvent);
+            return printTaskMessage(newEvent.toString(), this.taskList.size());
+        } else {
+            String errorMessage = "\n"
+                    + "   -------------------------------------------- \n"
+                    + "      OOPS!!! The description of an event cannot be empty. \n"
+                    + "   -------------------------------------------- \n";
+            throw new DukeException(errorMessage);
+        }
+    }
+
+    public String printTaskMessage(String input, int numTasks) {
+        return "   -------------------------------------------- \n"
+                + "   Got it. I've added this task: \n"
+                + "      " + input + "\n"
+                + String.format("   Now you have %d tasks in the list.", numTasks) + "\n"
+                + "   -------------------------------------------- \n";
+    }
+
 
     public String getFirstWord(String input) {
         String[] arr = input.split(" ", 2);
