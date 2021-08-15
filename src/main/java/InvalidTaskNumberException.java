795337347aa1d5100269d8f014c15caa433651f9
yzhilong <44795294+yzhilong@users.noreply.github.com>
2021-08-15 18:48:24 +0800

    Change error message to fit deletions

--- src/main/java/InvalidTaskNumberException.java
+++ src/main/java/InvalidTaskNumberException.java
@@ -3,7 +3,7 @@ public class InvalidTaskNumberException extends DukeException {
     public InvalidTaskNumberException(int n) {
         super(n > 0
                 ? String.format("Sorry, this task index is invalid! Please input an integer between 1-%d", n)
-                : "You need to add tasks before you can mark them as completed!");
+                : "Sorry, this task index is invalid! Please add tasks before using this command");
     }
 
 }
