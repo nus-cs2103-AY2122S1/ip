974eafa9c0b28e22e9598e9508688456ef529691
yzhilong <44795294+yzhilong@users.noreply.github.com>
2021-08-15 15:12:38 +0800

    shift logic into helper function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,52 +9,85 @@ public class Duke {
     private static final String WELCOME_MESSAGE = "Hello! I'm Duke\nWhat can I do for you?";
     private static final String BYE_MESSAGE = "Bye. Hope to see you again soon!";
 
-
+    private static void parseInput(String input, List<Task> tasks) {
+        String[] commands = input.split("\\s+");
+        String keyWord = commands[0];
+        String taskDetails;
+        String extraDetails;
+        String message = "";
+        Task newTask = null;
+        int index = 0;
+        switch(keyWord) {
+            case "list":
+                index = 1;
+                for (Task task : tasks) {
+                    message += String.format("%d. %s\n", index, task);
+                    index++;
+                }
+                message = message.substring(0,message.length()-1);
+                break;
+            case "todo":
+                taskDetails = String.join(" ", Arrays.copyOfRange(commands, 1, commands.length));
+                newTask = new Todo(taskDetails);
+                break;
+            case "deadline":
+                for (int i = 1; i < commands.length; i++) {
+                    if (commands[i].equals("/by")) {
+                        index = i;
+                        break;
+                    }
+                }
+                taskDetails = String.join(" ", Arrays.copyOfRange(commands, 1, index));
+                extraDetails = String.join(" ", Arrays.copyOfRange(commands, index+1, commands.length));
+                newTask = new Deadline(taskDetails, extraDetails);
+                break;
+            case "event":
+                for (int i = 1; i < commands.length; i++) {
+                    if (commands[i].equals("/at")) {
+                        index = i;
+                        break;
+                    }
+                }
+                taskDetails = String.join(" ", Arrays.copyOfRange(commands, 1, index));
+                extraDetails = String.join(" ", Arrays.copyOfRange(commands, index+1, commands.length));
+                newTask = new Deadline(taskDetails, extraDetails);
+                break;
+            case "done":
+                try {
+                    int taskIndex = Integer.parseInt(commands[1]) - 1;
+                    if (taskIndex < 0 || taskIndex >= tasks.size()) {
+                        message = String.format("Sorry, this task index is out of bounds! Please input an integer between 0-%d", tasks.size());
+                    } else {
+                        Task task = tasks.get(taskIndex);
+                        if (task.markDone()) {
+                            message = String.format("Nice! I've marked this task as done:\n    %s", task);
+                        } else {
+                            message = String.format("This was completed previously!:\n    %s", task);
+                        }
+                    }
+                } catch (NumberFormatException e) {
+                    message = String.format("Sorry, this task index is invalid! Please input an integer between 0-%d", tasks.size());
+                }
+        }
+        if (newTask != null) {
+            tasks.add(newTask);
+            message = String.format(
+                    "Got it. I've added this task:\n    %s\nNow you have %d tasks in the list.",
+                    newTask,
+                    tasks.size()
+            );
+        }
+        printOut(message);
+    }
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         Scanner scanner = new Scanner(System.in);
         List<Task> tasks = new ArrayList<>();
 
         printOut(WELCOME_MESSAGE);
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
-            switch(input) {
-                case "list":
-                    int index = 1;
-                    String message = "";
-                    for (Task task : tasks) {
-                        message += String.format("%d. %s\n", index, task);
-                        index++;
-                    }
-                    printOut(message.substring(0,message.length()-1));
-                    break;
-                default:
-                    String[] commands = input.split("\\s+");
-                    if (commands.length == 2 && commands[0].equals("done")) {
-                        try {
-                            Task task = tasks.get(Integer.parseInt(commands[1])-1);
-                            if (task.markDone()) {
-                                String msg = String.format("Nice! I've marked this task as done:\n    %s", task);
-                                printOut(msg);
-                            } else {
-                                String msg = String.format("This task has already been marked as done:\n    %s", task);
-                                printOut(msg);
-                            }
-                        } catch (NumberFormatException e) {
-                            tasks.add(new Task(input));
-                            printOut(String.format("added: %s", input));
-                        }
-                    } else {
-                        tasks.add(new Task(input));
-                        printOut(String.format("added: %s", input));
-                    }
-                    break;
-            }
+            parseInput(input, tasks);
             input = scanner.nextLine();
         }
         printOut(BYE_MESSAGE);
