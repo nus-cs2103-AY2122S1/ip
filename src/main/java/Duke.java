e05468ddfaf82e55933721c3e630efdb67fc14e9
1waykiat <1waykiat@gmail.com>
2021-08-15 17:28:23 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,28 @@
-import java.lang.reflect.Array;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
+    public static class Task {
+        private final String name;
+        private boolean completed;
+
+        public Task(String name) {
+            this.name = name;
+            completed = false;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+        public String getCompleted() {
+            return (completed ? "X" : " ");
+        }
+
+        private void toggleCompleted() {
+            this.completed = !this.completed;
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -13,22 +33,33 @@ public class Duke {
 
         boolean loop = true;
         Scanner textInput = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         while (loop) {
             String input = textInput.nextLine();
+            String[] inputSplit = input.split(" ");
 
-            switch(input) {
+            switch(inputSplit[0]) {
                 case "bye":
                     loop = false;
                     break;
                 case "list":
                     for (int i = 1; i <= list.size(); i++) {
-                        System.out.println(i + ". " + list.get(i - 1));
+                        Task task = list.get(i - 1);
+                        System.out.println(i + "."
+                                + "[" + task.getCompleted() + "] "
+                                + task.getName());
                     }
                     break;
+                case "done":
+                    int index = Integer.parseInt(inputSplit[1]);
+                    Task task = list.get(index - 1);
+                    task.toggleCompleted();
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println("[X] " + task.getName());
+                    break;
                 default:
-                    list.add(input);
+                    list.add(new Task(input));
                     System.out.println("added: " + input);
             }
         }
