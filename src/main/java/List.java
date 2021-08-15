8618efcf3906761d3287f7b06eeff4d95b4b621d
houtenteo <houtenteo@gmail.com>
2021-08-15 12:53:20 +0800

    Created 'List' class

---
+++ src/main/java/List.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class List {
+    private ArrayList<String> list = new ArrayList<String>();
+
+    public void addItem(String item) {
+        this.list.add(item);
+    }
+}
