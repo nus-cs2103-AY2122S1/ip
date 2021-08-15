a488fa51ea6ed877a57ca07a7b48f46cf53e1591
Willy Angga <ampedwilly@gmail.com>
2021-08-15 21:56:01 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String input = "";
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello! I'm Amped");
+        System.out.println("What can I do for you?");
+        do {
+            input = scan.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Good Bye. Have a nice day!");
+            }
+            else {
+                System.out.println(input);
+            }
+        } while (!input.equals("bye"));
     }
 }
