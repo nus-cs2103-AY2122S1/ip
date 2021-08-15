acd085ac6fc70d18ac9c86d141b813088bf3da83
Lee Zheng Han <e0544595@u.nus.edu>
2021-08-15 20:20:59 +0800

    Implemented bot echoing and termination

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Main class for the bot.
+ */
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " ____        _        \n" + "|  _ \\ _   _| | _____ \n" + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
+        String greetings = "Hello from\n" + logo + "\nWhat can I do for you?";
+        new Response(greetings).print();
+
+        echoInput(new BufferedReader(new InputStreamReader(System.in)));
+    }
+
+    private static void echoInput(BufferedReader reader) {
+        String input = "";
+        try {
+            input = reader.readLine();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        if (input.equals("bye")) {
+            terminate();
+        } else {
+            new Response(input).print();
+            echoInput(reader);
+        }
+    }
+
+    private static void terminate() {
+        new Response("Bye. Hope to see you again soon!").print();
     }
 }
---
+++ src/main/java/Response.java
@@ -0,0 +1,20 @@
+/**
+ * Encapsulates a response of the bot.
+ */
+public class Response {
+    private static final String PARTITION = "____________________________________________________________";
+    private static final String ANSI_CYAN = "\u001B[36m";
+    private static final String ANSI_RESET = "\u001B[0m";
+    private final String message;
+
+    public Response(String message) {
+        this.message = message;
+    }
+
+    /**
+     * Prints the response in the system's output.
+     */
+    public void print() {
+        System.out.println(ANSI_CYAN + PARTITION + "\n" + message + "\n" + PARTITION + ANSI_RESET);
+    }
+}
