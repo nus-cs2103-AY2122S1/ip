6458a5eb4efa18e86ae745297c19a8cadc9cd05f
Chang Chuan Hao <e0543479@u.nus.edu>
2021-08-15 12:16:53 +0800

    Added Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,35 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String greeting = "____________________________________________________________\n"
+                + "Hello! I'm Python>Java\n"
+                + "What must I do for you?\n"
+                + "____________________________________________________________";
+        System.out.println(greeting);
+
+        boolean run = true;
+        while(run) {
+            run = eval();
+        }
+    }
+
+    public static boolean eval() {
+        Scanner inputReader = new Scanner(System.in);
+        String input = inputReader.nextLine();
+
+        switch (input) {
+            case "bye":
+                System.out.println("____________________________________________________________\n"
+                        + "Bye. Don't come again!\n"
+                        + "____________________________________________________________");
+                return false;
+            default:
+                System.out.println("____________________________________________________________\n"
+                        + input + "\n"
+                        + "____________________________________________________________\n");
+                return true;
+
+        }
     }
 }
