ea81c9fe9f1285a64c4015ee6adc26941443d8d6
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-08-15 20:45:03 +0800

    Add DukeInvalidCommandException class

---
+++ src/main/java/duke/ui/DukeInvalidCommandException.java
@@ -0,0 +1,7 @@
+package duke.ui;
+
+public class DukeInvalidCommandException extends IllegalArgumentException {
+    public DukeInvalidCommandException(String message) {
+        super(message);
+    }
+}
