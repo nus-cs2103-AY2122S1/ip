e28068eca7f0fd52d218183e62ac40d10f6626e1
Tan Jia Xian <e0543981@u.nus.edu>
2021-08-15 20:45:33 +0800

    added command mark as done and added getDescription to Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,15 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<String> taskList = new ArrayList<>();
+        List<Task> taskList = new ArrayList<>();
 
         // Hello message
         System.out.println(
             "Lollipop: Hello! I am your personal chatbot, Lollipop! " +
             "What would you like to do today?");
+        System.out.println("");
         System.out.print("You: ");
         String command = scanner.nextLine();
 
@@ -19,13 +22,21 @@ public class Duke {
             if (command.equals("list")) {
                 System.out.println("Lollipop: Here are your tasks");
                 for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println((i + 1) + ". " + taskList.get(i));
+                    Task task = taskList.get(i);
+                    System.out.printf("%d. [%s] %s%n", i + 1, task.getStatusIcon(), task.getDescription());
                 }
+            } else if (command.contains("done")) {
+                int taskNumber = parseInt(command.split(" ")[1]);
+                Task task = taskList.get(taskNumber - 1);
+                task.markAsDone();
+                System.out.printf("Lollipop: %s has been marked as done.%n", task.getDescription());
             } else {
-                taskList.add(command);
-                System.out.println("Lollipop: " + command + " has been added.");
+                Task task = new Task(command);
+                taskList.add(task);
+                System.out.printf("Lollipop: %s has been added.%n", task.getDescription());
             }
 
+            System.out.println("");
             System.out.print("You: ");
             command = scanner.nextLine();
         }
