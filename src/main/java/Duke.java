bc18a47ba397a8edd89d837ec143d7ecbabe46af
Jonas Chow <jonaschow99@gmail.com>
2021-08-15 12:41:46 +0800

    Add Greet, Echo, Exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LINE_SEPARATOR = "_________________________________________\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        greet();
+
+        while (sc.hasNextLine()) {
+            String msg = sc.nextLine();
+            if (msg.equals("bye")) {
+                exit();
+            } else {
+                printMessage(msg);
+            }
+        }
+    }
+
+    private static void printMessage(String msg) {
+        System.out.println(LINE_SEPARATOR + msg + "\n" + LINE_SEPARATOR);
+    }
+
+    private static void greet() {
+        String greeting = "Hello! I'm iP Man.\nWhat can I do for you?";
+        printMessage(greeting);
+    }
+
+    private static void exit() {
+        printMessage("Bye. Hope to see you again soon!");
+        System.exit(0);
     }
 }
