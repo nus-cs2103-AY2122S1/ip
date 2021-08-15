6871ef5bfb88191d0d776b7853fa4484cc6ed3a8
ramapriyan912001 <ramapriyan912001@gmail.com>
2021-08-15 16:31:32 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
+
+    public String byeString() {
+        return "Bye. Hope to see you again soon!";
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if(input.equals("bye"))  {
+            System.out.println(byeString());
+        } else {
+            interpretInput(input);
+            run();
+        }
+    }
+
+    public void interpretInput(String input) {
+        System.out.println(input);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        String hello = "Hello! I'm Duke\n" +
+                "What can I do for you?";
+        System.out.println(hello);
+        duke.run();
     }
 }
