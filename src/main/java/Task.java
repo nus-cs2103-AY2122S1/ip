65cab1e9b49947018dac0d9939aa0890090be546
e0543403 <jamesdouglashester@yahoo.com.sg>
2021-08-15 10:25:47 +0800

    Add Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    
+}
