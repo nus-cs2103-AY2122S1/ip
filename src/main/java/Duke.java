19e7774f95ac98ff1436414293f8d6ef415c926e
EltonGohJH <eltongjh@u.nus.edu>
2021-08-15 13:19:30 +0800

    Complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,50 @@
+import java.util.Scanner;
+
 public class Duke {
+    /**
+     * Prints a line.
+     */
+    private static void printLine() {
+        System.out.println("_________________________________");
+    }
+
+    /**
+     * Prints the greeting message.
+     */
+    private static void greetUser() {
+        printLine();
+        System.out.println("Greetings! I am Duke.");
+        System.out.println("How can I assist you?");
+        printLine();
+    }
+
+    /**
+     * Prints the farewell message.
+     */
+    private static void farewellUser() {
+        printLine();
+        System.out.println("Bye! See you soon!");
+        printLine();
+    }
+
+    /**
+     * Interact with the user.
+     *
+     * @param args array of strings.
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greetUser();
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        while (!userInput.equalsIgnoreCase("bye")) {
+            printLine();
+            System.out.println(userInput);
+            printLine();
+            userInput = scanner.nextLine();
+        }
+        scanner.close();
+
+        farewellUser();
     }
 }
