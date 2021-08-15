2d8674f594adca9960c04606cdc87c8442a1abb0
houtenteo <houtenteo@gmail.com>
2021-08-15 11:50:00 +0800

    first commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        start();
     }
+
+    public static void start() {
+        System.out.println(
+                "Hello! I'm Duke \n"
+                        + "What can I do for you? \n"
+        );
+
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.next());
+    }
+
 }
