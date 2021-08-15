b0856425dcaa55720d139741dcc0e245acec4eba
felissaf <felissafaustine@gmail.com>
2021-08-15 22:00:48 +0800

    added add and list function, changed the introduction message

--- src/main/java/MyJournal.java
+++ src/main/java/MyJournal.java
@@ -1,20 +1,30 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-public class Duke {
+public class MyJournal {
     public static void main(String[] args) {
+        String[] items = new String[100];
         String ans = "";
         Scanner reader = new Scanner(System.in);
-        System.out.println("Hello! I'm Duke\n"
-                + "What can I do for you?");
+        int count = 0;
+        System.out.println("Hello!\n"
+                + "1. Type a task to be added into your todo list.\n"
+                + "2. Type 'list' if you want to generate your todo list.\n"
+                + "3. Type 'bye' to exit");
         while (true) {
             ans = reader.nextLine();
-            if (!ans.equals("bye")) {
-                System.out.println(ans);
-            } else {
+            if (ans.equals("bye")) {
                 break;
+            } else if (ans.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + items[i]);
+                }
+            } else {
+                items[count] = ans;
+                System.out.println("added: " + ans);
+                count++;
             }
         }
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Bye. Hope to see you again soon!:)");
     }
 }
