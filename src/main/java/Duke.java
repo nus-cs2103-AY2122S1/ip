5d1660313ebca4bb1052d53af77e02506ce41836
1waykiat <1waykiat@gmail.com>
2021-08-15 19:55:18 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,9 +17,47 @@ public class Duke {
         public String getCompleted() {
             return (completed ? "X" : " ");
         }
-
         private void toggleCompleted() {
             this.completed = !this.completed;
+        }
+        public String toString() {
+            return "[" + (completed ? "X" : " ") + "] " + this.name;
+        }
+    }
+
+    public static class ToDo extends Task {
+        public ToDo(String name) {
+            super(name);
+        }
+
+        @Override
+        public String toString() {
+            return "[T]" + super.toString();
+        }
+    }
+
+    public static class Deadline extends Task {
+        private String doneBy;
+        public Deadline(String name, String by) {
+            super(name);
+            this.doneBy = by;
+        }
+        @Override
+        public String toString() {
+            return "[D]" + super.toString() + " (by: " + doneBy + ")";
+        }
+    }
+
+    public static class Event extends Task{
+        private String at;
+        public Event(String name, String at) {
+            super(name);
+            this.at = at;
+        }
+
+        @Override
+        public String toString() {
+            return "[E]" + super.toString() + "(at: " + at + ")";
         }
     }
 
@@ -37,9 +75,34 @@ public class Duke {
 
         while (loop) {
             String input = textInput.nextLine();
-            String[] inputSplit = input.split(" ");
+            String[] inputSplit = input.split(" ", 2);
 
             switch(inputSplit[0]) {
+                case "todo":
+                    Task todo = new ToDo(inputSplit[1]);
+                    list.add(todo);
+                    System.out.println("Got it. I've added this task: \t");
+                    System.out.println(todo.toString());
+                    System.out.println("Now you have " + list.size() + " tasks in your list.");
+                    break;
+                case "deadline":
+                    String desc = inputSplit[1].split("/by ", 2)[0];
+                    String dead = inputSplit[1].split("/by ", 2)[1];
+                    Task deadline = new Deadline(desc, dead);
+                    list.add(deadline);
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println(deadline.toString());
+                    System.out.println("Now you have " + list.size() + " tasks in your list.");
+                    break;
+                case "event":
+                    String name = inputSplit[1].split("/at ", 2)[0];
+                    String at = inputSplit[1].split("/at ", 2)[1];
+                    Task event = new Event(name, at);
+                    list.add(event);
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println(event.toString());
+                    System.out.println("Now you have " + list.size() + " tasks in your list.");
+                    break;
                 case "bye":
                     loop = false;
                     break;
@@ -47,8 +110,7 @@ public class Duke {
                     for (int i = 1; i <= list.size(); i++) {
                         Task task = list.get(i - 1);
                         System.out.println(i + "."
-                                + "[" + task.getCompleted() + "] "
-                                + task.getName());
+                                + task.toString());
                     }
                     break;
                 case "done":
