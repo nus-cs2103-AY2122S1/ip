582f0d977e80712e687d735a54bf29495f5fb6e1
kheekheekhee <eekarheenicholas@gmail.com>
2021-08-15 19:51:31 +0800

    Add ability to store user inputs and display them back to user and isolate greeting and exiting to their own functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,56 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
+    private final ArrayList<String> taskList = new ArrayList<>();
+
     public void run() {
-        System.out.println("__________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("__________________________________");
+
+        greet();
 
         String input;
         Scanner sc = new Scanner(System.in);
 
-        while(true) {
-            input = sc.nextLine();
-            System.out.println("__________________________________");
-            if (input.equals("bye")) {
-                sc.close();
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("__________________________________");
-                break;
+        while(!(input = sc.nextLine()).equals("bye")) {
+            if (input.equals("list")) {
+                displayList();
+            } else {
+                addToList(input);
             }
-            System.out.println(input);
-            System.out.println("__________________________________");
         }
+
+        sc.close();
+
+        exit();
+    }
+
+    private void greet() {
+        System.out.println("__________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("__________________________________");
+    }
+
+    private void exit() {
+        System.out.println("__________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("__________________________________");
+    }
+
+    private void addToList(String task) {
+        this.taskList.add(task);
+        System.out.println("__________________________________");
+        System.out.println("added: " + task);
+        System.out.println("__________________________________");
+    }
+
+    private void displayList() {
+        System.out.println("__________________________________");
+        for (int i = 0; i < this.taskList.size(); i++) {
+            System.out.println(i + 1 + ". " + taskList.get(i));
+        }
+        System.out.println("__________________________________");
     }
 
     public static void main(String[] args) {
