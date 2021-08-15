f9b3daccec73a241697e5d15f8f0301672153fe2
houtenteo <houtenteo@gmail.com>
2021-08-15 13:14:57 +0800

    Added some functionalities

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -6,4 +6,16 @@ public class List {
     public void addItem(String item) {
         this.list.add(item);
     }
+
+    public ArrayList<String> getList() {
+        return this.list;
+    }
+
+    public int getLength() {
+        return this.list.size();
+    }
+
+    public String getItemAtIndex(int i) {
+        return list.get(i);
+    }
 }
