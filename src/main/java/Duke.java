64186f81a8b693517ce507a0856da60998b15978
ZhaoPeiduo <e0389118@u.nus.edu>
2021-08-15 15:41:37 +0800

    Level-2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,38 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void echo() {
+
+    private static void echoWithAdd() {
+        ArrayList<String> userInputRecord = new ArrayList<>();
         Scanner myScanner = new Scanner(System.in);
         String userInput = myScanner.nextLine();
         while(!userInput.equals("bye")) {
-            System.out.println("    ____________________________________________________________\n" +
-                    "     " + userInput + "\n" +
-                    "    ____________________________________________________________");
+            if(userInput.equals("list")) {
+                printUserInputRecord(userInputRecord);
+            } else {
+                userInputRecord.add(userInput);
+                System.out.println("    ____________________________________________________________\n" +
+                        "      added: " + userInput + "\n" +
+                        "    ____________________________________________________________");
+            }
             userInput = myScanner.nextLine();
         }
+    }
+
+    private static void  printUserInputRecord(ArrayList<String> userInputRecord) {
+        if(userInputRecord.isEmpty()) {
+            System.out.println("    ____________________________________________________________\n" +
+                    "       Ah oh, seems like nothing is added yet :( \n" +
+                    "       Try to input something first! \n" +
+                    "    ____________________________________________________________"
+            );
+        }
+        System.out.println("    ____________________________________________________________ch");
+        for (int i = 0; i < userInputRecord.size(); i++) {
+            System.out.println("       " + (i + 1) + ". " + userInputRecord.get(i));
+        }
+        System.out.println("    ____________________________________________________________");
     }
 
     public static void main(String[] args) {
@@ -18,7 +41,7 @@ public class Duke {
                 "     Can I help you? \n" +
                 "    ____________________________________________________________";
         System.out.println(greeting);
-        echo();
+        echoWithAdd();
         System.out.println("    ____________________________________________________________\n" +
                 "     Bye. Hope to see you again soon!\n" +
                 "    ____________________________________________________________");
