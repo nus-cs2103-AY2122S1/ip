56a1476749a7a4a7f40f6f2b3cbc87b220dee032
Zhen Xuan <tanzhenxuan@gmail.com>
2021-08-16 01:41:42 +0800

    Complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+package main.java;
+import java.util.Scanner;
+
+/**
+ * Duke is a Personal Assistant Chatbot that helps a person
+ * keep track of various things.
+ */
 public class Duke {
+    //Standard replies and horizontal line
+    private static final String line = "\t____________________________________________________________\n";
+    private static final String intro = "Hello! I'm Duke\n\t What can I do for you?";
+    private static final String bye = "Bye. Hope to see you again soon!";
+
+    /**
+     * Provides horizontal lines with indentation.
+     *
+     * @param str
+     * @return formatted reply
+     */
+    private static String reply(String str) {
+        return line + "\t " + str + "\n" + line;
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(reply(intro));
+        Scanner sc = new Scanner(System.in);
+        boolean on = true;
+        while (on) {
+            String input = sc.nextLine();
+            switch (input) {
+                case "bye":
+                    on = false;
+                    System.out.println(reply(bye));
+                    sc.close();
+                    break;
+                default: System.out.println(reply(input));
+            }
+        }
     }
 }
