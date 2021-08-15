0893a5bc4a1f50377f67822a7af527c02fef8c82
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-08-15 21:56:58 +0800

    Format CommandType enum

--- src/main/java/duke/ui/CommandType.java
+++ src/main/java/duke/ui/CommandType.java
@@ -6,5 +6,11 @@ package duke.ui;
  * @author Jay Aljelo Saez Ting
  */
 public enum CommandType {
-    EXIT, ADD_TODO_TASK, LIST_TASKS, MARK_TASK_DONE, ADD_DEADLINE_TASK, ADD_EVENT_TASK, DELETE_TASK
+    EXIT,
+    ADD_TODO_TASK,
+    LIST_TASKS,
+    MARK_TASK_DONE,
+    ADD_DEADLINE_TASK,
+    ADD_EVENT_TASK,
+    DELETE_TASK
 }
