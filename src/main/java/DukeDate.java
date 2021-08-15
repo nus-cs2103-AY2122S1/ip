516ed040ba180b140b3dbdf89e09a95ecde09398
Jeff Sieu <jeffsy00@gmail.com>
2021-08-16 00:30:09 +0800

    Add date recognition - Level 8

---
+++ src/main/java/DukeDate.java
@@ -0,0 +1,43 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+/**
+ * Represents a date in Duke. Can be any arbitrary string or an actual date.
+ */
+public abstract class DukeDate {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd MMM yyyy");
+    public static DukeDate of(String date) {
+        try {
+            return new DukeLocalDate(LocalDate.parse(date));
+        } catch (DateTimeParseException e) {
+            return new DukeStringDate(date);
+        }
+    }
+
+    private static class DukeStringDate extends DukeDate {
+        private String date;
+
+        private DukeStringDate(String date) {
+            this.date = date;
+        }
+
+        @Override
+        public String toString() {
+            return date;
+        }
+    }
+
+    private static class DukeLocalDate extends DukeDate {
+        private LocalDate date;
+
+        private DukeLocalDate(LocalDate date) {
+            this.date = date;
+        }
+
+        @Override
+        public String toString() {
+            return date.format(DATE_TIME_FORMATTER);
+        }
+    }
+}
