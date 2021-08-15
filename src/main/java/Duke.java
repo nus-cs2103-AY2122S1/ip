7ab2de83422d37b24bff367c5b3c8ac81db6c665
Zheng Tao Jun <>
2021-08-15 17:39:27 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        //Greet
+        System.out.println("Hello! I'm Duke!\nWhat can I do for you?\n");
+
+        ArrayList<String> task = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (int i = 1; i < task.size() + 1; i++) {
+                    System.out.println(i + ". " + task.get(i - 1));
+                }
+            } else {
+                task.add(input);
+                System.out.println("added: " + input);
+            }
             input = sc.nextLine();
         }
 
