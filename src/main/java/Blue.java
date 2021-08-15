c89ee2e9ac327c1ec7ed11d31bf52f7f2fd395b7
GnohChengYi <chengyignoh@gmail.com>
2021-08-15 14:33:09 +0800

    Rename the name of chatbot to Blue, and update files that contain Duke

---
+++ src/main/java/Blue.java
@@ -0,0 +1,10 @@
+public class Blue {
+    public static void main(String[] args) {
+        String logo = " ____  _                \n"
+                + "|  . \\| | _   _   ____  \n"
+                + "|____/| || | | | /  _  \\\n"
+                + "|  . \\| || |_| ||   ___/\n"
+                + "|____/|_||_____| \\_____/\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
