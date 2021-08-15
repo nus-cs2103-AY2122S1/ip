8579de42235cbb204e73177c83cd831b6be71504
Kar Wi <t.karwi@yahoo.com>
2021-08-15 22:31:05 +0800

    Implement Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+        List<String> list = new ArrayList();
         String divider = "____________________________________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,11 +19,23 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
         while (!input.equals("bye")) {
-            // prompt for next input only when invoke next()
-            System.out.println(divider + "\n" + input + "\n" + divider);
+            // System.out.println(divider + "\n" + input + "\n" + divider);
+            if (input.equals("list")) {
+                System.out.println(divider);
+                int index = 1;
+                for (String item : list) {
+                    System.out.println(index + ". " + item);
+                    index++;
+                }
+                System.out.println(divider);
+            } else {
+                list.add(input);
+                System.out.println(divider + "\n" + "added: " + input + "\n" + divider);
+                // prompt for next input only when invoke next()
+            }
             input = sc.next();
         }
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider + "\n" + "Bye. Hope to see you again soon!" + "\n" + divider);
         sc.close();
     }
 }
