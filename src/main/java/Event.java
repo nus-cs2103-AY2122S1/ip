e1a07932edf0fd105d23897a797c1237ab48646d
ntwbruce <e0544240@u.nus.edu>
2021-08-16 02:51:41 +0800

    Created Event.java

---
+++ src/main/java/Event.java
@@ -0,0 +1,13 @@
+public class Event extends Task {
+    private final String period;
+
+    public Event(String taskName, String period) {
+        super(taskName);
+        this.period = period;
+    }
+
+    @Override
+    public String listEntry() {
+        return "[E]" + super.listEntry() + "at: " + this.period;
+    }
+}
