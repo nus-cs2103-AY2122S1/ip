7e08efbb951a8fda177051c8bfe1a37b979c2818
skythefire <minganntan@gmail.com>
2021-08-15 12:29:37 +0800

    Abstracted out store array into a Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Duke {
 
     private static final String LINE = "----------------------------------------------";
-    private String[] store = new String[100];
-    private int items = 0;
+//    private String[] store = new String[100];
+    private Task[] taskList = new Task[100];
+    private int size = 0;
 
     public void greeting() {
         // Credits to https://textart4u.blogspot.com/2014/04/frog-leap-unicode-copy-paste-text-art.html
@@ -30,14 +31,14 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println("See you again in my frog hole! RIBBIT!");
             } else if (input.equals("list")) {
-                for (int i = 0; i < items; i++) {
-                    System.out.println(i + 1 + ". " + store[i]);
+                for (int i = 0; i < size; i++) {
+                    System.out.println(i + 1 + "." + taskList[i].print());
                 }
                 System.out.println(LINE);
             } else {
-                store[items] = input;
-                items++;
-                System.out.println("croaked: " + input + "\n" + LINE);
+                taskList[size] = new Task(input);
+                System.out.println("croaked: " + taskList[size].getName() + "\n" + LINE);
+                size++;
             }
         } while (!input.equals("bye"));
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private final String name;
+    private boolean completed;
+
+    public Task(String name) {
+        this.name = name;
+        this.completed = false;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void done() {
+        this.completed = true;
+    }
+
+    public String print() {
+        if (this.completed) {
+            return "[X] " + name;
+        } else {
+            return "[ ] " + name;
+        }
+    }
+}
