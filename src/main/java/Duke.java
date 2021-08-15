1c5665d4a1ff2d32def74943e4f5deaea972a914
mokdarren <mokkwd@gmail.com>
2021-08-15 20:58:33 +0800

    .gitignore: Add line to ignore all .class files Duke: Add echo feature and program introduction

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String div = "____________________________________________________________\n";
+        System.out.println(div + "Hello! I'm Bobby\nWhat can I do for you?\n" + div);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(div + "Bye. Hope to see you again soon!\n" + div);
+                break;
+            }
+            System.out.println(div + userInput + "\n" + div);
+        }
     }
+
+
 }
