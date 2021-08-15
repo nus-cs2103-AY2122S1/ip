a458d2e7f3d4ed7d18e8b8051147cc405960b90c
e0543403 <jamesdouglashester@yahoo.com.sg>
2021-08-15 11:02:06 +0800

    Replace String list with Task list in DukeList.java

--- src/main/java/DukeList.java
+++ src/main/java/DukeList.java
@@ -1,12 +1,12 @@
 public class DukeList {
-    private String[] list = new String[100];
+    private Task[] list = new Task[100];
     private int count = 0;
 
     public DukeList() {}
 
 
     public void add(String text) {
-        list[count] = (count + 1) + ". " + text;
+        list[count] = new Task(text);
         count += 1;
         System.out.println("added: " + text);
     }
@@ -14,9 +14,9 @@ public class DukeList {
 
     public void list() {
         for (int i = 0; i < count; i++) {
-            System.out.println(list[i]);
+            System.out.println((i + 1) + "." + list[i].toString());
         }
     }
 
-    
+
 }
