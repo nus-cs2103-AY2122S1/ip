0bfd64ff264dd6d7cf1fde5596d0a102e12fddac
shaliniseshadri <77195499+shaliniseshadri@users.noreply.github.com>
2021-08-15 19:22:08 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.*;
+
 public class Duke {
+    private static final String HELLO = "\nHello! I'm Duke \nWhat can I do for you?\n";
+    private static final String LINE = "===============================================";
+    private static final String logo =
+            " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private static final String BYE = "\nBye. Hope to see you again soon!\n";
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke chatbot = new Duke();
+        chatbot.start();
+    }
+
+    public void start() {
+        System.out.println(logo + HELLO + LINE);
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(LINE + "\n" + userInput + "\n" + LINE);
+            userInput = sc.nextLine();
+        }
+        System.out.println(LINE + BYE + LINE);
+        sc.close();
     }
 }
+
