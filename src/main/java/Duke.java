e135849734684c21cbefa2c0da2cd4f9374a6a54
ntwbruce <e0544240@u.nus.edu>
2021-08-15 16:37:37 +0800

    Added add and list functionality to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,8 @@ public class Duke {
         Scanner input = new Scanner(System.in);
         boolean bye = false;
         String[] intro = {"What can I do for you?", "How can I help you?", "What's up?", "What's good?"};
-        String[] tone = {".", "?", "!", "~"};
+        String[] list = new String[100];
+        int listLength = 0;
 
         System.out.println("  ____________________________________________________________");
         System.out.printf("  Hello! I'm Duck.\n  %s\n", intro[(int)(intro.length * Math.random())]);
@@ -20,8 +21,13 @@ public class Duke {
             if (text.equals("bye")) {
                 System.out.println("  See you next time!");
                 bye = true;
+            } else if (text.equals("list")) {
+                for (int i = 0; i < listLength; ++i) {
+                    System.out.println("  " + (i + 1) + ". " + list[i]);
+                }
             } else {
-                System.out.println("  " + text + tone[(int)(tone.length * Math.random())]);
+                list[listLength++] = text;
+                System.out.println("  added: " + text);
             }
 
             System.out.println("  ____________________________________________________________\n");
