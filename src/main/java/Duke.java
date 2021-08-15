90bdf53d6d987d9438a06b93d4e4437e317fc709
houtenteo <houtenteo@gmail.com>
2021-08-15 18:29:01 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,7 @@ public class Duke {
     }
 
     public static void start() {
-        ArrayList<String> l = new ArrayList<String>();
+        ArrayList<Task> l = new ArrayList<Task>();
 
         System.out.println(
                 "Yo! Duke here \n"
@@ -27,17 +27,29 @@ public class Duke {
                 } else {
                     System.out.println("Your list items:");
                     for (int i = 0; i < listLength; i ++) {
-                        System.out.printf("%d. %s \n", i + 1, l.get(i));
+                        Task t = l.get(i);
+                        String statusIcon = t.getStatusIcon();
+                        System.out.printf("%d. %s %s \n", i + 1, statusIcon, t.getTaskName());
                     }
                 }
-
+            } else if (input.substring(0, 4).equals("done")) {
+                try {
+                    int index = Integer.parseInt(input.substring(5));
+                    l.get(index - 1).markComplete();
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid index, please try again");
+                } catch (IndexOutOfBoundsException ee) {
+                    System.out.println("Invalid index, please try again");
+                }
             } else {
-                l.add(input);
+                Task newTask = new Task(input, false);
+                l.add(newTask);
                 System.out.println("Added `" + input + "` to your list");
             }
             input = s.nextLine();
         }
 
         System.out.println("Good riddance! Time to continue my beauty sleep :)");
+
     }
 }
