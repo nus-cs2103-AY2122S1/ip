cc09377ecec71391620d01707036649ef87199d0
El0hime <dillontkw@gmail.com>
2021-08-16 00:06:47 +0800

    Level-2
    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,19 +12,37 @@ public class Duke {
             "     Hello! I'm Duke\n" +
             "     What can I do for you?\n" +
             "    ____________________________________________________________";
+        //Array to store whatever text entered by the user
+        String[] userList = new String[100];
+        //Pointer for array
+        int pointer = 0;
 
         Scanner userInput = new Scanner(System.in);
         System.out.println(greetingStatement);
 
         while (true) {
-            String userName = userInput.nextLine();
+            String task = userInput.nextLine();
             //Stop duke if user types "bye"
-            if (userName.equals("bye")) {
+            if (task.equals("bye")) {
                 break;
             }
-            System.out.println("    ____________________________________________________________\n    " +
-                userName + "\n" +
-                "    ____________________________________________________________");
+            //prints out all the stored tasks if user types "list"
+            else if (task.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < pointer; i++) {
+                    System.out.println("    " + (i + 1 +  ". " + userList[i]));
+                }
+                System.out.println("    ____________________________________________________________");
+            }
+            //else add the task to array and print it out to user
+            else {
+                userList[pointer] = task;
+                pointer++;
+                System.out.println("    ____________________________________________________________\n    " +
+                    "added: " + task + "\n" +
+                    "    ____________________________________________________________");
+            }
+
         }
 
         System.out.println(byeStatement);
