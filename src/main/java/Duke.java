f589c7f480cd53dc459a6605b99e33dcf3568a4b
ngjiayuan <ngjiayuan.z@gmail.com>
2021-08-15 22:12:41 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,8 @@ public class Duke {
     private static boolean inSession;
     private static String greetMessage = "Hello! I'm Duke \nWhat can I do for you?";
     private static String exitMessage = "Bye. Hope to see you again soon!";
+    private static String[] toDoList = new String[100];
+    private static int counter = 0;
 
     public static void start() {
         inSession = true;
@@ -15,8 +17,11 @@ public class Duke {
                 case ("bye"):
                     exit();
                     break;
+                case ("list"):
+                    displayList();
+                    break;
                 default:
-                    echo(input);
+                    add(input);
             }
         }
     }
@@ -30,8 +35,20 @@ public class Duke {
         System.out.println(exitMessage);
     }
 
-    public static void echo(String input) {
-        System.out.println(input);
+    public static void add(String input) {
+        toDoList[counter] = input;
+        counter++;
+        System.out.println("added: " + input);
+    }
+
+    public static void displayList() {
+        int count = 1;
+        for (String s : toDoList) {
+            if (s != null) {
+                System.out.println(count + ". " + s);
+                count++;
+            }
+        }
     }
 
     public static void main(String[] args) {
