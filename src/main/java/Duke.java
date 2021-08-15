b9f07ec650f896e8cd82d4c2bc6eb325550e9e3b
EssWhyy <swenyitang99@gmail.com>
2021-08-15 21:22:01 +0800

    Implement level 2 of individual project
    New features:
    *App now keeps track of items entered by user, up to a number of 100 items
    *App can list items entered by user if they type 'list'
    
    Edited:
    *Starting instructions to describe new features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,9 +8,13 @@ public class Duke {
                 + " | '  \\| '_| | '  \\/ -_) -_|_-</ -_) -_) / /(_-<\n"
                 + " |_|_|_|_|   |_|_|_\\___\\___/__/\\___\\___|_\\_\\/__/\n"
                 + "\n";
+        String[] items = new String[100];
+        int currentIndex = 0;
+
         System.out.println(logo);
         System.out.println("I'm Mr Meeseeks look at me!");
-        System.out.println("Type in anything and I will repeat it to you!");
+        System.out.println("Type in anything and I will keep track of it for you!");
+        System.out.println("Type \"list\" to show all items inputted so far");
         System.out.println("Type \"bye\" to exit");
 
 
@@ -21,7 +25,23 @@ public class Duke {
                 break;
             }
 
-            System.out.println(input);
+            if (input.equals("list")){
+                //list out all tracked items
+                System.out.println("Items I have so far:");
+                for (int i = 1; i<currentIndex+1; i++){
+                    System.out.println(i + ". " + items[i-1]);
+                }
+            } else {
+                if (currentIndex == 99) {
+                    //prevent out of bounds error
+                    System.out.println("Oops! My task list is full. No new items can be added");
+                } else {
+                    //add item to the end of the array.
+                    System.out.println("Added in " + input);
+                    items[currentIndex] = input;
+                    currentIndex++;
+                }
+            }
         }
     }
 }
