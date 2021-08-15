bfa5aa13a8a367b8328d9b4e2eee5ccc0a92bb6f
dannylty <dannylty98@gmail.com>
2021-08-15 23:51:37 +0800

    Add level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static String echo(String inputString) {
+        return inputString;
+    }
+
+    public static String styleResponse(String inputString) {
+        return "     --------------------\n     " + inputString + "\n     --------------------";
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello from\n" + logo);
+        while (true) {
+            String currLine = sc.nextLine();
+            if (currLine.equals("bye")) {
+                System.out.println(styleResponse("Bye. Hope to see you again soon!"));
+                break;
+            }
+            System.out.println(styleResponse(echo(currLine)));
+        }
+        sc.close();
     }
 }
