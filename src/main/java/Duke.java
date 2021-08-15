3551699822a68fe25a58c19f99180f8ef1a1f59c
Lemonsr <46766154+Lemonsr@users.noreply.github.com>
2021-08-15 11:03:08 +0800

    Add Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -11,11 +13,29 @@ public class Duke {
 
         String divHead = "╔═══════════*.·:·.☽✧    ✦    ✧☾.·:·.*═══════════╗\n";
         String divTail = "\n╚═══════════*.·:·.☽✧    ✦    ✧☾.·:·.*═══════════╝\n";
+
+        String[] list = new String[100];
+        int numItems = 0;
+
         System.out.println(logo + divHead + "⚜  Hello, i'm Ai-su! How may I help you today?  ⚜" + divTail);
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(divHead + ' ' + input + divTail);
+            if (input.equals("list")) {
+                System.out.println(divHead);
+                if (numItems == 0) {
+                    System.out.println("There's nothing added to the list yet!");
+                } else {
+                    for (int i = 0; i < numItems; i++) {
+                        System.out.println(i + 1 + ". " + list[i]);
+                    }
+                }
+                System.out.println(divTail);
+            } else {
+                list[numItems] = input;
+                numItems += 1;
+                System.out.println(divHead + ' ' + "added: " + input + divTail);
+            }
             input = sc.nextLine();
         }
         System.out.println(divHead + " See you next time!" + divTail);
