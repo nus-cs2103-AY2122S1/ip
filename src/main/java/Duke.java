660142f618d61607c91ca991a94b7e8eeacdb040
Ng Xiang Jun <ngxiangjun1999@gmail.com>
2021-08-15 15:55:39 +0800

    Add greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
+    public void run() {
+        this.greetUser();
+    }
+
+    public void printMessage(String message) {
+        String formatDisplay = String.format("    %s", message);
+        System.out.println(formatDisplay);
+    }
+
+    public void greetUser() {
+        String greetMessage = "Hello! I'm Saitama";
+        String detailsMessage = "I do 100 sit-ups, 100 push-ups, 100 squats and a 10 kilometer run every day! No cap";
+        printMessage(greetMessage);
+        printMessage(detailsMessage);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        new Duke().run();
     }
 }
