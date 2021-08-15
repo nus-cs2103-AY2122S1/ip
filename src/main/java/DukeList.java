8a37c81d96ee5de9b81d688270f4d728b31c1f99
e0543403 <jamesdouglashester@yahoo.com.sg>
2021-08-15 09:50:06 +0800

    Add add to DukeList.java

--- src/main/java/DukeList.java
+++ src/main/java/DukeList.java
@@ -1,5 +1,15 @@
 public class DukeList {
     private String[] list = new String[100];
+    private int count = 0;
 
     public DukeList() {}
+
+
+    public void add(String text) {
+        list[count] = (count + 1) + ". " + text;
+        count += 1;
+        System.out.println("added: " + text);
+    }
+
+    
 }
