23f3adbcc27e7fa77537fa1d92f6a955737ee21e
Willy Angga <ampedwilly@gmail.com>
2021-08-15 22:38:06 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -6,13 +8,20 @@ public class Duke {
         Scanner scan = new Scanner(System.in);
         System.out.println("Hello! I'm Amped");
         System.out.println("What can I do for you?");
+        List<String> ls = new ArrayList<>();
         do {
             input = scan.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Good Bye. Have a nice day!");
             }
+            else if (input.equals("list")) {
+                for(int i = 0; i < ls.size(); i++) {
+                    System.out.println((i + 1) + ". " + ls.get(i));
+                }
+            }
             else {
-                System.out.println(input);
+                System.out.println("added: " + input);
+                ls.add(input);
             }
         } while (!input.equals("bye"));
     }
