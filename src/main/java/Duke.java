debd49c4261a7bb9e608000298f7e52ed24c2ac4
shaliniseshadri <77195499+shaliniseshadri@users.noreply.github.com>
2021-08-15 19:42:11 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,6 +11,7 @@ public class Duke {
             + "|____/ \\__,_|_|\\_\\___|\n";
     private static final String BYE = "\nBye. Hope to see you again soon!\n";
 
+    private final ArrayList<String> taskList = new ArrayList<>();
 
     public static void main(String[] args) {
         Duke chatbot = new Duke();
@@ -22,11 +23,27 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println(LINE + "\n" + userInput + "\n" + LINE);
+            if (userInput.equals("list")) {
+                printList(taskList);
+            } else {
+                addTaskToList(userInput, taskList);
+            }
             userInput = sc.nextLine();
         }
         System.out.println(LINE + BYE + LINE);
         sc.close();
     }
-}
 
+    public void printList(ArrayList<String> list) {
+        for(int i = 0; i < list.size(); i++) {
+            int ind = i + 1;
+            System.out.println(ind + ". " + list.get(i));
+        }
+        System.out.println(LINE);
+    }
+
+    public void addTaskToList(String task, ArrayList<String> list) {
+        list.add(task);
+        System.out.println("added: " + task + "\n" + LINE);
+    }
+}
