2cace461dcc270948e1e7536309f62c0a16f3cbf
Py0000 <>
2021-08-15 18:38:17 +0800

    Level 5. Add New Exceptions

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class DukeException {
+public class DukeException extends Exception {
+
+    public DukeException(String message) {
+        super(message);
+    }
 }
+
+class UnknownTagException extends DukeException {
+    public UnknownTagException() {
+        super("No such tag! Please input the correct tag!");
+    }
+}
+
+class NoSuchTaskException extends DukeException {
+    public NoSuchTaskException() {
+        super("You do not have this many task added yet, please check and try again.");
+    }
+}
+
+class IllegalFormatException extends DukeException {
+    public IllegalFormatException() {
+        super("Wrong Format used! Please you the correct format!");
+    }
+}
+
