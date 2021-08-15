821a6fde83e11e2c13e5a33cf64d9f7b51180e5e
Gabau <au.gabriel@yahoo.com.sg>
2021-08-15 10:57:57 +0800

    Completed level 1 tasks.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private static String greetings = "Greetings! I'm Duke\n\tWhat can I do for you?";
+    private static String bye = "Godspeed young padawan!";
+    private static String hline = "\t----------------------------";
+    private void print(String s) {
+        System.out.println(hline);
+        System.out.println("\t" + s);
+        System.out.println("\n" + hline);
+    }
+    private void print_logo() {
+        System.out.println("Hello from\n" + this.logo);
+
+    }
+
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        //initialising Duke
+        this.print_logo();
+        this.print(greetings);
+        String inpt = sc.nextLine();
+        while(!inpt.equals("bye")) {
+            print(inpt);
+            inpt = sc.nextLine();
+        }
+        this.print(bye);
+    }
+
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke d = new Duke();
+        d.run();
+
+
+
     }
 }
