b1fde843eeb854532bd5f5af6d6867c4164af749
wz27 <howenzhong@gmail.com>
2021-08-15 14:32:23 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
+        displayLogo();
+        greet();
+        echo();
+    }
+
+    public static void displayLogo() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+    }
+
+    public static void greet() {
+        String greeting = "Hello! I'm Duke\n"
+                + "What can I do for you?\n";
+        System.out.println(greeting);
+    }
+
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String str = sc.nextLine();
+            if (str.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(str);
+        }
     }
 }
