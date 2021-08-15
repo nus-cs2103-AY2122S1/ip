67d843f8afc60fb4612a500f6dbb731885938bbd
trash-bin99 <77224811+trash-bin99@users.noreply.github.com>
2021-08-15 22:43:36 +0800

    Add Level-1 increment.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "\nWhat can I do for you today?");
+
+        while(true) {
+            String input = scan.next();
+
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(input);
+        }
+        scan.close();
+        System.out.println("See you again next time!");
+
     }
 }
