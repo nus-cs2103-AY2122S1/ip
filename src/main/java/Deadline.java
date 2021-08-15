87df72de54c0ca66d731fb689fe1ecd0090c5de0
ntwbruce <e0544240@u.nus.edu>
2021-08-16 02:49:16 +0800

    Created Deadline.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,13 @@
+public class Deadline extends Task {
+    private final String deadline;
+
+    public Deadline(String taskName, String deadline) {
+        super(taskName);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String listEntry() {
+        return "[D]" + super.listEntry() + "by: " + this.deadline;
+    }
+}
