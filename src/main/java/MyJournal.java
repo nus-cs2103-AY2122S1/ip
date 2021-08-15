230da98ed5f18768f105026a4388f279118b6639
felissaf <felissafaustine@gmail.com>
2021-08-15 22:50:54 +0800

    added mark as done function

--- src/main/java/MyJournal.java
+++ src/main/java/MyJournal.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class MyJournal {
     public static void main(String[] args) {
-        String[] items = new String[100];
+        Task[] items = new Task[100];
         String ans = "";
         Scanner reader = new Scanner(System.in);
         int count = 0;
@@ -17,10 +17,26 @@ public class MyJournal {
                 break;
             } else if (ans.equals("list")) {
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + items[i]);
+                    System.out.println((i + 1) + ". " + (items[i].getState() ? "[X] ": "[ ] ") + items[i].getTaskName());
                 }
+            } else if (ans.substring(0,4).equals("done")) {
+//                if (reader.hasNextInt()) {
+                int index = Integer.parseInt(ans.substring(5, ans.length()), 10) - 1;
+//                            reader.nextInt();
+//
+                if (items[index] == null || index >= count) {
+                    System.out.println("That task does not exist");
+                    continue;
+                } else {
+                    items[index].setState(true);
+                    System.out.println("Okay!! I have marked this task as done:\n"
+                            + "[X] " + items[index].getTaskName());
+                }
+//                } else {
+//                    System.out.println("Which task do you want to mark as done?");
+//                }
             } else {
-                items[count] = ans;
+                items[count] = new Task(ans);
                 System.out.println("added: " + ans);
                 count++;
             }
