505de843ef638be850878f881c63f547ac829ea3
nicolej2122 <nicole.joseph2122@gmail.com>
2021-08-15 22:18:09 +0800

    Add Greet, Echo and Exit commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "   _       _           \n"
+                + "  /_\\  ___| |__  _   _ \n"
+                + " //_\\\\/ __| '_ \\| | | |\n"
+                + "/  _  \\__ \\ | | | |_| |\n"
+                + "\\_/ \\_/___/_| |_|\\__, |\n"
+                + "                 |___/ \n";
+
+        //Greeting message
+        System.out.println("Hello! I'm\n" +logo+ "\nWhat can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        //Echo
+        while(sc.hasNext()){
+            //Exit command
+            System.out.println("............................................................");
+            String command = sc.nextLine().trim();
+            if(command.equalsIgnoreCase("bye")){
+                System.out.println("\n\tByeBye! Hope to see you again!");
+                break;
+            }else {
+                System.out.println("\n\t"+command);
+            }
+            System.out.println("____________________________________________________________");
+        }
+        sc.close();
     }
 }
