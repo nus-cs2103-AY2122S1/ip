3d085bc8aeb80be99873636ea4fbdd676d4413ac
Ng Xiang Jun <ngxiangjun1999@gmail.com>
2021-08-15 16:05:50 +0800

    Add echo feature that exits program when user types bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Duke {
     public void run() {
         this.greetUser();
+        this.echo();
+        this.exit();
     }
 
     public void printMessage(String message) {
@@ -13,6 +17,23 @@ public class Duke {
         String detailsMessage = "I do 100 sit-ups, 100 push-ups, 100 squats and a 10 kilometer run every day! No cap";
         printMessage(greetMessage);
         printMessage(detailsMessage);
+    }
+
+    public void echo() {
+        Scanner sc = new Scanner(System.in);
+        boolean continueEcho = true;
+        while (continueEcho) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                continueEcho = false;
+            } else {
+                printMessage(userInput);
+            }
+        }
+    }
+
+    public void exit() {
+        printMessage("Hope to see you again!! ^_^");
     }
 
     public static void main(String[] args) {
