7f9c1e409bc1f6e487ab7828946785f903d0299f
El0hime <dillontkw@gmail.com>
2021-08-15 23:50:17 +0800

    Level-1
    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //Statement to show the user upon exit
+        String byeStatement = "    ____________________________________________________________\n" +
+            "     Bye. Hope to see you again soon!\n" +
+            "    ____________________________________________________________";
+
+        //Statement to show the user upon running Duke
+        String greetingStatement = "    ____________________________________________________________\n" +
+            "     Hello! I'm Duke\n" +
+            "     What can I do for you?\n" +
+            "    ____________________________________________________________";
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println(greetingStatement);
+
+        while (true) {
+            String userName = userInput.nextLine();
+            //Stop duke if user types "bye"
+            if (userName.equals("bye")) {
+                break;
+            }
+            System.out.println("    ____________________________________________________________\n    " +
+                userName + "\n" +
+                "    ____________________________________________________________");
+        }
+
+        System.out.println(byeStatement);
+
     }
 }
