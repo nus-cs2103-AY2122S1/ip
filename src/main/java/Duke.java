1a4c26228b0f276d006b9b4a70b4034287959af6
Nay Lin Han <jamesliu09@gmail.com>
2021-08-16 02:08:59 +0800

    Added Greeting, echoes user input, and exits on 'bye' user input.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String indent = "    ____________________________________________________________\n";
+        String tab = "     ";
+        System.out.println(indent + tab + "Hello! I'm Duke\n" + tab + "What can I do for you?\n" + indent);
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            String input = sc.next();
+            if (input.equals("bye")) {
+                System.out.println(indent + tab + "Bye. Hope to see you again soon!\n" + indent);
+                break;
+            }
+            System.out.println(indent + tab + input + "\n" + indent);
+        }
     }
 }
