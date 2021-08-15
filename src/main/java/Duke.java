aabdcedb10a4d679a35852614ae5a9d2b6961108
lowkangn <e0543462@u.nus.edu>
2021-08-15 19:47:42 +0800

    Skeletal version of Duke, echos commands entered

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner sc = new Scanner(System.in);
+        boolean dukeOpen = true;
+
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+        while (dukeOpen) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                dukeOpen = false;
+                System.out.println("Bye. Hope to see you again soon!");
+            } else {
+                System.out.println(userInput);
+            }
+        }
     }
 }
