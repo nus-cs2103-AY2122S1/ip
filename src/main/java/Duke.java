56b8b41341764aefae49a02360c71c7742739557
Tan Jia Xian <e0543981@u.nus.edu>
2021-08-15 20:24:58 +0800

    added commands for adding task and retrieving task list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> taskList = new ArrayList<>();
 
+        // Hello message
         System.out.println(
             "Lollipop: Hello! I am your personal chatbot, Lollipop! " +
             "What would you like to do today?");
         System.out.print("You: ");
         String command = scanner.nextLine();
 
+        // Commands
         while (!command.equals("bye")) {
-            System.out.println("Lollipop: " + command);
+            if (command.equals("list")) {
+                System.out.println("Lollipop: Here are your tasks");
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println((i + 1) + ". " + taskList.get(i));
+                }
+            } else {
+                taskList.add(command);
+                System.out.println("Lollipop: " + command + " has been added.");
+            }
+
             System.out.print("You: ");
             command = scanner.nextLine();
         }
 
-        System.out.println(
-            "Lollipop: See you again soon!");
+        // Goodbye message
+        System.out.println("Lollipop: See you again soon!");
     }
 }
