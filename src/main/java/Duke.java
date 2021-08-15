b8a2b675056b97b5d772c0a715e7a9de3a9f818f
ramapriyan912001 <ramapriyan912001@gmail.com>
2021-08-15 16:49:13 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,21 +3,63 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public ArrayList<String> tasks = new ArrayList<>();
+    public ArrayList<Task> tasks = new ArrayList<>();
+
+    public class Task {
+        public String task;
+        public Boolean done;
+
+        public Task(String task, Boolean done) {
+            this.task = task;
+            this.done = done;
+        }
+
+        public Task(String task) {
+            this.task = task;
+            this.done = false;
+        }
+
+        public void setDone(Boolean done) {
+            this.done = done;
+        }
+
+        public String checkBox() {
+            if(this.done) {
+                return "[X]";
+            } else {
+                return "[ ]";
+            }
+        }
+
+        @Override
+        public String toString() {
+            return checkBox() + " " + task;
+        }
+    }
 
     public String byeString() {
         return "Bye. Hope to see you again soon!";
     }
 
-    public void addTask(String task) {
+    public void addTask(Task task) {
         tasks.add(task);
         System.out.println("added: " + task);
     }
 
+    public void doneTask(int taskNumber) {
+        taskNumber--;
+        Task task = tasks.get(taskNumber);
+        task.setDone(true);
+        tasks.set(taskNumber, task);
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(task);
+    }
+
     public void taskListString() {
         StringBuilder ans = new StringBuilder();
+        ans.append("Here are the tasks in your list:\n");
         for(int i = 0; i < tasks.size(); i++) {
-            String task = tasks.get(i);
+            Task task = tasks.get(i);
             if(i != tasks.size()-1) {
                 ans.append(String.format("%d. %s\n", i + 1, task));
             } else {
@@ -41,8 +83,11 @@ public class Duke {
     public void interpretInput(String input) {
         if(input.equals("list")) {
             taskListString();
-        } else {
-            addTask(input);
+        } else if(input.startsWith("done")) {
+            doneTask(Integer.parseInt(input.substring(5)));
+        }
+        else {
+            addTask(new Task(input));
         }
     }
 
