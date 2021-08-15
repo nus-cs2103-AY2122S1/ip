919209fec73d5b2ba28bf77d8b231fab08d2bc9e
ngjiayuan <ngjiayuan.z@gmail.com>
2021-08-15 22:09:43 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static boolean inSession;
+    private static String greetMessage = "Hello! I'm Duke \nWhat can I do for you?";
+    private static String exitMessage = "Bye. Hope to see you again soon!";
+
+    public static void start() {
+        inSession = true;
+        greet();
+        while (inSession) {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine().toLowerCase();
+            switch (input) {
+                case ("bye"):
+                    exit();
+                    break;
+                default:
+                    echo(input);
+            }
+        }
+    }
+
+    public static void greet() {
+        System.out.println(greetMessage);
+    }
+
+    public static void exit() {
+        inSession = false;
+        System.out.println(exitMessage);
+    }
+
+    public static void echo(String input) {
+        System.out.println(input);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke.start();
     }
 }
