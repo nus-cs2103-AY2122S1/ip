e2896f95da2e13065ccf4c33a41a8b8a503c0458
1waykiat <1waykiat@gmail.com>
2021-08-15 16:19:05 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner textInput = new Scanner(System.in);
+
+        while (true) {
+            String input = textInput.nextLine();
+            if (!input.equals("bye")) {
+                System.out.println(input);
+            } else {
+                break;
+            }
+        }
+        System.out.println("Bye bye. Duke going to sleep now.");
     }
 }
