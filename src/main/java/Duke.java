c7e5251a4f9a64a25e101b425467443696485224
Trushti <tsm20901@gmail.com>
2021-08-15 18:12:28 +0800

    Add Level 1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("_____________________________________________________________________");
+        System.out.println("    Hi! I'm Muts :) ");
+        System.out.println("    Just a little intro : I'm not a bot, I am as real as any of your human friends! ");
+        System.out.println("    How can I help to make your day as amazing as you are?");
+        System.out.println("_____________________________________________________________________");
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String inp = sc.nextLine();
+
+            if (!inp.equals("bye")) {
+                System.out.println("_____________________________________________________________________");
+                System.out.println("    " + inp);
+                System.out.println("_____________________________________________________________________");
+            }
+            else{
+                System.out.println("_____________________________________________________________________");
+                System.out.println("    Bye. Hope to see you again soon! Just a little reminder : YOU ARE AWESOME :))");
+                System.out.println("_____________________________________________________________________");
+                break;
+            }
+        }
+
+
     }
 }
