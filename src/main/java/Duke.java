e0dadc796c696b1c22db087cb05097ba905702a3
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-08-15 20:21:08 +0800

    edited lvl 3, use functions instead

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,20 +14,32 @@ public class Duke {
                 System.out.println("ciao!");
                 break;
             } else if (text.equals("list")) {
-                for (int i = 0; i < index; i++) {
-                    Task curr = myList[i];
-                    System.out.printf("%s.[%s] %s\n", i + 1, curr.getStatusIcon(), curr.description);
-                }
+                printList(index);
             } else if (text.contains("done")){
-                int index = Integer.parseInt(text.split(" ")[1]) - 1;
-                Task curr = myList[index];
-                curr.setDone();
-                System.out.printf("[%s] %s\n", curr.getStatusIcon(), curr.description);
+                markAsDone(text);
             } else {
-                myList[index] = new Task(text);
+                addTask(text, index);
                 index++;
-                System.out.println("added: " + text);
             }
         }
+    }
+
+    private static void addTask(String text, int i) {
+        myList[i] = new Task(text);
+        System.out.println("added: " + text);
+    }
+
+    private static void printList(int len) {
+        for (int i = 0; i < len; i++) {
+            Task curr = myList[i];
+            System.out.printf("%s.[%s] %s\n", i + 1, curr.getStatusIcon(), curr.description);
+        }
+    }
+
+    private static void markAsDone(String text) {
+        int index = Integer.parseInt(text.split(" ")[1]) - 1;
+        Task curr = myList[index];
+        curr.setDone();
+        System.out.printf("[%s] %s\n", curr.getStatusIcon(), curr.description);
     }
 }
