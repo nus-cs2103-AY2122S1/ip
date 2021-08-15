ec34bfd09ddbe8af319f62a555ecef5d15bbdc30
Zhou-Jiahao-1998 <54730603+Zhou-Jiahao-1998@users.noreply.github.com>
2021-08-15 22:49:16 +0800

    Completed level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void greet() {
+        String message = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(message);
+    }
+
+    public static void echo(String input) {
+        if (input != "bye") {
+            System.out.println(input);
+        } else {
+            System.out.println("TEST");
+            bye();
+        }
+    }
+
+    public static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args)
+            throws IOException
+    {
+        greet();
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        String input;
+
+        while (!(input = reader.readLine()).equals("bye")) {
+            echo(input);
+        }
+        bye();
     }
 }
