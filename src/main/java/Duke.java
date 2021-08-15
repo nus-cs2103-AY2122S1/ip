2cc0ae7be204ef796205d3914f3e49cc464ffd99
LauTengHon <e0543465@u.nus.edu>
2021-08-15 16:29:00 +0800

    add greet, echo and exit functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        ChatBot bot = new ChatBot();
+        System.out.println(bot.getStartMessage());
+
+        // create object of Scanner to take inputs
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        Boolean running = true;
+
+
+        while (running) {
+            if (input.equals("bye")) {
+                System.out.println(bot.getExitMessage());
+                running = false;
+            } else {
+                System.out.println(input);
+                input = sc.nextLine();
+            }
+        }
+        sc.close();
+
+
     }
+}
+
+// default modifier this class is only used in Duke.java
+class ChatBot {
+
+    static String line = "______________________________________\n";
+
+    public String getStartMessage() {
+        return line + "Hello! I'm Duke\n" + "What can I do for you?\n" + line;
+    }
+
+    public String getExitMessage() {
+        return line + "bye! for now...\n" + line;
+    }
+
 }
