165dd332a0560e0a9d42d0ec61521720cf205bc1
ramapriyan912001 <ramapriyan912001@gmail.com>
2021-08-15 16:36:25 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,28 @@ import java.util.Scanner;
 
 public class Duke {
 
+    public ArrayList<String> tasks = new ArrayList<>();
+
     public String byeString() {
         return "Bye. Hope to see you again soon!";
+    }
+
+    public void addTask(String task) {
+        tasks.add(task);
+        System.out.println("added: " + task);
+    }
+
+    public void taskListString() {
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0; i < tasks.size(); i++) {
+            String task = tasks.get(i);
+            if(i != tasks.size()-1) {
+                ans.append(String.format("%d. %s\n", i + 1, task));
+            } else {
+                ans.append(String.format("%d. %s", i + 1, task));
+            }
+        }
+        System.out.println(ans);
     }
 
     public void run() {
@@ -19,7 +39,11 @@ public class Duke {
     }
 
     public void interpretInput(String input) {
-        System.out.println(input);
+        if(input.equals("list")) {
+            taskListString();
+        } else {
+            addTask(input);
+        }
     }
 
     public static void main(String[] args) {
