8518b1b8043f7288a48f278e32b6e8e7bd97ede5
G <garyyansen@gmail.com>
2021-08-15 13:56:59 +0800

    Complete level 4

--- src/main/java/Nyx.java
+++ src/main/java/Nyx.java
@@ -21,33 +21,57 @@ public class Nyx {
         Scanner sc = new Scanner(System.in);
         ArrayList<Task> taskList = new ArrayList<>();
         while (true) {
-            String command = sc.nextLine();
+            String input = sc.nextLine();
+            String[] splitInput = input.split(" ", 2);
+            String command = splitInput[0];
+            String info = "";
+            if (splitInput.length > 1) {
+                info = splitInput[1];
+            }
             System.out.println(LINE);
 
-            if (command.equals("list")) {
-                System.out.println(INDENTATION + "Here are the tasks in your list:\n");
+            if (input.equals("list")) {
+                System.out.println(INDENTATION + "Here are the tasks in your list:");
                 for (int i = 1; i <= taskList.size(); i++) {
                     System.out.printf("%s%d. %s%n",INDENTATION, i, taskList.get(i - 1));
                 }
-            } else if (command.equals("bye")) {
+            } else if (input.equals("bye")) {
                 System.out.println(INDENTATION + "Bye. Hope to see you again soon!\n");
                 System.out.println(LINE + "\n");
                 break;
-            } else if (command.contains("done ")) {
-                String taskNum = command.split(" ")[1];
-
-                if (taskNum.matches("\\d") && (Integer.parseInt(taskNum) - 1) < taskList.size()) {
-                    int index = Integer.parseInt(taskNum) - 1;
+            } else if (command.equals("done")) {
+                if (info.matches("\\d") && (Integer.parseInt(info) - 1) < taskList.size()) {
+                    int index = Integer.parseInt(info) - 1;
                     taskList.get(index).setDone();
                     System.out.printf("%sNice! I've marked this task as done:%n%s  %s%n",
                             INDENTATION, INDENTATION, taskList.get(index));
                 } else {
                     System.out.println(INDENTATION + "Invalid task index. Please try again!");
                 }
-            } else {
-                Task task = new Task(command);
+            } else if (command.equals("todo")) {
+                ToDo task = new ToDo(info);
                 taskList.add(task);
-                System.out.println(INDENTATION + "added: " + command);
+                System.out.printf("%sGot it. I've added this task:%n%s  %s%n",
+                        INDENTATION, INDENTATION, task);
+                System.out.printf("%sNow you have %d tasks in the list.%n", INDENTATION, taskList.size());
+            } else if (command.equals("deadline")) {
+                String[] splitInfo = info.split("/");
+                String date = splitInfo[1].split(" ",2)[1];
+                Deadline task = new Deadline(splitInfo[0].strip(), date);
+                taskList.add(task);
+                System.out.printf("%sGot it. I've added this task:%n%s  %s%n",
+                        INDENTATION, INDENTATION, task);
+                System.out.printf("%sNow you have %d tasks in the list.%n", INDENTATION, taskList.size());
+            } else if (command.equals("event")) {
+                String[] splitInfo = info.split("/");
+                String date = splitInfo[1].split(" ",2)[1];
+                Event task = new Event(splitInfo[0].strip(), date);
+                taskList.add(task);
+                System.out.printf("%sGot it. I've added this task:%n%s  %s%n",
+                        INDENTATION, INDENTATION, task);
+                System.out.printf("%sNow you have %d tasks in the list.%n", INDENTATION, taskList.size());
+            } else {
+                System.out.println("I dont understand this command... Please try again.");
             }
             System.out.println(LINE + "\n");
         }
