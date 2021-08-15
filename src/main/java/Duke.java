cc6b8fc24b71b6a9054b54ad27db238ce71c78b8
Lemonsr <46766154+Lemonsr@users.noreply.github.com>
2021-08-15 10:37:25 +0800

    Customize Duke (now Aisu)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,19 +2,22 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String divider = "____________________________________________________________";
-        System.out.println(divider + "\n Hello! I'm Duke\n What can I do for you?\n" + divider + '\n');
+        String logo = " █████╗  ██╗      ███████╗██╗   ██╗ \n" +
+                      " ██╔══██╗██║      ██╔════╝██║   ██║ \n" +
+                      " ███████║██║█████╗███████╗██║   ██║ \n" +
+                      " ██╔══██║██║╚════╝╚════██║██║   ██║ \n" +
+                      " ██║  ██║██║      ███████║╚██████╔╝ \n" +
+                      " ╚═╝  ╚═╝╚═╝      ╚══════╝ ╚═════╝ \n";
+
+        String divHead = "╔═══════════*.·:·.☽✧    ✦    ✧☾.·:·.*═══════════╗\n";
+        String divTail = "\n╚═══════════*.·:·.☽✧    ✦    ✧☾.·:·.*═══════════╝\n";
+        System.out.println(logo + divHead + "⚜  Hello, i'm Ai-su! How may I help you today?  ⚜" + divTail);
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(divider + "\n " + input + "\n\n" + divider + '\n');
+            System.out.println(divHead + ' ' + input + divTail);
             input = sc.nextLine();
         }
-        System.out.println(divider + "\n Bye. Hope to see you again soon!\n\n" + divider + '\n');
+        System.out.println(divHead + " See you next time!" + divTail);
     }
 }
