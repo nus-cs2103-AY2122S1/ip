b53a0732b2e13a1d36765b647a1a455930f18d3e
Tan Jia Xian <e0543981@u.nus.edu>
2021-08-15 18:26:41 +0800

    added greet echo exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(
+            "Lollipop: Hello! I am your personal chatbot, Lollipop! " +
+            "What would you like to do today?");
+        System.out.print("You: ");
+        String command = scanner.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println("Lollipop: " + command);
+            System.out.print("You: ");
+            command = scanner.nextLine();
+        }
+
+        System.out.println(
+            "Lollipop: See you again soon!");
     }
 }
