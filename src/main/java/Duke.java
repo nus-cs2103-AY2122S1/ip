98015d4df53d75899498f6a050e28c25055bfd25
Mohamed-Noriman <seaweed.iman@gmail.com>
2021-08-15 20:04:53 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,26 @@
+import java.util.ArrayList;
 import java.util.Scanner;  // Import the Scanner class
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> todo = new ArrayList<>();
         String line = "-------------------------------------";
-        System.out.println(line + "\n" + "Hello I am Duke\nWhat can I do for you today?\n" + line);
+        System.out.println(line + "\n" + "Good Morning Master Wayne, Alfred here.\nWhat can I do for you today?\n" + line);
 
         String command = scanner.nextLine();
         while (!command.equals("bye")) {
-            System.out.println(line + "\n" + command + "\n" + line);
-            command = scanner.nextLine();
+            if(command.equals("list")) {
+                for(int i = 0; i < todo.size(); i++) {
+                    System.out.println(i + ". " + todo.get(i));
+                }
+                command = scanner.nextLine();
+            } else {
+                System.out.println(line + "\n" + "Understood, Master Wayne. Added: " + command + "\n" + line);
+                todo.add(command);
+                command = scanner.nextLine();
+            }
         };
-        System.out.println("Bye bye Dubai! Have a nice day!\n");
+        System.out.println("Have a pleasant day, Master Wayne.\n");
     }
 }
