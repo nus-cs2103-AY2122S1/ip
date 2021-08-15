6a360f97ec4cb0a04334df48ed1825662915dd6e
Ethan Wong <E0550474@u.nus.edu>
2021-08-15 16:50:01 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 public class Duke {
+    static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    static String line = "____________________________________________________________";
+    static String indent = "    ";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println(indent + line + "\n" +
+                            indent + "Hello, I'm Duke!\n" +
+                            indent + "How can I help you?\n" +
+                            indent + line);
+        String in = sc.nextLine();
+        while(!in.equals("bye")) {
+            System.out.println(indent + line + "\n" +
+                                indent + in + "\n" +
+                                indent + line);
+            in = sc.nextLine();
+        }
+        System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
     }
 }
