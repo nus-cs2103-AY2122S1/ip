7a9e568261a0eeda81c8df107bc34bc28f3931ff
mukundrs <mukundrs@hotmail.com>
2021-08-17 07:46:05 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean c = true;
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        while (c) {
+            String s = sc.nextLine();
+            if (s.equals("bye")) {
+                c = false;
+                System.out.println("Bye. Hope to see you again soon!");
+            } else {
+                System.out.println(s);
+            }
+        }
     }
 }
