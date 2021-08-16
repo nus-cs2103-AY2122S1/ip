7c5be8c16947a5e30a5d213f7d67619efb64925e
vigneshsankariyer1234567890 <68676914+vigneshsankariyer1234567890@users.noreply.github.com>
2021-08-16 16:30:23 +0800

    Add A-Classes
    Introduced a new class, Task

---
+++ src/main/java/Task.java
@@ -0,0 +1,58 @@
+public class Task {
+    private static int maxId = 1;
+
+    private final int id;
+    private String name;
+    private boolean completed;
+
+    private Task(String name) {
+        this.id = Task.maxId;
+        this.name = name;
+        this.completed = false;
+        Task.increaseMax();
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public boolean getCompleted() {
+        return this.completed;
+    }
+
+    public void updateName(String input) {
+        this.name = input;
+    }
+
+    public void markAsCompleted() {
+        this.completed = true;
+    }
+
+    public String details() {
+        String checkbox = "[" + " " + "]";
+        return checkbox + " " + this.getName();
+    }
+
+    public static void increaseMax() {
+        Task.maxId++;
+    }
+
+    public static void resetMaxId() {
+        Task.maxId = 1;
+    }
+
+    public static int getMaxId() {
+        return Task.maxId;
+    }
+
+    @Override
+    public String toString() {
+        String checkbox = "[" + (this.getCompleted() ? "X" : " ") + "]";
+        return this.getId() + "." + checkbox + " " + this.getName();
+    }
+
+}
