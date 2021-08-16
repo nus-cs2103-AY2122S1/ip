00f99ab2667d6aa54cebaf4b90d2e334c031ef1e
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2021-08-16 17:24:31 +0800

    Added ability to echo commands given to chatbot.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+    /* Runs Scanner object, and echoes what is typed by user. If user says bye, then end the function. */
+    public static void reply() {
+        Scanner myObj = new Scanner(System.in);
+        String command;
+        while (true) {
+            command = myObj.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Goodbye!");
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from \n" + logo);
+        System.out.println("What can I do for you?");
+        reply();
     }
 }
