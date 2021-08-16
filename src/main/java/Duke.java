52bc3cb9ee471e5c53e0708b39ae460dcbfa2e02
JeffZincatz <jeffzincatz@gmail.com>
2021-08-16 20:11:57 +0800

    abstract Duke greeting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
+        greet();
+    }
+
+    /**
+     * Print a message that is enclosed by 2 horizontal lines.
+     * @param message The message to be printed between 2 horizontal lines.
+     */
+    private static void printMessage(String message) {
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine + "\n" + message + "\n" + horizontalLine);
+    }
+
+    /**
+     * Greeting message of Duke.
+     */
+    static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printMessage(logo + "\nHello! I'm Duke\nWhat can I do for you?");
     }
 }
