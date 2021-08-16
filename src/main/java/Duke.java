7fd3b198b23aad78acf3e98aec52d8fb108158be
clyveycui <cuilangyuan@hotmail.com>
2021-08-16 15:50:13 +0800

    Level-1
    Week 2 Level-1, greet and echo and exit for Duck

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,58 @@
+import java.util.Scanner;
 public class Duke {
+    private static String selfIntro = "Hello, I'm Duck\n       What do you need?";
+
+    private static String goodBye = "See ya next time! *quack* *quack* *quack*";
+
+    private static String logo =
+            "⠀⠀⠀⠀⠀⠀⠀⣠⡀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠤⠤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⠀⠀⠀⠀⠀⢀⣾⣟⠳⢦⡀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠉⠉⠉⠉⠉⠒⣲⡄\n" +
+            "⠀⠀⠀⠀⠀⣿⣿⣿⡇⡇⡱⠲⢤⣀⠀⠀⠀⢸⠀⠀⠀1984⠀⣠⠴⠊⢹⠁\n" +
+            "⠀⠀⠀⠀⠀⠘⢻⠓⠀⠉⣥⣀⣠⠞⠀⠀⠀⢸⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⢸⠀\n" +
+            "⠀⠀⠀⠀⢀⣀⡾⣄⠀⠀⢳⠀⠀⠀⠀⠀⠀⢸⢠⡄⢀⡴⠁⠀⠀⠀⠀⠀⡞⠀\n" +
+            "⠀⠀⠀⣠⢎⡉⢦⡀⠀⠀⡸⠀⠀⠀⠀⠀⢀⡼⣣⠧⡼⠀⠀⠀⠀⠀⠀⢠⠇⠀\n" +
+            "⠀⢀⡔⠁⠀⠙⠢⢭⣢⡚⢣⠀⠀⠀⠀⠀⢀⣇⠁⢸⠁⠀⠀⠀⠀⠀⠀⢸⠀⠀\n" +
+            "⠀⡞⠀⠀⠀⠀⠀⠀⠈⢫⡉⠀⠀⠀⠀⢠⢮⠈⡦⠋⠀⠀⠀⠀⠀⠀⠀⣸⠀⠀\n" +
+            "⢀⠇⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⡀⣀⡴⠃⠀⡷⡇⢀⡴⠋⠉⠉⠙⠓⠒⠃⠀⠀\n" +
+            "⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠀⠀⡼⠀⣷⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⡞⠀⠀⠀⠀⠀⠀⠀⣄⠀⠀⠀⠀⠀⠀⡰⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+            "⢧⠀⠀⠀⠀⠀⠀⠀⠈⠣⣀⠀⠀⡰⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀";
+
+    private static void greet() {
+        System.out.println(logo);
+        printLine(selfIntro);
+    }
+
+    private static void sayBye() {
+        printLine(goodBye);
+    }
+
+    public static void printLine(String content) {
+        System.out.println(
+                "       ------------------------------------------------\n       "
+                + content
+                + "\n       ------------------------------------------------"
+            );
+
+    }
+
+    public static void runDuck() {
+        Scanner sc = new Scanner(System.in);
+        String userInput;
+        Duke.greet();
+        boolean bye = false;
+        while (!bye) {
+            userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                bye = true;
+                Duke.sayBye();
+            } else {
+                Duke.printLine(userInput);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        runDuck();
     }
 }
