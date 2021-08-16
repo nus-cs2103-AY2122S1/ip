f4fbfb8e67e96b1980704bb4367c4d64dcdf57f7
softmagnet <zhiwei3141@gmail.com>
2021-08-16 20:16:57 +0800

    Level Completion: Level 1
    Completed level 1.

---
+++ src/main/java/Yoyo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Yoyo {
+    public static void main(String[] args) {
+        String greetings = "Hello! I'm Yoyo\n"
+                + "What can I do for you?\n";
+        System.out.println(greetings);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+                break;
+            } else {
+                System.out.println("========================================================================");
+                System.out.println(input);
+                System.out.println("========================================================================\n");
+            }
+        }
+
+
+    }
+}
