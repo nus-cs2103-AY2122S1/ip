330859cdcf2ad4529ea9b965c22dca289bd6f966
JoelChanZhiYang <joelchanzhiyang@gmail.com>
2021-08-16 22:34:09 +0800

    Fixed the bug where a new scanner was instantiated after every response. Now only one scanner is used. Added semi-automated testing. Could still be improved

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,11 +7,12 @@ public class Duke {
 
     public static void main(String[] args) {
         System.out.println("Hello, I am Duke. What can I do for you?");
-        waitResponse();
+        Scanner scanner = new Scanner(System.in);
+        waitResponse(scanner);
+        scanner.close();
     }
 
-    private static void waitResponse() {
-        Scanner scanner = new Scanner(System.in);
+    private static void waitResponse(Scanner scanner) {
         if (scanner.hasNext()) {
             String command = scanner.next();
             String action = scanner.nextLine();
@@ -23,25 +24,25 @@ public class Duke {
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println(String.format("%d. %s", i + 1, tasks.get(i)));
                 }
-                waitResponse();
+                waitResponse(scanner);
             } else if (command.equals("done")) {
                 int taskNumber = Integer.parseInt(action.trim());
                 if (taskNumber <= tasks.size()) {
                     Task taskToComplete = tasks.get(taskNumber - 1);
                     taskToComplete.complete();
-                    System.out.println("Nice! I've marked this task as done:\n");
+                    System.out.println("Nice! I've marked this task as done:");
                     System.out.println(taskToComplete);
                 } else {
                     System.out.println("This task does not exist! Use the list command to see your tasks.");
                 }
-                waitResponse();
+                waitResponse(scanner);
             } else if (command.equals("todo")) {
                 Task newTask = new Todo(action.trim());
                 tasks.add(newTask);
                 System.out.println(String.format(
                         "Got it. I've added this task: \n\t %s \nNow you have %d task in the list.",
                         newTask, tasks.size()));
-                waitResponse();
+                waitResponse(scanner);
             } else if (command.equals("deadline")) {
                 String[] deadlineInputs = action.trim().split("/by", 2);
                 Task newTask = new Deadline(deadlineInputs[0].trim(), deadlineInputs[1].trim());
@@ -49,7 +50,7 @@ public class Duke {
                 System.out.println(String.format(
                         "Got it. I've added this task: \n  %s \nNow you have %d task in the list.",
                         newTask, tasks.size()));
-                waitResponse();
+                waitResponse(scanner);
             } else if (command.equals("event")) {
                 String[] eventInputs = action.trim().split("/at", 2);
                 Task newTask = new Event(eventInputs[0], eventInputs[1]);
@@ -57,10 +58,10 @@ public class Duke {
                 System.out.println(String.format(
                         "Got it. I've added this task: \n\t %s \nNow you have %d task in the list.",
                         newTask, tasks.size()));
-                waitResponse();
+                waitResponse(scanner);
             } else {
                 System.out.println("Sorry! What is this command?");
-                waitResponse();
+                waitResponse(scanner);
             }
         }
     }
