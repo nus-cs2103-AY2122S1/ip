8a5dd7e714495cea6c465d88fb2e5d6ebd32896b
kwokieee <bryankwokyf@gmail.com>
2021-08-17 00:37:26 +0800

    Add Deadline.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Deadline {
+/**
+ * The Deadline class encapsulates a task that has a deadline.
+ */
+public class Deadline extends Task {
+    /** The deadline of the task. */
+    private String by;
+
+    /**
+     * Constructor for the Deadline class.
+     *
+     * @param description The description of the task.
+     * @param by The deadline of the task.
+     */
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    /**
+     * Returns the string representation of the task.
+     *
+     * @return A string representing the Deadline task.
+     */
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + this.by + ")";
+    }
 }
