2ab4872dd50e8bb623362713fb446951137426d2
Cedric Ong <cedricong99@gmail.com>
2021-08-17 00:28:34 +0800

    add Greet, echo, exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you today? ");
+
+        String userInput = "";
+
+        while (!userInput.equals("bye")) {
+            userInput = scanner.nextLine();
+            System.out.println(userInput);
+        }
+
+        System.out.println("Good bye.");
+
     }
 }
