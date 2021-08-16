11bfd425920933fbc6b80d0b300a772475f66d4a
Nicolas <nicolas.chang99@gmail.com>
2021-08-16 21:40:59 +0800

    Add list command to Kermit
    Typing to text (with the exception of "bye" and "list") would add the text to a list. The list command would print all the items is in the list

--- src/main/java/Kermit.java
+++ src/main/java/Kermit.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import java.util.ArrayList;
+
 public class Kermit {
     /**
      * Adds a top and bottom horizontal line to text
@@ -14,6 +16,10 @@ public class Kermit {
         Scanner sc = new Scanner(System.in);
         String command = "";
 
+        String stringifiedList = "";
+        // store user's objects
+        ArrayList<String> list = new ArrayList<>();
+
         final String introductionText = "Hello I am Kermit ( *・∀・)ノ゛, eaten any flies today?\nWhat can I do for you?";
         final String goodbyeText = "Bye. Hope to see you again soon!";
 
@@ -25,7 +31,20 @@ public class Kermit {
             if (command.equals("bye")) {
                 System.out.println(formatText(goodbyeText));
                 break;
+            // List out all objects that user added to list
+            } else if (command.equals("list")){
+                stringifiedList = "";
+                for (int i = 0; i < list.size(); i++) {
+                    stringifiedList += (i + 1) + ". " + list.get(i);
+                    // After last item should not have line break
+                    if (i < list.size() - 1) {
+                        stringifiedList += "\n";
+                    }
+                }
+                System.out.println(formatText(stringifiedList));
+            // Add objects to list
             } else {
+                list.add(command);
                 System.out.println(formatText("Added: " + command));
             }
         }
