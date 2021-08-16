f4439d3e9a842dece01fe18953bee34d116b4619
xyliew25 <e0550381@u.nus.edu>
2021-08-17 00:48:31 +0800

    complete Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,11 +21,37 @@ public class Duke {
         + "     Bye. Hope to see you again soon!\n"
         + "    ____________________________________________________________\n";
 
+    // List command
+    final String listCommand = "list";
+    String[] list = new String[100];
+    int index = 0;
+
     // Duke chatbot
     while (!command.equals(exitCommand)) {
+      // Get command
       command = scanner.nextLine();
+
+      // Initialize response content
+      String responseContent = "";
+
+      // List command
+      if (command.equals(listCommand)) {
+        for (int i = 0; i < index; i++) {
+          if (i != index - 1) {
+            responseContent = responseContent + (i + 1) + ". " + list[i] + "\n     ";
+          } else {
+            responseContent = responseContent + (i + 1) + ". " + list[i];
+          }
+        }
+      } else {
+        responseContent = "added: " + command;
+        list[index] = command;
+        index++;
+      }
+
+      // Response
       String response = "    ____________________________________________________________\n"
-          + "     " + command + "\n"
+          + "     " + responseContent + "\n"
           + "    ____________________________________________________________\n";
       System.out.println(response);
     }
