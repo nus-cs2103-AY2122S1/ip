dd89060065b84d8dd675985e3aa5cd51f0182a50
jaredlhf <jlhfqaz@gmail.com>
2021-08-17 01:44:52 +0800

    implemented done functionality

--- src/main/java/Jared.java
+++ src/main/java/Jared.java
@@ -12,11 +12,20 @@ public class Jared {
     }
 
     private static void list() {
-        String res = "";
+        String res = "Here are the tasks in your list:\n";
         for (int i = 0; i < historyCount; i++) {
             Task currTask = history[i];
             res += String.format("%d.[%s] %s\n", i + 1, currTask.getStatusIcon(), currTask.toString());
         }
+        System.out.println(res);
+    }
+
+    private static void done(int index) {
+        int taskNum = index - 1;
+        Task currTask = history[taskNum];
+        currTask.markDone();
+        String res = String.format("Nice! I've marked this task as done:\n[%s] %s",
+                currTask.getStatusIcon(), currTask.toString());
         System.out.println(res);
     }
 
@@ -30,12 +39,15 @@ public class Jared {
         System.out.println(intro);
         while (true) {
             String next = scan.nextLine();
+            String command = next.split(" ")[0];
             if (next.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
             } else if (next.equals("list")) {
                 list();
+            } else if (command.equals("done")) {
+                int index = Integer.valueOf(next.split(" ")[1]);
+                done(index);
             } else {
-
                 add(next);
             }
         }
