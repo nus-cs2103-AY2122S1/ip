8589e061a34ac14380bd1c56c0323ba4681068f6
botr99 <brandonong999@gmail.com>
2021-08-16 16:54:51 +0800

    Implement greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 public class Duke {
     private static final String HORIZONTAL_LINE = "____________________________________________________________";
 
-    public static void formatDukeResponse(String response) {
-        System.out.println(HORIZONTAL_LINE + "\n" + response + "\n" + HORIZONTAL_LINE);
+    public static String formatDukeResponse(String response) {
+        return HORIZONTAL_LINE + "\n" + response + "\n" + HORIZONTAL_LINE + "\n";
     }
 
     public static void printWelcomeMessage() {
@@ -11,12 +13,32 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-
         String welcomeMessage = logo + "Hello! I'm Duke.\n" + "What can I do for you";
-        formatDukeResponse(welcomeMessage);
+        System.out.println(formatDukeResponse(welcomeMessage));
+    }
+
+    public static void printExitMessage() {
+        String exitMessage = "Bye. Hope to see you again soon!";
+        System.out.println(formatDukeResponse(exitMessage));
+    }
+
+    public static void printEchoMessage(String message) {
+        System.out.println(formatDukeResponse(message));
     }
 
     public static void main(String[] args) {
         printWelcomeMessage();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                sc.close();
+                break;
+            }
+            printEchoMessage(userInput);
+        }
+
+        printExitMessage();
     }
 }
