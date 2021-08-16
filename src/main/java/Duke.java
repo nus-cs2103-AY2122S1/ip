f166b4f62a1ff72bfaa2b4e32d8ef0d00b5b2575
Tanishq4331 <tanishq43311@gmail.com>
2021-08-16 18:10:30 +0800

    Complete Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        String toRepeat = scanner.next();
+
+        while (!toRepeat.equals("bye")) {
+            System.out.println("\t" + toRepeat + "\n");
+            toRepeat = scanner.next();
+        }
+
+        scanner.close();
     }
 }
