6cc02b13406a9f5d4e70d5500073a500549c92c9
Wanyu <>
2021-08-16 17:28:31 +0800

    -Skeletal version of the program that echoes input, unless input is "bye" of any combination of upper/lower case letters, then program terminates and does not respond to further inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,31 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String separator = "     _______________________________________________________";
         System.out.println("Hello from\n" + logo);
+        System.out.println(separator);
+        System.out.println("     Hi! I am Duke!\n" + "     What can I do for you?");
+        System.out.println(separator);
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String curr = sc.nextLine();
+            System.out.println(curr);
+            System.out.println(separator);
+            String message = chat(curr);
+            System.out.println("     " + message);
+            System.out.println(separator);
+            if (message.equals("Bye. Hope to see you again soon!")){
+                System.exit(0);
+            }
+        }
+    }
+
+    public static String chat(String s){
+        String check = s.replaceAll(" ", "");
+        if (check.toLowerCase().equals("bye")) {
+            return "Bye. Hope to see you again soon!";
+        } else {
+            return s;
+        }
     }
 }
