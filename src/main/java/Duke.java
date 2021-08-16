a6925c68e0934d2bd3a5e9492555d4dcd1b5bcd9
yk Wei <ykwei7@gmail.com>
2021-08-16 15:19:26 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,21 +12,37 @@ public class Duke {
         String greetingMsg = "Hello! I'm Duke\nWhat can I do for you?";
         String breakline = "____________________________________________________________";
         String exitCmd = "bye";
+        String listCmd = "list";
         String cmd;
         String byeMsg = "Bye. Hope to see you again soon!";
         Scanner scanner = new Scanner(System.in);
+
+        String[] tasks = new String[100];
 
         System.out.println(breakline);
         System.out.println(greetingMsg);
         System.out.println(breakline);
 
+        int count = 0;
         do {
             cmd = scanner.nextLine();
-            System.out.println(cmd);
-            System.out.println(breakline);
+
+            if(cmd.equals(exitCmd)) {
+                System.out.println(byeMsg);
+                System.out.println(breakline);
+                break;
+            } else if(cmd.equals(listCmd)) {
+                for(int i=0;i < count; i++) {
+                    System.out.printf("%d. %s\n", i+1, tasks[i]);
+                }
+                System.out.println(breakline);
+            } else {
+                tasks[count] = cmd;
+                System.out.printf("added: %s\n", cmd);
+                System.out.println(breakline);
+                count++;
+            }
         } while (!cmd.equals(exitCmd));
 
-        System.out.println(byeMsg);
-        System.out.println(breakline);
     }
 }
