59d8e4d336679c468ea8bd9d14c8e56a5cf7f243
kheekheekhee <eekarheenicholas@gmail.com>
2021-08-16 17:33:56 +0800

    Change the events test and actual outputs of events to "at" instead of "by"

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -9,6 +9,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (by: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + at + ")";
     }
 }
