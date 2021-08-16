ba61a2faa07d35537c1d22784a8e1dd13998b2a4
Py0000 <>
2021-08-16 17:37:52 +0800

    Level 5, Add JavaDoc/Comments

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -5,18 +5,30 @@ public class DukeException extends Exception {
     }
 }
 
+/**
+ * Thrown when the user input an unknown string to the chat bot.
+ * Chat bot does not recognise the input given.
+ */
 class UnknownTagException extends DukeException {
     public UnknownTagException() {
         super("No such tag! Please input the correct tag!");
     }
 }
 
+/**
+ * Thrown when user tries to mark/delete tasks that are not in the list of tasks.
+ * User has enter the wrong value.
+ */
 class NoSuchTaskException extends DukeException {
     public NoSuchTaskException() {
         super("You do not have this many task added yet, please check and try again.");
     }
 }
 
+/**
+ * Thrown when user did not use the correct format when using the chat bot.
+ * Example: deadline without /by, event without /at, etc
+ */
 class IllegalFormatException extends DukeException {
     public IllegalFormatException() {
         super("Wrong Format used! Please you the correct format!");
