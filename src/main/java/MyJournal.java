dd29a6da3d7def131f162aa889e886853744ed2c
felissaf <felissafaustine@gmail.com>
2021-08-16 16:26:15 +0800

    Added delete feature

--- src/main/java/MyJournal.java
+++ src/main/java/MyJournal.java
@@ -13,34 +13,49 @@ public class MyJournal {
                 + "2. Type 'list' if you want to generate your task list.\n"
                 + "3. Type 'bye' to exit");
         while (true) {
-            input = reader.nextLine();
-            String thisLine = input;
-            Scanner line = new Scanner(thisLine);
-            String firstWord = line.next();
-            if (input.equals("bye")) {
-                break;
-            } else if (firstWord.equals("done")) {
-                if (line.hasNextInt()) {
-                    int index = line.nextInt() - 1;
-                    if (index >= items.size() || index < 0 || items.get(index) == null) {
-                        System.out.println("That task does not exist");
-                    } else {
-                        items.get(index).setState(true);
-                        System.out.println("Okay!! I have marked this task as done:\n"
+            try {
+                input = reader.nextLine();
+                String thisLine = input;
+                Scanner line = new Scanner(thisLine);
+                String firstWord = line.next();
+                if (input.equals("bye")) {
+                    break;
+                } else if (firstWord.equals("done")) {
+                    if (line.hasNextInt()) {
+                        int index = line.nextInt() - 1;
+                        if (index >= items.size() || index < 0 || items.get(index) == null) {
+                            throw new InvalidTaskNumberException("OOPS!!! Please enter a valid task number!");
+                        } else {
+                            items.get(index).setState(true);
+                            System.out.println("Okay!! I have marked this task as done:\n"
                                     + items.get(index));
+                        }
+                    } else {
+                        throw new InvalidTaskNumberException("OOPS!!! Please specify the task "
+                                + "that needs to be marked as done!");
+                    }
+                } else if (firstWord.equals("delete")) {
+                    if (line.hasNextInt()) {
+                        int index = line.nextInt() - 1;
+                        if (index >= items.size() || index < 0 || items.get(index) == null) {
+                            throw new InvalidTaskNumberException("OOPS!!! Please enter a valid task number!");
+                        } else {
+                            Task temp = items.get(index);
+                            items.remove(index);
+                            System.out.println("Okay!! I have removed the following task:\n"
+                                    + temp);
+                        }
+                    } else {
+                        throw new InvalidTaskNumberException("OOPS!!! Please specify the task that needs to be deleted!");
+                    }
+                } else if (firstWord.equals("list")) {
+                    for (int i = 0; i < items.size(); i++) {
+                        System.out.println((i + 1) + ". " + items.get(i));
                     }
                 } else {
-                    System.out.println("Input insufficient. Please specify the task that needs to be marked as done.");
-                }
-            } else if (firstWord.equals("list")) {
-                for (int i = 0; i < items.size(); i++) {
-                    System.out.println((i + 1) + ". " + items.get(i));
-                }
-            } else {
-                Scanner currLine = new Scanner(input);
-                String taskName = "";
-                String time = "";
-                try {
+                    Scanner currLine = new Scanner(input);
+                    String taskName = "";
+                    String time = "";
                     if (firstWord.equals("todo")) {
                         currLine.next();
                         if (currLine.hasNext()) {
@@ -98,11 +113,13 @@ public class MyJournal {
                     } else {
                         throw new InvalidTypeException("OOPS!!! Please put either todo/event/deadline!");
                     }
-                } catch (EmptyDescriptionException exception) {
-                    System.out.println(exception.getMessage());
-                } catch (InvalidTypeException exception) {
-                    System.out.println(exception.getMessage());
                 }
+            } catch (EmptyDescriptionException exception) {
+                System.out.println(exception.toString());
+            } catch (InvalidTypeException exception) {
+                System.out.println(exception.toString());
+            } catch (InvalidTaskNumberException exception) {
+                System.out.println(exception.toString());
             }
         }
         System.out.println("Bye. Hope to see you again soon!:)");
