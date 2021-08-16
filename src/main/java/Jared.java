2c58fc6e7922886c76ed034c9430bf477d1346ca
jaredlhf <jlhfqaz@gmail.com>
2021-08-16 23:45:47 +0800

    implemented Add functionality

--- src/main/java/Jared.java
+++ src/main/java/Jared.java
@@ -1,6 +1,23 @@
 import java.util.Scanner;
 
 public class Jared {
+    private static String[] history = new String[100];
+    private static int historyCount = 0;
+
+    private static void add(String input) {
+        System.out.println("added: " + input);
+        Jared.history[historyCount] = input;
+        historyCount ++;
+    }
+
+    public static void list() {
+        String res = "";
+        for (int i = 0; i < historyCount; i++) {
+            res += String.format("%d. %s\n", i, Jared.history[i]);
+        }
+        System.out.println(res);
+    }
+
     public static void main(String[] args) {
         String intro = "____________________________________________________________\n" +
                 "Hello! I'm Jared\n" +
@@ -13,10 +30,13 @@ public class Jared {
             String next = scan.nextLine();
             if (next.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
+            } else if (next.equals("list")) {
+                list();
             } else {
-                System.out.println(next);
+                add(next);
             }
         }
 
     }
 }
+
