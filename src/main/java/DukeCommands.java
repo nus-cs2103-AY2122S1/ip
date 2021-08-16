91d854ea97383ac85648d50cf53cc21ac621e0d9
Jonathan <jon.lee.jing.yu@gmail.com>
2021-08-17 00:35:49 +0800

    Add javadocs
    Cleanup existing code

--- src/main/java/DukeCommands.java
+++ src/main/java/DukeCommands.java
@@ -9,17 +9,19 @@ public enum DukeCommands {
             (Map<String, String> map, ArrayList<Task> list) -> {
                 Duke.printMsg("Bye. Hope to see you again soon!");
                 return false;
-            }),
-    LIST("list", (Map<String, String> map, ArrayList<Task> list) -> {
-        StringBuilder output = new StringBuilder("Here are the tasks in your list:\n");
-        for (Task i : list) {
-            output.append(String.format("%d. %s\n", list.indexOf(i) + 1, i));
-        }
-        output.deleteCharAt(output.length() - 1);
-        Duke.printMsg(output.toString());
-        return true;
-    }),
-    DONE("done",
+            }), LIST("list",
+            (Map<String, String> map, ArrayList<Task> list) -> {
+                StringBuilder output =
+                        new StringBuilder("Here are the tasks in your list:\n");
+                for (Task i : list) {
+                    output.append(String.format("%d. %s\n",
+                            list.indexOf(i) + 1,
+                            i));
+                }
+                output.deleteCharAt(output.length() - 1);
+                Duke.printMsg(output.toString());
+                return true;
+            }), DONE("done",
             (Map<String, String> map, ArrayList<Task> list) -> {
                 Parser.parseInt(map.get("done"))
                         .map(x -> {
@@ -28,10 +30,8 @@ public enum DukeCommands {
                             } else {
                                 Task toBeCompleted = list.get(x - 1);
                                 toBeCompleted.markAsDone();
-                                Duke.printMsg(String.format(
-                                        "I have marked the following task " +
-                                                "as completed:\n" +
-                                                "  %s", toBeCompleted.toString()));
+                                Duke.printMsg(String.format("I have marked the following task " + "as completed:\n" + "  %s",
+                                        toBeCompleted.toString()));
 
                             }
                             return x;
@@ -40,40 +40,41 @@ public enum DukeCommands {
 
 
                 return true;
-            }),
-    EVENT("event",
+            }), EVENT("event",
             (Map<String, String> map, ArrayList<Task> list) -> {
                 if (!map.containsKey("/at")) {
                     throw new DukeException("Missing positional argument '/at'.");
                 } else if (map.get("event") == null || map.get("/at") == null) {
                     throw new DukeException("Event body or /at body cannot be empty.");
                 } else {
-                    Task event = new Event(map.get("event"), map.get("/at"));
+                    Task event = new Event(map.get("event"),
+                            map.get("/at"));
                     list.add(event);
-                    Duke.printMsg(
-                            addTaskFormatter(event, list.size())
-                    );
+                    Duke.printMsg(addTaskFormatter(event,
+                            list.size()));
                 }
                 return true;
-            }),
-    DELETE("delete",
+            }), DELETE("delete",
             (Map<String, String> map, ArrayList<Task> list) -> {
-                Parser.parseInt(map.get("delete")).map(x -> {
-                    if (x < 1 || x > list.size()) {
-                        Duke.printMsg("Invalid index.");
-                    } else {
-                        Task toBeDeleted = list.get(x - 1);
-                        list.remove(x - 1);
-                        Duke.printMsg(
-                                removeTaskFormatter(toBeDeleted, list.size())
+                Parser.parseInt(map.get("delete"))
+                        .map(x -> {
+                            if (x < 1 || x > list.size()) {
+                                Duke.printMsg("Invalid index.");
+                            } else {
+                                Task toBeDeleted = list.get(x - 1);
+                                list.remove(x - 1);
+                                Duke.printMsg(removeTaskFormatter(toBeDeleted,
+                                        list.size()));
+                            }
+                            return x;
+                        })
+                        .orElseThrow(() ->
+                                new DukeException(
+                                        "The delete command is missing the positional argument 'index'."
+                                )
                         );
-                    }
-                    return x;
-                }).orElseThrow(() -> new DukeException("The delete command is missing the positional argument 'index'."));
                 return true;
-            }
-    ),
-    DEADLINE("deadline",
+            }), DEADLINE("deadline",
             (Map<String, String> map, ArrayList<Task> list) -> {
 
                 if (!map.containsKey("/by")) {
@@ -81,71 +82,88 @@ public enum DukeCommands {
                 } else if (map.get("deadline") == null || map.get("/by") == null) {
                     throw new DukeException("Deadline body cannot be empty.");
                 } else {
-                    Task event = new Deadline(map.get("deadline"), map.get("/by"));
+                    Task event = new Deadline(map.get("deadline"),
+                            map.get("/by"));
                     list.add(event);
-                    Duke.printMsg(
-                            addTaskFormatter(event, list.size())
-                    );
+                    Duke.printMsg(addTaskFormatter(event,
+                            list.size()));
                 }
                 return true;
-            }),
-    TODO("todo",
+            }), TODO("todo",
             (Map<String, String> map, ArrayList<Task> list) -> {
                 if (map.get("todo") == null) {
                     throw new DukeException("Todo body cannot be empty.");
                 } else {
                     Task event = new Todo(map.get("todo"));
                     list.add(event);
-                    Duke.printMsg(
-                            addTaskFormatter(event, list.size())
-                    );
+                    Duke.printMsg(addTaskFormatter(event,
+                            list.size()));
                 }
                 return true;
-            }),
-    INVALID("invalid",
+            }), INVALID("invalid",
             (Map<String, String> map, ArrayList<Task> list) -> {
                 throw new DukeException("Unrecognized command.");
             });
 
 
-
     final String command;
     final DukeActions action;
 
+    /**
+     * Simple constructor to initialise a DukeCommands enum
+     * @param command The command that this instance serves.
+     * @param action The action that the command has.
+     */
     DukeCommands(String command, DukeActions action) {
         this.command = command;
         this.action = action;
     }
 
+    /**
+     * Takes in a raw string command and matches the first word of the
+     * command to the available enums.
+     * @param command The raw user input.
+     * @return An optional of the enum. Returns an empty optional if not found.
+     */
     public static Optional<DukeCommands> getCommand(String command) {
-        return Arrays.stream(DukeCommands.values()).filter(x ->
-                command.toLowerCase().startsWith(x.command)
-        ).findFirst();
+        return Arrays.stream(DukeCommands.values())
+                .filter(x -> command.toLowerCase()
+                        .startsWith(x.command))
+                .findFirst();
     }
 
-    private static String addTaskFormatter(Task task, int tasks) {
-        return String.format("I have added the following task:\n" +
-                        "  %s\n" +
+
+    /**
+     * Returns the string version of the message when a task is successfully
+     * added.
+     * @param task The task that is added.
+     * @param numTasks The number of tasks that are now in the list.
+     * @return String of the message.
+     */
+    private static String addTaskFormatter(Task task, int numTasks) {
+        return String.format("I have added the following task:\n" + "  %s\n" +
                         "Now you have %s in the list.",
                 task.toString(),
-                tasks == 1 ?
-                        String.format("%d task", 1) :
-                        String.format("%d tasks", tasks));
+                numTasks == 1 ? String.format("%d task",
+                        1) : String.format("%d tasks",
+                        numTasks));
     }
 
-    private static String removeTaskFormatter(Task task, int tasks) {
-        return String.format("I have removed the following task:\n" +
-                        "  %s\n" +
+
+    /**
+     * Returns the string version of the message when a task is successfully
+     * removed.
+     * @param task The task that is removed.
+     * @param numTasks The number of tasks remaining in the list.
+     * @return String of the message.
+     */
+    private static String removeTaskFormatter(Task task, int numTasks) {
+        return String.format("I have removed the following task:\n" + "  %s\n" +
                         "Now you have %s in the list.",
                 task.toString(),
-                tasks == 1 ?
-                        String.format("%d task", 1) :
-                        String.format("%d tasks", tasks));
+                numTasks == 1 ? String.format("%d task",
+                        1) : String.format("%d tasks",
+                        numTasks));
     }
-
-    public String printer() {
-        return this.command;
-    }
-
 
 }
