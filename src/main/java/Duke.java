25c030b40881f3bf001ff60e7a261d9363cc5c10
Jeromesyl <jsyl3214@gmail.com>
2021-08-16 21:23:41 +0800

    Add user input and program exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
 public class Duke {
+    public static String getInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n\n";
+        String greeting = "Hello! I'm Duke.\n" + "What can I do for you? :)";
+        System.out.println(logo + greeting);
+
+        String input;
+        while (true) {
+            input = getInput();
+            if (input.toUpperCase().equals("BYE")) {
+                break;
+            }
+            System.out.println(input);
+        }
+        System.out.println("Bai bai!");
+
     }
+
 }
