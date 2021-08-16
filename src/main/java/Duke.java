f03a4281f5ee35f043a514214295494d4b665b1f
greyguy21 <e0559779@u.nus.edu>
2021-08-17 00:45:31 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -13,6 +14,7 @@ public class Duke {
 
     private static void start() {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         while (sc.hasNext()) {
             String s = sc.nextLine();
@@ -20,16 +22,28 @@ public class Duke {
                 exit();
                 break;
             }
+            if (s.equals("list")) {
+                list(list);
+                continue;
+            }
+            list.add(s);
             echo(s);
         }
         sc.close();
     }
 
     private static void echo(String s) {
-        System.out.println(s);
+        System.out.println("added: " + s);
     }
 
     private static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void list(ArrayList<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            System.out.println(String.format("%d. %s", i + 1, s));
+        }
     }
 }
