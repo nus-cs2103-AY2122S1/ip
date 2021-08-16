879241c11085c5950fe8c2b62fe76bf12ebae157
GnohChengYi <chengyignoh@gmail.com>
2021-08-16 16:07:16 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested

--- src/main/java/Blue.java
+++ src/main/java/Blue.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Blue {
+
     private static final String LOGO = " ____  _                \n"
             + "|  . \\| | _   _   ____  \n"
             + "|____/| || | | | /  _  \\\n"
@@ -10,6 +13,8 @@ public class Blue {
             + "What can I do for you?";
     private static final String EXIT_CONTENT = "Bye. Hope to never see you again!";
 
+    private static final List<String> storedText = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println(LOGO);
         greet();
@@ -17,12 +22,9 @@ public class Blue {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             command = scanner.nextLine();
-            if (command.equals(Command.EXIT)) {
-                speak(EXIT_CONTENT);
+            boolean shouldContinue = handle(command);
+            if (!shouldContinue)
                 break;
-            } else {
-                echo(command);
-            }
         }
         scanner.close();
     }
@@ -31,7 +33,32 @@ public class Blue {
         speak(GREET_CONTENT);
     }
 
-    private static void echo(String content) {
+    private static boolean handle(String command) {
+        if (command.equals(Command.EXIT)) {
+            speak(EXIT_CONTENT);
+            return false;
+        }
+        if (command.equals(Command.LIST))
+            listText();
+        else
+            storeAndDisplayText(command);
+        return true;
+    }
+
+    private static void storeAndDisplayText(String text) {
+        storedText.add(text);
+        String content = "added: " + text;
+        speak(content);
+    }
+
+    private static void listText() {
+        String[] lines = new String[storedText.size()];
+        for (int i = 0; i < storedText.size(); i++) {
+            String intString = Integer.toString(i + 1);
+            String text = storedText.get(i);
+            lines[i] = intString + ". " + text;
+        }
+        String content = String.join("\n", lines);
         speak(content);
     }
 
--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,3 +1,4 @@
 class Command {
+    final static String LIST = "list";
     final static String EXIT = "bye";
 }
