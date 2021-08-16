29b42662cfbab0181b205e98f8a03106533704df
Chan Yu Cheng <e0560244@u.nus.edu>
2021-08-16 11:11:38 +0800

    Add echo and exit when user types bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Urgh I hate having to wake up. Why did you do that");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            Duke.echo(input);
+            input = sc.nextLine();
+        }
+        Duke.sayBye();
+    }
+
+    private static void sayBye() {
+        System.out.println("Don't wake me up again");
+    }
+
+    private static void echo(String input) {
+        System.out.println(input);
     }
 }
