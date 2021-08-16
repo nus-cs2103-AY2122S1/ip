8ff12271ed7a6b47ab72bc7cf0934ab6f3af6c4b
driip-ddrop <germaine5152seah@gmail.com>
2021-08-16 23:59:04 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,26 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        startBot();
+
+        //level 1
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String message = sc.nextLine();
+
+            if (message.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(message);
+            }
+        }
+        sc.close();
+    }
+
+    public static void startBot() {
+        String intro = "Hello! I'm Duke\n" +
+                "     What can I do for you?";
+        System.out.println(intro);
     }
 }
