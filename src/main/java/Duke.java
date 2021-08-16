facb5668b7a6f66e7b078e6cc18e50cb97c2a08f
shaliniseshadri <77195499+shaliniseshadri@users.noreply.github.com>
2021-08-16 10:54:42 +0800

    Level-4 A-Inheritance

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,10 +22,10 @@ public class Duke {
         System.out.println(logo + HELLO + LINE);
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
-        while (!userInput.equals("bye")) {
-            if (userInput.equals("list")) {
+        while (!userInput.equalsIgnoreCase("bye")) {
+            if (userInput.equalsIgnoreCase("list")) {
                 printList(taskList);
-            } else if (userInput.contains("done")) {
+            } else if (userInput.split("\\s")[0].equalsIgnoreCase("done")) {
                 completeTask(userInput);
             } else {
                 addTaskToList(userInput, taskList);
@@ -53,9 +53,43 @@ public class Duke {
     }
 
     public void addTaskToList(String inp, ArrayList<Task> list) {
-        Task task = new Task(inp);
-        list.add(task);
-        System.out.println("added: " + inp + "\n" + LINE);
+        boolean valid = false;
+        boolean keywordPresent = true;
+        Task task;
+        try {
+            String[] splitTasks = inp.split("\\s", 2);
+            String taskType = splitTasks[0].toLowerCase(); ;
+
+            switch (taskType) {
+                case "todo":
+                    valid = true;
+                    task = new ToDo(splitTasks[1]);
+                    list.add(task);
+                    break;
+                case "deadline":
+                    valid = true;
+                    String desc = splitTasks[1].split("/by")[0];
+                    String date = splitTasks[1].split("/by")[1];
+                    task = new Deadline(desc, date);
+                    list.add(task);
+                    break;
+                case "event":
+                    valid = true;
+                    String details = splitTasks[1].split("/at")[0];
+                    String at = splitTasks[1].split("/at")[1];
+                    task = new Event(details, at);
+                    list.add(task);
+                    break;
+                default:
+                    keywordPresent = false;
+                    task = new Task("");
+                    System.out.println("Invalid Keyword");
+                    break;
+            }
+            checkValidity(task, valid, keywordPresent);
+        } catch (IndexOutOfBoundsException ex) {
+                System.out.println("Please enter a valid task description (and date if required)" + "\n" + LINE); // temp validation
+            }
     }
 
     public void completeTask(String input) {
@@ -72,6 +106,27 @@ public class Duke {
             }
         } catch (NumberFormatException | IndexOutOfBoundsException exception) {
             System.out.println("Please enter a valid task number" + "\n" +LINE);
+        }
+    }
+
+    public void printRemainingTasks() {
+        if (taskList.size() == 1) {
+            System.out.println("Now you have 1 task in the list." + "\n" + LINE);
+        } else {
+            System.out.println(String.format("Now you have %s tasks in the list.", taskList.size()) + "\n" + LINE);
+        }
+    }
+
+    // temp elementary input validation - will be replaced by exceptions in subsequent iterations.
+    public void checkValidity(Task task, boolean valid, boolean keywordPresent){
+        if (valid) {
+            System.out.println("Got it. I've added this task:");
+            System.out.println(task.toString());
+            printRemainingTasks();
+        } else if (keywordPresent){
+            System.out.println("Please enter a valid task description and date (if applicable)"+ "\n" + LINE);
+        } else {
+            System.out.println("Please use the permitted keywords (todo, deadline, event) to specify your task type" + "\n" + LINE);
         }
     }
 }
