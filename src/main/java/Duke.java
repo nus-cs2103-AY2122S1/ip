30a7d6ca8cde4e78d84ee471c1a93afc160640ff
JoelChanZhiYang <joelchanzhiyang@gmail.com>
2021-08-16 09:53:20 +0800

    Finished level 1. Added Greet, echo and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello, I am Duke. What can I do for you?");
+        waitResponse();
+    }
+
+    private static void waitResponse() {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNext()){
+            String next = scanner.next();
+            if (next.equals("bye")) {
+                System.out.println("Bye. Hope to see you again!");
+                return;
+            } else {
+                System.out.println(next);
+                waitResponse();
+            }
+        }
     }
 }
