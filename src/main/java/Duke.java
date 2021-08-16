4e2f7c840db00ee87e031701b35d9a3952fa61ca
elizabethchow1234@gmail.com <elizabethchow1234@gmail.com>
2021-08-16 21:44:08 +0800

    Add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,30 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void printLine() {
-        System.out.println("    ------------------------------------------");
+    /**
+     * Print with 4 spaces infront of param str.
+     *
+     * @param str A String to be printed
+     */
+    public static void printWithTabIndent(String str) {
+        System.out.println(String.format("    %s", str));
     }
 
+    /**
+     * Print horizontal line.
+     */
+    public static void printLine() {
+        printWithTabIndent("------------------------------------------");
+    }
+
+    /**
+     * Pretty print the message with the horizontal lines and the param message.
+     *
+     * @param message A String to be printed
+     */
     public static void printMessage(String message) {
         printLine();
-        System.out.println(String.format("    %s", message));
+        printWithTabIndent(message);
         printLine();
     }
 
@@ -20,12 +37,27 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int count = -1;
+
         printMessage("Hello! I'm Duke\n    What can I do for you?");
         String message = scanner.nextLine();
-        while(!message.equals("bye")) {
-            printMessage(message);
+        while (!message.equals("bye")) {
+            if (message.equals("list") && count == -1) {
+                printMessage("Nothing in the list!");
+            } else if (message.equals("list")) {
+                printLine();
+                for (int i = 0; i <= count; i++) {
+                    printWithTabIndent(String.format("%d. %s", i + 1, tasks[i]));
+                }
+                printLine();
+            } else {
+                printMessage("added: " + message);
+                count++;
+                tasks[count] = message;
+            }
             message = scanner.nextLine();
-        };
+        }
         printMessage("Bye. Hope to see you again soon!");
     }
 }
