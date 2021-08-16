9267a12310ceb37831b324cc682c5174dce26ea0
benluiwj <wenjie.lui@gmail.com>
2021-08-16 22:06:43 +0800

    Added javadoc

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
-
+/**
+ * Duke class for level-2 contains the main method that takes
+ * in inputs.
+ */
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
--- src/main/java/Response.java
+++ src/main/java/Response.java
@@ -1,10 +1,25 @@
+/**
+ * Response class contains the logic for processing the commands from Duke.
+ * At level-1, it supports (i) the bye command,
+ * (ii) as well as echoing the user input
+ */
 public class Response {
     private final String input;
 
+    /**
+     * Constructor to initialise the Response class
+     * @param input The input from the user to be echoed
+     */
     Response(String input) {
         this.input = input;
     }
 
+    /**
+     * Echos the user input or says bye to the user depending on
+     * the user input
+     * @return a string that is either the user input,
+     * or a farewell statement
+     */
     String Echo() {
         if (input.equals("bye")) {
             return "Bye. Hope to see you again soon!";
