ed3383747dc1ebda438945cb062ca782716aeef6
yin72257 <ruohangyin@gmail.com>
2021-08-16 13:52:05 +0800

    Completed Duke Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,31 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printLine();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        printLine();
+        try {
+            Scanner scanner = new Scanner(System.in);
+            boolean run = true;
+            while (run) {
+                String input = scanner.next();
+                if (input.equals("bye")) {
+                    input = "Bye. Hope to see you again soon!";
+                    run = false;
+                }
+                printLine();
+                System.out.println("    " + input);
+                printLine();
+            }
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public static void printLine() {
+        System.out.println("    ____________________________________________________________");
     }
 }
