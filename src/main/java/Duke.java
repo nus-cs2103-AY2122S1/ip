2ad25edb011fb7b2587214b705eb8ba34770649e
timiditi <timothy.ng.jw@gmail.com>
2021-08-16 19:19:35 +0800

    Implement skeletal version of Duke
    Echo commands by user and exit when user types 'bye'.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    /**
+     *
+     */
+    private static void echo(String input) {
+        System.out.println(input);
+    }
+    /**
+     * Main function.
+     * @param args
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Howdy! The name's\n" + logo + "\nWhat can I do for ya?");
+
+        Scanner sc = new Scanner(System.in);
+        boolean exit = false;
+
+        while (!exit) {
+
+            String input = sc.next();
+
+            switch (input) {
+                case "bye":
+                    System.out.println("Seeya!");
+                    exit = true;
+                    break;
+                default:
+                    echo(input);
+                    break;
+            }
+
+        }
+
+        sc.close();
     }
 }
