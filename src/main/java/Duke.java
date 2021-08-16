3ff2fbf799d45206a34017863f4bd076653603c0
Timothyoung97 <timothyoung97@gmail.com>
2021-08-17 01:03:17 +0800

    Level 1 - add greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,52 @@
+import java.time.Period;
+import java.util.Scanner;
+
 public class Duke {
+
+    private final String HORIZONTAL_LINE =
+            "\t  .--.      .-'.      .--.      .--.      .--.      .--.      .`-.      .--.\n" +
+                    "\t:::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\\n" +
+                    "\t'      `--'      `.-'      `--'      `--'      `--'      `-.'      `--'      `";
+    private final String GREETING = "Hallo! My name's Peter!\n\tHow may I be of service to you?";
+    private final String GOODBYE = "Good day! I'm gonna find Lois if you're not using me!";
+
+    void printHorizontalLine(){
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    void printNewLine(String input){
+        System.out.println("\t" + input);
+    }
+
+    void greetUser(){
+        printHorizontalLine();
+        printNewLine(GREETING);
+        printHorizontalLine();
+    }
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo =
+                "░░░░░░░░░░░░░░░░▄▄▄███████▄▄░░░░░░░░░░░░\n" +
+                        "░░░░░░░░░░░░░░▄███████████████▄░░░░░░░░░\n" +
+                        "░░░░░░░░░░░░░█▀▀▀▄░░░░█████▀▀███▄░░░░░░░\n" +
+                        "░░░░░░░░░░░░█░░▄░░█▄▄█░░░░▀▄▄█████░░░░░░\n" +
+                        "░░░░░░░░░▄▀▀▀▄▄▀▀▀▀▀▀▄░░▀░░█▀▀▀░▀██░░░░░\n" +
+                        "░░░░░░░▄▀░░░░░█░░░░░░▀▄▄▄▄█░░░░░░░▀▄░░░░\n" +
+                        "░░░░░░▄▀░░░░░▄█▀▄▄▄▄░░░░░▄░░░░░░░░░█░░░░\n" +
+                        "░░░░░░█░░░░░▄█▀▄▄▄▄▄▄▄▄▄▀▀░░░░░░░░░▀▄░░░\n" +
+                        "░░░░░█░░░░░░▀█▄░░░░░░░░░░░░░░░░░░░░░█░░░\n" +
+                        "░░░░░█░░░░░░░░▀▄░░░░░░░░░░░░░░░░░░░░█░░░\n" +
+                        "░░░░░█░░░░░░░░▄█░░░░░░░░░░░░░░░░░░░░█░░░\n" +
+                        "░░░░░█░░░░░░▄▀░░░░░░░░▀▄░░░░░░░░░░░░█░░░\n" +
+                        "░░░░░█░░░░░░▀▄░░░▄░░░░░█░░░░░░░░░░░░█░░░\n" +
+                        "░░░░░█░░░░░░░░▀▀▀▀▀█▄▄▀░░░░░░░░░░░░░█░░░\n" +
+                        "░░░░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▄██░░\n" +
+                        "░░░░░█▄░░░░░░░░░░░░░░░░░░░░░░░░░░▄▀▀▄▀▄▄\n" +
+                        "░░░░▄█▀▄░░░░░░░░░░░░░░░░░░░░░░▄▄▀░░▄▀░░░\n" +
+                        "░▄░▀░░█░▀▄▄░░░░░░░░░░░░░░░▄▄▄▀░░░▄▀░░░░░\n" +
+                        "▀░░░░░░▀▄░░▀▄░░░░░░░░▄▄▄▀▀░░░░▄▀▀░░░░░░░";
+        System.out.println(logo);
+        new Duke().greetUser();
     }
 }
