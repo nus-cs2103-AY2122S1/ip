43e262d630923388ef8bdc3e2b2283b50193fa89
Ng Tze Henn <tzehenn@gmail.com>
2021-08-16 11:42:50 +0800

    Implement Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
+    static final String NAME = "Tze Henn";
+
+    public static void lineGenerator() {
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
+        lineGenerator();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println("Hello! I'm " + NAME);
+        System.out.println("What can I do for you?");
+        lineGenerator();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nEnter command: ");
+        String input = sc.next();
+
+
+        while (!input.equals("bye")) {
+            lineGenerator();
+            System.out.println(input);
+            lineGenerator();
+            System.out.print("\nEnter command: ");
+            input = sc.next();
+        }
+        lineGenerator();
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
