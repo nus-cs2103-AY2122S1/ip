af08c788534a4b2046ea030ad426692a1acdc2b0
Zheng Tao Jun <>
2021-08-16 21:12:50 +0800

    Level-6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -30,66 +30,73 @@ public class Duke {
                 int taskNo = Integer.parseInt(input.substring(input.length() - 1));
                 task.get(taskNo - 1).markAsDone();
                 System.out.println("Nice! I've marked this task as done:\n  " + task.get(taskNo - 1));
+            } else if (input.length() >= 6 && input.substring(0, 6).equals("delete")) {
+                int taskNo = Integer.parseInt(input.substring(input.length() - 1));
+                Task deletedTask = task.get(taskNo - 1);
+                task.remove(taskNo - 1);
+                System.out.printf("Noted. I've removed this task:\n  %s\nNow you have %d task(s) in the list.%n",
+                        deletedTask, task.size());
             } else {
-                //Adding task
-                Task newTask = new Task("");
+                    //Adding task
+                    Task newTask = new Task("");
 
-                try {
-                    //Determine the type of task and content
-                    int spaceIndex = input.indexOf(" ");
-                    String typeofTask = input;
-                    String content = input;
-                    //If there is more than one word
-                    if (spaceIndex != -1) {
-                        typeofTask = input.substring(0, spaceIndex);
-                        content = input.substring(spaceIndex + 1);
-                    } else {
-                        // 1 word means there is no description
-                        content = " ";
-                    }
-                    //Type of task is not one of the three: todo, deadline, event
-                    if (!typeofTask.equals("todo") && !typeofTask.equals("deadline") && !typeofTask.equals("event")) {
-                        throw new InvalidTaskException("Not supported! Supported tasks: todo, deadline, event");
-                    }
-
-                    //There is no description
-                    if (content.isBlank()) {
-                        throw new NoDescriptionException("Description of task cannot be empty!");
-                    }
-
-
-                    switch (typeofTask) {
-                        case "todo":
-                            newTask = new Todo(content);
-                            break;
-                        case "deadline": {
-                            int index = content.indexOf("by");
-                            if (index == -1) {
-                                throw new WrongDescriptionException("Deadline not included! Try: deadline ... /by ...");
-                            } else {
-                                newTask = new Deadline(content.substring(0, index - 2), content.substring(index + 3));
-                            }
-                            break;
+                    try {
+                        //Determine the type of task and content
+                        int spaceIndex = input.indexOf(" ");
+                        String typeofTask = input;
+                        String content = input;
+                        //If there is more than one word
+                        if (spaceIndex != -1) {
+                            typeofTask = input.substring(0, spaceIndex);
+                            content = input.substring(spaceIndex + 1);
+                        } else {
+                            // 1 word means there is no description
+                            content = " ";
                         }
-                        case "event": {
-                            int index = content.indexOf("at");
-                            if (index == -1) {
-                                throw new WrongDescriptionException("Time of event not included! Try: event ... /at ...");
-                            } else {
-                                newTask = new Event(content.substring(0, index - 2), input.substring(index + 3));
-                            }
-                            break;
+                        //Type of task is not one of the three: todo, deadline, event
+                        if (!typeofTask.equals("todo") && !typeofTask.equals("deadline") && !typeofTask.equals("event")) {
+                            throw new InvalidTaskException("Not supported! Supported tasks: todo, deadline, event");
                         }
-                    }
-                } catch (DukeException e) {
-                    System.out.println(e.getMessage());
-                    input = sc.nextLine();
-                    continue;
-                }
 
-                task.add(newTask);
-                System.out.printf("Got it. I've added this task:\n  %s\nNow you have %d task(s) in the list.%n",
-                        newTask, task.size());
+                        //There is no description
+                        if (content.isBlank()) {
+                            throw new NoDescriptionException("Description of task cannot be empty!");
+                        }
+
+
+                        switch (typeofTask) {
+                            case "todo":
+                                newTask = new Todo(content);
+                                break;
+                            case "deadline": {
+                                int index = content.indexOf("by");
+                                if (index == -1) {
+                                    throw new WrongDescriptionException("Deadline not included! Try: deadline ... /by ...");
+                                } else {
+                                    newTask = new Deadline(content.substring(0, index - 2), content.substring(index + 3));
+                                }
+                                break;
+                            }
+                            case "event": {
+                                int index = content.indexOf("at");
+                                if (index == -1) {
+                                    throw new WrongDescriptionException("Time of event not included! Try: event ... /at ...");
+                                } else {
+                                    newTask = new Event(content.substring(0, index - 2), input.substring(index + 3));
+                                }
+                                break;
+                            }
+                        }
+                    } catch (DukeException e) {
+                        System.out.println(e.getMessage());
+                        input = sc.nextLine();
+                        continue;
+                    }
+
+                    task.add(newTask);
+                    System.out.printf("Got it. I've added this task:\n  %s\nNow you have %d task(s) in the list.%n",
+                            newTask, task.size());
+
 
             }
 
