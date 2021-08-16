8cff6e6b3b56094af43aec498f4568e1e7e918fb
Javiier-pzk <javierphon@gmail.com>
2021-08-16 16:39:45 +0800

    Implement Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,14 +11,15 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        greetEchoExit();
+        addToList();
     }
 
     /**
-     * Method for Duke to Greet the user, Echo what the user says and Exit when being told to.
+     * Method for duke to add things to a list and display them when called for by the user.
      * Inputs are taken by a scanner from the user's keyboard.
      */
-    public static void greetEchoExit() {
+    public static void addToList() {
+        ArrayList<String> taskList = new ArrayList<>();
         System.out.println("-----------------------------------------");
         System.out.println(" Hello! I am Duke");
         System.out.println(" What can I do for you?");
@@ -32,11 +34,20 @@ public class Duke {
                 System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println("    -----------------------------------------");
                 break;
-            } else {
+            } else if (command.equals("list")) {
                 System.out.println("    -----------------------------------------");
-                System.out.println("     " + command);
+                for (int i = 0; i < taskList.size(); i++) {
+                    int currNum = i + 1;
+                    System.out.println("     " + currNum + ". " + taskList.get(i));
+                }
                 System.out.println("    -----------------------------------------");
                 System.out.println();
+            } else {
+                System.out.println("    -----------------------------------------");
+                System.out.println("     added: " + command);
+                System.out.println("    -----------------------------------------");
+                System.out.println();
+                taskList.add(command);
             }
         }
         sc.close();
