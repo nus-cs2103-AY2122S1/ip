b7005269c84349c025877c8ff9724fdcf4ddc86d
Kleon Ang <kleonang@gmail.com>
2021-08-17 01:28:52 +0800

    Add printReply, add and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    private final static String EXIT_COMMAND = "bye";
+    private static final String EXIT_COMMAND = "bye";
+    private static final String LIST_COMMAND = "list";
+    private static List<String> tasks = new ArrayList<>();
+
+    private static void printReply(String string) {
+        System.out.println(new Reply(string));
+    }
 
     private static void greet() {
         String logo = " ____        _        \n"
@@ -10,22 +18,42 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println(new Reply("Hello! I'm Duke\nWhat can I do for you?"));
+        printReply("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    private static void add(String string) {
+        tasks.add(string);
+        printReply("added: " + string);
+    }
+
+    private static void list() {
+        StringBuilder tasksBuilder = new StringBuilder();
+        for (int i = 0; i < tasks.size(); ++i) {
+            String counter = String.valueOf(i + 1);
+            tasksBuilder.append(counter).append(". ").append(tasks.get(i));
+            if (i < tasks.size() - 1) {
+                // Append newline for all tasks before last task
+                tasksBuilder.append("\n");
+            }
+        }
+        printReply(tasksBuilder.toString());
     }
 
     public static void main(String[] args) {
         greet();
-        String readIn = new String();
+        String readIn;
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             readIn = scanner.nextLine();
             if (readIn.equals(EXIT_COMMAND)) {
                 break;
+            } else if (readIn.equals(LIST_COMMAND)) {
+                list();
+            } else {
+                add(readIn);
             }
-            System.out.println(new Reply(readIn));
         }
-
-        System.out.println(new Reply("Bye. Hope to see you again soon!"));
+        printReply("Bye. Hope to see you again soon!");
     }
 }
