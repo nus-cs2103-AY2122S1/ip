b20aaca73a5d271f2d49d28260329cb24dca6369
Tan Jia Xian <e0543981@u.nus.edu>
2021-08-16 19:01:53 +0800

    added exceptions for missing descriptions, keywords, invalid task number and invalid number format

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,52 +5,94 @@ import java.util.Scanner;
 import static java.lang.Integer.parseInt;
 
 public class Duke {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        run(scanner);
+    }
+
+    public static void run(Scanner scanner) {
         List<Task> taskList = new ArrayList<>();
 
         // Hello message
         System.out.println(
-            "Lollipop: Hello! I am your personal chatbot, Lollipop! " +
-            "What would you like to do today?");
+                "Lollipop: Hello! I am your personal chatbot, Lollipop! " +
+                        "What would you like to do today?");
         System.out.println("");
         System.out.print("You: ");
         String command = scanner.nextLine();
 
         // Commands
         while (!command.equals("bye")) {
-            if (command.equals("list")) {
-                System.out.println("Lollipop: Here are your tasks");
-                for (int i = 0; i < taskList.size(); i++) {
-                    Task task = taskList.get(i);
-                    System.out.printf("%d. %s%n", i + 1, task.toString());
+            try {
+                if (command.equals("list")) {
+                    System.out.println("Lollipop: Here are your tasks");
+                    for (int i = 0; i < taskList.size(); i++) {
+                        Task task = taskList.get(i);
+                        System.out.printf("%d. %s%n", i + 1, task.toString());
+                    }
+
+                } else if (command.startsWith("done")) {
+                    int taskNumber = parseInt(command.split(" ")[1]);
+                    Task task = taskList.get(taskNumber - 1);
+                    task.markAsDone();
+                    System.out.printf("Lollipop: %s has been marked as done.%n", task.toString());
+
+                } else if (command.startsWith("todo")) {
+                    String[] splitCommand = command.split(" ", 2);
+                    if (splitCommand.length == 1) {
+                        throw new DukeException("Please fill in a description for todo.");
+                    }
+                    String description = splitCommand[1];
+                    Task task = new Todo(description);
+                    taskList.add(task);
+                    System.out.printf("Lollipop: %s has been added.%n", task.toString());
+
+                } else if (command.startsWith("deadline")) {
+                    String[] splitCommand = command.split(" ", 2);
+                    if (splitCommand.length == 1) {
+                        throw new DukeException("Please fill in a description for deadline.");
+                    }
+
+                    String description = splitCommand[1];
+                    String[] splitDescription = description.split(" /by ");
+                    if (splitDescription.length == 1) {
+                        throw new DukeException("Please add in /by, following by a dateline.");
+                    }
+
+                    description = splitDescription[0];
+                    String deadline = splitDescription[1];
+                    Task task = new Deadline(description, deadline);
+                    taskList.add(task);
+                    System.out.printf("Lollipop: %s has been added.%n", task.toString());
+
+                } else if (command.startsWith(("event"))) {
+                    String[] splitCommand = command.split(" ", 2);
+                    if (splitCommand.length == 1) {
+                        throw new DukeException("Please fill in a description for event.");
+                    }
+
+                    String description = splitCommand[1];
+                    String[] splitDescription = description.split(" /at ");
+                    if (splitDescription.length == 1) {
+                        throw new DukeException("Please add in /at, followed by the event's time.");
+                    }
+
+                    description = splitDescription[0];
+                    String time = splitDescription[1];
+                    Task task = new Event(description, time);
+                    taskList.add(task);
+                    System.out.printf("Lollipop: %s has been added.%n", task.toString());
+
+                } else {
+                    throw new DukeException("I do not understand what that means :(");
                 }
-            } else if (command.startsWith("done")) {
-                int taskNumber = parseInt(command.split(" ")[1]);
-                Task task = taskList.get(taskNumber - 1);
-                task.markAsDone();
-                System.out.printf("Lollipop: %s has been marked as done.%n", task.toString());
-            } else if (command.startsWith("todo")) {
-                String description = command.split(" ", 2)[1];
-                Task task = new Todo(description);
-                taskList.add(task);
-                System.out.printf("Lollipop: %s has been added.%n", task.toString());
-            } else if (command.startsWith("deadline")) {
-                String description = command.split(" ", 2)[1];
-                String[] splitDescription = description.split(" /by ");
-                description = splitDescription[0];
-                String deadline = splitDescription[1];
-                Task task = new Deadline(description, deadline);
-                taskList.add(task);
-                System.out.printf("Lollipop: %s has been added.%n", task.toString());
-            } else if (command.startsWith(("event"))) {
-                String description = command.split(" ", 2)[1];
-                String[] splitDescription = description.split(" /at ");
-                description = splitDescription[0];
-                String time = splitDescription[1];
-                Task task = new Event(description, time);
-                taskList.add(task);
-                System.out.printf("Lollipop: %s has been added.%n", task.toString());
+            } catch (DukeException e) {
+                System.out.printf("Lollipop: %s%n", e.getMessage());
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Lollipop: No such task number found.");
+            } catch (NumberFormatException e) {
+                System.out.println("Lollipop: Please input a number.");
             }
 
             System.out.println("");
