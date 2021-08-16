04e41bc82947695548e722dade06a59162b50233
felix-ong <felixongweicong@gmail.com>
2021-08-16 22:11:23 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * Represents a personal assistant chatbot that responds to command line inputs.
@@ -9,15 +10,21 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello, I am Duke!(≧◡≦)\n" + "How may I help you?");
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> inputs = new ArrayList<>();
 
         while (true) {
-            String command = sc.nextLine();
+            String input = sc.nextLine();
 
-            if (command.equals("bye")) {
+            if (input.equals("bye")) {
                 System.out.println("Bye! Feel free to ask me for help again anytime! (≧▽≦)/");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < inputs.size(); i++) {
+                    System.out.printf("%d. %s%n", i + 1, inputs.get(i));
+                }
             } else {
-                System.out.println(command);
+                inputs.add(input);
+                System.out.println("Added: " + input);
             }
         }
     }
