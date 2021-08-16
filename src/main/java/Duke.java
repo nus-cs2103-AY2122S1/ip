203526ac858494cd44345d5b2c392203873bb647
ruoyann <68816578+ruoyann@users.noreply.github.com>
2021-08-16 17:43:06 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,23 +3,20 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm Duke\n What can I do for you?");
 
         boolean stop = false;
-        ArrayList inputArr = new ArrayList();
+        ArrayList<Task> inputArr = new ArrayList<Task>();
 
 
         while (!stop) {
             Scanner sc = new Scanner(System.in);
             String input = sc.nextLine();
             if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < inputArr.size(); i++) {
-                    System.out.println((i+1) + ". " + inputArr.get(i));
+                    Task currTask = inputArr.get(i);
+                    System.out.println((i+1) + "." + "[" + currTask.getStatusIcon() + "] " + currTask.getDescription());
                 }
             }
             else if (input.equals("bye")) {
@@ -27,8 +24,14 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
+            else if (input.startsWith("done")) {
+                int number = Integer.parseInt(String.valueOf(input.charAt(5)));
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + inputArr.get(number-1).getDescription());
+                inputArr.get(number - 1).markAsDone();
+            }
             else {
-                inputArr.add(input);
+                inputArr.add(new Task(input));
                 System.out.println("added: " + input);
             }
         }
