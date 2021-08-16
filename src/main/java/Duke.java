bfcfe902dd5d67adce594c44916a7a8a518c0db3
ntwbruce <e0544240@u.nus.edu>
2021-08-17 00:53:31 +0800

    Added try-catch block and invalid input exception handling functionality to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,52 +10,61 @@ public class Duke {
         System.out.println("  ____________________________________________________________");
         System.out.print("  Hello! I'm Duck.\n  What's up?\n");
         System.out.println("  ____________________________________________________________\n");
-
+        
         while (!bye) {
-            System.out.print("> ");
-            String text = input.nextLine();
+            try {
+                System.out.print("> ");
+                String text = input.nextLine();
 
-            System.out.println("  ____________________________________________________________");
+                System.out.println("  ____________________________________________________________");
 
+                if (text.equals("bye")) { // bye sequence exits the loop, ends process
+                    System.out.println("  See you next time!");
+                    bye = true;
+                    
+                } else if (text.equals("list")) { // list iterates through taskList and prints each Task's listEntry
+                    System.out.println("  Here are the tasks in your list:");
+                    for (int i = 0; i < listLength; ++i) {
+                        System.out.println("  " + (i + 1) + "." + taskList[i].listEntry());
+                    }
+                    
+                } else if (text.split(" ")[0].equals("done")) { // if done keyword used, calls the Task's setDone()
+                    int toSet = Integer.parseInt(text.split(" ")[1]);
+                    taskList[toSet - 1].setDone();
+                    System.out.print("  Nice! I've marked this task as done:\n    " + taskList[toSet - 1].listEntry() + "\n");
+                    
+                } else if (text.split(" ").length > 1){ // task name and details provided, new task to be added
+                    Task newTask;
 
-            if (text.equals("bye")) { // bye sequence exits the loop, ends process
-                System.out.println("  See you next time!");
-                bye = true;
-            } else if (text.equals("list")) { // list iterates through taskList and prints each Task's listEntry
-                System.out.println("  Here are the tasks in your list:");
-                for (int i = 0; i < listLength; ++i) {
-                    System.out.println("  " + (i + 1) + "." + taskList[i].listEntry());
+                    // split text string, first string will be the type of task and second string will be the task details
+                    String[] taskString = text.split(" ", 2);
+                    String taskType = taskString[0];
+                    String taskDetails = taskString[1];
+
+                    // determine type of task, create new task
+                    if (taskType.equals("deadline")) {
+                        String[] details = taskDetails.split(" /by ");
+                        newTask = new Deadline(details[0], details[1]);
+                    } else if (taskType.equals("event")) {
+                        String[] details = taskDetails.split(" /at ");
+                        newTask = new Event(details[0], details[1]);
+                    } else if (taskType.equals("todo")) {
+                        newTask = new Todo(taskDetails);
+                    } else { // taskName is invalid
+                        throw new DukeException("invalidInput");
+                    }
+                    // add task to taskList
+                    taskList[listLength++] = newTask;
+                    System.out.print("  Got it. I've added this task:\n    " + newTask.listEntry() + "\n  Now you have " + listLength + " tasks in the list.\n");
+                
+                } else { // single word command is invalid
+                    throw new DukeException("invalidInput");
                 }
-            } else if (text.split(" ")[0].equals("done")) { // if done keyword used, calls the Task's setDone()
-                int toSet = Integer.parseInt(text.split(" ")[1]);
-                taskList[toSet - 1].setDone();
-                System.out.print("  Nice! I've marked this task as done:\n    " + taskList[toSet - 1].listEntry() + "\n");
-            } else { // new Task to be added
-                Task newTask;
-
-                // split text string, first string will be the type of task and second string will be the task details
-                String[] taskString = text.split(" ", 2);
-                String taskType = taskString[0];
-                String taskDetails = taskString[1];
-
-                // determine type of task, create new task
-                if (taskType.equals("deadline")) {
-                    String[] details = taskDetails.split(" /by ");
-                    newTask = new Deadline(details[0], details[1]);
-                } else if (taskType.equals("event")) {
-                    String[] details = taskDetails.split(" /at ");
-                    newTask = new Event(details[0], details[1]);
-                } else {
-                    newTask = new Todo(taskDetails);
-
-                }
-
-                // add task to taskList
-                taskList[listLength++] = newTask;
-                System.out.print("  Got it. I've added this task:\n    " + newTask.listEntry() + "\n  Now you have " + listLength + " tasks in the list.\n");
+                
+            } catch (DukeException ignored) {
+            } finally {
+                System.out.println("  ____________________________________________________________\n");
             }
-
-            System.out.println("  ____________________________________________________________\n");
         }
 
         input.close();
