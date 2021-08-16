835e4bbc7a5620a233904ce64d8d74234976d46f
leonghongfai <leonghongfai@gmail.com>
2021-08-16 18:01:55 +0800

    Added basic skeleton

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,30 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello FROM\n" + logo);
+        greetings();
     }
+
+    private static void greetings() {
+        System.out.println("Hello! I'm Duke\n");
+        System.out.println("What can I do for you?");
+        System.out.println("__________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String command = scanner.next();
+            if (!command.equals("bye")) {
+                System.out.println("    ______________________________________");
+                System.out.printf("     %s\n",command);
+                System.out.println("    ______________________________________");
+            } else {
+                System.out.println("    ______________________________________");
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("    ______________________________________");
+                scanner.close();
+                break;
+            }
+        }
+    }
+
+
 }
