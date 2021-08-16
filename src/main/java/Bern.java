ee718f5448208e2f2e8390ba406ec18c26377f24
Bernardus Krishna <krishnabernardus@gmail.com>
2021-08-16 16:06:11 +0800

    Finished Level-2: Add, List

--- src/main/java/Bern.java
+++ src/main/java/Bern.java
@@ -9,14 +9,28 @@ public class Bern {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+
+        String[] listTask = new String[100];
+        int available = 0;
+
         System.out.println("Hi! I'm Bern, your trustworthy chatbot. \nWhat can I do for you?");
+
         while (true) {
             String input = myObj.nextLine();
-            if (!input.equals("bye")) {
-                System.out.println(input);
-            } else {
+            if (!input.equals("bye") && !input.equals("list")) {
+                System.out.println("added: " + input);
+                listTask[available] = input;
+                available += 1;
+            } else if (input.equals("bye")){
                 System.out.println("Bye. Hope to see you soon and hope you found my service useful!");
                 break;
+            } else {
+                for (int i = 0; i < available; i++) {
+                    System.out.println(String.valueOf(i + 1) + ". " + listTask[i]);
+                }
+                if (available == 0) {
+                    System.out.println("There is no task.");
+                }
             }
         }
     }
