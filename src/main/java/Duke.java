e242569a39f43a91510c89aa09e17c1c0c281611
Yong Ray Wen Joshua <e0544562@u.nus.edu>
2021-08-16 17:52:24 +0800

    implement skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String userInput = "";
+        while (!userInput.equals("bye")) {
+            userInput = sc.nextLine();
+            System.out.println(userInput);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
