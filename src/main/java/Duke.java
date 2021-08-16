ccaf05a163e092e6fc2ee9d43fa3147fc5f38149
tanruiquan <ruiquann@gmail.com>
2021-08-16 19:40:54 +0800

    Add greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
+        greet();
+    }
+
+    private static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(LINE);
+        System.out.println(logo);
+        System.out.println("Hi, I'm Duke, your Personal Assistant Chatbot\n" +
+                "What can I do for you today?");
+        System.out.println(LINE);
     }
 }
