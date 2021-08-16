39c77d8a99b69b46794f9d158bfbb446bee70ed4
Chin Xing Yi Rebecca <rebeccachinxy@gmail.com>
2021-08-17 00:58:33 +0800

    Level 1 - greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    private final String BORDER = "\t_________________________________________________\n";
+    private final String MESSAGE_GREET =  "Hi, my name is Nee ´･ᴗ･`. How can I help you?";
+    private final String MESSAGE_EXIT = "Goodbye!~";
+
+    /**
+     * Prints user input sandwiched between two horizontal lines.
+     *
+     * @param input
+     */
+    public void print(String input) {
+        String format = BORDER + "\t%s\n" + BORDER;
+        System.out.printf(format, input.replaceAll("\n", "\n\t"));
     }
+
+    public void run() {
+        // Greet user
+        print(MESSAGE_GREET);
+
+        // User input
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while (!input.equalsIgnoreCase("bye")) {
+            print(input);
+            input = sc.nextLine();
+        }
+        sc.close();
+
+        // Goodbye message
+        print(MESSAGE_EXIT);
+    }
+
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.run();
+    }
+
 }
