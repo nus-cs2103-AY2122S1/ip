577a221c961d981435fc3089778105523417fc92
Tan Yan Feng <tanyf.justin@hotmail.com>
2021-08-17 02:30:30 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,34 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        start();
+    }
+
+    public static void start() {
+        System.out.println("\nInput:");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        replyTo(userInput);
+    }
+
+    public static void reply(String dukeReply) {
+        String lines = "--------------------------------------------------------------------------------------------";
+        String newString = lines + "\nOutput: \n" + dukeReply + "\n" + lines + "\n";
+        System.out.println(newString);
+    }
+
+    public static void replyTo(String userInput) {
+        if (userInput.equals("bye")) {
+            reply("Good bye");
+            System.exit(0); //Should have no error message, hence 0
+        } else {
+            echo(userInput);
+        }
+    }
+
+    public static void echo(String userInput) {
+        reply(userInput);
+        start(); // repeat
     }
 }
