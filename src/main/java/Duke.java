c6c28960817259dbcb381c3d07294adb26e414a7
clyveycui <cuilangyuan@hotmail.com>
2021-08-16 19:43:02 +0800

    Week 2 Level-5
    Duck now detects basic command line errors and handles it

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -36,22 +36,40 @@ public class Duke {
 
     }
 
-    private static String addTask(String input, TaskType tType) {
+    private static String addTask(String input, TaskType tType) throws DukeCommandException {
         String msg;
         String[] inputArr;
+        String[] taskArr;
         switch (tType) {
             case TODO:
                 inputArr = input.split(" ", 2);
+                if(inputArr.length != 2 || inputArr[1].equals("")) {
+                    throw new DukeCommandException("todo");
+                }
                 msg = tList.addTask(new ToDo(inputArr[1]));
                 break;
                 //Possible exception: no string after todo.
             case DEADLINE:
-                inputArr = input.split(" ", 2)[1].split(" /by ", 2);
-                msg = tList.addTask(new Deadline(inputArr[0], inputArr[1]));
+                inputArr = input.split(" ", 2);
+                if (inputArr.length != 2) {
+                    throw new DukeCommandException("deadline");
+                }
+                taskArr = inputArr[1].split(" /by ", 2);
+                if (taskArr.length != 2 || taskArr[1].equals("")) {
+                    throw new DukeCommandException("deadline");
+                }
+                msg = tList.addTask(new Deadline(taskArr[0], taskArr[1]));
                 break;
             case EVENT:
-                inputArr = input.split(" ", 2)[1].split(" /at ", 2);
-                msg = tList.addTask(new Event(inputArr[0], inputArr[1]));
+                inputArr = input.split(" ", 2);
+                if (inputArr.length != 2) {
+                    throw new DukeCommandException("deadline");
+                }
+                taskArr = inputArr[1].split(" /at ", 2);
+                if (taskArr.length != 2 || taskArr[1].equals("")) {
+                    throw new DukeCommandException("deadline");
+                }
+                msg = tList.addTask(new Event(taskArr[0], taskArr[1]));
                 break;
             default:
                 msg = "If you see this, something has went terribly wrong";
@@ -68,39 +86,53 @@ public class Duke {
         while (!bye) {
             userInput = sc.nextLine();
             inputArr = userInput.split(" ", 2);
-            if (inputArr.length == 1) {
-                switch (inputArr[0]) {
-                    case "bye":
-                        bye = true;
-                        Duke.sayBye();
-                        break;
-                    case "list":
-                        printLine(tList.getTasks());
-                        break;
-                    default:
-                        printLine(userInput);
+            try {
+                if (inputArr.length == 1) {
+                    switch (inputArr[0]) {
+                        case "bye":
+                            bye = true;
+                            Duke.sayBye();
+                            break;
+                        case "list":
+                            printLine(tList.getTasks());
+                            break;
+                        case "todo":
+                            throw new DukeCommandException("todo");
+                        case "deadline":
+                            throw new DukeCommandException("deadline");
+                        case "done":
+                            throw new DukeCommandException("done");
+                        case "event":
+                            throw new DukeCommandException("event");
+                        default:
+                            throw new DukeCommandException(inputArr[0]);
+                    }
+                } else {
+                    switch (inputArr[0]) {
+                        case "done":
+                                printLine(
+                                        tList.markComplete(
+                                                Integer.parseInt(inputArr[1])
+                                        )
+                                );
+                            break;
+                        case "todo":
+                            //todo: Currently all task adding may accept empty fields. Need to deal with that
+                            printLine(addTask(userInput, TaskType.TODO));
+                            break;
+                        case "deadline":
+                            printLine(addTask(userInput, TaskType.DEADLINE));
+                            break;
+                        case "event":
+                            printLine(addTask(userInput, TaskType.EVENT));
+                            break;
+                        default:
+                            throw new DukeCommandException(inputArr[0]);
+                    }
                 }
-            } else {
-                switch (inputArr[0]) {
-                    case "done":
-                        printLine(
-                                tList.markComplete(
-                                        Integer.parseInt(inputArr[1])
-                                )
-                        );
-                        break;
-                    case "todo":
-                        printLine(addTask(userInput, TaskType.TODO));
-                        break;
-                    case "deadline":
-                        printLine(addTask(userInput, TaskType.DEADLINE));
-                        break;
-                    case "event":
-                        printLine(addTask(userInput, TaskType.EVENT));
-                        break;
-                    default:
-                        printLine(userInput);
-                }
+            }
+            catch (DukeCommandException dce) {
+                printLine(dce.getMsg());
             }
         }
     }
---
+++ src/main/java/DukeCommandException.java
@@ -0,0 +1,25 @@
+public class DukeCommandException extends Exception{
+    private String msg;
+    public DukeCommandException(String command) {
+        switch (command) {
+            case "todo":
+                msg = "Wrong usage of \"todo\".\nCorrect usage: \"todo TASK_NAME\".";
+                break;
+            case "deadline":
+                msg = "Wrong usage of \"deadline\".\nCorrect usage: \"deadline TASK_NAME /by TIME\".";
+                break;
+            case "event":
+                msg = "Wrong usage of \"event\".\nCorrect usage: \"deadline TASK_NAME /at TIME\".";
+                break;
+            case "done":
+                msg = "Wrong usage of \"done\".\nCorrect usage: \"done TASK_NUMBER\".";
+                break;
+            default:
+                msg = command + " is not a supported command.";
+        }
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
