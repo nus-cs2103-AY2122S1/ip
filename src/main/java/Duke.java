a2c8271da18c4b3e7bd16e6090c34cad8eb616f7
Wanyu <>
2021-08-16 18:22:48 +0800

    -changed local variable, "curr" to "currentLine" -added documentation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
+/** this class implements the Duke assistant/chat-bot
+ * @author damithc
+ * edited by Wanyu
+ */
+
 public class Duke {
+
+    /**
+     * Greets the user as well as reads user's inputs with a scanner
+     * terminates if user has given the correct keyword
+     *
+     * @param args the command-line argument for the program to execute
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,18 +26,25 @@ public class Duke {
         System.out.println(separator);
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
-            String curr = sc.nextLine();
-            System.out.println(curr);
+            String currentLine = sc.nextLine();
+            System.out.println(currentLine);
             System.out.println(separator);
-            String message = chat(curr);
+            String message = chat(currentLine);
             System.out.println("     " + message);
             System.out.println(separator);
             if (message.equals("Bye. Hope to see you again soon!")){
                 System.exit(0);
             }
         }
+        System.exit(0);
     }
 
+    /**
+     * To determine the appropriate response from input command
+     *
+     * @param s input commands that is read by scanner in 'main'
+     * @return the corresponding response message as a String
+     */
     public static String chat(String s){
         String check = s.replaceAll(" ", "");
         if (check.toLowerCase().equals("bye")) {
