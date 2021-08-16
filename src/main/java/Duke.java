77ce8f19c25611f7b97e2e981fcbe945ae404b5d
xyliew25 <e0550381@u.nus.edu>
2021-08-17 00:16:33 +0800

    complete Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+  public static void main(String[] args) {
+    // Greet
+    String greeting = "    ____________________________________________________________\n"
+        + "     Hello! I'm Duke\n"
+        + "     What can I do for you?\n"
+        + "    ____________________________________________________________\n";
+    System.out.println(greeting);
+
+    // Create scanner
+    Scanner scanner = new Scanner(System.in);
+
+    // Initialize command
+    String command = "";
+
+    // Exit command
+    String exitCommand = "bye";
+    String exitResponse = "    ____________________________________________________________\n"
+        + "     Bye. Hope to see you again soon!\n"
+        + "    ____________________________________________________________\n";
+
+    // Duke chatbot
+    while (!command.equals(exitCommand)) {
+      command = scanner.nextLine();
+      String response = "    ____________________________________________________________\n"
+          + "     " + command + "\n"
+          + "    ____________________________________________________________\n";
+      System.out.println(response);
     }
+
+    // Exit
+    System.out.println(exitResponse);
+  }
 }
