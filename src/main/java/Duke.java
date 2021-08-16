b0deb645e9b927086b864581c82ea6e08bc14c36
James-Kua <james@Jamess-MacBook-Pro.local>
2021-08-16 18:51:59 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void greet() {
+        System.out.println("Hello! I'm Duke" + '\n' + "What can I do for you?");
+    }
+
+    public static void start() {
+
+        String exit = "bye";
+        String input;
+        String lineBreak = "========================================================================";
+
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+        System.out.println(lineBreak);
+
+        if (input.equals(exit)) {
+            System.out.println("Bye. Hope to see you again soon!" + '\n' + lineBreak);
+        } else {
+            System.out.println(input + '\n' + lineBreak);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +30,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        greet();
+        start();
     }
 }
