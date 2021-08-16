928bb878c03dc698df66bf3776d282c7ed6e4531
kwokieee <bryankwokyf@gmail.com>
2021-08-16 16:50:37 +0800

    Add Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
