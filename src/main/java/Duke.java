ab8dfff0ed9c41b0ec61641643aa6fde44bf4a9f
luminousleek <68138671+luminousleek@users.noreply.github.com>
2021-08-16 10:59:49 +0800

    change Task array to ArrayList, fix bug for validating Event input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Duke {
@@ -22,21 +24,17 @@ public class Duke {
     }
 
     /**
-     * Lists the elements of a given array of Tasks and enumerates them
+     * Lists the elements of a given ArrayList of Tasks and enumerates them
      *
-     * @param arr the given array of Tasks
+     * @param lst the given array of Tasks
      * @return a String that looks like an ordered list of Tasks
      */
-    private static String listTaskArr(Task[] arr) {
+    private static String listTaskArr(ArrayList<Task> lst) {
         StringBuilder res = new StringBuilder();
-        int i = 0;
+        ListIterator<Task> iter = lst.listIterator();
 
-        while (arr[i] != null) {
-            res.append(i + 1).append(".").append(arr[i]);
-            if (i < arr.length - 1 && arr[i + 1] != null) {
-                res.append("\n");
-            }
-            i++;
+        while (iter.hasNext()) {
+            res.append(iter.nextIndex() + 1).append(".").append(iter.next());
         }
         return res.toString();
     }
@@ -44,8 +42,7 @@ public class Duke {
     public static void main(String[] args) {
         // initialise scanner and String array
         Scanner sc = new Scanner(System.in);
-        Task[] taskList = new Task[100];
-        int idx = 0; // pointer for string array
+        ArrayList<Task> taskList = new ArrayList<>();
         String msg = "";
 
         // Greet
@@ -58,27 +55,38 @@ public class Duke {
                 if (input.equals("list")) {
                     msg = listTaskArr(taskList);
                 } else if (input.startsWith("done")) {
+                    // input validation
                     int num = Integer.parseInt(input.substring(5));
-                    if (num < 1 || num > idx) throw new DukeException("you typed an invalid number: " + num);
-                    taskList[num - 1].markDone();
-                    msg = "Nice! I've marked this task as done:\n  " + taskList[num - 1];
+                    if (num < 1 || num > taskList.size()) throw new DukeException("you typed an invalid number: " + num);
+
+                    taskList.get(num - 1).markDone();
+                    msg = "Nice! I've marked this task as done:\n  " + taskList.get(num - 1);
                 } else if (input.startsWith("todo")) {
+                    // input validation
                     if (input.length() < 5 || input.substring(5).isBlank()) throw new DukeException("the description of the todo cannot be empty!");
-                    taskList[idx++] = new ToDo(input.substring(5));
-                    msg = "Got it. I've added this task:\n  " + taskList[idx - 1] + "\nNow you have " + idx;
-                    msg = idx == 1 ? msg + " task in the list" : msg + " tasks in the list.";
+
+                    ToDo newTodo = new ToDo(input.substring(5));
+                    taskList.add(newTodo);
+                    msg = "Got it. I've added this task:\n  " + newTodo + "\nNow you have " + taskList.size();
+                    msg = taskList.size() == 1 ? msg + " task in the list" : msg + " tasks in the list.";
                 } else if (input.startsWith("deadline")) {
+                    // input validation
                     if (input.length() < 9 || input.substring(9).isBlank()) throw new DukeException("the description of the deadline cannot be empty!");
-                    if (input.indexOf("/by") == -1) throw new DukeException("you are missing the /by keyword");
-                    taskList[idx++] = new Deadline(input.substring(9));
-                    msg = "Got it. I've added this task:\n  " + taskList[idx - 1] + "\nNow you have " + idx;
-                    msg = idx == 1 ? msg + " task in the list" : msg + " tasks in the list.";
+                    if (!input.contains("/by")) throw new DukeException("you are missing the /by keyword");
+
+                    Deadline newDeadline = new Deadline(input.substring(9));
+                    taskList.add(newDeadline);
+                    msg = "Got it. I've added this task:\n  " + newDeadline + "\nNow you have " + taskList.size();
+                    msg = taskList.size() == 1 ? msg + " task in the list" : msg + " tasks in the list.";
                 } else if (input.startsWith("event")) {
-                    if (input.length() < 9 || input.substring(9).isBlank()) throw new DukeException("the description of the event cannot be empty!");
-                    if (input.indexOf("/at") == -1) throw new DukeException("you are missing the /at keyword");
-                    taskList[idx++] = new Event(input.substring(6));
-                    msg = "Got it. I've added this task:\n  " + taskList[idx - 1] + "\nNow you have " + idx;
-                    msg = idx == 1 ? msg + " task in the list" : msg + " tasks in the list.";
+                    // input validation
+                    if (input.length() < 6 || input.substring(6).isBlank()) throw new DukeException("the description of the event cannot be empty!");
+                    if (!input.contains("/at")) throw new DukeException("you are missing the /at keyword");
+
+                    Event newEvent = new Event(input.substring(6));
+                    taskList.add(newEvent);
+                    msg = "Got it. I've added this task:\n  " + newEvent + "\nNow you have " + taskList.size();
+                    msg = taskList.size() == 1 ? msg + " task in the list" : msg + " tasks in the list.";
                 } else {
                     throw new DukeException("you typed in something i don't recognise");
                 }
