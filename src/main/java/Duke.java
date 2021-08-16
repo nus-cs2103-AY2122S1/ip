81f9fea5524302a9ffc3593c390fa732364e6296
James-Kua <james@Jamess-MacBook-Pro.local>
2021-08-16 20:59:43 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    public static ArrayList<String> itemList = new ArrayList<String>();
 
     public static void greet() {
         System.out.println("Hello! I'm Duke" + '\n' + "What can I do for you?");
@@ -11,16 +14,25 @@ public class Duke {
         String exit = "bye";
         String input;
         String lineBreak = "========================================================================";
+        String items = "list";
 
         Scanner in = new Scanner(System.in);
         input = in.nextLine();
         System.out.println(lineBreak);
 
         if (input.equals(exit)) {
-            System.out.println("Bye. Hope to see you again soon!" + '\n' + lineBreak);
+            itemList.clear();
+            System.out.println("Bye. Hope to see you again soon!");
+        } else if (input.equals(items)) {
+            for (int i = 0; i < itemList.size(); i++) {
+                System.out.println(i + 1 + "." + " " + itemList.get(i));
+            }
         } else {
-            System.out.println(input + '\n' + lineBreak);
+            itemList.add(input);
+            System.out.println("added:" + " " + input);
         }
+        System.out.println(lineBreak);
+        start();
     }
 
     public static void main(String[] args) {
