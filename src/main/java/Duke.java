145822f33b088330a6d0e769a8a2d25ada823e70
botr99 <brandonong999@gmail.com>
2021-08-16 16:32:39 +0800

    Change greet display format

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
-    public static void main(String[] args) {
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    public static void formatDukeResponse(String response) {
+        System.out.println(HORIZONTAL_LINE + "\n" + response + "\n" + HORIZONTAL_LINE);
+    }
+
+    public static void printWelcomeMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String welcomeMessage = logo + "Hello! I'm Duke.\n" + "What can I do for you";
+        formatDukeResponse(welcomeMessage);
+    }
+
+    public static void main(String[] args) {
+        printWelcomeMessage();
     }
 }
