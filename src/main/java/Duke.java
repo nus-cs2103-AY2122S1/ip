642077fa0c93c86e76b1834c5e1b38998a0cccd0
luminousleek <68138671+luminousleek@users.noreply.github.com>
2021-08-16 10:39:02 +0800

    added DukeException and error handling, add more test cases

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,17 +28,17 @@ public class Duke {
      * @return a String that looks like an ordered list of Tasks
      */
     private static String listTaskArr(Task[] arr) {
-        String res = "";
+        StringBuilder res = new StringBuilder();
         int i = 0;
 
         while (arr[i] != null) {
-            res = res + (i + 1) + "." + arr[i];
+            res.append(i + 1).append(".").append(arr[i]);
             if (i < arr.length - 1 && arr[i + 1] != null) {
-                res = res + "\n";
+                res.append("\n");
             }
             i++;
         }
-        return res;
+        return res.toString();
     }
 
     public static void main(String[] args) {
@@ -54,29 +54,40 @@ public class Duke {
         // Add, List and Mark done
         String input = sc.nextLine();
         while (!input.equals("bye")){
-            if (input.equals("list")) {
-                msg = listTaskArr(taskList);
-            } else if (input.startsWith("done")) {
-                int num = Integer.parseInt(input.substring(5));
-                taskList[num - 1].markDone();
-                msg = "Nice! I've marked this task as done:\n  " + taskList[num - 1];
-            } else if (input.startsWith("todo")) {
-                taskList[idx++] = new ToDo(input.substring(5));
-                msg = "Got it. I've added this task: \n  " + taskList[idx - 1] + "\nNow you have " + idx;
-                msg = idx == 1 ? msg + " task in the list" : msg + " tasks in the list.";
-            } else if (input.startsWith("deadline")) {
-                taskList[idx++] = new Deadline(input.substring(9));
-                msg = "Got it. I've added this task: \n  " + taskList[idx - 1] + "\nNow you have " + idx;
-                msg = idx == 1 ? msg + " task in the list" : msg + " tasks in the list.";
-            } else if (input.startsWith("event")) {
-                taskList[idx++] = new Event(input.substring(6));
-                msg = "Got it. I've added this task: \n  " + taskList[idx - 1] + "\nNow you have " + idx;
-                msg = idx == 1 ? msg + " task in the list" : msg + " tasks in the list.";
-            } else {
-                msg = "sorry i don't know what this means: " + input;
+            try {
+                if (input.equals("list")) {
+                    msg = listTaskArr(taskList);
+                } else if (input.startsWith("done")) {
+                    int num = Integer.parseInt(input.substring(5));
+                    if (num < 1 || num > idx) throw new DukeException("you typed an invalid number: " + num);
+                    taskList[num - 1].markDone();
+                    msg = "Nice! I've marked this task as done:\n  " + taskList[num - 1];
+                } else if (input.startsWith("todo")) {
+                    if (input.length() < 5 || input.substring(5).isBlank()) throw new DukeException("the description of the todo cannot be empty!");
+                    taskList[idx++] = new ToDo(input.substring(5));
+                    msg = "Got it. I've added this task:\n  " + taskList[idx - 1] + "\nNow you have " + idx;
+                    msg = idx == 1 ? msg + " task in the list" : msg + " tasks in the list.";
+                } else if (input.startsWith("deadline")) {
+                    if (input.length() < 9 || input.substring(9).isBlank()) throw new DukeException("the description of the deadline cannot be empty!");
+                    if (input.indexOf("/by") == -1) throw new DukeException("you are missing the /by keyword");
+                    taskList[idx++] = new Deadline(input.substring(9));
+                    msg = "Got it. I've added this task:\n  " + taskList[idx - 1] + "\nNow you have " + idx;
+                    msg = idx == 1 ? msg + " task in the list" : msg + " tasks in the list.";
+                } else if (input.startsWith("event")) {
+                    if (input.length() < 9 || input.substring(9).isBlank()) throw new DukeException("the description of the event cannot be empty!");
+                    if (input.indexOf("/at") == -1) throw new DukeException("you are missing the /at keyword");
+                    taskList[idx++] = new Event(input.substring(6));
+                    msg = "Got it. I've added this task:\n  " + taskList[idx - 1] + "\nNow you have " + idx;
+                    msg = idx == 1 ? msg + " task in the list" : msg + " tasks in the list.";
+                } else {
+                    throw new DukeException("you typed in something i don't recognise");
+                }
+                wrapPrint(msg);
+            } catch (NumberFormatException e) {
+                wrapPrint("oh no! you typed in an invalid input! " + e.getMessage());
+            } catch (DukeException e) {
+                wrapPrint(e.getMessage());
             }
-            wrapPrint(msg);
-            msg = "";
             input = sc.nextLine();
         }
 
