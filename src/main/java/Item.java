4b6d03591b66b2063c60aa26aa574cdcd9fd5587
Zhou-Jiahao-1998 <54730603+Zhou-Jiahao-1998@users.noreply.github.com>
2021-08-16 11:43:18 +0800

    Completed Level-3

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,11 +1,18 @@
 public class Item {
     private String name;
+    private boolean done;
 
     public Item(String name) {
         this.name = name;
+        this.done = false;
     }
 
-    public String getName() {
-        return this.name;
+    public void markAsDone() {
+        this.done = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (this.done ? "X" : " ") + "] " + this.name;
     }
 }
--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import static java.lang.Integer.parseInt;
 
 public class List extends ArrayList<Item> {
     public static ArrayList<Item> todos;
@@ -9,12 +10,25 @@ public class List extends ArrayList<Item> {
 
     public void addTask(String input) {
         if (input.equals("list")) {
-            for (int i = 0; i < todos.size(); i++) {
-                System.out.println(i + 1 + ". " + todos.get(i).getName());
-            }
+            showList();
+        } else if (input.startsWith("done ")) {
+            int index = parseInt(input.substring(5));
+            done(index);
         } else {
             todos.add(new Item(input));
             System.out.println("added: " + input);
         }
+    }
+
+    public void showList() {
+        for (int i = 0; i < todos.size(); i++) {
+            System.out.println(i + 1 + ". " + todos.get(i).toString());
+        }
+    }
+
+    public void done(int index) {
+        Item temp = todos.get(index - 1);
+        temp.markAsDone();
+        System.out.println("Nice! I've marked this task as done: \n" + temp);
     }
 }
