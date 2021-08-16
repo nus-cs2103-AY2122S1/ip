6d9cffa9103f54edfd3ccb91ec26cc41da5256dd
JoelChanZhiYang <joelchanzhiyang@gmail.com>
2021-08-16 10:26:17 +0800

    Added the function to add to the todo list as well as listing off the todo list.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<String> tasks = new ArrayList<>();
+
+
     public static void main(String[] args) {
         System.out.println("Hello, I am Duke. What can I do for you?");
         waitResponse();
@@ -9,12 +13,19 @@ public class Duke {
     private static void waitResponse() {
         Scanner scanner = new Scanner(System.in);
         if (scanner.hasNext()){
-            String next = scanner.next();
+            String next = scanner.nextLine();
             if (next.equals("bye")) {
                 System.out.println("Bye. Hope to see you again!");
                 return;
+            } else if (next.equals("list")){
+                System.out.println("List of Tasks:");
+                for (int i = 0; i<tasks.size(); i++) {
+                    System.out.println(String.format("%d. %s", i + 1, tasks.get(i)));
+                }
+                waitResponse();
             } else {
-                System.out.println(next);
+                tasks.add(next);
+                System.out.println(String.format("Added: %s", next));
                 waitResponse();
             }
         }
