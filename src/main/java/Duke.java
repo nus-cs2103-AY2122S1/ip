e5226dc4930a609fccb29e3dcaeaadc454d92668
ooawagaeri <oastreach@gmail.com>
2021-08-16 15:44:30 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+
+        String greet = "____________________________________________________________\n" +
+                "Hello! I'm Duke\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________";
+        System.out.println(greet);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            String output = "";
+
+            if (input.equals("list")) {
+                output = "list";
+            }
+            else if (input.equals("blah")) {
+                output = "blah";
+            }
+            else if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(output);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
