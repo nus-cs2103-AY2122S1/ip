db0690e76ce6990e8395d703c956b24be1027807
elizabethchow1234@gmail.com <elizabethchow1234@gmail.com>
2021-08-16 20:59:53 +0800

    Add Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printLine() {
+        System.out.println("    ------------------------------------------");
+    }
+
+    public static void printMessage(String message) {
+        printLine();
+        System.out.println(String.format("    %s", message));
+        printLine();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +18,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);
+        printMessage("Hello! I'm Duke\n    What can I do for you?");
+        String message = scanner.nextLine();
+        while(!message.equals("bye")) {
+            printMessage(message);
+            message = scanner.nextLine();
+        };
+        printMessage("Bye. Hope to see you again soon!");
     }
 }
