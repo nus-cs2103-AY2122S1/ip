1e8779812c7e5a3d65dd5fbf26613061f2501513
erwinqxy <erwin.quek.xian.yi@gmail.com>
2021-08-16 22:59:21 +0800

    Level-1 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,36 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
+
+        // Some String format
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
-        String in = "____________________________________________________________\n" +
-                "Hello! I'm Duke\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________";
-        System.out.println(in);
+        String start = "____________________________________________________________\n";
+        String end = "____________________________________________________________";
+        String welcome = "Hello! I'm Duke. A friendly chatbot!! :)\n" +
+                    "What can I do for you?\n";
+        String end_message = "Bye. I hope to talk to you again soon! :)";
+
+        //Print welcome message to the user
+        System.out.println(start + welcome + end);
+
+        //Init new scanner to take in inputs
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+
+        //A loop to check for bye. Else will echo the users input
+        while (!input.equals("bye")) {
+            System.out.println(start + input + "\n" +  end);
+            input = s.nextLine();
+        }
+
+        //If the input is bye, return this message
+        System.out.println(start + end_message + "\n" + end);
+        s.close();
     }
 }
