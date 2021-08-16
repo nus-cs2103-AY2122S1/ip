a02bc30361f15fed36e0b5f151954296028ad357
Nay Lin Han <jamesliu09@gmail.com>
2021-08-17 03:26:29 +0800

    Revert "Adding error checking for list (for empty list)"
    This reverts commit 654438d9314f2796a1a87d85bf1c61d6dee1c374.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -33,9 +33,6 @@ public class Duke {
                     break;
                 // if it is "list", we list the stored inputs
                 } else if (inputs[0].equals("list")) {
-                    if (storage.isEmpty()) {
-                        throw new DukeException("☹ OOPS!!! List is empty!");
-                    }
                     System.out.println(
                         StringFormat.tabAndFormat(storage.toString())
                     );
