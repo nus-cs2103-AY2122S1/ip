d8af6fe89eb1c1c2bb3f05ab065c99deb889cecc
YuPei <sunyupei1999@gmail.com>
2021-08-16 20:38:00 +0800

    finished level 1, added greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+
+
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +10,29 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke. \n" + "What can I do for you?");
+
+
+
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            String data = scan.nextLine();
+            if (data.equals("bye")) {
+                break;
+            }
+            printMsg(data);
+        }
+
+        printMsg("Bye. Hope to see you again soon!");
+
     }
+
+    public static void printMsg(String msg) {
+        System.out.println("-----------------------\n" +
+                msg +
+                "\n-----------------------\n");
+    }
+
+
 }
