a25d6c55568f19645b1ccbda6c19a884543c754e
Yong Ray Wen Joshua <e0544562@u.nus.edu>
2021-08-16 20:48:07 +0800

    implement marking tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,8 +5,8 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
-        String[] tasks = new String[100];
-        int taskIndex = 0;
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
@@ -14,18 +14,31 @@ public class Duke {
         // check userInput
         while (!userInput.equals("bye")) {
             if (userInput.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < 100; i++) {
-                    String task = tasks[i];
+                    Task task = tasks[i];
                     if (task == null) {
                         break;
                     } else {
-                        System.out.println((i + 1) + ". " + task);
+                        System.out.println((i + 1) + ". [" + task.getStatusIcon() + "] " + task.description);
                     }
                 }
-            } else { // userInput is not "bye" and not "list"
-                tasks[taskIndex] = userInput;
+            } else if (userInput.startsWith("done ")) {
+                try {
+                    int taskIndex = Integer.parseInt(userInput.split("done ")[1]) - 1;
+                    tasks[taskIndex].setDone();
+                    System.out.println("Nice! I've marked this task as done:\n" +
+                            " [X] " + tasks[taskIndex].description);
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: Invalid task number");
+                }
+            } else { // userInput is not "bye", not "list", and does not start with "done "
+                if (taskCount >= 100) {
+                    System.out.println("Error: Task list full");
+                }
+                tasks[taskCount] = new Task(userInput);
                 System.out.println("added: " + userInput);
-                taskIndex++;
+                taskCount++;
             }
             userInput = sc.nextLine(); // get new userInput
         }
