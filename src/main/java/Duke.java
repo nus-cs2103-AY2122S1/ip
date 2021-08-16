63eea2489305d45699e5cc5a22b57270760934f6
justinhoejj <hoejj05@gmail.com>
2021-08-16 22:21:22 +0800

    Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,6 +23,7 @@ public class Duke {
             switch (firstWord) {
                 case "list":
                     todoList.list();
+                    break;
                 case "done":
                     if (userInput.matches("^done [0-9]+")) {
                         int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
@@ -31,6 +32,16 @@ public class Duke {
                         throw new DukeException(">:( follow format below:\n" +
                                 "done <number between 1 and 100>");
                     }
+                    break;
+                case "delete":
+                    if (userInput.matches("^delete [0-9]+")) {
+                        int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                        todoList.deleteTask(taskNumber);
+                    } else {
+                        throw new DukeException(">:( follow format below:\n" +
+                                "done <number between 1 and 100>");
+                    }
+                    break;
                 case "deadline":
                     if (userInput.matches("^deadline .+ /by .+")) {
                         String[] deadlineDetails = userInput.substring(indexOfSpace + 1).split(" /by ");
@@ -41,6 +52,7 @@ public class Duke {
                         throw new DukeException(">:( Follow format below:\n" +
                                 "deadline <taskname...> /by <datetime...>");
                     }
+                    break;
                 case "event":
                     if(userInput.matches("^event .+ /at .+")) {
                         String[] deadlineDetails = userInput.substring(indexOfSpace + 1).split(" /at ");
@@ -51,6 +63,7 @@ public class Duke {
                         throw new DukeException(">:( Follow format below:\n" +
                                 "deadline <taskname...> /at <datetime...>");
                     }
+                    break;
                 case "todo":
                     if(userInput.matches("^todo .+")) {
                         String name = userInput.substring(indexOfSpace + 1);
@@ -59,6 +72,7 @@ public class Duke {
                         throw new DukeException(">:( include task name after todo:\n" +
                                 "todo <some task name>");
                     }
+                    break;
                 default:
                     throw new DukeException("Say something I can understand!! >:(");
             }
--- src/main/java/TodoList.java
+++ src/main/java/TodoList.java
@@ -1,64 +1,76 @@
+import java.util.LinkedList;
+
 public class TodoList {
-    private final Task[] tasks = new Task[100];
-    private int availableIndex = 0;
+    private final LinkedList<Task> tasks = new LinkedList<>();
     public TodoList() { }
 
     public void addTodo(String taskName) {
         Todo todo = new Todo(taskName);
-        tasks[availableIndex] = todo;
-        availableIndex++;
-        PrintResponse.print(String.format("Caan Do!\n\tadded: %s\n" +
+        tasks.add(todo);
+        PrintResponse.print(String.format("Caan Do!\n  added: %s\n" +
                         "Look at me! " +
                         "%d tasks in the list now!",
                 taskName,
-                availableIndex));
+                tasks.size()));
     }
 
     public void list() {
         StringBuilder result = new StringBuilder();
-        for(int i = 0; i < availableIndex; i++) {
-            result.append(String.format("%d. %s\n", i + 1, tasks[i].toString()));
+        for(int i = 0; i < tasks.size(); i++) {
+            result.append(String.format("%d. %s\n", i + 1, tasks.get(i).toString()));
         }
         PrintResponse.print(
                 String.format("Ooh yeah! Here are your %d tasks\n%s",
-                        availableIndex,
+                        tasks.size(),
                         result.toString()));
     }
 
     public void markAsDone(int taskNumber) throws DukeException{
         try {
-            Task task = tasks[taskNumber - 1];
+            Task task = tasks.get(taskNumber - 1);
             task.markAsDone();
             String response = String.format("Ooh yeah! Task %d marked as done:\n  %s",
                     taskNumber,
                     task);
             PrintResponse.print(response);
         } catch (IndexOutOfBoundsException | NullPointerException e) {
-            throw new DukeException(String.format("Task number %d invalid", taskNumber));
+            throw new DukeException(String.format("Task number %d invalid.", taskNumber));
         }
     }
 
     public void addDeadline(String name, String dateTime) {
         Deadline deadline = new Deadline(name, dateTime);
-        tasks[availableIndex] = deadline;
-        availableIndex++;
+        tasks.add(deadline);
         PrintResponse.print(
                 String.format("Caan Do!\n" +
                                 "  added: %s\n" +
                                 "Look at me! %d tasks in the list now!",
-                        name,
-                        availableIndex));
+                        deadline,
+                        tasks.size()));
     }
 
     public void addEvent(String name, String dateTime) {
         Event event = new Event(name, dateTime);
-        tasks[availableIndex] = event;
-        availableIndex++;
+        tasks.add(event);
         PrintResponse.print(
                 String.format("Caan Do!\n" +
                         "  added: %s\n" +
                         "Look at me! %d in the list now!",
-                        name,
-                        availableIndex));
+                        event,
+                        tasks.size()));
+    }
+
+    public void deleteTask(int taskNumber) throws DukeException{
+        try {
+            Task task = tasks.remove(taskNumber - 1);
+            String response = String.format("Ooh yeah! Task %d deleted:\n  %s" +
+                            "\nNow you have %d tasks in the list.",
+                    taskNumber,
+                    task,
+                    tasks.size());
+            PrintResponse.print(response);
+        } catch (IndexOutOfBoundsException | NullPointerException e) {
+            throw new DukeException(String.format("Task number %d invalid.", taskNumber));
+        }
     }
 }
