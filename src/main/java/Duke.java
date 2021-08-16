7a089cd124d0a86e3adc09332ad3cac33268ba4a
leonghongfai <leonghongfai@gmail.com>
2021-08-16 19:00:49 +0800

    Added level-2 stuff

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -12,25 +13,34 @@ public class Duke {
     }
 
     private static void greetings() {
+        String[] store = new String[100];
+        int pointer = 1;
         System.out.println("Hello! I'm Duke\n");
         System.out.println("What can I do for you?");
         System.out.println("__________________________________________");
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
-            String command = scanner.next();
-            if (!command.equals("bye")) {
-                System.out.println("    ______________________________________");
-                System.out.printf("     %s\n",command);
-                System.out.println("    ______________________________________");
-            } else {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
                 System.out.println("    ______________________________________");
                 System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println("    ______________________________________");
                 scanner.close();
                 break;
+            } else if (command.equals("list")){
+                System.out.println("    ______________________________________");
+                for (int i = 0; i < pointer - 1; i++) {
+                    System.out.printf("     %d. %s\n", i + 1, store[i]);
+                }
+                System.out.println("    ______________________________________");
+            } else {
+                store[pointer - 1] = command;
+                pointer = pointer += 1;
+                System.out.println("    ______________________________________");
+                System.out.printf("     added: %s\n",command);
+                System.out.println("    ______________________________________");
+
             }
         }
     }
-
-
 }
