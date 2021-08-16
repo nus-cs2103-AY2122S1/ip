ff567099101cd0f97478dac7075d7e0869326ab2
Li-Lehao <e0492462@u.nus.edu>
2021-08-16 20:29:47 +0800

    Level-2. Add, list. Added the feature to store the text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    /** The data structure to store all the tasks. **/
+    ArrayList<String> tasks = new ArrayList<>();
+
     /**
-     * A public static method to print message with certain indentation and format.
+     * A public method to print message with certain indentation and format.
      * Receive an array of String, and output one String per line.
      *
      * @param messages
      */
-    public static void printMessage(String messages[]) {
+    public void printMessage(String messages[]) {
         System.out.println("    ____________________________________________________________");
         int n = messages.length;
         for (int i = 0; i < n; i++) {
@@ -19,9 +23,9 @@ public class Duke {
     }
 
     /**
-     * A public static method to output the greeting message.
+     * A public method to output the greeting message.
      */
-    public static void greet() {
+    public void greet() {
         String greetMessage[] = new String[2];
         greetMessage[0] = "Hello! I'm Duke";
         greetMessage[1] = "What can I do for you?";
@@ -29,18 +33,57 @@ public class Duke {
     }
 
     /**
-     * A public static method to repeat commands entered by the user,
-     * and exits when the user input "bye" is detected.
+     * A public method to check whether the input command is an exit command.
+     *
+     * @param inputCommand The user input command.
+     * @return A boolean value indicates whether the input command is an exit command.
      */
-    public static void echoAndExit() {
+    public boolean isExitCommand(String inputCommand) {
+        if (inputCommand.equals("bye")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * A method that list all current tasks.
+     *
+     * @return An array of String, each String contains the number and name of a task.
+     */
+    public String[] listAllTasks() {
+        int n = tasks.size();
+        String taskList[] = new String[n];
+        for (int i = 0; i < n; i++) {
+            taskList[i] = String.format("%d. %s", i + 1, tasks.get(i));
+        }
+        return taskList;
+    }
+
+    /**
+     * A method that read the name of a task, and then add the task to the task list.
+     *
+     * @param task The name of the task.
+     */
+    public void addTask(String task) {
+        tasks.add(task);
+    }
+
+    /**
+     * A method that allows Duke to read the input command and react.
+     */
+    public void chat() {
         Scanner sc = new Scanner(System.in);
         while (true) {
-            String inputMessage = sc.nextLine();
-            if (inputMessage.equals("bye")) {
+            String inputCommand = sc.nextLine();
+            if (isExitCommand(inputCommand)) {
                 printMessage(new String[] {"Bye. Hope to see you again soon!"});
                 return;
+            } else if (inputCommand.equals("list")){
+                printMessage(listAllTasks());
             } else {
-                printMessage(new String[] {inputMessage});
+                addTask(inputCommand);
+                printMessage(new String[] {"added: " + inputCommand});
             }
         }
     }
@@ -52,7 +95,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        greet();
-        echoAndExit();
+        Duke chatBot = new Duke();
+        chatBot.greet();
+        chatBot.chat();
     }
 }
