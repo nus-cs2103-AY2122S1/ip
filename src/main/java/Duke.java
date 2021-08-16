7feb185a8cf55b4faa86ba7934e70872c60c8c46
eeliana <pjne2301@gmail.com>
2021-08-16 16:33:42 +0800

    Abstracted greet, echo, exit to functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,44 +2,54 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class Duke {
-    public static void main(String[] args) {
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-         */
-        String indent = "    ";
-        String greeting = indent + "Hello! I'm Duke\n"
-                + indent+ "What can I do for you?";
-        String exitMessage = indent + "Bye. Hope to see you again soon!";
+    private static String indent = "    ";
 
+    private static void divider() {
         StringBuilder builder = new StringBuilder(100);
         Stream.generate(() -> '-').limit(60).forEach(e -> builder.append(e));
         String line = indent + '+' + builder.toString() + "+\n";
+        System.out.println(line);
+    }
 
-        // Greeting the user
-        System.out.println(line);
+    private static void greet() {
+        String greeting = indent + "Hello! I'm Duke\n"
+                + indent+ "What can I do for you?";
+
+        divider();
         System.out.println(greeting);
-        System.out.println(line);
+        divider();
+    }
+
+    private static void exit() {
+        String exitMessage = indent + "Bye. Hope to see you again soon!";
+
+        divider();
+        System.out.println(exitMessage);
+        divider();
+    }
+
+    private static void echo(String command) {
+        divider();
+        System.out.println(indent + command);
+        divider();
+    }
+
+    public static void main(String[] args) {
+        // Greeting the user
+        greet();
 
         // Taking in commands
         Scanner sc = new Scanner(System.in);
 
         while (true) {
             String command = sc.nextLine();
-            System.out.println(line);
             if (command.equals("bye")) {
-                System.out.println(exitMessage);
-                System.out.println(line);
+                exit();
                 break;
             } else {
                 // Echo the command
-                System.out.println(indent + command);
+                echo(command);
             }
-            System.out.println(line);
         }
 
     }
