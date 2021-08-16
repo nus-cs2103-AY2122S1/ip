0652fd2ce51f177fae90ce820a2f9bbd9aa406b6
greyguy21 <e0559779@u.nus.edu>
2021-08-17 00:31:29 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        start();
+    }
+
+    private static void start() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            if (s.equals("bye")) {
+                exit();
+                break;
+            }
+            echo(s);
+        }
+        sc.close();
+    }
+
+    private static void echo(String s) {
+        System.out.println(s);
+    }
+
+    private static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
