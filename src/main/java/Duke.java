3e7e87ff67a13dd166690009e87ba710a19d536f
Chan Yu Cheng <e0560244@u.nus.edu>
2021-08-16 14:44:44 +0800

    'Level-2'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,35 @@
 import java.util.*;
 
 public class Duke {
-    public static void main(String[] args) {
 
+    String[] tasks = new String[100];
+    int count = 0;
+
+    private void addTask(String task) {
+        tasks[count] = task;
+        count++;
+        System.out.println("added: " + task);
+    }
+
+    private String getTasks() {
+        String tasksStr = "";
+        for (int i = 0; i < count; i++) {
+            tasksStr += (i + 1) + "." + this.tasks[i] + "\n";
+        }
+        return (tasksStr);
+    }
+
+    public static void main(String[] args) {
+        Duke currentDuke = new Duke();
         System.out.println("Urgh I hate having to wake up. Why did you do that");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            Duke.echo(input);
+            if (input.equals("list")) {
+                System.out.println(currentDuke.getTasks());
+            } else {
+                currentDuke.addTask(input);
+            }
             input = sc.nextLine();
         }
         Duke.sayBye();
@@ -17,7 +39,7 @@ public class Duke {
         System.out.println("Don't wake me up again");
     }
 
-    private static void echo(String input) {
-        System.out.println(input);
-    }
+//    private static void echo(String input) {
+//        System.out.println(input);
+//    }
 }
