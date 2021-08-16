02f3ae8d80dfddd6038fbf3440032cf9aecc16b6
felix-ong <felixongweicong@gmail.com>
2021-08-16 21:13:40 +0800

    Implement skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Represents a personal assistant chatbot that responds to command line inputs.
+ *
+ * @author felix-ong
+ */
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello, I am Duke!(≧◡≦)\n" + "How may I help you?");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String command = sc.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println("Bye! Feel free to ask me for help again anytime! (≧▽≦)/");
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
     }
 }
