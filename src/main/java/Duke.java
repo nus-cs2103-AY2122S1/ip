b71beec9018e43e6f1cd3891581baeec559308d4
calvintanwj <calvintanwj127@gmail.com>
2021-08-16 12:00:27 +0800

    Implement greet, echo, and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "\t ____        _        \n"
+                + "\t|  _ \\ _   _| | _____ \n"
+                + "\t| | | | | | | |/ / _ \\\n"
+                + "\t| |_| | |_| |   <  __/\n"
+                + "\t|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello from\n" + logo);
+        System.out.println("\tHello! I'm Duke" + "\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t" + command);
+            System.out.println("\t____________________________________________________________");
+            command = sc.nextLine();
+        }
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
     }
 }
