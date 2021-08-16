68fd05658bfcb012d837535f8e0c890f7fe9a1ff
nicolej2122 <nicole.joseph2122@gmail.com>
2021-08-16 20:57:55 +0800

    Error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,57 +3,76 @@ public class Duke {
     List<Task> commands = new ArrayList<>();
 
     void list(){
-        if (commands.size() == 0){
-            System.out.println("There are no tasks in your list!");
-        } else {
-            System.out.println("Here are the tasks in your list:");
-            for (int i = 0; i < commands.size(); i++) {
-                System.out.println((i + 1) + ". " + commands.get(i));
+        try {
+            if (commands.size() == 0) {
+                throw new EmptyTaskListException();
+            } else {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < commands.size(); i++) {
+                    System.out.println((i + 1) + ". " + commands.get(i));
+                }
             }
+        } catch (DukeException e){
+            System.out.println(e.getMessage());
         }
+
     }
     void add(String input){
         String[] inputSplit = input.split("\\s", 2);
         String commandType = inputSplit[0].toLowerCase();
-        boolean correctInput = true;
-        switch (commandType){
-            case "todo":
-                commands.add(new ToDo(inputSplit[1]));
-                break;
-            case "deadline":
-                String[] deadline = inputSplit[1].split("/by");
-                commands.add(new Deadline(deadline[0], deadline[1]));
-                break;
-            case "event":
-                String[] event = inputSplit[1].split("/at");
-                commands.add(new Event(event[0], event[1]));
-                break;
-            default:
-                correctInput = false;
-                System.out.println("I'm sorry, I don't know what that means! ☹");
-        }
-        if(correctInput){
+        try {
+            if(inputSplit.length < 2){
+                throw new EmptyTaskDescriptionException();
+            }
+            switch (commandType) {
+                case "todo":
+                    commands.add(new ToDo(inputSplit[1]));
+                    break;
+                case "deadline":
+                    String[] deadline = inputSplit[1].split("/by");
+                    if(deadline.length < 2){
+                        throw new NoTimeException();
+                    }
+                    commands.add(new Deadline(deadline[0], deadline[1]));
+                    break;
+                case "event":
+                    String[] event = inputSplit[1].split("/at");
+                    if(event.length < 2){
+                        throw new NoTimeException();
+                    }
+                    commands.add(new Event(event[0], event[1]));
+                    break;
+                default:
+                    throw new UnknownInputException();
+            }
             System.out.println("I have added this task:");
             System.out.println(commands.get(commands.size()-1));
             System.out.println("You now have " + commands.size() + " tasks in your list");
+        } catch (DukeException e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    void done(int listNumber) {
+        try {
+            if (listNumber < commands.size()) {
+                commands.get(listNumber).markAsDone();
+                System.out.println("Good job! I've marked this task as completed:");
+                System.out.println(commands.get(listNumber));
+                commands.remove(listNumber);
+                System.out.println("You now have " + commands.size() + " tasks in your list");
+            } else {
+                throw new InvalidTaskException();
+            }
+        } catch (DukeException e){
+            System.out.println(e.getMessage());
         }
 
     }
 
-    void done(int listNumber){
-        if(listNumber < commands.size()) {
-            commands.get(listNumber).markAsDone();
-            System.out.println("Good job! I've marked this task as completed:");
-            System.out.println(commands.get(listNumber));
-            commands.remove(listNumber);
-            System.out.println("You now have " + commands.size() + " tasks in your list");
-        } else{
-            System.out.println("There is no such task in your to-do list! ☹️");
-        }
-
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Duke duke = new Duke();
         String logo = "   _       _\n"
                 + "  /_\\  ___| |__  _   _\n"
@@ -71,25 +90,28 @@ public class Duke {
             System.out.println("............................................................");
             String input = sc.nextLine().trim();
             String[] command = input.split("\\s+", 2);
-            if(command[0].equalsIgnoreCase("bye")){
-                System.out.println("\n\tByeBye! Hope to see you again!");
-                break;
-            } else if(command[0].equalsIgnoreCase("list")){
-                duke.list();
-            } else if (command[0].equalsIgnoreCase("done")){
-                if (!command[0].equals(input)) {
-                    if(!duke.commands.isEmpty()) {
-                        int listNumber = Integer.parseInt(command[1]);
-                        duke.done(listNumber - 1);
-                    } else{
-                        System.out.println("There are no tasks in your to-do list! ☹️");
+            try {
+                if (command[0].equalsIgnoreCase("bye")) {
+                    System.out.println("\n\tByeBye! Hope to see you again!");
+                    break;
+                } else if (command[0].equalsIgnoreCase("list")) {
+                    duke.list();
+                } else if (command[0].equalsIgnoreCase("done")) {
+                    if (!command[0].equals(input)) {
+                        if (!duke.commands.isEmpty()) {
+                            int listNumber = Integer.parseInt(command[1]);
+                            duke.done(listNumber - 1);
+                        } else {
+                            throw new EmptyTaskListException();
+                        }
+                    } else {
+                        throw new InvalidDoneCommandException();
                     }
-                } else{
-                    System.out.println("You haven't mentioned the task number! ☹️");
+                } else {
+                    duke.add(input);
                 }
-            }
-            else {
-                duke.add(input);
+            } catch (DukeException e){
+                System.out.println(e.getMessage());
             }
             System.out.println("____________________________________________________________");
         }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,41 @@
+public class DukeException extends Exception {
+    DukeException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
+    class EmptyTaskListException extends DukeException {
+        EmptyTaskListException() {
+            super("There are no tasks in your to-do list! ☹");
+        }
+    }
+
+    class InvalidTaskException extends DukeException {
+        InvalidTaskException() {
+          super("There is no such task in your to-do list! ☹");
+      }
+    }
+
+    class UnknownInputException extends DukeException {
+        UnknownInputException() {
+            super("I'm sorry, I don't know what that means! ☹");
+        }
+    }
+
+    class InvalidDoneCommandException extends DukeException {
+        InvalidDoneCommandException() {
+            super("You haven't mentioned the task number! ☹");
+        }
+    }
+
+    class EmptyTaskDescriptionException extends DukeException {
+        EmptyTaskDescriptionException() {
+            super("Please provide a task description! ☹");
+        }
+    }
+
+    class NoTimeException extends DukeException {
+        NoTimeException() {
+            super("Please provide a timeline for your task! ☹");
+        }
+    }
