26306e46818f8572d4c3202c289bd4fcbe785d7e
ooawagaeri <oastreach@gmail.com>
2021-08-16 17:38:54 +0800

    Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,24 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static String cut(String input, String start) {
-        return input.substring(input.indexOf(start) + start.length() + 1);
+    public static String cut(String input, String start) throws DukeException {
+        String result;
+        try {
+            result = input.substring(input.indexOf(start) + start.length() + 1);
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new DukeException(String.format("The field(s) of %s cannot be empty.", start));
+        }
+        return result;
     }
 
-    public static String cut(String input, String start, String end) {
-        return input.substring(input.indexOf(start) + start.length() + 1, input.indexOf(end));
+    public static String cut(String input, String start, String end) throws DukeException {
+        String result;
+        try {
+            result = input.substring(input.indexOf(start) + start.length() + 1, input.indexOf(end));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new DukeException(String.format("The field(s) of %s cannot be empty.", start));
+        }
+        return result;
     }
 
     public static void main(String[] args) {
@@ -32,68 +44,62 @@ public class Duke {
             String input = scanner.nextLine();
             String output = "";
 
-            if (input.equals("list")) {
-                output += "Here are the tasks in your list:\n";
+            try {
+                if (input.equals("list")) {
+                    output += "Here are the tasks in your list:\n";
 
-                int count = 1;
-                for (Task task: library) {
-                    output += String.format("%d.%s\n", count++, task);
+                    int count = 1;
+                    for (Task task : library) {
+                        output += String.format("%d.%s\n", count++, task);
+                    }
                 }
+                else if (input.equals("bye")) {
+                    break;
+                } else if (input.contains("done")) {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+
+                    Task target = library.get(index);
+                    target.setDone();
+
+                    output += "Nice! I've marked this task as done:\n";
+                    output += target + "\n";
+                } else if (input.contains("todo")) {
+                    String name = cut(input, "todo");
+
+                    Todo newTodo = new Todo(name);
+                    library.add(newTodo);
+
+                    output += "Got it. I've added this task:\n"
+                            + newTodo + "\n"
+                            + String.format("Now you have %d tasks in the list.\n", library.size());
+                } else if (input.contains("deadline")) {
+                    String name = cut(input, "deadline", "/by");
+                    String time = cut(input, "/by");
+
+                    Deadline newDeadline = new Deadline(name, time);
+                    library.add(newDeadline);
+
+                    output += "Got it. I've added this task:\n"
+                            + newDeadline + "\n"
+                            + String.format("Now you have %d tasks in the list.\n", library.size());
+                } else if (input.contains("event")) {
+                    String name = cut(input, "event", "/at");
+                    String time = cut(input, "/at");
+
+                    Event newEvent = new Event(name, time);
+                    library.add(newEvent);
+
+                    output += "Got it. I've added this task:\n"
+                            + newEvent + "\n"
+                            + String.format("Now you have %d tasks in the list.\n", library.size());
+                } else {
+                    throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e) {
+                output += "☹ OOPS!!! " + e.getMessage() + "\n";
+            } catch (IndexOutOfBoundsException e) {
+                output += "☹ OOPS!!! The task selected does not exist.\n";
             }
-            else if (input.equals("blah")) {
-                output = "blah\n";
-            }
-            else if (input.equals("bye")) {
-                break;
-            }
-            else if (input.contains("done")) {
-                int index = Integer.parseInt(input.split(" ")[1]) - 1;
-
-                Task target = library.get(index);
-                target.setDone();
-
-                output += "Nice! I've marked this task as done:\n";
-                output += target + "\n";
-            }
-            else if (input.contains("todo")) {
-                String name = cut(input, "todo");
-
-                Todo newTodo = new Todo(name);
-                library.add(newTodo);
-
-                output += "Got it. I've added this task:\n"
-                        + newTodo + "\n"
-                        + String.format("Now you have %d tasks in the list.\n", library.size());
-            }
-            else if (input.contains("deadline")) {
-                String name = cut(input, "deadline", "/by");
-                String time = cut(input, "/by");
-
-                Deadline newDeadline = new Deadline(name, time);
-                library.add(newDeadline);
-
-                output += "Got it. I've added this task:\n"
-                        + newDeadline + "\n"
-                        + String.format("Now you have %d tasks in the list.\n", library.size());
-            }
-            else if (input.contains("event")) {
-                String name = cut(input, "event", "/at");
-                String time = cut(input, "/at");
-
-                Event newEvent = new Event(name, time);
-                library.add(newEvent);
-
-                output += "Got it. I've added this task:\n"
-                        + newEvent + "\n"
-                        + String.format("Now you have %d tasks in the list.\n", library.size());
-            }
-            else {
-                Task newTask = new Task(input);
-                library.add(newTask);
-
-                output = String.format("added: %s\n", input);
-            }
-
             System.out.println(output);
         }
         System.out.println("Bye. Hope to see you again soon!");
