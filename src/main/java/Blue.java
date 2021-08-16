f4f0168ffd71d4769bf82632e97486cc400a1718
GnohChengYi <chengyignoh@gmail.com>
2021-08-16 15:45:45 +0800

    Greet the user, echo commands entered by the user, and exits when the user types "bye"

--- src/main/java/Blue.java
+++ src/main/java/Blue.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
+
 public class Blue {
+    private static final String LOGO = " ____  _                \n"
+            + "|  . \\| | _   _   ____  \n"
+            + "|____/| || | | | /  _  \\\n"
+            + "|  . \\| || |_| ||   ___/\n"
+            + "|____/|_||_____| \\_____/\n";
+    private static final String GREET_CONTENT = "Hello! I'm Blue\n"
+            + "What can I do for you?";
+    private static final String EXIT_CONTENT = "Bye. Hope to never see you again!";
+
     public static void main(String[] args) {
-        String logo = " ____  _                \n"
-                + "|  . \\| | _   _   ____  \n"
-                + "|____/| || | | | /  _  \\\n"
-                + "|  . \\| || |_| ||   ___/\n"
-                + "|____/|_||_____| \\_____/\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(LOGO);
+        greet();
+        String command = "";
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            command = scanner.nextLine();
+            if (command.equals(Command.EXIT)) {
+                speak(EXIT_CONTENT);
+                break;
+            } else {
+                echo(command);
+            }
+        }
+        scanner.close();
+    }
+
+    private static void greet() {
+        speak(GREET_CONTENT);
+    }
+
+    private static void echo(String content) {
+        speak(content);
+    }
+
+    private static void speak(String content) {
+        String line = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+        System.out.println(line);
+        System.out.println(content);
+        System.out.println(line);
     }
 }
---
+++ src/main/java/Command.java
@@ -0,0 +1,3 @@
+class Command {
+    final static String EXIT = "bye";
+}
