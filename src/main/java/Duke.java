f27525dc288b210e727ce2f81e1dafac64aa9c90
Chin Xing Yi Rebecca <rebeccachinxy@gmail.com>
2021-08-17 01:15:12 +0800

    Level 2 - add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,14 +6,43 @@ public class Duke {
     private final String MESSAGE_GREET =  "Hi, my name is Nee ´･ᴗ･`. How can I help you?";
     private final String MESSAGE_EXIT = "Goodbye!~";
 
+    private ArrayList<String> tasks;
+
+    /**
+     * A private constructor to initialize the Arraylist to the given one.
+     */
+    private Duke() {
+        this.tasks = new ArrayList<>(100);
+    }
+
+    /**
+     * Adds a task to the list of tasks
+     * @param taskName the new task to be added.
+     */
+    private void addTask(String taskName) {
+        this.tasks.add(taskName);
+        print("added: " + taskName);
+    }
+
+    /**
+     * Prints the list of tasks.
+     */
+    private void printTasks() {
+        System.out.printf(BORDER);
+        for (int i = 0; i < this.tasks.size(); i++) {
+            System.out.println((i + 1) + ".\t" + this.tasks.get(i));
+        }
+        System.out.println(BORDER);
+    }
+
     /**
      * Prints user input sandwiched between two horizontal lines.
      *
-     * @param input
+     * @param input the user input.
      */
     public void print(String input) {
-        String format = BORDER + "\t%s\n" + BORDER;
-        System.out.printf(format, input.replaceAll("\n", "\n\t"));
+        String s = BORDER + "\t%s\n" + BORDER;
+        System.out.printf(s, input.replaceAll("\n", "\n\t"));
     }
 
     public void run() {
@@ -24,8 +54,17 @@ public class Duke {
         String input = sc.nextLine();
 
         while (!input.equalsIgnoreCase("bye")) {
-            print(input);
+            switch (input.toLowerCase()) {
+                case "list":
+                    printTasks();
+                    break;
+                default:
+                    addTask(input);
+                    break;
+
+            }
             input = sc.nextLine();
+
         }
         sc.close();
 
@@ -37,5 +76,4 @@ public class Duke {
         Duke duke = new Duke();
         duke.run();
     }
-
 }
