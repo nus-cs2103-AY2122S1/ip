5344800b0448a521023e5486424bac2e6349812c
yin72257 <ruohangyin@gmail.com>
2021-08-16 16:15:26 +0800

    Finished Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,13 +19,16 @@ public class Duke {
             Scanner scanner = new Scanner(System.in);
             boolean run = true;
             while (run) {
-                String input = scanner.next();
-                if (input.equals("bye")) {
-                    input = "Bye. Hope to see you again soon!";
-                    run = false;
-                }
+                String input = scanner.nextLine();
                 printLine();
-                System.out.println("    " + input);
+                if (input.equals("bye")) {
+                    System.out.println("    " + "Bye. Hope to see you again soon!");
+                    run = false;
+                } else if (input.equals("list")) {
+                    listItems();
+                } else {
+                    addItem(input);
+                }
                 printLine();
             }
             scanner.close();
@@ -30,6 +36,18 @@ public class Duke {
             System.out.println(e);
         }
 
+    }
+
+    public static void listItems() {
+        for (int i = 0; i < list.size(); i++) {
+            int num = i + 1;
+            System.out.println("    " + num + ". " + list.get(i));
+        }
+    }
+
+    public static void addItem(String s) {
+        list.add(s);
+        System.out.println("    added: " + s);
     }
 
     public static void printLine() {
