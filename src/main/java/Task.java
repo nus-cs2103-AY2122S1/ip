adc125b5ede828e377babca86753a08e2842505c
Javiier-pzk <javierphon@gmail.com>
2021-08-16 20:40:57 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void setIsDone(boolean done) {
+        this.isDone = done;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
