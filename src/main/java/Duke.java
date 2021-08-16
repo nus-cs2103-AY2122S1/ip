64c20eaa0e7a69fdf26305f77a5de037e06235dd
cyyeu <60065592+cyyeu@users.noreply.github.com>
2021-08-16 18:55:17 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,7 +11,7 @@ public class Duke {
         + "| | | | | | | |/ / _ \\\n"
         + "| |_| | |_| |   <  __/\n"
         + "|____/ \\__,_|_|\\_\\___|\n";
-    private static final ArrayList<String> tasks = new ArrayList<>();
+    private static final ArrayList<Task> tasks = new ArrayList<>();
 
     /**
      * Starts the bot
@@ -27,29 +27,66 @@ public class Duke {
     private static void handleCommands() {
         Scanner sc = new Scanner(System.in);
         while (true) {
-            String cmd = sc.nextLine();
+            String input = sc.nextLine();
+            String[] splitInput = input.split(" ");
+            String cmd = splitInput[0];
             switch (cmd) {
+            case "done":
+                if (splitInput.length != 2) {
+                    robotPrint("Invalid command! Please use as \"done [task id]\"");
+                    break;
+                }
+                markTaskDone(splitInput[1]);
+                break;
             case "list":
+                if (splitInput.length != 1) {
+                    robotPrint("Invalid command! Please use as \"list\"");
+                    break;
+                }
                 printAllTasks();
                 break;
             case "bye":
+                if (splitInput.length != 1) {
+                    robotPrint("Invalid command! Please use as \"bye\"");
+                    break;
+                }
                 sc.close();
                 robotPrint("Bye. Hope to see you again soon!");
                 return;
             default:
-                addTask(cmd);
+                addTask(input);
             }
         }
     }
 
+    private static void markTaskDone(String index) {
+        // try to parse int
+        int i = 0;
+        try {
+            i = Integer.parseInt(index);
+        } catch (NumberFormatException e) {
+            robotPrint("Unable to parse task id!");
+        }
+
+        if (i < 0 || i > tasks.size()) {
+            robotPrint("Invalid task id!");
+        }
+
+        Task task = tasks.get(i - 1);
+        task.markAsDone();
+        robotPrint("Nice! I've marked this task as done:");
+        System.out.println("\t " + task);
+    }
+
     private static void addTask(String task) {
-        tasks.add(task);
+        tasks.add(new Task(task));
         robotPrint("added --> " + task);
     }
 
     private static void printAllTasks() {
+        robotPrint("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
-            robotPrint((i + 1) + ". " + tasks.get(i));
+            System.out.println("\t" + ((i + 1) + ". " + tasks.get(i)));
         }
     }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    /**
+     * Constructor of the Task class
+     *
+     * @param description description of this task
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Marks this task as done
+     */
+    protected void markAsDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Returns a string representation of this task
+     *
+     * @return string representation of this task
+     */
+    @Override
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + this.description;
+    }
+}
