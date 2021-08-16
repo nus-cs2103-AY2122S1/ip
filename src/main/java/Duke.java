64d550ce2972e18f352d3128a56c31f5f0c3bcfd
Cyn7hius <64959508+Cyn7hius@users.noreply.github.com>
2021-08-16 12:23:21 +0800

    Added task list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
 
         String divider = "---------------------------";
+
+        ArrayList<String> taskList = new ArrayList<>();
 
         Scanner in = new Scanner(System.in);
         System.out.println(
@@ -14,14 +17,27 @@ public class Duke {
                         divider);
 
         while (true) {
-            String echo = in.nextLine();  // Read user input
+            String nextTask = in.nextLine();  // Read user input
 
-            if (echo.equals("bye")) {
+
+            if (nextTask.equals("list")) {
+                System.out.println(divider);
+                System.out.println("Here are the items in your To Do List: ");
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println(i+1 + ". " + taskList.get(i));
+                }
+                System.out.println("\n" + divider);
+                continue;
+            }
+
+            taskList.add(nextTask);
+
+            if (nextTask.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
 
-            System.out.println(divider + "\n" + echo + "\n" + "\n" + divider);
+            System.out.println(divider + "\n" + "added: " + nextTask + "\n" + "\n" + divider);
         }
     }
 }
