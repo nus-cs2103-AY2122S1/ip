9871f969680206e2a30a07f8d680f96850c3c633
vigneshsankariyer1234567890 <68676914+vigneshsankariyer1234567890@users.noreply.github.com>
2021-08-16 13:59:34 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        printInitialGreeting();
+        String input = sc.nextLine();
+        while(!input.equals("bye")) {
+            echo(input);
+            input = sc.nextLine();
+        }
+        printExitMessage();
+    }
+
+    public static void echo(String input) {
+        System.out.println(input);
+    }
+
+    public static void printInitialGreeting() {
+        System.out.println("Hello I'm Duke\n" +
+                "What can I do for you?");
+    }
+
+    public static void printExitMessage() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printLogo() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
