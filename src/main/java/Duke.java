35a607fa5f428165cd28980dfdab9e44dc29c252
Zhang Ziqing <zhangziqing9926@gmail.com>
2021-08-17 01:01:52 +0800

    Implement Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // creates an object of Scanner
+        Scanner sc = new Scanner(System.in);
+
+        String name = "Catobot";
+        String banner = "(=^･ω･^=)(=^･ω･^=)(=^･ω･^=)";
+        String greeting = "Hello I am " + name + " (>^ω^<)\nWhat can I do for you meow?";
+        System.out.println(banner + "\n" + greeting + "\n" + banner);
+
+        // takes input from the keyboard
+        String response = sc.nextLine();
+        while (!response.equals("bye")) {
+            // prints the response
+            System.out.println(banner + "\n" + response + "\n" + banner);
+            response = sc.nextLine();
+        }
+        String byeMessage = "Bye meow! I will always wait here meow(>^ω^<)";
+        System.out.println(banner + "\n" + byeMessage + "\n" + banner);
+        // closes the scanner
+        sc.close();
     }
 }
