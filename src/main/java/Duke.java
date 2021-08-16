b5958b183b23546f8456f215e35075d33aa878e3
XPS15\benta <benjamin_tan_from.tp@tech.gov.sg>
2021-08-16 21:29:23 +0800

    Added Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,61 @@
+import java.util.Scanner;
+
 public class Duke {
+    static String linebreak = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.print("               _,........__\n");
+        System.out.print("            ,-'            \"`-.\n");
+        System.out.print("          ,'                   `-.\n");
+        System.out.print("        ,'                        \\\n");
+        System.out.print("      ,'                           .\n");
+        System.out.print("      .'\\               ,\"\".       `\n");
+        System.out.print("     ._.'|             / |  `       \\\n");
+        System.out.print("     |   |            `-.'  ||       `.\n");
+        System.out.print("     |   |            '-._,'||       | \\\n");
+        System.out.print("     .`.,'             `..,'.'       , |`-.\n");
+        System.out.print("     l                       .'`.  _/  |   `.\n");
+        System.out.print("     `-.._'-   ,          _ _'   -\" \\  .     `\n");
+        System.out.print("`.\"\"\"\"\"'-.`-...,---------','         `. `....__.\n");
+        System.out.print(".'        `\"-..___      __,'\\          \\  \\     \\\n");
+        System.out.print("\\_ .          |   `\"\"\"\"'    `.           . \\     \\\n");
+        System.out.print("  `.          |              `.          |  .     L\n");
+        System.out.print("    `.        |`--...________.'.        j   |     |\n");
+        System.out.print("      `._    .'      |          `.     .|   ,     |\n");
+        System.out.print("         `--,\\       .            `7\"\"' |  ,      |\n");
+        System.out.print("            ` `      `            /     |  |      |    _,-'\"\"\"`-.\n");
+        System.out.print("             \\ `.     .          /      |  '      |  ,'          `.\n");
+        System.out.print("              \\  v.__  .        '       .   \\    /| /              \\\n");
+        System.out.print("               \\/    `\"\"\\\"\"\"\"\"\"\"`.       \\   \\  /.''                |\n");
+        System.out.print("                `        .        `._ ___,j.  `/ .-       ,---.     |\n");
+        System.out.print("                ,`-.      \\         .\"     `.  |/        j     `    |\n");
+        System.out.print("               /    `.     \\       /         \\ /         |     /    j\n");
+        System.out.print("              |       `-.   7-.._ .          |\"          '         /\n");
+        System.out.print("              |          `./_    `|          |            .     _,'\n");
+        System.out.print("              `.           / `----|          |-............`---'\n");
+        System.out.print("                \\          \\      |          |\n");
+        System.out.print("               ,'           )     `.         |\n");
+        System.out.print("                7____,,..--'      /          |\n");
+        System.out.print("                                  `---.__,--.'\n");
+        System.out.println(linebreak);
+        System.out.println(" Greetings! I'm Squirtle\n Squirt Squirt?");
+
+        System.out.println(linebreak);
+
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.print("Give me a command master: ");
+            String inputString = input.nextLine();
+
+            if (inputString.equals("bye")) {
+                System.out.println(linebreak);
+                System.out.println(" See you again master!");
+                System.out.println(linebreak);
+                break;
+            }
+            System.out.println(linebreak);
+            System.out.println(inputString);
+            System.out.println(linebreak);
+        }
     }
 }
