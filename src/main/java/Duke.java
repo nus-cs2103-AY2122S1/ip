25f9b1d7e801f6399022f40ecbed099aad4c1c31
Trushti <tsm20901@gmail.com>
2021-08-16 10:46:40 +0800

    Add Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,20 +9,37 @@ public class Duke {
         System.out.println("    How can I help to make your day as amazing as you are?");
         System.out.println("_____________________________________________________________________");
 
+        String[] arr = new String[100];
+        int i = 0;
+
         while (true) {
             Scanner sc = new Scanner(System.in);
             String inp = sc.nextLine();
+            arr[i++] = inp;
 
-            if (!inp.equals("bye")) {
+            if (inp.equals("list")) {
                 System.out.println("_____________________________________________________________________");
-                System.out.println("    " + inp);
+                arr[--i] = null;
+                for (int j=0; j < arr.length; j++){
+                    if (arr[j] != null) {
+                        System.out.println("    " + (j + 1) + ". " + arr[j]);
+                    }
+                    else {
+                        break;
+                    }
+                }
                 System.out.println("_____________________________________________________________________");
             }
-            else{
+            else if (inp.equals("bye")) {
                 System.out.println("_____________________________________________________________________");
                 System.out.println("    Bye. Hope to see you again soon! Just a little reminder : YOU ARE AWESOME :))");
                 System.out.println("_____________________________________________________________________");
                 break;
+            }
+            else{
+                System.out.println("_____________________________________________________________________");
+                System.out.println("    added: " + inp);
+                System.out.println("_____________________________________________________________________");
             }
         }
 
