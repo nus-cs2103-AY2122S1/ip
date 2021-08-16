db1d84a02b5d2bac77d99de447aa45f0529586a4
vigneshsankariyer1234567890 <68676914+vigneshsankariyer1234567890@users.noreply.github.com>
2021-08-16 15:29:50 +0800

    Add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,50 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Duke {
+    private final List<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        printInitialGreeting();
+        Duke duke = new Duke();
+
+        duke.printInitialGreeting();
         String input = sc.nextLine();
         while(!input.equals("bye")) {
-            echo(input);
+            duke.response(input);
             input = sc.nextLine();
         }
-        printExitMessage();
+        duke.close();
     }
 
-    public static void echo(String input) {
-        System.out.println(input);
+    public void response(String input) {
+        if (input.equals("list")) {
+            printList();
+        } else {
+            addToList(input);
+        }
     }
 
-    public static void printInitialGreeting() {
+    public void printInitialGreeting() {
         System.out.println("Hello I'm Duke\n" +
                 "What can I do for you?");
     }
 
-    public static void printExitMessage() {
+    public void close() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private void printList() {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i));
+        }
+    }
+
+    private void addToList(String input) {
+        this.list.add(input);
+        System.out.println("added: " + input);
     }
 
     public static void printLogo() {
