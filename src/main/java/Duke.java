fcc45f493667f1eda50b5ab0545aa59145e3cb46
chunweii <chunweilee.99@gmail.com>
2021-08-17 01:14:34 +0800

    Add basic skeletal chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static Scanner sc;
     public static void main(String[] args) {
+        sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello, I am\n" + logo);
+        System.out.println("What can I do for you today?");
+        System.out.println("------------------");
+        String input;
+        while (sc.hasNextLine()) {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            }
+            System.out.println(input);
+            System.out.println("------------------");
+        }
     }
 }
