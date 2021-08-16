457ceb171ce07922d99329d1135b2b46c25879d4
ntwbruce <e0544240@u.nus.edu>
2021-08-17 04:43:03 +0800

    Reformatted Duke for greater clarity

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,17 +18,17 @@ public class Duke {
 
                 System.out.println("  ____________________________________________________________");
 
-                if (text.equals("bye")) { // bye sequence exits the loop, ends process
+                if (text.equals("bye")) { // bye function: exits the loop, ends process
                     System.out.println("  See you next time!");
                     bye = true;
                     
-                } else if (text.equals("list")) { // list iterates through taskList and prints each Task's listEntry
+                } else if (text.equals("list")) { // list function: iterates through taskList and prints each Task's listEntry
                     System.out.println("  Here are the tasks in your list:");
                     for (int i = 0; i < listLength; ++i) {
                         System.out.println("  " + (i + 1) + "." + taskList[i].listEntry());
                     }
                     
-                } else if (text.split(" ")[0].equals("done")) { // if done keyword used, calls the Task's setDone()
+                } else if (text.split(" ")[0].equals("done")) { // done function: sets a task to done
                     if (text.split(" ").length == 1) {
                         throw new DukeException("invalidDone");
                     } else {
@@ -41,60 +41,71 @@ public class Duke {
                         }
                     }
                     
-                } else if (text.split(" ").length > 1){ // task name and details provided, new task to be added
-                    Task newTask;
+                } else { // task function: add tasks
+                    if (text.split(" ").length == 1) { // task details not given or not valid task
+                        switch (text) {
+                            case "deadline":
+                                throw new DukeException("deadlineDesc");
 
-                    // split text string, first string will be the type of task and second string will be the task details
-                    String[] taskString = text.split(" ", 2);
-                    String taskType = taskString[0];
-                    String taskDetails = taskString[1];
+                            case "event":
+                                throw new DukeException("eventDesc");
 
-                    // determine type of task, create new task
-                    if (taskType.equals("deadline")) {
-                        String[] details = taskDetails.split(" /by ");
-                        if (details.length == 1) {
-                            throw new DukeException("deadlineTime");
-                        } else {
-                            newTask = new Deadline(details[0], details[1]);
+                            case "todo":
+                                throw new DukeException("todoDesc");
+
+                            default:
+                                throw new DukeException("invalidInput");
                         }
-                        
-                    } else if (taskType.equals("event")) {
-                        String[] details = taskDetails.split(" /at ");
-                        if (details.length == 1) {
-                            throw new DukeException("eventPeriod");
-                        } else {
-                            newTask = new Event(details[0], details[1]);
-                        }
-                        
-                    } else if (taskType.equals("todo")) {
-                        newTask = new Todo(taskDetails);
-                        
-                    } else { // taskName is invalid
-                        throw new DukeException("invalidInput");
-                    }
-                    // add task to taskList
-                    taskList[listLength++] = newTask;
-                    System.out.print("  Got it. I've added this task:\n    " + newTask.listEntry() + "\n  Now you have " + listLength + " tasks in the list.\n");
-                
-                } else { // single word command is invalid or incomplete task input
-                    if (text.equals("deadline")) {
-                        throw new DukeException("deadlineDesc");
-                        
-                    } else if (text.equals("event")) {
-                        throw new DukeException("eventDesc");
-                        
-                    } else if (text.equals("todo")) {
-                        throw new DukeException("todoDesc");
-                        
+
                     } else {
-                        throw new DukeException("invalidInput");
+                        Task newTask;
+
+                        // split text string, first string will be the type of task and second string will be the task details
+                        String[] taskString = text.split(" ", 2);
+                        String taskType = taskString[0];
+                        String taskDetails = taskString[1];
+
+                        // determine type of task, create new task
+                        switch (taskType) {
+                            case "deadline": {
+                                String[] details = taskDetails.split(" /by ");
+                                
+                                if (details.length == 1) { // time of deadline not given
+                                    throw new DukeException("deadlineTime");
+                                } else {
+                                    newTask = new Deadline(details[0], details[1]);
+                                }
+                                break;
+                            }
+                            
+                            case "event": {
+                                String[] details = taskDetails.split(" /at ");
+                                
+                                if (details.length == 1) { // period of event not given
+                                    throw new DukeException("eventPeriod");
+                                } else {
+                                    newTask = new Event(details[0], details[1]);
+                                }
+                                break;
+                            }
+                            
+                            case "todo":
+                                newTask = new Todo(taskDetails);
+                                break;
+                                
+                            default:  // taskName is invalid
+                                throw new DukeException("invalidInput");
+                        }
+                        // add task to taskList
+                        taskList[listLength++] = newTask;
+                        System.out.print("  Got it. I've added this task:\n    " + newTask.listEntry() + "\n  Now you have " + listLength + " tasks in the list.\n");
                     }
                 }
                 
             } catch (DukeException e) {
                 System.out.println(e.getMessage());
 
-            } catch (NumberFormatException e) { // throws if the index given in the done command is not an integer
+            } catch (NumberFormatException e) { // throws if the index given in the done function is not an integer
                 System.out.println(new DukeException("invalidDone").getMessage());
                 
             } finally {
