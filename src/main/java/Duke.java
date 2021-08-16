06a9cba9a75d79cc7fd221b71c67bcf01955ddba
mokdarren <mokkwd@gmail.com>
2021-08-16 14:26:04 +0800

    Duke: Ensure that bye statement terminates program

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,41 +14,45 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         Task[] tasks = new Task[100];
         int totalTasks = 0;
+        label:
         while (true) {
             String userInput = sc.nextLine();
-            List<String> userInputList = new LinkedList<String>(Arrays.asList(userInput.split(" ")));
+            List<String> userInputList = new LinkedList<>(Arrays.asList(userInput.split(" ")));
             String command = userInputList.get(0);
-            if (command.equals("bye")) {
+            switch (command) {
+            case "bye":
                 System.out.println(div + "Bye. Hope to see you again soon!\n" + div);
-            } else if (command.equals("list")) {
+                break label;
+            case "list":
                 System.out.println(div + "Here are the tasks in your list:");
                 for (int i = 0; i < totalTasks; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
                 System.out.println(div);
-            } else if (command.equals("done")) {
+                break;
+            case "done":
                 Task taskCompleted = tasks[Integer.parseInt(userInputList.get(1)) - 1];
                 taskCompleted.markAsDone();
                 System.out.println(div + "Nice! I've marked this task as done:");
                 System.out.println("  " + taskCompleted + "\n" + div);
-            }
-            else {
+                break;
+            default:
                 // if not a specified command, add new task to list
                 // remove first command string
                 userInputList.remove(0);
                 if (command.equals("todo")) {
-                    String description = String.join(" ",userInputList);
+                    String description = String.join(" ", userInputList);
                     tasks[totalTasks] = new ToDo(description);
                 } else {
                     if (command.equals("deadline")) {
                         //split description and by
-                        String[] userInputArgs = String.join(" ",userInputList).split("/by ");
+                        String[] userInputArgs = String.join(" ", userInputList).split("/by ");
                         String description = userInputArgs[0];
                         String by = userInputArgs[1];
                         tasks[totalTasks] = new Deadline(description, by);
-                    } else if (command.equals("event")){
+                    } else if (command.equals("event")) {
                         //split description and at
-                        String[] userInputArgs = String.join(" ",userInputList).split("/at ");
+                        String[] userInputArgs = String.join(" ", userInputList).split("/at ");
                         String description = userInputArgs[0];
                         String at = userInputArgs[1];
                         tasks[totalTasks] = new Event(description, at);
@@ -58,6 +62,7 @@ public class Duke {
                 System.out.println(div + "Got it. I've added this task:\n  " + tasks[totalTasks - 1] + "\n"
                         + "Now you have " + totalTasks + " tasks in the list.\n" + div);
 
+                break;
             }
         }
     }
