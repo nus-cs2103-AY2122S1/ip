436ca3a8827a85c71bee876b32a0fe9f988f9015
charltonator <charltonlucian@gmail.com>
2021-08-16 16:21:15 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        GreetingBot newBot = new GreetingBot();
+        newBot.startBot();
     }
 }
+
+
+
+
--- src/main/java/GreetingBot.java
+++ src/main/java/GreetingBot.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class GreetingBot {
+import java.util.Scanner;
+
+public class GreetingBot {
+
+    public GreetingBot() {
+
+    }
+
+    public void startBot() {
+        greet();
+    }
+
+
+    private void greet() {
+        String greetingMessage = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(greetingMessage);
+        echo();
+    }
+
+    private void echo() {
+        Scanner inputScanner = new Scanner(System.in);
+        while (true) {
+            String nextLine = inputScanner.nextLine();
+            if (nextLine.equals("bye")) {
+                exit();
+            } else {
+                System.out.println(nextLine);
+            }
+        }
+    }
+
+    private void exit() {
+        String exitMessage = "Bye. Hope to see you again soon!";
+        System.out.println(exitMessage);
+    }
 }
