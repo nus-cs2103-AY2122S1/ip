c93f6a00579327d64d0c8cae233da27eea94b24b
Jai <jaylulla9@gmail.com>
2021-08-16 18:46:42 +0800

    Add Level 1 Features to the Chatbot
    The Chatbot should be able to interact with the user.
    
    To achieve that, the first step done is to echo back what
    the user enters.
    
    The Chatbot would shutdown when the
    user inputs "bye"(Case Sensitivity is taken care of).

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * A Chatbot to help manage your daily schedule.
+ */
 public class Duke {
+
+    /**
+     * Returns an interactive session with a Chatbot. Basically a mean to interact with it.
+     *
+     * @param args Here, usually nothing is passed.
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String userInput;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("________________________________________");
+        System.out.println("Hello, I am Retriever\nHow Can I Help You Today?");
+        System.out.println("________________________________________");
+
+        do {
+            userInput = sc.nextLine();
+            if(userInput.toLowerCase().compareTo("bye") != 0) {
+                System.out.println("-> " + userInput);
+            }
+            System.out.println("________________________________________");
+        } while (userInput.toLowerCase().compareTo("bye") != 0);
+
+        System.out.println("-> Sad To See You Go!");
+
     }
 }
