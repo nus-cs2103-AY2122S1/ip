c43e4df072471f6337217de469591f1269e5499c
rootkie <rkxuanfeng@gmail.com>
2021-08-16 16:24:47 +0800

    Add functional reply and greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Duke {
+    private static void reply(String msg) {
+        String indentedMsg = Arrays.asList(msg.split("\n")).stream()
+                                   .map(s -> String.format("\t%s\n", s))
+                                   .collect(Collectors.joining(""));
+        System.out.printf(
+                        "\t____________________________________\n" +
+                        "%s" +
+                        "\t____________________________________\n", indentedMsg);
+    }
+
+    private static void greet() {
+        reply("Hello! I'am Duke\n" +
+                "What can I do for you?");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
     }
 }
