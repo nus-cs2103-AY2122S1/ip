a85fbc1ca44581062409585ef9d6153dc9832612
Gao Haochun <e0376955@u.nus.edu>
2021-08-16 21:12:04 +0800

    t:

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(greet());
+        String input = sc.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println(echo(input));
+            input = sc.nextLine();
+        }
+
+        System.out.println(exit());
     }
+
+    public static String greet() {
+        String output = "    ____________________________________________________________\n"
+                + "     Hello! I'm Duke\n"
+                + "     What can I do for you?\n"
+                + "    ____________________________________________________________\n";
+
+        return output;
+    }
+
+    public static String echo(String input) {
+        String output = "    ____________________________________________________________\n"
+                + "     " + input +"\n"
+
+                + "    ____________________________________________________________\n";
+
+        return output;
+    }
+
+    public static String exit() {
+        String output = "    ____________________________________________________________\n"
+                + "     Bye. Hope to see you again soon!\n"
+                + "    ____________________________________________________________\n";
+        return output;
+    }
+
+
 }
