31f5c645ee725182c084edc66dfccb08896180b2
Shezad <shezadhassan22@gmail.com>
2021-08-16 21:39:20 +0800

    More error handling in add and delete functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,10 +7,10 @@ public class Duke {
     public static void main(String[] args) {
         // Introduction message
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo + "\nWhat can I do for you?");
         drawLine();
 
