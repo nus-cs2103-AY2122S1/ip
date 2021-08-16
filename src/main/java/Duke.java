2a9de85ef538d60ba087583f8e98c859324a8479
Jeromesyl <jsyl3214@gmail.com>
2021-08-16 22:34:44 +0800

    Add level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
-    public static String getInput() {
+    private static ArrayList<String> taskList = new ArrayList<String>();
+
+    public static int handleInput() {
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+        String input;
+        do{
+            input = scanner.nextLine();
+            switch (input.toUpperCase()) {
+                case "BYE":
+                    System.out.println("Bai bai!");
+                    break;
+                case "LIST":
+                    System.out.println("Here are your tasks!");
+                    for (int i = 0; i<taskList.size(); i++) {
+                        System.out.println(String.format("%d. %s", i+1, taskList.get(i)));
+                    }
+                    break;
+                default:
+                    taskList.add(input);
+                    System.out.println("Added: " + input);
+            }
+
+        }
+        while (!input.toUpperCase().equals("BYE"));
+        return 0;
     }
+
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -14,16 +38,7 @@ public class Duke {
         String greeting = "Hello! I'm Duke.\n" + "What can I do for you? :)";
         System.out.println(logo + greeting);
 
-        String input;
-        while (true) {
-            input = getInput();
-            if (input.toUpperCase().equals("BYE")) {
-                break;
-            }
-            System.out.println(input);
-        }
-        System.out.println("Bai bai!");
-
+        handleInput();
     }
 
 }
