5a662893ed582540c883b4d138932d4c39318781
Chesterwongz <psyforce99@gmail.com>
2021-08-16 23:10:10 +0800

    Level-3
    Tasks can now be marked as done.  Only works when task to be marked is a valid number. This will be address in Level-5 error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,24 +6,18 @@ import java.lang.String;
  * @author Chesterwongz
  */
 public class Duke {
-
-    private static final String[] todoList = new String[100];
+    private final static int MAX_LIST_LENGTH = 100;
+    private static final Task[] taskList = new Task[MAX_LIST_LENGTH];
     private static int listIndex = 0;
-
-    /**
-     * Frames the message with underscore lines.
-     *
-     * @param msg The String we want to frame.
-     * @return The framed String.
-     */
-    private static void print(String msg) {
-        String line = "________________________________";
-        System.out.println(line);
-        System.out.println(msg);
-        System.out.println(line);
-        System.out.println();
+    private static boolean isInputBye(String input) {
+        return input.compareToIgnoreCase("bye") == 0;
     }
-
+    private static boolean isInputList(String input) {
+        return input.compareToIgnoreCase("list") == 0;
+    }
+    private static boolean isInputDone(String input) {
+        return input.startsWith("done ");
+    }
     /**
      * Prints the Duke logo.
      */
@@ -33,7 +27,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|";
-        print("Hello from\n" + logo);
+        echo("Hello from\n" + logo);
     }
     /**
      * Prints our greeting.
@@ -41,36 +35,66 @@ public class Duke {
     private static void greet() {
         String greeting = "Hello! I'm Duke\n"
                 + "What can I do for you?";
-        print(greeting);
+        echo(greeting);
     }
     /**
      * Prints our goodbye.
      */
     private static void bye() {
         String bye = "Bye! Hope to see you again soon!";
-        print(bye);
+        echo(bye);
+    }
+
+    /**
+     * Adds item to the todolist.
+     * @param str
+     */
+    private static void addToList(String str) {
+        taskList[listIndex++] = new Task(str);
+        echo("added: " + str);
     }
     /**
-     * Echos the user's input.
+     * Marks task as done in taskList.
+     * @param input The input string.
+     */
+    private static void doTask(String input) {
+        int taskID = Integer.parseInt(input.substring(input.indexOf(" ") + 1));
+        String msg;
+        int taskIndex = taskID - 1;
+        if (taskList[taskIndex] != null) {
+            taskList[taskIndex].markAsDone();
+            msg = "Nice! I've marked this task as done:\n  "
+                    + taskList[taskIndex].toString();
+        } else {
+            msg = "OOPS!!! Task " + taskID + " doesn't exist!";
+        }
+        echo(msg);
+    }
+    /**
+     * Frames the message with underscore lines and prints it.
+     *
+     * @param msg The String we want to frame.
+     * @return The framed String.
      */
     private static void echo(String msg) {
-        print(msg);  // Output user input
+        String line = "________________________________";
+        System.out.println(line);
+        System.out.println(msg);
+        System.out.println(line);
+        System.out.println();
     }
-
-    private static void addToList(String str) {
-        todoList[listIndex++] = str;
-        print("added: " + str);
-    }
-
+    /**
+     * Prints out the todolist.
+     */
     private static void printList() {
         int index = 1;
         String line = "________________________________";
         System.out.println(line);
-        for (String item : todoList) {
+        for (Task item : taskList) {
             if (item == null) {
                 break;
             }
-            System.out.println(index++ + ". " + item);
+            System.out.println(index++ + "." + item);
         }
         System.out.println(line);
         System.out.println();
@@ -84,16 +108,15 @@ public class Duke {
         while (!isExited) {
             // String input
             String input = sc.nextLine();
-            switch (input) {
-                case "bye":
-                    isExited = true;
-                    bye();
-                    break;
-                case "list":
-                    printList();
-                    break;
-                default:
-                    addToList(input);
+            if (isInputBye(input)) {
+                isExited = true;
+                bye();
+            } else if (isInputList(input)) {
+                printList();
+            } else if (isInputDone(input)) {
+                doTask(input);
+            } else {
+                addToList(input);
             }
         }
     }
