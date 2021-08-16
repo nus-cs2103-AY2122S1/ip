f2bb30e8cda4a0ea553ec7dcb906c8f00b5f5910
Nicolas <nicolas.chang99@gmail.com>
2021-08-16 18:22:46 +0800

    Add helper function to format text

--- src/main/java/Kermit.java
+++ src/main/java/Kermit.java
@@ -1,10 +1,15 @@
 public class Kermit {
+
+    /**
+     * Adds a top and bottom horizontal line to text
+     * @param text Text to be formatted.
+     * @return Formatted version of text.
+     */
+    public static String formatText(String text) {
+        String horizontalDivider = "____________________________________________________________";
+        return horizontalDivider + "\n" + text + "\n" + horizontalDivider;
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(formatText("Hello from Kermit, eaten any flies today?\nWhat can I do for you?"));
     }
 }
