b9a1799217617a0f4f3b74291b14e325f81064aa
DESKTOP-IO09L3B\wilbu <wilbur.fongthemasterchief@gmail.com>
2021-08-16 16:14:05 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,69 @@
+import java.util.Locale;
+
 public class Duke {
+
+    public static boolean active;
+    public static String startMessage = "Hello! I'm Duke \nWhat can I do for you?";
+    public static String endMessage = "Bye! Hope to see you again soon!";
+    public static String[] list = new String[100];
+    public static int listIndex = 0;
+
+
+    public static void awaken() {
+        Duke.active = true;
+
+        Duke.sendStartMessage();
+        while (active) {
+            java.util.Scanner scanner = new java.util.Scanner(System.in);
+            String input = scanner.nextLine();
+
+            if (input.equals("list")) {
+                Duke.printList();
+            }
+
+            if (!input.equals("list") && !input.equals("bye")) {
+                Duke.addToList(input);
+            }
+
+            if (input.equals("bye")) {
+                Duke.active = false;
+                Duke.sendEndMessage();
+                break;
+            }
+        }
+    }
+
+    public static void addToList(String input) {
+        Duke.list[Duke.listIndex] = input;
+        Duke.listIndex++;
+        System.out.println("added: " + input);
+    }
+
+    public static void printList() {
+        int number = 1;
+
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] != null) {
+                System.out.println(number + ". " + list[i]);
+                number++;
+            }
+        }
+    }
+
+    public static void sendStartMessage() {
+        System.out.println("____________________________________________________________");
+        System.out.println(startMessage);
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void sendEndMessage() {
+        System.out.println("____________________________________________________________");
+        System.out.println(endMessage);
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+    /*
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -30,7 +88,8 @@ public class Duke {
                 break;
             }
         }
+     */
+
+        Duke.awaken();
     }
-
-
 }
