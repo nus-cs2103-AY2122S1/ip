ca7c17dd7aa1f6d9ae1dbfa5cb5d0d5fe5a9caaa
Cyn7hius <64959508+Cyn7hius@users.noreply.github.com>
2021-08-16 12:54:35 +0800

    Added ability to mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
 
-        String divider = "---------------------------";
+        // ASCII divider to clean up the output
+        final String divider = "---------------------------";
 
-        ArrayList<String> taskList = new ArrayList<>();
+        // Initialise the taskList
+        ArrayList<Task> taskList = new ArrayList<>();
 
         Scanner in = new Scanner(System.in);
         System.out.println(
@@ -19,24 +21,43 @@ public class Duke {
         while (true) {
             String nextTask = in.nextLine();  // Read user input
 
+            // Uses a series of checks to only check for "done (number)"
+            if (nextTask.contains("done ")) {
+                if (Character.isDigit(nextTask.charAt(5))) {
+                    int taskIndex = Character.getNumericValue(nextTask.charAt(5)) - 1;
+                    taskList.get(taskIndex).markAsDone();
+                    System.out.println("Nice! I've marked this task as done: " +
+                            "\n" +
+                            taskList.get(taskIndex) +
+                            "\n" +
+                            divider);
+                }
+                // case where "done" is a task e.g. "done my homework"
+                else {
+                    taskList.add(new Task(nextTask));
+                    System.out.println(divider + "\n" + "added: " + nextTask + "\n" + "\n" + divider);
+                }
+                continue;
+            }
 
+            // If we want to show the list, it has to not add it to taskList
             if (nextTask.equals("list")) {
                 System.out.println(divider);
                 System.out.println("Here are the items in your To Do List: ");
                 for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println(i+1 + ". " + taskList.get(i));
+                    System.out.println(i + 1 + ". " + taskList.get(i));
                 }
                 System.out.println("\n" + divider);
                 continue;
             }
 
-            taskList.add(nextTask);
 
             if (nextTask.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
 
+            taskList.add(new Task(nextTask));
             System.out.println(divider + "\n" + "added: " + nextTask + "\n" + "\n" + divider);
         }
     }
