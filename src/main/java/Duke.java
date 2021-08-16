edba4bcb8181d4a25aa701e5c79cd43b2bb18d2f
michael-lee-sk <e0543977@u.nus.edu>
2021-08-16 17:55:49 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,29 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    private static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "[X]" : "[ ]");
+        }
+
+        public void setDone() {
+            this.isDone = true;
+        }
+
+        public String getDescription() {
+            return this.description;
+        }
+    }
+
     public static void main(String[] args) {
 
         final String INTRO = "Hello! I'm Duke\n" +
@@ -10,7 +33,7 @@ public class Duke {
         final String OUTRO = "Bye. Hope to see you again soon!";
         Scanner sc = new Scanner(System.in);
 
-        ArrayList<String> lst = new ArrayList<>();
+        ArrayList<Task> lst = new ArrayList<>();
         String command;
 
 //        String logo = " ____        _        \n"
@@ -30,11 +53,21 @@ public class Duke {
 
             } else if (command.equals("list")) {
                 for (int i = 0; i < lst.size(); i++) {
-                    System.out.println((i + 1) + ". " + lst.get(i));
+                    Task currentTask = lst.get(i);
+                    System.out.println((i + 1) + "." + currentTask.getStatusIcon() + " " + currentTask.getDescription());
                 }
 
+            }  else if (command.substring(0, 4).equals("done")) {
+            int index = Integer.parseInt(command.substring(5)) - 1;
+            String subtext = "Nice! I've marked this task as done:\n";
+
+            Task currentTask = lst.get(index);
+            currentTask.setDone();
+
+            System.out.println(subtext + currentTask.getStatusIcon() + " " + currentTask.getDescription());
+
             } else {
-                lst.add(command);
+                lst.add(new Task(command));
                 System.out.println("added: " + command);
             }
         }
