632d0a4e0d7cad0b39b324796228ce6d3fe8f237
Cyn7hius <64959508+Cyn7hius@users.noreply.github.com>
2021-08-16 17:02:12 +0800

    Added new subclasses of Task. Added ability to add 3 different types of tasks: todo, event and deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -56,9 +56,40 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
+            
 
-            taskList.add(new Task(nextTask));
-            System.out.println(divider + "\n" + "added: " + nextTask + "\n" + "\n" + divider);
+            // new todo task
+            if (nextTask.substring(0, 5).equals("todo ")) {
+                String todoDesc = nextTask.substring(5);
+                taskList.add(new Todo(todoDesc));
+            }
+
+            // new event task
+            if (nextTask.substring(0, 6).equals("event ")) {
+                // need to check that for event and deadline they use the /at and /by properly else reject
+                if (!nextTask.contains("/at ")) {
+                    System.out.println("Please use this format: 'event <task> /at <date and time>' to specify the date and time!");
+                    continue;
+                }
+                int eventDateIndex = nextTask.indexOf("/at ") + 4;
+                String eventDesc = nextTask.substring(6, eventDateIndex - 4); //skip "event "
+                taskList.add(new Event(eventDesc, nextTask.substring(eventDateIndex)));
+            }
+
+            // new deadline task
+            if (nextTask.substring(0, 9).equals("deadline ")) {
+                // need to check that for event and deadline they use the /at and /by properly else reject
+                if (!nextTask.contains("/by ")) {
+                    System.out.println("Please use this format: 'deadline <task> /by <date and time>' to specify the date and time!");
+                    continue;
+                }
+                int deadlineDateIndex = nextTask.indexOf("/by ") + 4;
+                String deadlineDesc = nextTask.substring(9, deadlineDateIndex - 4); //skip the "deadline "
+                taskList.add(new Deadline(deadlineDesc, nextTask.substring(deadlineDateIndex)));
+            }
+
+            System.out.println(divider + "\n" + "added: " + taskList.get(taskList.size() - 1) + "\n"  +
+                    "now you have: " + taskList.size() + " tasks! type 'list' to see them!"  + "\n" + divider);
         }
     }
 }
