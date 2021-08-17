9ecc1630bf0f65bc39582d4e8698c51c448422ff
muhammad-khair <muhammad_khair@outlook.com>
2021-08-18 03:31:28 +0800

    Add Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,31 @@
+public class Event extends Task {
+
+    private String timing;
+
+    public Event(String description, boolean isDone, String timing) {
+        super(description, isDone);
+        this.timing = timing;
+    }
+
+    public Event(String description, String timing) {
+        super(description);
+        this.timing = timing;
+    }
+
+    public String getTiming() {
+        return timing;
+    }
+
+    public void setTiming(String timing) {
+        this.timing = timing;
+    }
+
+    public String getFormattedTiming() {
+        return "( at: " + timing + ")";
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " " + getFormattedTiming();
+    }
+}
