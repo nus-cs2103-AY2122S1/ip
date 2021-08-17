f213bb7bead609ddf690a7bfd8403f0f48b0f29a
Willy Angga <ampedwilly@gmail.com>
2021-08-17 09:05:59 +0800

    Add automated-testing, bug fixes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -65,7 +65,8 @@ public class Duke {
                 + "   e.g \"deadline submit homework /by Sunday 12 pm\" \n"
                 + "2. \"list\" to see the list of tasks \n"
                 + "3. \"done [number]\" to mark a particular task as done \n"
-                + "4. \"bye\" to exit"
+                + "4. \"delete [number]\" to delete a particular task \n"
+                + "5. \"bye\" to exit"
         );
         List<Task> ls = new ArrayList<>();
         do {
@@ -74,39 +75,35 @@ public class Duke {
                 arr = input.split(" ");
                 if (input.equals("bye")) {
                     System.out.println("Good Bye. Have a nice day!");
-                }
-                else if (arr[0].equals("done")
-                        && Integer.parseInt(arr[1]) <= ls.size()
-                        && Integer.parseInt(arr[1]) > 0) {
-                    System.out.println("Nice! I've marked this task as done: ");
-                    ls.get(parseInt(arr[1]) - 1).markAsDone();
-                    System.out.println(ls.get(parseInt(arr[1]) - 1).toString());
-                }
-                else if (arr[0].equals("done") && ls.size() == 0) {
-                    throw new InvalidCommandException("You have not added any task!");
-                }
-                else if (arr[0].equals("done")
-                        && (Integer.parseInt(arr[1]) > ls.size()
-                        || Integer.parseInt(arr[1]) <= 0)) {
-                    throw new InvalidValueException("Enter a valid number!");
-                }
-                else if (arr[0].equals("delete")
-                        && Integer.parseInt(arr[1]) <= ls.size()
-                        && Integer.parseInt(arr[1]) > 0) {
-                    System.out.println("Noted. I've removed this task: ");
-                    System.out.println(ls.get(parseInt(arr[1]) - 1).toString());
-                    ls.remove(parseInt(arr[1]) - 1);
-
-                }
-                else if (arr[0].equals("delete") && ls.size() == 0) {
-                    throw new InvalidCommandException("You have not added any task!");
-                }
-                else if (arr[0].equals("delete")
-                        && (Integer.parseInt(arr[1]) > ls.size()
-                        || Integer.parseInt(arr[1]) <= 0)) {
-                    throw new InvalidValueException("Enter a valid number!");
-                }
-                else if (arr[0].equals("todo")) {
+                } else if (arr[0].equals("done")) {
+                    if (arr.length == 1) {
+                        throw new InvalidCommandException("Please specify a number");
+                    } else if (arr[0].equals("done") && ls.size() == 0) {
+                        throw new InvalidCommandException("You have not added any task!");
+                    } else if (arr[0].equals("done")
+                            && (Integer.parseInt(arr[1]) > ls.size()
+                            || Integer.parseInt(arr[1]) <= 0)) {
+                        throw new InvalidValueException("Enter a valid number!");
+                    } else {
+                        System.out.println("Nice! I've marked this task as done: ");
+                        ls.get(parseInt(arr[1]) - 1).markAsDone();
+                        System.out.println(ls.get(parseInt(arr[1]) - 1).toString());
+                    }
+                } else if (arr[0].equals("delete")) {
+                    if (arr.length == 1) {
+                        throw new InvalidCommandException("Please specify a number");
+                    } else if (arr[0].equals("delete") && ls.size() == 0) {
+                        throw new InvalidCommandException("You have not added any task!");
+                    } else if (arr[0].equals("delete")
+                            && (Integer.parseInt(arr[1]) > ls.size()
+                            || Integer.parseInt(arr[1]) <= 0)) {
+                        throw new InvalidValueException("Enter a valid number!");
+                    } else {
+                        System.out.println("Noted. I've removed this task: ");
+                        System.out.println(ls.get(parseInt(arr[1]) - 1).toString());
+                        ls.remove(parseInt(arr[1]) - 1);
+                    }
+                } else if (arr[0].equals("todo")) {
                     if (arr.length < 2) {
                         throw new EmptyDescriptionException("Missing description / date");
                     }
@@ -114,43 +111,41 @@ public class Duke {
                     ls.add(new Todo(arr[0]));
                     System.out.println(ls.get(ls.size() - 1).toString());
                     System.out.println("Now you have " + ls.size() + " tasks in the list.");
-                }
-                else if (arr[0].equals("deadline")) {
+                } else if (arr[0].equals("deadline")) {
                     if (arr.length < 2) {
                         throw new EmptyDescriptionException("Missing description / date");
                     }
                     System.out.println("Got it. I've added this task: ");
                     ls.add(new Deadline(getDescription(arr), getDeadline(arr)));
+                    System.out.println(ls.get(ls.size() - 1).toString());
                     System.out.println("Now you have " + ls.size() + " tasks in the list.");
-                }
-                else if (arr[0].equals("event")) {
+                } else if (arr[0].equals("event")) {
                     if (arr.length < 2) {
                         throw new EmptyDescriptionException("Missing description / date");
                     }
                     System.out.println("Got it. I've added this task: ");
                     ls.add(new Event(getDescription(arr), getDeadline(arr)));
+                    System.out.println(ls.get(ls.size() - 1).toString());
                     System.out.println("Now you have " + ls.size() + " tasks in the list.");
-                }
-                else if (input.equals("list")) {
-                    System.out.println("Here are the tasks in your list: ");
-                    for(int i = 0; i < ls.size(); i++) {
-                        System.out.println((i + 1) + "." + ls.get(i).toString());
+                } else if (input.equals("list")) {
+                    if (ls.size() == 0) {
+                        System.out.println("You currently do not have any task!");
+                    } else {
+                        System.out.println("Here are the tasks in your list: ");
+                        for (int i = 0; i < ls.size(); i++) {
+                            System.out.println((i + 1) + "." + ls.get(i).toString());
+                        }
                     }
-                }
-                else {
+                } else {
                     throw new InvalidCommandException("Command not Found");
                 }
-            }
-            catch (InvalidCommandException e) {
+            } catch (InvalidCommandException e) {
+                System.out.println(e.toString());
+            } catch (EmptyDescriptionException e) {
+                System.out.println(e.toString());
+            } catch (InvalidValueException e) {
                 System.out.println(e.toString());
             }
-            catch (EmptyDescriptionException e) {
-                System.out.println(e.toString());
-            }
-            catch (InvalidValueException e) {
-                System.out.println(e.toString());
-            }
-
         } while (!input.equals("bye"));
     }
 }
