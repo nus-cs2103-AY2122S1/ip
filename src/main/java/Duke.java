fa4dcfe7b98a96b6800f3fb8463760b9a4469bea
mweeruien <mweeruien@gmail.com>
2021-08-18 01:45:53 +0800

    Completed Level 4, with previous commands now supporting the Deadline, Todo and Event tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,78 +2,76 @@ import java.util.*;
 
 public class Duke {
 
+    // This class represents the tasks added by the user
+    class Task {
+        protected final String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+            public String toString() {
+                return "[" + this.getStatusIcon() + "] ";
+            }
+    }
+
+    class Deadline extends Task {
+        protected final String dateBy;
+
+        public Deadline(String description, String dateBy) {
+            super(description);
+            this.dateBy = dateBy;
+        }
+
+        @Override
+        public String toString() {
+            return "[D]" + super.toString() + description + " (by: " + dateBy + ")";
+        }
+    }
+
+    class Event extends Task {
+        protected final String eventDetails;
+
+        public Event(String description, String eventDetails) {
+            super(description);
+            this.eventDetails = eventDetails;
+        }
+
+        @Override
+        public String toString() {
+            return "[E]" + super.toString() + description + " (at: " + eventDetails + ")";
+        }
+    }
+
+    class Todo extends Task {
+
+        public Todo(String description) {
+            super(description);
+        }
+
+        @Override
+        public String toString() {
+            return "[T]" + super.toString() + description;
+        }
+    }
+
     public static void main(String[] args) {
-
-        // This class represents the tasks added by the user
-        class Task {
-            protected final String description;
-            protected boolean isDone;
-
-            public Task(String description) {
-                this.description = description;
-                this.isDone = false;
-            }
-
-            public String getStatusIcon() {
-                return (isDone ? "X" : " "); // mark done task with X
-            }
-
-            public void markAsDone() {
-                this.isDone = true;
-            }
-
-//            public String toString() {
-//                return "[" + this.getStatusIcon() + "] " + this.description;
-//            }
-        }
-
-        class Deadline extends Task {
-            protected final String dateBy;
-
-            public Deadline(String description, String dateBy) {
-                super(description);
-                this.dateBy = dateBy;
-            }
-
-            @Override
-            public String toString() {
-                return "[D]" + super.toString() + " (by: " + dateBy + ")";
-            }
-        }
-
-        class Event extends Task {
-            protected final String eventDetails;
-
-            public Event(String description, String eventDetails) {
-                super(description);
-                this.eventDetails = eventDetails;
-            }
-
-            @Override
-            public String toString() {
-                return "[E]" + super.toString() + " (at: " + eventDetails + ")";
-            }
-        }
-
-        class Todo extends Task {
-            protected final String todoItem;
-
-            public Todo(String description, String todoItem) {
-                super(description);
-                this.todoItem = todoItem;
-            }
-
-            @Override
-            public String toString() {
-                return "[D]" + super.toString() + " (dateBy: " + todoItem + ")";
-            }
-        }
-
 
         String linebreak = "~~~~~~~~~~";
         String command; // this is the container for the command received from the user
         Task[] todoList = new Task[100]; // this array stores previous commands
         int pointer = 0; // this tracks the newest position to add an item in todoList
+        Duke duke = new Duke();
 
         // The chat bot name is Notaro bc it's Not-a-ro-bot :>
         // This is the introduction of the chat bot, and includes a list of the commands for the user
@@ -91,12 +89,12 @@ public class Duke {
             Scanner sc = new Scanner(System.in);
             command = sc.nextLine();
 
-            if (command.equals("bye")) {
-                System.out.println("Bye bye!! Thanks for stopping by!");
+            if (command.toLowerCase().equals("bye")) {
+                System.out.println("Bye bye!! It was nice meeting you!");
                 System.out.println(linebreak);
                 break;
 
-            } else if (command.equals("list")) {
+            } else if (command.toLowerCase().equals("list")) {
                 for (int counter = 0; counter < 100; counter++) {
                     if (todoList[counter] != null) {
                         System.out.println((counter + 1) + ". " + todoList[counter].toString());
@@ -106,7 +104,7 @@ public class Duke {
                     }
                 }
 
-            } else if (command.split(" ")[0].equals("done")) {
+            } else if (command.toLowerCase().split(" ")[0].equals("done")) {
                 System.out.println("Yay good job!!");
                 int ref = Integer.parseInt(command.split(" ")[1]) - 1;
                 todoList[ref].markAsDone();
@@ -114,13 +112,41 @@ public class Duke {
                 System.out.println(linebreak);
 
 
-            // Else, assume that an item is added to the chat bot
+            // Else, an item has been added to the chat bot
             } else {
+
+                String taskType = command.toLowerCase().split(" ", 2)[0];
                 System.out.println("added: " + command);
                 System.out.println(linebreak);
-                todoList[pointer] = new Task(command);
+
+                if (taskType.equals("todo")) {
+                    String taskInfo = command.split(" ", 2)[1];
+                    todoList[pointer] = duke.new Todo(taskInfo);
+
+                } else if (taskType.equals("deadline")) {
+                    String taskInfo = command.split(" ", 2)[1];
+                    String description = taskInfo.split("/by",2)[0];
+                    String dateBy = taskInfo.split("/by",2)[1];
+                    todoList[pointer] = duke.new Deadline(description, dateBy);
+
+                } else if (taskType.equals("event")) {
+                    String taskInfo = command.split(" ", 2)[1];
+                    String description = taskInfo.split("/at",2)[0];
+                    String eventDetails = taskInfo.split("/at",2)[1];
+                    todoList[pointer] = duke.new Event(description, eventDetails);
+
+                } else {
+                    System.out.println("Sorry! I don't understand this command :(");
+                    break;
+                }
+
                 pointer++;
+
             }
         }
     }
 }
+
+
+
+
