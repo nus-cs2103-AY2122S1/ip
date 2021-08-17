7a198e35568aac3330b20def7bc9a4d50f24d827
leonghongfai <leonghongfai@gmail.com>
2021-08-17 17:34:16 +0800

    Added task completion

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,11 +8,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello FROM\n" + logo);
-        greetings();
+        commands();
     }
 
-    private static void greetings() {
-        String[] store = new String[100];
+    private static void commands() {
+        Task[] store = new Task[100];
         int pointer = 1;
         System.out.println("Hello! I'm Duke\n");
         System.out.println("What can I do for you?");
@@ -21,6 +20,7 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             String command = scanner.nextLine();
+            Task task = new Task(command);
             if (command.equals("bye")) {
                 System.out.println("    ______________________________________");
                 System.out.println("     Bye. Hope to see you again soon!");
@@ -29,17 +29,47 @@ public class Duke {
                 break;
             } else if (command.equals("list")){
                 System.out.println("    ______________________________________");
+                System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < pointer - 1; i++) {
-                    System.out.printf("     %d. %s\n", i + 1, store[i]);
+                    System.out.printf("     %d.[%s] %s\n", i + 1, store[i].getStatusIcon() ,store[i].toString());
                 }
                 System.out.println("    ______________________________________");
-            } else {
-                store[pointer - 1] = command;
+            } else if (command.length() >= 6 && command.substring(0,4).equals("done")) {
+                String restOfCommand = command.substring(5,command.length());
+               boolean numeric = false;
+               try {
+                   int temp = Integer.parseInt(restOfCommand);
+                   numeric = true;
+               } catch (NumberFormatException err) {
+                   numeric = false;
+                }
+               if (numeric) {
+                   int taskNum = Integer.parseInt(restOfCommand) - 1;
+                   try {
+                       Task currTask = store[taskNum];
+                       currTask.setCompleted();
+                       System.out.println("    ______________________________________");
+                       System.out.println("     Nice! I've marked this task as done:");
+                       System.out.printf("       [%s] %s\n", currTask.getStatusIcon(), currTask.toString());
+                       System.out.println("    ______________________________________");
+                   } catch (NullPointerException err) {
+                       System.out.println("    ______________________________________");
+                       System.out.println("     This task does not exist yet!");
+                       System.out.println("    ______________________________________");
+                   }
+               } else {
+                   store[pointer - 1] = task;
+                   pointer = pointer += 1;
+                   System.out.println("    ______________________________________");
+                   System.out.printf("     added: %s\n",task.toString());
+                   System.out.println("    ______________________________________");
+               }
+            } else  {
+                store[pointer - 1] = task;
                 pointer = pointer += 1;
                 System.out.println("    ______________________________________");
-                System.out.printf("     added: %s\n",command);
+                System.out.printf("     added: %s\n",task.toString());
                 System.out.println("    ______________________________________");
-
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    private String name;
+    private boolean completion;
+
+    public Task(String name) {
+        this.name = name;
+        this.completion = false;
+    }
+
+    public void setCompleted() {
+        this.completion = true;
+    }
+
+    public String getStatusIcon() {
+        return (this.completion
+                ?"X"
+                :" ");
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
