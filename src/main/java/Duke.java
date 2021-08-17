51dc7afaf7f2e5a7df7da1f5892a36f269eded76
chengseong <chengseong02@hotmail.com>
2021-08-17 16:46:04 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] list = new String[100];
+        int index = 1;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -12,7 +14,15 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
         while(!command.equals("bye")){
-            System.out.println(command);
+            if(command.equals("list")){
+                for(int i = 1; i < index; i++){
+                    System.out.println(i + ". " + list[i]);
+                }
+            } else {
+                System.out.println("added: " + command);
+                list[index] = command;
+                index++;
+            }
             command = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
