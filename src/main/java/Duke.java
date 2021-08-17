5f4865f315500e2cae941a488d5cdbb0eb5512ad
DESKTOP-PILGCOV\yzlim <yzlim23@gmail.com>
2021-08-17 20:27:55 +0800

    Add ability to store and list user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,43 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static String command;
+    private static ArrayList<String> arr = new ArrayList<>();
+    private static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String command;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        while(true) {
+        do {
             System.out.print(">> ");
             command = in.next();
-            if (command.equals("bye")) break;
-            else System.out.println(command);
+            handleInput(command);
         }
+        while(!command.equals("bye"));
         System.out.println("Bye, hope to see you again!");
+    }
+
+    /**
+     * Handles the different input commands to Duke.
+     * @param command input command from the user
+     */
+    public static void handleInput(String command) {
+        switch (command) {
+            case "bye":
+                break;
+            case "list":
+                for (int i = 0; i < arr.size(); i++) {
+                    System.out.println(i + 1 + ": " + arr.get(i));;
+                }
+                break;
+            default:
+                arr.add(command);
+                System.out.println("Added " + command);
+        }
     }
 }
