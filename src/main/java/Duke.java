1261b0989bad95c3a94e80e24d0d6b0cad240fb6
angnobel <nobelang78@gmail.com>
2021-08-18 02:29:19 +0800

    Level-1: Fix Bye printing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Duke {
+    public static ArrayList<String> list = new ArrayList<String> ();
+
     public static void main(String[] args) {
         echo();
     }
@@ -9,13 +11,13 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner in = new Scanner(System.in);
 
-        String s = "";
+        String s = in.nextLine();
 
         while (!s.equals("Bye")) {
-            s = in.nextLine();
             System.out.println("---------");
             System.out.println(s);
             System.out.println("---------");
+            s = in.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon!");
