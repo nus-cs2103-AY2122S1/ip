025698966efa188ebf28f931d90e0d7aed1729eb
HongshuW <hongshu.wang.uni@gmail.com>
2021-08-17 08:59:50 +0800

    Add Processor.java

---
+++ src/main/java/Processor.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Processor {
+    private Scanner input;
+
+    /**
+     * Constructor of the class `Processor`.
+     */
+    public Processor() {
+        this.input = new Scanner(System.in);
+    }
+}
