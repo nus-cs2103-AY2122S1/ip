cb48c2d1c481bf51504b5f4abf8b87a7bd54a860
mweeruien <mweeruien@gmail.com>
2021-08-17 23:04:30 +0800

    Added the name (Notaro) and changed the introduction

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
+        // This string was the initial string
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+         */
+
+        // The chat bot name is Notaro bc it's Not-a-ro-bot :>
+        // This is the introduction of the chat bot
+        System.out.println("Hi! I'm Taro, short for Notaro because I'm Not-a-ro-bot!!");
+        System.out.println("What can I do for you today?");
     }
 }
