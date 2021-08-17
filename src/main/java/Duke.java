d60cea3c3417688caa7f4ddb3a04968f70821c83
marcuschj <haojie.chua@gmail.com>
2021-08-17 20:32:06 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    private static ArrayList<String> list = new ArrayList<>();
+
+    private static Scanner sc;
+
     public static void main(String[] args) {
         String logo
                 = " _____       _                    _        \n"
@@ -10,22 +16,39 @@ public class Duke {
                 + "|_|   |_|   |_|\\____ |_|  |_|______|       \n";
 
         System.out.println("Hi there! Start chatting with your new \n" + logo);
-        System.out.println("What's up today?");
+        System.out.println("What would you like to do today?");
 
-        echo();
+        sc = new Scanner(System.in);  // Create a Scanner object
+
+        handleInput();
     }
 
-    public static void echo() {
-        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+    private static String printList() {
+        String listString = "";
+        for (int i = 0; i < list.size(); i++) {
+            int index = i + 1;
+            listString = listString + index + ". " + list.get(i) + "\n";
+        }
+        return listString;
+    }
+
+    private static void handleInput() {
+
         String message = sc.nextLine();
-        String friendGreeting = "(*^_^*) your virtual friend says: \n";
+        String friendGreeting = "(*^_^*) Friend says: \n";
 
         if (message.equals("bye")) {
             System.out.println(friendGreeting + "See you again my friend!");
+            return;
+        }
+        else if (message.equals("list")) {
+            System.out.println(friendGreeting + "Your to-do list: \n");
+            System.out.println(printList());
         }
         else {
-            System.out.println(friendGreeting + message);
-            echo();
+            list.add(message);
+            System.out.println(friendGreeting + "added: " + message + " to your to-do list!");
         }
+        handleInput();
     }
 }
