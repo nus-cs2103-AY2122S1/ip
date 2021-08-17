bce22ae77282d230346cc0df6995588826b2b011
peilinye <peilinye18@gmail.com>
2021-08-17 20:37:07 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke :) \nWhat can I do for you?");
+        for (int i = 0; i < 60; i++) {
+            System.out.print("_");
+        }
+        System.out.println();
+
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String input = in.nextLine();
+            if (!input.equals("bye")) {
+                System.out.println(input);
+            } else {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
+        in.close();
     }
 }
