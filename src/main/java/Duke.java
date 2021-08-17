87a4a1d6414c7f345e23b637f86e29be10475461
Neha-5678 <>
2021-08-18 00:05:34 +0800

    Increment1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String message = "Hello! I'm Duke \n" + "What can I do for you? \n";
+        System.out.println(message);
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while(!input.equals("bye")) {
+            System.out.println("\t" + input);
+            input = scanner.nextLine();
+        }
+        String ending = "Bye. Hope to see you again soon!";
+        System.out.println("\t" + ending);
     }
 }
