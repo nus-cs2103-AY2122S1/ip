affbfbe62d6fd37b840a337d373e03db436cb649
kaushikkrdy <kaushikkr@hotmail.com>
2021-08-17 22:23:42 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class Duke {
 
-    ArrayList<String> toDoList;
+    protected ArrayList<Task> toDoList;
 
     public Duke() {
-        this.toDoList = new ArrayList<String>(100);
+        this.toDoList = new ArrayList<Task>(100);
     }
 
     public static void main(String[] args) {
@@ -17,13 +17,16 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            String command = scanner.nextLine();
+            String command = scanner.nextLine().trim();
             if (command.equals("bye")) {
                 duke.bye();
                 break;
             } else if (command.equals("list")) {
                 duke.list();
+            } else if (command.startsWith("done")) {
+                duke.done(command.substring(5));
             } else {
+                System.out.println("here");
                 duke.add(command);
             }
         }
@@ -38,19 +41,29 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public void add(String command) {
+    public void add(String description) {
         if (toDoList.size() < 100) {
-            toDoList.add(command);
-            System.out.println(String.format("added: %s", command));
+            toDoList.add(new Task(description));
+            System.out.println(String.format("added: %s", description));
         }
 
     }
 
     public void list() {
+        System.out.println("Here are the tasks in your list:");
         int num = 1;
-        for (String task: toDoList) {
-            System.out.println(String.format("%d. %s", num, task));
+        for (Task task: toDoList) {
+            System.out.println(String.format("%d.%s", num, task));
             num++;
+        }
+    }
+
+    public void done(String num) {
+        int listNum = Integer.parseInt(num);
+        if (listNum <= toDoList.size()) {
+            toDoList.get(listNum - 1).setIsDone(true);
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(toDoList.get(listNum - 1));
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void setIsDone(boolean value) {
+        this.isDone = value;
+    }
+
+    public String getStatusIcon() {
+        return (isDone? "[X]" : "[ ]");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", getStatusIcon(), this.description);
+    }
+}
