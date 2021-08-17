745b5d663dcd18e0d2e5b2d52311d0c0c5b41ed0
kaushikkrdy <kaushikkr@hotmail.com>
2021-08-17 22:05:48 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
+    ArrayList<String> toDoList;
+
+    public Duke() {
+        this.toDoList = new ArrayList<String>(100);
+    }
 
     public static void main(String[] args) {
 
@@ -11,12 +17,14 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            String command = scanner.next();
+            String command = scanner.nextLine();
             if (command.equals("bye")) {
                 duke.bye();
                 break;
+            } else if (command.equals("list")) {
+                duke.list();
             } else {
-                duke.echo(command);
+                duke.add(command);
             }
         }
 
@@ -30,7 +38,19 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public void echo(String command) {
-        System.out.println(command);
+    public void add(String command) {
+        if (toDoList.size() < 100) {
+            toDoList.add(command);
+            System.out.println(String.format("added: %s", command));
+        }
+
+    }
+
+    public void list() {
+        int num = 1;
+        for (String task: toDoList) {
+            System.out.println(String.format("%d. %s", num, task));
+            num++;
+        }
     }
 }
