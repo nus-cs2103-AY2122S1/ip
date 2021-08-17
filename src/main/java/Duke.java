04a5aaac02db1c6209f2ae977db12c4f53a64f1b
aikenwx <59946240+aikenwx@users.noreply.github.com>
2021-08-17 13:47:43 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Good Day Sir/Mdm, I am Duke\nWhat can I do for you?\n");
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String s = sc.nextLine();
+
+            if (s.equals("bye")) {
+                System.out.println("Goodbye Sir/Mdm. Hope to serve you again soon!\n");
+                break;
+            }
+
+            System.out.println(s + "\n");
+
+        }
+
     }
+
 }
+
+
+
