91946285f43a9236cdf16c77f4e08741e1505cce
BenjaminTan99 <benjamin.tan.chin.han@gmail.com>
2021-08-17 20:29:20 +0800

    Added Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -6,9 +7,19 @@ public class Duke {
 
         Scanner newScan = new Scanner(System.in);
         String userInput = newScan.nextLine();
+        ArrayList<String> contents = new ArrayList<>();
 
         while (!userInput.equals("bye")) {
-            System.out.println("    ***\n" + "     " + userInput + "\n    ***");
+            if (userInput.equals("list")) {
+                int counter = 1;
+                for (String x:contents) {
+                    System.out.println(counter + ". " + contents.get(counter - 1) + "\n");
+                    counter++;
+                }
+            } else {
+                System.out.println("    ***\n" + "     " + userInput + "\n    ***");
+                contents.add(userInput);
+            }
             newScan = new Scanner(System.in);
             userInput = newScan.nextLine();
         }
