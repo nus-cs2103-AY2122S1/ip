7f4b9a7e627f502103e916e505373b3e92b0bbe7
LAPTOP-SJ8328VV\Ethan <atomiccore9@rocketmail.com>
2021-08-17 12:46:02 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +9,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        while(true){
+            Scanner sc = new Scanner(System.in);
+            String command = sc.nextLine();
+            if(command.equals("bye")){
+                System.out.println("Bye have a good time\n");
+                break;
+            }else {
+                System.out.println(command + "\n");
+            }
+        }
     }
+
+
 }
