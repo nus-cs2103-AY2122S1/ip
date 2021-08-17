802682c30b8f22943519a21903417d6090bcd4a2
Wilfredwongkc <Wilfredwongkc@gmail.com>
2021-08-17 21:01:45 +0800

    Added Greeting, echo and exit feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String s = "";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+
+        System.out.println(logo);
+        System.out.println("___________________________________________");
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("___________________________________________");
+
+        while(true) {
+            Scanner input = new Scanner(System.in);
+            s = input.nextLine();
+
+            if (s.equals("bye")) {
+                System.out.println("___________________________________________");
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println("___________________________________________");
+                break;
+            }
+
+            System.out.println("___________________________________________");
+            System.out.println("\t" + s);
+            System.out.println("___________________________________________");
+        }
     }
 }
