0959a1ca130d05e754c0c3a40c02651de7dd90e6
Tanishq4331 <tanishq43311@gmail.com>
2021-08-17 12:50:48 +0800

    Fix level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,24 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        Scanner scanner = new Scanner(System.in);
-        String toRepeat = scanner.next();
 
-        while (!toRepeat.equals("bye")) {
-            System.out.println("\t" + toRepeat + "\n");
-            toRepeat = scanner.next();
+        public static void main(String[] args) {
+            String logo = " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+            System.out.println("Hello from\n" + logo);
+            Scanner scanner = new Scanner(System.in);
+            String toRepeat = scanner.nextLine();
+
+            while (!toRepeat.equals("bye")) {
+                System.out.println("\t" + toRepeat + "\n");
+                toRepeat = scanner.nextLine();
+            }
+
+            scanner.close();
         }
 
-        scanner.close();
-    }
 }
