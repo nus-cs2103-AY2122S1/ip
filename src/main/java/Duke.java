53f6c77d816c49a016f84d7ffdd505afe7de89ec
Timothyoung97 <timothyoung97@gmail.com>
2021-08-17 18:27:19 +0800

    Complete Level 4
    * Make class Task to become abstract class
    * Add ToDo, Event, Deadline class. All of them are inherited from Task.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -29,11 +29,13 @@ public class Duke {
                     "\t  .--.      .-'.      .--.      .--.      .--.      .--.      .`-.      .--.\n" +
                     "\t:::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\\n" +
                     "\t'      `--'      `.-'      `--'      `--'      `--'      `-.'      `--'      `";
-    private final String GREETING = "Hallo! My name's Peter!\n\tHow may I be of service to you?";
-    private final String GOODBYE = "Good day! I'm gonna find Lois if you're not using me!";
-    private final String TASK_REPORT = "Here are all your tasks! No Procrastination!";
-    private final String TASK_COMPLETE = "Task Completed: ";
-    private final String CHEERING = "Great job! You're the best! Keep up the good work! Oho! Oho! Ohooooooo!";
+    private final String MESSAGE_GREETING = "Hallo! My name's Peter!\n\tHow may I be of service to you?";
+    private final String MESSAGE_GOODBYE = "Good day! I'm gonna find Lois if you're not using me!";
+    private final String MESSAGE_TASK_REPORT = "Here are all your tasks! No Procrastination!";
+    private final String MESSAGE_TASK_COMPLETE = "Task Completed: ";
+    private final String MESSAGE_CHEERING = "Great job! You're the best! Keep up the good work! Oho! Oho! Ohooooooo!";
+    private final String MESSAGE_ADD_TASK_NOTICE = "GOSH! You have one more task.";
+    private final String MESSAGE_ADD_TASK_SUMMARY = "Now you have %s task in the list";
     private Task[] taskTracker = new Task[100];
     private int taskNo = 0;
 
@@ -52,13 +54,39 @@ public class Duke {
     }
 
     private void addTask(String input){
-        taskTracker[taskNo++] = new Task(input);
-        response("Task Added: " + input);
+        //taskTracker[taskNo++] = new Task(input);
+        String[] inputToken = tokenize(input);
+        String taskType = inputToken[0];
+
+        if (taskType.equals("todo")){
+            taskTracker[taskNo++] = new ToDo(inputToken[1]);
+        } else if (taskType.equals("deadline")){
+            String[] deadlineTask = inputToken[1].split(" /by ", 2);
+            taskTracker[taskNo++] = new Deadline(deadlineTask[0].trim(), deadlineTask[1].trim());
+        } else if (taskType.equals("event")){
+            String[] eventTask = inputToken[1].split(" /at ", 2);
+            taskTracker[taskNo++] = new Event(eventTask[0].trim(), eventTask[1].trim());
+        } else{
+            response("Wrong input");
+        }
+        addTaskReport();
     }
 
-    private String formatTask() {
+    private void addTaskReport(){
+        String output = String.format("%s\n\t\t%s\n\t", MESSAGE_ADD_TASK_NOTICE, taskTracker[taskNo-1].toString())
+                + String.format(MESSAGE_ADD_TASK_SUMMARY, taskNo);
+        response(output);
+    }
+
+    private String[] tokenize(String input){
+        String[] token = input.split(" ", 2);
+        return token;
+    }
+
+    private String printAllTask() {
         StringBuilder formattedTask = new StringBuilder();
-        formattedTask.append(TASK_REPORT + "\n\t");
+        formattedTask.append(MESSAGE_TASK_REPORT + "\n\t");
+        System.out.println("clear");
         for (int i = 0; i < taskTracker.length; i++) {
             if (taskTracker[i] != null) {
                 String eachLine = (i + 1) + ". " + taskTracker[i].toString() + "\n\t";
@@ -73,19 +101,19 @@ public class Duke {
         Integer index = Integer.parseInt(input.split(" ", 2)[1]);
         Task completedTask = taskTracker[index - 1];
         completedTask.maskAsDone();
-        String output = TASK_COMPLETE + (index) + ". " + completedTask.taskTitle + "\n\t" + CHEERING;
+        String output = MESSAGE_TASK_COMPLETE + (index) + ". " + completedTask.taskTitle + "\n\t" + MESSAGE_CHEERING;
         response(output);
     }
 
-    private void run() {
+    private void interact() {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String input = sc.nextLine().trim();
             if (input.equals("bye")) {
-                response(GOODBYE);
+                response(MESSAGE_GOODBYE);
                 return;
             }else if (input.equals("list")){
-                response(formatTask());
+                response(printAllTask());
             }else if (input.startsWith("done")) {
                 completeTask(input);
             }else {
@@ -97,8 +125,8 @@ public class Duke {
 
     void initiate() {
         System.out.println("\t" + LOGO.replaceAll("\n", "\n\t"));
-        response(GREETING);
-        this.run();
+        response(MESSAGE_GREETING);
+        this.interact();
     }
 
     public static void main(String[] args) {
