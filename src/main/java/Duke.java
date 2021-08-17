649e622a200870edb733c4948f8e3bbd37436fa2
Zhang Ziqing <zhangziqing9926@gmail.com>
2021-08-18 01:33:22 +0800

    Test using Automated Text UI Testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class Duke {
     private static String name = "Catobot";
-    private static String banner = "(=^･ω･^=)(=^･ω･^=)(=^･ω･^=)(=^･ω･^=)";
-    private static String greeting = "Hello I am " + name + " (>^ω^<)\n    What can I do for you meow?";
-    private static String byeMessage = "Bye meow! I will always wait here meow(>^ω^<)";
+    private static String banner = "(=^^=)(=^^=)(=^^=)(=^^=)";
+    private static String greeting = "Hello I am " + name + " (>^^<)\n    What can I do for you meow?";
+    private static String byeMessage = "Bye meow! I will always wait here meow(>^^<)";
 
     public static void main(String[] args) {
         // creates an object of Scanner
