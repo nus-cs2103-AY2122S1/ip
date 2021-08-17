2a3821b74db7fd22d80ed1eec12d3d318eaf93df
jaredlhf <jlhfqaz@gmail.com>
2021-08-17 13:07:20 +0800

    implemented Todo class

--- src/main/java/Jared.java
+++ src/main/java/Jared.java
@@ -4,18 +4,26 @@ public class Jared {
     private static Task[] history = new Task[100];
     private static int historyCount = 0;
 
-    private static void add(String input) {
-        Task newTask = new Task(input);
-        System.out.println("added: " + newTask.toString());
+    private static void add(String command, String desc) {
+        Task newTask;
+        if (command.equals("todo")) {
+            newTask = new Todo(desc);
+        } else {
+            newTask = new Task(desc);
+        }
         history[historyCount] = newTask;
         historyCount ++;
+        System.out.println(String.format("Got it. I've added this task:\n" +
+                        "%s\nNow you have %d tasks in the list.",
+                newTask.toString(), historyCount)
+        );
     }
 
     private static void list() {
         String res = "Here are the tasks in your list:\n";
         for (int i = 0; i < historyCount; i++) {
             Task currTask = history[i];
-            res += String.format("%d.[%s] %s\n", i + 1, currTask.getStatusIcon(), currTask.toString());
+            res += String.format("%d. %s\n", i + 1, currTask.toString());
         }
         System.out.println(res);
     }
@@ -24,8 +32,8 @@ public class Jared {
         int taskNum = index - 1;
         Task currTask = history[taskNum];
         currTask.markDone();
-        String res = String.format("Nice! I've marked this task as done:\n[%s] %s",
-                currTask.getStatusIcon(), currTask.toString());
+        String res = String.format("Nice! I've marked this task as done:\n%s",
+                currTask.toString());
         System.out.println(res);
     }
 
@@ -39,16 +47,21 @@ public class Jared {
         System.out.println(intro);
         while (true) {
             String next = scan.nextLine();
-            String command = next.split(" ")[0];
-            if (next.equals("bye")) {
+            String[] inputArr = next.split(" ",2);
+            String command = inputArr[0];
+            String desc = "0";
+            if (inputArr.length > 1) {
+                desc = next.split(" ", 2)[1];
+            }
+
+            if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-            } else if (next.equals("list")) {
+            } else if (command.equals("list")) {
                 list();
             } else if (command.equals("done")) {
-                int index = Integer.valueOf(next.split(" ")[1]);
-                done(index);
+                done(Integer.valueOf(desc));
             } else {
-                add(next);
+                add(command, desc);
             }
         }
 
