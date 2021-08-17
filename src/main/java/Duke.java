fdd756144fc4c56e9c283e07b701678511e207ea
GWphua <phuaguanwei99@gmail.com>
2021-08-18 02:10:39 +0800

    Added Greet, Echo, and Exit to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
+
+    private static String greet() {
+        return "Hello from\n"
+                + " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n"
+                + "What can I do for you?";
+    }
+
+    private static String echo(String wordToEcho) {
+        return wordToEcho;
+    }
+
+    private static String sayGoodbye() {
+        return "Bye. Hope to see you again soon!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet());
+
+        for(String str : args) {
+            System.out.println(str);
+            if (str.equals("bye")) {
+                System.out.println(sayGoodbye());
+                System.exit(0);
+
+            } else {
+                System.out.println(echo(str));
+            }
+        }
     }
 }
