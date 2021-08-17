849df1becdf449205eccdf57ccd9a89ad7b4c58e
Faruq <faruqy1999@gmail.com>
2021-08-17 19:24:47 +0800

    A-textUiTestingA-textUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -65,6 +65,8 @@ public class Duke {
         }
     }
 
+    private Scanner scanner = new Scanner(System.in);
+
     public void insertNewTask(Task t) {
         listArr[listArrCount] = t;
         listArrCount++;
@@ -77,81 +79,80 @@ public class Duke {
     }
 
     private void addList() {
-        Scanner scanner = new Scanner(System.in);
-        String listItem = scanner.nextLine();
-
-        if(listItem.equals("bye")) {
-            textFrame("Bye bye from the List Adder!");
-        }  else {
-            if (listItem.equals("list")) {
-                String listString = "";
-                if (listArr[0] == null) {
-                    listString = "\n Your list is empty!";
-                } else {
-                    for (int i = 0; i < 10; i++) {
-                        if (listArr[i] != null) {
-                            int count = i + 1;
-                            listString += "\n " + count + ". " + listArr[i];
+        while(scanner.hasNext()){
+            String listItem = scanner.nextLine();
+            if(listItem.equals("bye")) {
+                textFrame("Bye bye from the List Adder!");
+            }  else {
+                if (listItem.equals("list")) {
+                    String listString = "";
+                    if (listArr[0] == null) {
+                        listString = "\n Your list is empty!";
+                    } else {
+                        for (int i = 0; i < 10; i++) {
+                            if (listArr[i] != null) {
+                                int count = i + 1;
+                                listString += "\n " + count + ". " + listArr[i];
+                            }
                         }
                     }
-                }
-                textFrame(" This be ye list of things to do Sire: " + listString );
-            } else if (listItem.length() > 5 && listItem.substring(0, 5).equals("done ")) {
-                String number = listItem.substring(5, listItem.length());
-                try {
-                    int i = Integer.parseInt(number) - 1;
-                    if (i >= listArrCount) {
-                        System.out.println(" You don't have that many tasks! \n");
-                    } else {
-                        listArr[i].setDone();
-                        textFrame("Good job, I have marked the task as done!" + "\n" + listArr[i].toString());
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println("That is not a valid index!\n");
+                    textFrame(" This be ye list of things to do Sire:" + listString );
+                } else if (listItem.length() > 5 && listItem.substring(0, 5).equals("done ")) {
+                    String number = listItem.substring(5, listItem.length());
+                    try {
+                        int i = Integer.parseInt(number) - 1;
+                        if (i >= listArrCount || i <= -1) {
+                            System.out.println(" That task does not exist!\n");
+                        } else {
+                            listArr[i].setDone();
+                            textFrame("Good job, I have marked the task as done!" + "\n" + listArr[i].toString());
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("That is not a valid index!\n");
 
-                }
-            } else if (listItem.length() > 5 && listItem.substring(0, 5).equals("todo ")) {
-                Task t = new ToDo(listItem.substring(5, listItem.length()));
-                insertNewTask(t);
-                textFrame("Got it, I've added this task: \n" + t);
-            } else if (listItem.length() > 9 && listItem.substring(0,9).equals("deadline ")){
-                int index = listItem.indexOf(" /by ");
-                if(index != -1) {
-                    String taskName = listItem.substring(9, index);
-                    String by =  listItem.substring(index + 5, listItem.length());
-                    Task t = new Deadline(taskName, by);
+                    }
+                } else if (listItem.length() > 5 && listItem.substring(0, 5).equals("todo ")) {
+                    Task t = new ToDo(listItem.substring(5, listItem.length()));
                     insertNewTask(t);
-                    textFrame("Got it, I've added this task: \n" + t);
+                    textFrame("Got it, I've added this task:\n" + t);
+                } else if (listItem.length() > 9 && listItem.substring(0,9).equals("deadline ")){
+                    int index = listItem.indexOf(" /by ");
+                    if(index != -1) {
+                        String taskName = listItem.substring(9, index);
+                        String by =  listItem.substring(index + 5, listItem.length());
+                        Task t = new Deadline(taskName, by);
+                        insertNewTask(t);
+                        textFrame("Got it, I've added this task:\n" + t);
+                    } else {
+                        textFrame("Woah that is some wonky Deadline you got there. Try harder!");
+                    }
+                } else if (listItem.length() > 6 && listItem.substring(0,6).equals("event ")) {
+                    int index = listItem.indexOf(" /at ");
+                    if(index != -1) {
+                        String taskName = listItem.substring(6, index);
+                        String at =  listItem.substring(index + 5, listItem.length());
+                        Task t = new Event(taskName, at);
+                        insertNewTask(t);
+                        textFrame("Got it, I've added this task:\n" + t);
+                    } else {
+                        textFrame("Woah that is some wonky Event you got there. Try harder!");
+                    }
                 } else {
-                    textFrame("Woah that is some wonky Deadline you got there. Try harder!");
+                    textFrame("I am not too sure what that task is... please specify!!");
                 }
-            } else if (listItem.length() > 6 && listItem.substring(0,6).equals("event ")) {
-                int index = listItem.indexOf(" /at ");
-                if(index != -1) {
-                    String taskName = listItem.substring(9, index);
-                    String at =  listItem.substring(index + 5, listItem.length());
-                    Task t = new Event(taskName, at);
-                    insertNewTask(t);
-                    textFrame("Got it, I've added this task: \n" + t);
-                } else {
-                    textFrame("Woah that is some wonky Event you got there. Try harder!");
-                }
-            } else {
-                textFrame("I am not too sure what that task is... please specify!!");
             }
-            addList();
         }
     }
 
     public static void main(String[] args) {
-        String logo = "                     ,,       \n" +
-                "`7MM\"\"\"Yp,          *MM       \n" +
-                "  MM    Yb           MM       \n" +
-                "  MM    dP  ,pW\"Wq.  MM,dMMb. \n" +
+        String logo = "                     ,,\n" +
+                "`7MM\"\"\"Yp,          *MM\n" +
+                "  MM    Yb           MM\n" +
+                "  MM    dP  ,pW\"Wq.  MM,dMMb.\n" +
                 "  MM\"\"\"bg. 6W'   `Wb MM    `Mb\n" +
                 "  MM    `Y 8M     M8 MM     M8\n" +
                 "  MM    ,9 YA.   ,A9 MM.   ,M9\n" +
-                ".JMMmmmd9   `Ybmd9'  P^YbmdP' "
+                ".JMMmmmd9   `Ybmd9'  P^YbmdP'"
                 + "\n\n"
                 + "      .--..--..--..--..--..--.\n"
                 + "    .' \\  (`._   (_)     _   \\\n"
