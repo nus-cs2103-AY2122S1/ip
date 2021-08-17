b01be2c98518900b89d09becee146cac4a23b73d
lwj1711 <limweijie1711@yahoo.com.sg>
2021-08-17 22:13:19 +0800

    Add assistant functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static final String LINE_HORIZONTAL = "_______________________________";
+
+    /***
+     * Initializes the bot.
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        // Prints initial message as prompt.
+        System.out.printf("%s\nGreetings. This is Elsa.\nWhat can I do for you?\n%s\n", LINE_HORIZONTAL, LINE_HORIZONTAL);
+
+        // Initializes scanner to take input from user.
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        // Takes in input and performs actions accordingly.
+        while(!(input = scanner.nextLine()).equalsIgnoreCase("bye")) {
+            System.out.printf("%s\n%s\n%s\n", LINE_HORIZONTAL, input, LINE_HORIZONTAL);
+        }
+
+        // Bids farewell to user upon "bye" input.
+        System.out.printf("%s\nGoodbye. Hope to see you again!\n", LINE_HORIZONTAL);
+
     }
 }
