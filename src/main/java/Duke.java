f577d7e7cdb1605e0ee010f41fc5b1d91950975e
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2021-08-17 14:38:10 +0800

    Add I/O skeleton to Duke.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        monitor();
+    }
+
+    private static void monitor() {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            renderOutput(input);
+            if (input.strip().equalsIgnoreCase("bye")) {
+                break;
+            }
+        }
+    }
+
+    private static void renderOutput(String output) {
+        System.out.println("    ____________________________________________________________");
+        output.lines().map(x -> "     " + x).forEach(System.out::println);
+        System.out.println("    ____________________________________________________________");
     }
 }
