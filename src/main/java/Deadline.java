b05bab118603727882fdbcecb0adaf37d52ea8bd
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-08-17 15:25:42 +0800

    Created Deadline Class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,13 @@
+public class Deadline extends Task {
+    protected String byWhen;
+
+    public Deadline(String description, String byWhen) {
+        super(description);
+        this.byWhen = byWhen;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (" + byWhen + ")";
+    }
+}
