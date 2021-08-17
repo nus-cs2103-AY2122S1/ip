0a3992f461a6160ccb50d1aeffb853979c8733d5
zhuoyang125 <zhuoyang125@gmail.com>
2021-08-17 15:47:33 +0800

    add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        String bearPicStart = "┏ʕ •ᴥ•ʔ┛";
+        String bearPicEnd = " ＼ʕ •ᴥ•ʔ／";
+        System.out.println("Hi, I'm Duke the Bear! \n");
+        System.out.println(bearPicStart + "\n");
+        System.out.println("How can I help you?");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye bye! Thank u Beary Much!\n");
+                System.out.println(bearPicEnd);
+                break;
+            }
+            System.out.println(input);
+        }
     }
 }
