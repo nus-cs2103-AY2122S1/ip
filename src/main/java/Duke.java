13e9333319d361859df55bb94fde9060f4704b5c
juliussneezer04 <vishnu.sundaresan@gmail.com>
2021-08-17 22:52:51 +0800

    Level-1 Completed.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private final static String LOGO =  " ____        _        \n"
+                                        + "|  _ \\ _   _| | _____ \n"
+                                        + "| | | | | | | |/ / _ \\\n"
+                                        + "| |_| | |_| |   <  __/\n"
+                                        + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private final static String TERMINATION_COMMAND = "bye";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + LOGO);
+        System.out.println("What can I do for you?");
+        Scanner inputScanner = new Scanner(System.in);
+        String input = inputScanner.nextLine();
+        while (!(input.equals(TERMINATION_COMMAND))) {
+            System.out.println(input);
+            input = inputScanner.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
