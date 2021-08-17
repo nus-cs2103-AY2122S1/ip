61b067a6e0943a9f33052fac0d8232c3397f5b7e
Eunice <eunicesimsingyee@gmail.com>
2021-08-17 19:42:47 +0800

    Add Javadoc comments to Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,3 +1,7 @@
+/**
+ * Tracks a task with a description and whether it has been completed yet.
+ *
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
@@ -11,11 +15,20 @@ public class Task {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    /**
+     * Sets this task to be done.
+     *
+     */
     public void markAsDone() {
         this.isDone = true;
         System.out.println("Nice! I've marked this task as done:\n  [X] " + description);
     }
 
+    /**
+     * Returns the Task's String.
+     *
+     * @return String form of Task
+     */
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + this.description;
