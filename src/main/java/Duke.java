9c3ab3f35b185734c46344267bf7325fe86efdd1
xyliew25 <e0550381@u.nus.edu>
2021-08-17 12:13:22 +0800

    complete Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,41 +12,71 @@ public class Duke {
     // Create scanner
     Scanner scanner = new Scanner(System.in);
 
-    // Initialize command
+    // Initialize command variables
+    String commandLine = "";
     String command = "";
+    int doneTask = -1;
 
     // Exit command
-    String exitCommand = "bye";
+    final String exitCommand = "bye";
     String exitResponse = "    ____________________________________________________________\n"
         + "     Bye. Hope to see you again soon!\n"
         + "    ____________________________________________________________\n";
 
     // List command
     final String listCommand = "list";
-    String[] list = new String[100];
+    Task[] list = new Task[100];
     int index = 0;
+
+    // Done command
+    final String doneCommand = "done";
 
     // Duke chatbot
     while (!command.equals(exitCommand)) {
       // Initialize response content
       String responseContent = "";
 
-      // List command
-      if (command.equals(listCommand)) {
-        for (int i = 0; i < index; i++) {
-          if (i != index - 1) {
-            responseContent = responseContent + (i + 1) + ". " + list[i] + "\n     ";
-          } else {
-            responseContent = responseContent + (i + 1) + ". " + list[i];
+      // Draft response content based on command
+      switch (command) {
+        case listCommand:
+          responseContent = "Here are the tasks in your list:\n";
+          for (int i = 0; i < index; i++) {
+            if (i != index - 1) {
+              responseContent = responseContent + "     " + (i + 1) + "." + list[i] + "\n";
+            } else {
+              responseContent = responseContent + "     " + (i + 1) + "." + list[i];
+            }
           }
-        }
-      } else if (command.equals("")) {
-        responseContent = "";
-      } else {
-        responseContent = "added: " + command;
-        list[index] = command;
-        index++;
+          break;
+        case doneCommand:
+          list[doneTask].markAsDone();
+          responseContent = "Nice! I've marked this task as done:\n" +
+              "       " + list[doneTask];
+          break;
+        case "":
+          responseContent = "";
+          break;
+        default:
+          list[index] = new Task(command);
+          index++;
+          responseContent = "added: " + command;
+          break;
       }
+//      if (command.equals(listCommand)) {
+//        for (int i = 0; i < index; i++) {
+//          if (i != index - 1) {
+//            responseContent = responseContent + (i + 1) + "." + list[i] + "\n     ";
+//          } else {
+//            responseContent = responseContent + (i + 1) + "." + list[i];
+//          }
+//        }
+//      } else if (command.equals("")) {
+//        responseContent = "";
+//      } else {
+//        responseContent = "added: " + command;
+//        list[index] = new Task(command);
+//        index++;
+//      }
 
       // Response
       String response = "    ____________________________________________________________\n"
@@ -57,7 +87,13 @@ public class Duke {
       }
 
       // Get command
-      command = scanner.nextLine();
+      commandLine = scanner.nextLine();
+      command = commandLine.split(" ")[0];
+      if (command.equals(doneCommand)) {
+        doneTask = Integer.parseInt(commandLine.split(" ")[1]) - 1;
+      } else {
+        command = commandLine;
+      }
     }
 
     // Exit
