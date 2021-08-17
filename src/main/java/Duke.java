d074738513c66424ccdd98463d4c9f121b3e13a9
DESKTOP-PILGCOV\yzlim <yzlim23@gmail.com>
2021-08-17 18:18:49 +0800

    Add greetings, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String command;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        while(true) {
+            System.out.print(">> ");
+            command = in.next();
+            if (command.equals("bye")) break;
+            else System.out.println(command);
+        }
+        System.out.println("Bye, hope to see you again!");
     }
 }
