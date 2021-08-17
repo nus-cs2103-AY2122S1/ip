68f427a246aeeb8ba025a1d615969298c59bbb1f
Jason <jason.ng.zq99@gmail.com>
2021-08-17 23:47:58 +0800

    Edit text-ui-test folder Updated ACTUAL.TXT and EXPECTED.TXT to match current update to Duke app

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -23,7 +23,7 @@ public class Printer {
     }
 
     public void PrintList(ArrayList<Task> message) {
-        System.out.println("Here are the tasks in your list: ");
+        System.out.println("Here are the tasks in your list:");
         for (int i = 1; i <= message.size(); i++) {
             Task thisTask = message.get(i-1);
             String toPrint = String.format("%d. %s", i, thisTask);
