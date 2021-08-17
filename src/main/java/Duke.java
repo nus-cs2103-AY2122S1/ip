72e235b3061e02672c035112df88e173fa152780
aikenwx <59946240+aikenwx@users.noreply.github.com>
2021-08-17 23:06:53 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Duke {
 
-    private String[] tasks = new String[100];
+    private Task[] tasks = new Task[100];
 
     private int count = 0;
 
@@ -12,21 +13,50 @@ public class Duke {
         while (true) {
             Scanner sc = new Scanner(System.in);
             String input = sc.nextLine();
-
             if (input.equals("bye")) {
                 System.out.println("Goodbye Sir/Mdm. Hope to serve you again soon!\n");
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Here are your tasks Sir/Mdm:");
                 this.list();
-            } else {
-                this.add(input);
-                System.out.println("added: " + input + "\n");
-            }
+            } else if (input.split(" ")[0].equals("done")) {
 
+                String[] parsedInput = input.split(" ");
+
+                if (parsedInput.length != 2) {
+                    System.out.println("Please specify a task you would like marked as done Sir/Mdm:");
+                    this.list();
+                    continue;
+                }
+
+                int taskToMark;
+
+                try {
+                    taskToMark = Integer.parseInt(parsedInput[1]) - 1;
+                } catch (NumberFormatException e) {
+                    System.out.println("Please enter a proper number within this range Sir/Mdm:");
+                    this.list();
+                    continue;
+                }
+
+                if (taskToMark < 0 || taskToMark > count - 1) {
+                    System.out.println("Please specify a task within this range Sir/Mdm:");
+                    list();
+                    continue;
+                }
+
+                tasks[taskToMark].markAsDone();
+                System.out.println("Good job Sir/Mdm! I shall mark this task as complete:\n" +
+                        tasks[taskToMark] + "\n");
+            } else {
+                Task newTask = new Task(input);
+                this.add(newTask);
+                System.out.println("added: " + newTask + "\n");
+            }
         }
     }
 
-    private void add(String task) {
+    private void add(Task task) {
         tasks[this.count] = task;
         this.count++;
     }
