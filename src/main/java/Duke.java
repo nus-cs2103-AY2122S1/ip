a8a85f1d989c31522105b99197b5e57c03ee9b0e
juliussneezer04 <vishnu.sundaresan@gmail.com>
2021-08-18 00:56:57 +0800

    Level-3 Completed.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,41 @@ import java.util.Scanner;
 
 public class Duke {
 
+    private static class Entry {
+
+        private String entry;
+
+        private boolean isDone;
+
+        Entry(String value) {
+            this.entry = value;
+            this.isDone = false;
+        }
+
+        public void revertEntry() {
+            this.isDone = !this.isDone;
+        }
+
+        public boolean markEntryAsDone() {
+            if (this.isDone) {
+                return false;
+            } else {
+                this.isDone = true;
+                return this.isDone;
+            }
+        }
+
+        public String getEntryOnly() {
+            return this.entry;
+        }
+
+        @Override
+        public String toString () {
+            char isDoneDisplay = this.isDone ? 'X' : ' ';
+            return ("[" + isDoneDisplay + "] " + entry);
+        }
+    }
+
     private final static String LOGO =  " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
