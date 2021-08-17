8fd6bca0bb1bb4ca103d548f835530c7114959f5
Chow Rui Yan <chowruiyan@hotmail.co.uk>
2021-08-17 16:23:18 +0800

    Add Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Duke {
@@ -21,8 +22,14 @@ public class Duke {
     }
 
     private static class Todo extends Task{
-        public Todo(String task) {
-            super.task = task;
+        public Todo(String input) throws DukeIllegalInputException {
+            String[] inputs = input.split(" ");
+            if (inputs.length == 1) {
+                throw new DukeIllegalInputException("Insufficient input received! Please add in description of the task.");
+            }
+            int tFirst = input.indexOf(" ");
+            String tTask = input.substring(tFirst + 1);
+            super.task = tTask;
         }
 
         @Override
@@ -34,8 +41,19 @@ public class Duke {
     private static class Deadline extends Task {
         private String by;
 
-        public Deadline(String task, String by) {
-            super.task = task;
+        public Deadline(String input) throws DukeIllegalInputException {
+            if (!input.contains("/by")) {
+                throw new DukeIllegalInputException("Invalid input! Please add in the deadline for the task.");
+            }
+            String[] inputs = input.split(" ");
+            if (inputs.length == 1) {
+                throw new DukeIllegalInputException("Insufficient input received! Please add in description of the task.");
+            }
+            int dFirst = input.indexOf(" ");
+            int dSecond = input.indexOf("/");
+            String dTask = input.substring(dFirst + 1, dSecond - 1);
+            String by = input.substring(dSecond + 4);
+            super.task = dTask;
             this.by = by;
         }
 
@@ -48,14 +66,37 @@ public class Duke {
     private static class Event extends Task {
         private String at;
 
-        public Event(String task, String at) {
-            super.task = task;
+        public Event(String input) throws DukeIllegalInputException {
+            if (!input.contains("/at")) {
+                throw new DukeIllegalInputException("Invalid input! Please add in information about the event.");
+            }
+            String[] inputs = input.split(" ");
+            if (inputs.length == 1) {
+                throw new DukeIllegalInputException("Insufficient input received! Please add in description of the task.");
+            }
+            int eFirst = input.indexOf(" ");
+            int eSecond = input.indexOf("/");
+            String eTask = input.substring(eFirst + 1, eSecond - 1);
+            String at = input.substring(eSecond + 4);
+            super.task = eTask;
             this.at = at;
         }
 
         @Override
         public String toString() {
             return "[E]" + super.toString() + " (at: " + at + ")";
+        }
+    }
+
+    private static class DukeException extends Exception {
+        public DukeException(String message) {
+            super(message);
+        }
+    }
+
+    private static class DukeIllegalInputException extends DukeException {
+        public DukeIllegalInputException(String message) {
+            super(message);
         }
     }
 
@@ -83,11 +124,29 @@ public class Duke {
             return this.open;
         }
 
-        public void add(Task task) {
-            list[index] = task;
-            index++;
-            System.out.println("Got it. I've added this task:\n" + "  " + task + "\n" +
-                    "Now you have " + index + " tasks in the list.");
+        public void add(String input, int i) {
+            try {
+                Task task;
+                switch (i) {
+                    case 1:
+                        task = new Todo(input);
+                        break;
+                    case 2:
+                        task = new Deadline(input);
+                        break;
+                    case 3:
+                        task = new Event(input);
+                        break;
+                    default:
+                        throw new DukeIllegalInputException("Not a valid Task!!");
+                }
+                list[index] = task;
+                index++;
+                System.out.println("Got it. I've added this task:\n" + "  " + task + "\n" +
+                        "Now you have " + index + " tasks in the list.");
+            } catch (DukeIllegalInputException e) {
+                System.out.println(e.getMessage());
+            }
         }
 
         public void showList() {
@@ -98,9 +157,22 @@ public class Duke {
             }
         }
 
-        public void markAsDone(int index) {
-            list[index - 1].markAsDone();
-            System.out.println("Nice! I've marked this task as done:\n  " + list[index - 1]);
+        public void markAsDone(String input) throws DukeIllegalInputException {
+            String[] inputs = input.split(" ");
+            if (inputs.length == 1) {
+                throw new DukeIllegalInputException("Insufficient input received! Please indicate the task number of the completed task.");
+            }
+            try {
+                int index = Integer.parseInt(inputs[1]);
+                if (list[index - 1] == null) {
+                    throw new DukeIllegalInputException("Invalid task number! The given task number:" + index +
+                            " does not exist, please enter a valid task number.");
+                }
+                list[index - 1].markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n  " + list[index - 1]);
+            } catch (NumberFormatException e) {
+                throw new DukeIllegalInputException("Invalid input! Please enter the task number after 'done'.");
+            }
         }
     }
     public static void main(String[] args) {
@@ -108,10 +180,10 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         tdl.open();
         while (tdl.isOpen()) {
-            String input = sc.nextLine();
+            String input = sc.nextLine().strip();
             String[] inputs = input.split(" ");
             String action = inputs[0];
-            switch (action) {
+            switch (action.toLowerCase()) {
                 case "bye":
                     tdl.exit();
                     break;
@@ -119,33 +191,23 @@ public class Duke {
                     tdl.showList();
                     break;
                 case "done":
-                    int index = Integer.parseInt(inputs[1]);
-                    tdl.markAsDone(index);
+                    try {
+                        tdl.markAsDone(input);
+                    } catch (DukeIllegalInputException e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case "todo":
-                    int tFirst = input.indexOf(" ");
-                    String tTask = input.substring(tFirst + 1);
-                    Todo t = new Todo(tTask);
-                    tdl.add(t);
+                    tdl.add(input, 1);
                     break;
                 case "deadline":
-                    int dFirst = input.indexOf(" ");
-                    int dSecond = input.indexOf("/");
-                    String dTask = input.substring(dFirst + 1, dSecond - 1);
-                    String by = input.substring(dSecond + 4);
-                    Deadline d = new Deadline(dTask, by);
-                    tdl.add(d);
+                    tdl.add(input, 2);
                     break;
                 case "event":
-                    int eFirst = input.indexOf(" ");
-                    int eSecond = input.indexOf("/");
-                    String eTask = input.substring(eFirst + 1, eSecond - 1);
-                    String at = input.substring(eSecond + 4);
-                    Event e = new Event(eTask, at);
-                    tdl.add(e);
+                    tdl.add(input, 3);
                     break;
                 default:
-                    System.out.println("Sorry, I do not understand what was written\nPlease try again.");
+                    System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                     break;
             }
         }
