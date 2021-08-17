7ebd6469be1785f48ccd1bf30d074718d2c6c9af
Koh Jia Xian <39460640+koh-jx@users.noreply.github.com>
2021-08-17 12:38:58 +0800

    IndexOutOfBounds exception

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -29,11 +29,17 @@ public class TaskList {
     }
 
     public void toggleDone(int index) {
-        boolean result = tasks.get(index - 1).toggleDone();
-        System.out.println(result
-                ? Display.OUTPUT_DISPLAY + "sugoi! Duke-san marked this task as done!"
-                : Display.OUTPUT_DISPLAY + "Duke-san marked this task as not done!");
-        System.out.println(Display.OUTPUT_SPACES + tasks.get(index - 1));
+        try {
+            boolean result = tasks.get(index - 1).toggleDone();
+            System.out.println(result
+                    ? Display.OUTPUT_DISPLAY + "sugoi! Duke-san marked this task as done!"
+                    : Display.OUTPUT_DISPLAY + "Duke-san marked this task as not done!");
+            System.out.println(Display.OUTPUT_SPACES + tasks.get(index - 1));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("There's no task at index " + (index - 1) + "!!");
+        }
+
+
     }
 
     public void displayList() {
