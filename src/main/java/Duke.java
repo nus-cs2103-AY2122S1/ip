e765315b4895f61e09bd1bd2ac707b462b040c4b
Jovyn Tan <jovyn.tls@gmail.com>
2021-08-17 15:06:27 +0800

    Add greeting to the user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,9 @@
+/**
+ * A personal assistant chatbot that helps a person to keep track of various things.
+ *
+ * @author Jovyn Tan
+ * @version CS2103 AY21/22 Sem 1
+ */
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +12,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+    }
+
+    /**
+     * Prints a greeting to the user.
+     */
+    public static void greet() {
+        System.out.println("---------------------------------------");
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println("---------------------------------------");
     }
 }
