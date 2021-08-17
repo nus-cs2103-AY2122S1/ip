871fa32bb58a6765a9da5d30b727ef080c9a9203
cheweejia <eejiachew@gmail.com>
2021-08-17 17:40:00 +0800

    Edited Duke.java to handle Task objects

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    private final static ArrayList<String> list = new ArrayList<>();
-    private final static String bar = "________________________________";
+    private final static ArrayList<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -12,49 +11,61 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println(reply("Hello i is Duke\nWhat u want?"));
-        add();
+        reply("Hello i is Duke\nWhat u want?");
+        manageInput();
     }
 
-    private static String reply(String content) {
-        return bar + "\n" + content + "\n" + bar;
-    }
-
-    private static void exit() {
-        System.out.println(reply("i zao first"));
-    }
-
-    private static void echo() {
+    private static void manageInput() {
         Scanner obj = new Scanner(System.in);
         String input = obj.nextLine();
         if (input.equals("bye")) {
             exit();
+        } else if (input.equals("list")) {
+            displayList();
+        } else if (input.contains("done")) {
+            done(input);
         } else {
-            System.out.println(reply(input));
-            echo();
+            add(input);
         }
     }
 
-    private static void add() {
-        Scanner obj = new Scanner(System.in);
-        String input = obj.nextLine();
-        if (input.equals("list")) {
-            displayList();
-        } else if (input.equals("bye")) {
-            exit();
-        } else {
-            list.add(input);
-            System.out.println(reply("added: " + input));
-            add();
-        }
+    private static void reply(String content) {
+        System.out.println("________________________________");
+        System.out.println(content);
+        System.out.println("________________________________");
+    }
+
+    private static void exit() {
+        reply("i zao first");
+    }
+
+    private static void echo(String input) {
+        reply(input);
+        manageInput();
+    }
+
+    private static void add(String input) {
+        Task newTask = new Task(input);
+        list.add(newTask);
+        reply("added: " + input);
+        manageInput();
     }
 
     private static void displayList() {
-        System.out.println(bar);
+        System.out.println("________________________________");
         for (int i = 0; i < list.size(); i++) {
-            System.out.println((i + 1) + "." + list.get(i));
+            Task task = list.get(i);
+            System.out.println((i + 1) + "." + task.toString());
         }
-        System.out.println(bar);
-        add();
+        System.out.println("________________________________");
+        manageInput();
+    }
+
+    private static void done(String input) {
+        int taskNumber = Integer.parseInt(input.substring(5));
+        Task currTask = list.get(taskNumber - 1);
+        currTask.markAsDone();
+        reply("noice this thing done:\n" + currTask);
+        manageInput();
     }
 }
