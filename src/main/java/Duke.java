314c21aaaaf7ad07f27637c0b984d966f79aec38
LeopardMerkava <andrewbaruch4@gmail.com>
2021-08-17 09:02:56 +0800

    Set Level 1. Added Greeting, Echo, and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,32 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+
+        String greeting = "Hello! I am \n"
+                    + logo
+                    + "The awesome bot helper! \n"
+                    + "How can I help you today?";
+
+        String goodbye = "Bye. Hope to see you again soon!";
+
+        System.out.print(reply(greeting));
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+
+        while(true) {
+            String userInput = myObj.nextLine();
+            if(userInput.equals("bye")) {
+                break;
+            } else {
+                System.out.print(reply(userInput));
+            }
+        }
+
+        System.out.print(reply(goodbye));
+    }
+
+    public static String reply(String output) {
+        String hline = "____________________________________________________________ \n";
+        return (hline + output + '\n' + hline);
     }
 }
