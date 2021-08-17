c19759c0d4e04b3ab9102cc5adea7d41e792f90d
zacharylwy <zacharylau1303@gmail.com>
2021-08-17 18:11:28 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,25 +3,18 @@ import java.util.Scanner;
 
 public class Duke {
     public Duke() {}
+    private static boolean loopStatus;
     private static int listCount = 0;
     private static Task[] dukeList = new Task[100];
     private static String greetText = "Hello I'm Duke\nWhat can I do for you?\n";
     private static String exitText = "Bye. Hope to see you again soon!";
     private static String addedText = "Got it. I've added this task:\n";
-
-
-    private static String listLengthText = "\nNow you have " + listCount + " tasks in the list";
     public void greet() {
         System.out.print(greetText);
     }
 
-    public void addToList(String input) {
-        dukeList[listCount] = new Task(input);
-        listCount++;
-        System.out.println("added: " + input);
-    }
-
     public void exit() {
+
         System.out.println(exitText);
     }
 
@@ -49,64 +42,105 @@ public class Duke {
             if (item != null && (item.getNumber() == itemNumber)) {
                 item.markAsDone();
                 message = ("Nice! I've marked this task as done:\n"
-                                        + item.getStatusIcon() + " " + item.getDescription());
+                                        + " " + item.toString());
             }
         }
         System.out.println(message);
     }
 
-    public void addDeadline(String description, String by){
-        Deadline newDL = new Deadline(description, by);
+    public void addDeadline(String input) throws DukeException{
+        // First check if the user has only input the one word "deadline".
+        if (input.split(" ", 2).length == 1) {
+            throw new DukeException("☹ Oops! Looks like you are missing the description and the deadline date! Try again :-)");
+        }
+        // If "deadline" is entered with more words, check information.
+        String[] information = input.split(" ", 2);
+        String [] description = information[1].split("/by ", 2);
+
+        //In the case where date is not entered.
+        if (description.length == 1) {
+            throw new DukeException("☹ Oops! Looks like you are missing the deadline date! Try again :-)");
+        }
+        Deadline newDL = new Deadline(description[0], description[1]);
         dukeList[listCount] = newDL;
         listCount++;
-        System.out.println(addedText + newDL.toString() + listLengthText);
+        System.out.println(addedText + newDL.toString() + "\nNow you have " + listCount + " tasks in the list");
     }
 
-    public void addEvent(String description, String at) {
-        Event newEV = new Event(description, at);
+    public void addEvent(String input) throws DukeException{
+        // First check if the user has only input the one word "event".
+        if (input.split(" ", 2).length == 1) {
+            throw new DukeException("☹ Oops! Looks like you are missing the description and the event location! Try again :-)");
+        }
+        // If "event" is entered with more words, check information.
+        String[] information = input.split(" ", 2);
+        String [] description = information[1].split("/at ", 2);
+
+        //In the case where location is not entered.
+        if (description.length == 1) {
+            throw new DukeException("☹ Oops! Looks like you are missing the event location! Try again :-)");
+        }
+        Event newEV = new Event(description[0], description[1]);
         dukeList[listCount] = newEV;
         listCount++;
-        System.out.println(addedText + newEV.toString()+ listLengthText);
+        System.out.println(addedText + newEV.toString() + "\nNow you have " + listCount + " tasks in the list");
     }
 
-    public void addTodo(String description) {
-        Todo newTD = new Todo(description);
+    public void addTodo(String input) throws DukeException {
+        // First check if the user has only input the one word "todo".
+        if (input.split(" ", 2).length == 1) {
+            throw new DukeException("☹ Oops! Looks like you are missing the description! Try again :-)");
+        }
+        //If "todo" is entered with more words, use the information.
+        String[] information = input.split(" ", 2);
+        Todo newTD = new Todo(information[1]);
         dukeList[listCount] = newTD;
         listCount++;
-        System.out.println(addedText + newTD.toString() + listLengthText);
+        System.out.println(addedText + newTD.toString() + "\nNow you have " + listCount + " tasks in the list");
     }
 
     public void start() {
+        loopStatus = true;
         greet();
-        label:
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        while (loopStatus) {
             String input = scanner.nextLine().toLowerCase();
-            String firstWord = input.split(" ")[0].toLowerCase();
+            String firstWord = input.split(" ", 2)[0];
             switch (firstWord) {
                 case "bye":
+                    loopStatus = false;
                     exit();
-                    break label;
+                    break;
                 case "list":
                     showList();
                     break;
                 case "deadline":
-                    String[] deadDesc = input.split(" ", 2)[1].split("/by ", 2);
-                    addDeadline(deadDesc[0], deadDesc[1]);
+                    try {
+                        addDeadline(input);
+                    } catch (DukeException e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case "event":
-                    String[] eventDesc = input.split(" ", 2)[1].split("/at ", 2);
-                    addEvent(eventDesc[0], eventDesc[1]);
+                    try {
+                        addEvent(input);
+                    } catch (DukeException e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case "todo":
-                    String description = input.split(" ", 2)[1];
-                    addTodo(description);
+                    try {
+                        addTodo(input);
+                    } catch (DukeException e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case "done":
                     markDone(input.substring(input.length() - 1));
                     break;
                 default:
-                    System.out.println("Oops! Looks like you have entered something incorrectly. Try again :-)");
+                    //If no cases above are entered, Duke will not understand the command and prompt the user.
+                    System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         }
     }
