5283e55ca035750d2b938b17eb92bcc19070efe7
NecroWolf28 <56968759+NecroWolf28@users.noreply.github.com>
2021-08-17 19:53:43 +0800

    Necro chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,29 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String tab = "      ";
+        String line = "------------------------------------------------------------";
+        System.out.println(line);
+        System.out.println("Hello. My name is Necro.");
+        System.out.println("What can I do for you on this horrible day?");
+        System.out.println(line);
+        System.out.println("");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(tab + line);
+                System.out.println(tab + " " + "Farewell, may we never meet again.");
+                System.out.println(tab + line);
+                break;
+            } else {
+                System.out.println(tab + line);
+                System.out.println(tab + " " + input);
+                System.out.println(tab + line);
+            }
+        }
     }
 }
