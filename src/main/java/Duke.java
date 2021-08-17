9b2180e9b3bc9fcc3a75b50ec0c47bccd9b74711
DesmondTo <e0550619@u.nus.edu>
2021-08-17 22:20:55 +0800

    Add echo and exit feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         // Greet the user
@@ -5,5 +7,20 @@ public class Duke {
                 Printer.logo +
                 "\tI'm Desmond,\n" +
                 "\thow may I serve you?\n");
+
+        // Initialize scanner to get user input
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        // Echo to user input and exit when the input is "bye"
+        while (true) {
+            if (input.equals("bye")) {
+                Printer.beautyPrint("Bye (*´▽｀)ノシ. Have a good day!\n");
+                break;
+            }
+            Printer.beautyPrint(input + "\n");
+            input = scanner.nextLine();
+        }
     }
 }
+
