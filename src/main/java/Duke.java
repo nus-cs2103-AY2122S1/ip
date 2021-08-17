10c17b0e2672491daf11ac58dfdcfafe37cbe4fa
DesmondTo <e0550619@u.nus.edu>
2021-08-17 21:55:12 +0800

    Add greeting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Greet the user
+        Printer.beautyPrint("Welcome to\n" +
+                Printer.logo +
+                "\tI'm Desmond,\n" +
+                "\thow may I serve you?\n");
     }
 }
