e107eabfca63c513d4f591d16b090f9fe8802ccb
Darren Heng <darren2pro@gmail.com>
2021-08-17 19:03:14 +0800

    Complete Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,17 @@
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    static ArrayList<String> tasks = new ArrayList<>();
+    static ArrayList<Task> tasks = new ArrayList<>();
 
     private static void getLstItems() {
         int counter = 1;
-        for (String task : tasks) {
-            System.out.println(String.valueOf(counter) + ". " + task);
+        for (Task task : tasks) {
+            System.out.println(String.valueOf(counter) + "." + task.toString());
             counter += 1;
         }
     }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -22,22 +22,35 @@ public class Duke {
         System.out.println("Hello! I'm Naruto\nWhat can I do for you?\n" + horizontalLines);
         Scanner in = new Scanner(System.in);
         String userInput;
+
         while (true) {
             userInput = in.nextLine();
             if (userInput.equals("bye")) {
                 System.out.println(horizontalLines);
                 System.out.println("See ya! Hope to see you again!" + "\n" + horizontalLines);
                 return;
-            }
-            if (userInput.equals("list")) {
+            } else if (userInput.equals("list")) {
                 System.out.println(horizontalLines);
+                System.out.println("Here are the tasks in your list:");
                 getLstItems();
                 System.out.println(horizontalLines);
                 continue;
+            } else if (userInput.substring(0, 4).equals("done")) {
+                int taskNum = Integer.valueOf(userInput.substring(5)) - 1;
+                Task currTask = tasks.get(taskNum);
+                currTask.markAsDone();
+                System.out.println("Good job! I've marked this task as done:");
+                System.out.println(currTask.toString());
+                System.out.println(horizontalLines);
+            } else {
+                // All other cases add it to tasks
+                System.out.println(horizontalLines);
+                Task newTask = new Task(userInput);
+                tasks.add(newTask);
+                System.out.println("added: " + userInput + "\n" + horizontalLines);
             }
-            System.out.println(horizontalLines);
-            tasks.add(userInput);
-            System.out.println("added: " + userInput + "\n" + horizontalLines);
         }
+
     }
+
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,19 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        String checkbox = this.isDone ? "[X] " : "[ ] ";
+        return checkbox + this.description;
+    }
+}
