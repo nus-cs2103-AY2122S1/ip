acd853212874fd07715a344ca4e126153f9e66bc
JeffZincatz <jeffzincatz@gmail.com>
2021-08-17 10:55:42 +0800

    use an ArrayList<Task> to store the tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -52,8 +53,7 @@ public class Duke {
      */
     static void add() {
         String command = "";
-        Task[] list = new Task[100];
-        int index = 0;
+        ArrayList<Task> list = new ArrayList<Task>();
         while (true) {
             command = scanner.nextLine();
             if (command.equals("bye")) {
@@ -71,14 +71,12 @@ public class Duke {
                         try {
                             int doneIndex = Integer.parseInt(command.substring(5));
                             try {
-                                list[doneIndex - 1].markAsDone();
-                                printMessage("Nice! I've marked this task as done:\n\t" + list[doneIndex - 1]);
-                            } catch (NullPointerException e) {
+                                list.get(doneIndex - 1).markAsDone();
+                                printMessage("Nice! I've marked this task as done:\n\t" + list.get(doneIndex - 1));
+                            } catch (IndexOutOfBoundsException e) {
                                 // Task at doneIndex does not exist
                                 printMessage("Task " + doneIndex + " does not exist. Please check your task list!");
-                            } catch (ArrayIndexOutOfBoundsException e) {
-                                // mark done index more than 100
-                                printMessage("Duke can only save up to 100 tasks ;(");
+
                             }
                         } catch (NumberFormatException | StringIndexOutOfBoundsException e) {
                             // command done is not followed by a number
@@ -97,9 +95,8 @@ public class Duke {
                             todoDescription.append(" ").append(actions[i]);
                         }
                         Todo todo = new Todo(todoDescription.toString());
-                        list[index] = todo;
-                        index++;
-                        printMessage("Got it. I've added this task:\n\t" + todo + "\nNow you have " + index + " tasks in the list.");
+                        list.add(todo);
+                        printMessage("Got it. I've added this task:\n\t" + todo + "\nNow you have " + list.size() + " tasks in the list.");
                         break;
                     case "event":
                         String[] eventSplit = command.split(" /at ");
@@ -120,9 +117,8 @@ public class Duke {
                             break;
                         }
                         Event event = new Event(eventDescription, at);
-                        list[index] = event;
-                        index++;
-                        printMessage("Got it. I've added this task:\n\t" + event + "\nNow you have " + index + " tasks in the list.");
+                        list.add(event);
+                        printMessage("Got it. I've added this task:\n\t" + event + "\nNow you have " + list.size() + " tasks in the list.");
                         break;
                     case "deadline":
                         String[] ddlSplit = command.split(" /by ");
@@ -143,9 +139,8 @@ public class Duke {
                             break;
                         }
                         Deadline deadline = new Deadline(ddlDescription, by);
-                        list[index] = deadline;
-                        index++;
-                        printMessage("Got it. I've added this task:\n\t" + deadline + "\nNow you have " + index + " tasks in the list.");
+                        list.add(deadline);
+                        printMessage("Got it. I've added this task:\n\t" + deadline + "\nNow you have " + list.size() + " tasks in the list.");
                         break;
                     default:
                         // Message for unrecognised task type
@@ -163,14 +158,13 @@ public class Duke {
      * @param list The list of tasks stored.
      * @return The string of messages.
      */
-    private static String listToString(Task[] list) {
+    private static String listToString(ArrayList<Task> list) {
         StringBuilder result = new StringBuilder();
-        if (list[0] != null) {
+        if (!list.isEmpty()) {
             result.append("Here are the tasks in your list:\n");
-            for (int i = 0; i < list.length; i++) {
-                if (list[i] == null) break;
-                result.append(i + 1).append(". ").append(list[i]);
-                if (list[i + 1] != null) result.append("\n");
+            result.append(1).append(". ").append(list.get(0));
+            for (int i = 1; i < list.size(); i++) {
+                result.append("\n").append(i + 1).append(". ").append(list.get(i));
             }
         } else {
             result.append("There is not task yet. Try to add a task first.");
