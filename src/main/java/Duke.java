0ffa321432ad954c4ebf899150041d18d7a29b55
angnobel <nobelang78@gmail.com>
2021-08-18 02:18:05 +0800

    Level 1: Implemet echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        echo();
+    }
+
+    public static void echo() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        Scanner in = new Scanner(System.in);
+
+        String s = "";
+
+        while (!s.equals("Bye")) {
+            s = in.nextLine();
+            System.out.println("---------");
+            System.out.println(s);
+            System.out.println("---------");
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+        in.close();
+        return;
     }
 }
