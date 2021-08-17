c2dc3c130231e908b12fdffc5454644dda11d739
Chesterwongz <psyforce99@gmail.com>
2021-08-17 15:33:41 +0800

    exception handling
    Most, if not all possible errors are being handled in the current version.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,6 +18,7 @@ public class Duke {
     private static boolean isInputDone(String input) {
         return input.startsWith("done ");
     }
+
     /**
      * Prints the Duke logo.
      */
@@ -29,6 +30,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|";
         echo("Hello from\n" + logo);
     }
+
     /**
      * Prints our greeting.
      */
@@ -37,6 +39,7 @@ public class Duke {
                 + "What can I do for you?";
         echo(greeting);
     }
+
     /**
      * Prints our goodbye.
      */
@@ -46,29 +49,87 @@ public class Duke {
     }
 
     /**
-     * Adds item to the todolist.
+     * Adds a task to the taskList.
      * @param task
      */
     private static void addToList(Task task) {
         taskList[listIndex++] = task;
         echo("Got it. I've added this task:\n  " + task + "\nNow you have " + listIndex + " tasks in the list.");
     }
+
+    /**
+     * Adds a To-do task to the taskList.
+     * @param input The String array containing the to-do description at index 1.
+     * @throws DukeException
+     */
+    private static void addToDo(String[] input) throws DukeException {
+        if (input.length > 1) {
+            addToList(new ToDo(input[1]));
+        } else {
+            throw new DukeException("OOPS!!! The description of a todo cannot be empty ☹");
+        }
+    }
+    /**
+     * Adds a deadline task to the taskList.
+     * @param input The String array containing the deadline description at index 1.
+     * @throws DukeException
+     */
+    private static void addDeadline(String[] input) throws DukeException {
+        if (input.length > 1) {
+            String[] deadline = input[1].split("/", 2);
+            if (deadline.length > 1 && deadline[1].length() > 3) {
+                addToList(new Deadline(deadline[0], deadline[1].substring(3)));
+            } else {
+                throw new DukeException("OOPS!!! Please add the deadline with the right format ☹");
+            }
+        } else {
+            throw new DukeException("OOPS!!! The description of a deadline cannot be empty ☹");
+        }
+    }
+    /**
+     * Adds a event task to the taskList.
+     * @param input The String array containing the event description at index 1.
+     * @throws DukeException
+     */
+    private static void addEvent(String[] input) throws DukeException {
+        if (input.length > 1) {
+            String[] event = input[1].split("/", 2);
+            if (event.length > 1 && event[1].length() > 3) {
+                addToList(new Event(event[0], event[1].substring(3)));
+            } else {
+                throw new DukeException("OOPS!!! Please add the event with the right format ☹");
+            }
+        } else {
+            throw new DukeException("OOPS!!! The description of a event cannot be empty ☹");
+        }
+    }
     /**
      * Marks task as done in taskList.
      * @param input The input string.
+     * @throws DukeException
      */
-    private static void doTask(String input) {
-        int taskID = Integer.parseInt(input);
-        String msg;
-        int taskIndex = taskID - 1;
-        if (taskList[taskIndex] != null) {
-            taskList[taskIndex].markAsDone();
-            msg = "Nice! I've marked this task as done:\n  "
-                    + taskList[taskIndex].toString();
-        } else {
-            msg = "OOPS!!! Task " + taskID + " doesn't exist!";
+    private static void doTask(String[] input) throws DukeException {
+        if (input.length < 2) {
+            throw new DukeException("OOPS!!! Please input the task number to be marked as done!");
         }
-        echo(msg);
+        try {
+            int taskID = Integer.parseInt(input[1]);
+            if (taskID < 1 || taskID > listIndex) {
+                throw new DukeException("OOPS!!! Please enter a valid task number!");
+            }
+            String msg;
+            int taskIndex = taskID - 1;
+            if (taskList[taskIndex] != null) {
+                taskList[taskIndex].markAsDone();
+                msg = "Nice! I've marked this task as done:\n  "
+                        + taskList[taskIndex].toString();
+            } else {
+                msg = "OOPS!!! Task " + taskID + " doesn't exist!";
+            }
+            echo(msg);
+        } catch (NumberFormatException e) {
+            throw new DukeException("OOPS!!! Please enter a valid task number!");
+        }
     }
     /**
      * Frames the message with underscore lines and prints it.
@@ -77,7 +138,7 @@ public class Duke {
      * @return The framed String.
      */
     private static void echo(String msg) {
-        String line = "________________________________";
+        String line = "__________________________________________________________";
         System.out.println(line);
         System.out.println(msg);
         System.out.println(line);
@@ -87,7 +148,7 @@ public class Duke {
      * Prints out the todolist.
      */
     private static void printList() {
-        String line = "________________________________";
+        String line = "__________________________________________________________";
         System.out.println(line);
         System.out.println("Here are the tasks in your list:");
         for (int index = 0; index < listIndex; index++) {
@@ -104,43 +165,37 @@ public class Duke {
         boolean isExited = false;
         while (!isExited) {
             // String input
-            String input = sc.nextLine();
-            switch(input) {
-                case "bye":
-                    isExited = true;
-                    bye();
-                    break;
-                case "list":
-                    printList();
-                    break;
-                default:
-                    String[] split = input.split(" ", 2);
-                    switch(split[0]) {
-                        case "done":
-                            if (split.length > 1) {
-                                doTask(split[1]);
+            try {
+                String input = sc.nextLine();
+                switch (input) {
+                    case "bye":
+                        isExited = true;
+                        bye();
+                        break;
+                    case "list":
+                        printList();
+                        break;
+                    default:
+                        String[] split = input.split(" ", 2);
+                        switch (split[0]) {
+                            case "done":
+                                doTask(split);
                                 break;
-                            }
-                        case "todo":
-                            if (split.length > 1) {
-                                addToList(new ToDo(split[1]));
+                            case "todo":
+                                addToDo(split);
                                 break;
-                            }
-                        case "deadline":
-                            if (split.length > 1) {
-                                String[] deadline = split[1].split("/", 2);
-                                addToList(new Deadline(deadline[0], deadline[1].substring(3)));
+                            case "deadline":
+                                addDeadline(split);
                                 break;
-                            }
-                        case "event":
-                            if (split.length > 1) {
-                                String[] event = split[1].split("/", 2);
-                                addToList(new Event(event[0], event[1].substring(3)));
+                            case "event":
+                                addEvent(split);
                                 break;
-                            }
-                        default:
-                            echo("OOPS! Something went wrong x.x");
-                    }
+                            default:
+                                echo("OOPS!!! I'm sorry, but I don't know what that means ☹ ");
+                        }
+                }
+            } catch (DukeException e) {
+                echo(e.getMessage());
             }
         }
     }
