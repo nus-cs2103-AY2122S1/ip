178b145fbb66a15750b488c91192c1f23fe4cd2c
Stuart Long <chayboon@gmail.com>
2021-08-17 15:20:45 +0800

    Complete Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -11,6 +12,16 @@ public class Duke {
         System.out.println(message);
         System.out.println(line);
     }
+
+    public static void print(ArrayList<String> ls) {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        for (int i = 0; i < ls.size() ; i++) {
+            System.out.println((i + 1) + ". " + ls.get(i));
+        }
+        System.out.println(line);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -18,12 +29,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+        ArrayList<String> tasks = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         greet();
         String next = sc.nextLine();
         while (!next.equals("bye")) {
-            print(next);
+            if (next.equals("list")) {
+                print(tasks);
+            } else {
+                tasks.add(next);
+                print("added: " + next);
+            }
             next = sc.nextLine();
+
+            //            print(next);
+        //            next = sc.nextLine();
         }
         print("Bye. Hope to see you again soon!");
     }
