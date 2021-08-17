3c890dd03c7435b7b95b6fe70996f3bf5585df16
Banana3021 <anaravi21@gmail.com>
2021-08-18 01:08:39 +0800

    level 1 added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,11 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.io.IOException;
-
 public class Duke {
-
-    private static String welcomeLabel = "Hello! I'm Banana \n" + "     What can I do for you?";
-    private static String byeLabel = "Bye. Hope to see you again soon!";
-
     public static void main(String[] args) {
-        /*(String logo = " ____        _        \n"
+        String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+<<<<<<< HEAD
         System.out.println("Hello from\n" + logo);*/
 
         System.out.println(displayLabel(welcomeLabel));
@@ -86,4 +79,8 @@ public class Duke {
         }
     }
 
+=======
+        System.out.println("Hello from\n" + logo);
+    }
+>>>>>>> parent of 4e38753 (level 1 modified)
 }
