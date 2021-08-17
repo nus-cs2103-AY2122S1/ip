963e14af51c4d8061ffbeb271ce9180592d7cd33
Chen Hsiao Ting <hsiaoting0919@gmail.com>
2021-08-18 01:48:32 +0800

    Level-2, Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -13,25 +15,34 @@ public class Duke {
         System.out.println("Duke: Hello! I'm Duke\nWhat can I do for you?");
         System.out.println(divider);
 
-        boolean done = false;
+        boolean exit = false;
+        ArrayList<String> request = new ArrayList<String>();
 
-        while (!done) {
+        while (!exit) {
+            System.out.print("You: ");
             Scanner sc = new Scanner(System.in);
             String str = sc.nextLine();
             if (str.equals("bye")) {
-                done = true;
+                exit = true;
                 break;
+            } else if (str.equals("list")) {
+                int count = 1;
+                System.out.println(divider);
+                for (String s : request) {
+                    System.out.println(count + ". " + s);
+                    count += 1;
+                }
+                System.out.println(divider);
+            } else {
+                request.add(str);
+                System.out.println(divider);
+                System.out.println("added: " + str);
+                System.out.println(divider);
             }
-
-            System.out.println("Duke: " + str);
-            System.out.println(divider);
         }
 
         System.out.println(divider);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(divider);
-
-
-
     }
 }
