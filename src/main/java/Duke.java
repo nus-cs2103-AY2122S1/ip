86c56b202674d8581b2e7ac72e2c43a253b43168
pualixue <pualixue@live.com.my>
2021-08-17 18:33:14 +0800

    Greet user and echo commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printWithLines("Hello! I'm 8-Bit!\nWhat can I do for you?");
+        getUserInput();
+    }
+
+    private static void printWithLines(String msg) {
+        String line = "-------------------------------------------------------";
+        System.out.println(line + "\n" + msg + "\n" + line);
+    }
+
+    private static void getUserInput() {
+        Scanner sc = new Scanner(System.in);
+
+        String command = sc.nextLine();
+        while (!command.equals("bye")) {
+            printWithLines(command);
+            command = sc.nextLine();
+        }
+
+        printWithLines("Bye. Hope to see you again soon!");
+        sc.close();
     }
 }
