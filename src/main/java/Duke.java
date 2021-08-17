99446646601181901d08f81227268bf7598384eb
RachelCheah <55133167+RachelCheah@users.noreply.github.com>
2021-08-17 19:26:54 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,15 +10,29 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
+
+
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        ArrayList<String> aList = new ArrayList<>();  // Initialize list
         System.out.println("Hello! I'm Duke\n" +   // Welcome Message
                 "What can I do for you?");
 
         String input = scanner.nextLine();  // Read user input
         while (!input.equals("bye")){
-            System.out.println("Username is: " + input);  // Output user input
+            switch(input) {
+                case "list":
+                    for (int i = 1; i <= aList.size(); i++){
+                    System.out.println(String.valueOf(i) + " : " + aList.get(i - 1));
+                    }
+                    break;
+                default:
+                    aList.add(input);
+                    System.out.println("added: " + input);  // Output user input
+            }
+
             input = scanner.nextLine();  // Read user input
         }
+        System.out.println("Bye. Hope to see you again soon!");
 
     }
 }
