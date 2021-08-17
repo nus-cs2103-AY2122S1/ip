ab058afd556e38439b0bc8befc2cfa567e178980
muhammad-khair <muhammad_khair@outlook.com>
2021-08-18 01:46:35 +0800

    Refactor logo to static variable and method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
+
+    private final static String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private final static String LINE_SPLIT =
+            "____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printLogo();
+    }
+
+    public static void printLogo() {
+        System.out.println("Hello from\n" + LOGO);
+        System.out.println(LINE_SPLIT);
     }
 }
