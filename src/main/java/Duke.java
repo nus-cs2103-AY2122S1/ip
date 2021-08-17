cdceac73c10fb0b91aa9ffa00b11476ba013aa15
uyencfi <77076051+uyencfi@users.noreply.github.com>
2021-08-17 18:01:28 +0800

    Add skeletal implementation to echo commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static void start() {
+        String greetings = "Hello! What can I do for you?";
+        System.out.println(greetings);
+
+        Scanner sc = new Scanner(System.in);
+        String cmd = "";
+        while (!cmd.equals("bye")) {
+            System.out.println(cmd + "\n");
+            cmd = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        start();
     }
 }
