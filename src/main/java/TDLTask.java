318e4c0a17c4a01c55da82280818db5473aab23e
TLChicken <tehlightningchicken@gmail.com>
2021-08-18 02:18:22 +0800

    Add TDLTask encapsulation for use with the TDList

---
+++ src/main/java/TDLTask.java
@@ -0,0 +1,64 @@
+/**
+ *  This class encapsulates a Task element in Duke's TDList, and features
+ *  various methods to manipulate the Task.
+ */
+public class TDLTask {
+    private String taskName;
+    private boolean isDone;
+
+    /**
+     * A constructor used to create a new TDLTask.
+     *
+     * @param taskName      The name and description of the task.
+     */
+    public TDLTask(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    /**
+     * Sets the current task as finished.
+     */
+    public void setAsDone() {
+        isDone = true;
+    }
+
+    /**
+     * Checks whether the task is finished.
+     *
+     * @return true if finished, false if undone.
+     */
+    public boolean isTaskDone() {
+        return isDone;
+    }
+
+    /**
+     * Gets the name/description of the task.
+     *
+     * @return the name/description of the task.
+     */
+    public String getTaskName() {
+        return taskName;
+    }
+
+    /**
+     * Gets the name/description of the task, as well as whether it is done
+     * neatly formatted in one line.
+     *
+     * @return the String containing the line describing the task.
+     */
+    public String getLineOfTaskInfo() {
+        String doneCheckboxStr = "";
+        if (isDone) {
+            doneCheckboxStr = "[X]";
+        } else {
+            doneCheckboxStr = "[ ]";
+        }
+
+        String returnThis = doneCheckboxStr + " " + this.getTaskName();
+
+        return returnThis;
+    }
+
+
+}
