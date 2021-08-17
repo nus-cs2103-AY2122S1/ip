ba1911cca16599d205623b87f35101791b1fcbad
ambroseboo <ambroseb9977@gmail.com>
2021-08-17 13:15:57 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,6 +8,8 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
         boolean exit = false;
+        String[] tasks = new String[100];
+        int current = 0;
         while (!exit) {
             String userInput = myObj.nextLine();
             if (userInput.equals("bye")) {
@@ -15,10 +17,20 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 exit = true;
+            } else if (userInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < 100; i++) {
+                    if (tasks[i] != null) {
+                        System.out.println(i + 1 + ". " + tasks[i]);
+                    }
+                }
+                System.out.println("____________________________________________________________");
             } else {
                 System.out.println("____________________________________________________________");
-                System.out.println(userInput);
+                System.out.println("added: " + userInput);
                 System.out.println("____________________________________________________________");
+                tasks[current] = userInput;
+                current++;
             }
         }
     }
