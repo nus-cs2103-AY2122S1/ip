1f7f40d1273e80470811ba979fdeabf0365afa10
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2021-08-17 18:23:39 +0800

    Teach Duke to deal with errors such as incorrect inputs entered by the user.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,26 +15,39 @@ public class Duke {
             command = myObj.nextLine();
             String[] commandSplit = command.split(" ");
             String firstCommand = commandSplit[0];
-            if (command.equals("bye")) {
-                System.out.println("Goodbye!");
-                break;
-            } else if (commandSplit[0].equals("done") && Duke.isInteger(commandSplit[1])) {
-                int taskIndex = Integer.parseInt(commandSplit[1]);
-                tasks.markTaskDone(taskIndex);
-                String task = tasks.getTask(taskIndex);
-                System.out.println("Nice! I've marked this task as done: ");
-                System.out.println(task);
-            } else if (firstCommand.equals("deadline") || firstCommand.equals("todo") || firstCommand.equals("event") ) {
-                String date = Duke.findDateInCommand(commandSplit);
-                String taskDesc = Duke.findTaskDescription(commandSplit);
-                tasks.addTask(taskDesc, firstCommand, date);
-                System.out.println("Got it. I've added this task: ");
-//                System.out.println(tasks.getTasksLength());
-                System.out.println(tasks.getTask(tasks.getTasksLength()));
-                System.out.println("Now you have " + tasks.getTasksLength() + " tasks in the list.");
-            } else if (command.equals("list")) {
-                tasks.listTasks();
+            try {
+                if (command.equals("bye")) {
+                    System.out.println("Goodbye!");
+                    break;
+                } else if (commandSplit[0].equals("done") && Duke.isInteger(commandSplit[1])) {
+                    int taskIndex = Integer.parseInt(commandSplit[1]);
+                    tasks.markTaskDone(taskIndex);
+                    String task = tasks.getTask(taskIndex);
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println(task);
+                } else if (firstCommand.equals("deadline") || firstCommand.equals("todo") || firstCommand.equals("event") ) {
+                    String date = Duke.findDateInCommand(commandSplit, firstCommand);
+                    String taskDesc = Duke.findTaskDescription(commandSplit);
+                    String aOrAn = firstCommand.equals("event") ? "an" : "a";
+                    if (taskDesc.equals("")) {
+                        throw new DukeException("☹ OOPS!!! The description of " + aOrAn + " " + firstCommand + " cannot be empty.");
+                    } else if (date.equals("") && !firstCommand.equals("todo")) {
+                        throw new DukeException("☹ OOPS!!! The date of " + aOrAn + " " + firstCommand + " cannot be empty.");
+                    } else {
+                        tasks.addTask(taskDesc, firstCommand, date);
+                        System.out.println("Got it. I've added this task: ");
+                        System.out.println(tasks.getTask(tasks.getTasksLength()));
+                        System.out.println("Now you have " + tasks.getTasksLength() + " tasks in the list.");
+                    }
+                } else if (command.equals("list")) {
+                    tasks.listTasks();
+                } else {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
+
         }
     }
 
@@ -57,10 +70,15 @@ public class Duke {
      * @param commandList The array of the split up command.
      * @return The date in the command (if any).
      */
-    private static String findDateInCommand(String[] commandList) {
+    private static String findDateInCommand(String[] commandList, String taskType) {
         int startingIndexOfDate = -1;
         for (int i = 0; i < commandList.length; i++) {
             if (commandList[i].equals("/by") || commandList[i].equals("/at")) {
+                if (taskType.equals("deadline") && commandList[i].equals("/at")) {
+                    throw new DukeException("☹ OOPS!!! Use /by for deadlines!");
+                } else if (taskType.equals("event") && commandList[i].equals("/by")) {
+                    throw new DukeException("☹ OOPS!!! Use /at for events!");
+                }
                 startingIndexOfDate = i + 1;
                 break;
             }
