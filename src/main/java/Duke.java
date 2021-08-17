fc36129e83c7c3a80330da6952fa95b1ffd422a3
NoraYUuu <norafish79@gmail.com>
2021-08-17 18:12:05 +0800

    level-1: greet, echo, exit added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
+    //for division
+    private static String ind = "    ";
+    //for sentences
+    private static String ind2 = "     ";
+    private static String div = ind + "____________________________________________________________";
+    private static String END = "bye";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greeting();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String next = sc.nextLine();
+            if (next.equals(END)) {
+                myPrint("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                myPrint(next);
+            }
+            }
+    }
+
+    private static void greeting() {
+        String g = "Hello! I'm Duke";
+        String g2 = "What can I do for you?";
+        myPrint(g + "\n" + ind2+ g2);
+    }
+
+    private static void myPrint(String s) {
+        System.out.println(div + "\n" + ind2 + s + "\n" + div);
     }
 }
