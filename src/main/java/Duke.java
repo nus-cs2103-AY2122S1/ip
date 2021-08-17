9138d7a4509c53641db3f5d53fc574a3e5c75e9a
eugenecsa <eugenechanec@gmail.com>
2021-08-17 15:18:27 +0800

    Edited Duke.java file for Level-1 (Greet, Echo, Exit).

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
+        String logo = " ____   ___  _        \n"
+                + "|  _ \\/  _ \\| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\___/|_|\\_\\___|\n";
+        System.out.println("----------------------------------------");
+        System.out.println(logo);
+        System.out.println("Hello! I'm Doke\nWhat do you want??");
+        System.out.println("----------------------------------------\n");
+        echoMessage();
+    }
+
+    /**
+     * Reads messages from input, echos back if message is not bye, exits otherwise
+     */
+    public static void echoMessage() {
+        Scanner messageReader = new Scanner(System.in);
+        boolean end = false;
+        while (!end) {
+            String message = messageReader.nextLine();
+            System.out.println("\n----------------------------------------");
+            if (message.equals("bye")) {
+                System.out.println("    Bye. Hope to see you again!");
+                end = true;
+            } else {
+                System.out.println("    " + message);
+            }
+            System.out.println("----------------------------------------\n");
+        }
+
     }
 }
