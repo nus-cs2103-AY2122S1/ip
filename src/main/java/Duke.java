1356275136dbaa8654232e38ffc41c7f9bed4cec
qvinhprolol <vinhnq4902@gmail.com>
2021-08-17 22:57:00 +0800

    Add basic greetings to the program

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
 public class Duke {
+
+    private static void insertSeparateLine() {
+        String separateLine = "____________________________________________________________";
+        System.out.println("\t" + separateLine);
+    }
+
+    public static void greetings() {
+        String logo = "\t  ____        _        \n"
+                + "\t |  _ \\ _   _| | _____ \n"
+                + "\t | | | | | | | |/ / _ \\\n"
+                + "\t | |_| | |_| |   <  __/\n"
+                + "\t |____/ \\__,_|_|\\_\\___|\n";
+
+        insertSeparateLine();
+        System.out.println(logo);
+        System.out.println("\t" + " Hello! I'm Duke");
+        System.out.println("\t" + " What can I do for you?");
+        insertSeparateLine();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke.greetings();
+
     }
 }
