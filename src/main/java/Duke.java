96d38aa480cf5eb8b53cf9f92b7d7846f6493462
rgbpokka <jeremyyeozhichen99@gmail.com>
2021-08-18 07:10:43 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,27 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (respond(input)) break;
+        }
+    }
+
+    public static void greet() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    public static boolean respond(String command) {
+        if (! command.equals("bye")) {
+            System.out.println(command);
+            return false;
+        } else {
+            System.out.println("Bye. Hope to see you again soon!");
+            return true;
+        }
     }
 }
