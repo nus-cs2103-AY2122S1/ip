c52118ad6475323b678f7d2bb5b4e1ca59d2d57c
ChuangZheQuan <zhequan7chuang@gmail.com>
2021-08-17 21:51:11 +0800

    Level 2 commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,15 +11,23 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
-
+        String[] listOfItems = new String[100];
+        int index = 0;
         while (true) {
-            String input = sc.nextLine();
-            System.out.print("output: ");
-            if (input.equals("bye")) {
+            String input = sc.nextLine().toLowerCase();
+            boolean isLastCharDigit =  Character.isDigit(input.charAt(input.length() - 1));
+            if (input.equals("list")) {
+                System.out.println("list");
+                for (int i = 0; i < index; i++) {
+                    System.out.println("\t" + (i + 1) + ". " + listOfItems[i]);
+                }
+            } else if (input.equals("bye")) {
                 System.out.println(" \t Bye. Hope to see you again soon!");
                 break;
-            } else {
-                System.out.println("\t" + input);
+            }else {
+                listOfItems[index] = input;
+                index++;
+                System.out.println("\tadded: " + input);
             }
         }
     }
