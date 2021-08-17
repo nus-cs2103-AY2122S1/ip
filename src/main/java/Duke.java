2a900240e6574b0e72cf1be09a23de1cdfcf13fd
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-08-17 15:12:26 +0800

    Moved blocks of code within the if else statements of getCommand() into their own functions to clean up code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,30 +21,50 @@ public class Duke {
 
         // Checks if user commands for specific commands
         if (command.toLowerCase().equals("bye")) {
-            // User command is bye, print bye message and programme stops
-            System.out.println("\t Bye. Hope to see you again soon");
+            sayBye();
         } else if (command.toLowerCase().equals("list")) {
-            // User command is list, print current list of commands and continues asking for commands
-            if (counter == 0) {
-                System.out.println("Nothing has been added to the list");
-            }
-            for (int i = 0; i < counter; i++) {
-                System.out.println((i + 1) + ". " + taskList[i]);
-            }
-            getCommand();
+            showList();
         } else if(command.toLowerCase().contains("done ")) {
             int taskDone = Character.getNumericValue(command.charAt(command.length() - 1)) - 1;
-            taskList[taskDone].markAsDone();
-            System.out.println("Good job! This task has been completed:\n");
-            System.out.println(taskList[taskDone].toString());
-            getCommand();
+            finishTask(taskDone);
         } else {
-            // User command is something else, add command to command list and continue asking for commands
-            System.out.println("\t added: " + command);
-            Task t = new Task(command);
-            taskList[counter] = t;
-            counter++;
-            getCommand();
+            addTask(command);
         }
+    }
+
+    // Function for when user inputs "bye"
+    private static void sayBye() {
+        // User command is bye, print bye message and programme stops
+        System.out.println("\t Bye. Hope to see you again soon");
+    }
+
+    // Function for when user inputs "list"
+    private static void showList() {
+        // User command is list, print current list of commands and continues asking for commands
+        if (counter == 0) {
+            System.out.println("Nothing has been added to the list");
+        }
+        for (int i = 0; i < counter; i++) {
+            System.out.println((i + 1) + ". " + taskList[i]);
+        }
+        getCommand();
+    }
+
+    // Function for when user inputs "done"
+    private static void finishTask(int taskDone) {
+        taskList[taskDone].markAsDone();
+        System.out.println("Good job! This task has been completed:\n");
+        System.out.println(taskList[taskDone].toString());
+        getCommand();
+    }
+
+    // Function for when user command is something else
+    private static void addTask(String command) {
+        // User command is something else, add command to command list and continue asking for commands
+        System.out.println("\t added: " + command);
+        Task t = new Task(command);
+        taskList[counter] = t;
+        counter++;
+        getCommand();
     }
 }
