86b73fb76fbab75c659dea1c38f1b7b0bbd7c011
Ong Zheng Lin <ongzhenglin1999@gmail.com>
2021-08-17 23:41:39 +0800

    Level-2, changed login function from if-else statements into switch statement and partially implemented level-3 checkboxes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,20 +11,36 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine(); // scan the line for the user's input
         List list = new List();
-        while (true) {//create loop for the chat
-            if (input.equals("bye") || input.equals("Bye") || input.equals("BYE")) {//input is bye
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else if(input.equals("list")) {// shows the current list
-                list.show();
-                input = sc.nextLine();
-            } else { //input is not bye so add input to list
-                System.out.println("added: " + input);
-                list.add(input);
-                input = sc.nextLine();
+        boolean shouldContinue = true;
+        while (shouldContinue) {//create loop for the chat
+//            if (input.equals("bye") || input.equals("Bye") || input.equals("BYE")) {//input is bye
+//                System.out.println("Bye. Hope to see you again soon!");
+//                break;
+//            } else if(input.equals("list")) {// shows the current list
+//                list.show();
+//                input = sc.nextLine();
+//            } else { //input is not bye so add input to list
+//                System.out.println("added: " + input);
+//                list.add(input);
+//                input = sc.nextLine();
+//            }
+            switch(input) {
+                case "bye":
+                case "Bye":
+                case "BYE":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    shouldContinue = false;
+                    break;
+                case "list":
+                    list.show();
+                    input = sc.nextLine();
+                    break;
+                default:
+                    System.out.println("added: " + input);
+                    list.add(input);
+                    input = sc.nextLine();
             }
-
         }
-
+        sc.close();
     }
 }
--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,20 +1,24 @@
 import java.util.ArrayList;
 
 public class List {
-    private ArrayList<String> list;
+    private ArrayList<Task> list;
 
     public List() {
         list = new ArrayList<>();
     }
 
     public void add(String text) {
-        list.add(text);
+        list.add(new Task(text));
+    }
+
+    public void setIndexDone(int index) {// starts from 1
+        list.get(index - 1).setDone();
     }
 
     public void show() {
         int length = list.size();
         for(int i = 1; i <= length; i++) {
-            System.out.println(i + ". " + list.get(i - 1));
+            System.out.println(i + ".[" + list.get(i - 1).getStatusIcon() + "] " + list.get(i - 1).getDescription());
         }
     }
 }
