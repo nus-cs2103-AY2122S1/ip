58562c3bd101b5c34c5c69020ff6c80e32bc211f
Alissa <alissayarmantho1@gmail.com>
2021-08-18 01:01:40 +0800

    Add Level 1 Requirements - Add Command enums - Add Greetings Message - Add Goodbye Message

--- src/main/java/Saber.java
+++ src/main/java/Saber.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Saber {
     protected static String logo = "                            __,    _,  _ __  ______ _ __\n" +
             "              />           (      / | ( /  )(  /   ( /  )\n" +
@@ -11,7 +13,33 @@ public class Saber {
             "            \\<\n" +
             "             \\>\n";
 
+    protected static String lineBreak = "      |                                     |\n" +
+            "{o)xxx|===============- * -===============|xxx(o}\n" +
+            "      |                                     |\n";
+
+    protected static String greeting = lineBreak + "      I hear your command, Master.\n" +
+            "      What can I do for you?\n" + lineBreak;
+
+    protected static String goodbye = lineBreak + "      Am I ... no longer needed, Master?\n" +
+            "      I understand. I shall excuse myself.\n" + lineBreak;
+
+    enum Command {
+        bye,
+    }
+
     public static void main(String[] args) {
-        System.out.println("\n" + Saber.logo);
+        boolean end = false;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n" + logo);
+        System.out.println(greeting);
+        while (!end) {
+            String inputCommand = sc.nextLine();
+            if (inputCommand.equals(Command.bye.name())) {
+                end = true;
+            } else {
+                System.out.println(lineBreak + "      "  + inputCommand + "\n" + lineBreak);
+            }
+        }
+        System.out.println(goodbye);
     }
 }
