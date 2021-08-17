52e697312cf0e6092479f20975ffc35b626def11
marcuschj <haojie.chua@gmail.com>
2021-08-17 20:12:53 +0800

    Level 1 customisation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,8 +8,8 @@ public class Duke {
                 + "| |_  |/  _|| |/ _ \\ |  __  |____| |       \n"
                 + "| __| | /   | |  __/ | / \\  |  __  |       \n"
                 + "|_|   |_|   |_|\\____ |_|  |_|______|       \n";
-        System.out.println("Hi there! Start chatting with your new \n" + logo);
 
+        System.out.println("Hi there! Start chatting with your new \n" + logo);
         System.out.println("What's up today?");
 
         echo();
@@ -18,12 +18,13 @@ public class Duke {
     public static void echo() {
         Scanner sc = new Scanner(System.in);  // Create a Scanner object
         String message = sc.nextLine();
+        String friendGreeting = "(*^_^*) your virtual friend says: \n";
 
         if (message.equals("bye")) {
-            System.out.println("See you again my friend!");
+            System.out.println(friendGreeting + "See you again my friend!");
         }
         else {
-            System.out.println(message);
+            System.out.println(friendGreeting + message);
             echo();
         }
     }
