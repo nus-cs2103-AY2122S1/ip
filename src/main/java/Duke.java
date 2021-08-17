ed09dd04861b15700b61bc8e5bdf8623c446f96c
kaldius <quantf@gmail.com>
2021-08-18 00:33:59 +0800

    Add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String command;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "What can I do for you?\n");
+        while(true) {
+            System.out.print("You: ");
+            command = sc.nextLine();
+            System.out.print("Duke: ");
+            if (command.equals("bye")) {
+                System.out.println("Nice talking to you, goodbye!");
+                break;
+            } else {
+                System.out.println(command + "\n");
+            }
+        }
     }
 }
