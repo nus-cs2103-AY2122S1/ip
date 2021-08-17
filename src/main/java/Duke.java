6a510da44e737eba497b16718f5d9f044b666fec
chengseong <chengseong02@hotmail.com>
2021-08-17 17:06:04 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int index = 1;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,12 +15,19 @@ public class Duke {
         String command = sc.nextLine();
         while(!command.equals("bye")){
             if(command.equals("list")){
+                System.out.println("Here are the tasks in your list: ");
                 for(int i = 1; i < index; i++){
                     System.out.println(i + ". " + list[i]);
                 }
+            } else if (command.split(" ")[0].equals("done")) {
+                int toComplete = Integer.parseInt(command.split(" ")[1]);
+                list[toComplete].complete();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(list[toComplete]);
             } else {
                 System.out.println("added: " + command);
-                list[index] = command;
+                Task toAdd = new Task(command);
+                list[index] = toAdd;
                 index++;
             }
             command = sc.nextLine();
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+class Task {
+    private int done;
+    private String toDo;
+
+    Task(String toDo) {
+        this(0, toDo);
+    }
+
+    Task(int done, String toDo){
+        this.done = done;
+        this.toDo = toDo;
+    }
+
+    void complete(){
+        this.done = 1;
+    }
+
+    @Override
+    public String toString(){
+        if(done == 1){
+            return String.format("[X] %s", toDo);
+        } else {
+            return String.format("[] %s", toDo);
+        }
+    }
+
+}
