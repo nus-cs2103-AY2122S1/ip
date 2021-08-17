fa04f5a6d19588e86caedfb078eb6a1243c0013d
Chi Xu <chixucontinue@gmail.com>
2021-08-17 17:29:52 +0800

    Add greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,49 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    private static final String HORIZONTAL_LINE = "  -----------------------";
+    private static final String INDENTATION = "    ";
+
+    public static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(INDENTATION + "Hello, I am Duke.");
+        System.out.println(INDENTATION + "What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void echo(String s) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(INDENTATION + s);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void bye() {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(INDENTATION + "Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void main(String[] args) {
+        greet();
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String input = sc.nextLine();
+            switch (input) {
+                case "bye":
+                    bye();
+                    sc.close();
+                    return;
+                default:
+                    echo(input);
+            }
+        }
+        sc.close();
+        return;
     }
 }
