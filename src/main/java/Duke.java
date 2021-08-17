1811d78f326e3fd15044a9579c946266b6964702
timiditi <timothy.ng.jw@gmail.com>
2021-08-17 12:25:36 +0800

    Implement add and list functionality
    User input is now added to an item list. 'list' command prints the list.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,36 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
+    private static ArrayList<String> itemList = new ArrayList<>();
+
     /**
-     *
+     * Adds item to itemList.
+     * @param item Item to add to itemList.
+     */
+    private static void add(String item) {
+        itemList.add(item);
+        System.out.println("added: " + item);
+    }
+
+    /**
+     * Prints out itemList.
+     */
+    private static void readList() {
+        for (int i = 0; i < itemList.size(); i++) {
+            System.out.println((i + 1) + ". " + itemList.get(i));
+        }
+    }
+
+    /**
+     * Echo function. Prints out given input String.
+     * @param input The String to be printed.
      */
     private static void echo(String input) {
         System.out.println(input);
     }
+
     /**
      * Main function.
      * @param args
@@ -27,15 +50,22 @@ public class Duke {
 
             String input = sc.next();
 
+            System.out.println("____________________________________________________________");
+
             switch (input) {
                 case "bye":
                     System.out.println("Seeya!");
                     exit = true;
                     break;
+                case "list":
+                    readList();
+                    break;
                 default:
-                    echo(input);
+                    add(input);
                     break;
             }
+
+            System.out.println("____________________________________________________________");
 
         }
 
