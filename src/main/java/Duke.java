f2fe4f30a8ac1f12cbc4a5a7cc572d98016b6111
Jason <jason.ng.zq99@gmail.com>
2021-08-17 11:39:34 +0800

    Add ability to echo commands, and terminates on "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Im Duke\nWhat can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String border = "____________________________________________________________";
+        while (!input.equals("bye")) {
+            System.out.println(border);
+            System.out.println(input);
+            System.out.println(border);
+            input = sc.nextLine();
+        }
+        System.out.println(border);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(border);
     }
 }
