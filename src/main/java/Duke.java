3df64d16a155288a5adaba4415d4d884e3f5a6e9
Stephanie <stephaniechenshiyao@gmail.com>
2021-08-17 18:05:20 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.*;
+
 public class Duke {
+    private static final String SEPARATOR = "__________________________________________\n";
+    private static final String GREETING = "Hello! I'm Talky McTalkFace\n"
+            + "What can I do for you?\n";
+    private static final String EXIT = "Bye. Hope to see you again soon!\n";
+    private static final String PROMPT = "What would you like to do?";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String ExitCommand = "bye";
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+
+        System.out.print(SEPARATOR + GREETING + SEPARATOR);
+
+        while (!input.equals(ExitCommand)) {
+            System.out.println(PROMPT);
+            input = sc.nextLine();
+            if (input.equals(ExitCommand)) {
+                System.out.print(SEPARATOR + EXIT + SEPARATOR);
+            } else {
+                System.out.print(SEPARATOR + input + "\n" + SEPARATOR);
+            }
+        }
+
     }
 }
