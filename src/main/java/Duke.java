68787b9d405ef25c070ea149c5637ccca61a6f30
Ricky Andrew <ricky-aandrew@hotmail.com>
2021-08-17 09:34:13 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+    private void printMessage(String message) {
+        System.out.println(STATICS.INDENTED_HORIZONTAL_LINE);
+        System.out.println(STATICS.INDENT + message);
+        System.out.println(STATICS.INDENTED_HORIZONTAL_LINE + "\n");
+    }
+
+    private void printIntro() {
+        System.out.println(STATICS.logo);
+        printMessage(STATICS.INTRODUCTION);
+    }
+
+    private void printBye() {
+        printMessage("Bye. Hope to see you again soon!");
+    }
+
+    public void start() {
+        String userInput = "";
+        Scanner sc = new Scanner(System.in);
+
+        this.printIntro();
+
+        while (true) {
+            userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                this.printBye();
+                break;
+            }
+            this.printMessage(userInput);
+        }
+        sc.close();
     }
 }
---
+++ src/main/java/STATICS.java
@@ -0,0 +1,10 @@
+public class STATICS {
+    public static String logo = " ____        _        \n"
+                              + "|  _ \\ _   _| | _____ \n"
+                              + "| | | | | | | |/ / _ \\\n"
+                              + "| |_| | |_| |   <  __/\n"
+                              + "|____/ \\__,_|_|\\_\\___|\n";
+    public static String INDENTED_HORIZONTAL_LINE = "    ____________________________________________________________";
+    public static String INDENT = "     ";
+    public static String INTRODUCTION = "Hello! I'm Duke\n" + STATICS.INDENT + "What can I do for you?";
+}
