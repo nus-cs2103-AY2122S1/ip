72c1b6f2811d11ea5e29006a09bbd56e61fb269c
Leeroy Liu <leeroyliu999@gmail.com>
2021-08-17 14:39:57 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,15 +6,30 @@ public class Duke {
         String you = "You";
         boolean endChat = false;
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int listLen = 0;
 
         System.out.println(String.format("%s: Hello! I'm %s. What can I do for you?", chatbot, chatbot));
         while (!endChat) {
             System.out.print(you + ": ");
             String input = scanner.next();
-            if (input.equals("bye")) {
-                endChat = true;
-            } else {
-                System.out.println(String.format("%s: %s", chatbot, input));
+            switch (input) {
+                case "bye":
+                    endChat = true;
+                    break;
+                case "list":
+                    System.out.println(chatbot + ":");
+                    if (listLen == 0) {
+                        System.out.println("No item in list!");
+                    }
+                    for(int i = 0; i < listLen; i++) {
+                        System.out.println(String.format("%d. %s", i + 1, list[i]));
+                    }
+                    break;
+                default:
+                    list[listLen] = input;
+                    System.out.println(String.format("%s: added: %s", chatbot, input));
+                    listLen++;
             }
         }
         System.out.println(String.format("%s: Bye. Hope to see you again soon!", chatbot));
