cc21abaa2d16d22be2a0e16330a8086b47cd72d6
ambroseboo <ambroseb9977@gmail.com>
2021-08-17 23:38:20 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -32,8 +32,8 @@ public class Duke {
                 System.out.println("____________________________________________________________");
                 if (tasks[index - 1] != null) {
                     tasks[index - 1].markAsDone();
-                    System.out.println(" Nice! I've marked this task as done: ");
-                    System.out.println("   " + tasks[index - 1].getTaskNoNum());
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println("  " + tasks[index - 1].getTaskNoNum());
                 } else {
                     System.out.println("There is no such task.");
                 }
