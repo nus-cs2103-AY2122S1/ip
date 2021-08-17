147626a03d21ba114ef968c04244467a6361e2fc
DESKTOP-MM81EQR\Nathan <e0543978@u.nus.edu>
2021-08-17 15:26:22 +0800

    Level-1 increments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,8 +4,12 @@ public class Duke {
     public static void main(String[] args) {
 
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-
-
-
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        while (! command.equals("bye")) {
+            System.out.println(command);
+            command = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
