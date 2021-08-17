09ef9fb66aa6354ba721977b1f25d2d3d68cb57f
Ryan Peh <qwertz0808@gmail.com>
2021-08-17 14:19:15 +0800

    add greet, echo and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = " __          __  _                            _______    \n" +
+                " \\ \\        / / | |                          |__   __|   \n" +
+                "  \\ \\  /\\  / /__| | ___ ___  _ __ ___   ___     | | ___  \n" +
+                "   \\ \\/  \\/ / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\    | |/ _ \\ \n" +
+                "    \\  /\\  /  __/ | (_| (_) | | | | | |  __/    | | (_) |\n" +
+                "     \\/  \\/ \\___|_|\\___\\___/|_|_|_| |_|\\___|    |_|\\___/ \n" +
+                "               | \\ | |/ __ \\|  __ \\|  \\/  |              \n" +
+                "               |  \\| | |  | | |__) | \\  / |              \n" +
+                "               | . ` | |  | |  _  /| |\\/| |              \n" +
+                "               | |\\  | |__| | | \\ \\| |  | |              \n" +
+                "               |_| \\_|\\____/|_|  \\_\\_|  |_|\n";
+        String divider = "─────────────────────────────────────────────────────────\n";
+        String doubleDivider = "═════════════════════════════════════════════════════════\n";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(doubleDivider + greeting + doubleDivider);
+        System.out.println("Please enter the text to be echoed.\n" + divider);
+
+        while (true) {
+            String next = scanner.nextLine();
+            if (next.equals("bye") || next.equals("Bye")) {
+                System.out.println(divider + "Bye. Hope to see you again soon!\n" + divider);
+                break;
+            }
+            System.out.println(divider + next + "\n" + divider);
+        }
     }
 }
