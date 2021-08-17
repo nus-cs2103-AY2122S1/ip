55c9c1b406fd21a48b4e00651ab0ff94a7a0b49a
marcuschj <haojie.chua@gmail.com>
2021-08-17 20:05:29 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello hello from\n" + logo);
+        String logo
+                = " _____       _                    _        \n"
+                + "|  ___|  __  _  ___   _,____     | |       \n"
+                + "| |_  |/  _|| |/ _ \\ |  __  |____| |       \n"
+                + "| __| | /   | |  __/ | / \\  |  __  |       \n"
+                + "|_|   |_|   |_|\\____ |_|  |_|______|       \n";
+        System.out.println("Hi there! Start chatting with your new \n" + logo);
+
+        System.out.println("What's up today?");
+
+        echo();
+    }
+
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+        String message = sc.nextLine();
+
+        if (message.equals("bye")) {
+            System.out.println("See you again my friend!");
+        }
+        else {
+            System.out.println(message);
+            echo();
+        }
     }
 }
