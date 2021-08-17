0a45bb43821ffae505306cbd7c1877976d577bc5
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-08-17 16:09:31 +0800

    added exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,42 +6,58 @@ public class Duke {
 
     public static void main(String[] args) {
         System.out.println("hi sis, type out your task right away! :D");
-
         while (true) {
-            Scanner sc = new Scanner(System.in);
-            String text = sc.nextLine();
-            if (text.equals("q")) {
-                System.out.println("ciao!");
-                break;
-            } else if (text.equals("ls")) {
-                printList(index);
-            } else if (text.contains("done")){
-                markAsDone(text);
-            } else {
-                addTask(text, index);
+            try {
+                Scanner sc = new Scanner(System.in);
+                String text = sc.nextLine();
+                if (text.equals("q")) {
+                    System.out.println("ciao!");
+                    break;
+                } else if (text.equals("ls")) {
+                    printList(index);
+                } else if (text.contains("done")){
+                    markAsDone(text);
+                } else {
+                    addTask(text, index);
+                }
+            } catch (DukeException e) {
+                System.err.println(e);
             }
         }
     }
 
-    private static void addTask(String text, int i) {
-        if (text.contains("todo")) {
-            myList[i] = new Todo(text.split(" ", 2)[1]);
-        } else if (text.contains("deadline")) {
-            String[] task = text.split(" ", 2)[1].split(" /by ", 2);
-            myList[i] = new Deadline(task[0], task[1]);
-        } else if (text.contains("event")) {
-            String[] task = text.split(" ", 2)[1].split(" /at ", 2);
-            myList[i] = new Event(task[0], task[1]);
-        } else {
-            System.out.println("Please use the keyword --todo, deadline or event--");
-            return;
+    private static void addTask(String text, int i) throws DukeException {
+        text = text.trim();
+        try {
+            if (text.contains("todo")) {
+                myList[i] = new Todo(text.split(" ", 2)[1]);
+            } else if (text.contains("deadline")) {
+                try {
+                    String[] task = text.split(" ", 2)[1].split(" /by ", 2);
+                    myList[i] = new Deadline(task[0], task[1]);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    throw new DukeException("use the format --deadline  xx /by xx--");
+                }
+            } else if (text.contains("event")) {
+                try {
+                    String[] task = text.split(" ", 2)[1].split(" /at ", 2);
+                    myList[i] = new Event(task[0], task[1]);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    throw new DukeException("use the format --event xx /at xx--");
+                }
+            } else {
+                throw new DukeException("Please use the keyword --todo, deadline or event--");
+            }
+            System.out.println("added: " + myList[i]);
+            index++;
+            System.out.printf("Now you have %d task(s) in the list.\n", index);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException("The description of a task cannot be empty.");
         }
-        System.out.println("added: " + myList[i]);
-        index++;
-        System.out.printf("Now you have %d task(s) in the list.\n", index);
     }
 
     private static void printList(int len) {
+        if (len == 0) { System.out.println("The list is empty!"); }
         for (int i = 0; i < len; i++) {
             Task curr = myList[i];
             System.out.printf("%s. %s\n", i + 1, curr);
