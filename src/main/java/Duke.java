8c5d751dac185b291560a69e9cb15ce6bbbf2d23
Marcus <txk.marcus@gmail.com>
2021-08-17 18:41:18 +0800

    Add ability to store and display list to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -25,19 +26,31 @@ public class Duke {
                 "      ██░░░░░░██░░░░░░██░░░░░░░░░░████\n" +
                 "████████████████████████████████████████\n";
 
-        String separator =  "\n▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂\n";
+        String separator = "════════════════════════════════════════";
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Woof from\n" + logo);
-        System.out.println("Woof! I'm Biscuit.\nWhat can I do for you?" + separator);
-        while (true) {
+        System.out.println("Woof! I'm Biscuit.\nWhat can I do for you?\n" + separator);
+        boolean isContinue = true;
+        ArrayList<String> list = new ArrayList<>();
+        while (isContinue) {
             String input = scanner.nextLine();
-            if (input.equals("bye")) {
-                System.out.println("Bye! Hope to see you again soon! \uD83D\uDC3E");
-                break;
-            } else {
-                System.out.println(separator + input + separator);
+            System.out.println(separator);
+            switch (input) {
+                case "list":
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println(i + 1 + ". " + list.get(i));
+                    }
+                    break;
+                case "bye":
+                    System.out.println("Bye! Hope to see you again soon! \uD83D\uDC3E");
+                    isContinue = false;
+                    break;
+                default:
+                    System.out.println("added: " + input);
+                    list.add(input);
             }
+            System.out.println(separator);
         }
     }
 }
