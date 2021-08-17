19e0c95c559b02946aa4903639ae1b21cd0d5809
yk Wei <ykwei7@gmail.com>
2021-08-17 14:13:32 +0800

    Level-5

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,14 +13,14 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp C:\Users\ykwei\OneDrive\Desktop\Yk\Academic\Y2S1\CS2103T\Projects\ip\src\main\java -Xlint:none -d ../bin ../src/main/java/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath C:\Users\ykwei\OneDrive\Desktop\Yk\Academic\Y2S1\CS2103T\Projects\ip\bin Duke < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
