d37e8c904cc2f063968a1e9db9e7d4c527316c79
Banana3021 <anaravi21@gmail.com>
2021-08-18 01:28:16 +0800

    level 1-4 finished

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.IOException;
+
+
 public class Duke {
+
+    private static String welcomeLabel = "Hello! I'm Banana \n" + "     What can I do for you?";
+    private static String byeLabel = "Bye. Hope to see you again soon!";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-<<<<<<< HEAD
-        System.out.println("Hello from\n" + logo);*/
+        System.out.println("Hello from\n" + logo);
 
         System.out.println(displayLabel(welcomeLabel));
         //Level1(new Scanner(System.in));
@@ -79,8 +87,140 @@ public class Duke {
         }
     }
 
-=======
-        System.out.println("Hello from\n" + logo);
+    // Adding onto Level 2, marks tasks as done.
+    public static void Level3(Scanner userInput) {
+        String input = userInput.nextLine();
+        ArrayList<Task> tasks = new ArrayList<>();
+        while (!input.equals("bye")) {
+            if (input.contains("done")) {
+                int index = Integer.parseInt(input.substring(5, 6)) - 1;
+                tasks.get(index).setIsDone();
+                System.out.println(displayLabel("Nice! I've marked this task as done: \n" +
+                        "       " + tasks.get(index).toString()));
+            } else if (!input.equals("list")) {
+                tasks.add(new Task(input));
+                System.out.println(displayLabel("added: " + input));
+            }  else {
+                String itemCollection = getItems(tasks);
+                System.out.println(displayLabel("Here are the tasks in your list: \n"
+                        + "     " + itemCollection));
+            }
+            input = userInput.nextLine();
+        }
     }
->>>>>>> parent of 4e38753 (level 1 modified)
+
+    public static void Level4(Scanner userInput) {
+        String input = userInput.nextLine();
+        ArrayList<Task> tasks = new ArrayList<>();
+        while (!input.equals("bye")) {
+            if (input.contains("done")) {
+                int index = Integer.parseInt(input.substring(5, 6)) - 1;
+                tasks.get(index).setIsDone();
+                System.out.println(displayLabel("Nice! I've marked this task as done: \n" +
+                        "       " + tasks.get(index).toString()));
+            } else if (!input.equals("list")) {
+                if (input.contains("todo")) {
+                    String info = input.substring(5);
+                    tasks.add(new ToDo(info));
+                } else if (input.contains("deadline")) {
+                    String[] info = input.substring(9).split("/by");
+                    tasks.add(new Deadline(info[0], info[1]));
+                } else if (input.contains("event")) {
+                    String[] info = input.substring(6).split("/at");
+                    tasks.add(new Event(info[0], info[1]));
+                } else {
+                    tasks.add(new Task(input));
+                }
+                System.out.println(displayLabel("Got it. I've added this task:  \n" +
+                        "       " + tasks.get(tasks.size() - 1).toString() + "\n     Now you have "
+                        + Integer.toString(tasks.size()) + " tasks in the list."));
+            }  else {
+                String itemCollection = getItems(tasks);
+                System.out.println(displayLabel(itemCollection));
+            }
+            input = userInput.nextLine();
+        }
+        System.out.println(displayLabel(byeLabel));
+    }
+
+
 }
+
+class Task {
+
+    private String newTask;
+    private boolean isDone;
+
+    public Task(String newTask) {
+        this.newTask = newTask;
+    }
+
+    public String getIsDone() {
+        return isDone ? "[X]" : "[ ]";
+    }
+
+    public void setIsDone() {
+        isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return getIsDone() + " " + newTask;
+    }
+
+}
+
+class ToDo extends Task {
+
+    private String newTask;
+
+    public ToDo(String newTask) {
+        super(newTask);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+
+}
+
+class Deadline extends Task {
+
+    private String newTask;
+    private String deadLine;
+
+    public Deadline(String newTask, String deadLine) {
+        super(newTask);
+        this.deadLine = deadLine;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + deadLine + ")";
+    }
+
+}
+
+class Event extends Task {
+
+    private String newTask;
+    private String timing;
+
+    public Event(String newTask, String timing) {
+        super(newTask);
+        this.timing = timing;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + timing + ")";
+    }
+
+}
+
+
+
+
+
+
