37ca3d96f6b609f24a2471a40be03e059a3daa31
mrmrinal <mrinalganesh@gmail.com>
2021-08-18 01:37:27 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private static List<String> items = new ArrayList<String>(100);
     public static void main(String[] args) {
         Scanner userSc = new Scanner(System.in);
-
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-
         String name = "JARVIS";
-        System.out.println("Hello I am " + name +"\nIs there anything I can do for you Sir?\n");
+
+
+        System.out.println("Hello I am " + name +".\nIs there anything I can do for you Sir?\n");
 
         String userInput = userSc.nextLine();
         while(!userInput.equals("bye")){
-            echo(userInput);
+            if(userInput.equals("list")){
+                list();
+            } else {
+                echo(userInput);
+            }
             userInput = userSc.nextLine();
         }
+
         System.out.println("Goodbye Sir! Will take good care of your plants in the meantime.");
     }
 
     public static void echo(String userInput){
-        System.out.println(userInput);
+        items.add(userInput);
+        System.out.println("added: " + userInput + "\n");
+    }
+
+    public static void list(){
+        for(int i = 1; i <= items.size(); i++){
+            System.out.println(i + ". " + items.get(i - 1));
+        }
+        System.out.println("");
     }
 
 
