216f20a009b332cfd221f6b4cfb2c1b3c693204c
david-eom <david.eom.hyo.shik@gmail.com>
2021-08-17 22:58:30 +0800

    Refactor `main`

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
-    public static void main(String[] args) {
+
+    public static void printLogo() {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(logo);
+    }
+
+    public static void main(String[] args) {
+        Duke.printLogo();
     }
 }
