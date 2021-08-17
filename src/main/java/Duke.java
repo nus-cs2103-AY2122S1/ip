9c2a17af982379fb0683fa4dfbe990217df575dc
Leeroy Liu <leeroyliu999@gmail.com>
2021-08-17 15:54:35 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
         String you = "You";
         boolean endChat = false;
         Scanner scanner = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int listLen = 0;
 
         System.out.println(String.format("%s: Hello! I'm %s. What can I do for you?", chatbot, chatbot));
@@ -23,11 +23,29 @@ public class Duke {
                         System.out.println("No item in list!");
                     }
                     for(int i = 0; i < listLen; i++) {
-                        System.out.println(String.format("%d. %s", i + 1, list[i]));
+                        System.out.println(String.format("%d.[%s] %s", i + 1, list[i].getStatusIcon(),
+                                list[i].getDescription()));
+                    }
+                    break;
+                case "done":
+                    String str = scanner.next();
+                    try {
+                        int taskNo = Integer.parseInt(str);
+                        if (taskNo <= listLen) {
+                            Task task = list[taskNo - 1];
+                            task.markAsDone();
+                            System.out.println(String.format("%s: Nice! I've marked this task as done:\n\t[%s] %s",
+                                    chatbot, task.getStatusIcon(), task.getDescription()));
+                        } else {
+                            System.out.println(String.format("%s: Error in finding task number: %d", chatbot, taskNo));
+                        }
+
+                    } catch (NumberFormatException e) {
+                        System.out.println(String.format("%s: Error in parsing number: %s", chatbot, str));
                     }
                     break;
                 default:
-                    list[listLen] = input;
+                    list[listLen] = new Task(input);
                     System.out.println(String.format("%s: added: %s", chatbot, input));
                     listLen++;
             }
