0080b07d19b387d284afe119a648a7da347b7cfe
Eunice <eunicesimsingyee@gmail.com>
2021-08-18 00:04:56 +0800

    Change if-else statements in Duke.java to a switch
    To improve readibility of code.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 
 public class Duke {
     private static Scanner scanner;
+    private static ArrayList<Task> savedInputs;
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
+        savedInputs = new ArrayList<>(100);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         chat();
     }
@@ -15,7 +17,6 @@ public class Duke {
      */
     private static void chat() {
         String input;
-        ArrayList<Task> savedInputs = new ArrayList<>(100);
 
         while (true) {
             input = scanner.nextLine();
@@ -24,48 +25,76 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 scanner.close();
                 break;
-
-            } else if (input.equals("list")) {
-                StringBuilder outputList = new StringBuilder();
-                outputList.append("Here are the tasks in your list:\n");
-                for (int i = 1; i <= savedInputs.size(); i++) {
-                    outputList.append(i + "." + savedInputs.get(i-1).toString() + "\n");
-                }
-                System.out.println(outputList.toString());
-
-            } else if (input.startsWith("done")) {
-                int pos = Integer.valueOf(input.split(" ")[1]);
-                if (pos > 0 && pos <= savedInputs.size()) {
-                    savedInputs.get(pos - 1).markAsDone();
-                } else {
-                    System.out.println("Invalid task number to mark as done");
-                }
-
-            } else if (input.startsWith("todo")) {
-                Todo todo = new Todo(input.substring(5));
-                System.out.println("Got it. I've added this task:\n  " + todo.toString());
-                savedInputs.add(todo);
-                System.out.println("Now you have " + savedInputs.size() + " tasks in the list.");
-
-            } else if (input.startsWith("deadline")) {
-                String by = input.split(" /by ")[1];
-                String description = input.split(" /by ")[0].substring(9);
-
-                Deadline deadline = new Deadline(description, by);
-                System.out.println("Got it. I've added this task:\n  " + deadline.toString());
-                savedInputs.add(deadline);
-                System.out.println("Now you have " + savedInputs.size() + " tasks in the list.");
-
-            } else if (input.startsWith("event")) {
-                String at = input.split(" /at ")[1];
-                String description = input.split(" /at ")[0].substring(6);
-
-                Event event = new Event(description, at);
-                System.out.println("Got it. I've added this task:\n  " + event.toString());
-                savedInputs.add(event);
-                System.out.println("Now you have " + savedInputs.size() + " tasks in the list.");
-            } else {
             }
+
+            int caseId = findCase(input);
+
+            switch(caseId) {
+                case 1:
+                    StringBuilder outputList = new StringBuilder();
+                    outputList.append("Here are the tasks in your list:\n");
+                    for (int i = 1; i <= savedInputs.size(); i++) {
+                        outputList.append(i + "." + savedInputs.get(i - 1).toString() + "\n");
+                    }
+                    System.out.println(outputList.toString());
+                    break;
+                case 2:
+                    int pos = Integer.valueOf(input.split(" ")[1]);
+                    savedInputs.get(pos - 1).markAsDone();
+                    break;
+
+                case 3:
+                    Todo todo = new Todo(input.substring(5));
+                    System.out.println("Got it. I've added this task:\n  " + todo.toString());
+                    savedInputs.add(todo);
+                    System.out.println("Now you have " + savedInputs.size() + " tasks in the list.");
+                    break;
+
+                case 4:
+                    String by = input.split(" /by ")[1];
+                    String deadlineDescription = input.split(" /by ")[0].substring(9);
+
+                    Deadline deadline = new Deadline(deadlineDescription, by);
+                    System.out.println("Got it. I've added this task:\n  " + deadline.toString());
+                    savedInputs.add(deadline);
+                    System.out.println("Now you have " + savedInputs.size() + " tasks in the list.");
+                    break;
+
+                case 5:
+                    String at = input.split(" /at ")[1];
+                    String eventDescription = input.split(" /at ")[0].substring(6);
+
+                    Event event = new Event(eventDescription, at);
+                    System.out.println("Got it. I've added this task:\n  " + event.toString());
+                    savedInputs.add(event);
+                    System.out.println("Now you have " + savedInputs.size() + " tasks in the list.");
+                    break;
+
+                default:
+
+            }
+        }
+    }
+
+    /**
+     * Returns the identifier of each case (for switch in chat method).
+     *
+     * @param input User entered into Command Line
+     * @return caseId
+     */
+    private static int findCase(String input) {
+        if (input.equals("list")) {
+            return 1;
+        } else if (input.startsWith("done ")) {
+            return 2;
+        } else if (input.startsWith("todo ")) {
+            return 3;
+        } else if (input.startsWith("deadline ")) {
+            return 4;
+        } else if (input.startsWith("event ")) {
+            return 5;
+        } else {
+            return 6;
         }
     }
 }
