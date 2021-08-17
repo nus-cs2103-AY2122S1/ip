96d3c43a3f00bd6ce1f39cbff2c57e344be6831c
Tanishq4331 <tanishq43311@gmail.com>
2021-08-17 12:57:41 +0800

    Complete Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,22 +3,39 @@ import java.util.Scanner;
 
 public class Duke {
 
-        public static void main(String[] args) {
-            String logo = " ____        _        \n"
-                    + "|  _ \\ _   _| | _____ \n"
-                    + "| | | | | | | |/ / _ \\\n"
-                    + "| |_| | |_| |   <  __/\n"
-                    + "|____/ \\__,_|_|\\_\\___|\n";
-            System.out.println("Hello from\n" + logo);
-            Scanner scanner = new Scanner(System.in);
-            String toRepeat = scanner.nextLine();
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        String toAdd = scanner.nextLine();
 
-            while (!toRepeat.equals("bye")) {
-                System.out.println("\t" + toRepeat + "\n");
-                toRepeat = scanner.nextLine();
+        String[] tasks = new String[100];
+        int index = 0;
+
+        while (!toAdd.equals("bye")) {
+            if (toAdd.equals("list")) {
+                displayTasks(tasks);
+            } else {
+                tasks[index] = toAdd;
+                System.out.println("\t added: " + toAdd + "\n");
+                index++;
             }
-
-            scanner.close();
+            toAdd = scanner.nextLine();
         }
+
+        scanner.close();
+    }
+
+    private static void displayTasks(String[] tasks) {
+        for(int i = 0; i < tasks.length && tasks[i] != null; i++) {
+            int index = i + 1;
+            System.out.println("\t " + index + ". " + tasks[i]);
+        }
+        System.out.println();
+    }
 
 }
