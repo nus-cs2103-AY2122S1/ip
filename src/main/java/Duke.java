5054c27380ba415ec451501535c44577e5d5e3de
Richard Li Defa <richardlidefa@gmail.com>
2021-08-17 17:39:53 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,59 @@ import java.util.Scanner;
 
 public class Duke {
     private static int listSize = 0;
-    private static String[] list = new String[100]; // List to store all the tasks
+    private static Task[] list = new Task[100]; // List to store all the tasks
+
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public void complete() {
+            this.isDone = true;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + this.getStatusIcon() + "] " + description;
+        }
+    }
+
+    public static void handleInput(String input) {
+        if (input.equals("list")) {
+            for (int i = 0; i < listSize; i++) {
+                System.out.println((i + 1) + "." + list[i]);
+            }
+        } else if (input.startsWith("done ")) {
+            try {
+                completeTask(input);
+            } catch (NumberFormatException e) {
+                addTask(input);
+            }
+        } else {
+            addTask(input);
+        }
+    }
+
+    public static void addTask(String desc) {
+        list[listSize] = new Task(desc);
+        listSize++;
+        System.out.println("added: " + desc);
+    }
+
+    public static void completeTask(String input) {
+        int completedTaskNumber = Integer.parseInt(input.substring(5)) - 1;
+        list[completedTaskNumber].complete();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(list[completedTaskNumber]);
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -18,18 +70,6 @@ public class Duke {
                 break;
             }
             handleInput(input);
-        }
-    }
-
-    private static void handleInput(String input) {
-        if (input.equals("list")) {
-            for (int i = 0; i < listSize; i++) {
-                System.out.println((i + 1) + ". " + list[i]);
-            }
-        } else {
-            list[listSize] = input;
-            listSize++;
-            System.out.println("added: " + input);
         }
     }
 }
