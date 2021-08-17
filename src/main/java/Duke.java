0a4ba7234e14885ac9e4d06e17ed56bd55f08f06
nzixuan <nzixuan@gmail.com>
2021-08-17 17:47:13 +0800

    Added task object and ability to mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Duke {
-    String[] list = new String[100];
-    int index = 0;
+    ArrayList<Task> list = new ArrayList<>();
     static final String divider = "\t____________________________________________________________\n";
 
     Duke() {
@@ -16,31 +16,52 @@ public class Duke {
     }
 
     public boolean process(String str) {
+        Scanner scanner = new Scanner(str);
+        String firstWord;
+        if (!scanner.hasNext()) {
+            //No input given
+            return true;
+        }
+        firstWord = scanner.next();
         if (str.equals("bye")) {
             System.out.println(divider + "\tBye. Hope to see you again soon!\n" + divider);
             return false;
         } else if (str.equals("list")) {
             displayList();
-            return true;
-        } else {
-            addList((str));
-            return true;
+        } else if (firstWord.equals("done")) {
+            if(!scanner.hasNext()){
+                System.out.println(divider + "\tTask number should be given.\n" + divider);
+                return true;
+            }
+            //Exceptions
+            int i = Integer.parseInt(scanner.next());
+            if (i > 0 && i <=list.size()) {
+                list.get(i-1).markDone();
+            }
+            else {
+                System.out.println(divider + "\tNo such task found in list.\n" + divider);
+            }
         }
+        else {
+            addList((str));
+        }
+        return true;
     }
 
     private void addList(String str) {
-        list[index++] = str;
-        System.out.println(divider +"\tadded: "+ str+"\n" + divider);
+        Task t = new Task(str);
+        list.add(t);
+        System.out.println(divider +"\tadded: "+ t+"\n" + divider);
     }
 
     private void displayList() {
-        if (index == 0) {
+        if (list.size() == 0) {
             System.out.println(divider + "\tlist empty\n" + divider);
             return;
         }
         System.out.print(divider);
-        for (int i = 0; i <index; i++) {
-            System.out.println("\t"+(i+1) + ". " + list[i]);
+        for (int i = 0; i <list.size(); i++) {
+            System.out.println("\t"+(i+1) + ". " + list.get(i));
         }
         System.out.println(divider);
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+    Boolean done;
+    String description;
+    public Task(String str) {
+        done = false;
+        description = str;
+    }
+
+    public void markDone() {
+        done = true;
+        System.out.println("\tNice! I've marked this task as done: ");
+        System.out.println("\t" +this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "["+ (done?'X':' ') + "] " + description;
+
+    }
+}
