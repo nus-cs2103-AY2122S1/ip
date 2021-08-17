b66dcec69cd46eaf2a58a92c8056a00714e27a6e
Zenlzb <zen@Lees-MacBook-Pro-2.local>
2021-08-17 21:48:37 +0800

    Add List and Add to List Functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,30 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Duke {
+    public static List<String> list = new ArrayList<>();
+    public static String formatMessage(String s) {
+        return "    ____________________________________________________________\n     " +
+                s + "\n" +
+                "    ____________________________________________________________";
+    }
+    public static void addToList(String s) {
+        list.add(s);
+    }
+    public static String printList() {
+        String res = "";
+        for (int counter = 1; counter<=list.size(); counter++) {
+            res = res + counter + ". " + list.get(counter-1);
+            if (counter != list.size()) {
+                res = res + "\n     ";
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(
-                "    ____________________________________________________________\n" +
-                "     Hello! I'm Duke\n" +
-                "     What can I do for you?\n" +
-                "    ____________________________________________________________"
-        );
+        System.out.println(formatMessage("Hello! I'm Duke\n" + "     What can I do for you?"));
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
             }
-            System.out.println(
-                    "    ____________________________________________________________\n     " +
-                    input + "\n" +
-                    "    ____________________________________________________________"
-            );
+            if (input.equals("list")) {
+                System.out.println(formatMessage(printList()));
+                continue;
+            }
+            addToList(input);
+            System.out.println(formatMessage("added: " + input));
         }
-        System.out.println(
-                "    ____________________________________________________________\n" +
-                "     Bye. Hope to see you again soon!\n" +
-                "    ____________________________________________________________"
-        );
-
-
+        System.out.println(formatMessage("Bye. Hope to see you again soon!"));
     }
 }
