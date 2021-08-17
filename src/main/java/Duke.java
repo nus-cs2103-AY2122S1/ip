0d3d52dffa5d2cedddac70516587a42c3dc57ae0
HongshuW <hongshu.wang.uni@gmail.com>
2021-08-17 09:34:32 +0800

    Duke class: change greeting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 public class Duke {
+    private static final String GREETING_MESSAGE = "____________________________________________________________\n" +
+            "Hello! I'm Duke\n" +
+            "What can I do for you?\n" +
+            "____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(Duke.GREETING_MESSAGE);
     }
 }
