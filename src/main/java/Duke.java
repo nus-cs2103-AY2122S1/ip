1b940e827c7dc62bc9ed56d596adaf20a496682f
Ong Zheng Lin <ongzhenglin1999@gmail.com>
2021-08-17 21:59:03 +0800

    Level-1, echoing input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine(); // scan the line for the user's input
+        while (true) {//create loop for the chat
+            if (input.equals("bye") || input.equals("Bye") || input.equals("BYE")) {//input is bye
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else { //input is not bye so echo input
+                System.out.println(input);
+                input = sc.nextLine();
+            }
+
+        }
+
     }
 }
