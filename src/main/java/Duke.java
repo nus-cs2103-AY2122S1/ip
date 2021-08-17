c90b981427c5d1f731fc7ff70e13741b87f56558
tchiong <57283702+tchiong@users.noreply.github.com>
2021-08-17 16:50:44 +0800

    Level-2
    Added Ability to store tasks into List and retrieve the List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,42 +1,87 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+
+    //List of important keywords
+    enum Keywords {
+        list,
+        bye,
+        add,
+    }
+
+    //Logo
     static String logo = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
-    static String divider = "____________________________________________________________";
 
-    static void greeting() {
+    //Prints a divider
+    static void printDivider() {
+        String divider = "____________________________________________________________";
+        System.out.println(divider);
+    }
+
+    //Initial Greeting from Duke
+    static void greetings() {
         System.out.println(logo);
-        System.out.println(divider);
+        printDivider();
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
-        System.out.println(divider);
+        printDivider();
     }
 
+    //Goodbye from Duke
     static void close() {
-        System.out.println(divider);
+        printDivider();
         System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println(divider);
+        printDivider();
     }
+
+    //Prints list of task
+    //Input: List
+    static void printList(List lst) {
+        for(int i = 0; i < lst.size(); i++) {
+            System.out.println((i+1) + ": " + lst.get(i));
+        }
+        printDivider();
+    }
+
 
     public static void main(String[] args) {
-        greeting();
+        List<String> list = new ArrayList<String>();
+        greetings();
         Scanner userInput = new Scanner(System.in);
 
+        //Waits for userInput
         while (true) {
-
             String input = userInput.nextLine();
+            Keywords key = Keywords.add;
+
+            //Looks for keywords
             if (input.equals("bye")) {
-                break;
-            } else {
-                System.out.println(divider);
-                System.out.println(input);
-                System.out.println(divider);
+                key = Keywords.bye;
+            } else if (input.equals("list")) {
+                key = Keywords.list;
+            }
+
+            //Performs the Appropriate Action
+            switch (key) {
+                case bye:
+                    close();
+                    System.exit(0);
+                    break;
+                case list:
+                    printList(list);
+                    break;
+                case add:
+                    list.add(input);
+                    printDivider();
+                    System.out.println("added: " + input);
+                    printDivider();
+                    break;
             }
         }
-
-        close();
     }
 }
