7737f874d7739034d4fe62f4641246dbed6ad81d
softmagnet <zhiwei3141@gmail.com>
2021-08-17 15:00:57 +0800

    Level completion: complete level 5
    Completed level 5.

--- src/main/java/Yoyo.java
+++ src/main/java/Yoyo.java
@@ -4,7 +4,6 @@ public class Yoyo {
     private static Task[] tasks = new Task[100];
     private static int numTasks = 0;
 
-
     private enum TaskType {
         TODO,
         EVENT,
@@ -14,14 +13,16 @@ public class Yoyo {
     /**
      * Prints a decoration line for output.
      */
-    private static void printLineDecoration() {
-        System.out.println("========================================================================");
+    private static void outputWrapper() {
+        System.out.println("============================================================");
     }
 
     public static void main(String[] args) {
-        String greetings = "Hello! I'm Yoyo\n"
-                + "What can I do for you?\n";
+        String greetings = "Hello! I'm Yoyo.\n"
+                + "What can I do for you?";
+        outputWrapper();
         System.out.println(greetings);
+        outputWrapper();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -29,27 +30,32 @@ public class Yoyo {
             String[] inputWords = input.split(" ", 2);
             String command = inputWords[0];
             if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!\n");
+                outputWrapper();
+                System.out.println("Bye. Hope to see you again soon!");
+                outputWrapper();
                 break;
             } else if (command.equals("list")) {
-                printLineDecoration();
-                for (int i = 0; i < numTasks; i++) {
-                    System.out.println(i + 1 + "." + tasks[i].showStatus());
+                outputWrapper();
+                if (numTasks == 0) {
+                    System.out.println("You have no task at the moment.");
+                } else {
+                    for (int i = 0; i < numTasks; i++) {
+                        System.out.println(i + 1 + "." + tasks[i].showStatus());
+                    }
                 }
-                printLineDecoration();
-                System.out.println("\n");
+                outputWrapper();
             } else if (command.equals("done")) {
                 try {
                     int taskIndex = Integer.parseInt(inputWords[1]) - 1;
                     tasks[taskIndex].toggleDone();
-                    printLineDecoration();
+                    outputWrapper();
                     System.out.println("Nice! I've marked this task as done:\n"
                             + tasks[taskIndex].showStatus());
-                    printLineDecoration();
-                    System.out.println();
-
+                    outputWrapper();
                 } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                    outputWrapper();
                     System.out.println("Please enter a valid index!\n");
+                    outputWrapper();
                 }
             } else {
                 if (command.equals("todo")) {
@@ -60,7 +66,9 @@ public class Yoyo {
                 } else if (command.equals("event")) {
                     String[] taskInfo = inputWords[1].split(" /");
                     if (taskInfo.length < 2) {
-                        System.out.println("Please add a timing for the event!\n");
+                        outputWrapper();
+                        System.out.println("Please add a timing for the event!");
+                        outputWrapper();
                     } else {
                         Task newTask = new Event(taskInfo[0], taskInfo[1]);
                         tasks[numTasks] = newTask;
@@ -70,7 +78,9 @@ public class Yoyo {
                 } else if (command.equals("deadline")) {
                     String[] taskInfo = inputWords[1].split(" /");
                     if (taskInfo.length < 2 ) {
-                        System.out.println("Please add a timing for the event!\n");
+                        outputWrapper();
+                        System.out.println("Please add a timing for the event!");
+                        outputWrapper();
                     } else {
                         Task newTask = new Deadline(taskInfo[0], taskInfo[1]);
                         tasks[numTasks] = newTask;
@@ -78,7 +88,9 @@ public class Yoyo {
                         printAddMessage(newTask);
                     }
                 } else {
-                    System.out.println("Invalid Command\n");
+                    outputWrapper();
+                    System.out.println("Invalid Command");
+                    outputWrapper();
                 }
             }
         }
@@ -90,13 +102,12 @@ public class Yoyo {
      * @param newTask The task that has been created.
      */
     private static void printAddMessage(Task newTask) {
-        printLineDecoration();
+        outputWrapper();
         System.out.print("Got it. I've added this task:\n   "
                 + newTask.showStatus()
                 + "\nNow you have "
                 + numTasks
                 + " tasks in the list.\n");
-        printLineDecoration();
-        System.out.println();
+        outputWrapper();
     }
 }
