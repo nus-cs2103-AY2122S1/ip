1d7d050d3a5fc3926e66e78f0aab280fbdd11cc2
LiuZhilan <e0559386@u.nus.edu>
2021-08-17 15:40:17 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static String[] tasksList = new String[100];
+
+    public static void addTask(String task, int index) {
+        tasksList[index] = task;
+        System.out.println("added: " + task);
+    }
+
+    public static void listTask() {
+        for (int i = 0; i < tasksList.length; i++) {
+            if (tasksList[i] == null) break;
+            System.out.println(i + ". " +tasksList[i]);
+        }
+    }
     public static void main(String[] args) {
         // String logo = " ____        _        \n"
         //         + "|  _ \\ _   _| | _____ \n"
@@ -9,7 +22,7 @@ public class Duke {
         //         + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from Neko!\nWhat can I do for you?\n(Nothing actually cos I will just repeat what you say!)\n");
         Scanner sc = new Scanner(System.in);  
-        String[] tasksList = new String[100];
+        
         int index = 0;
         
         while (sc.hasNext()) {
@@ -19,13 +32,9 @@ public class Duke {
                 break;
             }
             if (userInput.equals("list")) {
-                for (int i = 0; i < tasksList.length; i++) {
-                    if (tasksList[i] == null) break;
-                    System.out.println(i + ". " +tasksList[i]);
-                }
+                Duke.listTask();
             } else {
-                tasksList[index] = userInput;
-                System.out.println("added: " + userInput);
+                Duke.addTask(userInput, index);
                 index++;
             }
             
