d06475a563769d4f6c506c01243260cccdb72443
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2021-08-17 17:49:36 +0800

    Use the input/output redirection technique to semi-automate the testing of Duke.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 /**
  * This class represents the chat bot, Duke.
  */
@@ -99,10 +98,10 @@ public class Duke {
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from \n" + logo);
         System.out.println("What can I do for you?");
         Duke.reply();
