8d4fd4d0d26c4e94bf67ba41b0823d6aa8421446
Mohamed-Noriman <seaweed.iman@gmail.com>
2021-08-17 16:12:46 +0800

    Finished Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,136 +10,181 @@ public class Duke {
 
         String command = scanner.nextLine();
         while (!command.equals("bye")) {
-            if(command.equals("list")) {
+            if (command.equals("list")) {
 
                 System.out.println(line);
-
-                for(int i = 0; i < list.size(); i++) {
+                for (int i = 0; i < list.size(); i++) {
                     Task task = list.get(i);
-                    System.out.println((i+1) + ". " + " " + task.toString());
+                    System.out.println((i + 1) + ". " + " " + task.toString());
                 }
                 System.out.println(line);
-
-                command = scanner.nextLine();
-            } else if (command.startsWith("done ")) {
-
-                System.out.println(line);
-
-                String[] splitup = command.split(" ");
-                int index = Integer.parseInt(splitup[1]);
-                list.get(index - 1).markAsDone();
-                System.out.println("Very well, Master Wayne. This task has been marked as per your request.");
-                System.out.println((index) + ". " + list.get(index - 1)); //actual index is index - 1
-
-                System.out.println(line);
-
-                command = scanner.nextLine();
-
-            } else if (command.startsWith("todo ")) {
-
-                String[] splitup = command.split(" ");
-                String desc = "";
-                for (int i = 1; i < splitup.length; i++) {
-                    if (i == 1) {
-                        desc = splitup[1];
-                    } else {
-                        desc = desc + " " + splitup[i];
-                    }
-                }
-                Todo todo = new Todo(desc);
-                list.add(todo);
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(todo.toString());
-                if (list.size() == 1) {
-                    System.out.println("Now you have 1 task in the list.");
-                } else {
-                    System.out.println("Now you have " + list.size() +" tasks in the list.");
-                }
-
-                command = scanner.nextLine();
-
-            } else if (command.startsWith("deadline ")) {
-
-                String[] splitup = command.split(" ");
-                String desc = "";
-                String by = "";
-                boolean byfound = false;
-                for (int i = 1; i < splitup.length; i++) {
-                    if (byfound) {
-                        if (by == "") {
-                            by = splitup[i];
-                        } else {
-                            by = by + " " + splitup[i];
-                        }
-                    } else {
-                        if (i == 1) {
-                            desc = splitup[1];
-                        } else if (splitup[i].equals("/by")) {
-                            byfound = true;
-                        } else {
-                            desc = desc + " " + splitup[i];
-                        }
-                    }
-                }
-
-                Deadline deadline = new Deadline(desc, by);
-                list.add(deadline);
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(deadline.toString());
-                if (list.size() == 1) {
-                    System.out.println("Now you have 1 task in the list.");
-                } else {
-                    System.out.println("Now you have " + list.size() +" tasks in the list.");
-                }
-
-                command = scanner.nextLine();
-
-            } else if (command.startsWith("event ")) {
-
-                String[] splitup = command.split(" ");
-                String desc = "";
-                String at = "";
-                boolean atFound = false;
-                for (int i = 1; i < splitup.length; i++) {
-                    if (atFound) {
-                        if (at == "") {
-                            at = splitup[i];
-                        } else {
-                            at = at + " " + splitup[i];
-                        }
-                    } else {
-                        if (i == 1) {
-                            desc = splitup[1];
-                        } else if (splitup[i].equals("/at")) {
-                            atFound = true;
-                        } else {
-                            desc = desc + " " + splitup[i];
-                        }
-                    }
-                }
-
-                Event event = new Event(desc, at);
-                list.add(event);
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(event.toString());
-                if (list.size() == 1) {
-                    System.out.println("Now you have 1 task in the list.");
-                } else {
-                    System.out.println("Now you have " + list.size() +" tasks in the list.");
-                }
 
                 command = scanner.nextLine();
 
             } else {
-                System.out.println(line + "\n" + "Understood, Master Wayne. Added: " + command + "\n" + line);
-                Task t = new Task(command);
-                list.add(t);
-                command = scanner.nextLine();
+                String[] split = command.split(" ");
+                String desc = "";
+
+                if (split[0].equals("done")) {
+
+                    try {
+                        checkLength(split.length);
+                        int index = Integer.parseInt(split[1]);
+                        checkIndex(index, list.size());
+
+                        System.out.println(line);
+                        list.get(index - 1).markAsDone();
+                        System.out.println("Very well, Master Wayne. This task has been marked as per your request.");
+                        System.out.println((index) + ". " + list.get(index - 1)); //actual index is index - 1
+                        System.out.println(line);
+                    } catch (DukeException e) {
+                        System.out.println("***WARNING*** An error has occured Master Wayne: " + e.getMessage());
+                    }
+
+                    command = scanner.nextLine();
+
+                } else if (split[0].equals("todo")) {
+
+                    for (int i = 1; i < split.length; i++) {
+                        if (i == 1) {
+                            desc = split[1];
+                        } else {
+                            desc = desc + " " + split[i];
+                        }
+                    }
+
+                    try {
+                        checkDesc(desc);
+                        Todo todo = new Todo(desc);
+                        list.add(todo);
+
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(todo.toString());
+
+                        if (list.size() == 1) {
+                            System.out.println("Now you have 1 task in the list.");
+                        } else {
+                            System.out.println("Now you have " + list.size() + " tasks in the list.");
+                        }
+                    } catch (DukeException e) {
+                        System.out.println("***WARNING*** An error has occured Master Wayne: " + e.getMessage());
+                    }
+
+                    command = scanner.nextLine();
+
+                } else if (split[0].equals("deadline")) {
+
+                    String by = "";
+                    boolean byfound = false;
+
+                    for (int i = 1; i < split.length; i++) {
+                        if (byfound) {
+                            if (by == "") {
+                                by = split[i];
+                            } else {
+                                by = by + " " + split[i];
+                            }
+                        } else {
+                            if (i == 1) {
+                                desc = split[1];
+                            } else if (split[i].equals("/by")) {
+                                byfound = true;
+                            } else {
+                                desc = desc + " " + split[i];
+                            }
+                        }
+                    }
+
+                    try {
+                        checkDesc(desc);
+                        Deadline deadline = new Deadline(desc, by);
+                        list.add(deadline);
+
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(deadline.toString());
+                        if (list.size() == 1) {
+                            System.out.println("Now you have 1 task in the list.");
+                        } else {
+                            System.out.println("Now you have " + list.size() + " tasks in the list.");
+                        }
+
+                    } catch (DukeException e) {
+                        System.out.println("***WARNING*** An error has occured Master Wayne: " + e.getMessage());
+                    }
+
+                    command = scanner.nextLine();
+
+                } else if (split[0].equals("event")) {
+
+                    String at = "";
+                    boolean atFound = false;
+
+                    for (int i = 1; i < split.length; i++) {
+                        if (atFound) {
+                            if (at == "") {
+                                at = split[i];
+                            } else {
+                                at = at + " " + split[i];
+                            }
+                        } else {
+                            if (i == 1) {
+                                desc = split[1];
+                            } else if (split[i].equals("/at")) {
+                                atFound = true;
+                            } else {
+                                desc = desc + " " + split[i];
+                            }
+                        }
+                    }
+
+                    try {
+                        checkDesc(desc);
+                        Event event = new Event(desc, at);
+                        list.add(event);
+
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(event.toString());
+
+                        if (list.size() == 1) {
+                            System.out.println("Now you have 1 task in the list.");
+                        } else {
+                            System.out.println("Now you have " + list.size() + " tasks in the list.");
+                        }
+                    } catch (DukeException e) {
+                        System.out.println("***WARNING*** An error has occured Master Wayne: " + e.getMessage());
+                    }
+
+                    command = scanner.nextLine();
+
+                } else {
+                    System.out.println("*** Apologies, Master Wayne. But I don't know what that means ***");
+                    command = scanner.nextLine();
+                }
             }
-        };
+            ;
+        }
         System.out.println("Have a pleasant day, Master Wayne.\n");
+
+
+    }
+
+    public static void checkDesc(String test) throws DukeException {
+        if (test.equals("")) {
+            throw new DukeException("The description is empty");
+        }
+    }
+
+    public static void checkLength(int l) throws DukeException {
+        if (l == 1) {
+            throw new DukeException("Please give an index number");
+        }
+    }
+
+    public static void checkIndex(int i, int lengthOfList) throws DukeException {
+        if (i <= 0) {
+            throw new DukeException("Please give an index number > 0");
+        } else if (i > lengthOfList) {
+            throw new DukeException("Maximum index number is " + lengthOfList);
+        }
     }
 }
