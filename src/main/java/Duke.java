31062accfa1265e5eb4685ca036350005503c2bc
skythefire <minganntan@gmail.com>
2021-08-17 15:20:41 +0800

    Handle some errors in Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,56 +28,55 @@ public class Duke {
         System.out.println(frog + greeting + "How may I help you?\n" + LINE);
     }
 
-    public void echo() {
-        Scanner scanner = new Scanner(System.in);
-
-        while (scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-
-            if (input.equals("bye")) {
-                System.out.println("See you again in my frog hole! RIBBIT!");
-                scanner.close();
-                return;
-            } else if (input.startsWith("done")) {
-                int index = input.charAt(5) - 49;
-                taskList[index].markAsDone();
-                System.out.println("You have swallowed that pesky fly! RIBBIT!");
-                System.out.println("  " + taskList[index].toString());
-                System.out.println(LINE);
-            } else if (input.equals("list")) {
-                System.out.println("Here is your menu for today:");
-                for (int i = 0; i < size; i++) {
-                    System.out.println(i + 1 + "." + taskList[i].toString());
-                }
-                System.out.println(LINE);
-            } else {
-                if (input.startsWith("todo")) {
-                    String name = input.substring(5);
-                    taskList[size] = new ToDo(name);
-                } else if (input.startsWith("deadline")) {
-                    int index = input.indexOf("/by");
-                    String name = input.substring(9, index);
-                    String by = input.substring(index + 4);
-                    taskList[size] = new Deadline(name, by);
-                } else { // is an Event
-                    int index = input.indexOf("/at");
-                    String name = input.substring(6, index);
-                    String at = input.substring(index + 4);
-                    taskList[size] = new Event(name, at);
-                }
-
-                System.out.println("A fly has been added to the menu:");
-                System.out.println("  " + taskList[size].toString());
-                size++;
-                System.out.println("Now you have " + size + " flies to eat! RIBBIT!");
-                System.out.println(LINE);
+    public void echo(String input) {
+        if (input.equals("bye")) {
+            System.out.println("See you again in my frog hole! RIBBIT!");
+            System.exit(0);
+        } else if (input.startsWith("done")) {
+            int index = input.charAt(5) - 49;
+            taskList[index].markAsDone();
+            System.out.println("You have swallowed that pesky fly! RIBBIT!");
+            System.out.println("  " + taskList[index].toString());
+        } else if (input.equals("list")) {
+            System.out.println("Here is your menu for today:");
+            for (int i = 0; i < size; i++) {
+                System.out.println(i + 1 + "." + taskList[i].toString());
             }
+        } else {
+            if (input.startsWith("todo")) {
+                taskList[size] = new ToDo(input.substring(5));
+            } else if (input.startsWith("deadline")) {
+                int index = input.indexOf("/by");
+                taskList[size] = new Deadline(input.substring(9, index),
+                        input.substring(index + 4));
+            } else { // is an Event
+                int index = input.indexOf("/at");
+                taskList[size] = new Event(input.substring(6, index),
+                        input.substring(index + 4));
+            }
+
+            System.out.println("A fly has been added to the menu:");
+            System.out.println("  " + taskList[size].toString());
+            size++;
+            System.out.println("Now you have " + size + " flies to eat! RIBBIT!");
         }
     }
 
     public static void main(String[] args) {
         Duke jo = new Duke();
         jo.greeting();
-        jo.echo();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            try {
+                DukeException.checkInput(input);
+                jo.echo(input);
+            } catch (DukeException e) {
+                System.out.println("ERROR: " + e.getMessage());
+            } finally {
+                System.out.println(LINE);
+            }
+        }
+        scanner.close();
     }
 }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,17 @@
+public class DukeException extends Exception {
+
+    public DukeException(String str) {
+        super(str);
+    }
+
+    public static void checkInput(String str) throws DukeException {
+        if (str.startsWith("todo") || str.startsWith("deadline") || str.startsWith("event")
+            || str.startsWith("list") || str.startsWith("bye") || str.startsWith("done")) {
+            if (str.equals("todo") || str.equals("deadline") || str.equals("event")) {
+                throw new DukeException("Jo needs to know what type of fly it is! :(");
+            }
+        } else {
+            throw new DukeException("Jo does not understand not-frog speak! :(");
+        }
+    }
+}
