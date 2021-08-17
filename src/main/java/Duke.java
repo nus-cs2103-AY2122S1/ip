c4fcb85450418a9d0cf00cae3f81ee0e3a3a7ce9
Galvin <e0543848@u.nus.edu>
2021-08-17 19:50:54 +0800

    Add ability to store and display text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,26 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
+        ArrayList<String> storage = new ArrayList<>();
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         String input = Sc.nextLine();
-        while(!input.equals("bye")){
-            System.out.println(input);
+
+        while(true){
+            if(input.equals("bye")){break;}
+
+            switch (input){
+                case "list":
+                    for (int i = 0; i < storage.size(); i++) {
+                        System.out.println(storage.get(i));
+                    }
+                    break;
+                default:
+                    storage.add(input);
+                    System.out.println("added: " + input);
+
+            }
             input = Sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
