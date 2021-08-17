a3bada72ae23fdb3f0f64f705972410184178b80
Hang Zelin <69517383+HangZelin@users.noreply.github.com>
2021-08-17 19:57:35 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,51 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+
+    static String line = "____________________________________________________________\n";
+
+    public static void HelloMessage() {
+        String Hello_message = "Hello! I'm Duke\n" +
+                                "What can I do for you?\n";
+
+        System.out.println(line + Hello_message + line);
+    }
+
+
+    public static void PrintMessage(){
+        Scanner scanner = new Scanner(System.in);
+        String Echo = "";
+        String Goodbye_message = "Bye. Hope to see you again soon!\n";
+
+        //Use loop to determine if a user enters "bye" or not.
+        while (true) {
+            Echo = scanner.nextLine();
+            if (Echo.equals("bye")) {
+                System.out.println(line + Goodbye_message + line);
+                break;
+            }
+            else {
+                System.out.println(line + Echo + "\n" + line);
+            }
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String Hello_message = "Hello! I'm Duke\n" +
-                "What can I do for you?\n";
-        String line = "____________________________________________________________\n";
-        String Echo = "";
-        String Goodbye_message = "Bye. Hope to see you again soon!\n";
 
-        System.out.println(line+ Hello_message + line);
+        //Print Hello.
+        HelloMessage();
 
-        //Use loop to determine if a user enters "bye" or not.
-        while (true) {
-            Echo = scanner.nextLine();
-            if (!Echo.equals("bye")) {
-                System.out.println(line + Echo + "\n" + line);
-            } else {
-                System.out.println(line + Goodbye_message + line);
-                break;
-            }
-        }
+        //Echo Message();
+        PrintMessage();
+
     }
 }
