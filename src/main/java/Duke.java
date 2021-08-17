113c27e91e56d6e05e14cb3b374a3e0f9e61e81a
Clive <e0531337@u.nus.edu>
2021-08-18 01:59:12 +0800

    Completed level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,41 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static void echo(String line) {
-        System.out.println(line);
-    }
+    static String[] listOfText = new String[100];
 
     private static void bye() {
         System.out.println("Bye. Hope to see you again soon!");
     }
+
+    private static void list(String line) {
+        for(int i = 0; i < 100; i++) {
+            if(listOfText[i] == null) {
+                listOfText[i] = line;
+                break;
+            }
+        }
+        System.out.println("added: " + line);
+    }
+
+    private static void printList() {
+        for(int i = 0; i < 100; i++) {
+            if(listOfText[i] != null) {
+                System.out.println(i+1 + ". " + listOfText[i]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         String input = sc.nextLine();
         while(!(input.equals("bye"))) {
-            echo(input);
+            list(input);
             input = sc.nextLine();
+
+            if(input.equals("list")) {
+                printList();
+            }
         }
 
         if(input.equals("bye")) {
