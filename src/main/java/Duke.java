8b0fe5c465d499fb0669082bb2114aecce4d56ed
Ryan Chung <timelord0830@gmail.com>
2021-08-18 04:43:02 +0800

    Add the task class with ability to mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ public class Duke {
     private final static String GREETING_MSG = "Hello! I'm Duke\nWhat can I do for you?";
     private final static String GOODBYE_MSG = "Bye. Hope to see you again soon!";
     private final static Scanner SCANNER = new Scanner(System.in);
-    private static String[] tasks = new String[100];
+    private final static Task[] tasks = new Task[100];
     private static int numTasks = 0;
 
     private static void greet() {
@@ -15,25 +15,35 @@ public class Duke {
         System.out.println(GOODBYE_MSG);
     }
 
-    private static void addTask(String task) {
-        tasks[numTasks++] = task;
-        System.out.println(String.format("added: %s", task));
+    private static void addTask(String description) {
+        final Task taskToAdd = new Task(description);
+        tasks[numTasks++] = taskToAdd;
+        System.out.println(String.format("added: %s", taskToAdd.getDescription()));
     }
 
     private static void listTasks() {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; tasks[i] != null; i++) {
-            System.out.println(String.format("%d. %s", i + 1, tasks[i]));
+            System.out.println(String.format("%d.%s", i + 1, tasks[i]));
         }
+    }
+
+    private static boolean isMarkTask(String cmd) {
+        String[] cmd_split = cmd.split(" ");
+        return cmd_split.length == 2 && cmd_split[0].equals("done") && cmd_split[1].matches("[0-9]+");
     }
 
 
     public static void main(String[] args) {
         greet();
-        for (String s = SCANNER.nextLine(); !s.equals("bye"); s = SCANNER.nextLine()) {
-            if (s.equals("list")) {
+        for (String cmd = SCANNER.nextLine(); !cmd.equals("bye"); cmd = SCANNER.nextLine()) {
+            String[] cmd_split = cmd.split(" ");
+            if (isMarkTask(cmd)) {
+                tasks[Integer.parseInt(cmd_split[1]) - 1].markAsDone();
+            } else if (cmd.equals("list")) {
                 listTasks();
             } else {
-                addTask(s);
+                addTask(cmd);
             }
         }
         exit();
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    protected String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        System.out.println("Nice! I've marked this task as done:");
+        isDone = true;
+        System.out.println("\t" + this);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String toString() {
+        return String.format("[%s] %s", getStatusIcon(), description);
+    }
+}
