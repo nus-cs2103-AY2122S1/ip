9f251107fac0afa91134d9147f4b1622772cbec3
Yttruire <teozhihao99@gmail.com>
2021-08-17 22:46:19 +0800

    Added Greet, Echo and Exit commands
    - Changed the format of all messages sent to user
    - Changed greeting message
    - Added support for user input (Specifically only 3 commands)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duke {
+    /**
+     * Sends a message to the user in the specified format
+     * @param message The message to be sent to the user
+     */
+    public static void sendMessage(String message) {
+        System.out.println("    ____________________________________________________________\n" +
+                "    " + message.replace("\n", "\n    ") +
+                "\n    ____________________________________________________________");
+    }
+
+    /**
+     * Returns a String containing the input command by the user
+     * @param sc Scanner object to receive user input
+     * @return The given command. Currently, takes the entire line as a single string (command) and in lowercase
+     */
+    public static String getCommand(Scanner sc) {
+        return sc.nextLine().toLowerCase();
+    }
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        final String welcomeMessage = "Hello! I'm Duke\nWhat can I do for you?";
+        sendMessage(welcomeMessage);
+
+        String userCommand = "";
+        while (true) {
+            userCommand = getCommand(sc);
+
+            if (userCommand.equals("bye")) {
+                // EXIT command
+                sendMessage("Bye. Hope to see you again soon!");
+                System.exit(0);
+            } else {
+                // ECHO command
+                sendMessage(userCommand);
+            }
+        }
     }
 }
