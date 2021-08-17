3ce69cbfe48b167a92f80d8956d44ddf5f0998e1
Richard Li Defa <richardlidefa@gmail.com>
2021-08-17 16:55:55 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,35 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static int listSize = 0;
+    private static String[] list = new String[100]; // List to store all the tasks
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
+
+
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
+                // Exit loop and thus program when input is bye
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(input);
+            handleInput(input);
+        }
+    }
+
+    private static void handleInput(String input) {
+        if (input.equals("list")) {
+            for (int i = 0; i < listSize; i++) {
+                System.out.println((i + 1) + ". " + list[i]);
+            }
+        } else {
+            list[listSize] = input;
+            listSize++;
+            System.out.println("added: " + input);
         }
     }
 }
