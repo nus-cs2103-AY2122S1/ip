b21bc1748cce7e2f467f7c051e050233ad93c6a1
Stuart Long <chayboon@gmail.com>
2021-08-18 03:30:10 +0800

    Complete Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -34,6 +34,32 @@ public class Duke {
         }
     }
 
+    public static void printAdd(Task toAdd, int size) {
+        String message = "Got it. I've added this task:\n" + "  " + toAdd + "\nNow you have " + size + " tasks in the list.";
+        print(message);
+    }
+
+    public static String[] splitInput(String input) {
+        String[] str = input.split("/");
+        String[] first = str[0].split(" ");
+        String[] second = str[1].split(" ");
+        String description = "";
+        String deadline = "";
+        for (int i = 1; i < first.length; i++) {
+            description += first[i];
+            if (i != first.length - 1) {
+                description += " ";
+            }
+        }
+        for (int i = 1; i < second.length; i++) {
+            deadline += second[i];
+            if (i != second.length - 1) {
+                deadline += " ";
+            }
+        }
+        return new String[]{description, deadline};
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -55,8 +81,30 @@ public class Duke {
                 String message = "Nice! I've marked this task as done:\n" + "  " + tasks.get(index);
                 print(message);
             } else {
-                tasks.add(new Task(next));
-                print("added: " + next);
+                String[] splited = next.split(" ");
+                if (splited[0].equals("todo")) {
+                    String toAdd = "";
+                    for (int i = 1; i < splited.length; i++) {
+                        toAdd += splited[i];
+                        if (i != splited.length - 1) {
+                            toAdd += " ";
+                        }
+                    }
+                    ToDo add = new ToDo(toAdd);
+                    tasks.add(add);
+                    printAdd(add, tasks.size());
+                } else if (splited[0].equals("deadline")) {
+                    String[] str = splitInput(next);
+                    Deadline add = new Deadline(str[0], str[1]);
+                    tasks.add(add);
+                    printAdd(add, tasks.size());
+                } else if (splited[0].equals("event")) {
+                    String[] str = splitInput(next);
+                    Event add = new Event(str[0], str[1]);
+                    tasks.add(add);
+                    printAdd(add, tasks.size());
+                }
+
             }
             next = sc.nextLine();
 
