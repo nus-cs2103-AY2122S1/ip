1c124032a04727352a8a69fb9c674864bf933c70
YuPei <sunyupei1999@gmail.com>
2021-08-17 23:25:09 +0800

    changed if-else to switch

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -12,31 +11,39 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke. \n" + "What can I do for you?");
 
-
-
         Scanner scan = new Scanner(System.in);
-        ArrayList<String> myList = new ArrayList<>();
+        ArrayList<Task> myList = new ArrayList<>();
+        boolean tryMore = true;
 
-        while (true) {
+        while (tryMore) {
             String data = scan.nextLine();
-            if (data.equals("bye")) {
-                break;
-            } else if (data.equals("list")) {
-                System.out.println("-----------------------");
-                for (int i = 1; i <= myList.size(); i++) {
-                    System.out.println(i + ". " + myList.get(i - 1));
-                }
-                System.out.println("-----------------------");
-            } else {
-                printMsg(data);
-                myList.add(data);
+
+
+            switch (data.toLowerCase()) {
+                case "bye":
+                    tryMore = false;
+                    break;
+                case "list":
+                    for (int i = 1; i <= myList.size(); i++) {
+                        System.out.println(i + ". " + myList.get(i - 1).toString());
+                    }
+                    break;
+                case "done .*":
+                    System.out.println("Here");
+                    break;
+                default:
+                    printMsg(data);
+                    myList.add(new Task(data));
             }
 
 
+//            } else if ((data.length() >= 2) && data.substring(0, 3).equals("done")) {
+//                myList.get(data.charAt(5)).isDone = true;
+//                System.out.println(" Nice! I've marked this task as done: \n"
+//                        + myList.get(data.charAt(5)).toString());
+
         }
-
         printMsg("Bye. Hope to see you again soon!");
-
     }
 
     public static void printMsg(String msg) {
