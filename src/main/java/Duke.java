c3db722f0a5f3d6e9fa453dc145edca0eddc3d1f
Yttruire <teozhihao99@gmail.com>
2021-08-18 00:24:10 +0800

    Added Tasks and DONE command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,57 +7,48 @@ public class Duke {
      * Sends a message to the user in the specified format
      * @param message The message to be sent to the user
      */
-    public static void sendMessage(String message) {
+    protected static void sendMessage(String message) {
         System.out.println("    ____________________________________________________________\n    " +
                 message.replace("\n", "\n    ") +
                 "\n    ____________________________________________________________");
     }
 
-    /**
-     * Returns a String containing the input command by the user
-     * @param sc Scanner object to receive user input
-     * @return The given command. Currently, takes the entire line as a single string (command) wiuthout parsing
-     */
-    public static String getCommand(Scanner sc) {
-        return sc.nextLine();
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> listData = new ArrayList<>();
+        List<Task> listData = new ArrayList<>();
 
         final String welcomeMessage = "Hello! I'm Duke\nWhat can I do for you?";
         sendMessage(welcomeMessage);
 
-        String userCommand = "";
+        String userInput;
         while (true) {
-            userCommand = getCommand(sc);
+            userInput = sc.nextLine();
 
-            if (userCommand.equals("bye")) {
+            if (userInput.equals("bye")) {
                 // EXIT command
                 sendMessage("Bye. Hope to see you again soon!");
                 System.exit(0);
 
-            } else if (userCommand.equals("list")) {
+            } else if (userInput.equals("list")) {
                 // LIST command
                 // Construct the string containing the list of items that have been stored in preparation to send to user
-                StringBuilder listMessage = new StringBuilder();
+                StringBuilder listMessage = new StringBuilder("Here are the tasks in your list:");
 
-                // Add first element (if it exists)
-                if (!listData.isEmpty()) {
-                    listMessage.append("1. ").append(listData.get(0));
-                }
-
-                // Add remaining elements (if any)
-                for (int i = 1; i < listData.size(); ++i) {
+                // Add all elements in the list
+                for (int i = 0; i < listData.size(); ++i) {
                     listMessage.append("\n").append(i + 1).append(". ").append(listData.get(i));
                 }
 
                 sendMessage(listMessage.toString());
+            } else if (userInput.indexOf("done ") == 0) {
+                int index = Integer.parseInt(userInput.substring(5)) - 1;
+                listData.get(index).markAsDone();
+
+                sendMessage("Nice! I've marked this task as done:\n  " + listData.get(index));
             } else {
                 // ADD command
-                sendMessage("added: " + userCommand);
-                listData.add(userCommand);
+                sendMessage("added: " + userInput);
+                listData.add(new Task(userInput));
             }
         }
     }
