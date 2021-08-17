bb7ec7456b2dd34f5cca2e8f0f42849c5f34adc3
James-Kua <james@Jamess-MacBook-Pro.local>
2021-08-17 15:06:14 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,28 +3,7 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static class Task {
-        protected String description;
-        protected boolean isDone;
 
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return (isDone ? "X" : " "); // mark done task with X
-        }
-
-        public void markAsDone() {
-            isDone = true;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-    }
 
     public static ArrayList<Task> itemList = new ArrayList<Task>();
 
@@ -39,6 +18,13 @@ public class Duke {
         return firstWord;
     }
 
+    public static String getSecondWord(String input) {
+        String arr[] = input.split(" ", 2);
+
+        String secondWord = arr[1].strip();
+        return secondWord;
+    }
+
     public static Integer getTaskNumber(String input) {
         String arr[] = input.split(" ", 2);
 
@@ -46,51 +32,82 @@ public class Duke {
         return taskNumber;
     }
 
-    public static void addTask(String input) {
-        Task t = new Task(input);
-        itemList.add(t);
-        System.out.println("added:" + " " + input);
-        start();
-    }
-
     public static void markTaskDone(String input) {
         //get which task number is completed
         int taskDoneNum = getTaskNumber(input);
         Task taskDone = itemList.get(taskDoneNum - 1);
         taskDone.markAsDone();
-        System.out.println("Nice! I've marked this task as done: " + '\n' + "[" + taskDone.getStatusIcon() + "] " + taskDone.getDescription());
+        System.out.println("Nice! I've marked this task as done:" + '\n' + taskDone.toString());
     }
 
     public static void printItemList() {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < itemList.size(); i++) {
-            System.out.println(i + 1 + "." + "[" + itemList.get(i).getStatusIcon() + "] " + itemList.get(i).getDescription());
+            System.out.println(i + 1 + "." + itemList.get(i).toString());
         }
+    }
+
+    public static void printTaskNumber() {
+        System.out.println("Now you have " + itemList.size() + (itemList.size() == 1 ? " task" : " tasks") + " in the list.");
+    }
+
+    public static void addComplete(Task t) {
+        itemList.add(t);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(t.toString());
+        printTaskNumber();
+    }
+
+    public static void addToDo(String description) {
+        Task task = new Todo(description);
+        addComplete(task);
+    }
+
+    public static void addDeadline(String description, String time) {
+        Deadline deadline = new Deadline(description, time);
+        addComplete(deadline);
+    }
+
+    public static void addEvent(String description, String time) {
+        Event event = new Event(description, time);
+        addComplete(event);
     }
 
     public static void start() {
 
-        String exit = "bye";
         String input;
         String lineBreak = "========================================================================";
-        String items = "list";
 
         Scanner in = new Scanner(System.in);
         input = in.nextLine();
         System.out.println(lineBreak);
 
-        if (input.equals(exit)) {
+        if (input.equals("bye")) {
             itemList.clear();
             System.out.println("Bye. Hope to see you again soon!");
-        } else if (input.equals(items)) {
+        } else if (input.equals("list")) {
             printItemList();
-        } else {
+        }
+        else {
             String check = getFirstWord(input);
 
             if (check.equals("done")) {
                 markTaskDone(input);
+            } else if (check.equals("todo")) {
+                String description = input.substring(5);
+                addToDo(description);
+            } else if (check.equals("deadline")) {
+                String[] arr = input.split("/");
+                String description = getSecondWord(arr[0]);
+                String time = getSecondWord(arr[1]);
+                addDeadline(description, time);
+            } else if (check.equals("event")) {
+                String[] arr = input.split("/");
+                String description = getSecondWord(arr[0]);
+                String time = getSecondWord(arr[1]);
+                addEvent(description, time);
             } else {
-                addTask(input);
+                addToDo(input);
             }
         }
         System.out.println(lineBreak);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
 }
