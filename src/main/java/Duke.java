411c9c368c57471f1882fea9a376fa4cc38c143a
jianoway <77208762+jianoway@users.noreply.github.com>
2021-08-18 01:55:13 +0800

    Add delete functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class Duke {
 
@@ -82,6 +82,34 @@ public class Duke {
         }
     }
 
+    public static int deleteCheck(String response, int currentCapacity) {
+        String strippedResponse = response.strip();
+        if (strippedResponse.startsWith("delete")) {
+            if (strippedResponse.length() < 8) {
+                return 0;
+            } else {
+                String cutString = strippedResponse.substring(7);
+                char[] chars = cutString.toCharArray();
+                StringBuilder sb = new StringBuilder();
+                for (char c : chars) {
+                    if (Character.isDigit(c)) {
+                        sb.append(c);
+                    } else {
+                        return 0;
+                    }
+                }
+                Integer intToCheck = Integer.parseInt(sb.toString());
+                if (intToCheck <= currentCapacity) {
+                    return intToCheck;
+                } else {
+                    return -1;
+                }
+            }
+        } else {
+            return 0;
+        }
+    }
+
     public static String responseToTodo(String response) {
         return response.substring(5);
     }
@@ -96,7 +124,7 @@ public class Duke {
         String byeString = "bye";
         String listFullMessage = "Your current list is full! Unable to add any more items!";
         String taskAddedMessage  = "Got it. I've added this task:\n  %s\nNow you have %d task%s in the list\n";
-        Task[] userTaskList = new Task[100];
+        ArrayList<Task> userTaskList = new ArrayList<Task>(100);
         int currentCapacity = 0;
 
         while (true) {
@@ -108,32 +136,49 @@ public class Duke {
             } else if (response.startsWith("done ")) {
                 if (doneCheck(response, currentCapacity) > 0) {
                     int positionToMarkDone = doneCheck(response, currentCapacity);
-                    userTaskList[positionToMarkDone - 1].markDone();
+                    userTaskList.get(positionToMarkDone - 1).markDone();
                     String replyString = "Nice! I've marked this task as done:\n";
-                    replyString += String.format("  %s", userTaskList[positionToMarkDone - 1]);
+                    replyString += String.format("  %s", userTaskList.get(positionToMarkDone - 1));
                     System.out.println(replyString);
                     continue;
                 } else if (doneCheck(response, currentCapacity) == -1) {
                     System.out.println("No such task exists! Please try again!");
                     continue;
+                } else {
+                    System.out.println("Please indicate the task you'd like to mark as done!");
                 }
             } else if (response.startsWith("undo ")) {
                 if (undoCheck(response, currentCapacity) > 0) {
                     int positionToMarkUndone = undoCheck(response, currentCapacity);
-                    userTaskList[positionToMarkUndone - 1].markUndone();
+                    userTaskList.get(positionToMarkUndone - 1).markUndone();
                     String replyString = "Oh no! I've marked this task as undone:\n";
-                    replyString += String.format("  %s", userTaskList[positionToMarkUndone - 1]);
+                    replyString += String.format("  %s", userTaskList.get(positionToMarkUndone - 1));
                     System.out.println(replyString);
                     continue;
-                } else if (doneCheck(response, currentCapacity) == -1) {
+                } else if (undoCheck(response, currentCapacity) == -1) {
                     System.out.println("No such task exists! Please try again!");
                     continue;
+                } else {
+                    System.out.println("Please indicate the task you'd like to undo!");
                 }
+            } else if (response.startsWith("delete ")){
+                if (deleteCheck(response, currentCapacity) > 0) {
+                    int positionToRemove = deleteCheck(response, currentCapacity);
+                    Task removedTask = userTaskList.remove(positionToRemove - 1);
+                    currentCapacity -= 1;
+                    System.out.printf("Noted. I've removed this task:\n  %s\nNow you have %d task%s in the list.\n", removedTask, currentCapacity, pluralOrNo(currentCapacity));
+                } else if (deleteCheck(response, currentCapacity) == -1) {
+                    System.out.println("No such task exists! Please try again!");
+                    continue;
+                } else {
+                    System.out.println("Please indicate the task you'd like to delete!");
+                }
+
             } else if (response.equals(listCommand)) {
                 if (currentCapacity > 0) {
                     String toPrint = "Here are the tasks in your list:\n";
                     for (int i = 0; i < currentCapacity; i++) {
-                        Task task = userTaskList[i];
+                        Task task = userTaskList.get(i);
                         toPrint += String.format("%d. %s\n", i + 1, task);
                     }
                     System.out.print(toPrint);
@@ -150,8 +195,8 @@ public class Duke {
                     continue;
                 }
                 if (currentCapacity < 100) {
-                    userTaskList[currentCapacity] = new ToDo(responseToTodo(response));
-                    System.out.printf(taskAddedMessage, userTaskList[currentCapacity], currentCapacity + 1, pluralOrNo(currentCapacity + 1));
+                    userTaskList.add(new ToDo(responseToTodo(response)));
+                    System.out.printf(taskAddedMessage, userTaskList.get(currentCapacity), currentCapacity + 1, pluralOrNo(currentCapacity + 1));
                     currentCapacity += 1;
                 } else {
                     System.out.println(listFullMessage);
@@ -178,8 +223,8 @@ public class Duke {
                     String[] eventSplit = response.split("/at");
                     String description = eventSplit[0].strip().substring(6);
                     String date = eventSplit[1].strip();
-                    userTaskList[currentCapacity] = new Event(description, date);
-                    System.out.printf(taskAddedMessage, userTaskList[currentCapacity], currentCapacity + 1, pluralOrNo(currentCapacity + 1));
+                    userTaskList.add(new Event(description, date));
+                    System.out.printf(taskAddedMessage, userTaskList.get(currentCapacity), currentCapacity + 1, pluralOrNo(currentCapacity + 1));
                     currentCapacity += 1;
                 } else {
                     System.out.println(listFullMessage);
@@ -210,8 +255,8 @@ public class Duke {
                         continue;
                     }
                     String date = deadlineSplit[1].strip();
-                    userTaskList[currentCapacity] = new Deadline(description, date);
-                    System.out.printf(taskAddedMessage, userTaskList[currentCapacity], currentCapacity + 1, pluralOrNo(currentCapacity + 1));
+                    userTaskList.add(new Deadline(description, date));
+                    System.out.printf(taskAddedMessage, userTaskList.get(currentCapacity), currentCapacity + 1, pluralOrNo(currentCapacity + 1));
                     currentCapacity += 1;
                 } else {
                     System.out.println(listFullMessage);
