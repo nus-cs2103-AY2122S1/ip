08173b8b760b35f38ed025f4cba6df6a6dcec5fd
mweeruien <mweeruien@gmail.com>
2021-08-17 23:26:11 +0800

    completed level 1, repeats user input and ends when user inputs "bye"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
+        String linebreak = "~~~~~~~~~~";
+        String command;
+
         // This string was the initial string
         /*
         String logo = " ____        _        \n"
@@ -15,6 +18,24 @@ public class Duke {
         // The chat bot name is Notaro bc it's Not-a-ro-bot :>
         // This is the introduction of the chat bot
         System.out.println("Hi! I'm Taro, short for Notaro because I'm Not-a-ro-bot!!");
-        System.out.println("What can I do for you today?");
+        System.out.println("What can I do for you today? :>");
+        System.out.println("(Tip: Type \"bye\" to leave!)");
+        System.out.println(linebreak);
+
+
+        // This part listens for user input and repeats
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            command = sc.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println("Bye bye!! Thanks for stopping by!");
+                System.out.println(linebreak);
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println(linebreak);
+        }
     }
 }
