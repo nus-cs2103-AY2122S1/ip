613d09b462e03cac9072481c3445883e1e8cd56c
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-08-17 15:48:42 +0800

    Created functions to handle adding Todo, Deadline, Event in Duke class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,11 +24,17 @@ public class Duke {
             sayBye();
         } else if (command.toLowerCase().equals("list")) {
             showList();
-        } else if(command.toLowerCase().contains("done ")) {
+        } else if(command.toLowerCase().contains("done")) {
             int taskDone = Character.getNumericValue(command.charAt(command.length() - 1)) - 1;
             finishTask(taskDone);
+        } else if (command.toLowerCase().contains("todo")) {
+            addTodo(command);
+        } else if (command.toLowerCase().contains("deadline")) {
+            addDeadline(command);
+        } else if (command.toLowerCase().contains("event")) {
+            addEvent(command);
         } else {
-            addTask(command);
+            otherCommand(command);
         }
     }
 
@@ -58,13 +64,52 @@ public class Duke {
         getCommand();
     }
 
-    // Function for when user command is something else
-    private static void addTask(String command) {
-        // User command is something else, add command to command list and continue asking for commands
-        System.out.println("\t added: " + command);
-        Task t = new Task(command);
+    private static void addTodo(String command) {
+        // Take the substring containing the task description of the command
+        String description = command.substring(5);
+        System.out.println("\t added: " + description);
+        ToDo t = new ToDo(description);
         taskList[counter] = t;
         counter++;
+        getCommand();
+    }
+
+    private static void addDeadline(String command) {
+        // Template of command: deadline {description} /by {byWhen}
+        // Find the end of description, which is the start of byWhen
+        int endOfDescription = command.indexOf("/");
+
+        // Find substring containing task description
+        String description = command.substring(9, endOfDescription);
+        System.out.println("\t added: " + description);
+
+        String byWhen = command.substring(endOfDescription);
+        Deadline t = new Deadline(description, byWhen);
+        taskList[counter] = t;
+        counter++;
+        getCommand();
+    }
+
+    private static void addEvent(String command) {
+        // Template of command: event {description} /by {startTime}
+        // Find the end of description, which is the start of startTime
+        int endOfDescription = command.indexOf("/");
+
+        // Find substring containing task description
+        String description = command.substring(6, endOfDescription);
+        System.out.println("\t added: " + description);
+
+        String startTime = command.substring(endOfDescription);
+        Event t = new Event(description, startTime);
+        taskList[counter] = t;
+        counter++;
+        getCommand();
+    }
+
+    // Function for when user command is something else
+    private static void otherCommand(String command) {
+        // User command is something else, add command to command list and continue asking for commands
+        System.out.println(command);
         getCommand();
     }
 }
