3c692f94907ec1a14440299674865f12954dd543
nniiggeell <e0559733@u.nus.edu>
2021-08-19 00:18:48 +0800

    Add javadocs to Response.java

--- src/main/java/Response.java
+++ src/main/java/Response.java
@@ -1,15 +1,25 @@
 import java.util.ArrayList;
 
+/**
+ * Represents a response the bot makes to the user.
+ * @author Nigel Tan
+ */
 public class Response {
     private final String horizontalLine = "    ____________________________________________________________\n";
     private ArrayList<Task> lst;
     private int count;
 
+    /**
+     * Constructor
+     */
     public Response() {
         this.lst = new ArrayList<>();
         this.count = 1;
     }
 
+    /**
+     * This method handles the greet event.
+     */
     public void greet() {
         System.out.println(horizontalLine +
                 "    Hello! I'm LebronChatBot\n" +
@@ -17,6 +27,10 @@ public class Response {
                 horizontalLine);
     }
 
+    /**
+     * This method handles the echo event.
+     * @param task the Task that the user wants to add
+     */
     public void echo(Task task) {
         lst.add(task);
         System.out.println(horizontalLine + "    added: " +
@@ -24,12 +38,18 @@ public class Response {
                 horizontalLine);
     }
 
+    /**
+     * This method handles the exit event when the user says bye.
+     */
     public void exit() {
         System.out.println(horizontalLine +
                 "    Bye. Hope to see you again soon!\n" +
                 horizontalLine);
     }
 
+    /**
+     * This method handles the display list event when the user says list.
+     */
     public void display() {
         this.count = 1;
         System.out.println(horizontalLine);
@@ -41,6 +61,10 @@ public class Response {
         System.out.println(horizontalLine);
     }
 
+    /**
+     * This method handles the markDone event when the user says done, marking the Task as done.
+     * @param pos the position of the Task in the ArrayList lst
+     */
     public void markDone(int pos) {
         Task task = lst.get(pos);
         task.mark();
