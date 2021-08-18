69a7643ff456f90548a2fcd296d934487026cd14
Siddharth Srivastava <siddharth.srivastava2508@gmail.com>
2021-08-19 02:25:44 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
 
+    private List<String> LIST;
+
+    public Duke() {
+        LIST = new ArrayList<String>();
+    }
+
     private void greet() {
         System.out.println("______________________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
         System.out.println("______________________________________________________________________\n");
     }
 
@@ -16,21 +24,28 @@ public class Duke {
         while(!(command.equals("bye"))) {
             command = getInput.nextLine();
             System.out.println("______________________________________________________________________");
-            if(command.equals("bye")) {
+            if (command.equals("bye")) {
                 System.out.println(buffer + "Bye. Hope to see you again soon!");
-                System.out.println("______________________________________________________________________\n");
+            } else if (command.equals("list")) {
+                for (int i = 0; i < LIST.size(); i++) {
+                    System.out.println(buffer + (i + 1) + ". " + LIST.get(i));
+                }
             } else {
-                System.out.println(buffer + command);
-                System.out.println("______________________________________________________________________\n");
+                LIST.add(command);
+                System.out.println(buffer + "added: " + command);
             }
+            System.out.println("______________________________________________________________________\n");
         }
     }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String buffer = "     ";
+        String logo =   buffer + " ____        _        \n" +
+                        buffer + "|  _ \\ _   _| | _____ \n" +
+                        buffer + "| | | | | | | |/ / _ \\\n" +
+                        buffer + "| |_| | |_| |   <  __/\n" +
+                        buffer + "|____/ \\__,_|_|\\_\\___|\n";
+        
         System.out.println("Hello from\n" + logo);
 
         Duke instance = new Duke();
