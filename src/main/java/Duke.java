be4bec7337f02725d12af119cf904dea8cf49b06
Chen-Yiyang <iycspfa@gmail.com>
2021-08-18 15:27:45 +0800

    Basic Greet, Echo and Exit displays

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+    // constants for special commands
+    private static final String CMD_BYE = "bye";
+
+    private static void displayGreetings() {
+        System.out.println("\tHello this is Yiyang-bot :D");
+        System.out.println("\tWhat can I do for you?");
+    }
+
+    private static void displayBye() {
+        System.out.println("\tBye. Hope to see you again.");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        displayGreetings();
+
+        Scanner sc = new Scanner(System.in);
+        String currCommand = sc.nextLine();
+
+        while (!currCommand.equals(CMD_BYE)) {
+            System.out.println("\t" + currCommand);
+
+            currCommand = sc.nextLine();
+        }
+
+        displayBye();
     }
 }
