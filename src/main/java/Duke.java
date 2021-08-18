f76b2de3b0163d82190be1436fc9970be63fd685
ChuangZheQuan <zhequan7chuang@gmail.com>
2021-08-18 11:07:01 +0800

    Level 3 Commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,6 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
 
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
@@ -17,8 +11,10 @@ public class Duke {
             String input = sc.nextLine().toLowerCase();
             boolean isLastCharDigit =  Character.isDigit(input.charAt(input.length() - 1));
             if (input.equals("list")) {
+                System.out.println("list");
                 System.out.println("\tHere are the tasks in your list:");
                 for (int i = 0; i < index; i++) {
+                    System.out.println("\t" + (i + 1) + ". " + listOfItems[i]);
                     System.out.println("\t" + (i + 1) + ". [" + listOfItems[i].getStatusIcon() + "] " + listOfItems[i].getDescription());
                 }
             } else if (input.equals("bye")) {
@@ -28,7 +24,7 @@ public class Duke {
                 char itemIndex = input.charAt(input.length() - 1);
                 int i = Integer.parseInt(String.valueOf(itemIndex)) - 1;
                 if (i < index) {
-                    listOfItems[i].setStatusAsDone();
+                    listOfItems[i].markAsDone();
                     System.out.println("Nice! I've marked this task as done:\n\t" +
                             "[" + listOfItems[i].getStatusIcon() + "] " + listOfItems[i].getDescription());
                 } else {
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,19 +2,20 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
-    public Task(String desc) {
-        this.description = desc;
+    public Task(String description) {
+        this.description = description;
         this.isDone = false;
-    }
-    public String getDescription() {
-        return description;
     }
 
     public String getStatusIcon() {
-        return (isDone ? "X" : " ");
+        return (isDone ? "X" : " "); // mark done task with X
     }
 
-    public void setStatusAsDone() {
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void markAsDone() {
         this.isDone = true;
     }
 }
