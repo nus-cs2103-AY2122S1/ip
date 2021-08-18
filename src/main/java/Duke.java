23ad40bf8de68e8b3581351e36441ed11f7124f2
Aishwarya Nair <aishrn1202@gmail.com>
2021-08-18 13:38:25 +0800

    Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class Duke {
+
+    private static List<String> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         printFormatted("Hola! I'm Blitz :) \n\tWhat can I do for you?");
         String command = scanner.nextLine();
         while(!command.equals("bye")) {
-            printFormatted(command);
+            if(command.equals("list")) {
+                printList();
+            } else {
+                tasks.add(command);
+                printFormatted("added: " + command);
+            }
             command = scanner.nextLine();
         }
         printFormatted("Adiós. Hope to see you again soon!");
@@ -18,6 +29,24 @@ public class Duke {
             System.out.print('_');
         }
         System.out.println("\n \t" + str);
+        System.out.print('\t');
+        for(int i = 0; i < 40; i++) {
+            System.out.print('_');
+        }
+        System.out.println('\n');
+    }
+
+    public static void printList() {
+        System.out.print('\t');
+        for(int i = 0; i < 40; i++) {
+            System.out.print('_');
+        }
+        System.out.print('\n');
+        int ctr = 1;
+        for(String task: tasks) {
+            System.out.println("\t" + ctr + ". " + task);
+            ctr++;
+        }
         System.out.print('\t');
         for(int i = 0; i < 40; i++) {
             System.out.print('_');
