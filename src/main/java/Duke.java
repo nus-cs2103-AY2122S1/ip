cb7448b240eaa30cc056d2080e24c38361e54ba2
Sourabh Jaiswal <sourabhrj31@gmail.com>
2021-08-18 15:57:24 +0800

    level-1 ip greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        displayWelcomeMessage();
+        String input = br.readLine();
+        while(!input.equals("bye")) {
+            displayCommand(input);
+            input = br.readLine();
+        }
+        displayByeMessage();
+    }
+
+    static void printLines() {
+        System.out.println("------------------------------------------------------------------");
+    }
+
+    static void displayWelcomeMessage() {
+        printLines();
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        printLines();
+    }
+
+    static void displayByeMessage() {
+        printLines();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLines();
+    }
+
+    static void displayCommand(String command) {
+        printLines();
+        System.out.println(command);
+        printLines();
     }
 }
