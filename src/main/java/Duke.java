e5257d7f9cf5b258466ea32c25341de70915f6df
e0543860 <e0543860@u.nus.edu>
2021-08-18 16:20:51 +0800

    Level 3, Mark as Done, new Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,35 +1,53 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello, I'm Duke!\nWhat can I do for you?");
 
+        //initialise scanner
         Scanner sc = new Scanner(System.in);
         boolean finished = false;
-        String[] listOfWords = new String[100];
+
+        //array to hold tasks
+        Task[] listOfTasks = new Task[100];
         int arrayCounter = 0;
+
+        //string array to hold keywords
+        String[] commandWords = {"bye", "list", "done"};
 
         while(!finished) {
             String userResponse = sc.nextLine();
 
-            switch (userResponse) {
-                case "bye":
-                    System.out.println("Bye. Hope to see you again soon!");
-                    finished = true;
-                    break;
-                case "list":
-                    for (int i = 0; i < listOfWords.length; i++) {
-                        if (listOfWords[i] != null) {
-                            System.out.println((i + 1) + ". " + listOfWords[i]);
-                        }
+            //bye
+            if (userResponse.contains(commandWords[0])) {
+                System.out.println("Bye. Hope to see you again soon!");
+                finished = true;
+                break;
+            //list
+            } else if (userResponse.contains(commandWords[1])) {
+                for (int i = 0; i < listOfTasks.length; i++) {
+                    if (listOfTasks[i] != null) {
+                        System.out.println((i + 1) + "." + listOfTasks[i].toString());
                     }
-                    break;
-                default:
-                    System.out.println("added: " + userResponse);
-                    listOfWords[arrayCounter] = userResponse;
-                    arrayCounter++;
+                }
+            //done
+            } else if (userResponse.contains(commandWords[2])) {
+                char indexDone = userResponse.charAt(5);
+                if (Character.isDigit(indexDone)) {
+                    int numberIndex = Character.getNumericValue(indexDone) - 1;
+                    listOfTasks[numberIndex].markAsDone();
+                    System.out.println("Nice, I've marked this task as done!\n" + listOfTasks[numberIndex].toString());
+                } else {
+                    System.out.println("Invalid task number!");
+                }
+            } else {
+                System.out.println("added: " + userResponse);
+                listOfTasks[arrayCounter] = new Task(userResponse);
+                arrayCounter++;
             }
-
         }
+
     }
+
 }
