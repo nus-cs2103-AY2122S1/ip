29aa451ddad3bc39cf2d713d8102040f1bdd7771
angnobel <nobelang78@gmail.com>
2021-08-18 10:08:16 +0800

    Level 4: Support for 3 types of task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,13 +17,14 @@ public class Duke {
         while (!s.equals("bye")) {
             if (s.equals("list")) {
                 int index = 1;
+                
                 System.out.println("---------");
                 for (Task item : list) {
                     System.out.println(index + ". " + item.toString());
                     index++;
                 }
                 System.out.println("---------");
-            } else if (s.contains("done")) {
+            } else if (s.startsWith("done")) {
                 int index = Integer.parseInt(s.split(" ")[1]) - 1;
                 list.get(index).setStatus(true);
 
@@ -32,9 +33,25 @@ public class Duke {
                 System.out.println(list.get(index));
                 System.out.println("---------");
             } else {
+                // Adding tasks
+
+                if (s.startsWith("todo")) {
+                    list.add(new ToDo(s.replace("todo", "").trim()));
+                } else if (s.startsWith("deadline")) {
+                    String[] data = s.replace("deadline ", "").split("/by");
+                    list.add(new Deadline(data[0].trim(), data[1].trim()));
+                } else if (s.startsWith("event")) {
+                    String[] data = s.replace("event ", "").split("/at");
+                    list.add(new Event(data[0].trim(), data[1].trim()));
+                } else {
+                    list.add(new Task(s));
+                }
+
+                int len = list.size();
                 System.out.println("---------");
-                list.add(new Task(s));
-                System.out.println("added: " + s);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(list.get(len - 1).toString());
+                System.out.println("Now you have " + len + " task in the list");
                 System.out.println("---------");
             }
 
