7bb910fbde6189c9834d1bbfb134a51c9b4f31c7
CuongNgo <phuccuongngo99@gmail.com>
2021-08-18 09:27:04 +0800

    Duke: Extract a start command keyword.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,22 +16,26 @@ public class Duke {
 
         while (sc.hasNextLine()) {
             String userInput = sc.nextLine();
+            String[] inputArray = userInput.split(" ");
+            String startCommand = inputArray[0];
 
-            if (userInput.equals("bye")) {
+            if (startCommand.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (userInput.equals("list")) {
+            } else if (startCommand.equals("list")) {
                 //Print the list here
                 System.out.println("Here are the tasks in your list");
                 System.out.println(taskList);
-            } else if (userInput.startsWith("done ") && Util.isInteger(userInput.substring(5))) {
-                //Extract id of task
-                int index = Integer.parseInt(userInput.substring(5)) - 1;
-                //Mark the task as done
-                taskList.markAsDone(index);
-                //Print out confirmation message
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(taskList.getTask(index));
+            } else if (startCommand.equals("done")) {
+                if (inputArray.length > 1 && Util.isInteger(inputArray[1])) {
+                    //Extract id of task
+                    int index = Integer.parseInt(userInput.substring(5)) - 1;
+                    //Mark the task as done
+                    taskList.markAsDone(index);
+                    //Print out confirmation message
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(taskList.getTask(index));
+                }
             } else {
                 //Add userInput to taskList
                 Task newTask = new Task(userInput);
