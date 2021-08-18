47a288e98454036fff8779e9e1c5278d756c0dce
Win Lynn <ngaiwinlynn0323@gmail.com>
2021-08-18 15:57:41 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,6 +10,9 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+        ArrayList<String> todoList = new ArrayList<>();
+
         System.out.println("Hello from\n" + logo +"\n");
         System.out.println("What can I do for you?");
         System.out.println(divider);
@@ -17,8 +21,18 @@ public class Duke {
         String s = in.nextLine();
 
         while (!s.contentEquals("bye")) {
+
             System.out.println(divider);
-            System.out.println(s);
+
+            if (s.contentEquals("list")) {
+                for (int i = 0; i < todoList.size(); i++) {
+                    System.out.println((i + 1) + ". " + todoList.get(i));
+                }
+            } else {
+                todoList.add(s);
+                System.out.println("added: " + s);
+            }
+
             System.out.println(divider);
             s = in.nextLine();
         }
