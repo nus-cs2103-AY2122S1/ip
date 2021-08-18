1a17eb62970a0ef8a8f1de51179e98245ab79712
DESKTOP-BDICAPP\james <e0543403@u.nus.edu>
2021-08-18 13:17:35 +0800

    Add javadocs to Deadlines.java

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,11 +1,26 @@
+/**
+ * Encapsulates a task to be done with a deadline.
+ */
 public class Deadlines extends Task {
+    /** When the task must be done by. */
     private String limit;
 
+    /**
+     * Constructor of a Deadlines object.
+     *
+     * @param description Description of the task.
+     * @param limit When the task must be done by.
+     */
     public Deadlines(String description, String limit) {
         super(description);
         this.limit = limit;
     }
 
+    /**
+     * Returns the string form of the Deadlines object.
+     *
+     * @return The string form of the Deadlines.
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + this.limit + ")";
