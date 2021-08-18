3a281c13f780a50c2396fbeb103977572f8a2cf8
Jason <jasonang0210@gmail.com>
2021-08-19 01:41:11 +0800

    'changes'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
-    private static Task[] myList = new Task[100];
-    private static int myListLength = 0;
+    private static ArrayList<Task> myList = new ArrayList<>();
     public static boolean done = false;
 
     private static void endBot() {
@@ -13,8 +13,8 @@ public class Duke {
 
     private static void listTasks() {
         System.out.println("Here is your to-do list!");
-        for (int i = 0; i < myListLength; i++) {
-            System.out.println(i + 1 + ". " + myList[i].toString());
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println(i + 1 + ". " + myList.get(i).toString());
         }
     }
 
@@ -22,27 +22,40 @@ public class Duke {
         try {
             if (input.startsWith("todo")) {
                 String remaining = input.substring(5);
-                myList[myListLength] = new ToDo(remaining);
+                myList.add(new ToDo(remaining));
             } else if (input.startsWith("deadline")) {
                 String remaining = input.substring(9);
                 String[] segments = remaining.split(" /");
-                myList[myListLength] = new Deadline(segments[0], segments[1]);
+                myList.add(new Deadline(segments[0], segments[1]));
             } else if (input.startsWith("event")) {
                 String remaining = input.substring(6);
                 String[] segments = remaining.split(" /");
-                myList[myListLength] = new Event(segments[0], segments[1]);
+                myList.add(new Event(segments[0], segments[1]));
             } else {
                 System.out.println("Invalid task. Please specify the type of task.");
                 return;
             }
             System.out.println("I've added this task:");
-            System.out.println(myList[myListLength]);
-            myListLength++;
-            System.out.println("Now you have " + myListLength + " tasks.");
+            System.out.println(myList.get(myList.size() - 1));
+            System.out.println("Now you have " + myList.size() + " tasks.");
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid format. Please follow this format. <type of task> <description> /<date if necessary>");
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println("Invalid input. Please type something after specifying the type of task.");
+        }
+    }
+
+    private static void deleteTask(String input) {
+        String[] segments = input.split(" ");
+        try {
+            int index = Integer.parseInt(segments[segments.length - 1]);
+            Task myTask = myList.get(index - 1);
+            myList.remove(index - 1);
+            System.out.println("I've deleted this task:\n[X] " + myTask.description);
+        } catch (NumberFormatException e) {
+            System.out.println("Please input a number after the keyword: delete");
+        } catch (IndexOutOfBoundsException | NullPointerException e ) {
+            System.out.println("Please input a valid task index");
         }
     }
 
@@ -51,12 +64,12 @@ public class Duke {
         String[] segments = input.split(" ");
         try {
             int index = Integer.parseInt(segments[segments.length - 1]);
-            Task myTask = myList[index - 1];
+            Task myTask = myList.get(index - 1);
             myTask.markAsDone();
             System.out.println("I've marked this task as done:\n[X] " + myTask.description);
         } catch (NumberFormatException e) {
             System.out.println("Please input a number after the keyword: done");
-        } catch (ArrayIndexOutOfBoundsException | NullPointerException e ) {
+        } catch (IndexOutOfBoundsException | NullPointerException e ) {
             System.out.println("Please input a valid task index");
         }
     }
@@ -72,6 +85,8 @@ public class Duke {
                 listTasks();
             } else if (input.startsWith("done")) {
                 markAsDone(input);
+            } else if (input.startsWith("delete")) {
+                deleteTask(input);
             } else {
                 addTask(input);
             }
