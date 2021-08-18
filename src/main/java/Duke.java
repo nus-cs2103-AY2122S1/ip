0fedc23207fe3fedc828561fbe4c73cc04c4f84d
Win Lynn <ngaiwinlynn0323@gmail.com>
2021-08-18 19:02:26 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,7 +22,7 @@ public class Duke {
 
         public void markAsDone() {
             this.isDone = true;
-            System.out.println("Nice! I've marked this task as done: ");
+            System.out.println("Nice! I've marked this task as done:");
             System.out.println(this);
         }
 
