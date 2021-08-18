b280da4c0a2ea9f831dfa9a98cc3bc082cd32ef1
kaldius <quantf@gmail.com>
2021-08-18 11:32:11 +0800

    Clean up code with helper functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,22 +22,35 @@ public class Duke {
                 System.out.println("Nice talking to you, goodbye!");
                 break;
             } else if (command.equals("list")) {
-                System.out.println("Here's your todo list!");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i).toString());
-                }
-                System.out.println();
+                printList(tasks);
             } else if (command.contains("done")) {
-                tasks.get(command.charAt(command.length() - 1) - '0' - 1).doTask();
-                System.out.println("Here's your todo list!");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i).toString());
-                }
-                System.out.println();
+                doTask(command.charAt(command.length() - 1) - '0' - 1, tasks);
+                printList(tasks);
             } else {
                 tasks.add(new Task(command));
                 System.out.println("\""+ command + "\" has been added to your todo list.\n");
             }
         }
+    }
+
+    /**
+     * Prints out the todo list to the console
+     * @param tasks The list of tasks.
+     */
+    private static void printList(List<Task> tasks) {
+        System.out.println("Here's your todo list!");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i).toString());
+        }
+        System.out.println();
+    }
+
+    /**
+     * Marks the taskNum-th item in the tasks list as completed.
+     * @param taskNum The index of the task to be marked.
+     * @param tasks The list of tasks.
+     */
+    private static void doTask(int taskNum, List<Task> tasks) {
+        tasks.get(taskNum).doTask();
     }
 }
