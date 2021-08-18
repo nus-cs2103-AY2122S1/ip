ba35aaa105f8fc5919a0692ee44531564bc2af04
jaredlhf <jlhfqaz@gmail.com>
2021-08-18 21:55:43 +0800

    refactor Error handling

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,8 +1,4 @@
 public class DukeException extends Exception {
-    public DukeException() {
-        super();
-    }
-
     public DukeException(String message) {
         super(message);
     }
--- src/main/java/Jared.java
+++ src/main/java/Jared.java
@@ -7,25 +7,31 @@ public class Jared {
     private static void add(String command, String next) throws DukeException {
         Task newTask;
         String desc;
+        String date;
         if (command.equals("todo")) {
             try {
                 desc = next.split(" ", 2)[1];
             } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("OOPS!!! The description of a todo cannot be empty.\n" +
-                        "Please enter a description of your task:");
-                Scanner scan = new Scanner(System.in);
-                desc = scan.nextLine();
+                throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
             }
             newTask = new Todo(desc);
         } else if (command.equals("deadline")) {
-            String body = next.split(" ",2)[1];
-            desc = body.split("/by",2)[0];
-            String date = body.split("/by",2)[1];
+            try {
+                String body = next.split(" ", 2)[1];
+                desc = body.split("/by", 2)[0];
+                date = body.split("/by", 2)[1];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+            }
             newTask = new Deadline(desc, date);
         } else if (command.equals("event")) {
-            String body = next.split(" ", 2)[1];
-            desc = body.split("/at",2)[0];
-            String date = body.split("/at",2)[1];
+            try {
+                String body = next.split(" ", 2)[1];
+                desc = body.split("/at", 2)[0];
+                date = body.split("/at", 2)[1];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new DukeException("OOPS!!! The description of a event cannot be empty.");
+            }
             newTask = new Event(desc, date);
         } else {
             throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
@@ -46,24 +52,24 @@ public class Jared {
         System.out.println(res);
     }
 
-    private static void done(String next) {
+    private static void done(String next) throws DukeException {
         int taskNum;
+        Task currTask;
         try {
             taskNum = Integer.valueOf(next.split(" ", 2)[1]);
-            Task currTask = history.get(taskNum-1);
+            currTask = history.get(taskNum-1);
         } catch (Exception e) {
-            System.out.println("Invalid. Please enter the index of task you have completed:");
-            Scanner scan = new Scanner(System.in);
-            taskNum = Integer.valueOf(scan.nextLine());
+            throw new DukeException("Invalid task number provided");
         }
-        int index = taskNum - 1;
-        Task currTask = history.get(index);
         currTask.markDone();
         String res = String.format("Nice! I've marked this task as done:\n%s",
                 currTask.toString());
         System.out.println(res);
     }
 
+    /**
+     * Main function
+     */
     public static void main(String[] args) {
         String intro = "____________________________________________________________\n" +
                 "Hello! I'm Jared\n" +
@@ -76,21 +82,20 @@ public class Jared {
             String next = scan.nextLine();
             String[] inputArr = next.split(" ",2);
             String command = inputArr[0];
-
-            if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.exit(0);
-                scan.close();
-            } else if (command.equals("list")) {
-                list();
-            } else if (command.equals("done")) {
-                done(next);
-            } else {
-                try {
+            try {
+                if (command.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.exit(0);
+                    scan.close();
+                } else if (command.equals("list")) {
+                    list();
+                } else if (command.equals("done")) {
+                    done(next);
+                } else {
                     add(command, next);
-                } catch (DukeException e) {
-                    System.out.println(e.getMessage());
                 }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
         }
 
