0086fea5cfe823f9d990f12cc59352a1baf9cea8
timothywongej <e0559903@u.nus.edu>
2021-08-18 10:54:33 +0800

    Add Level-1 Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String solidLine = "\t" + "-------------------------------------";
+
+        System.out.println(logo + "\n" + solidLine);
+        System.out.println("\t" + "Hello! I'm Duke");
+        System.out.println("\t" + "What can I do for you?");
+        System.out.println(solidLine);
+
+        String req = "";
+        while (true) {
+            req = sc.nextLine();
+            if (req.equals("bye")) {
+                break;
+            }
+            System.out.println(solidLine);
+            System.out.println("\t" + req);
+            System.out.println(solidLine);
+        }
+
+        System.out.println(solidLine);
+        System.out.println("\t" + "Bye. Hope to see you again soon!");
+        System.out.println(solidLine);
+
     }
 }
