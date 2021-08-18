d4751c5399a35ca708b4d5b5c5002b4722050430
Jennifer <e0313611@u.nus.edu>
2021-08-18 22:28:28 +0800

    Implement greeting and echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,19 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "\n------------------------------------------\n";
+        String startGreeting = line + "Hello! I'm Duke\n"
+                + "What can I do for you?" + line;
+        String endGreeting = line + "Bye. Hope to see you again soon!" + line;
+        String exitString = "bye";
+        System.out.println("Hello from\n" + logo + startGreeting);
+
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        while(!userInput.toLowerCase().equals(exitString)) {
+            System.out.print(line + userInput + line);
+            userInput = sc.nextLine();
+        }
+        System.out.println(endGreeting);
     }
 }
