021d91861a6c3a3985a9510cce3d35104e04c664
Nicole Luo <nicoleluo@r-245-112-25-172.comp.nus.edu.sg>
2021-08-18 10:26:46 +0800

    Implement skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static final String PARTITION = "______________________";
+
+    private static boolean response(String command) {
+        //returns true if bot should exit, i.e. command is bye
+        if (command.toLowerCase().equals("bye")) {
+            System.out.println(PARTITION
+                    + "\n Bye. Hope to see you again soon! \n"
+                    + PARTITION);
+            return true;
+        } else {
+            System.out.println(PARTITION + "\n"
+                    + command + "\n" + PARTITION);
+            return false;
+        }
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println(PARTITION
+                + "\n Hello! I'm Duke"
+                + "\n What can I do for you? \n" + PARTITION);
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            if (response(sc.next())) break;
+        }
+        sc.close();
+
     }
 }
