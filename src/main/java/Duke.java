d7be21c00c437f60d83461f79516785508594619
Jason <jasonang0210@gmail.com>
2021-08-19 00:02:30 +0800

    'changes'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Duke {
 
+    private static String[] myList = new String[100];
+    private static int myListLength = 0;
+
     private static void echo() {
         Scanner myScanner = new Scanner(System.in);
         boolean done = false;
@@ -16,8 +19,28 @@ public class Duke {
         }
     }
 
+    private static void add() {
+        Scanner myScanner = new Scanner(System.in);
+        boolean done = false;
+        while (!done) {
+            String input = myScanner.nextLine();
+            if (input.equals("bye")) {
+                done = true;
+                System.out.println("Bye for now!");
+            } else if (input.equals("list")) {
+                for (int i = 0; i < myListLength; i++) {
+                    System.out.println(i + 1 + ". " + myList[i]);
+                }
+            } else {
+                myList[myListLength] = input;
+                myListLength++;
+                System.out.println("added: " + input);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Sup! I'm Luka, your personal assistant.\n");
-        echo();
+        add();
     }
 }
