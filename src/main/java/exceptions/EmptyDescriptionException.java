6193babb93c73608acf97354dfd3e69d3195b64a
CuongNgo <phuccuongngo99@gmail.com>
2021-08-18 14:16:27 +0800

    Move all exceptions to 1 folder

--- src/main/java/exceptions/EmptyDescriptionException.java
+++ src/main/java/exceptions/EmptyDescriptionException.java
@@ -1,3 +1,7 @@
+package exceptions;
+
+import exceptions.DukeException;
+
 public class EmptyDescriptionException extends DukeException {
     public EmptyDescriptionException(String taskCategory) {
         super("☹ OOPS!!! The description of a " + taskCategory + " cannot be empty.");
--- src/main/java/exceptions/EmptyTimeException.java
+++ src/main/java/exceptions/EmptyTimeException.java
@@ -1,3 +1,7 @@
+package exceptions;
+
+import exceptions.DukeException;
+
 public class EmptyTimeException extends DukeException {
     public EmptyTimeException(String taskCategory) {
         super("☹ OOPS!!! The time of a " + taskCategory + " cannot be empty.");
