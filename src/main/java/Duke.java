bc396d5249a1ea7beedec940f881ced3e278d425
Samuel-bit-prog <e0544506@u.nus.edu>
2021-08-18 22:46:29 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,13 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "What can I do for you?");
+        Scanner s = new Scanner(System.in);
+        String command = s.nextLine();
+        while(!command.equals("bye")) {
+            System.out.println(command);
+            command = s.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
