66821f901a4c78643e395a549bae6ace7beb52ef
Jingwencloud <jingwen098765432@gmail.com>
2021-08-18 20:17:19 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone(){
+        isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String toString()
+    {
+        return this.description;
+    }
+
+}
