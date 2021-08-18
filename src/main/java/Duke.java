879a0b8ddb4e68513eb40cd6f25d28723cfdc542
Lutfi Randiva <luffingluffy@gmail.com>
2021-08-18 14:30:35 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String intro = "Hello! I'm Biscuit.\n"
+                + "What do you want me to do?\n";
+        System.out.println(intro);
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userInput = sc.nextLine();
+
+            if (userInput.equals("bye")) {
+                String bye = "Bye. Hope to see you again soon!";
+                System.out.println(bye);
+                break;
+            }
+
+            System.out.println(userInput);
+        }
     }
 }
