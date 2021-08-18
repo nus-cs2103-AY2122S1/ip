4638dbc0787e4473af2c85b20af2f5245c508a5f
xyliew25 <e0550381@u.nus.edu>
2021-08-18 12:25:38 +0800

    complete Level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,6 +16,7 @@ public class Duke {
     String commandLine;
     String command = "";
     String description = "";
+    String responseContent = "";
 
     // Exit command
     final String exitCommand = "bye";
@@ -45,76 +46,77 @@ public class Duke {
 
     // Duke chatbot
     while (!command.equals(exitCommand)) {
-      // Initialize response content
-      String responseContent = "";
-
-      // Draft response content based on command
-      switch (command) {
-        case listCommand:
-          responseContent = "Here are the tasks in your list:\n";
-          for (int i = 0; i < index; i++) {
-            if (i != index - 1) {
-              responseContent = responseContent + "     " + (i + 1) + "." + list[i] + "\n";
+      if (responseContent.equals("")) {
+        // Draft response content based on command
+        switch (command) {
+          case listCommand:
+            if (index == 0) {
+              responseContent = "You do not have any tasks.";
             } else {
-              responseContent = responseContent + "     " + (i + 1) + "." + list[i];
+              responseContent = "Here are the tasks in your list:\n";
+              for (int i = 0; i < index; i++) {
+                if (i != index - 1) {
+                  responseContent = responseContent + "     " + (i + 1) + "." + list[i] + "\n";
+                } else {
+                  responseContent = responseContent + "     " + (i + 1) + "." + list[i];
+                }
+              }
             }
-          }
-          break;
-        case doneCommand:
-          if (doneTask > -1) {
+            break;
+          case doneCommand:
             list[doneTask].markAsDone();
-          }
-          responseContent = "Nice! I've marked this task as done:\n" +
-              "       " + list[doneTask];
-          break;
-        case todoCommand:
-          list[index] = new Todo(description);
-          index++;
-          if (index == 1) {
-            responseContent = "Got it. I've added this task:\n"
-                + "       " + list[index - 1] + "\n"
-                + "     Now you have " + index + " task in the list.";
-          } else {
-            responseContent = "Got it. I've added this task:\n"
-                + "       " + list[index - 1] + "\n"
-                + "     Now you have " + index + " tasks in the list.";
-          }
-          break;
-        case deadlineCommand:
-          list[index] = new Deadline(description, by);
-          index++;
-          if (index == 1) {
-            responseContent = "Got it. I've added this task:\n"
-                + "       " + list[index - 1] + "\n"
-                + "     Now you have " + index + " task in the list.";
-          } else {
-            responseContent = "Got it. I've added this task:\n"
-                + "       " + list[index - 1] + "\n"
-                + "     Now you have " + index + " tasks in the list.";
-          }
-          break;
-        case eventCommand:
-          list[index] = new Event(description, at);
-          index++;
-          if (index == 1) {
-            responseContent = "Got it. I've added this task:\n"
-                + "       " + list[index - 1] + "\n"
-                + "     Now you have " + index + " task in the list.";
-          } else {
-            responseContent = "Got it. I've added this task:\n"
-                + "       " + list[index - 1] + "\n"
-                + "     Now you have " + index + " tasks in the list.";
-          }
-          break;
-        default:
-          break;
+            responseContent = "Nice! I've marked this task as done:\n" +
+                "       " + list[doneTask];
+            break;
+          case todoCommand:
+            list[index] = new Todo(description);
+            index++;
+            if (index == 1) {
+              responseContent = "Got it. I've added this task:\n"
+                  + "       " + list[index - 1] + "\n"
+                  + "     Now you have " + index + " task in the list.";
+            } else {
+              responseContent = "Got it. I've added this task:\n"
+                  + "       " + list[index - 1] + "\n"
+                  + "     Now you have " + index + " tasks in the list.";
+            }
+            break;
+          case deadlineCommand:
+            list[index] = new Deadline(description, by);
+            index++;
+            if (index == 1) {
+              responseContent = "Got it. I've added this task:\n"
+                  + "       " + list[index - 1] + "\n"
+                  + "     Now you have " + index + " task in the list.";
+            } else {
+              responseContent = "Got it. I've added this task:\n"
+                  + "       " + list[index - 1] + "\n"
+                  + "     Now you have " + index + " tasks in the list.";
+            }
+            break;
+          case eventCommand:
+            list[index] = new Event(description, at);
+            index++;
+            if (index == 1) {
+              responseContent = "Got it. I've added this task:\n"
+                  + "       " + list[index - 1] + "\n"
+                  + "     Now you have " + index + " task in the list.";
+            } else {
+              responseContent = "Got it. I've added this task:\n"
+                  + "       " + list[index - 1] + "\n"
+                  + "     Now you have " + index + " tasks in the list.";
+            }
+            break;
+          default:
+            break;
+        }
       }
 
       // Response
-      String response = "    ____________________________________________________________\n"
-          + "     " + responseContent + "\n"
-          + "    ____________________________________________________________\n";
       if (!responseContent.equals("")) {
+        String response = "    ____________________________________________________________\n"
+            + "     " + responseContent + "\n"
+            + "    ____________________________________________________________\n";
         System.out.println(response);
       }
 
@@ -123,27 +125,64 @@ public class Duke {
       command = commandLine.split(" ")[0];
       String rest;
 
-      switch (command) {
-        case doneCommand:
-          rest = commandLine.split(" ", 2)[1];
-          doneTask = Integer.parseInt(rest) - 1;
-          break;
-        case deadlineCommand:
-          rest = commandLine.split(" ", 2)[1];
-          description = rest.split(" /by ")[0];
-          by = rest.split(" /by ")[1];
-          break;
-        case eventCommand:
-          rest = commandLine.split(" ", 2)[1];
-          description = rest.split(" /at ")[0];
-          at = rest.split(" /at ")[1];
-          break;
-        case todoCommand:
-          rest = commandLine.split(" ", 2)[1];
-          description = rest;
-          break;
-        default:
-          break;
+      try {
+        switch (command) {
+          case doneCommand:
+            if (commandLine.split(" ").length == 1) {
+              throw new MissingArgumentException("Done", "task");
+            } else {
+              rest = commandLine.split(" ")[1];
+              doneTask = Integer.parseInt(rest) - 1;
+              if (doneTask < 0) {
+                throw new InvalidArgumentException(doneTask);
+              } else if (doneTask >= index) {
+                throw new InvalidArgumentException(index);
+              }
+            }
+            break;
+          case deadlineCommand:
+            if (commandLine.split(" ").length == 1) {
+              throw new EmptyDescriptionException("Deadline");
+            } else {
+              rest = commandLine.split(" ", 2)[1];
+              if (rest.split(" /by ").length == 1) {
+                throw new MissingArgumentException("Deadline", "/by");
+              } else {
+                description = rest.split(" /by ")[0];
+                by = rest.split(" /by ")[1];
+              }
+            }
+            break;
+          case eventCommand:
+            if (commandLine.split(" ").length == 1) {
+              throw new EmptyDescriptionException("Event");
+            } else {
+              rest = commandLine.split(" ", 2)[1];
+              if (rest.split(" /at ").length == 1) {
+                throw new MissingArgumentException("Event", "/at");
+              } else {
+                description = rest.split(" /at ")[0];
+                at = rest.split(" /at ")[1];
+              }
+            }
+            break;
+          case todoCommand:
+            if (commandLine.split(" ").length == 1) {
+              throw new EmptyDescriptionException("Todo");
+            } else {
+              rest = commandLine.split(" ", 2)[1];
+              description = rest;
+            }
+            break;
+          case "":
+          case listCommand:
+            break;
+          default:
+            throw new UnknownCommandException();
+        }
+        responseContent = "";
+      } catch (DukeException error) {
+        responseContent = error.toString();
       }
     }
 
