d83fc7a72fc891504c0b30532783108eb91ad597
Mengzhe Wang <mengzhe_wang@u.nus.edu>
2021-08-19 01:53:36 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,45 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("------------------------\n"
+        String end = "bye";
+        String display = "list";
+        String lineBreak = "------------------------------";
+        ArrayList<String> inputs = new ArrayList<>();
+
+        // Welcome message
+        System.out.println(lineBreak
+                + "\n"
                 + "Hello! I'm Azure.\n"
                 + "How can I help you today?\n"
-                + "------------------------");
+                + lineBreak);
 
         Scanner myObj = new Scanner(System.in);
 
-        String end = "bye";
         // String input
         while(true) {
             String input = myObj.nextLine();
-            if (!input.equals(end)) {
-                System.out.println("     " + input);
+
+            // normal input
+            if (!input.equals(end) && !input.equals(display)) {
+                System.out.println("     added: " + input + "\n" + lineBreak + "\n");
+                inputs.add(input);
                 continue;
             }
-            System.out.println("     Bye! See you next time!");
+
+            // display saved inputs
+            if (input.equals(display)) {
+                for (int i = 0; i < inputs.size(); i++) {
+                    int index = i + 1;
+                    System.out.println(index + ". " + inputs.get(i));
+                }
+                System.out.println(lineBreak + "\n");
+                continue;
+            }
+
+            // exit
+            System.out.println("     Bye! See you next time! :)" + "\n" + lineBreak + "\n");
             break;
         }
     }
