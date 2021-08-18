2463ac901892249a64f94411d1052aaefee9e63e
Preshita01 <pandipreshita@gmail.com>
2021-08-18 19:10:40 +0800

    Add Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,38 @@
+public class Task  {
+    private static Task[] taskList = new Task[100];
+    private static int counter = 0;
+
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void addTask() {
+        Task.taskList[counter] = this;
+        counter++;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[]"); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public static Task[] getTaskList() {
+        return Task.taskList;
+    }
+
+    public static int getCounter() {
+        return Task.counter;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+}
