eec5f1fe2ae1c2d128d2f3ee171fede8d3dfa117
yulonglim <yulonglimbleh@gmail.com>
2021-08-18 14:45:48 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.*;
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "\nWhat can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String str = sc.nextLine();
+            if (str.equalsIgnoreCase("bye")) {
+                System.out.println("\n_________________________\n" + "Bye. Hope to see you again soon!" + "\n_________________________\n");
+                return;
+            } else {
+                System.out.println("\n_________________________\n" + str + "\n_________________________\n");
+            }
+        }
     }
 }
