fc1f3e431032b53abb929a6d7c2ee49b6295bf87
dt-td <77134471+dt-td@users.noreply.github.com>
2021-08-18 13:57:07 +0800

    Add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<String> list;
+
     public static void main(String[] args) {
+        list = new ArrayList<>();
         startDeanBot();
     }
 
@@ -12,34 +16,43 @@ public class Duke {
                 +     "  | | | |/ _ \\/ _` | '_  \\  \n"
                 +     "  | |_| |  __| (_| | | | |    \n"
                 +     "  |____/ \\___|\\__,_|_| |_|  \n";
-        printDoubleBorder();
-        System.out.println("Hello! I'm:\n" + logo + "\nHow may I help you?");
-        printDoubleBorder();
+        printDoubleLineBorder();
+        System.out.println("Hello! I'm\n" + logo + "\nHow may I help you?");
+        printDoubleLineBorder();
 
-        Scanner input = new Scanner(System.in);  // Create a Scanner object
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
 
-        while (input.hasNextLine()) {
-            String userInput = input.nextLine();
-            if (userInput.equals("bye")) {
-                printSingleBorder();
-                System.out.println("Bye. Hope to see you again soon!");
-                printSingleBorder();
-                break;
-            } else {
-                printSingleBorder();
-                System.out.println(userInput);
-                printSingleBorder();
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            switch(input) {
+                case "bye":
+                    printSingleLineBorder();
+                    System.out.println("Bye. Hope to see you again soon!");
+                    printSingleLineBorder();
+                    break;
+                case "list":
+                    printSingleLineBorder();
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println((i + 1) + ". " + list.get(i));
+                    }
+                    printSingleLineBorder();
+                    break;
+                default:
+                    list.add(input);
+                    printSingleLineBorder();
+                    System.out.println("Added: " + input);
+                    printSingleLineBorder();
             }
         }
     }
 
     // Print double line border.
-    private static void printDoubleBorder() {
+    private static void printDoubleLineBorder() {
         System.out.println("==================================================");
     }
 
     // Prints single line border.
-    private static void printSingleBorder() {
+    private static void printSingleLineBorder() {
         System.out.println("--------------------------------------------------");
     }
 }
