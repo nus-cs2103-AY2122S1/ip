7908567c59e96fad3b901272bd7e4ced44858c3e
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2021-08-18 22:43:33 +0800

    Mark as Done. Added new class Task.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,22 +4,40 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! This is Jarvis.");
         System.out.println("What can I do for you sir?");
-        String [] tasks = new String[100];
+        System.out.println("---------------------------------");
+        Task [] tasks = new Task[100];
         int taskIndex = 0;
         Scanner sc = new Scanner(System.in);
         while (true) {
-            String task = sc.nextLine();
-            if (task.equals("bye")) {
+            String taskDescription = sc.nextLine();
+            if (taskDescription.equals("bye")) {
+                System.out.println("---------------------------------");
                 System.out.println("Goodbye Sir. Hope you have a pleasant day sir.");
+                System.out.println("---------------------------------");
                 break;
-            } else if (task.equals("list")) {
+            } else if (taskDescription.equals("list")) {
+                System.out.println("---------------------------------");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskIndex; i++) {
-                    System.out.printf("%d. %s%n", i + 1, tasks[i]);
+                    Task task = tasks[i];
+                    System.out.printf("%d.[%s] %s%n", i + 1, task.getStatusIcon(), task.getDescription());
                 }
+                System.out.println("---------------------------------");
+            } else if (taskDescription.startsWith("done ")) {
+                String [] splitText = taskDescription.split(" ");
+                int serialNum = Integer.parseInt(splitText[1]);
+                Task task = tasks[serialNum - 1];
+                task.markAsDone();
+                System.out.println("---------------------------------");
+                System.out.println("I have marked this task as done");
+                System.out.printf("[%s] %s%n", task.getStatusIcon(), task.getDescription());
+                System.out.println("---------------------------------");
             } else {
-                tasks[taskIndex] = task;
+                tasks[taskIndex] = new Task(taskDescription);
                 taskIndex++;
-                System.out.println("added: " + task);
+                System.out.println("---------------------------------");
+                System.out.println("added: " + taskDescription);
+                System.out.println("---------------------------------");
             }
         }
         sc.close();
