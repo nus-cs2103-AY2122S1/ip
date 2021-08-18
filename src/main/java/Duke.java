c6c1ff5f89513d2ac33cbb456bc0190a61775a99
weiquu <ztolenstar@hotmail.com>
2021-08-18 19:58:39 +0800

    Completed Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
   public static void main(String[] args) {
@@ -10,6 +11,7 @@ public class Duke {
        + "|____/ \\__,_|_|\\_\\___|\n";
        System.out.println("Hello from\n" + logo);
     */
+    ArrayList<String> userText = new ArrayList<>();
     String LINE = "    --------------------------------------------------";
     String INDENTATION = "      ";
     Scanner scan = new Scanner(System.in);
@@ -17,12 +19,20 @@ public class Duke {
     System.out.println(LINE + "\n" + INDENTATION + "Hello! I'm Duke\n" + INDENTATION + "What can I do for you?\n" + LINE);
     
     while (true) {
-      String command = scan.next();
-      if (!command.equals("bye")) {
-        System.out.println(LINE + "\n" + INDENTATION + command + "\n" + LINE);
-      } else {
+      String command = scan.nextLine();
+      if (command.equals("list")) {
+        System.out.println(LINE);
+        for (int i = 0; i < userText.size(); i++) {
+          int tempNum = i + 1;
+          System.out.println(INDENTATION + tempNum  + ". " + userText.get(i));
+        }
+        System.out.println(LINE);
+      } else if (command.equals("bye")) {
         System.out.println(LINE + "\n" + INDENTATION + "Bye. Hope to see you again soon!\n" + LINE);
         break;
+      } else {
+        userText.add(command);
+        System.out.println(LINE + "\n" + INDENTATION + "added: " + command + "\n" + LINE);
       }
     }
   }
