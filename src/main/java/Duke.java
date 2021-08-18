62c8b60637639ec8a5df2dd7b689855bec7dcb32
ErnestCuong <hltc11032000@gmail.com>
2021-08-18 23:14:38 +0800

    Add Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -8,15 +9,24 @@ public class Duke {
         System.out.println("*******************************************");
 
         Scanner myScanner= new Scanner(System.in);
-        Boolean hasQuit = false;
+        boolean hasQuit = false;
+        String[] tasks = new String[100];
+        int numOfTasks = 1;
+        // ArrayList<String> tasks = new ArrayList<>();
 
         while (!hasQuit && myScanner.hasNextLine()) {
             String userInput = myScanner.nextLine();
             if (userInput.equals("bye")) {
                 hasQuit = true;
                 System.out.println("Bye. Hope to see you again soon!");
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < numOfTasks - 1; i++) {
+                    System.out.println(tasks[i]);
+                }
             } else {
-                System.out.println(userInput);
+                tasks[numOfTasks - 1] = numOfTasks + ". " + userInput;
+                numOfTasks += 1;
+                System.out.println("added: " + userInput);
             }
             System.out.println("*******************************************");
         }
