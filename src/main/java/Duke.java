d75fb5e63c949e4311d7068fdd94405b67f20249
meixuanjin <jin.meixuan02@gmail.com>
2021-08-19 03:06:14 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,17 +7,27 @@ public class Duke {
         System.out.println("-----------------------------------------");
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
         System.out.println("-----------------------------------------");
+        List<String> tasks = new ArrayList<>();
+
+        int count = 1;
         while(true) {
-            String repeat = sc.nextLine();
-            if (repeat.equals("bye")) {
+            String task = sc.nextLine();
+            if (task.equals("bye")) {
                 System.out.println("-----------------------------------------");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("-----------------------------------------");
                 sc.close();
                 break;
             }
-            System.out.println("-----------------------------------------");
-            System.out.println(repeat);
+            if (task.equals("list")) {
+                System.out.println("-----------------------------------------");
+                tasks.forEach(System.out::println);
+            } else {
+                tasks.add(count + ". " + task);
+                count++;
+                System.out.println("-----------------------------------------");
+                System.out.println("added: " + task);
+            }
             System.out.println("-----------------------------------------");
         }
     }
