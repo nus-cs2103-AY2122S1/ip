d53e9e8dd2eaad2b86e72647184ab9e4e3d0daa4
Markus Lim <markuslim24@gmail.com>
2021-08-18 15:23:18 +0800

    level2-Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,27 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        String[] list = new String[100];
+        int counter = 0;
 
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         String input = scanner.nextLine();
         while(!input.equals("bye")){
-            System.out.println(input);
-            input = scanner.nextLine();
+            if(input.equals("list")){
+                for(int i = 0; i < list.length;i++){
+                    if(list[i] == null){
+                        break;
+                    }else {
+                        System.out.println((i + 1) + ". " + list[i]);
+                    }
+                }
+                input = scanner.nextLine();
+            }else {
+                System.out.println("added: " + input);
+                list[counter] = input;
+                counter++;
+                input = scanner.nextLine();
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
