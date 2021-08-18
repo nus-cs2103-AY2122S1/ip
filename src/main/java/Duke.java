2cda9eb66558cf0cd266061b70bad7088b8bc34b
Chen-Yiyang <iycspfa@gmail.com>
2021-08-18 15:30:54 +0800

    Implement add and list commands. Add in a separate Task class.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class Duke {
+class Duke {
     // constants for special commands
     private static final String CMD_BYE = "bye";
+    private static final String CMD_LIST = "list";
+
+    // static fields
+    private static List<Task> tasks = new ArrayList<>();
 
     private static void displayGreetings() {
         System.out.println("\tHello this is Yiyang-bot :D");
         System.out.println("\tWhat can I do for you?");
     }
+
+    private static void displayList() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(String.format("\t%d. %s",
+                    i+1, tasks.get(i)));
+        }
+    }
+
 
     private static void displayBye() {
         System.out.println("\tBye. Hope to see you again.");
@@ -20,7 +34,12 @@ public class Duke {
         String currCommand = sc.nextLine();
 
         while (!currCommand.equals(CMD_BYE)) {
-            System.out.println("\t" + currCommand);
+            if (currCommand.equals(CMD_LIST)) {
+                displayList();
+            } else {
+                tasks.add(new Task(currCommand));
+                System.out.println("\t added: " + currCommand);
+            }
 
             currCommand = sc.nextLine();
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,12 @@
+class Task {
+    private final String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
