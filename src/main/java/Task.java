1ed4eb3f094bd04624c342cb2739df0438ec717c
moreTriangles <adam@adam.local>
2021-08-18 23:57:28 +0800

    Add setter for isDone

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -14,6 +14,10 @@ public class Task {
         return isDone;
     }
 
+    public void setDone(boolean done) {
+        this.isDone = done;
+    }
+
     @Override
     public String toString() {
         return this.name;
