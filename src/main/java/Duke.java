981cbe612048f2d137700618d14176b1de3e4aec
Zhouyirui <72332263+Zhouyirui@users.noreply.github.com>
2021-08-18 10:14:32 +0800

    Level-2 of Duke bot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
+    private static ArrayList<Task> list = new ArrayList<Task>();
+    private static void printList() {
+        for(int i = 0; i < list.size(); i++) {
+            int index = i + 1;
+            System.out.println( index + ": " + list.get(i));
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String logo = " ____        _        \n"
@@ -15,7 +25,14 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(command);
+            if (command.equals("list")) {
+                printList();
+                continue;
+            }
+            Task newTask = new Task(command);
+            System.out.println("added: " + newTask);
+            list.add(newTask);
+
         }
     }
 }
