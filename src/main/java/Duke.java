f01a8253d04bd01077be0eb615b2585d0764d7c5
Hannah Chia <chia.hannahk@gmail.com>
2021-08-18 17:04:28 +0800

    Feat: Implement add and list command
    Solve Level 2.
    
    I use ArrayList to store the list, as it is quite adaptable to changes
    I might need to make in the future.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,8 @@ public class Duke {
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello, I'm Katheryne of the Adventurers Guild");
+        ArrayList<String> lst = new ArrayList<String>();
+        System.out.println("Greetings Traveller, I'm Katheryne of the Adventurers Guild.");
         System.out.println("Right now, I only echo your statements.");
         while (true) {
             try {
@@ -17,8 +18,14 @@ public class Duke {
                 if (userInput.equalsIgnoreCase("BYE")) {
                     System.out.println("Ad Astra Abyssoque, Traveller!");
                     break;
+                } else if (userInput.equalsIgnoreCase("list")) {
+                        System.out.println("Here's the list I've stored for you:");
+                        for(int i = 1; i <= lst.size(); i++) {
+                            System.out.println(i + ") " + lst.get(i - 1));
+                        }
                 } else {
-                    System.out.println(userInput);
+                    lst.add(userInput);
+                    System.out.println("Okay, I've added '"+ userInput + "' to your list");
                 }
             } catch(IOException exc) {
                 System.out.println("I/O Exception: " + exc);
