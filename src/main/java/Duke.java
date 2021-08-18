3e74c9c388e730eea4293f003ed8a4baeece00b0
tsy24 <szeying3@gmail.com>
2021-08-18 17:00:59 +0800

    Add ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,23 +6,30 @@ public class Duke {
         String startText = "Hello! I'm Duke\n" + "What can I do for you?";
         System.out.println(startText);
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> savedInputs = new ArrayList<>();
-        while (sc.hasNextLine()) {
-            String command = sc.nextLine();
+        ArrayList<Task> savedInputs = new ArrayList<>();
+        while (sc.hasNext()) {
+            String command = sc.next();
             if (command.equals("bye")) {
                 String closingMessage =  "Goodbye! Hope to see you again soon!\n";
                 System.out.println(outputTemplate(closingMessage));
                 sc.close();
                 break;
             } else if (command.equals("list")) {
-                String list = "";
+                String list = "These are the tasks in your list: \n";
                 for (int i = 0; i < savedInputs.size(); i++) {
-                    list += Integer.toString(i + 1) + ". " + savedInputs.get(i) + "\n";
+                    list += Integer.toString(i + 1) + ". " + savedInputs.get(i).toString() + "\n";
                 }
                 System.out.println(outputTemplate(list));
+            } else if (command.equals("done")) {
+                int taskNumber = sc.nextInt();
+                Task completedTask = savedInputs.get(taskNumber - 1);
+                completedTask.markAsDone();
+                String message = "Good work! Task is now marked as done: \n" + completedTask.toString() + "\n";
+                System.out.println(outputTemplate(message));
             } else {
-                savedInputs.add(command);
-                System.out.println(outputTemplate(command + "\n"));
+                String description = command + sc.nextLine();
+                savedInputs.add(new Task(description));
+                System.out.println(outputTemplate(description + "\n"));
             }
         }
     }
