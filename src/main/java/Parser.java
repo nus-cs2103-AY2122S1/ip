54b6c0f75b6223f3624ed9c57440134724a314b2
Siew Hui Zhuan <huizhuansam@hotmail.com>
2021-08-18 16:34:24 +0800

    Add parser methods

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,39 +1,45 @@
-import java.util.Arrays;
-
-/**
- * Utility class that handles parsing of strings to other useful data structures.
- */
+/** Utility class that handles parsing of strings to other useful data structures. */
 public class Parser {
-    /**
-     * Parses the user's input into a string array of tokens, delimited by whitespace.
-     *
-     * @param input the user's input
-     * @return an array of tokens represented as strings
-     * @throws InvalidInputException if the input string is null
-     */
-    public static String[] tokenParser(String input) throws InvalidInputException {
-        if (input == null) {
-            throw new InvalidInputException();
-        }
-        return input.trim().split("\\s+");
+    private static Commands commandParser(String fullCommand) {
+        return Commands.valueOfLabel(fullCommand.trim().split(" ")[0]);
     }
 
-    /**
-     * Parses the user's input into a string containing the description of the task.
-     *
-     * @param input the user's input
-     * @param command the type of command
-     * @return the description
-     * @throws EmptyDescriptionException if the input does not contain a description
-     */
-    public static String descriptionParser(String input, Command command) throws EmptyDescriptionException {
-        String[] tokens = Arrays.stream(input.trim()
-                .split((command.toString())))
-                .map(String::trim)
-                .toArray(String[]::new);
-        if (tokens.length < 2) {
-            throw new EmptyDescriptionException();
+    private static String contentParser(String fullCommand) {
+        String[] token = fullCommand.trim().split(" ", 2);
+        if (token.length < 2) {
+            return "";
         }
-        return tokens[1];
+        return token[1];
+    }
+
+    private static void blankCommandChecker(String fullCommand) throws EmptyCommandException {
+        if (fullCommand.trim().length() < 1) {
+            throw new EmptyCommandException();
+        }
+    }
+
+    public static Command parse(String fullCommand) throws DukeException {
+        blankCommandChecker(fullCommand);
+        Command command;
+        switch (commandParser(fullCommand)) {
+        case EXIT:
+            command = ExitCommand.of(contentParser(fullCommand));
+            break;
+        case LIST:
+            command = ListCommand.of(contentParser(fullCommand));
+            break;
+        case ADD:
+            command = AddCommand.of(contentParser(fullCommand));
+            break;
+        case DONE:
+            command = DoneCommand.of(contentParser(fullCommand));
+            break;
+        case DELETE:
+            command = DeleteCommand.of(contentParser(fullCommand));
+            break;
+        default:
+            throw new UnknownCommandException(fullCommand);
+        }
+        return command;
     }
 }
