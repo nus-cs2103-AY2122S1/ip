889bce652fba841a79833fc9c0b41e8346a9dde0
DonnyS57 <donnysuriyanto@gmail.com>
2021-08-18 15:11:08 +0800

    Finish Level 1.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        run();
+    }
+
+    public static void print(String text) {
+        System.out.println("=======================================");
+        text.lines().map(x -> "    " + x).forEach(x -> System.out.println(x));
+        System.out.println("=======================================");
+    }
+
+    public static void run() {
+        print("Hello! My name is Alexa \nHow can I help you today?");
+
+        while(true) {
+            Scanner newInput = new Scanner(System.in);
+            String input = newInput.nextLine();
+            if (input.equals("bye")) {
+                print("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                print(input);
+            }
+        }
+    }
+
+    public static void add() {
+
     }
 }
