c8b0f31ef9e5d7651833e3fe6f29a2a68d700a61
rish-16 <mail.rishabh.anand@gmail.com>
2021-08-19 00:19:24 +0800

    Add Task class for abstraction

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,41 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    static class Task {
+        private String description;
+        private Boolean status;
+
+        public Task(String desc) {
+            this.description = desc;
+            this.status = false;
+        }
+
+        public void setDone() {
+            // sets the task as completed
+            this.status = true;
+        }
+
+        public Boolean getStatus() {
+            return this.status;
+        }
+
+        public String getStatusString() {
+            return this.status ? "[X]" : "[ ]";
+        }
+
+        public String getDescription() {
+            return this.description;
+        }
+
+        public Boolean equals(Task t) {
+            if (t.getDescription().equals(this.description)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -13,8 +48,7 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
-        ArrayList<String> dataStore = new ArrayList<String>();
-        ArrayList<Boolean> status = new ArrayList<Boolean>();
+        ArrayList<Task> dataStore = new ArrayList<Task>();
 
         while (true) {
             Scanner sc = new Scanner(System.in);
@@ -26,20 +60,24 @@ public class Duke {
                 break;
             } else if (input.equals("list")) {
                 for (int i = 0; i < dataStore.size(); i++) {
-                    if (status.get(i).equals(true)) {
-                        System.out.println(i+1 + ". [X] " + dataStore.get(i));
+                    Task task = dataStore.get(i);
+
+                    if (task.getStatus()) {
+                        System.out.println(i+1 + ". " + task.getStatusString() + " " + task.getDescription());
                     } else {
-                        System.out.println(i+1 + ". [ ] " + dataStore.get(i));
+                        System.out.println(i+1 + ". " + task.getStatusString() + " " + task.getDescription());
                     }
                 }
             } else if (input.contains("done")) {
                 int idx = Integer.parseInt(input.split(" ")[1]);
+                Task task = dataStore.get(idx-1);
+                
                 System.out.println("Nice! I've marked this task as done: ");
-                System.out.println("[X] " + dataStore.get(idx));
-                status.set(idx-1, true);
+                task.setDone();
+                System.out.println(task.getStatusString() + " " + task.getDescription());
             } else {
-                dataStore.add(input);
-                status.add(false);
+                Task task = new Task(input);
+                dataStore.add(task);
                 System.out.println("added: " + input);
             }
         }
