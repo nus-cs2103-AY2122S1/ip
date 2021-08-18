0cc20fe6ccda5a557f88e4053eee6f667aff11ee
LuoDan <69646558+luodan01@users.noreply.github.com>
2021-08-19 02:03:51 +0800

    Add List and Add functionalities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private static List<String> taskList = new ArrayList<>();
+
+    private static void displayTasks() {
+        for (int i = 1; i < taskList.size() + 1; i++) {
+            System.out.println(i + ". " + taskList.get(i - 1));
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         String input = sc.nextLine();
+
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                displayTasks();
+            } else {
+                taskList.add(input);
+                System.out.println("added: " + input);
+            }
             input = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
