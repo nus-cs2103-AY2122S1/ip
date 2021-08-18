990dc289720d2f8154b0097887691e45b3a4b401
Kar Wi <t.karwi@yahoo.com>
2021-08-18 22:14:31 +0800

    Implement A-Enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,20 +5,117 @@ import java.util.NoSuchElementException;
 
 
 public class Duke {
+    private static final List<Task> list = new ArrayList<>();
+    private static final String divider = "____________________________________________________________";
+    private static final String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
     // TODO: handle more errors
     // TODO: use enum
-    public static void main(String[] args) {
-        List<Task> list = new ArrayList<>();
-        String divider = "____________________________________________________________";
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+    private enum Action {
+        LIST, DONE, REMOVE, TODO, DEADLINE, EVENT
+    }
+
+    private static void launch() {
         System.out.println(divider);
         System.out.println(logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         System.out.println(divider);
+    }
+
+    private static void close() {
+        System.out.println(divider + "\n" + "Bye. Hope to see you again soon!" + "\n" + divider);
+    }
+
+    private static void handleAction(Action action, Scanner sc) throws DukeException {
+        switch (action) {
+            case LIST: {
+                int index = 1;
+                for (Task task : list) {
+                    System.out.println(index + ". " + task);
+                    index++;
+                }
+                break;
+            }
+            case DONE: {
+                int index = sc.nextInt();
+                if (index < 1 || index > list.toArray().length) {
+                    System.out.println(list.toArray().length > 0
+                            ? "OOPS!!! I'm sorry, index is out of range! " +
+                            "Please try with a number from 1 to " + list.toArray().length
+                            : "OOPS!!! I'm sorry, there is nothing in the list yet.");
+                } else {
+                    Task task = list.get(index - 1);
+                    task.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:\n  " + task);
+                }
+                break;
+            }
+            case REMOVE: {
+                int index = sc.nextInt();
+                if (index < 1 || index > list.toArray().length) {
+                    System.out.println(list.toArray().length > 0
+                            ? "OOPS!!! I'm sorry, index is out of range! " +
+                            "Please try with a number from 1 to " + list.toArray().length
+                            : "OOPS!!! I'm sorry, there is nothing in the list yet.");
+                } else {
+                    Task task = list.remove(index - 1);
+                    System.out.println("Noted. I've removed this task:\n  " +
+                            task +
+                            "\nNow you have " + list.toArray().length + " tasks in the list.");
+                }
+                break;
+            }
+            case TODO: {
+                String description = sc.nextLine();
+                Task task = new Todo(description);
+                list.add(task);
+                System.out.println("Got it. I've added this task:\n  " +
+                        task +
+                        "\nNow you have " + list.toArray().length + " task(s) in the list.");
+                break;
+            }
+            case DEADLINE: {
+                String input2 = sc.nextLine();
+                String[] line = input2.split(" /by ");
+                if (line.length != 2) {
+                    throw new DukeException(
+                            "☹ OOPS!!! Seems like you have entered a wrong format for a deadline task. " +
+                                    "Try this instead: deadline <description> /by <date>"
+                    );
+                }
+                Task task = new Deadline(line[0], line[1]);
+                list.add(task);
+                System.out.println("Got it. I've added this task:\n  " +
+                        task +
+                        "\nNow you have " + list.toArray().length + " task(s) in the list.");
+                break;
+            }
+            case EVENT: {
+                String input2 = sc.nextLine();
+                String[] line = input2.split(" /at ");
+                if (line.length != 2) {
+                    throw new DukeException(
+                            "☹ OOPS!!! Seems like you have entered a wrong format for an event task. " +
+                                    "Try this instead: event <description> /at <date>"
+                    );
+                }
+                Task task = new Event(line[0], line[1]);
+                list.add(task);
+                System.out.println("Got it. I've added this task:\n  " +
+                        task +
+                        "\nNow you have " + list.toArray().length + " task(s) in the list.");
+                break;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch();
         try (Scanner sc = new Scanner(System.in)) {
             String input = sc.next();
             while (!input.equals("bye")) {
@@ -26,81 +123,27 @@ public class Duke {
                     System.out.println(divider);
                     switch (input) {
                         case "list": {
-                            int index = 1;
-                            for (Task task : list) {
-                                System.out.println(index + ". " + task);
-                                index++;
-                            }
+                            handleAction(Action.LIST, sc);
                             break;
                         }
                         case "done": {
-                            int index = sc.nextInt();
-                            if (index < 1 || index > list.toArray().length) {
-                                System.out.println(list.toArray().length > 0
-                                        ? "OOPS!!! I'm sorry, index is out of range! " +
-                                        "Please try with a number from 1 to " + list.toArray().length
-                                        : "OOPS!!! I'm sorry, there is nothing in the list yet.");
-                            } else {
-                                Task task = list.get(index - 1);
-                                task.markAsDone();
-                                System.out.println("Nice! I've marked this task as done:\n  " + task);
-                            }
+                            handleAction(Action.DONE, sc);
                             break;
                         }
                         case "remove": {
-                            int index = sc.nextInt();
-                            if (index < 1 || index > list.toArray().length) {
-                                System.out.println(list.toArray().length > 0
-                                        ? "OOPS!!! I'm sorry, index is out of range! " +
-                                        "Please try with a number from 1 to " + list.toArray().length
-                                        : "OOPS!!! I'm sorry, there is nothing in the list yet.");
-                            } else {
-                                Task task = list.remove(index - 1);
-                                System.out.println("Noted. I've removed this task:\n  " +
-                                        task +
-                                        "\nNow you have " + list.toArray().length + " tasks in the list.");
-                            }
+                            handleAction(Action.REMOVE, sc);
                             break;
                         }
                         case "todo": {
-                            String description = sc.nextLine();
-                            Task task = new Todo(description);
-                            list.add(task);
-                            System.out.println("Got it. I've added this task:\n  " +
-                                    task +
-                                    "\nNow you have " + list.toArray().length + " task(s) in the list.");
+                            handleAction(Action.TODO, sc);
                             break;
                         }
                         case "deadline": {
-                            String input2 = sc.nextLine();
-                            String[] line = input2.split(" /by ");
-                            if (line.length != 2) {
-                                throw new DukeException(
-                                        "☹ OOPS!!! Seems like you have entered a wrong format for a deadline task. " +
-                                                "Try this instead: deadline <description> /by <date>"
-                                );
-                            }
-                            Task task = new Deadline(line[0], line[1]);
-                            list.add(task);
-                            System.out.println("Got it. I've added this task:\n  " +
-                                    task +
-                                    "\nNow you have " + list.toArray().length + " task(s) in the list.");
+                            handleAction(Action.DEADLINE, sc);
                             break;
                         }
                         case "event": {
-                            String input2 = sc.nextLine();
-                            String[] line = input2.split(" /at ");
-                            if (line.length != 2) {
-                                throw new DukeException(
-                                        "☹ OOPS!!! Seems like you have entered a wrong format for an event task. " +
-                                                "Try this instead: event <description> /at <date>"
-                                );
-                            }
-                            Task task = new Event(line[0], line[1]);
-                            list.add(task);
-                            System.out.println("Got it. I've added this task:\n  " +
-                                    task +
-                                    "\nNow you have " + list.toArray().length + " task(s) in the list.");
+                            handleAction(Action.EVENT, sc);
                             break;
                         }
                         default: {
@@ -115,7 +158,7 @@ public class Duke {
                     input = sc.next();
                 }
             }
-            System.out.println(divider + "\n" + "Bye. Hope to see you again soon!" + "\n" + divider);
+            close();
         } catch (NoSuchElementException e) {
             // happens in unit test
         }
