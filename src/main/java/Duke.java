91bf25f694e67eda7e089439c34fe5c9980bc9ec
LuoZhijie-tom <e0492460@u.nus.edu>
2021-08-18 16:49:43 +0800

    added Level-3: mark as done and class Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,16 +3,21 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String command = new String();
-        ArrayList<String> store = new ArrayList<>();
+        ArrayList<Task> store = new ArrayList<>();
+
         System.out.println("Hello! I'm Tom! What can I do for you?");
         command = sc.nextLine();
         while(!command.equals("bye")){
             if(command.equals("list")){
                 for(int i = 0; i < store.size(); i++){
-                    System.out.println((i + 1) + "." + store.get(i));
+                    System.out.println((i + 1) + "." + "[" + store.get(i).getStatusIcon() + "] " + store.get(i).getDescription());
                 }
+            } else if(command.substring(0,4).equals("done")){
+                int textNumber = Integer.parseInt(command.substring(5));
+                store.get(textNumber - 1).done();
+                System.out.println("Nice! I've marked this task as done:\n" + "[" + store.get(textNumber - 1).getStatusIcon() + "] " + store.get(textNumber - 1).getDescription());
             } else {
-                store.add(command);
+                store.add(new Task(command));
                 System.out.println("added: " + command);
             }
             command = sc.nextLine();
