313b52a4187bb0d94d0237c74e074982b46964c3
Poh Hui-En Ruth <iluvcelebi@gmail.com>
2021-08-18 16:18:13 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.lang.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +8,42 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String linebreak = "~~*********___\\(owo)/___\\(owo)/___*********~~\n";
+
+        System.out.println(linebreak
+                + "Hewwo!! From:\n"
+                + logo
+                + "How mayw Iw hewlp youw, Mastwer? uwu\n"
+                + linebreak);
+
+        Scanner in = new Scanner(System.in);
+
+        // checks loop
+        boolean continueloop = true;
+
+        while (continueloop) {
+            String str = in.nextLine();
+
+            if (str.equalsIgnoreCase("bye")) {
+                // breaks loop
+                continueloop = false;
+            } else {
+                System.out.println(linebreak
+                        +  '\n'
+                        + str
+                        + '\n'
+                        + '\n'
+                        + linebreak);
+            }
+        }
+
+        // closing lines
+        System.out.println(linebreak
+                + '\n'
+                + "Goodbywe, Mastwer! Seew youw soown!"
+                + '\n'
+                + '\n'
+                + linebreak);
     }
 }
+
