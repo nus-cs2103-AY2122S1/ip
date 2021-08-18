3543f7eccdc70763186a7c5a9de1629b364dbccc
1waykiat <1waykiat@gmail.com>
2021-08-19 01:16:27 +0800

    A-Enums

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,44 @@ import java.util.ArrayList;
  */
 
 public class Duke {
+    public enum TaskType {
+        TODO, DEADLINE, EVENT
+    }
+
+    public static void addTask(TaskType t, String input, ArrayList<Task> list) {
+        try {
+            switch (t) {
+                case TODO:
+                    Task todo = new ToDo(input);
+                    list.add(todo);
+                    todo.addResponse(list.size());
+                    break;
+                case DEADLINE:
+                    if (input.split("/by ", 2).length < 2) {
+                        throw new DukeException("Deadline not specified!");
+                    }
+                    String desc = input.split("/by ", 2)[0];
+                    String dead = input.split("/by ", 2)[1];
+                    Task deadline = new Deadline(desc, dead);
+                    list.add(deadline);
+                    deadline.addResponse(list.size());
+                    break;
+                case EVENT:
+                    if (input.split("/at ", 2).length < 2) {
+                        throw new DukeException("Date of event not specified!");
+                    }
+                    String name = input.split("/at ", 2)[0];
+                    String at = input.split("/at ", 2)[1];
+                    Task event = new Event(name, at);
+                    list.add(event);
+                    event.addResponse(list.size());
+                    break;
+            }
+        } catch (DukeException e) {
+            System.out.println(e.toString().split(" ", 2)[1]);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -23,46 +61,30 @@ public class Duke {
             String[] inputSplit = input.split(" ", 2);
 
             try {
-                switch (inputSplit[0]) {
+                switch(inputSplit[0]) {
                     case "todo":
                         if (inputSplit.length < 2) {
-                            throw new DukeException("Description of ToDo cannot be empty!");
+                            throw new DukeException("Description of task cannot be empty!");
                         }
-                        Task todo = new ToDo(inputSplit[1]);
-                        list.add(todo);
-                        todo.addResponse(list.size());
+                        addTask(TaskType.TODO, inputSplit[1], list);
                         break;
                     case "deadline":
                         if (inputSplit.length < 2) {
-                            throw new DukeException("Description cannot be empty!");
-                        } else if (inputSplit[1].split("/by ", 2).length < 2) {
-                            throw new DukeException("Deadline not specified!");
+                            throw new DukeException("Description of task cannot be empty!");
                         }
-                        String desc = inputSplit[1].split("/by ", 2)[0];
-                        String dead = inputSplit[1].split("/by ", 2)[1];
-                        Task deadline = new Deadline(desc, dead);
-                        list.add(deadline);
-                        deadline.addResponse(list.size());
+                        addTask(TaskType.DEADLINE, inputSplit[1], list);
                         break;
                     case "event":
                         if (inputSplit.length < 2) {
-                            throw new DukeException("Description cannot be empty!");
-                        } else if (inputSplit[1].split("/at ", 2).length < 2) {
-                            throw new DukeException("Date of event not specified!");
+                            throw new DukeException("Description of task cannot be empty!");
                         }
-                        String name = inputSplit[1].split("/at ", 2)[0];
-                        String at = inputSplit[1].split("/at ", 2)[1];
-                        Task event = new Event(name, at);
-                        list.add(event);
-                        event.addResponse(list.size());
-                        break;
-                    case "bye":
-                        loop = false;
+                        addTask(TaskType.EVENT, inputSplit[1], list);
                         break;
                     case "list":
                         if (list.size() == 0) {
                             throw new DukeException("The list is empty!!");
                         }
+                        System.out.println("Here are your tasks:");
                         for (int i = 1; i <= list.size(); i++) {
                             Task task = list.get(i - 1);
                             System.out.println(i + "."
@@ -70,8 +92,8 @@ public class Duke {
                         }
                         break;
                     case "done":
-                        if(inputSplit.length < 2) {
-                            throw new DukeException("Please enter the index of the task you wish to delete.");
+                        if (inputSplit.length < 2) {
+                            throw new DukeException("Please enter the number of task to mark as completed!");
                         }
                         try {
                             int index = Integer.parseInt(inputSplit[1]);
@@ -88,15 +110,23 @@ public class Duke {
                         break;
                     case "delete":
                         int i = Integer.parseInt(inputSplit[1]);
-                        System.out.println("Noted. I've removed this task: ");
-                        System.out.println(list.get(i - 1));
-                        list.remove(i - 1);
-                        System.out.println("Now you have " + list.size() + " tasks in your list.");
+                        try {
+                            Task t = list.get(i - 1);
+                            System.out.println("Noted. I've removed this task: ");
+                            System.out.println(t);
+                            list.remove(i - 1);
+                            System.out.println("Now you have " + list.size() + " tasks in your list.");
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("That is an invalid index!!");
+                        }
+                        break;
+                    case "bye":
+                        loop = false;
                         break;
                     default:
                         throw new DukeException("That is not within my scope of action!");
                 }
-            } catch (DukeException e){
+            } catch (DukeException e) {
                 System.out.println(e.toString().split(" ", 2)[1]);
             }
         }
