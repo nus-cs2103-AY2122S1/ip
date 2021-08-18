36d4000d462611375df230d9c58b8cfed31c3a95
sharmaine <e0417626@u.nus.edu>
2021-08-19 03:40:18 +0800

    Level 3. Mark as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,26 +5,49 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> sList = new ArrayList<>();
+        List<Task> taskList = new ArrayList<>();
 
-        String divider = " ------------------------------------------- \n";
+        String divider = "\n ------------------------------------------- \n";
         String indent = "       ";
 
-        System.out.println(indent + "Hello! I'm Viper :) \n");
+        System.out.println(indent + "Hello! I'm Viper :)");
         System.out.println(indent + "How can I help you today?");
         System.out.println(divider);
 
         String str = sc.nextLine();
 
-        while (!str.equalsIgnoreCase("bye")) {
-            if (str.equalsIgnoreCase("list")) {
-                for (int i = 0; i < sList.size(); i++) {
-                    int curr = i + 1;
-                    System.out.println(indent + curr + ". " + sList.get(i));
+        while (!str.trim().equalsIgnoreCase("bye")) {
+            //list out items in the list
+            if (str.trim().equalsIgnoreCase("list")) {
+                System.out.println(indent + "Here are the tasks in your list:");
+                for (int i = 0; i < taskList.size(); i++) {
+                    int currIndex = i + 1;
+                    Task currTask = taskList.get(i);
+                    System.out.println(indent + currIndex + ". [" + currTask.getStatusIcon() + "] "
+                            + currTask.description);
                 }
+                System.out.println(divider);
+            }
+            //mark numbered task as done
+            else if (str.toLowerCase().contains("done")) {
+                int taskNo = Integer.parseInt(str.substring(5)) - 1;
+                //check if taskNo exists:
+                if (taskNo < taskList.size()) {
+                    Task currTask = taskList.get(taskNo);
+                    currTask.isDone = true;
+                    System.out.println(indent + "Good job on completing your task!");
+                    System.out.println(indent + "I've marked this task as done:");
+                    System.out.println(indent + "[" + currTask.getStatusIcon() + "] " + currTask.description);
+                }
+                else {
+                    System.out.println(indent + "Sorry, this task does not exist. :(");
+                }
+                System.out.println(divider);
+
             }
             else {
-                sList.add(str);
+                Task newTask = new Task(str);
+                taskList.add(newTask);
                 System.out.println(indent + "added: " + str);
                 System.out.println(divider);
             }
