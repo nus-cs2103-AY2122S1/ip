e1e04600de3d8d1c7afbd3e2e4266e655a1a3544
tanhuakun <huakun99@gmail.com>
2021-08-18 20:43:44 +0800

    Level 1, added Greet Echo Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static String endWord = "bye";
+
+    private void greetEchoExit() {
+        System.out.println("Hello, what can I do for you.\n");
+        Scanner sc = new Scanner(System.in);
+        String inputWord = sc.nextLine();
+        while(!inputWord.equals(Duke.endWord)) {
+            System.out.println(inputWord + "\n");
+            inputWord = sc.nextLine();
+        }
+        System.out.println("Bye bye");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +22,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Duke duke = new Duke();
+        duke.greetEchoExit();
     }
+
 }
