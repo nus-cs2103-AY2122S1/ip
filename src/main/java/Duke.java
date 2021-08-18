9f04dc1ed1c00c7cbb8c9c8533e2b4f850102de3
hanif-kamal <hanif.kamal47@gmail.com>
2021-08-19 01:15:22 +0800

    Add ToDo, Deadline and Event functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,9 +11,14 @@ public class Duke {
 
     private static void listTasks() {
         if (list.size() < 1) {
-            System.out.println("You haven't added anything to the list yet! Here, add something.");
+            System.out.println("You haven't added anything to the list yet! Try adding something.");
         } else {
-            System.out.println("Here are the tasks in your list: ");
+            if (list.size() == 1) {
+                System.out.println("Here is the sole task in your list:");
+            }
+            else {
+                System.out.println("Here are the tasks in your list:");
+            }
             for (int i = 1; i <= list.size(); i++) {
                 System.out.println(i + ". " + list.get(i - 1));
             }
@@ -23,7 +28,7 @@ public class Duke {
     private static void completeTask(int index) {
         Task task = list.get(index - 1);
         task.setDone();
-        System.out.println("Nice! I've marked this task as done: \n" + "  " + task);
+        System.out.println("Nice! I've marked this task as done:\n" + "  " + task);
     }
 
     public static void main(String[] args) {
@@ -49,9 +54,57 @@ public class Duke {
                 } catch (StringIndexOutOfBoundsException e) { //If 'done' is not followed by anything
                     System.out.println("Please add a number after the command 'done'. Try again.");
                 }
-            } else { //Should be a valid task to be added
-                addToList(new Task(input));
-                System.out.println("added: " + input);
+            } else if (input.startsWith("deadline")) {
+                if (!input.contains("/by")) {
+                    System.out.println("Please state the deadline for this task with /by! Try again.");
+                } else {
+                    try {
+                        int deadlineIndex = input.indexOf("/by") + 4; //need try catch
+                        String deadline = input.substring(deadlineIndex);
+                        String firstTrimmed = input.substring(9);
+                        String lastTrimmed = firstTrimmed.substring(0, firstTrimmed.indexOf("/by"));
+                        Deadline createdDeadlineTask = new Deadline(lastTrimmed, deadline);
+                        addToList(createdDeadlineTask);
+                        System.out.println("Got it. I've added this task:\n" + "  " + createdDeadlineTask + "\n"
+                                + "Now you have " + list.size() + " task" + (list.size() == 1 ? "" : "s") +
+                                " in the list.");
+                    } catch (StringIndexOutOfBoundsException e) {
+                        System.out.println("Please ensure that there is a task description and deadline. Try again.");
+                    }
+                }
+            } else if (input.startsWith("todo")){
+                try {
+                    String trimmed = input.substring(5);
+                    Todo createdTodoTask = new Todo(trimmed);
+                    addToList(createdTodoTask);
+                    System.out.println("Got it. I've added this task:\n" + "  " + createdTodoTask + "\n"
+                            + "Now you have " + list.size() + " task" + (list.size() == 1 ? "" : "s") +
+                            " in the list.");
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println("Please add the task information. Try again.");
+                }
+            } else if (input.startsWith("event")){
+                if (!input.contains("/at")) {
+                    System.out.println("Please state when the event will be held with /at! Try again.");
+                } else {
+                    try {
+                        int atIndex = input.indexOf("/at") + 4;
+                        String at = input.substring(atIndex);
+                        String firstTrimmed = input.substring(6);
+                        String lastTrimmed = firstTrimmed.substring(0, firstTrimmed.indexOf("/at"));
+                        Event createdEventTask = new Event(lastTrimmed, at);
+                        addToList(createdEventTask);
+                        System.out.println("Got it. I've added this task:\n" + "  " + createdEventTask + "\n"
+                                + "Now you have " + list.size() + " task" + (list.size() == 1 ? "" : "s") +
+                                " in the list.");
+                    } catch (StringIndexOutOfBoundsException e) {
+                        System.out.println("Please ensure that there is an event time. " +
+                                "Try again.");
+                    }
+                }
+            } else {
+                System.out.println("I didn't quite get what you meant. To add a task, begin with 'deadline', 'event'" +
+                        " or 'todo'.");
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
