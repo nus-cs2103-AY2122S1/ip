99729759ca79f62ffac1cf0b51e3279de5b7c427
moreTriangles <adam@adam.local>
2021-08-18 19:06:03 +0800

    Implement skeletal functions: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.print("Command: ");
+            String command = sc.next();
+            if (command.equals("bye")) {
+                System.out.println(dukeSays("¡Adiós! See you soon!"));
+                running = false;
+            } else {
+                System.out.println(dukeSays(command));
+            }
+        }
+    }
+
+    private static String dukeSays(String str) {
+        return "Duke says: " + str;
     }
 }
+
