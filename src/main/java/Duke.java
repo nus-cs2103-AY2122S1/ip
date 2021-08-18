781bd62c61a0c12ecb00fb6b6e5bae94d21ddacb
e0543860 <e0543860@u.nus.edu>
2021-08-18 16:44:21 +0800

    Level 4, ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,7 +14,7 @@ public class Duke {
         int arrayCounter = 0;
 
         //string array to hold keywords
-        String[] commandWords = {"bye", "list", "done"};
+        String[] commandWords = {"bye", "list", "done", "todo", "deadline", "event", "/by", "/at"};
 
         while(!finished) {
             String userResponse = sc.nextLine();
@@ -40,6 +40,45 @@ public class Duke {
                     System.out.println("Nice, I've marked this task as done!\n" + listOfTasks[numberIndex].toString());
                 } else {
                     System.out.println("Invalid task number!");
+                }
+            //to do
+            } else if (userResponse.contains(commandWords[3])) {
+                String desc = userResponse.substring(5);
+                listOfTasks[arrayCounter] = new Todo(desc);
+                arrayCounter++;
+
+                System.out.println("Got it. I'll add this task:");
+                System.out.println(listOfTasks[arrayCounter - 1].toString());
+                System.out.println("Now you've got " + arrayCounter + " tasks in your list.");
+            //deadline
+            } else if (userResponse.contains(commandWords[4])) {
+                if (userResponse.contains(commandWords[6])) {
+                    int slashIndex = userResponse.indexOf("/by");
+                    String desc = userResponse.substring(9,slashIndex - 1);
+                    String time = userResponse.substring(slashIndex + 4);
+                    listOfTasks[arrayCounter] = new Deadline(desc, time);
+                    arrayCounter++;
+
+                    System.out.println("Got it. I'll add this task:");
+                    System.out.println(listOfTasks[arrayCounter - 1].toString());
+                    System.out.println("Now you've got " + arrayCounter + " tasks in your list.");
+                } else {
+                    System.out.println("Invalid description!");
+                }
+            //event
+            } else if (userResponse.contains(commandWords[5])) {
+                if (userResponse.contains(commandWords[7])) {
+                    int slashIndex = userResponse.indexOf("/at");
+                    String desc = userResponse.substring(6,slashIndex - 1);
+                    String time = userResponse.substring(slashIndex + 4);
+                    listOfTasks[arrayCounter] = new Event(desc, time);
+                    arrayCounter++;
+
+                    System.out.println("Got it. I'll add this task:");
+                    System.out.println(listOfTasks[arrayCounter - 1].toString());
+                    System.out.println("Now you've got " + arrayCounter + " tasks in your list.");
+                } else {
+                    System.out.println("Invalid description!");
                 }
             } else {
                 System.out.println("added: " + userResponse);
