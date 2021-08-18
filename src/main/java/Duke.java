44a347d1a1ca16d72d2f777cf3539515eaebf506
meixuanjin <jin.meixuan02@gmail.com>
2021-08-19 02:43:08 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.*;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-----------------------------------------");
+        System.out.println("Hello! I'm Duke \nWhat can I do for you?");
+        System.out.println("-----------------------------------------");
+        while(true) {
+            String repeat = sc.nextLine();
+            if (repeat.equals("bye")) {
+                System.out.println("-----------------------------------------");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("-----------------------------------------");
+                sc.close();
+                break;
+            }
+            System.out.println("-----------------------------------------");
+            System.out.println(repeat);
+            System.out.println("-----------------------------------------");
+        }
     }
+
+
 }
