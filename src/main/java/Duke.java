df0721f1800d6391947713b4738e40b48cf423e1
moreTriangles <adam@adam.local>
2021-08-18 22:54:15 +0800

    Implement Add and List functionality, added some spacing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,23 +8,32 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
         Scanner sc = new Scanner(System.in);
+        TaskManager tm = new TaskManager();
         boolean running = true;
 
         while (running) {
-            System.out.print("Command: ");
-            String command = sc.next();
-            if (command.equals("bye")) {
-                System.out.println(dukeSays("¡Adiós! See you soon!"));
-                running = false;
-            } else {
-                System.out.println(dukeSays(command));
+            System.out.print("Command >> ");
+            String command = sc.nextLine();
+
+            switch (command) {
+                case "bye":
+                    System.out.println(dukeSays("¡Adiós! See you soon!"));
+                    running = false;
+                    break;
+                case "list":
+                    tm.list();
+                    break;
+                default:
+                    tm.add(new Task(command));
+                    System.out.println(dukeSays("added: " + command));
             }
         }
     }
 
     private static String dukeSays(String str) {
-        return "Duke says: " + str;
+        return "Duke says >> " + str + "\n";
     }
 }
 
