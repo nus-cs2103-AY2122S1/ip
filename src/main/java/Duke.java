d9dddaa988993828e18f9c0b377cc18969a9bc99
hpkoh <hpkoh99@gmail.com>
2021-08-18 17:24:44 +0800

    Level 1
    Add Greeting/Exit message
    Bot echoes user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String WELCOME_MSG = "wwwwwwwwwwwwwwwwhats up";
+    private static final String EXIT_MSG = "gggggggggggggggooooooodbye";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println(WELCOME_MSG);
+
+        Scanner sc  = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while(!input.equals("bye") ) {
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+
+        System.out.println(EXIT_MSG);
     }
 }
