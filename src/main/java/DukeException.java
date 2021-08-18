b463fb64819f02d3a365cb32cb3b479e619ecf13
DESKTOP-BDICAPP\james <e0543403@u.nus.edu>
2021-08-18 13:27:25 +0800

    Add javadocs to DukeException.java

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,4 +1,12 @@
+/**
+ * Encapsulation of an Exception thrown by Duke's processes.
+ */
 public class DukeException extends Exception {
+    /**
+     * Constructor of a DukeException
+     *
+     * @param message The message sent due to an Exception occurring.
+     */
     public DukeException(String message) {
         super(message);
     }
