77d2650b71c539f707e40ff9c54aeeb88edcb15d
meerian <myronneozx@gmail.com>
2021-08-18 15:10:21 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int pointer = 0;
 
         System.out.println("──────────────────────────────────────────");
@@ -18,32 +18,74 @@ public class Duke {
         System.out.println("What can i do for you?");
         System.out.println("──────────────────────────────────────────");
 
-        String input = scanner.nextLine();
+        String input = scanner.next();
 
         while (!Objects.equals(input, "bye")) {
             switch(input) {
                 case "list":
                     System.out.println("──────────────────────────────────────────");
                     for (int i = 0; i < pointer; i++) {
-                        String cur = list[i];
+                        String cur = list[i].toString();
                         int label = i + 1;
                         System.out.println(label + ". " + cur);
                     }
                     System.out.println("──────────────────────────────────────────");
-                    input = scanner.nextLine();
+                    input = scanner.next();
+                    break;
+
+                case "done":
+                    int temp = scanner.nextInt();
+                    if (temp < 1) {
+                        System.out.println("──────────────────────────────────────────");
+                        System.out.println("invalid value!");
+                        System.out.println("──────────────────────────────────────────");
+                        input = scanner.next();
+                        break;
+                    }
+                    Task cur = list[temp - 1];
+                    cur.Done();
+                    System.out.println("──────────────────────────────────────────");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(cur);
+                    System.out.println("──────────────────────────────────────────");
+                    input = scanner.next();
                     break;
 
                 default:
+                    String label = input + scanner.nextLine();
                     System.out.println("──────────────────────────────────────────");
-                    list[pointer] = input;
+                    Task task = new Task(label);
+                    list[pointer] = task;
                     pointer++;
-                    System.out.println("added: " + input);
+                    System.out.println("added: " + task);
                     System.out.println("──────────────────────────────────────────");
-                    input = scanner.nextLine();
+                    input = scanner.next();
                     break;
             }
         }
         System.out.println("──────────────────────────────────────────");
         System.out.println("Bye, hope to see you again soon!");
+    }
+
+    private static class Task {
+        private boolean done = false;
+        private String name;
+
+        Task(String name) {
+            this.name = name;
+        }
+
+        public void Done() {
+            this.done = true;
+        }
+
+        @Override
+        public String toString() {
+            if (done) {
+                return "[X] " + name;
+            } else {
+                return "[ ] " + name;
+            }
+        }
     }
 }
