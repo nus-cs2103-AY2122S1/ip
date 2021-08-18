a7a93bb1d8f82a98ae7a92eb1b5f42929579d878
Siew Hui Zhuan <huizhuansam@hotmail.com>
2021-08-18 16:30:59 +0800

    Add command class

---
+++ src/main/java/DeleteCommand.java
@@ -0,0 +1,23 @@
+public class DeleteCommand extends Command {
+    private int index;
+
+    private DeleteCommand(int index) {
+        this.index = index;
+    }
+
+    public static DeleteCommand of(String index) {
+        return new DeleteCommand(Integer.parseInt(index));
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui) throws NoSuchTaskException {
+        ui.print("Noted. I've removed this task:",
+                tasks.deleteTask(index).toString(),
+                String.format("Now you have %d %s in the list.", tasks.size(), tasks.size() == 1 ? "task" : "tasks"));
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
---
+++ src/main/java/ExitCommand.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+public class ExitCommand extends Command {
+    public static ExitCommand of(String content) throws BadInputFormatException {
+        if (content.trim().length() > 1) {
+            throw new BadInputFormatException();
+        }
+        return new ExitCommand();
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui) throws IOException {
+        ui.print("Bye. See ya l8er allig8er!", "\033[3m*shutting down......*\033[0m");
+        ui.cleanup();
+    }
+
+    @Override
+    public boolean isExit() {
+        return true;
+    }
+}
