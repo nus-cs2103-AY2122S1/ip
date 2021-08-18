dc89a7120b6c19efe364c5b7892afef0e9b35851
lethiciars <santosolethicia@gmail.com>
2021-08-18 20:54:44 +0800

    Add todo,event,deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,39 +13,54 @@ public class Duke {
         System.out.println("What do you need to do today?");
         System.out.println("_______________________");
 
-        Scanner commands = new Scanner(System.in);
-        String task = commands.nextLine();
+        Scanner input = new Scanner(System.in);
+        String task = input.nextLine();
         int i = 0;
 
         while (!task.equals("bye")) {
+            System.out.println("_______________________");
+
             if (task.equals("list")) {
-                System.out.println("_______________________");
+                System.out.println("Here's your to do list: ");
                 for (int j = 0; j < i; j++) {
                     System.out.printf("%d. %s%n", j + 1, tasks[j]);
                 }
-                System.out.println("_______________________");
+
             } else if (task.matches("done \\d{1}")){
                 int taskNo = Integer.parseInt(task.substring(5));
                 int taskIndex = taskNo - 1;
+
                 if (taskIndex < 0 || taskIndex >= i) {
-                    System.out.println("_______________________");
                     System.out.println("Oops! PLease enter a valid task number.");
-                    System.out.println("_______________________");
                 } else {
                     tasks[taskIndex].markDone();
-                    System.out.println("_______________________");
                     System.out.println("Good job! I've marked this task as done:");
                     System.out.println(tasks[taskIndex]);
-                    System.out.println("_______________________");
                 }
-            } else {
-                tasks[i] = new Task(task);
-                System.out.println("_______________________");
-                System.out.printf("added: %s%n", task);
-                System.out.println("_______________________");
+
+            } else if (task.matches("todo (.*)")){
+                tasks[i] = new ToDo(task.substring(5));
+                System.out.printf("added: %s%n", tasks[i]);
                 i += 1;
+                System.out.printf("Now you have %d tasks in your list.%n", i);
+
+            } else if (task.matches("deadline (.*)/by (.*)")){
+                int n = task.indexOf(" /by");
+                tasks[i] = new Deadline(task.substring(9, n), task.substring(n + 5));
+                System.out.printf("added: %s%n", tasks[i]);
+                i += 1;
+                System.out.printf("Now you have %d tasks in your list.%n", i);
+
+            } else if (task.matches("event (.*)/at (.*)")) {
+                int n = task.indexOf(" /at");
+                tasks[i] = new Event(task.substring(6, n), task.substring(n + 5));
+                System.out.printf("added: %s%n", tasks[i]);
+                i += 1;
+                System.out.printf("Now you have %d tasks in your list.%n", i);
             }
-            task = commands.nextLine();
+
+            System.out.println("_______________________");
+            task = input.nextLine();
 
         }
         System.out.println("_______________________");
