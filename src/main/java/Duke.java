9afcf0544899d9c014d45537e362636103561cde
nguyiyang <nguyiyang47@gmail.com>
2021-08-18 17:23:49 +0800

    add greeting, echo and exit functionality to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    /**
+     * Prints the user input.
+     *
+     * @param s String provided by the user.
+     */
+    public static void echo(String s) {
+        System.out.println(s);
+    }
+
+    /**
+     * Chatbot that echoes the user input unless 'bye' is given as input
+     *
+     * @param args The command line arguments. Not required here.
+     **/
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I am Lania! How may I be of assistance?");
+        System.out.println("Enter 'bye' to exit");
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        while(!input.equals("bye")) {
+            echo(input);
+            input = s.nextLine();
+        }
+        System.out.println("Bye. Lania looks forward to seeing you again!");
     }
 }
