338435b7d17d0fafa4269fbaa505b53dc1fd2f57
RonaldTanSingWei <ronaldtansingwei@gmail.com>
2021-08-18 11:33:55 +0800

    Increment Two, added to Duke.java, need to fix bug where space causes two items to be added to list instead of one

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,13 +9,27 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner input = new Scanner(System.in);
+        String[] storedInfo = new String[100];
+        int count = 0;
         while (input.hasNext()) {
             String in = input.next();
-            if (in.equals("bye")) {
+            if (in.equals("bye") || in.equals("Bye")) {
                 break;
             }
-                System.out.println(in);
+            if (in.equals("list") || in.equals("List")) {
+                for (String item: storedInfo) {
+                    if(item != null) {
+                        System.out.println(item);
+                    }
+                }
+                continue;
             }
+                System.out.println("added: " + in);
+                storedInfo[count] = in;
+                count++;
+
+            }
+
 
         System.out.println("Bye. Hope to see you again!");
     }
