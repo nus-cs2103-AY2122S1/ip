d2b01938162e8a1e9404705da9557223203188d9
bentanjunrong <seraphi123@yahoo.com.sg>
2021-08-18 18:29:03 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public abstract class Task {
+    private final String taskName;
+    private boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName.trim();
+        this.isDone = false;
+    }
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public void setIsDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", isDone ? "X" : " ", taskName);
+    }
+}
