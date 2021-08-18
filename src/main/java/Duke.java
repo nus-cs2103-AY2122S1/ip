134633f54cba59283863d228f7882e0cf30c6ad4
LuoZhijie-tom <e0492460@u.nus.edu>
2021-08-18 16:04:08 +0800

    added level-1: Greet,Echo,Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
+import java.util.*;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String command = new String();
+        System.out.println("Hello! I'm Tom! What can I do for you?");
+        command = sc.nextLine();
+        while(!command.equals("bye")){
+            System.out.println(command);
+            command = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
