697d7b010391b6422c5ff43252af3b4260bfde60
muhammad-khair <muhammad_khair@outlook.com>
2021-08-18 08:02:03 +0800

    Added tests to check for error-prone inputs

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -3,8 +3,10 @@
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
-REM delete output from previous run
+REM delete outputs from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
+if exist ACTUAL_EXCEPTION.TXT del ACTUAL_EXCEPTION.TXT
+
 
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
@@ -14,8 +16,16 @@ IF ERRORLEVEL 1 (
 )
 REM no error here, errorlevel == 0
 
+REM --- TEST NORMAL INPUTS ---
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
+
+REM --- TEST INPUTS THAT GIVE RISE TO EXCEPTIONS/ERROR MESSAGES ---
+REM run the program, feed commands from input_exception.txt file and redirect the output to the ACTUAL_EXCEPTION.TXT
+java -classpath ..\bin Duke < input_exception.txt > ACTUAL_EXCEPTION.TXT
+
+REM compare the output to the expected output
+FC ACTUAL_EXCEPTION.TXT EXPECTED_EXCEPTION.TXT
