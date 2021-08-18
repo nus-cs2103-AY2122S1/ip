49e69d2ec6c6f9355d5649d07af0b61bd67463a2
wanninglim <e0556585@u.nus.edu>
2021-08-19 00:22:58 +0800

    Clean up code

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,10 +1,5 @@
 public class DukeException extends RuntimeException {
-    String err;
     public DukeException(String err) {
         super(err);
-    }
-
-    public String toString() {
-        return err;
     }
 }
