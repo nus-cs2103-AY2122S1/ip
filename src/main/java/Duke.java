384c8a2501b5faade4a112a9eade30cba5fbb9f6
tanhuakun <huakun99@gmail.com>
2021-08-18 22:07:25 +0800

    Added Level-3 mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,43 +4,62 @@ public class Duke {
 
     public static String endWord = "bye";
 
-    String[] storedList;
+    Task[] storedList;
     int currentIndex;
 
     private Duke() {
-        storedList = new String[100];
+        storedList = new Task[100];
         currentIndex = 0;
     }
 
     private void addList() {
         System.out.println("Hello, what can I do for you.\n");
         Scanner sc = new Scanner(System.in);
-        String inputWord = sc.nextLine();
-        while(!inputWord.equals(Duke.endWord)) {
-            switch(inputWord) {
+        String[] strArr;
+        String input = sc.nextLine();
+        strArr = input.split(" ", 2);
+        String inputFirst = strArr[0];
+
+        while(!input.equals(Duke.endWord)) {
+            switch(inputFirst) {
                 case "list":
                     printList();
                     break;
+                case "done":
+                    markDone(getIndexFromStringArr(strArr));
+                    break;
                 default:
-                    addToList(inputWord);
+                    addToList(input);
                     break;
             }
-            inputWord = sc.nextLine();
+            input = sc.nextLine();
+            strArr = input.split(" ", 2);
+            inputFirst = strArr[0];
         }
         System.out.println("Bye bye");
     }
 
     private void addToList(String str) {
-        storedList[currentIndex] = str;
+        storedList[currentIndex] = new Task(str);
         currentIndex++;
         System.out.println("added: " + str + "\n");
     }
 
     private void printList() {
         for (int i = 0; i < currentIndex; i++) {
-            System.out.println(String.format("%d. %s", i + 1, storedList[i]));
+            System.out.println(String.format("%d.%s", i + 1, storedList[i]));
         }
         System.out.println("\n");
+    }
+
+    private int getIndexFromStringArr(String[] arr) {
+        return Integer.parseInt(arr[1]) - 1;
+    }
+
+    private void markDone(int index) {
+        storedList[index].markDone();
+        System.out.println(String.format("Task %d is done", index + 1));
+        System.out.println(storedList[index]);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/Task.java
@@ -0,0 +1,17 @@
+public class Task {
+    protected String taskName;
+    protected boolean isDone;
+
+    public Task(String taskName) {
+        isDone = false;
+        this.taskName = taskName;
+    }
+
+    public String toString() {
+        return String.format("[%s] %s", isDone ? "X" : "", taskName);
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+}
