4e914f9ea700d4655faa580e8d290c74f8356eff
weichang18 <e0543893@u.nus.edu>
2021-08-18 20:32:30 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,21 +9,32 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-                + "    My favorite partner is back! How can I help?\n"
+                + "      My favorite partner is back! How can I help?\n"
                 + "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        String taskList[] = new String[100];
         Scanner sc = new Scanner(System.in);
+        int i = 0;
         while (true) {
             String str = sc.nextLine();
-            if(str.equals("bye")) {
+            if (str.equals("bye")) {
                 System.out.print("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-                    + "     Have a good day, friend!\n"
-                    + "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                        + "      Have a good day, friend!\n"
+                        + "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
                 break;
+            } else if (str.equals("list")) {
+                System.out.print("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                    for (int j = 0; j < i; j++) {
+                        String listItem = "      " + (j + 1) + ". " + taskList[j] + "\n";
+                        System.out.print(listItem);
+                }
+                System.out.print("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
             } else {
+                taskList[i++] = str;
                 System.out.print("    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-                + "     " + str + "\n"
-                + "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                        + "      added: " + str + "\n"
+                        + "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                }
             }
         }
     }
-}
+
