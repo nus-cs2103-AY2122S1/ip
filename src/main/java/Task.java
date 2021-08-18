210c96ef674a2a9612a534c6da90b9de621b63e5
Imerbear <e0543864@u.nus.edu>
2021-08-19 02:48:33 +0800

    Tweaked Task to allow for children classes

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,49 +1,32 @@
-class Task {
-    private String Description;
+public class Task {
+    private String description;
     private boolean done;
+    public String type;
 
     /**
      * Private constructor for Task
      *
      * @param description is the string of the description of the given task
      */
-    private Task(String description) {
-        this.Description = description;
+    public Task(String description, String type) {
+        this.description = description;
         this.done = false;
+        this.type = type;
     }
 
-    /**
-     * Public method to call the private constructor of Task
-     *
-     * @param description is the string of the description of the given task
-     * @return A new Task Object with the given string description
-     */
-    public static Task createTask(String description) {
-        return new Task(description);
-    }
-
-    /**
-     * Getter for getting the description of the task
-     *
-     * @return a String of the description of the task
-     */
-    public String getDescription() {
-        return this.Description;
-    }
-
-    /**
-     * Getter for finding out whether the task is marked as completed or not
-     *
-     * @return a boolean on whether the task is marked as completed
-     */
-    public boolean getProgress() {
-        return this.done;
-    }
-
-    /**
-     * Setter for marking the task as complete
-     */
     public void setComplete() {
         this.done = true;
+    }
+
+    public String taskCompletion() {
+        if (done) {
+            return "[X]";
+        } else {
+            return "[ ]";
+        }
+    }
+    @Override
+    public String toString() {
+        return this.description;
     }
 }
