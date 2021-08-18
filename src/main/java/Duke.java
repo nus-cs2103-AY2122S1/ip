72f91329deda56043f204e46a6837263c9c7f69e
pualixue <pualixue@live.com.my>
2021-08-18 13:29:04 +0800

    Add support for deleting tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private static final String LINE = "-------------------------------------------------------";
-    private static final Task[] tasks = new Task[100];
-    private static int nextIndex = 0;
+    private static final ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         printWithLines("Hello! I'm 8-Bit!\nWhat can I do for you?");
@@ -47,6 +47,9 @@ public class Duke {
                 case "event":
                     processEvent(msg);
                     break;
+                case "delete":
+                    processDelete(msg);
+                    break;
                 default:
                     throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :(");
             }
@@ -56,11 +59,10 @@ public class Duke {
     }
 
     private static void addTask(Task task) {
-        tasks[nextIndex] = task;
-        nextIndex++;
+        tasks.add(task);
 
         printWithLines("Got it. I've added this task:\n  " + task
-                + "\nNow you have " + nextIndex + " tasks in the list.");
+                + "\nNow you have " + tasks.size() + " tasks in the list.");
     }
 
     private static void processList(String msg) throws DukeException {
@@ -69,15 +71,16 @@ public class Duke {
         }
 
         StringBuilder listOfTasks = new StringBuilder();
-        if (nextIndex == 0) {
-            listOfTasks = new StringBuilder("There are no tasks currently.");
+        if (tasks.size() == 0) {
+            listOfTasks = new StringBuilder("There are no tasks in your list currently.");
+            printWithLines(listOfTasks.toString());
         } else {
-            for (int i = 0; i < nextIndex; i++) {
-                String newTask = (i + 1) + ". " + tasks[i] + "\n";
+            for (int i = 0; i < tasks.size(); i++) {
+                String newTask = (i + 1) + ". " + tasks.get(i) + "\n";
                 listOfTasks.append(newTask);
             }
+            printWithLines("Here are the tasks in your list:\n" + listOfTasks);
         }
-        printWithLines("Here are the tasks in your list:\n" + listOfTasks);
     }
 
     private static void processDone(String msg) throws DukeException {
@@ -91,11 +94,11 @@ public class Duke {
         }
 
         int index = Integer.parseInt(msg.split(" ")[1]) - 1;
-        if (index >= nextIndex || index < 0) { // number exceeding no. of tasks in list or negative
+        if (index >= tasks.size() || index < 0) { // number exceeding no. of tasks in list or negative
             throw new DukeException("OOPS!!! Task " + (index + 1) + " does not exist.");
         }
-        tasks[index].markAsDone();
-        printWithLines("Great job on completing this task!\n" + tasks[index].toString());
+        tasks.get(index).markAsDone();
+        printWithLines("Great job on completing this task!\n" + tasks.get(index).toString());
     }
 
     private static void processToDo(String msg) throws DukeException {
@@ -134,5 +137,24 @@ public class Duke {
         String eventAt = descriptionAndAt[1];
         Event event = new Event(eventDescription, eventAt);
         addTask(event);
+    }
+
+    private static void processDelete(String msg) throws DukeException {
+        if (msg.split(" ").length != 2) { // not in the format "delete <integer>"
+            throw new DukeException("OOPS!!! Please enter in this format:\ndelete <integer>");
+        }
+        try {
+            Integer.parseInt(msg.split(" ")[1]);
+        } catch (NumberFormatException e) { // not integer
+            throw new DukeException("OOPS!!! Please enter in this format:\ndelete <integer>");
+        }
+
+        int index = Integer.parseInt(msg.split(" ")[1]) - 1;
+        if (index >= tasks.size() || index < 0) { // number exceeding no. of tasks in list or negative
+            throw new DukeException("OOPS!!! Task " + (index + 1) + " does not exist.");
+        }
+        Task deletedTask = tasks.remove(index);
+        printWithLines("Noted. I've removed this task:\n  " + deletedTask
+                + "\nNow you have " + tasks.size() + " tasks in the list.");
     }
 }
