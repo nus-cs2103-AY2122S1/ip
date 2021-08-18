00786ac636584737691caf973cb8a14af1ee6293
HongshuW <hongshu.wang.uni@gmail.com>
2021-08-18 20:44:58 +0800

    Add ToDo.java

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ToDo {
+public class ToDo extends Task {
+    /**
+     * Constructor of the class `ToDo`.
+     *
+     * @param description Description of the task.
+     */
+    public ToDo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
 }
