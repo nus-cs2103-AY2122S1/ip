1990201d90244eb338dd9c653c4160233b8c416c
somekoreanboi <77215389+somekoreanboi@users.noreply.github.com>
2021-08-19 06:56:22 +0800

    level 3 incremented

--- src/main/java/JWBot.java
+++ src/main/java/JWBot.java
@@ -9,22 +9,29 @@ public class JWBot {
         String greeting = "Wassup bro! I'm JWBot\n"
                 + "How can I help you?\n";
         String byeMessage = "You leaving already? See you soon bro!";
-        List<String> items = new ArrayList<>();
+        List<Task> items = new ArrayList<>();
 
         System.out.println(greeting);
 
         String input = "";
 
         while(!input.equals("bye")) {
-            input = sc.next();
+            input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println(byeMessage);
             } else if (input.equals("list")) {
+                System.out.println("OK bro, the tasks in your list are: ");
                 for (int i = 1; i < items.size() + 1; i++) {
-                    System.out.println(i + ". " + items.get(i - 1));
+                    System.out.println(i + ". " + items.get(i - 1).getStatus());
                 }
+            } else if (input.startsWith("done ")) {
+                String[] separated = input.split(" ");
+                int index = Integer.parseInt(separated[1]);
+                items.get(index - 1).markAsDone();
+                System.out.println("OK Bro, I noted you've done this task:\n" +
+                        items.get(index - 1).getStatus());
             } else {
-                items.add(input);
+                items.add(new Task(input));
                 System.out.println("OK bro, we just added: " + input);
             }
         }
