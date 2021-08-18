7c6199dcf79ad6cc603adffc9572a075f20f0613
lingshanng <nglingshan456@gmail.com>
2021-08-18 15:13:17 +0800

    Level 1 Greet Echo Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String separator = "------------------------------------------------------------------";
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println("Hello I'm Duke :)");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+
+        boolean end = false;
+        String endCmd = "bye";
+        Scanner sc = new Scanner(System.in);
+
+        while (!end) {
+            String cmd = sc.nextLine();
+            System.out.println(separator);
+
+            if (cmd.equals(endCmd)) {
+                System.out.println("Bye bye! See you again soon!");
+                end = true;
+            } else {
+                System.out.println(cmd);
+            }
+
+            System.out.println(separator);
+        }
     }
 }
