9d8a6ca050a7c4d741703f3aacbe8af050986323
janjanchen <ssp.18s20.janicechen@gmail.com>
2021-08-18 19:00:05 +0800

    Level-3

--- src/main/java/Recieve.java
+++ src/main/java/Recieve.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Recieve {
     private static int index = 1;
     private String input;
-    private static String[] inputs = new String[100];
+    private static Task[] inputs = new Task[100];
 
 
     public Recieve() {
@@ -14,16 +14,32 @@ public class Recieve {
 
         while (true) {
             input = sc.nextLine();
-            if (this.input.equals("bye")) {
+
+            if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            else if (this.input.equals("list")) {
-                for(int i = 1; i < index; i++) {
-                    System.out.println(i + ". " + inputs[i - 1]);
+            else if (input.startsWith("done ") && input.length() >= 6) {
+                int pos = Integer.parseInt(input.substring(5)) - 1;
+
+                if (pos < index - 1) {
+                    inputs[pos].markAsDone();
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println(inputs[pos].getStatusIcon() + inputs[pos].description);
                 }
-            } else {
-                inputs[index - 1] = this.input;
+                else {
+                    System.out.println("There are only " + pos + " tasks!");
+                }
+            }
+            else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for(int i = 1; i < index; i++) {
+                    System.out.println(i + "." + inputs[i - 1].getStatusIcon() + inputs[i - 1].description);
+                }
+            }
+            else {
+                Task task = new Task(input);
+                inputs[index - 1] = task;
                 index++;
                 System.out.println("added: " + this.input);
             }
