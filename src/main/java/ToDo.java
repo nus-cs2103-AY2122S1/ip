c13dd8e9e567d318a1c6a2dd2a741323c1b00515
kaldius <quantf@gmail.com>
2021-08-18 11:53:35 +0800

    Add ToDo class

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,17 @@
+/**
+ * The ToDo class extends Task and encapsulates
+ */
+public class ToDo extends Task {
+    public ToDo(String message) {
+        super(message);
+    }
+
+    /**
+     * Overrides toString() method.
+     * @return String representation of the ToDo object.
+     */
+    @Override
+    public String toString() {
+        return "[T] " + super.toString();
+    }
+}
