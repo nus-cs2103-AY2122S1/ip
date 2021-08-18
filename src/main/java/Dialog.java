06e12890b1ba32f713269e0a811ae74938c6b51c
Kan Jitpakdi <ryukan2000@gmail.com>
2021-08-19 01:09:41 +0800

    Level-1

---
+++ src/main/java/Dialog.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Dialog {
+    private static HashMap<String, Dialog> archive = new HashMap<>();
+    private final ArrayList<String> sentences;
+
+    private Dialog(ArrayList<String> sentences) {
+        this.sentences = sentences;
+    };
+
+    public void add(String sentence) {
+        this.sentences.add(sentence);
+    }
+
+    public static Dialog generate(String id) {
+        if (archive.containsKey(id)) {
+            return archive.get(id);
+        } else {
+            final Dialog newDialog = new Dialog(new ArrayList<>());
+            archive.put(id, newDialog);
+            return newDialog;
+        }
+    }
+
+    public static Dialog getDialog(String id) throws Exception{
+        return archive.get(id);
+    }
+
+    @Override
+    public String toString() {
+        String dialogs = this.sentences.stream().reduce("    ", (s1, s2) -> s1 + s2 + "\n    ");
+        return "    ____________________________________________________________ \n" +
+                dialogs +
+                "____________________________________________________________";
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        Dialog greeting = Dialog.generate("greeting");
+
+
+        // not sure if we are allowed to change the file name
+        String logo =
+                "     ___       __       __    ______  _______ \n" +
+                "        /   \\     |  |     |  |  /      ||   ____|\n" +
+                "       /  ^  \\    |  |     |  | |  ,----'|  |__   \n" +
+                "      /  /_\\  \\   |  |     |  | |  |     |   __|  \n" +
+                "     /  _____  \\  |  `----.|  | |  `----.|  |____ \n" +
+                "    /__/     \\__\\ |_______||__|  \\______||_______|\n";
+
+        greeting.add(logo);
+        greeting.add("Hello! I'm Alice, your personal assistant");
+        greeting.add("What can I do for you?");
+        System.out.println(greeting);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            Dialog local = Dialog.generate(input);
+            local.add(input);
+            System.out.println(local);
+            input = sc.nextLine();
+        }
+
+        Dialog bye = Dialog.generate("bye");
+        bye.add("Bye. Hope to see you again soon!");
+        System.out.println(bye);
     }
 }
