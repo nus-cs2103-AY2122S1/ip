22a5bb6366d7c10fed76cd660adb0af9d068a6e2
joshenx <e0525998@u.nus.edu>
2021-08-18 23:41:24 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+	
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        boolean isActive = true;
+        String welcomeMsg = "Hey, I'm Duke.\n"
+                + "What's up?\n";
+        String exitMsg = "Bye! Hope I helped!\n"
+                + "See you next time :)\n";
+
+        System.out.println(welcomeMsg);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Scanner sc = new Scanner(System.in);
+
+        while (isActive) {
+
+            String input = sc.nextLine();
+            if (input.equals("bye")) { // Exit routine
+                isActive = false;
+            } else {
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println(input);
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            }
+        }
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println(exitMsg);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 }
