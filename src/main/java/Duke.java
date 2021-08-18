1a925e28c0bb4974fdb6b80f7b31bbe089a9fe61
random689 <freezerfiregithub@gmail.com>
2021-08-18 20:56:29 +0800

    Added DukeExceptions to catch the errors for: 1. Invalid command for "done", "todo", "deadline" and "event" 2. Catch invalid numbers (out of range) for "done"
    Also updated to remove the extra space in each space (using trim)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
         String welcomeLine = "Hello! I'm Duke\n" + "What can I do for you?\n";
         String goodbyeLine = "Bye. Hope to see you again soon!";
         System.out.println(welcomeLine);
