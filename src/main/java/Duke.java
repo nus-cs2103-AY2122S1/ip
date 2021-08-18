fb0c2eda83fd9fdd597109da74454dcffa9e66f1
tanhuakun <huakun99@gmail.com>
2021-08-18 20:57:45 +0800

    Added Level-2 Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,15 +4,43 @@ public class Duke {
 
     public static String endWord = "bye";
 
-    private void greetEchoExit() {
+    String[] storedList;
+    int currentIndex;
+
+    private Duke() {
+        storedList = new String[100];
+        currentIndex = 0;
+    }
+
+    private void addList() {
         System.out.println("Hello, what can I do for you.\n");
         Scanner sc = new Scanner(System.in);
         String inputWord = sc.nextLine();
         while(!inputWord.equals(Duke.endWord)) {
-            System.out.println(inputWord + "\n");
+            switch(inputWord) {
+                case "list":
+                    printList();
+                    break;
+                default:
+                    addToList(inputWord);
+                    break;
+            }
             inputWord = sc.nextLine();
         }
         System.out.println("Bye bye");
+    }
+
+    private void addToList(String str) {
+        storedList[currentIndex] = str;
+        currentIndex++;
+        System.out.println("added: " + str + "\n");
+    }
+
+    private void printList() {
+        for (int i = 0; i < currentIndex; i++) {
+            System.out.println(String.format("%d. %s", i + 1, storedList[i]));
+        }
+        System.out.println("\n");
     }
 
     public static void main(String[] args) {
@@ -24,7 +52,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         Duke duke = new Duke();
-        duke.greetEchoExit();
+        duke.addList();
     }
 
 }
