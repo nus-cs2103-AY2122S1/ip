bf28beaa24f6236775c2bba83185d9affbc301ab
peilinye <peilinye18@gmail.com>
2021-08-18 22:36:23 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
+        String logo = " ____        _\n"
+                + "|  _ \\ _   _| | _____\n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke :) \nWhat can I do for you?");
+        System.out.println("Hello! I'm Duke :)\nWhat can I do for you?");
         for (int i = 0; i < 60; i++) {
             System.out.print("_");
         }
@@ -54,7 +54,7 @@ public class Duke {
     }
 
     public static void listTasks(Task[] tasks, int count) {
-        System.out.println("Here are the tasks in your list: ");
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < count; i++) {
             Task t = tasks[i];
             System.out.println((i + 1) + "." + t.toString());
@@ -70,7 +70,7 @@ public class Duke {
         } else {
             Task t = tasks[index];
             t.markAsDone();
-            System.out.println("Nice! I've marked this task as done: \n[X] " + t.name);
+            System.out.println("Nice! I've marked this task as done:\n[X] " + t.name);
         }
     }
 
