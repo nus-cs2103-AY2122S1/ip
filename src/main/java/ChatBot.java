b9636646f5b372d0429225b95e3afe55f441f660
thanwinnikki <thanwinnikki@gmail.com>
2021-08-18 17:01:34 +0800

    Added delete function to chat bot.

--- src/main/java/ChatBot.java
+++ src/main/java/ChatBot.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ChatBot {
 
-    private Task[] list = new Task[100];
+    private ArrayList<Task> list = new ArrayList<Task>();
     private int lastIndex = 0;
+    private int currIndex = 1;
 
 
     private String welcomeMessage() {
@@ -19,22 +21,17 @@ public class ChatBot {
     }
 
     private boolean listEmpty() {
-        boolean empty = true;
-        for (Task task : list) {
-            if (task != null) {
-                empty = false;
-                break;
-            }
-        }
-        return empty;
+        return list.isEmpty();
+    }
+
+    private void printItem(Task task) {
+        System.out.println(currIndex + "." + task.printTask());
+        currIndex++;
     }
 
     private void printList() {
-        for (int i = 0; i <= lastIndex; i++) {
-            if (list[i] != null) {
-                System.out.println(i + 1 + "." + this.list[i].printTask());
-            }
-        }
+        list.forEach((task) -> printItem(task));
+        currIndex = 1;
     }
 
     private boolean checkDone(String str) {
@@ -48,8 +45,9 @@ public class ChatBot {
     private void doneSeq(String str) {
         System.out.println("Good job for this thing done man:");
         int indexNum = Integer.parseInt(str.replaceAll("[^0-9]", ""));
-        list[indexNum - 1].setComplete();
-        System.out.println("   " + list[indexNum - 1].printTask());
+        Task currTask = list.get(indexNum - 1);
+        currTask.setComplete();
+        System.out.println("   " + currTask.printTask());
     }
 
     private boolean checkToDo(String str) {
@@ -65,8 +63,8 @@ public class ChatBot {
             throw new InputError("Description Please!");
         }
         System.out.println("Alrighty! I have added this task:");
-        list[lastIndex] = new ToDo(str.substring(5));
-        System.out.println("   " + list[lastIndex].printTask());
+        list.add(new ToDo(str.substring(5)));
+        System.out.println("   " + list.get(lastIndex).printTask());
         lastIndex++;
         System.out.println("Now you have " + lastIndex + " task(s) in total!");
     }
@@ -84,8 +82,8 @@ public class ChatBot {
             throw new InputError("Description Please!");
         }
         System.out.println("Alrighty! I have added this task:");
-        list[lastIndex] = new Deadline(str.substring(9, str.indexOf("/")), str.substring(str.indexOf("/") + 4));
-        System.out.println("   " + list[lastIndex].printTask());
+        list.add(new Deadline(str.substring(9, str.indexOf("/")), str.substring(str.indexOf("/") + 4)));
+        System.out.println("   " + list.get(lastIndex).printTask());
         lastIndex++;
         System.out.println("Now you have " + lastIndex + " task(s) in total!");
     }
@@ -93,7 +91,7 @@ public class ChatBot {
     private boolean checkEvent(String str) {
         boolean isEvent = false;
         if (str.length() >= 5) {
-            isEvent= str.substring(0,5).equals("event");
+            isEvent = str.substring(0,5).equals("event");
         }
         return isEvent;
     }
@@ -103,11 +101,34 @@ public class ChatBot {
             throw new InputError("Description Please!");
         } else {
             System.out.println("Alrighty! I have added this task:");
-            list[lastIndex] = new Event(str.substring(6, str.indexOf("/")), str.substring(str.indexOf("/") + 4));
-            System.out.println("   " + list[lastIndex].printTask());
+            list.add(new Event(str.substring(6, str.indexOf("/")), str.substring(str.indexOf("/") + 4)));
+            System.out.println("   " + list.get(lastIndex).printTask());
             lastIndex++;
             System.out.println("Now you have " + lastIndex + " task(s) in total!");
         }
+    }
+
+    private boolean checkDelete(String str) {
+        boolean isDelete = false;
+        if (str.length() >= 6) {
+            isDelete = str.substring(0,6).equals("delete");
+        }
+        return isDelete;
+    }
+
+    private void deleteSeq(String str) throws InputError {
+        if (str.length() == 5 ) {
+            throw new InputError("No Task to input");
+        }
+        int indexNum = Integer.parseInt(str.replaceAll("[^0-9]", ""));
+        if (indexNum > lastIndex) {
+            throw new InputError("Invalid Number");
+        }
+        System.out.println("Alrighty! I have deleted this task:");
+        Task removed = list.remove(indexNum - 1);
+        System.out.println("   " + removed.printTask());
+        lastIndex--;
+        System.out.println("Now you have " + lastIndex + " task(s) in total!");
     }
 
 
@@ -118,6 +139,7 @@ public class ChatBot {
         boolean todoInput = checkToDo(input);
         boolean deadlineInput = checkDeadLine(input);
         boolean eventInput = checkEvent(input);
+        boolean deleteInput = checkDelete(input);
 
         if (input.equals("bye")) {                                          //bye input
             System.out.println("See ya again later!");
@@ -149,6 +171,13 @@ public class ChatBot {
             } catch (InputError e) {
                 System.out.println("Here is the error boss. " + e);
             }
+        } else if (deleteInput) {
+            try {
+                deleteSeq(input);
+            } catch (InputError e) {
+                System.out.println("Here is the error boss. " + e);
+                System.out.println("You have " + lastIndex + " item(s) in your list!");
+            }
         } else {
             try {
                 throw new InputError("Invalid Input");
@@ -166,5 +195,4 @@ public class ChatBot {
         System.out.println(this.welcomeMessage());
         startInput();
     }
-
 }
