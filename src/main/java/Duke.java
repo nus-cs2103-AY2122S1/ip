8a7ff55536fd124ed44d253124c9de976d405b3e
LeeHyungWoon <leehyungwoonsamuel@gmail.com>
2021-08-18 15:08:07 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,7 +10,7 @@ public class Duke {
         System.out.println(start);
 
         // Array of String to store user inputs
-        String[] StringArray = new String[100];
+        Task[] TaskArray = new Task[100];
         int index = 0;
 
         // Define the scanner to read user inputs
@@ -27,29 +27,96 @@ public class Duke {
                 System.out.println(bye);
                 break;
             } else if (input.equals("list")) {
+                // If the user types "list", show the list of tasks
                 String list = "-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~\n";
                 for (int i = 0; i < index; i++) {
                     list = list
                             + String.valueOf(i + 1)
                             + ". "
-                            + StringArray[i]
+                            + TaskArray[i].getTaskState()
                             + "\n";
                 }
                 list += "-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~\n";
                 System.out.println(list);
-            } else {
+            } else if (input.length() > 5 && input.substring(0, 5).equals("done ")) {
+                // If the user types "done X" where X is a non-zero integer, mark the task as complete
+                // TODO Error handling for inputs
+                int doneIndex = Integer.parseInt(input.substring(5)) - 1;
+                TaskArray[doneIndex].toggleState();
+                String done = "-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~\n"
+                        + "Great! I've marked the following task as done:\n"
+                        + TaskArray[doneIndex].getTaskState()
+                        + "\n";
+                done += "-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~\n";
+                System.out.println(done);
+            }else {
                 // If the user types a command, store the command
-                StringArray[index] = input;
+                TaskArray[index] = new Task(input);
                 index++;
 
                 // After storing it, repeat it back to the user
                 String cmd = "-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~\n"
-                        + "Command Added: "
+                        + "Task Added: "
                         + input
                         + "\n"
                         + "-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~\n";
                 System.out.println(cmd);
             }
         }
+    }
+}
+
+/**
+ * Task class that encapsulates the individual task passed into the bot.
+ */
+class Task {
+
+    /**
+     * Variable that holds the task input as String
+     */
+    protected String task;
+    protected boolean done;
+
+    /**
+     * Constructor for the Task class
+     */
+    Task(String task) {
+        this.task = task;
+        this.done = false;
+    }
+
+    /**
+     * Retrieves ONLY the task that the class is holding.
+     *
+     * @return The task String held by the Task object.
+     */
+    public String getTask() {
+        return this.task;
+    }
+
+    /**
+     * Retrieves ONLY the state of the task.
+     * Displays X if done, else displays whitespace.
+     *
+     * @return The String representation of the task completion state.
+     */
+    public String getState() {
+        return this.done ? "X" : " ";
+    }
+
+    /**
+     * Retrieves the completion state of the task, followed by the task name.
+     *
+     * @return The String representation of the task completion state and the task name.
+     */
+    public String getTaskState() {
+        return this.done ? "[X] " + this.task : "[ ] " + this.task;
+    }
+
+    /**
+     * Toggles the current state by negating the boolean state value. (Done vs not done)
+     */
+    public void toggleState() {
+        this.done = !this.done;
     }
 }
