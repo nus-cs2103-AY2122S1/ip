1228767f2e31a2fe1851b9aaca38b2380e56628d
atyhamos <atyhalive@gmail.com>
2021-08-18 12:32:52 +0800

    Add Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String horizontalBars = "\n____________________________________________________________\n";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String name = "Duchess";
+        PrettyPrint("Good day. I am " + name + "\nWhat can I do for you?");
+        String output = sc.nextLine();
+        while (!output.equals("bye"))
+        {
+            PrettyPrint(output);
+            output = sc.nextLine();
+        }
+        PrettyPrint("I bid thee farewell.");
     }
+    public static void PrettyPrint(String input)
+    {
+        System.out.println(horizontalBars + input + horizontalBars);
+    }
+
+
 }
