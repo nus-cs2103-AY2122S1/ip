6d55de3ba12ba580f198e56ccffb9e2902b05a56
Sourabh Jaiswal <sourabhrj31@gmail.com>
2021-08-18 16:33:17 +0800

    creating a list of commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,38 +1,66 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Duke {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private Scanner sc = new Scanner(System.in);
+    private ArrayList<String> dukeList = new ArrayList<String>();
+
+    public static void main(String[] args)  {
+        Duke duke = new Duke();
+        duke.runDuke();
+    }
+
+    public void runDuke() {
         displayWelcomeMessage();
-        String input = br.readLine();
-        while(!input.equals("bye")) {
-            displayCommand(input);
-            input = br.readLine();
+        String command = sc.nextLine();
+        while(!command.equals("bye")) {
+            executeCommand(command);
+            command = sc.nextLine();
         }
         displayByeMessage();
     }
 
-    static void printLines() {
+    void printLines() {
         System.out.println("------------------------------------------------------------------");
     }
 
-    static void displayWelcomeMessage() {
+    void displayWelcomeMessage() {
         printLines();
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         printLines();
     }
 
-    static void displayByeMessage() {
+    void displayByeMessage() {
         printLines();
         System.out.println("Bye. Hope to see you again soon!");
         printLines();
     }
 
-    static void displayCommand(String command) {
+     void displayCommand(String command) {
         printLines();
         System.out.println(command);
+        printLines();
+    }
+
+    void executeCommand(String command) {
+        if(!command.equals("list") && !command.equals("bye")) {
+            addToDukeList(command);
+        } else if (command.equals("list")) {
+            displayDukeList();
+        }
+    }
+
+    void addToDukeList(String command) {
+        dukeList.add(command);
+        displayCommand("added: " + command);
+    }
+
+    void displayDukeList() {
+        printLines();
+        for (int i = 0;i < dukeList.size(); i++) {
+            System.out.println( (i+1) + ". " + dukeList.get(i));
+        }
         printLines();
     }
 }
