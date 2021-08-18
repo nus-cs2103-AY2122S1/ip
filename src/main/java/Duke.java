5d87069e82739024f68386ada7fd18166b725193
lwj1711 <limweijie1711@yahoo.com.sg>
2021-08-18 15:16:41 +0800

    Add mark as done feature.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -12,7 +11,8 @@ public class Duke {
     public static void main(String[] args) {
 
         // Prints initial message as prompt.
-        System.out.printf("%s\nGreetings! This is Elsa.\nWhat can I do for you?\n%s\n", LINE_HORIZONTAL, LINE_HORIZONTAL);
+        System.out.printf("%s\nGreetings! This is Elsa.\n" +
+                "What can I do for you?\n%s\n", LINE_HORIZONTAL, LINE_HORIZONTAL);
 
         // Initializes scanner to take input from user.
         Scanner scanner = new Scanner(System.in);
@@ -23,12 +23,15 @@ public class Duke {
 
         // Takes in input and performs actions accordingly.
         while(!(input = scanner.nextLine()).equalsIgnoreCase("bye")) {
-            switch (input.toLowerCase()) {
-                case "list":
-                    printList(numTasks);
-                    break;
-                default:
-                    numTasks = addTask(input, numTasks);
+            // If user asks for list of tasks.
+            if (input.equalsIgnoreCase("list")) {
+                printList(numTasks);
+                // If user wants to mark a task as done.
+            } else if (input.toLowerCase().indexOf("done") != -1){
+                int toMark = Integer.parseInt(input.substring(5));
+                markTaskAsDone(toMark);
+            } else {
+                numTasks = addTask(input, numTasks);
             }
         }
 
@@ -46,7 +49,7 @@ public class Duke {
         System.out.println(LINE_HORIZONTAL);
         for (int i = 0; i < numTasks; i++) {
             String taskName = list[i].toString();
-            System.out.printf("%d. %s\n", i + 1, taskName);
+            System.out.printf("%d.%s\n", i + 1, taskName);
         }
         System.out.println(LINE_HORIZONTAL);
     }
@@ -62,5 +65,16 @@ public class Duke {
         list[numTasks] = new Task(input);
         System.out.printf("%s\nGotcha!\nadded: %s\n%s\n", LINE_HORIZONTAL, input, LINE_HORIZONTAL);
         return numTasks + 1;
+    }
+
+    /***
+     * Marks the corresponding task as done and prints confirmation.
+     * @param toMark The index of the task to be marked.
+     */
+    public static void markTaskAsDone(int toMark) {
+        list[toMark - 1].markAsDone();
+        System.out.printf("%s\nGreat job!\n" +
+                "The following task is marked as done:\n", LINE_HORIZONTAL);
+        System.out.printf("\t%s\n%s\n", list[toMark - 1].toString(), LINE_HORIZONTAL);
     }
 }
