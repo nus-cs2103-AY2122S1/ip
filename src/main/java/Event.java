5f40f3c45189713514a5b0e3edfcdcc608d35d98
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-08-19 01:39:35 +0800

    Create Event.java

---
+++ src/main/java/Event.java
@@ -0,0 +1,14 @@
+public class Event extends Task {
+    protected String time;
+
+    public Event(String name, String time){
+        super(name)
+        this.time = time;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("[E][%s] %s (at: %s)",
+                this.isDone ? "X" : " ", name, time);
+    }
+}
