a06d902780ac3a3da9c6597de105f1067ace540f
Ryan Chung <timelord0830@gmail.com>
2021-08-18 14:13:05 +0800

    Add functionality to add todo, deadline and event type tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     private final static String GREETING_MSG = "Hello! I'm Duke\nWhat can I do for you?";
     private final static String GOODBYE_MSG = "Bye. Hope to see you again soon!";
+    private final static String ADD_TASK_MSG_TEMPLATE = "Got it. I've added this task:\n\t%s"
+                                                        + "\nNow you have %d tasks in the list.\n";
     private final static Scanner SCANNER = new Scanner(System.in);
     private final static Task[] tasks = new Task[100];
     private static int numTasks = 0;
@@ -15,10 +17,23 @@ public class Duke {
         System.out.println(GOODBYE_MSG);
     }
 
-    private static void addTask(String description) {
-        final Task taskToAdd = new Task(description);
+
+    private static void addTodo(String description) {
+        Task taskToAdd = new Todo(description);
         tasks[numTasks++] = taskToAdd;
-        System.out.printf("added: %s%n", taskToAdd.getDescription());
+        System.out.printf(ADD_TASK_MSG_TEMPLATE, taskToAdd, numTasks);
+    }
+
+    private static void addDeadline(String description, String by) {
+        Task taskToAdd = new Deadline(description, by);
+        tasks[numTasks++] = taskToAdd;
+        System.out.printf(ADD_TASK_MSG_TEMPLATE, taskToAdd, numTasks);
+    }
+
+    private static void addEvent(String description, String at) {
+        Task taskToAdd = new Event(description, at);
+        tasks[numTasks++] = taskToAdd;
+        System.out.printf(ADD_TASK_MSG_TEMPLATE, taskToAdd, numTasks);
     }
 
     private static void listTasks() {
@@ -28,22 +43,22 @@ public class Duke {
         }
     }
 
-    private static boolean isMarkTask(String cmd) {
-        String[] cmd_split = cmd.split(" ");
-        return cmd_split.length == 2 && cmd_split[0].equals("done") && cmd_split[1].matches("[0-9]+");
-    }
-
 
     public static void main(String[] args) {
         greet();
         for (String cmd = SCANNER.nextLine(); !cmd.equals("bye"); cmd = SCANNER.nextLine()) {
-            String[] cmd_split = cmd.split(" ");
-            if (isMarkTask(cmd)) {
-                tasks[Integer.parseInt(cmd_split[1]) - 1].markAsDone();
+            if (cmd.matches("todo .+")) {
+                addTodo(cmd.substring("todo ".length()));
+            } else if (cmd.matches("deadline .+ /by .+")) {
+                String[] bySplit = cmd.split(" /by ", 2);
+                addDeadline(bySplit[0].substring("deadline ".length()), bySplit[1]);
+            } else if (cmd.matches("event .+ /at .+")) {
+                String[] atSplit = cmd.split(" /at ", 2);
+                addEvent(atSplit[0].substring("event ".length()), atSplit[1]);
             } else if (cmd.equals("list")) {
                 listTasks();
-            } else {
-                addTask(cmd);
+            } else if (cmd.matches("done [0-9]+")) {
+                tasks[Integer.parseInt(cmd.substring("done ".length())) - 1].markAsDone();
             }
         }
         exit();
