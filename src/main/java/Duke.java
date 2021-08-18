f8c13f40d320660839eedb63856bca6262c57dd7
nniiggeell <e0559733@u.nus.edu>
2021-08-19 00:02:29 +0800

    Update Duke.java for level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,18 +9,24 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         //System.out.println("Hello from\n" + logo);
         Response response = new Response();
+        int position = 0;
         Scanner sc = new Scanner(System.in);
         response.greet();
         String text = sc.nextLine();
         while (!text.equals("bye")) {
             if (text.equals("list")) {
                 response.display();
-            } else {
-                response.echo(text);
+            } else if (text.contains("done")) {
+                String[] splitWords = text.split(" ");
+                //convert the number behind "done" into an int
+                int pos = Integer.parseInt(splitWords[1]);
+                response.markDone(pos - 1);
+            }
+            else {
+                response.echo(new Task(text, position));
             }
             text = sc.nextLine();
         }
         response.exit();
-
     }
 }
