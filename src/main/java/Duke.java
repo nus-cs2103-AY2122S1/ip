8f17707ee9d46016894f95a91b4c7c2551bc8e56
BananaTechs <tehogben@gmail.com>
2021-08-18 19:15:41 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
+    static Scanner sc = new Scanner(System.in);
+    static boolean running = true;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +11,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("hello name is duke");
+        System.out.println("how is help today;");
+
+        while (running) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("okay is bye!!");
+                running = false;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
