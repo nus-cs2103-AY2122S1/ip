7bdbffc488cee85ab2fbe0e92f44d0ec238450dc
tanqiya <35829668+tanqiya@users.noreply.github.com>
2021-08-19 07:39:34 +0800

    Create Duck that echos user input. Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //enum userFunctions {BYE};
+        String GREETING = "Hello! I'm Duck \n"
+                + "*quack*  >(.)__\n"
+                + "          (___/ \n"
+                + "What can I do for you?";
+        String BYE = "Bye. Hope to see you again soon!\n"
+                + "   __(.)>   *quack*\n"
+                + "~~ \\___)\n";
+        String userInput = null;
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println(GREETING);
+        while (true) {
+            userInput = myObj.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(BYE);
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
+
+
     }
+
+
 }
