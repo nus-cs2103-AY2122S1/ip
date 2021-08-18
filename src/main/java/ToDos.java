2af3c8126c264efda2bef11c3f3b43dc54b4814b
DESKTOP-BDICAPP\james <e0543403@u.nus.edu>
2021-08-18 12:53:48 +0800

    Add javadocs to ToDos.java

--- src/main/java/ToDos.java
+++ src/main/java/ToDos.java
@@ -1,9 +1,23 @@
+/**
+ * Encapsulates a task to be done.
+ */
 public class ToDos extends Task {
 
+    /**
+     * Constructor for a ToDos object.
+     *
+     * @param description Description of the task.
+     */
     public ToDos(String description) {
         super(description);
     }
 
+
+    /**
+     * Returns the string form of the ToDos object.
+     *
+     * @return The string form of the ToDos.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
