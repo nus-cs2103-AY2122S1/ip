f096d44c4e26d8ada5a4922f069e6ba276683d81
tianyue58 <e0563952@u.nus.edu>
2021-08-18 17:44:47 +0800

    add "Add", "List" features in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,11 +4,14 @@ public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello! I'm Duke created by Tianyue\n" +
+        String[] list = new String[100];
+        int cnt = 1;
+        System.out.println("Hello! I'm Duke created by Tianyue.\n" +
                 "What can I do for you?");
 
 
         String text = scanner.nextLine();
+
 
 
         while(!text.isEmpty()) {
@@ -17,8 +20,21 @@ public class Duke {
                 break;
             }
 
-            System.out.println(text);
-            text = scanner.nextLine();
+            if (text.equals("list")) {
+                for (int i = 0; i < cnt - 1; i++) {
+                    System.out.println(i + 1 + ". " + list[i]);
+                }
+                text = scanner.nextLine();
+            }
+
+            else {
+                System.out.println("added: " + text);
+                list[cnt - 1] = text;
+                cnt++;
+                text = scanner.nextLine();
+            }
+
+
         }
     }
 }
