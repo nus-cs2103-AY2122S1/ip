95d0f856520518a5df55b9cc6afec3fc7be78f4e
DESKTOP-BDICAPP\james <e0543403@u.nus.edu>
2021-08-18 13:23:19 +0800

    Add javadocs to Events.java

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -1,11 +1,26 @@
+/**
+ * Encapsulates a task to be done at a specified time.
+ */
 public class Events extends Task {
+    /** When the task begins */
     private String limit;
 
+    /**
+     * Constructor of an Events object.
+     *
+     * @param description Description of the task.
+     * @param limit When the task begins.
+     */
     public Events(String description, String limit) {
         super(description);
         this.limit = limit;
     }
 
+    /**
+     * Returns the string form of the Events object.
+     *
+     * @return The string form of the Events.
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (at: " + this.limit + ")";
