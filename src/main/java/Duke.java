feee88c4c1f0928a4767132f8b9ef0cbc61e01eb
dt-td <77134471+dt-td@users.noreply.github.com>
2021-08-18 12:53:05 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        startDeanBot();
+    }
+
+    // Starts Dean bot.
+    private static void startDeanBot() {
+        String logo = "   ____                       \n"
+                +     "  |  _ \\  ___  __ _ _ ___    \n"
+                +     "  | | | |/ _ \\/ _` | '_  \\  \n"
+                +     "  | |_| |  __| (_| | | | |    \n"
+                +     "  |____/ \\___|\\__,_|_| |_|  \n";
+        printDoubleBorder();
+        System.out.println("Hello! I'm:\n" + logo + "\nHow may I help you?");
+        printDoubleBorder();
+
+        Scanner input = new Scanner(System.in);  // Create a Scanner object
+
+        while (input.hasNextLine()) {
+            String userInput = input.nextLine();
+            if (userInput.equals("bye")) {
+                printSingleBorder();
+                System.out.println("Bye. Hope to see you again soon!");
+                printSingleBorder();
+                break;
+            } else {
+                printSingleBorder();
+                System.out.println(userInput);
+                printSingleBorder();
+            }
+        }
+    }
+
+    // Print double line border.
+    private static void printDoubleBorder() {
+        System.out.println("==================================================");
+    }
+
+    // Prints single line border.
+    private static void printSingleBorder() {
+        System.out.println("--------------------------------------------------");
     }
 }
