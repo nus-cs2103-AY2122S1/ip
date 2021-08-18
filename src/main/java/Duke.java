1b11f781c0f2cf6202a352e9b39e315dd99a4294
Kai Siang <kslui99@gmail.com>
2021-08-19 01:20:38 +0800

    complete level-3: mark as done, and extension: A-Classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static ArrayList<String> listOfItems = new ArrayList<>();
+    private ArrayList<Task> tasks;
+
+    public Duke() {
+        this.tasks = new ArrayList<>();
+    }
 
     /**
      * Formats an output by adding a top and bottom border and displays it to the terminal
@@ -14,6 +18,8 @@ public class Duke {
     }
 
     public static void main(String[] args) {
+        Duke duke = new Duke();
+
         // Greet the user
         String introduction = "Hello! I'm Horacio, your personal assistant. \n" +
                 "\t What can I do for you? \n";
@@ -28,15 +34,22 @@ public class Duke {
                 display("Bye. Hope to see you again soon!");
                 break;
             } else if (input.equals("list")) {
-                // Display list of items when the user types list
+                // Display list of tasks when the user types list
                 String output = "";
-                for (int i = 0; i < listOfItems.size(); i++) {
-                    output += String.valueOf(i + 1) + ". " + listOfItems.get(i) + "\n\t ";
+                for (int i = 0; i < duke.tasks.size(); i++) {
+                    output += String.valueOf(i + 1) + ". " + duke.tasks.get(i) + "\n\t ";
                 }
                 display(output);
+            } else if (input.substring(0, 4).equals("done")) {
+                // Mark task as done when the user types "done x" where x is an integer
+                int index = Integer.parseInt(input.substring(5, 6));
+                Task t = duke.tasks.get(index - 1);
+                t.markAsDone();
+                display("Nice! I've marked this task as done: \n\t\t " + t);
             } else {
-                // Otherwise, store input text into list
-                listOfItems.add(input);
+                // Otherwise, store task into list
+                Task t = new Task(input);
+                duke.tasks.add(t);
                 display("added: " + input);
             }
         }
