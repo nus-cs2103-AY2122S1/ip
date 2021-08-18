7f6a9e457c55158d82ebfa53f518c7dc7b9f1967
WangGLJoseph <joseph.cnsg@gmail.com>
2021-08-18 20:25:55 +0800

    Level-2: Added Add, List functions for Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-    private static int lv = 1;
-    private static String[] features = {"Nothing", "Greet, Echo, Exit"};
-    private static String[] featuresCombined;
+    private static int lv = 2;
+    private static String[] features = {"", "Greet, Echo, Exit", ", Add, List"};
     private static boolean canExit = false;
 
     /**
@@ -27,11 +27,17 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+
+        // populating featuresCombined so each level has all elements of levels before it
+        StringBuilder featuresCombined = new StringBuilder();
+        for (int count = 0; count <= lv; count++) {
+            featuresCombined.append(features[count]);
+        }
+
         // Welcome message
         String welcome = "Hello! I'm Duke: Level " + lv + "\n"
                 + "What would you like to do today?\n"
-                + "My current features are: " + features[lv];
-
+                + "My current features are: " + featuresCombined;
 
         System.out.println(sandwich(welcome));
 
@@ -41,16 +47,29 @@ public class Duke {
 
         // Scanner to read user inputs
         Scanner scanner = new Scanner(System.in);
+
+        // storing userInput
+        ArrayList<String> inputs = new ArrayList<>();
+
         while (!canExit) {
             String userInput = scanner.nextLine();
-            if (userInput.equals("bye")) {
+            if (userInput.equals("bye")) { // user inputs 'bye', set canExit to true and Exit
                 canExit = true;
                 System.out.println(sandwich(goodbye));
-            } else {
-                System.out.println(sandwich("You have entered: " + userInput));
+            } else { // user inputs 'list' or something else
+                if (userInput.equals("list")) { // user inputs 'list', return all text stored
+                    StringBuilder userInputsList = new StringBuilder();
+                    for (int i = 0; i < inputs.size() - 1; i++) { // new line except for last item
+                        userInputsList.append(i + 1).append(". ").append(inputs.get(i)).append("\n");
+                    }
+                    userInputsList.append(inputs.size()).append(". ")
+                            .append(inputs.get(inputs.size() - 1));
+                    System.out.println(sandwich(userInputsList.toString()));
+                } else { // store userInput, tell user their input has been added
+                    inputs.add(userInput);
+                    System.out.println(sandwich("added: " + userInput));
+                }
             }
         }
     }
-
-
 }
