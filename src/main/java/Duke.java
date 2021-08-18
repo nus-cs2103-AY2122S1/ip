665aa4128a9e2c1c1ef43ff3f364aa0393ab72c4
Galvin <e0543848@u.nus.edu>
2021-08-18 14:28:31 +0800

    Add ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,47 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Duke {
+    static ArrayList<Task> storage = new ArrayList<>();
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
-        ArrayList<String> storage = new ArrayList<>();
+
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
-        String input = Sc.nextLine();
+        String first = Sc.next();
+        String rest = Sc.nextLine();
 
         while(true){
-            if(input.equals("bye")){break;}
+            if(first.equals("bye")){break;}
 
-            switch (input){
+            switch (first){
                 case "list":
-                    for (int i = 0; i < storage.size(); i++) {
-                        System.out.println(storage.get(i));
+                    Duke.printTasks();
+                    break;
+                case "done":
+                    int taskID = Integer.parseInt(rest.trim());
+                    if(taskID >= storage.size()){
+                        System.out.println("no such task");
+                    }else{
+                        Task t = storage.get(taskID);
+                        t.markDone();
+                        Duke.printTasks();
                     }
                     break;
                 default:
-                    storage.add(input);
-                    System.out.println("added: " + input);
+                    storage.add(new Task(first + rest));
+                    System.out.println("added: " + first + rest);
 
             }
-            input = Sc.nextLine();
+            first = Sc.next();
+            rest = Sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
         Sc.close();
+    }
+
+    private static void printTasks(){
+        for (int i = 0; i < storage.size(); i++) {
+            System.out.println(storage.get(i));
+        }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String name;
+    private boolean done;
+    public Task(String name){
+        this.name = name;
+        this.done = false;
+    }
+
+    /**
+     * marks this task as done
+     */
+    public void markDone(){
+        this.done = true;
+    }
+
+    /**
+     * override toString method to add a check box beside task
+     * @return String
+     */
+    @Override
+    public String toString(){
+        String check = done ? "X" : " ";
+        return "[" + check + "] " + name;
+    }
+}
