838c27f4f909b1e98e3df85c2b431e40afa5be1a
jeremy-7007 <71486951+jeremy-7007@users.noreply.github.com>
2021-08-19 00:04:24 +0800

    Commit level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Nukem\n"
+                + "What can I do for you?");
+        String tempString;
+        Scanner input = new Scanner(System.in);
+        while (!(tempString = input.nextLine()).equals("bye")) {
+            System.out.println(tempString);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
