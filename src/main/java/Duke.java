a4340ef0cd54614a6c4a80ac8a0e6f75de32cf35
Zhang Guangxuan <zhangguangxuan99@gmail.com>
2021-08-18 10:22:57 +0800

    feat: add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;  // Import the Scanner clas
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String botName = "HuAI";
+        System.out.printf("Hello! I'm %s\n", botName);
+        System.out.printf("What can I do for you?\n");
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        while (true) {
+            String command = scanner.nextLine();
+            boolean aborted = false;
+            switch (command) {
+                case "bye":
+                    aborted = true;
+                    System.out.printf("Bye. Hope to see you again soon!\n");
+                    break;
+                default:
+                    System.out.printf("%s\n", command);
+                    break;
+            }
+            if (aborted) {
+                break;
+            }
+        }
     }
 }
