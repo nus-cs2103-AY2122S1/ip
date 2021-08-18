e550497153865ee18c5c0f7f6abb7d926a8748b0
Bernardus Krishna <krishnabernardus@gmail.com>
2021-08-18 15:50:43 +0800

    Finished Level-4: ToDo, Event, Deadline

--- src/main/java/Bern.java
+++ src/main/java/Bern.java
@@ -17,29 +17,53 @@ public class Bern {
 
         while (true) {
             String input = myObj.nextLine();
-            if (input.length() >= 6 && input.substring(0, 4).equals("done") && Integer.parseInt(input.substring(5)) <= available){
+            if (input.length() >= 6 && input.substring(0, 4).equals("done")){
                 int index = Integer.parseInt(input.substring(5)) - 1;
                 listTask[index].markAsDone();
                 System.out.println("Good job! I've marked this task as done:\n"
-                        + "[" + listTask[index].getStatusIcon() + "]"
-                        + listTask[index].getDescription());
-            } else if (!input.equals("bye") && !input.equals("list")) {
-                System.out.println("added: " + input);
-                listTask[available] = new Task(input);
+                        + listTask[index].toString());
+            } else if (input.length() >= 10 && input.substring(0, 8).equals("deadline")) {
+                String task = input.substring(9, input.indexOf('/') - 1);
+                String by = input.substring(input.indexOf('/') + 4);
+                listTask[available] = new Deadline(task, by);
+                System.out.println("Got it. I've added this task:\n"
+                        + listTask[available].toString() + "\n"
+                        + "Now you have " + String.valueOf(available + 1)
+                        + (available == 0 ? "task in the list" : " tasks in the list")
+                );
+                available += 1;
+            } else if (input.length() >= 7 && input.substring(0, 5).equals("event")) {
+                String task = input.substring(6, input.indexOf('/') - 1);
+                String at = input.substring(input.indexOf('/') + 4);
+                listTask[available] = new Event(task, at);
+                System.out.println("Got it. I've added this task:\n"
+                        + listTask[available].toString() + "\n"
+                        + "Now you have " + String.valueOf(available + 1)
+                        + (available == 0 ? "task in the list" : " tasks in the list")
+                );
+                available += 1;
+            } else if (input.length() >= 6 && input.substring(0, 4).equals("todo")) {
+                listTask[available] = new ToDo(input.substring(5));
+                System.out.println("Got it. I've added this task:\n"
+                        + listTask[available].toString() + "\n"
+                        + "Now you have " + String.valueOf(available + 1)
+                        + (available == 0 ? " task in the list" : " tasks in the list")
+                );
                 available += 1;
             } else if (input.equals("bye")){
                 System.out.println("Bye. Hope to see you soon and hope you found my service useful!");
                 break;
-            } else { // input equals "list"
+            } else if (input.equals("list")){
                 for (int i = 0; i < available; i++) {
                     System.out.println(String.valueOf(i + 1)
                             + ". "
-                            + "[" + listTask[i].getStatusIcon() + "] "
-                            + listTask[i].getDescription());
+                            + listTask[i].toString());
                 }
                 if (available == 0) {
                     System.out.println("There is no task.");
                 }
+            } else {
+                System.out.println("Command not understood");
             }
         }
     }
