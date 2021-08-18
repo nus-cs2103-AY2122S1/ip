a89112ee9cf9c2b356b302487a991af3c7aca11e
Aishwarya Nair <aishrn1202@gmail.com>
2021-08-18 13:03:55 +0800

    greet,echo,exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        printFormatted("Hola! I'm Blitz :) \n\tWhat can I do for you?");
+        String command = scanner.nextLine();
+        while(!command.equals("bye")) {
+            printFormatted(command);
+            command = scanner.nextLine();
+        }
+        printFormatted("Adiós. Hope to see you again soon!");
+    }
+
+    public static void printFormatted(String str) {
+        System.out.print('\t');
+        for(int i = 0; i < 40; i++) {
+            System.out.print('_');
+        }
+        System.out.println("\n \t" + str);
+        System.out.print('\t');
+        for(int i = 0; i < 40; i++) {
+            System.out.print('_');
+        }
+        System.out.println('\n');
     }
 }
