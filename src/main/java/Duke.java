e4f4a3fb19ec489fdb4b39fd5dbf2f8c08a37eb7
DonnyS57 <donnysuriyanto@gmail.com>
2021-08-19 00:39:02 +0800

    Finish Level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,26 +16,29 @@ public class Duke {
     }
 
     public static void run() {
-        while(true) {
-            Scanner newInput = new Scanner(System.in);
+        Scanner newInput = new Scanner(System.in);
+        while(newInput.hasNextLine()) {
             String input = newInput.nextLine();
             Task currentTask = new Task(input);
-            if (input.equals("bye")) {
-                print("Bye. Hope to see you again soon!");
-                break;
-            } else if (input.equals("list")) {
-                list();
-            } else if (input.length() >= 5 && input.substring(0, 5).equals("done ")) {
-                done(input);
-            } else if (input.length() >= 5 && input.substring(0, 5).equals("todo ")) {
-                todo(input);
-            } else if (input.length() >= 9 && input.substring(0, 9).equals("deadline ")) {
-                deadline(input);
-            } else if (input.length() >= 6 && input.substring(0, 6).equals("event ")) {
-                event(input);
-            } else {
-                    storage.add(currentTask);
-                    print("added: " + input);
+            try {
+                if (input.equals("bye")) {
+                    print("Bye. Hope to see you again soon!");
+                    break;
+                } else if (input.equals("list")) {
+                    list();
+                } else if (input.length() >= 5 && input.substring(0, 5).equals("done ")) {
+                    done(input);
+                } else if (input.length() >= 5 && input.substring(0, 5).equals("todo ")) {
+                    todo(input);
+                } else if (input.length() >= 9 && input.substring(0, 9).equals("deadline ")) {
+                    deadline(input);
+                } else if (input.length() >= 6 && input.substring(0, 6).equals("event ")) {
+                    event(input);
+                } else {
+                    invalidInput();
+                }
+            } catch (DukeException err){
+                print(err.getMessage());
             }
         }
     }
@@ -50,14 +53,20 @@ public class Duke {
         print(sentence);
     }
 
-    public static void done(String doneEntry) {
+    public static void done(String doneEntry) throws DukeException {
         int taskNumber = Integer.parseInt(doneEntry.substring(5,6));
+        if (taskNumber > (storage.size() - 1)) {
+            throw new DukeException("Sorry ☹, please enter a valid task to delete!");
+        }
         Task doneTask = storage.get(taskNumber - 1);
         doneTask.markAsDone();
         print("Congratulations on finishing this task!\n [X] " + doneTask.getDescription());
     }
 
-    public static void todo(String todoEntry) {
+    public static void todo(String todoEntry) throws DukeException {
+        if (todoEntry.length() == 5) {
+            throw new DukeException("Sorry ☹, please enter a description!");
+        }
         String todoTitle = todoEntry.substring(5);
         Todo newToDo = new Todo(todoTitle);
         storage.add(newToDo);
@@ -65,8 +74,15 @@ public class Duke {
                 + " tasks in the list" );
     }
 
-    public static void deadline(String deadlineEntry) {
+    public static void deadline(String deadlineEntry) throws DukeException {
+        if (deadlineEntry.length() == 9) {
+            throw new DukeException("Sorry ☹, please enter a description!");
+        }
         int indexOfSlash = deadlineEntry.indexOf("/");
+        if (indexOfSlash == -1) {
+            throw new DukeException("Sorry ☹, please enter a deadline!");
+        }
+        System.out.println(indexOfSlash);
         String deadlineDate = deadlineEntry.substring(indexOfSlash + 3);
         String deadlineTitle = deadlineEntry.substring(9, indexOfSlash);
         Deadline newDeadline = new Deadline(deadlineTitle, deadlineDate);
@@ -75,14 +91,24 @@ public class Duke {
                 + " tasks in the list" );
     }
 
-    public static void event(String eventEntry) {
+    public static void event(String eventEntry) throws DukeException {
+        if (eventEntry.length() == 6) {
+            throw new DukeException("Sorry ☹, please enter a description!");
+        }
         int indexOfSlash = eventEntry.indexOf("/");
+        if (indexOfSlash == -1) {
+            throw new DukeException("Sorry ☹, please enter an event time!");
+        }
         String eventDate = eventEntry.substring(indexOfSlash + 3);
         String eventTitle = eventEntry.substring(6, indexOfSlash);
         Event newEvent = new Event(eventTitle, eventDate);
         storage.add(newEvent);
         print("Alright. I'm adding this task:\n  " + newEvent.toString() + "\nNow there are " + storage.size()
                 + " tasks in the list" );
+    }
+
+    public static void invalidInput() throws DukeException {
+        throw new DukeException("Sorry ☹, please enter a valid command!");
     }
 
 }
