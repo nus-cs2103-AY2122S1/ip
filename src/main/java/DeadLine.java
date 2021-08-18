cb5f52d2910222313475f3319b2c0729865bdf4f
Imerbear <e0543864@u.nus.edu>
2021-08-19 02:47:47 +0800

    Added DeadLine Task

---
+++ src/main/java/DeadLine.java
@@ -0,0 +1,18 @@
+public class DeadLine extends Task {
+    private String dueDate;
+
+    /**
+     * Constructor for DeadLine
+     *
+     * @param description is the string of the description of the given task
+     */
+    public DeadLine(String description, String dueDate) {
+        super(description, "deadline");
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (Due: " + this.dueDate + ")";
+    }
+}
