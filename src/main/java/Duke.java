e986050525af0d4f3f5bde97f9b33b9622d8b2bf
tianyue58 <e0563952@u.nus.edu>
2021-08-18 17:13:22 +0800

    add echo feature to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! I'm Duke created by Tianyue\n" +
+                "What can I do for you?");
+
+
+        String text = scanner.nextLine();
+
+
+        while(!text.isEmpty()) {
+            if (text.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(text);
+            text = scanner.nextLine();
+        }
     }
 }
