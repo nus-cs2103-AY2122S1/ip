bbc5a79af2455cd8f7b17e44e98623e860f60ecf
Yao Yi <yaoyi2000326@gmail.com>
2021-08-18 18:32:42 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.*;
+
 public class Duke {
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        boolean running = true;
+        String horizontal = "____________________\n";
+        String exitText = horizontal + "Bye!\n" + horizontal;
+
+        String logo =
+              "                     _               _         \n" +
+                      "                    | |             | |        \n" +
+                      "  _ __ ___     ___  | |   ___     __| |  _   _ \n" +
+                      " | '_ ` _ \\   / _ \\ | |  / _ \\   / _` | | | | |\n" +
+                      " | | | | | | |  __/ | | | (_) | | (_| | | |_| |\n" +
+                      " |_| |_| |_|  \\___| |_|  \\___/   \\__,_|  \\__, |\n" +
+                      "                                          __/ |\n" +
+                      "                                         |___/ ";
+
         System.out.println("Hello from\n" + logo);
+
+
+        Scanner sc = new Scanner(System.in);
+        while (running) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                running = false;
+                System.out.println(exitText);
+            } else {
+                String wrapped = horizontal + input + "\n" + horizontal;
+                System.out.println(wrapped);
+            }
+        }
+        sc.close();
     }
 }
