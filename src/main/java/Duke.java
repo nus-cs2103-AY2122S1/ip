11ab4e629ecfacf6919edcb9b1c3e43901d1e1fa
Hannah Chia <chia.hannahk@gmail.com>
2021-08-18 17:34:09 +0800

    Feat: Add ability to mark tasks as done
    * Changed some words in the chatbot, so conversation flows more easily
    * Created the Task class to store Task related methods
    
    Might want to change the messages for markAsDone...

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,23 +8,30 @@ public class Duke {
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
-        ArrayList<String> lst = new ArrayList<String>();
+        ArrayList<Task> lst = new ArrayList<Task>();
         System.out.println("Greetings Traveller, I'm Katheryne of the Adventurers Guild.");
-        System.out.println("Right now, I only echo your statements.");
+        System.out.println("Tell me what you need to do.");
         while (true) {
             try {
                 BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
                 String userInput = in.readLine();
+                String[] inputWords = userInput.split(" ");
                 if (userInput.equalsIgnoreCase("BYE")) {
                     System.out.println("Ad Astra Abyssoque, Traveller!");
                     break;
+                } else if (inputWords[0].equalsIgnoreCase("done") || inputWords[0].equalsIgnoreCase("complete")) {
+                    int i = Integer.parseInt(inputWords[1]);
+                    if (i < lst.size()) {
+                        lst.get(i - 1).markAsDone();
+                        System.out.println(lst.get(i));
+                    }
                 } else if (userInput.equalsIgnoreCase("list")) {
-                        System.out.println("Here's the list I've stored for you:");
-                        for(int i = 1; i <= lst.size(); i++) {
-                            System.out.println(i + ") " + lst.get(i - 1));
-                        }
+                    System.out.println("Here's the list I've stored for you:");
+                    for(int i = 1; i <= lst.size(); i++) {
+                        System.out.println(i + ") " + lst.get(i - 1));
+                    }
                 } else {
-                    lst.add(userInput);
+                    lst.add(new Task(userInput));
                     System.out.println("Okay, I've added '"+ userInput + "' to your list");
                 }
             } catch(IOException exc) {
