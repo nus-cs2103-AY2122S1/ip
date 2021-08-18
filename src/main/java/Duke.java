5619c897b95a6113bb997f3e011aca18ec0f48f6
LuoZhijie-tom <e0492460@u.nus.edu>
2021-08-18 17:34:09 +0800

    get the command from String.split(" ")

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,25 +2,28 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String input = new String();
         String command = new String();
         ArrayList<Task> store = new ArrayList<>();
 
         System.out.println("Hello! I'm Tom! What can I do for you?");
-        command = sc.nextLine();
-        while(!command.equals("bye")){
-            if(command.equals("list")){
+        input = sc.nextLine();
+        while(!input.equals("bye")){
+            String words[] = input.split(" ");
+            if(input.equals("list")){
                 for(int i = 0; i < store.size(); i++){
-                    System.out.println((i + 1) + "." + "[" + store.get(i).getStatusIcon() + "] " + store.get(i).getDescription());
+                    System.out.println((i + 1) + "." + "[" + store.get(i).getStatusIcon() + "] "
+                            + store.get(i).getDescription());
                 }
-            } else if(command.substring(0,4).equals("done")){
-                int textNumber = Integer.parseInt(command.substring(5));
+            } else if(words[0].equals("done")){
+                int textNumber = Integer.parseInt(words[1]);
                 store.get(textNumber - 1).done();
                 System.out.println("Nice! I've marked this task as done:\n" + "[" + store.get(textNumber - 1).getStatusIcon() + "] " + store.get(textNumber - 1).getDescription());
             } else {
-                store.add(new Task(command));
-                System.out.println("added: " + command);
+                store.add(new Task(input));
+                System.out.println("added: " + input);
             }
-            command = sc.nextLine();
+            input = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
