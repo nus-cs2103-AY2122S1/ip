a51d9c42813741a055b2c6eae4172b5ea794d18a
meerian <myronneozx@gmail.com>
2021-08-18 13:30:42 +0800

    Level 2: Add, List completed.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int pointer = 0;
+
         System.out.println("──────────────────────────────────────────");
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,14 +19,31 @@ public class Duke {
         System.out.println("──────────────────────────────────────────");
 
         String input = scanner.nextLine();
-        System.out.println("──────────────────────────────────────────");
 
         while (!Objects.equals(input, "bye")) {
-            System.out.println(input);
-            System.out.println("──────────────────────────────────────────");
-            input = scanner.nextLine();
-            System.out.println("──────────────────────────────────────────");
+            switch(input) {
+                case "list":
+                    System.out.println("──────────────────────────────────────────");
+                    for (int i = 0; i < pointer; i++) {
+                        String cur = list[i];
+                        int label = i + 1;
+                        System.out.println(label + ". " + cur);
+                    }
+                    System.out.println("──────────────────────────────────────────");
+                    input = scanner.nextLine();
+                    break;
+
+                default:
+                    System.out.println("──────────────────────────────────────────");
+                    list[pointer] = input;
+                    pointer++;
+                    System.out.println("added: " + input);
+                    System.out.println("──────────────────────────────────────────");
+                    input = scanner.nextLine();
+                    break;
+            }
         }
+        System.out.println("──────────────────────────────────────────");
         System.out.println("Bye, hope to see you again soon!");
     }
 }
