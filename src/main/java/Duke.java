697ae7751fd99da14fbdc91a5e709dfb6b1b7d19
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2021-08-18 22:19:26 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,14 +4,23 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! This is Jarvis.");
         System.out.println("What can I do for you sir?");
+        String [] tasks = new String[100];
+        int taskIndex = 0;
         Scanner sc = new Scanner(System.in);
         while (true) {
             String task = sc.nextLine();
             if (task.equals("bye")) {
                 System.out.println("Goodbye Sir. Hope you have a pleasant day sir.");
                 break;
+            } else if (task.equals("list")) {
+                for (int i = 0; i < taskIndex; i++) {
+                    System.out.printf("%d. %s%n", i + 1, tasks[i]);
+                }
+            } else {
+                tasks[taskIndex] = task;
+                taskIndex++;
+                System.out.println("added: " + task);
             }
-            System.out.println(task);
         }
         sc.close();
     }
