c3d0d5e26989f84939de7875b502cf7264691759
HongshuW <hongshu.wang.uni@gmail.com>
2021-08-18 20:42:56 +0800

    Add Deadline.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,24 @@
+public class Deadline extends Task {
+    protected String by;
+
+    /**
+     * Constructor of the class `Deadline`.
+     *
+     * @param description Description of the task.
+     * @param by Deadline of the task.
+     */
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    /**
+     * Converts a task with deadline to a string.
+     *
+     * @return The string representation of a task with deadline.
+     */
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
