1f027cfc9ae00ea1bbb537fc7d9d2954e6b4d029
DESKTOP-CSTCHE9\Joseph Nathanael <josephnathanael.2014@gmail.com>
2021-08-18 15:05:43 +0800

    Greet, Echo, Exit feature ()

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you? :)");
+        Scanner sc = new Scanner(System.in);
+        String inpt = sc.nextLine();
+        while(!inpt.equals("bye")){
+            System.out.println(inpt);
+            inpt = sc.nextLine();
+        }
+        System.out.println("Goodbye, I will miss you!");
     }
 }
