b28a3743cc4bc204dfa94f23a1e9695dd9a6784b
JasonC01 <chungys02@gmail.com>
2021-08-18 17:33:47 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] itemList = new String[100];
+    private static int index = 0;
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -16,9 +20,17 @@ public class Duke {
             if(input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            }else if (input.equals("list")) {
+                for(int i = 0; i < index; i++){
+                    System.out.println((i + 1) + ". " + itemList[i]);
+                }
             }else {
-                System.out.println(input);
+                System.out.println("added: " + input);
+                itemList[index++] = input;
             }
         }
+    }
+    private static void addList(String input) {
+
     }
 }
