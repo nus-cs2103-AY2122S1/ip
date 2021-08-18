967ba061f61e92bdea2d7f75c154d207d0d1a771
weiquu <ztolenstar@hotmail.com>
2021-08-19 03:57:04 +0800

    Completed Level-6: Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,9 +14,10 @@ public class Duke {
        */
     ArrayList<Task> userText = new ArrayList<>();
     String DONE_REGEX = "done [0-9]+";
-    String TODO_REGEX = "todo [\\w\\s-]*";
-    String DEADLINE_REGEX = "deadline [\\w\\s-]* \\/by [\\w\\s-]*";
-    String EVENT_REGEX = "event [\\w\\s-]* \\/at [\\w\\s-]*";
+    String DELETE_REGEX = "delete [0-9]+";
+    String TODO_REGEX = "todo [\\w\\s-]+";
+    String DEADLINE_REGEX = "deadline [\\w\\s-]+ \\/by [\\w\\s-]+";
+    String EVENT_REGEX = "event [\\w\\s-]+ \\/at [\\w\\s-]+";
     String LINE = "    --------------------------------------------------";
     String INDENTATION = "      ";
     Scanner scan = new Scanner(System.in);
@@ -43,6 +44,12 @@ public class Duke {
           int index = Integer.parseInt(indexStr) - 1;
           userText.get(index).markAsDone();
           System.out.println(LINE + "\n" + INDENTATION + "Nice! I've marked this task as done:\n" + INDENTATION + "  " + userText.get(index) + "\n"  + LINE);
+        } else if (Pattern.matches(DELETE_REGEX, command)) { 
+          String indexStr = command.substring(7);
+          int index = Integer.parseInt(indexStr) - 1;
+          System.out.println(LINE + "\n" + INDENTATION + "Noted. I've removed this task:\n" + INDENTATION + "  " + userText.get(index));
+          userText.remove(index);
+          System.out.println(INDENTATION + "Now you have " + userText.size() + " tasks in the list.\n" + LINE);
         } else if (command.equals("todo")) { 
           throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
         } else if (Pattern.matches(TODO_REGEX, command)) {
