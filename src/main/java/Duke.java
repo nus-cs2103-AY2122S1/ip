e1943accdb3fd324224923a27b00d769e2c436ef
Jingwencloud <jingwen098765432@gmail.com>
2021-08-18 14:27:18 +0800

    Update Duke class to greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
 public class Duke {
+
+    public static void getInput(Scanner scanner)
+    {
+        String input = scanner.next();
+        if(input.equals("bye"))
+        {
+            System.out.println("  ---------------------------------------------");
+            System.out.println("    Bye. Hope to see you again soon!");
+            System.out.println("  ---------------------------------------------");
+            scanner.close();
+        } else
+        {
+            System.out.println("  ---------------------------------------------");
+            System.out.println("    " + input);
+            System.out.println("  ---------------------------------------------");
+            getInput(scanner);
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("  ---------------------------------------------");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("  ---------------------------------------------");
+
+        Scanner scanner = new Scanner(System.in);
+        getInput(scanner);
+
     }
 }
