236c392fa6ab33c14b296e3cafff20be3f71d2d6
bingcheng45 <bingcheng45@gmail.com>
2021-08-18 14:52:03 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
+/**
+ * This program Duke is a chatbot.
+ *
+ * @author: Toh Bing Cheng
+ * @version: CS2103T AY21 Semester 1
+ */
 public class Duke {
     public static void main(String[] args) {
+        runProgram();
+    }
+
+    /**
+     * This method runs the program indefinitely till user types in "bye"
+     */
+    public static void runProgram() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        boolean run = true;
+        Scanner scanner = new Scanner(System.in);
+
+        while(run) {
+            // wait to read in the user's input
+            String input = scanner.nextLine();
+            // Ensures that all bye would end the program.
+            if (input.toUpperCase().equals("BYE")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                run = false;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
