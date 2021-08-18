e1c58401b59b3b9960ac257d96f7d94e92cf9374
Nguyen Thuan Hung <e0550385@u.nus.edu>
2021-08-18 16:57:22 +0800

    Level 0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void printLine() {
+        System.out.println("--------------------------------------------------");
+    }
+
+    public static void printBigIcon() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printLine();
+    }
+
+    public static void printHello() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        printLine();
+    }
+
+    public static void printExit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void main(String[] args) {
+        printLine();
+        printBigIcon();
+        printHello();
+        printExit();
+//        Scanner myScanner = new Scanner(System.in);
+//
+//        String userCommand = myScanner.nextLine();  // Read user input
+//        System.out.println(userCommand);
     }
 }
