f2a4d12e9f0fc0943c6c2d5d3323b97b9f2e5653
Preshita01 <pandipreshita@gmail.com>
2021-08-18 19:11:13 +0800

    Add mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,5 @@
 import java.util.Scanner;
 public class Duke {
-    private static String taskList[] = new String[100];
-    private static int counter = 0;
-
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -26,23 +23,28 @@ public class Duke {
                     break;
                 case ("list"):
                     int index = 0;
-                    while (index < counter) {
-                        System.out.println("\t" + (index + 1) + ". " + taskList[index] + "\n");
+                    while (index < Task.getCounter()) {
+                        System.out.println("\t" + (index + 1) + "." + Task.getTaskList()[index].getStatusIcon()
+                                + " " + Task.getTaskList()[index].getDescription());
                         index++;
                     }
                     break;
                 default:
-                    addTask(instruction);
-                    System.out.println("\tadded: " + instruction);
+                    if (instruction.startsWith("done")) {
+                        int taskNum = Integer.parseInt(instruction.substring(5)) - 1;
+                        Task.getTaskList()[taskNum].markAsDone();
+                        System.out.println("\tGood job! I have marked this task as done:");
+                        System.out.println("\t\t" + Task.getTaskList()[taskNum].getStatusIcon()
+                                + " " + Task.getTaskList()[taskNum].getDescription());
+                    } else {
+                        Task newTask = new Task(instruction);
+                        newTask.addTask();
+                        System.out.println("\tadded: " + instruction);
+                    }
                     break;
             }
             System.out.println("----------------------------------\n");
         } while (!instruction.equals("bye"));
     }
-
-    public static void addTask(String task) {
-        taskList[counter] = task;
-        counter++;
-    }
-
 }
+
