e07608a98bbe713fca8ab657881614185c819bb1
Tan Joe Wel <>
2021-08-18 15:38:11 +0800

    Added the "add" and "list" feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
 
     public static void main(String[] args) {
+        String[] storedInputs = new String[100];
+        int index = 0;
         Scanner scanObj = new Scanner(System.in);
 
         String greeting = "Hello! I'm BunnyBot, Joe Wel's personal slave!\n" + "What can I do for you?";
@@ -16,12 +18,23 @@ public class Duke {
                         + "    ----------------------------";
                 System.out.println(byeMsg);
                 break;
+            } else if (userInput.equals("list")) {
+                String message = "    ----------------------------\n";
+                int i = 0;
+                while (storedInputs[i] != null) {
+                    message += "    " + (i+1) + ". " + storedInputs[i] + "\n";
+                    i++;
+                }
+                message += "    ----------------------------\n";
+                System.out.println(message);
+            } else {
+                String echo = "    ----------------------------\n"
+                        + "    " + "added: " + userInput + "\n"
+                        + "    ----------------------------";
+                System.out.println(echo);
+                storedInputs[index] = userInput;
+                index++;
             }
-            String echo = "    ----------------------------\n"
-                    + "    " + userInput + "\n"
-                    + "    ----------------------------";
-            System.out.println(echo);
-
         }
     }
 }
