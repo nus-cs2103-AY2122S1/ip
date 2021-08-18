e72fa50fa27ba73293b06ec449f5f36886f1624d
szelongq <72333104+szelongq@users.noreply.github.com>
2021-08-18 17:01:49 +0800

    Implement Duke Level 2
    Add accepting and storing user inputted tasks into a list.
    Add listing all tasks in the list.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,13 +15,35 @@ public class Duke {
         System.out.print(user);
 
         Scanner scanner = new Scanner(System.in);
+        String userInput;
+        int nTasks = 0;
+        String[] tasks = new String[100];
+        for (int i = 0; i < 100; i++)
+            tasks[i] = "";
 
-        String userInput = scanner.nextLine();
-        while (!userInput.equalsIgnoreCase("bye")) {
-            System.out.println(duke + userInput);
-            System.out.print(user);
+
+        do {
             userInput = scanner.nextLine();
-        }
+
+            if (userInput.equalsIgnoreCase("bye"))
+                break;
+
+            if (userInput.equalsIgnoreCase("list")) {
+                System.out.println(duke + "\n\tTasks:");
+                if (nTasks == 0)
+                    System.out.println("\t\tNothing in list");
+                else
+                    for (int i = 0; i < nTasks; i++) {
+                        System.out.printf("\t\t%d. %s\n", i + 1, tasks[i]);
+                    }
+            }
+            else {
+                tasks[nTasks++] = userInput;
+                System.out.println(duke + "\n\tAdded: " + userInput);
+            }
+
+            System.out.print(user);
+        } while (true);
 
         System.out.println(duke + "Bye. Have a nice day.");
     }
