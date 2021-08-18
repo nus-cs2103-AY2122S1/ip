ffbeceaf99220107e7c89a8d6aef51b46bd49298
szelongq <72333104+szelongq@users.noreply.github.com>
2021-08-18 16:41:40 +0800

    Implement Duke Level 1
    Add greetings when starting Duke.
    Add echoing of user input.
    Add exiting of program on entering "bye".

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
+        String duke = "\nDuke: ";
+        String user = "\nUser: ";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(duke + "Hi, what do you want from me?\n");
+        System.out.print(user);
+
+        Scanner scanner = new Scanner(System.in);
+
+        String userInput = scanner.nextLine();
+        while (!userInput.equalsIgnoreCase("bye")) {
+            System.out.println(duke + userInput);
+            System.out.print(user);
+            userInput = scanner.nextLine();
+        }
+
+        System.out.println(duke + "Bye. Have a nice day.");
     }
 }
