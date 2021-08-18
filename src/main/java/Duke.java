3e44b050b9f10e543a6da907e379cc677c2de59c
random689 <freezerfiregithub@gmail.com>
2021-08-18 20:37:27 +0800

    Added DukeExceptions to catch the errors for: 1. Invalid command for "done", "todo", "deadline" and "event" 2. Catch invalid numbers (out of range) for "done"
    Also updated to remove the extra space in each space (using trim)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,50 +10,79 @@ public class Duke {
         Task[] listArray = new Task[100];
         int count = 0;
         int index;
+        String[] split;
         Scanner sc = new Scanner(System.in);
         do {
-            input = sc.next(); //Reads the next input
-            switch (input) {
-                case "bye":  //Shuts down the bot when it gets a "bye" prompt
-                    isRunning = false;
-                    System.out.println(goodbyeLine);
-                    break;
-                case "list":  //Returns the list when it gets a "list" prompt
-                    for (int i = 0; i < count; i++) {
-                        System.out.println(i + 1 + ". " + listArray[i]);
-                    }
-                    break;
-                case "done":  //Marks the task as done when it gets the "done x" prompt and updates the task as well.
-                    index = sc.nextInt();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  " + listArray[index - 1].done());
-                    break;
-                case "todo":  //Inputs a todo task when given the "todo" prompt
-                    input = sc.nextLine();
-                    listArray[count++] = new Todo(input);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + listArray[count - 1]);
-                    System.out.println("Now you have " + count + " in the list.");
-                    break;
-                case "deadline": //Inputs a Deadline task when given the "deadline" prompt
-                    input = sc.nextLine();
-                    listArray[count++] = new Deadline(input.split(" /by ")[0], input.split(" /by ")[1]);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + listArray[count - 1]);
-                    System.out.println("Now you have " + count + " in the list.");
-                    break;
-                case "event": //Inputs an Event task when given the "event" prompt
-                    input = sc.nextLine();
-                    listArray[count++] = new Event(input.split(" /at ")[0], input.split(" /at ")[1]);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + listArray[count - 1]);
-                    System.out.println("Now you have " + count + " in the list.");
-                    break;
-                default:
-                    System.out.println("Oh No! I can't seem to understand you!");
-                    System.out.println("Try something else :(");
-                    System.out.println("But please don't leave me :((((((");
-                    break;
+            try {
+                input = sc.next(); //Reads the next input
+                switch (input) {
+                    case "bye":  //Shuts down the bot when it gets a "bye" prompt
+                        isRunning = false;
+                        System.out.println(goodbyeLine);
+                        break;
+                    case "list":  //Returns the list when it gets a "list" prompt
+                        if (count == 0) {
+                            System.out.println("The list is empty!"); //When the list is empty
+                        } else {
+                            for (int i = 0; i < count; i++) {
+                                System.out.println(i + 1 + ". " + listArray[i]);
+                            }
+                        }
+                        break;
+                    case "done":  //Marks the task as done when it gets the "done x" prompt and updates the task as well.
+                        input = sc.nextLine();
+                        if (input.isEmpty()) { //Catch if there is no number after the command
+                            throw new DukeException("☹ OOPS!!! done will require a task number to update.");
+                        }
+                        index = Integer.parseInt(input.trim());
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  " + listArray[index - 1].done());
+                        break;
+                    case "todo":  //Inputs a todo task when given the "todo" prompt
+                        input = sc.nextLine();
+                        if (input.isEmpty()) { //Catch if todo description is empty
+                            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                        }
+                        listArray[count++] = new Todo(input.trim());
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println("  " + listArray[count - 1]);
+                        System.out.println("Now you have " + count + " in the list.");
+                        break;
+                    case "deadline": //Inputs a Deadline task when given the "deadline" prompt
+                        input = sc.nextLine();
+                        if (input.isEmpty()) { //Catch if deadline description is empty
+                            throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                        }
+                        split = input.split(" /by ");
+                        if (split.length <= 1) {
+                            throw new DukeException("☹ OOPS!!! Your deadline input format is not valid!");
+                        }
+                        listArray[count++] = new Deadline(split[0].trim(), split[1]);
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println("  " + listArray[count - 1]);
+                        System.out.println("Now you have " + count + " in the list.");
+                        break;
+                    case "event": //Inputs an Event task when given the "event" prompt
+                        input = sc.nextLine();
+                        if (input.isEmpty()) { //Catch if event description is empty
+                            throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
+                        }
+                        split = input.split(" /at ");
+                        if (split.length <= 1) {
+                            throw new DukeException("☹ OOPS!!! Your event input format is not valid!");
+                        }
+                        listArray[count++] = new Event(split[0].trim() , split[1]);
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println("  " + listArray[count - 1]);
+                        System.out.println("Now you have " + count + " in the list.");
+                        break;
+                    default:
+                        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+            } catch (NumberFormatException e2) { //For catching any invalid number after the done command
+                System.out.println("☹ OOPS!!! done will require a valid integer value input to update.");
             }
         } while (isRunning);
     }
