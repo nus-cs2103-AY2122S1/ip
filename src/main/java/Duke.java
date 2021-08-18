65bab2d8ec1e37b2033b0b86d63260d41757ea4f
Moley456 <kingstarwei10@gmail.com>
2021-08-18 17:57:10 +0800

    implement Duke level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        boolean end = false;
+
+        while(!end) {
+            Scanner sc = new Scanner(System.in);
+            String command = sc.nextLine();
+
+            if (command.equalsIgnoreCase("bye")) {
+                end = !end;
+                System.out.println("Bye. Hope to see you again soon!");
+            } else {
+                System.out.println(command);
+            }
+        }
+
     }
 }
