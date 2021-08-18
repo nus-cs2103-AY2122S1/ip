3ad3f3995712c0e7c1ba39face58ee267c6be5ec
bentanjunrong <seraphi123@yahoo.com.sg>
2021-08-18 18:26:51 +0800

    Modified Duke's texts

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -43,32 +43,35 @@ public class Duke {
         System.out.print("               ,'           )     `.         |\n");
         System.out.print("                7____,,..--'      /          |\n");
         System.out.print("                                  `---.__,--.'\n");
-        System.out.println(" BOW BEFORE ME\n FOR I AM SQUIRTLE, DESTROYER OF MEN, TAKER OF LIVES.");
+        System.out.println(
+                " BOW BEFORE ME, FOR I AM SQUIRTLE, DESTROYER OF MEN, TAKER OF LIVES.\n THE GODS FEARED MY EXISTENCE, SO THEY BANISHED ME TO YOUR MORTAL REALM TO SAVE YOUR MISERABLE LIFE.");
         System.out.println(LINEBREAK);
+        System.out.print("SO WHAT DO YOU WANT DO, INSECT?\n ");
     }
 
     private void run() {
         List<String> commands = new ArrayList<String>();
         greet();
         Scanner input = new Scanner(System.in);
-        System.out.println("\nDANCE FOR ME, INSECT.");
 
         while (true) {
             String inputString = input.nextLine().trim();
 
             if (inputString.equals("bye")) {
-                print(" RUN WHILE YOU CAN, WEAKLING.");
+                print(" LIVE OUT YOUR PATHETIC LIFE, WEAKLING.");
                 break;
             } else if (inputString.equals("list")) {
                 System.out.println(LINEBREAK);
+                System.out.println("YOU WISH FOR THESE FOOLISH THINGS:");
                 for (int i = 0; i < commands.size(); i++) {
-                    System.out.printf(" %s. %s\n", i, commands.get(i));
+                    System.out.printf(" %s. %s\n", i + 1, commands.get(i));
                 }
                 System.out.println(LINEBREAK);
             } else {
-                print(" " + inputString);
+                print(" SO YOU WANT TO: " + inputString);
                 commands.add(inputString);
             }
+            System.out.print("WHAT ELSE DO YOU WANT, INSECT?\n ");
         }
         input.close();
     }
