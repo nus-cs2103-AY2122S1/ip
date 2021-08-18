24889014ae29f4434495abc78d9f92cb53f048e7
Bernardus Krishna <krishnabernardus@gmail.com>
2021-08-18 21:13:22 +0800

    Finished Level-6: Delete

--- src/main/java/Bern.java
+++ src/main/java/Bern.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Bern {
     public static boolean isANumber(String s) {
@@ -12,8 +13,7 @@ public class Bern {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
 
-        Task[] listTask = new Task[100];
-        int available = 0;
+        ArrayList<Task> arListTask = new ArrayList<>();
 
         System.out.println("Hi! I'm Bern, your trustworthy chatbot.\nWhat can I do for you?");
 
@@ -25,13 +25,13 @@ public class Bern {
                         throw new EmptyDescriptionException("done");
                     } else if (!isANumber(input.substring(5))) {
                         throw new InvalidCommandException(input);
-                    } else if (Integer.parseInt(input.substring(5)) > available) {
+                    } else if (Integer.parseInt(input.substring(5)) > arListTask.size()) {
                         throw new IndexException(input);
                     }
                     int index = Integer.parseInt(input.substring(5)) - 1;
-                    listTask[index].markAsDone();
+                    arListTask.get(index).markAsDone();
                     System.out.println("Good job! I've marked this task as done:\n"
-                            + listTask[index].toString());
+                            + arListTask.get(index).toString());
                 } else if (input.length() >= 8 && input.substring(0, 8).equals("deadline")) {
                     if (input.length() == 8 || (input.length() == 9 && input.substring(8, 9).equals(" "))) {
                         throw new EmptyDescriptionException("deadline");
@@ -39,13 +39,12 @@ public class Bern {
                     // TODO Haven't handled if there is no /by
                     String task = input.substring(9, input.indexOf('/') - 1);
                     String by = input.substring(input.indexOf('/') + 4);
-                    listTask[available] = new Deadline(task, by);
+                    arListTask.add(new Deadline(task, by));
                     System.out.println("Got it. I've added this task:\n"
-                            + listTask[available].toString() + "\n"
-                            + "Now you have " + String.valueOf(available + 1)
-                            + (available == 0 ? "task in the list" : " tasks in the list")
+                            + arListTask.get(arListTask.size() - 1).toString() + "\n"
+                            + "Now you have " + String.valueOf(arListTask.size())
+                            + (arListTask.size() == 1 ? " task in the list" : " tasks in the list")
                     );
-                    available += 1;
                 } else if (input.length() >= 5 && input.substring(0, 5).equals("event")) {
                     if (input.length() == 5 || (input.length() == 6 && input.substring(5, 6).equals(" "))) {
                         throw new EmptyDescriptionException("event");
@@ -53,37 +52,51 @@ public class Bern {
                     // TODO Haven't handled if there is no /at
                     String task = input.substring(6, input.indexOf('/') - 1);
                     String at = input.substring(input.indexOf('/') + 4);
-                    listTask[available] = new Event(task, at);
+                    arListTask.add(new Event(task, at));
                     System.out.println("Got it. I've added this task:\n"
-                            + listTask[available].toString() + "\n"
-                            + "Now you have " + String.valueOf(available + 1)
-                            + (available == 0 ? "task in the list" : " tasks in the list")
+                            + arListTask.get(arListTask.size() - 1).toString() + "\n"
+                            + "Now you have " + String.valueOf(arListTask.size())
+                            + (arListTask.size() == 1 ? " task in the list" : " tasks in the list")
                     );
-                    available += 1;
                 } else if (input.length() >= 4 && input.substring(0, 4).equals("todo")) {
                     if (input.length() == 4 || (input.length() == 5 && input.substring(4, 5).equals(" "))) {
                         throw new EmptyDescriptionException("todo");
                     }
-                    listTask[available] = new ToDo(input.substring(5));
+                    arListTask.add(new ToDo(input.substring(5)));
                     System.out.println("Got it. I've added this task:\n"
-                            + listTask[available].toString() + "\n"
-                            + "Now you have " + String.valueOf(available + 1)
-                            + (available == 0 ? " task in the list" : " tasks in the list")
+                            + arListTask.get(arListTask.size() - 1).toString() + "\n"
+                            + "Now you have " + String.valueOf(arListTask.size())
+                            + (arListTask.size() == 1 ? " task in the list" : " tasks in the list")
                     );
-                    available += 1;
                 } else if (input.equals("bye")){
                     System.out.println("Bye. Hope to see you soon and hope you found my service useful!");
                     break;
                 } else if (input.equals("list")){
-                    for (int i = 0; i < available; i++) {
+                    for (int i = 0; i < arListTask.size(); i++) {
                         System.out.println(String.valueOf(i + 1)
                                 + ". "
-                                + listTask[i].toString());
+                                + arListTask.get(i).toString());
                     }
-                    if (available == 0) {
+                    if (arListTask.size() == 0) {
                         System.out.println("There is no task.");
                     }
-                } else {
+                } else if (input.length() >= 6 && input.substring(0, 6).equals("delete")){
+                    if (input.length() == 6 || (input.length() == 7 && input.substring(6, 7).equals(" "))) {
+                        throw new EmptyDescriptionException("done");
+                    } else if (!isANumber(input.substring(7))) {
+                        throw new InvalidCommandException(input);
+                    } else if (Integer.parseInt(input.substring(7)) > arListTask.size()) {
+                        throw new IndexException(input);
+                    }
+                    int index = Integer.parseInt(input.substring(7)) - 1;
+                    String temp = arListTask.get(index).toString();
+                    arListTask.remove(index);
+                    System.out.println("Noted! I've removed this task:\n"
+                            + temp + "\n"
+                            + "Now you have " + String.valueOf(arListTask.size())
+                            + (arListTask.size() == 1 ? " task in the list" : " tasks in the list"));
+                }
+                else {
                     throw new InvalidCommandException(input);
                 }
             } catch (BernException e) {
