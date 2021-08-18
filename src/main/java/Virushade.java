08bc5691d9e27138cf959208ab7060f59fdd0a94
GWphua <phuaguanwei99@gmail.com>
2021-08-18 18:02:20 +0800

    Changed functionalities, renamed Duke to Virushade.

---
+++ src/main/java/Virushade.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Virushade {
+    /**
+     * Prints the greeting.
+     */
+    private static void greet() {
+        System.out.println( "Hello from\n"
+                + "__      __ ( )                ____\n"
+                + "\\ \\    / /  _   ____  _   _  / __/\n"
+                + " \\ \\  / /  | | | ,__|| | | | \\ \\\n"
+                + "  \\ \\/ /   | | | |   | |_| | _) \\\n"
+                + "   \\__/    |_| |_|    \\__,_||___/\n"
+                + "What can <<Virushade>> do for you?");
+    }
+
+    /**
+     * The echo function that Virushade uses. Echoes whatever words that Virushade is given.
+     * @param wordToEcho The word provided by the user for Duke to echo.
+     */
+    private static void echo(String wordToEcho) {
+        System.out.println(wordToEcho);
+    }
+
+    /**
+     * Virushade first says goodbye before exiting.
+     */
+    private static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.exit(0);
+    }
+
+    /**
+     * The running sequence for Virushade.
+     */
+    public static void run() {
+        greet();
+
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextLine()) {
+            String str = sc.nextLine();
+            if (str.equals("bye")) {
+                break;
+            } else {
+                echo(str);
+            }
+        }
+
+        sc.close();
+
+        exit();
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+}
