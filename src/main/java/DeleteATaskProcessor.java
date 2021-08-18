64789bae82154a42c7f3b39d4e5b6a00e0459823
HongshuW <hongshu.wang.uni@gmail.com>
2021-08-19 00:45:17 +0800

    Add DeleteATaskProcessor.java

---
+++ src/main/java/DeleteATaskProcessor.java
@@ -0,0 +1,21 @@
+public class DeleteATaskProcessor extends Processor {
+    /**
+     * Constructor of the class `DeleteATaskProcessor`.
+     *
+     * @param task Task deleted.
+     */
+    public DeleteATaskProcessor(Task task) {
+        super("delete");
+        this.task = task;
+    }
+
+    /**
+     * Updates the message to be printed.
+     */
+    @Override
+    public void process() {
+        this.message = String.format(
+                "Noted. I've removed this task:\n  %s\n" +
+                        "Now you have %o tasks in the list.\n", this.task, Duke.getNumOfTasks());
+    }
+}
