7e479b11a40a7608cd1122eefe704bc65b539952
czhi-bin <iopwslw@gmail.com>
2021-08-18 12:55:36 +0800

    Add greeting, echos, exit functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    String HORIZONTAL_LINE = "____________________________________________________________";
+    String INDENTATION = "    ";
+
+    public Duke(){
+    }
+
+    private void greet(){
+        String msg = "Hello! I'm Duke\n";
+        msg += INDENTATION + "What can I do for you?";
+        printMessageWithFormat(msg);
+    }
+
+    private void printMessageWithFormat(String msg){
+        System.out.println(INDENTATION + HORIZONTAL_LINE);
+        System.out.println(INDENTATION + msg);
+        System.out.println(INDENTATION + HORIZONTAL_LINE);
+    }
+
+    private void exit(){
+        String msg = "Bye. Hope to see you again soon!";
+        printMessageWithFormat(msg);
+    }
+
+    private void echo(){;
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        while (!command.equals("bye")){
+            printMessageWithFormat(command);
+            command = sc.nextLine();
+        }
+        exit();
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +41,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Duke duke = new Duke();
+        duke.greet();
+        duke.echo();
     }
 }
