ef882e1fb877238c82c83cbb5f3433d03986cb55
Imran <imranr2@msn.com>
2021-08-18 20:48:32 +0800

    refactor: Shift Main and Lifeline into lifeline package

--- src/main/java/lifeline/Lifeline.java
+++ src/main/java/lifeline/Lifeline.java
@@ -1,3 +1,5 @@
+package lifeline;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
--- src/main/java/lifeline/Main.java
+++ src/main/java/lifeline/Main.java
@@ -1,3 +1,5 @@
+package lifeline;
+
 public class Main {
     public static void main(String[] args) {
         Lifeline lifeline = new Lifeline("./save/tasks.json");
