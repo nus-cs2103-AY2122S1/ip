5105bc0277ce76db07cedb19f2057010a811140f
whoisjustinngo <justin.ngo@u.nus.edu>
2021-08-18 18:17:47 +0800

    MyList.java: init custom list class

---
+++ src/main/java/MyList.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyList {
+
+    private List<String> items;
+
+    MyList() {
+        this.items = new ArrayList<String>(100);
+    }
+
+    protected void addItem(String item) {
+        this.items.add(item);
+    }
+
+    protected String getAllItems() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < this.items.size(); i++) {
+            String itemNum = Integer.toString(i + 1) + ". ";
+            sb.append(itemNum);
+            sb.append(this.items.get(i));
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
