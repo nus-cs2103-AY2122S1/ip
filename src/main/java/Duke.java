bd941de0011f82431892f3a58a109804e576c921
tsy24 <szeying3@gmail.com>
2021-08-18 14:55:40 +0800

    Add list ability

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -5,21 +6,29 @@ public class Duke {
         String startText = "Hello! I'm Duke\n" + "What can I do for you?";
         System.out.println(startText);
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            String command = sc.next();
+        ArrayList<String> savedInputs = new ArrayList<>();
+        while (sc.hasNextLine()) {
+            String command = sc.nextLine();
             if (command.equals("bye")) {
-                String closingMessage =  "Goodbye! Hope to see you again soon!";
+                String closingMessage =  "Goodbye! Hope to see you again soon!\n";
                 System.out.println(outputTemplate(closingMessage));
                 sc.close();
                 break;
+            } else if (command.equals("list")) {
+                String list = "";
+                for (int i = 0; i < savedInputs.size(); i++) {
+                    list += Integer.toString(i + 1) + ". " + savedInputs.get(i) + "\n";
+                }
+                System.out.println(outputTemplate(list));
             } else {
-                System.out.println(outputTemplate(command));
+                savedInputs.add(command);
+                System.out.println(outputTemplate(command + "\n"));
             }
         }
     }
 
     private static String outputTemplate(String output) {
         String line = "~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~ * ~";
-        return line + "\n" + output + "\n" + line;
+        return line + "\n" + output + line;
     }
 }
