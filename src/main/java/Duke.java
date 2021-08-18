12fa95e39d8e196a8895d41c855d2f003adb5337
Allard <AllardQuek@users.noreply.github.com>
2021-08-18 11:43:14 +0800

    Store text entered and display tasks when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,50 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-    public final static String lines = "----------------------------------------------------\n";
+    private final static String logo = " ____        _        \n"
+                                     + "|  _ \\ _   _| | _____ \n"
+                                     + "| | | | | | | |/ / _ \\\n"
+                                     + "| |_| | |_| |   <  __/\n"
+                                     + "|____/ \\__,_|_|\\_\\___|\n";
+    private final static String lines = "\t----------------------------------------------------\n";
+    private final static String greetings = "Greetings friend! I am your personal assistant,\n" + 
+                                            logo + 
+                                            "\nWhat can I do for you?\n";
+    private static ArrayList<Task> tasks = new ArrayList<Task>();
 
+    /**
+     * Prints a message with a line of dashes before and after it.
+     */
     public static void printFormattedMessage(String message) {
-        System.out.println(lines + message + lines);
+        System.out.println(lines + "\t" + message + lines);
     }
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Greetings friend! I am your personal assistant,\n" + 
-                            logo + 
-                            "\nWhat can I do for you?\n");
+        System.out.println(greetings);
         Scanner sc = new Scanner(System.in);
-        String command;
-        
-        do {
-            command = sc.nextLine();
-            printFormattedMessage(command + "\n");
-        } while (!command.equals("bye"));
+        String command = sc.nextLine();
 
+        while (!command.equals("bye")) {
+            // * Print list of tasks if command is "list"
+            String taskListMessage = "I present to you, your collection of tasks!\n\n";
+
+            if (command.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    String t = "\t" + i + ". " + tasks.get(i);
+                    taskListMessage += t + "\n";
+                }
+                printFormattedMessage(taskListMessage);
+            } else {
+                // * For any other text input, add a task to the list
+                tasks.add(new Task(command));
+                printFormattedMessage("added: " + command + "\n");  
+            }
+            
+            // * Ask user for next command
+            command = sc.nextLine();         
+        }
+        
         printFormattedMessage("Bye. Hope to see you again soon!\n");
         sc.close();
     }
