4dc1009d3ef8d2ac07f61b8734b7d1c6048c7421
LuoZhijie-tom <e0492460@u.nus.edu>
2021-08-18 16:15:03 +0800

    added Level-2: add and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,10 +3,18 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String command = new String();
+        ArrayList<String> store = new ArrayList<>();
         System.out.println("Hello! I'm Tom! What can I do for you?");
         command = sc.nextLine();
         while(!command.equals("bye")){
-            System.out.println(command);
+            if(command.equals("list")){
+                for(int i = 0; i < store.size(); i++){
+                    System.out.println((i + 1) + "." + store.get(i));
+                }
+            } else {
+                store.add(command);
+                System.out.println("added: " + command);
+            }
             command = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
