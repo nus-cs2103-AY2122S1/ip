9fc9a944c1fcd6f0147f2252d489cfb7e34944de
Ryan Peh <qwertz0808@gmail.com>
2021-08-19 00:28:29 +0800

    Add error handling functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,8 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static final String DIVIDER = "──────────────────────────────────────────────────────────\n";
-    private static final String DOUBLE_DIVIDER = "══════════════════════════════════════════════════════════\n";
+    private static final String DIVIDER = "───────────────────────────────────────────────────────────────────────\n";
+    private static final String DOUBLE_DIVIDER = "═══════════════════════════════════════" +
+            "════════════════════════════════\n";
 
     public static Scanner scanner = new Scanner(System.in);
     public static ArrayList<Task> taskList = new ArrayList<>();
@@ -27,22 +28,35 @@ public class Duke {
         System.out.print(DIVIDER);
     }
 
-    public static Task createTask(String taskType, String taskDesc) {
+    public static Task createTask(String taskType, String taskDesc) throws DukeException {
         switch (taskType) {
             case "todo":
+                if (taskDesc.isEmpty()) {
+                    throw new DukeException("Invalid Command. The description of a " + taskType + " cannot be empty.");
+                }
                 return new Todo(taskDesc);
             case "event":
+                if (taskDesc.isEmpty()) {
+                    throw new DukeException("Invalid Command. The description of a " + taskType + " cannot be empty.");
+                } else if (!taskDesc.contains(" /at ")) {
+                    throw new DukeException("Invalid Command. The description of a event must include a '/at'.");
+                }
                 String[] eventParams = taskDesc.split(" /at ");
                 return new Event(eventParams[0], eventParams[1]);
             case "deadline":
+                if (taskDesc.isEmpty()) {
+                    throw new DukeException("Invalid Command. The description of a " + taskType + " cannot be empty.");
+                } else if (!taskDesc.contains(" /by ")) {
+                    throw new DukeException("Invalid Command. The description of a deadline must include a '/by'.");
+                }
                 String[] deadlineParams = taskDesc.split(" /by ");
                 return new Deadline(deadlineParams[0], deadlineParams[1]);
             default:
-                return null;
+                throw new DukeException("Invalid Command. Are you sure you have typed in the command properly?");
         }
     }
 
-    public static void addTask(String taskType, String taskDesc) {
+    public static void addTask(String taskType, String taskDesc) throws DukeException {
         Task task = createTask(taskType, taskDesc);
         taskList.add(task);
         System.out.println(DIVIDER + "Got it. I have added this task:\n  " + task +
@@ -59,32 +73,35 @@ public class Duke {
 
     public static void getInputs() {
         while (true) {
-            String input = scanner.nextLine();
-            String taskType, taskDesc;
+            try {
+                String input = scanner.nextLine();
+                String taskType, taskDesc;
 
-            if (input.contains(" ")) {
-                taskType = input.substring(0, input.indexOf(' '));
-                taskDesc = input.substring(input.indexOf(' ') + 1);
-            } else {
-                taskType = input;
-                taskDesc = "";
+                if (input.contains(" ")) {
+                    taskType = input.substring(0, input.indexOf(' '));
+                    taskDesc = input.substring(input.indexOf(' ') + 1);
+                } else {
+                    taskType = input;
+                    taskDesc = "";
+                }
+
+                switch (taskType) {
+                    case "bye":
+                        byeUser();
+                        return;
+                    case "list":
+                        printList();
+                        break;
+                    case "done":
+                        markTaskAsDone(taskDesc);
+                        break;
+                    default:
+                        addTask(taskType, taskDesc);
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.print(DIVIDER + e.getMessage() + "\n" + DIVIDER);
             }
-
-            switch (taskType) {
-                case "bye":
-                    byeUser();
-                    return;
-                case "list":
-                    printList();
-                    break;
-                case "done":
-                    markTaskAsDone(taskDesc);
-                    break;
-                default:
-                    addTask(taskType, taskDesc);
-                    break;
-            }
-
         }
     }
 
