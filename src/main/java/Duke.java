87638eb8f459b766113649b8747cdb0766b7e7a4
MGRL2201 <e0638854@u.nus.edu>
2021-08-18 22:04:56 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         System.out.println("    ____________________________________________________________");
         System.out.println("     Hello! I'm Duke");
@@ -13,18 +13,25 @@ public class Duke {
         while(!input.replaceAll("\\s","").toLowerCase().equals("bye")) {
             if (input.replaceAll("\\s","").toLowerCase().equals("list")) {
                 System.out.println("    ____________________________________________________________");
+                System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println(String.format("     %d. %s", (i + 1), list.get(i)));
+                    Task item = list.get(i);
+                    System.out.println(String.format("     %d.[%s] %s", (i + 1), item.getStatusIcon(), item.description));
                 }
+            } else if (input.length() > 5 && input.toLowerCase().substring(0, 5).equals("done ")) {
+                String modInput = input.replaceAll("\\s","");
+                Task item = list.get(Integer.parseInt(modInput.substring(modInput.length() - 1)) - 1);
+                item.markAsDone();
                 System.out.println("    ____________________________________________________________");
-                input = in.nextLine();
+                System.out.println("     Nice! I've marked this task as done: ");
+                System.out.println(String.format("       [X] %s", item.description));
             } else {
-                list.add(input);
+                list.add(new Task(input));
                 System.out.println("    ____________________________________________________________");
                 System.out.println("     added: " + input);
-                System.out.println("    ____________________________________________________________");
-                input = in.nextLine();
             }
+            System.out.println("    ____________________________________________________________");
+            input = in.nextLine();
         }
         System.out.println("    ____________________________________________________________");
         System.out.println("     Bye. Hope to see you again soon!");
