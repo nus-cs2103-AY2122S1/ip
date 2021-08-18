e4d2e844865ee4f1485363ae424e9d877de2d780
TohWeiJie01 <tohweijie01@gmail.com>
2021-08-18 16:54:01 +0800

    Improved Logo for Duke.java and added private fields and constructor to initialise Duke in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
+    private String command;
+
+    private Duke(String command) {
+        this.command = command;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("Hello from\n" + logo + "How may I help you today master?\n");
+        System.out.println("-------------------------------------------------------------------");
     }
 }
