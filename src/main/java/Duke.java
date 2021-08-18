bb73dccd2470650fa1e6e45504c9a0fbcf13ab10
Jason <jasonang0210@gmail.com>
2021-08-19 00:33:11 +0800

    'changes'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,45 +2,57 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private static String[] myList = new String[100];
+    private static Task[] myList = new Task[100];
     private static int myListLength = 0;
+    public static boolean done = false;
 
-    private static void echo() {
-        Scanner myScanner = new Scanner(System.in);
-        boolean done = false;
-        while (!done) {
-            String input = myScanner.nextLine();
-            if (input.equals("bye")) {
-                done = true;
-                System.out.println("Bye for now!");
-            } else {
-                System.out.println(input);
-            }
+    private static void endBot() {
+        done = true;
+        System.out.println("Bye for now!");
+    }
+
+    private static void listTasks() {
+        System.out.println("Here is your to-do list!");
+        for (int i = 0; i < myListLength; i++) {
+            System.out.println(i + 1 + ". [" + myList[i].getStatusIcon() + "] " + myList[i].description);
         }
     }
 
-    private static void add() {
-        Scanner myScanner = new Scanner(System.in);
-        boolean done = false;
-        while (!done) {
-            String input = myScanner.nextLine();
-            if (input.equals("bye")) {
-                done = true;
-                System.out.println("Bye for now!");
-            } else if (input.equals("list")) {
-                for (int i = 0; i < myListLength; i++) {
-                    System.out.println(i + 1 + ". " + myList[i]);
-                }
-            } else {
-                myList[myListLength] = input;
-                myListLength++;
-                System.out.println("added: " + input);
-            }
+    private static void addTask(String input) {
+        myList[myListLength] = new Task(input);
+        myListLength++;
+        System.out.println("added: " + input);
+    }
+
+
+    private static void markAsDone(String input) {
+        String[] segments = input.split(" ");
+        try {
+            int index = Integer.parseInt(segments[segments.length - 1]);
+            Task myTask = myList[index - 1];
+            myTask.markAsDone();
+            System.out.println("I've marked this task as done:\n[X] " + myTask.description);
+        } catch (NumberFormatException e) {
+            System.out.println("Please input a number after the keyword: done");
+        } catch (ArrayIndexOutOfBoundsException | NullPointerException e ) {
+            System.out.println("Please input a valid task index");
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Sup! I'm Luka, your personal assistant.\n");
-        add();
+        Scanner myScanner = new Scanner(System.in);
+        while (!done) {
+            String input = myScanner.nextLine();
+            if (input.equals("bye")) {
+                endBot();
+            } else if (input.equals("list")) {
+                listTasks();
+            } else if (input.startsWith("done")) {
+                markAsDone(input);
+            } else {
+                addTask(input);
+            }
+        }
     }
 }
