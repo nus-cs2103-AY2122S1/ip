b0e139dbc297f004884879c3c9fdf4706e753899
Clive <e0531337@u.nus.edu>
2021-08-18 19:10:33 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-    static String[] listOfText = new String[100];
+    static Task[] listOfText = new Task[100];
 
     private static void bye() {
         System.out.println("Bye. Hope to see you again soon!");
@@ -10,7 +10,7 @@ public class Duke {
     private static void list(String line) {
         for(int i = 0; i < 100; i++) {
             if(listOfText[i] == null) {
-                listOfText[i] = line;
+                listOfText[i] = new Task(line);
                 break;
             }
         }
@@ -20,9 +20,19 @@ public class Duke {
     private static void printList() {
         for(int i = 0; i < 100; i++) {
             if(listOfText[i] != null) {
-                System.out.println(i+1 + ". " + listOfText[i]);
+                System.out.println(i+1 + "." + listOfText[i].getStatusIcon() + " " + listOfText[i].description);
             }
         }
+    }
+
+    private static void changeStatus(int number) { //need check if number is valid
+        if ((number > 0) && (number <= 100)) {
+            if(listOfText[number-1] != null) {
+                listOfText[number-1].markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n[X] " + listOfText[number-1].description );
+            }
+        }
+
     }
 
     public static void main(String[] args) {
@@ -31,9 +41,13 @@ public class Duke {
         String input = sc.nextLine();
         while(!(input.equals("bye"))) {
 
-
             if(input.equals("list")) {
                 printList();
+                input = sc.nextLine();
+            }else if (input.length() > 5 && input.substring(0, 5).equals("done ")) {
+                String[] split = input.split("\\s+");
+                int number= Integer.parseInt(split[1]);
+                changeStatus(number);
                 input = sc.nextLine();
             } else {
                 list(input);
@@ -46,3 +60,4 @@ public class Duke {
         }
     }
 }
+
