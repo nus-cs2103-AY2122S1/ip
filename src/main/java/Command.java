957590cb9504502bf11bd0ec5c8c560ac73e9120
nbvannhi <nbvannhi@gmail.com>
2021-08-19 04:06:28 +0800

    Add ToDo, Event, Deadline

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -13,22 +13,31 @@ public class Command {
     sc.close();
   }
 
-  protected boolean run(String cmd) {
+  public boolean run(String cmd) {
     System.out.println(SPLIT_LINE);
     String[] line = cmd.split(" ");
     switch (line[0].toUpperCase()) {
       case "GREET": greet(); break;
-      case "BYE": exit(); break;
       case "ECHO": echo(cmd); break;
+      case "BYE": exit(); break;
       case "LIST": list(); break;
       case "DONE": done(Integer.parseInt(line[1])); break;
+      case "TODO": todo(cmd); break;
+      case "DEADLINE":
+      case "EVENT":
+        String desc = cmd.substring(cmd.indexOf(" ") + 1, cmd.indexOf("/") - 1);
+        String date = cmd.substring(cmd.indexOf("/") + 4);
+        if (line[0].toUpperCase().equals("DEADLINE"))
+          deadline(desc, date);
+        else event(desc, date);
+        break;
       default: add(cmd);
     }
     System.out.println(SPLIT_LINE);
     return !cmd.equals("bye");
   }
 
-  protected void greet() {
+  public void greet() {
     String logo =
       "\t ██████╗ ██╗      ██████╗  ██████╗ ███╗   ███╗\n" +
       "\t ██╔══██╗██║     ██╔═══██╗██╔═══██╗████╗ ████║\n" +
@@ -43,35 +52,60 @@ public class Command {
     System.out.println(logo + greeting);
   }
 
-  protected void echo(String str) {
+  public void echo(String str) {
     System.out.println("\t " + str);
   }
 
-  protected void exit() {
+  public void exit() {
     System.out.println("\t Bye. Hope to see you again soon!");
   }
 
-  protected void add(String str) {
-    Task t = new Task(str);
+  public void add(String desc) {
+    Task t = new Task(desc);
     Task.tasks.add(t);
     System.out.println("\t added: " + t.description);
   }
 
-  protected void list() {
+  public void list() {
     System.out.println("\t Here are the tasks in your list:");
     for (int i = 1; i <= Task.tasks.size(); ++i) {
       Task t = Task.tasks.get(i-1);
-      System.out.println("\t " + i + ". [" +
-        t.getStatusIcon() + "] " +
-        t.description);
+      System.out.println("\t " + i + ". " + t);
     }
   }
 
-  protected void done(int i) {
+  public void done(int i) {
     Task t = Task.tasks.get(i-1);
     t.markAsDone();
     System.out.println(
       "\t Nice! I've marked this task as done:\n" +
-      "\t   [" + t.getStatusIcon() + "] " + t.description);
+      "\t   " + t);
+  }
+
+  public void todo(String desc) {
+    ToDo td = new ToDo(desc);
+    Task.tasks.add(td);
+    System.out.println(
+      "\t Got it. I've added this task:\n" +
+      "\t   " + td + "\n" +
+      "\t Now you have " + Task.tasks.size() + " tasks in the list.");
+  }
+
+  public void deadline(String desc, String by) {
+    Deadline d = new Deadline(desc, by);
+    Task.tasks.add(d);
+    System.out.println(
+      "\t Got it. I've added this task:\n" +
+        "\t   " + d + "\n" +
+        "\t Now you have " + Task.tasks.size() + " tasks in the list.");
+  }
+
+  public void event(String desc, String at) {
+    Event e = new Event(desc, at);
+    Task.tasks.add(e);
+    System.out.println(
+      "\t Got it. I've added this task:\n" +
+        "\t   " + e + "\n" +
+        "\t Now you have " + Task.tasks.size() + " tasks in the list.");
   }
 }
