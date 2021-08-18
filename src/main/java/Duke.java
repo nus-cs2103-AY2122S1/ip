9b2cfaeccd94c04a855dd06cded76b6b4924011e
Chen-Yiyang <iycspfa@gmail.com>
2021-08-18 15:36:12 +0800

    Level-3 Implement Mark-as-Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,7 @@ class Duke {
     // constants for special commands
     private static final String CMD_BYE = "bye";
     private static final String CMD_LIST = "list";
+    private static final String CMD_DONE = "done";
 
     // static fields
     private static List<Task> tasks = new ArrayList<>();
@@ -16,6 +17,12 @@ class Duke {
     }
 
     private static void displayList() {
+        if (tasks.size() == 0) {
+            System.out.println("\tYou don't have any task now.");
+            return;
+        }
+
+        System.out.println("\tHere are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println(String.format("\t%d. %s",
                     i+1, tasks.get(i)));
@@ -27,21 +34,33 @@ class Duke {
         System.out.println("\tBye. Hope to see you again.");
     }
 
+    private static void markAsDone(int id) {
+        tasks.get(id-1).setDone(true);
+
+        System.out.println("\tNice! I've marked this task as done: ");
+        System.out.println("\t  " + tasks.get(id-1));
+    }
+
     public static void main(String[] args) {
         displayGreetings();
 
         Scanner sc = new Scanner(System.in);
-        String currCommand = sc.nextLine();
+        String currLine = sc.nextLine();
+        String currCommand = currLine.split(" ")[0];
 
         while (!currCommand.equals(CMD_BYE)) {
             if (currCommand.equals(CMD_LIST)) {
                 displayList();
-            } else {
-                tasks.add(new Task(currCommand));
-                System.out.println("\t added: " + currCommand);
+            } else if (currCommand.equals(CMD_DONE)) {
+                int doneId = Integer.parseInt(currLine.split(" ")[1]);
+                markAsDone(doneId);
+            } else {    // add
+                tasks.add(new Task(currLine));
+                System.out.println("\t added: " + currLine);
             }
 
-            currCommand = sc.nextLine();
+            currLine = sc.nextLine();
+            currCommand = currLine.split(" ")[0];
         }
 
         displayBye();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,22 @@
 class Task {
-    private final String name;
+    private String name;
+    private boolean isDone;
 
     public Task(String name) {
         this.name = name;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
     @Override
     public String toString() {
-        return this.name;
+        return String.format("[%s] %s", this.getStatusIcon(), this.name);
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
     }
 }
