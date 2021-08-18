97f79b9134f4362a691139654803654768ea5e39
Jason <jasonang0210@gmail.com>
2021-08-18 23:32:57 +0800

    changes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static void echo() {
+        Scanner myScanner = new Scanner(System.in);
+        boolean done = false;
+        while (!done) {
+            String input = myScanner.nextLine();
+            if (input.equals("bye")) {
+                done = true;
+                System.out.println("Bye for now!");
+            } else {
+                System.out.println(input);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Sup! I'm Luka, your personal assistant.\n");
+        echo();
     }
 }
