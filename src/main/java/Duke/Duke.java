56ddc3ad8b4140257d2a4065942a555be56542a5
Cai Kaian <caipngg@gmail.com>
2021-08-18 16:05:44 +0800

    Fix ordering of modifiers in Duke class

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -1,12 +1,12 @@
 package Duke;
 
 public class Duke {
-    private final static String LOGO = " ____        _        \n"
+    private static final String LOGO = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
-    private final static String GREETING_MESSAGE = "Hello from\n" + LOGO;
+    private static final String GREETING_MESSAGE = "Hello from\n" + LOGO;
 
     private void say(String message) {
         System.out.println(message);
