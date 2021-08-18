9d8c7e1b9495fd259ed946d484a7781659783108
itzblim <>
2021-08-18 17:06:53 +0800

    Level-1 Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    /**
+     * Main execution when Duke is run.
+     *
+     * @param args Will not be used
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String welcomeMsg = "What's up, I'm duke!";
+        printMsg(welcomeMsg);
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean exit = false;
+
+        while(!exit) {
+            String nextInput = sc.nextLine();
+            if (nextInput.equals("bye")) {
+                printMsg("Bye mate!");
+                exit = true;
+            } else {
+                printMsg(nextInput);
+            }
+        }
+    }
+
+    /**
+     * Prints out an indented message with dividers.
+     *
+     * @param msg Message to be printed, as a string.
+     */
+    public static void printMsg(String msg) {
+        System.out.println("    ---");
+        System.out.println("    " + msg);
+        System.out.println("    ---");
     }
 }
