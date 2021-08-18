ad21dcfef892fc7b9da39361fc23e877b4b1ccf1
jodichoo <jodi.choo@gmail.com>
2021-08-18 17:34:10 +0800

    Refactor some code in Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
     private static final TaskList list = new TaskList();
 
-    private static String[] processInput(String[] arr) {
+    private static String[] parseInput(String[] arr) {
         String desc = "";
         String time = "";
         boolean slash = false;
@@ -25,7 +26,23 @@ public class Duke {
     }
 
     private static boolean isAddingNewTask(String str) {
-        return str.equals("todo") || str.equals("deadline") || str.equals("event");
+        return str.startsWith("todo") || str.startsWith("deadline") || str.startsWith("event");
+    }
+
+    private static void addTask(String command, String[] args) {
+        switch (command) {
+            case "todo":
+                list.add(new Todo(args[0]));
+                break;
+            case "deadline":
+                list.add(new Deadline(args[0], args[1]));
+                break;
+            case "event":
+                list.add(new Event(args[0], args[1]));
+                break;
+            default:
+                break;
+        }
     }
 
     public static void main(String[] args) {
@@ -38,32 +55,27 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         Scanner sc = new Scanner(System.in);
         while (true) {
-            String input = sc.nextLine();
+            String input = sc.nextLine().strip();
 
             String[] split = input.split(" ");
-            String first = split[0];
 
             if (input.equals("bye")) {
+                //exit the program
                 break;
             } else if (input.equals("list")) {
+                //print list of tasks
                 System.out.println(list.toString());
-            } else if (first.equals("done")) {
+            } else if (input.startsWith("done")) {
+                //mark task as done
                 list.done(split[1]);
-            } else if (isAddingNewTask(first)) {
-                String[] processed = processInput(split);
-                String desc = processed[0];
+            } else if (isAddingNewTask(input)) {
+                //user is adding a new task
+                String command = split[0];
+                String[] taskArgs = parseInput(split);
 
-                if (first.equals("todo")) {
-                    list.add(new Todo(desc));
-                } else if (first.equals("deadline")) {
-                    String time = processed[1];
-                    list.add(new Deadline(desc, time));
-                } else if (first.equals("event")) {
-                    String time = processed[1];
-                    list.add(new Event(desc, time));
-                }
+                addTask(command, taskArgs);
             } else {
-                System.out.println("Invalid command");
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :(");
             }
         }
         sc.close();
