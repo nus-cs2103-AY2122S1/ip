5b69cb16b76264a28522e351459a4166f1bda569
Lutfi Randiva <luffingluffy@gmail.com>
2021-08-18 15:09:35 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,35 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        String intro = "Hello! I'm Biscuit.\n"
-                + "What do you want me to do?\n";
-        System.out.println(intro);
 
+    static String introMsg = "Hello! I'm Biscuit.\n"
+            + "What do you want me to do?\n";
+    static String byeMsg = "Bye. Hope to see you again soon!";
+
+    static void introduce() {
+        System.out.println(introMsg);
+    }
+
+    static void reply() {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String userInput = sc.nextLine();
 
             if (userInput.equals("bye")) {
-                String bye = "Bye. Hope to see you again soon!";
-                System.out.println(bye);
+                System.out.println(byeMsg);
                 break;
             }
 
             System.out.println(userInput);
         }
+    }
+
+    static void run() {
+        introduce();
+        reply();
+    }
+
+    public static void main(String[] args) {
+        run();
     }
 }
