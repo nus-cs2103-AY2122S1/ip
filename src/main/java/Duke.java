3873522b3faf89463aa6cada7d4a564897172c60
eltonyeh <e0550632@u.nus.edu>
2021-08-19 02:40:06 +0800

    Add a skeletal version of Duke that echos the command and exit when type bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "\t____        _        \n"
+                + "\t|  _ \\ _   _| | _____ \n"
+                + "\t| | | | | | | |/ / _ \\\n"
+                + "\t| |_| | |_| |   <  __/\n"
+                + "\t|____/ \\__,_|_|\\_\\___|\n";
+        String horizontalLine = "\t____________________________________________________________";
+        String greetingMessage = horizontalLine
+                + "\n\tHello from \n"
+                + logo
+                + "\tHow can I help you?\n"
+                + horizontalLine;
+        String byeMessage = horizontalLine
+                + "\n\tBye! Thank you for chatting with me!\n"
+                + horizontalLine;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(greetingMessage);
+
+        String input = scanner.next();
+
+        while (!input.equals("bye")) {
+            System.out.println(horizontalLine + "\n\t" + input + "\n" + horizontalLine);
+            input = scanner.next();
+        }
+
+        System.out.println(byeMessage);
     }
 }
