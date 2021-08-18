13f2692cedf13f36c4cf65f64df75e6a339e902b
Allard <AllardQuek@users.noreply.github.com>
2021-08-18 08:45:58 +0800

    first attempt Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+    public final static String lines = "----------------------------------------------------\n";
+
+    public static void printFormattedMessage(String message) {
+        System.out.println(lines + message + lines);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Greetings friend! I am your personal assistant,\n" + 
+                            logo + 
+                            "\nWhat can I do for you?\n");
+        Scanner sc = new Scanner(System.in);
+        String command;
+        
+        do {
+            command = sc.nextLine();
+            printFormattedMessage(command + "\n");
+        } while (!command.equals("bye"));
+
+        printFormattedMessage("Bye. Hope to see you again soon!\n");
+        sc.close();
     }
 }
