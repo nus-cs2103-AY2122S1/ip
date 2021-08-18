9dac262891c5525cc8e1e61a4704644092a3973f
Lutfi Randiva <luffingluffy@gmail.com>
2021-08-18 17:10:41 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
 public class Duke {
-    static String[] botList = new String[100];
+    static Task[] botList = new Task[100];
     static int numItems = 0;
 
     static String introMsg = "Hello! I'm Biscuit.\n"
             + "What do you want me to do?\n";
     static String byeMsg = "Bye. Hope to see you again soon!";
+    static String listMsg = "Here are the tasks in your list:\n";
+    static String doneMsg = "Nice! I've marked this task as done: \n";
 
     static void introduce() {
         System.out.println(introMsg);
@@ -14,24 +16,31 @@ public class Duke {
 
     static void reply() {
         Scanner sc = new Scanner(System.in);
+        outerLoop:
         while (true) {
             String userInput = sc.nextLine();
+            String[] tokens = userInput.split("\\s+", 2);
+            String command = tokens[0];
 
-            if (userInput.equals("list")) {
-                for (int i = 0; i < numItems; i++) {
-                    System.out.println(botList[i]);
-                }
-                continue;
-            }
-
-            if (userInput.equals("bye")) {
-                System.out.println(byeMsg);
-                break;
-            }
-
-            else {
-                botList[numItems++] = numItems + ". " + userInput;
-                System.out.println("added: " + userInput);
+            switch (command) {
+                case "list":
+                    System.out.println(listMsg);
+                    for (int i = 0; i < numItems; i++) {
+                        System.out.println((i + 1) + "." + botList[i]);
+                    }
+                    break;
+                case "done":
+                    int param = Integer.parseInt(tokens[1].strip()) - 1;
+                    System.out.println(doneMsg);
+                    botList[param].markAsDone();
+                    System.out.println(botList[param]);
+                    break;
+                case "bye":
+                    System.out.println(byeMsg);
+                    break outerLoop;
+                default: // Adds task
+                    botList[numItems++] = new Task(userInput);
+                    System.out.println("added: " + userInput);
             }
         }
     }
