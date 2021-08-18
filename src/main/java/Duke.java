4f5b7e035759d20606d10e3a1fa29918d46c60ed
ErnestCuong <hltc11032000@gmail.com>
2021-08-19 03:55:21 +0800

    Add Event, ToDo, Deadline Classes and update Duke to Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,6 +25,7 @@ public class Duke {
         return true;
     }
 
+
     public static void main(String[] args) {
         System.out.println("*******************************************");
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
@@ -37,6 +38,7 @@ public class Duke {
 
         while (!hasQuit && myScanner.hasNextLine()) {
             String userInput = myScanner.nextLine();
+
             if (userInput.equals("bye")) { // if user enters "bye"
                 hasQuit = true;
                 System.out.println("Bye. Hope to see you again soon!");
@@ -51,6 +53,7 @@ public class Duke {
                     && isInt(userInput.substring(5))
             ) { // if user wants to mark a task as done
                 int taskNum = Integer.parseInt(userInput.substring(5));
+
                 if (taskNum > numOfTasks || taskNum < 1) {
                     System.out.println("There is no task number " + taskNum);
                 } else {
@@ -59,10 +62,53 @@ public class Duke {
                     System.out.println("Nice! I've marked this task as done:\n  "
                             + curr.toString());
                 }
-            } else { // any other input from user
-                tasks[numOfTasks] = new Task(userInput);
+            } else if (userInput.length() > 5
+                    && userInput.substring(0, 5).equals("todo ")
+            ) {
+                Task curr = new ToDo(userInput.substring(5));
+                tasks[numOfTasks] = curr;
                 numOfTasks += 1;
-                System.out.println("added: " + userInput);
+                System.out.println("Got it. I've added this task:\n  "
+                        + curr.toString() + "\nNow you have " + numOfTasks
+                        + " task(s) in the list.");
+            } else if (userInput.length() >= 13
+                    && userInput.substring(0, 6).equals("event ")
+            ) {
+                int stopAt = 7;
+                while (stopAt + 5 < userInput.length() && !userInput.substring(stopAt, stopAt + 5).equals(" /at ")) {
+                    stopAt += 1;
+                }
+
+                if (!userInput.substring(stopAt, stopAt + 5).equals(" /at ")) {
+                    System.out.println("Wrong input!");
+                } else {
+                    Task curr = new Event(userInput.substring(6, stopAt), userInput.substring(stopAt + 5));
+                    tasks[numOfTasks] = curr;
+                    numOfTasks += 1;
+                    System.out.println("Got it. I've added this task:\n  "
+                            + curr.toString() + "\nNow you have " + numOfTasks
+                            + " task(s) in the list.");
+                }
+            } else if (userInput.length() >= 16
+                    && userInput.substring(0, 9).equals("deadline ")
+            ) {
+                int stopAt = 10;
+                while (stopAt + 5 < userInput.length() && !userInput.substring(stopAt, stopAt + 5).equals(" /by ")) {
+                    stopAt += 1;
+                }
+
+                if (!userInput.substring(stopAt, stopAt + 5).equals(" /by ")) {
+                    System.out.println("Wrong input!");
+                } else {
+                    Task curr = new Deadline(userInput.substring(9, stopAt), userInput.substring(stopAt + 5));
+                    tasks[numOfTasks] = curr;
+                    numOfTasks += 1;
+                    System.out.println("Got it. I've added this task:\n  "
+                            + curr.toString() + "\nNow you have " + numOfTasks
+                            + " task(s) in the list.");
+                }
+            } else { // any other input from user
+                System.out.println("Wrong input");
             }
             System.out.println("*******************************************");
         }
