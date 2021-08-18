3a4a3d011ec43ae33ea4d8684a7fdcc56e113144
jeremy-7007 <71486951+jeremy-7007@users.noreply.github.com>
2021-08-19 00:38:26 +0800

    Commit level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,12 +8,28 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
         System.out.println("Hello! I'm Nukem\n"
                 + "What can I do for you?");
+        String[] storage = new String[100];
+        int index = 0;
         String tempString;
         Scanner input = new Scanner(System.in);
         while (!(tempString = input.nextLine()).equals("bye")) {
-            System.out.println(tempString);
+            switch (tempString) {
+                case ("list"):
+                    for (int i = 0; i < storage.length; i++) {
+                        if (storage[i] == null) break;
+                        else {
+                            System.out.println((i + 1) + ". " + storage[i]);
+                        }
+                    }
+                    break;
+                default:
+                    storage[index] = tempString;
+                    index++;
+                    System.out.println("added: " + tempString);
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
