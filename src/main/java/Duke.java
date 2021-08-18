e9a5e0ee8006cd428815745cd2a4aef967ee09d4
Tang Zhiying <e0559633@u.nus.edu>
2021-08-18 21:00:23 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("   (=^ ･ｪ･^=) < Hello! I'm TiTi~ ");
+        System.out.println("                What would you like to do nya? ");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("   (=^ ･ｪ･^=) < " + input);
+            input = sc.nextLine();
+        }
+
+        System.out.println("   (=^ ‐ｪ‐^=) < Already done? Come back soon nya~");
     }
 }
