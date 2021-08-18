2f8c0ff6612ce0ba253fcec5840d390d4f8882a1
Jiwei99 <jwlim99@gmail.com>
2021-08-18 17:57:16 +0800

    Implement a skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,53 @@
+import java.util.Scanner;
+
 public class Duke {
+    private boolean active;
+
+    public Duke() {
+        this.active = true;
+    }
+
+    public void executeCommand(String command) {
+        switch (command) {
+            case "bye":
+                this.terminate();
+                break;
+            default:
+                this.echo(command);
+        }
+    }
+
+    public void terminate() {
+        this.active = false;
+        String message =
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(message);
+    }
+
+    public void echo(String input) {
+        String message =
+                "____________________________________________________________\n" +
+                " " + input + "\n" +
+                "____________________________________________________________\n";
+        System.out.println(message);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        Scanner sc = new Scanner(System.in);
+        String message =
+                "____________________________________________________________\n" +
+                " Hello! I'm Duke\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n";
+
+        System.out.println(message);
+        while (duke.active) {
+            String input = sc.nextLine();
+            duke.executeCommand(input);
+        }
     }
 }
+
