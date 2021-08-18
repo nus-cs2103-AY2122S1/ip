3c733cdd423275c4ce275fcdd14d9669bd380d2d
Zhang Guangxuan <zhangguangxuan99@gmail.com>
2021-08-18 10:45:59 +0800

    feat: add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;  // Import the Scanner clas
 
 public class Duke {
@@ -6,6 +7,7 @@ public class Duke {
         System.out.printf("Hello! I'm %s\n", botName);
         System.out.printf("What can I do for you?\n");
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        TaskList taskList = new TaskList();
         while (true) {
             String command = scanner.nextLine();
             boolean aborted = false;
@@ -14,8 +16,15 @@ public class Duke {
                     aborted = true;
                     System.out.printf("Bye. Hope to see you again soon!\n");
                     break;
+                case "list":
+                    ArrayList<Task> tasks = taskList.getTasks();
+                    for (int i = 0 ; i < tasks.size(); i++){
+                        System.out.printf("%d. %s\n", i+1, tasks.get(i));
+                    }
+                    break;
                 default:
-                    System.out.printf("%s\n", command);
+                    taskList.addTask(new Task(command));
+                    System.out.printf("added: %s\n", command);
                     break;
             }
             if (aborted) {
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks;
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+}
