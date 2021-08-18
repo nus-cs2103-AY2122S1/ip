bf73f982c020a0fd67cee2a98ef2948e25cd1e3c
HongshuW <hongshu.wang.uni@gmail.com>
2021-08-18 20:49:56 +0800

    Add Event.java

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Event {
+public class Event extends Task {
+    protected String at;
+
+    /**
+     * Constructor of the class `Event`.
+     *
+     * @param description Description of the task.
+     * @param at Time slot of the task.
+     */
+    public Event(String description, String at) {
+        super(description);
+        this.at = at;
+    }
+
+    /**
+     * Converts a task with time range to string.
+     *
+     * @return The string representation of a task with time range.
+     */
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + this.at + ")";
+    }
 }
