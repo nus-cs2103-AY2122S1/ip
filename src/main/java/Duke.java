72d701f73de00d0a9a7d52926e520b7c846377e2
hanif-kamal <hanif.kamal47@gmail.com>
2021-08-19 01:38:00 +0800

    Add error handling for incorrect inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,9 +9,9 @@ public class Duke {
         list.add(task);
     }
 
-    private static void listTasks() {
+    private static void listTasks() throws DukeException{
         if (list.size() < 1) {
-            System.out.println("You haven't added anything to the list yet! Try adding something.");
+            throw new DukeException("You haven't added anything to the list yet! Try adding something.");
         } else {
             if (list.size() == 1) {
                 System.out.println("Here is the sole task in your list:");
@@ -35,76 +35,80 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         Scanner scanner = new Scanner(System.in);
 
-        while(true) {
+        while (true) {
             String input = scanner.nextLine();
-            if (input.equals("bye")) { //Close Duke
-                break;
-            } else if (input.equals("list")) { //List current tasks and prompt for more input
-                listTasks();
-            } else if (input.startsWith("done")) {
-                try { //Treat the substring after 'done' as an int.
-                    int listIndex = Integer.parseInt(input.substring(5));
-                    if (listIndex <= list.size() && listIndex >= 1) {
-                        completeTask(listIndex);
+            try {
+                if (input.equals("bye")) {
+                    break;
+                } else if (input.equals("list")) {
+                    listTasks();
+                } else if (input.startsWith("done")) {
+                    try {
+                        int listIndex = Integer.parseInt(input.substring(5));
+                        if (listIndex <= list.size() && listIndex >= 1) {
+                            completeTask(listIndex);
+                        } else {
+                            throw new DukeException("Couldn't find that task in the list! Try again.");
+                        }
+                    } catch (NumberFormatException e) { //If 'done' is followed by a non-integer
+                        throw new DukeException("Please make sure only a number follows the command 'done'. Try again.");
+                    } catch (StringIndexOutOfBoundsException e) { //If 'done' is not followed by anything
+                        throw new DukeException("Please add a number after the command 'done'. Try again.");
+                    }
+                } else if (input.startsWith("deadline")) {
+                    if (!input.contains("/by")) {
+                        throw new DukeException("Please state the deadline for this task with /by! Try again.");
                     } else {
-                        System.out.println("Couldn't find that task in the list! Try again.");
+                        try {
+                            int deadlineIndex = input.indexOf("/by") + 4;
+                            String deadline = input.substring(deadlineIndex);
+                            String firstTrimmed = input.substring(9);
+                            String lastTrimmed = firstTrimmed.substring(0, firstTrimmed.indexOf("/by"));
+                            Deadline createdDeadlineTask = new Deadline(lastTrimmed, deadline);
+                            addToList(createdDeadlineTask);
+                            System.out.println("Got it. I've added this task:\n" + "  " + createdDeadlineTask + "\n"
+                                    + "Now you have " + list.size() + " task" + (list.size() == 1 ? "" : "s") +
+                                    " in the list.");
+                        } catch (StringIndexOutOfBoundsException e) {
+                            throw new DukeException("Please ensure that there is a task description and deadline. " +
+                                    "Try again.");
+                        }
                     }
-                } catch (NumberFormatException e) { //If 'done' is followed by a non-integer
-                    System.out.println("Please make sure only a number follows the command 'done'. Try again.");
-                } catch (StringIndexOutOfBoundsException e) { //If 'done' is not followed by anything
-                    System.out.println("Please add a number after the command 'done'. Try again.");
-                }
-            } else if (input.startsWith("deadline")) {
-                if (!input.contains("/by")) {
-                    System.out.println("Please state the deadline for this task with /by! Try again.");
-                } else {
+                } else if (input.startsWith("todo")) {
                     try {
-                        int deadlineIndex = input.indexOf("/by") + 4; //need try catch
-                        String deadline = input.substring(deadlineIndex);
-                        String firstTrimmed = input.substring(9);
-                        String lastTrimmed = firstTrimmed.substring(0, firstTrimmed.indexOf("/by"));
-                        Deadline createdDeadlineTask = new Deadline(lastTrimmed, deadline);
-                        addToList(createdDeadlineTask);
-                        System.out.println("Got it. I've added this task:\n" + "  " + createdDeadlineTask + "\n"
+                        String trimmed = input.substring(5);
+                        Todo createdTodoTask = new Todo(trimmed);
+                        addToList(createdTodoTask);
+                        System.out.println("Got it. I've added this task:\n" + "  " + createdTodoTask + "\n"
                                 + "Now you have " + list.size() + " task" + (list.size() == 1 ? "" : "s") +
                                 " in the list.");
                     } catch (StringIndexOutOfBoundsException e) {
-                        System.out.println("Please ensure that there is a task description and deadline. Try again.");
+                        throw new DukeException("Please add the task information. Try again.");
                     }
-                }
-            } else if (input.startsWith("todo")){
-                try {
-                    String trimmed = input.substring(5);
-                    Todo createdTodoTask = new Todo(trimmed);
-                    addToList(createdTodoTask);
-                    System.out.println("Got it. I've added this task:\n" + "  " + createdTodoTask + "\n"
-                            + "Now you have " + list.size() + " task" + (list.size() == 1 ? "" : "s") +
-                            " in the list.");
-                } catch (StringIndexOutOfBoundsException e) {
-                    System.out.println("Please add the task information. Try again.");
-                }
-            } else if (input.startsWith("event")){
-                if (!input.contains("/at")) {
-                    System.out.println("Please state when the event will be held with /at! Try again.");
+                } else if (input.startsWith("event")) {
+                    if (!input.contains("/at")) {
+                        throw new DukeException("Please state when the event will be held with /at! Try again.");
+                    } else {
+                        try {
+                            int atIndex = input.indexOf("/at") + 4;
+                            String at = input.substring(atIndex);
+                            String firstTrimmed = input.substring(6);
+                            String lastTrimmed = firstTrimmed.substring(0, firstTrimmed.indexOf("/at"));
+                            Event createdEventTask = new Event(lastTrimmed, at);
+                            addToList(createdEventTask);
+                            System.out.println("Got it. I've added this task:\n" + "  " + createdEventTask + "\n"
+                                    + "Now you have " + list.size() + " task" + (list.size() == 1 ? "" : "s") +
+                                    " in the list.");
+                        } catch (StringIndexOutOfBoundsException e) {
+                            throw new DukeException("Please ensure that there is an event time. Try again.");
+                        }
+                    }
                 } else {
-                    try {
-                        int atIndex = input.indexOf("/at") + 4;
-                        String at = input.substring(atIndex);
-                        String firstTrimmed = input.substring(6);
-                        String lastTrimmed = firstTrimmed.substring(0, firstTrimmed.indexOf("/at"));
-                        Event createdEventTask = new Event(lastTrimmed, at);
-                        addToList(createdEventTask);
-                        System.out.println("Got it. I've added this task:\n" + "  " + createdEventTask + "\n"
-                                + "Now you have " + list.size() + " task" + (list.size() == 1 ? "" : "s") +
-                                " in the list.");
-                    } catch (StringIndexOutOfBoundsException e) {
-                        System.out.println("Please ensure that there is an event time. " +
-                                "Try again.");
-                    }
+                    throw new DukeException("I didn't quite get what you meant. To add a task, begin with " +
+                            "'deadline', 'event' or 'todo'.");
                 }
-            } else {
-                System.out.println("I didn't quite get what you meant. To add a task, begin with 'deadline', 'event'" +
-                        " or 'todo'.");
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
