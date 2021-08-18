b2b0c53e1d238efaab2b48bb569da40c17e58702
Daniel Sim <dsrecompassvale@gmail.com>
2021-08-18 18:50:56 +0800

    Add basic echo and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,17 +2,33 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo);
-        System.out.println("Hello there, I'm Duke!");
-        System.out.println("What can I do for you today?");
+        String logo = "\t ____        _        \n"
+                + "\t|  _ \\ _   _| | _____ \n"
+                + "\t| | | | | | | |/ / _ \\\n"
+                + "\t| |_| | |_| |   <  __/\n"
+                + "\t|____/ \\__,_|_|\\_\\___|\n";
+        String format = "\t%s\n";
+        String horizontalLine = "______________________________________________________";
+
+        System.out.print(logo);
+        System.out.printf(format, horizontalLine);
+        System.out.printf(format, "Hello there, I'm Duke!");
+        System.out.printf(format, "What can I do for you today?");
+        System.out.printf(format, horizontalLine);
+
         Scanner sc = new Scanner(System.in);
         while (true) {
-            break;
+            String input = sc.next();
+            if (input.equals("bye")) {
+                System.out.printf(format, horizontalLine);
+                System.out.printf(format, "Goodbye. Have a nice day!");
+                System.out.printf(format, horizontalLine);
+                break;
+            } else {
+                System.out.printf(format, horizontalLine);
+                System.out.printf(format, input);
+                System.out.printf(format, horizontalLine);
+            }
         }
     }
 }
