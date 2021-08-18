7bd2d34584d80cb8eaa2b1e5121394bd53a4a904
nniiggeell <e0559733@u.nus.edu>
2021-08-19 00:21:16 +0800

    Add javadocs to Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,22 +1,42 @@
+/**
+ * This class represents a Task the user gives to the bot.
+ * @author Nigel Tan
+ */
 public class Task {
     private String name;
     private boolean isDone;
     private int position;
 
+    /**
+     * Constructor.
+     * @param name the name of the task
+     * @param pos the position of the task in the list
+     */
     public Task(String name, int pos) {
         this.name = name;
         this.isDone = false;
         this.position = pos;
     }
 
+    /**
+     * This method marks the task as true.
+     */
     public void mark() {
         this.isDone = true;
     }
 
+    /**
+     * This method returns the status icon of the tast, X if true, blank if false.
+     * @return X if true, blank if false
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    /**
+     * This method returns the name of the Task
+     * @return name of the Task
+     */
     public String getName() {
         return this.name;
     }
