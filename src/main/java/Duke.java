71bc8c935e4b18f8627588d1a62ef55f797b22b9
e0543860 <e0543860@u.nus.edu>
2021-08-18 15:19:09 +0800

    Level 2, Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,9 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         boolean finished = false;
+        String[] listOfWords = new String[100];
+        int arrayCounter = 0;
+
         while(!finished) {
             String userResponse = sc.nextLine();
 
@@ -14,8 +17,17 @@ public class Duke {
                     System.out.println("Bye. Hope to see you again soon!");
                     finished = true;
                     break;
+                case "list":
+                    for (int i = 0; i < listOfWords.length; i++) {
+                        if (listOfWords[i] != null) {
+                            System.out.println((i + 1) + ". " + listOfWords[i]);
+                        }
+                    }
+                    break;
                 default:
-                    System.out.println(userResponse);
+                    System.out.println("added: " + userResponse);
+                    listOfWords[arrayCounter] = userResponse;
+                    arrayCounter++;
             }
 
         }
