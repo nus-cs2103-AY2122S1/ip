a47515178afcec660e5eba31d6eef00ab2300286
joshenx <e0525998@u.nus.edu>
2021-08-19 00:24:32 +0800

    Add gitignore files and Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Duke {
 	
     public static void main(String[] args) {
         boolean isActive = true;
+        ArrayList<String> list = new ArrayList<>(100);
         String welcomeMsg = "Hey, I'm Duke.\n"
                 + "What's up?\n";
         String exitMsg = "Bye! Hope I helped!\n"
@@ -18,9 +19,14 @@ public class Duke {
             String input = sc.nextLine();
             if (input.equals("bye")) { // Exit routine
                 isActive = false;
+            } else if (input.equals("list")) {
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println(arrayToString(list));
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             } else {
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                System.out.println(input);
+                System.out.println("added: " + input + "\n");
+                list.add(input);
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             }
         }
@@ -28,5 +34,15 @@ public class Duke {
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println(exitMsg);
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
+    public static String arrayToString(ArrayList<String> list) {
+        String answer = "";
+        int counter = 1;
+        for (String item : list) {
+            answer += String.format("%d: %s\n", counter, item);
+            counter++;
+        }
+        return answer;
     }
 }
