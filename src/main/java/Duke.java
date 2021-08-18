1c168329b3f32facfd225203f8f82e23eba9734a
rish-16 <mail.rishabh.anand@gmail.com>
2021-08-19 00:54:09 +0800

    Add multiple types of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class Duke {
     static class Task {
-        private String description;
-        private Boolean status;
+        protected String description;
+        protected Boolean status;
 
         public Task(String desc) {
             this.description = desc;
@@ -35,6 +35,58 @@ public class Duke {
                 return false;
             }
         }
+
+        @Override
+        public String toString() {
+            return this.getStatusString() + " " + this.getDescription();
+        }
+    }
+
+    static class ToDo extends Task {
+        public ToDo(String desc) {
+            super(desc);
+        }
+        
+        @Override
+        public String toString() {
+            return "[T]" + super.toString();
+        }
+    }
+
+    static class Deadline extends Task {
+        protected String deadline;
+
+        public Deadline(String desc, String deadline) {
+            super(desc);
+            this.deadline = deadline;
+        }
+
+        @Override
+        public String toString() {
+            return "[D]" 
+                    + super.toString()
+                    + " (by: "
+                    + this.deadline
+                    + ")";
+        }
+    }
+
+    static class Event extends Task {
+        protected String timing;
+
+        public Event(String desc, String timing) {
+            super(desc);
+            this.timing = timing;
+        }
+
+        @Override
+        public String toString() {
+            return "[E]" 
+                    + super.toString()
+                    + " (at: "
+                    + this.timing
+                    + ")";
+        }
     }
 
     public static void main(String[] args) {
@@ -63,18 +115,43 @@ public class Duke {
                     Task task = dataStore.get(i);
 
                     if (task.getStatus()) {
-                        System.out.println(i+1 + ". " + task.getStatusString() + " " + task.getDescription());
+                        System.out.println(i+1 + ". " + task.toString());
                     } else {
-                        System.out.println(i+1 + ". " + task.getStatusString() + " " + task.getDescription());
+                        System.out.println(i+1 + ". " + task.toString());
                     }
                 }
+            } else if (input.contains("task")) {
+                ToDo todo = new ToDo(input);
+                dataStore.add(todo);
+
+                System.out.println("Got it. I've added this task: ");
+                System.out.println("    " + todo.toString());
+                System.out.println("Now you have " + dataStore.size() + " tasks in the list.");
+            } else if (input.contains("deadline")) {
+                String query = input.split("/by")[0];
+                String limit = input.split("/by")[1];
+                Deadline deadlineTask = new Deadline(query, limit);
+                dataStore.add(deadlineTask);
+
+                System.out.println("Got it. I've added this task: ");
+                System.out.println("    " + deadlineTask.toString());
+                System.out.println("Now you have " + dataStore.size() + " tasks in the list.");
+            } else if (input.contains("event")) {
+                String query = input.split("/at")[0];
+                String datetime = input.split("/at")[1];
+                Event eventTask = new Event(query, datetime);
+                dataStore.add(eventTask);
+
+                System.out.println("Got it. I've added this task: ");
+                System.out.println("    " + eventTask.toString());
+                System.out.println("Now you have " + dataStore.size() + " tasks in the list.");
             } else if (input.contains("done")) {
                 int idx = Integer.parseInt(input.split(" ")[1]);
                 Task task = dataStore.get(idx-1);
                 
                 System.out.println("Nice! I've marked this task as done: ");
                 task.setDone();
-                System.out.println(task.getStatusString() + " " + task.getDescription());
+                System.out.println("    " + task.toString());
             } else {
                 Task task = new Task(input);
                 dataStore.add(task);
