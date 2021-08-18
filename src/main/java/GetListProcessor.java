1dda7110ff813c951e3d3527cc858ed5faf85d5c
HongshuW <hongshu.wang.uni@gmail.com>
2021-08-18 15:40:28 +0800

    Add GetListProcessor.java

---
+++ src/main/java/GetListProcessor.java
@@ -0,0 +1,14 @@
+public class GetListProcessor extends Processor {
+    /**
+     * Constructor of the class `GetListProcessor`.
+     */
+    public GetListProcessor() {
+        super("list");
+    }
+
+    /**
+     * Generates the message to be printed.
+     */
+    @Override
+    public void process() {}
+}
