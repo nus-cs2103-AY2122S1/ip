e83d15242c1b5c8a722b9e8fda533ee0b51918ee
Chua Kai Jun <chuakaijun.ckj@gmail.com>
2021-08-18 14:58:56 +0800

    Implemented a skeletal version of Duke that greets and echoes the user.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,36 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner scanner = new Scanner((System.in));
+
+        say("Hello, I'm Duke.", "Make me do something.");
+
+        while (true) {
+            String userInput = scanner.nextLine().strip();
+
+            // Check if user is attempting to exit.
+            if (userInput.equals("bye")) {
+                say("Bye bye, see you next time.");
+                break;
+            }
+
+            // Echo user
+            say(userInput);
+        }
+    }
+
+    /**
+     * Makes duke speak.
+     *
+     * Prints a list of lines with indentation.
+     * @param lines list of lines for Duke
+     */
+    static void say(String... lines) {
+        System.out.println("    ____________________________________________________________");
+        for (String line : lines) {
+            System.out.println("    " + line);
+        }
+        System.out.println("    ____________________________________________________________");
     }
 }
