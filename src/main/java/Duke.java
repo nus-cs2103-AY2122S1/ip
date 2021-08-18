8095cef49bff6630d16e36e7d863e9e7b620cbfe
HalphasX <70026153+HalphasX@users.noreply.github.com>
2021-08-19 04:07:21 +0800

    Level-4 Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,39 +25,91 @@ public class Duke {
         }
 
         public static void addCommand(String inputString) {
-            System.out.println("____________________________________________________________ \n" +
-                    "added: " + inputString + "\n" +
-                    "____________________________________________________________");
-            Task inputTask = new Task(inputString);
-            arrayList.add(inputTask);
+            String[] word = inputString.split(" ", 2);
+            String command = word[0];
+            String desc = word[1];
+            switch (command) {
+                case "todo": {
+                    ToDo task = new ToDo(inputString);
+                    System.out.println("____________________________________________________________\n" +
+                            "Got it. I've added this task:");
+                    System.out.println(task.toString());
+                    arrayList.add(task);
+                    System.out.println("Now you have " + arrayList.size() + " task(s) in the list.");
+                    System.out.println("____________________________________________________________");
+                    break;
+                }
+                case "deadline": {
+                    String[] descriptions = desc.split("/", 2);
+                    String description = descriptions[0];
+                    String timeline = descriptions[1];
+                    Deadline task = new Deadline(description, timeline);
+                    System.out.println("____________________________________________________________\n" +
+                            "Got it. I've added this task:");
+                    System.out.println(task.toString());
+                    arrayList.add(task);
+                    System.out.println("Now you have " + arrayList.size() + " task(s) in the list.");
+                    System.out.println("____________________________________________________________");
+                    break;
+                }
+                case "event": {
+                    String[] descriptions = desc.split("/", 2);
+                    String description = descriptions[0];
+                    String timeline = descriptions[1];
+                    Event task = new Event(description, timeline);
+                    System.out.println("____________________________________________________________\n" +
+                            "Got it. I've added this task:");
+                    System.out.println(task.toString());
+                    arrayList.add(task);
+                    System.out.println("Now you have " + arrayList.size() + " task(s) in the list.");
+                    System.out.println("____________________________________________________________");
+                    break;
+                }
+                default:
+                    System.out.println("____________________________________________________________");
+                    System.out.println("There is no task command starting with : '" + command + "'!");
+                    System.out.println("The commands for setting tasks are : 'todo', 'deadline', and 'event'.");
+                    System.out.println("____________________________________________________________");
+                    break;
+            }
         }
 
         public static void listCommand() {
+            System.out.println("____________________________________________________________");
             System.out.println("Here are the tasks in your list:");
             for (int i = 1; i < arrayList.size() + 1; i++) {
                 Task currTask = arrayList.get(i - 1);
-                System.out.println(i + "." + "[" + currTask.getStatusIcon() + "] " + currTask.getDescription());
+                System.out.println(i + "." + currTask.toString());
             }
+            System.out.println("____________________________________________________________");
         }
 
         public static void removeCommand(int i) {
             if (arrayList.size() < i) {
+                System.out.println("____________________________________________________________");
                 System.out.println("Sorry! There are no tasks with index number " + i + "! :(");
+                System.out.println("____________________________________________________________");
             } else {
                 Task iTask = arrayList.get(i - 1);
-                System.out.println("You have removed task : " + iTask.getDescription() + ".");
+                System.out.println("____________________________________________________________");
+                System.out.println("You have removed task : " + i + ". " + iTask.getDescription() + ".");
+                System.out.println("____________________________________________________________");
                 arrayList.remove(i - 1);
             }
         }
 
         public static void doneCommand(int i) {
             if (arrayList.size() < i) {
+                System.out.println("____________________________________________________________");
                 System.out.println("Sorry! I can't find the tasks you ask for! :(");
+                System.out.println("____________________________________________________________");
             } else {
                 Task iTask = arrayList.get(i - 1);
                 iTask.statusDone();
+                System.out.println("____________________________________________________________");
                 System.out.println("Nice! I've marked this task as done: ");
-                System.out.println(i + ". [" + iTask.getStatusIcon() + "] " + iTask.getDescription());
+                System.out.println(i + ". " + iTask.toString());
+                System.out.println("____________________________________________________________");
             }
         }
     }
@@ -66,12 +118,12 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String inputString = sc.nextLine();
         String[] word = inputString.split(" ", 2);
-        String firstWord = word[0];
-        if (firstWord.equals("done")) {
+        String command = word[0];
+        if (command.equals("done")) {
             int secondWord = Integer.parseInt(word[1]);
             Commands.doneCommand(secondWord);
             printString();
-        } else if (firstWord.equals("remove")) {
+        } else if (command.equals("remove")) {
             int secondWord = Integer.parseInt(word[1]);
             Commands.removeCommand(secondWord);
             printString();
