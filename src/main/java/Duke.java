d92fa3c305dd80460c794d8a6388ab7603bf9c1f
DESKTOP-CSTCHE9\Joseph Nathanael <josephnathanael.2014@gmail.com>
2021-08-18 15:28:05 +0800

    Add, list feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,10 +10,18 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you? :)");
-        Scanner sc = new Scanner(System.in);
-        String inpt = sc.nextLine();
+        Scanner sc = new Scanner(System.in); //initialize scanner
+        String inpt = sc.nextLine(); //scanning user's first input
+        ArrayList<String> lst = new ArrayList<>(); // initialize array list
+
+        // printing user's input & adding it to an array list, loops until input is "bye"
         while(!inpt.equals("bye")){
-            System.out.println(inpt);
+            if(inpt.equals("list")){
+                lst.forEach(x -> System.out.println((lst.indexOf(x) + 1) +". " + x ));
+            } else {
+                System.out.println("added: " + inpt);
+                lst.add(inpt);
+            }
             inpt = sc.nextLine();
         }
         System.out.println("Goodbye, I will miss you!");
