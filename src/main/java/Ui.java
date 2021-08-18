3eb3e47719f4bdbe1e37e3863cec41b6db48327a
BenjaminLHR <benjamin.lee.hr@gmail.com>
2021-08-18 15:12:11 +0800

    Create skeleton for Ui

---
+++ src/main/java/Ui.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Ui {
+    private Scanner sc;
+
+    public Ui() {
+        sc = new Scanner(System.in);
+    }
+
+    public void greet() {
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        displayLine();
+    }
+
+    public void sayBye() {
+        displayLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        displayLine();
+    }
+
+    public void echo(String str) {
+        displayLine();
+        System.out.println(str);
+        displayLine();
+    }
+
+    public void displayLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public String readCommand() {
+        return sc.nextLine();
+    }
+
+}
