bb3da7f4d1e5efc423da7d5ee6df9b64dcc6fcb2
angkoonhwee <akoonhwee@gmail.com>
2021-08-19 01:59:27 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String end = "    ---------------------------------------------------------------------------------";
         System.out.println("Hello from\n" + logo);
+        String str = sc.nextLine();
+        while (true) {
+            System.out.println(end + "\n" + "     " + str + "\n" + end);
+            str = sc.nextLine();
+            if (str.equals("Bye!")) {
+                System.out.println(end + "\n" + "     " + "Bye. Hope to see you again soon! :D");
+                break;
+            }
+        }
     }
 }
