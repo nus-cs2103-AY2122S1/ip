e9953a238eadfec6fce7038922f76df5eef91ead
joshenx <e0525998@u.nus.edu>
2021-08-19 03:31:18 +0800

    fixed typo in Event.java and added Text Ui Testing

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -18,6 +18,6 @@ public class Event extends Task{
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + String.format(" (by: %s)", deadline);
+        return "[E]" + super.toString() + String.format(" (at: %s)", deadline);
     }
 }
