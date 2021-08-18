ee8faf7650776cda0c5b5a05d02ee8585f11c6b1
Preshita01 <pandipreshita@gmail.com>
2021-08-18 23:41:22 +0800

    Add DukeException.java

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,14 @@
+public class DukeException extends Exception {
+
+    protected String message;
+
+    public DukeException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
