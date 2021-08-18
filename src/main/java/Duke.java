ec349ad01cfacada4e19444343f1fb8d202bd127
Raveen Prabhu <raveenprabhu99@gmail.com>
2021-08-19 04:09:45 +0800

    Level-5-Handle-Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,49 +27,53 @@ public class Duke {
             System.out.print("Enter a text: ");
 
             String str = sc.nextLine();
-            // command "bye"
-            if (str.equals("bye")) {
-                end = true;
-                System.out.println(horizontalLines);
-                System.out.println("Bye. Hope to see you again soon!");
+            str = str.trim();
+            try {
+                // command "bye"
+                if (str.equals("bye")) {
+                    end = true;
+                    System.out.println(horizontalLines);
+                    System.out.println("Bye. Hope to see you again soon!");
+                    sc.close();
+                }
+
+                //command done
+                else if (str.contains("done")) {
+                    System.out.println(horizontalLines);
+                    System.out.println(markDone(str));
+                }
+
+                //command list
+                else if (str.equals("list")) {
+                    System.out.println(horizontalLines);
+                    System.out.println(list(""));
+                }
+
+                //command to do
+                else if (str.contains("todo")) {
+
+                    System.out.println(horizontalLines);
+                    System.out.println(todoTask(str));
+                }
+
+                //command deadline
+                else if (str.contains("deadline")) {
+                    System.out.println(horizontalLines);
+                    System.out.println(deadlineTask(str));
+                }
+
+                else if (str.contains("event")) {
+                    System.out.println(horizontalLines);
+                    System.out.println(eventsTask(str));
+                }
+
+                else {
+                    throw new DukeException("Command is not valid!");
+                }
+            } catch(DukeException e) {
+                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
 
-            //command done
-            else if (str.contains("done")) {
-                System.out.println(horizontalLines);
-                System.out.println(markDone(str));
-            }
-
-            //command list
-            else if (str.equals("list")) {
-                System.out.println(horizontalLines);
-                System.out.println(list(""));
-            }
-
-            //command to do
-            else if (str.contains("todo")) {
-                str = str.substring(5);
-                System.out.println(horizontalLines);
-                System.out.println(todoTask(str));
-            }
-
-            //command deadline
-            else if (str.contains("deadline")) {
-                System.out.println(horizontalLines);
-                System.out.println(deadlineTask(str));
-            }
-
-            else if (str.contains("event")) {
-                System.out.println(horizontalLines);
-                System.out.println(eventsTask(str));
-            }
-
-            else {
-                System.out.println(horizontalLines);
-                tasks.add(new Task(str));
-                System.out.println( "added: " + str);
-                i++;
-            }
         }
         System.out.println(horizontalLines);
 
@@ -90,30 +94,44 @@ public class Duke {
         return "Nice! I've marked this task as done: \n" + tasks.get(a);
     }
 
-    public static String todoTask(String str) {
-        Task task = new Todo(str);
-        tasks.add(task);
-        return "Got it. I've added this task: \n"
-                + task
-                + "\nNow you have " + tasks.size() + " tasks in the list.";
+    public static String todoTask(String str) throws DukeException {
+        try {
+            str = str.substring(5);
+            Task task = new Todo(str);
+            tasks.add(task);
+            return "Got it. I've added this task: \n"
+                    + task
+                    + "\nNow you have " + tasks.size() + " tasks in the list.";
+        } catch (StringIndexOutOfBoundsException e) {
+            return "☹ OOPS!!! The description of a todo cannot be empty.";
+        }
+
     }
 
     public static String deadlineTask(String str) {
-        int i = str.indexOf("/");
-        Task t = new Deadline(str.substring(0, i), str.substring(i + 4));
-        tasks.add(t);
-        return "Got it. I've added this task: \n"
-                + t
-                + "\nNow you have " + tasks.size() + " tasks in the list.";
+        try {
+            int i = str.indexOf("/");
+            Task t = new Deadline(str.substring(0, i), str.substring(i + 4));
+            tasks.add(t);
+            return "Got it. I've added this task: \n"
+                    + t
+                    + "\nNow you have " + tasks.size() + " tasks in the list.";
+        } catch (StringIndexOutOfBoundsException e) {
+            return "☹ OOPS!!! The description of a todo cannot be empty.";
+        }
     }
 
     public static String eventsTask(String str) {
-        int i = str.indexOf("/");
-        Task t = new Events(str.substring(0, i), str.substring(i + 4));
-        tasks.add(t);
-        return "Got it. I've added this task: \n"
-                + t
-                + "\nNow you have " + tasks.size() + " tasks in the list.";
+        try {
+            int i = str.indexOf("/");
+            Task t = new Events(str.substring(0, i), str.substring(i + 4));
+            tasks.add(t);
+            return "Got it. I've added this task: \n"
+                    + t
+                    + "\nNow you have " + tasks.size() + " tasks in the list.";
+        } catch (StringIndexOutOfBoundsException e) {
+            return "☹ OOPS!!! The description of a todo cannot be empty.";
+        }
     }
 }
 
