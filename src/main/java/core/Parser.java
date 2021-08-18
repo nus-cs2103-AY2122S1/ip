0f1e68c05c6089eb00e0251aabe7cb7ce5858eb1
BenjaminLHR <benjamin.lee.hr@gmail.com>
2021-08-18 22:52:44 +0800

    Modified switch cases to handle todo, deadline and event command types.

--- src/main/java/core/Parser.java
+++ src/main/java/core/Parser.java
@@ -12,9 +12,34 @@ public class Parser {
                 return new DoneCommand(Integer.valueOf(splittedInput[1]));
             case "bye":
                 return new ByeCommand();
+            case "todo":
+                return Parser.todoHandler(input);
+            case "deadline":
+                return Parser.deadlineHandler(input);
+            case "event":
+                return Parser.eventHandler(input);
             default:
-                return new AddTaskCommand(input);
+                return null;
         }
+    }
+
+    public static Command todoHandler(String input) {
+        String[] splittedInput = input.split(" ", 2);
+        return new TodoCommand(splittedInput[1]);
+    }
+
+    public static Command deadlineHandler(String input) {
+        String details = input.split(" ", 2)[1];
+        String regexForDetails = "/by";
+        String splittedDetails[] = details.split(regexForDetails);
+        return new DeadlineCommand(splittedDetails[0].trim(), splittedDetails[1].trim());
+    }
+
+    public static Command eventHandler(String input) {
+        String details = input.split(" ", 2)[1];
+        String regexForDetails = "/at";
+        String splittedDetails[] = details.split(regexForDetails);
+        return new EventCommand(splittedDetails[0].trim(), splittedDetails[1].trim());
     }
 
     // Add static method to check validity
