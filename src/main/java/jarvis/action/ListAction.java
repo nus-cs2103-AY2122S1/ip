372dbd81cd8ccadc47f4e90011fe833363307c98
sivayogasubramanian <sivayogasubramanian@gmail.com>
2021-08-18 22:43:41 +0800

    Handle case when list is empty

--- src/main/java/jarvis/action/ListAction.java
+++ src/main/java/jarvis/action/ListAction.java
@@ -1,11 +1,15 @@
 package jarvis.action;
 
+import jarvis.exception.JarvisException;
 import jarvis.output.Output;
 import jarvis.task.TaskList;
 
 public class ListAction extends Action {
     @Override
-    public void execute(TaskList taskList) {
+    public void execute(TaskList taskList) throws JarvisException {
+        if (taskList.getTaskListSize() == 0) {
+            throw new JarvisException("Your list is currently empty! Try adding some tasks.");
+        }
         Output.showTaskList(taskList);
     }
 }
