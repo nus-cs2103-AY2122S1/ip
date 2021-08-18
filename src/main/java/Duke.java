df8688ba9e1927a5cd676ec8c89ad6641b93f9d1
yulonglim <yulonglimbleh@gmail.com>
2021-08-18 15:14:25 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Duke {
-    private static String[] list = new String[100];
+    private static Task[] list = new Task[100];
     private static int listIndex = 0;
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -17,11 +17,17 @@ public class Duke {
                 System.out.println("\n_________________________\n" + "Bye. Hope to see you again soon!" + "\n_________________________\n");
                 return;
             } else if (str.equalsIgnoreCase("list")){
+                System.out.println("\n_________________________\n");
                 for(int i = 0 ; i < listIndex; i++){
-                    System.out.println("\n_________________________\n" + (i + 1) + ". " + list[i ] + "\n_________________________\n");
+                    System.out.println((i + 1) + ". " + list[i].toString());
                 }
+                System.out.println("\n_________________________\n");
+            } else if (str.contains("done")){
+                int index = Integer.parseInt(str.split(" ")[1]) - 1;
+                list[index].markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + list[index].toString());
             } else {
-                list[listIndex] = str;
+                list[listIndex] = new Task(str);
                 listIndex++;
                 System.out.println("\n_________________________\n" + "added: " + str + "\n_________________________\n");
             }
