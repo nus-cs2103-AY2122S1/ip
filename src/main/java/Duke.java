006639aaf5e5b7b7c7a2967d94f2680181f626f0
Tang Zhiying <e0559633@u.nus.edu>
2021-08-18 22:47:48 +0800

    Level-2: : Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,42 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("   (=^ ･ｪ･^=) < Hello! I'm TiTi~ ");
-        System.out.println("                What would you like to do nya? ");
-
+        ArrayList<String> list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
+        String starter = "   (=^ ･ｪ･^=) < ";
+        String buffer =  "                ";
+
+        // Welcome text
+        System.out.println(starter + "Hello! I'm TiTi~ ");
+        System.out.println(buffer + "What would you like to do nya? ");
+
         String input = sc.nextLine();
-        while (!input.equals("bye")) {
-            System.out.println("   (=^ ･ｪ･^=) < " + input);
+        while (true) {
+            // exit cue
+            if (input.equals("bye")) {
+                System.out.println(starter + "Already done? Come back again soon nya~");
+                break;
+            }
+
+            // display list
+            if (input.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    if (i == 0) {
+                        System.out.println(starter + (i + 1) + ". " + list.get(i));
+                    } else {
+                        System.out.println(buffer + (i + 1) + ". " + list.get(i));
+                    }
+                }
+                input = sc.nextLine();
+                continue;
+            }
+
+            // add input to list
+            System.out.println(starter + "added: " + input);
+            list.add(input);
             input = sc.nextLine();
         }
-
-        System.out.println("   (=^ ‐ｪ‐^=) < Already done? Come back soon nya~");
     }
 }
