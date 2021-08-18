d71652ba3a9390101a16ac1091308ba624af1f62
dt-td <77134471+dt-td@users.noreply.github.com>
2021-08-18 14:46:04 +0800

    Add Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static ArrayList<String> list;
+    private static ArrayList<Task> list;
 
     public static void main(String[] args) {
         list = new ArrayList<>();
@@ -32,15 +32,33 @@ public class Duke {
                     break;
                 case "list":
                     printSingleLineBorder();
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < list.size(); i++) {
-                        System.out.println((i + 1) + ". " + list.get(i));
+                        System.out.println((i + 1) + "." + list.get(i));
                     }
                     printSingleLineBorder();
                     break;
                 default:
-                    list.add(input);
                     printSingleLineBorder();
-                    System.out.println("Added: " + input);
+                    String[] splitInput = input.split(" ");
+                    if (splitInput[0].equals("done")) {
+                        try {
+                            int taskIndex = Integer.parseInt(splitInput[1]) - 1;
+                            if (taskIndex < 0 || taskIndex > list.size() - 1) {
+                                throw new NumberFormatException();
+                            }
+                            Task task = list.get(taskIndex);
+                            task.markDone();
+                            System.out.println("Nice! I've marked this task as done:");
+                            System.out.println(task);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid Task Number");
+                        }
+                    } else {
+                        Task task = new Task(input);
+                        list.add(task);
+                        System.out.println("Added: " + input);
+                    }
                     printSingleLineBorder();
             }
         }
