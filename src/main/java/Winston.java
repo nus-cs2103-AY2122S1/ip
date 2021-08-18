8ba1e09fc01815c3c4f4c83d10e6e48fb5bdc6e9
Imerbear <e0543864@u.nus.edu>
2021-08-18 17:25:39 +0800

    Added arraylist to store data for list function

--- src/main/java/Winston.java
+++ src/main/java/Winston.java
@@ -1,14 +1,42 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Winston {
+    private ArrayList<Task> list;
+
+    public Winston() {
+        list = new ArrayList<>();
+    }
+
+    private void addTask(String description) {
+        list.add(Task.createTask(description));
+    }
+
+    public String getList() {
+        Integer counter = 1;
+        String result = "List\n";
+        for (Task task : this.list) {
+            result += "\t" + counter + ". " + task.getDescription() + "\n";
+            counter += 1;
+        }
+        result = result + "End";
+        return result;
+    }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String cmd = "Hi there! Winston reporting.\nWhat can I do for you?";
+        System.out.println("Hi there! Winston reporting.\nWhat can I do for you?");
+        String cmd = "";
+        Winston winston1 = new Winston();
 
         while (!cmd.equals("bye")) {
-            System.out.println(cmd);
-            cmd = scan.next();
+            if (cmd.equals("list")) {
+                System.out.println(winston1.getList());
+            } else if (!cmd.equals("")) {
+                winston1.addTask(cmd);
+                System.out.println("\tAdded " + cmd +  " to list");
+            }
+            cmd = scan.nextLine();
         }
 
         scan.close();
