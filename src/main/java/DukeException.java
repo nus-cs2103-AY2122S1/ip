551610817699a9452953d15618bc59ed3fbf9f06
HongshuW <hongshu.wang.uni@gmail.com>
2021-08-18 23:31:33 +0800

    Add DukeException.java

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,10 @@
+public class DukeException extends Exception {
+    /**
+     * Constructor of the class `DukeException`.
+     *
+     * @param message Error message.
+     */
+    public DukeException(String message) {
+        super(message);
+    }
+}
