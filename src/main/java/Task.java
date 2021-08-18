6a097bce21ecd9a706e3cf6c6b4c3db722d5bffa
Allard <AllardQuek@users.noreply.github.com>
2021-08-18 14:11:09 +0800

    add done field for Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,5 +1,6 @@
 public class Task {
 	private String name;
+	private boolean done = false;
 
 	public Task(String name) {
 		this.name = name;
@@ -9,12 +10,24 @@ public class Task {
 		return name;
 	}
 
+	public boolean isDone() {
+		return done;
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+	public String getCheckBox() {
+        return (done ? "[X] " : "[ ] "); // mark done task with X
+    }
+
 	@Override
 	public String toString() {
-		return this.name;
+		return getCheckBox() + this.name;
 	}
 }
