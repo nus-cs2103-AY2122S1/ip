59779aada1ab74db925499c74b0dd4b9cb9c3d04
nniiggeell <e0559733@u.nus.edu>
2021-08-18 23:10:10 +0800

    Update Response.java for level-2

--- src/main/java/Response.java
+++ src/main/java/Response.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+
 public class Response {
-    private String horizontalLine = "    ____________________________________________________________\n";
+    private final String horizontalLine = "    ____________________________________________________________\n";
+    private ArrayList<String> lst;
+    private int count;
+
+    public Response() {
+        this.lst = new ArrayList<>();
+        this.count = 1;
+    }
 
     public void greet() {
         System.out.println(horizontalLine +
@@ -9,7 +18,8 @@ public class Response {
     }
 
     public void echo(String text) {
-        System.out.println(horizontalLine + "    " +
+        lst.add(text);
+        System.out.println(horizontalLine + "    " + "added: " +
                 text + "\n" +
                 horizontalLine);
     }
@@ -18,5 +28,15 @@ public class Response {
         System.out.println(horizontalLine +
                 "    Bye. Hope to see you again soon!\n" +
                 horizontalLine);
+    }
+
+    public void display() {
+        this.count = 1;
+        System.out.println(horizontalLine);
+        lst.forEach(item -> {
+            System.out.println("    " + count + ". " + item);
+            count++;
+        });
+        System.out.println(horizontalLine);
     }
 }
