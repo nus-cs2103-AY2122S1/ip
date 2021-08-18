b16cfbc83266cb11017ea129a6b80cbd74ffa1d8
Tetrerox <tetrerox@gmail.com>
2021-08-18 21:41:53 +0800

    Completed Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -6,13 +8,21 @@ public class Duke {
                 "What can i do for you?";
         System.out.println(introMessage);
         Scanner scanner = new Scanner(System.in);
+        List<String> userList = new ArrayList<String>();
         while (true) {
             String userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.equals("list")) {
+                int counter = 1;
+                for (String text : userList) {
+                    System.out.println(counter + ". " + text);
+                    counter++;
+                }
             } else {
-                System.out.println(userInput);
+                userList.add(userInput);
+                System.out.println("added: " + userInput);
             }
         }
     }
