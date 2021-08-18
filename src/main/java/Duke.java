d20071eacb54762ff93d62a7cab7df8db94921d8
Jeromesyl <jsyl3214@gmail.com>
2021-08-18 17:27:49 +0800

    Add Level 5. Handle errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,63 +16,83 @@ public class Duke {
         }
     }
 
-    public static void addTodo(String task) {
-        Todo todo = new Todo(task);
-        taskList.add(todo);
-        System.out.println("Okay! I've added this task:\n" + todo.toString());
-        System.out.printf("You have %d task(s) in the list.\n\n", taskList.size());
+    public static void addTodo(String input) throws DukeException {
+        try {
+            String task = input.substring(5);
+            Todo todo = new Todo(task);
+            taskList.add(todo);
+            System.out.println("Okay! I've added this task:\n" + todo.toString());
+            System.out.printf("You have %d task(s) in the list.\n\n", taskList.size());
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new DukeException("The description of a Todo cannot be empty!");
+        }
     }
 
-    public static void addDeadline(String task) {
-        int split = task.indexOf("/by");
-        String description = task.substring(0,split);
-        String by = task.substring(split+4);
-        Deadline deadline = new Deadline(description, by);
-        taskList.add(deadline);
-        System.out.println("Okay! I've added this task:\n" + deadline.toString());
-        System.out.printf("You have %d task(s) in the list.\n\n", taskList.size());
+    public static void addDeadline(String input) throws DukeException {
+        try {
+            String task = input.substring(9);
+            int split = task.indexOf("/by");
+            String description = task.substring(0,split);
+            String by = task.substring(split+4);
+            Deadline deadline = new Deadline(description, by);
+            taskList.add(deadline);
+            System.out.println("Okay! I've added this task:\n" + deadline.toString());
+            System.out.printf("You have %d task(s) in the list.\n\n", taskList.size());
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new DukeException("Invalid format. Please try again.\n");
+        }
     }
 
-    public static void addEvent(String task) {
-        int split = task.indexOf("/at");
-        String description = task.substring(0,split);
-        String at = task.substring(split+4);
-        Event event = new Event(description, at);
-        taskList.add(event);
-        System.out.println("Okay! I've added this task:\n" + event.toString());
-        System.out.printf("You have %d task(s) in the list.\n\n", taskList.size());
+    public static void addEvent(String input) throws DukeException {
+        try {
+            String task = input.substring(6);
+            int split = task.indexOf("/at");
+            String description = task.substring(0,split);
+            String at = task.substring(split+4);
+            Event event = new Event(description, at);
+            taskList.add(event);
+            System.out.println("Okay! I've added this task:\n" + event.toString());
+            System.out.printf("You have %d task(s) in the list.\n\n", taskList.size());
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new DukeException("Invalid format. Please try again.\n");
+        }
     }
 
-    public static void markTask(int index) {
-        Task task = taskList.get(index - 1);
-        task.markDone();
-        System.out.println("Okay! This task has been marked done:");
-        System.out.println(task.toString() + "\n");
+    public static void markTask(String input) throws DukeException {
+        try {
+            int index = Integer.parseInt(input.substring(5));
+            Task task = taskList.get(index - 1);
+            task.markDone();
+            System.out.println("Okay! This task has been marked done:");
+            System.out.println(task.toString() + "\n");
+        } catch (IndexOutOfBoundsException | NumberFormatException e) {
+            throw new DukeException("Invalid Task. Please try again.\n");
+        }
     }
 
     public static int handleInput() {
         Scanner scanner = new Scanner(System.in);
         String input;
         do{
-            input = scanner.nextLine();
-            if (input.equalsIgnoreCase("BYE")) {
-                System.out.println("Bai bai!");
-            } else if (input.equalsIgnoreCase("LIST")) {
-                printTasks();
-            } else if (input.toUpperCase().startsWith("DONE")) {
-                int index = Integer.parseInt(input.substring(5));
-                markTask(index);
-            } else if (input.toUpperCase().startsWith("TODO")) {
-                String task = input.substring(5);
-                addTodo(task);
-            } else if (input.toUpperCase().startsWith("DEADLINE")) {
-                String task = input.substring(9);
-                addDeadline(task);
-            } else if (input.toUpperCase().startsWith("EVENT")) {
-                String task = input.substring(6);
-                addEvent(task);
-            } else {
-                System.out.println(input + "\n");
+                input = scanner.nextLine();
+            try{
+                if (input.equalsIgnoreCase("BYE")) {
+                    System.out.println("Bai bai!");
+                } else if (input.equalsIgnoreCase("LIST")) {
+                    printTasks();
+                } else if (input.toUpperCase().startsWith("DONE")) {
+                    markTask(input);
+                } else if (input.toUpperCase().startsWith("TODO")) {
+                    addTodo(input);
+                } else if (input.toUpperCase().startsWith("DEADLINE")) {
+                    addDeadline(input);
+                } else if (input.toUpperCase().startsWith("EVENT")) {
+                    addEvent(input);
+                } else {
+                    throw new DukeException("Sorry, I don't understand. :O");
+                }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());;
             }
         }
         while (!input.equalsIgnoreCase("BYE"));
@@ -80,7 +100,7 @@ public class Duke {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -97,7 +117,9 @@ public class Duke {
         System.out.println(logo + greeting);
         System.out.println(usage);
 
+
         handleInput();
+
     }
 
 }
