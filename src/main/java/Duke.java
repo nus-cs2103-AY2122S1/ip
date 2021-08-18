20714dd637d8605f233d11e3ea81407667fbf6e2
lethiciars <santosolethicia@gmail.com>
2021-08-18 15:16:47 +0800

    Add add,list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,25 +2,41 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
-        System.out.println("How can I help you?");
+        System.out.println("What do you need to do today?");
         System.out.println("_______________________");
+
         Scanner commands = new Scanner(System.in);
-        String toEcho = commands.nextLine();
-        while (!toEcho.equals("bye")) {
-            String toPrint = toEcho;
-            System.out.println("_______________________");
-            System.out.println(toPrint);
-            System.out.println("_______________________");
-            toEcho = commands.nextLine();
+        String task = commands.nextLine();
+        int i = 0;
+
+        while (!task.equals("bye")) {
+            if (task.equals("list")) {
+                System.out.println("_______________________");
+                for (int j = 0; j < i; j++) {
+                    System.out.printf("%d. %s%n", j+1, tasks[j]);
+                }
+                System.out.println("_______________________");
+            } else {
+                tasks[i] = task;
+                System.out.println("_______________________");
+                System.out.printf("added: %s%n", tasks[i]);
+                System.out.println("_______________________");
+                i += 1;
+            }
+            task = commands.nextLine();
+
         }
         System.out.println("_______________________");
         System.out.println("See you! Have a nice day!");
         System.out.println("_______________________");
+
     }
 }
