7d5174594da95b9944119d95736f8cff69b64e0c
BananaTechs <tehogben@gmail.com>
2021-08-18 23:36:31 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Duke {
     static Scanner sc = new Scanner(System.in);
     static boolean running = true;
-    static String[] tasks = new String[100];
+    static Task[] tasks = new Task[100];
     static int taskIndex = 0;
+    static String defaultErrorMsg = "im sorry I is no understand.";
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -14,7 +15,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("hello name is duke");
-        System.out.println("how is help today;");
+        System.out.println("how is help today; （´・｀ ）♡");
 
         while (running) {
             String input = sc.nextLine();
@@ -27,13 +28,34 @@ public class Duke {
                     System.out.println("is no tasks today.");
                 } else {
                     for (int i = 1; i <= taskIndex; i++) {
-                        System.out.println(i + ". " + tasks[i - 1]);
+                        Task currTask = tasks[i - 1];
+                        System.out.println(i + ".[" + currTask.getStatusIcon() + "] "
+                                + currTask.getDescription());
+                    }
+                }
+            } else if (input.length() >= 5) {
+                if (input.substring(0, 5).equals("done ")) {
+                    try {
+                        int i = Integer.parseInt(input.substring(5));
+                        if (i > taskIndex) {
+                            System.out.println("we is dont have that many tasks yet.");
+                        } else if (i <= 0) {
+                            System.out.println("what kind of number is (||❛︵❛.)");
+                        } else {
+                            Task task = tasks[i - 1];
+                            task.markAsDone();
+                            System.out.println("is done!");
+                            System.out.println("[" + task.getStatusIcon() + "] " + task.getDescription());
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println(defaultErrorMsg);
                     }
                 }
             } else {
                 if (taskIndex < 100) {
-                    tasks[taskIndex] = input;
-                    System.out.println("added: " + input);
+                    Task newTask = new Task(input);
+                    tasks[taskIndex] = newTask;
+                    System.out.println("added: " + newTask.getDescription());
                     taskIndex++;
                 } else {
                     System.out.println("memory is full please is try later.");
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
