acf1eea6651dacfa8e2ecdf187e11115fffa2902
sueann-chua <sue.ann.chua@gmail.com>
2021-08-18 13:39:38 +0800

    Level-2: Add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -8,6 +9,7 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+        ArrayList<String> todo = new ArrayList<>();
 
         System.out.println("---------------------------------------------");
         System.out.println("Hello! I'm Duke!\n" + "What can I do for you?");
@@ -15,11 +17,19 @@ public class Duke {
 
         while (true) {
             Scanner input = new Scanner (System.in);
-            String action = input.next();
-            if (!action.equals("bye")){
+            String action = input.nextLine();
+            if (!action.equals("bye") && !action.equals("list")){
+                todo.add(action);
                 System.out.println("---------------------------------------------\n"
-                        + "     " + action + "\n"
+                        + "     added: " + action + "\n"
                         + "---------------------------------------------");
+            } else if (action.equals("list")){
+                System.out.println("---------------------------------------------");
+                for (int i = 0; i < todo.size(); i++) {
+                    int j = i + 1;
+                    System.out.println("     " + j + ". " + todo.get(i));
+                }
+                System.out.println("---------------------------------------------");
             } else {
                 System.out.println("---------------------------------------------\n"
                         + "     Bye. Hope to see you again soon!" + "\n"
