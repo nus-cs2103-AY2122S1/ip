219c6f8c5effb59c239c75053a6283bb1d3b2718
Zenlzb <leezenbeen@u.nus.edu>
2021-08-18 13:09:11 +0800

    Restructure main body to use switch cases

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 public class Duke {
@@ -19,14 +18,14 @@ public class Duke {
                 " in the list");
     }
     public static String printList() {
-        String res = "";
+        StringBuilder res = new StringBuilder();
         for (int counter = 1; counter<=list.size(); counter++) {
-            res = res + counter + ". " + list.get(counter-1);
+            res.append(counter).append(". ").append(list.get(counter - 1));
             if (counter != list.size()) {
-                res = res + "\n     ";
+                res.append("\n     ");
             }
         }
-        return res;
+        return res.toString();
     }
 
     public static void main(String[] args) {
@@ -34,52 +33,73 @@ public class Duke {
         System.out.println(formatMessage("Hello! I'm Duke\n" + "     What can I do for you?"));
         while (true) {
             String input = scanner.nextLine();
-            if (input.equals("bye")) {
+            String[] params = input.split(" ", 2);
+            String[] parts;
+            if(params[0].equals("bye")) {
                 break;
-            } else if (input.equals("list")) {
-                System.out.println(formatMessage(printList()));
-            } else if (input.startsWith("done")) {
-                String arg = input.substring(5);
-                try {
-                    int index = Integer.parseInt(arg);
-                    if (index > list.size()) {
-                        System.out.println(formatMessage("There are only " + list.size() + " tasks"));
-                        continue;
-                    } else if (index == 0) {
-                        System.out.println(formatMessage("There is no task 0"));
-                        continue;
-                    }
-                    Task t = list.get(index-1);
-                    t.completeTask();
-                    System.out.println(formatMessage("Nice! I've marked this task as done\n       " + t));
+            }
+            switch (params[0]) {
+                case "list":
+                    System.out.println(formatMessage(printList()));
+                    break;
+                case "done":
+                    String arg = params[1];
+                    try {
+                        int index = Integer.parseInt(arg);
+                        if (index > list.size()) {
+                            System.out.println(formatMessage("There are only " + list.size() + " tasks"));
+                            break;
+                        } else if (index == 0) {
+                            System.out.println(formatMessage("There is no task 0"));
+                            break;
+                        }
+                        Task t = list.get(index-1);
+                        t.completeTask();
+                        System.out.println(formatMessage("Nice! I've marked this task as done\n       " + t));
 
-                } catch (NumberFormatException e) {
-                    System.out.println(formatMessage("Please enter a number after done"));
-                }
-            } else if (input.startsWith("todo")) {
-                Todo t = new Todo(input.substring(5));
-                addToList(t);
-                System.out.println(taskAddedMessage(t));
-            } else if (input.startsWith("deadline")) {
-                if (!input.contains("/by")) {
-                    System.out.println(formatMessage("Please enter the deadline of the task after /by"));
-                    continue;
-                }
-                String[] parts = input.split(" /by ");
-                Deadline d = new Deadline(parts[0].substring(9), parts[1]);
-                addToList(d);
-                System.out.println(taskAddedMessage(d));
-            } else if (input.startsWith("event")) {
-                if (!input.contains("/at")) {
-                    System.out.println(formatMessage("Please enter the start date of the task after /at"));
-                    continue;
-                }
-                String[] parts = input.split(" /at ");
-                Event e = new Event(parts[0].substring(6), parts[1]);
-                addToList(e);
-                System.out.println(taskAddedMessage(e));
-            } else {
-                System.out.println(formatMessage("That is not a recognised command"));
+                    } catch (NumberFormatException e) {
+                        System.out.println(formatMessage("Please enter a number after done"));
+                    }
+                    break;
+                case "todo":
+                    if (params.length == 1) {
+                        System.out.println(formatMessage("Please enter the name of the task after todo"));
+                        break;
+                    }
+                    Todo t = new Todo(params[1]);
+                    addToList(t);
+                    System.out.println(taskAddedMessage(t));
+                    break;
+                case "deadline":
+                    if (params.length == 1) {
+                        System.out.println(formatMessage("Please enter the name of the task after deadline"));
+                        break;
+                    }
+                    if (!params[1].contains("/by")) {
+                        System.out.println(formatMessage("Please enter the deadline of the task after /by"));
+                        break;
+                    }
+                    parts = params[1].split(" /by ");
+                    Deadline d = new Deadline(parts[0], parts[1]);
+                    addToList(d);
+                    System.out.println(taskAddedMessage(d));
+                    break;
+                case "event":
+                    if (params.length == 1) {
+                        System.out.println(formatMessage("Please enter the name of the task after event"));
+                        break;
+                    }
+                    if (!params[1].contains("/at")) {
+                        System.out.println(formatMessage("Please enter the start date of the task after /at"));
+                        break;
+                    }
+                    parts = params[1].split(" /at ");
+                    Event e = new Event(parts[0], parts[1]);
+                    addToList(e);
+                    System.out.println(taskAddedMessage(e));
+                    break;
+                default:
+                    System.out.println(formatMessage("That is not a recognised command"));
             }
         }
         System.out.println(formatMessage("Bye. Hope to see you again soon!"));
