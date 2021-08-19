4b9ff592423bfd0bda1925acfa3ed41ff5de8a7a
rushilramesh <rushilramesh@gmail.com>
2021-08-19 11:46:32 +0800

    A-TextUiTesting

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,6 +12,7 @@ then
     rm ACTUAL.TXT
 fi
 
+
 # compile the code into the bin folder, terminates if error occurred
 if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
 then
