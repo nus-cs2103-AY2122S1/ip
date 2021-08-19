14aae8d4eb9ced848021974fef86c4fea79a4d42
Cai Kaian <caipngg@gmail.com>
2021-08-19 11:19:19 +0800

    Add ability to mark a todo as done

--- src/main/java/Duke/Todo/Todo.java
+++ src/main/java/Duke/Todo/Todo.java
@@ -1,14 +1,27 @@
 package Duke.Todo;
 
 public class Todo {
-    private String todo;
+    private static final String DONE_ICON = "X";
+    private static final String NOT_DONE_ICON = " ";
 
-    public Todo(String todo) {
-        this.todo = todo;
+    private String description;
+    private boolean isDone;
+
+    public Todo(String description) {
+        this(description, false);
+    }
+
+    public Todo(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
     }
 
     @Override
     public String toString() {
-        return this.todo;
+        return String.format("[%s] %s", this.isDone ? DONE_ICON : NOT_DONE_ICON, this.description);
     }
 }
