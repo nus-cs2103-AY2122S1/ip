cfebfa4ba78ebf0e706a14bb440b5c3574838479
Kangxun <e0543787@u.nus.edu>
2021-08-19 17:05:19 +0800

    Redo Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,29 +17,47 @@ public class Duke {
         System.out.println(line);
 
         // List to store commands
-        ArrayList<String> cmdList = new ArrayList<>();
+        ArrayList<Task> cmdList = new ArrayList<>();
         int count = 0;
 
         // Create a scanner to read from standard input.
         Scanner sc = new Scanner(System.in);
         String cmd = sc.nextLine();
         while (!cmd.equals("bye")) {
-            if (!cmd.equals("list")) {
-                System.out.println(line);
-                cmdList.add(cmd);
-                count++;
-                System.out.println("added: " + cmd);
-                System.out.println(line);
-            } else {
+            if (cmd.equals("list")) {       // when command given is "list"
                 System.out.println(line);
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + cmdList.get(i));
+                    System.out.println((i + 1) + ".[" + cmdList.get(i).getStatusIcon() + "] " + cmdList.get(i));
                 }
+                System.out.println(line);
+            } else if (cmd.matches("^done [0-9]+$")) {      // when command given is "done"
+                String[] temp = cmd.split(" ");
+                int itemNo = Integer.parseInt(temp[1]);
+
+                // Make sure itemNo is within limit
+                if ((itemNo <= count) && (itemNo > 0)) {
+                    cmdList.get(itemNo - 1).markDone();
+                    System.out.println(line);
+                    System.out.println("congrats on completing this task! :");
+                    System.out.println("  [X] " + cmdList.get(itemNo - 1));
+                } else {        // add command to list
+                    System.out.println(line);
+                    cmdList.add(new Task(cmd));
+                    count++;
+                    System.out.println("added: " + cmd);
+                }
+                System.out.println(line);
+            }else {         // add command to list
+                System.out.println(line);
+                cmdList.add(new Task(cmd));
+                count++;
+                System.out.println("added: " + cmd);
                 System.out.println(line);
             }
             cmd = sc.nextLine();
         }
 
+        // when command given is "bye"
         System.out.println(line);
         System.out.println("bye friend!");
         System.out.println(line);
