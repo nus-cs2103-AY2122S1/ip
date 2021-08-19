dc74ffbaecb999fec7db9a591e0c5c8aae4e24f4
GabrielWLM <e0550342@u.nus.edu>
2021-08-19 21:54:44 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,26 +12,40 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
+        String[] list = new String[100];
+        int amount = 0;
+
         String lineBreak = "\t____________________________________________________________";
         System.out.println(lineBreak
-                + "\n\tHello! I'm Luke, your slightly useful personal assistant!\n"
-                + "\tWhat can I do for you, my liege?\n"
-                + "\ttype 'bye' to end\n"
+                + "\n\t Hello! I'm Luke, your slightly useful personal assistant!\n"
+                + "\t What can I do for you, my liege?\n"
+                + "\t Type 'list' to show previous inputs\n"
+                + "\t Type 'bye' to end\n"
                 + lineBreak);
 
         String response = scanner.nextLine();
 
         while (!response.equals("bye")) {
-            System.out.println(lineBreak
-                    + "\n\t"
-                    + response
-                    + "\n"
-                    + lineBreak);
+            if(response.equals("list")) {
+                System.out.println(lineBreak);
+                for (int i = 0; i < amount; i++) {
+                    System.out.println("\n\t " + (i + 1) + ". " + list[i]);
+                }
+                System.out.println(lineBreak);
+            } else {
+                System.out.println(lineBreak
+                        + "\n\t added: "
+                        + response
+                        + "\n"
+                        + lineBreak);
+                list[amount] = response;
+                amount++;
+            }
             response = scanner.nextLine();
         }
 
         System.out.println(lineBreak
-                + "\n\tBye! Talk again sometime!\n"
+                + "\n\t Bye! Talk again sometime!\n"
                 + lineBreak);
 
         scanner.close();
