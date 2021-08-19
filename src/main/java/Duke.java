184eae4a0791402ed91ab5c768fffb735916291b
Gordon25 <73068242+Gordon25@users.noreply.github.com>
2021-08-19 11:32:40 +0800

    Added ability for Duke to support task class, mark task as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,39 +5,53 @@ import java.util.function.DoubleToIntFunction;
  * A class that encapsulates the duke chat bot, which has ability to greet, echo and exit.
  */
 public class Duke {
-    private String[] list = new String[100];
+    private Task[] tasks = new Task[100];
     int counter = 0;
 
     /**
-     * Constructor for the Duke class
+     * Constructor for the Duke class.
      * Does not take in any parameters, prints out a statement to greet user.
      */
 
     public Duke() {
-        System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
+        System.out.println("~~ Hello! I'm Duke ~~~~~~~~~\n~~ What can I do for you? ~~");
     }
 
     /**
-     * method to add a text to the list in the chat bot,
-     * adds the text to the next available index of the list,
+     * method to add a task to the list in the chat bot,
+     * adds the task to the next available slot in tasks,
      * then prints a statement saying the text has been added.
-     * @param input
+     * @param task
      */
-    public void add(String input) {
-        list[counter] = input;
+    public void add(Task task) {
+        tasks[counter] = task;
         counter++;
-        System.out.println("\tadded: " + input);
+        System.out.println("~~ added: " + task.toString() + " ~~");
     }
 
     /**
-     * method to iteratively print out the texts stored in the list
-     * in order of which they are added
+     * method to iteratively print out each task stored in tasks
+     * in order of which they are added.
      */
-    
+
     public void iterate() {
+        System.out.println("\tHere are the tasks in your list: ");
         for(int i = 0; i < counter; i++) {
-            System.out.println("\t" + (i + 1) + ". " + list[i]);
+            System.out.println("\t" + (i + 1) + "." + tasks[i].toString());
         }
+    }
+
+    /**
+     * method to duke to mark a task as done.
+     * Marks task indexed num - 1 in tasks done,
+     * then, prints out the task.
+     * @param num
+     */
+    public void markDone(int num) {
+        Task taskDone = tasks[num - 1];
+        taskDone.markAsDone();
+        System.out.println("\tNice! I've marked this task as done: ");
+        System.out.println("\t~" + taskDone.toString() + "~ ");
     }
 
     public static void main(String[] args) {
@@ -53,12 +67,17 @@ public class Duke {
         while (!input.equals("bye")) {
             if (input.equals("list")) {
                 duke.iterate();
+            } else if (input.contains("done")) {
+                String index = input.substring("done ".length());
+                int taskNum = Integer.parseInt(index);
+                duke.markDone(taskNum);
             } else {
-                duke.add(input);
+                duke.add(new Task(input));
             }
-                input = sc.nextLine();
+
+            input = sc.nextLine();
         }
-        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("@@@ Bye. Hope to see you again soon! @@@");
         sc.close();
     }
 }
