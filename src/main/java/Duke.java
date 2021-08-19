41bc8bd25e3f6eba2e23b17914f4215c9e624742
Aishwarya Nair <aishrn1202@gmail.com>
2021-08-19 13:54:43 +0800

    add delete item feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
 
-    private static Task[] tasks = new Task[100];
-    private static int noOfTasks = 0;
+    private static List<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
 
@@ -44,61 +45,83 @@ public class Duke {
 
     /*To print list of tasks between horizontal lines */
     public static void printList() {
+        int ctr = 1;
         printLine();
         System.out.println("\tHere are the tasks in your list:");
-        if(noOfTasks > 0) {
-            for(int i = 0; i < noOfTasks; i++) {
-                System.out.println("\t" + (i + 1) + "." + tasks[i]);
-            }
-        } else {
-            System.out.println("\tNo items added to list yet!");
+        for(Task task: tasks) {
+            System.out.println("\t" + ctr+ "." + tasks.get(ctr - 1));
+            ctr++;
         }
         printLine();
     }
 
-    /*To get the index of the task to be marked as done */
-    public static int getCompletedTask(String task) {
+    /*To get the index of the task completed or to be deleted */
+    public static int getTaskNumber(String keyword, String task) {
         int idx = 0;
-        for(int i = task.length() - 1; i > 4; i--) {
+        for(int i = task.length() - 1; i > keyword.length(); i--) {
             idx+= (task.charAt(i) - 48) * Math.pow(10, task.length() - 1 - i);
         }
-        return idx <= noOfTasks? idx - 1: -1;
+        return idx - 1;
     }
-    public static int checkIndex(String command) throws DukeException {
-        int index = getCompletedTask(command);
-        if(index == -1) {
-            throw new DukeException("You are attempting to mark an invalid task number!");
+
+    /*Returns index of task completed or to be deleted if valid, otherwise, throws exception*/
+    public static int getIndex(String command) throws DukeException {
+        String keyword = command.substring(0, command.indexOf(' '));
+        int index = getTaskNumber(keyword,command);
+
+        if(index < 0 || index >= tasks.size()) {
+            throw new DukeException("You are attempting to " + (keyword.equals("done") ? "mark" : "delete")
+                    +" an invalid task number!");
         }
         return index;
     }
 
+    /*Method to check for the validity of user input */
     public static void checkCommand(String command) throws DukeException {
 
+        //stores the first word (keyword) in the user input
         String keyword = command.substring(0, command.contains(" ") ? command.indexOf(' ') : command.length());
+
+        /*stores whether keyword is a task or calling a feature */
         boolean isTask = keyword.equals("todo") || keyword.equals("event") || keyword.equals("deadline");
+        boolean isFeature = keyword.equals("list") || keyword.equals("done") || keyword.equals("delete");
 
-        if(isTask|| keyword.equals("list") || keyword.equals("done")){
+        /*suppose it's a valid keyword */
+        if(isTask|| isFeature){
 
+            //when keyword is not followed by anything
             if(isTask && command.length() < keyword.length() + 2) {
                 throw new DukeException("The description of a " + keyword + " cannot be empty.");
             }
 
             if(keyword.equals("list")) {
-                if(noOfTasks == 0) {
+                //in case user asks to print list before any item is added
+                if(tasks.size() == 0) {
                     throw new DukeException("No items added to list yet!");
                 }
                 printList();
             } else if(keyword.equals("done")) {
                 try {
-                    int index = checkIndex(command);
-                    Task finished = tasks[index];
+                    int index = getIndex(command);
+                    Task finished = tasks.get(index);
                     finished.markAsDone();
                     printFormatted("Nice! I've marked this task as done:\n" + "\t\t" +
                             finished);
                 } catch (DukeException ex) {
                     printFormatted(ex.toString());
                 }
-            } else {
+            } else if(keyword.equals("delete")) {
+                try {
+                    int index = getIndex(command);
+                    Task deleted = tasks.remove(index);
+                    printFormatted("Noted. I've removed this task:" + "\n\t\t" + deleted + "\n\tNow you have " +
+                            tasks.size() + " tasks in the list.");
+
+                } catch (DukeException ex) {
+                    printFormatted(ex.toString());
+                }
+
+            } else { //if the keyword is a task to add
                 Task current = new Task("");
                 if(keyword.equals("todo")) {
                     current = new Todo(command.substring(5));
@@ -110,17 +133,12 @@ public class Duke {
                             command.substring(command.indexOf('/') + 4, command.indexOf('-')),
                             command.substring(command.indexOf('-') + 1));
                 }
-                tasks[noOfTasks++] = current;
+                tasks.add(current);
                 printFormatted("Got it. I've added this task:" + "\n\t\t" + current + "\n\tNow you have " +
-                        noOfTasks + " tasks in the list.");
+                        tasks.size() + " tasks in the list.");
             }
-        } else {
+        } else { //if the keyword is not valid
             throw new DukeException("Sorry, but I don't know what that means :-(");
         }
-
-
     }
-
-
-
 }
