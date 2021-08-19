47b94bd162b2d38e98b47bbb12f6514d34821d7e
Neo Jing Xuan <neojxuan@gmail.com>
2021-08-19 18:25:59 +0800

    add mark tasks as done feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,17 +5,26 @@ public class Duke {
 
     public static void main(String[] args) {
         int MAX = 100;
-        String[] tasks = new String[MAX];
+        Task[] tasks = new Task[MAX];
         System.out.println("Hello...\nWhat do you want?\n");
         Scanner myScanner = new Scanner(System.in);
         String answer = myScanner.nextLine();
+
         while (!answer.equals("bye")) {
-            if (answer.equals("list")) {
+            String[] parts = answer.split(" ");
+            String taskType = parts[0];
+            if (taskType.equals("done")) {
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+                tasks[taskIndex].markAsDone();
+                System.out.println("I've marked this task as done: ");
+                System.out.println("\t" + tasks[taskIndex]);
+            } else if (taskType.equals("list")) {
+                System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
             } else {
-                tasks[taskCount] = answer;
+                tasks[taskCount] = new Task(answer);
                 taskCount += 1;
                 System.out.println("added: " + answer);
             }
