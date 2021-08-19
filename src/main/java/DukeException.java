f1e5cada04d30fbe80d367668c385cd233c8c430
wonyeji <75670073+wonyeji@users.noreply.github.com>
2021-08-20 03:11:52 +0800

    A-TextUiTesting

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,8 +1,8 @@
 public class DukeException {
-    private String toDoError = "☹ OOPS!!! The description of a todo cannot be empty.";
-    private String randomError = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
-    private String deadlineError = "☹ OOPS!!! Please indicate a valid deadline.";
-    private String eventError = "☹ OOPS!!! Please indicate a valid event date.";
+    private String toDoError = "OOPS!!! The description of a todo cannot be empty.";
+    private String randomError = "OOPS!!! I'm sorry, but I don't know what that means :-(";
+    private String deadlineError = "OOPS!!! Please indicate a valid deadline.";
+    private String eventError = "OOPS!!! Please indicate a valid event date.";
 
     public String toDo() {
         return toDoError;
