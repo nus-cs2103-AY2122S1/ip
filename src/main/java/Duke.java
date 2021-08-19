f0547ee19fbc084841505e52bd80d3e317cc241f
Neo Jing Xuan <neojxuan@gmail.com>
2021-08-19 18:25:39 +0800

    add ability to store and list user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,24 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
-        System.out.println("Hello...\nWhat do you want?");
+        int MAX = 100;
+        String[] tasks = new String[MAX];
+        System.out.println("Hello...\nWhat do you want?\n");
         Scanner myScanner = new Scanner(System.in);
         String answer = myScanner.nextLine();
         while (!answer.equals("bye")) {
-            System.out.println(answer);
+            if (answer.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = answer;
+                taskCount += 1;
+                System.out.println("added: " + answer);
+            }
             answer = myScanner.nextLine();
         }
         System.out.println("Whatever...");
