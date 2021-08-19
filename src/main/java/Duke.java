8249b5ad60bd642478bb3cbdc969dffb6445864c
Alyssa <alyssa_savier@yahoo.com>
2021-08-19 17:11:38 +0800

    Add Error Handling for 2 types of errors Error 1: Description of a todo list cannot be empty Error 2: Invalid command/input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -37,64 +37,37 @@ public class Duke {
             } else {
                 int inDataLength = inData.length();
                 if (inDataLength < 6) {
-//                    jokerList[i] = inData;
-//
-//                    taskList[i] = new Task(inData);
-//                    if (!Objects.equals(inData, "bye")) {
-//                        System.out.println(
-//                                "____________________________________________________________ \n"
-//                                + "added: "
-//                                + inData
-//                                + "\n"
-//                                + "____________________________________________________________");
-//                    }
-//                    i++;
+                    try {
+                        if (!Objects.equals(inData, "bye") && Objects.equals(inData.substring(0, 4), "todo")) {
+                            inData.substring(0, 6);
+                        } else {
+                            try {
+                                if (!Objects.equals(inData, "bye") && !Objects.equals(inData.substring(0, 4), "done")) {
+                                    System.out.println("____________________________________________________________\n" +
+                                            "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n" +
+                                            "____________________________________________________________");
+                                }
+                            } catch (Exception e) {
+                                System.out.println("____________________________________________________________\n" +
+                                        "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n" +
+                                        "____________________________________________________________");
+                            }
+                        }
+                    } catch (Exception e) {
+                        System.out.println("____________________________________________________________\n" +
+                                "☹ OOPS!!! The description of a todo cannot be empty.\n" +
+                                "____________________________________________________________");
+                    }
                 } else {
-
-                    if (Objects.equals(inData.substring(0, 5), "todo ")) {
-                        jokerList[i] = inData.substring(5, inDataLength);
-                        taskList[i] = new ToDo(inData.substring(5, inDataLength));
-                        System.out.println(
-                                "____________________________________________________________ \n"
-                                        + "Got it. I've added this task: \n"
-                                        + "[" + taskList[i].getTask() + "]"
-                                        + "[" + taskList[i].getStatusIcon() + "] "
-                                        + taskList[i].getDescription()
-                                        + "\n"
-                                        + "Now you have " + (i + 1) + " tasks in the list.\n"
-                                        + "____________________________________________________________");
-                        i++;
-
-                    } if (inDataLength > 6) {
-                        if (Objects.equals(inData.substring(0, 9), "deadline ")) {
-                        String[] segments = inData.split("/by ");
-                        String date = segments[segments.length - 1];
-
-                        int segmentedLength = segments[0].length();
-                        String description = segments[0].substring(9, segmentedLength);
-                        jokerList[i] = description;
-                        taskList[i] = new Deadline(description, date);
-
-                        System.out.println(
-                                "____________________________________________________________ \n"
-                                        + "Got it. I've added this task: \n"
-                                        + "[" + taskList[i].getTask() + "]"
-                                        + "[" + taskList[i].getStatusIcon() + "] "
-                                        + taskList[i].getDescription()
-                                        + "\n"
-                                        + "Now you have " + (i + 1) + " tasks in the list.\n"
-                                        + "____________________________________________________________");
-                        i++;
-
-                        } else if (Objects.equals(inData.substring(0, 6), "event ")) {
-                            String[] segments = inData.split("/at ");
-                            String date = segments[segments.length - 1];
-
-                            int segmentedLength = segments[0].length();
-                            String description = segments[0].substring(6, segmentedLength);
-                            jokerList[i] = description;
-                            taskList[i] = new Event(description, date);
-
+                    if (!inData.contains("deadline") && !inData.contains("todo") && !inData.contains("event")
+                            && !inData.contains("done") && !inData.contains("list") && !inData.contains("bye")) {
+                        System.out.println("____________________________________________________________\n" +
+                                "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n" +
+                                "____________________________________________________________");
+                    } else {
+                        if (!Objects.equals(inData, "bye") && Objects.equals(inData.substring(0, 5), "todo ")) {
+                            jokerList[i] = inData.substring(5, inDataLength);
+                            taskList[i] = new ToDo(inData.substring(5, inDataLength));
                             System.out.println(
                                     "____________________________________________________________ \n"
                                             + "Got it. I've added this task: \n"
@@ -105,46 +78,89 @@ public class Duke {
                                             + "Now you have " + (i + 1) + " tasks in the list.\n"
                                             + "____________________________________________________________");
                             i++;
+
                         }
-                    }
-                     else {
-                        if (Objects.equals(inData.substring(0, 5), "done ")) {
-                            if (isNumeric(inData.substring(5, inDataLength))) {
-                                int taskNo = Integer.parseInt(inData.substring(5, inDataLength));
-                                if (taskNo <= 100 && taskNo <= i) {
-                                    taskList[taskNo - 1].markAsDone();
-                                    System.out.println(
-                                            "____________________________________________________________\n"
-                                                    + "Nice! I've marked this task as done: \n"
-                                                    + " [" + taskList[taskNo - 1].getStatusIcon() + "] "
-                                                    + taskList[taskNo - 1].getDescription()
-                                                    + "\n____________________________________________________________");
-                                } else {
-                                    System.out.println(
-                                            "____________________________________________________________ \n"
-                                                    + "invalid number!"
-                                                    + "\n____________________________________________________________");
-                                }
+                        if (inData.contains("deadline") || inData.contains("event")) {
+                            if (Objects.equals(inData.substring(0, 9), "deadline ")) {
+                                String[] segments = inData.split("/by ");
+                                String date = segments[segments.length - 1];
+
+                                int segmentedLength = segments[0].length();
+                                String description = segments[0].substring(9, segmentedLength);
+                                jokerList[i] = description;
+                                taskList[i] = new Deadline(description, date);
+
+                                System.out.println(
+                                        "____________________________________________________________ \n"
+                                                + "Got it. I've added this task: \n"
+                                                + "[" + taskList[i].getTask() + "]"
+                                                + "[" + taskList[i].getStatusIcon() + "] "
+                                                + taskList[i].getDescription()
+                                                + "\n"
+                                                + "Now you have " + (i + 1) + " tasks in the list.\n"
+                                                + "____________________________________________________________");
+                                i++;
+
+                            } else if (Objects.equals(inData.substring(0, 6), "event ")) {
+                                String[] segments = inData.split("/at ");
+                                String date = segments[segments.length - 1];
+
+                                int segmentedLength = segments[0].length();
+                                String description = segments[0].substring(6, segmentedLength);
+                                jokerList[i] = description;
+                                taskList[i] = new Event(description, date);
+
+                                System.out.println(
+                                        "____________________________________________________________ \n"
+                                                + "Got it. I've added this task: \n"
+                                                + "[" + taskList[i].getTask() + "]"
+                                                + "[" + taskList[i].getStatusIcon() + "] "
+                                                + taskList[i].getDescription()
+                                                + "\n"
+                                                + "Now you have " + (i + 1) + " tasks in the list.\n"
+                                                + "____________________________________________________________");
+                                i++;
                             }
                         } else {
-                            jokerList[i] = inData;
-                            taskList[i] = new Task(inData);
-                            System.out.println(
-                                    "____________________________________________________________ \n"
-                                            + "added: "
-                                            + inData
-                                            + "\n"
-                                            + "____________________________________________________________");
-                            i++;
+                            if (Objects.equals(inData.substring(0, 5), "done ")) {
+                                if (isNumeric(inData.substring(5, inDataLength))) {
+                                    int taskNo = Integer.parseInt(inData.substring(5, inDataLength));
+                                    if (taskNo <= 100 && taskNo <= i) {
+                                        taskList[taskNo - 1].markAsDone();
+                                        System.out.println(
+                                                "____________________________________________________________\n"
+                                                        + "Nice! I've marked this task as done: \n"
+                                                        + " [" + taskList[taskNo - 1].getStatusIcon() + "] "
+                                                        + taskList[taskNo - 1].getDescription()
+                                                        + "\n____________________________________________________________");
+                                    } else {
+                                        System.out.println(
+                                                "____________________________________________________________ \n"
+                                                        + "invalid number!"
+                                                        + "\n____________________________________________________________");
+                                    }
+                                }
+                            }
+//                        } else {
+//                            jokerList[i] = inData;
+//                            taskList[i] = new Task(inData);
+//                            System.out.println(
+//                                    "____________________________________________________________ \n"
+//                                            + "added: "
+//                                            + inData
+//                                            + "\n"
+//                                            + "____________________________________________________________");
+//                            i++;
+//                        }
                         }
                     }
                 }
             }
+            System.out.println(
+                    "____________________________________________________________ \n"
+                            + "Bye. Hope to see you again soon! \n"
+                            + "____________________________________________________________");
         }
-        System.out.println(
-                "____________________________________________________________ \n"
-                + "Bye. Hope to see you again soon! \n"
-                + "____________________________________________________________");
     }
 
     public static boolean isNumeric(String string) {
