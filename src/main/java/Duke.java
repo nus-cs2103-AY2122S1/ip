57ec7cb288bed70145f6ca73d0452a93cd3fbe2e
nhjryan <e0544583@u.nus.edu>
2021-08-20 01:33:59 +0800

    Added Level 6. Delete and necessary unit tests + minor edits in Duke
    Made minor edits to indentation and replaced topLine and bottomLine string with a msgTemplate method.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,63 +1,59 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        String topLine = "____________________________________________________________\n";
-        String bottomLine = "____________________________________________________________";
-        String startUp = "Hello! I'm Duke\n"
-                + "What can I do for you?\n";
-        Task[] list = new Task[100];
-        int count = 0;
+        String startUp = " Hello! I'm Duke\n"
+                + " What can I do for you?";
+        ArrayList<Task> list = new ArrayList<>();
         Scanner myObj = new Scanner(System.in);
 
-        System.out.println(topLine + startUp + bottomLine);
+        msgTemplate(startUp);
 
         while(true) {
             String input = myObj.nextLine();
 
             if (input.startsWith("todo")) {
                 String item = input.substring(5);
-                list[count] = new Todo(item);
-                count++;
-                System.out.println(topLine + " Got it. I've added this task:\n" + " " + list[count - 1] + System.lineSeparator() + " Now you have " + count + " tasks in the list.\n" + bottomLine);
+                list.add(new Todo(item));
+                msgTemplate(" Got it. I've added this task:\n" + "  " + list.get(list.size() - 1) + System.lineSeparator() + " Now you have " + list.size() + " tasks in the list.");
             } else if (input.startsWith("deadline")) {
                 String item = input.substring(9, input.indexOf("/by ") - 1);
                 String date = input.substring(input.indexOf("/by ") + 4);
-                list[count] = new Deadline(item, date);
-                count++;
-                System.out.println(topLine + " Got it. I've added this task:\n" + " " + list[count - 1] + System.lineSeparator() + " Now you have " + count + " tasks in the list.\n" + bottomLine);
+                list.add(new Deadline(item, date));
+                msgTemplate(" Got it. I've added this task:\n" + "  " + list.get(list.size() - 1) + System.lineSeparator() + " Now you have " + list.size() + " tasks in the list.");
             } else if (input.startsWith("event")) {
                 String item = input.substring(6, input.indexOf("/at ") - 1);
                 String date = input.substring(input.indexOf("/at ") + 4);
-                list[count] = new Event(item, date);
-                count++;
-                System.out.println(topLine + " Got it. I've added this task:\n" + " " + list[count - 1] + System.lineSeparator() + " Now you have " + count + " tasks in the list.\n" + bottomLine);
+                list.add(new Event(item, date));
+                msgTemplate(" Got it. I've added this task:\n" + "  " + list.get(list.size() - 1) + System.lineSeparator() + " Now you have " + list.size() + " tasks in the list.");
             } else if (input.startsWith("done")) {
                 int index = Integer.parseInt(input.substring(5)) - 1;
-                list[index].setDone();
-                System.out.println(topLine + "Nice! I've marked this task as done:\n" + list[index] + System.lineSeparator() + bottomLine);
-                continue;
+                list.get(index).setDone();
+                msgTemplate(" Nice! I've marked this task as done:\n" + "  " + list.get(index));
+            } else if (input.startsWith("delete")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                Task task = list.get(index);
+                list.remove(index);
+                msgTemplate(" Noted. I've removed this task:\n" + "  " + task + System.lineSeparator() + " Now you have " + list.size() + " tasks in the list.");
             } else if (input.equals("list")) {
-                System.out.print(topLine);
-                for (int i = 0; i < list.length; i++) {
-                    if (list[i] == null) {
-                        break;
-                    }
-                    System.out.println(" " + (i + 1) + ". " + list[i]);
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(" " + (i + 1) + "." + list.get(i));
                 }
-                System.out.print(topLine);
-                continue;
+                System.out.println("____________________________________________________________");
             } else if (input.equals("bye")) {
-                System.out.println(topLine + "Bye. Hope to see you again soon!" + System.lineSeparator() + bottomLine);
+                msgTemplate(" Bye. Hope to see you again soon!");
                 break;
             } else {
-                System.out.println(topLine + "No such command.\n" + bottomLine);
-                continue;
+                msgTemplate(" No such command.");
             }
-
-
-
-
         }
+    }
+
+    public static void msgTemplate(String msg){
+        String topLine = "____________________________________________________________\n";
+        String bottomLine = "____________________________________________________________";
+        System.out.println(topLine + msg + System.lineSeparator() + bottomLine);
     }
 }
