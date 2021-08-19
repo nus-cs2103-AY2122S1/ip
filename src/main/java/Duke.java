b9df5989fa732e0ec3307735a8c5a4965b95d13a
trash-bin99 <77224811+trash-bin99@users.noreply.github.com>
2021-08-19 18:54:03 +0800

    Add some comments and improve chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ *
+ *
+ *
+ */
 public class Duke {
 
     private enum Tasks {DEADLINE, EVENT, TODO}
@@ -26,6 +31,7 @@ public class Duke {
 
             //case if nothing is entered
             if (firstString.equals("")) {
+                System.out.println("...why so quiet? Come on, say something to me!");
                 continue;
             }
 
@@ -37,7 +43,7 @@ public class Duke {
             //cases for specified keywords
             if (firstString.equals("list")) {
                 if (listIndex == 0) {
-                    System.out.println("There are currently no tasks! Stop being so lazy and start doing your tasks!");
+                    System.out.println("There are currently no tasks! Stop being so lazy and start adding your tasks!");
                     continue;
                 }
                 for (int i = 0; i < listIndex; i++) {
@@ -58,29 +64,30 @@ public class Duke {
                     int arrayIndex = index - 1;
                     //case if entered index does not correspond to a task
                     if (index > listIndex || index < 1) {
-                        System.out.println("Sorry, that task does not exist!");
+                        System.out.println("That task does not exist! Don't try to trick me!");
                         continue;
                     }
                     Task currentTask = taskArray.get(arrayIndex);
                     if (firstString.equals("done")) {
+                        //case to complete a task
                         currentTask.setCompleted();
                         System.out.println("Ok, very nice. I have set the following task as completed.\n" + currentTask.toString());
-                        System.out.println("Now you have " + listIndex + " tasks remaining. Get to work!");
                     } else {
                         //remaining case is to delete the task.
                         taskArray.remove(currentTask);
                         listIndex -= 1;
                         System.out.println("Ok, very nice. I have deleted the following task.\n" + currentTask.toString());
-                        System.out.println("Now you have " + listIndex + " tasks remaining. Get to work!");
                     }
+                    System.out.println("Now you have " + listIndex + " tasks remaining. Get to work!");
 
                 } catch (NumberFormatException e) {
-                    System.out.println("Please enter a valid number!");
+                    System.out.println("Enter a valid number! Or do you not know basic math?");
                 } finally {
                     continue;
                 }
             }
 
+            //cases for the 3 task types
             if (firstString.equals("todo")) {
                 createTask(Tasks.TODO, inputArray);
                 continue;
@@ -97,84 +104,84 @@ public class Duke {
             }
 
             //case if first string input is not a keyword
-            System.out.println("Whoops, I could not understand your input. Please try again!");
+            System.out.println("I could not understand your input. Please stop speaking gibberish to me!");
         }
 
         //exit from the program
         scan.close();
-        System.out.println("See you again next time!");
+        System.out.println("kthxbye");
     }
 
     private static void createTask(Tasks taskType, String[] array) {
         //preliminary check if more than 1 string was entered
         if (array.length < 2) {
             //case if no name is entered for the task
-            System.out.println("Whoops! Please enter a name for your Task!");
+            System.out.println("Hey, I'm gonna need a name for your Task!");
             return;
         }
 
         StringBuilder str = new StringBuilder();
         Task tempTask = null;
         switch (taskType) {
-        case TODO: {
-            for (int i = 1; i < array.length; i++) {
-                str.append(array[i]).append(" ");
-            }
-
-            tempTask = new Todos(str.toString());
-            break;
-        }
-
-        case DEADLINE:
-            String time = "";
-            boolean stringHasEnded = false;
-
-            for (int i = 1; i < array.length; i++) {
-                String currentArrayElement = array[i];
-                if (stringHasEnded) {
-                    time = currentArrayElement;
-                    break;
+            case TODO: {
+                for (int i = 1; i < array.length; i++) {
+                    str.append(array[i]).append(" ");
                 }
-                if (currentArrayElement.equals("/by")) {
-                    stringHasEnded = true;
-                    continue;
+
+                tempTask = new Todos(str.toString());
+                break;
+            }
+
+            case DEADLINE:
+                String time = "";
+                boolean stringHasEnded = false;
+
+                for (int i = 1; i < array.length; i++) {
+                    String currentArrayElement = array[i];
+                    if (stringHasEnded) {
+                        time = currentArrayElement;
+                        break;
+                    }
+                    if (currentArrayElement.equals("/by")) {
+                        stringHasEnded = true;
+                        continue;
+                    }
+                    str.append(currentArrayElement).append(" ");
                 }
-                str.append(currentArrayElement).append(" ");
-            }
-            //check if a time was entered
-            if (!stringHasEnded) {
-                System.out.println("Please enter a time for your deadline!");
-                return;
-            }
-
-            tempTask = new Deadlines(str.toString(), time);
-            break;
-
-
-        case EVENT:
-            String eventTime = "";
-            boolean stringHasTerminated = false;
-
-            for (int i = 1; i < array.length; i++) {
-                String currentArrayElement = array[i];
-                if (stringHasTerminated) {
-                    eventTime = currentArrayElement;
-                    break;
+                //check if a time was entered
+                if (!stringHasEnded) {
+                    System.out.println("Do you not have a deadline? If you do, you might as well enter it right?");
+                    return;
                 }
-                if (currentArrayElement.equals("/at")) {
-                    stringHasTerminated = true;
-                    continue;
-                }
-                str.append(currentArrayElement).append(" ");
-            }
-            //check if a duration was entered
-            if (!stringHasTerminated) {
-                System.out.println("Please enter a duration for your event!");
-                return;
-            }
 
-            tempTask = new Events(str.toString(), eventTime);
-            break;
+                tempTask = new Deadlines(str.toString(), time);
+                break;
+
+
+            case EVENT:
+                String eventTime = "";
+                boolean stringHasTerminated = false;
+
+                for (int i = 1; i < array.length; i++) {
+                    String currentArrayElement = array[i];
+                    if (stringHasTerminated) {
+                        eventTime = currentArrayElement;
+                        break;
+                    }
+                    if (currentArrayElement.equals("/at")) {
+                        stringHasTerminated = true;
+                        continue;
+                    }
+                    str.append(currentArrayElement).append(" ");
+                }
+                //check if a duration was entered
+                if (!stringHasTerminated) {
+                    System.out.println("So you have an event, but not a time period when it is happening?");
+                    return;
+                }
+
+                tempTask = new Events(str.toString(), eventTime);
+                break;
 
         }
 
