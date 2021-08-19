99554d58afb302039d49ae6564fc18c168c519eb
Samay Sagar <manu.sagarg1@gmail.com>
2021-08-19 15:05:56 +0800

    Level 2 Adding tasks to a list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private final static String welcome_default = " ____        _        \n"
@@ -9,9 +10,16 @@ public class Duke {
     private final static String line = "\t----------------------------------------------------\n";
     private final static String welcome_changed = line + "\t" + "Hewwo fweind, I am fuwwy, your personal assitant,\n" +
             "\t" + "How can I help you?\n" + line;
+    private static ArrayList<String> tasklist = new ArrayList<>();
 
     private static void fuwwyEcho(String echo) {
         System.out.println(line + "\t" + echo + "\n" + line);
+    }
+
+    private static void addTask(String task) {
+        tasklist.add(task);
+        fuwwyEcho("Uwu added:\t"
+                + task);
     }
 
     public static void main(String[] args) {
@@ -20,8 +28,20 @@ public class Duke {
         String command = sc.nextLine();
 
         while (!command.equals("bye")) {
-            fuwwyEcho(command);
-            command = sc.nextLine();
+
+            if (command.equals("list")) {
+                String output = "\n";
+                for (int i = 0; i < tasklist.size(); i++) {
+                    int n = i + 1;
+                    String list = "\t" + n + ". " + tasklist.get(i);
+                    output += list + "\n";
+                }
+                fuwwyEcho(output);
+                command = sc.nextLine();
+            } else {
+                addTask(command);
+                command = sc.nextLine();
+            }
         }
 
         fuwwyEcho("Bye. Hope to see you again soon!\n");
