be10448345b3557cdfb2b19daa99580799a842fa
kheekheekhee <eekarheenicholas@gmail.com>
2021-08-19 21:43:56 +0800

    Change access levels of attributes in Deadline, Event and Task to private

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,6 +1,6 @@
 public class Deadline extends Task {
 
-    protected String by;
+    private String by;
 
     public Deadline(String description, String by) {
         super(description);
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,6 +1,6 @@
 public class Event extends Task {
 
-    protected String at;
+    private String at;
 
     public Event(String description, String at) {
         super(description);
