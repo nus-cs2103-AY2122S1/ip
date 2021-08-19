5c1c2113e50155a92de6468abe1da876bbc64bfe
amzhy <e0324381@u.nus.edu>
2021-08-19 18:11:18 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private  static Scanner sc;
-    private static Task userTasks;
+    private static ArrayList<Task> userTasks = new ArrayList<>();
     private static String bye_input = "bye";
     private static String bye_output = "Bye. Hope to see you again soon!";
     private static String list_input = "list";
+    private static String done_input = "done";
     private static String indent = "    ";
-    private static String div_line = "    ____________________________________________________________\n";
+    private static String div_line = "    ____________________________________________________________";
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -16,20 +18,35 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         greet();
-        userTasks = new Task();
         sc = new Scanner(System.in);
         while (sc.hasNext()) {
             String next_input = sc.nextLine();
+            String[] arr = next_input.split(" ");
+
             if (next_input.equals(bye_input)) {
                 exit();
                 break;
             } else if (next_input.equals(list_input)){
-                userTasks.printAll();
+                printAllTasks();
+            } else if (arr[0].equals(done_input)) {
+                int i = Integer.parseInt(arr[1]) - 1;
+                userTasks.get(i).markDone();
+                echo("Nice! I have marked this task as done:\n" + indent + userTasks.get(i));
             } else {
-                userTasks.add(next_input);
+                Task t = new Task(next_input);
+                userTasks.add(t);
                 echo("added : " + next_input);
             }
         }
+    }
+
+    private static void printAllTasks() {
+        System.out.println(div_line);
+        System.out.println(indent + "Here are the tasks in your list:");
+        for (int i = 1; i < userTasks.size() + 1; i++) {
+            System.out.println(indent + i + " " + userTasks.get(i-1));
+        }
+        System.out.println(div_line);
     }
 
     private static void exit() {
@@ -37,7 +54,7 @@ public class Duke {
     }
 
     private static void echo(String next_input) {
-        System.out.println(div_line + indent + next_input + "\n" + div_line);
+        System.out.println(div_line + "\n" + indent + next_input + "\n" + div_line);
     }
 
     private static void greet() {
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,19 +1,19 @@
-import java.util.ArrayList;
-
 public class Task {
-    private ArrayList<String> list = new ArrayList<>();
-    private static String indent = "    ";
-    private static String div_line = "    ____________________________________________________________";
+    private boolean isDone;
+    private String detail;
 
-    public void add(String item) {
-        this.list.add(item);
+    public Task(String detail) {
+        this.detail = detail;
+        this.isDone = false;
     }
 
-    public void printAll() {
-        System.out.println(div_line);
-        for (int i = 1; i < this.list.size() + 1; i++) {
-            System.out.println(indent + i + " " + list.get(i-1));
-        }
-        System.out.println(div_line + "\n");
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        String box = isDone ? "[X] " : "[ ] ";
+        return box + this.detail;
     }
 }
