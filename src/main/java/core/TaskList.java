7c505780554b9590932e33d3bca63fc7b4245b0c
BenjaminLHR <benjamin.lee.hr@gmail.com>
2021-08-19 14:45:05 +0800

    Add delete method to taskList

--- src/main/java/core/TaskList.java
+++ src/main/java/core/TaskList.java
@@ -6,16 +6,16 @@ import tasks.Task;
 import java.util.ArrayList;
 
 public class TaskList {
-    private ArrayList<Task> taskList;
+    private ArrayList<Task> listOfTasks;
 
     public TaskList() {
-        taskList = new ArrayList<>();
+        listOfTasks = new ArrayList<>();
     }
 
     public void addTask(Task task) {
-        taskList.add(task);
+        listOfTasks.add(task);
         String outputLine1 = String.format("Got it. I've added this task:\n%s\n", task);
-        String outputLine2 = String.format("Now you have %s tasks in the list.", taskList.size());
+        String outputLine2 = String.format("Now you have %s tasks in the list.", listOfTasks.size());
         String output = outputLine1 + outputLine2;
         Ui.formatAndPrint(output);
     }
@@ -23,7 +23,7 @@ public class TaskList {
     public void listTasks() {
         StringBuilder sb = new StringBuilder("Here are the tasks in your list:\n");
         int index = 1;
-        for (Task task : taskList) {
+        for (Task task : listOfTasks) {
             sb.append(String.format("%s. %s\n", index, task));
             index++;
         }
@@ -32,10 +32,24 @@ public class TaskList {
     }
 
     public void markAsDone(int index) {
-        Task taskToMark = taskList.get(index - 1);
+        Task taskToMark = listOfTasks.get(index - 1);
         taskToMark.setCompleted();
         String outputLine1 = "Nice! I've marked this task as done:\n";
         String output = outputLine1 + taskToMark;
         Ui.formatAndPrint(output);
+    }
+
+    public void delete(int index) {
+        Task taskToRemove = listOfTasks.get(index - 1);
+        listOfTasks.remove(index - 1);
+        String outputLine1 = "Noted. I've removed this task: \n";
+        String outputLine2 = taskToRemove.toString() + "\n";
+        String outputLine3 = String.format("Now you have %s tasks in the list.", listOfTasks.size());
+        String output = outputLine1 + outputLine2 + outputLine3;
+        Ui.formatAndPrint(output);
+    }
+
+    public int getSize() {
+        return listOfTasks.size();
     }
 }
