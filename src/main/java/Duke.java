3e87c13358afd6a0c22b75240cd3d19a3f337091
GabrielWLM <e0550342@u.nus.edu>
2021-08-19 21:30:46 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
+
+        Scanner scanner = new Scanner(System.in);
+
+        String logo = " _           _        \n"
+                + "| |    _   _| | _____ \n"
+                + "| |   | | | | |/ / _ \\\n"
+                + "| |___| |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String lineBreak = "\t____________________________________________________________";
+        System.out.println(lineBreak
+                + "\n\tHello! I'm Luke, your slightly useful personal assistant!\n"
+                + "\tWhat can I do for you, my liege?\n"
+                + "\ttype 'bye' to end\n"
+                + lineBreak);
+
+        String response = scanner.nextLine();
+
+        while (!response.equals("bye")) {
+            System.out.println(lineBreak
+                    + "\n\t"
+                    + response
+                    + "\n"
+                    + lineBreak);
+            response = scanner.nextLine();
+        }
+
+        System.out.println(lineBreak
+                + "\n\tBye! Talk again sometime!\n"
+                + lineBreak);
+
+        scanner.close();
     }
 }
