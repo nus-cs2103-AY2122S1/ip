dddbe946458209a68153320aa65788a8de8d50aa
noobmaster19 <dionneo123@gmail.com>
2021-08-19 15:59:31 +0800

    'level2'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,14 +2,20 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         boolean userEnded = false;
-        System.out.print("Hello! I'm Duke What can I do for you?"); 
+        ArrayList<String> userInputs = new ArrayList<>();
+        System.out.println("Hello! I'm Duke What can I do for you?"); 
         while (!userEnded) {
-            Scanner sc= new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
             String str= sc.nextLine();
             if (str.equals("bye")) {
                 userEnded = true;
+            } else if (str.equals("list")) {
+                for (int i = 0; i < userInputs.size();i++) {
+                    System.out.println((i + 1) + ". " + userInputs.get(i)); 
+                }
             } else {
-                System.out.print(str); 
+                userInputs.add(str);
+                System.out.println("added: " + str); 
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
