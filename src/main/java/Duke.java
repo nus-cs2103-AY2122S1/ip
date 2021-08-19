9ee21584d2fa2dfb2ed027a94d961a4e5bf75279
loose-bus-change <e0661408@u.nus.edu>
2021-08-19 16:09:50 +0800

    Finished Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,19 +17,25 @@ public class Duke {
         System.out.println("Hello! This is Duke, your very own chat bot.");
         System.out.println("What can I help you with ?");
         Client client = new Client();
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Task> list = new ArrayList<Task>();
         while (true) {
             String input = client.input();
+            Task taskInput = new Task(input);
             if (input.equals("bye")) {
                 System.out.println("It's sad to see you go :(");
                 System.out.println("Goodbye, hope to you another day!");
                 break;
             } else if (input.equals("list")) {
                     for (int i = 0; i < list.size(); i++) {
-                        System.out.println(i+1 + ". " + list.get(i));
+                        System.out.println(i+1 + ".[" + list.get(i).getStatusIcon() + "] " + list.get(i).description);
                     }
-                } else {
-                    list.add(input);
+                } else if (input.startsWith("done") && input.length() < 9) {
+                int value = Integer.parseInt(input.replaceAll("[^0-9]", ""));
+                list.get(value - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("[X] " + list.get(value-1).description);
+            } else {
+                    list.add(new Task(input));
                     System.out.println("added: " + input);
                 }
         }
