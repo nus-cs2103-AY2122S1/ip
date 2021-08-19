6a59fbed7a6b97d7513e7675d9fcb8529de85650
loose-bus-change <e0661408@u.nus.edu>
2021-08-19 22:17:03 +0800

    Finished Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,17 +27,40 @@ public class Duke {
                 break;
             } else if (input.equals("list")) {
                     for (int i = 0; i < list.size(); i++) {
-                        System.out.println(i+1 + ".[" + list.get(i).getStatusIcon() + "] " + list.get(i).description);
+                        System.out.println(i+1 + "." + list.get(i).toString()); //".[" + list.get(i).getStatusIcon() + "] " + list.get(i).description);
                     }
                 } else if (input.startsWith("done") && input.length() < 9) {
                 int value = Integer.parseInt(input.replaceAll("[^0-9]", ""));
                 list.get(value - 1).markAsDone();
                 System.out.println("Nice! I've marked this task as done: ");
                 System.out.println("[X] " + list.get(value-1).description);
+            } else if (input.startsWith("todo")) {
+                String firstTodo = input.substring(5);
+                list.add(new Todo(firstTodo));
+                System.out.println("Got it. I've added this task: ");
+                System.out.println(new Todo(firstTodo));
+                System.out.println("Now you have " + list.size() + " tasks in the list");
+            } else if (input.startsWith("deadline")) {
+                String[] temp = input.split("/");
+                String firstDeadline = temp[0].substring(9);
+                String secondDeadline = temp[1].substring(3);
+                list.add(new Deadline(firstDeadline,secondDeadline));
+                System.out.println("Got it. I've added this task: ");
+                System.out.println(new Deadline(firstDeadline,secondDeadline));
+                System.out.println("Now you have " + list.size() + " tasks in the list");
+            } else if (input.startsWith("event")) {
+                String[] tempEvent = input.split("/");
+                String firstEvent = tempEvent[0].substring(6);
+                String secondEvent = tempEvent[1].substring(3);
+                list.add(new Event(firstEvent,secondEvent));
+                System.out.println("Got it. I've added this task: ");
+                System.out.println(new Event(firstEvent,secondEvent));
+                System.out.println("Now you have " + list.size() + " tasks in the list");
             } else {
                     list.add(new Task(input));
                     System.out.println("added: " + input);
                 }
         }
+
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,6 +7,10 @@ public class Task {
         this.isDone = false;
     }
 
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
