a35c54825b9d702209aa6e6844a305aa4165885d
eltonyeh <e0550632@u.nus.edu>
2021-08-19 20:35:21 +0800

    Add the error handling mechanism

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,7 +11,7 @@ public class Duke {
             + LOGO
             + "\tHow can I help you?";
 
-    private static final String FAREWELL_TEXT = "\tBye! Thank you for chatting with me!";
+    private static final String FAREWELL_TEXT = "\t☹☹☹ OOPS!!! Why do you choose to leave me!";
 
     private static final String HORIZONTAL_LINE = "____________________________________________________________";
 
@@ -36,8 +36,97 @@ public class Duke {
         return itemList.toString();
     }
 
-    private static String wrapAsMessage(String text) {
+    public static String wrapAsMessage(String text) {
         return "\t" + HORIZONTAL_LINE + "\n" + text + "\n\t" + HORIZONTAL_LINE;
+    }
+
+    private static void printAddedTask(Task task) {
+        String addMessage = "\tGot it. I've added this task:\n\t\t"
+                + task
+                + "\n\tTask(s) remaining in the list: "
+                + Duke.numOfTasks;
+        System.out.println(Duke.wrapAsMessage(addMessage));
+    }
+
+    public static void applyCommand(String command) throws DukeException {
+        if (command.contains(" ")) {
+            String commandType = command.split(" ", 2)[0];
+            String description = command.split(" ", 2)[1].trim();
+
+            switch (commandType) {
+                case "todo": {
+                    if (description.isEmpty()) {
+                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                    } else {
+                        Task task = new ToDo(description);
+                        Duke.addTask(task);
+                        printAddedTask(task);
+                    }
+                    return;
+                }
+                case "deadline": {
+                    if (description.contains("/by")) {
+                        String[] information = description.split("/by ");
+                        Task task = new Deadline(information[0], information[1]);
+                        Duke.addTask(task);
+                        printAddedTask(task);
+                    } else {
+                        throw new DukeException("☹ OOPS!!! You did not provide the time." +
+                                "\n\t Please use the command /by");
+                    }
+                    return;
+                }
+                case "event": {
+                    if (description.contains("/at")) {
+                        String[] information = description.split("/at ");
+                        Task task = new Event(information[0], information[1]);
+                        Duke.addTask(task);
+                        printAddedTask(task);
+                    } else {
+                        throw new DukeException("☹ OOPS!!! You did not provide the time." +
+                                "\n\t Please use the command /at");
+                    }
+                    return;
+                }
+                case "done": {
+                    if (description.matches("\\d+")) {
+                        int item = Integer.parseInt(description);
+                        if (item == 0) {
+                            throw new DukeException("☹ OOPS!!! The item should be an positive integer.");
+                        }
+                        if (item > numOfTasks) {
+                            throw new DukeException("☹ OOPS!!! The item number is out of bound!");
+                        }
+                        if (Duke.tasks[item - 1].isDone) {
+                            throw new DukeException("☹ OOPS!!! The task is already done!");
+                        }
+                        Duke.tasks[item - 1].setDone(true);
+                        String doneMessage = "\tNice! I've marked this task as done:\n\t\t"
+                                + Duke.tasks[item - 1];
+                        System.out.println(wrapAsMessage(doneMessage));
+                    } else {
+                        throw new DukeException("☹ OOPS!!! The item should be an positive integer.");
+                    }
+                    return;
+                }
+                default: {
+                    throw new DukeException("☹ OOPS!!! I don't understand that!");
+                }
+            }
+        }
+
+        switch (command) {
+            case "":
+                throw new DukeException("Your command cannot be empty!");
+            case "list":
+                String listMessage = Duke.wrapAsMessage(Duke.printList());
+                System.out.println(listMessage);
+                break;
+            case "bye":
+                break;
+            default:
+                throw new DukeException("☹ OOPS!!! I don't understand that!");
+        }
     }
 
     public static void main(String[] args) {
@@ -51,35 +140,13 @@ public class Duke {
         String input = scanner.next();
 
         while (!input.equals("bye")) {
-            String[] inputSplit = input.split(" ", 2);
-            if (input.equals("list")) {
-                String listMessage = Duke.wrapAsMessage(Duke.printList());
-                System.out.println(listMessage);
-            } else if (inputSplit[0].equals("done")) {
-                int item = Integer.parseInt(inputSplit[1]);
-                Duke.tasks[item - 1].setDone(true);
-                String doneMessage = "\tNice! I've marked this task as done:\n\t\t"
-                        + Duke.tasks[item - 1];
-                System.out.println(wrapAsMessage(doneMessage));
-            } else {
-                Task task = null;
-                if (inputSplit[0].equals("todo")) {
-                    task = new ToDo(inputSplit[1]);
-                } else if (inputSplit[0].equals("deadline")) {
-                    String[] information = inputSplit[1].split("/by ");
-                    task = new Deadline(information[0], information[1]);
-                } else if (inputSplit[0].equals("event")) {
-                    String[] information = inputSplit[1].split("/at ");
-                    task = new Event(information[0], information[1]);
-                }
-                Duke.addTask(task);
-                String addMessage = "\tGot it. I've added this task:\n\t\t"
-                        + task
-                        + "\n\tTask(s) remaining in the list: "
-                        + Duke.numOfTasks;
-                System.out.println(Duke.wrapAsMessage(addMessage));
+            try {
+                Duke.applyCommand(input);
+            } catch (DukeException e) {
+                System.out.println(Duke.wrapAsMessage("\t" + e.getMessage()));
+            } finally {
+                input = scanner.next();
             }
-            input = scanner.next();
         }
 
         System.out.println(byeMessage);
