5edfce5a206af66329637d07343acae4d60532f6
Shivam <shivam.desire1@gmail.com>
2021-08-19 08:49:08 +0800

    first commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by Shivam Tiwari
+ * Chat bot to echo everything we input.
+ * Features: Greetings, Echo and Exit on command
+ */
+
 public class Duke {
+
+    static String[] list = new String[1000];
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +17,31 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String input = "";
+        System.out.println("Hello, I'm Duke");
+        System.out.println("What can I do for you");
+        Scanner sc = new Scanner(System.in);
+
+        //loop to check if next inout is available
+        while (sc.hasNext()) {
+            input = sc.nextLine();
+            
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+
+                //exit command for when entered exit code
+                System.exit(1);
+                break;
+            }
+            Echo(input);
+        }
     }
+
+    /**
+     * @param input function to echo the input value
+     */
+    static void Echo(String input) {
+        System.out.println(input);
+    }
+
 }
