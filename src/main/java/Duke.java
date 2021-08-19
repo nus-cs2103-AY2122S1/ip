916e1a4f3abb85d5ba87e13d003eaa215d3e5d9f
loose-bus-change <e0661408@u.nus.edu>
2021-08-19 15:23:33 +0800

    Finished Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,19 +14,24 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        //System.out.println("Hello from\n" + logo);
         System.out.println("Hello! This is Duke, your very own chat bot.");
         System.out.println("What can I help you with ?");
         Client client = new Client();
+        ArrayList<String> list = new ArrayList<String>();
         while (true) {
             String input = client.input();
             if (input.equals("bye")) {
                 System.out.println("It's sad to see you go :(");
                 System.out.println("Goodbye, hope to you another day!");
                 break;
-            } else {
-                System.out.println(input);
-            }
+            } else if (input.equals("list")) {
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println(i+1 + ". " + list.get(i));
+                    }
+                } else {
+                    list.add(input);
+                    System.out.println("added: " + input);
+                }
         }
     }
 }
