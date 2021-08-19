9abe3fef2804d9d7f15cbd6213d59095d1655a90
Kevin <skullspine9@gmail.com>
2021-08-19 12:32:24 +0800

    Add Level-5: Handle Errors * Add DukeException class to handle handle errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,12 +5,18 @@ public class Duke {
     // Constants
     private static final String WELCOME_MESSAGE = "Hello! I'm Duke\nWhat can I do for you?";
     private static final String LIST_MESSAGE = "Here are the tasks in your list:\n%s";
+    private static final String NO_TASKS_IN_LIST_MESSAGE = "You have no tasks currently. Go create some!";
     private static final String DONE_MESSAGE = "Nice! I've marked this task as done:\n %s";
     private static final String NUMBER_OF_TASKS_MESSAGE = "Now you have %d %s in the list.";
     private static final String ADD_TASK_MESSAGE = "Got it. I've added this task:\n%s\n" + NUMBER_OF_TASKS_MESSAGE;
+    private static final String INVALID_DONE_NUMBER = "Please input a valid task number";
     private static final String MISSING_DONE_NUMBER_MESSAGE = "Please input a number after the done command";
     private static final String INCOHERENT_INPUT_MESSAGE = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
     private static final String EXIT_MESSAGE = "Bye. Hope to see you again soon!";
+    private static final String MISSING_DELETE_NUMBER_MESSAGE = "Please input a number after the delete command";
+    private static final String INVALID_DEADLINE_MESSAGE = "Invalid use of deadline command. Use 'deadline <text> /by <datetime>'";
+    private static final String INVALID_EVENT_MESSAGE = "Invalid use of event command. Use 'event <text> /at <datetime>'";
+    private static final String MISSING_TODO_MESSAGE = "Please input text after the todo command";
 
     // Commands
     private static final String EXIT_COMMAND = "bye";
@@ -47,37 +53,57 @@ public class Duke {
         for (String input = sc.nextLine(); canContinue(input); input = sc.nextLine()) {
             String[] inputArr = input.split(" ");
             String firstCommand = inputArr[0];
-            switch (firstCommand) {
-                case LIST_COMMAND:
-                    display_message(String.format(LIST_MESSAGE, taskList.toString()));
-                    break;
-                case DONE_COMMAND:
-                    if (inputArr.length < 2) {
-                        display_message(MISSING_DONE_NUMBER_MESSAGE);
-                    } else {
-                        String secondCommand = inputArr[1];
-                        int taskIndex = Integer.parseInt(secondCommand);
-                        display_message(String.format(DONE_MESSAGE, taskList.markTaskAsDone(taskIndex)));
-                    }
-                    break;
-                case TODO_COMMAND:
-                    String todoText = input.substring(TODO_COMMAND.length() + 1).trim();
-                    display_message(String.format(ADD_TASK_MESSAGE, taskList.addTask(new ToDo(todoText)), taskList.size(), taskList.size() <= 1 ? "task" : "tasks"));
-                    break;
-                case DEADLINE_COMMAND:
-                    String[] deadlineInfo = input.split(DEADLINE_DELIMITER);
-                    String deadline = deadlineInfo[1].trim();
-                    String deadlineText = deadlineInfo[0].substring(DEADLINE_COMMAND.length() + 1).trim();
-                    display_message(String.format(ADD_TASK_MESSAGE, taskList.addTask(new Deadline(deadlineText, deadline)), taskList.size(), taskList.size() <= 1 ? "task" : "tasks"));
-                    break;
-                case EVENT_COMMAND:
-                    String[] eventInfo = input.split(EVENT_DELIMITER);
-                    String event = eventInfo[1].trim();
-                    String eventText = eventInfo[0].substring(EVENT_COMMAND.length() + 1).trim();
-                    display_message(String.format(ADD_TASK_MESSAGE, taskList.addTask(new Event(eventText, event)), taskList.size(), taskList.size() <= 1 ? "task" : "tasks"));
-                    break;
-                default:
-                    display_message(INCOHERENT_INPUT_MESSAGE);
+            try {
+                switch (firstCommand) {
+                    case LIST_COMMAND:
+                        if (taskList.size() == 0) {
+                            display_message(NO_TASKS_IN_LIST_MESSAGE);
+                        } else {
+                            display_message(String.format(LIST_MESSAGE, taskList.toString()));
+                        }
+                        break;
+                    case DONE_COMMAND:
+                        if (inputArr.length < 2) {
+                            throw new DukeException(MISSING_DONE_NUMBER_MESSAGE);
+                        }
+                        String secondCommandDone = inputArr[1];
+                        try {
+                            int taskIndex = Integer.parseInt(secondCommandDone);
+                            display_message(String.format(DONE_MESSAGE, taskList.markTaskAsDone(taskIndex)));
+                        } catch (NumberFormatException err) {
+                            throw new DukeException(INVALID_DONE_NUMBER);
+                        }
+                        break;
+                    case TODO_COMMAND:
+                        if (inputArr.length < 2) {
+                            throw new DukeException(MISSING_TODO_MESSAGE);
+                        }
+                        String todoText = input.substring(TODO_COMMAND.length() + 1).trim();
+                        display_message(taskList.addTask(new ToDo(todoText)));
+                        break;
+                    case DEADLINE_COMMAND:
+                        String[] deadlineInfo = input.split(DEADLINE_DELIMITER);
+                        if (inputArr.length < 4 || deadlineInfo.length < 2) {
+                            throw new DukeException(INVALID_DEADLINE_MESSAGE);
+                        }
+                        String deadline = deadlineInfo[1].trim();
+                        String deadlineText = deadlineInfo[0].substring(DEADLINE_COMMAND.length() + 1).trim();
+                        display_message(taskList.addTask(new Deadline(deadlineText, deadline)));
+                        break;
+                    case EVENT_COMMAND:
+                        String[] eventInfo = input.split(EVENT_DELIMITER);
+                        if (inputArr.length < 4 || eventInfo.length < 2) {
+                            throw new DukeException(INVALID_EVENT_MESSAGE);
+                        }
+                        String event = eventInfo[1].trim();
+                        String eventText = eventInfo[0].substring(EVENT_COMMAND.length() + 1).trim();
+                        display_message(taskList.addTask(new Event(eventText, event)));
+                        break;
+                    default:
+                        display_message(INCOHERENT_INPUT_MESSAGE);
+                }
+            } catch (DukeException err) {
+                display_message(err.getMessage());
             }
         }
         sc.close();
