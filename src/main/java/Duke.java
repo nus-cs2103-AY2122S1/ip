26b2daea933394cb22e494043ba6b9cb60870278
Gordon25 <73068242+Gordon25@users.noreply.github.com>
2021-08-19 10:26:48 +0800

    Added ability to greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
+    public Duke() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +11,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Duke duke = new Duke();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+        sc.close();
     }
 }
