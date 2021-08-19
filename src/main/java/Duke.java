076749d3616567a05e9d422f62d0e3cb7bfc90b8
Lee Yat Bun <benlee1300@gmail.com>
2021-08-19 11:30:17 +0800

    Added the ability to store and display user's inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,11 +4,48 @@ public class Duke {
     private static final String sep =
             "-------------------------------------------------------";
 
+    private static class textList {
+        private final String[] lst;
+        private  final String[] emptyList =
+                new String[] {"The list is empty"};
+        private int count;
+
+        public textList(int len) {
+            this.lst = new String[len];
+            count = 0;
+        }
+
+        public void addItem(String item) {
+            // Checks if the list is full
+            if (count >= lst.length) {
+                printMsg("Length of list exceeded.");
+            } else {
+                lst[count++] = item;
+                printMsg("added: " + item);
+            }
+        }
+
+        public String[] getList() {
+            // Returns emptylist if the list contains no items
+            if (count == 0) {
+                return emptyList;
+            }
+
+            String[] temp = new String[count];
+            for (int i = 0; i < count; ++i) {
+                temp[i] = (i + 1) + ". " + this.lst[i];
+            }
+
+            return temp;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        printMsg("Hello! I'm Duke\n    Talk to me!");
+        printMsg("Hello! I'm Duke\n    I can remember what you said!");
 
+        textList inputs = new textList(100);
         String input;
 
         while (true) {
@@ -18,17 +55,21 @@ public class Duke {
             if (input.equals("bye")) {
                 printMsg("Bye! Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                printMsg(inputs.getList());
             } else {
-                printMsg("::: " + input);
+                inputs.addItem(input);
             }
         }
 
         s.close();
     }
 
-    private static void printMsg(String msg) {
+    private static void printMsg(String... msgs) {
         System.out.println(sep);
-        System.out.println(msg);
+        for (String msg:msgs) {
+            System.out.println(msg);
+        }
         System.out.println(sep);
     }
 }
