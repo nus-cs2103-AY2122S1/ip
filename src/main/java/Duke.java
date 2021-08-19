b0037be63e6dd8226d7a75dd767bb1f5bb618cb2
luluyousef <lolwa.yousef@outlook.com>
2021-08-19 21:54:37 +0800

    Add and List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,32 @@
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String response = "";
+        String [] response = new String[100];
+        int ctr = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("_______________________________________________");
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         System.out.println("_______________________________________________");
 
         while (scanner.hasNext()) {
-            response = scanner.nextLine();
-            if (response.equals("bye")) {
+            response[ctr] = scanner.nextLine();
+            if (response[ctr].equals("list")) {
+                System.out.println("_______________________________________________");
+                for (int i = 0; i < ctr; i++) {
+                    System.out.println( (i + 1) + ". " + response[i] );
+                }
+                System.out.println("_______________________________________________");
+            } else if (response[ctr].equals("bye")) {
                 System.out.println("_______________________________________________");
                 System.out.println("Bye! Hope to see you again soon!");
                 System.out.println("_______________________________________________");
                 System.exit(0);
+            } else {
+                System.out.println("_______________________________________________");
+                System.out.println("added: " + response[ctr]);
+                System.out.println("_______________________________________________");
+                ctr++;
             }
-            System.out.println("_______________________________________________");
-            System.out.println(response);
-            System.out.println("_______________________________________________");
         }
     }
 }
