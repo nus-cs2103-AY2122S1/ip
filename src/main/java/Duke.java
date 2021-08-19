a9451f8a163e76754148661c8fb6405f980a37d0
amzhy <e0324381@u.nus.edu>
2021-08-19 18:59:20 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,7 +21,7 @@ public class Duke {
         sc = new Scanner(System.in);
         while (sc.hasNext()) {
             String next_input = sc.nextLine();
-            String[] arr = next_input.split(" ");
+            String[] arr = next_input.split(" ", 2);
 
             if (next_input.equals(bye_input)) {
                 exit();
@@ -33,11 +33,25 @@ public class Duke {
                 userTasks.get(i).markDone();
                 echo("Nice! I have marked this task as done:\n" + indent + userTasks.get(i));
             } else {
-                Task t = new Task(next_input);
+                Task t;
+                if (arr[0].equals("todo")) {
+                    t = new ToDo(arr[1]);
+                } else if (arr[0].equals("event")){
+                    String[] detail = arr[1].split("/at ", 2);
+                    t = new Event(detail[0], detail[1]);
+                } else {
+                    String[] detail = arr[1].split("/by ", 2);
+                    t = new Deadline(detail[0], detail[1]);
+                }
                 userTasks.add(t);
-                echo("added : " + next_input);
+                echo("Got it. I've added this task:\n" + indent + t + "\n" + indent +
+                        "Now you have " + userTasks.size() + " tasks in the list.");
             }
         }
+    }
+
+    private static void echo(String next_input) {
+        System.out.println(div_line + "\n" + indent + next_input + "\n" + div_line);
     }
 
     private static void printAllTasks() {
@@ -51,10 +65,6 @@ public class Duke {
 
     private static void exit() {
         echo(bye_output);
-    }
-
-    private static void echo(String next_input) {
-        System.out.println(div_line + "\n" + indent + next_input + "\n" + div_line);
     }
 
     private static void greet() {
---
+++ src/main/java/ToDo.java
@@ -0,0 +1,11 @@
+public class ToDo extends Task {
+
+    public ToDo(String detail) {
+        super(detail);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
