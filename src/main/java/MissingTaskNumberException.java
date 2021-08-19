c9ca569a6ab0fa2bbb9e11fd586cc8195dba9e1a
moreTriangles <adam@adam.local>
2021-08-19 19:01:44 +0800

    Add Javadoc for MissingTaskNameException

--- src/main/java/MissingTaskNumberException.java
+++ src/main/java/MissingTaskNumberException.java
@@ -1,3 +1,6 @@
+/**
+ * Exception to indicate when the user forgets to enter task number
+ */
 public class MissingTaskNumberException extends DukeException {
     public MissingTaskNumberException(String error) {
         super(error);
