5c7e1717bccb351d4649ae993217557e667612b7
SpdPnd98 <bryantee1998@gmail.com>
2021-08-19 13:34:49 +0800

    Added Add feature

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -1,11 +1,15 @@
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Locale;
-import java.util.Scanner;
+import javax.sound.midi.SysexMessage;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Chatbot {
     private enum ChatCommands {
-        BYE("bye");
+        BYE("bye"),
+        LIST("list");
 
         private final String command;
 
@@ -30,15 +34,18 @@ public class Chatbot {
     }
 
     private Scanner scanner;
+    private List<String> memory;
 
     public Chatbot() {
         scanner = new Scanner(System.in);
+        memory = new LinkedList<>();
 
         System.out.println("Hey there, I'm Duke! Nice to meet you!");
         System.out.println("I'm a bot build by SpdPnd98, let me know how I can help you!");
         System.out.println("Here are some things I can do now:");
         System.out.println("1. Bye");
-        System.out.println("I'll repeat after you if I don't understand you :)");
+        System.out.println("I'll learn what you're saying and " +
+                           "repeat it to you next time you say it if it's something new!");
     }
 
     public void chat() {
@@ -54,22 +61,46 @@ public class Chatbot {
     private ChatContinue interpret () {
         String input = scanner.nextLine();
         ChatCommands command = ChatCommands.toEnum(input);
-        if (command == null) {
-            System.out.println(input);
-            return ChatContinue.CONTINUE;
+        return command == null ? customMessages(input) : builtInCommands(command);
+    }
+
+    private ChatContinue customMessages(String input) {
+        if (memory.contains(input)) {
+            System.out.println("Repeating what you said: " + input);
+        } else {
+            memory.add(input);
+            System.out.println("added: " + input);
         }
+        return ChatContinue.CONTINUE;
+    }
+
+    private ChatContinue builtInCommands(ChatCommands command) {
         switch (command) {
             case BYE:
                 return farewell();
+            case LIST:
+                return list();
             default:
                 System.out.println("How did you get here?");
                 return ChatContinue.END;
         }
     }
 
-    // boolean to indiacte end of chat
     private ChatContinue farewell() {
         System.out.println("Bye. Hope to see you again soon!");
         return ChatContinue.END;
+    }
+
+    private ChatContinue list() {
+        if (memory.size() == 0) {
+            System.out.println("You haven't said anything I don't know yet... Good! :)");
+        } else {
+            System.out.println("You said these phrases before:");
+            Iterator<String> itr = memory.iterator();
+            for (int i = 1; i <= memory.size(); i++) {
+                System.out.println(i + ". " + itr.next());
+            }
+        }
+        return ChatContinue.CONTINUE;
     }
 }
