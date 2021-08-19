8143591422786b30497a06780b3443b52f26425c
WuaaAj <e0492455@u.nus.edu>
2021-08-19 20:38:40 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,18 +2,37 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        //Greeting
+        //Greet
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("\n\n");
-        System.out.println("Hello! I'm Duke.\n");
-        System.out.println("What can I do for you?");
-        System.out.println("\n");
-        System.out.println("____________________________________________________________");
-        System.out.println("\n");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Duke.");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        //Echo
+        while(!exit){
+            String input = scanner.nextLine();
+
+            System.out.println("    ____________________________________________________________");
+
+            if(input.equals("list")){
+                System.out.println("    list");
+            }else if(input.equals("bye")){
+                System.out.println("    Bye. Hope to see you again soon!");
+                exit = true;
+            }else {
+                System.out.println("    blah");
+            }
+
+            System.out.println("    ____________________________________________________________");
+        }
     }
 }
