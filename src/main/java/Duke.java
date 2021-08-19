8c0b98bf69219a57e5eedd0e60bb32ace95468b1
mingyi456 <mingyi456@hotmail.com>
2021-08-19 13:21:21 +0800

    Level-1 commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,28 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Dude");
+        System.out.println("What can I do for you?");
+
+        Scanner sc =  new Scanner(System.in);
+        boolean isRunning = true;
+
+        while (isRunning) {
+            String input = prompt(sc);
+            isRunning = processInput(input);
+        }
+    }
+
+    private static String prompt(Scanner sc) {
+        return sc.nextLine();
+    }
+
+    static boolean processInput(String str){
+        if (str.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+            return false;
+        }
+        System.out.println(str);
+        return true;
     }
 }
