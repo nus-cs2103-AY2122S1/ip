f246aa095e7f47bac9b642c48b2a3b3ba7b97ba9
rehad-a <rahaad@hotmail.com>
2021-08-19 22:56:39 +0800

    implemented Level-3 and added Task.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,20 +13,26 @@ public class Duke {
         String line = "____________________________________________________________\n";
         System.out.println(line + "Hello I'm Duke\nWhat can I do for you?\n" + line);
 
-        String[] commands = new String[100];
+        Task[] tasks = new Task[100];
         int ctr = 0;
-        commands[ctr] = sc.nextLine().toLowerCase();
-        while (!commands[ctr].equals("bye")) {
-            if (commands[ctr].equals("list")) {
-                System.out.print(line);
+        String command = sc.nextLine().toLowerCase();
+
+        while (!command.equals("bye")) {
+            if (command.contains("done")) {
+                int idx = Integer.parseInt(command.split(" ")[1]) - 1;
+                System.out.printf("%sNice! I've marked this task as done:\n[X] %s\n%s\n",line, tasks[idx].description, line);
+                tasks[idx].updateStatus();
+            } else if (command.equals("list")) {
+                System.out.println(line+ "Here are the tasks in your list:");
                 for (int i = 0; i < ctr; i++) {
-                    System.out.printf("%d. %s\n", i + 1, commands[i]);
+                    System.out.printf("%d.[%s] %s\n", i + 1, tasks[i].getStatusIcon(), tasks[i].description);
                 }
                 System.out.println(line);
             } else {
-                System.out.printf("%sadded: %s\n%s\n", line, commands[ctr], line);
+                System.out.printf("%sadded: %s\n%s\n", line, command, line);
+                tasks[ctr++] = new Task(command);
             }
-            commands[++ctr] = sc.nextLine().toLowerCase();
+            command = sc.nextLine().toLowerCase();
         }
 
         System.out.println(line + "Bye. Hope to see you again soon!\n" + line);
