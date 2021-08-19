e27cebf37d7bd66174450284b42727612780c968
IrvingHe000 <heoutong@gmail.com>
2021-08-19 09:08:39 +0800

    Change the storation of tasks from array to a collection

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -8,8 +10,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        Task[] tasks = new Task[100];
-        int numOfTasks = 0;
+        List<Task> tasks = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         System.out.println("    ____________________________________________________________");
         System.out.println("    Hello! I'm Irving.");
@@ -24,35 +25,35 @@ public class Duke {
                         throw new Exception("todoEmpty");
                     }
                     String taskName = task.substring(5);
-                    tasks[numOfTasks++] = new Todo(taskName);
+                    tasks.add(new Todo(taskName));
                     System.out.println("     Got it. I've added this task:");
-                    System.out.println("      " + tasks[numOfTasks - 1].toString());
-                    System.out.println("     Now you have " + numOfTasks + " tasks in the list.");
+                    System.out.println("      " + tasks.get(tasks.size() - 1).toString());
+                    System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
                 } else if (task.contains("deadline")) {
                     int position = task.indexOf('/');
                     String taskName = task.substring(9, position - 1);
                     String deadlineTime = task.substring(position + 4);
-                    tasks[numOfTasks++] = new Deadline(taskName, deadlineTime);
+                    tasks.add(new Deadline(taskName, deadlineTime));
                     System.out.println("     Got it. I've added this task:");
-                    System.out.println("      " + tasks[numOfTasks - 1].toString());
-                    System.out.println("     Now you have " + numOfTasks + " tasks in the list.");
+                    System.out.println("      " + tasks.get(tasks.size() - 1).toString());
+                    System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
                 } else if (task.contains("event")) {
                     int position = task.indexOf('/');
                     String taskName = task.substring(6, position - 1);
                     String eventTime = task.substring(position + 4);
-                    tasks[numOfTasks++] = new Event(taskName, eventTime);
+                    tasks.add(new Event(taskName, eventTime));
                     System.out.println("     Got it. I've added this task:");
-                    System.out.println("      " + tasks[numOfTasks - 1].toString());
-                    System.out.println("     Now you have " + numOfTasks + " tasks in the list.");
+                    System.out.println("      " + tasks.get(tasks.size() - 1).toString());
+                    System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
                 } else if (task.contains("done")) {
                     int itemDone = Integer.parseInt(task.substring(5));
-                    tasks[itemDone - 1].done = true;
+                    tasks.get(itemDone - 1).done = true;
                     System.out.println("    Nice! I've marked this task as done:");
-                    System.out.println("      " + tasks[itemDone - 1].toString());
+                    System.out.println("      " + tasks.get(itemDone - 1).toString());
                 } else if (task.equals("list")) {
                     System.out.println("    Here are the tasks in your list:");
-                    for (int i = 1; i <= numOfTasks; i++) {
-                        System.out.println("    " + i + "." + tasks[i - 1].toString());
+                    for (int i = 1; i <= tasks.size(); i++) {
+                        System.out.println("    " + i + "." + tasks.get(i - 1).toString());
                     }
                 } else {
                     throw new Exception("Cannot Understand");
