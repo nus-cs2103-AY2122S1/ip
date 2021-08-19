5e92ee784ec17a2c62eefa6f9c267d1e3d83d0cc
CheongYeeMing <e0526001@u.nus.edu>
2021-08-19 16:51:30 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        // Greetings
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        // Take in user input
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String str = sc.nextLine();
+            System.out.println("    ____________________________________________________________");
+            if (str.equals("bye")){
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                break;
+            }
+            System.out.println("     " + str);
+            System.out.println("    ____________________________________________________________");
+        }
     }
 }
