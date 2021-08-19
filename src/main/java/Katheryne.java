8af289ac3a94e0e28e9e5fdd0e27fd32cc8f36b1
Hannah Chia <chia.hannahk@gmail.com>
2021-08-19 23:14:05 +0800

    Tests, Errors: Automated UI testing + more exception handling
    Originally used BufferedReader. Switched to Scanner because I was more
    comfortable with it.
    
    Added more descriptive Katheryne exceptions and made entering indexes
    more comprehensive. Added automated UI testing for A-testing

--- src/main/java/Katheryne.java
+++ src/main/java/Katheryne.java
@@ -11,10 +11,10 @@ public class Katheryne {
         ArrayList<Task> lst = new ArrayList<Task>();
         System.out.println("Ad astra abyssosque! I am Katheryne, the receptionist here at the Adventurers' Guild.");
         System.out.println("How may I assist?");
+        Scanner in = new Scanner(System.in);
         while (true) {
             try {
-                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-                String userInput = in.readLine();
+                String userInput = in.nextLine();
                 String[] keywordInput = userInput.split(" ", 2);
                 if (userInput.equalsIgnoreCase("BYE")) {
                     System.out.println("Ad Astra Abyssoque, Traveller!");
@@ -24,18 +24,22 @@ public class Katheryne {
                         throw new KatheryneExceptions("Traveller, please specify by index which task you completed.");
                     }
                     int i = Integer.parseInt(keywordInput[1]);
-                    if (i < lst.size()) {
+                    if (i <= lst.size() && i > 0) {
                         lst.get(i - 1).markAsDone();
                         System.out.println(lst.get(i - 1));
+                    } else {
+                        throw new KatheryneExceptions("That's not a valid index...");
                     }
                 } else if (keywordInput[0].equalsIgnoreCase("delete")) {
                     if (keywordInput.length == 1) {
                         throw new KatheryneExceptions("Traveller, please specify by index what I should delete.");
                     }
                     int i = Integer.parseInt(keywordInput[1]);
-                    System.out.println("Okay, I'll delete the following item:");
-                    if (i <= lst.size()) {
+                    if (i <= lst.size() && i > 0) {
+                        System.out.println("Okay, I'll delete the following item:");
                         System.out.println(lst.remove(i - 1));
+                    } else {
+                        throw new KatheryneExceptions("That's not a valid index...");
                     }
                 } else if (userInput.equalsIgnoreCase("list")) {
                     System.out.println("Here's the list I've stored for you:");
@@ -49,7 +53,7 @@ public class Katheryne {
                     String[] parsedDeadline = keywordInput[1].split(" /by ");
                     if (parsedDeadline.length == 2) {
                         lst.add(new Deadline(parsedDeadline[0], parsedDeadline[1]));
-                        System.out.println(parsedDeadline[0] + " added to your list, do by " + parsedDeadline[1]);
+                        System.out.println("'" + parsedDeadline[0] + "' added to your list, do by " + parsedDeadline[1]);
                     } else {
                         throw new KatheryneExceptions("A deadline needs a description and a /by time to complete it");
                     }
@@ -60,7 +64,7 @@ public class Katheryne {
                     String[] parsedEvent = keywordInput[1].split(" /at ");
                     if (parsedEvent.length == 2) {
                         lst.add(new Event(parsedEvent[0], parsedEvent[1]));
-                        System.out.println(parsedEvent[0] + " added to your list, do at " + parsedEvent[1]);
+                        System.out.println("'" + parsedEvent[0] + "' added to your list, scheduled for " + parsedEvent[1]);
                     } else {
                         throw new KatheryneExceptions("An event needs a description and an /at time when it occurs");
                     }
@@ -69,14 +73,12 @@ public class Katheryne {
                         throw new KatheryneExceptions("A todo needs a description ");
                     } else {
                         lst.add(new Todo(keywordInput[1]));
-                        System.out.println("Todo item " + keywordInput[1] + " added to your list");
+                        System.out.println("Todo item '" + keywordInput[1] + "' added to your list");
                     }
                 } else {
                     throw new UnknownCommandException();
                 }
                 System.out.println("There are currently " + lst.size() + " items in your list.");
-            } catch(IOException exc) {
-                System.out.println("I/O Exception: " + exc);
             } catch(UnknownCommandException e) {
                 System.out.println(e.getMessage());
             } catch (KatheryneExceptions e) {
@@ -84,9 +86,12 @@ public class Katheryne {
             } catch (NumberFormatException e) {
                 System.out.println("You need to specify a number in the correct format. ERROR: " 
                         + e.getMessage());
-            }
+            } 
+//            finally {
+//                continue;
+//            }
 
-    }
+        }
     }
 
 }
