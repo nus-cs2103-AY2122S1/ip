7441899ef05622629de3954269f9b3a25625b92e
Jonathan Tan Xin Jie <e0543805@u.nus.edu>
2021-08-19 17:00:59 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        boolean away = false;
+        String bar = " -----------------------------------\n";
+        System.out.println(bar + "    Hello! I'm SaDOS\n" +
+                "    What can I do for you?\n" +
+                "    Send \"bye\" to exit,\n" +
+                "    I won't hold it against you\n" +
+                bar);
+        Scanner sc = new Scanner(System.in);
+        do {
+            String str = sc.nextLine();
+            if (str.equalsIgnoreCase("bye")) {
+                away = true;
+                System.out.println(bar + "    Oh you've got to go?\n" +
+                        "    Alright, I'll just be here...\n" +
+                        "    Waiting....\n" +
+                        "    You'll be back right?\n" +
+                        bar);
+            } else {
+                System.out.println(bar + "    " + str + "\n" + bar);
+            }
+        }
+        while (!away);
     }
 }
