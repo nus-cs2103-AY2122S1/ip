134b156452bc4709ed1fb15fc2a1597d4ab08a68
Samay Sagar <manu.sagarg1@gmail.com>
2021-08-19 14:24:22 +0800

    Level 1 Greeting and echo messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+    private final static String welcome_default = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private final static String line = "\t----------------------------------------------------\n";
+    private final static String welcome_changed = line + "\t" + "Hewwo fweind, I am fuwwy, your personal assitant,\n" +
+            "\t" + "How can I help you?\n" + line;
+
+    private static void fuwwyEcho(String echo) {
+        System.out.println(line + "\t" + echo + "\n" + line);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(welcome_changed);
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+
+        while (!command.equals("bye")) {
+            fuwwyEcho(command);
+            command = sc.nextLine();
+        }
+
+        fuwwyEcho("Bye. Hope to see you again soon!\n");
+        sc.close();
     }
 }
