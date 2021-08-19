0600370133b49cd8a167ebbdec33fe3c76105639
Benjamin Tan <benjamin@dev.ofcr.se>
2021-08-19 15:09:17 +0800

    Store text input by users and add `list` command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     private static String welcomeText = "Hey there I'm Duke!\n"
             + "How can I help you today?\n";
     private static String byeText = "Bye! Hope to see you again!\n";
 
+
     public static void main(String[] args) {
         System.out.print(welcomeText);
+
         Scanner in = new Scanner(System.in);
+        List<String> inputList = new ArrayList<>();
 
         while (true) {
             String input = in.nextLine();
@@ -15,8 +20,14 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.print(byeText);
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < inputList.size(); i++) {
+                    String item = inputList.get(i);
+                    System.out.println((i + 1) + ". " + item);
+                }
             } else {
                 System.out.println(input);
+                inputList.add(input);
             }
         }
     }
