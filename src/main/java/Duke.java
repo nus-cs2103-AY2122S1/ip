4d0258b633f7432214bd9f1d2bacc8219d26775f
loose-bus-change <e0661408@u.nus.edu>
2021-08-19 11:59:19 +0800

    Finished Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,12 @@
+import java.util.*;
+
+class Client {
+    public String input() {
+        Scanner scan = new Scanner(System.in);
+        return scan.nextLine();
+    }
+}
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +14,19 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! This is Duke, your very own chat bot.");
+        System.out.println("What can I help you with ?");
+        Client client = new Client();
+        while (true) {
+            String input = client.input();
+            if (input.equals("bye")) {
+                System.out.println("It's sad to see you go :(");
+                System.out.println("Goodbye, hope to you another day!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
