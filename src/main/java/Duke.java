56dd70a123e64992d5cf887c326e6e23b4b76f7f
Kimowarui <zhangyubin83567593@gmail.com>
2021-08-19 22:10:13 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 import com.sun.source.tree.DoWhileLoopTree;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,18 +10,30 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String line = "❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤\n";
-        System.out.println("Hello from\n" + logo + line + "What can I do for you?\n" + line);
+        String line = "❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤";
+        ArrayList<String> task = new ArrayList<>();
+        System.out.println("Hello from\n" + logo + line + "\nWhat can I do for you?\n" + line);
 
         String input;
         Scanner s = new Scanner(System.in);
         input = s.nextLine();
 
         while(!input.equals("bye")) {
-            System.out.println(line + input + "\n" + line);
+            if(input.equals("list")) {
+                int i = 1;
+                System.out.println(line);
+                for(String str: task) {
+                    System.out.println(i + ". " + str);
+                    i++;
+                }
+                System.out.println(line);
+            } else {
+                System.out.println(line + "\n" + "added: " + input + "\n" + line);
+                task.add(input);
+            }
             input = s.nextLine();
         }
 
-        System.out.println(line + "Bye. Hope to see you again soon!\n" + line);
+        System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
     }
 }
