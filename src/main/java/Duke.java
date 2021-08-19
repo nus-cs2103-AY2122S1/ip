f1d4322357fdcfbbead1ad1e5d04dacaac01b511
wengYing227 <e0492479@u.nus.edu>
2021-08-19 21:03:04 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void pack(String[] strings) {
+        System.out.println("    ---------------------------------------------");
+        for(int i = 0; i < strings.length; i++) {
+            System.out.println("     " + strings[i]);
+        }
+        System.out.println("    ---------------------------------------------");
+    }
+
+    //    Level-1 starts
+    public static void greet(){
+        String[] greeting = new String[2];
+        greeting[0] = "Hello! I'm Duke";
+        greeting[1] = "What can I do for you?";
+        pack(greeting);
+    }
+
+    public static void echo(){
+        Scanner scan = new Scanner(System.in);
+        String[] s = new String[1];
+        do{
+            s[0] = scan.next();
+            if(s[0].equals("bye")) break;
+            pack(s);
+        } while(scan.hasNext());
+    }
+
+    public static void exit(){
+        String[] bye = new String[]{"Bye. Hope to see you again soon!"};
+        pack(bye);
+    }
+
+    public static void level1(){
+        greet();
+        echo();
+        exit();
+    }
+    //Level-1 ends
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        level1();
     }
 }
