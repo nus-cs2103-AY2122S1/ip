b242801e17bfa5b9f45582538fc0d934a8f744d8
Benjamin Tan <benjamin@dev.ofcr.se>
2021-08-19 14:58:50 +0800

    Add basic greet, echo, and delete functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String welcomeText = "Hey there I'm Duke!\n"
+            + "How can I help you today?\n";
+    private static String byeText = "Bye! Hope to see you again!\n";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.print(welcomeText);
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            String input = in.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.print(byeText);
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
