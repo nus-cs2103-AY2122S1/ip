a8e7713b4f78a063108fe3b63bb347767bd1c97f
Kangxun <e0543787@u.nus.edu>
2021-08-19 19:04:42 +0800

    Redo Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,66 +23,77 @@ public class Duke {
         // Create a scanner to read from standard input.
         Scanner sc = new Scanner(System.in);
         String cmd = sc.nextLine();
+
         while (!cmd.equals("bye")) {
-            if (cmd.equals("list")) {       // when command given is "list"
-                System.out.println(line);
-                for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + "." + cmdList.get(i));
-                }
-                System.out.println(line);
-            } else if (cmd.matches("^done [0-9]+$")) {      // when command given is "done"
-                String[] temp = cmd.split(" ");
-                int itemNo = Integer.parseInt(temp[1]);
-
-                // Make sure itemNo is within limit
-                if ((itemNo <= count) && (itemNo > 0)) {
-                    cmdList.get(itemNo - 1).markDone();
+            try {
+                if (cmd.equals("list")) {       // when command given is "list"
                     System.out.println(line);
-                    System.out.println("congrats on completing this task! :");
-                    System.out.println("  " + cmdList.get(itemNo - 1));
-                } else {        // add command to list
+                    for (int i = 0; i < count; i++) {
+                        System.out.println((i + 1) + "." + cmdList.get(i));
+                    }
                     System.out.println(line);
-                    cmdList.add(new Task(cmd));
-                    count++;
-                    System.out.println("added: " + cmd);
+                } else if (cmd.matches("^done [0-9]+$")) {      // when command given is "done"
+                    String[] temp = cmd.split(" ");
+                    int itemNo = Integer.parseInt(temp[1]);
+
+                    // Make sure itemNo is within limit
+                    if ((itemNo <= count) && (itemNo > 0)) {
+                        cmdList.get(itemNo - 1).markDone();
+                        System.out.println(line);
+                        System.out.println("congrats on completing this task! :");
+                        System.out.println("  " + cmdList.get(itemNo - 1));
+                    } else {        // throw exception
+                        throw new DukeException("i cant seem to find the task you're looking for");
+                    }
+                    System.out.println(line);
+                } else if (cmd.matches("^todo .*$")
+                        || cmd.matches("^deadline .* /by .*$")
+                        || cmd.matches("^event .* /at .*$")) {
+                    String[] temp = cmd.split(" ", 2);
+
+                    if (temp[1].matches(" *")) {       // make sure desc is not empty
+                                                    // (case when only a whitespace follows a command)
+                        throw new DukeException("the description of a " + temp[0] + " cannot be empty");
+                    } else {
+                        System.out.println(line);
+                        System.out.println("noted! ive added this task:");
+
+                        // Separate into cases
+                        switch (temp[0]) {
+                            case "todo":
+                                cmdList.add(new Todo(temp[1]));
+                                System.out.println("  " + cmdList.get(count));
+                                break;
+                            case "deadline":
+                                String[] temp_deadline = temp[1].split(" /by ", 2);
+                                cmdList.add(new Deadline(temp_deadline[0], temp_deadline[1]));
+                                System.out.println("  " + cmdList.get(count));
+                                break;
+                            case "event":
+                                String[] temp_event = temp[1].split(" /at ", 2);
+                                cmdList.add(new Event(temp_event[0], temp_event[1]));
+                                System.out.println("  " + cmdList.get(count));
+                                break;
+                        }
+                        count++;
+                        System.out.println("now you have " + count + " tasks in your list");
+                        System.out.println(line);
+                    }
+                } else {         // throw exception
+                    // Error handling for todo, deadline & event
+                    if (cmd.equals("todo") || cmd.matches("deadline *") || cmd.matches("event *")) {
+                        throw new DukeException("the description of a " + cmd.split(" ", 2)[0] + " cannot be empty");
+                    } else {
+                        throw new DukeException("im sorry, but i dont know what that means :(");
+                    }
                 }
+                cmd = sc.nextLine();
+            } catch (DukeException e) {
                 System.out.println(line);
-            } else if (cmd.matches("^todo .*$")
-                || cmd.matches("^deadline .* /by .*$")
-                || cmd.matches("^event .* /at .*$")) {
-            String[] temp = cmd.split(" ", 2);
-
-            System.out.println(line);
-            System.out.println("noted! ive added this task:");
-
-            // Separate into cases
-            switch (temp[0]) {
-                case "todo":
-                    cmdList.add(new Todo(temp[1]));
-                    System.out.println("  " + cmdList.get(count));
-                    break;
-                case "deadline":
-                    String[] temp_deadline = temp[1].split(" /by ", 2);
-                    cmdList.add(new Deadline(temp_deadline[0], temp_deadline[1]));
-                    System.out.println("  " + cmdList.get(count));
-                    break;
-                case "event":
-                    String[] temp_event = temp[1].split(" /at ", 2);
-                    cmdList.add(new Event(temp_event[0], temp_event[1]));
-                    System.out.println("  " + cmdList.get(count));
-                    break;
+                System.out.println(e);
+                System.out.println(line);
+                cmd = sc.nextLine();
             }
-            count++;
-            System.out.println("now you have " + count + " tasks in your list");
-            System.out.println(line);
-        } else {         // add command to list
-                System.out.println(line);
-                cmdList.add(new Task(cmd));
-                count++;
-                System.out.println("added: " + cmd);
-                System.out.println(line);
-            }
-            cmd = sc.nextLine();
         }
 
         // when command given is "bye"
