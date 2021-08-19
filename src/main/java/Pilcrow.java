72fa41e4a99ebfeda993b480db3e118c78ec3d32
okyntary <okyntary@gmail.com>
2021-08-19 11:36:33 +0800

    Pilcrow: Add add and list functionalities

--- src/main/java/Pilcrow.java
+++ src/main/java/Pilcrow.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Pilcrow {
     public static void main(String[] args) {
+        ArrayList<String> textHistory = new ArrayList<>();
+
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
         while (!text.equals("bye")) {
-            text = "You entered: \n" + text;
-            System.out.println(text);
+            if (text.equals("list")) {
+                System.out.println(Pilcrow.printHistory(textHistory));
+            } else {
+                textHistory.add(text);
+                text = "Added: \n" + text;
+                System.out.println(text);
+            }
             text = scanner.nextLine();
         }
         String logo = "  _____\n"
@@ -19,5 +27,13 @@ public class Pilcrow {
                 + "    _| |_\n"
                 + "   |_|_|_|\n";
         System.out.println("C'est fini.\n" + logo);
+    }
+
+    public static String printHistory(ArrayList<String> history) {
+        String historyText = "";
+        for (int i = 0; i < history.size(); i++) {
+            historyText += (i + 1) + ". " + history.get(i) + "\n";
+        }
+        return historyText;
     }
 }
