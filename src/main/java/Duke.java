c0e4cc472b8eb715bea01392e566dc79ea6abfeb
asherhy <hongyongrs@gmail.com>
2021-08-19 16:51:54 +0800

    commit 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,36 @@
+import java.util.ArrayList;
 import java.util.Scanner;  // Import the Scanner class
 
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+
+        ArrayList<String> list = new ArrayList<>();
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        String line = "    ____________________________________________________________";
+        String spacing = "     ";
+        System.out.println(line);
+        System.out.println(spacing + "Hello! I'm Duke");
+        System.out.println(spacing + "What can I do for you?");
+        System.out.println(line);
         String command = myObj.nextLine();  // Read user input
-        while (!command.equals("bye")) {
-            System.out.println(command);
+        while (!command.toLowerCase().equals("bye")) {
+            System.out.println(line);
+            if (command.toLowerCase().equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(String.format(spacing + "%d. %s", (i + 1), list.get(i)));
+                }
+            } else {
+                System.out.println(spacing + command);
+                list.add(command);
+            }
+            System.out.println(line);
             command = myObj.nextLine();
         }
-        if (command.equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon!");
-        }
+
+         if (command.toLowerCase().equals("bye")) {
+             System.out.println(line);
+             System.out.println(spacing + "Bye. Hope to see you again soon!");
+             System.out.println(line);
+         }
     }
 }
