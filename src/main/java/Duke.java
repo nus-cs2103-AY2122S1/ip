b228ba5ce9d2ac11b85f43b5dfb8ef99174aadae
CheahYan <cheahyan1103@gmail.com>
2021-08-19 19:09:55 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,25 +14,39 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String a = sc.nextLine();
-        ArrayList<String> history = new ArrayList<String>();
+        String[] b = a.split(" ", 2);
+
+        ArrayList<Task> history = new ArrayList<Task>();
 
         while (!a.equals("bye")) {
             if (a.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+
                 int length = history.size();
                 for (int i = 0; i < length; i++) {
                     System.out.println(String.valueOf(i + 1) + ". " + history.get(i));
                 }
                 a = sc.nextLine();
+                b = a.split(" ", 2);
+
+            } else if (b[0].equals("done")) {
+                int taskIndex = Integer.valueOf(b[1]);
+                history.get(taskIndex - 1).Done();
+                System.out.println("Nice! I have marked this task as done!");
+                System.out.println(history.get(taskIndex - 1));
+
+                a = sc.nextLine();
+                b = a.split(" ", 2);
             } else {
                 System.out.println(a);
-                history.add(a);
+                Task todo = new Task(a);
+                history.add(todo);
                 a = sc.nextLine();
+                b = a.split(" ", 2);
             }
         }
-        //if (a.equals("bye")) {
             System.out.println("Bye! Hope to see you again soon!");
             sc.close();
-        //}
 
     }
 }
