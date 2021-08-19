0caf30f332354d9d5e231b6a508b9febbe2b158e
CheahYan <cheahyan1103@gmail.com>
2021-08-19 17:54:23 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
@@ -13,9 +14,20 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String a = sc.nextLine();
+        ArrayList<String> history = new ArrayList<String>();
+
         while (!a.equals("bye")) {
-            System.out.println(a);
-            a = sc.nextLine();
+            if (a.equals("list")) {
+                int length = history.size();
+                for (int i = 0; i < length; i++) {
+                    System.out.println(String.valueOf(i + 1) + ". " + history.get(i));
+                }
+                a = sc.nextLine();
+            } else {
+                System.out.println(a);
+                history.add(a);
+                a = sc.nextLine();
+            }
         }
         //if (a.equals("bye")) {
             System.out.println("Bye! Hope to see you again soon!");
