1c496b15232f8dbcc1d18351c1927f34addadcea
twothicc <chenenliuu@gmail.com>
2021-08-19 14:39:29 +0800

    Created test directory to store JUnit test classes and installed JUnit 5 dependency. Created dummy JUnit tests for Task class in TaskTest class.

---
+++ test/TaskTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @org.junit.jupiter.api.Test
+    void getStatusIcon() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void markAsDone() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
