4cfc67b3707699700aef99398fcca306f0b552ea
naranghardik16 <naranghardik16@gmail.com>
2021-08-19 14:37:02 +0800

    First commit (Level-1)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    //This function prints the horizontal line
+    public static void printHorizontalLine() {
+        System.out.println("          ___________________________________________________________________________");
+    }
+
+    //This function prints the welcome message
+    public static void printWelcomeMessage() {
+        printHorizontalLine();
+        System.out.println("            Hello! I'm Duke");
+        System.out.println("            What can I do for you?");
+        printHorizontalLine();
+    }
+
+    //This function prints the bye message
+    public static void printByeMessage() {
+        printHorizontalLine();
+        System.out.println("            Bye. Hope to see you again soon!");
+        printHorizontalLine();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printWelcomeMessage();
+
+        Scanner in = new Scanner(System.in);
+
+        String str;
+
+        str = in.nextLine();
+
+        while (!str.equals("bye")) {
+            printHorizontalLine();
+            System.out.println("            " + str);
+            printHorizontalLine();
+            str = in.nextLine();
+        }
+
+        printByeMessage();
     }
 }
