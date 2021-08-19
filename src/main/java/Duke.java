7fcb304a56d6c788107ee064db217d6654337e32
peilinye <peilinye18@gmail.com>
2021-08-19 16:05:13 +0800

    clean up code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,56 +16,44 @@ public class Duke {
         System.out.println();
 
         ArrayList<Task> tasks = new ArrayList<Task>();
-        int count = 0;
 
         Scanner in = new Scanner(System.in);
         while (in.hasNext()) {
-            String input = in.nextLine();
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon! :)");
-                break;
-            } else if (input.equals("list")) {
-                listTasks(tasks, count);
+            String input = in.nextLine().strip();
+            String[] x = input.split(" ");
+            String cmd = x[0];
+            if (x.length == 1) {
+                if ("bye".equals(cmd)) {
+                    System.out.println("Bye. Hope to see you again soon! :)");
+                    break;
+                } else if ("list".equals(cmd)) {
+                    Task.listAllTasks();
+                } else if ("todo".equals(cmd) || "deadline".equals(cmd) || "event".equals(cmd)) {
+                    missingTaskName(cmd);
+                } else if ("done".equals(cmd) || "delete".equals(cmd)) {
+                    System.out.println("Indicate a task number beside this command ☻");
+                } else {
+                    System.out.println("☹︎wut☁︎☻ unknown command");
+                }
             } else {
-                String[] x = input.split(" ");
-                String cmd = x[0];
                 if (cmd.equals("done")) {
-                    int len = x.length;
-                    if (len > 2) {
+                    if (x.length > 2) {
                         System.out.println("Too many arguments for this command.");
                         continue;
-                    } else if (len == 1) {
-                        System.out.println("Indicate a task number that you have completed ☻");
                     }
-                    taskDone(x, tasks, count);
+                    Task.taskDone(getTaskNumber(x));
                 } else if (cmd.equals("delete")) {
-                    if (x.length == 1) {
-                        System.out.println("Indicate a task number to delete ☻");
-                    } else {
-                        deleteTask(x, tasks, count);
-                        count--;
+                    if (x.length > 2) {
+                        System.out.println("Too many arguments for this command.");
+                        continue;
                     }
+                    Task.deleteTask(getTaskNumber(x));
                 } else if (cmd.equals("todo")) {
-                    if (x.length == 1) {
-                        missingTaskName("todo");
-                    } else {
-                        addToDo(input, tasks, count);
-                        count++;
-                    }
+                    addToDo(input);
                 } else if (cmd.equals("deadline")) {
-                    if (x.length == 1) {
-                        missingTaskName("deadline");
-                    } else {
-                        addDeadline(input, tasks, count);
-                        count++;
-                    }
+                    addDeadline(input);
                 } else if (cmd.equals("event")) {
-                    if (x.length == 1) {
-                        missingTaskName("event");
-                    } else {
-                        addEvent(input, tasks, count);
-                        count++;
-                    }
+                    addEvent(input);
                 } else {
                     // unknown command received
                     System.out.println("☹︎wut☁︎☻ unknown command");
@@ -75,51 +63,24 @@ public class Duke {
         in.close();
     }
 
-    public static void listTasks(ArrayList<Task> tasks, int count) {
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < count; i++) {
-            Task t = tasks.get(i);
-            System.out.println((i + 1) + "." + t.toString());
-        }
-    }
-
-    public static void taskDone(String[] inputArr, ArrayList<Task> tasks, int count) {
-        int index = Integer.parseInt(inputArr[1]) - 1;
-        if (index >= count) {
-            System.out.println("No task of this number. Add new task or input a different number.");
-        } else if (index < 0) {
-            System.out.println("Input a task number from 1 - " + count);
-        } else {
-            Task t = tasks.get(index);
-            t.markAsDone();
-            System.out.println("Nice! I've marked this task as done:\n  " + t.toString());
-        }
-    }
-
-    public static void addToDo(String input, ArrayList<Task> tasks, int count) {
+    public static void addToDo(String input) {
         String name = input.substring(input.indexOf(" ") + 1);
         ToDo t = new ToDo(name);
-        tasks.add(t);
-        System.out.println("Okay! Task added:\n  " + t.toString());
-        System.out.println("You now have " + (count + 1) + " task(s) in the list.");
+        Task.addTask(t);
     }
 
-    public static void addDeadline(String input, ArrayList<Task> tasks, int count) {
+    public static void addDeadline(String input) {
         String name = input.substring(input.indexOf(" ") + 1, input.lastIndexOf("/by") - 1);
         String by = input.substring(input.lastIndexOf("/by") + 4);
         Deadline d = new Deadline(name, by);
-        tasks.add(d);
-        System.out.println("Okay! Task added:\n  " + d.toString());
-        System.out.println("You now have " + (count + 1) + " task(s) in the list.");
+        Task.addTask(d);
     }
 
-    public static void addEvent(String input, ArrayList<Task> tasks, int count) {
+    public static void addEvent(String input) {
         String name = input.substring(input.indexOf(" ") + 1, input.lastIndexOf("/at") - 1);
         String at = input.substring(input.lastIndexOf("/at") + 4);
         Event e = new Event(name, at);
-        tasks.add(e);
-        System.out.println("Okay! Task added:\n  " + e.toString());
-        System.out.println("You now have " + (count + 1) + " task(s) in the list.");
+        Task.addTask(e);
     }
 
     public static void missingTaskName(String cmd) {
@@ -127,18 +88,7 @@ public class Duke {
         System.out.println(str);
     }
 
-    public static void deleteTask(String[] inputArr, ArrayList<Task> tasks, int count) {
-        int index = Integer.parseInt(inputArr[1]) - 1;
-        if (index >= count) {
-            System.out.println("No task of this number. Add new task or input a different number.");
-        } else if (index < 0) {
-            System.out.println("Input a task number from 1 - " + count);
-        } else {
-            Task t = tasks.get(index);
-            tasks.remove(index);
-            System.out.println("Ok! I've deleted this task:\n  " + t.toString());
-            System.out.println("You now have " + (count - 1) + " task(s) in the list.");
-        }
+    public static int getTaskNumber(String[] inputArr) {
+        return Integer.parseInt(inputArr[1]) - 1;
     }
-
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class Task {
     protected String name;
     protected boolean isDone;
+    private static ArrayList<Task> listOfTasks = new ArrayList<Task>();
 
     public Task(String name) {
         this.name = name;
@@ -18,5 +21,47 @@ public class Task {
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + this.name;
+    }
+
+    public static void addTask(Task t) {
+        listOfTasks.add(t);
+        System.out.println("Okay! Task added:\n  " + t.toString());
+        System.out.println("You now have " + listOfTasks.size() + " task(s) in the list.");
+    }
+
+    public static void listAllTasks() {
+        System.out.println("Here are the tasks in your list:");
+        int size = listOfTasks.size();
+        for (int i = 0; i < size; i++) {
+            Task t = listOfTasks.get(i);
+            System.out.println((i + 1) + "." + t.toString());
+        }
+    }
+
+    public static void deleteTask(int index) {
+        int numOfTasks = listOfTasks.size();
+        if (index >= numOfTasks) {
+            System.out.println("No task of this number. Add new task or input a different number.");
+        } else if (index < 0) {
+            System.out.println("Input a task number from 1 - " + numOfTasks);
+        } else {
+            Task t = listOfTasks.get(index);
+            listOfTasks.remove(index);
+            System.out.println("Ok! I've deleted this task:\n  " + t.toString());
+            System.out.println("You now have " + (numOfTasks - 1) + " task(s) in the list.");
+        }
+    }
+
+    public static void taskDone(int index) {
+        int numOfTasks = listOfTasks.size();
+        if (index >= numOfTasks) {
+            System.out.println("No task of this number. Add new task or input a different number.");
+        } else if (index < 0) {
+            System.out.println("Input a task number from 1 - " + numOfTasks);
+        } else {
+            Task t = listOfTasks.get(index);
+            t.markAsDone();
+            System.out.println("Nice! I've marked this task as done:\n  " + t.toString());
+        }
     }
 }
