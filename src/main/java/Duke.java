7bd9d2bdcc9a58bcfaf074d7b4643e6001fa2504
HuangChing <ching2020huang@gmail.com>
2021-08-19 15:01:50 +0800

    add List function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
 public class Duke {
+    int numOfTasks = 0;
+    String[] tasks = new String[100];
 
+    Duke() {
+        numOfTasks= 0;
+        tasks = new String[100];
+    }
+
+    void toDo() {
+        for (int i = 0; i < this.numOfTasks; i++) {
+            System.out.println(i+1+"."+ this.tasks[i]);
+        }
+        if (this.numOfTasks == 0) {
+            System.out.println("List is empty!");
+        }
+    }
     public static void main(String[] args) {
+        Duke duke = new Duke();
         System.out.println("Hello I am Duke. \nWhat can I do for you?");
         Scanner scanner = new Scanner(System.in);
         String echo = scanner.nextLine();
@@ -10,7 +26,14 @@ public class Duke {
                 System.out.println("Bye! See you next time!");
                 break;
             }
+            if (echo.equals("List")) {
+                duke.toDo();
+                echo = scanner.nextLine();
+                continue;
+            }
             System.out.println(echo);
+            duke.tasks[duke.numOfTasks] = echo;
+            duke.numOfTasks = duke.numOfTasks + 1;
             echo = scanner.nextLine();
         }
 
