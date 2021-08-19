db172d0e4f508e4bf45211de03708fc73a45423a
YeluriKetan <e0603110@u.nus.edu>
2021-08-19 16:18:23 +0800

    greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
+    private void echo() {
+        Scanner echo = new Scanner(System.in);
+        while (echo.hasNextLine()) {
+            String next = echo.nextLine();
+            if (next.equalsIgnoreCase("bye")) {
+                System.out.println("    ------------------------------\n"
+                        + "    Bye. Hope to see you again soon!\n"
+                        + "    ------------------------------");
+                echo.close();
+                break;
+            } else {
+                System.out.println("    ------------------------------\n"
+                        + "    " + next + "\n"
+                        + "    ------------------------------");
+            }
+        }
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String hello = "    ------------------------------\n"
+                + "    Hello! I'm Duke\n"
+                + "    What can I do for you?\n"
+                + "    ------------------------------\n";
+
+        System.out.println("Hello from\n" + logo + hello);
+        Duke duke = new Duke();
+        duke.echo();
     }
 }
