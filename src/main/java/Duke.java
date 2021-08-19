52538a27b1c523fec4310a5d17223407f39a7c98
tanqiya <35829668+tanqiya@users.noreply.github.com>
2021-08-19 09:37:29 +0800

    Add completion status for task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,31 +12,52 @@ public class Duke {
                 + "   __(.)>   *quack*\n"
                 + "~~ \\___)\n";
 
-        String newUserInput = null;
-        String[] userInput = new String[100];
+        String[] tasks = new String[100];
+        boolean[] completionStatus = new boolean[100];
         int index = 0;
         boolean active = true;
 
-        Scanner myObj = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println(GREETING);
+        String newUserInput = null;
+        
         while (active) {
-            newUserInput = myObj.nextLine();
-            switch (newUserInput) {
+            newUserInput = scanner.nextLine();
+            String firstWord = newUserInput.contains(" ")
+                    ? newUserInput.split(" ")[0]
+                    : newUserInput;
+            switch (firstWord) {
                 case "bye":
                     active = false;
                     System.out.println(BYE);
                     break;
                 case "list":
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < index; i++) {
-                        System.out.println(i+1 + ". " + userInput[i]);
+                        System.out.println(i + 1 + ". ["
+                                + (completionStatus[i] ? "X] " : " ] ")
+                                + tasks[i]);
+                    }
+                    break;
+                case "done":
+                    int taskNo = Integer.parseInt(newUserInput.split(" ")[1]);
+                    if (taskNo < 1 || taskNo > index) {
+                        System.out.println("Error: No such task exists");
+                    } else {
+                        completionStatus[taskNo - 1] = true;
+                        System.out.println("Nice! I've marked this task as done: \n"
+                                + "[X] " + tasks[taskNo - 1]);
+
                     }
                     break;
                 default:
-                    userInput[index] = newUserInput;
+                    tasks[index] = newUserInput;
+                    completionStatus[index] = false;
                     index++;
                     System.out.println("added: " + newUserInput);
             }
         }
+        scanner.close();
     }
 
 
