0627c7d8e170d334086d807d68e10e91257efbad
over-fitted <overfitted@gmail.com>
2021-08-19 23:45:01 +0800

    Add Task class, add task completion marker and funtionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
 public class Duke {
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public String toString() {
+            return String.format("[%s] %s", this.getStatusIcon(), this.description);
+        }
+
+        public void complete() {
+            this.isDone = true;
+        }
+    }
+
     public static class TaskList {
-        private String[] tasks;
+        private Task[] tasks;
         private int numTask;
 
         public TaskList(){
-            this.tasks = new String[100];
+            this.tasks = new Task[100];
             this.numTask = 0;
         }
 
         public void add(String task) {
-            tasks[this.numTask] = task;
+            tasks[this.numTask] = new Task(task);
             this.numTask++;
             System.out.println("added: " + task);
         }
@@ -24,9 +47,16 @@ public class Duke {
                 System.out.println((i + 1)+ ". " + tasks[i]);
             }
         }
+
+        public void done(int taskNumber) {
+            tasks[taskNumber - 1].complete();
+            System.out.println("Nice! I've marked this task as done: ");
+            System.out.println("  " + tasks[taskNumber - 1]);
+        }
     }
 
     public static void main(String[] args) throws IOException {
+
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
 //                + "| | | | | | | |/ / _ \\\n"
@@ -54,6 +84,13 @@ public class Duke {
             // print out list
             if (input.equals("list")) {
                 storage.list();
+                continue;
+            }
+
+            // complete a task
+            if (Pattern.matches("done \\d", input)) {
+                String[] items = input.split(" ");
+                storage.done(Integer.parseInt(items[1]));
                 continue;
             }
 
