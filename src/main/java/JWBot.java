6edccfc8611a4cb3618cc8df80e5a4c2690084e4
somekoreanboi <77215389+somekoreanboi@users.noreply.github.com>
2021-08-19 08:10:30 +0800

    level 5 incremented

--- src/main/java/JWBot.java
+++ src/main/java/JWBot.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class JWBot {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JWBotException {
         Scanner sc = new Scanner(System.in);
 
         String greeting = "Wassup bro! I'm JWBot\n"
@@ -16,41 +16,56 @@ public class JWBot {
         String input = "";
 
         while(!input.equals("bye")) {
-            input = sc.nextLine();
-            if (input.equals("bye")) {
-                System.out.println(byeMessage);
-            } else if (input.equals("list")) {
-                System.out.println("OK bro, the tasks in your list are: ");
-                for (int i = 1; i < items.size() + 1; i++) {
-                    System.out.println(i + ". " + items.get(i - 1));
+            try {
+                input = sc.nextLine();
+                if (input.equals("bye")) {
+                    System.out.println(byeMessage);
+                } else if (input.equals("list")) {
+                    System.out.println("OK bro, the tasks in your list are: ");
+                    for (int i = 1; i < items.size() + 1; i++) {
+                        System.out.println(i + ". " + items.get(i - 1));
+                    }
+                    System.out.println("Bro, now you have " + items.size() + " task(s) stored in the list!");
+                } else if (input.startsWith("done ")) {
+                    try {
+                        String[] separated = input.split(" ");
+                        int index = Integer.parseInt(separated[1]);
+                        items.get(index - 1).markAsDone();
+                        System.out.println("OK Bro, I noted you've done this task:\n" +
+                                items.get(index - 1));
+                    } catch (Exception e) {
+                        throw new JWBotException("Sorry bro, I think you chose an incorrect index number!");
+                    }
+                } else if (input.startsWith("deadline ")) {
+                    try {
+                        String content = input.split(" ", 2)[1];
+                        String[] separated = content.split(" /by ");
+                        Deadline deadline = new Deadline(separated[0], separated[1]);
+                        items.add(deadline);
+                        System.out.println("OK bro, I just added: " + deadline);
+                    } catch (Exception e) {
+                        throw new JWBotException("Sorry bro, I think you made an error with the deadline format!");
+                    }
+                } else if (input.startsWith("todo ")) {
+                    String content = input.split(" ", 2)[1];
+                    Todo todo = new Todo(content);
+                    items.add(todo);
+                    System.out.println("OK bro, I just added: " + todo);
+                } else if (input.startsWith("event ")) {
+                    try {
+                        String content = input.split(" ", 2)[1];
+                        String[] separated = content.split(" /at ");
+                        Event event = new Event(separated[0], separated[1]);
+                        items.add(event);
+                        System.out.println("OK bro, I just added: " + event);
+                    } catch (Exception e) {
+                        throw new JWBotException("Sorry bro, I think you made an error with the event format!");
+                    }
+                } else {
+                        throw new JWBotException("Sorry bro, I don't understand what you mean!");
                 }
-                System.out.println("Bro, now you have " + items.size() + " tasks stored in the list!");
-            } else if (input.startsWith("done ")) {
-                String[] separated = input.split(" ");
-                int index = Integer.parseInt(separated[1]);
-                items.get(index - 1).markAsDone();
-                System.out.println("OK Bro, I noted you've done this task:\n" +
-                        items.get(index - 1));
-            } else if (input.startsWith("deadline ")) {
-                String content = input.split(" ", 2)[1];
-                String[] separated = content.split(" /by ");
-                Deadline deadline = new Deadline(separated[0], separated[1]);
-                items.add(deadline);
-                System.out.println("OK bro, I just added: " + deadline);
-            } else if (input.startsWith("todo ")) {
-                String content = input.split(" ", 2)[1];
-                Todo todo = new Todo(content);
-                items.add(todo);
-                System.out.println("OK bro, I just added: " + todo);
-            } else if (input.startsWith("event ")) {
-                String content = input.split(" ", 2)[1];
-                String[] separated = content.split(" /at ");
-                Event event = new Event(separated[0], separated[1]);
-                items.add(event);
-                System.out.println("OK bro, I just added: " + event);
-            } else {
-                items.add(new Task(input));
-                System.out.println("OK bro, I just added: " + input);
+            } catch (JWBotException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
---
+++ src/main/java/JWBotException.java
@@ -0,0 +1,5 @@
+public class JWBotException extends Exception {
+    public JWBotException(String e) {
+        super(e);
+    }
+}
