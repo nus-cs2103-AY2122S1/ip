58049df35e546c3217d45eb017e0d124bc9751da
tianyue58 <e0563952@u.nus.edu>
2021-08-19 15:23:05 +0800

    delete "\n" in print statement

--- src/main/java/UnknownException.java
+++ src/main/java/UnknownException.java
@@ -1,5 +1,5 @@
 public class UnknownException extends Exception {
     public UnknownException() {
-        super("OOPS!!! I'm sorry, but I don't know what that means :-(\n");
+        super("OOPS!!! I'm sorry, but I don't know what that means :-(");
     }
 }
