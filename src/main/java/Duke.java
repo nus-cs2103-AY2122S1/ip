64c1d833357fe60ed6012037dd8fecfd36288f0b
HuangChing <ching2020huang@gmail.com>
2021-08-19 11:41:48 +0800

    add echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 public class Duke {
+    public static String echo(Scanner scanner) {
+        String echo = scanner.nextLine();
+        return echo;
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I am Duke. \nWhat can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+        String answer = echo(scanner);
+        System.out.println(answer);
     }
 }
