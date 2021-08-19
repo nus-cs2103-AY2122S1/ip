b5b485320fbde3d4df1ea5636871f1cadc42d80a
Cai Kaian <caipngg@gmail.com>
2021-08-19 17:17:43 +0800

    Add error handling for done task command

--- src/main/java/Duke/Commands/DoneTaskCommand.java
+++ src/main/java/Duke/Commands/DoneTaskCommand.java
@@ -1,6 +1,7 @@
 package Duke.Commands;
 
 import Duke.Duke;
+import Duke.Task.InvalidTaskException;
 import Duke.Task.Task;
 
 import java.util.HashSet;
@@ -10,10 +11,18 @@ import java.util.Set;
 class DoneTaskCommand extends Command {
     private static final Set<String> KEYWORDS = new HashSet<>(List.of("done"));
     private static final String DONE_TASK_SUCCESS_MESSAGE = "Good job! I've marked this task as done:\n\t%s";
+    private static final String INVALID_NUMBER_FORMAT_MESSAGE = "\"%s\" is not a valid task number.";
 
     @Override
-    public void run(Duke duke, Duke.UserInput input) {
-        int taskIndex = Integer.parseInt(input.getArgs()) - 1;
+    public void run(Duke duke, Duke.UserInput input) throws InvalidTaskException {
+        String args = input.getArgs();
+        int taskIndex;
+        try {
+            taskIndex = Integer.parseInt(args);
+        } catch (NumberFormatException e) {
+            throw new InvalidTaskException(String.format(INVALID_NUMBER_FORMAT_MESSAGE, args));
+        }
+
         Task task = duke.getTaskList().get(taskIndex);
         task.markAsDone();
         duke.say(String.format(DONE_TASK_SUCCESS_MESSAGE, task));
--- src/main/java/Duke/Task/TaskList.java
+++ src/main/java/Duke/Task/TaskList.java
@@ -4,14 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TaskList {
+    private static final String EMPTY_LIST_MESSAGE = "Hooray! You have no tasks currently.";
+    private static final String CANNOT_FIND_TASK_MESSAGE = "Cannot find task with number %d.";
     private final List<Task> list = new ArrayList<>();
 
     public void add(Task newTask) {
         this.list.add(newTask);
     }
 
-    public Task get(int taskIndex) {
-        return this.list.get(taskIndex);
+    public Task get(int taskIndex) throws InvalidTaskException {
+        try {
+            return this.list.get(taskIndex - 1);
+        } catch (IndexOutOfBoundsException e) {
+            throw new InvalidTaskException(String.format(CANNOT_FIND_TASK_MESSAGE, taskIndex));
+        }
     }
 
     public int size() {
@@ -20,6 +26,10 @@ public class TaskList {
 
     @Override
     public String toString() {
+        if (this.size() == 0) {
+            return EMPTY_LIST_MESSAGE;
+        }
+
         StringBuilder builder = new StringBuilder();
         for(int i = 0; i < this.list.size(); i++) {
            builder.append(String.format("%d. %s\n", i + 1, this.list.get(i).toString()));
