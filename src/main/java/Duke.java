15cfb009a4ebab78cf59f8ddde45c5fab0102dcb
pranav_ganesh <e0661424@u.nus.edu>
2021-08-19 15:34:37 +0800

    Finished Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,6 +19,41 @@ class Task {
     public void markAsDone() {
         this.isDone = true;
     }
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "]" + " " + this.description;
+    }
+}
+class Deadline extends Task {
+    protected String by;
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + "(by: " + by + ")";
+    }
+}
+class Event extends Task {
+    protected String at;
+    public Event(String description, String at) {
+        super(description);
+        this.at = at;
+    }
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + at + ")";
+    }
+}
+class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
 }
 public class Duke {
     public static void main(String[] args) {
@@ -40,7 +75,7 @@ public class Duke {
                 int count = 1;
                 for (int i = 0; i < userList.size(); i++) {
                     Task t = userList.get(i);
-                    System.out.println("   " + count + ".[" + t.getStatusIcon() + "] " + t.description);
+                    System.out.println("   " + count + ". " +  t.toString());
                     count++;
                 }
             } else if (command.startsWith("done") && Character.isDigit(command.charAt(command.length() - 1))
@@ -53,12 +88,34 @@ public class Duke {
                     Task t = userList.get(value - 1);
                     t.markAsDone();
                     System.out.println("   Nice! I've marked this task as done: ");
-                    System.out.println("     [" + t.getStatusIcon() + "] " + t.description);
+                    System.out.println("     " + t.toString());
                 }
             } else {
-                Task task = new Task(command);
-                userList.add(task);
-                System.out.println("   added: " + command);
+                if (command.startsWith("todo")) {
+                    Task task = new Todo(command.substring(5));
+                    userList.add(task);
+                    System.out.println("   Got it. I've added this task: ");
+                    System.out.println("     " + task.toString());
+                    System.out.println("   Now you have " + userList.size() + " tasks in the list.");
+                } else if (command.startsWith("deadline")) {
+                    String[] parts =  command.split("/");
+                    Task task = new Deadline(parts[0].substring(9), parts[1].substring(3));
+                    userList.add(task);
+                    System.out.println("   Got it. I've added this task: ");
+                    System.out.println("     " + task.toString());
+                    System.out.println("   Now you have " + userList.size() + " tasks in the list.");
+                } else if (command.startsWith("event")) {
+                    String[] parts =  command.split("/");
+                    Task task = new Event(parts[0].substring(6), parts[1].substring(3));
+                    userList.add(task);
+                    System.out.println("   Got it. I've added this task: ");
+                    System.out.println("     " + task.toString());
+                    System.out.println("   Now you have " + userList.size() + " tasks in the list.");
+                } else {
+                    Task task = new Task(command);
+                    userList.add(task);
+                    System.out.println("   added: " + command);
+                }
             }
         }
     }
