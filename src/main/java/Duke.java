cf26b028f3d05e8e79cb1733bd41b61c9be83e31
clementkfj <clement.kfj@gmail.com>
2021-08-19 11:25:26 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String lineProducer() {
+        return "   -----------------------------------------";
+    }
+    public static String indentationAdder() {
+        return "\n    ";
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println(lineProducer() + indentationAdder() + "Hello I'm your friendly chatbot Duke!" +
+                indentationAdder() + "What can I help you with?\n" + lineProducer() );
+        while (sc.hasNextLine()) {
+            String str = sc.nextLine();
+            if (str.equals("bye")) {
+                System.out.println(lineProducer() + indentationAdder() + "Bye! Please visit me again!\n" + lineProducer());
+                break;
+            }
+            System.out.println(lineProducer() + indentationAdder() + str + "\n" + lineProducer());
+        }
+
+        sc.close();
     }
 }
