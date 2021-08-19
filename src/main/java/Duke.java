a27f3196cc1f20bde03ef264fba67179046276eb
zhuoyang125 <zhuoyang125@gmail.com>
2021-08-19 17:27:48 +0800

    add Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException{
         Scanner scanner = new Scanner(System.in);
         List<Task> inputs = new ArrayList<Task>();
 
@@ -22,49 +22,67 @@ public class Duke {
                 }
             } else if (input.matches("done\\s[1-9][0-9]?")) {
                 int taskToComplete = Integer.valueOf(input.split(" ")[1]);
-                inputs.get(taskToComplete - 1).markAsCompleted();
-                System.out.println("I have marked the task as done!");
-                System.out.println("Current List:");
-                System.out.println("---------------");
-                for (int i = 0; i < inputs.size(); i++) {
-                    System.out.println( (i+1) + ": " + inputs.get(i));
+                if (taskToComplete - 1 >= 0 && taskToComplete - 1 <inputs.size()) {
+                    inputs.get(taskToComplete - 1).markAsCompleted();
+                    System.out.println("I have marked the task as done!");
+                    System.out.println("Current List:");
+                    System.out.println("---------------");
+                    for (int i = 0; i < inputs.size(); i++) {
+                        System.out.println( (i+1) + ": " + inputs.get(i));
+                    }
+                } else {
+                    throw new InvalidTaskIDException();
                 }
-            } else if (input.matches("todo\\s(.*?)")) {
-                String taskname = input.split(" ", 2)[1];
-                ToDo todo = new ToDo(taskname);
-                inputs.add(todo);
-                System.out.println("A ToDo has been added\n");
-                System.out.println("Current List:");
-                System.out.println("---------------");
-                for (int i = 0; i < inputs.size(); i++) {
-                    System.out.println( (i+1) + ": " + inputs.get(i));
+            } else if (input.matches("todo(.*?)")) {
+                if (input.split(" ").length < 2) {
+                    throw new EmptyTaskDescriptionException();
+                } else {
+                    String taskname = input.split(" ", 2)[1];
+                    ToDo todo = new ToDo(taskname);
+                    inputs.add(todo);
+                    System.out.println("A ToDo has been added\n");
+                    System.out.println("Current List:");
+                    System.out.println("---------------");
+                    for (int i = 0; i < inputs.size(); i++) {
+                        System.out.println( (i+1) + ": " + inputs.get(i));
+                    }
                 }
-            } else if (input.matches("deadline\\s(.*?)/by\\s(.*?)")) {
-                String firstCommand = input.split("/by", 2)[0];
-                String taskname = firstCommand.split(" ", 2)[1];
-                String dueDate = input.split("/by", 2)[1];
-                Deadline deadline = new Deadline(taskname.trim(), dueDate);
-                inputs.add(deadline);
-                System.out.println("A Deadline has been added\n");
-                System.out.println("Current List:");
-                System.out.println("---------------");
-                for (int i = 0; i < inputs.size(); i++) {
-                    System.out.println( (i+1) + ": " + inputs.get(i));
+
+            } else if (input.matches("deadline(.*?)")) {
+                if (input.split(" ").length < 2) {
+                    throw new EmptyTaskDescriptionException();
+                } else {
+                    String firstCommand = input.split("/by", 2)[0];
+                    String taskname = firstCommand.split(" ", 2)[1];
+                    String dueDate = input.split("/by", 2)[1];
+                    Deadline deadline = new Deadline(taskname.trim(), dueDate);
+                    inputs.add(deadline);
+                    System.out.println("A Deadline has been added\n");
+                    System.out.println("Current List:");
+                    System.out.println("---------------");
+                    for (int i = 0; i < inputs.size(); i++) {
+                        System.out.println( (i+1) + ": " + inputs.get(i));
+                    }
                 }
-            } else if (input.matches("event\\s(.*?)/at(.*?)")) {
-                String firstCommand = input.split("/at", 2)[0];
-                String taskname = firstCommand.split(" ", 2)[1];
-                String duration = input.split("/at", 2)[1];
-                Event event = new Event(taskname.trim(), duration);
-                inputs.add(event);
-                System.out.println("An Event has been added\n");
-                System.out.println("Current List:");
-                System.out.println("---------------");
-                for (int i = 0; i < inputs.size(); i++) {
-                    System.out.println( (i+1) + ": " + inputs.get(i));
+
+            } else if (input.matches("event(.*?)")) {
+                if (input.split(" ").length < 2) {
+                    throw new EmptyTaskDescriptionException();
+                } else {
+                    String firstCommand = input.split("/at", 2)[0];
+                    String taskname = firstCommand.split(" ", 2)[1];
+                    String duration = input.split("/at", 2)[1];
+                    Event event = new Event(taskname.trim(), duration);
+                    inputs.add(event);
+                    System.out.println("An Event has been added\n");
+                    System.out.println("Current List:");
+                    System.out.println("---------------");
+                    for (int i = 0; i < inputs.size(); i++) {
+                        System.out.println((i + 1) + ": " + inputs.get(i));
+                    }
                 }
             } else
-                System.out.println("Sorry I don't understand what you mean! Try again :(");
+                throw new InvalidCommandException();
             }
 
 
---
+++ src/main/java/InvalidCommandException.java
@@ -0,0 +1,5 @@
+public class InvalidCommandException extends DukeException {
+    public InvalidCommandException() {
+        super("I'm not sure what you mean. (T_T)");
+    }
+}
