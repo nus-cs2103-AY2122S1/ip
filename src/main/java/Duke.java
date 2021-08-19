696945970184f56f08f5fdbf35d61b80fcede4b4
CheongYeeMing <e0526001@u.nus.edu>
2021-08-19 17:03:30 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,21 +10,35 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         // Greetings
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        // List to store all user inputs
+        List<String> list = new ArrayList<>();
 
         // Take in user input
         while (true) {
             Scanner sc = new Scanner(System.in);
             String str = sc.nextLine();
-            System.out.println("    ____________________________________________________________");
-            if (str.equals("bye")){
+            if (!str.equals("list")) list.add(str);
+            if (str.equals("bye")) {
+                System.out.println("    ____________________________________________________________");
                 System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println("    ____________________________________________________________");
                 break;
+            } else if (str.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println("     " + (i+1) + ". " + list.get(i));
+                }
+                System.out.println("    ____________________________________________________________");
+            } else {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     added: " + str);
+                System.out.println("    ____________________________________________________________");
             }
-            System.out.println("     " + str);
-            System.out.println("    ____________________________________________________________");
         }
     }
 }
