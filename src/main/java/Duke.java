d6063f9fda983db3ef8474b74a778115b57d311f
Neo Jing Xuan <neojxuan@gmail.com>
2021-08-19 18:26:09 +0800

    add different types of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Duke {
     private static int taskCount = 0;
+    private static final int MAX = 100;
+    private static Task[] tasks = new Task[MAX];
 
     public static void main(String[] args) {
-        int MAX = 100;
-        Task[] tasks = new Task[MAX];
         System.out.println("Hello...\nWhat do you want?\n");
         Scanner myScanner = new Scanner(System.in);
         String answer = myScanner.nextLine();
@@ -13,6 +13,7 @@ public class Duke {
         while (!answer.equals("bye")) {
             String[] parts = answer.split(" ");
             String taskType = parts[0];
+            String taskDetails = answer.substring(answer.indexOf(" ") + 1);
             if (taskType.equals("done")) {
                 int taskIndex = Integer.parseInt(parts[1]) - 1;
                 tasks[taskIndex].markAsDone();
@@ -24,12 +25,31 @@ public class Duke {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
             } else {
-                tasks[taskCount] = new Task(answer);
-                taskCount += 1;
-                System.out.println("added: " + answer);
+                addNewTask(taskType, taskDetails);
             }
             answer = myScanner.nextLine();
         }
         System.out.println("Whatever...");
+    }
+
+    private static void addNewTask(String taskType, String taskDetails) {
+        if (taskType.equals("todo")) {
+            tasks[taskCount] = new Todo(taskDetails);
+        } else if (taskType.equals("event")) {
+            String[] parts = taskDetails.split(" /at ");
+            String description = parts[0];
+            String at = parts[1]; 
+            tasks[taskCount] = new Event(description, at);
+        } else if (taskType.equals("deadline")) {
+            String[] parts = taskDetails.split(" /by ");
+            String description = parts[0];
+            String by = parts[1]; 
+            tasks[taskCount] = new Event(description, by);
+        } else {
+            return;
+        }
+        System.out.println("Got it. I've added this task:\n\t" + tasks[taskCount]);
+        taskCount += 1;
+        System.out.println("Now you have " + taskCount + " tasks in the list.");
     }
 }
