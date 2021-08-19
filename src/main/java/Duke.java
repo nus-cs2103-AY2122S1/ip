aa566a839e06012345d0c363e506959678a2e6a0
wonyeji <75670073+wonyeji@users.noreply.github.com>
2021-08-19 17:49:50 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
+//    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+//    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Jacky\nWhat can I do for you?");
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        while (input != null) {
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.exit(0);
+            }
+            System.out.println(input);
+            input = in.nextLine();
+        }
     }
 }
