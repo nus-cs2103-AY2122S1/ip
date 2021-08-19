29fa65907a64d3dff5cba1ac037c0160d7b583f7
dannytayjy <danny_tayjunyang@hotmail.com>
2021-08-19 23:05:08 +0800

    Implement Level-5: Handle errors via Exception Handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,91 +22,146 @@ public class Duke {
                     echo(exitMessage());
                     break;
                 case "list":
-                    displayTasksList(tasksList);
+                    try {
+                        displayTasksList(tasksList);
+                    } catch (DukeException e) {
+                        echo(e.getMessage());
+                    }
                     break;
                 case "done":
-                    markDone(command, tasksList);
+                    try {
+                        markDone(command, tasksList);
+                    } catch (DukeException e) {
+                        echo(e.getMessage());
+                    }
                     break;
                 case "todo":
-                    addToDoTask(command, tasksList);
+                    try {
+                        addToDoTask(command, tasksList);
+                    } catch (DukeException e) {
+                        echo(e.getMessage());
+                    }
                     break;
                 case "deadline":
-                    addDeadlineTask(command, tasksList);
+                    try {
+                        addDeadlineTask(command, tasksList);
+                    } catch (DukeException e) {
+                        echo(e.getMessage());
+                    }
                     break;
                 case "event":
-                    addEventTask(command, tasksList);
+                    try {
+                        addEventTask(command, tasksList);
+                    } catch (DukeException e) {
+                        echo(e.getMessage());
+                    }
                     break;
                 default:
+                    try {
+                        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means!");
+                    } catch (DukeException e) {
+                        echo(e.getMessage());
+                    }
                     break;
             }
         } while (!commandLine.equals("bye"));
     }
 
-    static void displayTasksList(List<Task> tasksList) {
-        String response = "\t" + "Here are the tasks in your list:" +
-                System.lineSeparator();
+    static void displayTasksList(List<Task> tasksList) throws DukeException {
+        if (tasksList.size() != 0) {
+            String response = "\t" + "Here are the tasks in your list:" +
+                    System.lineSeparator();
 
-        for (int i = 0; i < tasksList.size(); i++) {
-            if (i != 0) {
-                response += System.lineSeparator();
+            for (int i = 0; i < tasksList.size(); i++) {
+                if (i != 0) {
+                    response += System.lineSeparator();
+                }
+
+                response += "\t\t" + (i + 1) + "." + "\t" + tasksList.get(i).toString();
             }
 
-            response += "\t\t" + (i + 1) + "." + "\t" + tasksList.get(i).toString();
-        }
-
-        echo(response);
-    }
-
-    static void markDone(String[] command, List<Task> tasksList) {
-        int taskNum = Integer.parseInt(command[1]);
-
-        if (taskNum <= tasksList.size()) {
-            tasksList.get(taskNum - 1).setDone();
-            Task taskDone = tasksList.get(taskNum - 1);
-
-            String response = taskDoneMessage() + System.lineSeparator() +
-                    "\t\t" + taskDone;
             echo(response);
+        } else {
+            throw new DukeException("There are no tasks in your list!");
         }
     }
 
-    static void addToDoTask(String[] command, List<Task> tasksList) {
-        Task newToDoTask = new ToDo(TaskType.TODO, command[1]);
-        tasksList.add(newToDoTask);
+    static void markDone(String[] command, List<Task> tasksList) throws DukeException {
+        if (command.length == 2) {
+            int taskNum = Integer.parseInt(command[1]);
 
-        String response = taskAddedMessage() + System.lineSeparator() +
-                "\t\t" + newToDoTask + System.lineSeparator() +
-                numOfTasksInList(tasksList);
-        echo(response);
+            if (taskNum <= tasksList.size()) {
+                tasksList.get(taskNum - 1).setDone();
+                Task taskDone = tasksList.get(taskNum - 1);
+
+                String response = taskDoneMessage() + System.lineSeparator() +
+                        "\t\t" + taskDone;
+                echo(response);
+            } else {
+                throw new DukeException("☹ Please select a valid task number to be marked as done.");
+            }
+        } else {
+            throw new DukeException("☹ Please select the task number to be marked as done.");
+        }
     }
 
-    static void addDeadlineTask(String[] command, List<Task> tasksList) {
-        String[] deadlineTaskDetails = command[1].split("/", 2);
-
-        if (deadlineTaskDetails.length == 2) {
-            Task newDeadlineTask = new Deadline(TaskType.DEADLINE,
-                    deadlineTaskDetails[0], deadlineTaskDetails[1]);
-            tasksList.add(newDeadlineTask);
+    static void addToDoTask(String[] command, List<Task> tasksList) throws DukeException {
+        if (command.length == 2) {
+            Task newToDoTask = new ToDo(TaskType.TODO, command[1]);
+            tasksList.add(newToDoTask);
 
             String response = taskAddedMessage() + System.lineSeparator() +
-                    "\t\t" + newDeadlineTask + System.lineSeparator() +
+                    "\t\t" + newToDoTask + System.lineSeparator() +
                     numOfTasksInList(tasksList);
             echo(response);
+        } else {
+            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
         }
     }
 
-    static void addEventTask(String[] command, List<Task> tasksList) {
-        String[] eventTaskDetails = command[1].split("/", 2);
+    static void addDeadlineTask(String[] command, List<Task> tasksList) throws DukeException {
+        if (command.length == 2) {
+            String[] deadlineTaskDetails = command[1].split("/", 2);
 
-        if (eventTaskDetails.length == 2) {
-            Task newEventTask = new Event(TaskType.EVENT,
-                    eventTaskDetails[0], eventTaskDetails[1]);
-            tasksList.add(newEventTask);
+            if (deadlineTaskDetails.length == 2) {
+                Task newDeadlineTask = new Deadline(TaskType.DEADLINE,
+                        deadlineTaskDetails[0], deadlineTaskDetails[1]);
+                tasksList.add(newDeadlineTask);
 
-            String response = taskAddedMessage() + System.lineSeparator() +
-                    "\t\t" + newEventTask + System.lineSeparator() +
-                    numOfTasksInList(tasksList);
-            echo(response);
+                String response = taskAddedMessage() + System.lineSeparator() +
+                        "\t\t" + newDeadlineTask + System.lineSeparator() +
+                        numOfTasksInList(tasksList);
+                echo(response);
+            } else {
+                throw new DukeException("☹ OOPS!!! The description and/or " +
+                        "specific date/time of a deadline is not valid.");
+            }
+        } else {
+            throw new DukeException("☹ OOPS!!! The description and " +
+                    "specific date/time of a deadline cannot be empty.");
+        }
+    }
+
+    static void addEventTask(String[] command, List<Task> tasksList) throws DukeException {
+        if (command.length == 2) {
+            String[] eventTaskDetails = command[1].split("/", 2);
+
+            if (eventTaskDetails.length == 2) {
+                Task newEventTask = new Event(TaskType.EVENT,
+                        eventTaskDetails[0], eventTaskDetails[1]);
+                tasksList.add(newEventTask);
+
+                String response = taskAddedMessage() + System.lineSeparator() +
+                        "\t\t" + newEventTask + System.lineSeparator() +
+                        numOfTasksInList(tasksList);
+                echo(response);
+            } else {
+                throw new DukeException("☹ OOPS!!! The description and/or " +
+                        "specific date/time of a deadline is not valid.");
+            }
+        } else {
+            throw new DukeException("☹ OOPS!!! The description and " +
+                    "specific date/time of an event cannot be empty.");
         }
     }
 
