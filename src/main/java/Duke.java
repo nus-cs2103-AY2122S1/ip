98b4bdc7bc9c6b97883b0ca2257ea2942a6c8540
Kimowarui <zhangyubin83567593@gmail.com>
2021-08-19 22:39:01 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,7 +11,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String line = "❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤";
-        ArrayList<String> task = new ArrayList<>();
+        ArrayList<Task> task = new ArrayList<>();
         System.out.println("Hello from\n" + logo + line + "\nWhat can I do for you?\n" + line);
 
         String input;
@@ -19,17 +19,26 @@ public class Duke {
         input = s.nextLine();
 
         while(!input.equals("bye")) {
+            String[] parts = input.split(" ");
             if(input.equals("list")) {
                 int i = 1;
                 System.out.println(line);
-                for(String str: task) {
-                    System.out.println(i + ". " + str);
+                System.out.println("Here are the tasks in your list:");
+                for(Task t: task) {
+                    System.out.println(i + "." + t.toString());
                     i++;
                 }
                 System.out.println(line);
-            } else {
+            } else if (parts[0].equals("done")) {
+                int index = Integer.valueOf(parts[1]);
+                Task thisTask = task.get(index - 1);
+                thisTask.markDone();
+                System.out.println(line);
+                System.out.println("Nice! I've marked this task as done:\n" + thisTask.toString());
+                System.out.println(line);
+            }else {
                 System.out.println(line + "\n" + "added: " + input + "\n" + line);
-                task.add(input);
+                task.add(new Task(input));
             }
             input = s.nextLine();
         }
