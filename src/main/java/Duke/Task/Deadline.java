04f8180c5d1a25fba8c5a9f5d409fa8366c2e24f
Cai Kaian <caipngg@gmail.com>
2021-08-19 12:54:05 +0800

    Add deadline and event classes

---
+++ src/main/java/Duke/Task/Deadline.java
@@ -0,0 +1,22 @@
+package Duke.Task;
+
+public class Deadline extends Task {
+    private static final String TASK_TYPE_ICON = "D";
+
+    private final String endDate;
+
+    public Deadline(String description, String endDate) {
+        super(description);
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" (by: %s)", this.endDate);
+    }
+
+    @Override
+    protected String getTaskTypeIcon() {
+        return TASK_TYPE_ICON;
+    }
+}
---
+++ src/main/java/Duke/Task/Event.java
@@ -0,0 +1,22 @@
+package Duke.Task;
+
+public class Event extends Task {
+    private static final String TASK_TYPE_ICON = "E";
+
+    private final String period;
+
+    public Event(String description, String period) {
+        super(description);
+        this.period = period;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" (at: %s)", this.period);
+    }
+
+    @Override
+    protected String getTaskTypeIcon() {
+        return TASK_TYPE_ICON;
+    }
+}
