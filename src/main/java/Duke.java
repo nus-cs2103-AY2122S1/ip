41e34cab253ed95caa06814fd19a9c052884c3ce
Kangxun <e0543787@u.nus.edu>
2021-08-19 16:48:39 +0800

    Redo Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -15,13 +16,27 @@ public class Duke {
         System.out.println("how can i assist you?");
         System.out.println(line);
 
+        // List to store commands
+        ArrayList<String> cmdList = new ArrayList<>();
+        int count = 0;
+
         // Create a scanner to read from standard input.
         Scanner sc = new Scanner(System.in);
         String cmd = sc.nextLine();
         while (!cmd.equals("bye")) {
-            System.out.println(line);
-            System.out.println(cmd);
-            System.out.println(line);
+            if (!cmd.equals("list")) {
+                System.out.println(line);
+                cmdList.add(cmd);
+                count++;
+                System.out.println("added: " + cmd);
+                System.out.println(line);
+            } else {
+                System.out.println(line);
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + cmdList.get(i));
+                }
+                System.out.println(line);
+            }
             cmd = sc.nextLine();
         }
 
