7645743f4351c498b596fae027de3f22a8dbce5b
GabrielWLM <e0550342@u.nus.edu>
2021-08-19 22:38:29 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,6 +13,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         String[] list = new String[100];
+        boolean[] done = new boolean[100];
         int amount = 0;
 
         String lineBreak = "\t____________________________________________________________";
@@ -20,18 +21,29 @@ public class Duke {
                 + "\n\t Hello! I'm Luke, your slightly useful personal assistant!\n"
                 + "\t What can I do for you, my liege?\n"
                 + "\t Type 'list' to show previous inputs\n"
+                + "\t Type 'done #' to indicate that task # has been done\n"
                 + "\t Type 'bye' to end\n"
                 + lineBreak);
 
         String response = scanner.nextLine();
 
         while (!response.equals("bye")) {
-            if(response.equals("list")) {
+            if (response.equals("list")) {
                 System.out.println(lineBreak);
+                System.out.println("\t Here are the tasks in your list:");
                 for (int i = 0; i < amount; i++) {
-                    System.out.println("\n\t " + (i + 1) + ". " + list[i]);
+                    String doneI = " ";
+                    if (done[i]) doneI = "X";
+                    System.out.println("\t " + (i + 1) + ".[" + doneI + "] " + list[i]);
                 }
                 System.out.println(lineBreak);
+            } else if (response.matches("done ([0-9]|[1-9][0-9])")) {
+                int taskNo = Integer.parseInt(response.replaceAll("\\D", "")) - 1;
+                System.out.println(lineBreak);
+                System.out.println("\t Nice! I've marked this task as done:");
+                System.out.println("\t   [X] " + list[taskNo]);
+                System.out.println(lineBreak);
+                done[taskNo] = true;
             } else {
                 System.out.println(lineBreak
                         + "\n\t added: "
