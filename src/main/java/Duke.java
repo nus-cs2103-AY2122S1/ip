5052638df64b5777eaae83ee2c65881c8ba67147
Benjamin Tan <benjamin@dev.ofcr.se>
2021-08-19 17:13:41 +0800

    Improve formatting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,34 +1,50 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.lang.StringBuilder;
 
 public class Duke {
-    private static String welcomeText = "Hey there I'm Duke!\n"
-            + "How can I help you today?\n";
-    private static String byeText = "Bye! Hope to see you again!\n";
+    private static String WELCOME_TEXT = "Hey there I'm Duke!\n" + "How can I help you today?";
+    private static String BYE_TEXT = "Bye! Hope to see you again!";
 
+    private static void printMessage(String string) {
+        System.out.print("------------------------------------------------\n" + string + "\n"
+                + "------------------------------------------------\n\n");
+    }
 
     public static void main(String[] args) {
-        System.out.print(welcomeText);
+        printMessage(WELCOME_TEXT);
 
         Scanner in = new Scanner(System.in);
         List<String> inputList = new ArrayList<>();
 
         while (true) {
+            System.out.print("> ");
             String input = in.nextLine();
 
             if (input.equals("bye")) {
-                System.out.print(byeText);
+                printMessage(BYE_TEXT);
                 break;
             } else if (input.equals("list")) {
+                StringBuilder builder = new StringBuilder();
+
                 for (int i = 0; i < inputList.size(); i++) {
                     String item = inputList.get(i);
-                    System.out.println((i + 1) + ". " + item);
+                    builder.append(i + 1);
+                    builder.append(". ");
+                    builder.append(item);
+                    if (i < inputList.size() - 1) {
+                        builder.append("\n");
+                    }
                 }
+
+                printMessage(builder.toString());
             } else {
-                System.out.println(input);
+                printMessage(input);
                 inputList.add(input);
             }
         }
+
+        in.close();
     }
 }
