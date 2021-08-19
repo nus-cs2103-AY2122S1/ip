642214e9e038417d1c8eb5e2d94bd83d56e24201
bklimey <bklim@comp.nus.edu.sg>
2021-08-19 22:04:04 +0800

    cleaning up of code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,55 +16,48 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
 
-        // Commands constants
-        final String bye = "bye";
-        final String list = "list";
-        final String done = "done";
-        final String todo = "todo";
-        final String deadline = "deadline";
-        final String event = "event";
-
         List<Task> userList = new ArrayList<>();
 
         while (true) {
             String inp = sc.nextLine();
-            String[] parts = inp.split(" ");
             if (inp.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (inp.equals("list")) {
+            } else if (inp.equals("list")) { //check if command is "list"
                 int counter = 1;
                 for (Task task:userList) {
                     System.out.println(counter + "." + task);
-                    //System.out.println(counter + ".[" + task.getStatusIcon() + "] " + task);
                     counter++;
                 }
-            } else if (inp.startsWith("done ")) {
-                //parts.length == 2 &&
-                // Need to check for invalid input
+            } else if (inp.startsWith("done ")) { //check if command is "done"
                 int index = Integer.parseInt(inp.substring(5)) - 1;
                 Task task = userList.get(index);
                 task.markDone();
                 System.out.println("Nice! I've marked this task as done: ");
                 System.out.println(task);
-                // System.out.println("[" + task.getStatusIcon() + "] " + task);
-            } else if (inp.startsWith("todo ")) { //todo command
+            } else if (inp.startsWith("todo ")) { //check if command is "todo"
                 if (inp.length() == 5) {
                     System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
                 }
                 Task task = new Todo(inp.substring(5));
                 userList.add(task);
-                System.out.println("added: " + task);
-            } else if (inp.startsWith("deadline ")) { //deadline command
+                System.out.println("Got it. I've added this task: ");
+                System.out.println(task);
+                System.out.println("Now you have " + userList.size() + " tasks in the list.");
+            } else if (inp.startsWith("deadline ")) { //check if command is "deadline"
                 int index = inp.indexOf("/by ");
                 Task task = new Deadline(inp.substring(9, index), inp.substring(index + 4));
                 userList.add(task);
-                System.out.println("added: " + task);
-            } else if (inp.startsWith("event ")) { //event command
+                System.out.println("Got it. I've added this task: ");
+                System.out.println(task);
+                System.out.println("Now you have " + userList.size() + " tasks in the list.");
+            } else if (inp.startsWith("event ")) { //check if command is "event"
                 int index = inp.indexOf("/at "); // find index of "/at "
                 Task task = new Event(inp.substring(6, index), inp.substring(index + 4));
                 userList.add(task);
-                System.out.println("added: " + task);
+                System.out.println("Got it. I've added this task: ");
+                System.out.println(task);
+                System.out.println("Now you have " + userList.size() + " tasks in the list.");
             } else {
                 // Invalid input
                 System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
