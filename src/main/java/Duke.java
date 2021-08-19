7dad589eaf27d7b0b857ed138d92b92d48cc96bd
ErnestCuong <hltc11032000@gmail.com>
2021-08-19 19:53:12 +0800

    Add Level 5 and Update EXPECTED.TXT

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,64 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static boolean isInt(String str) { // check if a string is integer
-        if (str == null) {
-            return false;
+    static Task[] tasks = new Task[100];
+    static int numOfTasks = 0;
+
+    public static void markAsDone(String s) throws DukeException {
+        try {
+            int taskNum = Integer.parseInt(s.substring(5));
+            Task curr = tasks[taskNum - 1];
+            curr.markAsDone();
+            System.out.println("Nice! I've marked this task as done:\n  "
+                    + curr.toString());
+        } catch (NumberFormatException nfe) {
+            throw new DukeException("Please only enter an integer after command 'done'!");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException("Task number " + s.substring(5) + " does not exist!");
+        } catch (NullPointerException npe) {
+            throw new DukeException("Task number " + s.substring(5) + " does not exist!");
         }
-        int length = str.length();
-        if (length == 0) {
-            return false;
+    }
+
+    public static void addToDo(String s) throws DukeException {
+        try {
+            Task curr = new ToDo(s.substring(5));
+            tasks[numOfTasks] = curr;
+            numOfTasks += 1;
+            System.out.println("Got it. I've added this task:\n  "
+                    + curr.toString() + "\nNow you have " + numOfTasks
+                    + " task(s) in the list.");
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("The description of a todo cannot be empty.");
         }
-        int i = 0;
-        if (str.charAt(0) == '-') {
-            if (length == 1) {
-                return false;
-            }
-            i = 1;
+    }
+
+    public static void addEvent(String s) throws DukeException {
+        try {
+            int at = s.lastIndexOf(" /at ");
+            Task curr = new Event(s.substring(6, at), s.substring(at + 5));
+            tasks[numOfTasks] = curr;
+            numOfTasks += 1;
+            System.out.println("Got it. I've added this task:\n  "
+                    + curr.toString() + "\nNow you have " + numOfTasks
+                    + " task(s) in the list.");
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("The description and time of an event cannot be empty.");
         }
-        for (; i < length; i++) {
-            char c = str.charAt(i);
-            if (c < '0' || c > '9') {
-                return false;
-            }
+    }
+
+    public static void addDeadline(String s) throws DukeException {
+        try {
+            int by = s.lastIndexOf(" /by ");
+            Task curr = new Deadline(s.substring(9, by), s.substring(by + 5));
+            tasks[numOfTasks] = curr;
+            numOfTasks += 1;
+            System.out.println("Got it. I've added this task:\n  "
+                    + curr.toString() + "\nNow you have " + numOfTasks
+                    + " task(s) in the list.");
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("The description and time of a deadline cannot be empty.");
         }
-        return true;
     }
 
     public static void main(String[] args) {
@@ -32,83 +68,35 @@ public class Duke {
 
         Scanner myScanner= new Scanner(System.in); // open scanner
         boolean hasQuit = false;
-        Task[] tasks = new Task[100];
-        int numOfTasks = 0;
 
         while (!hasQuit && myScanner.hasNextLine()) {
             String userInput = myScanner.nextLine();
 
-            if (userInput.equals("bye")) { // if user enters "bye"
-                hasQuit = true;
-                System.out.println("Bye. Hope to see you again soon!");
-            } else if (userInput.equals("list")) { // if user enters "list"
-                for (int i = 0; i < numOfTasks; i++) {
-                    Task curr = tasks[i];
-                    int taskNum = i + 1;
-                    System.out.println(taskNum + "." + curr.toString());
+            try {
+                if (userInput.equals("bye")) { // if user enters "bye"
+                    hasQuit = true;
+                    System.out.println("Bye. Hope to see you again soon!");
+                } else if (userInput.equals("list")) { // if user enters "list"
+                    for (int i = 0; i < numOfTasks; i++) {
+                        Task curr = tasks[i];
+                        int taskNum = i + 1;
+                        System.out.println(taskNum + "." + curr.toString());
+                    }
+                } else if (userInput.startsWith("done")) {
+                    markAsDone(userInput);
+                } else if (userInput.startsWith("todo")) {
+                    addToDo(userInput);
+                } else if (userInput.startsWith("event")) {
+                    addEvent(userInput);
+                } else if (userInput.startsWith("deadline")) {
+                    addDeadline(userInput);
+                } else { // any other input from user
+                    throw new DukeException("I'm sorry, but I don't know what that means :-(");
                 }
-            } else if (userInput.length() > 5
-                    && userInput.substring(0, 4).equals("done")
-                    && isInt(userInput.substring(5))
-            ) { // if user wants to mark a task as done
-                int taskNum = Integer.parseInt(userInput.substring(5));
-
-                if (taskNum > numOfTasks || taskNum < 1) {
-                    System.out.println("Wrong input!");
-                } else {
-                    Task curr = tasks[taskNum - 1];
-                    curr.markAsDone();
-                    System.out.println("Nice! I've marked this task as done:\n  "
-                            + curr.toString());
-                }
-            } else if (userInput.length() > 5
-                    && userInput.substring(0, 5).equals("todo ")
-            ) {
-                Task curr = new ToDo(userInput.substring(5));
-                tasks[numOfTasks] = curr;
-                numOfTasks += 1;
-                System.out.println("Got it. I've added this task:\n  "
-                        + curr.toString() + "\nNow you have " + numOfTasks
-                        + " task(s) in the list.");
-            } else if (userInput.length() >= 13
-                    && userInput.substring(0, 6).equals("event ")
-            ) {
-                int stopAt = 7;
-                while (stopAt + 5 < userInput.length() && !userInput.substring(stopAt, stopAt + 5).equals(" /at ")) {
-                    stopAt += 1;
-                }
-
-                if (!userInput.substring(stopAt, stopAt + 5).equals(" /at ")) {
-                    System.out.println("Wrong input!");
-                } else {
-                    Task curr = new Event(userInput.substring(6, stopAt), userInput.substring(stopAt + 5));
-                    tasks[numOfTasks] = curr;
-                    numOfTasks += 1;
-                    System.out.println("Got it. I've added this task:\n  "
-                            + curr.toString() + "\nNow you have " + numOfTasks
-                            + " task(s) in the list.");
-                }
-            } else if (userInput.length() >= 16
-                    && userInput.substring(0, 9).equals("deadline ")
-            ) {
-                int stopAt = 10;
-                while (stopAt + 5 < userInput.length() && !userInput.substring(stopAt, stopAt + 5).equals(" /by ")) {
-                    stopAt += 1;
-                }
-
-                if (!userInput.substring(stopAt, stopAt + 5).equals(" /by ")) {
-                    System.out.println("Wrong input!");
-                } else {
-                    Task curr = new Deadline(userInput.substring(9, stopAt), userInput.substring(stopAt + 5));
-                    tasks[numOfTasks] = curr;
-                    numOfTasks += 1;
-                    System.out.println("Got it. I've added this task:\n  "
-                            + curr.toString() + "\nNow you have " + numOfTasks
-                            + " task(s) in the list.");
-                }
-            } else { // any other input from user
-                System.out.println("Wrong input");
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
+
             System.out.println("*******************************************");
         }
         myScanner.close(); // close scanner
