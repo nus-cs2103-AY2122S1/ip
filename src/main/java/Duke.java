908b40c8d36bb148b69cfdea475ae831e0585c6f
Yao Yi <yaoyi2000326@gmail.com>
2021-08-19 11:23:12 +0800

    Level-3 task done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,10 +2,9 @@ import java.util.*;
 
 public class Duke {
 
-
     public static void main(String[] args) {
         boolean running = true;
-        String horizontal = "____________________\n";
+        String horizontal = "_______________________";
         String exitText = horizontal + "Bye!\n" + horizontal;
         String logo =
               "                     _               _         \n" +
@@ -18,25 +17,37 @@ public class Duke {
                       "                                         |___/ ";
 
         System.out.println("Hello from\n" + logo);
-        List<String> stored = new ArrayList<>();
+        List<Task> stored = new ArrayList<>();
 
 
         Scanner sc = new Scanner(System.in);
         while (running) {
             String input = sc.nextLine();
+            System.out.println(horizontal);
+
             if (input.equals("bye")) {
+                System.out.println("Byebye ~ nya");
                 running = false;
             } else if (input.equals("list")){
+                System.out.println("Here are your tasks ~ OwO");
                 for (int i = 0; i < stored.size(); i++){
-                    System.out.println((i+1) + ". " + stored.get(i));
+                    System.out.println((i+1) + ". " + stored.get(i).toString());
                 }
+            } else if (input.startsWith("done")) {
+                Scanner s = new Scanner(input);
+                s.next(); //jump 'done'
+                int num = s.nextInt();
+                stored.get(num-1).markAsDone();
             } else {
-                stored.add(input);
-                String wrapped = horizontal + "added: " + input + "\n" + horizontal;
-                System.out.println(wrapped);
+                stored.add(new Task(input));
+                System.out.println("added: " + input);
             }
+            System.out.println(horizontal);
+
+
         }
+
         sc.close();
-        System.out.println(exitText);
+        //System.out.println(exitText);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println("Yayyyy done yayyy~~");
+        System.out.println(toString());
+    }
+
+    @Override
+    public String toString() {
+        return ("[" + getStatusIcon() + "] "+ this.description);
+    }
+
+}
