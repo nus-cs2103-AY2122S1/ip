0a682a89904660ab160f8bb54b787e0b74df6f05
Jonathan Tan Xin Jie <e0543805@u.nus.edu>
2021-08-19 22:14:01 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,37 +1,20 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        boolean away = false;
-        int listLen = 0;
-        Task[] list = new Task[100];
-        String bar = " -------------------------------------------------------------";
-        System.out.println(bar + "\n    Hello! I'm SaDOS\n" +
-                "    What can I do for you?\n" +
-                "    Send \"bye\" to exit,\n" +
-                "    I won't hold it against you\n" +
-                "    (Mark tasks as done by sending \"/done x\", where x is item number)\n" +
-                bar);
-        Scanner sc = new Scanner(System.in);
-        do {
-            String str = sc.nextLine();
-            if (str.equalsIgnoreCase("bye")) {
-                away = true;
-                System.out.println(bar + "\n    Oh you've got to go?\n" +
-                        "    Alright, I'll just be here...\n" +
-                        "    Waiting....\n" +
-                        "    You'll be back right?\n" +
-                        bar);
-            } else if (str.equalsIgnoreCase("list")) {
-                System.out.println(bar);
-                for (int i = 1; i <= listLen; i++) {
-                    System.out.println("    " + i + "." + list[i - 1].toString());
-                }
-                System.out.println(bar);
-            } else{
-                if (str.indexOf(' ') > 0) { // checking if command given was >= two words
-                    String[] splitted = str.split(" ", 2);
-                    str = splitted[1];
+    static String bar = " -------------------------------------------------------------";
+    int listLen = 0;
+    Task[] list = new Task[100];
+    public void updateList (String input) {
+        if (input.equalsIgnoreCase("list")) {
+            System.out.println(bar);
+            for (int i = 1; i <= listLen; i++) {
+                System.out.println("    " + i + "." + list[i - 1].toString());
+            }
+            System.out.println(bar);
+        } else{
+                if (input.indexOf(' ') > 0) { // checking if command given was >= two words
+                    String[] splitted = input.split(" ", 2);
+                    input = splitted[1];
                     if (splitted[0].equalsIgnoreCase("/done") && splitted[1] != null) { // done-ing item
                         int num = Integer.parseInt(splitted[1]);
                         if (0 < num && num <= listLen) {
@@ -41,18 +24,18 @@ public class Duke {
                             System.out.println(bar + "\n    Item number not present. Try again?\n" + bar);
                         }
                     } else if (splitted[0].equalsIgnoreCase("todo")) { // adding todo
-                        if (str.isEmpty()) {
-                            System.out.println(bar + "\n    Task name not provided. FORMAT: \" TODO TASKNAME\"\n" + bar);
+                        if (input.isEmpty()) {
+                            System.out.println(bar + "\n    Task name not provided\n" + "    FORMAT: \" TODO TASKNAME\"\n" + bar);
                         } else {
-                            list[listLen] = new Todo(str);
+                            list[listLen] = new Todo(input);
                             listLen++;
-                            System.out.println(bar + "\n    added: " + str + "\n    Now you have " + listLen + " tasks in your list\n" +
+                            System.out.println(bar + "\n    added: " + input + "\n    Now you have " + listLen + " tasks in your list\n" +
                                     bar);
                         }
                     } else if (splitted[0].equalsIgnoreCase("deadline")) { // adding deadline
-                        String[] deadlineSplit = str.split("(?i) /by ");
+                        String[] deadlineSplit = input.split("(?i) /by ");
                         if (deadlineSplit.length == 1) {
-                            System.out.println(bar + "\n    Task name or deadline not provided. FORMAT: \" DEADLINE TASKNAME /by DEADLINE\"\n" + bar);
+                            System.out.println(bar + "\n    Task name or deadline not provided.\n" + "    FORMAT: \" DEADLINE TASKNAME /by DEADLINE\"\n" + bar);
                         } else {
                             list[listLen] = new Deadline(deadlineSplit[0], deadlineSplit[1]);
                             listLen++;
@@ -60,9 +43,9 @@ public class Duke {
                                     bar);
                         }
                     } else if (splitted[0].equalsIgnoreCase("event")) { // adding event
-                        String[] eventSplit = str.split("(?i) /at ");
+                        String[] eventSplit = input.split("(?i) /at ");
                         if (eventSplit.length == 1) {
-                            System.out.println(bar + "\n    Event name or date not provided. FORMAT: \" EVENT TASKNAME /at DATE\"\n" + bar);
+                            System.out.println(bar + "\n    Event name or date not provided.\n" + "    FORMAT: \" EVENT TASKNAME /at DATE\"\n" + bar);
                         } else {
                             list[listLen] = new Event(eventSplit[0], eventSplit[1]);
                             listLen++;
@@ -71,27 +54,50 @@ public class Duke {
                         }
                     }
                     else { // no task type provided
-                        System.out.println(bar + "\n    You have not specified the type of task this is! Type \"help\" for help\n" + bar);
+                        System.out.println(bar + "\n    Task type or task name not specified!\n" + "    Type \"help\" for help\n" + bar);
                     }
                 }
                 else { // only one word
-                    if (str.equalsIgnoreCase("help")) {
+                    if (input.equalsIgnoreCase("help")) {
                         System.out.println(bar + "\n    ToDos: tasks without any date/time attached to it e.g., visit new theme park\n" +
                                 "      FORMAT: todo TASKNAME\n" +
                                 "    Deadlines: tasks that need to be done before a specific date/time e.g., submit report by 11/10/2019 5pm\n" +
                                 "      FORMAT: deadline TASKNAME /by DEADLINE\n" +
                                 "    Events: tasks that start at a specific time and ends at a specific time e.g., team project meeting on 2/10/2019 2-4pm\n" +
                                 "      FORMAT: todo TASKNAME /by DATE\n" +
-                                "    \"/done x\" where x is the task number to mark task as done.\n" + bar);
+                                "    \"/done x\" where x is the task number to mark task as done.\n" +
+                                "    \"/delete x\" where x is the task number to be deleted. \n"+ bar);
                     } else {
-                        System.out.println(bar + "\n    You have not specified the type of task this is! Type \"help\" for help\n" + bar);
+                        System.out.println(bar + "\n    Task type or task name not specified!\n" + "    Type \"help\" for help\n" + bar);
                     }
                 }
             }
         }
-        while (!away);
+    public static void main(String[] args) {
+        Duke currentList = new Duke();
+        boolean away = false;
+        System.out.println(bar + "\n    Hello! I'm SaDOS\n" +
+                "    What can I do for you?\n" +
+                "    Send \"bye\" to exit,\n" +
+                "    I won't hold it against you\n" +
+                "    Send \"help\" for help!\n" +
+                bar);
+        Scanner sc = new Scanner(System.in);
+        do {
+            String str = sc.nextLine();
+            if (str.equalsIgnoreCase("bye")) {
+                System.out.println(bar + "\n    Oh you've got to go?\n" +
+                        "    Alright, I'll just be here...\n" +
+                        "    Waiting....\n" +
+                        "    You'll be back right?\n" +
+                        bar);
+                away = true;
+            } else {
+                currentList.updateList(str);
+            }
+        } while (!away);
     }
-    public static class Task {
+    public class Task {
         protected String description;
         protected boolean isDone;
 
@@ -108,7 +114,7 @@ public class Duke {
             isDone = true;
         }
     }
-    public static class Deadline extends Task {
+    public class Deadline extends Task {
 
         protected String by;
 
@@ -122,7 +128,7 @@ public class Duke {
             return "[D]" + super.toString() + " (by: " + by + ")";
         }
     }
-    public static class Todo extends Task {
+    public class Todo extends Task {
 
         public Todo(String description) {
             super(description);
@@ -133,7 +139,7 @@ public class Duke {
             return "[T]" + super.toString();
         }
     }
-    public static class Event extends Task {
+    public class Event extends Task {
 
         protected String at;
 
@@ -146,5 +152,6 @@ public class Duke {
         public String toString() {
             return "[E]" + super.toString() + " (at: " + at + ")";
         }
+
     }
 }
