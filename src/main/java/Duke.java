4993b9168bebc113a503d73c7b1cf38c4626ffc8
Gabriel <77230723+gycgabriel@users.noreply.github.com>
2021-08-19 11:18:09 +0800

    Add custom exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,14 @@ import java.util.function.UnaryOperator;
 
 public class Duke {
 
+    final static String BOX_LINE = "__________________________________________________________________________________\n";
+    final static String BOX_MIDDLE = "|  Cat:  ";
+
+    public static String Box(String message) {
+        return Duke.BOX_LINE + Duke.BOX_MIDDLE + message + "\n" + Duke.BOX_LINE;
+    }
+
+
     public static void main(String[] args) {
         // Credits to https://manytools.org/hacker-tools/convert-images-to-ascii-art/go/ and Mafumafu Line stickers
         String LOGO = "" +
@@ -39,21 +47,22 @@ public class Duke {
                 //"                            ...,,,,(@%#&@@@@@@@@@*.,//(@@@@@@@@@@&@@/.,.....                        \n" +
                 //"                            ...,,,.@@@@@@@@@@@@@@/&%%(*@@@@@@@@@@@&@%#&*....                        \n" +
                 "\n";
-        String BOX_LINE = "____________________________________________________________\n";
-        String BOX_MIDDLE = "|  Cat:  ";
-        String GREET = BOX_LINE + BOX_MIDDLE + "Meow-ning!\n" + BOX_LINE;
-        String EXIT = BOX_LINE + BOX_MIDDLE + "See you again, meow!\n" + BOX_LINE;
+        String GREET = Box("Meow-ning!");
+        String EXIT = Box("See you again, meow!");
+
+        /*
         String TASK_NOT_EXIST = BOX_LINE + BOX_MIDDLE + "Meow? I can't find that task...\n" + BOX_LINE;
-        String LIST = BOX_LINE + BOX_MIDDLE + "Here are the tasks in your list, meow:";
         String DEADLINE_NO_BY = BOX_LINE + BOX_MIDDLE + "Meow? There's no deadline... Please use /by params. \n" + BOX_LINE;
         String EVENT_NO_AT = BOX_LINE + BOX_MIDDLE + "Meow? There's no date... Please use /at params. \n" + BOX_LINE;
         String NOTHING_AFT_CMD = BOX_LINE + BOX_MIDDLE + "Meow? There's nothing after your command... Meow meow meow?\n" + BOX_LINE;
+        */
 
-        BinaryOperator<String> AddTaskMessage = (x, y) -> BOX_LINE + BOX_MIDDLE + "Meow. I've added this task:\n   "
-                + x + "\n" + "|  Now you have " + y + " tasks in the list." + "\n" + BOX_LINE;
-        UnaryOperator<String> DoneTaskMessage =  (task) -> BOX_LINE + BOX_MIDDLE +
-                "Good job, meow! Marked this task as done:\n   " + task + "\n" + BOX_LINE;
-        UnaryOperator<String> EchoMessage = (msg) -> BOX_LINE + BOX_MIDDLE + msg + ", meow!\n" + BOX_LINE;
+        BinaryOperator<String> AddTaskMessage = (x, y) -> Box("Meow. I've added this task:\n   " +
+                x + "\n" + "|  Now you have " + y + " tasks in the list.");
+        UnaryOperator<String> DoneTaskMessage =  (task) -> Box("Good job, meow! Marked this task as done:\n   " +
+                task);
+        UnaryOperator<String> ListMessage = (items) -> Box("Here are the tasks in your list, meow:" + items);
+        UnaryOperator<String> EchoMessage = (msg) -> Box(msg + ", meow? Not a command...");
 
         ArrayList<Task> taskList = new ArrayList<>();
 
@@ -66,97 +75,98 @@ public class Duke {
 
         while (!input.equalsIgnoreCase("bye")) {
 
-            String[] inputArr = input.trim().split("\\s+",2);
+            String[] inputArr = input.trim().split("\\s+", 2);
             String command = inputArr[0].toUpperCase();
 
-            switch (command) {
-                case "LIST":
-                    System.out.println(LIST);
-                    for (int i = 0; i < taskList.size(); i++) {
-                        System.out.println("   " + (i + 1) + ". " + taskList.get(i));
-                    }
-                    System.out.println(BOX_LINE);
-                    break;
+            try {
+                switch (command) {
+                    case "LIST":
+                        String s = "";
+                        for (int i = 0; i < taskList.size(); i++) {
+                            s += ("\n   " + (i + 1) + ". " + taskList.get(i));
+                        }
 
-                case "DONE":
-                    if (!input.matches("done [0-9]+")) {
-                        System.out.println(TASK_NOT_EXIST);
+                        System.out.println(ListMessage.apply(s));
                         break;
-                    }
 
-                    int i = -1;
-                    try {
-                        i = Integer.parseInt(input.split(" ", 2)[1]) - 1;
-                    } catch (NumberFormatException e) {
-                        System.out.println(TASK_NOT_EXIST);
+                    case "DONE":
+                        if (!input.matches("done [0-9]+")) {
+                            throw new TaskIndexNotInteger(taskList.size());
+                        }
+
+                        int i = -1;
+                        try {
+                            i = Integer.parseInt(input.split(" ", 2)[1]) - 1;
+                        } catch (NumberFormatException e) {
+                            throw new TaskIndexNotInteger(taskList.size());
+                        }
+
+                        if (i >= 0 && i < taskList.size()) {
+                            Task t = taskList.get(i);
+                            t.markAsDone();
+                            System.out.println(DoneTaskMessage.apply(t.toString()));
+                        } else {
+                            throw new TaskIndexOutOfBounds(taskList.size());
+                        }
+
                         break;
-                    }
 
-                    if (i >= 0 && i < taskList.size()) {
-                        Task t = taskList.get(i);
-                        t.markAsDone();
-                        System.out.println(DoneTaskMessage.apply(t.toString()));
-                    } else {
-                        System.out.println(TASK_NOT_EXIST);
-                    }
-
-                    break;
-
-                case "TODO":
-                    if (inputArr.length < 2) {
-                        System.out.println(NOTHING_AFT_CMD);
+                    case "TODO":
+                        if (inputArr.length < 2) {
+                            throw new NothingAfterCommand(command);
+                        }
+                        Task todo = new Todo(inputArr[1]);
+                        taskList.add(todo);
+                        System.out.println(AddTaskMessage.apply(todo.toString(), Integer.toString(taskList.size())));
                         break;
-                    }
-                    Task todo = new Todo(inputArr[1]);
-                    taskList.add(todo);
-                    System.out.println(AddTaskMessage.apply(todo.toString(), Integer.toString(taskList.size())));
-                    break;
 
-                case "DEADLINE":
-                    if (inputArr.length < 2) {
-                        System.out.println(NOTHING_AFT_CMD);
+                    case "DEADLINE":
+                        if (inputArr.length < 2) {
+                            throw new NothingAfterCommand(command);
+                        }
+                        if (!inputArr[1].contains("/by")) {
+                            throw new MissingParams("by");
+                        }
+                        inputArr = inputArr[1].split(" /by ", 2);
+                        if (inputArr.length < 2) {
+                            throw new MissingArguments(command);
+                        }
+                        Task deadline = new Deadline(inputArr[0], inputArr[1]);
+                        taskList.add(deadline);
+                        System.out.println(AddTaskMessage.apply(deadline.toString(), Integer.toString(taskList.size())));
                         break;
-                    }
 
-                    inputArr = inputArr[1].split(" /by ", 2);
-                    if (inputArr.length < 2) {
-                        System.out.println(DEADLINE_NO_BY);
+                    case "EVENT":
+                        if (inputArr.length < 2) {
+                            throw new NothingAfterCommand(command);
+                        }
+                        if (!inputArr[1].contains("/at")) {
+                            throw new MissingParams("at");
+                        }
+                        inputArr = inputArr[1].split(" /at ", 2);
+                        if (inputArr.length < 2) {
+                            throw new MissingArguments(command);
+                        }
+                        Task event = new Event(inputArr[0], inputArr[1]);
+                        taskList.add(event);
+                        System.out.println(AddTaskMessage.apply(event.toString(), Integer.toString(taskList.size())));
                         break;
-                    }
-                    Task deadline = new Deadline(inputArr[0], inputArr[1]);
-                    taskList.add(deadline);
-                    System.out.println(AddTaskMessage.apply(deadline.toString(), Integer.toString(taskList.size())));
-                    break;
 
-                case "EVENT":
-                    if (inputArr.length < 2) {
-                        System.out.println(NOTHING_AFT_CMD);
+                    default:
+                        System.out.println(EchoMessage.apply(input));
                         break;
-                    }
+                }
 
-                    inputArr = inputArr[1].split(" /at ", 2);
-                    if (inputArr.length < 2) {
-                        System.out.println(EVENT_NO_AT);
-                        break;
-                    }
-                    Task event = new Event(inputArr[0], inputArr[1]);
-                    taskList.add(event);
-                    System.out.println(AddTaskMessage.apply(event.toString(), Integer.toString(taskList.size())));
-                    break;
-
-                default:
-                    System.out.println(EchoMessage.apply(input));
-                    break;
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                input = sc.nextLine();
             }
-            input = sc.nextLine();
 
         }
 
         System.out.println(EXIT);
 
         sc.close();
-
-
-
     }
 }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,49 @@
+
+public class DukeException extends Exception {
+
+    public DukeException(String message) {
+        super(message);
+    }
+
+}
+
+class TaskIndexNotInteger extends DukeException {
+
+    TaskIndexNotInteger(int length) {
+        super(Duke.Box("Meow? That's not an integer... Please enter a task number between 0 and " + length));
+    }
+
+}
+
+class TaskIndexOutOfBounds extends DukeException {
+
+    TaskIndexOutOfBounds(int length) {
+        super(Duke.Box("Meow? I can't find that task... Please enter a task number between 0 and " + length));
+    }
+
+}
+
+class MissingParams extends DukeException {
+
+    MissingParams(String params) {
+        super(Duke.Box("Meow? Missing params... Please use /" + params + " params."));
+    }
+
+}
+
+class NothingAfterCommand extends DukeException {
+
+    NothingAfterCommand(String command) {
+        super(Duke.Box("Meow? There's nothing after your command " + command + "... Meow meow meow?"));
+    }
+
+}
+
+class MissingArguments extends DukeException {
+
+    MissingArguments(String command) {
+        super(Duke.Box("Meow? There's missing arguments in your command " + command + "... Meow meow meow?"));
+    }
+
+}
+
