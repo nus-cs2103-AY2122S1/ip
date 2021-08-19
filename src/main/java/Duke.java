610f872917559294bd78c7a514a846d4c6fa4c1f
wpinrui <wongpinrui@gmail.com>
2021-08-19 09:53:37 +0800

    Reindent switch case to follow style guide

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -40,54 +40,54 @@ public class Duke {
         String[] strings = input.split(" ");
         String operation = strings[0];
         switch (operation) {
-            case "bye":
-            case "list":
-                return new Command(operation);
-            case "done":
-                if (strings.length == 1) {
-                    throw new DukeException("You need to indicate which task number should be marked as done.");
-                }
-                try {
-                    int doneTaskNum = Integer.parseInt(strings[1]);
-                    return new Command(strings[0], doneTaskNum);
-                } catch (NumberFormatException e) {
-                    throw new DukeException("The task to be marked as done should be indicated its list index.");
-                }
-            case "todo":
-                if (strings.length == 1) {
-                    throw new DukeException("The description of a todo cannot be empty.");
-                }
-                return new Command(strings[0], input.substring(5));
-            case "deadline":
-                if (strings.length == 1) {
-                    throw new DukeException("The description of a deadline cannot be empty.");
-                }
-                String[] descriptionAndBy = input.substring(9).split("/by ");
-                if (descriptionAndBy.length == 1) {
-                    throw new DukeException("The done-by date of a deadline cannot be empty.");
-                }
-                return new Command(strings[0], descriptionAndBy[0], descriptionAndBy[1]);
-            case "event":
-                if (strings.length == 1) {
-                    throw new DukeException("The description of an event cannot be empty.");
-                }
-                String[] descriptionAndAt = input.substring(6).split("/at ");
-                if (descriptionAndAt.length == 1) {
-                    throw new DukeException("The timing of an event cannot be empty.");
-                }
-                return new Command(strings[0], descriptionAndAt[0], descriptionAndAt[1]);
-            case "delete":
-                if (strings.length == 1) {
-                    throw new DukeException("You need to indicate which task number should be deleted.");
-                }
-                try {
-                    int delTaskNum = Integer.parseInt(strings[1]);
-                    return new Command(strings[0], delTaskNum);
-                } catch (NumberFormatException e) {
-                    throw new DukeException("The task to be deleted should be indicated its list index.");
-                }
-            default:
-                throw new DukeException("Sorry, I do not understand this command.");
+        case "bye":
+        case "list":
+            return new Command(operation);
+        case "done":
+            if (strings.length == 1) {
+                throw new DukeException("You need to indicate which task number should be marked as done.");
+            }
+            try {
+                int doneTaskNum = Integer.parseInt(strings[1]);
+                return new Command(strings[0], doneTaskNum);
+            } catch (NumberFormatException e) {
+                throw new DukeException("The task to be marked as done should be indicated its list index.");
+            }
+        case "todo":
+            if (strings.length == 1) {
+                throw new DukeException("The description of a todo cannot be empty.");
+            }
+            return new Command(strings[0], input.substring(5));
+        case "deadline":
+            if (strings.length == 1) {
+                throw new DukeException("The description of a deadline cannot be empty.");
+            }
+            String[] descriptionAndBy = input.substring(9).split("/by ");
+            if (descriptionAndBy.length == 1) {
+                throw new DukeException("The done-by date of a deadline cannot be empty.");
+            }
+            return new Command(strings[0], descriptionAndBy[0], descriptionAndBy[1]);
+        case "event":
+            if (strings.length == 1) {
+                throw new DukeException("The description of an event cannot be empty.");
+            }
+            String[] descriptionAndAt = input.substring(6).split("/at ");
+            if (descriptionAndAt.length == 1) {
+                throw new DukeException("The timing of an event cannot be empty.");
+            }
+            return new Command(strings[0], descriptionAndAt[0], descriptionAndAt[1]);
+        case "delete":
+            if (strings.length == 1) {
+                throw new DukeException("You need to indicate which task number should be deleted.");
+            }
+            try {
+                int delTaskNum = Integer.parseInt(strings[1]);
+                return new Command(strings[0], delTaskNum);
+            } catch (NumberFormatException e) {
+                throw new DukeException("The task to be deleted should be indicated its list index.");
+            }
+        default:
+            throw new DukeException("Sorry, I do not understand this command.");
         }
     }
 
@@ -105,37 +105,37 @@ public class Duke {
             }
             Task task;
             switch (command.getOperation()) {
-                case "bye":
-                    print(outroString);
-                    break label;
-                case "list":
-                    print(taskListString());
-                    break;
-                case "done":
-                    task = taskList.get(command.getIndex() - 1);
-                    task.setDone(true);
-                    print(doneString(task));
-                    break;
-                case "todo":
-                    task = new ToDo(command.getDescription());
-                    taskList.add(task);
-                    print(addedString(task));
-                    break;
-                case "deadline":
-                    task = new Deadline(command.getDescription(), command.getTime());
-                    taskList.add(task);
-                    print(addedString(task));
-                    break;
-                case "event":
-                    task = new Event(command.getDescription(), command.getTime());
-                    taskList.add(task);
-                    print(addedString(task));
-                    break;
-                case "delete":
-                    task = taskList.get(command.getIndex() - 1);
-                    taskList.remove(command.getIndex() - 1);
-                    print(deletedString(task));
-                    break;
+            case "bye":
+                print(outroString);
+                break label;
+            case "list":
+                print(taskListString());
+                break;
+            case "done":
+                task = taskList.get(command.getIndex() - 1);
+                task.setDone(true);
+                print(doneString(task));
+                break;
+            case "todo":
+                task = new ToDo(command.getDescription());
+                taskList.add(task);
+                print(addedString(task));
+                break;
+            case "deadline":
+                task = new Deadline(command.getDescription(), command.getTime());
+                taskList.add(task);
+                print(addedString(task));
+                break;
+            case "event":
+                task = new Event(command.getDescription(), command.getTime());
+                taskList.add(task);
+                print(addedString(task));
+                break;
+            case "delete":
+                task = taskList.get(command.getIndex() - 1);
+                taskList.remove(command.getIndex() - 1);
+                print(deletedString(task));
+                break;
             }
         }
     }
