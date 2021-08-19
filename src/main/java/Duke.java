a320dbde3c3a73e617cda1f30adbac54b97b64e4
Kimowarui <zhangyubin83567593@gmail.com>
2021-08-19 12:02:07 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import com.sun.source.tree.DoWhileLoopTree;
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +9,18 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤\n";
+        System.out.println("Hello from\n" + logo + line + "What can I do for you?\n" + line);
+
+        String input;
+        Scanner s = new Scanner(System.in);
+        input = s.nextLine();
+
+        while(!input.equals("bye")) {
+            System.out.println(line + input + "\n" + line);
+            input = s.nextLine();
+        }
+
+        System.out.println(line + "Bye. Hope to see you again soon!\n" + line);
     }
 }
