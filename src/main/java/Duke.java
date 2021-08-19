6705c1623d5b0f519ee23fc07256adb78ae04bc0
itzblim <>
2021-08-19 11:30:06 +0800

    Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,31 +17,51 @@ public class Duke {
         boolean exit = false;
 
         while(!exit) {
-            String nextInput = sc.nextLine();
-            if (nextInput.equals("bye")) {
-                printMsg("Bye mate!");
-                exit = true;
-            } else if (nextInput.equals("list")){
-                printTasks();
-            } else if (nextInput.startsWith("done")) {
-                printMsg(Task.markTask(Integer.parseInt(nextInput.substring(5)) - 1));
-            } else if (nextInput.startsWith("todo")) {
-                printMsg(Task.addTask(new Todo(nextInput.substring(5))));
-            } else if (nextInput.startsWith("deadline")) {
-                int byIndex = nextInput.indexOf("/by");
-                printMsg(Task.addTask(new Deadline(
-                        nextInput.substring(9, byIndex - 1),
-                        nextInput.substring(byIndex + 4)
-                )));
-            } else if (nextInput.startsWith("event")) {
-                int atIndex = nextInput.indexOf("/at");
-                printMsg(Task.addTask(new Event(
-                        nextInput.substring(6, atIndex - 1),
-                        nextInput.substring(atIndex + 4)
-                )));
-            } else {
-                printMsg(Task.addTask(new Task(nextInput)));
+            try {
+                String nextInput = sc.nextLine();
+                if (nextInput.equals("bye")) {
+                    printMsg("Bye mate!");
+                    exit = true;
+                } else if (nextInput.equals("list")){
+                    printTasks();
+                } else if (nextInput.startsWith("done ")) {
+                    printMsg(Task.markTask(Integer.parseInt(nextInput.substring(5)) - 1));
+                } else if (nextInput.startsWith("todo")) {
+                    if (nextInput.length() < 6) {
+                        throw new DukeException("The description of a todo cannot be empty.");
+                    }
+                    printMsg(Task.addTask(new Todo(nextInput.substring(5))));
+                } else if (nextInput.startsWith("deadline")) {
+                    if (nextInput.length() < 10) {
+                        throw new DukeException("The description of a deadline cannot be empty.");
+                    }
+                    int byIndex = nextInput.indexOf("/by");
+                    if (byIndex == -1) {
+                        throw new DukeException("/by not found.");
+                    }
+                    printMsg(Task.addTask(new Deadline(
+                            nextInput.substring(9, byIndex - 1),
+                            nextInput.substring(byIndex + 4)
+                    )));
+                } else if (nextInput.startsWith("event")) {
+                    if (nextInput.length() < 7) {
+                        throw new DukeException("The description of an event cannot be empty.");
+                    }
+                    int atIndex = nextInput.indexOf("/at");
+                    if (atIndex == -1) {
+                        throw new DukeException("/at not found.");
+                    }
+                    printMsg(Task.addTask(new Event(
+                            nextInput.substring(6, atIndex - 1),
+                            nextInput.substring(atIndex + 4)
+                    )));
+                } else {
+                    throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                }
+            } catch(DukeException e) {
+                printMsg(e.getMessage());
             }
+
         }
     }
 
