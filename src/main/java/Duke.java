25f964f5c5ad1a86df72ddec5c5f9a9a0d34bfaa
Winston Cahya <winstoncahya90@gmail.com>
2021-08-19 15:26:48 +0800

    Level- 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 public class Duke {
+
+    public static void talk() {
+        String userInput = "";
+        System.out.println("Hello, What Can I do for you ?");
+        while(!userInput.equals("bye")) {
+            Scanner scanner = new Scanner(System.in);
+            userInput = scanner.next();
+            if(userInput.equals("bye")) {
+                System.out.println("Bye, Hope to see you again soon !");
+            }
+            System.out.println("Duke : " + userInput);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +21,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Duke.talk();
     }
 }
