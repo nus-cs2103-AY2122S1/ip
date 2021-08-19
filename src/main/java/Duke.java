8d60add1748da32ca587d642392d8b1d01da2b11
clementkfj <clement.kfj@gmail.com>
2021-08-19 12:46:25 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,20 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    private ArrayList<String> xs = new ArrayList<>();
+
     public static String lineProducer() {
         return "   -----------------------------------------";
     }
+
     public static String indentationAdder() {
         return "\n    ";
     }
+
     public static void main(String[] args) {
+        Duke duke = new Duke();
         Scanner sc = new Scanner(System.in);
         System.out.println(lineProducer() + indentationAdder() + "Hello I'm your friendly chatbot Duke!" +
                 indentationAdder() + "What can I help you with?\n" + lineProducer() );
@@ -16,8 +23,16 @@ public class Duke {
             if (str.equals("bye")) {
                 System.out.println(lineProducer() + indentationAdder() + "Bye! Please visit me again!\n" + lineProducer());
                 break;
+            } else if (str.equals("list")) {
+                System.out.println(lineProducer());
+                for (int i = 0; i < duke.xs.size(); i++) {
+                    System.out.println("    " + (i + 1) + ": " + duke.xs.get(i));
+                }
+                System.out.println(lineProducer());
+                continue;
             }
-            System.out.println(lineProducer() + indentationAdder() + str + "\n" + lineProducer());
+            duke.xs.add(str);
+            System.out.println(lineProducer() + indentationAdder() + "added: " + str + "\n" + lineProducer());
         }
 
         sc.close();
