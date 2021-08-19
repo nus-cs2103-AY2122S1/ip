0e47d6303eda1ae77da8362bc4dfbbb7eeae4c34
YaleChen299 <proverb299@gmail.com>
2021-08-19 08:32:04 +0800

    Complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            handleInput(sc.nextLine());
+        }
+    }
+
+    private static void greet() {
+        String logo =
+                " _   _ _ _\n"+
+                "| | | (_) | _____\n"+
+                "| |_| | | |/ / _ \\\n"+
+                "|  _  | |   < (_) |\n"+
+                "|_| |_|_|_|\\_\\___/\n";
+
+        hikoPrint("Hello from\n" + logo + "What can I do for you?\n");
+    }
+
+    private static void hikoPrint(String str) {
+        System.out.print(ANSI_PURPLE + str + ANSI_RESET + "\n> ");
+    }
+
+    private static void handleInput(String input) {
+        switch (input) {
+            case "bye":
+                handleBye();
+                break;
+            default:
+                hikoPrint(input);
+        }
+    }
+
+    private static void handleBye() {
+        System.out.println(ANSI_PURPLE + "Bye! Hope to see you again soon!" + ANSI_RESET);
+        System.exit(0);
     }
 }
