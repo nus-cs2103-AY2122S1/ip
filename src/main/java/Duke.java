84d0e79654a050b9fee66db4bcb13af369861a37
WuaaAj <e0492455@u.nus.edu>
2021-08-19 20:19:47 +0800

    Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        //Greeting
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("\n\n");
+        System.out.println("Hello! I'm Duke.\n");
+        System.out.println("What can I do for you?");
+        System.out.println("\n");
+        System.out.println("____________________________________________________________");
+        System.out.println("\n");
     }
 }
