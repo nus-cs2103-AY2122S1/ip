ad1bb826969d80c020d44dd7f465ef7424077c01
Kangxun <e0543787@u.nus.edu>
2021-08-19 16:28:07 +0800

    Redo Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String line = "===========================";
+        System.out.println(line);
+        System.out.println("hello! i'm calico ☺");
+        System.out.println("how can i assist you?");
+        System.out.println(line);
+
+        // Create a scanner to read from standard input.
+        Scanner sc = new Scanner(System.in);
+        String cmd = sc.nextLine();
+        while (!cmd.equals("bye")) {
+            System.out.println(line);
+            System.out.println(cmd);
+            System.out.println(line);
+            cmd = sc.nextLine();
+        }
+
+        System.out.println(line);
+        System.out.println("bye friend!");
+        System.out.println(line);
     }
 }
