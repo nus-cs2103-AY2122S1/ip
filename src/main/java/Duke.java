b23decdc16fc426b11ac3c5f21704ee9180da0d6
wengYing227 <e0492479@u.nus.edu>
2021-08-19 21:10:10 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static String[] list = new String[100];
+    private static Task[] tasks = new Task[100];
     private static int len = 0;
-    public static void pack(String string) {
+
+    public static void pack(String string) { // format the output
         System.out.println("---------------------------------------------");
         System.out.println(string);
         System.out.println("---------------------------------------------");
@@ -20,29 +21,45 @@ public class Duke {
         do{
             s = scan.nextLine();
             if(s.equals("bye")) break;
-            if(s.equals("List")) {
+            if(s.equals("List") || s.equals("list")) {
                 readList();
                 continue;
+            }
+            if(s.length() > 4 && s.substring(0,4).equals("done")) {
+                char temp = s.charAt(5);
+                if(Character.isDigit(temp)) {
+                    int item = Integer.parseInt(s.substring(5, 6));
+                    done(tasks[item-1]);
+                    continue;
+                }
             }
             addList(s);
         } while(scan.hasNextLine());
     }
 
     public static void addList(String item) {
-        list[len] = item;
+        tasks[len] = new Task(item);
         len++;
         pack("added: " + item);
     }
 
     public static void readList(){
-        String items = "";
+        String out = "";
         for(int i = 1; i < len; i++) {
-            items += i + ". " + list[i-1] + "\n";
+            out += i + ".[" + tasks[i-1].getStatusIcon() + "] " + tasks[i-1].getDescription() + "\n";
         }
         if(len > 0) {
-            items += len + ". " + list[len-1];
+            out += len + ".[" + tasks[len-1].getStatusIcon() + "] " + tasks[len-1].getDescription();
         }
-        pack(items);
+
+        pack(out);
+    }
+
+    public static void done(Task task) { // mark task as done and do the output
+        String out = "Nice! I've marked this task as done:\n  ";
+        task.markAsDone();
+        out += "[" + task.getStatusIcon() + "] " + task.getDescription();
+        pack(out);
     }
 
     public static void exit(){
@@ -50,13 +67,13 @@ public class Duke {
         pack(bye);
     }
 
-    public static void level2(){
+    public static void level3(){
         greet();
         scan();
         exit();
     }
 
     public static void main(String[] args) {
-        level2();
+        level3();
     }
 }
