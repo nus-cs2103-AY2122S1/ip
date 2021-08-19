55acbda5ff05fce64cdc4d3b98efdd04b5f4aab7
xiongjya <xiongjingyaa@gmail.com>
2021-08-19 22:04:20 +0800

    Use enum instead of using if-else branches in taskListener

---
+++ src/main/java/exceptions/IndexOutOfBoundsException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class IndexOutOfBoundsException extends Exception {
+    public IndexOutOfBoundsException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() {
+        return "☹ OOPS!!! The number you have entered is out of bounds, please enter another number! ☹\n";
+    }
+}
--- src/main/java/tasks/ToDoList.java
+++ src/main/java/tasks/ToDoList.java
@@ -1,11 +1,17 @@
 package tasks;
 
 import exceptions.EmptyTaskException;
+import exceptions.IndexOutOfBoundsException;
 import exceptions.InvalidCommandException;
 import java.util.ArrayList;
 
 public class ToDoList {
     private final String SEPARATOR = "-----------------------------------------------------------------";
+    private enum taskType {
+        TODO,
+        DEADLINE,
+        EVENT
+    }
     private final ArrayList<Task> tasks;
     private int count;
 
@@ -15,16 +21,42 @@ public class ToDoList {
     }
 
     public int taskListener(String input) {
-        return (input.equals("list"))
-                ? listTasks()
-                : toMarkAsDone(input)
-                ? markAsDone(input)
-                : toDeleteTask(input)
-                ? deleteTask(input)
-                : addTask(input);
+        try {
+            String[] arr = input.split(" ", 2);
+            String command = arr[0];
+
+            switch (command) {
+                case "todo":
+                    addTask(taskType.TODO, input);
+                    break;
+                case "deadline":
+                    addTask(taskType.DEADLINE, input);
+                    break;
+                case "event":
+                    addTask(taskType.EVENT, input);
+                    break;
+                case "list":
+                    listTasks();
+                    break;
+                case "done":
+                    markAsDone(input);
+                    break;
+                case "delete":
+                    deleteTask(input);
+                    break;
+                default:
+                    throw new InvalidCommandException(input);
+            }
+        } catch (InvalidCommandException e) {
+            System.out.println(SEPARATOR + "\n"
+                    + e
+                    + SEPARATOR);
+        }
+
+        return 0;
     }
 
-    public int listTasks() {
+    public void listTasks() {
         int n = 0;
 
         System.out.println(SEPARATOR);
@@ -37,133 +69,96 @@ public class ToDoList {
         }
 
         System.out.println(SEPARATOR);
-
-        return 0;
     }
 
-    public int addTask(String input) {
+    public void addTask(taskType t, String input) {
         String[] arr = input.split(" ", 2);
 
-        Task recentlyAdded;
+        try {
+            Task recentlyAdded;
 
-        if (arr[0].equals("todo")) {
-            try {
-                recentlyAdded = arr.length == 1
-                                ? ToDo.addToDo("")
-                                : ToDo.addToDo(arr[1]);
-            } catch (EmptyTaskException e) {
+            if (t == taskType.TODO) {
+                if (arr.length == 1) {
+                    recentlyAdded = ToDo.addToDo("");
+                }
+
+                recentlyAdded = ToDo.addToDo(arr[1]);
+            } else if (t == taskType.DEADLINE) {
+                recentlyAdded = Deadline.addDeadline(arr[1]);
+            } else {
+                recentlyAdded = Event.addEvent(arr[1]);
+            }
+
+            this.tasks.add(recentlyAdded);
+            count++;
+
+            String taskCount = "Now you have " + count + " task(s) in the list!";
+
+            System.out.println(SEPARATOR + "\n"
+                    + "Received order! I've added this task:\n"
+                    + "     " + recentlyAdded + "\n"
+                    + taskCount + "\n"
+                    + SEPARATOR);
+        } catch (EmptyTaskException e) {
+            System.out.println(SEPARATOR + "\n"
+                    + e
+                    + SEPARATOR);
+        }
+    }
+
+    public void markAsDone(String input) {
+        try {
+            String[] arr = input.split(" ");
+
+            int index = Integer.parseInt(arr[1]) - 1;
+
+            if (index >= count) {
+                throw new IndexOutOfBoundsException("");
+            } else {
+                Task t = this.tasks.get(index);
+
+                t.markAsDone();
+
+                String encouragement = "Good job! I've marked this task as done:";
+                String reward = "Bubbles will reward you with a piece of candy.";
+
                 System.out.println(SEPARATOR + "\n"
-                        + e
+                        + encouragement + "\n"
+                        + "     " + t + "\n"
+                        + reward + "\n"
                         + SEPARATOR);
-
-                return 0;
             }
-        } else if (arr[0].equals("deadline")) {
-            recentlyAdded = Deadline.addDeadline(arr[1]);
-        } else if (arr[0].equals("event")) {
-            recentlyAdded = Event.addEvent(arr[1]);
-        } else {
-            try {
-                throw new InvalidCommandException(input);
-            } catch (InvalidCommandException e) {
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(SEPARATOR + "\n"
+                    + e
+                    + SEPARATOR);
+        }
+    }
+
+    public void deleteTask(String input) {
+        try {
+            String[] arr = input.split(" ");
+
+            int index = Integer.parseInt(arr[1]) - 1;
+
+            if (index >= count) {
+                throw new IndexOutOfBoundsException("");
+            } else {
+                Task removed = this.tasks.remove(index);
+                count--;
+
+                String taskCount = "Now you have " + count + " task(s) in the list!";
+
                 System.out.println(SEPARATOR + "\n"
-                        + e
+                        + "Noted! I've removed this task:\n"
+                        + "     " + removed + "\n"
+                        + taskCount + "\n"
                         + SEPARATOR);
-
-                return 0;
             }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(SEPARATOR + "\n"
+                    + e
+                    + SEPARATOR);
         }
-
-        this.tasks.add(recentlyAdded);
-        count++;
-
-        String taskCount = "Now you have " + count + " task(s) in the list!";
-
-        System.out.println(SEPARATOR + "\n"
-                + "Received order! I've added this task:\n"
-                + "     " + recentlyAdded + "\n"
-                + taskCount + "\n"
-                + SEPARATOR);
-
-        return 0;
-    }
-
-    // to check if user input text is a valid command for marking a task as done
-    public boolean toMarkAsDone(String input) {
-        String[] arr = input.split(" ");
-
-        boolean toDo = false;
-
-        if ((arr[0].equals("done")) && (arr.length == 2)) {
-            try {
-                Integer.valueOf(arr[1]);
-                toDo = true;
-            } catch (NumberFormatException e) {
-                return false;
-            }
-        }
-        // a possible error arrives from this feature:
-        // when # input is invalid
-
-        return toDo;
-    }
-
-    public int markAsDone(String input) {
-        String[] arr = input.split(" ");
-
-        int index = Integer.parseInt(arr[1]) - 1;
-
-        Task t = this.tasks.get(index);
-
-        t.markAsDone();
-
-        String encouragement = "Good job! I've marked this task as done:";
-        String reward = "Bubbles will reward you with a piece of candy.";
-
-        System.out.println(SEPARATOR + "\n"
-                + encouragement + "\n"
-                + "     " + t + "\n"
-                + reward + "\n"
-                + SEPARATOR);
-
-        return 0;
-    }
-
-    // to check if user input text is a valid command for deleting a task
-    public boolean toDeleteTask(String input) {
-        String[] arr = input.split(" ");
-
-        boolean toDelete = false;
-
-        if (arr[0].equals("delete") && (arr.length == 2)) {
-            try {
-                Integer.valueOf(arr[1]);
-                toDelete = true;
-            } catch (NumberFormatException e) {
-                return false;
-            }
-        }
-        // a possible error arrives from this feature:
-        // when # input is invalid
-
-        return toDelete;
-    }
-
-    public int deleteTask(String input) {
-        String[] arr = input.split(" ");
-
-        int index = Integer.parseInt(arr[1]) - 1;
-
-        Task removed = this.tasks.remove(index);
-        count--;
-
-        String taskCount = "Now you have " + count + " task(s) in the list!";
-
-        System.out.println(SEPARATOR + "\n"
-                + "Noted! I've removed this task:\n"
-                + "     " + removed + "\n"
-                + taskCount + "\n"
-                + SEPARATOR);
-        return 0;
     }
 }
