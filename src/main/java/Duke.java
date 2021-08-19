29333d4526acaf57d26eb78df428a8f2dd824ab0
Mengzhe Wang <mengzhe_wang@u.nus.edu>
2021-08-19 19:54:27 +0800

    Level 3: mark as done. Added in Task class.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Duke {
     public static void main(String[] args) {
         String end = "bye";
         String display = "list";
         String lineBreak = "------------------------------";
-        ArrayList<String> inputs = new ArrayList<>();
+        String markDone = "done";
+        ArrayList<Task> inputs = new ArrayList<>();
+
 
         // Welcome message
         System.out.println(lineBreak
@@ -20,19 +24,40 @@ public class Duke {
         // String input
         while(true) {
             String input = myObj.nextLine();
+            Task newTask = new Task(input);
+
+            // mark a task as Done
+            // regex: "done" + space + number
+            // if number < inputs.size(), mark input[number] as done
+            // Pattern pattern = Pattern.compile("done\\s\\d+");
+
+            if (input.contains("done") && input.contains(" ")) {
+                String[] parts = input.split(" ");
+                int index = Integer.parseInt(parts[1]) - 1;
+                if (parts[0].equals(markDone) && index < inputs.size()) {
+                    inputs.get(index).setDone();
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println("[X] " + inputs.get(index).taskName);
+                    System.out.println(lineBreak + "\n");
+                }
+                continue;
+            }
 
             // normal input
             if (!input.equals(end) && !input.equals(display)) {
                 System.out.println("     added: " + input + "\n" + lineBreak + "\n");
-                inputs.add(input);
+                inputs.add(newTask);
                 continue;
             }
 
             // display saved inputs
             if (input.equals(display)) {
+                System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < inputs.size(); i++) {
                     int index = i + 1;
-                    System.out.println(index + ". " + inputs.get(i));
+                    System.out.println(index + ". "
+                            + "[" + inputs.get(i).getStatusIcon() + "] "
+                            + inputs.get(i).taskName);
                 }
                 System.out.println(lineBreak + "\n");
                 continue;
---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+     protected String taskName;
+     protected boolean isDone;
+
+     public Task(String taskName) {
+         this.taskName = taskName;
+         this.isDone = false;
+     }
+
+     public void setDone() {
+         isDone = true;
+     }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+}
