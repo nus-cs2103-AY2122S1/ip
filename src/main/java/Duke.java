925d81bc81e7947f17cfb158998a61129bc01178
jeremy-7007 <71486951+jeremy-7007@users.noreply.github.com>
2021-08-19 23:17:50 +0800

    Commit level 6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -11,8 +12,7 @@ public class Duke {
 
         System.out.println("Hello! I'm Nukem\n"
                 + "What can I do for you?");
-        Task[] storage = new Task[100];
-        int index = 0;
+        ArrayList<Task> storage = new ArrayList<>();
         String tempString;
         Scanner input = new Scanner(System.in);
         // Keep asking for input as long as "bye" is not typed
@@ -30,14 +30,11 @@ public class Duke {
                             throw new DukeException("Command 'list' should not take any input!");
                         } else {    // list all the tasks
                             System.out.println("Here are the tasks in your list:");
-                            for (int i = 0; i < storage.length; i++) {
-                                if (storage[i] == null) break;
-                                else {
-                                    Task nextTask = storage[i];
-                                    System.out.println((i + 1)
-                                            + "."
-                                            + nextTask.toString());
-                                }
+                            for (int i = 0; i < storage.size(); i++) {
+                                Task nextTask = storage.get(i);
+                                System.out.println((i + 1)
+                                        + "."
+                                        + nextTask.toString());
                             }
                         }
                         break;
@@ -47,38 +44,37 @@ public class Duke {
                         } else {    // if there are exactly 2 words in the input...
                             try {   // if the second word is an integer, mark the corresponding task as done
                                 int tempIndex = Integer.parseInt(wordArray[1]) - 1;
-                                if (tempIndex < 0 || tempIndex > 99 || storage[tempIndex] == null) {
-                                    throw new DukeException("Invalid task number!");
-                                } else {
-                                    Task tempTask = storage[tempIndex];
-                                    tempTask.markAsDone();
-                                    System.out.println("Nice! I've marked this task as done:\n"
-                                            + "  "
-                                            + tempTask.toString());
-                                }
+                                Task tempTask = storage.get(tempIndex);
+                                tempTask.markAsDone();
+                                System.out.println("Nice! I've marked this task as done:\n"
+                                        + "  "
+                                        + tempTask.toString());
                             } catch (NumberFormatException nfe) {   // if 2nd word is not integer, show a warning
                                 throw new DukeException("Please follow command 'done' with an integer!");
+                            } catch (IndexOutOfBoundsException oob) {   // If the task number is out of bound, warn
+                                throw new DukeException("Invalid task number!");
                             }
                         }
                         break;
-                    case ("todo"):
-                        if (wordArray.length < 2) {
+                    case ("todo"):  // "todo" is typed as the first word
+                        if (wordArray.length < 2) { // if nothing is typed after the command, warn
                             throw new DukeException("Please enter a task name!");
-                        } else {
+                        } else {    // else, add the new task with everything behind the command as description
                             String newDesc = tempString.substring(5);
-                            storage[index] = new Todo(newDesc);
-                            Task tempTask = storage[index];
-                            index++;
+                            Task newTodo = new Todo(newDesc);
+                            storage.add(newTodo);
                             System.out.println("Got it. I've added this task:\n"
-                                    + "  " + tempTask.toString() + "\n"
-                                    + "Now you have " + getLength(storage)
-                                    + " task" + (getLength(storage) > 1 ? "s" : "") + " in the list.");
+                                    + "  " + newTodo.toString() + "\n"
+                                    + "Now you have " + storage.size()
+                                    + " task" + (storage.size() > 1 ? "s" : "") + " in the list.");
                         }
                         break;
-                    case ("deadline"):
-                        if (wordArray.length < 4) {
+                    case ("deadline"):  // "deadline" is typed as the first word
+                        if (wordArray.length < 4) { // if less than 4 words are typed, warn
                             throw new DukeException("Please follow format: 'deadline [task name] /by [time]'");
                         }
+                        // Scan through the input to extract the description before "/by"
+                        // and the date after "/by"
                         String newDeadlineDesc = "";
                         String newBy = "";
                         int deadlinePointer = 1;
@@ -87,30 +83,32 @@ public class Duke {
                             deadlinePointer++;
                         }
                         deadlinePointer++;
-                        if (newDeadlineDesc.length() > 0) {
+                        if (newDeadlineDesc.length() > 0) { // delete the last space
                             newDeadlineDesc = newDeadlineDesc.substring(0, newDeadlineDesc.length() - 1);
                         }
                         while (deadlinePointer < wordArray.length) {
                             newBy += wordArray[deadlinePointer] + " ";
                             deadlinePointer++;
                         }
+                        // delete the last space
                         if (newBy.length() > 0) newBy = newBy.substring(0, newBy.length() - 1);
-                        if (newDeadlineDesc.equals("") || newBy.equals("")) {
+                        if (newDeadlineDesc.equals("") || newBy.equals("")) {   // if either the description or date is empty
                             throw new DukeException("Please follow format: 'deadline [task name] /by [time]'");
-                        } else {
-                            storage[index] = new Deadline(newDeadlineDesc, newBy);
-                            Task tempTask = storage[index];
-                            index++;
+                        } else {    // else add a new Deadline
+                            Task newDeadline = new Deadline(newDeadlineDesc, newBy);
+                            storage.add(newDeadline);
                             System.out.println("Got it. I've added this task:\n"
-                                    + "  " + tempTask.toString() + "\n"
-                                    + "Now you have " + getLength(storage)
-                                    + " task" + (getLength(storage) > 1 ? "s" : "") + " in the list.");
+                                    + "  " + newDeadline.toString() + "\n"
+                                    + "Now you have " + storage.size()
+                                    + " task" + (storage.size() > 1 ? "s" : "") + " in the list.");
                         }
                         break;
-                    case ("event"):
-                        if (wordArray.length < 4) {
+                    case ("event"): // "event" is typed as the first word
+                        if (wordArray.length < 4) { // if less than 4 words are typed, warn
                             throw new DukeException("Please follow format: 'event [task name] /at [time]'");
                         }
+                        // Scan through the input to extract the description before "/at"
+                        // and the date after "/at"
                         String newEventDesc = "";
                         String newAt = "";
                         int eventPointer = 1;
@@ -119,24 +117,44 @@ public class Duke {
                             eventPointer++;
                         }
                         eventPointer++;
-                        if (newEventDesc.length() > 0) {
+                        if (newEventDesc.length() > 0) {    // delete the last space
                             newEventDesc = newEventDesc.substring(0, newEventDesc.length() - 1);
                         }
                         while (eventPointer < wordArray.length) {
                             newAt += wordArray[eventPointer] + " ";
                             eventPointer++;
                         }
+                        // delete the last space
                         if (newAt.length() > 0) newAt = newAt.substring(0, newAt.length() - 1);
-                        if (newEventDesc.equals("") || newAt.equals("")) {
+                        if (newEventDesc.equals("") || newAt.equals("")) {  // if either the description or date is empty
                             throw new DukeException("Please follow format: 'event [task name] /at [time]'");
-                        } else {
-                            storage[index] = new Event(newEventDesc, newAt);
-                            Task tempTask = storage[index];
-                            index++;
+                        } else {    // else add a new Event
+                            Task newEvent = new Event(newEventDesc, newAt);
+                            storage.add(newEvent);
                             System.out.println("Got it. I've added this task:\n"
-                                    + "  " + tempTask.toString() + "\n"
-                                    + "Now you have " + getLength(storage)
-                                    + " task" + (getLength(storage) > 1 ? "s" : "") + " in the list.");
+                                    + "  " + newEvent.toString() + "\n"
+                                    + "Now you have " + storage.size()
+                                    + " task" + (storage.size() > 1 ? "s" : "") + " in the list.");
+                        }
+                        break;
+                    case ("delete"):    // "delete" is typed as the first word
+                        if (wordArray.length != 2) {    // if there are not exactly 2 words in the input, show a warning
+                            throw new DukeException("Please follow command 'delete' with an integer!");
+                        } else {    // if there are exactly 2 words in the input...
+                            try {   // if the second word is an integer, delete the corresponding task
+                                int tempIndex = Integer.parseInt(wordArray[1]) - 1;
+                                String taskString = storage.get(tempIndex).toString();
+                                storage.remove(tempIndex);
+                                System.out.println("Noted. I've removed this task:\n"
+                                        + "  "
+                                        + taskString + "\n"
+                                        + "Now you have " + storage.size()
+                                        + " task" + (storage.size() > 1 ? "s" : "") + " in the list.");
+                            } catch (NumberFormatException nfe) {   // if 2nd word is not integer, show a warning
+                                throw new DukeException("Please follow command 'delete' with an integer!");
+                            } catch (IndexOutOfBoundsException oob) {   // If the task number is out of bound, warn
+                                throw new DukeException("Invalid task number!");
+                            }
                         }
                         break;
                     default:    // No command word is recognised
@@ -147,13 +165,5 @@ public class Duke {
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
-    }
-
-    public static <T> int getLength(T[] arr){
-        int count = 0;
-        for(T element : arr) {
-            if (element != null) ++count;
-        }
-        return count;
     }
 }
