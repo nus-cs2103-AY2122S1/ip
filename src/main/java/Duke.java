621907834f7a3df7a24d6051ff9493c11d3232e2
Samuel-bit-prog <e0544506@u.nus.edu>
2021-08-19 20:37:41 +0800

    Level-3 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Duke {
-    private static final String[] list = new String[100];
+    private static final Task[] list = new Task[100];
     private static int listNumber = 0;
 
     public static void main(String[] args) {
@@ -15,19 +15,27 @@ public class Duke {
         Scanner s = new Scanner(System.in);
         String command = s.nextLine();
         while(!command.equals("bye")) {
-            switch(command) {
+            switch(command.split(" ")[0]) {
                 case "list":
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < listNumber; i++) {
                         int num = i + 1;
                         if (list[i] != null) {
-                            System.out.println(num + ". " + list[i]);
+                            System.out.println(num + "." + list[i].getStatusIcon()+ list[i].description);
                         }
                     }
                     command = s.nextLine();
                     break;
+                case "done":
+                    int finished = Integer.parseInt(command.split(" ")[1]) - 1;
+                    list[finished].markAsDone();
+                    System.out.println("Nice! I've marked this task as done:\n" + "  " + list[finished].getStatusIcon()
+                            + list[finished].description);
+                    command = s.nextLine();
+                    break;
                 default:
                     System.out.println("added: " + command);
-                    list[listNumber]= command;
+                    list[listNumber]= new Task(command);
                     listNumber += 1;
                     command = s.nextLine();
             }
