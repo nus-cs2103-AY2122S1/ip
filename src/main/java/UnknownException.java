e3210376d2adb19db8f539ff178dd67db3946f27
tianyue58 <e0563952@u.nus.edu>
2021-08-19 15:08:31 +0800

    add new line using "\n"

--- src/main/java/UnknownException.java
+++ src/main/java/UnknownException.java
@@ -1,7 +1,5 @@
-package Exception;
-
 public class UnknownException extends Exception {
     public UnknownException() {
-        super("OOPS!!! I'm sorry, but I don't know what that means :-(");
+        super("OOPS!!! I'm sorry, but I don't know what that means :-(\n");
     }
 }
