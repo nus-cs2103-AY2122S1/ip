23e7bda8afdd629a5fce429f87fb32c756edd967
Poh Hui-En Ruth <iluvcelebi@gmail.com>
2021-08-19 15:30:17 +0800

    Level-5

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,10 +1,10 @@
 public class DukeException extends Exception {
-    protected String linebreak = "~~*********___\\(owo)/___\\(owo)/___*********~~\n\n";
+    protected String linebreak = "~~*********___\\(owo)/___\\(owo)/___*********~~";
 
     public DukeException(String message) {
         super("~~*********___\\(owo)/___\\(owo)/___*********~~\n\n"
                 + message
-                + "~~*********___\\(owo)/___\\(owo)/___*********~~\n\n");
+                + "\n~~*********___\\(owo)/___\\(owo)/___*********~~");
     }
 
     public DukeException(String message, Throwable cause) {
