eb6f9d9f0379abd647824e383378a6c0dc9d54f7
SpdPnd98 <bryantee1998@gmail.com>
2021-08-19 23:11:24 +0800

    Adding exception handling for duke

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -5,13 +5,13 @@ public class Event extends Task{
         super(str.split(" /", 2)[0]);
         String[] command = str.split(" /", 2);
         if (command.length == 1) {
-            throw new IllegalArgumentException("No commands specified for task 'deadline'!");
+            throw new DukeException("No commands specified for task 'deadline'!");
         }
         String[] commandAndDate = command[1].split(" ", 2);
         if(!commandAndDate[0].equals("at")) {
-            throw new IllegalArgumentException("Unknown command provided to Event! did you use '/at'?");
+            throw new DukeException("Unknown command provided to Event! did you use '/at'?");
         } else if (commandAndDate.length == 1) {
-            throw new IllegalArgumentException("No date specified!");
+            throw new DukeException("No date specified!");
         }
         this.date = commandAndDate[1];
     }
