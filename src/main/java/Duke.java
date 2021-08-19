705b60a23b0993c4402a320812bdd67d28168118
Gordon25 <73068242+Gordon25@users.noreply.github.com>
2021-08-19 10:36:34 +0800

    Added javadocs and indented output of the chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 
+/**
+ * A class that encapsulates the duke chat bot, which has ability to greet, echo and exit.
+ */
 public class Duke {
+    /**
+     * Constructor for the Duke class
+     * Does not take in any parameters, prints out a statement to greet user.
+     */
     public Duke() {
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
     }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,10 +23,10 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(input);
+            System.out.println("\t" + input);
             input = sc.nextLine();
         }
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("\tBye. Hope to see you again soon!");
         sc.close();
     }
 }
