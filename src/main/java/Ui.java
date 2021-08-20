86e6741ea63cc68b8f42f9fc2f10150714177787
twothicc <chenenliuu@gmail.com>
2021-08-20 19:36:15 +0800

    Add methods to Ui class to print standard responses

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -57,4 +57,42 @@ public class Ui {
             return this.COMMAND;
         }
     }
+
+    public void showWelcome() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    public void showGoodbye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public void showMarkSuccess(Task task) {
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(task.toString());
+    }
+
+    public void showDeleteSuccess(Task task, int size) {
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(task.toString());
+        System.out.println("Now you have " + size + " tasks in the list.");
+    }
+
+    public void showAddSuccess(Task task, int size) {
+        System.out.println("Got it. I have added this task:");
+        System.out.println(task.toString());
+        System.out.println("Now you have " + size + " tasks in the list.");
+    }
+
+    public void showListSuccess() {
+        System.out.println("Here are the tasks in your list:");
+    }
+
+    public void showDateListSuccess(String formattedDateString) {
+        System.out.println("Here are the Deadlines or Events that fall on " + formattedDateString + ":");
+    }
+
+    public void showDateListSummary(String formattedDateString, int counter, int deadlines, int events) {
+        System.out.println("A total of " + counter + " events (" + deadlines + " deadlines and " +
+                events + " events) fall on " + formattedDateString);
+    }
 }
