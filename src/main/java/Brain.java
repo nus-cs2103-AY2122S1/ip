6ec1ad9c8bf0089006e2bdbc560db674fba7a101
idgrr <77221700+idgrr@users.noreply.github.com>
2021-08-20 20:31:05 +0800

    Updated minor styling

--- src/main/java/Brain.java
+++ src/main/java/Brain.java
@@ -8,7 +8,6 @@ public class Brain {
         this.speech = speech;
     }
 
-
     /**
      * Takes the first word and decides the next course of action.
      * @param input takes in a string based on the scanner
@@ -20,57 +19,57 @@ public class Brain {
         String userReq ="";
         try {
             switch (parsedInput[0]) {
-                case "bye":
-                    speech.goodbye();
-                    cont = false;
-                    break;
-                case "list":
-                    String[] listOutput = storage.getStorage();
-                    speech.listMsg(listOutput, storage);
-                    break;
-                case "done":
-                    userReq = parsedInput[1];
-                    try {
-                        String outputMsg = storage.done(userReq);
-                        speech.doneMsg(outputMsg);
-                    } catch (InvalidDoneFormatException e){
-                        speech.error(e.toString());
-                    }
-                    break;
-                case "deadline":
-                    userReq = parsedInput[1];
-                    try{
-                        String deadlineSuccess = storage.deadline(userReq);
-                        speech.taskAdded(deadlineSuccess, storage);
-                    } catch (InvalidDeadlineFormatException e) {
-                        speech.error(e.toString());
-                    }
-                    break;
-                case "todo":
-                    userReq = parsedInput[1];
-                    String todoSuccess = storage.todo(userReq);
-                    speech.taskAdded(todoSuccess, storage);
-                    break;
-                case "event":
-                    userReq = parsedInput[1];
-                    try{
-                        String eventSuccess = storage.event(userReq);
-                        speech.taskAdded(eventSuccess, storage);
-                    } catch (InvalidEventFormatException e) {
-                        speech.error(e.toString());
-                    }
-                    break;
-                case "delete":
-                    userReq = parsedInput[1];
-                    try {
-                        String deleteSuccess = storage.delete(userReq);
-                        speech.taskDeleted(deleteSuccess, storage);
-                    } catch (InvalidDeleteFormatException e) {
-                        speech.error(e.toString());
-                    }
-                    break;
-                default:
-                    throw new IllegalArgumentException();
+            case "bye":
+                speech.goodbye();
+                cont = false;
+                break;
+            case "list":
+                String[] listOutput = storage.getStorage();
+                speech.listMsg(listOutput, storage);
+                break;
+            case "done":
+                userReq = parsedInput[1];
+                try {
+                    String outputMsg = storage.done(userReq);
+                    speech.doneMsg(outputMsg);
+                } catch (InvalidDoneFormatException e){
+                    speech.error(e.toString());
+                }
+                break;
+            case "deadline":
+                userReq = parsedInput[1];
+                try{
+                    String deadlineSuccess = storage.deadline(userReq);
+                    speech.taskAdded(deadlineSuccess, storage);
+                } catch (InvalidDeadlineFormatException e) {
+                    speech.error(e.toString());
+                }
+                break;
+            case "todo":
+                userReq = parsedInput[1];
+                String todoSuccess = storage.todo(userReq);
+                speech.taskAdded(todoSuccess, storage);
+                break;
+            case "event":
+                userReq = parsedInput[1];
+                try{
+                    String eventSuccess = storage.event(userReq);
+                    speech.taskAdded(eventSuccess, storage);
+                } catch (InvalidEventFormatException e) {
+                    speech.error(e.toString());
+                }
+                break;
+            case "delete":
+                userReq = parsedInput[1];
+                try {
+                    String deleteSuccess = storage.delete(userReq);
+                    speech.taskDeleted(deleteSuccess, storage);
+                } catch (InvalidDeleteFormatException e) {
+                    speech.error(e.toString());
+                }
+                break;
+            default:
+                throw new IllegalArgumentException();
             }
         } catch (IllegalArgumentException e) {
             speech.error(new InvalidInputException("User input an invalid action.").toString());
