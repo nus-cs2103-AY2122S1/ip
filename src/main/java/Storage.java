a2596f3d3a0f4815094ec35a0ce973714a268759
e0543403 <e0543403@u.nus.edu>
2021-08-21 17:12:37 +0800

    Add skeleton class Storage.java to java directory

---
+++ src/main/java/Storage.java
@@ -0,0 +1,18 @@
+public class Storage {
+    private String path;
+    private DukeList list;
+
+    public Storage(String path, DukeList list) {
+        this.path = path;
+    }
+
+
+    public void save() {
+
+    }
+
+
+    public void load() {
+
+    }
+}
