3d100c9d65554b16684a9664cd84504bf1aaba72
Wayne Tee <waynetee37@gmail.com>
2021-08-22 00:49:53 +0800

    Rename filePath to fileName

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -7,16 +7,16 @@ import java.nio.file.Files;
 import java.util.List;
 
 class Storage {
-    private final String filePath;
+    private final String fileName;
     private final FileWriter writer;
 
-    Storage(String filePath) throws IOException {
-        this.filePath = filePath;
-        writer = new FileWriter(filePath, true);
+    Storage(String fileName) throws IOException {
+        this.fileName = fileName;
+        writer = new FileWriter(fileName, true);
     }
 
     List<String> readAllLines() throws IOException {
-        return Files.readAllLines(new File(filePath).toPath());
+        return Files.readAllLines(new File(fileName).toPath());
     }
 
     void write(String s) throws IOException {
