8d0c7ca1ecbb531133b501d11bad63f9e75da6bb
Siew Hui Zhuan <huizhuansam@hotmail.com>
2021-08-21 14:32:01 +0800

    Add Javadocs to Commands class

--- src/main/java/duke/command/ExitCommand.java
+++ src/main/java/duke/command/ExitCommand.java
@@ -7,7 +7,15 @@ import duke.util.Ui;
 
 import java.io.IOException;
 
+/** Represents the "exit" command. */
 public class ExitCommand extends Command {
+    /**
+     * ExitCommand factory method.
+     *
+     * @param content The user's input content.
+     * @return An ExitCommand object.
+     * @throws BadInputFormatException If the user's input contains more than a singular "bye" keyword.
+     */
     public static ExitCommand of(String content) throws BadInputFormatException {
         if (content.trim().length() > 1) {
             throw new BadInputFormatException();
@@ -15,6 +23,14 @@ public class ExitCommand extends Command {
         return new ExitCommand();
     }
 
+    /**
+     * Cleans up the UI and writes to disk.
+     *
+     * @param tasks The list of tasks in the program.
+     * @param ui The UI object.
+     * @param storage The storage utility.
+     * @throws IOException If the storage encounters and IO exception.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws IOException {
         ui.print("Bye. See ya l8er allig8er!", "\033[3m*shutting down......*\033[0m");
