0ec0870a2977d147c354ca6a64c090e57d9dbfeb
chetwin low <chetwinlow@gmail.com>
2021-08-21 18:47:29 +0800

    Level-8 Completed

--- src/main/java/Add.java
+++ src/main/java/Add.java
@@ -17,10 +17,10 @@ class Add extends Command {
             }
             return new Add(list, new Todo(temp.substring(1)));
         } else if (input.startsWith("deadline")) {
-            String[] arr = input.split(" /by");
+            String[] arr = input.split(" /by ");
             return new Add(list, new Deadline(arr[0].substring(9), arr[1]));
         } else if (input.startsWith("event")) {
-            String[] arr = input.split("/at");
+            String[] arr = input.split("/at ");
             return new Add(list, new Event(arr[0].substring(6), arr[1]));
         } else {
             throw new IllegalCommandException("OOPS!!! I'm sorry, but I don't know what that means :-(");
--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,28 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.Arrays;
+
 class Deadline extends Task {
 
+    protected LocalDate date;
+    protected LocalTime time;
     protected String by;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        String[] arr = by.split(" ");
+        String[] arr1 = arr[0].split("\\/");
+        this.date = LocalDate.parse(String.format("%s-%s-%s", arr1[2], arr1[1], arr1[0].length() == 1 ? "0" + arr1[0] : arr1[0]));
+        this.time = LocalTime.parse(arr[1].substring(0, 2) + ":" + arr[1].substring(2));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " +
+                this.date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " " +
+                this.time.format(DateTimeFormatter.ofPattern("hh:mm a")) + ")";
     }
 }
