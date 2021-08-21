e627ccabbac0192c243c75a857e55501cdeb90e0
e0543403 <e0543403@u.nus.edu>
2021-08-21 19:48:31 +0800

    Add load method functionality and closed fileWriter in save method

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Storage {
     private String path;
@@ -14,6 +17,7 @@ public class Storage {
         try {
             FileWriter fileWriter = new FileWriter(this.path);
             fileWriter.write(list.toString());
+            fileWriter.close();
         } catch (IOException e) {
             System.out.println(e.toString());
         }
@@ -21,5 +25,30 @@ public class Storage {
 
 
     public void load() {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(this.path));
+            String data = bufferedReader.readLine();
+            String type;
+            String state;
+            String body;
+
+            String[] segregate;
+
+            while (data != null) {
+                segregate = data.split(" ", 3);
+
+                type = segregate[0];
+                state = segregate[1];
+                body = segregate[2];
+
+                this.list.loadData(type, state, body);
+
+                data = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
