0a65d70a80e747782bf915c3eb831be455f183a8
Imran <imranr2@msn.com>
2021-08-21 12:16:57 +0800

    docs: Add Javadoc for ui

--- src/main/java/lifeline/ui/Ui.java
+++ src/main/java/lifeline/ui/Ui.java
@@ -5,17 +5,35 @@ import java.util.Scanner;
 import lifeline.task.Task;
 import lifeline.task.TaskList;
 
+/**
+ * The class UI handles all information to be displayed to the user
+ */
 public class Ui {
+
+    /**
+     * Scanner object to read inputs from the user
+     */
     private Scanner sc;
 
     public Ui() {
         this.sc = new Scanner(System.in);
     }
 
+    /**
+     * Returns the input from the user
+     *
+     * @return Input from user
+     */
     public String readCommand() {
         return sc.nextLine().trim();
     }
 
+    /**
+     * Returns string representation of the list of tasks created by the user
+     *
+     * @param taskList Lists of tasks created by user
+     * @return String representing list of tasks
+     */
     public String showTaskList(TaskList taskList) {
         StringBuilder stringBuilder = new StringBuilder();
         if (taskList.size() == 0) {
@@ -37,24 +55,48 @@ public class Ui {
         return stringBuilder.toString();
     }
 
+    /**
+     * Returns information regarding a task
+     *
+     * @param task Task to get information from
+     * @return Information about task
+     */
     public String showTask(Task task) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(task + "\n");
         return stringBuilder.toString();
     }
 
+    /**
+     * Returns information on deleted task
+     *
+     * @param task Deleted task
+     * @return Information on deleted task
+     */
     public String showDeletedTask(Task task) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("I have removed the task:\n" + task + "\n");
         return stringBuilder.toString();
     }
 
+    /**
+     * Returns information on added task
+     *
+     * @param task Addded task
+     * @return Information on added task
+     */
     public String showAddedTask(Task task) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("I have added this task for you:\n" + task + "\n");
         return stringBuilder.toString();
     }
 
+    /**
+     * Returns information on completed task
+     *
+     * @param task Completed task
+     * @return Information on completed task
+     */
     public String showCompletedTask(Task task) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("You have completed the " + task.getClass().getSimpleName().toLowerCase() + ":\n"
@@ -62,13 +104,25 @@ public class Ui {
         return stringBuilder.toString();
     }
 
+
+    /**
+     * Returns string representation of error message
+     *
+     * @param errorMessage Error to be displayed to user
+     * @return Error message
+     */
     public String showError(String errorMessage) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(errorMessage + "\n");
         return stringBuilder.toString();
     }
 
-    public String greet(TaskList taskList) {
+    /**
+     * Returns welcome message when user starts the program
+     *
+     * @return Welcome message
+     */
+    public String greet() {
         String lifeline = " _      _____ ______ ______ _      _____ _   _ ______\n"
                 + "| |    |_   _|  ____|  ____| |    |_   _| \\ | |  ____|\n"
                 + "| |      | | | |__  | |__  | |      | | |  \\| | |__\n"
@@ -81,18 +135,22 @@ public class Ui {
         return stringBuilder.toString();
     }
 
+    /**
+     * Returns goodbye message when user exits the program
+     *
+     * @return Goodbye message
+     */
     public String exit() {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Goodbye! Thanks for chatting with me!\n");
         return stringBuilder.toString();
     }
 
-    public String echo(String input) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("You have said \"" + input + "\"\n");
-        return stringBuilder.toString();
-    }
-
+    /**
+     * Displays message to the user
+     *
+     * @param response Message to be displayed to user
+     */
     public void printToConsole(String response) {
         System.out.println(response);
     }
