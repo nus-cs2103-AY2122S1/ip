9aefefb5b4af857e230751691ad21baa755def33
Py0000 <>
2021-08-22 00:06:52 +0800

    Add Level 7 save functionally.

--- src/main/java/exceptions/DukeException.java
+++ src/main/java/exceptions/DukeException.java
@@ -5,15 +5,8 @@ public class DukeException extends Exception {
     }
 }
 
-/**
- * Thrown when the user input an unknown string to the chat bot.
- * Chat bot does not recognise the input given.
- */
-class UnknownTagException extends DukeException {
-    public UnknownTagException() {
-        super("No such tag! Please input the correct tag!");
-    }
-}
+
+
 
 /**
  * Thrown when user tries to mark/delete tasks that are not in the list of tasks.
