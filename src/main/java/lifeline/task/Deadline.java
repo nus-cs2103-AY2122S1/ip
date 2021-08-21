b1d53c1a52fbf649be3a3ca7320b21d9acb31da5
Imran <imranr2@msn.com>
2021-08-21 12:16:33 +0800

    docs: Add Javadoc for Deadline

--- src/main/java/lifeline/task/Deadline.java
+++ src/main/java/lifeline/task/Deadline.java
@@ -3,14 +3,34 @@ package lifeline.task;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * The Deadline class encapsulates a task with a deadline.
+ */
 public class Deadline extends Task {
+
+    /**
+     * Date and time for which the task has to be completed
+     */
     private LocalDateTime by;
 
+    /**
+     * Default constructor for a Deadline
+     *
+     * @param name Name of the task with a deadline
+     * @param by Indicates when the task has to completed by.
+     */
     public Deadline(String name, LocalDateTime by) {
         super(name);
         this.by = by;
     }
 
+    /**
+     * Constructor to be used when loading saved deadline
+     *
+     * @param name Name of the task with a deadline
+     * @param by Indicates when the task has to completed by.
+     * @param isDone Indicates if the task is completed
+     */
     public Deadline(String name, LocalDateTime by, boolean isDone) {
         super(name, isDone);
         this.by = by;
