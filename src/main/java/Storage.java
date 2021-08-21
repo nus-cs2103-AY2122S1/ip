589124688deb959b3c358565556bdde0f0958ad6
Wayne Tee <waynetee37@gmail.com>
2021-08-21 17:43:47 +0800

    Add storage

---
+++ src/main/java/Storage.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+class Storage {
+    private final String filePath;
+    private final FileWriter writer;
+
+    Storage(String filePath) throws IOException {
+        this.filePath = filePath;
+        writer = new FileWriter(filePath, true);
+    }
+
+    List<String> readAllLines() throws IOException {
+        return Files.readAllLines(new File(filePath).toPath());
+    }
+
+    void write(String s) throws IOException {
+        writer.write(s + '\n');
+    }
+
+    void close() throws IOException {
+        writer.close();
+    }
+}
