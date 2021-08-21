c17a89719588f8eabe156b87856bb5054eec4f9c
Py0000 <>
2021-08-22 00:29:09 +0800

    Extract into exceptions package

---
+++ src/main/java/exceptions/DukeException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+/**
+ * Represents the possible exception that can be thrown by this chat bot.
+ */
+public class DukeException extends Exception {
+
+    public DukeException(String message) {
+        super(message);
+    }
+}
+
+
+
