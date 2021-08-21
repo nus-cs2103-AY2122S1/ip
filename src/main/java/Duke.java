21dd4eb6fadaf6c3c000a0c607fde5e37f2a029a
ZhaoPeiduo <e0389118@u.nus.edu>
2021-08-21 22:03:35 +0800

    Complete Level-7: Automatically save and load users' input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,26 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<Task> userInputRecord;
+    private static final String indentation = "     ";
+    private static final String subIndentation = "       ";
+
+    public static void main(String[] args) {
+        System.out.println(formatMessage( "Hello! I'm Peoduo\n" + indentation + "Can I help you?\n"));
+        echo();
+        System.out.println(formatMessage("Bye. Hope to see you again soon!\n"));
+    }
 
     //This method creates the while loop for echo, and calls different methods depending on user's input
     private static void echo() {
-        ArrayList<Task> userInputRecord = new ArrayList<>();
+        userInputRecord = new ArrayList<>();
+        autoLoad();
         Scanner myScanner = new Scanner(System.in);
         String userInput = myScanner.nextLine();
 
@@ -16,17 +31,13 @@ public class Duke {
                 if(isDoneCommand(userInput)) {
                     markAsDone(userInput, userInputRecord);
                 } else {
-                    System.out.println("    ____________________________________________________________\n" +
-                            "     OOPS!!! I'm sorry, but I don't know what that means :-(\n" +
-                            "    ____________________________________________________________");
+                    System.out.println(formatMessage("OOPS!!! I'm sorry, but I don't know what that means :-(\n"));
                 }
             } else if(userInput.startsWith("delete")) {
                 if (isDeleteCommand(userInput)) {
                     delete(userInput, userInputRecord);
                 } else {
-                    System.out.println("    ____________________________________________________________\n" +
-                            "     OOPS!!! I'm sorry, but I don't know what that means :-(\n" +
-                            "    ____________________________________________________________");
+                    System.out.println(formatMessage("OOPS!!! I'm sorry, but I don't know what that means :-(\n"));
                 }
             } else {
                 add(userInput, userInputRecord);
@@ -43,16 +54,12 @@ public class Duke {
             try {
                 String description = userInput.substring(5);
                 if (description.trim().isEmpty()) {
-                    System.out.println("    ____________________________________________________________\n" +
-                            "     OOPS!!! The description of a todo cannot be empty.\n" +
-                            "    ____________________________________________________________");
+                    System.out.println(formatMessage("OOPS!!! The description of a todo cannot be empty.\n"));
                     return;
                 }
                 task = new ToDo(description);
             } catch (StringIndexOutOfBoundsException e) {
-                System.out.println("    ____________________________________________________________\n" +
-                        "     OOPS!!! The description of a todo cannot be empty.\n" +
-                        "    ____________________________________________________________");
+                System.out.println(formatMessage("OOPS!!! The description of a todo cannot be empty.\n"));
                 return;
             }
         } else if(userInput.startsWith("deadline")) {
@@ -62,9 +69,7 @@ public class Duke {
                 String description = userInput.substring(9,byPosition); //Length of "deadline " = 9
                 task = new Deadline(description,ddl);
             } catch (StringIndexOutOfBoundsException e) {
-                System.out.println("    ____________________________________________________________\n" +
-                        "     OOPS!!! The description of a deadline cannot be empty.\n" +
-                        "    ____________________________________________________________");
+                System.out.println(formatMessage("OOPS!!! The description of a deadline cannot be empty.\n" ));
                 return;
             }
 
@@ -75,34 +80,26 @@ public class Duke {
                 String description = userInput.substring(6, atPosition);//Length of "event " = 6
                 task = new Event(description, time);
             } catch (StringIndexOutOfBoundsException e) {
-                System.out.println("    ____________________________________________________________\n" +
-                        "     OOPS!!! The description of an event cannot be empty.\n" +
-                        "    ____________________________________________________________");
+                System.out.println(formatMessage("OOPS!!! The description of an event cannot be empty.\n"));
                 return;
             }
         } else {
-            System.out.println("    ____________________________________________________________\n" +
-                    "     OOPS!!! I'm sorry, but I don't know what that means :-(\n" +
-                    "    ____________________________________________________________");
+            System.out.println(formatMessage("OOPS!!! I'm sorry, but I don't know what that means :-(\n"));
             return;
         }
 
         userInputRecord.add(task);
-        System.out.println("    ____________________________________________________________\n" +
-                "     Got it. I've added this task:\n" +
-                "       " + task + "\n" +
-                "     Now you have " + userInputRecord.size() + " tasks in the list.\n" +
-                "    ____________________________________________________________");
+        autoSave();
+        System.out.println(formatMessage( "Got it. I've added this task:\n" +
+                subIndentation + task + "\n" + indentation +
+                "Now you have " + userInputRecord.size() + " tasks in the list.\n" ));
     }
 
     //This method prints the saved list of events from the user.
     private static void printUserInputRecord(ArrayList<Task> userInputRecord) {
         if(userInputRecord.isEmpty()) {
-            System.out.println("    ____________________________________________________________\n" +
-                    "       Ah oh, seems like nothing is added yet :( \n" +
-                    "       Try to input something first! \n" +
-                    "    ____________________________________________________________"
-            );
+            System.out.println(formatMessage("Ah oh, seems like nothing is added yet :( \n" + indentation +
+                            "Try to input something first! \n" ));
 
         } else {
             System.out.println("    ____________________________________________________________");
@@ -114,7 +111,8 @@ public class Duke {
         }
     }
 
-    /**This method checks if a String starting with done is indeed a done command.
+    /**
+     * This method checks if a String starting with done is indeed a done command.
      * Examples:
      * Case 1: done 3 => valid, standard form
      * Case 2: done         5 ==> valid, after trimmed
@@ -136,19 +134,18 @@ public class Duke {
 
     //This method marks a saved event as done
     private static void markAsDone(String userInput, ArrayList<Task> userInputRecord) {
-        int itemToComplete = Integer.parseInt(userInput.replaceAll("[^0-9]", "")) - 1;
         try {
+            int itemToComplete = Integer.parseInt(userInput.replaceAll("[^0-9]", "")) - 1;
             Task taskDone = userInputRecord.get(itemToComplete);
             taskDone.setDone(true);
             userInputRecord.set(itemToComplete, taskDone);
-            System.out.println("    ____________________________________________________________\n" +
-                    "     Nice! I've marked this task as done:\n" +
-                    "     " + userInputRecord.get(itemToComplete) + "\n" +
-                    "    ____________________________________________________________");
+            autoSave();
+            System.out.println(formatMessage("Nice! I've marked this task as done:\n" +
+                            subIndentation + userInputRecord.get(itemToComplete) + "\n"));
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("    ____________________________________________________________\n" +
-                    "      Oops, the ID of the task does not exist\n" +
-                    "    ____________________________________________________________");
+            System.out.println(formatMessage("Oops, the ID of the task does not exist!\n"));
+        } catch (NumberFormatException e) {
+            System.out.println(formatMessage("Please enter a valid ID!\n"));
         }
     }
 
@@ -157,27 +154,82 @@ public class Duke {
             int itemToDelete = Integer.parseInt(userInput.replaceAll("[^0-9]", "")) - 1;
             Task itemDeleted = userInputRecord.get(itemToDelete);
             userInputRecord.remove(itemToDelete);
-            System.out.println("    ____________________________________________________________\n" +
-                    "     Noted. I've removed this task:\n" +
-                    "     " + itemDeleted + "\n" +
-                    "     " + "Now you have " + userInputRecord.size() + " tasks in the list.\n" +
-                    "    ____________________________________________________________");
+            autoSave();
+            System.out.println(formatMessage("Noted. I've removed this task:\n" +
+                    subIndentation+ itemDeleted + "\n" + indentation +
+                    "Now you have " + userInputRecord.size() + " tasks in the list.\n"));
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("    ____________________________________________________________\n" +
-                    "      Oops, the ID of the task does not exist\n" +
-                    "    ____________________________________________________________");
+            System.out.println(formatMessage("Oops, the ID of the task does not exist!\n"));
+        } catch (NumberFormatException e) {
+            System.out.println(formatMessage("Please enter a valid ID!\n"));
         }
     }
 
-    public static void main(String[] args) {
-        String greeting = "    ____________________________________________________________\n" +
-                "     Hello! I'm Peoduo\n" +
-                "     Can I help you?\n" +
-                "    ____________________________________________________________";
-        System.out.println(greeting);
-        echo();
-        System.out.println("    ____________________________________________________________\n" +
-                "     Bye. Hope to see you again soon!\n" +
-                "    ____________________________________________________________");
+    private static void autoSave() {
+        boolean directoryExists = Files.exists( Paths.get("data"));
+        boolean fileExists = Files.exists(Paths.get("data","record"));
+        if(!directoryExists) {
+            try {
+                Files.createDirectory(Path.of("data"));
+            } catch (IOException e) {
+                System.out.println("Something is wrong " + e);
+            }
+        } else if(!fileExists) {
+            try {
+                Files.createFile(Path.of("data","record"));
+            } catch (IOException e) {
+                System.out.println("Something is wrong " + e);
+            }
+        }
+        try {
+            FileWriter writer = new FileWriter(Paths.get("data","record").toString());
+            for (Task task : userInputRecord) {
+                writer.write(task.toString());
+                writer.write(System.getProperty("line.separator"));
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Something is wrong " + e);
+        }
+    }
+
+    private static void autoLoad() {
+        try {
+            Scanner scanner = new Scanner(Paths.get("data","record"));
+            while (scanner.hasNextLine()) {
+                String itemInfo = scanner.nextLine();
+                if(itemInfo.startsWith("[T]")) {
+                    Task task = new ToDo(itemInfo.substring(7));
+                    if(itemInfo.contains("[X]")) {
+                        task.setDone(true);
+                    }
+                    userInputRecord.add(task);
+                } else if(itemInfo.startsWith("[D]")) {
+                    Task task = new Deadline(itemInfo.substring(7, itemInfo.indexOf("(by")),
+                            itemInfo.substring(itemInfo.indexOf("(by") + 5, itemInfo.length() -1));
+                    if(itemInfo.contains("[X]")) {
+                        task.setDone(true);
+                    }
+                    userInputRecord.add(task);
+                } else if(itemInfo.startsWith("[E]")) {
+                    Task task = new Event(itemInfo.substring(7, itemInfo.indexOf("(at")),
+                            itemInfo.substring(itemInfo.indexOf("(at") + 5, itemInfo.length() -1));
+                    if(itemInfo.contains("[X]")) {
+                        task.setDone(true);
+                    }
+                    userInputRecord.add(task);
+                }
+            }
+            scanner.close();
+        } catch (Exception ignored) {
+
+        }
+    }
+
+
+
+    private static String formatMessage(String message) {
+        return "    ____________________________________________________________\n" + indentation +
+                message + "    ____________________________________________________________";
     }
 }
