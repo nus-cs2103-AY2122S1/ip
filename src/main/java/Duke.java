15f2dd1d1eddb615ebf70d6c095e9c8dbe40c836
Wayne Tee <waynetee37@gmail.com>
2021-08-21 19:05:25 +0800

    Add Parser

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,114 +1,39 @@
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Duke {
     static TaskList tasks = new TaskList();
+    static Parser parser = new Parser(tasks);
     static Storage storage;
 
     public static void main(String[] args) {
-        greet();
+        Ui.show("Hello! I'm Duke", "What can I do for you?");
         loadStorage();
         while (true) {
             try {
-                String input = Ui.nextLine();
-                if (input.equals("bye")) {
+                String command = Ui.nextLine();
+                if (parser.isExit(command)) {
                     Ui.show("Bye. Hope to see you again soon!");
                     storage.close();
                     return;
                 } else {
-                    Ui.show(parse(input));
+                    Ui.show(parser.parse(command));
                 }
-                storage.write(input);
+                storage.write(command);
             } catch (Exception e) {
                 Ui.show(e.getMessage());
             }
         }
     }
 
-    static void greet() {
-        Ui.show("Hello! I'm Duke", "What can I do for you?");
-    }
-
     static void loadStorage() {
         try {
             storage = new Storage("duke.txt");
-            List<String> commands = storage.readAllLines();
-            for (String command : commands)
-                parse(command);
+            for (String command : storage.readAllLines())
+                parser.parse(command);
         } catch (IOException | DukeException e) {
             e.printStackTrace();
         }
     }
 
-    static String[] parse(String input) throws DukeException {
-        if (input.equals("list")) {
-            return list();
-        } else if (input.startsWith("done")) {
-            return done(input);
-        } else if (input.startsWith("delete")) {
-            return delete(input);
-        } else {
-            return addTask(input);
-        }
-    }
-
-    static String[] list() {
-        List<String> out = new ArrayList<>();
-        out.add("Here are the tasks in your list:");
-        for (int i = 0; i < tasks.size(); i++) {
-            out.add(String.format("%d.%s", i + 1, tasks.get(i)));
-        }
-        return out.toArray(new String[0]);
-    }
-
-    static String[] done(String input) {
-        int ind = Integer.parseInt(input.split(" ", 2)[1]);
-        Task task = tasks.get(ind - 1);
-        task.setDone();
-        return new String[]{"Nice! I've marked this task as done:", "  " + task};
-    }
-
-    static String[] addTask(String input) throws DukeException {
-        Task task = makeTask(input);
-        tasks.add(task);
-        return new String[]{
-                "Got it. I've added this task:", "  " + task, getTasksLeftMsg()};
-    }
-
-    static Task makeTask(String input) throws DukeException {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
-        String[] parts = input.split(" ", 2);
-        String type = parts[0];
-        switch (type) {
-        case "todo":
-            if (parts.length == 1) {
-                throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
-            }
-            return new Todo(parts[1]);
-        case "deadline": {
-            String[] subparts = parts[1].split(" /by ", 2);
-            return new Deadline(subparts[0], LocalDateTime.parse(subparts[1], formatter));
-        }
-        case "event": {
-            String[] subparts = parts[1].split(" /at ", 2);
-            return new Event(subparts[0], LocalDateTime.parse(subparts[1], formatter));
-        }
-        default:
-            throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-        }
-    }
-
-    static String[] delete(String input) {
-        int ind = Integer.parseInt(input.split(" ", 2)[1]);
-        Task task = tasks.remove(ind - 1);
-        return new String[]{"Noted. I've removed this task:", "  " + task, getTasksLeftMsg()};
-    }
-
-    static String getTasksLeftMsg() {
-        return String.format("Now you have %d %s in the list.", tasks.size(), tasks.size() == 1 ? "task" : "tasks");
-    }
 
 }
---
+++ src/main/java/Parser.java
@@ -0,0 +1,84 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+    TaskList tasks;
+
+    Parser(TaskList taskList) {
+        tasks = taskList;
+    }
+
+    boolean isExit(String input) {
+        return input.equals("bye");
+    }
+
+    String[] parse(String input) throws DukeException {
+        if (input.equals("list")) {
+            return list();
+        } else if (input.startsWith("done")) {
+            return done(input);
+        } else if (input.startsWith("delete")) {
+            return delete(input);
+        } else {
+            return addTask(input);
+        }
+    }
+
+    String[] list() {
+        List<String> out = new ArrayList<>();
+        out.add("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            out.add(String.format("%d.%s", i + 1, tasks.get(i)));
+        }
+        return out.toArray(new String[0]);
+    }
+
+    String[] done(String input) {
+        int ind = Integer.parseInt(input.split(" ", 2)[1]);
+        Task task = tasks.get(ind - 1);
+        task.setDone();
+        return new String[]{"Nice! I've marked this task as done:", "  " + task};
+    }
+
+    String[] addTask(String input) throws DukeException {
+        Task task = makeTask(input);
+        tasks.add(task);
+        return new String[]{
+                "Got it. I've added this task:", "  " + task, getTasksLeftMsg()};
+    }
+
+    Task makeTask(String input) throws DukeException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        String[] parts = input.split(" ", 2);
+        String type = parts[0];
+        switch (type) {
+        case "todo":
+            if (parts.length == 1) {
+                throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+            }
+            return new Todo(parts[1]);
+        case "deadline": {
+            String[] subparts = parts[1].split(" /by ", 2);
+            return new Deadline(subparts[0], LocalDateTime.parse(subparts[1], formatter));
+        }
+        case "event": {
+            String[] subparts = parts[1].split(" /at ", 2);
+            return new Event(subparts[0], LocalDateTime.parse(subparts[1], formatter));
+        }
+        default:
+            throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+    }
+
+    String[] delete(String input) {
+        int ind = Integer.parseInt(input.split(" ", 2)[1]);
+        Task task = tasks.remove(ind - 1);
+        return new String[]{"Noted. I've removed this task:", "  " + task, getTasksLeftMsg()};
+    }
+
+    String getTasksLeftMsg() {
+        return String.format("Now you have %d %s in the list.", tasks.size(), tasks.size() == 1 ? "task" : "tasks");
+    }
+}
