53a795dd60d3f1dadb0cf26b9263eeb84ac84269
Frederick Pek <ssixthlhm@gmail.com>
2021-08-22 00:06:00 +0800

    Add minimal support of LocalDate to Deadline class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,27 +1,43 @@
 import java.util.*;
+import java.time.*;
+import java.time.format.*;
 
 public class Deadline extends Task {
 	private String by;
+	private LocalDate date;
 
 	public static Deadline init(String line) {
 		int pos = line.indexOf("/by");
 		if (pos == -1 || line.length() < pos + 5) {
 			throw new TaskException(new Deadline("?", "?"));
 		}
-		return new Deadline(line.substring(9, pos), line.substring(pos+4));
+		return new Deadline(line.substring(9, pos-1), line.substring(pos+4));
 	}
 
 	public Deadline(String description, String by) {
 		super(description);
 		this.type = "Deadline";
-		this.by = by;
+
+		try {
+			this.date = LocalDate.parse(by);
+			this.by = date.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+		} catch (DateTimeParseException ex) {
+			this.date = null;
+			this.by = by;
+		}
 	}
 
 	public String getFormat() {
 		return "deadline {description} /by {by when?}";
 	}
 
+	public List<String> getSaveParameters() {
+		List<String> params = super.getSaveParameters();
+		params.add(by);
+		return params;
+	}
+
 	public String toString() {
-		return super.toString() + "(by: " + by + ")";
+		return super.toString() + " (by: " + by + ")";
 	}
 }
