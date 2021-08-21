683932773c563d31553cc649c9dddbde56662fd0
marcuspeh <hkmpeh@gmail.com>
2021-08-21 23:42:10 +0800

    Add test files for package duke.task

---
+++ src/test/java/duke/task/ToDosTest.java
@@ -0,0 +1,34 @@
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ToDosTest {
+    @Test
+    public void testToStringConversion(){
+        ToDos todo1 = new ToDos("CS2103");
+        assertEquals("[T][ ] CS2103", todo1.toString());
+
+        ToDos todo2 = new ToDos("CS2103", true);
+        assertEquals("[T][X] CS2103", todo2.toString());
+
+        ToDos todo3 = new ToDos("CS2103", false);
+        assertEquals("[T][ ] CS2103", todo3.toString());
+}
+
+    @Test
+    public void testSaveOutputConversion() {
+            ToDos todo1 = new ToDos("CS2103");
+            assertEquals("T | CS2103 | 0", todo1.saveOutput());
+
+            ToDos todo2 = new ToDos("CS2103", true);
+            assertEquals("T | CS2103 | 1", todo2.saveOutput());
+
+            ToDos todo3 = new ToDos("CS2103", false);
+            assertEquals("T | CS2103 | 0", todo3.saveOutput());
+    }
+}
