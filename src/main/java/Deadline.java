219319469a2eb1867d450a3b9a529a5168ec3ecf
BryannYeap <e0543723@u.nus.edu>
2021-08-21 21:58:08 +0800

    Add Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,6 +1,11 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task{
 
     private String by;
+    private LocalDate date;
 
     /**
      * A constructor for this deadline Task.
@@ -8,9 +13,17 @@ public class Deadline extends Task{
      * @param description the description of what the task is.
      * @param by the specific date/time that this task has to be done by.
      */
-    public Deadline(String description, String by) {
+    public Deadline(String description, String by) throws InvalidParamException {
         super(description);
         this.by = by;
+
+        try {
+            this.date = LocalDate.parse(by);
+            this.by = date.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+        } catch (DateTimeParseException e) {
+            throw new InvalidParamException("\n\nThe deadline should be a valid date in the form: yyyy-mm-dd\n"
+                    + "i.e. 2021-12-25");
+        }
     }
 
     /**
