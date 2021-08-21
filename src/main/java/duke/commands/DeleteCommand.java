8dcd98fb04e1bb1995fa750ca5ac4fc826da14d7
dannylty <dannylty98@gmail.com>
2021-08-22 01:40:32 +0800

    Refactor all commands

--- src/main/java/duke/commands/DeleteCommand.java
+++ src/main/java/duke/commands/DeleteCommand.java
@@ -1,6 +1,8 @@
 package duke.commands;
 
+import duke.Duke;
 import duke.Item;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class DeleteCommand extends Command {
@@ -8,11 +10,15 @@ public class DeleteCommand extends Command {
 
     @Override
     public void parseArg(String line) {
-        this.index = 0;
+        this.index = Integer.valueOf(line.substring(7)) - 1;
     }
 
     @Override
     public void execute(LinkedList<Item> itemList) {
         System.out.println("Invoked DeleteCommand");
+        ArrayList<String> printBuffer = new ArrayList<>();
+        printBuffer.add("Noted. I've removed this task:");
+        printBuffer.add("  " + itemList.remove(this.index).toString());
+        System.out.println(Duke.styleResponse(printBuffer));
     }
 }
--- src/main/java/duke/commands/DoneCommand.java
+++ src/main/java/duke/commands/DoneCommand.java
@@ -1,16 +1,27 @@
 package duke.commands;
 
+import duke.Duke;
 import duke.Item;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class DoneCommand extends Command {
+    private int index;
 
     @Override
     public void parseArg(String args) {
+        this.index = Integer.valueOf(args.substring(5)) - 1;
     }
 
     @Override
     public void execute(LinkedList<Item> itemList) {
         System.out.println("Invoked DoneCommand");
+
+        Item x = itemList.get(this.index);
+        ArrayList<String> printBuffer = new ArrayList<>();
+        printBuffer.add("Nice! I've marked this task as done:");
+        x.markAsDone();
+        printBuffer.add("  " + x.toString());
+        System.out.println(Duke.styleResponse(printBuffer));
     }
 }
