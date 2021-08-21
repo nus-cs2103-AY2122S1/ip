230da5c48473606c93be550b65b1212b0193c650
Imran <imranr2@msn.com>
2021-08-21 12:16:53 +0800

    docs: Add Javadoc for ToDo

--- src/main/java/lifeline/task/ToDo.java
+++ src/main/java/lifeline/task/ToDo.java
@@ -1,11 +1,26 @@
 package lifeline.task;
 
+/**
+ * The class ToDo represents a task with no deadline
+ * It is a subclass of Task
+ */
 public class ToDo extends Task {
 
+    /**
+     * Default constructor for a ToDo
+     *
+     * @param name Name of ToDo
+     */
     public ToDo(String name) {
         super(name);
     }
 
+    /**
+     * Constructor to be used when loading saved ToDo
+     *
+     * @param name Name of ToDo
+     * @param isDone Indicate whether the ToDo is completed
+     */
     public ToDo(String name, boolean isDone) {
         super(name, isDone);
     }
