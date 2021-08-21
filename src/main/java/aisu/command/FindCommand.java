c5730d6aa0c1565ba127254b7f0d3de402e2cd0e
Lemonsr <46766154+Lemonsr@users.noreply.github.com>
2021-08-21 22:20:58 +0800

    FindCommand class: Add javadoc

--- src/main/java/aisu/command/FindCommand.java
+++ src/main/java/aisu/command/FindCommand.java
@@ -5,6 +5,11 @@ import aisu.Storage;
 import aisu.TaskList;
 import aisu.Ui;
 
+/**
+ * Command to find tasks with a keyword from the tasklist.
+ *
+ * @author Liaw Xin Yan
+ */
 public class FindCommand extends Command {
     private final String input;
 
@@ -12,12 +17,24 @@ public class FindCommand extends Command {
         this.input = input.trim();
     }
 
+    /**
+     * Finds tasks with a keyword and displays them to user.
+     *
+     * @param tasklist TaskList used in Aisu.
+     * @param storage  Storage used in Aisu.
+     * @param ui       User interface used in Aisu.
+     */
     @Override
-    public void execute(TaskList tasklist, Storage storage, Ui ui) throws AisuException {
+    public void execute(TaskList tasklist, Storage storage, Ui ui) {
         String result = tasklist.findTasksWith(this.input);
         ui.showToUser("Here's what I found:", result);
     }
-
+    
+    /**
+     * Checks if the command is an Exit command.
+     *
+     * @return True if it is an Exit command.
+     */
     @Override
     public boolean isExit() {
         return false;
