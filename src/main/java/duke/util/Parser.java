c088aed0cbd566295c5a55eb6ac8ddf4b98f5755
Siew Hui Zhuan <huizhuansam@hotmail.com>
2021-08-21 13:35:23 +0800

    Add Javadocs to utility classes

--- src/main/java/duke/util/Parser.java
+++ src/main/java/duke/util/Parser.java
@@ -11,12 +11,26 @@ import duke.exception.DukeException;
 import duke.exception.EmptyCommandException;
 import duke.exception.UnknownCommandException;
 
-/** Utility class that handles parsing of strings to other useful data structures. */
+/** Utility class that handles parsing of user input to program command. */
 public class Parser {
+    /**
+     * Returns a Commands enum type from the user's full command input by regex splitting the command string and
+     * returning the first word.
+     *
+     * @param fullCommand The full command input from the user.
+     * @return Commands enum type.
+     */
     private static Commands commandParser(String fullCommand) {
         return Commands.valueOfLabel(fullCommand.trim().split(" ")[0]);
     }
 
+    /**
+     * Returns a string containing the content of the full command input by removing the command keyword and returning
+     * the remainder of the string using regex.
+     *
+     * @param fullCommand The full command input from the user.
+     * @return The content of the command as string.
+     */
     private static String contentParser(String fullCommand) {
         String[] token = fullCommand.trim().split(" ", 2);
         if (token.length < 2) {
@@ -25,12 +39,25 @@ public class Parser {
         return token[1];
     }
 
+    /**
+     * Checks for empty user input.
+     *
+     * @param fullCommand The full command input from the user.
+     * @throws EmptyCommandException If the command is empty or consists of only whitespace.
+     */
     private static void blankCommandChecker(String fullCommand) throws EmptyCommandException {
         if (fullCommand.trim().length() < 1) {
             throw new EmptyCommandException();
         }
     }
 
+    /**
+     * Parses the user's full input to Command object.
+     *
+     * @param fullCommand The full command input from the user.
+     * @return A Command object.
+     * @throws DukeException If an exception is found.
+     */
     public static Command parse(String fullCommand) throws DukeException {
         blankCommandChecker(fullCommand);
         Command command;
