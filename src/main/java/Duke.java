c1afbed219670aab473bf679b5346f017fb1ed7f
lowkangn <e0543462@u.nus.edu>
2021-08-22 14:08:37 +0800

    Add enums for tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,57 +2,54 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    enum TaskType {
+        TODO,
+        DEADLINE,
+        EVENT,
+        UNKNOWN
+    }
+
+    private final ArrayList<Task> taskList;
+    private int taskIndex;
+
+    public Duke() {
+        this.taskList = new ArrayList<>();
+        this.taskIndex = 0;
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        boolean dukeOpen = true;
-        ArrayList<Task> taskList = new ArrayList<>();
-        int taskIndex = 0;
+        Duke duke = new Duke();
+        boolean isDukeOpen = true;
 
-        System.out.println("Hello! I'm Duke\n" +
-                "What can I do for you?");
-        while (dukeOpen) {
+        System.out.println("Hello! I'm Duke\n"
+                + "What can I do for you?");
+
+        while (isDukeOpen) {
             try {
                 String userInput = sc.nextLine();
                 if (userInput.equals("bye")) {
-                    dukeOpen = false;
+                    //Closes the program.
+                    isDukeOpen = false;
                     System.out.println("Bye. Hope to see you again soon!");
 
                 } else if (userInput.equals("list")) {
                     // List all tasks in the task list.
-                    int i = 0;
                     System.out.println("Here are the tasks in your list:");
-                    for (Task task : taskList) {
-                        if (task != null) {
-                            System.out.println(++i + "." + task.toString());
-                        } else {
-                            break;
-                        }
-                    }
-                    if (i == 0) {
-                        System.out.println("Your list is currently empty.");
-                    }
+                    duke.listTasks();
 
                 } else if (userInput.startsWith("done")) {
                     // Mark a certain task as done.
                     String[] splitString = userInput.split(" ");
+
                     if (splitString.length > 1) {
-
                         int taskNumber = Integer.parseInt(splitString[1].trim());
-
-                        try {
-                            Task doneTask = taskList.get(taskNumber - 1);
-                            doneTask.markDone();
-                            System.out.println("Nice! I've marked this task as done:\n" + "  " +
-                                    doneTask.toString());
-
-                        } catch (Exception e) {
-                            throw new DukeException("☹ OOPS!!! That task does not exist.");
-                        }
+                        duke.markTaskAsDone(taskNumber);
 
                     } else {
-                        throw new DukeException("☹ OOPS!!! Please state which task number " +
-                                "you want to mark as done.");
+                        throw new DukeException("☹ OOPS!!! Please state which task number "
+                                + "you want to mark as done.");
                     }
 
                 } else if (userInput.startsWith("delete")) {
@@ -60,73 +57,135 @@ public class Duke {
                     String[] splitString = userInput.split(" ", 2);
 
                     if (splitString.length > 1 && splitString[1].trim().length() > 0) {
-
                         int taskNumber = Integer.parseInt(splitString[1].trim());
-
-                        try {
-                            Task deletedTask = taskList.get(taskNumber - 1);
-                            taskList.remove(taskNumber - 1);
-                            taskIndex--;
-                            String taskCount = (taskIndex == 1) ? "1 task" : taskIndex + " tasks";
-                            System.out.println("Noted. I've removed this task:\n" + "  " +
-                                    deletedTask.toString() + "\n" + "Now you have " + taskCount +
-                                    " in the list.");
-
-                        } catch (Exception e) {
-                            throw new DukeException("☹ OOPS!!! That task does not exist.");
-                        }
+                        duke.deleteTask(taskNumber);
 
                     } else {
-                        throw new DukeException("☹ OOPS!!! Please state which task number you want to delete.");
+                        throw new DukeException("☹ OOPS!!! Please state which task number"
+                                + "you want to delete.");
                     }
 
-                } else if (userInput.startsWith(("todo")) || userInput.startsWith("deadline")
-                        || userInput.startsWith("event")) {
-                    // Adds a Task to the task list.
-                    Task newTask;
-                    String[] splitString = userInput.split(" ", 2);
+                } else {
+                    TaskType type = userInput.startsWith(("todo"))
+                            ? TaskType.TODO
+                            : userInput.startsWith("deadline")
+                            ? TaskType.DEADLINE
+                            : userInput.startsWith("event")
+                            ? TaskType.EVENT
+                            : TaskType.UNKNOWN;
 
-                    if (splitString.length > 1 && splitString[1].trim().length() > 0) {
-                        String substring = splitString[1].trim();
+                    if (type == TaskType.UNKNOWN) {
+                        // Incorrect user input.
+                        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
 
-                        if (userInput.startsWith("todo")) {
-                            newTask = new ToDo(substring);
+                    } else {
+                        // Adds a Task to the task list.
+                        String[] splitString = userInput.split(" ", 2);
 
-                        } else if (userInput.startsWith("deadline")) {
-                            String[] nameAndTime = substring.split(" /by ");
-                            if (nameAndTime.length > 1 && nameAndTime[1].trim().length() > 0) {
-                                newTask = new Deadline(nameAndTime[0], nameAndTime[1]);
-                            } else {
-                                throw new DukeException("☹ OOPS!!! Please provide a date or " +
-                                        "time for the deadline.");
+                        if (splitString.length > 1 && splitString[1].trim().length() > 0) {
+                            String substring = splitString[1].trim();
+                            Task newTask;
+                            switch (type) {
+                                case TODO:
+                                    newTask = new ToDo(substring);
+                                    break;
+                                case DEADLINE:
+                                    String[] nameAndDeadline = substring.split(" /by ");
+                                    if (nameAndDeadline.length > 1 && nameAndDeadline[1].trim().length() > 0) {
+                                        newTask = new Deadline(nameAndDeadline[0], nameAndDeadline[1]);
+                                    } else {
+                                        throw new DukeException("☹ OOPS!!! Please provide a date or "
+                                                + "time for the deadline.");
+                                    }
+                                    break;
+                                default:
+                                    String[] nameAndTime = substring.split(" /at ");
+                                    if (nameAndTime.length > 1 && nameAndTime[1].trim().length() > 0) {
+                                        newTask = new Event(nameAndTime[0], nameAndTime[1]);
+                                    } else {
+                                        throw new DukeException("☹ OOPS!!! Please provide a date or "
+                                                + "time for the event.");
+                                    }
+                                    break;
                             }
+                            duke.addTask(newTask);
 
                         } else {
-                            String[] nameAndTime = substring.split(" /at ");
-                            if (nameAndTime.length > 1 && nameAndTime[1].trim().length() > 0) {
-                                newTask = new Event(nameAndTime[0], nameAndTime[1]);
-                            } else {
-                                throw new DukeException("☹ OOPS!!! Please provide a date or " +
-                                        "time for the event.");
-                            }
+                            throw new DukeException("☹ OOPS!!! The description of a " + splitString[0]
+                                    + " cannot be empty.");
                         }
-
-                        taskList.add(taskIndex, newTask);
-                        taskIndex++;
-                        String taskCount = (taskIndex == 1) ? "1 task" : taskIndex + " tasks";
-                        System.out.println("Got it. I've added this task:\n" + "  " + newTask.toString() +
-                                "\n" + "Now you have " + taskCount + " in the list.");
-
-                    } else {
-                        throw new DukeException("☹ OOPS!!! The description of a " + splitString[0] +
-                                " cannot be empty.");
                     }
-                } else {
-                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
             } catch (DukeException de) {
                 System.out.println(de.getMessage());
             }
         }
+    }
+
+    /**
+     * Lists the current tasks in the task list.
+     */
+    public void listTasks() {
+        int i = 0;
+        for (Task task : this.taskList) {
+            if (task != null) {
+                System.out.println(++i + "." + task.toString());
+            } else {
+                break;
+            }
+        }
+        if (i == 0) {
+            System.out.println("Your list is currently empty.");
+        }
+    }
+
+    /**
+     * Marks a certain task in the task list as done, using its task number.
+     *
+     * @param taskNumber The number of the task in the task list.
+     * @throws DukeException If the task number does not exist.
+     */
+    public void markTaskAsDone(int taskNumber) throws DukeException {
+        try {
+            Task doneTask = this.taskList.get(taskNumber - 1);
+            doneTask.markDone();
+            System.out.println("Nice! I've marked this task as done:\n" + "  "
+                    + doneTask.toString());
+        } catch (Exception e) {
+            throw new DukeException("☹ OOPS!!! That task does not exist.");
+        }
+    }
+
+    /**
+     * Deletes a certain task from the task list, using its task number.
+     *
+     * @param taskNumber The number of the task in the task list.
+     * @throws DukeException If the task number does not exist.
+     */
+    public void deleteTask(int taskNumber) throws DukeException {
+        try {
+            Task deletedTask = this.taskList.get(taskNumber - 1);
+            this.taskList.remove(taskNumber - 1);
+            this.taskIndex--;
+            String taskCount = (this.taskIndex == 1) ? "1 task" : this.taskIndex + " tasks";
+            System.out.println("Noted. I've removed this task:\n" + "  "
+                    + deletedTask.toString() + "\n" + "Now you have " + taskCount
+                    + " in the list.");
+
+        } catch (Exception e) {
+            throw new DukeException("☹ OOPS!!! That task does not exist.");
+        }
+    }
+
+    /**
+     * Adds a task to the task list.
+     * @param task The task to be added to the task list.
+     */
+    public void addTask(Task task) {
+        this.taskList.add(task);
+        this.taskIndex++;
+        String taskCount = (this.taskIndex == 1) ? "1 task" : this.taskIndex + " tasks";
+        System.out.println("Got it. I've added this task:\n" + "  " + task.toString()
+                + "\n" + "Now you have " + taskCount + " in the list.");
     }
 }
