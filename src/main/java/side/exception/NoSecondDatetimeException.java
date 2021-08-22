6b4b83be575a113bb55d02cdb3ae430e607866eb
luayida99 <77190041+luayida99@users.noreply.github.com>
2021-08-22 22:28:40 +0800

    Add back deleted file to resolve conflict

---
+++ src/main/java/side/exception/NoSecondDatetimeException.java
@@ -0,0 +1,9 @@
+package side.exception;
+
+public class NoSecondDatetimeException extends SideException {
+
+    public NoSecondDatetimeException() {
+        super("Don't know when your event ends...");
+    }
+}
+
