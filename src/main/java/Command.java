1a217600e4d6d9994268761a9c03c8fdb5bb869d
bklimey <bklim@comp.nus.edu.sg>
2021-08-23 00:36:13 +0800

    A-Enums

---
+++ src/main/java/Command.java
@@ -0,0 +1,37 @@
+public enum Command {
+    DONE {
+        public String toString() {
+            return "done";
+        }
+    },
+    BYE {
+        public String toString() {
+            return "bye";
+        }
+    },
+    LIST {
+        public String toString() {
+            return "list";
+        }
+    },
+    DELETE {
+        public String toString() {
+            return "delete";
+        }
+    },
+    TODO {
+        public String toString() {
+            return "todo";
+        }
+    },
+    DEADLINE {
+        public String toString() {
+            return "deadline";
+        }
+    },
+    EVENT {
+        public String toString() {
+            return "event";
+        }
+    },
+}
