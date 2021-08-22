29250b719f341f83a11d10ee2b50a98f0b876cfd
xlzior <wenjun.lye@gmail.com>
2021-08-22 16:54:57 +0800

    Level 8: Parse date and time information into LocalDate objects

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,24 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    protected String by;
-    public Deadline(String name, String by) {
+    protected LocalDate by;
+    public Deadline(String name, String by) throws IrisException {
         super(name);
-        this.by = by;
+        try {
+            this.by = LocalDate.parse(by);
+        } catch (DateTimeParseException exception) {
+            throw new IrisException("Invalid date provided.");
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), this.by);
+        return String.format(
+                "[D]%s (by: %s)",
+                super.toString(),
+                this.by.format(DateTimeFormatter.ofPattern("MMM d yyyy"))
+        );
     }
 }
