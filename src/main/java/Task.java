7798d462242bfdc7b5a12fbdf08f099664aa619f
En Rong <53928333+chownces@users.noreply.github.com>
2021-08-22 19:00:57 +0800

    Add save to hard disk functionality

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,18 @@
 /**
  * This class encapsulates a task in the Duke application.
  */
-public class Task {
+public abstract class Task {
     private final String description;
     private boolean isDone;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+    }
+
+    public Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
     }
 
     /**
@@ -21,4 +26,6 @@ public class Task {
     public String toString() {
         return String.format("[%s] %s", isDone ? "X" : " ", this.description);
     }
+
+    public String toSaveFormat() { return String.format("%s,%s", isDone ? "1" : "0", this.description); };
 }
