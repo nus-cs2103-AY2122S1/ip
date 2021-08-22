2f996c3f724df2e9ff0436d02e8daadcbb3b965a
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-08-22 17:08:26 +0800

    Update tests to delete data before starting tests

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,6 +6,9 @@ if not exist ..\bin mkdir ..\bin
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
+REM delete data from previous run
+if exist data rmdir data /s /q
+
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\duke\Duke.java
 IF ERRORLEVEL 1 (
