4c843084765d01dc83e11924b289f10e99c9f037
yzhilong <44795294+yzhilong@users.noreply.github.com>
2021-08-22 19:37:04 +0800

    Add methods to output representation of task data

--- src/main/java/Tasks/Deadline.java
+++ src/main/java/Tasks/Deadline.java
@@ -1,4 +1,5 @@
 package Tasks;
+import java.io.File;
 
 public class Deadline extends Task {
     private String timeInfo;
@@ -8,5 +9,10 @@ public class Deadline extends Task {
     }
     public String toString() {
         return String.format("[D]%s (by: %s)", super.toString(), this.timeInfo);
+    }
+
+    @Override
+    public String dataToString() {
+        return String.format("D | %s | %s", super.dataToString(), this.timeInfo);
     }
 }
--- src/main/java/Tasks/Event.java
+++ src/main/java/Tasks/Event.java
@@ -1,5 +1,7 @@
 package Tasks;
 
+import java.io.File;
+
 public class Event extends Task {
     private String timeInfo;
     public Event(String taskDetails, String timeInfo) {
@@ -10,5 +12,10 @@ public class Event extends Task {
     @Override
     public String toString() {
         return String.format("[E]%s (at: %s)", super.toString(), this.timeInfo);
+    }
+
+    @Override
+    public String dataToString() {
+        return String.format("E | %s | %s", super.dataToString(), this.timeInfo);
     }
 }
