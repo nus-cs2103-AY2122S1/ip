3b5f7b17202c28aea6153be2533ea557b382fc4c
eezj35 <justineezj0@gmail.com>
2021-08-22 20:08:55 +0800

    Implement Level-8

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,5 +1,10 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadlines extends Task{
     protected String by;
+    protected LocalDateTime localDateTime;
 
     public Deadlines(String description, String by){
         super(description);
@@ -8,12 +13,21 @@ public class Deadlines extends Task{
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + by + ")";
+        setLocalDateAndTime();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+        return "[D]" + super.toString() + "(by: " + localDateTime.format(dtf) + ")";
     }
 
     @Override
     public void displayTask(){
         System.out.println(toString());
+    }
+
+    private void setLocalDateAndTime() {
+        int whitespaceIdx = by.indexOf(" ");
+        String date = by.substring(0, whitespaceIdx);
+        String time = by.substring(whitespaceIdx+1);
+        localDateTime = LocalDateTime.parse(date+"T"+time);
     }
 }
 
--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -1,5 +1,10 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Events extends Task{
     protected String at;
+    protected LocalDateTime localDateTime;
 
     public Events(String description, String at){
         super(description);
@@ -8,11 +13,21 @@ public class Events extends Task{
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + at + ")";
+        setLocalDateAndTime();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+        return "[E]" + super.toString() + "(at: " + localDateTime.format(dtf) + ")";
     }
 
     @Override
     public void displayTask(){
         System.out.println(toString());
     }
+
+    private void setLocalDateAndTime() {
+        int whitespaceIdx = at.indexOf(" ");
+        String date = at.substring(0, whitespaceIdx);
+        String time = at.substring(whitespaceIdx+1);
+        localDateTime = LocalDateTime.parse(date+"T"+time);
+    }
+
 }
