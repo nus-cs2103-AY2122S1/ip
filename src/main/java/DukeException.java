8ad7385c74a24d4eed69ce6e7a024a7a825d2c97
Gordon25 <73068242+Gordon25@users.noreply.github.com>
2021-08-23 01:29:57 +0800

    Change exception message for StringIndexOutOfBoundsException Exception

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -17,7 +17,7 @@ public class DukeException extends Exception{
         if (e instanceof ArrayIndexOutOfBoundsException) {
             message = "☹ OOPS!!! The task does not exist.\n";
         } else if (e instanceof StringIndexOutOfBoundsException) {
-            message = "☹ OOPS!!! The description of a task cannot be empty.\n";
+            message = "☹ OOPS!!! The task description or command is incomplete.\n";
         } else {
             message = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n";
         }
