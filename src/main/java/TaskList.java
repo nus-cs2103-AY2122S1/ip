8e51efdd4161368828fc3515405f5b55e257a48e
NaokiShum <cxjdwedyk097@gmail.com>
2021-08-22 19:09:05 +0800

    Added TaskList class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class TaskList {
+import java.util.ArrayList;
+
+public class TaskList {
+    protected ArrayList<Task> list;
+
+    TaskList(ArrayList<Task> list){
+        this.list = list;
+    }
+
+    public void add(Task t){
+        list.add(t);
+    }
+
+    public Task get(int getNum){
+        return list.get(getNum);
+    }
+
+    public int size(){
+        return list.size();
+    }
+
+    public void remove(int delNum){
+        list.remove(delNum);
+    }
+
 }
