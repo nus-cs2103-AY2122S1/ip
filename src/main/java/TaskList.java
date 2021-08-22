65b22d8a70a0bb63f4fdc504779f2ce75bbac614
GnohChengYi <chengyignoh@gmail.com>
2021-08-22 22:37:57 +0800

    Save tasks in hard disk whenever task list changes

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,8 +1,14 @@
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskList {
-    private final List<Task> tasks;
+    private List<Task> tasks;
 
     public TaskList() {
         tasks = new ArrayList<>();
@@ -10,6 +16,7 @@ public class TaskList {
 
     public void add(Task task) {
         tasks.add(task);
+        save();
     }
 
     public Task get(int i) {
@@ -17,7 +24,31 @@ public class TaskList {
     }
 
     public Task remove(int i) {
-        return tasks.remove(i - 1);
+        Task task = tasks.remove(i - 1);
+        save();
+        return task;
+    }
+
+    private void save() {
+        try {
+            Path directoryPath = Paths.get("data");
+            if (!Files.exists(directoryPath)) {
+                Files.createDirectory(directoryPath);
+            }
+            Path filePath = Paths.get("data", "blue.txt");
+            if (!Files.exists(filePath)) {
+                Files.createFile(filePath);
+            }
+            BufferedWriter writer = Files.newBufferedWriter(filePath);
+            for (Task task : tasks) {
+                writer.write(task.toString());
+                writer.newLine();
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public int size() {
