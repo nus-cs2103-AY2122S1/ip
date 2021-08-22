43407279bdba94449c8742619a91dfc97a88a675
XXJJXJ <yexinjian1506@gmail.com>
2021-08-22 16:52:49 +0800

    Add FindCommand class

---
+++ src/main/java/duke/commands/FindCommand.java
@@ -0,0 +1,31 @@
+package duke.commands;
+
+import duke.exceptions.EmptyListException;
+import duke.exceptions.TaskNotFoundException;
+import duke.Storage;
+import duke.TaskList;
+import duke.Ui;
+
+/**
+ * This is a FindCommand class that extends Command
+ */
+public class FindCommand extends Command {
+
+    /**
+     * This is the class field of a FindCommand
+     */
+    private final String keyword;
+
+    /**
+     * This is the FindCommand constructor.
+     */
+    public FindCommand(String keyword) {
+        super("find");
+        this.keyword = keyword;
+    }
+
+    @Override
+    public void execute(TaskList taskList, Storage store, Ui ui) throws EmptyListException, TaskNotFoundException {
+        taskList.printFindTasks(ui, this.keyword);
+    }
+}
