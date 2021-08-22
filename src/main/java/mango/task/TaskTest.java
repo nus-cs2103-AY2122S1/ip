60b502fbb7b0aacf7276bb155781663608e680ec
Lauren Shak <laurenshak01@gmail.com>
2021-08-22 23:06:18 +0800

    Add JUnit tests to test the behavior of the task package
    Code has no test that checks if it works correctly.
    
    JUnit automated tasks can check if the methods in the various classes such as Deadline in the task package are working as they should be.
    
    Let's
    * implement some JUnit tests in a TaskTest class
    * test the getDescription() and isValid() methods

---
+++ src/main/java/mango/task/TaskTest.java
@@ -0,0 +1,34 @@
+package mango.task;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TaskTest {
+    @Test
+    public void testDeadlineDescription() {
+        assertEquals("abc (by: Oct 15 2009)", new Deadline("abc", "2009-10-15").getDescription());
+    }
+
+    @Test
+    public void testEventDescription() {
+        assertEquals("abc (at: Mon)", new Event("abc", "Mon").getDescription());
+    }
+
+    @Test
+    public void testTodoDescription() {
+        assertEquals("abc", new Todo("abc").getDescription());
+    }
+
+    @Test
+    public void testDeadline_isValid_exceptionThrown() {
+        try {
+            assertEquals(false, Deadline.isValid(new String[] {"deadline"}));
+            fail(); // the test should not reach this line
+        } catch (Exception e) {
+            assertEquals(" ☹ OOPS!!! The description of a deadline cannot be empty.", e.getMessage());
+        }
+    }
+
+
+}
