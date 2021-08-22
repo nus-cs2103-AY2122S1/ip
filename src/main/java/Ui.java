acc19dc6b39c7ac0089a5e32a1d62fdead7b3e2e
Py0000 <>
2021-08-22 13:27:30 +0800

    Add Ui class to handle interactions of chat bot with user.

---
+++ src/main/java/Ui.java
@@ -0,0 +1,84 @@
+import tasks.Task;
+
+import java.util.ArrayList;
+
+public class Ui {
+    // Lines used to indicate a block of message
+    private static final  String HORIZONTAL_LINE_HEAD = "\t____________________________________________________________";
+    private static final String HORIZONTAL_LINE_TAIL = String.format("\n%s\n", HORIZONTAL_LINE_HEAD);
+
+    private enum Message {
+        WELCOME_MSG("Hello! I am Matthew!\n\t What can I do for you?"),
+        EXIT_MSG("Good Bye. Don't have a good day... Have a great day!!!"),
+        DISPLAY_LIST_MSG("\t There are the tasks you have:"),
+        TASK_DONE_MSG("Well done! You have completed: \n\t  "),
+        TASK_ADDED_MSG("Got it. I've added this task:\n\t   "),
+        TASK_DELETED_MSG("Got it. I've deleted this task:\n\t   "),
+        CHECK_TASK_COUNT_MSG(" tasks are in your list now!");
+
+
+        private final String msg;
+
+        Message(String msg) {
+            this.msg = msg;
+        }
+    }
+
+    private static void wrapMessageInLines(String msg) {
+        String formattedMsg = String.format("%s\n\t %s%s", HORIZONTAL_LINE_HEAD, msg, HORIZONTAL_LINE_TAIL);
+        System.out.println(formattedMsg);
+    }
+
+    public static void greetUser() {
+        wrapMessageInLines(Message.WELCOME_MSG.msg);
+    }
+
+    public static void exit() {
+        wrapMessageInLines(Message.EXIT_MSG.msg);
+    }
+
+    private static void displayListStart() {
+        System.out.println(String.format("%s\n%s", HORIZONTAL_LINE_HEAD, Message.DISPLAY_LIST_MSG.msg));
+    }
+
+    private static void displayListEnd() {
+        System.out.println(HORIZONTAL_LINE_TAIL);
+    }
+
+    public static void taskDoneMessage(Task task) {
+        wrapMessageInLines(Message.TASK_DONE_MSG.msg + task);
+    }
+
+    public static void displayList(ArrayList<Task> taskList) {
+        displayListStart();
+
+        for (int i = 0; i < taskList.size(); i++) {
+            Task item = taskList.get(i);
+            String itemDetails = String.format("\t %s.%s", (i + 1), item);
+
+            System.out.println(itemDetails);
+        }
+
+        displayListEnd();
+    }
+
+    public static void taskDeletedMessage(Task task, int count) {
+        String deletedTaskInfo = Message.TASK_DELETED_MSG.msg + task;
+        String updatedListInfo = String.format("%d %s", count, Message.CHECK_TASK_COUNT_MSG.msg);
+        wrapMessageInLines(deletedTaskInfo + "\n\t " + updatedListInfo);
+    }
+
+    public static void taskAddedMessage(Task task, int count) {
+        String addedTaskInfo = Message.TASK_ADDED_MSG.msg + task;
+        String updatedListInfo = String.format("%d %s", count, Message.CHECK_TASK_COUNT_MSG.msg);
+        wrapMessageInLines(addedTaskInfo + "\n\t " + updatedListInfo);
+    }
+
+    public static void notifyError(String errorMsg) {
+        wrapMessageInLines(errorMsg);
+    }
+
+
+
+
+}
