2b172f48700d3198fc2106c4ba9bf95ad168ddd6
DesmondTo <e0550619@u.nus.edu>
2021-08-23 07:36:26 +0800

    Update task list to global variable

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 
 public class Duke {
 
+    // Initialize string array to store the list
+    private static final ArrayList<Task> TASKS = new ArrayList<>();
+    private static int numOfTask = 0;
+
     enum Command {
         LIST, DONE, TODO, EVENT, DEADLINE, DELETE
     }
@@ -45,9 +49,9 @@ public class Duke {
                 task = new Deadline(descriptions[0], descriptions[1]);
                 break;
             }
-            
+
             if (task != null) {
-                tasks.add(task);
+                TASKS.add(task);
                 printAddOrDelete(true, task, ++numOfTask);
             }
         } catch (EmptyDescriptionException | IncompleteDescriptionException e) {
@@ -62,10 +66,6 @@ public class Duke {
                 "\tI'm Desmond,\n" +
                 "\thow may I serve you?\n");
 
-        // Initialize string array to store the list
-        ArrayList<Task> tasks = new ArrayList<>();
-        int numOfTask = 0;
-
         // Initialize scanner to get user input
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
@@ -78,21 +78,21 @@ public class Duke {
                 switch (Command.valueOf(command[0].toUpperCase())) {
                 case LIST:
                     Printer.prettyPrint("Here are the tasks in your list:\n" +
-                            Printer.listTask(tasks, numOfTask));
+                            Printer.listTask(TASKS, numOfTask));
                     break;
                 case DONE:
-                    tasks.get(Integer.parseInt(command[1]) - 1).markAsDone();
+                    TASKS.get(Integer.parseInt(command[1]) - 1).markAsDone();
                      break;
                 case DELETE:
-                    printAddOrDelete(false, tasks.get(Integer.parseInt(command[1]) - 1), --numOfTask);
-                    tasks.remove(Integer.parseInt(command[1]) - 1);
+                    printAddOrDelete(false, TASKS.get(Integer.parseInt(command[1]) - 1), --numOfTask);
+                    TASKS.remove(Integer.parseInt(command[1]) - 1);
                     break;
                 case TODO:
                     // Fallthrough
                 case EVENT:
                     // Fallthrough
                 case DEADLINE:
-                    addThenPrint(command, tasks, numOfTask++);
+                    addThenPrint(command, TASKS, numOfTask++);
                     break;
                 default:
                     throw new UnknownCommandException("I'm sorry, but I don't know what that means :-(");
