eb244d4133bca91d71da18ac57acbfb597ed6a0e
jodichoo <jodi.choo@gmail.com>
2021-08-23 00:41:22 +0800

    Add new Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Ui {
+    private static final String LOGO = " ____        _\n"
+            + "|  _ \\ _   _| | _____\n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private Scanner sc;
+    
+    public Ui() {
+        this.sc = new Scanner(System.in);
+    }
+    
+    public void greet() {
+        System.out.println(LOGO);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    }
+    
+    public void showError(DukeException e) {
+        e.print();
+    }
+    
+    public void displayList(TaskList list) {
+        System.out.println(list.toString());
+    }
+   
+    public String getInput() {
+        return sc.nextLine().strip(); 
+    }
+    
+    public void stopInput() {
+        System.out.println("Byebye"); 
+        sc.close(); 
+    }
+}
