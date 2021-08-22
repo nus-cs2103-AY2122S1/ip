d306899dba41864a2f743a5f12a440faeef59f52
xyliew25 <e0550381@u.nus.edu>
2021-08-22 16:01:23 +0800

    add toStringData method to Task and its subclasses

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -7,6 +7,11 @@ public class Deadline extends Task {
   }
 
   @Override
+  public String toStringData() {
+    return "D | " + super.toStringData() + " | " + this.by;
+  }
+
+  @Override
   public String toString() {
     return "[D]" + super.toString() + " (by: " + this.by + ")";
   }
