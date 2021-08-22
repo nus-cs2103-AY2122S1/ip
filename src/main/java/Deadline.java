ec95861f05f0182d9a9eb2f52b5de95d38f093fd
KT27Learn <66994197+KT27Learn@users.noreply.github.com>
2021-08-22 16:16:37 +0800

    Revert "Branch level 7"

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,8 @@
-
 /**
  * CS2103T Individual Project AY 21/22 Sem 1
  * Project Duke
  *
- * Current Progress: Level 7. Save
+ * Current Progress: A-Enums
  *
  * Description:
  * Extends the Task Class which where it is a task that needs
