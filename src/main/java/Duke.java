6177e73a10b9d0df5617e9373c07f4b763da25fc
Atin Sakkeer Hussain <atinsaki@gmail.com>
2021-08-22 14:08:01 +0800

    Seperate into Different Classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,39 +8,6 @@ import java.util.*;
 
 public class Duke {
 
-    // Colors used to Display Text
-    public static final String COLOR_RESET = "\u001B[0m";
-    public static final String COLOR_BLUE = "\u001B[34m";
-    public static final String COLOR_CYAN = "\u001B[36m";
-    public static final String COLOR_PURPLE = "\u001B[35m";
-    public static final String COLOR_RED = "\u001B[31m";
-
-    // The General Strings Used by the ChatBot
-    private static final String logo = "\n" +
-            "\t\t\t\t\t /$$      /$$ /$$                 /$$$$$$$              /$$\n" +
-            "\t\t\t\t\t| $$  /$ | $$| $$                | $$__  $$            | $$\n" +
-            "\t\t\t\t\t| $$ /$$$| $$| $$$$$$$   /$$$$$$ | $$  \\ $$  /$$$$$$  /$$$$$$\n" +
-            "\t\t\t\t\t| $$/$$ $$ $$| $$__  $$ /$$__  $$| $$$$$$$  /$$__  $$|_  $$_/\n" +
-            "\t\t\t\t\t| $$$$_  $$$$| $$  \\ $$| $$  \\ $$| $$__  $$| $$  \\ $$  | $$\n" +
-            "\t\t\t\t\t| $$$/ \\  $$$| $$  | $$| $$  | $$| $$  \\ $$| $$  | $$  | $$ /$$\n" +
-            "\t\t\t\t\t| $$/   \\  $$| $$  | $$|  $$$$$$/| $$$$$$$/|  $$$$$$/  |  $$$$/\n" +
-            "\t\t\t\t\t|__/     \\__/|__/  |__/ \\______/ |_______/  \\______/    \\___/\n";
-
-    private static final String line = "________________________________________________________________________________________________________________";
-
-    private static final String bye = "\n" +
-            "\t\t\t  /$$$$$$                            /$$ /$$$$$$$                            /$$\n" +
-            "\t\t\t /$$__  $$                          | $$| $$__  $$                          | $$\n" +
-            "\t\t\t| $$  \\__/  /$$$$$$   /$$$$$$   /$$$$$$$| $$  \\ $$ /$$   /$$  /$$$$$$       | $$\n" +
-            "\t\t\t| $$ /$$$$ /$$__  $$ /$$__  $$ /$$__  $$| $$$$$$$ | $$  | $$ /$$__  $$      | $$\n" +
-            "\t\t\t| $$|_  $$| $$  \\ $$| $$  \\ $$| $$  | $$| $$__  $$| $$  | $$| $$$$$$$$      |__/\n" +
-            "\t\t\t| $$  \\ $$| $$  | $$| $$  | $$| $$  | $$| $$  \\ $$| $$  | $$| $$_____/\n" +
-            "\t\t\t|  $$$$$$/|  $$$$$$/|  $$$$$$/|  $$$$$$$| $$$$$$$/|  $$$$$$$|  $$$$$$$       /$$\n" +
-            "\t\t\t\\______/  \\______/  \\______/  \\_______/|_______/  \\____  $$ \\_______/      |__/\n" +
-            "\t\t\t                                                   /$$  | $$\n" +
-            "\t\t\t                                                  |  $$$$$$/\n" +
-            "\t\t\t                                                   \\______/\n";
-
     // The Global Variables used by the ChatBot
     private static final Scanner cmdReader = new Scanner(System.in);
     public final UI ui;
@@ -66,7 +33,7 @@ public class Duke {
         while (true) {
             try {
                 String command;
-                System.out.print(COLOR_PURPLE + "> " + COLOR_RESET);
+                System.out.print(UI.COLOR_PURPLE + "> " + UI.COLOR_RESET);
                 command = cmdReader.nextLine().trim();
                 if (parser.parse(command, ui, storage, taskList) == -1) {
                     break;
