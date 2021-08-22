df1ec678c5e4241c85bcdb585b129b5544396134
NaokiShum <cxjdwedyk097@gmail.com>
2021-08-22 19:10:05 +0800

    Modified for more OOP

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,34 +1,34 @@
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.time.format.DateTimeParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Duke {
+
     public static void main(String[] args) throws FileNotFoundException {
-        boolean exit = false;
         String line = "____________________________________________________________\n";
-        String logo = line + " Hello! I'm Duke\n" + " What can I do for you?\n" + line;
+        Ui ui = new Ui();
+        ui.Start();
+        boolean exit = false;
         Scanner sc = new Scanner(System.in);
-        System.out.println(logo);
         String filePath = "data/duke.txt";
-        Save save = new Save(filePath);
-        ArrayList<Task> arrList = save.readFile(filePath);
-        int count = arrList.size();
+        Storage save = new Storage(filePath);
+        TaskList tasks = new TaskList(save.readFile(filePath));
+        int count = tasks.size();
 
         while(sc.hasNext()){
             String command = sc.next();
             switch(command){
                 case "bye":
-                    System.out.println(line + "Bye. Hope to see you again soon!\n" + line);
+                    ui.Bye();
                     exit = true;
                     break;
                 case "list":
                     String list = "";
                     int listNum = 1;
-                    for(Task x:arrList){
-                        list += listNum + "." + x +"\n";
+                    for(int i = 0;i<tasks.size();i++){
+                        list += listNum + "." + tasks.get(i) +"\n";
                         listNum++;
                     }
                     System.out.println(line + list + line);
@@ -36,38 +36,34 @@ public class Duke {
                 case "done":
                     int doneNum = sc.nextInt() - 1;
                     try{
-                        arrList.get(doneNum).markAsDone();
-                        System.out.println(line + "Nice! I've marked this task as done:\n" + arrList.get(doneNum));
+                        tasks.get(doneNum).markAsDone();
+                        ui.Done(tasks,doneNum);
                         break;
-                    }catch (Exception e){
-                        System.out.println("No such task");
+                    }catch (IndexOutOfBoundsException e){
+                        ui.showDoneError();
                         break;
                     }
                 case "delete":
                     int delNum = sc.nextInt()-1;
                     try{
-                        Task delete = arrList.get(delNum);
+                        Task delete = tasks.get(delNum);
                         count--;
-                        System.out.println(line + "Noted. I've removed this task:\n" + delete +
-                                "\nNow you have " + count + " tasks in the list.\n" + line);
-                        arrList.remove(delNum);
+                        ui.Delete(delete,count);
+                        tasks.remove(delNum);
                         break;
                     }catch (IndexOutOfBoundsException e){
-                        System.out.println("\n" + line +
-                                "\n☹ OOPS!!! No such task to be deleted.\n" + line);
+                        ui.showDeleteError();
                         break;
                     }
                 case "todo":
                     try {
                         Task todo = new Todo(sc.nextLine().trim(), count);
-                        arrList.add(todo);
+                        tasks.add(todo);
                         count++;
-                        System.out.println(line + "Got it. I've added this task:\n" + todo +
-                                "\nNow you have " + count + " tasks in the list.\n" + line);
+                        ui.todo(todo,count);
                         break;
                     } catch (Exception e){
-                        System.out.println("\n" + line +
-                                "\n☹ OOPS!!! The description of a todo cannot be empty.\n" + line);
+                        ui.showTodoError();
                         break;
                     }
                 case "deadline":
@@ -76,39 +72,34 @@ public class Duke {
                         LocalDate d1 = LocalDate.parse(deadlineArr[1].trim());
                         Task deadline = new Deadline(deadlineArr[0].trim(),
                                 d1.format(DateTimeFormatter.ofPattern("MMM dd YYYY")), count);
-                        arrList.add(deadline);
+                        tasks.add(deadline);
                         count++;
-                        System.out.println(line + "Got it. I've added this task:\n" + deadline +
-                                "\nNow you have " + count + " tasks in the list.\n" + line);
+                        ui.deadline(deadline,count);
                         break;
                     } catch (DateTimeParseException e){
-                        System.out.println("\n" + line +
-                                "\n☹ OOPS!!! The time must be in the format YYYY-MM-DD\n" + line);
+                        ui.showDeadlineError1();
+                        break;
                     } catch (Exception e){
-                        System.out.println("\n" + line +
-                                "\n☹ OOPS!!! The description of a deadline cannot be empty.\n" + line);
+                        ui.showDeadlineError2();
                         break;
                     }
                 case "event":
                     try {
                         String[] eventArr = sc.nextLine().split("/at");
                         Task event = new Event(eventArr[0].trim(),eventArr[1].trim(), count);
-                        arrList.add(event);
+                        tasks.add(event);
                         count++;
-                        System.out.println(line + "Got it. I've added this task:\n" + event +
-                                "\nNow you have " + count + " tasks in the list.\n" + line);
+                        ui.event(event,count);
                         break;
                     } catch (Exception e){
-                        System.out.println("\n" + line +
-                                "\n☹ OOPS!!! The description of a event cannot be empty.\n" + line);
+                        ui.showEventError();
                         break;
                     }
                 default:
-                    System.out.println("\n" + line +
-                            "\n☹ OOPS!!! I'm sorry, but I don't know what that means :-(.\n" + line);
+                    ui.defaultError();
             }
 
-            save.writeToFile(filePath,arrList);
+            save.writeToFile(filePath,tasks);
 
             if(exit){
                 break;
