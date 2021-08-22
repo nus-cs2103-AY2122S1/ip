666c124ad5c60fb4511eb80529404ba02ad05352
Yuichiro Fukushima <e0407814@u.nus.edu>
2021-08-23 00:38:58 +0900

    unit-test

--- unit-test/DukeTest.java
+++ unit-test/DukeTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import Duke.InputNotValidError;
 import Duke.Parser;
+import Duke.Ui;
 
 import static org.junit.Assert.*;
 
@@ -13,13 +14,29 @@ public class DukeTest{
        // 準備する
        Parser commandpraser = new Parser("nothing lol");
        boolean valid;
-    try {
-        valid = commandpraser.isValid();
-    } catch (InputNotValidError e) {
-        valid = false;
-    }
-       boolean expected = false;
+        try {
+            valid = commandpraser.isValid();
+        } catch (InputNotValidError e) {
+            valid = false;
+        }
+        boolean expected = false;
 
-       assertEquals(valid, expected);
+        assertEquals(valid, expected);
    }
+
+   @Test
+   public void returnGreatSent(){
+
+        Ui ui = new Ui();
+        String greatsent = ui.great();
+        String expected = 
+        " ____        _        \n"
+        + "|  _ \\ _   _| | _____ \n"
+        + "| | | | | | | |/ / _ \\\n"
+        + "| |_| | |_| |   <  __/\n"
+        + "|____/ \\__,_|_|\\_\\___|\n";
+
+        assertEquals(greatsent, expected);
+   }
+
 }
