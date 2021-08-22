6f22a8b395efb8ca822669524cd06f4378f0e702
jodichoo <jodi.choo@gmail.com>
2021-08-22 18:10:26 +0800

    Add methods to convert task to data for save file

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,11 @@
-public class Task {
+public abstract class Task {
     private String description;
     private boolean isDone;
+    private String icon; 
 
-    public Task(String description) {
+    public Task(String description, String icon) {
         this.description = description;
+        this.icon = icon; 
         this.isDone = false;
     }
 
@@ -14,6 +16,11 @@ public class Task {
     public void markAsDone() {
         isDone = true;
         System.out.println("Nice! I've marked this task as done:\n" + this.toString());
+    }
+    
+    public String toData() {
+        String isDoneNum = isDone ? "1" : "0"; 
+        return String.format("%s|%s|%s", icon, isDoneNum, description); 
     }
 
     @Override
