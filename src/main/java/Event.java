c508fe709a084825449fbaded9cb66726e2e9aa3
Tan Jia Xian <e0543981@u.nus.edu>
2021-08-23 00:09:13 +0800

    Update Event to take in a date

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    private String time;
+    private LocalDate time;
 
     public Event(String description, String time) {
         super(description);
-        this.time = time;
+        this.time = LocalDate.parse(time);
+    }
+
+    private String getTime() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+        return this.time.format(formatter);
     }
 
     @Override
     public String toString() {
-        return String.format("[E]%s (at: %s)", super.toString(), this.time);
+        return String.format("[E]%s (at: %s)", super.toString(), this.getTime());
     }
 }
