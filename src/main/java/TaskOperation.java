21db7506b58aeb15b26ec1ebea5a75ae1f25ca59
LMAOboxhack <linfangyuan99@gmail.com>
2021-08-22 21:25:13 +0800

    Add task list persistence

--- src/main/java/TaskOperation.java
+++ src/main/java/TaskOperation.java
@@ -1,4 +1,9 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class TaskOperation {
     private String description;
@@ -10,9 +15,23 @@ public abstract class TaskOperation {
 
     public void execute() {
         performOperation();
+        saveToDisk();
     }
 
     abstract void performOperation();
+
+    private void saveToDisk() {
+        Path pathToOut = Paths.get("./output/tasklist.txt");
+        try {
+            Files.createDirectories(pathToOut.getParent());
+            if (!Files.exists(pathToOut)) {
+                Files.createFile(pathToOut);
+            }
+            Files.write(pathToOut, tasks.stream().map(Object::toString).collect(Collectors.toList()));
+        } catch (IOException e) {
+            System.out.printf("Failed to save: %s\n", e.getMessage());
+        }
+    }
 
     protected String getTasksOverview() {
         return tasks.isEmpty() ? "You have no tasks.\n"
