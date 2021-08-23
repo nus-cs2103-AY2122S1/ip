137a841a7b641a4093f1de81b77f83b2aeea491e
Kai Siang <kslui99@gmail.com>
2021-08-23 21:24:56 +0800

    Use LocalDate for dates and times

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,17 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String at;
+    protected LocalDate at;
 
     public Event(String description, String at) {
         super(description);
-        this.at = at;
+        LocalDate d = LocalDate.parse(at);
+        this.at = d;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + this.at + ")";
+        return "[E]" + super.toString() + " (at: " + this.at.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
