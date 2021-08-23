4f81d79263940634ac57e3e2aeea9b9780b4bf21
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-08-23 23:13:06 +0800

    add LocalDate and LocalTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,14 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Represents a deadline, which is a subtype of a Task.
  */
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate date = null;
+    protected LocalTime time = null;
 
     /**
      * A constructor of a Deadline.
@@ -13,16 +18,30 @@ public class Deadline extends Task {
      */
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        if (by.contains(" ")) {
+            this.date = LocalDate.parse(by.split(" ", 2)[0]);
+            this.time = LocalTime.parse(by.split(" ", 2)[1]);
+        } else if (by.contains("-")) {
+            this.date = LocalDate.parse(by);
+        } else {
+            this.time = LocalTime.parse(by);
+        }
     }
 
     /**
      * Returns a string representation of a Deadline.
      *
-     * @return [D], the description and the by.
+     * @return [D], the description, the date and/or the time.
      */
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        String toPrint = "[D]" + super.toString() + " (by: ";
+        if (date != null) {
+            toPrint += date.format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
+        }
+        if (time != null) {
+            toPrint += " " + time.format(DateTimeFormatter.ofPattern("HH:mm"));
+        }
+        return toPrint + ")";
     }
 }
