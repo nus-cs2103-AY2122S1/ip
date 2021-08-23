49f13f063af99f44a3ea3c490f36dc854ebb0a3a
DesmondTo <e0550619@u.nus.edu>
2021-08-23 21:43:15 +0800

    Add Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,9 @@
+public class Ui {
+    protected static final String logo = "\t ____        _        \n"
+            + "\t|  _ \\ _   _| | _____ \n"
+            + "\t| | | | | | | |/ / _ \\\n"
+            + "\t| |_| | |_| |   <  __/\n"
+            + "\t|____/ \\__,_|_|\\_\\___|\n\n";
+
+    private static final String divider = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+}
