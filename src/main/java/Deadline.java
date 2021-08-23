faab48a636aa8afd8c6c43e0ba11422cd3f2c178
thanwinnikki <thanwinnikki@gmail.com>
2021-08-24 01:12:07 +0800

    Add datetime format to deadline task.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,10 +1,27 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
 
     private String deadLine;
+    private LocalDateTime dateTime;
 
     public Deadline(String task, String deadLine) {
         super(task);
         this.deadLine = deadLine;
+        timeSetter(deadLine);
+        timeFormChange();
+    }
+
+    private void timeSetter(String timeInput) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy HHmm");
+        dateTime = LocalDateTime.parse(timeInput,formatter);
+    }
+
+    private String timeFormChange() {
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("h.mm a");
+        return dateFormat.format(dateTime) + ", " + timeFormatter.format(dateTime);
     }
 
     @Override
@@ -15,6 +32,6 @@ public class Deadline extends Task{
         } else {
             result = "[D][ ] ";
         }
-        return result + super.task + "(by: " + this.deadLine + ")";
+        return result + super.task + "(by: " + timeFormChange() + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,6 +1,10 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
 
     private String timing;
+    private LocalDateTime dateTime;
 
     public Event(String task, String timing) {
         super(task);
