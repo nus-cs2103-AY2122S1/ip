00c34d6545b96d0b66b162a651b716ba1315c2a9
e0543403 <e0543403@u.nus.edu>
2021-08-23 18:41:05 +0800

    Add JavaDocs for Duke.java

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,24 +2,43 @@ package duke;
 
 import duke.task.DukeList;
 
+/**
+ * Encapsulation of Duke's main class.
+ */
 public class Duke {
+    /** Duke's list. */
     private DukeList list;
 
+    /** Duke's data storage. */
     private Storage storage;
 
+    /** Duke's Ui. */
     private Ui ui;
 
+    /**
+     * Constructs a Duke object.
+     *
+     * @param path PATH of where save file for saved data is placed.
+     */
     public Duke(String path) {
         this.list = new DukeList();
         this.storage = new Storage(path, this.list);
         this.ui = new Ui(this.list, this.storage);
     }
 
+    /**
+     * Runs Duke.
+     */
     public void run() {
         this.storage.load();
         this.ui.run();
     }
 
+    /**
+     * Runs Duke.
+     *
+     * @param args User input.
+     */
     public static void main(String[] args) {
         String PATH = System.getProperty("user.dir");
 
