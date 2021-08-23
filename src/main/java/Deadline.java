54749d747ac618a5f9ebc3fdd9287a0e987813fe
Feng Zhunyi <leofengbar@gmail.com>
2021-08-23 15:03:37 +0800

    Revert "Level-7"
    This reverts commit 8ea8fc469a20a6d9cf6f1b2dca0fa70d0dd05cf7.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -2,40 +2,15 @@ public class Deadline extends Task{
     private String name;
     private boolean isDone;
     private int index;
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean isDone() {
-        return isDone;
-    }
-
-    @Override
-    public int getIndex() {
-        return index;
-    }
-
+    private String type;
     private String time;
 
     public Deadline(String name, boolean isDone, int index, String time) {
-        super();
-        this.name = name;
-        this.isDone = isDone;
-        this.index = index;
+        super(name, isDone, index);
         this.time = time;
     }
 
     public String getTime() {
         return time;
     }
-
-    @Override
-    public String toString() {
-        return "D" +  name  +" "+ isDone  +" "+ index  +" "+ time + '\n';
-    }
-
-
 }
