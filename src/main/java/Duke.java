e489205c990b0b988c56766723a932b150b7a599
Jason <jasonang0210@gmail.com>
2021-08-24 03:03:49 +0800

    'changes'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
+import java.io.FileWriter;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;  // Import the File class
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
 
 public class Duke {
 
@@ -9,6 +13,28 @@ public class Duke {
     private static void endBot() {
         done = true;
         System.out.println("Bye for now!");
+    }
+
+    private static void saveTasks() {
+        try {
+            File myDirectory = new File("./data/");
+            if (! myDirectory.exists()) {
+                myDirectory.mkdir();
+            }
+
+            File myFile = new File("./data/duke.txt");
+            myFile.createNewFile();
+            FileWriter myWriter = new FileWriter("./data/duke.txt");
+            String output = "";
+            for (int i = 0; i < myList.size(); i++) {
+                output = output + myList.get(i) + "\n";
+            }
+            myWriter.write(output);
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
     private static void listTasks() {
@@ -38,6 +64,7 @@ public class Duke {
             System.out.println("I've added this task:");
             System.out.println(myList.get(myList.size() - 1));
             System.out.println("Now you have " + myList.size() + " tasks.");
+            saveTasks();
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid format. Please follow this format. <type of task> <description> /<date if necessary>");
         } catch (StringIndexOutOfBoundsException e) {
@@ -52,6 +79,7 @@ public class Duke {
             Task myTask = myList.get(index - 1);
             myList.remove(index - 1);
             System.out.println("I've deleted this task:\n[X] " + myTask.description);
+            saveTasks();
         } catch (NumberFormatException e) {
             System.out.println("Please input a number after the keyword: delete");
         } catch (IndexOutOfBoundsException | NullPointerException e ) {
@@ -67,6 +95,7 @@ public class Duke {
             Task myTask = myList.get(index - 1);
             myTask.markAsDone();
             System.out.println("I've marked this task as done:\n[X] " + myTask.description);
+            saveTasks();
         } catch (NumberFormatException e) {
             System.out.println("Please input a number after the keyword: done");
         } catch (IndexOutOfBoundsException | NullPointerException e ) {
