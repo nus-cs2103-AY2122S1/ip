c792dab77260c18bb2443af43e38315782459813
kflim <redbulldown@gmail.com>
2021-08-23 11:43:36 +0800

    Add JavaDoc comments to the code
    Most of the code has documentation.
    
    Some methods require further documentation for clarification.
    
    Adding more comments will help improve clarity.

--- src/main/java/utility/TaskList.java
+++ src/main/java/utility/TaskList.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Represents a list of tasks that can perform operations such as delete and add.
+ * Represents a list of tasks that can perform operations such as delete, add and list.
  */
 public class TaskList {
     private List<Task> tasks;
