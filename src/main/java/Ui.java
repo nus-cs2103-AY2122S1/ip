e774612a6ccc6cadb9ad9329964d9434dd77c00b
DesmondTo <e0550619@u.nus.edu>
2021-08-23 21:57:06 +0800

    Implement user-interactional methods

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ui {
     protected static final String logo = "\t ____        _        \n"
             + "\t|  _ \\ _   _| | _____ \n"
@@ -6,4 +8,35 @@ public class Ui {
             + "\t|____/ \\__,_|_|\\_\\___|\n\n";
 
     private static final String divider = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+
+    private void showLine() {
+        System.out.println("\t" + Ui.divider);
+    }
+
+    private void showLoadingERROR() {
+        this.showLine();
+        System.out.println("\t☹ OOPS!!! File loading failed :-(");
+        this.showLine();
+    }
+
+    private void showWelcome() {
+        this.showLine();
+        System.out.println("\tWelcome to");
+        System.out.println(Ui.logo);
+        System.out.println("\tI'm Desmond, how may I serve you? :)");
+        this.showLine();
+    }
+
+    private String readCommand() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        scanner.close();
+        return input;
+    }
+
+    private void showError(String errorMessage) {
+        this.showLine();
+        System.out.println("\tSomething went wrong: " + errorMessage);
+        this.showLine();
+    }
 }
