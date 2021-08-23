a2c007e8fcb53bf62ecaa6027729a8c38b8cc750
Preshita01 <pandipreshita@gmail.com>
2021-08-23 19:45:33 +0800

    Add toPrintToFile() to differentiate between 2 types of date time stamps used

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -3,6 +3,7 @@ import java.time.format.DateTimeFormatter;
 
 public class Event extends Task {
     protected String time;
+    protected String timeToDisplay;
     protected LocalDateTime eventTime;
 
     public Event(String description, String time) {
@@ -15,7 +16,7 @@ public class Event extends Task {
         int min = Integer.parseInt(time.substring(14, 16));
 
         LocalDateTime taskDate = LocalDateTime.of(year, month, date, hour, min);
-        this.time = taskDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm"));
+        this.timeToDisplay = taskDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm"));
         eventTime = taskDate;
     }
 
@@ -25,6 +26,11 @@ public class Event extends Task {
 
     @Override
     public String toString() {
+        return "[E]" + super.toString() + " (at: " + timeToDisplay + ")";
+    }
+
+    @Override
+    public String toPrintToFile() {
         return "[E]" + super.toString() + " (at: " + time + ")";
     }
 }
