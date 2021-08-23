53644bced1d451b7e8ab10d9c956e9f6eb01f809
Preshita01 <pandipreshita@gmail.com>
2021-08-23 19:45:42 +0800

    Add toPrintToFile() to differentiate between 2 types of date time stamps used

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,10 +1,15 @@
-public class Todo extends Task{
+public class Todo extends Task {
     public Todo(String description) {
         super(description);
     }
 
     @Override
     public String toString() {
+        return "[T]" + super.toString();
+    }
+
+    @Override
+    public String toPrintToFile() {
         return "[T]" + super.toString();
     }
 }
