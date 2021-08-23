22a9624ae8b336b474626f74e2f423b580af6d79
Ng Xiang Jun <ngxiangjun1999@gmail.com>
2021-08-23 16:57:27 +0800

    Create Parser class to deal with making sense of the user command

---
+++ src/main/java/Parser.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Parser {
+
+    public static boolean parse(Command command, Tasklist tl, Storage storage) throws DukeException {
+        boolean shouldContinue = true;
+        CommandKeyword keyword = command.getKeyword();
+        String restOfCommand = command.getRestOfCommand();
+        switch (keyword) {
+            case TODO:
+            case DEADLINE:
+            case EVENT:
+                ArrayList<Task> tasksAfterAdd = tl.addTask(restOfCommand, keyword);
+                if (tasksAfterAdd != null) {
+                    storage.save(tasksAfterAdd);
+                }
+                break;
+            case LIST:
+                if (restOfCommand.equals("")) {
+                    tl.display();
+                    break;
+                } else {
+                    throw new InvalidCommandException();
+                }
+            case DONE:
+                int indexToMark = Parser.stringToInt(restOfCommand) - 1;
+                ArrayList<Task> tasksAfterDone = tl.markTask(indexToMark);
+                if (tasksAfterDone != null) {
+                    storage.save(tasksAfterDone);
+                }
+                break;
+            case DELETE:
+                int indexToDelete = Parser.stringToInt(restOfCommand) - 1;
+                ArrayList<Task> tasksAfterDelete = tl.deleteTask(indexToDelete);
+                if (tasksAfterDelete != null) {
+                    storage.save(tasksAfterDelete);
+                }
+                break;
+            case BYE:
+                if (restOfCommand.equals("")) {
+                    shouldContinue = false;
+                    break;
+                } else {
+                    throw new InvalidCommandException();
+                }
+            default:
+                /* will never be executed because the error would have been caught in run() method
+                   if the user input a command that is invalid */
+        }
+        return shouldContinue;
+    }
+
+    public static int stringToInt(String description) throws InvalidCommandException {
+        if (description.matches("\\d+")) {
+            return Integer.parseInt(description);
+        } else {
+            throw new InvalidCommandException();
+        }
+    }
+}
