36f0bb61c7a5a6950ba5a4eb911add7e1bc37cad
Ng Xiang Jun <ngxiangjun1999@gmail.com>
2021-08-23 16:56:22 +0800

    Implement Storage class to deal with loading/saving tasks in file

---
+++ src/main/java/Storage.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+    private File file;
+    private Ui ui;
+
+    public Storage(Ui ui) {
+        this.ui = ui;
+        try {
+            String dir = System.getProperty("user.dir");
+            Path path = Paths.get(dir, "data");
+
+            // create the data folder if it does not exist
+            if (!Files.exists(path)) {
+                path = Files.createDirectory(path);
+            }
+
+            File file = path.resolve("duke.txt").toFile();
+            this.file = file;
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            // this exception should not occur because the input(path) is fixed
+        }
+    }
+
+    public ArrayList load() {
+        try {
+            Scanner sc = new Scanner(this.file);
+            ArrayList<String> tasksInfo = new ArrayList<>();
+            while (sc.hasNextLine()) {
+                tasksInfo.add(sc.nextLine());
+            }
+            return tasksInfo;
+        } catch (IOException e) {
+            this.ui.showLoadingError();
+            return null;
+        }
+    }
+
+    public void save(ArrayList<Task> tasks) {
+        try {
+            FileWriter fw = new FileWriter(this.file);
+            for (int i = 0; i < tasks.size(); i++) {
+                String taskInfo = tasks.get(i).stringToStore();
+                fw.write(taskInfo);
+            }
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Failed to update storage.");
+        }
+    }
+}
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,11 @@
-import java.io.File;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public abstract class Task {
     protected String description;
     protected boolean isDone;
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("MMM dd yyyy");
 
     public Task(String description) {
         this.description = description;
@@ -25,6 +28,26 @@ public abstract class Task {
     @Override
     public String toString() {
         return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+
+    public static Task convertToTask(String taskInfo) {
+        String[] arr = taskInfo.split(" \\| ");
+        char letter = arr[0].charAt(0);
+        boolean isDone = arr[1].equals("X");
+        switch (letter) {
+            case 'T':
+                return new Todo(arr[2], isDone);
+            case 'D':
+                LocalDate deadlineDate = LocalDate.parse(arr[3], Task.DATE_TIME_FORMATTER);
+                LocalTime deadlineTime = LocalTime.parse(arr[4]);
+                return new Deadline(arr[2], deadlineDate, deadlineTime, isDone);
+            case 'E':
+                LocalDate eventDate = LocalDate.parse(arr[3], Task.DATE_TIME_FORMATTER);
+                String timeRange = arr[4];
+                return new Event(arr[2], eventDate, timeRange, isDone);
+            default:
+                return null;
+        }
     }
 
     public abstract String stringToStore();
