0710d18aa67ab486c22dd6cfaf37e9e6176295d0
Zenlzb <leezenbeen@u.nus.edu>
2021-08-23 16:49:22 +0800

    Update UI class to include more messages

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class UI {
+    private final Scanner sc;
+
+    public UI() {
+        sc = new Scanner(System.in);
+    }
+
+    public String readCommand() {
+        return sc.nextLine();
+    }
+
     public String formatMessage(String s) {
         return "    ____________________________________________________________\n     " +
                 s + "\n" +
                 "    ____________________________________________________________";
     }
+
     public String formatMessage(String[] messages) {
         StringBuilder res = new StringBuilder("    ____________________________________________________________" );
         for(String s : messages) {
@@ -12,7 +25,23 @@ public class UI {
         res.append("\n    ____________________________________________________________");
         return res.toString();
     }
+
     public void welcomeMessage() {
-        System.out.println(formatMessage("Hello! I'm Duke\n" + "     What can I do for you?"));
+        print("Hello! I'm Duke\n" + "     What can I do for you?");
+    }
+
+    public void goodByeMessage() {
+        print("Bye. Hope to see you again soon!");
+    }
+
+    public void unrecognisedCommand() {
+        print("That is not a recognised command");
+    }
+
+    public void print(String s) {
+        System.out.println(formatMessage(s));
+    }
+    public void print(String[] s) {
+        System.out.println(formatMessage(s));
     }
 }
