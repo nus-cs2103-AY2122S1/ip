bd7402f072053df74e6041e3106e7e25eba2e2a2
gordonlzy <gordonlzy123@gmail.com>
2021-08-23 20:11:13 +0800

    implement dates and times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,10 +1,30 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
     protected String by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+
+        String[] dateTime = by.split(" ");
+
+        String rawDate = dateTime[0];
+        String[] dayMonthYear = rawDate.split("/");
+        LocalDate date = LocalDate.of(Integer.parseInt(dayMonthYear[2]), Integer.parseInt(dayMonthYear[1]), Integer.parseInt(dayMonthYear[0]));
+        String formattedDate = date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+
+        if (dateTime.length > 1) {
+            // store date and time
+            String rawTime = dateTime[1];
+            String processedRawTime = rawTime.substring(0, 2) + ":" + rawTime.substring(2);
+            LocalTime time = LocalTime.parse(processedRawTime);
+            String formattedTime = time.format(DateTimeFormatter.ofPattern("h a"));
+
+            this.by = formattedDate + ", " + formattedTime;
+        }
     }
 
     @Override
