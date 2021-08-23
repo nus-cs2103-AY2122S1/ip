1298dfd208f845e956df0ab71b5b87e394593fa2
Preshita01 <pandipreshita@gmail.com>
2021-08-23 12:26:44 +0800

    Store event time as a LocalDateTime object in Event.java

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,9 +1,20 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
     protected String time;
 
     public Event(String description, String time) {
         super(description);
         this.time = time;
+        int date = Integer.parseInt(time.substring(0, 2));
+        int month = Integer.parseInt(time.substring(3, 5));
+        int year = Integer.parseInt(time.substring(6, 10));
+        int hour = Integer.parseInt(time.substring(11, 13));
+        int min = Integer.parseInt(time.substring(14, 16));
+
+        LocalDateTime taskDate = LocalDateTime.of(year, month, date, hour, min);
+        this.time = taskDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm"));
     }
 
     @Override
