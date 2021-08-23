288c2d3a70e21382f4e985d343a2d9adfb561a52
luminousleek <68138671+luminousleek@users.noreply.github.com>
2021-08-23 15:23:18 +0800

    Remove commented out logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 
 public class Duke {
 
-//    private static String logo = " ____        _        \n"
-//            + "|  _ \\ _   _| | _____ \n"
-//            + "| | | | | | | |/ / _ \\\n"
-//            + "| |_| | |_| |   <  __/\n"
-//            + "|____/ \\__,_|_|\\_\\___|\n";
-
-
     /**
      * Wraps a given message above and below with lines and prints it
      *
