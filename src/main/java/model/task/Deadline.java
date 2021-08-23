1f14ef4c453cec24c4395c2c268a3462cc3392cd
Amadeus <amadeusw6@gmail.com>
2021-08-23 13:56:22 +0800

    Minimum Date/Time support

--- src/main/java/model/task/Deadline.java
+++ src/main/java/model/task/Deadline.java
@@ -1,14 +1,21 @@
 package model.task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String dueDate;
+    protected LocalDate dueDate;
     public Deadline(String entry, String dueDate) {
         super(entry);
-        this.dueDate = dueDate;
+        this.dueDate = LocalDate.parse(dueDate);
+    }
+
+    private String dateTimeFormatter(LocalDate dateTime) {
+        return dateTime.format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + dueDate + ")";
+        return "[D]" + super.toString() + " (by: " + dateTimeFormatter(dueDate) + ")";
     }
 }
--- src/main/java/model/task/Event.java
+++ src/main/java/model/task/Event.java
@@ -1,14 +1,21 @@
 package model.task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String startTime;
+    protected LocalDate startTime;
     public Event(String entry, String startTime) {
         super(entry);
-        this.startTime = startTime;
+        this.startTime = LocalDate.parse(startTime);
+    }
+
+    private String dateTimeFormatter(LocalDate dateTime) {
+        return dateTime.format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + startTime + ")";
+        return "[E]" + super.toString() + " (at: " + dateTimeFormatter(startTime) + ")";
     }
 }
