0fe3f2d4236509d6b201867fca7ff0cfa3526e36
DESKTOP-CSTCHE9\Joseph Nathanael <josephnathanael.2014@gmail.com>
2021-08-23 16:22:58 +0800

    Dates and Times feature

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,22 +1,34 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
     private String Dtime;
+    private LocalDateTime localDtime;
 
     public Deadline(String description, String s) {
         super(description);
-        this.Dtime = s;
+        this.Dtime = s; // accept format yyyy-mm-dd
+        this.localDtime = LocalDateTime.parse(s.substring(1), DateTimeFormatter.ofPattern("uuuu-MM-dd kkmm"));
+
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by:" + this.Dtime + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd kkmm");
+        String formatDateTime = this.localDtime.format(formatter);
+        return "[D]" + super.toString() + " (by:" + formatDateTime + ")";
     }
 
     @Override
     public String toStringConvert(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd kkmm");
+        String formatDateTime = this.localDtime.format(formatter);
         if(this.isCompleted()) {
-            return "D | 1 | " +  this.getString() + "|" + this.Dtime;
+            return "D | 1 | " +  this.getString() + "| " + formatDateTime;
         } else {
-            return "D | 0 | " +  this.getString() + "|" + this.Dtime;
+            return "D | 0 | " +  this.getString() + "| " + formatDateTime;
         }
     }
+
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,23 +1,39 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+
 public class Event extends Task {
     private String schedule;
+    private LocalDateTime localDtime;
+    private LocalTime hour;
 
     public Event(String description, String s) {
         super(description);
-        this.schedule = s;
+        this.schedule = s; // accept "yyyy-mm-dd kkmm" format
+        this.localDtime = LocalDateTime.parse(s.substring(1), DateTimeFormatter.ofPattern("uuuu-MM-dd kkmm"));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at:" + this.schedule + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd kkmm");
+        String formatDateTime = this.localDtime.format(formatter);
+
+        return "[E]" + super.toString() + " (at:" + formatDateTime + ")";
     }
 
 
     @Override
     public String toStringConvert(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd kkmm");
+        String formatDateTime = this.localDtime.format(formatter);
+
         if(this.isCompleted()) {
-            return "E | 1 | " +  this.getString() + "|" + this.schedule;
+            return "E | 1 | " +  this.getString() + "| " + formatDateTime;
         } else {
-            return "E | 0 | " +  this.getString() + "|" + this.schedule;
+            return "E | 0 | " +  this.getString() + "| " + formatDateTime;
         }
     }
+
+
 }
