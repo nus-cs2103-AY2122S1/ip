71ad44b7f75dd831f1b6fe5abce45868b0fb6afd
eezj35 <justineezj0@gmail.com>
2021-08-24 01:38:44 +0800

    Implement  A-MoreOOP
    Add Ui class to extract user interactions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,31 +8,40 @@ public class Duke {
 
     private static StorageList sl = new StorageList();
     private static File file;
+    private static Ui ui = new Ui();
+
+    private static final int TASK_TODO = 1;
+    private static final int TASK_DEADLINE = 2;
+    private static final int TASK_EVENT = 3;
+    private static final int TASK_OUTOFBOUNDS = 4;
+    private static final int TASK_UNKNOWN = 5;
+
+    public Duke(String filePath) {
+//        storage = new Storage(filePath);
+//        try {
+//            tasks = new TaskList(storage.load());
+//        } catch (DukeException e) {
+//            ui.showLoadingError();
+//            tasks = new TaskList();
+//        }
+    }
 
     public static void main(String[] args) throws DukeException {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello I am\n" + logo);
-
+        ui.greeting();
 
         try{
             file = new File("data/duketest.txt");
             if (!file.exists()) {
                 throw new FileNotFoundException();
             } else {
-                System.out.println("I have received your file! Added tasks!\n");
+                ui.fileConfirmation();
                 Scanner sc = new Scanner(file);
                 readFile(sc);
                 result();
             }
 
         } catch (FileNotFoundException e) {
-            System.out.println("I am unable to find your file. " +
-                    "Check that your 'duketest' file exists," +
-                    " or that your 'data' folder exists.");
+            ui.fileNotFoundMsg();
         }
 
     }
@@ -82,30 +91,30 @@ public class Duke {
                     marking(input);
                 } else if (input.length() >= 4 && input.substring(0, 4).equals("todo")) {
                     if(input.length() == 4) {
-                        throw new DukeException("   ☹ OOPS!!! The description of a todo cannot be empty.");
+                        throw new DukeException(ui.taskErrorMsg(TASK_TODO));
                     }
                     ToDos todo = new ToDos(input.substring(5));
                     sl.add(todo);
-                    linesToPrint(todo.toString(), sl.size());
+                    ui.taskAddedMsg(todo.toString(), sl.size());
                 } else if (input.length() >= 8 && input.substring(0, 8).equals("deadline")) {
                     if (input.length() == 8) {
-                        throw new DukeException("   ☹ OOPS!!! The description of a deadline cannot be empty.");
+                        throw new DukeException(ui.taskErrorMsg(TASK_DEADLINE));
                     }
                     String by = input.substring(input.indexOf("/") + 4);
                     Deadlines dl = new Deadlines(input.substring(9, input.indexOf("/")), by);
                     sl.add(dl);
-                    linesToPrint(dl.toString(), sl.size());
+                    ui.taskAddedMsg(dl.toString(), sl.size());
                 } else if (input.length() >= 5 && input.substring(0, 5).equals("event")) {
                     if (input.length() == 5) {
-                        throw new DukeException("   ☹ OOPS!!! The description of an event cannot be empty.");
+                        throw new DukeException(ui.taskErrorMsg(TASK_EVENT));
                     }
                     String at = input.substring(input.indexOf("/") + 4);
                     Events event = new Events(input.substring(6,  input.indexOf("/")), at);
                     sl.add(event);
-                    linesToPrint(event.toString(), sl.size());
+                    ui.taskAddedMsg(event.toString(), sl.size());
                 } else if (input.length() >= 6 && input.substring(0, 6).equals("delete")) {
                     if (input.length() == 6) {
-                        throw new DukeException("   ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                        throw new DukeException(ui.taskErrorMsg(TASK_UNKNOWN));
                     }
                     int idx = Integer.parseInt(input.substring(7)) - 1;
                     delete(idx);
@@ -114,20 +123,20 @@ public class Duke {
                     String desc = task.getDescription();
                     switch (desc) {
                     case "bye":
-                        System.out.println("    Bye. Hope to see you again soon!");
+                        ui.bye();
                         return;
                     case "list":
                         sl.display();
                         break;
                     default:
-                        throw new DukeException("   ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                        throw new DukeException(ui.taskErrorMsg(TASK_UNKNOWN));
                     }
                 }
                 save();
             } catch (DukeException e) {
                 System.out.println(e.getMessage());
             } catch (IOException e) {
-                System.out.println("Something went wrong!");
+                ui.ioErrorMsg();
             }
         }
 
@@ -166,16 +175,9 @@ public class Duke {
     private static void delete(int idx) {
         String desc = sl.get(idx).getDescription();
         sl.delete(idx);
-        System.out.println("    Noted. I've removed this task:");
-        System.out.println("        " + desc);
-        System.out.println("    Now you have " + sl.size() + " tasks in the list.");
+        ui.taskDeleteMsg(desc, sl.size());
     }
 
-    public static void linesToPrint(String task, int size) {
-        System.out.println("    Got it. I've added this task:");
-        System.out.println("        " + task);
-        System.out.println("    Now you have " + size + " tasks in the list.");
-    }
 
     public static void marking(String input) throws DukeException {
         if (input.length() >= 6) {
@@ -183,16 +185,16 @@ public class Duke {
                 int taskNum = Integer.parseInt(input.substring(5)) - 1;
                 if (taskNum < sl.size() && taskNum >= 0) {
                     sl.get(taskNum).markAsDone();
-                    System.out.println("    Nice! I've marked this task as done:");
+                    ui.taskDoneConfirmation();
                     sl.get(taskNum).displayTask();
                 } else {
-                    throw new DukeException("   ☹ OOPS!!! index out of bounds");
+                    throw new DukeException(ui.taskErrorMsg(TASK_OUTOFBOUNDS));
                 }
             } else {
-                throw new DukeException("   ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                throw new DukeException(ui.taskErrorMsg(TASK_UNKNOWN));
             }
         } else {
-            throw new DukeException("   ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            throw new DukeException(ui.taskErrorMsg(TASK_UNKNOWN));
 
         }
     }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,62 @@
+public class Ui {
+
+    public Ui() {}
+
+    public void bye() {
+        System.out.println("    Bye. Hope to see you again soon!");
+    }
+
+    public void greeting() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello I am\n" + logo);
+    }
+
+    public void fileConfirmation() {
+        System.out.println("I have received your file! Added tasks!\n");
+    }
+
+    public void fileNotFoundMsg() {
+        System.out.println("I am unable to find your file. " +
+                "Check that your 'duketest' file exists," +
+                " or that your 'data' folder exists.");
+    }
+
+    public void ioErrorMsg() {
+        System.out.println("Something went wrong!");
+    }
+
+    public void taskAddedMsg(String task, int size) {
+        System.out.println("    Got it. I've added this task:");
+        System.out.println("        " + task);
+        System.out.println("    Now you have " + size + " tasks in the list.");
+    }
+
+    public void taskDeleteMsg(String desc, int size) {
+        System.out.println("    Noted. I've removed this task:");
+        System.out.println("        " + desc);
+        System.out.println("    Now you have " + size + " tasks in the list.");
+    }
+
+    public void taskDoneConfirmation() {
+        System.out.println("    Nice! I've marked this task as done:");
+    }
+
+    public String taskErrorMsg(int i) {
+        switch (i) {
+        case 1:
+            return "   ☹ OOPS!!! The description of a todo cannot be empty.";
+        case 2:
+            return "   ☹ OOPS!!! The description of a deadline cannot be empty.";
+        case 3:
+            return "   ☹ OOPS!!! The description of an event cannot be empty.";
+        case 4:
+            return "   ☹ OOPS!!! index out of bounds";
+        default:
+            return "   ☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+        }
+    }
+}
