37e0926d99d8fd32dee54a02d54b5d2662117c8b
houtenteo <houtenteo@gmail.com>
2021-08-23 18:27:05 +0800

    Add storage class
    Deals with the uploading and downloading of the data to and from the Data.txt file

---
+++ src/main/java/Storage.java
@@ -0,0 +1,84 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Storage {
+    private MyList list;
+    private Ui ui;
+
+    public Storage(MyList list) {
+        this.list = list;
+        this.ui = new Ui();
+    }
+
+    public void load() {
+        try {
+            File dataFile = new File("src/main/java/Data.txt");
+            Scanner s = new Scanner(dataFile);
+            Duke.setFormat(s.nextLine());
+            while (s.hasNextLine()) {
+                String input = s.nextLine();
+                Task t = getTaskFromString(input);
+                this.list.loadTask(t);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No data to load");
+        }
+    }
+
+    public Task getTaskFromString(String s) {
+        String taskType = s.substring(0, 3);
+        String taskDescription = s.substring(7);
+        Task t = null;
+        try {
+            if (s.substring(3,6). equals("[X]")) {
+                switch (taskType) {
+                case "[T]":
+                    t = new Todo(taskDescription, true);
+                    break;
+                case "[D]":
+                    t =  new Deadline(taskDescription, true);
+                    break;
+                case "[E]":
+                    t =  new Event(taskDescription, true);
+                    break;
+                }
+            } else {
+                switch (taskType) {
+                case "[T]":
+                    t = new Todo(taskDescription, false);
+                    break;
+                case "[D]":
+                    t = new Deadline(taskDescription, false);
+                    break;
+                case "[E]":
+                    t = new Event(taskDescription, false);
+                    break;
+                }
+            }
+        } catch (WrongCommandFormatException e) {
+            this.ui.formatExceptionMessage(e);
+            this.ui.loadingError();
+        }
+        return t;
+    }
+
+    public void writeToFile() {
+        try {
+            int listLength = this.list.getListSize();
+            String input = Duke.getFormat() + "\n";
+            FileWriter fw = new FileWriter("src/main/java/Data.txt");
+            for (int i = 0; i < listLength; i++) {
+                Task t = this.list.getTask(i);
+                input += t.createData() + "\n";
+            }
+            fw.write(input);
+            fw.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
