9d9a09d9f9dafea936038329f7c84fde4ba23d3a
nguyiyang <nguyiyang47@gmail.com>
2021-08-24 04:49:57 +0800

    Update tasks to handle date and time formats

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,7 +1,11 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
     /** Deadline of a task */
     protected String by;
+    protected LocalDateTime dateTime;
 
     /**
      * Constructor for Deadline. Takes in a String description and by.
@@ -13,10 +17,12 @@ public class Deadline extends Task {
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        this.dateTime = LocalDateTime.parse(by, formatter);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + dateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy ha")) + ")";
     }
 }
