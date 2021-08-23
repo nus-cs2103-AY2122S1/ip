e8ff7bec50d3fded4485a049d4bd45728b6d744d
Chin Xing Yi Rebecca <rebeccachinxy@gmail.com>
2021-08-24 01:28:12 +0800

    A-CodingStandard - comply with coding standard

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -1,6 +1,10 @@
 package duke.exception;
 
 public class DukeException extends Exception {
+    /**
+     * Parent exception class.
+     * @param e Error thrown.
+     */
     public DukeException(String e) {
         super(e);
     }
