94168e095a2d7ba01fbce005e1048bd4920c422c
yyhangz <y.y.hangz@hotmail.com>
2021-08-23 14:32:05 +0800

    Add java date format for Event and Deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,24 @@
-public class Deadline extends Task {
-    protected String finishDate;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
-    public Deadline(String taskDescription, String finishDate) {
+public class Deadline extends Task {
+    protected LocalDate finishDate;
+
+    public Deadline(String taskDescription, String dateString) throws InvalidDukeCommandException {
         super(taskDescription);
-        this.finishDate = finishDate;
+        try {
+            int[] dateArgs = Arrays.stream(dateString.split("-")).mapToInt(Integer::valueOf).toArray();
+            this.finishDate = LocalDate.of(dateArgs[0], dateArgs[1], dateArgs[2]);
+        } catch (NumberFormatException | DateTimeException e) {
+            throw new InvalidDukeCommandException("Deadline date has to be declared in the format yyyy-mm-dd.");
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("[D] %s (by: %s)", super.toString(), this.finishDate);
+        return String.format("[D] %s (by: %s)", super.toString(), this.finishDate.format(DateTimeFormatter
+                .ofPattern("dd MMM yyyy")));
     }
 }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
-import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Duke {
