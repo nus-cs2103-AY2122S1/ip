a52ff78e1558806d053f9411201ec95c0ad40871
G <garyyansen@gmail.com>
2021-08-23 18:20:40 +0800

    Complete level 7

--- src/main/java/Nyx.java
+++ src/main/java/Nyx.java
@@ -1,12 +1,36 @@
+import java.io.IOException;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Nyx {
     private static final String LINE = "\t____________________________________________________________\n";
-    private static final ArrayList<Task> taskList = new ArrayList<>();
+    private final ArrayList<Task> taskList;
+    private final DataManager dataManager;
 
-    private static void respond(String input) throws NyxException,
-            NumberFormatException, IndexOutOfBoundsException {
+    public Nyx(ArrayList<Task> taskList, DataManager dataManager) {
+        this.taskList = taskList;
+        this.dataManager = dataManager;
+    }
+
+    private static Nyx start(String folderName, String fileName) throws IOException, NyxException {
+        String logo = "\t __      _\n"
+                + "\t|   \\   | |__    __ __     __\n"
+                + "\t| |\\ \\  | |\\ \\  / / \\ \\   / /\n"
+                + "\t| | \\ \\ | | \\ \\/ /   \\ \\ / / \n"
+                + "\t| |  \\ \\| |  \\  /    /    /\n"
+                + "\t| |   \\   |  / /    / / \\ \\\n"
+                + "\t|_|    \\__| /_/    /_/   \\_\\\n";
+        System.out.println(LINE);
+        System.out.println("\tHello! This is\n" + logo + "\n\tWhat can I do for you?");
+        System.out.println(LINE);
+
+        DataManager dataManager = new DataManager(folderName, fileName);
+
+        return new Nyx(dataManager.loadData(), dataManager);
+    }
+
+    private void respond(String input) throws NyxException, IOException {
         String[] splitInput = input.split(" ", 2);
         String command = splitInput[0];
         String info = "";
@@ -32,6 +56,7 @@ public class Nyx {
                 taskList.get(index).setDone();
                 System.out.printf("\tNice! I've marked this task as done:%n\t  %s%n",
                         taskList.get(index));
+                dataManager.overwriteData(taskList);
             } else {
                 System.out.println("\tNo task to mark!");
             }
@@ -42,6 +67,7 @@ public class Nyx {
             }
             ToDo task = new ToDo(info);
             taskList.add(task);
+            dataManager.addData(task);
             System.out.printf("\tGot it. I've added this task:%n\t  %s%n", task);
             System.out.printf("\tNow you have %d tasks in the list.%n", taskList.size());
             break;
@@ -58,6 +84,7 @@ public class Nyx {
             String[] splitInfo = info.split(" /by ");
             Deadline task = new Deadline(splitInfo[0].strip(), splitInfo[1]);
             taskList.add(task);
+            dataManager.addData(task);
             System.out.printf("\tGot it. I've added this task:%n\t  %s%n", task);
             System.out.printf("\tNow you have %d tasks in the list.%n", taskList.size());
             break;
@@ -69,6 +96,7 @@ public class Nyx {
             String[] splitInfo = info.split(" /at ");
             Event task = new Event(splitInfo[0].strip(), splitInfo[1]);
             taskList.add(task);
+            dataManager.addData(task);
             System.out.printf("\tGot it. I've added this task:%n\t  %s%n", task);
             System.out.printf("\tNow you have %d tasks in the list.%n", taskList.size());
             break;
@@ -78,6 +106,7 @@ public class Nyx {
             if (!taskList.isEmpty()) {
                 Task task = taskList.get(index);
                 taskList.remove(index);
+                dataManager.overwriteData(taskList);
                 System.out.printf("\tNoted! I've removed this task:%n\t  %s%n", task);
                 System.out.printf("\tNow you have %d tasks in the list.%n", taskList.size());
             } else {
@@ -90,33 +119,29 @@ public class Nyx {
     }
 
     public static void main(String[] args) {
-        String logo = "\t __      _\n"
-                + "\t|   \\   | |__    __ __     __\n"
-                + "\t| |\\ \\  | |\\ \\  / / \\ \\   / /\n"
-                + "\t| | \\ \\ | | \\ \\/ /   \\ \\ / / \n"
-                + "\t| |  \\ \\| |  \\  /    /    /\n"
-                + "\t| |   \\   |  / /    / / \\ \\\n"
-                + "\t|_|    \\__| /_/    /_/   \\_\\\n";
-        System.out.println(LINE);
-        System.out.println("\tHello! This is\n" + logo + "\n\tWhat can I do for you?");
-        System.out.println(LINE);
+        try {
+            Nyx nyx = Nyx.start("data", "nyx.txt");
 
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
 
-        while (!input.equals("bye")) {
-            try {
-                respond(input);
-            } catch (NumberFormatException | IndexOutOfBoundsException e) {
-                System.out.println("\tInvalid task index. Please try again!");
-            } catch(NyxException e) {
-                System.out.println(e.getMessage());
-            } finally {
-                System.out.println(LINE);
-                input = sc.nextLine();
+            while (!input.equals("bye")) {
+                try {
+                    nyx.respond(input);
+                } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                    System.out.println("\tInvalid task index. Please try again!");
+                } catch(NyxException | IOException e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    System.out.println(LINE);
+                    input = sc.nextLine();
+                }
             }
+
+            nyx.respond("bye");
+            sc.close();
+        } catch (IOException | NyxException e) {
+            System.out.println(e.getMessage());
         }
-        respond("bye");
-        sc.close();
     }
 }
