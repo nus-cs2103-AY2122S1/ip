b1b7fa116d74b5f8372e4fe3990baf125b19db5d
sheehui <tansheehui@gmail.com>
2021-08-23 13:08:24 +0800

    Change static methods to non-static
    Created a Duke instance so methods can be non-static

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,201 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Duke {
+class Duke {
 
   private static final String LINE =
     "     ____________________________________________________________\n";
   private static final String INDENT = "      ";
-
-  private static void formatOutput(String inp) {
-    System.out.println(LINE + INDENT + inp + '\n' + LINE);
-  }
-
-  private static void getInput() {
+  private boolean isEndChat = false;
+  private static final ArrayList<Task> taskList = new ArrayList<>();
+ 
+  private void getInput() {
     Scanner sc = new Scanner(System.in);
     while (sc.hasNextLine()) {
       String input = sc.nextLine();
-      Logic.takeInput(input);
-      if (Logic.endChat()) break;
+      takeInput(input);
+      if (isEndChat) {
+        sc.close();
+        break;
+      }
     }
+  }
+
+  private void endChat() {
+    isEndChat = false;
+  }
+
+  private static String getFirstWord(String text) {
+    int index = text.indexOf(' ');
+    if (index > -1) { // Check if there is more than one word.
+      return text.substring(0, index).trim(); // Extract first word.
+    } else {
+      return text; // Text is the first word itself.
+    }
+  }
+
+  public void takeInput(String input) {
+    try {
+      String command = getFirstWord(input);
+      if (input.equals("bye")) {
+        renderOutput("Bye. Hope to see you again soon!");
+        endChat();
+      } else if (input.equals("list")) {
+        renderList();
+      } else if (command.equals("done")) {
+        markTaskComplete(input);
+      } else if (command.equals("todo")) {
+        addTodoTask(input);
+      } else if (command.equals("deadline")) {
+        addDeadlineTask(input);
+      } else if (command.equals("event")) {
+        addEventTask(input);
+      } else if (command.equals("delete")) {
+        deleteTask(input);
+      } else {
+        throw new InvalidInputException();
+      }
+    } catch (UserInputError e) {
+      renderOutput(e.getMessage());
+    }
+  }
+
+  private String getDesc(String input) {
+    return input.split(" ", 2)[1];
+  }
+
+  //check for incomplete input
+  private void checkDescExist(String input)
+    throws NoDescriptionException {
+    if (input.split(" ").length == 1) {
+      throw new NoDescriptionException(
+        "Oops! Please add description for your command."
+      );
+    }
+  }
+
+  private int getTaskNumber(String cmd) {
+    String[] result = cmd.split(" ");
+    if (result[1].matches("\\d+")) {
+      return Integer.parseInt(result[1]);
+    }
+    return -1;
+  }
+
+  private void renderList() {
+    StringBuilder op = new StringBuilder();
+    for (int i = 0; i < taskList.size(); i++) {
+      op
+        .append(i + 1)
+        .append(". ")
+        .append(taskList.get(i).toString())
+        .append("\n");
+    }
+    renderOutput("Here are the tasks in your list:\n" + op);
+  }
+
+  private void checkIndexRange(int index)
+    throws ExceedListSizeException {
+    if (index < 0) {
+      throw new ExceedListSizeException(
+        "Invalid task reference!\nIndex should be more than 0."
+      );
+    }
+
+    if (index > taskList.size() - 1) {
+      throw new ExceedListSizeException(
+        "Invalid task reference!\nYou currently have " +
+        taskList.size() +
+        " tasks."
+      );
+    }
+  }
+
+  private void markTaskComplete(String cmd) throws UserInputError {
+    checkDescExist(cmd);
+    int index = getTaskNumber(cmd) - 1;
+    checkIndexRange(index);
+
+    Task task = taskList.get(index);
+    if (task.isDone()) {
+      renderOutput("Great! But you have already completed this task!");
+    } else {
+      task.markDone();
+      renderOutput("Nice! I've marked this task as done: \n" + task);
+    }
+  }
+
+  private void addTodoTask(String input) throws UserInputError {
+    checkDescExist(input);
+    Task newTask = new Todo(getDesc(input));
+    taskList.add(newTask);
+    addTaskOutput(newTask);
+  }
+
+  private String[] separateDetails(String str, String key)
+    throws NoDescriptionException {
+    if (str.split(key).length == 1) {
+      throw new NoDescriptionException(
+        "Oops! Please use the correct format with " +
+        key +
+        " to indicate\ndatetime"
+      );
+    }
+    return str.split(key);
+  }
+
+  private void addDeadlineTask(String input) throws UserInputError {
+    checkDescExist(input);
+    String[] details = separateDetails(getDesc(input), "/by");
+    Task newTask = new Deadline(details[0], details[1]);
+    taskList.add(newTask);
+    addTaskOutput(newTask);
+  }
+
+  private void addEventTask(String input) throws UserInputError {
+    checkDescExist(input);
+    String[] details = separateDetails(getDesc(input), "/at");
+    Task newTask = new Event(details[0], details[1]);
+    taskList.add(newTask);
+    addTaskOutput(newTask);
+  }
+
+  private void addTaskOutput(Task task) {
+    String output =
+      "Got it. I've added this task:\n" +
+      INDENT +
+      task.toString() +
+      "\nNow you have " +
+      taskList.size() +
+      " tasks in the list.";
+    renderOutput(output);
+  }
+
+  private void deleteTask(String input) throws UserInputError {
+    checkDescExist(input);
+    int index = getTaskNumber(input) - 1;
+    checkIndexRange(index);
+
+    Task deleted = taskList.get(index);
+    taskList.remove(index);
+    deleteTaskOutput(deleted);
+  }
+
+  private void deleteTaskOutput(Task task) {
+    String output =
+      "Noted. I've removed this task:\n" +
+      INDENT +
+      task.toString() +
+      "\nNow you have " +
+      taskList.size() +
+      " tasks in the list.";
+    renderOutput(output);
+  }
+
+  private void renderOutput(String op) {
+    System.out.println(LINE);
+    op.lines().forEach(line -> System.out.println("      " + line));
+    System.out.println(LINE);
   }
 
   public static void main(String[] args) {
@@ -30,6 +209,8 @@ public class Duke {
     System.out.println(LINE.trim());
     output.lines().forEach(op -> System.out.println("      " + op));
     System.out.println(LINE.trim());
-    getInput();
+
+    Duke bot = new Duke();
+    bot.getInput();
   }
 }
