4348b6f49d7cdaaa1cefb558cc8ee33897a3c9e9
e0543403 <e0543403@u.nus.edu>
2021-08-23 18:37:05 +0800

    Add JavaDocs for Ui.java

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -4,23 +4,41 @@ import duke.task.DukeList;
 
 import java.util.Scanner;
 
+/**
+ * Encapsulates the user interaction from duke.
+ */
 public class Ui {
 
+    /** Parser of user input. */
     private Parser parser;
 
+    /** Duke's logo. */
     private static String logo = " ____        _        \n"
             + "|  _ \\ _   _| | _____ \n"
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
-    private static String greeting = "\nHello I'm duke.Duke!\n"
+
+    /** Duke's greeting. */
+    private static String greeting = "\nHello I'm Duke!\n"
             + "What can I do for you?\n";
+
+    /** Duke's farewells. */
     private static String exit = "Bye. Hope to see you again soon!";
 
+    /**
+     * Constructs a Ui object.
+     *
+     * @param list Duke's list.
+     * @param storage Duke's storage.
+     */
     public Ui(DukeList list, Storage storage) {
         this.parser = new Parser(list, storage);
     }
 
+    /**
+     * Starts Duke's interactions with the user.
+     */
     public void run() {
         System.out.println("Hello from\n" + logo + greeting);
 
