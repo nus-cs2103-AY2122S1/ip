3c7e9d744e7d880a8de330a36e737db97a04711c
ntwbruce <e0544240@u.nus.edu>
2021-08-24 03:42:45 +0800

    Implement the Parser class
    The Parser class takes over the Duke class's job of taking in an
    instruction from the user, interpreting it and performing the relevant
    action after.
    
    It will either return a Command (to be implemented) for the Duke class
    to perform, or throw an exception to the Duke class to handle.
    
    Let's implement the Parser class to parse user input and return a
    Command to be executed.

---
+++ src/main/java/Parser.java
@@ -0,0 +1,126 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+
+public class Parser {
+    
+    public static Command parse(String fullCommand, TaskList taskList) throws DukeException {
+        
+        // task function: add tasks
+        switch (fullCommand.split("\\s+")[0]) {
+        case "bye":   // bye function: exits the loop, ends process
+            return new Command("bye");
+            
+        case "list":   // list function: iterates through taskList, prints Tasks' listEntry
+            return new Command("list");
+            
+        case "done":   // done function: sets a task to done
+            if (fullCommand.split("\\s+").length == 1) {
+                throw new DukeException(DukeExceptionType.INVALIDINDEX);
+            } else {
+                int toSet = Integer.parseInt(fullCommand.split("\\s+")[1]);
+                if (taskList.isInvalidIndex(toSet)) {
+                    throw new DukeException(DukeExceptionType.INVALIDINDEX);
+                } else {
+                    return new Command("done", toSet - 1);
+                }
+            }
+            
+        case "delete":   // delete function: delete a task
+            if (fullCommand.split("\\s+").length == 1) {
+                throw new DukeException(DukeExceptionType.INVALIDINDEX);
+            } else {
+                int toDelete = Integer.parseInt(fullCommand.split("\\s+")[1]);
+                if (taskList.isInvalidIndex(toDelete)) {
+                    throw new DukeException(DukeExceptionType.INVALIDINDEX);
+                } else {
+                    return new Command("delete", toDelete - 1);
+                }
+            }
+            
+        case "find":
+            if (fullCommand.split("\\s+").length == 1) {
+                throw new DukeException(DukeExceptionType.INVALIDFIND);
+            } else {
+                LocalDate desiredDate = LocalDate.parse(fullCommand.split("\\s+")[1]);
+                return new Command("find", desiredDate);
+            }
+            
+        default:
+            if (fullCommand.split("\\s+").length == 1) { // task details not given or not valid task
+                switch (fullCommand) {
+                case "deadline":
+                    throw new DukeException(DukeExceptionType.DEADLINEDESC);
+
+                case "event":
+                    throw new DukeException(DukeExceptionType.EVENTDESC);
+
+                case "todo":
+                    throw new DukeException(DukeExceptionType.TODODESC);
+
+                default:
+                    throw new DukeException(DukeExceptionType.INVALIDINPUT);
+                }
+
+            } else {
+                Task newTask;
+
+                // split text string, first string will be the task type and second string will be task details
+                String[] taskString = fullCommand.split("\\s+", 2);
+                String taskType = taskString[0];
+                String taskDetails = taskString[1];
+
+                // determine type of task, create new task
+                switch (taskType) {
+                case "deadline": {
+                    String[] details = taskDetails.split(" /by ");
+
+                    if (details.length == 1) { // time of deadline not given
+                        throw new DukeException(DukeExceptionType.DEADLINETIME);
+                    } else {
+                        String[] deadline = details[1].split(" ");
+                        if (deadline.length == 1) {
+                            newTask = new Deadline(details[0], LocalDate.parse(details[1]));
+                        } else {
+                            newTask = new Deadline(details[0], LocalDate.parse(deadline[0]),
+                                    LocalTime.parse(deadline[1]));
+                        }
+                    }
+                    break;
+                }
+                case "event": {
+                    String[] details = taskDetails.split(" /at ");
+
+                    if (details.length == 1) { // period of event not given
+                        throw new DukeException(DukeExceptionType.EVENTPERIOD);
+                    } else {
+                        String[] periodRange = details[1].split(" ");
+                        if (periodRange.length == 2) {
+                            newTask = new Event(details[0], LocalDate.parse(periodRange[0]),
+                                    LocalDate.parse(periodRange[1]));
+                        } else if (periodRange.length == 3) {
+                            newTask = new Event(details[0], LocalDate.parse(periodRange[0]),
+                                    LocalTime.parse(periodRange[1]), LocalTime.parse(periodRange[2]));
+                        } else if (periodRange.length == 4) {
+                            newTask = new Event(details[0],
+                                    LocalDate.parse(periodRange[0]), LocalTime.parse(periodRange[1]),
+                                    LocalDate.parse(periodRange[2]), LocalTime.parse(periodRange[3]));
+                        } else {
+                            throw new DukeException(DukeExceptionType.INVALIDPERIOD);
+                        }
+                    }
+                    break;
+                }
+                case "todo":
+                    newTask = new Todo(taskDetails);
+                    break;
+
+                default:  // taskName is invalid
+                    throw new DukeException(DukeExceptionType.INVALIDINPUT);
+                }
+                // add task to taskList
+                return new Command(taskType, newTask);
+            }
+        }
+    }
+}
