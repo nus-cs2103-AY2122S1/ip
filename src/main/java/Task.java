fcdb6b7df5801a509cb9255baf837d4cd1d631bc
Cyn7hius <64959508+Cyn7hius@users.noreply.github.com>
2021-08-23 15:54:34 +0800

    Add ability to save task list between each program execution

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,8 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected String taskType;
+    protected String dueDate;
 
     public Task(String description) {
         this.description = description;
@@ -14,6 +16,23 @@ public class Task {
     public void markAsDone() {
         this.isDone = true;
     }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getTaskType() {
+        return this.taskType;
+    }
+
+    public String getDueDate() {
+        return this.dueDate;
+    }
+
+    public int getDoneStatus() {
+        return this.isDone ? 1 : 0;
+    }
+
 
     @Override
     public String toString() {
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -2,10 +2,19 @@ public class Todo extends Task {
 
     public Todo(String desc) {
         super(desc);
+        this.taskType = "T";
+        this.dueDate = "";
+    }
+
+    public Todo(String desc, Boolean isDone) {
+        super(desc);
+        this.taskType = "T";
+        this.dueDate = "";
+        this.isDone = true;
     }
 
     @Override
     public String toString() {
-        return "[T]" + super.toString();
+        return "[" + taskType + "]" + super.toString();
     }
 }
