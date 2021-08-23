56c61e6e237cd6c6ae32a16f201bc18053aaf020
Preshita01 <pandipreshita@gmail.com>
2021-08-23 19:50:04 +0800

    Add Ui.java to deal with interactions with the user

---
+++ src/main/java/Ui.java
@@ -0,0 +1,18 @@
+public class Ui {
+    public Ui() {
+        String logo = "\n" +
+                "       __       ___      .______     ____    ____  __       _______.\n" +
+                "      |  |     /   \\     |   _  \\    \\   \\  /   / |  |     /       |\n" +
+                "      |  |    /  ^  \\    |  |_)  |    \\   \\/   /  |  |    |   (----`\n" +
+                ".--.  |  |   /  /_\\  \\   |      /      \\      /   |  |     \\   \\    \n" +
+                "|  `--'  |  /  _____  \\  |  |\\  \\----.  \\    /    |  | .----)   |   \n" +
+                " \\______/  /__/     \\__\\ | _| `._____|   \\__/     |__| |_______/    \n" +
+                "                                                                    \n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("----------------------------------");
+        System.out.println("Hi! I am Jarvis, your personal assistant :)\n");
+        System.out.println("What can I do for you?");
+        System.out.println("----------------------------------");
+    }
+}
