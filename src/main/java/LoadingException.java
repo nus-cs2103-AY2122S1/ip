14e3d51246e4f1aad7725ec51e2a1658e6b7c0cf
xyliew25 <e0550381@u.nus.edu>
2021-08-23 18:34:40 +0800

    add file exception classes

---
+++ src/main/java/LoadingException.java
@@ -0,0 +1,5 @@
+public class LoadingException extends DukeException {
+  public LoadingException() {
+    super("Duke forgot about your old task list liao, Duke create a new one for you ah!");
+  }
+}
---
+++ src/main/java/StorageException.java
@@ -0,0 +1,5 @@
+public class StorageException extends DukeException {
+  public StorageException() {
+    super("Duke is having goldfish memory today so Duke is not going to memorize your task blekk!");
+  }
+}
