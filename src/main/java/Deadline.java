16ff4afb568dcaadcc53fe508c83c2737ef52678
G <garyyansen@gmail.com>
2021-08-23 18:15:08 +0800

    Overload constructor and add dataFormat method in Task and its subclasses

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,18 @@
 public class Deadline extends Task {
     private final String by;
 
-    public Deadline(String content, String by) {
-        super(content);
+    public Deadline(String content, String by, boolean isDone) {
+        super(content, isDone);
         this.by = by;
+    }
+
+    public Deadline(String content, String by) {
+        this(content, by, false);
+    }
+
+    @Override
+    public String dataFormat() {
+        return String.format("D, %d, %s, %s\n", isDoneInt(), getContent(), by);
     }
 
     @Override
