7e7e0d60e94279aa4d93834032342d52c5ec072c
Zhou-Jiahao-1998 <54730603+Zhou-Jiahao-1998@users.noreply.github.com>
2021-08-23 10:11:33 +0800

    Format output date correctly

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -20,7 +20,8 @@ public class Deadline extends Task{
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by:" + this.deadline + ")";
+        return "[D]" + super.toString() + "(by: "
+                + this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy" + ")"));
     }
 
     @Override
