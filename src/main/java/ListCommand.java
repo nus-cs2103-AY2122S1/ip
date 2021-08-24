faa87151ca6acc362c33062aa5fce78d75751dec
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-08-24 22:53:05 +0800

    Update ListCommand execute() method

--- src/main/java/ListCommand.java
+++ src/main/java/ListCommand.java
@@ -4,11 +4,6 @@ public class ListCommand extends Command {
 
     @Override
     public void execute(TaskList taskList, Ui ui, Storage storage) throws DukeException {
-        try {
-            storage.writeCurrentData(taskList.getTasks());
-        } catch(IOException e) {
-            System.out.println(e.getMessage());
-        }
         ui.displayListOfTasks(taskList);
     }
 }
