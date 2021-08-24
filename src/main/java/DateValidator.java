b88072f74ff1073658dffa5eedd440168c21e854
meerian <myronneozx@gmail.com>
2021-08-25 03:02:36 +0800

    Teach Duke how to understand dates and times.

---
+++ src/main/java/DateValidator.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateValidator {
+    private DateTimeFormatter format;
+
+    public DateValidator(DateTimeFormatter format) {
+        this.format = format;
+    }
+
+    public boolean isValid(String string) {
+        try {
+            LocalDate.parse(string, format);
+        } catch (DateTimeParseException e ) {
+            return false;
+        }
+
+        return true;
+    }
+}
--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,26 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
     private String when;
+    private LocalDate time = null;
+
     public Deadline(String str1, String str2) throws DukeException {
         super(str1);
         this.when = str2;
     }
 
+    public Deadline(String str1, LocalDate time) throws DukeException {
+        super(str1);
+        this.time = time;
+    }
+
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + when + ")";
+        if (time == null) {
+            return "[D]" + super.toString() + " (by: " + when + ")";
+        } else {
+            return "[D]" + super.toString() + " (by: " + time.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
+        }
     }
 }
