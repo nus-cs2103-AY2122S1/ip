039e16cf344641ffedbb6d3f67f3f1784875fb87
asherhy <hongyongrs@gmail.com>
2021-08-25 00:47:45 +0800

    Add DukeStorage.java and TaskHandler.java

---
+++ src/main/java/DukeStorage.java
@@ -0,0 +1,79 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+public class DukeStorage {
+    private static final String FILE_NAME = "./data/allTasks.txt";
+    private static final File file = new File(FILE_NAME);
+
+    public static ArrayList<Task> loadFile() throws DukeException {
+        try {
+            Scanner sc = new Scanner(file);
+            ArrayList<Task> loadedTasks = new ArrayList<>();
+            while (sc.hasNextLine()) {
+                loadedTasks.add(formatToRead(sc.nextLine()));
+            }
+            return loadedTasks;
+        } catch (FileNotFoundException e) {
+            createFile();
+            return new ArrayList<>();
+        }
+    }
+
+    private static void createFile() {
+        try {
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+        } catch (IOException e) {
+            System.err.println("IOException: ");
+        }
+    }
+
+    public static void writeFile(Task task) throws DukeException {
+        try {
+            FileWriter fileWriter = new FileWriter(FILE_NAME, true);
+            fileWriter.write(task.formatToWrite() + System.lineSeparator());
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new DukeException("IOException: ");
+        }
+    }
+
+    public static void updateFile(ArrayList<Task> tasks) throws DukeException {
+        try {
+            FileWriter fileWriter = new FileWriter(FILE_NAME);
+            for (Task t: tasks) {
+                fileWriter.write(t.formatToWrite() + System.lineSeparator());
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new DukeException("IOException: ");
+        }
+    }
+
+    private static Task formatToRead(String s) throws DukeException {
+        String[] info = s.split(" \\| ");
+        Task task;
+
+        switch (info[0]) {
+            case "D":
+                task = new Deadline(info[2], info[3]);
+                break;
+            case "E":
+                task = new Event(info[2], info[3]);
+                break;
+            case "T":
+                task = new Todo(info[2]);
+                break;
+            default:
+                throw new DukeException("Error unknown value: " + info[0]);
+        }
+        if (info[1].equals("1")) {
+            task.markAsDone();
+        }
+        return task;
+    }
+}
