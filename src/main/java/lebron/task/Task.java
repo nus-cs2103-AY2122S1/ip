f01e32b602956d3c724478ca05da4b9889d4834c
nniiggeell <e0559733@u.nus.edu>
2021-08-24 23:59:44 +0800

    Move files to new directory
    For A-Packages, included packages and import statements.

---
+++ src/main/java/lebron/task/Task.java
@@ -0,0 +1,62 @@
+package lebron.task;
+
+/**
+ * This class represents a Task the user gives to the bot.
+ *
+ * @author Nigel Tan
+ */
+public class Task {
+
+    private String name;
+    private boolean isDone;
+
+    /**
+     * Constructor.
+     *
+     * @param name the name of the task
+     */
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    /**
+     * This method marks the task as true.
+     */
+    public void mark() {
+        this.isDone = true;
+    }
+
+    /**
+     * This method returns the status icon of the tast, X if true, blank if false.
+     *
+     * @return X if true, blank if false
+     */
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    /**
+     * This method returns the name of the Task.
+     *
+     * @return name of the Task
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDoneValue() {
+        return isDone ? "1" : "0";
+    }
+
+    public String toFile() {
+        return " | " + getDoneValue() + " | " + name;
+    }
+
+    @Override
+    public String toString() {
+        String format = String.format("[%s] ", this.getStatusIcon());
+        return format + this.name;
+    }
+
+}
