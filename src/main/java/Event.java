e27b3563f481802d976992035033e72b9b9a2918
wengYing227 <e0492479@u.nus.edu>
2021-08-24 14:09:29 +0800

    Adds Date and Time

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,49 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String at;
+    protected LocalDateTime at;
+    private Boolean hasTime = true;
 
     public Event(String description, String at) {
         super(description);
-        this.at = at;
+        this.at = parseTime(at);
+    }
+
+    public LocalDateTime parseTime(String time) {
+        String newDate;
+        String[] str = time.split(" ");
+        String[] oldDate = str[0].split("/");
+        LocalDateTime localTime;
+        if(str.length > 1) {
+            String hour = str[1].substring(0,2);
+            String min = str[1].substring(2,4);
+
+            localTime = LocalDateTime.of(Integer.parseInt(oldDate[2]), Integer.parseInt(oldDate[1]), Integer.parseInt(oldDate[0]),
+                    Integer.parseInt(hour), Integer.parseInt(min));
+        } else {
+            localTime = LocalDate.of(Integer.parseInt(oldDate[2]), Integer.parseInt(oldDate[1]), Integer.parseInt(oldDate[0]))
+                    .atStartOfDay();
+            hasTime = false;
+        }
+        return localTime;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + at + ")";
+        String string = "[E]" + super.toString() + "(at: " ;
+        if (hasTime) {
+            string += at.format(DateTimeFormatter.ofPattern("HH:mm, MMM dd yyyy")) + ")";
+        } else {
+            string += at.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
+        }
+        return string;
+    }
+
+    public static void main(String[] args) {
+        Event e = new Event("return book", "2/12/2019 1800");
+        System.out.println(e);
     }
 }
 
