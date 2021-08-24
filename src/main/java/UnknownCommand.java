d69721daafe9ab7c188b54d4342a493ba592034c
DesmondTo <e0550619@u.nus.edu>
2021-08-24 17:46:07 +0800

    Complete UnknownCommand class implementation

--- src/main/java/UnknownCommand.java
+++ src/main/java/UnknownCommand.java
@@ -1,8 +1,26 @@
-public class UnknownCommand extends Command{
+public class UnknownCommand extends Command {
 
     @Override
-    public void execute(TaskList tasks, Ui ui, Storage storage) {
-
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws UnknownCommandException {
+        String suggestions = String.format("%s\n" +
+                "\t%s\n" +
+                "\t %s\n" +
+                "\t %s\n" +
+                "\t %s\n" +
+                "\t %s\n" +
+                "\t %s\n" +
+                "\t %s\n" +
+                "\t %s\n",
+                "I'm sorry, but I don't know what that means :-(",
+                "Try the following commands instead:\n",
+                "1. list - List all the existing tasks",
+                "2. bye - Exit from Duke's service",
+                "3. done {N} -  Mark task of number 'N' as done",
+                "4. delete {N} - Delete task of number 'N'",
+                "5. todo {description} - Add a todo with the specified 'description'",
+                "6. event {description} /at {date} - Add an event with the specified 'description' happening at 'date'",
+                "7. deadline {description} /by {date} - Add a deadline with the specified 'description' expires at 'date'");
+        throw new UnknownCommandException(suggestions);
     }
 
     @Override
