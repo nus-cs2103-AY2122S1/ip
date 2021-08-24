4dab6a604c8d32915edd50e87e1a700a7610ad09
Darren Heng <darren2pro@gmail.com>
2021-08-24 15:24:11 +0800

    Complete Level-7

--- src/main/java/ToDos.java
+++ src/main/java/ToDos.java
@@ -1,11 +1,17 @@
 public class ToDos extends Task {
 
-    public ToDos(String description) {
-        super(description);
+    public ToDos(String description, boolean isDone) {
+        super(description, isDone);
     }
-
+    @Override
+    public String persistedDataStringFormat() {
+        char isDone01 = this.isDone ? '1' : '0';
+        return String.format("T,%c,%s", isDone01, this.description);
+    }
     @Override
     public String toString() {
-        return String.format("[T]" + super.toString());
+        return "[T]" + super.toString();
     }
+
+
 }
