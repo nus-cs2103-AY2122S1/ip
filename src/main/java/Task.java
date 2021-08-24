c5c7777a4724a1161e91efb08bd2b2b473dc41df
GWphua <phuaguanwei99@gmail.com>
2021-08-25 02:21:28 +0800

    Add ability to read Dates and Times

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Task {
 
     private boolean isDone;
@@ -35,6 +39,59 @@ public class Task {
      */
     public String getTaskDescription() {
         return taskDescription;
+    }
+
+    /**
+     * Converts the input string from 24-hour time to 12-hour time representation.
+     * @param timeString The input string
+     * @return The converted string.
+     */
+    private String convertTime(String timeString) {
+        try {
+            int timeIn24hRepresentation = Integer.parseInt(timeString);
+
+            if (timeIn24hRepresentation < 0 || timeIn24hRepresentation > 2359
+                    || Integer.parseInt(timeString.substring(2,3)) > 5) {
+                return timeString;
+            } else {
+                int hours = timeIn24hRepresentation/100;
+                int minutes = timeIn24hRepresentation%100;
+
+                if (hours > 12) {
+                    hours -= 12;
+                    return hours + ":" + minutes + "PM";
+                } else {
+                    return hours + ":" + minutes + "AM";
+                }
+            }
+        } catch (NumberFormatException e) {
+            return timeString;
+        }
+    }
+
+    /**
+     * Converts the inputString into DD MMM YYYY format, or 12-hour time representation if applicable.
+     * @param inputString The input string.
+     * @return A string in DD MMM YYYY TIME format if inputString is in "YYYY-MM-DD TIME" format.
+     */
+    public String convert(String inputString) {
+        if (inputString.length() < 10) {
+            return convertTime(inputString);
+        }
+
+        String dateString = inputString.trim().substring(0, 10);
+        String timeString = inputString.trim().substring(10).trim();
+        try {
+            String date = LocalDate.parse(dateString).format(DateTimeFormatter.ofPattern("d MMM yyyy"));
+
+            if (timeString.length() == 4) {
+                return date + " " + convertTime(timeString);
+            }
+
+            return date + timeString;
+        } catch (DateTimeParseException e) {
+            return inputString;
+        }
     }
 
     /**
