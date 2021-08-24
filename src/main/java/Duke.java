b911d5a5ed0b79baa46ebc1b04086f3eea1ee7aa
El0hime <dillontkw@gmail.com>
2021-08-25 02:01:52 +0800

    Tweak the code to comply with a coding standard
    Did not change much as code was tweaked when adding JavaDoc comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,7 +24,7 @@ public class Duke {
         while (!isExit) {
             try {
                 String fullCommand = ui.readCommand();
-                ui.showLine(); // show the divider line ("_______")
+                ui.showLine();
                 Command c = Parser.parse(fullCommand);
                 c.execute(tasks, ui, storage);
                 isExit = c.isExit();
