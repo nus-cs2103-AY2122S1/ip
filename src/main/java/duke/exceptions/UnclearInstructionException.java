1ed9414dc4697fd5063fd41c4f304962c4d2b643
tianyue58 <e0563952@u.nus.edu>
2021-08-24 11:56:09 +0800

    Add JavaDoc comments

--- src/main/java/duke/exceptions/UnclearInstructionException.java
+++ src/main/java/duke/exceptions/UnclearInstructionException.java
@@ -1,3 +1,5 @@
+package duke.exceptions;
+
 public class UnclearInstructionException extends Exception {
     public UnclearInstructionException(String message) {
         super(message);
--- src/main/java/duke/exceptions/UnknownException.java
+++ src/main/java/duke/exceptions/UnknownException.java
@@ -1,3 +1,5 @@
+package duke.exceptions;
+
 public class UnknownException extends Exception {
     public UnknownException() {
         super("OOPS!!! I'm sorry, but I don't know what that means :-(");
