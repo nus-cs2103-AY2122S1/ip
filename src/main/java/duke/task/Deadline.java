6727fa9cbd510cf17416115b845e534bf2e50b3f
KelvinSoo <e0544272@u.nus.edu.sg>
2021-08-24 23:13:19 +0800

    Add more JavaDoc comments

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -46,21 +46,37 @@ public class Deadline extends Task {
 
     }
 
+    /**
+     * A method to get the state of the task.
+     * @return The status icon
+     */
     @Override
     public String getStatusIcon() {
         return "[D]" + super.getStatusIcon();
     }
 
+    /**
+     * A method to get data about the task in a savable format.
+     * @return The savable data
+     */
     @Override
     public String getMetaData() {
         return String.format("D|%s|%s", super.getMetaData(), dateLine);
     }
 
+    /**
+     * A method to get the description of the task.
+     * @return The description of the task including date
+     */
     @Override
     public String getDescription() {
         return String.format("%s (by: %s)", super.getDescription(), this.dateLine);
     }
 
+    /**
+     * A method to get all task details.
+     * @return All the task details
+     */
     @Override
     public String toString() {
         return String.format("%s %s", getStatusIcon(), getDescription());
