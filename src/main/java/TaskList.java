d2ea3d56e2f72132b8c79e911aae56b609c7bede
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-08-24 21:46:28 +0800

    Create TaskList class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> tasks = new ArrayList<>();
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    // For ToDo, event, deadline
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+
+    // For done command
+    public void markTaskAsDone(int index) {
+        this.tasks.get(index).markAsDone();
+    }
+
+    // For delete command
+    public void deleteTask(int index) {
+        this.tasks.remove(index);
+    }
+
+    // For List command
+    @Override
+    public String toString() {
+        String str = "";
+
+        for (int i = 0; i < tasks.size(); i++) {
+            str += "\t " + (i + 1) + ". " + this.tasks.get(i);
+        }
+
+        return str;
+    }
+}
