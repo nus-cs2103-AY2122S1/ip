0d64b0f42f1fbdc70bb0cbbf6764eb4da2ffa426
junwei26 <limjunwei26@gmail.com>
2021-08-24 10:10:58 +0800

    Add handling of dates for deadlines

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        int[] deadlineArr = Arrays.stream(by.split("-")).mapToInt(Integer::parseInt).toArray();
+        this.by = LocalDate.of(deadlineArr[0], deadlineArr[1], deadlineArr[2]);
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), by);
+        return String.format("[D]%s (by: %s)", super.toString(), by.format(DateTimeFormatter.ofPattern("dd MMMM yyyy")));
     }
 }
