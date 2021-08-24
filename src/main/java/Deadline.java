f4aa8f633eca0d120002384c5f988a76d7cb58de
kaldius <quantf@gmail.com>
2021-08-24 21:15:15 +0800

    Add ability to use java.time.LocalDate for Deadline and Event

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,15 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * The Deadline class encapsulates all the details of each deadline.
  */
-
 public class Deadline extends Task {
-    private final String endTime;
+    private final LocalDate endTime;
 
     public Deadline(String message, String endTime){
         super(message);
-        this.endTime = endTime;
+        this.endTime = LocalDate.parse(endTime);
     }
 
     /**
@@ -16,6 +18,16 @@ public class Deadline extends Task {
      */
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + endTime + ")";
+        return "[D]" + super.toString() + "(by: " +
+                endTime.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+    }
+    /**
+     * Converts contents to a storable String.
+     *
+     * @return a String that represents this Event in storage
+     */
+    @Override
+    public String toStorage() {
+        return "D|" + super.toStorage() + "/by " + this.endTime;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,12 +1,15 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * The Event class encapsulates all the details of each event.
  */
 public class Event extends Task {
-    private final String timePeriod;
+    private final LocalDate eventDate;
 
-    public Event(String message, String timePeriod){
+    public Event(String message, String eventDate){
         super(message);
-        this.timePeriod = timePeriod;
+        this.eventDate = LocalDate.parse(eventDate);
     }
 
     /**
@@ -15,6 +18,17 @@ public class Event extends Task {
      */
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + timePeriod + ")";
+        return "[E]" + super.toString() + "(at: " +
+                eventDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"))+ ")";
+    }
+
+    /**
+     * Converts contents to a storable String.
+     *
+     * @return a String that represents this Event in storage
+     */
+    @Override
+    public String toStorage() {
+        return "E|" + super.toStorage() + "/at " + this.eventDate;
     }
 }
