83cdcea6544719586f14243ab6c864060e29339c
nzixuan <nzixuan@gmail.com>
2021-08-24 19:47:38 +0800

    Add date time parsing to Deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,40 @@
-public class Deadline extends Task{
-    String date;
-    public Deadline(String desc, String date) {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Deadline extends Task {
+    private static final String REGEX = "(\\d{2}\\/\\d{2}\\/\\d{4})";
+
+    String dateDesc;
+    LocalDate date;
+
+    public Deadline(String desc, String dateDesc) {
         super(desc);
-        this.date= date;
+        parseDate(dateDesc);
+    }
+
+    private void parseDate(String dateDesc) {
+        Matcher m = Pattern.compile(REGEX).matcher(dateDesc);
+        if (m.find()) {
+            try {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+                this.date = LocalDate.parse(m.group(1), formatter);
+                this.dateDesc = dateDesc.replace(m.group(1), date.format(DateTimeFormatter.ofPattern("MMM dd yyyy")));
+            }
+            catch (DateTimeParseException e) {
+                this.dateDesc = dateDesc;
+                this.date = null;
+            }
+        } else {
+            this.dateDesc = dateDesc;
+            this.date = null;
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: "+date+")";
+        return "[D]" + super.toString() + "(by: " + this.dateDesc + ")";
     }
 }
