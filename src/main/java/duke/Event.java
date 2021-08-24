1d98bdf2f475cb39616265e9e9f8e8627a54f628
NUSmhk <e0550349@u.nus.edu>
2021-08-24 23:16:17 +0800

    Add JavaDoc comments for all non-private classes/methods not under duke.command package

--- src/main/java/duke/Event.java
+++ src/main/java/duke/Event.java
@@ -4,19 +4,34 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * This represents a type of task that has a duration.
+ */
 public class Event extends Task{
 
     protected LocalTime startTime;
     protected LocalTime endTime;
     protected LocalDate date;
 
-    public Event(String input, LocalDate date, LocalTime startTime, LocalTime endTime ) throws DukeException {
+    /**
+     * This constructor creates an Event task type object.
+     * @param input text description of Event task.
+     * @param date date of occurrence of Event.
+     * @param startTime start time of Event.
+     * @param endTime end time of Event.
+     */
+    public Event(String input, LocalDate date, LocalTime startTime, LocalTime endTime) {
         super(input);
         this.date = date;
         this.startTime = startTime;
         this.endTime = endTime;
     }
 
+    /**
+     * This method formats task description and duration in a user-friendly way,
+     * additionally marking the Event task type with [E]
+     * @return String of formatted task description and deadline.
+     */
     @Override
     public String toString() {
             return String.format("[E]%s (at: %s from: %s)", super.toString(),
--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -1,18 +1,34 @@
 package duke;
 
+/**
+ * This represents the parent class of all type of tasks Duke handles.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * This constructor creates a Task object which contains the description of task.
+     * The task can either be considered done or not according to user command.
+     * @param description text description of a general task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * This method marks the task as done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * This method formats task description by marking tasks that are done with a 'X' and
+     * leaving it blank if the task is not considered done.
+     * @return String of formatted task description and if task is marked done.
+     */
     @Override
     public String toString() {
         return (isDone ? "[X] " + this.description : "[ ] " + this.description); // mark done task with X
