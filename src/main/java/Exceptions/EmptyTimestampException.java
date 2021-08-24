928bcc908ef3d57f6a1953722ac45590414949e1
yzhilong <44795294+yzhilong@users.noreply.github.com>
2021-08-24 13:26:57 +0800

    Code cleanup

--- src/main/java/Exceptions/EmptyTimestampException.java
+++ src/main/java/Exceptions/EmptyTimestampException.java
@@ -3,6 +3,6 @@ package Exceptions;
 public class EmptyTimestampException extends DukeException {
 
     public EmptyTimestampException() {
-        super("The timestamp of a Tasks.Deadline/Tasks.Event task cannot be empty!");
+        super("The timestamp of a Deadline/Event task cannot be empty!");
     }
 }
