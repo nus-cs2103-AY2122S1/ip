35a7bdadfa524cd49c4f4976b6eda3a20f4801d1
marcuschj <haojie.chua@gmail.com>
2021-08-24 11:05:24 +0800

    Add pre-release for A-Jar

--- src/main/java/duke/DeadlineTest.java
+++ src/main/java/duke/DeadlineTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class DeadlineTest {
-    @Test
+    @Test 
     public void testDeadlineToString(){
         Deadline deadline = new Deadline("description", "tomorrow", false, false);
         Assert.assertEquals("[D][ ] description (by: tomorrow)", deadline.toString());
