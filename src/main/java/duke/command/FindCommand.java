1b160d0d5040801970976ff1209e29f6e2342911
calvintanwj <calvintanwj127@gmail.com>
2021-08-24 19:22:47 +0800

    Add find command
    Currently, users are unable to find a set of tasks in their lengthy task
    lists.
    
    Now, by implementing a new command, FindCommand, users can then search
    for a specified keyword, and in return receive a set of tasks that match
    the keyword.

---
+++ src/main/java/duke/command/FindCommand.java
@@ -0,0 +1,47 @@
+package duke.command;
+
+import duke.exception.DukeException;
+import duke.storage.Storage;
+import duke.task.Task;
+import duke.task.TaskList;
+import duke.ui.Ui;
+
+import java.util.ArrayList;
+
+public class FindCommand extends Command {
+
+    public String arguments;
+    
+    public FindCommand(String arguments) {
+        super("find");
+        this.arguments = arguments;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
+        if (arguments.isEmpty()) {
+            throw new DukeException("No matching string was entered.");
+        }
+        ArrayList<Task> matchedTasks = new ArrayList<>();
+        ui.printToUser("Here are the matching tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).getDescription().contains(arguments)) {
+                matchedTasks.add(tasks.get(i));
+            }
+        }
+        if (matchedTasks.isEmpty()) {
+            ui.printToUser("  There are no tasks that match your query. Try again."); 
+        } else {
+            for (int i = 0; i < matchedTasks.size(); i++) {
+                Task currTask = matchedTasks.get(i);
+                ui.printToUser((i + 1) + ". " + currTask);
+            }
+        }
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+
+}
