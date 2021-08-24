96bbe11a8b752fd03c7bbc6fee22397c6b355a2b
NaokiShum <cxjdwedyk097@gmail.com>
2021-08-24 19:11:20 +0800

    Added some J-Unit test

--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -12,7 +12,7 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (isDone ? "X" : " ");
     }
 
     public void markAsDone(){
--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest {
     @Test
-    public void dummyTest(){
+    public void Test1(){
         String test = "BYE";
         assertEquals("bye", Parser.parseCommand(test));
     }
