dc440d8dbaf2ef6c0da55d98526e2aa923781b75
DesmondTo <e0550619@u.nus.edu>
2021-08-24 17:50:21 +0800

    Complete ListCommand class implementation

--- src/main/java/ListCommand.java
+++ src/main/java/ListCommand.java
@@ -1,7 +1,17 @@
 public class ListCommand extends Command{
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-
+        StringBuilder result = new StringBuilder();
+        int currentIndex = 1;
+        for (Task task : tasks.getTasks()) {
+            if (task != null)
+                result.append("\n\t ")
+                        .append(currentIndex)
+                        .append(".")
+                        .append(task);
+            currentIndex ++;
+        }
+        System.out.println("\tHere are the tasks in your list:" + result);
     }
 
     @Override
