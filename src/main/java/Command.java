2e3c2eb38e9f9ced0034b0d75e04d6060ac7bfa4
alinaleehx <alinaleehx@gmail.com>
2021-08-24 14:47:54 +0800

    Add dates for all tasks objects.
    Accept dates in a yyyy-mm-dd format (e.g., 2019-10-15) and print in
    a different MMM dd yyyy format (e.g., Oct 15 2019).

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 
 /**
@@ -53,9 +55,16 @@ public class Command {
         if (description.isEmpty()) {
             throw new DukeException("OOPS!!! The description of an event cannot be empty.");
         } else if (at.isEmpty()) {
-            throw new DukeException("OOPS!!! No date for event! Use format of event description /at date");
+            throw new DukeException("OOPS!!! No date for event! Use format of event description /at YYYY-MM-DD");
         }
-        Event toAdd = new Event(description, at);
+
+        LocalDate date;
+        try {
+            date = LocalDate.parse(at.trim());
+        } catch (DateTimeParseException e) {
+           throw new DukeException("OOPS!! Date and Time must be specified by YYYY-MM-DD");
+        }
+        Event toAdd = new Event(description, date);
         records.add(toAdd);
         return String.format("Got it. I've added this task:\n\t %1$s \n\t" +
                         "Now you have %2$d tasks in the list.\n\t", toAdd.toString(), records.size());
@@ -73,9 +82,16 @@ public class Command {
         if (description.isEmpty()) {
             throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
         } else if (by.isEmpty()) {
-            throw new DukeException("OOPS!!! No date for deadline! Use format of deadline description /by date");
+            throw new DukeException("OOPS!!! No date for deadline! Use format of deadline description /by YYYY-MM-DD");
         }
-        Deadline toAdd = new Deadline(description, by);
+        LocalDate date;
+        try {
+            date = LocalDate.parse(by.trim());
+        } catch (DateTimeParseException e) {
+            throw new DukeException("OOPS!! Date and Time must be specified by YYYY-MM-DD");
+        }
+
+        Deadline toAdd = new Deadline(description, date);
         records.add(toAdd);
         return String.format("Got it. I've added this task:\n\t %1$s \n\t" +
                         "Now you have %2$d tasks in the list.\n\t", toAdd.toString(), records.size());
--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,20 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * The Deadline class extends Task class and encapsulate a deadline task
  * that need to be done before a specific date/time.
  */
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDate by) {
         super(description);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by +")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) +")";
     }
 }
