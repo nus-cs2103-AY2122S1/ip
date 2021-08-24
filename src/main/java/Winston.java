210a85b4afc566e2e1b5c7ad3cfb82b326d9277d
Imerbear <e0543864@u.nus.edu>
2021-08-24 17:29:25 +0800

    Changed indentations for switch and case

--- src/main/java/Winston.java
+++ src/main/java/Winston.java
@@ -16,8 +16,8 @@ public class Winston {
     }
 
     /**
-     *  Adds a task to the arraylist
-     *  
+     * Adds a task to the arraylist
+     *
      * @param task An Object of type Task to be added to the Arraylist
      */
     private void addTask(Task task) {
@@ -35,9 +35,9 @@ public class Winston {
 
     /**
      * Removing a Task from the arraylist based on position
-     * 
+     *
      * @param position the position of the Task to be removed from the arraylist
-     * Note: position will be (index of item in array list + 1)
+     *                 Note: position will be (index of item in array list + 1)
      */
     public void deleteTask(Integer position) {
         list.remove(position - 1);
@@ -45,7 +45,7 @@ public class Winston {
 
     /**
      * Number of tasks left to be completed
-     * 
+     *
      * @return the number of tasks left in the list that are not completed
      */
     public int size() {
@@ -67,7 +67,7 @@ public class Winston {
         Integer counter = 1;
         StringBuilder result = new StringBuilder("List of things to do:\n");
         for (Task task : this.list) {
-            result.append( "\t" + counter + ". " + task.taskCompletion() + task.toString() + "\n");
+            result.append("\t" + counter + ". " + task.taskCompletion() + task.toString() + "\n");
             counter += 1;
         }
         result.append("End");
@@ -82,64 +82,64 @@ public class Winston {
         Winston winston1 = new Winston();
         while (!cmd.equals("bye")) {
             switch (cmd) {
-                case "list":
-                    System.out.println(winston1.getList());
-                    System.out.println(winston1.size() + " tasks remaining.");
-                    break;
-                case "done":
-                    System.out.println(winston1.getList());
-                    System.out.println("Which task number have you completed?");
-                    try{
-                        winston1.markTask(scan.nextInt());
-                    } catch(InputMismatchException | IndexOutOfBoundsException e) {
-                        System.out.println("Please give a valid number. Resetting to home menu.");
-                        break;
-                    }
-                    System.out.println("Don't worry, I've got you. Task Marked!"+ "\n" +
-                            winston1.size() + " tasks remaining.");
-                    break;
-                case "todo":
-                    System.out.println("What task would you like to add?");
-                    winston1.addTask(new ToDoTask(scan.nextLine()));
-                    System.out.println("Task Added!");
-                    break;
-                case "deadline": {
-                    System.out.println("What task would you like to add?");
-                    String task = scan.nextLine();
-                    System.out.println("What is the due date of this task?");
-                    String dueDate = scan.nextLine();
-                    winston1.addTask(new DeadLine(task, dueDate));
-                    System.out.println("Task Added!");
+            case "list":
+                System.out.println(winston1.getList());
+                System.out.println(winston1.size() + " tasks remaining.");
+                break;
+            case "done":
+                System.out.println(winston1.getList());
+                System.out.println("Which task number have you completed?");
+                try {
+                    winston1.markTask(scan.nextInt());
+                } catch (InputMismatchException | IndexOutOfBoundsException e) {
+                    System.out.println("Please give a valid number. Resetting to home menu.");
                     break;
                 }
-                case "event": {
-                    System.out.println("What task would you like to add?");
-                    String task = scan.nextLine();
-                    System.out.println("When is this event?");
-                    String on = scan.nextLine();
-                    winston1.addTask(new Event(task, on));
-                    System.out.println("Task Added!");
+                System.out.println("Don't worry, I've got you. Task Marked!" + "\n" +
+                        winston1.size() + " tasks remaining.");
+                break;
+            case "todo":
+                System.out.println("What task would you like to add?");
+                winston1.addTask(new ToDoTask(scan.nextLine()));
+                System.out.println("Task Added!");
+                break;
+            case "deadline": {
+                System.out.println("What task would you like to add?");
+                String task = scan.nextLine();
+                System.out.println("What is the due date of this task?");
+                String dueDate = scan.nextLine();
+                winston1.addTask(new DeadLine(task, dueDate));
+                System.out.println("Task Added!");
+                break;
+            }
+            case "event": {
+                System.out.println("What task would you like to add?");
+                String task = scan.nextLine();
+                System.out.println("When is this event?");
+                String on = scan.nextLine();
+                winston1.addTask(new Event(task, on));
+                System.out.println("Task Added!");
+                break;
+            }
+            case "delete": {
+                System.out.println(winston1.getList());
+                System.out.println("What task would you like to delete?");
+                try {
+                    winston1.deleteTask(scan.nextInt());
+                } catch (InputMismatchException | IndexOutOfBoundsException e) {
+                    System.out.println("Please give a valid number. Resetting to home menu.");
                     break;
                 }
-                case "delete": {
-                    System.out.println(winston1.getList());
-                    System.out.println("What task would you like to delete?");
-                    try{
-                        winston1.deleteTask(scan.nextInt());
-                    } catch(InputMismatchException | IndexOutOfBoundsException e) {
-                        System.out.println("Please give a valid number. Resetting to home menu.");
-                        break;
-                    }
-                    
-                    System.out.println("Don't worry, I've got you. Task Deleted!" + "\n" +
-                            winston1.size() + " tasks remaining.");
-                }
-                case "": {
-                    break;
-                }
-                default: {
-                    System.out.print("Invalid command. Please input a valid command." + "\n");
-                }
+
+                System.out.println("Don't worry, I've got you. Task Deleted!" + "\n" +
+                        winston1.size() + " tasks remaining.");
+            }
+            case "": {
+                break;
+            }
+            default: {
+                System.out.print("Invalid command. Please input a valid command." + "\n");
+            }
             }
             cmd = scan.nextLine();
         }
