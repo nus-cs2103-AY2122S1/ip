827ff2a8c6752e0d094c3b4fa006d688fb310eb4
DesmondTo <e0550619@u.nus.edu>
2021-08-24 17:51:06 +0800

    Complete ExitCommand class implementation

--- src/main/java/ExitCommand.java
+++ src/main/java/ExitCommand.java
@@ -1,7 +1,7 @@
 public class ExitCommand extends Command{
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-
+        System.out.println("\tBye (*´▽｀)ノシ. Have a nice day!");
     }
 
     @Override
