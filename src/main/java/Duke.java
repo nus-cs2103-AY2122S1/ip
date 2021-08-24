6b1a6cd1ca8a18dede36f997bb6ef4695008b41b
ChenHsiaoTing <hsiaoting0919@gmail.com>
2021-08-24 21:28:22 +0800

    Level-7-Save

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,20 @@
+<<<<<<< Updated upstream
+=======
+/**
+ * Duke class encapsulate a chatbot service.
+ * It supports queries such as creating, marking and deleting tasks.
+ *
+ * @author: Chen Hsiao Ting
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+>>>>>>> Stashed changes
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
