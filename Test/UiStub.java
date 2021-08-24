36ea17805d7b3db739eff40c01f14ab28b5cd8d8
nniiggeell <e0559733@u.nus.edu>
2021-08-25 00:53:22 +0800

    Add UiStub.java
    A stub file for Ui.java

---
+++ Test/UiStub.java
@@ -0,0 +1,12 @@
+import lebron.task.Task;
+import lebron.Ui;
+
+import java.util.ArrayList;
+
+public class UiStub extends Ui {
+    @Override
+    public void replyAdd(ArrayList<Task> lst, Task task) {
+
+    }
+
+}
