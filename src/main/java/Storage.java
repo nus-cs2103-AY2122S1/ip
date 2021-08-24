f3ddfd8eb7222ad63b755a718d7469572bb8c0a3
Owen Tan <tanteongyuowen@gmail.com>
2021-08-25 01:34:51 +0800

    Create Storage class

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -10,41 +10,47 @@ public class Storage {
         this.tasks = tasks;
     }
 
-    public void load() throws IOException {
-        File fileDir = new File(path).getParentFile();
-        File file = new File(path);
+    public void load() {
+        try {
+            File fileDir = new File(path).getParentFile();
+            File file = new File(path);
 
-        if (!fileDir.exists()) {
-            fileDir.mkdirs();
-        }
-
-        if (file.createNewFile()) {
-        } else {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                tasks.add(parseTask(line));
+            if (!fileDir.exists()) {
+                fileDir.mkdirs();
             }
 
+            if (!file.createNewFile()) {
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line;
 
+                while ((line = reader.readLine()) != null) {
+                    tasks.add(parseTask(line));
+                }
+                reader.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Something went wrong with loading");
         }
     }
 
-    public void save() throws IOException {
-        FileWriter writer = new FileWriter(path);
-        for (Task task : tasks) {
-            writer.write(task.printFormat());
-            writer.write("\n");
+    public void save() {
+        try {
+            FileWriter writer = new FileWriter(path);
+            for (Task task : tasks) {
+                writer.write(task.printFormat());
+                writer.write("\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Something went wrong with saving");
         }
-        writer.close();
     }
 
     private Task parseTask(String task) {
-        String[] tokens = task.split(" | ");
+        String[] tokens = task.split(" \\| ");
         Task t = null;
         // T | 0 | description | addInfo
-        boolean isDone = tokens[1] == "1";
+        boolean isDone = tokens[1].equals("1");
         switch (tokens[0]) {
             case "T":
                 t = new Todo(tokens[2], isDone);
@@ -56,7 +62,6 @@ public class Storage {
                 t = new Event(tokens[2], isDone, tokens[3]);
                 break;
         }
-
         return t;
     }
 }
