92006250b37fc0af6eebe579a46f4d6175e46a4e
over-fitted <overfitted@gmail.com>
2021-08-25 01:23:17 +0800

    Convert date storage type in Deadline to LocalDate type

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,26 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    private String deadline;
+    private LocalDate deadline;
 
     public Deadline(String description, String deadline) {
         super(description);
-        this.deadline = deadline;
+        try{
+            this.deadline = LocalDate.parse(deadline);
+        } catch (DateTimeParseException e) {
+            this.deadline = null;
+            System.out.println("Deadline was not readable, saving as unspecified deadline");
+        }
+
     }
 
     @Override
     public String toString() {
         String stem = super.toString();
-        return String.format("[D]%s (by: %s)", stem, this.deadline);
+        return String.format("[D]%s (by: %s)", stem, this.deadline == null
+                                                     ? "unspecified"
+                                                     : this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 }
