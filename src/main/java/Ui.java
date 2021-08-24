431a7ac8f346cc643aa2bd4923804f41ff93c3eb
hanif-kamal <hanif.kamal47@gmail.com>
2021-08-24 22:41:53 +0800

    Create Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ui {
+    private Scanner scanner;
+
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void welcome() {
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+    }
+
+    public String getNextLine() {
+        return scanner.nextLine();
+    }
+
+    public void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
