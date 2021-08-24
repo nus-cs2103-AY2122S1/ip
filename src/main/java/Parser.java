8e7f91fb3df6cd3678938e64923120cb3a020bae
DesmondTo <e0550619@u.nus.edu>
2021-08-24 08:09:55 +0800

    Implement method for Parser class

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,2 +1,37 @@
 public class Parser {
+
+    private enum CommandType {
+        LIST, DONE, TODO, EVENT, DEADLINE, DELETE
+    }
+
+    public static Command parse(String fullCommand) {
+
+        String[] commandComponents = fullCommand.split(" ", 2);
+        String commandType = commandComponents[0].toUpperCase();
+        Command command;
+
+        switch (CommandType.valueOf(commandType)) {
+        case LIST:
+            command = new ListCommand();
+            break;
+        case DONE:
+            command = new DoneCommand();
+            break;
+        case DELETE:
+            command = new DeleteCommand();
+            break;
+        case TODO:
+            // Fallthrough
+        case EVENT:
+            // Fallthrough
+        case DEADLINE:
+            command = new AddCommand();
+            break;
+        default:
+            command = new UnknownCommand();
+            break;
+        }
+
+        return command;
+    }
 }
