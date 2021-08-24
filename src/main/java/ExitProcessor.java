3fb54d3aa4f1589a6f46170158b62590d7aa4c70
HongshuW <hongshu.wang.uni@gmail.com>
2021-08-24 23:41:20 +0800

    Add ExitProcessor.java

---
+++ src/main/java/ExitProcessor.java
@@ -0,0 +1,11 @@
+/**
+ * Represents a processor that stops the duke program.
+ */
+public class ExitProcessor extends Processor {
+    /**
+     * Constructor of the class `ExitProcessor`.
+     */
+    public ExitProcessor() {
+        super("bye");
+    }
+}
