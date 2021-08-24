bbbcb3fb06158d465b90f1e1daa14080b962d2dc
wonyeji <75670073+wonyeji@users.noreply.github.com>
2021-08-24 15:59:52 +0800

    Clean code from previous submission

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,16 +6,15 @@ public class Duke {
     private static DukeException dukeExp = new DukeException();
 
     public static void printList() {
-        System.out.println("Here are the tasks in your list:");
+        System.out.println("    Here are the tasks in your list:");
         for (int i = 0; i < list.size(); i++)
         {
-            System.out.println((i + 1) + ". " + list.get(i).toString());
+            System.out.println("    " + (i + 1) + ". " + list.get(i).toString());
         }
     }
 
     public static void printNoOfTasks() {
-        int count = list.size();
-        System.out.println("Now you have " + count + " tasks in the list.");
+        System.out.printf("    Now you have %d tasks in the list.", list.size());
     }
 
     public static void main(String[] args) {
@@ -28,19 +27,19 @@ public class Duke {
                 input = sc.nextLine();
                 continue;
             } else if (input.contains("done")) {
-                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("    Nice! I've marked this task as done: ");
                 int taskNo = Integer.parseInt(input.substring(5));
                 Task task = list.get(taskNo - 1);
                 task.markAsDone();
-                System.out.println(task);
+                System.out.println("      " + task);
                 input = sc.nextLine();
                 continue;
             } else if (input.contains("delete")) {
-                System.out.println("Noted. I've removed this task: ");
+                System.out.println("    Noted. I've removed this task: ");
                 int taskNo = Integer.parseInt(input.substring(7));
                 Task task = list.get(taskNo - 1);
                 list.remove(taskNo - 1);
-                System.out.println(task);
+                System.out.println("      " + task);
                 printNoOfTasks();
                 input = sc.nextLine();
                 continue;
@@ -50,33 +49,25 @@ public class Duke {
                 } else {
                     ToDo toDo = new ToDo(input.substring(5));
                     list.add(toDo);
-                    System.out.println("Got it. I've added this task:\n" + toDo.toString());
+                    System.out.println("    Got it. I've added this task:\n      " + toDo);
                     printNoOfTasks();
                 }
                 input = sc.nextLine();
                 continue;
             } else if (input.contains("deadline")) {
-//                if (input.length() < 10) {
-//                    System.out.println(dukeExp.deadline() todos
-//                } else {
-                    String[] split = input.split("/");
-                    Deadline deadline = new Deadline(split[0].substring(9), split[1].substring(3));
-                    list.add(deadline);
-                    System.out.println("Got it. I've added this task:\n" + deadline.toString());
-                    printNoOfTasks();
-//                }
+                String[] split = input.split("/");
+                Deadline deadline = new Deadline(split[0].substring(9), split[1].substring(3));
+                list.add(deadline);
+                System.out.println("    Got it. I've added this task:\n      " + deadline);
+                printNoOfTasks();
                 input = sc.nextLine();
                 continue;
             } else if (input.contains("event")) {
-//                if (input.length() < 7) {
-//                    System.out.println(dukeExp.event());
-//                } else {
-                    String[] split = input.split("/");
-                    Event event = new Event(split[0].substring(6), split[1].substring(3));
-                    list.add(event);
-                    System.out.println("Got it. I've added this task:\n" + event.toString());
-                    printNoOfTasks();
-//                }
+                String[] split = input.split("/");
+                Event event = new Event(split[0].substring(6), split[1].substring(3));
+                list.add(event);
+                System.out.println("    Got it. I've added this task:\n      " + event);
+                printNoOfTasks();
                 input = sc.nextLine();
                 continue;
             } else {
@@ -86,7 +77,7 @@ public class Duke {
             }
 
         }
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("    Bye. Hope to see you again soon!");
         System.exit(0);
     }
 }
