455c4775859c1883470201136981a9dc6cdb347f
Bryan Loh <lamofacade@gmail.com>
2021-08-24 22:20:39 +0800

    Refactor code to comply with CS2103T coding standard

--- src/test/java/duke/ResponseTest.java
+++ src/test/java/duke/ResponseTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ResponseTest {
+    private static String LINE = "    ____________________________________________________________";
+    private static String MESSAGE_INDENT = "     ";
+
     @Test
     public void toString_success() {
         assertEquals(
-                "    ____________________________________________________________" + System.lineSeparator()
-                + "     message" + System.lineSeparator()
-                + "    ____________________________________________________________"
+                ResponseTest.LINE + System.lineSeparator()
+                + ResponseTest.MESSAGE_INDENT + "message" + System.lineSeparator()
+                + ResponseTest.LINE
                 , new Response("message").toString());
     }
 }
