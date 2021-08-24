454f4e4fd2799d02993b189c5da0b6a3d2cd74e8
nniiggeell <e0559733@u.nus.edu>
2021-08-24 23:19:54 +0800

    Update Duke.java
    For A-MoreOOP.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,9 +9,11 @@ import java.util.Scanner;
  */
 
 public class Duke {
+
     private static final String FILE_PATH = "./data/duke.txt";
     private Storage storage;
-    private Response response;
+    private TaskList taskList;
+    private Ui ui;
     final String HORIZONTAL_LINE = "    ____________________________________________________________\n";
 
     public enum Command {
@@ -41,42 +43,50 @@ public class Duke {
 
     /**
      * Constructor for the chatbot.
+     *
      * @throws IOException
      */
     public Duke() throws IOException {
-        storage = new Storage(FILE_PATH);
+        this.ui = new Ui();
+        this.storage = new Storage(FILE_PATH);
         try {
-            ArrayList<Task> taskList = storage.loadFileContents(FILE_PATH);
-            response = new Response(taskList);
+            ArrayList<Task> loadList = storage.loadFileContents(FILE_PATH);
+            taskList = new TaskList(loadList);
         } catch (Exception e) {
-            ArrayList<Task> taskList = new ArrayList<>();
-            response = new Response(taskList);
+            ArrayList<Task> loadList = new ArrayList<>();
+            taskList = new TaskList(loadList);
         }
     }
 
+    /**
+     * Runs the bot.
+     *
+     * @throws IOException
+     */
     public void run() throws IOException {
         int position = 0;
-        response.greet();
+        ui.greet();
+        Parser parser = new Parser();
         Scanner sc = new Scanner(System.in);
-        String text = sc.nextLine();
 
+        String text = sc.nextLine();
         while (!text.equals("bye")) {
-            String[] splitWords = text.split(" ", 2);
-            String commandWord = splitWords[0];
+            String commandWord = parser.parseText(text);
+            String[] splitWords = parser.split(text);
             Command command = Command.fromString(commandWord);
             switch (command) {
             case LIST:
-                response.display();
+                ui.replyDisplay(taskList);
                 break;
             case DONE:
                 int pos = Integer.parseInt(splitWords[1]);
-                response.markDone(pos - 1);
-                storage.saveToFile(response.getLst());
+                taskList.markDone(pos - 1);
+                storage.saveToFile(taskList.getLst());
                 break;
             case TODO:
                 try {
-                    response.add(new ToDo(splitWords[1]));
-                    storage.saveToFile(response.getLst());
+                    taskList.add(new ToDo(splitWords[1]));
+                    storage.saveToFile(taskList.getLst());
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.err.println(HORIZONTAL_LINE +
                             "    :( OOPS! The description of a todo cannot be empty.\n" +
@@ -86,8 +96,8 @@ public class Duke {
             case DEADLINE:
                 try {
                     String[] splitBy = splitWords[1].split("/by ", 2);
-                    response.add(new Deadline(splitBy[0], splitBy[1]));
-                    storage.saveToFile(response.getLst());
+                    taskList.add(new Deadline(splitBy[0], splitBy[1]));
+                    storage.saveToFile(taskList.getLst());
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.err.println(HORIZONTAL_LINE +
                             "    :( OOPS! The description or a time of a deadline cannot be empty.\n" +
@@ -97,8 +107,8 @@ public class Duke {
             case EVENT:
                 try {
                     String[] splitAt = splitWords[1].split("/at ", 2);
-                    response.add(new Events(splitAt[0], splitAt[1]));
-                    storage.saveToFile(response.getLst());
+                    taskList.add(new Events(splitAt[0], splitAt[1]));
+                    storage.saveToFile(taskList.getLst());
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.err.println(HORIZONTAL_LINE +
                             "    :( OOPS! The description or a time of an event cannot be empty.\n" +
@@ -107,8 +117,8 @@ public class Duke {
                 break;
             case DELETE:
                 int pos2 = Integer.parseInt(splitWords[1]);
-                response.delete(pos2 - 1);
-                storage.saveToFile(response.getLst());
+                taskList.delete(pos2 - 1);
+                storage.saveToFile(taskList.getLst());
                 break;
             case OTHER:
                 System.out.println(HORIZONTAL_LINE +
@@ -118,7 +128,7 @@ public class Duke {
             }
             text = sc.nextLine();
         }
-        response.exit();
+        ui.exit();
     }
 
     public static void main(String[] args) throws Exception {
