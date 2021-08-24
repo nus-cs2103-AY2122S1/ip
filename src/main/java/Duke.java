0cab738446fa83a9d332a21cd9d3d1f42b5a37e8
Bryan Loh <lamofacade@gmail.com>
2021-08-24 19:04:52 +0800

    Add Ui class to deal with user interactions (#20)
    * Add Ui class to deal with user interactions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Queue;
 import java.util.LinkedList;
 
 public class Duke {
-    public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static String LOGO = " ____        _        \n"
                               + "|  _ \\ _   _| | _____ \n"
@@ -13,17 +9,18 @@ public class Duke {
                               + "| |_| | |_| |   <  __/\n"
                               + "|____/ \\__,_|_|\\_\\___|\n";
 
+    private static final Ui userInterface = new Ui();
     public static String TASK_COLLECTION_STORAGE_PATH = "./data/duke.txt";
     private static final TaskCollection tasks = new TaskCollection(TASK_COLLECTION_STORAGE_PATH);
     private static final Queue<Action> actions = new LinkedList<>();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Duke.actions.add(new WelcomeUser());
 
         while (true) {
             try {
                 if (Duke.actions.isEmpty()) {
-                    String input = Duke.getUserInput();
+                    String input = Duke.userInterface.getUserInput();
                     Request request = Request.create(Duke.tasks, input);
                     Duke.actions.add(request.action());
                 }
@@ -31,32 +28,14 @@ public class Duke {
                 Action action = Duke.actions.remove();
                 Response response = action.execute();
                 Duke.tasks.saveTasks();
-                Duke.printResponse(response);
+                Duke.userInterface.printResponse(response);
 
                 if (action instanceof GoodbyeUser) {
                     break;
                 }
             } catch (UserException exception) {
-                Duke.printResponse(exception.toResponse());
+                Duke.userInterface.printResponse(exception.toResponse());
             }
         }
-    }
-
-    /**
-     * Prints the Response to the console.
-     * @param response The Response to be printed.
-     */
-    public static void printResponse(Response response) {
-        System.out.println(response.toString());
-    }
-
-    /**
-     * Prompts the user for an input.
-     * @return The input string
-     * @throws IOException If an I/O error occurs
-     */
-    public static String getUserInput() throws IOException {
-        System.out.println();
-        return reader.readLine();
     }
 }
