8398f37c43f6ea288a869cad39699506566a91a5
Alissa <alissayarmantho1@gmail.com>
2021-08-25 02:25:27 +0800

    Add dependency for saving .json

---
+++ src/main/java/TaskStorage.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Storage {
+    
+}
