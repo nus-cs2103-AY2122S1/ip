728143d5ee85cbeb77de44b5b70054c0b647975a
DESKTOP-MM81EQR\Nathan <e0543978@u.nus.edu>
2021-08-24 17:34:03 +0800

    Add Level-8 Increments.
    Add dates and times.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,6 +7,11 @@ public class Task {
         this.isDone = false;
     }
 
+    public Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
 
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
@@ -17,6 +22,6 @@ public class Task {
     }
 
     public String toString() {
-        return "[" + this.getStatusIcon() + "] " + this.description;
+        return "|" + this.getStatusIcon() + "| " + this.description;
     }
 }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,11 +1,17 @@
+import java.time.LocalDate;
+
 public class Todo extends Task {
 
     public Todo(String description) {
         super(description);
     }
+    
+    public Todo(String description, boolean isDone) {
+        super(description, isDone);
+    }
 
     @Override
     public String toString() {
-        return "[T]" + super.toString();
+        return "T " + super.toString();
     }
 }
