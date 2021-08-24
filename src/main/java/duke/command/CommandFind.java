e79e90959c4d3f92d6cbe0bfef1dcf19b9bb98a6
DESKTOP-L6MVVEE\Sanguine <e0543860@u.nus.edu>
2021-08-24 21:44:30 +0800

    Add the find functionality

---
+++ src/main/java/duke/command/CommandFind.java
@@ -0,0 +1,47 @@
+package duke.command;
+
+import duke.DukeException;
+import duke.Storage;
+import duke.TaskList;
+import duke.Ui;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+
+
+public class CommandCheck extends Command {
+    public static final String KEYWORD = "check";
+    private ArrayList<String> arguments;
+
+
+    public CommandCheck(ArrayList<String> arguments) {
+        this.arguments = arguments;
+    }
+
+    @Override
+    public boolean isArgumentValid() {
+        try {
+            if (arguments.size() == 1) {
+                DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                LocalDate date = LocalDate.parse(arguments.get(0), dateFormatter);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void execute(TaskList tl, Storage st, Ui ui) {
+        if (isArgumentValid()) {
+            tl.printAllTasksOnDate(arguments.get(0));
+        } else {
+            throw new DukeException("Invalid argument for duke.command: check");
+        }
+    }
+
+}
