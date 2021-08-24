ed29a28efcec89e72b0a534ac03021cd1d39e28f
DesmondTo <e0550619@u.nus.edu>
2021-08-24 22:48:55 +0800

    Write Javadoc for EmptyDescriptionException class

--- src/main/java/duke/exception/EmptyDescriptionException.java
+++ src/main/java/duke/exception/EmptyDescriptionException.java
@@ -1,6 +1,13 @@
 package duke.exception;
 
-public class EmptyDescriptionException extends DukeException{
+/** An exception that handles command with empty description. */
+public class EmptyDescriptionException extends DukeException {
+
+    /**
+     * A constructor for class EmptyDescriptionException.
+     *
+     * @param messgae The message to be displayed when this exception is caught.
+     */
     public EmptyDescriptionException(String messgae) {
         super(messgae);
     }
