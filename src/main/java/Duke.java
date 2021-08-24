5b1445ebbc5ea480108d5391e985a6b7507061e1
eeliana <pjne2301@gmail.com>
2021-08-24 17:07:25 +0800

    Add nested class Parser to interpret user commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -10,6 +11,156 @@ import java.util.stream.Stream;
  * Class encapsulating a Duke and its commands.
  */
 public class Duke {
+
+    private enum UserCommands {
+        DONE, TODO, DEADLINE, EVENT, GET, DELETE, LIST, BYE;
+    }
+
+    private class Parser {
+        private String[] list_of_words = new String[0];
+        private String userInput ="";
+        private DateTimeManager manager = new DateTimeManager(
+                DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        private int taskIndex = -1;
+        private LocalDate date;
+
+        private UserCommands parseString(String userInput) throws DukeException {
+            this.userInput = userInput;
+
+            if (userInput.equals("bye")) {
+                return UserCommands.BYE;
+            } else if (userInput.equals("list")) {
+                return UserCommands.LIST;
+            }
+
+            // Separate them with space
+            String[] arrOfCommandWords = userInput.split(" ");
+            if (arrOfCommandWords.length <= 1) {
+                handleInvalidInputs(userInput);
+            }
+
+            this.list_of_words = arrOfCommandWords;
+            // Check the command word
+            String commandWord = arrOfCommandWords[0];
+            switch (commandWord) {
+            case "todo":
+                return UserCommands.TODO;
+            case "deadline":
+                parseDescription(userInput, "/by ");
+                return UserCommands.DEADLINE;
+            case "event":
+                parseDescription(userInput, "/at ");
+                return UserCommands.EVENT;
+            case "done":
+                try {
+                    int index = Integer.parseInt(arrOfCommandWords[1]) - 1;
+                    this.taskIndex = index;
+                } catch (NumberFormatException e) {
+                    throw new DukeException("Invalid task number");
+                }
+                return UserCommands.DONE;
+            case "get":
+                try {
+                    manager.parseDateTime(arrOfCommandWords[1]);
+                    return UserCommands.GET;
+                } catch (DateTimeParseException e) {
+                    throw new DukeException("Invalid date format.");
+                }
+            case "delete":
+                try {
+                    int index = Integer.parseInt(arrOfCommandWords[1]) - 1;
+                    this.taskIndex = index;
+                } catch (NumberFormatException e) {
+                    throw new DukeException("Invalid task number");
+                }
+                return UserCommands.DELETE;
+            default:
+                throw new DukeException("Sorry, I don't know what that means.");
+            }
+        }
+
+        private void parseDescription(String userInput, String command) {
+            try {
+                int indexOfDate = userInput.indexOf(command);
+                int startOfDescription = userInput.indexOf(' ');
+                if (indexOfDate < 0) {
+                    throw new DukeException("No date specified for task.");
+                }
+                String description = userInput.substring(startOfDescription, indexOfDate);
+                list_of_words[1] = description;
+                LocalDate date = manager.parseDateTime(userInput.substring(indexOfDate + command.length()));
+                this.date = date;
+            } catch (DateTimeParseException | DukeException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        /**
+         * Method for Duke to handle invalid inputs by the user.
+         *
+         * @param input The user input to Duke.
+         */
+        private void handleInvalidInputs(String input) {
+            try {
+                switch (input) {
+                case "todo": // fallthrough
+                case "deadline": // fallthrough
+                case "event": {
+                    throw new DukeException(
+                            String.format(
+                                    "☹ OOPS!!! The description of a %s cannot be empty.",
+                                    input
+                            )
+                    );
+                }
+                case "done": // fallthrough
+                case "delete":
+                    throw new DukeException("Please enter the task index.");
+                case "get":
+                    throw new DukeException("Please enter a date in dd/MM/yyyy format.");
+                default:
+                    throw new DukeException(
+                            "☹ OOPS!!! I'm sorry, but I don't know what that means :-("
+                    );
+                }
+            } catch (DukeException e) {
+                System.out.println(String.format("%4s%s",
+                        " ", e.getMessage()));
+            }
+        }
+
+
+        private void executeTasks(UserCommands type) {
+            switch (type) {
+            case BYE:
+                exit();
+                break;
+            case LIST:
+                returnTaskList();
+                break;
+            case GET:
+                returnTasksOnDate(list_of_words[1]);
+                break;
+            case TODO:
+                updateTasks(new ToDo(list_of_words[1]));
+                break;
+            case DEADLINE:
+                updateTasks(new Deadline(list_of_words[1], this.date));
+                break;
+            case EVENT:
+                updateTasks(new Event(list_of_words[1], this.date));
+                break;
+            case DELETE:
+                deleteTask(this.taskIndex);
+                break;
+            case DONE:
+                markTaskAsCompleted(this.taskIndex);
+                break;
+            }
+        }
+
+    }
+
     /**
      * Field for duke to keep track of task list.
      */
@@ -17,6 +168,7 @@ public class Duke {
     private Storage storage;
     private HashMap<LocalDate, ArrayList<Task>> dateTasks = new HashMap<>();
     private Ui ui;
+    private Parser parser = new Parser();
 
     /**
      * Constructor for Duke
@@ -74,133 +226,45 @@ public class Duke {
         System.out.println(dateTasks.getOrDefault(date, new ArrayList<>()));
     }
 
-    /**
-     * Method for Duke to handle invalid inputs by the user.
-     *
-     * @param input The user input to Duke.
-     */
-    private void handleInvalidInputs(String input) {
-        try {
-            switch (input) {
-                case "todo": // fallthrough
-                case "deadline": // fallthrough
-                case "event": {
-                    System.out.println(
 
-                    );
-                    throw new DukeException(
-                            String.format(
-                                    "☹ OOPS!!! The description of a %s cannot be empty.",
-                                    input
-                            )
-                    );
-                }
-                default:
-                    throw new DukeException(
-                            "☹ OOPS!!! I'm sorry, but I don't know what that means :-("
-                    );
-            }
-        } catch (DukeException e) {
-            divider();
-            System.out.println(String.format("%4s%s",
-                    " ", e.getMessage()));
-            divider();
-        }
+    private void updateTasks(Task task) {
+        this.taskList = this.taskList.add(task);
+        divider();
+        ui.outputMessage(Commands.ADD);
+        System.out.println(
+                String.format("%5s%s\n%4s%s", " ", task,
+                        " ", this.taskList.status())
+        );
+        divider();
+        storage.addTaskToFile(task);
     }
 
-    /**
-     * Method for Duke to update the task list according to the given tasks.
-     *
-     * @param command The command that specifies the type of task and its description.
-     */
-    private void updateTaskList(String command) {
-        // The type of the task indicated before the first space.
-        try {
-            int indexOfFirstSpace = command.indexOf(" ");
-
-            // Only got one word or no description entered.
-            if (indexOfFirstSpace == -1) {
-                handleInvalidInputs(command);
-                return;
-            }
-            String taskType = command.substring(0, indexOfFirstSpace);
-            String description = command.substring(indexOfFirstSpace + 1);
-
-            String customPattern ="dd/MM/yyyy";
-            DateTimeFormatter customFormatter = DateTimeFormatter.ofPattern(customPattern);
-            DateTimeManager dateTimeManager = new DateTimeManager(customFormatter);
-
-            Task newTask;
-            String dateTime;
-            LocalDate date;
-
-            switch (taskType) {
-                case "todo":
-                    newTask = new ToDo(description);
-                    break;
-                case "deadline":
-                    int deadlineIndex = description.indexOf("/by");
-                    dateTime = description.substring(deadlineIndex + 4);
-                    date = dateTimeManager.parseDateTime(dateTime);
-                    newTask = new Deadline(description.substring(0, deadlineIndex),
-                            date);
-                    // Keep track of tasks for a date
-                    dateTimeManager.updateDateTasks(dateTasks, date, newTask);
-                    break;
-                case "event":
-                    int timeIndex = description.indexOf("/at");
-                    dateTime = description.substring(timeIndex + 4);
-                    date = dateTimeManager.parseDateTime(dateTime);
-                    newTask = new Event(description.substring(0, timeIndex),
-                            date);
-                    // Keep track of tasks for a date
-                    dateTimeManager.updateDateTasks(dateTasks, date, newTask);
-                    break;
-                default:
-                    throw new DukeException(
-                            "☹ OOPS!!! I'm sorry, but I don't know what that means :-("
-                    );
-
-            }
-
-            divider();
-            this.taskList = this.taskList.add(newTask);
-            ui.outputMessage(Commands.ADD);
-            System.out.println(
-                    String.format("%5s%s\n%4s%s", " ", newTask,
-                            " ", this.taskList.status())
-            );
-            divider();
-            storage.addTaskToFile(newTask);
-        } catch (DukeException e) {
-            divider();
-            System.out.println(String.format("%4s%s",
-                    " ", e.getMessage()));
-            divider();
-        }
-    }
 
     /**
      * Method for Duke to mark the respective tasks as completed.
      *
      * @param index Index of the task to be deleted.
      */
-    private void markTaskAsCompleted(int index) throws DukeException {
-        boolean isValid = this.taskList.isValidTaskIndex(index);
-        divider();
-        if (isValid) {
-            String toUpdate = this.taskList.getTask(index).toString();
-            Task task = this.taskList.markTaskAsCompleted(index);
-            ui.outputMessage(Commands.DONE);
-            System.out.println(
-                    String.format("%6s%s\n%4s%s", " ", task,
-                            " ", this.taskList.status())
-            );
-            storage.markTaskAsCompleted(task.toString(), toUpdate);
-        } else {
-            throw new DukeException("There is no such task.");
+    private void markTaskAsCompleted(int index) {
+        try {
+            boolean isValid = this.taskList.isValidTaskIndex(index);
+            divider();
+            if (isValid) {
+                String toUpdate = this.taskList.getTask(index).toString();
+                Task task = this.taskList.markTaskAsCompleted(index);
+                ui.outputMessage(Commands.DONE);
+                System.out.println(
+                        String.format("%6s%s\n%4s%s", " ", task,
+                                " ", this.taskList.status())
+                );
+                storage.markTaskAsCompleted(task.toString(), toUpdate);
+            } else {
+                throw new DukeException("There is no such task.");
+            }
+            divider();
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
         }
-        divider();
     }
 
     /**
@@ -208,22 +272,26 @@ public class Duke {
      *
      * @param index Index of the task to be deleted.
      */
-    private void deleteTask(int index) throws DukeException {
-        boolean isValid = taskList.isValidTaskIndex(index);
-        divider();
-        if (isValid) {
-            Task task = taskList.getTask(index);
-            this.taskList = this.taskList.deleteTask(index);
-            ui.outputMessage(Commands.DELETE);
-            System.out.println(
-                    String.format("%6s%s\n%4s%s", " ", task,
-                            " ", taskList.status())
-            );
-            storage.deleteTaskFromFile(this.taskList);
-        } else {
-            throw new DukeException("There is no such task.");
+    private void deleteTask(int index) {
+        try {
+            boolean isValid = taskList.isValidTaskIndex(index);
+            divider();
+            if (isValid) {
+                Task task = taskList.getTask(index);
+                this.taskList = this.taskList.deleteTask(index);
+                ui.outputMessage(Commands.DELETE);
+                System.out.println(
+                        String.format("%6s%s\n%4s%s", " ", task,
+                                " ", taskList.status())
+                );
+                storage.deleteTaskFromFile(this.taskList);
+            } else {
+                throw new DukeException("There is no such task.");
+            }
+            divider();
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
         }
-        divider();
     }
 
 
@@ -242,45 +310,16 @@ public class Duke {
 
         while (true) {
             String command = sc.nextLine().strip();
-            if (command.equals("bye")) {
-                exit();
-                break;
-            } else if (command.equals("list")) {
-                returnTaskList();
-            } else if (command.startsWith("get")) {
-                String[] arrOfCommandWords = command.split(" ", 2);
-                if (arrOfCommandWords.length <= 1) {
-                    handleInvalidInputs(command);
-                }
-                returnTasksOnDate(arrOfCommandWords[1]);
-            } else if (command.startsWith("done")
-                    || command.startsWith("delete")) {
-                try {
-                    String[] arrOfCommandWords = command.split(" ", 2);
-                    if (arrOfCommandWords.length <= 1) {
-                        // No task specified.
-                        throw new DukeException("Please enter the task index.");
-                    }
-                    int taskIndex = Integer.parseInt(arrOfCommandWords[1]) - 1;
-                    if (command.startsWith("done")) {
-                        markTaskAsCompleted(taskIndex);
-                    } else {
-                        deleteTask(taskIndex);
-                    }
-                } catch (NumberFormatException|DukeException e){
-                    divider();
-                    System.out.println(String.format("%4s%s",
-                            " ", e.getMessage()));
-                    divider();
-                } finally {
-                    // TODO: implement cleanup
-                }
-            } else {
-                // Add the task to the task list
-                updateTaskList(command);
+            try {
+                UserCommands type = parser.parseString(command);
+                parser.executeTasks(type);
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+            } finally {
+
             }
         }
-        sc.close();
+        // sc.close();
     }
 
     /**
@@ -292,7 +331,6 @@ public class Duke {
         String directoryPath = "./data";
         String filePath = "./data/duke.text";
         File directory = new File(directoryPath);
-        System.out.println(directory.getAbsolutePath());
         // Check folder exists
         if (!directory.exists()) {
             boolean created = directory.mkdirs();
