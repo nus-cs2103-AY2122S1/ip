9c710507bac418ea6c52da527eb90160302e230c
sivayogasubramanian <sivayogasubramanian@gmail.com>
2021-08-24 19:28:14 +0800

    Store deadline as LocaoDateTime object

--- src/main/java/jarvis/action/DeadlineAction.java
+++ src/main/java/jarvis/action/DeadlineAction.java
@@ -1,15 +1,21 @@
 package jarvis.action;
 
+import jarvis.exception.InvalidDateTimeInputException;
 import jarvis.exception.TaskDetailsEmptyException;
 import jarvis.output.Output;
 import jarvis.task.Deadline;
 import jarvis.task.TaskList;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class DeadlineAction extends Action {
     private String taskDescription;
-    private String deadline;
+    private LocalDateTime deadline;
+    private static final String INPUT_FORMAT =  "dd-MM-yyyy HHmm";
 
-    public DeadlineAction(String userInputWithoutActionTrigger) throws TaskDetailsEmptyException {
+    public DeadlineAction(String userInputWithoutActionTrigger) throws TaskDetailsEmptyException, InvalidDateTimeInputException {
         String[] splitStrings = userInputWithoutActionTrigger.split("/by", 2);
         this.taskDescription = splitStrings[0].trim();
         if (taskDescription.equals("")) {
@@ -18,7 +24,14 @@ public class DeadlineAction extends Action {
         if (splitStrings.length < 2) {
             throw new TaskDetailsEmptyException("deadline");
         }
-        this.deadline = splitStrings[1].trim();
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(INPUT_FORMAT);
+
+        try {
+            this.deadline = LocalDateTime.parse(splitStrings[1].trim(), dateTimeFormatter);
+        } catch (DateTimeParseException e) {
+            throw new InvalidDateTimeInputException("deadline", INPUT_FORMAT);
+        }
     }
 
     @Override
--- src/main/java/jarvis/task/Deadline.java
+++ src/main/java/jarvis/task/Deadline.java
@@ -1,15 +1,21 @@
 package jarvis.task;
 
-public class Deadline extends Task {
-    private String deadline;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String deadline) {
+public class Deadline extends Task {
+    private static final String OUTPUT_FORMAT = "MMM d yyyy HHmm";
+    private LocalDateTime deadline;
+
+    public Deadline(String description, LocalDateTime deadline) {
         super(description);
         this.deadline = deadline;
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), this.deadline);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(OUTPUT_FORMAT);
+        String formattedDeadline = this.deadline.format(dateTimeFormatter);
+        return String.format("[D]%s (by: %s)", super.toString(), formattedDeadline);
     }
 }
