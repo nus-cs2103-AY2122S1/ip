4c0ea3bc7f3a7a0112ecf69ef9fb07e208c52a14
DesmondTo <e0550619@u.nus.edu>
2021-08-24 23:00:02 +0800

    Write Javadoc for Event class

--- src/main/java/duke/task/Event.java
+++ src/main/java/duke/task/Event.java
@@ -3,20 +3,37 @@ package duke.task;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/** A task class which falls under deadline category. */
 public class Event extends Task {
 
     protected LocalDate at;
 
+    /**
+     * A constructor for class Deadline.
+     *
+     * @param description The task description.
+     * @param at The date of the task being carried out.
+     */
     public Event(String description, LocalDate at) {
         super(description);
         this.at = at;
     }
 
+    /**
+     * Return a string representation in the format to be written in tasks.txt file.
+     *
+     * @return The string representation in the format to be written in tasks.txt file.
+     */
     @Override
     public String toDataString() {
         return String.format("EVENT %s | %s", super.toDataString(), this.at);
     }
 
+    /**
+     * Return a string representation of this task.
+     *
+     * @return The string representation of this task.
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (at: " + at.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
