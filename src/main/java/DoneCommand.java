8a5a3188fe9685e550678d63ef4fe46756343b9a
DesmondTo <e0550619@u.nus.edu>
2021-08-24 17:52:57 +0800

    Complete DoneCommand class implementation

--- src/main/java/DoneCommand.java
+++ src/main/java/DoneCommand.java
@@ -1,7 +1,18 @@
 public class DoneCommand extends Command{
+
+    private final int taskNum;
+
+    public DoneCommand(int taskNum) {
+        this.taskNum = taskNum;
+    }
+
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-
+        Task task = tasks.getTasks().get(this.taskNum - 1);
+        task.markAsDone();
+        storage.save(tasks);
+        System.out.println("\tNice! I've marked this task as done:");
+        System.out.println("\t " + task);
     }
 
     @Override
