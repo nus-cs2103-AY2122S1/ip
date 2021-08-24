a4c10a64905e3d3afc76cb315be5ab7afe5addfa
DesmondTo <e0550619@u.nus.edu>
2021-08-24 23:16:06 +0800

    Write Javadoc for Ui class

--- src/main/java/duke/util/Ui.java
+++ src/main/java/duke/util/Ui.java
@@ -2,6 +2,7 @@ package duke.util;
 
 import java.util.Scanner;
 
+/** A class that deals with interactions with the user. */
 public class Ui {
     protected static final String logo = "\t ____        _        \n"
             + "\t|  _ \\ _   _| | _____ \n"
@@ -13,19 +14,31 @@ public class Ui {
 
     private Scanner scanner;
 
+    /**
+     * A constructor for the class Ui.
+     */
     public Ui() {
         this.scanner = new Scanner(System.in);
     }
 
+    /**
+     * Print the string divider to the screen.
+     */
     public void showLine() {
         System.out.println("\t" + Ui.divider);
     }
 
+    /**
+     * Print loading error to the screen.
+     */
     public void showLoadingError() {
         System.out.println("\tNo record found.");
         System.out.println("\tInitializing...");
     }
 
+    /**
+     * Print welcome message to greet the user.
+     */
     public void showWelcome() {
         this.showLine();
         System.out.println("\tWelcome to");
@@ -34,6 +47,11 @@ public class Ui {
         this.showLine();
     }
 
+    /**
+     * Return the string representation of the user command.
+     *
+     * @return The string representation of the user command.
+     */
     public String readCommand() {
         String nextLine = this.scanner.nextLine();
         while (nextLine.equals("")) {
@@ -42,6 +60,11 @@ public class Ui {
         return nextLine;
     }
 
+    /**
+     * Print the error message to the screen.
+     *
+     * @param errorMessage The error message based on the exception caught/.
+     */
     public void showError(String errorMessage) {
         System.out.println("\tSomething went wrong: ");
         System.out.println("\t" + errorMessage);
