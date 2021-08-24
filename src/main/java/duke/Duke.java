e79a03dae613feb152185a42c40895b6968a0dc7
eeliana <pjne2301@gmail.com>
2021-08-24 19:50:40 +0800

    Update spacing

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -54,7 +54,6 @@ class Duke {
             switch (commandWord) {
             case "todo":
                 return UserCommands.TODO;
-
             case "deadline":
                 parseDescription(userInput, "/by ");
                 return UserCommands.DEADLINE;
