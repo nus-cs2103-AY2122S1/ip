5c21328742fccb33243d1f91e82e61a3a98be448
DesmondTo <e0550619@u.nus.edu>
2021-08-24 08:09:17 +0800

    Add UnknownCommand class

---
+++ src/main/java/UnknownCommand.java
@@ -0,0 +1,12 @@
+public class UnknownCommand extends Command{
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
