30024b80f0b6af1dd3067a1ed38aef9fdbaad872
DesmondTo <e0550619@u.nus.edu>
2021-08-24 17:53:49 +0800

    Complete DeleteCommand class implementation

--- src/main/java/DeleteCommand.java
+++ src/main/java/DeleteCommand.java
@@ -1,7 +1,19 @@
 public class DeleteCommand extends Command{
+
+    private final int taskNum;
+
+    public DeleteCommand(int taskNum) {
+        this.taskNum = taskNum;
+    }
+
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
-
+        Task task = tasks.getTasks().get(this.taskNum - 1);
+        tasks.delete(this.taskNum - 1);
+        storage.save(tasks);
+        System.out.println("\tNoted. I've %s this task:");
+        System.out.println("\t " + task);
+        System.out.println("\tNow you have " + tasks.getTaskNum() + " tasks in the list.");
     }
 
     @Override
