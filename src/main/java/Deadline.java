0ce2d20c49570a3e81bc9ab4cf7fc2fc7c2d49aa
jaredlhf <jlhfqaz@gmail.com>
2021-08-24 15:14:15 +0800

    add Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,29 @@
-public class Deadline extends Task {
-    protected String by;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task {
+    protected LocalDate date;
+    protected LocalTime time;
+
+    public Deadline(String description, LocalDate date) {
         super(description);
-        this.by = by;
+        this.date = date;
+    }
+
+    public Deadline(String description, LocalDate date, LocalTime time) {
+        super(description);
+        this.date = date;
+        this.time = time;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by:" + by + ")";
+        String res = "[D]" + super.toString() + " (by: "
+                + date.format(DateTimeFormatter.ofPattern("d MMM yyyy"));
+        if (time != null) {
+            res += " " + time.format(DateTimeFormatter.ofPattern("H:mm"));
+        }
+        return res += ")";
     }
 }
--- src/main/java/Jared.java
+++ src/main/java/Jared.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,7 +10,11 @@ public class Jared {
     private static void add(String command, String next) throws DukeException {
         Task newTask;
         String desc;
-        String date;
+        String dateStr;
+        String[] dateTime;
+        LocalDate date;
+        LocalTime time;
+
         if (command.equals("todo")) {
             try {
                 desc = next.split(" ", 2)[1];
@@ -18,21 +25,47 @@ public class Jared {
         } else if (command.equals("deadline")) {
             try {
                 String body = next.split(" ", 2)[1];
-                desc = body.split("/by", 2)[0];
-                date = body.split("/by", 2)[1];
+                desc = body.split("/by", 2)[0].trim();
+                dateStr = body.split("/by", 2)[1].trim();
+                dateTime = dateStr.split(" ");
+                String d = dateTime[0];
+                date = LocalDate.parse(d);
+
+                if (dateTime.length > 1) {
+                    String t = dateTime[1];
+                    time = LocalTime.parse(t);
+                    newTask = new Deadline(desc, date, time);
+                } else {
+                    newTask = new Deadline(desc, date);
+                }
             } catch (ArrayIndexOutOfBoundsException e) {
                 throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+            } catch (DateTimeParseException e) {
+                System.out.println(e.getMessage());
+                throw new DukeException("Invalid date. Please enter the date (yyyy-mm-dd)");
             }
-            newTask = new Deadline(desc, date);
         } else if (command.equals("event")) {
             try {
                 String body = next.split(" ", 2)[1];
-                desc = body.split("/at", 2)[0];
-                date = body.split("/at", 2)[1];
+                desc = body.split("/at", 2)[0].trim();
+                dateStr = body.split("/at", 2)[1].trim();
+                dateTime = dateStr.split(" ");
+                String d = dateTime[0];
+                date = LocalDate.parse(d);
+
+                if (dateTime.length > 1) {
+                    String t = dateTime[1];
+                    time = LocalTime.parse(t);
+                    newTask = new Event(desc, date, time);
+                } else {
+                    newTask = new Event(desc, date);
+                }
+
             } catch (ArrayIndexOutOfBoundsException e) {
                 throw new DukeException("OOPS!!! The description of a event cannot be empty.");
+            } catch (DateTimeParseException e) {
+                throw new DukeException("Invalid date and time. Please enter the date (yyyy-mm-dd)");
             }
-            newTask = new Event(desc, date);
         } else {
             throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
