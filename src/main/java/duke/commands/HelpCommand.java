33cda8c3deb20e17f62e3d24054031b560a09cf7
Xiaoyunnn <xiaoyun.wu00@gmail.com>
2021-08-24 19:40:47 +0800

    Add find command to help list

--- src/main/java/duke/commands/HelpCommand.java
+++ src/main/java/duke/commands/HelpCommand.java
@@ -32,6 +32,7 @@ public class HelpCommand extends Command {
                 EventCommand.MESSAGE_USAGE + "\n" +
                 DoneCommand.MESSAGE_USAGE + "\n" +
                 DeleteCommand.MESSAGE_USAGE + "\n" +
+                FindCommand.MESSAGE_USAGE + "\n" +
                 ListCommand.MESSAGE_USAGE + "\n" +
                 HelpCommand.MESSAGE_USAGE + "\n" +
                 ExitCommand.MESSAGE_USAGE);
