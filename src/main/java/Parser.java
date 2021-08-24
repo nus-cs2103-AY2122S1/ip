fd8e98032ae39d423ad6e3282839e6b363474604
HongshuW <hongshu.wang.uni@gmail.com>
2021-08-24 23:37:57 +0800

    Add Parser.java

---
+++ src/main/java/Parser.java
@@ -0,0 +1,11 @@
+import java.time.format.DateTimeParseException;
+
+/**
+ * Represents a parser that deals with making sense of the user command.
+ */
+public class Parser {
+    /**
+     * Constructor of the class `Parser`.
+     */
+    public Parser() {}
+}
