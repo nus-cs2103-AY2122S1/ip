420b0301112314cfc9a60ec486591a6209f24abb
eeliana <pjne2301@gmail.com>
2021-08-24 20:07:53 +0800

    Fix syntax errors.

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -1,8 +1,6 @@
 package duke;
 
 class DukeException extends Exception {
-
-public class DukeException extends Exception {
     protected DukeException(String message) {
         super(message);
     }
