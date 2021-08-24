95e4af4aeb452248db2f6998abd116fff2d531ed
botr99 <brandonong999@gmail.com>
2021-08-24 19:28:54 +0800

    Fix issue of ui starting though tasks are not initialized

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,26 @@
 import java.io.IOException;
 
 public class Duke {
+    private static final String STORAGE_LOCATION = "data/duke.txt";
     private TaskList tasks;
-    private Ui ui;
 
-    public Duke(String fileLocation) {
-        try {
-            Storage storage = new Storage(fileLocation);
-            tasks = new TaskList(storage);
-        } catch (IOException e) {
-            System.out.println("Something went wrong with accessing " + fileLocation);
-        } catch (FileParseException e) {
-            System.out.println(fileLocation + " is not of the proper format. "
-                    + "Either modify it to the right format or delete the file.");
-        }
-        ui = new Ui(tasks);
+    public Duke(String fileLocation) throws IOException, FileParseException {
+        Storage storage = new Storage(fileLocation);
+        tasks = new TaskList(storage);
     }
 
     public void run() {
-        ui.start();
+        new Ui(tasks).start();
     }
 
     public static void main(String[] args) {
-        new Duke("data/duke.txt").run();
+        try {
+            new Duke(STORAGE_LOCATION).run();
+        } catch (IOException e) {
+            System.out.println("Something went wrong with accessing " + STORAGE_LOCATION);
+        } catch (FileParseException e) {
+            System.out.println(STORAGE_LOCATION + " is not of the proper format. "
+                    + "Either modify it to the right format or delete the file.");
+        }
     }
 }
