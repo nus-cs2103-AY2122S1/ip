ddb78e60ff8fcf9fed8b6f74bb6445de07564974
Moley456 <kingstarwei10@gmail.com>
2021-08-24 22:43:37 +0800

    Implement Duke Level 8 - Dates

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,29 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
-    private String by;
+    private LocalDate by;
 
     public Deadline (String taskDetails, String by) {
         super(taskDetails);
-        this.by = by;
+        this.by = LocalDate.parse(by);
     }
 
+<<<<<<< Updated upstream
+=======
+    public String taskType() {
+        return "D";
+    }
+
+
+    @Override
+    public String getTaskDetails() {
+        return super.getTaskDetails() + " | " + by.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
+
+>>>>>>> Stashed changes
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by:" + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
