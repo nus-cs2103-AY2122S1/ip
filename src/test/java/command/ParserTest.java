720d4dede4cb965ceb59849e08dfb82f4fc6fad3
IrvingHe000 <heoutong@gmail.com>
2021-08-24 16:25:28 +0800

    Add JUnit test on Parser and TaskList

---
+++ src/test/java/command/ParserTest.java
@@ -0,0 +1,31 @@
+package command;
+
+import duke.command.InputTypes;
+import duke.command.Parser;
+import duke.task.Deadline;
+import duke.task.Task;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+    @Test
+    public void judgeTypeTest(){
+        String invalidInput = "This is not a valid input";
+        String validInput1 = "deadline go to school /by 2021-08-10 00:00";
+        String validInput2 = "todo Finish ip";
+        assertEquals(InputTypes.UNKNOWN, Parser.judgeType(invalidInput));
+        assertEquals(InputTypes.DEADLINE, Parser.judgeType(validInput1));
+        assertEquals(InputTypes.TODO, Parser.judgeType(validInput2));
+    }
+
+    @Test
+    public void isValidDeadlineTaskTest() throws Exception{
+        String input1 = "deadline task1 /by 2020-12-08 09:00";
+        String input2 = "deadline task2 /by 2010-12-08 20:00";
+        Task expectedTask1 = new Deadline("task1", "2020-12-08 09:00");
+        Task expectedTask2 = new Deadline("task2", "2010-12-08 20:00");
+        assertEquals(expectedTask1.toString(), Parser.isValidDeadlineTask(input1).toString());
+        assertEquals(expectedTask2.toString(), Parser.isValidDeadlineTask(input2).toString());
+    }
+}
---
+++ src/test/java/command/TaskListTest.java
@@ -0,0 +1,43 @@
+package command;
+
+import duke.command.TaskList;
+import duke.task.Task;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+    private static TaskList testingList = new TaskList();
+    private static Task task1 = new Task("task1");
+    private static Task task2 = new Task("task2");
+    private static Task task3 = new Task("task3");
+
+    @Test
+    public void sizeTest() {
+        testingList = new TaskList();
+        assertEquals(0, testingList.size());
+        testingList.add(task1);
+        testingList.add(task2);
+        assertEquals(2, testingList.size());
+        testingList.add(task3);
+        assertEquals(3, testingList.size());
+    }
+
+    @Test
+    public void getTest() {
+        testingList = new TaskList();
+        testingList.add(task1);
+        testingList.add(task2);
+        assertEquals(task1, testingList.get(0));
+        assertEquals(task2, testingList.get(1));
+    }
+
+    @Test
+    public void removeTest() {
+        testingList = new TaskList();
+        testingList.add(task1);
+        testingList.add(task2);
+        assertEquals(task2, testingList.remove(1));
+        assertEquals(task1, testingList.remove(0));
+    }
+}
