a8a9101dc70b417e4fffc6071c510198f177aadb
Tianqi-Zhu <tianqi_zhu2002@qq.com>
2021-08-24 23:41:54 +0800

    add javadoc

--- src/main/java/duke/FindCommand.java
+++ src/main/java/duke/FindCommand.java
@@ -1,12 +1,22 @@
 package duke;
 
+/**
+ * Command to find tasks with a keyword.
+ * 
+ * @author Tianqi-Zhu
+ */
 public class FindCommand implements Executable {
     private String keyword;
 
     public FindCommand(String keyword) {
         this.keyword = keyword;
     }
-
+    
+    /**
+     * Find the print the task when executed.
+     * 
+     * @param taskList current taskList
+     */
     public void execute(TaskList taskList) {
         taskList.find(keyword);
     }
