8c3f6acedd9bd0a25a88cdf073da4526900ca5a8
houtenteo <houtenteo@gmail.com>
2021-08-24 20:10:21 +0800

    Change to interface

--- src/main/java/duke/tasktype/Task.java
+++ src/main/java/duke/tasktype/Task.java
@@ -1,65 +1,24 @@
 package duke.tasktype;
 
-/**
- * The general task class
- *
- * @Author Houten Teo
- * @version CS2103T week 2
- */
+public interface Task {
 
-public class Task {
-
-    private String taskName;
-    private boolean isDone;
-
-    /**
-     * Constructor for the duke.tasks.Task class
-     * @param taskName Name of the task.
-     * @param isDone True if the task is completed and false otherwise.
-     */
-    public Task(String taskName, boolean isDone) {
-        this.taskName = taskName;
-        this.isDone = isDone;
-    }
-
-    /**
-     * Method to mark the task as complete.
-     * Additionally, prints out the taskName to allow user to validate.
-     */
-    public void markComplete() {
-        if (isDone) {
-            System.out.println("`" + taskName + "`" + " is already completed.");
-        } else {
-            this.isDone = true;
-            System.out.println(
-                    "Finally! Took you long enough to complete:" + taskName
-            );
-        }
-    }
+    public void markComplete();
 
     /**
      * Method to return the corresponding status icon depending on
      * whether the task has been completed or not.
      * @return The String representation of the icon.
      */
-    public String getStatusIcon() {
-        if (isDone) {
-            return "[X]";
-        } else {
-            return "[ ]";
-        }
-    }
+    public String getStatusIcon();
+
+    public String getDescription();
 
     /**
      * Method to return the corresponding type icon depending on
      * the type of the task.
      * @return
      */
-    public String getTypeIcon() {
-        return "[ ]";
-    }
+    public String getTypeIcon();
 
-    public String createData() {
-        return "";
-    }
+    public String createData();
 }
