71f435afbf5f050f3794b15d2adff8bf2ff12982
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-08-24 22:53:18 +0800

    Update SaveCommand execute() method

--- src/main/java/SaveCommand.java
+++ src/main/java/SaveCommand.java
@@ -1,7 +1,14 @@
+import java.io.IOException;
+
 public class SaveCommand extends Command {
 
     @Override
     public void execute(TaskList taskList, Ui ui, Storage storage) throws DukeException {
-
+        try {
+            storage.writeCurrentData(taskList.getTasks());
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+        ui.displaySaveMessage();
     }
 }
