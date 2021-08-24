91caf16042f309cc9122ceb33b279bafdf7db2c2
jennibearduit <77873928+jennibearduit@users.noreply.github.com>
2021-08-24 22:48:21 +0800

    Fix indentation and add JavaDoc

--- src/main/java/commands/FindCommand.java
+++ src/main/java/commands/FindCommand.java
@@ -1,21 +1,35 @@
 package commands;
 import tasks.TaskList;
 
+/**
+ * This is an FindCommand Class, which inherits from Command.
+ * The execution of this command will return a list of tasks
+ * which contains the keyword.
+ */
 public class FindCommand extends Command {
-        public static final String KEYWORD = "find";
-        private final String keyTerm;
+    public static final String KEYWORD = "find";
+    private final String keyTerm;
 
-        public FindCommand(String userInput) {
-            String inputFormat = String.format("\t\"%s [keyword/(s)]\"", KEYWORD);
-            String inputData = userInput.substring(KEYWORD.length()).trim();
-            this.keyTerm = inputData.toLowerCase();
-        }
+    /**
+     * Constructor for command.
+     * @param userInput The input string entered by the user.
+     */
+    public FindCommand(String userInput) {
+        String inputFormat = String.format("\t\"%s [keyword/(s)]\"", KEYWORD);
+        String inputData = userInput.substring(KEYWORD.length()).trim();
+        this.keyTerm = inputData.toLowerCase();
+    }
 
-        public String execute(TaskList taskList) {
-            // If task list is empty, only a "\n" will be printed
-            TaskList foundTasks = taskList.findTasks(keyTerm);
-            String output = "Here are the matching tasks in your list:\n";
-            output += foundTasks.toString();
-            return output;
-        }
+    /**
+     * Returns a list of tasks containing the specified keyword.
+     * @param taskList The existing list of tasks.
+     * @return The list of tasks containing the specified keyword.
+     */
+    public String execute(TaskList taskList) {
+        // If task list is empty, only a "\n" will be printed
+        TaskList foundTasks = taskList.findTasks(keyTerm);
+        String output = "Here are the matching tasks in your list:\n";
+        output += foundTasks.toString();
+        return output;
+    }
 }
