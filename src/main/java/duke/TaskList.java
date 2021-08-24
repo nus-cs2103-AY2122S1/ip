9c51f0b5239f5a01630c8fc91d157cdda3e4ba16
ntwbruce <e0544240@u.nus.edu>
2021-08-25 03:27:33 +0800

    Add JavaDoc for classes in the duke package
    Add JavaDoc comments to explain functionality of public methods to aid
    users' understanding.
    
    Let's add JavaDoc comments for public methods in the duke package
    classes.

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -15,32 +15,61 @@ public class TaskList {
         this.ui = ui;
         this.list = list;
     }
-    
+
+    /**
+     * Checks if the given index is a valid task index.
+     * 
+     * @param index The task index to be checked for validity.
+     * @return True if the given index is invalid.
+     */
     public boolean isInvalidIndex(int index) {
         return index > list.size() || index < 1;
     }
-    
+
+    /**
+     * Instructs the Ui to display the current task list.
+     */
     public void showList() {
         ui.showList(list);
     }
-    
+
+    /**
+     * Sets the task with the given index to done.
+     * 
+     * @param index The index of the task to be set to done.
+     */
     public void setTaskDone(int index) {
         Task toSetDone = list.get(index);
         storage.setDBEntryDone(toSetDone.databaseEntry());
         toSetDone.setDone();
         ui.showDone(toSetDone);
     }
-    
+
+    /**
+     * Deletes the task with the given index.
+     * 
+     * @param index The index of the task to be deleted.
+     */
     public void deleteTask(int index) {
         Task deleted = list.remove(index);
         storage.deleteDBEntry(deleted.databaseEntry());
         ui.showDelete(deleted, list.size());
     }
-    
+
+    /**
+     * Instructs the Ui to display a list of tasks that take place on the given date.
+     * 
+     * @param desiredDate The date used to find tasks.
+     */
     public void findTasks(LocalDate desiredDate) {
         ui.showFind(list, list.size(), desiredDate);
     }
-    
+
+    /**
+     * Adds the given task to the task list.
+     * 
+     * @param newTask The task to be added to the task list.
+     */
     public void addTask(Task newTask) {
         list.add(newTask);
         storage.addDBEntry(newTask.databaseEntry());
