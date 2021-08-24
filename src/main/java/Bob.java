2313e2b218dd20b4b90966de12d4cc197e858211
Felicia Ivane <e0552298@u.nus.edu>
2021-08-24 17:06:57 +0800

    Revert "Handle exceptions in Level 7 implementation"
    This reverts commit b841dacf31992645562a6853ac92204561e3d098.

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -10,84 +10,80 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Bob {
-    public static void main(String[] args) {
-        try {
-            System.out.println("Howwwwwwdy! I'm Bob");
-            System.out.println("What do you want?\n");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Howwwwwwdy! I'm Bob");
+        System.out.println("What do you want?\n");
 
-            String currDirectory = new File("").getAbsolutePath();
-            File dataDirectory = new File(currDirectory + "/data");
-            boolean directoryExists = dataDirectory.exists(); //Check for data directory
-            if (!directoryExists) { //Make data directory if it does not exist yet
-                dataDirectory.mkdir();
-            }
+        String currDirectory = new File("").getAbsolutePath();
+        File dataDirectory = new File(currDirectory + "/data");
+        boolean directoryExists = dataDirectory.exists(); //Check for data directory
+        if (!directoryExists) { //Make data directory if it does not exist yet
+            dataDirectory.mkdir();
+        }
 
-            boolean fileExists = new File(dataDirectory + "/bob.txt").exists(); //Check for bob.txt
-            if (!fileExists) { //Make bob.txt file if it does not exist yet
-                new File(dataDirectory, "bob.txt").createNewFile();
-            }
+        boolean fileExists = new File(dataDirectory + "/bob.txt").exists(); //Check for bob.txt
+        if (!fileExists) { //Make bob.txt file if it does not exist yet
+            new File(dataDirectory,"bob.txt").createNewFile();
+        }
 
-            Scanner scanner = new Scanner(System.in);
-            TaskList taskList = new TaskList();
-            taskList.initialiseTaskList();
+        Scanner scanner = new Scanner(System.in);
+        TaskList taskList = new TaskList();
+        taskList.initialiseTaskList();
 
-            String response = scanner.nextLine();
+        String response = scanner.nextLine();
 
-            while (!Objects.equals(response, "bye")) {
-                try {
-                    inputChecker(response, taskList);
+        while (!Objects.equals(response, "bye")) {
+            try {
+                inputChecker(response, taskList);
 
-                    if (Objects.equals(response, "list")) { //show list of tasks
-                        System.out.println(taskList.getList());
-                        response = scanner.nextLine();
-                    } else if (response.matches("done(.*)")) { //complete a task
-                        String[] splitResponse = response.split(" ", 2);
-                        System.out.println(taskList.markIndexCompleted(Integer.parseInt(splitResponse[1]) - 1));
-                        response = scanner.nextLine();
-                    } else if (response.matches("delete(.*)")) {
-                        String[] splitResponse = response.split(" ", 2);
-                        System.out.println(taskList.deleteIndex(Integer.parseInt(splitResponse[1]) - 1));
-                        response = scanner.nextLine();
-                    } else if (response.matches("todo(.*)") || response.matches("deadline(.*)") //add a new task
-                            || response.matches("event(.*)")) {
-                        String[] splitResponse = response.split(" ", 2);
-                        Task newTask;
+                if (Objects.equals(response, "list")) { //show list of tasks
+                    System.out.println(taskList.getList());
+                    response = scanner.nextLine();
+                } else if (response.matches("done(.*)")) { //complete a task
+                    String[] splitResponse = response.split(" ", 2);
+                    System.out.println(taskList.markIndexCompleted(Integer.parseInt(splitResponse[1]) - 1));
+                    response = scanner.nextLine();
+                } else if (response.matches("delete(.*)")) {
+                    String[] splitResponse = response.split(" ", 2);
+                    System.out.println(taskList.deleteIndex(Integer.parseInt(splitResponse[1]) - 1));
+                    response = scanner.nextLine();
+                } else if (response.matches("todo(.*)") || response.matches("deadline(.*)") //add a new task
+                        || response.matches("event(.*)")) {
+                    String[] splitResponse = response.split(" ", 2);
+                    Task newTask;
 
-                        if (Objects.equals(splitResponse[0], "todo")) {
-                            newTask = new Todo(splitResponse[1]);
-                        } else if (Objects.equals(splitResponse[0], "deadline")) {
-                            String[] splitAgain = splitResponse[1].split(" /by ", 2);
-                            newTask = new Deadline(splitAgain[0], splitAgain[1]);
-                        } else {
-                            String[] splitAgain = splitResponse[1].split(" /at ", 2);
-                            newTask = new Event(splitAgain[0], splitAgain[1]);
-                        }
-
-                        System.out.println(taskList.addTask(newTask)); //add a task
-                        response = scanner.nextLine();
+                    if (Objects.equals(splitResponse[0], "todo")) {
+                        newTask = new Todo(splitResponse[1]);
+                    } else if (Objects.equals(splitResponse[0], "deadline")) {
+                        String[] splitAgain = splitResponse[1].split(" /by ", 2);
+                        newTask = new Deadline(splitAgain[0], splitAgain[1]);
+                    } else {
+                        String[] splitAgain = splitResponse[1].split(" /at ", 2);
+                        newTask = new Event(splitAgain[0], splitAgain[1]);
                     }
-                } catch (InvalidInputException e) {
-                    System.out.println("That doesn't make any sense! >:(\n");
-                    response = scanner.nextLine();
-                } catch (NoTaskException e) {
-                    System.out.println("You didn't specify your task! >:(\n");
-                    response = scanner.nextLine();
-                } catch (NoDeadlineException e) {
-                    System.out.println("When is the deadline? >:(\n");
-                    response = scanner.nextLine();
-                } catch (NoEventTimingException e) {
-                    System.out.println("When is the event? >:(\n");
-                    response = scanner.nextLine();
-                } catch (OutOfBoundsException e) {
-                    System.out.println("Huh what task is that :/\n");
+
+                    System.out.println(taskList.addTask(newTask)); //add a task
                     response = scanner.nextLine();
                 }
+            } catch (InvalidInputException e) {
+                System.out.println("That doesn't make any sense! >:(\n");
+                response = scanner.nextLine();
+            } catch (NoTaskException e) {
+                System.out.println("You didn't specify your task! >:(\n");
+                response = scanner.nextLine();
+            } catch (NoDeadlineException e) {
+                System.out.println("When is the deadline? >:(\n");
+                response = scanner.nextLine();
+            } catch (NoEventTimingException e) {
+                System.out.println("When is the event? >:(\n");
+                response = scanner.nextLine();
+            } catch (OutOfBoundsException e) {
+                System.out.println("Huh what task is that :/\n");
+                response = scanner.nextLine();
             }
-
-            System.out.println("Bye! Shoo!");
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
         }
+
+        System.out.println("Bye! Shoo!");
     }
 
     private static void inputChecker(String response, TaskList tasklist) throws InvalidInputException, NoTaskException,
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -12,46 +12,42 @@ public class TaskList {
     private ArrayList<Task> taskList;
 
     public TaskList() {
-        this.taskList = new ArrayList<>();
+        this.taskList = new ArrayList<Task>();
     }
 
-    public void initialiseTaskList() {
-        try {
-            String path = new File("").getAbsolutePath() + "/data/bob.txt";
-            File f = new File(path);
-            Scanner s = new Scanner(f);
-            while (s.hasNext()) {
-                String curr = s.nextLine();
-                Task newTask;
-                boolean isComplete;
-                if (curr.matches("\\[T](.*)")) {
-                    String[] splitCurr = curr.split(" \\Q[\\E.\\Q]\\E ", 2);
-                    newTask = new Todo(splitCurr[1]);
-                    isComplete = curr.contains("[X]");
-                } else if (curr.matches("\\[D](.*)")) {
-                    String[] splitCurr = curr.split(" \\Q[\\E.\\Q]\\E ", 2);
-                    String[] splitRight = splitCurr[1].split(" \\(by: ", 2);
-                    String by = splitRight[1].substring(0, splitRight[1].length() - 1);
-                    newTask = new Deadline(splitRight[0], by);
-                    isComplete = curr.contains("[X]");
-                } else {
-                    String[] splitCurr = curr.split(" \\Q[\\E.\\Q]\\E ", 2);
-                    String[] splitRight = splitCurr[1].split(" \\(at: ", 2);
-                    String at = splitRight[1].substring(0, splitRight[1].length() - 1);
-                    newTask = new Event(splitRight[0], at);
-                    isComplete = curr.contains("[X]");
-                }
-                this.taskList.add(newTask);
-                if (isComplete) {
-                    newTask.markCompleted();
-                }
+    public void initialiseTaskList() throws IOException {
+        String path = new File("").getAbsolutePath() + "/data/bob.txt";
+        File f = new File(path);
+        Scanner s = new Scanner(f);
+        while (s.hasNext()) {
+            String curr = s.nextLine();
+            Task newTask;
+            boolean isComplete;
+            if (curr.matches("\\[T](.*)")) {
+                String[] splitCurr = curr.split(" \\Q[\\E.\\Q]\\E ", 2);
+                newTask = new Todo(splitCurr[1]);
+                isComplete = curr.contains("[X]");
+            } else if (curr.matches("\\[D](.*)")) {
+                String[] splitCurr = curr.split(" \\Q[\\E.\\Q]\\E ", 2);
+                String[] splitRight = splitCurr[1].split(" \\(by: ", 2);
+                String by = splitRight[1].substring(0, splitRight[1].length() - 1);
+                newTask = new Deadline(splitRight[0], by);
+                isComplete = curr.contains("[X]");
+            } else {
+                String[] splitCurr = curr.split(" \\Q[\\E.\\Q]\\E ", 2);
+                String[] splitRight = splitCurr[1].split(" \\(at: ", 2);
+                String at = splitRight[1].substring(0, splitRight[1].length() - 1);
+                newTask = new Event(splitRight[0], at);
+                isComplete = curr.contains("[X]");
             }
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+            this.taskList.add(newTask);
+            if (isComplete) {
+                newTask.markCompleted();
+            }
         }
     }
 
-    public String addTask(Task task) {
+    public String addTask(Task task) throws IOException {
         this.taskList.add(task);
         updateTextFile();
         return "Okay okay I've added the task:\n" + task.printTask() + "\n"
@@ -66,14 +62,14 @@ public class TaskList {
         return result;
     }
 
-    public String markIndexCompleted(int index) {
+    public String markIndexCompleted(int index) throws IOException {
         Task selectedTask = this.taskList.get(index);
         selectedTask.markCompleted();
         updateTextFile();
         return "Wow you finally did something productive!\n" + selectedTask.printTask() + "\n";
     }
 
-    public String deleteIndex(int index) {
+    public String deleteIndex(int index) throws IOException {
         Task selectedTask = this.taskList.get(index);
         this.taskList.remove(index);
         updateTextFile();
@@ -85,17 +81,13 @@ public class TaskList {
         return Integer.toString(this.taskList.size());
     }
 
-    private void updateTextFile() {
-        try {
-            String path = new File("").getAbsolutePath() + "/data/bob.txt";
-            FileWriter writer = new FileWriter(path);
-            writer.write("");
-            writer.close();
-            for (int i = 0; i < taskList.size(); i++) {
-                appendToFile(path, taskList.get(i).printTask());
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+    private void updateTextFile() throws IOException {
+        String path = new File("").getAbsolutePath() + "/data/bob.txt";
+        FileWriter writer = new FileWriter(path);
+        writer.write("");
+        writer.close();
+        for(int i = 0; i < taskList.size(); i++) {
+            appendToFile(path, taskList.get(i).printTask());
         }
     }
 
