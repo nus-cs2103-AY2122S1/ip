aad863c0cba8bfb197d50d24e5803f0b4ccfc691
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-08-24 22:48:13 +0800

    Create InvalidCommand class which inherits from Command

---
+++ src/main/java/InvalidCommand.java
@@ -0,0 +1,16 @@
+public class InvalidCommand extends Command {
+
+    @Override
+    public void execute(TaskList taskList, Ui ui, Storage storage) throws DukeException {
+        String errorMessage = "\t Sorry I do not understand this command \n";
+        errorMessage += "\t Please use one of the following commands: \n";
+        errorMessage += "\t \t list - To list the added tasks so far\n";
+        errorMessage += "\t \t todo {description} - To add a ToDo task\n";
+        errorMessage += "\t \t deadline {description} /by {time} - To add a Deadline task\n";
+        errorMessage += "\t \t event {description} /at {time} - To add an Event task\n";
+        errorMessage += "\t \t done {number} - To mark the indicated task as done\n";
+        errorMessage += "\t \t delete {number} - To delete the indicated task\n";
+        errorMessage += "\t \t bye (To exit programme)\n";
+        throw new DukeException(errorMessage);
+    }
+}
