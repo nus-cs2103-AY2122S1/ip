3fa4246b24b545bee5f3bee729d54ad415877077
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-08-24 22:48:37 +0800

    Update parse method in Parser to detect commands

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,7 +1,31 @@
+import java.util.Locale;
+
 public class Parser {
 
-    public static String parse(String command) {
-        String first = command.split(" ")[0];
-        return first;
+    public static Command parse(String command) {
+        String first = command.toLowerCase().split(" ")[0];
+        switch (first) {
+        case "bye":
+            break;
+        case "list":
+            break;
+        case "todo":
+            break;
+        case "deadline":
+            break;
+        case "event":
+            break;
+        case "delete":
+            break;
+        case "done":
+            break;
+        case "save":
+            break;
+        default:
+            return new InvalidCommand();
+        }
+
+        // Placeholder to remove error
+        return new InvalidCommand();
     }
 }
