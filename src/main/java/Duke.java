d692d7b75be2bda0ba24ce08cbac9e29384007f9
noobmaster19 <dionneo123@gmail.com>
2021-08-24 16:15:36 +0800

    'init'

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,49 @@
 import java.util.*;
+import java.io.*;
 
 public class Duke {
     static ArrayList<Task> userInputs = new ArrayList<>();
     static boolean userEnded = false;
+    static File data;
+    static File directory;
+
+    public static void initialize() {
+        directory = new File("data");
+        data = new File("data/duke.txt");
+        if (!directory.exists()){
+            directory.mkdirs();
+        } 
+        if (!data.exists()){
+            try {
+                data.createNewFile();
+            } catch (IOException e){
+                System.out.println(e);
+            }
+        }
+        try {
+            Scanner s = new Scanner(data);
+            while (s.hasNext()) {
+                String[] parsed = s.nextLine().split(",");
+                String type = parsed[0];
+                if (type.equals("todo")) {
+                    userInputs.add(new Todo(parsed[1]));
+                } else if (type.equals("event")) {
+                    userInputs.add(new Event(parsed[1], parsed[2]));
+                } else {
+                    userInputs.add(new Deadline(parsed[1], parsed[2]));
+                }
+            }  
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void appendToFile(String task) throws IOException {
+        FileWriter fw = new FileWriter(data, true);
+        fw.write(task);
+        fw.write(System.lineSeparator());
+        fw.close();
+    }
 
     public static void markAsDone(String input) {
         try {
@@ -21,7 +62,7 @@ public class Duke {
             System.out.println("Noted. I've removed this task: ");
             int taskIndex = Integer.parseInt(input.replaceAll("[^0-9]", ""));
             Task task = userInputs.get(taskIndex - 1);
-            userInputs.remove(taskIndex-1);
+            userInputs.remove(taskIndex - 1);
             System.out.println(" " + task);
             task = null;
             System.out.println("Now you have " + userInputs.size() + " tasks in the list.");
@@ -41,39 +82,47 @@ public class Duke {
         String description;
         String time;
         Task task;
-        if (input.startsWith("todo")) {
-            if (input.equals("todo")) {
-                throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+        try {
+            if (input.startsWith("todo")) {
+                if (input.equals("todo")) {
+                    throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                } else {
+                    description = input.split("todo ")[1];
+                    task = new Todo(description);
+                    appendToFile("todo" + "," + description);
+                }
+            } else if (input.startsWith("deadline")) {
+                if (input.equals("deadline")) {
+                    throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                } else {
+                    description = input.split("deadline ")[1].split(" /")[0];
+                    time = input.split("/by ")[1];
+                    task = new Deadline(description, time);
+                    appendToFile("deadline" + "," + description + "," + time);
+                }
+            } else if (input.startsWith("event")) {
+                if (input.equals("event")) {
+                    throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
+                } else {
+                    description = input.split("event ")[1].split(" /")[0];
+                    time = input.split("/at ")[1];
+                    task = new Event(description, time);
+                    appendToFile("event" + "," + description + "," + time);
+                }
             } else {
-                description = input.split("todo ")[1];
-                task = new Todo(description);
+                throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
-        } else if (input.startsWith("deadline")) {
-            if (input.equals("deadline")) {
-                throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
-            } else {
-                description = input.split("deadline ")[1].split(" /")[0];
-                time = input.split("/by ")[1];
-                task = new Deadline(description, time);
-            }
-        } else if (input.startsWith("event")) {
-            if (input.equals("event")) {
-                throw new DukeException("☹ OOPS!!! The description of a event cannot be empty.");
-            } else {
-                description = input.split("event ")[1].split(" /")[0];
-                time = input.split("/at ")[1];
-                task = new Event(description, time);
-            }
-        } else {
-            throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            userInputs.add(task);
+            System.out.println("Got it. I've added this task: ");
+            System.out.println(task);
+            System.out.println("Now you have " + userInputs.size() + " tasks in the list.");
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
         }
-        userInputs.add(task);
-        System.out.println("Got it. I've added this task: ");
-        System.out.println(task);
-        System.out.println("Now you have " + userInputs.size() + " tasks in the list.");
     }
 
     public static void main(String[] args) {
+        initialize();
         System.out.println("Hello! I'm Duke What can I do for you?");
         while (!userEnded) {
             Scanner sc = new Scanner(System.in);
