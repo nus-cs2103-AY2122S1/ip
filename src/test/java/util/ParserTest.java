dac5511efd1684a08e5a0b5d6efdd742ef1eb011
DesmondTo <e0550619@u.nus.edu>
2021-08-24 21:20:12 +0800

    Add Junit test for Parser class

--- src/test/java/util/ParserTest.java
+++ src/test/java/util/ParserTest.java
@@ -1,2 +1,28 @@
-package util;public class ParserTest {
+package util;
+
+import duke.exception.EmptyDescriptionException;
+import duke.exception.IncompleteDescriptionException;
+import duke.util.Parser;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ParserTest {
+
+    @Test
+    public void emptyCommandTest() {
+        assertThrows(EmptyDescriptionException.class, () -> Parser.parse(""));
+    }
+
+    @Test
+    public void incompleteCommandTest() {
+        assertThrows(EmptyDescriptionException.class, () -> Parser.parse("todo "));
+    }
+
+    @Test
+    public void allSpaceCommandTest() {
+        assertThrows(EmptyDescriptionException.class, () -> Parser.parse("      "));
+    }
+
 }
