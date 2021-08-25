a092684d289ffb3dc605bdcf2e88972e10f61995
Nicolas <nicolas.chang99@gmail.com>
2021-08-25 20:59:48 +0800

    Fix GetShortForm test

--- src/test/java/kermit/tasks/EventTest.java
+++ src/test/java/kermit/tasks/EventTest.java
@@ -2,11 +2,13 @@ package kermit.tasks;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EventTest {
     @Test
     public void testGetShortForm(){
-        assertEquals("E", new ToDos("Test text").getShortForm());
+        assertEquals("E", new Event("Test text", LocalDate.parse("2021-04-05")).getShortForm());
     }
 }
