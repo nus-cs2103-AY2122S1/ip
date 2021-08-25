f2ef62e16118a8333edf791e6137c10fbe598e5d
NecroWolf28 <56968759+NecroWolf28@users.noreply.github.com>
2021-08-26 03:38:52 +0800

    Add classes for the Save function

---
+++ src/main/java/Storage.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class FileManager {
+
+    private static void createNewFile(String fileName) {
+        File f = new File(fileName);
+    }
+
+    private static void printFileContents(String filePath) throws FileNotFoundException {
+        File f = new File(filePath);
+        Scanner s = new Scanner(f);
+        while (s.hasNext()) {
+            System.out.println(s.nextLine());
+        }
+    }
+
+    private static void writeToFile(String filePath, String textToAdd) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(textToAdd);
+        fw.close();
+    }
+
+    private static void appendToFile(String filePath, String textToAppend) throws IOException {
+        FileWriter fw = new FileWriter(filePath, true);
+        fw.write(textToAppend);
+        fw.close();
+    }
+
+    // need a file path generator method?
+
+}
