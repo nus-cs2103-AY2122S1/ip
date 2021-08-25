d51d6724abbf02340dd89b4447b1de9d95f5e9e0
Kar Wi <t.karwi@yahoo.com>
2021-08-25 13:43:25 +0800

    Implement Level 7

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,8 +1,11 @@
+import java.io.IOException;
+
 public class Deadline extends Task {
 
     protected String by;
+    private static final String label = "D";
 
-    public Deadline(String description, String by) throws DukeException {
+    public Deadline(String description, String by) throws DukeException, IOException {
         super(description);
         if (by.isEmpty()) {
             throw new DukeException("☹ OOPS!!! The deadline cannot be empty.");
@@ -12,6 +15,12 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by:" + by + ")";
+        return "[" + label + "]" + super.toString() + " (by: " + by + ")";
     }
+
+    @Override
+    public String toDataString() {
+        return label + super.toDataString() + " | " + by;
+    }
+
 }
