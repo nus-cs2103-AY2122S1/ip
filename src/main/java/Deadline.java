fb29827b7be9a5294baedf8bfc45ccff7fedd23b
Yttruire <teozhihao99@gmail.com>
2021-08-26 00:49:13 +0800

    Added support for proper Date and Time representation and DATE command
    Deadlines and events now represent datetime as a LocalDate object. This allows future manipulations of tasks using dates as well.
    
    DATE command has been added to allow the query of tasks that are due/occur on the specified date.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
-    protected String deadline;
+    protected LocalDate deadline;
 
     public Deadline(String description, String deadline) {
         super(description);
-        this.deadline = deadline;
+        this.deadline = LocalDate.parse(deadline);
+    }
+
+    @Override
+    public boolean hasDueDate(LocalDate dueDate) {
+        return dueDate.isEqual(this.deadline);
     }
 
     @Override
     public String toString() {
-        return "[D][" + getStatusIcon() + "] " + this.description + " (by: " + this.deadline + ")";
+        return "[D][" + getStatusIcon() + "] " + this.description + " (by: " + this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,11 @@
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Duke {
     /**
@@ -13,9 +18,35 @@ public class Duke {
                 "\n    ____________________________________________________________");
     }
 
+
+    public static void printTaskList(List<Task> taskData) {
+        // Construct the string containing the list of items that have been stored in preparation to send to user
+        StringBuilder listMessage = new StringBuilder("Here are the tasks in your list:");
+
+        // Add all elements in the list
+        for (int i = 0; i < taskData.size(); ++i) {
+            listMessage.append("\n").append(i + 1).append(". ").append(taskData.get(i));
+        }
+
+        sendMessage(listMessage.toString());
+    }
+
+    public static void printTaskList(List<Task> taskData, LocalDate date) {
+        // Construct the string containing the list of items that have been stored in preparation to send to user
+        StringBuilder listMessage = new StringBuilder("Here are the tasks in your list that are due on ")
+                .append(date.format(DateTimeFormatter.ofPattern("MMM d yyyy"))).append(":");
+
+        // Add all elements in the list
+        for (int i = 0; i < taskData.size(); ++i) {
+            listMessage.append("\n").append(i + 1).append(". ").append(taskData.get(i));
+        }
+
+        sendMessage(listMessage.toString());
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Task> listData = new ArrayList<>();
+        List<Task> taskData = new ArrayList<>();
 
         final String welcomeMessage = "Hello! I'm Duke\nWhat can I do for you?";
         sendMessage(welcomeMessage);
@@ -32,26 +63,18 @@ public class Duke {
 
                 } else if (userInput.equals("list")) {
                     // LIST command
-                    // Construct the string containing the list of items that have been stored in preparation to send to user
-                    StringBuilder listMessage = new StringBuilder("Here are the tasks in your list:");
-
-                    // Add all elements in the list
-                    for (int i = 0; i < listData.size(); ++i) {
-                        listMessage.append("\n").append(i + 1).append(". ").append(listData.get(i));
-                    }
-
-                    sendMessage(listMessage.toString());
+                    printTaskList(taskData);
                 } else if (userInput.startsWith("done")) {
                     int index = Integer.parseInt(userInput.substring(5)) - 1;
-                    listData.get(index).markAsDone();
+                    taskData.get(index).markAsDone();
 
-                    sendMessage("Nice! I've marked this task as done:\n  " + listData.get(index));
+                    sendMessage("Nice! I've marked this task as done:\n  " + taskData.get(index));
                 } else if (userInput.startsWith("delete")) {
                     int index = Integer.parseInt(userInput.substring(7)) - 1;
-                    Task removedTask = listData.remove(index);
+                    Task removedTask = taskData.remove(index);
 
                     sendMessage("Noted. I've removed this task:\n  " + removedTask +
-                            "\nNow you have " + listData.size() + " tasks in the list.");
+                            "\nNow you have " + taskData.size() + " tasks in the list.");
                 } else if (userInput.startsWith("todo")) {
                     // If no arguments provided
                     if (userInput.length() == 4) {
@@ -62,9 +85,9 @@ public class Duke {
                     }
 
                     Todo newTodo = new Todo(userInput.substring(5).trim());
-                    listData.add(newTodo);
+                    taskData.add(newTodo);
 
-                    sendMessage("Got it. I've added this task:\n  " + newTodo + "\nNow you have " + listData.size() + " tasks in the list.");
+                    sendMessage("Got it. I've added this task:\n  " + newTodo + "\nNow you have " + taskData.size() + " tasks in the list.");
                 } else if (userInput.startsWith("deadline")) {
                     // If no arguments provided
                     if (userInput.length() == 8) {
@@ -83,9 +106,9 @@ public class Duke {
                     }
 
                     Deadline newDeadline = new Deadline(description, deadlineDate.substring(4));
-                    listData.add(newDeadline);
+                    taskData.add(newDeadline);
 
-                    sendMessage("Got it. I've added this task:\n  " + newDeadline + "\nNow you have " + listData.size() + " tasks in the list.");
+                    sendMessage("Got it. I've added this task:\n  " + newDeadline + "\nNow you have " + taskData.size() + " tasks in the list.");
                 } else if (userInput.startsWith("event")) {
                     // If no arguments provided
                     if (userInput.length() == 5) {
@@ -104,15 +127,30 @@ public class Duke {
                     }
 
                     Event newEvent = new Event(description, datetime.substring(4));
-                    listData.add(newEvent);
+                    taskData.add(newEvent);
 
-                    sendMessage("Got it. I've added this task:\n  " + newEvent + "\nNow you have " + listData.size() + " tasks in the list.");
+                    sendMessage("Got it. I've added this task:\n  " + newEvent + "\nNow you have " + taskData.size() + " tasks in the list.");
+                } else if (userInput.startsWith("date")) {
+                    if (userInput.length() == 4) {
+                        throw new DukeException("A date must be provided to find events and deadlines occurring on that day");
+                    } else if (!userInput.startsWith("date ")) {
+                        // If it does not start with "date " after trimming, it is an invalid command
+                        throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                    }
+                    LocalDate queryDate = LocalDate.parse(userInput.substring(5));
+
+                    List<Task> dueTasks = taskData.stream().filter(x -> x.hasDueDate(queryDate)).collect(Collectors.toList());
+
+                    printTaskList(dueTasks, queryDate);
                 } else {
                     // Unknown command
                     throw new DukeException(("I'm sorry, but I don't know what that means :-("));
                 }
+
+            } catch (DateTimeParseException e) {
+                    sendMessage("Unknown date format. Please input a valid date in the format: YYYY-MM-DD");
             } catch (Exception e) {
-                sendMessage(e.getMessage());
+                    sendMessage(e.getMessage());
             }
         }
     }
