b32e26924cd4f219bcfca7cc761b454ddf27fadf
Kar Wi <t.karwi@yahoo.com>
2021-08-26 00:21:54 +0800

    Implement A-CodingStandard

--- src/main/java/duke/command/Command.java
+++ src/main/java/duke/command/Command.java
@@ -6,7 +6,6 @@ import duke.exception.DukeException;
 import duke.task.TaskList;
 
 public abstract class Command {
-
     public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException;
 
     public Boolean isExit() {
