43efdbfea664dfe9b9bdeabb9b8048705be0307d
HongshuW <hongshu.wang.uni@gmail.com>
2021-08-25 22:17:46 +0800

    Add FindTaskCommand.java

---
+++ src/main/java/duke/command/FindTaskCommand.java
@@ -0,0 +1,10 @@
+package duke.command;
+
+/**
+ * Represents a command that finds a task with keyword. A subclass of Command.
+ */
+public class FindTaskCommand extends Command {
+    public FindTaskCommand() {
+        super("find");
+    }
+}
