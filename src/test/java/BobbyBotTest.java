294db7fcd6fff1eede0641413a0c1b157a02e1c8
mokdarren <mokkwd@gmail.com>
2021-08-25 23:29:11 +0800

    Test: Add Junit tests to package

---
+++ src/test/java/BobbyBotTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class BobbyBotTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2,2);
+    }
+
+}
