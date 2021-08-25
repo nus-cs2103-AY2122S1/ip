141380c8a4d9357432972868af3e25d81992a52f
xnajasho <iifourxd@gmail.com>
2021-08-25 23:27:56 +0800

    Add JavaDoc Comments for Ui Class

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -2,8 +2,15 @@ package duke;
 
 import java.util.Scanner;
 
+/**
+ * Class which deals with the UI
+ */
+
 public class Ui {
+
+    /** prints a line divider */
     private final String line;
+
     private final Scanner sc;
 
     Ui() {
@@ -11,6 +18,9 @@ public class Ui {
         sc = new Scanner(System.in);
     }
 
+    /**
+     * Welcome message displayed upon app startup
+     */
     public void showWelcomeMessage() {
         String openingMessage = line
                 + "\n   Hello! This is Duke :)"
@@ -21,10 +31,16 @@ public class Ui {
         System.out.println(openingMessage);
     }
 
+    /**
+     * Displays a loading error upon encountering an InvalidStorageFilePathException
+     */
     public void showLoadingError() {
         System.out.println("   LOADING ERROR: Unable to load previously saved data...");
     }
 
+    /**
+     * Displays the error message of any checked/unchecked exceptions.
+     */
     public void showError(String errorMessage) {
         if (errorMessage.equals("") || errorMessage == null) {
             System.out.println("   Unknown error was not handled by DukeException");
@@ -33,6 +49,9 @@ public class Ui {
         }
     }
 
+    /**
+     * Displays the farewell message upon invoke of the 'bye' command.
+     */
     public void showByeMessage() {
         sc.close();
         String goodbyeMessage = "   Bye! Hope to see you again soon!";
@@ -43,27 +62,43 @@ public class Ui {
         System.out.println(this.line);
     }
 
+    /**
+     * Displays the description of the newly added task.
+     */
     public void showAddTaskMessage(String taskDescription, int currentListSize) {
         String output = String.format("   Got it. I've added this task: \n      %s\n   Now you have %d tasks in the list.",
                 taskDescription, currentListSize);
         System.out.println(output);
     }
 
+    /**
+     * Displays the description of the specified deleted task
+     */
     public void showDeletedTask(String taskDescription, int currentListSize) {
         String output = String.format("   Noted. I've deleted this task: \n      %s\n   Now you have %d tasks in the list.",
                 taskDescription, currentListSize);
         System.out.println(output);
     }
 
+    /**
+     * Displays the description of the task upon completion.
+     */
     public void showCompletedTask(String taskInfo) {
         String output = String.format("   Nice! I've marked this task as done: \n      %s", taskInfo);
         System.out.println(output);
     }
 
+    /**
+     * Invoked when user inputs an invalid command
+     */
     public void showInvalidCommand() {
         System.out.println("   INVALID INPUT: Start the sentence with either 'todo'/'deadline'/'event'/'list'/'done'/'delete'/'bye'");
     }
 
+    /**
+     * Outputs the content of the task list.
+     * @param lst refers to the current session's most updated task list
+     */
     public void printList(TaskList lst) {
         boolean isEmptyList = false;
         int counter = 1;
@@ -85,10 +120,16 @@ public class Ui {
         System.out.println(output);
     }
 
+    /**
+     * Reads the user input
+     */
     public String readCommand() {
         return sc.nextLine();
     }
 
+    /**
+     * Prints the message with information on the task type if the user input enters an empty description
+     */
     public String printEmptyDescription(String taskType) {
         return String.format("   OOPS!!! The description of a %s cannot be empty.", taskType);
     }
