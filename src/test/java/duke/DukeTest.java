5f2dbfb18eafea060f4bc902a76e5805321c4e18
HuangChing <ching2020huang@gmail.com>
2021-08-25 22:56:22 +0800

    add test class

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class DukeTest {
+package duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DukeTest {
+    @Test
+    public void dummyTest(){
+        assertEquals(2, 2);
+    }
+
+    public void
+
 }
