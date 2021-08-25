670535e77d8621d17b5535e71750a8b374aabf73
Lutfi Randiva <luffingluffy@gmail.com>
2021-08-25 08:37:23 +0800

    Add TaskList for OOP

---
+++ src/main/java/task/TaskList.java
@@ -0,0 +1,44 @@
+package task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private List<Task> tasks;
+
+    public TaskList(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    public int getSize() {
+        return tasks.size();
+    }
+
+    public Task get(int index) {
+        return tasks.get(index);
+    }
+
+    public List<Task> getList() {
+        return tasks;
+    }
+
+    public void add(Task task) {
+        tasks.add(task);
+    }
+
+    public void remove(int index) {
+        tasks.remove(index);
+    }
+
+    public List<String> enumerate() {
+        List<String> numList = new ArrayList<>();
+        for (int i = 0; i < tasks.size(); i++) {
+            numList.add((i + 1) + ". " + tasks.get(i));
+        }
+        return numList;
+    }
+}
