2bfb20fe54f1d94f5d804d6129f0fdb1c41083c1
Cedric Ong <cedricongjh@gmail.com>
2021-08-25 12:42:12 +0800

    Add methods to format Task for saving to text file

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -4,6 +4,13 @@ public class Deadline extends Task {
         super(description);
     }
 
+    public Deadline(String description, boolean done) { super(description, done); }
+
+    @Override
+    public String toFileData() {
+        return "D," + super.toFileData();
+    }
+
     @Override
     public String toString() {
         return "[D]" + super.toString();
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -4,6 +4,13 @@ public class Event extends Task {
         super(description);
     }
 
+    public Event(String description, boolean done) { super(description, done); }
+
+    @Override
+    public String toFileData() {
+        return "E," + super.toFileData();
+    }
+
     @Override
     public String toString() {
         return "[E]" + super.toString();
