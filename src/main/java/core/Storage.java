cd4f4c1548262f78973a5e7b1b8516e4d5520f4b
BenjaminLHR <benjamin.lee.hr@gmail.com>
2021-08-25 19:20:31 +0800

    Add load() method and remove loadStorageToTaskList() method

--- src/main/java/core/Storage.java
+++ src/main/java/core/Storage.java
@@ -2,10 +2,13 @@ package core;
 
 import tasks.Deadline;
 import tasks.Event;
+import tasks.Task;
 import tasks.Todo;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -26,63 +29,62 @@ public class Storage {
         taskList.saveContents(file);
     }
 
-    public TaskList loadStorageToTaskList() {
-        TaskList taskList = new TaskList();
+    public ArrayList<Task> load() {
+        ArrayList<Task> listOfTasks = new ArrayList<>();
         try {
             Scanner sc = new Scanner(file);
             while (sc.hasNext()) {
                 String nextLine = sc.nextLine();
                 switch(nextLine.charAt(0)) {
                 case 'T':
-                    loadTodo(nextLine, taskList);
+                    addTodoToList(nextLine, listOfTasks);
                     break;
                 case 'D':
-                    loadDeadline(nextLine, taskList);
+                    addDeadlineToList(nextLine, listOfTasks);
                     break;
                 case 'E':
-                    loadEvent(nextLine, taskList);
+                    addEventToList(nextLine, listOfTasks);
                     break;
                 }
-//                System.out.println(sc.nextLine());
             }
-            return taskList;
+            return listOfTasks;
         } catch (FileNotFoundException e) {
             System.out.println("Storage file not found: " + e.getMessage());
             System.out.println("Task List will be initialized to empty state.");
             System.out.println("Duke.txt will be created for you once you add tasks to the list.");
-            return taskList;
+            return listOfTasks;
         }
 
     }
-
-    public void loadTodo(String line, TaskList taskList) {
+    
+    public void addTodoToList(String line, List<Task> listOfTasks) {
         String regex = "/";
         String[] splittedLine = line.split(regex);
         Todo todo = new Todo(splittedLine[2]);
         if (splittedLine[1].equals("1")) {
             todo.setCompleted();
         }
-        taskList.addTaskQuietly(todo);
+        listOfTasks.add(todo);
     }
 
-    public void loadEvent(String line, TaskList taskList) {
+    public void addEventToList(String line, List<Task> listOfTasks) {
         String regex = "/";
         String[] splittedLine = line.split(regex);
         Event event = new Event(splittedLine[2], splittedLine[3]);
         if (splittedLine[1].equals("1")) {
             event.setCompleted();
         }
-        taskList.addTaskQuietly(event);
+        listOfTasks.add(event);
     }
 
-    public void loadDeadline(String line, TaskList taskList) {
+    public void addDeadlineToList(String line, List<Task> listOfTasks) {
         String regex = "/";
         String[] splittedLine = line.split(regex);
         Deadline deadline = new Deadline(splittedLine[2], splittedLine[3]);
         if (splittedLine[1].equals("1")) {
             deadline.setCompleted();
         }
-        taskList.addTaskQuietly(deadline);
+        listOfTasks.add(deadline);
     }
 
 }
