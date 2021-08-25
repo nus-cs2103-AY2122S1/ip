a86d8636bc54c4b5ecdbf416c61e104ec6dd741b
Nicolas <nicolas.chang99@gmail.com>
2021-08-25 22:33:14 +0800

    Add javadocs to ToDo

--- src/main/java/kermit/tasks/ToDos.java
+++ src/main/java/kermit/tasks/ToDos.java
@@ -1,14 +1,35 @@
 package kermit.tasks;
 
+
+/**
+ * A basic task. That tracks if a task is complete.
+ */
 public class ToDos extends Task {
+    /**
+     * ToDo constructor
+     *
+     * @param description Description of task.
+     */
     public ToDos(String description) {
         super(description);
     }
 
+    /**
+     * ToDo constructor
+     *
+     * @param description Description of task.
+     * @param isCompleted Boolean to set if task is completed.
+     */
     public ToDos(String description, boolean isCompleted) {
         super(description, isCompleted);
     }
 
+
+    /**
+     * Get abbreviation of task, usually first letter.
+     *
+     * @return String abbreviation of task.
+     */
     public String getShortForm() {
         return "T";
     }
