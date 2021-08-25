fad485350ae967a238efefae74f913c20fe21a2a
junwei26 <limjunwei26@gmail.com>
2021-08-25 23:18:43 +0800

    Fix code style after resolving merge conflicts

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,6 +1,12 @@
 package duke;
 
-import duke.command.*;
+import duke.command.Command;
+import duke.command.AddCommand;
+import duke.command.DeleteCommand;
+import duke.command.DoneTaskCommand;
+import duke.command.ExitCommand;
+import duke.command.PrintListCommand;
+import duke.command.FindCommand;
 
 public class Parser {
 
@@ -10,7 +16,7 @@ public class Parser {
      * @param userInput String that user enters into chat bot
      * @return boolean, true if user input contains a supported command, otherwise false
      */
-    private static String[] SUPPORTED_COMMANDS = {"bye", "list", "done", "deadline", "event","todo", "delete", "find"};
+    private static String[] SUPPORTED_COMMANDS = {"bye", "list", "done", "deadline", "event", "todo", "delete", "find"};
 
     private static boolean isCommandSupported(String userInput) {
         boolean isSupported = false;
