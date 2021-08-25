2575bf44542947bd6c395d8d70af0210e6ed0771
ShahDhruvNilesh <19668322+dhshah1@users.noreply.github.com>
2021-08-25 23:39:56 +0800

    Parse dates for deadline and events

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,5 +1,9 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    private String by;
+    private LocalDateTime by;
 
     public static Task create(String userInput) throws MalformedCommandException{
         try {
@@ -8,23 +12,23 @@ public class Deadline extends Task {
             String description = userParamsSplit[0];
             String by = "";
             if(userParamsSplit[1].startsWith("by")) {
-                by = userParamsSplit[1].replaceFirst("by", "");
+                by = userParamsSplit[1].replaceFirst("by", "").stripLeading();
             }
             return new Deadline(description, by);
         } catch(ArrayIndexOutOfBoundsException e) {
             throw new MalformedCommandException("Creating an deadline needs to follow the following format: " +
-                "deadline [description] /by [Description of deadline]");
+                "deadline [description] /by [d/MM/yyyy HHmm]");
         }
 
     }
 
     private Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("d/MM/yyyy HHmm"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by:" + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM d yyyy hh:mm a"))+ ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,5 +1,9 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
-    private String at;
+    private LocalDateTime at;
 
     public static Task create(String userInput) throws MalformedCommandException {
         try {
@@ -8,22 +12,22 @@ public class Event extends Task{
             String description = userParamsSplit[0];
             String at = "";
             if(userParamsSplit[1].startsWith("at")) {
-                at = userParamsSplit[1].replaceFirst("at", "");
+                at = userParamsSplit[1].replaceFirst("at", "").stripLeading();
             }
             return new Event(description, at);
         } catch(ArrayIndexOutOfBoundsException e) {
             throw new MalformedCommandException("Creating an event needs to follow the following format: " +
-                "event [description] /at [Description of when event occurs]");
+                "event [description] /at [d/MM/yyyy HHmm]");
         }
     }
 
     private Event(String description, String at) {
         super(description);
-        this.at = at;
+        this.at = LocalDateTime.parse(at, DateTimeFormatter.ofPattern("d/MM/yyyy HHmm"));
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at:" + at + ")";
+        return "[E]" + super.toString() + " (at: " + at.format(DateTimeFormatter.ofPattern("MMM d yyyy hh:mm a")) + ")";
     }
 }
