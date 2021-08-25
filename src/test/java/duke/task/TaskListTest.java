ef90dc09c34d0e7868f61632005107e121c6be03
leonghongfai <leonghongfai@gmail.com>
2021-08-26 00:38:07 +0800

    Edit incorrect test cases

--- src/test/java/duke/task/TaskListTest.java
+++ src/test/java/duke/task/TaskListTest.java
@@ -11,8 +11,8 @@ public class TaskListTest {
     public void testGenerateStore() {
         String[] input = new String[]{"T - 1 - sth", "E - 1 - sth3 - 2020-12-12", "D - 1 - sth2 - 2020-12-12"};
         TaskList tl = new TaskList(input);
-        assertEquals(tl.get(0).toString(), "[T][X] sth");
-        assertEquals(tl.get(1).toString(), "[E][X] sth3(at: Dec 12 2020)");
-        assertEquals(tl.get(2).toString(), "[D][X] sth2(by: Dec 12 2020)");
+        assertEquals("[T][X] sth", tl.get(0).toString());
+        assertEquals("[E][X] sth3(at: Dec 12 2020)", tl.get(1).toString());
+        assertEquals("[D][X] sth2(by: Dec 12 2020)", tl.get(2).toString());
     }
 }
