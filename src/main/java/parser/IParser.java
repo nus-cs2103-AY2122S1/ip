2f3009bac6ad5cffe9c0892e6227cdb2d1285284
radiankrisno <radiankrisno@gmail.com>
2021-08-25 15:39:23 +0800

    Add javadoc for all of the methods

--- src/main/java/parser/IParser.java
+++ src/main/java/parser/IParser.java
@@ -1,6 +1,21 @@
 package parser;
 
+/**
+ * Interface for the Parser object which parses command from the user.
+ */
 public interface IParser {
+    /**
+     * Parses the command entered by the user.
+     *
+     * @param line Command that is entered by the user.
+     * @return True if the command entered is not bye.
+     */
     boolean parseLine(String line);
+
+    /**
+     * Function that allows Parser to take the next command from the user.
+     *
+     * @return True if the command entered is not bye.
+     */
     boolean nextLine();
 }
