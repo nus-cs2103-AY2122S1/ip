cc20db969ab2c4148867160089e9d4628dbf8e3d
Ryan Peh <qwertz0808@gmail.com>
2021-08-25 17:46:49 +0800

    Add storing of date and time as dateTime

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,19 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String at;
+    protected final LocalDateTime at;
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm");
 
     public Event(String description, String at) {
         super(description);
-        this.at = at;
+        this.at = LocalDateTime.parse(at, formatter);
     }
 
     @Override
     public String toString() {
-        return "[E] " + super.toString() + " (at: " + at + ")";
+        return "[E] " + super.toString() + " (at: " + at.format(formatter) + ")";
     }
 }
