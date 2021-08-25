a6311b7eac4472bde0c5a50dd7f745d34de164b1
hanif-kamal <hanif.kamal47@gmail.com>
2021-08-25 20:10:20 +0800

    Swap expected and actual behaviour in test cases

--- src/test/java/duke/test/TodoTest.java
+++ src/test/java/duke/test/TodoTest.java
@@ -9,11 +9,11 @@ public class TodoTest {
     Todo todoComplete = new Todo("test 2", true);
     @Test
     public void toString_incompleteTask_success() {
-        assertEquals(todoIncomplete.toString(), "[T][] test 1");
+        assertEquals("[T][] test 1", todoIncomplete.toString());
     }
 
     @Test
     public void toString_completeTask_success() {
-        assertEquals(todoComplete.toString(), "[T][X] test 2");
+        assertEquals("[T][X] test 2", todoComplete.toString());
     }
 }
