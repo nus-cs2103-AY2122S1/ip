3708884720343b5ab460397bbc88a12df11dac5c
Ivan <77185900+wpinrui@users.noreply.github.com>
2021-08-25 14:55:28 +0800

    Add Parser class to interpret user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,62 +13,6 @@ public class Duke {
         this.storage = new Storage(filePath);
     }
 
-
-    public Command parseUserInput(String input) throws DukeException {
-        String[] strings = input.split(" ");
-        String operation = strings[0];
-        switch (operation) {
-        case "bye":
-        case "list":
-            return new Command(operation);
-        case "done":
-            if (strings.length == 1) {
-                throw new DukeException("You need to indicate which task number should be marked as done.");
-            }
-            try {
-                int doneTaskNum = Integer.parseInt(strings[1]);
-                return new Command(strings[0], doneTaskNum);
-            } catch (NumberFormatException e) {
-                throw new DukeException("The task to be marked as done should be indicated its list index.");
-            }
-        case "todo":
-            if (strings.length == 1) {
-                throw new DukeException("The description of a todo cannot be empty.");
-            }
-            return new Command(strings[0], input.substring(5));
-        case "deadline":
-            if (strings.length == 1) {
-                throw new DukeException("The description of a deadline cannot be empty.");
-            }
-            String[] descriptionAndBy = input.substring(9).split("/by ");
-            if (descriptionAndBy.length == 1) {
-                throw new DukeException("The done-by date of a deadline cannot be empty.");
-            }
-            return new Command(strings[0], descriptionAndBy[0], descriptionAndBy[1]);
-        case "event":
-            if (strings.length == 1) {
-                throw new DukeException("The description of an event cannot be empty.");
-            }
-            String[] descriptionAndAt = input.substring(6).split("/at ");
-            if (descriptionAndAt.length == 1) {
-                throw new DukeException("The timing of an event cannot be empty.");
-            }
-            return new Command(strings[0], descriptionAndAt[0], descriptionAndAt[1]);
-        case "delete":
-            if (strings.length == 1) {
-                throw new DukeException("You need to indicate which task number should be deleted.");
-            }
-            try {
-                int delTaskNum = Integer.parseInt(strings[1]);
-                return new Command(strings[0], delTaskNum);
-            } catch (NumberFormatException e) {
-                throw new DukeException("The task to be deleted should be indicated its list index.");
-            }
-        default:
-            throw new DukeException("Sorry, I do not understand this command.");
-        }
-    }
-
     public void run() {
         try {
             this.taskList = storage.readSave(taskList);
@@ -83,7 +27,7 @@ public class Duke {
             String userEntry = sc.nextLine();
             Command command;
             try {
-                command = parseUserInput(userEntry);
+                command = Parser.parseUserInput(userEntry);
             } catch (DukeException e) {
                 ui.print(e.toString());
                 continue;
