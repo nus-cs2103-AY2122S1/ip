cafccc1c4f22669bb06e8437aae8b00219541ae7
meixuanjin <jin.meixuan02@gmail.com>
2021-08-25 15:51:53 +0800

    Create class files

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -118,3 +118,34 @@ public class Duke {
         sc.close();
     }
 }
+
+// unable to create class file
+class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Returns the status of the task (X if done, blank if not done).
+     * @return the status of task
+     */
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    /**
+     * Marks a task as done.
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
