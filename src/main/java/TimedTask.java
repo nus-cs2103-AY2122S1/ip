d059a1e428c2a47931763aa364d2c0694a128c6e
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-08-25 20:36:03 +0800

    Rewrite constructors in Task classes
    Constructors has to be rewritten to accomadate changes to base Task class

--- src/main/java/TimedTask.java
+++ src/main/java/TimedTask.java
@@ -8,5 +8,10 @@ public abstract class TimedTask extends Task {
         this.time = LocalDate.parse(time);
     }
 
+    public TimedTask(String name, String time, boolean isDone) {
+        super(name, true, isDone);
+        this.time = LocalDate.parse(time);
+    }
+
     public abstract String format(DateTimeFormatter dtformatter);
 }
