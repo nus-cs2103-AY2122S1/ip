a29eddcae6c6acfe43f8d8f939b429c7c300a270
lingshanng <nglingshan456@gmail.com>
2021-08-25 15:16:24 +0800

    store and display deadline date and time as LocalDateTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,5 +1,9 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
 
     public Deadline(String description, String by) throws DukeException {
         super(description);
@@ -9,11 +13,21 @@ public class Deadline extends Task {
         if (by == "") {
             throw new DukeException("Looks like you forgot to include a deadline for the task.");
         }
-        this.by = by;
+        String inputPattern = "dd-MM-yyyy HH:mm";
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(inputPattern);
+            this.by = LocalDateTime.parse(by, formatter);
+        } catch (DateTimeParseException e) {
+            throw new DukeException("The deadline date is invalid. Please follow this format: " + inputPattern);
+        }
+    }
+    
+    private String getDateString() {
+        return by.format(DateTimeFormatter.ofPattern("MMM dd yyyy, hh:mm a"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + getDateString()  + ")";
     }
 }
