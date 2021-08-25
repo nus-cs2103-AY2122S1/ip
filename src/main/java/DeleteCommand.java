944fe0a3c46867ebc98ae3a10ca884401089d894
Imerbear <e0543864@u.nus.edu>
2021-08-25 23:01:01 +0800

    Add DeleteCommand class

---
+++ src/main/java/DeleteCommand.java
@@ -0,0 +1,15 @@
+public class DeleteCommand extends Command {
+    private int index;
+    
+    public DeleteCommand(TaskList taskList, int index) {
+        super(taskList);
+        this.index = index;
+    }
+
+    @Override
+    public void run() {
+        super.taskList.deleteTask(this.index);
+        Ui.printDontWorry();
+        Ui.printTasksLeft(super.taskList.size());
+    }
+}
