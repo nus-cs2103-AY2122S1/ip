a68f966fc73227d34777833769424f9057d21d3a
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-08-25 20:43:16 +0800

    Create new class for UserInterface

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    Scanner stdin;
+    public UserInterface() {
+        stdin = new Scanner(System.in);
+    }
+
+    public void displayGreeting() {
+        String logo = " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("----------------------");
+        System.out.println("Hello from\n" + logo);
+        System.out.println("----------------------");
+        System.out.println("What can I do for you?");
+    }
+
+    public void displayFarewell() {
+        System.out.println("----------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("----------------------");
+    }
+
+    public void print(String message) {
+        System.out.println(message);
+    }
+
+    public String getResponse() {
+        String response = "";
+        while (response.equals("")) {
+            response = stdin.nextLine();
+        }
+        return response;
+    }
+}
