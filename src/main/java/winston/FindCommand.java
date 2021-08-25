471fcb9cb8e1ce6efc126ed8979c9b047f6084b6
Imerbear <e0543864@u.nus.edu>
2021-08-26 02:59:30 +0800

    Add JavaDocs for FindCommand class

--- src/main/java/winston/FindCommand.java
+++ src/main/java/winston/FindCommand.java
@@ -2,12 +2,19 @@ package winston;
 
 public class FindCommand extends Command {
     private String str;
-    
+
+    /**
+     *  Represents an find command from the parent abstract class Command.
+     */
     public FindCommand(TaskList taskList, String str) {
         super(taskList);
         this.str = str;
     }
-    
+
+    /**
+     * Creates a new TaskList containing all tasks with given string as a substring and prints out
+     * a list containing all of these tasks
+     */
     @Override
     public void run() {
         TaskList tList = taskList.find(this.str);
