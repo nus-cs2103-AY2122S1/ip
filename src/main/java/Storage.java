eee745f68b9468eb6b3ef4443b598aa54780c8d0
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-08-25 22:08:35 +0800

    Add Storage loading from file

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Storage {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Storage {
+    private File file;
+    public Storage(Path filepath) {
+        file = filepath.toFile();
+    }
+
+    public TaskList load(UserInterface ui){
+        try {
+            Scanner fileReader = new Scanner(file);
+            TaskList taskList = new TaskList();
+            while (fileReader.hasNext()) {
+                String row = fileReader.nextLine();
+                String[] tokens = row.split(",");
+                if (tokens[0].equals("todo")) {
+                    Task task = new ToDo(tokens[1]);
+                    taskList.add(task);
+                } else if (tokens[0].equals("event")) {
+                    Task task = new Event(tokens[1], tokens[2]);
+                    taskList.add(task);
+                } else if (tokens[0].equals("deadline")) {
+                    Task task = new Deadline(tokens[1], tokens[2]);
+                    taskList.add(task);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            ui.displayError("File could not be found");
+        }
+    }
 }
