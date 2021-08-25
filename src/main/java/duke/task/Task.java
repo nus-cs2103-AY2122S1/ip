5d3cc1de5cdea7c7a7e56cea34de7843079457c3
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-08-25 16:57:08 +0800

    Add Documentation for Task and its subclasses

--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -1,9 +1,17 @@
 package duke.task;
 
+/**
+ * Abstract class representing Task that can be added to the TaskList.
+ */
 public abstract class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Class constructor which receives a description describing the Task, initializes with isDone as false.
+     *
+     * @param description Description of the Task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
@@ -13,15 +21,28 @@ public abstract class Task {
         return (isDone ? "X" : " ");
     }
 
+    /**
+     * Method which marks Task as done by changing the isDone field to true.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Method which returns a String with the description of the Task and whether it has been done.
+     *
+     * @return String representation of the Task.
+     */
     @Override
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.description;
     }
 
+    /**
+     * Method which returns a String with the representation of the Task to be written to the data file.
+     *
+     * @return String representation of the Task for the data file.
+     */
     public String toFile() {
         return "| " + (this.isDone ? 1 : 0) + " | " + this.description;
     }
