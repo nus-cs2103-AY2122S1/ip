867cc105b19d94fe3b2cd3e3c420cd7d7c7ab78d
Imerbear <e0543864@u.nus.edu>
2021-08-25 23:00:29 +0800

    Add AddEventCommand class

---
+++ src/main/java/AddEventCommand.java
@@ -0,0 +1,17 @@
+
+public class AddEventCommand extends Command{
+    private String dateAndTime;
+    private String description;
+    
+    public AddEventCommand(TaskList taskList, String description, String dateAndTime) {
+        super(taskList);
+        this.description = description;
+        this.dateAndTime = dateAndTime;
+    }
+    
+    @Override
+    public void run() {
+        super.taskList.addTask(new Event(this.description, this.dateAndTime));
+        Ui.printTasksLeft(taskList.size());
+    }
+}
