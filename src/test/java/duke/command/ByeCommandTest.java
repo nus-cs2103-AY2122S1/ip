df74c2570f37e18c14595d3768478696ed266249
bingcheng45 <bingcheng45@gmail.com>
2021-08-25 19:58:58 +0800

    Add JUnit test for command and task class

---
+++ src/test/java/duke/command/ByeCommandTest.java
@@ -0,0 +1,12 @@
+package duke.command;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ByeCommandTest {
+    @Test
+    public void dummyTest2(){
+        assertEquals(3,3);
+    }
+}
