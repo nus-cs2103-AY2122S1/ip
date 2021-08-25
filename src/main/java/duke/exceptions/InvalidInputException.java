3d8d4efa28a9b1c7cbaeb53422f508099f2daeb5
xnajasho <iifourxd@gmail.com>
2021-08-26 00:06:22 +0800

    Add JavaDoc Comments for all Exceptions

--- src/main/java/duke/exceptions/InvalidInputException.java
+++ src/main/java/duke/exceptions/InvalidInputException.java
@@ -1,6 +1,13 @@
 package duke.exceptions;
 
+/**
+ * Signals that there are some inputs which were given and Duke is not able to comprehend them
+ */
 public class InvalidInputException extends DukeException {
+
+    /**
+     * @param errorMessage will indicate the type of invalid input given
+     */
     public InvalidInputException(String errorMessage) {
         super(errorMessage);
     }
