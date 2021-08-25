9db48916b3150018fe277acea1c822b7bf81b323
Jonathan Tan Xin Jie <e0543805@u.nus.edu>
2021-08-25 22:08:39 +0800

    Rename Test class and methods

--- src/test/TasklistTest.java
+++ src/test/TasklistTest.java
@@ -9,24 +9,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Test class
  */
-public class Testest {
+public class TasklistTest {
+
     @Test
-    public void Duketest1(){
+    public void deadlineString_homeworkDetails_success(){
         Tasklist.Deadline testDeadline = new Tasklist.Deadline("homework", LocalDateTime.of(2021,
                 8,24,0,0));
         assertEquals(testDeadline.toString(), "[D][ ] homework (by: AUG 24 2021 @ 0000)");
     }
     @Test
-    public void Duketest2(){
+    public void eventString_napDetails_success(){
         Tasklist.Event testEvent = new Tasklist.Event("nap", LocalDateTime.of(2021,7,12,
                 15,0));
         testEvent.markDone();
         assertEquals(testEvent.toString(), "[E][X] nap (at: JUL 12 2021 @ 1500)");
     }
     @Test
-    public void Duketest3() {
+    public void todoString_blinkDetails_success() {
         Tasklist.Todo testDoto = new Tasklist.Todo("blink");
         testDoto.markDone();
         assertEquals(testDoto.toString(), "[T][X] blink");
-    }// AHH I give up idk how to test souts.
+    }// not sure how to test souts
 }
