74f90adc94458528f4e1e2d0df8def8c87e3f211
Imerbear <e0543864@u.nus.edu>
2021-08-25 23:01:28 +0800

    Add Parser class

---
+++ src/main/java/Parser.java
@@ -0,0 +1,65 @@
+import java.time.format.DateTimeParseException;
+import java.util.InputMismatchException;
+
+public class Parser {
+    private TaskList taskList;
+    
+    public Parser(TaskList taskList) {
+        this.taskList = taskList;
+    }
+    
+    public Command parse(String str) {
+        String[] splitBySpace = str.split(" ");
+        String cmd = splitBySpace[0];
+        if (cmd.equals("bye")) {
+            return new TerminateCommand(this.taskList);
+        } else if (cmd.equals("list")) {
+            return new ShowListCommand(this.taskList);
+        } else if (cmd.equals("done")) {
+            try {
+                return new DoneCommand(this.taskList, Integer.parseInt(splitBySpace[1]));
+            } catch (InputMismatchException | IndexOutOfBoundsException e) {
+                return new InvalidCommand(taskList);
+            }
+        } else if (cmd.equals("delete")) {
+            try {
+                return new DeleteCommand(taskList, Integer.parseInt(splitBySpace[1]));
+            } catch (InputMismatchException | IndexOutOfBoundsException e) {
+                return new InvalidCommand(taskList);
+            }
+        } else if (cmd.equals("todo")) {
+            try {
+                return new AddTodoCommand(this.taskList, str.substring(5));
+            } catch (InputMismatchException | IndexOutOfBoundsException e) {
+                return new InvalidCommand(taskList);
+            }
+        } else if (cmd.equals("deadline")) {
+            try {
+                int endIndex = str.indexOf("/by ");
+                String date = str.substring(endIndex + 4);
+                String task = str.substring(str.indexOf(" ") + 1, endIndex);
+                return new AddDeadlineCommand(this.taskList, task, date);
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid date format. Please give a valid date format. E.g 2021-12-12");
+                return new InvalidCommand(taskList);
+            } catch (InputMismatchException | IndexOutOfBoundsException e) {
+                return new InvalidCommand(taskList);
+            }
+
+        } else if (cmd.equals("event")) {
+            try {
+                int endIndex = str.indexOf("/at ");
+                String date = str.substring(endIndex + 4);
+                String task = str.substring(str.indexOf(" ") + 1, endIndex);
+                return new AddEventCommand(this.taskList, task, date);
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid date format. Please give a valid date format. E.g 2021-12-12");
+            } catch (InputMismatchException | IndexOutOfBoundsException e) {
+                return new InvalidCommand(taskList);
+            }
+            return new InvalidCommand(taskList);
+        } else {
+            return new InvalidCommand(taskList);
+        }
+    }
+}
