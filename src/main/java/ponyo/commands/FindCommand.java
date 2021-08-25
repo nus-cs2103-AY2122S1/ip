58a38583e6e6cdd7cc9881dcc8ed5f6f4e2ea548
wanninglim <e0556585@u.nus.edu>
2021-08-25 15:23:31 +0800

    Clean up code to match previous changes

--- src/main/java/ponyo/commands/FindCommand.java
+++ src/main/java/ponyo/commands/FindCommand.java
@@ -18,7 +18,7 @@ public class FindCommand extends Command {
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         ArrayList<Task> taskClone = new ArrayList<>();
 
-        for (int i = 1; i <= tasks.size(); i++) {
+        for (int i = 0; i < tasks.size(); i++) {
             if (tasks.retrieveTask(i).containsKeyword(keywordToFind)) {
                 taskClone.add(tasks.retrieveTask(i));
             }
@@ -26,13 +26,12 @@ public class FindCommand extends Command {
 
         if (taskClone.size() != 0) {
             ui.show("\tHere are the matching tasks in your list: ");
-            for (int i = 1; i <= taskClone.size(); i++) {
-                ui.show("\t" + i + "." + taskClone.get(i - 1));
+            for (int i = 0; i < taskClone.size(); i++) {
+                ui.show("\t" + (i + 1) + "." + taskClone.get(i));
             }
         } else {
             ui.show("There are no matching tasks in your list.");
         }
-
     }
 
     @Override
