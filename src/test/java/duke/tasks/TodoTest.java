9dd3eda9b9956f69b7cce6a9ffc439b53e867f29
Darren Heng <darren2pro@gmail.com>
2021-08-25 15:34:47 +0800

    Complete A-Jar

--- src/test/java/duke/tasks/TodoTest.java
+++ src/test/java/duke/tasks/TodoTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TodoTest {
     @Test
-    public void testSampleToDosPersistedData_toString_success(){
+    public void testSampleToDosPersistedData_toString_success() {
         ToDos sampleToDo = new ToDos("join sports club", true);
 
         assertEquals("T,1,join sports club", sampleToDo.persistedDataStringFormat(),
