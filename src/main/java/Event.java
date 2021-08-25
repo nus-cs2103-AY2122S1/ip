4a53ddc662e73e535249f8872eb1c2d842af7f42
sebbycake <leehur_sebastian@hotmail.com>
2021-08-25 14:43:59 +0800

    Change date from String to LocalDate objects, together with the input format

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,29 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String duration;
+    protected LocalDate duration;
+    protected LocalTime time;
 
-    public Event(String description, String duration) {
+    public Event(String description, String duration, String time) {
         super(description);
-        this.duration = duration;
+        this.duration = LocalDate.parse(duration);
+        this.time = LocalTime.parse(time);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + this.duration + ")";
+        return "[E]" + super.toString() + " (at: " + this.formatDate() + " " + this.formatTime() + ")";
+    }
+
+    public String formatDate() {
+        return this.duration.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
+
+    public String formatTime() {
+        return this.time.format(DateTimeFormatter.ofPattern("h:mm a"));
     }
 
 }
