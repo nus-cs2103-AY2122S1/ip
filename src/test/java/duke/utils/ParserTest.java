db2bc6cb1a3ea0e26e878cf8e0ceb03e8a73b6c2
Darren Heng <darren2pro@gmail.com>
2021-08-25 15:14:44 +0800

    Complete A-JUnit

--- src/test/java/duke/utils/ParserTest.java
+++ src/test/java/duke/utils/ParserTest.java
@@ -1,5 +1,112 @@
 package duke.utils;
 
+import duke.commands.Command;
+import duke.exceptions.DukeException;
+import duke.exceptions.EmptyTaskDescriptionException;
+import duke.exceptions.UnknownInputException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ParserTest {
+    @Test
+    public void testEmptyDescriptionError_forMultipleDifferentCommands_exceptionThrown() {
+        String cmdOne = "This is a random command that should throw an exception";
+        String cmdTwo = "deadline";
+        String cmdThree = "todo";
+        String cmdFour = "event";
+        String cmdFive = "sdjkhgkdjl";
+
+        try {
+            Parser.parse(cmdOne);
+            fail();
+        } catch (UnknownInputException e) {
+            assertEquals("Rasengan!! I don't know what that means :-(", e.getMessage());
+        } catch (EmptyTaskDescriptionException e1) {
+            fail();
+        }
+
+        try {
+            Parser.parse(cmdTwo);
+            fail();
+        } catch (EmptyTaskDescriptionException e) {
+            assertEquals("Rasengan!! The description of a deadline cannot be empty!", e.getMessage());
+        } catch (UnknownInputException e1) {
+            fail();
+        }
+
+        try {
+            Parser.parse(cmdThree);
+            fail();
+        } catch (EmptyTaskDescriptionException e) {
+            assertEquals("Rasengan!! The description of a todo cannot be empty!", e.getMessage());
+        } catch (UnknownInputException e1) {
+            fail();
+        }
+
+        try {
+            Parser.parse(cmdFour);
+            fail();
+        } catch (EmptyTaskDescriptionException e) {
+            assertEquals("Rasengan!! The description of a event cannot be empty!", e.getMessage());
+        } catch (UnknownInputException e1) {
+            fail();
+        }
+
+        try {
+            Parser.parse(cmdFive);
+            fail();
+        } catch (EmptyTaskDescriptionException e) {
+            fail();
+        } catch (UnknownInputException e1) {
+            assertEquals("Rasengan!! I don't know what that means :-(", e1.getMessage());
+        }
+
+    }
+
+    @Test
+    public void testCorrectCommandReturnedFromParser_success() {
+        String cmdOne = "todo read book";
+        String cmdTwo = "deadline return book /by 2021-10-11";
+        String cmdThree = "list";
+        String cmdFour = "bye";
+        String cmdFive = "event project meeting /at Aug 6th 2-4pm";
+
+        try {
+            Command c = Parser.parse(cmdOne);
+            assertTrue(c instanceof Command.ToDoCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+
+        try {
+            Command c = Parser.parse(cmdTwo);
+            assertTrue(c instanceof Command.DeadlineCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+
+        try {
+            Command c = Parser.parse(cmdThree);
+            assertTrue(c instanceof Command.ListAllCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+
+        try {
+            Command c = Parser.parse(cmdFour);
+            assertTrue(c instanceof Command.ExitCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+
+        try {
+            Command c = Parser.parse(cmdFive);
+            assertTrue(c instanceof Command.EventCommand);
+        } catch (DukeException e) {
+            fail();
+        }
+
+    }
 
 }
