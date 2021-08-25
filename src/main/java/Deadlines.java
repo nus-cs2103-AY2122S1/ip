28e6ab14b469d87a4ab3ff66d37390c3140931d1
Lee Yat Bun <benlee1300@gmail.com>
2021-08-25 16:20:31 +0800

    Implement data saving and loading

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,9 +1,23 @@
 public class Deadlines extends Task {
-    private String date;
+    private final String date;
 
     public Deadlines(String input) {
         super(input.split(" /by ", 2)[0].substring(9));
         this.date = input.split(" /by ", 2)[1];
+    }
+
+    public Deadlines(boolean completed, String name, String deadline) {
+        super(completed, name);
+        this.date = deadline;
+    }
+
+    @Override
+    public String getSaveData() {
+        if (this.isCompleted()) {
+            return String.format("D~1~%s~%s", this.getName(), this.date);
+        } else {
+            return String.format("D~0~%s~%s", this.getName(), this.date);
+        }
     }
 
     @Override
--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -6,6 +6,20 @@ public class Events extends Task {
         this.date = input.split(" /at ", 2)[1];
     }
 
+    public Events(boolean completed, String name, String deadline) {
+        super(completed, name);
+        this.date = deadline;
+    }
+
+    @Override
+    public String getSaveData() {
+        if (this.isCompleted()) {
+            return String.format("E~1~%s~%s", this.getName(), this.date);
+        } else {
+            return String.format("E~0~%s~%s", this.getName(), this.date);
+        }
+    }
+
     @Override
     public String toString() {
         return String.format("[E]%s (at: %s)", super.toString(), this.date);
