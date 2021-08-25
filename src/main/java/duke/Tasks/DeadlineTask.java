1e7cbc04e1fe2dfd1bca4ac5845fedeae8c60b46
TLChicken <tehlightningchicken@gmail.com>
2021-08-26 03:24:23 +0800

    Add LocalDate format for DeadlineTask
    DeadlineTask can now store 'byWhen' in LocalDate format
    when the input is in any of the following 3 formats:
    * DD/MM/YY
    * DD/MM/YYYY
    * DD Month YYYY
    When stored as a LocalDate, the list will print the
    dates out in the format Day Month Year.
    
    Otherwise it would just be stored as a String.

--- src/main/java/duke/Tasks/DeadlineTask.java
+++ src/main/java/duke/Tasks/DeadlineTask.java
@@ -1,5 +1,9 @@
 package duke.Tasks;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 /**
  *  This class encapsulates a Task element of Deadline type in Duke's TDList, and features
  *  various methods to manipulate the Task.
@@ -7,13 +11,98 @@ package duke.Tasks;
 
 public class DeadlineTask extends BaseTask {
 
-    String byWhen;
+
+    /** By default the 'by when' data will be stored in String form unless it is a date. */
+    private String byWhenStr;
+
+    /** If 'by when' data is given in date form, it will be stored here. */
+    private LocalDate byWhenDate;
+
+    /** Activates or deactivates Debug mode. */
+    private boolean debugMode = true;
+
 
     public DeadlineTask(String taskName, String byWhen) {
-        super(taskName);
-        this.byWhen = byWhen;
+        super(taskName, false);
+        this.parseByWhenInput(byWhen);
     }
 
+    /**
+     * Used for creating a new Deadline Task with ability to set all attributes.
+     *
+     * @param taskName the name or description of the task.
+     * @param byWhen describes when the task should be completed by.
+     * @param isCompleted true if task was already complete, false if incomplete.
+     */
+    public DeadlineTask(String taskName, String byWhen, boolean isCompleted) {
+        super(taskName, isCompleted);
+        this.parseByWhenInput(byWhen);
+    }
+
+    /**
+     * Stores the byWhen data of this Deadline Task, either in String form or LocalDate form.
+     * @param byWhen the time the task is upposed to be done by.
+     */
+    private void parseByWhenInput(String byWhen) {
+        try {
+            DateTimeFormatter usualStyleFormat = DateTimeFormatter.ofPattern("d/M/yy");
+            LocalDate byDate = LocalDate.parse(byWhen, usualStyleFormat);
+
+            this.byWhenDate = byDate;
+            this.byWhenStr = null;
+
+        } catch (DateTimeParseException e) {
+            debugPrint("Date Time Parse Exception!");
+
+            try {
+                // Try a different format instead. (4 digits for year)
+                DateTimeFormatter usualStyleFormat = DateTimeFormatter.ofPattern("d/M/yyyy");
+                LocalDate byDate = LocalDate.parse(byWhen, usualStyleFormat);
+
+                this.byWhenDate = byDate;
+                this.byWhenStr = null;
+            } catch (DateTimeParseException e2) {
+                debugPrint("DTE 2");
+
+                try {
+                    // Try a third format since the last one failed. (And this is the format stored in save file)
+                    DateTimeFormatter longDateFormat = DateTimeFormatter.ofPattern("d MMM uuuu");
+                    LocalDate byDate = LocalDate.parse(byWhen, longDateFormat);
+
+                    this.byWhenDate = byDate;
+                    this.byWhenStr = null;
+                } catch (DateTimeParseException e3) {
+                    debugPrint("DTE 3");
+                    this.byWhenStr = byWhen;
+                    this.byWhenDate = null;
+                }
+            }
+        }
+    }
+
+    /**
+     * Prints String if debug mode is activated.
+     * @param str the String to print.
+     */
+    private void debugPrint(String str) {
+        if (debugMode) {
+            System.out.println(str);
+        }
+    }
+
+    /**
+     * Gets the String representation of the by when.
+     * @return String representation of the LocalDate or just the plain String, whichever is relevant.
+     */
+    private String getByWhenStr() {
+        if (this.byWhenDate == null) {
+            return this.byWhenStr;
+        } else {
+            DateTimeFormatter longDateFormat = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+
+            return this.byWhenDate.format(longDateFormat);
+        }
+    }
 
     /**
      * Gets the type of the Task in enum form.
@@ -42,6 +131,17 @@ public class DeadlineTask extends BaseTask {
      */
     @Override
     public String getTaskDescription() {
-        return this.getTaskName() + "(by: " + this.byWhen + ")";
+        return this.getTaskName() + "(by: " + this.getByWhenStr() + ")";
+    }
+
+
+    /**
+     * Returns the extra info this task contains, which is the by when data.
+     *
+     * @return the String containing the extra info of this task if any. Returns null if there is none.
+     */
+    @Override
+    public String getTaskExtraInfo() {
+        return this.getByWhenStr();
     }
 }
