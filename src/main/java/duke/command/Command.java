4c47f4f13e5d9cfbc5192b2c5bbdb1ed8e5e5e00
wangpeialex <alexwangpeipei@gmail.com>
2021-08-25 22:52:24 +0800

    Add JavaDoc to Command class

--- src/main/java/duke/command/Command.java
+++ src/main/java/duke/command/Command.java
@@ -1,19 +1,29 @@
 package duke.command;
 
-import duke.exception.DukeException;
 import duke.util.Storage;
 import duke.util.TaskList;
 import duke.util.Ui;
 
+/**
+ * The abstract class that models a Command which can be called to execute.
+ */
 public abstract class Command {
     boolean isExit;
 
+    /**
+     * Constructor.
+     * By default is intialized as not to exit the main loop.
+     */
     public Command() {
         this.isExit = false;
     }
 
     public abstract void execute(TaskList tasks, Ui ui, Storage storage);
 
+    /**
+     * Returns a boolean indicating if the main loop should be broke.
+     * @return A boolean indicating if the main loop should be broke
+     */
     public boolean exeResult() {
         return isExit;
     }
