c0434ebf254eacf5a05e6d69b5e69369d42d7442
hanif-kamal <hanif.kamal47@gmail.com>
2021-08-26 00:12:50 +0800

    Restructure Parser class to use switch statements and string splitting

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -17,92 +17,122 @@ public class Parser {
     }
 
     public void parse(String input) throws DukeException {
-        if (input.equals("list")) {
-            list.printTasks();
-        } else if (input.startsWith("done")) {
-            try {
-                int listIndex = Integer.parseInt(input.substring(5));
-                if (listIndex <= list.size() && listIndex >= 1) {
-                    list.completeTask(listIndex);
-                } else {
-                    throw new DukeException("Couldn't find that task in the list! Try again.");
-                }
-            } catch (NumberFormatException e) {
-                throw new DukeException("Please make sure only a number follows the command 'done'. Try again.");
-            } catch (StringIndexOutOfBoundsException e) {
-                throw new DukeException("Please add a number after the command 'done'. Try again.");
+        String[] splitInput = input.split(" ", 2);
+        String first = splitInput[0];
+        if (splitInput.length == 1) {
+            switch (first) {
+            case ("list"):
+                list.printTasks();
+                break;
+            case ("done"):
+                throw new DukeException("Please ensure that there is a number after the command 'done'. Try again.");
+            case ("deadline"):
+                throw new DukeException("Please ensure that there is a task description after the command 'deadline' " +
+                        "and a deadline after '/by'. Try again.");
+            case ("event"):
+                throw new DukeException("Please ensure that there is a task description after the command 'deadline' " +
+                        "with a date and time after '/at'. Try again.");
+            case ("todo"):
+                throw new DukeException("Please ensure that there is a task description after the command 'todo'. " +
+                        "Try again.");
+            case ("delete"):
+                throw new DukeException("Please ensure that there is a number after the command 'delete'. Try again.");
+            default:
+                throw new DukeException("I didn't quite get what you meant. To add a task, begin with " +
+                        "'deadline', 'event' or 'todo'.");
             }
-        } else if (input.startsWith("deadline")) {
-            if (!input.contains("/by")) {
-                throw new DukeException("Please state the deadline for this task with /by! Try again.");
-            } else {
+        } else {
+            switch (first) {
+            case ("done"):
                 try {
-                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yy");
-                    int deadlineIndex = input.indexOf("/by") + 4;
-                    String deadlineString = input.substring(deadlineIndex);
-                    LocalDate deadline = LocalDate.parse(deadlineString, dtf);
-                    String firstTrimmed = input.substring(9);
-                    String lastTrimmed = firstTrimmed.substring(0, firstTrimmed.indexOf("/by") - 1);
-                    Deadline createdDeadlineTask = new Deadline(lastTrimmed, false, deadline);
-                    list.addToList(createdDeadlineTask);
-                    System.out.println("Got it. I've added this task:\n" + "  " + createdDeadlineTask + "\n"
-                            + "Now you have " + list.size() + " task" + (list.size() == 1 ? "" : "s") +
-                            " in the list.");
-                } catch (StringIndexOutOfBoundsException e) {
-                    throw new DukeException("Please ensure that there is a task description and deadline. " +
-                            "Try again.");
-                } catch (DateTimeParseException e) {
-                    throw new DukeException("Please ensure that your deadline is formatted in the following " +
-                            "way: DD/MM/YY");
+                    String listIndexString = splitInput[1];
+                    int listIndex = Integer.parseInt(listIndexString);
+                    if (listIndex <= list.size() && listIndex >= 1) {
+                        list.completeTask(listIndex);
+                        break;
+                    } else {
+                        throw new DukeException("Please ensure that a valid number follows the command 'done'. " +
+                                "Try again.");
+                    }
+                } catch (NumberFormatException e) {
+                    throw new DukeException("Please ensure only a number follows the command 'done'. Try again.");
                 }
-            }
-        } else if (input.startsWith("todo")) {
-            try {
-                String trimmed = input.substring(5);
-                Todo createdTodoTask = new Todo(trimmed, false);
+            case ("deadline"):
+                String restOfDeadline = splitInput[1];
+                if (!restOfDeadline.contains("/by")) {
+                    throw new DukeException("Please state the deadline for this task with /by! Try again.");
+                } else {
+                    try {
+                        String[] restOfDeadlineSplit = restOfDeadline.split(" /by ", 2);
+                        if (restOfDeadlineSplit.length < 2) {
+                            throw new DukeException("Please ensure that there is a task description and deadline. " +
+                                    "Try again");
+                        }
+                        String deadlineDescription = restOfDeadlineSplit[0];
+                        String deadlineDateString = restOfDeadlineSplit[1];
+                        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yy");
+                        LocalDate deadlineDate = LocalDate.parse(deadlineDateString, dtf);
+                        Deadline createdDeadlineTask = new Deadline(deadlineDescription, false, deadlineDate);
+                        list.addToList(createdDeadlineTask);
+                        System.out.println("Got it. I've added this task:\n" + "  " + createdDeadlineTask + "\n"
+                                + "Now you have " + list.size() + " task" + (list.size() == 1 ? "" : "s") +
+                                " in the list.");
+                        break;
+                    } catch (DateTimeParseException e) {
+                        throw new DukeException("Please ensure that your deadline is formatted in the following " +
+                                "way: DD/MM/YY");
+                    }
+                }
+            case ("event"):
+                String restOfEvent = splitInput[1];
+                if (!restOfEvent.contains("/at")) {
+                    throw new DukeException("Please state the date and time for this task with /at! Try again.");
+                } else {
+                    try {
+                        String[] restOfEventSplit = restOfEvent.split(" /at ", 2);
+                        if (restOfEventSplit.length < 2) {
+                            throw new DukeException("Please ensure that there is a task description with a date and " +
+                                    "time. Try again");
+                        }
+                        String eventDescription = restOfEventSplit[0];
+                        String eventDateTimeString = restOfEventSplit[1];
+                        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yy HHmm");
+                        LocalDateTime eventDateTime = LocalDateTime.parse(eventDateTimeString, dtf);
+                        Event createdEventTask = new Event(eventDescription, false, eventDateTime);
+                        list.addToList(createdEventTask);
+                        System.out.println("Got it. I've added this task:\n" + "  " + createdEventTask + "\n"
+                                + "Now you have " + list.size() + " task" + (list.size() == 1 ? "" : "s") +
+                                " in the list.");
+                        break;
+                    } catch (DateTimeParseException e) {
+                        throw new DukeException("Please ensure that your deadline is formatted in the following " +
+                                "way: DD/MM/YY");
+                    }
+                }
+            case ("todo"):
+                String todoDescription = splitInput[1];
+                Todo createdTodoTask = new Todo(todoDescription, false);
                 list.addToList(createdTodoTask);
                 System.out.println("Got it. I've added this task:\n" + "  " + createdTodoTask + "\n"
                         + "Now you have " + list.size() + " task" + (list.size() == 1 ? "" : "s") +
                         " in the list.");
-            } catch (StringIndexOutOfBoundsException e) {
-                throw new DukeException("Please add the task information. Try again.");
-            }
-        } else if (input.startsWith("event")) {
-            if (!input.contains("/at")) {
-                throw new DukeException("Please state when the event will be held with /at! Try again.");
-            } else {
+                break;
+            case ("delete"):
+                String toDeleteIndexString = splitInput[1];
                 try {
-                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yy HHmm");
-                    int atIndex = input.indexOf("/at") + 4;
-                    String atString = input.substring(atIndex);
-                    String firstTrimmed = input.substring(6);
-                    String lastTrimmed = firstTrimmed.substring(0, firstTrimmed.indexOf("/at") - 1);
-                    LocalDateTime at = LocalDateTime.parse(atString, dtf);
-                    Event createdEventTask = new Event(lastTrimmed, false, at);
-                    list.addToList(createdEventTask);
-                    System.out.println("Got it. I've added this task:\n" + "  " + createdEventTask + "\n"
-                            + "Now you have " + list.size() + " task" + (list.size() == 1 ? "" : "s") +
-                            " in the list.");
+                    int toDeleteIndex = Integer.parseInt(toDeleteIndexString);
+                    list.deleteTask(toDeleteIndex);
+                    break;
+                } catch (NumberFormatException e) {
+                    throw new DukeException("Please make sure only a number follows the command 'delete'. " +
+                            "Try again.");
                 } catch (StringIndexOutOfBoundsException e) {
-                    throw new DukeException("Please ensure that there is an event time. Try again.");
-                } catch (DateTimeParseException e) {
-                    throw new DukeException("Please ensure that your event is formatted in the following " +
-                            "way: DD/MM/YY HHmm (24 hr format)");
+                    throw new DukeException("Please add a number after the command 'delete'. Try again.");
                 }
+            default:
+                throw new DukeException("I didn't quite get what you meant. To add a task, begin with " +
+                        "'deadline', 'event' or 'todo'.");
             }
-        } else if (input.startsWith("delete")) {
-            try {
-                int toDeleteIndex = Integer.parseInt(input.substring(7));
-                list.deleteTask(toDeleteIndex);
-            } catch (NumberFormatException e) {
-                throw new DukeException("Please make sure only a number follows the command 'delete'. " +
-                        "Try again.");
-            } catch (StringIndexOutOfBoundsException e) {
-                throw new DukeException("Please add a number after the command 'delete'. Try again.");
-            }
-        } else {
-            throw new DukeException("I didn't quite get what you meant. To add a task, begin with " +
-                    "'deadline', 'event' or 'todo'.");
         }
     }
 }
