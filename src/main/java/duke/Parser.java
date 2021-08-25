5a4f45a6f6545d4f609c082978375d73cf5b2220
hanif-kamal <hanif.kamal47@gmail.com>
2021-08-25 20:19:51 +0800

    Change import statements

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -2,7 +2,6 @@ package duke;
 
 import duke.task.Deadline;
 import duke.task.Event;
-import duke.task.Task;
 import duke.task.Todo;
 
 import java.time.LocalDate;
--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -2,11 +2,14 @@ package duke;
 
 import duke.task.Deadline;
 import duke.task.Event;
-import duke.task.Task;
 import duke.task.Todo;
 
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
