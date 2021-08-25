0795c3685d141e34fb36577a652fa982d0b3ec3a
Nicolas <nicolas.chang99@gmail.com>
2021-08-25 20:33:07 +0800

    Add ToDo Test

---
+++ src/test/java/kermit/tasks/ToDoTest.java
@@ -0,0 +1,13 @@
+package kermit.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDoTest {
+    @Test
+    public void dummyTest(){
+        assertEquals(2, 2);
+    }
+}
+
