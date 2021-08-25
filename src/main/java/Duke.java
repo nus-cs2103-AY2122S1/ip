d874fa05a150cc346f5b17df3b16e7723ac72e19
sharmaine <sharmainec@u.nus.edu>
2021-08-25 22:49:39 +0800

    print formatting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,104 +3,98 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    private static final String indent = "       ";
+
+    private static void printMessage (String s1, String s2, String s3) {
+        System.out.println(indent + s1);
+        System.out.println(indent + s2);
+        System.out.println(indent + s3);
+    }
+
+    private static void printLine (String s) {
+        System.out.println(indent + s);
+    }
+
+    private static void printDivider () {
+        System.out.println("\n ------------------------------------------- \n");
+    }
+
     public static void main(String[] args) throws DukeException {
+        
         Scanner sc = new Scanner(System.in);
         List<Task> taskList = new ArrayList<>();
-
-        String divider = "\n ------------------------------------------- \n";
-        String indent = "       ";
-
-        System.out.println(indent + "Hello! I'm Viper :)");
-        System.out.println(indent + "How can I help you today?");
-        System.out.println(divider);
+        
+        printMessage("Hello! I'm Viper :)", "I love to help!", "How can I help you today?");
+        printDivider();
 
         String str = sc.nextLine();
 
         while (!str.trim().equalsIgnoreCase("bye")) {
-            //list out items in the list
-            System.out.println(divider);
+            printDivider();
+            
             if (str.trim().equalsIgnoreCase("list")) {
-                System.out.println(indent + "Here are the tasks in your list:");
+                printLine("Here are the tasks in your list:");
                 for (int i = 0; i < taskList.size(); i++) {
                     int currIndex = i + 1;
                     Task currTask = taskList.get(i);
-                    System.out.println(indent + currIndex + "." + currTask.toString());
+                    printLine(currIndex + "." + currTask.toString());
                 }
-            }
-            //mark numbered task as done
-            else if (str.toLowerCase().contains("done")) {
+            } else if (str.toLowerCase().contains("done")) {
                 int taskNo = Integer.parseInt(str.substring(4).trim()) - 1;
-                //check if taskNo exists:
                 if (taskNo < taskList.size()) {
                     Task currTask = taskList.get(taskNo);
                     currTask.isDone = true;
-                    System.out.println(indent + "Good job on completing your task!");
-                    System.out.println(indent + "I've marked this task as done:");
-                    System.out.println(indent + currTask.toString());
+                    printMessage("Good job on completing your task!", "I've marked this task as done:",
+                            currTask.toString());
+                } else {
+                    printLine("Sorry, this task does not exist. :(");
                 }
-                else {
-                    System.out.println(indent + "Sorry, this task does not exist. :(");
-                }
-            }
-            else if (str.toLowerCase().contains("delete")) {
+            } else if (str.toLowerCase().contains("delete")) {
                 int taskNo = Integer.parseInt(str.substring(6).trim()) - 1;
                 if (taskNo < taskList.size()) {
                     Task currTask = taskList.get(taskNo);
                     taskList.remove(taskNo);
-                    System.out.println(indent + "Ok!! I have removed the following task from your list:");
-                    System.out.println(indent + currTask.toString());
-                    System.out.println(indent + "Now you have " + taskList.size() + " task(s) left~");
+                    printMessage("Ok!! I have removed the following task from your list:", currTask.toString(),
+                            "Now you have " + taskList.size() + " task(s) left~ Yay!");
                 }
-            }
-            //for the various types of tasks available
-            //for todos:
-            else if (str.toLowerCase().contains("todo")) {
+            } else if (str.toLowerCase().contains("todo")) {
                 String desc = str.substring(4).trim();
                 Todos addTodo = new Todos(desc);
                 if (desc.isEmpty()) {
                     throw new DukeException("Oh no!! You cannot leave the description of a todo empty!!! ☹");
+                } else {
+                    taskList.add(addTodo);
+                    printMessage("Ok! I have added this task to your list:", "[T][ ] " + desc,
+                            "Now you have a total of " + taskList.size() + " task(s)!");
                 }
-                taskList.add(addTodo);
-                //print out msg:
-                System.out.println(indent + "Ok! I have added this task to your list: ");
-                System.out.println(indent + "[T][ ] " + desc);
-                System.out.println(indent + "Now you have a total of " + taskList.size() + " task(s)!");
-            }
-            //for deadline:
-            else if (str.toLowerCase().contains("deadline")) {
+            } else if (str.toLowerCase().contains("deadline")) {
                 String desc = str.substring(8).trim();
                 String[] stringParts = desc.split("/");
                 if (stringParts.length < 2) {
                     throw new DukeException("Oh no!! Something is missing from your description for the deadline!!! ☹");
+                } else {
+                    Deadlines addDeadline = new Deadlines(stringParts[0].trim(), stringParts[1].trim().substring(2).trim());
+                    taskList.add(addDeadline);
+                    printMessage("Oh no! A new deadline?! It's okay, you got this!", addDeadline.toString(),
+                            "Now you have a total of " + taskList.size() + " task(s)!");
                 }
-                Deadlines addDeadline = new Deadlines(stringParts[0].trim(), stringParts[1].trim().substring(2).trim());
-                taskList.add(addDeadline);
-                //print out msg:
-                System.out.println(indent + "Oh no! A new deadline?! It's okay, you got this!");
-                System.out.println(indent + "I have added this deadline to your list: ");
-                System.out.println(indent + addDeadline);
-                System.out.println(indent + "Now you have a total of " + taskList.size() + " task(s)!");
-            }
-            //for events:
-            else if (str.toLowerCase().contains("event")) {
+            } else if (str.toLowerCase().contains("event")) {
                 String desc = str.substring(6).trim();
                 String[] stringParts = desc.split("/");
                 if (stringParts.length < 2) {
                     throw new DukeException("Oh no!! Something is missing from your description for the event!!! ☹");
+                } else {
+                    Events addEvent = new Events(stringParts[0].trim(), stringParts[1].trim().substring(2).trim());
+                    taskList.add(addEvent);
+                    printMessage("Ok! I have added this event to your list:", addEvent.toString(),
+                            "Now you have a total of " + taskList.size() + " task(s)!");
                 }
-                Events addEvent = new Events(stringParts[0].trim(), stringParts[1].trim().substring(2).trim());
-                taskList.add(addEvent);
-                //print out msg:
-                System.out.println(indent + "Ok! I have added this event to your list: ");
-                System.out.println(indent + addEvent);
-                System.out.println(indent + "Now you have a total of " + taskList.size() + " task(s)!");
-            }
-            //every other type, which is invalid:
-            else {
+            } else {
                 throw new DukeException("Oops!! Sorry, I do not understand what you mean... ☹");
             }
             
-            System.out.println(divider);
+            printDivider();
+            
             String next = sc.nextLine();
             if (next.isEmpty()) {
                 break;
@@ -109,9 +103,8 @@ public class Duke {
                 str = next;
             }
         }
-        System.out.println(divider);
-        System.out.println(indent + "Bye! See you again~");
-        System.out.println(divider);
+        printDivider();
+        printLine("Bye! See you again~");
+        printDivider();
     }
-
 }
