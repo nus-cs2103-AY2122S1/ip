505686703ca70e58e99aa3a6fa37c87290f04c0c
Kar Wi <t.karwi@yahoo.com>
2021-08-25 14:02:15 +0800

    Implement Level 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,28 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate by;
 
     public Deadline(String description, String by) throws DukeException {
         super(description);
         if (by.isEmpty()) {
             throw new DukeException("☹ OOPS!!! The deadline cannot be empty.");
         }
-        this.by = by;
+        try {
+            this.by = LocalDate.parse(by);
+        } catch (DateTimeParseException e) {
+            throw new DukeException(
+                    "☹ OOPS!!! Seems like you have entered a wrong date format. " +
+                            "Try this instead: YYYY-MM-DD"
+            );
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by:" + by + ")";
+        return "[D]" + super.toString() + " (by:" + by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,17 +1,28 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
-    protected String at;
+    protected LocalDate at;
 
     public Event(String description, String at) throws DukeException {
         super(description);
         if (at.isEmpty()) {
             throw new DukeException("☹ OOPS!!! The date of an event cannot be empty.");
         }
-        this.at = at;
+        try {
+            this.at = LocalDate.parse(at);
+        } catch (DateTimeParseException e) {
+            throw new DukeException(
+                    "☹ OOPS!!! Seems like you have entered a wrong date format. " +
+                            "Try this instead: YYYY-MM-DD"
+            );
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at:" + at + ")";
+        return "[E]" + super.toString() + " (at:" + at.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
 
