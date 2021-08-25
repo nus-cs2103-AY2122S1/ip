2f33cb464894cea5bb2964deb6977b10b6e45317
trash-bin99 <77224811+trash-bin99@users.noreply.github.com>
2021-08-25 23:09:33 +0800

    Bugfix of DataParser class
    Change of parameter types in parse method from String to LocalDate.

--- src/main/java/DataParser.java
+++ src/main/java/DataParser.java
@@ -1,15 +1,17 @@
+import java.time.LocalDate;
+
 public class DataParser {
 
     public static Task readData(String str) {
         String[] splitArray = str.split("\\|");
         switch (splitArray[0]) {
         case "E":
-            Task event = new Events(splitArray[2], splitArray[3]);
+            Task event = new Events(splitArray[2], LocalDateParser.parse(splitArray[3]));
             if (splitArray[1].equals("1")) { event.setCompleted(); }
             return event;
             //no need for break, as function has terminated at the return statement
         case "D":
-            Task deadline = new Deadlines(splitArray[2], splitArray[3]);
+            Task deadline = new Deadlines(splitArray[2], LocalDateParser.parse(splitArray[3]));
             if (splitArray[1].equals("1")) { deadline.setCompleted(); }
             return deadline;
         //no need for break, as function has terminated at the return statement
