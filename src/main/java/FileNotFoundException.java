8ffd03868d8274fa1591f0bbdd06b9339d7f7bb3
HongshuW <hongshu.wang.uni@gmail.com>
2021-08-25 15:22:06 +0800

    Parser class: add parseCommandWithTime method

--- src/main/java/FileNotFoundException.java
+++
@@ -1,5 +0,0 @@
-public class FileNotFoundException extends DukeException {
-    public FileNotFoundException() {
-        super("☹ OOPS!!! The file cannot be found. A new file has been created, please try again!");
-    }
-}
