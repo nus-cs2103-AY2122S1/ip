aa23df2b34309d462ad0ffbfcf16ac565414dfc3
Nicolas <nicolas.chang99@gmail.com>
2021-08-25 10:04:17 +0800

    Remove thrown exception from overridden execute signature

--- src/main/java/ListTasks.java
+++ src/main/java/ListTasks.java
@@ -3,7 +3,7 @@ public class ListTasks extends Command {
     public ListTasks() {}
 
     @Override
-    void execute(ToDo taskList, Ui ui, Storage storage) throws KermitException {
+    void execute(ToDo taskList, Ui ui, Storage storage) {
         ui.showListItems(taskList);
     }
 
