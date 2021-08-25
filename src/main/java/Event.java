834915048a60b3a021726d7a2db7c7e244ae5596
erwinqxy <erwin.quek.xian.yi@gmail.com>
2021-08-25 16:52:04 +0800

    Add datetime attributes for Deadlines and Events

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,12 +1,19 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
     private String dateTime;
+    private final LocalDateTime dt;
 
     public Event(String description) {
         super(description.substring(0, description.indexOf("/at ")));
         this.dateTime = description.substring(description.indexOf("/at ") + 4);
+        this.dt = LocalDateTime.parse(this.dateTime);
     }
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + this.dateTime + ")";
+        String formatDate = dt.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+        String formatTime = dt.format(DateTimeFormatter.ofPattern("hh:mm"));
+        return "[E]" + super.toString() + "(at: " + formatDate +  ", " + formatTime + ")";
     }
 }
