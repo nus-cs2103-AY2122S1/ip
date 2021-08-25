a6d7e71acbd245f6a67901ca32dc12e1842416bc
tanruiquan <ruiquann@gmail.com>
2021-08-25 18:55:27 +0800

    Add Ui class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,98 +1,39 @@
-import java.util.Scanner;
-
 public class Duke {
 
-    private final String LINE = "____________________________________________________________";
-    private final Scanner sc = new Scanner(System.in);
-    private TaskList tasks;
     private final Storage storage;
+    private TaskList tasks;
+    private final Ui ui;
 
     public Duke(String filePath) {
-        storage = new Storage(filePath);
+        this.ui = new Ui();
+        this.storage = new Storage(filePath);
         try {
-            tasks = new TaskList(storage.load());
+            this.tasks = new TaskList(storage.load());
         } catch (DukeException e) {
-            System.out.println("Loading error: " + e.getMessage());
-            tasks = new TaskList();
+            ui.showLoadingError();
+            this.tasks = new TaskList();
         }
     }
 
-
-    public static void main(String[] args) {
-        new Duke("data/tasks.txt").run();
-    }
-
     public void run() {
-        greet();
+        ui.showWelcome();
         boolean isExit = false;
         while (!isExit) {
             try {
-                String input = sc.nextLine();
-                System.out.println(LINE);
+                String input = ui.readInput();
+                ui.showLine();
                 Command c = Parser.parse(input);
                 c.execute(tasks, storage);
                 isExit = c.isExit();
             } catch (DukeException e) {
-                System.out.println(e.getMessage());
+                ui.showError(e.getMessage());
             } finally {
-                System.out.println(LINE);
+                ui.showLine();
             }
         }
     }
 
-    private void greet() {
-        String logo = """
-                 ____        _       \s
-                |  _ \\ _   _| | _____\s
-                | | | | | | | |/ / _ \\
-                | |_| | |_| |   <  __/
-                |____/ \\__,_|_|\\_\\___|
-                """;
-        System.out.println(LINE);
-        System.out.println(logo);
-        System.out.println("Hi, I'm Duke, your Personal Assistant Chatbot\n" +
-                "What can I do for you today?");
-        System.out.println(LINE);
-    }
-
-    private void echo(String message) {
-        System.out.println(LINE);
-        System.out.println(message);
-        System.out.println(LINE);
-    }
-
-    private void exit() {
-        System.out.println(LINE);
-        System.out.println("Goodbye, hope to see you again soon!");
-        System.out.println(LINE);
-    }
-
-    private void add(Task task) throws DukeException {
-        tasks.addTask(task);
-        storage.save(tasks);
-        int len = tasks.size();
-        String message = String.format("Got it. I've added this task:\n  %s\nNow you have %d %s in the list.",
-                task.toString(),
-                len,
-                len <= 1 ? "task" : "tasks");
-        echo(message);
-    }
-
-    private void done(int index) throws DukeException {
-        Task task = tasks.markTaskAsDone(index);
-        storage.save(tasks);
-        String message = String.format("Nice! I've marked this task as done:\n  %s", task);
-        echo(message);
-    }
-
-    private void delete(int index) throws DukeException {
-        Task task = tasks.deleteTask(index);
-        storage.save(tasks);
-        int len = tasks.size();
-        String message = String.format("Noted. I've removed this task:\n  %s\nNow you have %d %s in the list.",
-                task.toString(),
-                len,
-                len <= 1 ? "task" : "tasks");
-        echo(message);
+    public static void main(String[] args) {
+        new Duke("data/tasks.txt").run();
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Ui {
+    private final String LINE = "____________________________________________________________";
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void showLoadingError() {
+        String message = "Oops, looks like there is a problem loading Duke";
+        System.out.println(message);
+    }
+
+    public void showWelcome() {
+        String logo = """
+                 ____        _       \s
+                |  _ \\ _   _| | _____\s
+                | | | | | | | |/ / _ \\
+                | |_| | |_| |   <  __/
+                |____/ \\__,_|_|\\_\\___|
+                """;
+        System.out.println(LINE);
+        System.out.println(logo);
+        System.out.println("Hi, I'm Duke, your Personal Assistant Chatbot\n" +
+                "What can I do for you today?");
+        System.out.println(LINE);
+    }
+
+    public void showLine() {
+        System.out.println(LINE);
+    }
+
+    public void showError(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public String readInput() {
+        return scanner.nextLine();
+    }
+}
