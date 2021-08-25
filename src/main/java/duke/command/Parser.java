e07590b79a00df6c143a4eb71dbc725365723ebd
lwj1711 <limweijie1711@yahoo.com.sg>
2021-08-26 00:57:34 +0800

    Complete Level-9

--- src/main/java/duke/command/Parser.java
+++ src/main/java/duke/command/Parser.java
@@ -14,20 +14,22 @@ public class Parser {
     /**
      * Takes in user input and performs an action accordingly.
      */
-    public String interpretCommand(String input) throws InvalidCommandException {
+    public String interpretCommand(String input) {
         if (input.equalsIgnoreCase("list")) {
             return "list";
-        } else if (input.toLowerCase().indexOf("done") != -1) {
+        } else if (input.length() >= 6 && input.substring(0, 4).toLowerCase().indexOf("done") != -1) {
             return "mark";
-        } else if (input.toLowerCase().indexOf("delete") != -1) {
+        } else if (input.length() >= 8 && input.substring(0, 6).toLowerCase().indexOf("delete") != -1) {
             return "delete";
-        } else if (input.toLowerCase().indexOf("todo") != -1) {
+        } else if (input.length() >= 6 && input.substring(0, 4).toLowerCase().indexOf("todo") != -1) {
             return "todo";
-        } else if (input.toLowerCase().indexOf("deadline") != -1) {
+        } else if (input.length() >= 10 && input.substring(0, 8).toLowerCase().indexOf("deadline") != -1) {
             return "deadline";
-        } else if (input.toLowerCase().indexOf("event") != -1) {
+        } else if (input.length() >= 7 && input.substring(0, 5).toLowerCase().indexOf("event") != -1) {
             return "event";
-        } else if (input.toLowerCase().equals("bye")) {
+        } else if (input.length() >= 6 && input.substring(0, 4).toLowerCase().indexOf("find") != -1) {
+            return "find";
+        } else if (input.length() >= 3 && input.substring(0, 3).toLowerCase().equals("bye")) {
             return "bye";
         } else {
             return "invalid";
