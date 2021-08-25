9b811816083e194288f8dfb3f50fae4341bceba4
itzblim <>
2021-08-25 13:33:49 +0800

    Add date parsing and formatting

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,20 +1,28 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 /**
  * Class for deadline tasks.
  */
 public class Deadline extends Task{
 
-    private final String by;
-
+    private final LocalDate byTime;
+    
     /**
      * Constructor for a deadline task.
      *
      * @param description String describing the task.
      * @param by Deadline string for the task.
+     * @throws DukeException Thrown if time could not be parsed.
      */
-    public Deadline(String description, String by) {
+    public Deadline(String description, String by) throws DukeException {
         super(description);
         this.category = TaskType.deadline;
-        this.by = by;
+        try {
+            this.byTime = LocalDate.parse(by);
+        } catch (DateTimeParseException e) {
+            throw new DukeException("Could not parse time input.");
+        }
     }
 
     /**
@@ -24,6 +32,7 @@ public class Deadline extends Task{
      */
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: "
+            + byTime.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
