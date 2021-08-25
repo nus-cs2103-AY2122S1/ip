e0beafba041f0068d15c8bc5a24b4c29e2a11890
Nicolas <nicolas.chang99@gmail.com>
2021-08-25 22:42:46 +0800

    Add javadocs to Task

--- src/main/java/kermit/tasks/Task.java
+++ src/main/java/kermit/tasks/Task.java
@@ -1,44 +1,70 @@
 package kermit.tasks;
 
 /**
- * Encapsulates a task to be completed
+ * Encapsulates a task to be completed.
  */
 public abstract class Task {
     private String description = "";
     private boolean isCompleted = false;
 
     /**
-     * kermit.command.Task constructor
+     * Task constructor.
      *
-     * @param description Name of task.
+     * @param description Description of task.
      */
     public Task(String description) {
         this.description = description;
     }
 
+    /**
+     * Task constructor.
+     *
+     * @param description Description of task.
+     * @param isCompleted Boolean to set if task is completed.
+     */
     public Task(String description, boolean isCompleted) {
         this.description = description;
         this.isCompleted = isCompleted;
     }
 
     /**
-     * Marks task as complete
+     * Marks task as complete.
      */
     public void markAsComplete() {
         this.isCompleted = true;
     }
 
+
+    /**
+     * Checks if task is complete.
+     *
+     * @return true if task is complete, false otherwise.
+     */
     public boolean isComplete() {
         return this.isCompleted;
     }
 
+    /**
+     * Gets description of task.
+     *
+     * @return Description string.
+     */
     public String getDescription() {
         return this.description;
     }
 
-    public abstract String getShortForm();
     /**
-     * @return String representation of task.
+     * Get abbreviation of task, usually first letter.
+     *
+     * @return String abbreviation of task.
+     */
+    public abstract String getShortForm();
+
+    /**
+     * Get string representation of a task including its short form and
+     * completion status
+     *
+     * @return Stringified task.
      */
     @Override
     public String toString() {
