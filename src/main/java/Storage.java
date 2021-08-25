1bdad7a90f7d03e2b317644dd047b776f805af20
Nicolas <nicolas.chang99@gmail.com>
2021-08-25 08:15:39 +0800

    Fix unable to read from existing data file bug

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,12 +1,11 @@
 import java.io.*;
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.Iterator;
 import java.util.Scanner;
 
 // todo
 // 1. Check existence of file and create it
-// 2. Read from file
-// 3. Save taskList to file
 
 public class Storage {
     private static final String fileErrorText = "Ribiit! Something was wrong with the file!";
@@ -14,46 +13,62 @@ public class Storage {
     private File toDoFile;
     private ToDo taskList;
 
-    public Storage(String filePath) throws FileNotFoundException {
+    public Storage(String filePath) throws IOException, KermitException {
         // Check if Kermit data exists, else create it
         this.toDoFile = new File(filePath);
         this.taskList = readToDoData(this.toDoFile);
     }
 
-    private ToDo readToDoData(File file) throws FileNotFoundException {
+    private ToDo readToDoData(File file) throws IOException, KermitException {
+        if (toDoFile.getParentFile() != null) {
+            toDoFile.getParentFile().mkdirs();
+        }
+
+        boolean didCreateFile = toDoFile.createNewFile();
         ToDo todo = new ToDo();
         String line;
         Task task;
         Scanner sc = new Scanner(file);
         // Read file line by line
-        while (sc.hasNextLine()) {
-            line = sc.nextLine();
-            String[] commands = line.split(" \\| ");
-            String taskShortForm = commands[0];
-            boolean isCompleted = commands[1].equals("1");
-            String description = commands[2];
-            LocalDate date = LocalDate.parse(commands[3]);
+        if (!didCreateFile) {
+            while (sc.hasNextLine()) {
+                line = sc.nextLine();
+                String[] commands = line.split(" \\| ");
+                String taskShortForm = commands[0];
+                boolean isCompleted = commands[1].equals("1");
+                String description = commands[2];
 
-            // Create task based on line data
-            switch (taskShortForm) {
-                case "T":
-                    task = new ToDos(description, isCompleted);
-                    todo.add(task);
-                    break;
-                case "D":
-                    task = new Deadline(description, date, isCompleted);
-                    todo.add(task);
-                    break;
-                case "E":
-                    task = new Event(description, date, isCompleted);
-                    todo.add(task);
-                    break;
+                // todo
+                LocalDate date = LocalDate.now();
+                if (taskShortForm.equals("E") || taskShortForm.equals("D")) {
+                    try {
+                        date = LocalDate.parse(commands[3]);
+                    } catch (DateTimeParseException e){
+                        throw new KermitException("Invalid date in data file!");
+                    }
+                }
+
+                // Create task based on line data
+                switch (taskShortForm) {
+                    case "T":
+                        task = new ToDos(description, isCompleted);
+                        todo.add(task);
+                        break;
+                    case "D":
+                        task = new Deadline(description, date, isCompleted);
+                        todo.add(task);
+                        break;
+                    case "E":
+                        task = new Event(description, date, isCompleted);
+                        todo.add(task);
+                        break;
+                }
             }
         }
         return todo;
     }
 
-    private static String formatWriteString(Task task) {
+    private String formatWriteString(Task task) {
         String delimiter = " | ";
 
         String taskComplete = task.isComplete() ? "1" : "0";
@@ -77,7 +92,7 @@ public class Storage {
             BufferedWriter bw = new BufferedWriter(fw);
             Iterator<Task> taskIter = taskList.iterator();
             while (taskIter.hasNext()) {
-                Task task = (Task) taskIter.next();
+                Task task = taskIter.next();
                 String taskData = formatWriteString(task);
                 bw.write(taskData);
                 if (taskIter.hasNext()) {
