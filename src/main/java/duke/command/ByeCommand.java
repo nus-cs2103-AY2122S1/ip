f37e7ccec2b1ad62291b2fad7736fa97a5067f42
JoelChanZhiYang <joelchanzhiyang@gmail.com>
2021-08-25 22:26:38 +0800

    Tweaked ByeCommand to fit coding standard

--- src/main/java/duke/command/ByeCommand.java
+++ src/main/java/duke/command/ByeCommand.java
@@ -1,8 +1,9 @@
 package duke.command;
 
+import duke.util.Storage;
 import duke.util.TaskList;
 import duke.util.Ui;
-import duke.util.Storage;
+
 
 
 public class ByeCommand extends Command {
@@ -11,7 +12,7 @@ public class ByeCommand extends Command {
     }
 
     @Override
-    public void execute(TaskList tasks, Ui ui, Storage storage){
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
         ui.showBye();
     }
 }
