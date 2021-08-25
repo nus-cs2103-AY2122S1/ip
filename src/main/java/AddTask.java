e4a03506dd97939f98d537bbfffd7f9dcb5b7861
Nicolas <nicolas.chang99@gmail.com>
2021-08-25 16:55:26 +0800

    Fix date parsing

--- src/main/java/AddTask.java
+++ src/main/java/AddTask.java
@@ -4,16 +4,30 @@ import java.time.format.DateTimeParseException;
 public class AddTask extends Command {
     Task task;
 
+    // parses dates in form dd-mm-yyyy to localdate
+    private static LocalDate parseDate(String dateString) throws KermitException{
+        String[] components = dateString.split("-");
+        try {
+            String day = components[0];
+            String month = components[1];
+            String year = components[2];
+            LocalDate parsedDate = LocalDate.parse(String.join("-", year, month, day));
+            return parsedDate;
+        } catch (IndexOutOfBoundsException | DateTimeParseException e) {
+            throw new KermitException("That is an invalid date!");
+        }
+    }
+
     public AddTask(String taskType, String description, String flag) throws KermitException {
         if (description.equals("")) {
-            throw new KermitException("The argument cannot be blank!");
+            throw new KermitException("The argument of the " + taskType + " command cannot be empty!");
         }
 
         if (taskType.equals("todo")) {
             this.task = new ToDos(description);
         } else {
             try {
-                LocalDate date = LocalDate.parse(flag);
+                LocalDate date = parseDate(flag);
                 switch (taskType) {
                     case "deadline":
                         task = new Deadline(description, date);
