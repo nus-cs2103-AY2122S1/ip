75faace00029fe0a55b32bba98c3ccf2020a3483
Allard <AllardQuek@users.noreply.github.com>
2021-08-26 01:31:48 +0800

    Add Ui component to deal with user interactions

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,3 +1,27 @@
 public class Ui {
+	private final static String LINE = "\t----------------------------------------------------\n";
 	
+    /**
+     * Print a welcome message to the user.
+     */
+    protected void greetUser() {
+        String LOGO = " ____        _        \n"
+                                     + "|  _ \\ _   _| | _____ \n"
+                                     + "| | | | | | | |/ / _ \\\n"
+                                     + "| |_| | |_| |   <  __/\n"
+                                     + "|____/ \\__,_|_|\\_\\___|\n";
+        String GREETING = "Greetings friend! I am your personal assistant,\n" 
+                            + LOGO 
+                            + "\nWhat can I do for you?\n";
+        System.out.println(GREETING);
+    }
+
+    /**
+     * Prints a message with a line of dashes before and after it.
+     * 
+     * @param message The message to print within 2 lines.
+     */
+    protected static void printFormattedMessage(String message) {
+        System.out.println(LINE + "\t" + message + LINE);
+    }
 }
