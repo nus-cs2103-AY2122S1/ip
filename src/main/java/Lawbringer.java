1719b7ae7e8df23748803bd60d1b85dc3203db9b
Tetrerox <tetrerox@gmail.com>
2021-08-25 22:39:25 +0800

    Add Storage.java and abstract it from Lawbringer.java

--- src/main/java/Lawbringer.java
+++ src/main/java/Lawbringer.java
@@ -1,18 +1,25 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.FileWriter;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Lawbringer {
-    public static void main(String[] args) throws IOException {
-        List<Task> tasks = Lawbringer.createTaskList();
 
+    private final Storage storage;
+
+    public Lawbringer(String filePath) {
+        storage = new Storage(filePath);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Lawbringer("src/main/java/data/Lawbringer.txt").run();
+    }
+
+    public void run() throws IOException {
+        List<Task> tasks = storage.createTaskList();
         String introMessage = "Hello! I'm Lawbringer!\n" +
                 "What can i do for you?";
         System.out.println(introMessage);
@@ -32,9 +39,9 @@ public class Lawbringer {
                     }
                 } else if (userInput.startsWith("done")) {
                     int index = Integer.parseInt(userInput.substring(5)) - 1;
-                    Lawbringer.deleteFromFile(tasks.get(index).toString());
+                    storage.deleteFromFile(tasks.get(index).toString());
                     tasks.get(index).markAsDone();
-                    Lawbringer.appendToFile(tasks.get(index).toString());
+                    storage.appendToFile(tasks.get(index).toString());
                     Task task = tasks.get(index);
                     String doneMessage = "Nice! I've marked this task as done:\n" +
                             "  " + task.toString();
@@ -42,10 +49,10 @@ public class Lawbringer {
                 } else if (userInput.startsWith("delete")) {
                     int index = Integer.parseInt(userInput.substring(7)) - 1;
                     Task task = tasks.get(index);
-                    Lawbringer.deleteFromFile(task.toString());
+                    storage.deleteFromFile(task.toString());
                     tasks.remove(index);
                     String doneMessage = "Noted. I've removed this task:\n" +
-                            "  " + task.toString() + "\nNow you have " + tasks.size() +
+                            "  " + task + "\nNow you have " + tasks.size() +
                             " task(s) in the list.";
                     System.out.println(doneMessage);
                 } else if (userInput.startsWith("todo")) {
@@ -63,7 +70,7 @@ public class Lawbringer {
                             todo + "\nNow you have " + tasks.size() +
                             " task(s) in the list.";
                     System.out.println(message);
-                    Lawbringer.appendToFile(todo.toString());
+                    storage.appendToFile(todo.toString());
                 } else if (userInput.startsWith("deadline")) {
                     int index = userInput.indexOf('/');
                     String by = userInput.substring(index+4);
@@ -75,7 +82,7 @@ public class Lawbringer {
                             deadline + "\nNow you have " + tasks.size() +
                             " task(s) in the list.";
                     System.out.println(message);
-                    Lawbringer.appendToFile(deadline.toString());
+                    storage.appendToFile(deadline.toString());
                 } else if (userInput.startsWith("event")) {
                     int index = userInput.indexOf('/');
                     String at = userInput.substring(index+4);
@@ -85,7 +92,7 @@ public class Lawbringer {
                             event + "\nNow you have " + tasks.size() +
                             " task(s) in the list.";
                     System.out.println(message);
-                    Lawbringer.appendToFile(event.toString());
+                    storage.appendToFile(event.toString());
                 } else {
                     throw new LawbringerException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
@@ -93,71 +100,5 @@ public class Lawbringer {
                 System.out.println(e.toString());
             }
         }
-    }
-
-    private static List<Task> createTaskList() throws IOException {
-        List<Task> tasks = new ArrayList<>();
-        // Creates an arrayList of tasks from filepath
-        File file = new File("src/main/java/data/Lawbringer.txt");
-        if (!file.exists()) {
-            file.getParentFile().mkdirs();
-            file.createNewFile();
-        }
-        Scanner s = new Scanner(file);
-        while (s.hasNext()) {
-            String line = s.nextLine();
-            char taskType = line.charAt(1);
-            boolean isDone = line.charAt(4) == 'X';
-            switch (taskType) {
-                case 'T':
-                    ToDo todo = new ToDo(line.substring(7));
-                    if (isDone) {
-                        todo.markAsDone();
-                    }
-                    tasks.add(todo);
-                    break;
-                case 'D':
-                    int byIndex = line.indexOf(" (by: ");
-                    Deadline deadline = new Deadline(line.substring(7,byIndex),
-                            line.substring(byIndex + 6, line.length() - 1));
-                    if (isDone) {
-                        deadline.markAsDone();
-                    }
-                    tasks.add(deadline);
-                    break;
-                case 'E':
-                    int atIndex = line.indexOf(" (at: ");
-                    Event event = new Event(line.substring(7,atIndex),
-                            line.substring(atIndex + 6, line.length() - 1));
-                    if (isDone) {
-                        event.markAsDone();
-                    }
-                    tasks.add(event);
-                    break;
-            }
-        }
-        return tasks;
-    }
-
-    private static void appendToFile(String textToAppend) throws IOException {
-        FileWriter fw = new FileWriter("src/main/java/data/Lawbringer.txt", true);
-        fw.write(textToAppend + "\n");
-        fw.close();
-    }
-
-    private static void deleteFromFile(String textToDelete) throws IOException {
-        File file = new File("src/main/java/data/Lawbringer.txt");
-        Scanner s = new Scanner(file);
-        String newFileText = "";
-        while (s.hasNext()) {
-            String line = s.nextLine();
-            if (textToDelete.equals(line)) {
-                continue;
-            }
-            newFileText += line + "\n";
-        }
-        FileWriter fw = new FileWriter("src/main/java/data/Lawbringer.txt");
-        fw.write(newFileText);
-        fw.close();
     }
 }
---
+++ src/main/java/Storage.java
@@ -0,0 +1,81 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Storage {
+    private final String filePath;
+
+    public Storage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    protected void appendToFile(String textToAppend) throws IOException {
+        FileWriter fw = new FileWriter(filePath, true);
+        fw.write(textToAppend + "\n");
+        fw.close();
+    }
+
+    protected void deleteFromFile(String textToDelete) throws IOException {
+        File file = new File(filePath);
+        Scanner s = new Scanner(file);
+        String newFileText = "";
+        while (s.hasNext()) {
+            String line = s.nextLine();
+            if (textToDelete.equals(line)) {
+                continue;
+            }
+            newFileText += line + "\n";
+        }
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(newFileText);
+        fw.close();
+    }
+
+    protected List<Task> createTaskList() throws IOException {
+        List<Task> tasks = new ArrayList<>();
+        // Creates an arrayList of tasks from filepath
+        File file = new File(filePath);
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+        }
+        Scanner s = new Scanner(file);
+        while (s.hasNext()) {
+            String line = s.nextLine();
+            char taskType = line.charAt(1);
+            boolean isDone = line.charAt(4) == 'X';
+            switch (taskType) {
+                case 'T':
+                    ToDo todo = new ToDo(line.substring(7));
+                    if (isDone) {
+                        todo.markAsDone();
+                    }
+                    tasks.add(todo);
+                    break;
+                case 'D':
+                    int byIndex = line.indexOf(" (by: ");
+                    Deadline deadline = new Deadline(line.substring(7,byIndex),
+                            line.substring(byIndex + 6, line.length() - 1));
+                    if (isDone) {
+                        deadline.markAsDone();
+                    }
+                    tasks.add(deadline);
+                    break;
+                case 'E':
+                    int atIndex = line.indexOf(" (at: ");
+                    Event event = new Event(line.substring(7,atIndex),
+                            line.substring(atIndex + 6, line.length() - 1));
+                    if (isDone) {
+                        event.markAsDone();
+                    }
+                    tasks.add(event);
+                    break;
+            }
+        }
+        return tasks;
+    }
+}
