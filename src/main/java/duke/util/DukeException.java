d7fb725e84c3f5a07bd28afb2ea608ecbf5ff0b7
Kevin Chua <skullspine9@gmail.com>
2021-08-26 15:32:13 +0800

    Follow the Coding Standard From this point onward, ensure any new code added are compliant with the given coding standard.

--- src/main/java/duke/util/DukeException.java
+++ src/main/java/duke/util/DukeException.java
@@ -1,7 +1,7 @@
 package duke.util;
 
 public class DukeException extends Exception {
-    public  DukeException(String error) {
+    public DukeException(String error) {
         super(error);
     }
 }
