119318085a322ce0c0d9427e7152e969480bbe5c
LMAOboxhack <linfangyuan99@gmail.com>
2021-08-26 13:36:06 +0800

    Update code to match coding standard

--- src/main/java/duke/tasklist/TaskList.java
+++ src/main/java/duke/tasklist/TaskList.java
@@ -9,50 +9,50 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class TaskList implements Serializable {
-    private List<Task> taskList;
+    private final List<Task> tasks;
 
-    public TaskList(List<Task> taskList) {
-        this.taskList = taskList;
+    public TaskList(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     public TaskList() {
-        this.taskList = new ArrayList<>();
+        this.tasks = new ArrayList<>();
     }
 
     public String listTasks() {
-        if (taskList.isEmpty()) {
+        if (tasks.isEmpty()) {
             return getTaskCountDesc();
         }
         StringBuilder sb = new StringBuilder();
         sb.append("Here are the tasks in your list:");
-        for (int i = 0; i < taskList.size(); i++) {
-            sb.append(String.format("\n%d.%s", i + 1, taskList.get(i).toString()));
+        for (int i = 0; i < tasks.size(); i++) {
+            sb.append(String.format("\n%d.%s", i + 1, tasks.get(i).toString()));
         }
         return sb.toString();
     }
 
     public String getTaskCountDesc() {
-        return taskList.isEmpty() ? "There are no tasks."
-                : String.format("Now you have %d task%s in the list.", taskList.size(),
-                (taskList.size() == 1 ? "" : "s"));
+        return tasks.isEmpty() ? "There are no tasks."
+                : String.format("Now you have %d task%s in the list.", tasks.size(),
+                (tasks.size() == 1 ? "" : "s"));
     }
 
     public void add(Task taskToAdd) {
-        taskList.add(taskToAdd);
+        tasks.add(taskToAdd);
     }
 
     public Task remove(int indexToDelete) {
-        return taskList.remove(indexToDelete);
+        return tasks.remove(indexToDelete);
     }
 
     public Task markAsDone(int indexToMarkDone) {
-        Task task = taskList.get(indexToMarkDone);
+        Task task = tasks.get(indexToMarkDone);
         task.markAsDone();
         return task;
     }
 
     public TaskList filterByDateTimeOrString(LocalDateTimeOrString dateTimeOrString) {
-        List<Task> filtered = taskList.stream().filter(x -> x.isAtTime(dateTimeOrString)).collect(Collectors.toList());
+        List<Task> filtered = tasks.stream().filter(x -> x.isAtTime(dateTimeOrString)).collect(Collectors.toList());
         return new TaskList(filtered);
     }
 }
