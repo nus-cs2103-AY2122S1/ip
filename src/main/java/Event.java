3b68434d2e70a4fc56321eea21d982f3f8c5df5b
Wilfredwongkc <Wilfredwongkc@gmail.com>
2021-08-26 16:46:24 +0800

    Add date and times feature

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,23 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String by;
+    protected LocalDate date;
 
     public Event(String description, String by) {
         super(description);
-        this.by = by;
+        if (by.contains("/")) {
+            date = LocalDate.parse(by.split("/")[2] + "-" + by.split("/")[1] + "-" +
+                    (Integer.parseInt(by.split("/")[0]) < 10 ? "0" + by.split("/")[0] :
+                            by.split("/")[0]));
+        } else {
+            date = LocalDate.parse(by);
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (by: " + by + ")";
+        return "[E]" + super.toString() + " (by: " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
