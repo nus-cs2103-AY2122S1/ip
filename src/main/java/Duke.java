a939e9eaad96154c5de4925b91a9febfefe29bdd
Wilfredwongkc <Wilfredwongkc@gmail.com>
2021-08-26 18:49:33 +0800

    Abstract storage class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,87 +15,6 @@ public class Duke {
         System.out.println("___________________________________________");
     }
 
-    static Path getFilePath() throws IOException {
-        Path folderPath = Paths.get("data");
-        Path filePath = Paths.get("data/duke.txt");
-        if (!Files.exists(folderPath)) {
-            folderPath = Files.createDirectory(folderPath);
-            filePath = Files.createFile(filePath);
-            System.out.println("Directory and File Created!");
-            return filePath;
-        } else {
-            System.out.println("Folder found!");
-            if (!Files.exists(filePath)) {
-                filePath = Files.createFile(filePath);
-                System.out.println("File Created!");
-            } else {
-                System.out.println("File found!");
-            }
-            return filePath;
-        }
-    }
-
-    static List<Task> load(Path p) throws IOException {
-        String currContent;
-        String[] parts;
-        ArrayList<Task> taskList = new ArrayList<>();
-        List<String> fileContents = Files.readAllLines(p);
-        for (int i = 0; i< fileContents.size(); i++) {
-            currContent = fileContents.get(i);
-            parts = currContent.split("\\|");
-            if (parts[0].equals("T")) {
-                Todo todo = new Todo(parts[2]);
-                if (parts[1].equals("1")) {
-                    todo.markAsDone();
-                }
-                taskList.add(todo);
-            } else if (parts[0].equals("D")) {
-                Deadline deadline = new Deadline(parts[2],parts[3]);
-                if (parts[1].equals("1")) {
-                    deadline.markAsDone();
-                }
-                taskList.add(deadline);
-            } else if (parts[0].equals("E")) {
-                Event event = new Event(parts[2],parts[3]);
-                if (parts[1].equals("1")) {
-                    event.markAsDone();
-                }
-                taskList.add(event);
-            }
-        }
-        return taskList;
-    }
-
-    static void write(List<Task> tasks, Path path) throws IOException {
-        String stringToInsert;
-        Task t;
-
-        Files.write(path, "".getBytes());
-        for (int i = 0; i < tasks.size(); i++) {
-            t = tasks.get(i);
-            if (t.getTaskType() == "Todo") {
-                if (t.getIsDone()) {
-                    stringToInsert = "T|1|" + t.getDescription() + "\n";
-                } else {
-                    stringToInsert = "T|0|" + t.getDescription() + "\n";
-                }
-            } else if (t.getTaskType() == "Deadline") {
-                if (t.getIsDone()) {
-                    stringToInsert = "D|1|" + t.getDescription() + "|" + t.getBy() + "\n";
-                } else {
-                    stringToInsert = "D|0|" + t.getDescription() + "|" + t.getBy() + "\n";
-                }
-            } else {
-                if (t.getIsDone()) {
-                    stringToInsert = "E|1|" + t.getDescription() + "|" + t.getBy() + "\n";
-                } else {
-                    stringToInsert = "E|0|" + t.getDescription() + "|" + t.getBy() + "\n";
-                }
-            }
-            Files.write(path, stringToInsert.getBytes(), StandardOpenOption.APPEND);
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         String s;
         int taskNumber;
@@ -105,8 +24,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
-        Path filePath = getFilePath();
-        List<Task> list = load(filePath);
+        Storage storage = new Storage();
+        List<Task> list = storage.load(storage.getFilePath());
+
+        System.out.println(list);
 
         System.out.println(logo);
         drawLine();
@@ -121,7 +42,7 @@ public class Duke {
 
                 if (s.startsWith("bye")) {
                     drawLine();
-                    write(list, filePath);
+                    storage.write(list, storage.getFilePath());
                     System.out.println("\tBye. Hope to see you again soon!");
                     drawLine();
                     break;
