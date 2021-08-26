fdbbf8c775247050d2e65b653b38aad785a48df3
Allard <AllardQuek@users.noreply.github.com>
2021-08-27 00:36:19 +0800

    Add tests for Deadline and ToDo

---
+++ src/test/java/DeadlineTest.java
@@ -0,0 +1,18 @@
+import duke.task.Deadline;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+	@Test
+    public void testDeadlineDisplayedCorrectly() {
+	    String datetime = "2021-08-27";
+	    LocalDate deadline = LocalDate.parse(datetime);
+	    String taskName = "finish ip project";
+        Deadline deadlineTask = new Deadline(taskName, deadline);
+	    String expectedResult = String.format("[D][ ] %s (by: Aug 27 2021)", taskName);
+        assertEquals(expectedResult, deadlineTask.toString());
+    }
+}
--- src/test/java/TodoTest.java
+++ src/test/java/TodoTest.java
@@ -1,1 +1,21 @@
-// Write tests for Todo class
+import duke.task.ToDo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class ToDoTest {
+    @Test
+    public void testCreateTodoTask(){
+        ToDo todoTask = new ToDo("work");
+        assertEquals(todoTask.toString(), "[T][ ] work");
+    }
+
+	@Test
+	public void testTodoTaskIsCompleted(){
+		ToDo todoTask = new ToDo("work");
+		todoTask.setDone(true);
+		assertTrue(todoTask.isDone());
+	}
+}
