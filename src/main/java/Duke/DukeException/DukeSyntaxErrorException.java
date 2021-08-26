b726db355315e9c73e3a123de0e3def2c7ff46db
hmanhduc2k <hmanhduc2k@gmail.com>
2021-08-26 19:41:17 +0800

    Change command statement

--- src/main/java/Duke/DukeException/DukeSyntaxErrorException.java
+++ src/main/java/Duke/DukeException/DukeSyntaxErrorException.java
@@ -2,6 +2,6 @@ package Duke.DukeException;
 
 public class DukeSyntaxErrorException extends DukeException {
     public DukeSyntaxErrorException(String command) {
-        super("Uh, what is that ah? I cannot recognize '" + command + "'", Type.SYNTAX_ERROR);
+        super("Uh, what is that ah? I cannot recognize '" + command + "'\n", Type.SYNTAX_ERROR);
     }
 }
