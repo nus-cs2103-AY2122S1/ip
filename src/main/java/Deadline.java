d08dd07a673749279479cf4c487e00d53c357d5f
LuoDan <69646558+luodan01@users.noreply.github.com>
2021-08-27 05:55:12 +0800

    Merge changes

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -2,15 +2,18 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task {
-    private LocalDate by;
+    private String by;
+    private LocalDate date;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = LocalDate.parse(by.trim());
+        this.date = LocalDate.parse(by.trim());
+        this.by = by;
     }
 
     public Deadline(String description, boolean isDone, String by) {
         super(description, isDone);
+        this.date = LocalDate.parse(by.trim());
         this.by = by;
     }
 
@@ -28,6 +31,6 @@ public class Deadline extends Task {
     @Override
     public String toString() {
         return super.toString() + " (by: " +
-                by.format(DateTimeFormatter.ofPattern("MMM dd yyyy"))+ ")";
+                date.format(DateTimeFormatter.ofPattern("MMM dd yyyy"))+ ")";
     }
 }
