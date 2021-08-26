bf4311e7740ed95a57a1541312b15c18b2ac7c14
erwinqxy <erwin.quek.xian.yi@gmail.com>
2021-08-26 20:34:13 +0800

    Add JavaDocs for DukeException

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -1,5 +1,8 @@
 package duke;
 
+/**
+ * Class to handle DukeExceptions
+ */
 public class DukeException extends Exception {
     public DukeException(String errorMessage) {
         super(errorMessage);
