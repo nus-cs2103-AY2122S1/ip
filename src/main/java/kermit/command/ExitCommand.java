126eab8c2920d5ca29f165e06bb7af31d160eb90
Nicolas <nicolas.chang99@gmail.com>
2021-08-26 10:11:22 +0800

    Add javadocs for exit command

--- src/main/java/kermit/command/ExitCommand.java
+++ src/main/java/kermit/command/ExitCommand.java
@@ -5,15 +5,35 @@ import kermit.Storage;
 import kermit.ToDo;
 import kermit.Ui;
 
+/**
+ * Exit command tells program to exit.
+ */
 public class ExitCommand extends Command {
+    /**
+     * Exit command constructor
+     */
     public ExitCommand() {
     }
 
+    /**
+     * Executes exit command.
+     * Notify user that program is exiting and saves the current state of the task list.
+     *
+     * @param taskList Instance of task list used.
+     * @param ui       Instance of Ui used.
+     * @param storage  Instance of storage class used.
+     * @throws KermitException if there is an error saving the file.
+     */
     public void execute(ToDo taskList, Ui ui, Storage storage) throws KermitException {
         ui.showGoodbyeMessage();
         storage.save(taskList);
     }
 
+    /**
+     * Returns if command is the exit command.
+     *
+     * @return Always returns true as this is the exit command.
+     */
     public boolean isExit() {
         return true;
     }
