7c44f6d18f0d886f5c24beaca57f0b8ecf5367a1
Wilfredwongkc <Wilfredwongkc@gmail.com>
2021-08-26 21:18:46 +0800

    Abstract ui class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,20 +9,13 @@ public class Duke {
     public static void main(String[] args) throws IOException {
         String s;
         int taskNumber;
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+
 
         Storage storage = new Storage();
         TaskList list = new TaskList(storage.load(storage.getFilePath()));
-        //List<Task> list = storage.load(storage.getFilePath());
+        Ui ui = new Ui(storage, list);
 
-        System.out.println(logo);
-        drawLine();
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
-        drawLine();
+        ui.greet();
 
         Scanner input = new Scanner(System.in);
 
@@ -31,71 +24,38 @@ public class Duke {
                 s = input.nextLine();
 
                 if (s.startsWith("bye")) {
-                    drawLine();
-                    storage.write(list.getTaskList(), storage.getFilePath());
-                    System.out.println("\tBye. Hope to see you again soon!");
-                    drawLine();
+                    ui.goodbye();
                     break;
                 } else if (s.equals("list")) {
-                    drawLine();
-                    System.out.println("\tHere are the tasks in your list:");
-                    for (int i = 0; i < list.length(); i++) {
-                        System.out.println("\t" + (i + 1) + ". " + list.getTask(i).toString());
-                    }
-                    drawLine();
+                    ui.listTasks();
                     continue;
                 } else if (s.startsWith("done")) {
                     if (s.length() == 4 || s.length() == 5) {
                         throw new DukeException("☹ OOPS!!! You did not put which task you want me to mark it complete.");
                     }
-                    drawLine();
-                    System.out.println("\tNice! I've marked this task as done:");
                     taskNumber = Integer.parseInt(s.substring(s.indexOf(" ") + 1)) - 1;
-                    list.getTask(taskNumber).markAsDone();
-                    System.out.println("\t\t[" + list.getTask(taskNumber).getStatusIcon() + "] "
-                            + list.getTask(taskNumber).getDescription());
-                    drawLine();
+                    ui.markDone(taskNumber);
                     continue;
                 } else if (s.startsWith("todo")) {
                     if (s.length() == 4 || s.length() == 5) {
                         throw new DukeException("\t☹ OOPS!!! The description of a todo cannot be empty.");
                     }
                     Todo todo = new Todo(s.substring(s.indexOf(" ") + 1));
-                    list.addTask(todo);
-                    drawLine();
-                    System.out.println("\tGot it. I've added this task:");
-                    System.out.println("\t\t" + todo);
-                    System.out.println("\tNow you have " + list.length() + " tasks in the list.");
-                    drawLine();
+                    ui.add(todo);
                 } else if (s.startsWith("deadline")) {
                     Deadline deadline = new Deadline(s.substring(s.indexOf(" ") + 1, s.indexOf(" /by")),
                             s.substring(s.indexOf("/by") + 4));
-                    list.addTask(deadline);
-                    drawLine();
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("\t" + deadline);
-                    System.out.println("Now you have " + list.length() + " tasks in the list.");
-                    drawLine();
+                    ui.add(deadline);
                 } else if (s.startsWith("event")) {
                     Event event = new Event(s.substring(s.indexOf(" ") + 1, s.indexOf(" /at")),
                             s.substring(s.indexOf("/at") + 4));
-                    list.addTask(event);
-                    drawLine();
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println("\t" + event);
-                    System.out.println("Now you have " + list.length() + " tasks in the list.");
-                    drawLine();
+                    ui.add(event);
                 } else if (s.startsWith("delete")) {
                     if (s.length() == 6 || s.length() == 7) {
                         throw new DukeException("\t☹ OOPS!!! You did not put which task you want me to delete.");
                     }
-                    drawLine();
-                    System.out.println("\tNoted. I've removed this task:");
                     taskNumber = Integer.parseInt(s.substring(s.indexOf(" ") + 1)) - 1;
-                    System.out.println("\t\t" + list.getTask(taskNumber));
-                    list.deleteTask(taskNumber);
-                    System.out.println("\tNow you have " + list.length() + " in the list.");
-                    drawLine();
+                    ui.delete(taskNumber);
                     continue;
                 } else {
                     throw new DukeException("\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class Ui {
+import java.io.IOException;
+
+public class Ui {
+    String logo;
+    Storage storage;
+    TaskList list;
+
+    public Ui(Storage storage, TaskList taskList) {
+        this.storage = storage;
+        this.list = taskList;
+        this.logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+    }
+
+    public void drawLine() {
+        System.out.println("___________________________________________");
+    }
+
+    public void greet() {
+        System.out.println(logo);
+        drawLine();
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        drawLine();
+    }
+
+    public void goodbye() throws IOException {
+        drawLine();
+        storage.write(list.getTaskList(), storage.getFilePath());
+        System.out.println("\tBye. Hope to see you again soon!");
+        drawLine();
+    }
+
+    public void listTasks() {
+        drawLine();
+        System.out.println("\tHere are the tasks in your list:");
+        for (int i = 0; i < list.length(); i++) {
+            System.out.println("\t" + (i + 1) + ". " + list.getTask(i).toString());
+        }
+        drawLine();
+    }
+
+    public void markDone(int taskNumber) {
+        drawLine();
+        System.out.println("\tNice! I've marked this task as done:");
+        list.getTask(taskNumber).markAsDone();
+        System.out.println("\t\t[" + list.getTask(taskNumber).getStatusIcon() + "] "
+                + list.getTask(taskNumber).getDescription());
+        drawLine();
+    }
+
+    public void delete(int taskNumber) {
+        drawLine();
+        System.out.println("\tNoted. I've removed this task:");
+        System.out.println("\t\t" + list.getTask(taskNumber));
+        list.deleteTask(taskNumber);
+        System.out.println("\tNow you have " + list.length() + " in the list.");
+        drawLine();
+    }
+
+    public void add(Task task) {
+        drawLine();
+        list.addTask(task);
+        System.out.println("\tGot it. I've added this task:");
+        System.out.println("\t\t" + task);
+        System.out.println("\tNow you have " + list.length() + " tasks in the list.");
+        drawLine();
+    }
 }
