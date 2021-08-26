23833da7869d55d5352e3a20c89b3442e2bf5f23
Nicolas <nicolas.chang99@gmail.com>
2021-08-26 10:14:32 +0800

    Add javadocs for complete task command

--- src/main/java/kermit/command/CompleteTaskCommand.java
+++ src/main/java/kermit/command/CompleteTaskCommand.java
@@ -6,10 +6,19 @@ import kermit.ToDo;
 import kermit.Storage;
 import kermit.tasks.Task;
 
+/**
+ * Command used to mark a task as complete.
+ */
 public class CompleteTaskCommand extends Command {
     // zero indexed
     private int taskNum;
 
+    /**
+     * Complete task command constructor.
+     *
+     * @param taskNum Task number to mark as complete (one-indexed).
+     * @throws KermitException if string is not a valid integer that cannot be parsed.
+     */
     // taskNum is number in task list, one indexed
     public CompleteTaskCommand(String taskNum) throws KermitException {
         try {
@@ -20,6 +29,15 @@ public class CompleteTaskCommand extends Command {
 
     }
 
+    /**
+     * Execute complete task command.
+     * Tries to mark task as complete, notifies user and saves the task list.
+     *
+     * @param taskList Instance of task list used.
+     * @param ui       Instance of Ui used.
+     * @param storage  Instance of storage class used.
+     * @throws KermitException if error completing task or saving task list.
+     */
     @Override
     public void execute(ToDo taskList, Ui ui, Storage storage) throws KermitException {
         try {
@@ -31,6 +49,11 @@ public class CompleteTaskCommand extends Command {
         }
     }
 
+    /**
+     * Returns if command is the exit command.
+     *
+     * @return false
+     */
     @Override
     public boolean isExit() {
         return false;
