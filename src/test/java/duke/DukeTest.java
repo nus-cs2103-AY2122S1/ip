c55af2c511363da60af517ad773b0ff1fdc4c260
HuangChing <ching2020huang@gmail.com>
2021-08-26 12:28:58 +0800

    Reformat code
    Reformatting code is important for the quality of the code.

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DukeTest {
     @Test
     public void dummy(){
-        assertEquals(2, 2);
+        assertEquals(0,0);
     }
 
 }
