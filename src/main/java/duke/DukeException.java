6f4d880d3d6c55a92139212d53249aa5347ebab4
erwinqxy <erwin.quek.xian.yi@gmail.com>
2021-08-26 20:54:39 +0800

    Add JavaDoc for Event class

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -1,7 +1,10 @@
 package duke;
 
 /**
- * Class to handle DukeExceptions
+ * To handle Duke Exception.
+ *
+ * @author Erwin Quek
+ * @version CS2103 AY21/22 Sem 1
  */
 public class DukeException extends Exception {
     public DukeException(String errorMessage) {
