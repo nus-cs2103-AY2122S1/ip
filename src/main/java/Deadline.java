dcc45cc40dfff9a84db34297b33bdc8d020b216c
Ryan Chung <timelord0830@gmail.com>
2021-08-26 15:55:27 +0800

    Add constuctors to accomodate datetime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,30 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
+    private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy");
     protected String by;
+    protected LocalDate byDate;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        this.byDate = null;
     }
+
+    public Deadline(String description, LocalDate byDate) {
+        super(description);
+        this.by = null;
+        this.byDate = byDate;
+    }
+
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), by);
+        return String.format(
+                "[D]%s (by: %s)", 
+                super.toString(), 
+                byDate != null ? byDate.format(DATE_TIME_FORMAT) : by
+        );
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,33 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
 public class Event extends Task {
+    private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy");
     protected String at;
+    protected LocalDate atDate;
+    
 
     public Event(String description, String at) {
         super(description);
         this.at = at;
+        this.atDate = null;
     }
+
+    public Event(String description, LocalDate atDate) {
+        super(description);
+        this.at = null;
+        this.atDate = atDate;
+    }
+    
+    
 
     @Override
     public String toString() {
-        return String.format("[E]%s (at: %s)", super.toString(), at);
+        return String.format(
+                "[E]%s (at: %s)", 
+                super.toString(), 
+                atDate != null ? atDate.format(DATE_TIME_FORMAT) : at
+        );
     }
 }
