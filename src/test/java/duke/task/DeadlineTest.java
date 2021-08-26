e00a17b3073b2859c74f19c931bcadc4599d6de2
YeluriKetan <e0603110@u.nus.edu>
2021-08-26 22:54:28 +0800

    Add JUnit tests

---
+++ src/test/java/duke/task/DeadlineTest.java
@@ -0,0 +1,25 @@
+package duke.task;
+
+import duke.Parser;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+    
+    @Test
+    public void toString_deadlineObject_stringRep() {
+        LocalDateTime deadlineDT = Parser.parseDateTime("2021 08 30 2359");
+        Deadline deadline = new Deadline("finish all tasks", deadlineDT);
+        assertEquals("[D][ ] finish all tasks (by: 30 Aug 2021, 11:59 PM)", deadline.toString());
+    }
+
+    @Test
+    public void toStorage_deadlineObject_storageRep() {
+        LocalDateTime deadlineDT = Parser.parseDateTime("2021 08 30 2359");
+        Deadline deadline = new Deadline("finish all tasks", deadlineDT);
+        assertEquals("D%false%finish all tasks%2021-08-30T23:59\n", deadline.toStorage());
+    }
+}
