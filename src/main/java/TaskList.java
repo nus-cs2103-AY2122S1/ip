4523a46daff351b846902f3d41ac263fcb6f05ec
GWphua <phuaguanwei99@gmail.com>
2021-08-26 13:43:54 +0800

    Revert "Revert "Add "Save" functionality.""
    This reverts commit bf6b0b5e68dffb1df9d7e6589d34a74ce1a8e750.

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,15 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class TaskList {
     /**
      * This is the array that Virushade keeps the tasks in.
      */
-    private static final ArrayList<Task> taskList = new ArrayList<>();
+    private static final ArrayList<Task> TASK_ARRAY_LIST = new ArrayList<>();
 
     /**
-     * listCount keeps track of the size of the TaskList.
+     * This is the file name of our file that stores data on TaskList.
+     */
+    private static final String TASK_LIST_FILE_NAME = "data/Virushade.txt";
+
+    /**
+     * This variable keeps track of the size of the TaskList.
      */
     private static int listCount = 0;
+
+    /**
+     * A function that writes an input string to a file.
+     *
+     * @param filePath The name of the file
+     * @param text The input string to write into the file.
+     */
+    private static void updateFile(String filePath, String text) {
+        try {
+            File f = new File(filePath);
+            if (!f.exists()) {
+                f.getParentFile().mkdirs();
+                f.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(f);
+            fw.write(text);
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Unable to update file.\n" + e);
+        }
+    }
 
     /**
      * Partitions the string into 2, seperated by the first '/'.
@@ -53,40 +84,44 @@ public class TaskList {
             }
 
             switch (taskType) {
-                case "TODO":
-                    addedTask = new ToDo(addedTaskDescription);
-                    break;
+            case "TODO":
+                addedTask = new ToDo(addedTaskDescription);
+                break;
 
-                case "DEADLINE":
-                    if (pair[1].startsWith("by ")) {
-                        addedTask = new Deadline(pair[0], pair[1].substring(3));
-                    } else {
-                        System.out.println("Please include a deadline after your task name. " +
-                                "(e.g. /by Sunday)");
-                        return;
-                    }
-                    break;
+            case "DEADLINE":
+                if (pair[1].startsWith("by ")) {
+                    addedTask = new Deadline(pair[0], pair[1].substring(3));
+                } else {
+                    System.out.println("Please include a deadline after your task name. " +
+                            "(e.g. /by Sunday)");
+                    return;
+                }
+                break;
 
-                case "EVENT":
-                    pair = slashPartition(addedTaskDescription);
-                    if (pair[1].startsWith("at ")) {
-                        addedTask = new Event(pair[0], pair[1].substring(3));
-                    } else {
-                        System.out.println("Please include a time after your task name. " +
-                                "(e.g. /at 12 noon)");
-                        return;
-                    }
-                    break;
+            case "EVENT":
 
-                // The add function would not reach this line at all.
-                default:
-                    addedTask = new Task(addedTaskDescription);
+                pair = slashPartition(addedTaskDescription);
+                if (pair[1].startsWith("at ")) {
+                    addedTask = new Event(pair[0], pair[1].substring(3));
+                } else {
+                    System.out.println("Please include a time after your task name. " +
+                            "(e.g. /at 12 noon)");
+                    return;
+                }
+                break;
+
+            // The add function would not reach this line at all.
+            default:
+                addedTask = new Task(addedTaskDescription);
+
             }
 
-            taskList.add(addedTask);
+            TASK_ARRAY_LIST.add(addedTask);
             listCount++;
             System.out.println("Added: " + addedTask.getTaskDescription());
             System.out.printf("Now you have %d tasks in the list.\n", listCount);
+            updateFile(TASK_LIST_FILE_NAME, generateList());
+
         } else {
             System.out.println("Sorry, Virushade cannot keep track of more than 100 tasks!!!");
         }
@@ -104,10 +139,11 @@ public class TaskList {
             if (index <= 0) {
                 System.out.println("Please enter an integer greater than 0.");
             } else if (index <= listCount) {
-                Task deletedTask = taskList.get(index - 1);
+                Task deletedTask = TASK_ARRAY_LIST.get(index - 1);
                 deletedTask.deleteMessage();
                 listCount--;
                 System.out.printf("You have %d tasks in the list.\n", listCount);
+                updateFile(TASK_LIST_FILE_NAME, generateList());
             } else {
                 System.out.println("Please check that you have entered the correct number!");
             }
@@ -131,8 +167,9 @@ public class TaskList {
             if (index <= 0) {
                 System.out.println("Please enter an integer greater than 0.");
             } else if (index <= listCount) {
-                Task doneTask = taskList.get(index - 1);
+                Task doneTask = TASK_ARRAY_LIST.get(index - 1);
                 doneTask.completeTask();
+                updateFile(TASK_LIST_FILE_NAME, generateList());
             } else {
                 System.out.println("Please check that you have entered the correct number!");
             }
@@ -147,17 +184,26 @@ public class TaskList {
      * Display the stored values in taskList for the user.
      */
     public static void list() {
-        int index = 0;
+        System.out.println(generateList());
+    }
 
+    /**
+     * @return String representation of the tasks within TASK_ARRAY_LIST.
+     */
+    private static String generateList() {
         if (listCount == 0) {
-            System.out.println("Nothing in the list as of now.");
-            return;
+            return "Nothing in the list as of now.";
         }
 
-        System.out.println("Here are the tasks in your list:");
+        int index = 0;
+        StringBuilder sb = new StringBuilder("Here are the tasks in your list:");
+
         while (index < listCount) {
-            System.out.println((index + 1) + "." + taskList.get(index).toString());
+            String taskName = (index + 1) + "." + TASK_ARRAY_LIST.get(index).toString();
+            sb.append(System.lineSeparator()).append(taskName);
             index++;
         }
+
+        return sb.toString();
     }
 }
