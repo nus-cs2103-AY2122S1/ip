ddb43778a138b038b2e98d643936575be5f8b82e
Richard Li Defa <richardlidefa@gmail.com>
2021-08-26 14:14:33 +0800

    Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,25 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+
+
 public class Deadline extends Task {
-    private String deadline;
+    private LocalDate deadline;
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
 
     public Deadline(String description, String by) {
         super(description);
-        this.deadline = by;
+        try {
+            this.deadline = LocalDate.parse(by);
+        } catch (DateTimeParseException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("Bad date format. Please input as yyyy-mm-dd.");
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + deadline + ")";
+        return "[D]" + super.toString() + " (by: " + deadline.format(formatter) + ")";
     }
 }
