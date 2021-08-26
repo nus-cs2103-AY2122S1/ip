ec46bcccb61c713e4e675dbec70f89762f35a45c
cheweejia <eejiachew@gmail.com>
2021-08-26 14:18:30 +0800

    Add date and time management

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,22 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected final String dateTime;
+    protected final LocalDateTime dateTime;
 
     public Deadline(String description, String dateTime) {
         super(description);
-        this.dateTime = dateTime;
+        this.dateTime = convert(dateTime);
+    }
+
+    private LocalDateTime convert(String dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
+        return LocalDateTime.parse(dateTime, formatter);
     }
 
     @Override
     public String toString() {
-        return "[D][" + this.getStatusIcon() + "] " + this.getDescription() + " (by:" + this.dateTime + ")";
+        return "[D][" + this.getStatusIcon() + "] " + this.getDescription() + " (by: "
+                + this.dateTime.format(DateTimeFormatter.ofPattern("dd/MM/yy HH:mm")) + ")";
     }
 }
