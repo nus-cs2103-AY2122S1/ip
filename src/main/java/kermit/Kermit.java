e833f5c1de288f610eb982ea440ed11650b51dbc
Nicolas <nicolas.chang99@gmail.com>
2021-08-26 11:20:42 +0800

    Delete formatUserDateFormat function
    formatUserDateFormat is deleted from Kermit.
    
    formatUserDateFormat is superseded by Parser class and does not handle date parsing by user.

--- src/main/java/kermit/Kermit.java
+++ src/main/java/kermit/Kermit.java
@@ -10,19 +10,6 @@ public class Kermit {
     private TaskList taskList;
     private Ui ui;
 
-    private static LocalDate formatUserDateFormat(String s) throws KermitException {
-        String[] components = s.split("-");
-        String day = components[0];
-        String month = components[1];
-        String year = components[2];
-        try {
-            LocalDate parsedDate = LocalDate.parse(String.join("-", year, month, day));
-            return parsedDate;
-        } catch (DateTimeParseException e) {
-            throw new KermitException("BAH That's not a time is it?? Try writing like this DD/MM/YYYY");
-        }
-    }
-
     public Kermit(String filePath) {
         ui = new Ui();
         storage = new Storage(filePath);
