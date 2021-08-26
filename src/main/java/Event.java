155e25572d5a6051aef31a15eb607e04aa85ac35
zhuoyang125 <zhuoyang125@gmail.com>
2021-08-26 10:05:25 +0800

    Add Level-8

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private String timePeriod;
+    private LocalDate timePeriod;
 
     public Event(String taskname, String timePeriod) {
         super(taskname);
-        this.timePeriod = timePeriod;
+        this.timePeriod = LocalDate.parse(timePeriod);
     }
 
     @Override
     public String toString() {
-        return "Event: " + super.toString() + " (at" + this.timePeriod + ")";
+        return "Event: " + super.toString() + " (at " + this.timePeriod.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
