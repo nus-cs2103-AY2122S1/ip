14b12a478dfa96630728277f6b3c1ae2989e0b7c
Cedric Ong <cedricongjh@gmail.com>
2021-08-26 14:42:59 +0800

    Fix event toString method

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -16,6 +16,6 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return String.format("[E]%s (by: %s)", super.toString(), super.getDateString());
+        return String.format("[E]%s (at: %s)", super.toString(), super.getDateString());
     }
 }
