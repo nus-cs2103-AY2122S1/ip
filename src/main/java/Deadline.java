a84f7c5c5c397fdb25234054b3307016f73a0744
Kangxun <e0543787@u.nus.edu>
2021-08-26 22:23:59 +0800

    Add Level 8. Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,44 @@
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate by;
+    protected Date time;
 
-    public Deadline(String description, String by) {
+    protected String dateToString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        return formatter.format(by);
+    }
+
+    protected String timeToString() {
+        SimpleDateFormat displayFormat = new SimpleDateFormat("hh:mm a");
+        return displayFormat.format(time);
+    }
+
+    public Deadline(String description, String by) throws DukeException {
         super(description);
-        this.by = by;
+        String[] temp = by.split(" ", 2);
+        try {
+            this.by = LocalDate.parse(temp[0]);
+        } catch (DateTimeParseException e) {
+            throw new DukeException("time has to be in the format yyyy-mm-dd HHmm instead of " + by);
+        }
+
+        try {
+            SimpleDateFormat parseFormat = new SimpleDateFormat("HHmm");
+            this.time = parseFormat.parse(temp[1]);
+        } catch (ParseException e) {
+            throw new DukeException("time has to be in the format yyyy-mm-dd HHmm instead of " + by);
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + dateToString() + " " + timeToString() + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,44 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
+
 public class Event extends Task {
 
-    protected String at;
+    protected LocalDate at;
+    protected Date time;
 
-    public Event(String description, String at) {
+    protected String dateToString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        return formatter.format(at);
+    }
+
+    protected String timeToString() {
+        SimpleDateFormat displayFormat = new SimpleDateFormat("hh:mm a");
+        return displayFormat.format(time);
+    }
+
+    public Event(String description, String at) throws DukeException {
         super(description);
-        this.at = at;
+        String[] temp = at.split(" ", 2);
+        try {
+            this.at = LocalDate.parse(temp[0]);
+        } catch (DateTimeParseException e) {
+            throw new DukeException("time has to be in the format yyyy-mm-dd HHmm instead of " + at);
+        }
+
+        try {
+            SimpleDateFormat parseFormat = new SimpleDateFormat("HHmm");
+            this.time = parseFormat.parse(temp[1]);
+        } catch (ParseException e) {
+            throw new DukeException("time has to be in the format yyyy-mm-dd HHmm instead of " + at);
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + dateToString() +  " " + timeToString() + ")";
     }
 }
