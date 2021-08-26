572d6f393ea9c93f3468bebc596a23ffed00e96b
juliussneezer04 <vishnu.sundaresan@gmail.com>
2021-08-27 00:56:37 +0800

    Merge branch 'branch-Level-8'

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,26 +1,40 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Entry{
 
-    private String event;
+    private LocalDate event;
 
     Event() {
         super();
     }
 
-    Event(String task, String event) {
+    Event(String task, String event) throws DukeException {
         super(task);
-        this.event = event;
+        try {
+            this.event = LocalDate.parse(event);
+        } catch (DateTimeParseException e) {
+            this.event = LocalDate.now();
+            throw new DukeException("Invalid timing format! Enter dates in yyyy-mm-dd format");
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(at: " + event + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        return "[E]" + super.toString() + "(at: " + this.event.format(formatter) + ")";
     }
 
     @Override
+<<<<<<< HEAD
     public boolean isEmpty() { return super.isEmpty() || event.length() < 1; }
 
     @Override
     public String saveString() {
         return "E" + super.saveString() + "," + this.event;
     }
+=======
+    public boolean isEmpty() { return super.isEmpty() && event.isAfter(LocalDate.now()); }
+>>>>>>> branch-Level-8
 }
