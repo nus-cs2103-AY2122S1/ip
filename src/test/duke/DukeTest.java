1da6082867332a23395f8cafab617edcd61f4ccf
Tee Chin <teechin1999@gmail.com>
2021-08-27 01:15:33 +0800

    Attempt JUnit tests

---
+++ src/test/duke/DukeTest.java
@@ -0,0 +1,12 @@
+package duke;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DukeTest {
+    @Test
+    public void dumbTest() {
+        DataHandler.addToLog()
+        assertEquals(2,2);
+    }
+}
