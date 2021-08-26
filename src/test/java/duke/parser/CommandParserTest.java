6367658bc90aa59a4d5bf6ff69080fa159b4bdb3
ganhongyao <75519136+ganhongyao@users.noreply.github.com>
2021-08-26 22:49:05 +0800

    Update CommandParser JUnit test (#23)

--- src/test/java/duke/parser/CommandParserTest.java
+++ src/test/java/duke/parser/CommandParserTest.java
@@ -10,53 +10,58 @@ import duke.command.AddToDoCommand;
 import duke.command.CompleteTaskCommand;
 import duke.command.DeleteTaskCommand;
 import duke.command.ExitCommand;
+import duke.command.FindCommand;
 import duke.command.HelpCommand;
 import duke.command.ListCommand;
 
 public class CommandParserTest {
     @Test
-    public void parser_addDeadlineCommand_deadlineCommandReturned() {
+    public void parse_addDeadlineCommand_deadlineCommandReturned() {
         assertTrue(CommandParser.parse("deadline return book /by 2021-08-31 2359") instanceof AddDeadlineCommand);
     }
 
     @Test
-    public void parser_addEventCommand_eventCommandReturned() {
+    public void parse_addEventCommand_eventCommandReturned() {
         assertTrue(CommandParser.parse("event book convention /at 2021-08-31 2100") instanceof AddEventCommand);
     }
 
     @Test
-    public void parser_addToDoCommand_toDoCommandReturned() {
+    public void parse_addToDoCommand_toDoCommandReturned() {
         assertTrue(CommandParser.parse("todo buy groceries") instanceof AddToDoCommand);
     }
 
     @Test
-    public void parser_completeTaskCommand_completeTaskCommandReturned() {
+    public void parse_completeTaskCommand_completeTaskCommandReturned() {
         assertTrue(CommandParser.parse("done 3") instanceof CompleteTaskCommand);
     }
 
     @Test
-    public void parser_deleteTaskCommand_deleteTaskCommandReturned() {
+    public void parse_deleteTaskCommand_deleteTaskCommandReturned() {
         assertTrue(CommandParser.parse("delete 3") instanceof DeleteTaskCommand);
     }
 
     @Test
-    public void parser_exitCommand_exitCommandReturned() {
+    public void parse_exitCommand_exitCommandReturned() {
         assertTrue(CommandParser.parse("bye") instanceof ExitCommand);
     }
 
     @Test
-    public void parser_helpCommand_helpCommandReturned() {
+    public void parse_helpCommand_helpCommandReturned() {
         assertTrue(CommandParser.parse("help") instanceof HelpCommand);
     }
 
     @Test
-    public void parser_helpCommand_listCommandReturned() {
+    public void parse_listCommands_listCommandsReturned() {
         assertTrue(CommandParser.parse("list") instanceof ListCommand);
         assertTrue(CommandParser.parse("list 2021-08-29") instanceof ListCommand);
     }
 
+    @Test void parse_findCommand_findCommandReturned() {
+        assertTrue(CommandParser.parse("find test") instanceof FindCommand);
+    }
+
     @Test
-    public void parser_unknownCommand_helpCommandReturned() {
+    public void parse_unknownCommand_helpCommandReturned() {
         assertTrue(CommandParser.parse("blah") instanceof HelpCommand);
     }
 }
