3f9e1778a1e7f95b634e3cd300acfae30026d373
ruoyann <ruoyany@gmail.com>
2021-08-26 17:34:03 +0800

    Level 8

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,20 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
     protected String at;
+    LocalDate atDate;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    String[] monthArray = new  String[] {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"};
 
     public Event(String description, String at) {
         super(description);
         this.at = at;
+        this.atDate = LocalDate.parse(at, formatter);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + monthArray[atDate.getMonthValue()] + " " + atDate.getDayOfMonth() + " " + atDate.getYear() + ")";
     }
 }
