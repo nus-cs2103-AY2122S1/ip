c92b6ce812bfe52734dba95166804107f8511b5c
Nicolas <nicolas.chang99@gmail.com>
2021-08-26 10:24:56 +0800

    Add javadocs for list tasks command.

--- src/main/java/kermit/command/ListTasksCommand.java
+++ src/main/java/kermit/command/ListTasksCommand.java
@@ -4,15 +4,33 @@ import kermit.Storage;
 import kermit.ToDo;
 import kermit.Ui;
 
+/**
+ * List command displays tasks in task list.
+ */
 public class ListTasksCommand extends Command {
 
+    /**
+     * ListTasks command constructor.
+     */
     public ListTasksCommand() {}
 
+    /**
+     * Executes exit command.
+     * List tasks including their short form, completion status and date associated with task
+     * @param taskList Instance of task list used.
+     * @param ui       Instance of Ui used.
+     * @param storage  Instance of storage class used.
+     */
     @Override
     public void execute(ToDo taskList, Ui ui, Storage storage) {
         ui.showListItems(taskList);
     }
 
+    /**
+     * Returns if command is the exit command.
+     *
+     * @return Always returns false.
+     */
     @Override
     public boolean isExit() {
         return false;
