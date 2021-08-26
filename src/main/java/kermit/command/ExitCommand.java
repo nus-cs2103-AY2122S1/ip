7a3fe0bf3be7f39f12290e674c8b8ed1a9c28c8f
Nicolas <nicolas.chang99@gmail.com>
2021-08-26 10:59:50 +0800

    Add missing override annotation

--- src/main/java/kermit/command/ExitCommand.java
+++ src/main/java/kermit/command/ExitCommand.java
@@ -9,11 +9,13 @@ public class ExitCommand extends Command {
     public ExitCommand() {
     }
 
+    @Override
     public void execute(ToDo taskList, Ui ui, Storage storage) throws KermitException {
         ui.showGoodbyeMessage();
         storage.save(taskList);
     }
 
+    @Override
     public boolean isExit() {
         return true;
     }
