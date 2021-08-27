9e919f90a084522d0857531d51a695b3945bab25
Linda124 <lindatomy1@gmail.com>
2021-08-27 10:27:39 +0800

    A-TextUiTesting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
+    
     public static void main (String[]args) {
         Scanner in = new Scanner(System.in);
         ListOfTasks xs = new ListOfTasks();
