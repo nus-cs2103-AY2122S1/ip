07a0e04b8ed9c832e89b437d9a9173abfaba0fa8
Tang Zhiying <e0559633@u.nus.edu>
2021-08-27 10:26:13 +0800

    Add ability to understand dates and time

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,10 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
     protected String by;
+    protected LocalDate date;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        checkDate(by);
+    }
+
+    private void checkDate(String by) {
+        if (by.matches("\\d{4}-\\d{2}-\\d{2}")) {
+            date = LocalDate.parse(by);
+            this.by = date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        }
     }
 
     @Override
