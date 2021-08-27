8d456fb23ec0f076def6acd0211eea673e7ef749
Tee Chin <teechin1999@gmail.com>
2021-08-27 17:43:30 +0800

    Add Junit test for Todo and Deadline

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,17 +1,34 @@
+import duke.Deadline;
 import duke.Todo;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
     @Test
-    public void dumbTest() {
-        assertEquals(2,2);
+    public void TodoTest() {
+        Todo todoDummy = new Todo("Test dummy");
+        String incompleteTodoDummyString = "[T][X] Test dummy";
+
+        //Test incomplete dummy with output string result
+        assertEquals(incompleteTodoDummyString, todoDummy.toString());
+
+        //Test completed dummy with output string result
+        todoDummy.completeTask();
+        String completedTodoDummyString = "[T][✓] Test dummy";
+        assertEquals(completedTodoDummyString, todoDummy.toString());
     }
 
+
     @Test
-    public void secondTest() {
-        Todo todo = new Todo("SMth");
-        assertEquals(todo, todo);
+    public void DeadlineTest() {
+        LocalDateTime testDeadlineDateTime = LocalDateTime.of(2020, 2, 28, 23, 00);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        Deadline deadlineDummy = new Deadline("Test deadline", testDeadlineDateTime);
+        assertEquals("[D][X] Test deadline 28/02/2020 23:00", deadlineDummy.toString());
+
     }
 }
