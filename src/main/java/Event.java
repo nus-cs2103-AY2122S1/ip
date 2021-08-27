bfb6cadf4d52d67a9ad3eeab95ef90eebb953cfc
WangGLJoseph <joseph.cnsg@gmail.com>
2021-08-27 13:39:27 +0800

    Update Deadline and Event to parse task description as string of date

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
     protected String at;
+    protected LocalDate date;
 
     public Event(String description, String at) {
         super(description);
         this.at = at;
+        this.date = LocalDate.parse(at);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return "[E]" + super.toString() + " (at: " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
