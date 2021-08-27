a9fa8400170eea1d51f070acb0dd148dda82d5a4
samyipsh <samyipsh@gmail.com>
2021-08-27 12:04:32 +0800

    add functionality in Deadline to parse and format string to LocalDateTime (MAY BREAK)

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,6 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
     private String type;
-    private String deadline;
+    private String userInputByDate;
+    private LocalDateTime dateTimeDeadline;
+    private LocalDateTime dateTimeTaskCreation;
+    private static DateTimeFormatter DATE_TIME_FORMATTER =
+        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public static Deadline of(String taskSummary, String byDate) {
         return new Deadline(taskSummary, byDate);
@@ -9,7 +16,25 @@ public class Deadline extends Task {
     public Deadline(String taskSummary, String byDate) {
         super(taskSummary);
         this.type = "Deadline";
-        this.deadline = byDate;
+        this.userInputByDate = byDate;
+        this.dateTimeTaskCreation = LocalDateTime.now();
+        this.updateDateTimeDeadline(byDate);
+    }
+
+    private static LocalDateTime stringToLocalDateTime(String text) {
+        return LocalDateTime.parse(text, Deadline.DATE_TIME_FORMATTER);
+    }
+
+    private static String localDateTimeToString(LocalDateTime localDateTime) {
+        return localDateTime.format(Deadline.DATE_TIME_FORMATTER);
+    }
+
+    private void updateDateTimeDeadline(String byDate) {
+        LocalDateTime parsedDateTime = Deadline.stringToLocalDateTime(byDate);
+        if (parsedDateTime.isBefore(dateTimeTaskCreation)) {
+            throw new IllegalArgumentException("Date passed as deadline is in the past: " + this.userInputByDate);
+        }
+        this.dateTimeDeadline = stringToLocalDateTime(byDate);
     }
 
     @Override
@@ -19,7 +44,8 @@ public class Deadline extends Task {
                 this.type.charAt(0),
                 this.isCompleted() ? "X" : "",
                 this.getTaskSummary(),
-                this.deadline
+                Deadline.localDateTimeToString(this.dateTimeDeadline)
+//                this.userInputByDate
         );
     }
 }
