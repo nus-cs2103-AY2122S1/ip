81708a4c3096de4b320d0c0718e5ba1e77b5eb55
david-eom <david.eom.hyo.shik@gmail.com>
2021-08-27 14:06:38 +0800

    Add JavaDoc for tasks

--- src/main/java/duke/task/Todo.java
+++ src/main/java/duke/task/Todo.java
@@ -1,15 +1,35 @@
 package duke.task;
 
+/**
+ * Represents a task to be done.
+ */
 public class Todo extends Task {
+    /**
+     * Constructor for the task.
+     *
+     * @param description description of the thing to be done
+     */
     public Todo(String description) {
         super(description);
     }
 
+
+    /**
+     * Convert the task into the specific string for data saving.
+     *
+     * @return the string representation in the specific format for database
+     */
     @Override
     public String stringifyTask() {
         return String.format("T|%d|%s", this.isDone ? 1 : 0, this.description);
     }
 
+
+    /**
+     * Return the string representation of the task.
+     *
+     * @return string representation of the task
+     */
     @Override
     public String toString() {
         return String.format("[T]%s", super.toString());
