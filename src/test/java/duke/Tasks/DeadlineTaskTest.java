a747a534c7e66bb5a29185b4bad458b4a1116a05
TLChicken <tehlightningchicken@gmail.com>
2021-08-27 23:42:14 +0800

    Add JUnit Test Base

---
+++ src/test/java/duke/Tasks/DeadlineTaskTest.java
@@ -0,0 +1,13 @@
+package duke.Tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTaskTest {
+    @Test
+    public void testTest() {
+        assertEquals(2, 2);
+    }
+
+}
