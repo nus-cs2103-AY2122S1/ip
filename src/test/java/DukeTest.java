0fe7bc7002bbfc7221f93f3fdd653ae569d4468d
Tee Chin <teechin1999@gmail.com>
2021-08-27 15:02:02 +0800

    Add dumnmy JUnit test

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,12 +1,17 @@
-package duke;
-
+import duke.Todo;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
     @Test
     public void dumbTest() {
-        DataHandler.addToLog()
         assertEquals(2,2);
+    }
+
+    @Test
+    public void secondTest() {
+        Todo todo = new Todo("SMth");
+        assertEquals(todo, todo);
     }
 }
