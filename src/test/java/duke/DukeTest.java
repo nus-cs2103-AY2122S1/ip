ce15e97f30375f0d9409de7869f37af112639840
Yang Yuzhao <yang.yuzhao@dhs.sg>
2021-08-27 11:45:16 +0800

    Complete A-JavaDoc.

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * This is the DukeTest class to test Duke class.
+ */
 public class DukeTest {
 	@Test
 	public void dummyTest() {
