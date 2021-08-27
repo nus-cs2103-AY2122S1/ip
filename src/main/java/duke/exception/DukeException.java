43918bc397d075f63e8fcc39c444cae6d8ea3d11
david-eom <david.eom.hyo.shik@gmail.com>
2021-08-27 13:57:18 +0800

    Add JavaDoc for duke exceptions

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -1,6 +1,15 @@
 package duke.exception;
 
+/**
+ * Abstract representation of all duke exceptions.
+ */
 public abstract class DukeException extends Exception {
+
+    /**
+     * Obtain the string representation of the exception.
+     *
+     * @return string representation of the exception
+     */
     @Override
     public String toString() {
         return String.format(":(( Oops!");
--- src/main/java/duke/exception/DukeNoTaskFoundException.java
+++ src/main/java/duke/exception/DukeNoTaskFoundException.java
@@ -1,12 +1,26 @@
 package duke.exception;
 
+/**
+ * Represents a duke exception when a task cannot be found in database.
+ */
 public class DukeNoTaskFoundException extends DukeException {
     private int taskNum;
 
+    /**
+     * Constructor for the DukeNoTaskFoundException.
+     *
+     * @param taskNum the number of task in the
+     */
     public DukeNoTaskFoundException(int taskNum) {
         this.taskNum = taskNum;
     }
 
+
+    /**
+     * Obtain the string representation of the exception.
+     *
+     * @return string representation of the exception
+     */
     @Override
     public String toString() {
         return String.format("%s I cannot find the task with number %d!",
