51b59c7c6f5b9e2ee1a4433ff9e19ab67f25ef3e
Allard <AllardQuek@users.noreply.github.com>
2021-08-29 02:45:31 +0800

    Add test to check if Deadline is marked done correctly

--- src/test/java/DeadlineTest.java
+++ src/test/java/DeadlineTest.java
@@ -1,9 +1,11 @@
 import duke.task.Deadline;
+import duke.task.ToDo;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DeadlineTest {
 	@Test
@@ -15,4 +17,14 @@ public class DeadlineTest {
 	    String expectedResult = String.format("[D][ ] %s (by: Aug 27 2021)", taskName);
         assertEquals(expectedResult, deadlineTask.toString());
     }
+
+	@Test
+	public void testDeadlineTaskIsCompleted(){
+		String datetime = "2021-08-27";
+		LocalDate deadline = LocalDate.parse(datetime);
+		String taskName = "finish ip project";
+		Deadline deadlineTask = new Deadline(taskName, deadline);
+		deadlineTask.setDone(true);
+		assertTrue(deadlineTask.isDone());
+	}
 }
