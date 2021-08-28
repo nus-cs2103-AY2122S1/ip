9539ac74a92d5f5a13b1f7775e8eeefd37601478
junghon3709 <junghonyip@gmail.com>
2021-08-28 16:22:35 +0800

    Add GUI

--- src/main/java/Tiger.java
+++ src/main/java/Tiger.java
@@ -1,23 +1,29 @@
 import tiger.app.Ui;
 
+
 public class Tiger {
 
+    public String getResponse(String input) {
+        return "fish";
+    }
     /**
      * Main entry point into the app.
      *
      * @param args Standard java main argument.
      */
+
     public static void main(String[] args) {
 
-        // TODO: implement flags for delete done combination, invalid todo combination
-
-        // features: fallthrough commands
-        // priority
-        // delete done, invalid todo combination
+//         TODO: implement flags for delete done combination, invalid todo combination
+//
+//         features: fallthrough commands
+//         priority
+//         delete done, invalid todo combination
 
         Ui ui = new Ui();
         ui.start();
         ui.runUntilStopped();
         ui.exit();
     }
+
 }
