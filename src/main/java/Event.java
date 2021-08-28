e3752d17678586e7214c74d6e0a4ef9820768a20
Samuel-bit-prog <e0544506@u.nus.edu>
2021-08-29 00:43:37 +0800

    Level-8 done

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
     protected String at;
+    protected LocalDate date;
+    protected String afterDate;
 
     public Event(String description, String at) {
         super(description);
         this.at = at;
+        String[] arr = at.split(" ", 2);
+        this.afterDate = arr.length == 2 ? arr[1] : "";
+        this.date = LocalDate.parse(arr[0]);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        return String.format("[E]%s (at: %s %s)", super.toString(),
+                this.date.format(DateTimeFormatter.ofPattern("MMM dd yyyy")), this.afterDate);
     }
 }
