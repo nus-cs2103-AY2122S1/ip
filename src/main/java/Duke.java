0c34fb308eba61d67361ed08066f0ac4120f8ede
asherhy <hongyongrs@gmail.com>
2021-08-29 01:43:05 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,34 +2,34 @@ import java.util.ArrayList;
 import java.util.Scanner;  // Import the Scanner class
 
 public class Duke {
-    public static void main(String[] args) {
 
-        DukeStorage storage = new DukeStorage();
-        ArrayList<Task> list = storage.loadFile();
-        TaskHandler taskHandler = new TaskHandler(list, storage);
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        String line = "    ____________________________________________________________";
-        String spacing = "     ";
-        System.out.println(line);
-        System.out.println(spacing + "Hello! I'm Duke");
-        System.out.println(spacing + "What can I do for you?");
-        System.out.println(line);
+    private Scanner myObj;
+
+    public Duke() {
+
+        this.myObj = new Scanner(System.in);  // Create a Scanner object
+    }
+
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.run();
+    }
+
+    public void run(){
+        Ui.printWelcomeMsg();
+        Parser parser = new Parser();
         String command = myObj.nextLine();  // Read user input
         while (!command.toLowerCase().equals("bye")) {
-            System.out.println(line);
             try {
-                taskHandler.execute(command);
+                parser.parse(command);
             } catch (DukeException e) {
-                System.out.println(e.getMessage());
+                Ui.prettify(e.getMessage());
             }
-            System.out.println(line);
             command = myObj.nextLine();
         }
 
-         if (command.toLowerCase().equals("bye")) {
-             System.out.println(line);
-             System.out.println(spacing + "Bye. Hope to see you again soon!");
-             System.out.print(line);
-         }
+        if (command.toLowerCase().equals("bye")) {
+            Ui.printGoodbyeMsg();
+        }
     }
 }
