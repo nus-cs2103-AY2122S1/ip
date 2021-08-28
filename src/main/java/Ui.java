1f681dfe061377da8a902f2f987f0599a69cee6e
asherhy <hongyongrs@gmail.com>
2021-08-29 01:41:08 +0800

    Add Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,77 @@
+import data.Messages;
+
+import java.security.PublicKey;
+
+public class Ui {
+
+    private static final String DIVIDER = "____________________________________________________________";
+    private static final String TAB = "  ";
+    private static final String LS = System.lineSeparator();
+
+
+    public static void prettify(String message){
+        System.out.println(TAB + DIVIDER);
+        System.out.println(message);
+        System.out.println(TAB + DIVIDER);
+    }
+
+    public static void printWelcomeMsg() {
+        String message =  TAB + TAB + Messages.MESSAGE_WELCOME_1 + LS + TAB + TAB + Messages.MESSAGE_WELCOME_2;
+        prettify(message);
+    }
+
+    public static void printGoodbyeMsg() {
+        prettify(Messages.MESSAGE_GOODBYE);
+    }
+
+    public static String emptyDescriptionMsg(String type) {
+        String errorMsg = TAB + TAB + "☹ OOPS!!! ";
+        switch(type) {
+            case "todo":
+                errorMsg += Messages.MESSAGE_EMPTY_TODO;
+                break;
+            case "deadline":
+                errorMsg += Messages.MESSAGE_EMPTY_DEADLINE;
+
+                break;
+            case "event":
+                errorMsg += Messages.MESSAGE_EMPTY_EVENT;
+                break;
+            case "delete":
+                errorMsg += Messages.MESSAGE_EMPTY_DELETE;
+                break;
+            case "done":
+                errorMsg += Messages.MESSAGE_EMPTY_DONE;
+        }
+        return errorMsg;
+    }
+
+    public static String unknownInputMsg() {
+        return TAB + TAB + Messages.MESSAGE_INPUT_UNKNOWN;
+    }
+
+    public static void printCreateDirectoryErr() {
+        System.err.println("IOException: unable to create directory");
+    }
+
+    public static String ioMsg(){
+        return Messages.MESSAGE_IO_ERR;
+    }
+
+    public static void printAddMsg(String task, Integer num) {
+        String msg = Messages.MESSAGE_ADD + LS + TAB + TAB + TAB + task + LS +
+                String.format(Messages.MESSAGE_TASKS_LEFT, num);
+        prettify(msg);
+    }
+
+    public static void printRemoveMsg(String task, Integer num) {
+        String msg = Messages.MESSAGE_REMOVE + LS + TAB + TAB + TAB + task + LS +
+                String.format(Messages.MESSAGE_TASKS_LEFT, num);
+        prettify(msg);
+    }
+
+    public static void printTaskDone(String task) {
+        String msg = TAB + Messages.MESSAGE_DONE + LS + TAB + TAB + task;
+        prettify(msg);
+    }
+}
---
+++ src/main/java/data/Messages.java
@@ -0,0 +1,27 @@
+package data;
+
+/**
+ * Container for user visible messages.
+ */
+public class Messages {
+
+    public static final String MESSAGE_WELCOME_1 = "Hello! I'm Duke";
+    public static final String MESSAGE_WELCOME_2 = "What can I do for you?";
+    public static final String MESSAGE_GOODBYE = "Bye. Hope to see you again soon!";
+    public static final String MESSAGE_EMPTY_DEADLINE = "The description and timing of a deadline" +
+            " cannot be empty.";
+    public static final String MESSAGE_EMPTY_TODO =  "The description of a todo cannot be empty.";
+
+    public static final String MESSAGE_EMPTY_EVENT = "The description and timing of a event cannot be empty." +
+            " cannot be empty.";
+    public static final String MESSAGE_EMPTY_DELETE = "There must be something to be deleted";
+    public static final String MESSAGE_EMPTY_DONE = "There must be something to be completed";
+    public static final String MESSAGE_INPUT_UNKNOWN = " ☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+    public static final String MESSAGE_IO_ERR = "IOException";
+    public static final String MESSAGE_ADD = "     Got it. I've added this task:";
+    public static final String MESSAGE_TASKS_LEFT = "     Now you have %d tasks in the list.";
+    public static final String MESSAGE_REMOVE = "     Noted. I've removed this task:";
+    public static final String MESSAGE_DONE = "Nice! I've marked this task as done:";
+
+
+}
