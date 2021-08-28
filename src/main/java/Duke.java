f3eaef68424bc5814489b4018cf64dc5c15551e3
sj1999-BIT <e0543446@u.nus.edu.sg>
2021-08-28 20:47:17 +0800

    level0
    Added intro and bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        private void greet() {
+            String line = "__________________________________\n";
+            System.out.println(line);
+            String logo = " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+            System.out.println(logo);
+            System.out.println("Hello! I'm Duke\n");
+            System.out.println("What can I do for you?");
+            System.out.println(line);
+        }
+
+        private void bye() {
+            String line = "__________________________________\n";
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(line);
+        }
+
+        public static void main(String[] args) {
+            Duke bot = new Duke();
+            bot.greet();
+            bot.bye();
+        }
     }
-}
