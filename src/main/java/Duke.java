26bfdad1ae2dede85af68fd4209220fca347f423
sj1999-BIT <e0543446@u.nus.edu.sg>
2021-08-28 20:59:00 +0800

    Level 2. Add, List
    Used an ArrayList to store list of items

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Duke {
 
     private String line = "__________________________________\n";
-
     private void greet() {
             System.out.println(line);
             String logo = " ____        _        \n"
@@ -23,27 +22,42 @@ public class Duke {
         System.out.println(line);
     }
 
-    private boolean echo() {
+    private boolean echo(ArrayList<String> lst) {
         Scanner user = new Scanner(System.in);
         String input = user.nextLine();
         if (input.equals("bye")) {
             bye();
             return false;
+        } else if (input.equals("list")) {
+            showList(lst);
+            return true;
         } else {
+            lst.add(input);
             System.out.println(line);
-            System.out.println(input);
+            System.out.println("added: " + input);
             System.out.println(line);
             return true;
         }
     }
 
+    private void showList(ArrayList<String> lst) {
+        int count = 0;
+
+        for (String str : lst ) {
+            count++;
+            System.out.format("%d. " + str + "\n", count);
+        }
+        System.out.println(line);
+    }
+
 
     public static void main(String[] args) {
-            Duke bot = new Duke();
-            bot.greet();
-            boolean end = true;
-            while (end) {
-                end = bot.echo();
-            }
+        Duke bot = new Duke();
+        ArrayList<String> list = new ArrayList<>();
+        bot.greet();
+        boolean end = true;
+        while (end) {
+            end = bot.echo(list);
         }
     }
+}
