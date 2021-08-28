713f9ffc41b3428b86283224b4b851d8a7f2cb91
Wee Han <weehanng@yahoo.com>
2021-08-28 19:45:11 +0800

    add gradle support

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
     @Test
-    public void dummyTest() {
+    public void initialTaskList_createNewDuke_zeroTasksInList() {
         Duke d = new Duke();
         assertEquals(d.getNumberOfTasks(), 0);
     }
