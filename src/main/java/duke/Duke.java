26295eb092d07bfb253b4a48da239deaf0c719cb
Chrus Chong <chrus1000@gmail.com>
2021-08-28 10:37:00 +0800

    Create GUI Launcher that runs Hello World
    Duke main method returns JavaFX runtime components are missing error.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,14 +1,16 @@
 package duke;
-
 import duke.ui.Ui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * Duke class used to run the duke.Duke chat-bot.
  * Contains methods that
  * (i)    initialises the Ui class and runs the chat-bot.
  */
-public class Duke {
-
+public class Duke extends Application {
     /**
      * Initialises the chatBot and runs the Duke chatBot.
      * @param args arguments for main method.
@@ -16,5 +18,14 @@ public class Duke {
     public static void main(String[] args) {
         Ui chatBot = new Ui();
         chatBot.run();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
