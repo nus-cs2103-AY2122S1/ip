59306085cef33f2f744a2cc03c3cd25b9c5d825a
Adam Oh Zhi Hong <adamohzhihong@gmail.com>
2021-08-28 17:21:41 +0800

    Add Gradle run, build and unit tests

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,26 +1,41 @@
-import java.time.LocalDate;
-
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import duke.Storage;
 import duke.task.Deadline;
 import duke.task.ToDo;
+
+
 
 /**
  * Class for testing Duke using JUnit
  */
 public class DukeTest {
+    // Deadline
     @Test
-    public void deadline_toString() {
+    public void toString_newDeadline_correctString() {
         assertEquals("[D][x] homework (by: Thursday, 11 November 2021)",
                 new Deadline("homework", LocalDate.of(2021, 11, 11), true).toString());
     }
 
+    // ToDo
     @Test
-    public void todo_setDone() {
+    public void todoSetDone_newTask_true() {
         ToDo todo = new ToDo("homework");
         todo.setDone(true);
         assertTrue(todo.isDone());
     }
+
+    // Storage
+    @Test
+    public void getFile_always_fileExists() {
+        Storage storage = new Storage("data/duke.txt");
+        assertEquals("duke.txt", storage.getFile().getName());
+    }
+
+
 }
