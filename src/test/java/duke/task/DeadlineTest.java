e46d943b450aee3b4c73c9c27ce4db7c40f9c2a5
SkyBlaise <zsc990124@gmail.com>
2021-08-28 18:37:51 +0800

    Fix some code style for files in test

--- src/test/java/duke/task/DeadlineTest.java
+++ src/test/java/duke/task/DeadlineTest.java
@@ -12,21 +12,21 @@ class DeadlineTest {
     public void constructor_success() {
         LocalDateTime dateTime = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d yyyy, h:mm a");
-        
+
         Deadline deadline = new Deadline("test deadline", dateTime);
-        
+
         String expected = "[D][ ] test deadline (by: " + dateTime.format(formatter) + ")";
         assertEquals(expected, deadline.toString());
     }
-    
+
     @Test
     public void markAsDone_success() {
         LocalDateTime dateTime = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d yyyy, h:mm a");
-        
+
         Deadline deadline = new Deadline("test deadline", dateTime);
         deadline.markAsDone();
-        
+
         String expected = "[D][X] test deadline (by: " + dateTime.format(formatter) + ")";
         assertEquals(expected, deadline.toString());
     }
--- src/test/java/duke/task/TodoTest.java
+++ src/test/java/duke/task/TodoTest.java
@@ -8,16 +8,16 @@ class TodoTest {
     @Test
     public void constructor_success() {
         Todo todo = new Todo("test todo");
-        
+
         String expected = "[T][ ] test todo";
         assertEquals(expected, todo.toString());
     }
-    
+
     @Test
     public void markAsDone_success() {
         Todo todo = new Todo("test todo");
         todo.markAsDone();
-        
+
         String expected = "[T][X] test todo";
         assertEquals(expected, todo.toString());
     }
