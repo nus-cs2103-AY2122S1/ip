6d29c987829671dbac68322303c32cea748efce3
sj1999-BIT <e0543446@u.nus.edu.sg>
2021-08-28 21:04:37 +0800

    Level 3. Mark as Done
    Simplified overall structure. Added an ArrayList as global variable. changed the property of done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,27 @@ import java.util.*;
 public class Duke {
 
     private String line = "__________________________________\n";
+    private ArrayList<Task> taskList = new ArrayList<>();
+
+    private class Task {
+        private String done = "[] ";
+        private String action;
+
+        public Task(String action) {
+            this.action = action;
+        }
+
+        public void done() {
+            System.out.println("Nice! I've marked this task as done: ");
+            done = "[X] ";
+            System.out.println("    " + this);
+        }
+
+        public String toString() {
+            return done + action;
+        }
+    }
+
     private void greet() {
             System.out.println(line);
             String logo = " ____        _        \n"
@@ -22,30 +43,43 @@ public class Duke {
         System.out.println(line);
     }
 
-    private boolean echo(ArrayList<String> lst) {
+    private boolean echo() {
         Scanner user = new Scanner(System.in);
         String input = user.nextLine();
         if (input.equals("bye")) {
             bye();
             return false;
-        } else if (input.equals("list")) {
-            showList(lst);
-            return true;
         } else {
-            lst.add(input);
-            System.out.println(line);
-            System.out.println("added: " + input);
-            System.out.println(line);
-            return true;
+            return choiceOfAction(input);
         }
     }
 
-    private void showList(ArrayList<String> lst) {
-        int count = 0;
+    private boolean choiceOfAction(String input) {
+        if (input.equals("list")) {
+            showList();
+            return true;
+        } else if (input.length() > 4 && input.substring(0, 4).equals("done")) {
+            int index = Character.getNumericValue(input.charAt(5));
+            if (index <= taskList.size() && index > 0) {
+                taskList.get(index - 1).done();
+            } else {
+                System.out.println("invalid index");
+            }
+        } else {
+            Task newTask = new Task(input);
+            taskList.add(newTask);
+            System.out.println(line);
+            System.out.println("added:" + newTask);
+        }
+        System.out.println(line);
+        return true;
+    }
 
-        for (String str : lst ) {
+    private void showList() {
+        int count = 0;
+        for (Task action : taskList ) {
             count++;
-            System.out.format("%d. " + str + "\n", count);
+            System.out.format("%d. " + action + "\n", count);
         }
         System.out.println(line);
     }
@@ -53,11 +87,10 @@ public class Duke {
 
     public static void main(String[] args) {
         Duke bot = new Duke();
-        ArrayList<String> list = new ArrayList<>();
         bot.greet();
         boolean end = true;
         while (end) {
-            end = bot.echo(list);
+            end = bot.echo();
         }
     }
 }
