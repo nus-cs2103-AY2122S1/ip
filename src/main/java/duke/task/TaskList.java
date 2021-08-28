b88f3f3cc55806e71d2ba3d95c16fa53f2ea63f1
Kangxun <e0543787@u.nus.edu>
2021-08-28 20:28:25 +0800

    Add JavaDoc comments

--- src/main/java/duke/task/TaskList.java
+++ src/main/java/duke/task/TaskList.java
@@ -6,39 +6,90 @@ import duke.DukeException;
 // import java packages
 import java.util.ArrayList;
 
+/**
+ * Represents a list of tasks to be completed. Supports operations on the tasks.
+ */
 public class TaskList {
     private ArrayList<Task> tasks;
 
+    /**
+     * Creates a TaskList for Duke chatbot using a given list of tasks.
+     *
+     * @param tasks Filepath to save tasks and load tasks.
+     */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Creates an empty TaskList for Duke chatbot.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Retrieves a task.
+     *
+     * @param i Index of task to be retrieved.
+     * @return Task.
+     */
     public Task get(int i) {
         return tasks.get(i);
     }
 
+    /**
+     * Retrieves length of the list of tasks.
+     *
+     * @return Length of task list.
+     */
     public int getLength() {
         return tasks.size();
     }
 
+    /**
+     * Retrieves the list of tasks.
+     *
+     * @return Tasks in an ArrayList.
+     */
     public ArrayList<Task> getTasks() {
         return this.tasks;
     }
 
-    public void markDone(int i) throws DukeException {
+    /**
+     * Marks a task as done.
+     *
+     * @param i Index of task to be marked.
+     */
+    public void markDone(int i) {
         tasks.get(i).markDone();
     }
 
-    public void add(Task t) throws DukeException {
+    /**
+     * Adds a task to the list of tasks.
+     *
+     * @param t Task to be added.
+     */
+    public void add(Task t) {
         tasks.add(t);
     }
 
-    public void remove(int i) throws DukeException {
+    /**
+     * Removes a task from the list of tasks.
+     *
+     * @param i Index of task to be removed.
+     */
+    public void remove(int i) {
         tasks.remove(i);
     }
 
+    /**
+     * Converts TaskList to string format.
+     *
+     * @return TaskList as a string.
+     */
+    @Override
+    public String toString() {
+        return "contains tasks";
+    }
 }
--- src/main/java/duke/util/Parser.java
+++ src/main/java/duke/util/Parser.java
@@ -1,77 +1,165 @@
 package duke.util;
 
+/**
+ * Parses the commands given by a user.
+ */
 public class Parser {
 
+    /**
+     * Parses a done command.
+     *
+     * @param cmd Command to be processed.
+     */
     public static int parseDoneCmd(String cmd) {
         String[] a = cmd.split(" ");
         return Integer.parseInt(a[1]);
     }
 
+    /**
+     * Checks if command is bye.
+     *
+     * @param cmd Command to be processed.
+     */
     public static boolean isNotBye(String cmd) {
         return !cmd.equals("bye");
     }
 
+    /**
+     * Checks if command is list.
+     *
+     * @param cmd Command to be processed.
+     */
     public static boolean isList(String cmd) {
         return cmd.equals("list");
     }
 
+    /**
+     * Checks if command is done.
+     *
+     * @param cmd Command to be processed.
+     */
     public static boolean isDone(String cmd) {
         return cmd.matches("^done [0-9]+$");
     }
 
+    /**
+     * Checks if command is a valid task.
+     *
+     * @param cmd Command to be processed.
+     */
     public static boolean isValidTask(String cmd) {
         return cmd.matches("^todo .*$")
                 || cmd.matches("^deadline .* /by .*$")
                 || cmd.matches("^event .* /at .*$");
     }
 
+    /**
+     * Checks if command is missing description.
+     *
+     * @param cmd Command to be processed.
+     */
     public static boolean isMissingArg(String cmd) {
         String[] a = cmd.split(" ", 2);
         return a[1].matches(" *");
     }
 
+    /**
+     * Gets the task name in a command.
+     *
+     * @param cmd Command to be processed.
+     */
     public static String getTaskName(String cmd) {
         String[] a = cmd.split(" ", 2);
         return a[0];
     }
 
+    /**
+     * Gets the task description in a command.
+     *
+     * @param cmd Command to be processed.
+     */
     public static String getDesc(String cmd) {
         String[] a = cmd.split(" ", 2);
         return a[1];
     }
 
+    /**
+     * Gets the deadline information in a deadline description.
+     *
+     * @param desc Command description to be processed.
+     */
     public static String getDlInfo(String desc) {
         String[] a = desc.split(" /by ", 2);
         return a[0];
     }
 
+    /**
+     * Gets the deadline due date in a deadline description.
+     *
+     * @param desc Command description to be processed.
+     */
     public static String getDlDue(String desc) {
         String[] a = desc.split(" /by ", 2);
         return a[1];
     }
 
+    /**
+     * Gets the event information in a event description.
+     *
+     * @param desc Command description to be processed.
+     */
     public static String getEvInfo(String desc) {
         String[] a = desc.split(" /at ", 2);
         return a[0];
     }
 
+    /**
+     * Gets the event due date in a event description.
+     *
+     * @param desc Command description to be processed.
+     */
     public static String getEvDue(String desc) {
         String[] a = desc.split(" /at ", 2);
         return a[1];
     }
 
+    /**
+     * Checks if command is delete.
+     *
+     * @param cmd Command to be processed.
+     */
     public static boolean isDelete(String cmd) {
         return cmd.matches("^delete [0-9]+$");
     }
 
+    /**
+     * Parses a delete command.
+     *
+     * @param cmd Command to be processed.
+     */
     public static int parseDeleteCmd(String cmd) {
         String[] a = cmd.split(" ");
         return Integer.parseInt(a[1]);
     }
 
+    /**
+     * Checks if command is empty.
+     *
+     * @param cmd Command to be processed.
+     */
     public static boolean isEmptyTask(String cmd) {
         return cmd.equals("todo")
                 || cmd.matches("deadline *")
                 || cmd.matches("event *");
+    }
+
+    /**
+     * Converts Parser to string format.
+     *
+     * @return Parser as a string.
+     */
+    @Override
+    public String toString() {
+        return "parses commands";
     }
 }
