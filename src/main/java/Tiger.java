1817267f93b1a04589dab3c4bbc830b15fb9add9
junghon3709 <junghonyip@gmail.com>
2021-08-28 09:25:13 +0800

    Minor test fixes

--- src/main/java/Tiger.java
+++ src/main/java/Tiger.java
@@ -10,12 +10,10 @@ public class Tiger {
     public static void main(String[] args) {
 
         // TODO: implement flags for delete done combination, invalid todo combination
-        // TODO: implement priority
 
         // features: fallthrough commands
         // priority
         // delete done, invalid todo combination
-        System.out.println("This is a test from gradle.");
 
         Ui ui = new Ui();
         ui.start();
