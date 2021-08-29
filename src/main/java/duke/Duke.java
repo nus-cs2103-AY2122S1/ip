b5e7f5dc89f62a99aaba34115b0d4cead4970d19
asherhy <hongyongrs@gmail.com>
2021-08-29 15:11:45 +0800

    update CodingStandard

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,16 +1,18 @@
+package duke;
+
 import duke.data.exception.DukeException;
 import duke.parser.Parser;
 import duke.ui.Ui;
 
 import java.util.Scanner;  // Import the Scanner class
 
+
 public class Duke {
 
     private Scanner myObj;
 
     public Duke() {
-
-        this.myObj = new Scanner(System.in);  // Create a Scanner object
+        this.myObj = new Scanner(System.in); 
     }
 
     public static void main(String[] args) {
