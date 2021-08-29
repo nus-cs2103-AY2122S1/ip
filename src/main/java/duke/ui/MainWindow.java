fb63cdca168ecd12f87accd4846a2ff1046f0473
Tan Jia Xian <e0543981@u.nus.edu>
2021-08-29 19:30:11 +0800

    Add handleExitProgram while closes application when user types Bye

--- src/main/java/duke/ui/MainWindow.java
+++ src/main/java/duke/ui/MainWindow.java
@@ -1,5 +1,6 @@
 package duke.ui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -7,8 +8,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import duke.Duke;
+import org.w3c.dom.Text;
+
 
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
@@ -46,13 +50,27 @@ public class MainWindow extends AnchorPane {
      * the dialog container. Clears the user input after processing.
      */
     @FXML
-    private void handleUserInput() {
+    private void handleUserInput(ActionEvent event) {
         String input = userInput.getText();
+        if (input.equals("bye")) { // Not ideal, how to fix?
+            handleExitProgram(event);
+        }
+
         String response = duke.getResponse(input);
         dialogContainer.getChildren().addAll(
                 DialogBox.getUserDialog(input, userImage),
                 DialogBox.getDukeDialog(response, dukeImage)
         );
         userInput.clear();
+    }
+
+    private void handleExitProgram(ActionEvent event) {
+        Object o = event.getSource();
+        if (o instanceof Button) {
+            ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+        } else {
+            ((Stage)(((TextField)event.getSource()).getScene().getWindow())).close();
+        }
+
     }
 }
