be2314a269106016cbca3d47560b9eb9775cf8bb
pranav_ganesh <e0661424@u.nus.edu>
2021-08-29 18:06:18 +0800

    Finish A-JUnit

---
+++ src/main/java/duke/task/TodoTest.java
@@ -0,0 +1,27 @@
+package duke.task;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class TodoTest {
+    @Test
+    public void getStatusIcon_todoObject_X() {
+        Task task = new Todo("Watch football");
+        task.markAsDone();
+        Assert.assertEquals("X", task.getStatusIcon());
+    }
+
+    @Test
+    public void taskListOnDisk_todoObject_success() {
+        Task task = new Todo("Watch football");
+        task.markAsDone();
+        Assert.assertEquals("duke.task.Todo |X| Watch football", task.taskListOnDisk());
+    }
+
+    @Test
+    public void toString_todoObject_success() {
+        Task task = new Todo("Watch football");
+        task.markAsDone();
+        Assert.assertEquals("[T][X] Watch football", task.toString());
+    }
+}
