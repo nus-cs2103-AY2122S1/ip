881bfc16cd91490b08c5b4dd562fc04a199646d3
CheahYan <cheahyan1103@gmail.com>
2021-08-29 23:16:40 +0800

    Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,30 @@
+<<<<<<< Updated upstream:src/main/java/Deadline.java
 public class Deadline extends Task {
     private String date;
+=======
+package duke;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Deadline extends Task {
+
+    protected LocalDateTime date;
+>>>>>>> Stashed changes:src/main/java/duke/Deadline.java
 
     public Deadline(String description, String date) {
         super(description);
-        this.date = date;
+
+<<<<<<< Updated upstream:src/main/java/Deadline.java
+=======
+        DateTimeFormatter scanned = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+        this.date = LocalDateTime.parse(date, scanned);
     }
 
+>>>>>>> Stashed changes:src/main/java/duke/Deadline.java
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.date + ")";
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd MMM yyyy hh:mma");
+        return "[D]" + super.toString() + " (by: " + this.date.format(dateFormat) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,31 @@
+<<<<<<< Updated upstream:src/main/java/Event.java
 public class Event extends Task {
 
     private String date;
+=======
+package duke;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Event extends Task {
+
+    protected LocalDateTime date;
+>>>>>>> Stashed changes:src/main/java/duke/Event.java
 
     public Event(String description, String date) {
         super(description);
-        this.date = date;
+
+<<<<<<< Updated upstream:src/main/java/Event.java
+=======
+        DateTimeFormatter scanned = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+        this.date = LocalDateTime.parse(date, scanned);
     }
 
+>>>>>>> Stashed changes:src/main/java/duke/Event.java
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + this.date + ")";
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd MMM yyyy hh:mma");
+        return "[E]" + super.toString() + " (at: " + this.date.format(dateFormat) + ")";
     }
 }
