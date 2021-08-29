78815a34fc35f2a396b4c75aac4c8a92188ae050
Tan Jia Xian <e0543981@u.nus.edu>
2021-08-29 23:46:02 +0800

    Change avatar images

--- src/main/java/duke/ui/MainWindow.java
+++ src/main/java/duke/ui/MainWindow.java
@@ -29,8 +29,8 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/UserAvatar.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/LollipopAvatar.png"));
 
     @FXML
     public void initialize() {
