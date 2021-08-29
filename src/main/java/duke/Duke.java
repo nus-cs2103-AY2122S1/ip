2a18a809fc57ea1de46b4e56b0fb286bf66783c5
charltonator <charltonlucian@gmail.com>
2021-08-29 14:09:25 +0800

    Add Gradle

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,12 +1,9 @@
 package duke;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
+
 
 import java.util.Scanner;
 
-public class Duke extends Application {
+public class Duke {
 
     public static void main(String[] args) {
         GreetingBot newBot = new GreetingBot();
@@ -14,15 +11,7 @@ public class Duke extends Application {
 
     }
 
-    @Override
-    public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
 
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
-
-    }
 }
 
 
