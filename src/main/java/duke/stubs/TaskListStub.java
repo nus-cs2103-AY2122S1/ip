099ff7235bca813e39383785aa8938aa5823afec
Eunice <eunicesimsingyee@gmail.com>
2021-08-30 01:45:44 +0800

    Add build.gradle and update test case for checkDate method

--- src/main/java/duke/stubs/TaskListStub.java
+++ src/main/java/duke/stubs/TaskListStub.java
@@ -39,6 +39,12 @@ public class TaskListStub implements TaskListInterface {
 
     @Override
     public ArrayList<Task> checkDate(LocalDate date) {
+
+        if (date.equals(LocalDate.parse("2021-12-04"))) {
+            ArrayList<Task> arrayList = new ArrayList<>();
+            arrayList.add(new DeadlineStub());
+            return arrayList;
+        }
         return null;
     }
 }
