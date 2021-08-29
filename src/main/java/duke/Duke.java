fd69d7a894797de83e12498586d309bd48caed0d
charltonator <charltonlucian@gmail.com>
2021-08-29 13:29:51 +0800

    Add Launcher

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,11 +1,26 @@
 package duke;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 import java.util.Scanner;
 
-public class Duke {
+public class Duke extends Application {
 
     public static void main(String[] args) {
         GreetingBot newBot = new GreetingBot();
         newBot.startBot("./data/list.txt");
+
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
 
     }
 }
