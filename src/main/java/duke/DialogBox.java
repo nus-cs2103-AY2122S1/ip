ce57b14d0e923defaef76c299de8e6bfc94a866f
Adam Oh Zhi Hong <adamohzhihong@gmail.com>
2021-08-29 17:51:29 +0800

    Add background color, padding, circle clipping

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -3,11 +3,17 @@ package duke;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 
 public class DialogBox extends HBox {
@@ -19,9 +25,15 @@ public class DialogBox extends HBox {
         displayPicture = iv;
 
         text.setWrapText(true);
+        text.setPadding(new Insets(5));
+        Circle circle = new Circle(75);
+        circle.setCenterX(75);
+        circle.setCenterY(75);
+        displayPicture.setClip(circle);
 
         this.setAlignment(Pos.TOP_RIGHT);
         this.getChildren().addAll(text, displayPicture);
+        this.setPadding(new Insets(10, 0, 10, 0));
     }
 
     public void flip() {
@@ -37,6 +49,10 @@ public class DialogBox extends HBox {
 
     public static DialogBox getDukeDialog(Label l, ImageView iv) {
         var db = new DialogBox(l, iv);
+        Paint dukeColor = Paint.valueOf("C5C5C5");
+        BackgroundFill dukeBackgroundFill = new BackgroundFill(dukeColor, null, null);
+        Background dukeBackground = new Background(dukeBackgroundFill);
+        db.setBackground(dukeBackground);
         db.flip();
         return db;
     }
