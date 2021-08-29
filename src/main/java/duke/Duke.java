2fff15e7cc22b86d1915ebce0b38c9a1be14143f
cookiedan42 <cookiedan42@gmail.com>
2021-08-29 18:30:15 +0800

    Add basic GUI functionality bootstrapped off CLI responses

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,51 +1,56 @@
 package duke;
 
 import duke.exceptions.DukeException;
+import duke.exceptions.DukeExitException;
+import duke.gui.Main;
 import duke.storage.Storage;
+import javafx.application.Application;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 /**
  * Main class for Duke.
  */
 public class Duke {
     static String border = "--------------------------------------------------";
-
+    private Parser parser;
+    private UI ui;
+    private TaskArrayList taskList;
+    private Path storagePath;
     /**
      * Main entry point for Duke project.
      *
      * @param args unused.
      */
     public static void main(String[] args) {
-        UI ui = new UI();
-
-        Path storagePath = Paths.get(".", "data", "duke.txt");
-        TaskArrayList taskList;
-        try {
-            taskList = Storage.load(storagePath);
-        } catch (DukeException | IOException e) {
-            taskList = new TaskArrayList();
-            ui.showLoadingError();
-        }
-        ui.displayLogo();
-        ui.displayWelcome();
-
-        Scanner userScanner = new Scanner(System.in);
-        Parser parser = new Parser(ui, taskList, storagePath);
-
-        boolean isExit = false;
-        while (!isExit) {
-            String userInput = userScanner.nextLine();
-            try {
-                isExit = parser.run(userInput);
-            } catch (DukeException e) {
-                ui.displayException(e);
-            } finally {
-                Storage.dump(taskList, storagePath);
-            }
-        }
+        Application.launch(Main.class, args);
     }
+
+    public Duke(){
+        storagePath = Paths.get(".", "data", "duke.txt");
+        try {
+            this.taskList = Storage.load(storagePath);
+        } catch (DukeException | IOException e) {
+            this.taskList = new TaskArrayList();
+        }
+        this.ui = new UI();
+        this.parser = new Parser(ui, taskList, storagePath);
+    }
+
+
+    public String getResponse(String userInput) throws DukeExitException{
+        try {
+            return parser.run(userInput);
+        }catch (DukeExitException e){
+            throw e;
+        } catch (DukeException e) {
+            return e.getMessage();
+        } finally {
+            Storage.dump(taskList, storagePath);
+        }
+//        return userInput;
+    }
+
 }
