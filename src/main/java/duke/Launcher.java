51d3732f58d66705f17d373e36b8b46f1605e57f
Adam Oh Zhi Hong <adamohzhihong@gmail.com>
2021-08-29 17:06:16 +0800

    Add basic GUI, Duke echos the user

---
+++ src/main/java/duke/Launcher.java
@@ -0,0 +1,12 @@
+package duke;
+
+import javafx.application.Application;
+
+/**
+ * JavaFx Launcher class to start the GUI for Duke
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Duke.class, args);
+    }
+}
