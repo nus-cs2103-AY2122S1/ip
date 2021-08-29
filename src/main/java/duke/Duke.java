606add481fbcb05c6945db690b755ddd351fc68a
Ruofan1023 <e0563986@u.nus.edu>
2021-08-29 13:06:00 +0800

    Complete Level-10

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -14,7 +14,6 @@ enum ActionType {
  */
 public class Duke {
 
-    private boolean isLive = true;
     private final Storage storage;
     private final TaskList taskList;
     private final Ui ui;
@@ -28,81 +27,78 @@ public class Duke {
         storage = new Storage();
         taskList = new TaskList(storage.loadFromDisk());
         ui = new Ui(this.taskList);
-        ui.greet();
     }
 
+
     /**
-     * Shows if the duke is still active.
+     * Gets the response from the duke given a input.
      *
-     * @return whether the duke is still active.
+     * @param input input command
+     * @return duke's response
      */
-    public boolean isLive() {
-        return isLive;
+    public String getResponse(String input) {
+        try {
+            return processCommand(input);
+        } catch (DukeException dukeException) {
+            return dukeException.toString();
+        }
     }
 
     /**
      * Processes the command input by user.
      *
      * @param command command input of the user.
+     * @return duke's response
      * @throws DukeException exceptions when processing the command
      */
-    public void processCommand(String command) throws DukeException {
+    public String processCommand(String command) throws DukeException {
         ActionType actionType = parser.parseActionType(command);
         switch (actionType) {
         case LIST:
-            ui.displayTasks();
-            break;
+            return ui.displayTasks();
         case TODO: {
             String task = parser.parseTask(command);
             ToDo toDo = taskList.createTodo(task);
             taskList.addTask(toDo);
-            ui.showAddedTask(toDo);
             storage.saveProgress();
-            break;
+            return ui.showAddedTask(toDo);
         }
         case DEADLINE: {
             String task = parser.parseTask(command);
             DeadLine ddl = taskList.createDeadLine(task);
             taskList.addTask(ddl);
-            ui.showAddedTask(ddl);
             storage.saveProgress();
-            break;
+            return ui.showAddedTask(ddl);
         }
         case EVENT: {
             String task = parser.parseTask(command);
             Event event = taskList.createEvent(task);
             taskList.addTask(event);
-            ui.showAddedTask(event);
             storage.saveProgress();
-            break;
+            return ui.showAddedTask(event);
         }
         case DONE: {
             int taskIdx = parser.getTaskIdx(command, taskList.size());
             Task toBeMarked = taskList.get(taskIdx - 1);
             taskList.markTaskAsDone(taskIdx - 1);
-            ui.showMarkedAsDone(toBeMarked);
             storage.saveProgress();
-            break;
+            return ui.showMarkedAsDone(toBeMarked);
         }
         case DELETE: {
             int taskIdx = parser.getTaskIdx(command, taskList.size());
             Task toBeDeleted = taskList.get(taskIdx - 1);
             taskList.deleteTask(toBeDeleted);
-            ui.showDeletedTask(toBeDeleted);
             storage.saveProgress();
-            break;
+            return ui.showDeletedTask(toBeDeleted);
         }
         case FIND: {
             String keyword = parser.parseTask(command);
             List<Task> matchedTasks = taskList.findTask(keyword);
-            ui.showFoundTask(matchedTasks);
-            break;
+            return ui.showFoundTask(matchedTasks);
         }
         case BYE: {
             storage.saveProgress();
-            ui.bye();
-            this.isLive = false;
-            break;
+            return ui.bye();
         }
         default:
             throw new UnrecognizableCommandException();
--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -1,27 +1,58 @@
 package duke;
-import java.util.Scanner;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * Main class for execution of the duke.
  */
-class Main {
+public class Main extends Application {
+
+    private Duke duke = new Duke();
+
+    /**
+     * Main entry for the program.
+     *
+     * @param args arguments.
+     */
     public static void main(String[] args) {
-        run();
+        Application.launch(Main.class, args);
+
     }
 
     /**
-     * Creates an interactive duke.
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param stage the primary stage for this application, onto which
+     *              the application scene can be set.
+     *              Applications may create other stages, if needed, but they will not be
+     *              primary stages.
+     * @throws Exception if something goes wrong
      */
-    public static void run() {
-        Duke duke = new Duke();
-        Scanner sc = new Scanner(System.in);
-        while (duke.isLive()) {
-            String command = sc.nextLine();
-            try {
-                duke.processCommand(command);
-            } catch (DukeException e) {
-                System.out.println(e.toString());
-            }
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+
+            MainWindow mainWindowController = fxmlLoader.<MainWindow>getController();
+            mainWindowController.setDuke(duke);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
