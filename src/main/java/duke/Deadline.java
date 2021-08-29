4c031466b85bbe18e671139421800cca38aa812b
CheahYan <cheahyan1103@gmail.com>
2021-08-29 23:39:46 +0800

    debugging after merge conflicts

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -1,13 +1,3 @@
-<<<<<<< HEAD:src/main/java/duke/Deadline.java
-package duke;
-
-public class Deadline extends Task {
-    protected String date;
-=======
-<<<<<<< Updated upstream:src/main/java/Deadline.java
-public class Deadline extends Task {
-    private String date;
-=======
 package duke;
 
 import java.time.LocalDateTime;
@@ -16,22 +6,12 @@ import java.time.format.DateTimeFormatter;
 public class Deadline extends Task {
 
     protected LocalDateTime date;
->>>>>>> Stashed changes:src/main/java/duke/Deadline.java
->>>>>>> parent of b489ae1 (Revert "Level-8"):src/main/java/Deadline.java
 
     public Deadline(String description, String date) {
         super(description);
 
-<<<<<<< Updated upstream:src/main/java/Deadline.java
-=======
         DateTimeFormatter scanned = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
         this.date = LocalDateTime.parse(date, scanned);
-    }
-
->>>>>>> Stashed changes:src/main/java/duke/Deadline.java
-    @Override
-    public String convertToFile() {
-        return super.description + " | " + this.date;
     }
 
     @Override
--- src/main/java/duke/Event.java
+++ src/main/java/duke/Event.java
@@ -1,15 +1,3 @@
-<<<<<<< HEAD:src/main/java/duke/Event.java
-package duke;
-
-public class Event extends Task {
-
-    protected String date;
-=======
-<<<<<<< Updated upstream:src/main/java/Event.java
-public class Event extends Task {
-
-    private String date;
-=======
 package duke;
 
 import java.time.LocalDateTime;
@@ -18,22 +6,13 @@ import java.time.format.DateTimeFormatter;
 public class Event extends Task {
 
     protected LocalDateTime date;
->>>>>>> Stashed changes:src/main/java/duke/Event.java
->>>>>>> parent of b489ae1 (Revert "Level-8"):src/main/java/Event.java
+
 
     public Event(String description, String date) {
         super(description);
 
-<<<<<<< Updated upstream:src/main/java/Event.java
-=======
         DateTimeFormatter scanned = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
         this.date = LocalDateTime.parse(date, scanned);
-    }
-
->>>>>>> Stashed changes:src/main/java/duke/Event.java
-    @Override
-    public String convertToFile() {
-        return super.description + " | " + this.date;
     }
 
     @Override
