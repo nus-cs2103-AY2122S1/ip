0522a085a1f48b178d3bc26b4f7ea9bd41bd73a6
Darren Heng <darren2pro@gmail.com>
2021-08-29 20:12:19 +0800

    Add launcher class

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,11 +6,15 @@ import duke.utils.Parser;
 import duke.utils.Storage;
 import duke.utils.TaskList;
 import duke.utils.Ui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * A chat-bot called Naruto that acts as a task list.
  */
-public class Duke {
+public class Duke extends Application {
     private final Storage storage;
     private final TaskList tasks;
     private final Ui ui;
@@ -21,6 +25,16 @@ public class Duke {
      * @param persistedData the relative path to the persisted data starting from the project directory.
      */
     public Duke(String persistedData) {
+        ui = new Ui();
+        storage = new Storage(persistedData);
+        tasks = new TaskList(storage.loadPersistedData());
+    }
+
+    /**
+     * Constructor for the Duke chat-bot.
+     */
+    public Duke() {
+        String persistedData = "data/duke.txt";
         ui = new Ui();
         storage = new Storage(persistedData);
         tasks = new TaskList(storage.loadPersistedData());
@@ -48,5 +62,33 @@ public class Duke {
     }
     public static void main(String[] args) {
         new Duke("data/duke.txt").run();
+    }
+
+    @Override
+    public void init() {
+        int i = 1 + 1;
+    }
+
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages.
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Naruto Chat-bot!");
+        Label helloWorld = new Label("Hello World!");
+        Scene scene = new Scene(helloWorld, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
