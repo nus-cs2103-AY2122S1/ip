86923fa0710a088d86a6aa66ed98f9e2f04336dd
Imran <imranr2@msn.com>
2021-08-30 11:50:48 +0800

    feat: Add alias for commands

--- src/main/java/lifeline/command/Command.java
+++ src/main/java/lifeline/command/Command.java
@@ -1,5 +1,8 @@
 package lifeline.command;
 
+import java.util.Arrays;
+import java.util.List;
+
 import lifeline.storage.Storage;
 import lifeline.task.TaskList;
 import lifeline.ui.Ui;
@@ -7,23 +10,29 @@ import lifeline.util.QuadFunction;
 
 public enum Command {
     // inspiration from https://alexpower.me/advanced-java-enum-features-you-need-to-know/
-    BYE(CommandHandler::handleBye),
-    DEADLINE(CommandHandler::handleDeadline),
-    DELETE(CommandHandler::handleDelete),
-    DONE(CommandHandler::handleDone),
-    EVENT(CommandHandler::handleEvent),
-    FIND(CommandHandler:: handleFind),
-    HELP(CommandHandler:: handleHelp),
-    LIST(CommandHandler::handleList),
-    TODO(CommandHandler::handleToDo);
+    BYE(CommandHandler::handleBye, "bye", "bb", "goodbye", "b"),
+    DEADLINE(CommandHandler::handleDeadline, "deadline", "d", "dl"),
+    DELETE(CommandHandler::handleDelete, "delete", "del"),
+    DONE(CommandHandler::handleDone, "done", "complete"),
+    EVENT(CommandHandler::handleEvent, "event", "e"),
+    FIND(CommandHandler:: handleFind, "find", "f"),
+    HELP(CommandHandler:: handleHelp, "help", "h"),
+    LIST(CommandHandler::handleList, "list", "ls", "l"),
+    TODO(CommandHandler::handleToDo, "todo", "t", "td");
 
     private QuadFunction<String, Storage, TaskList, Ui> execute;
+    private List<String> aliases;
 
-    Command(QuadFunction<String, Storage, TaskList, Ui> execute) {
+    Command(QuadFunction<String, Storage, TaskList, Ui> execute, String... aliases) {
         this.execute = execute;
+        this.aliases = Arrays.asList(aliases);
     }
 
     public QuadFunction<String, Storage, TaskList, Ui> getExecute() {
         return execute;
+    }
+
+    public boolean hasCommand(String command) {
+        return this.aliases.contains(command);
     }
 }
