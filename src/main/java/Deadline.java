985e487eb8e56dc1a9c1e779a13828fae113da4b
naranghardik16 <naranghardik16@gmail.com>
2021-08-30 14:26:07 +0800

    Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,16 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    private String by;
+    private LocalDateTime by;
 
     public Deadline (String description, String by){
         super(description);
-        this.by = by;
-    }
-
-    public String getBy(){
-        return by;
+        this.by = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm"));
     }
 
     @Override
     public String showTask(){
-        return "[D][" + (isDone ? "✗" : " ") + "] " + description + "(by: " + by + ")";
+        return "[D][" + (isDone ? "✗" : " ") + "] " + description + "(by: " + by.format(DateTimeFormatter.ofPattern("MMM dd yyyy, HH:mm")) + ")";
     }
 }
