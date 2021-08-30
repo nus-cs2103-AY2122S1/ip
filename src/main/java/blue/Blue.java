6296684ad29159fbbe0a7449ad608d4f9a6a03f9
GnohChengYi <chengyignoh@gmail.com>
2021-08-30 15:36:23 +0800

    Abstract out graphic interactions with user, use Blue to launch

--- src/main/java/blue/Blue.java
+++ src/main/java/blue/Blue.java
@@ -12,38 +12,17 @@ import blue.handler.FindHandler;
 import blue.handler.ListHandler;
 import blue.handler.ToDoHandler;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 /**
  * Entry point of the Blue application.
  * Initializes the application and interacts with the user.
  */
-public class Blue extends Application {
+public class Blue {
     private static final String DEFAULT_FILEPATH = "data/tasks.txt";
     private final Storage storage;
     private TaskList tasks;
     private final Ui ui;
     private HashMap<String, CommandHandler> commandHandlers;
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private AnchorPane mainLayout;
-
-    /**
-     * Default constructor to avoid NoSuchElementExceptions
-     */
-    public Blue() {
-        this(DEFAULT_FILEPATH);
-    }
 
     /**
      * Constructs a Blue instance.
@@ -127,59 +106,7 @@ public class Blue extends Application {
      * @param args Ignored.
      */
     public static void main(String[] args) {
+        Application.launch(GUi.class, args);
         new Blue("data/tasks.txt").run();
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        designAndShowLayout(primaryStage);
-        styleControls(primaryStage);
-    }
-
-    private void designAndShowLayout(Stage stage) {
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    private void styleControls(Stage stage) {
-        stage.setTitle("Blue");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-        AnchorPane.setLeftAnchor(userInput, 1.0);
     }
 }
---
+++ src/main/java/blue/DialogBox.java
@@ -0,0 +1,29 @@
+package blue;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class DialogBox extends HBox {
+    private Label text;
+    private ImageView displayPicture;
+
+    /**
+     * Creates dialog box.
+     *
+     * @param label     The Text Label.
+     * @param imageView The image of the character.
+     */
+    public DialogBox(Label label, ImageView imageView) {
+        text = label;
+        displayPicture = imageView;
+
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+
+        this.setAlignment(Pos.TOP_RIGHT);
+        this.getChildren().addAll(text, displayPicture);
+    }
+}
