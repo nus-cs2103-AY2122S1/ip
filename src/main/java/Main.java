bcc149e16c2e2f983d799f9f4ab73aa0b096f1c6
Ricky Andrew <ricky-aandrew@hotmail.com>
2021-08-30 11:50:00 +0800

    Start on launching an Application

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import seedu.duke.Duke;
 
-public class Main {
+public class Main extends Application {
     /**
      * Runs the main Duke ToDo Application
      * 
@@ -15,5 +19,14 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         Duke duke = new Duke();
         duke.start();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
