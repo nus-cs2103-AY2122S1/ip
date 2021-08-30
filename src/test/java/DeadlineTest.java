26a23bc63a234b9b0061f23b0c7b1acd1e7b6e19
Allard <AllardQuek@users.noreply.github.com>
2021-08-31 01:08:24 +0800

    Remove unused import

--- src/test/java/DeadlineTest.java
+++ src/test/java/DeadlineTest.java
@@ -1,5 +1,4 @@
 import duke.task.Deadline;
-import duke.task.ToDo;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -9,17 +8,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DeadlineTest {
 	@Test
-    public void testDeadlineDisplayedCorrectly() {
-	    String datetime = "2021-08-27";
-	    LocalDate deadline = LocalDate.parse(datetime);
-	    String taskName = "finish ip project";
-        Deadline deadlineTask = new Deadline(taskName, deadline);
-	    String expectedResult = String.format("[D][ ] %s (by: Aug 27 2021)", taskName);
-        assertEquals(expectedResult, deadlineTask.toString());
-    }
+	public void testDeadlineDisplayedCorrectly() {
+		String datetime = "2021-08-27";
+		LocalDate deadline = LocalDate.parse(datetime);
+		String taskName = "finish ip project";
+		Deadline deadlineTask = new Deadline(taskName, deadline);
+		String expectedResult = String.format("[D][ ] %s (by: Aug 27 2021)", taskName);
+		assertEquals(expectedResult, deadlineTask.toString());
+	}
 
 	@Test
-	public void testDeadlineTaskIsCompleted(){
+	public void testDeadlineTaskIsCompleted() {
 		String datetime = "2021-08-27";
 		LocalDate deadline = LocalDate.parse(datetime);
 		String taskName = "finish ip project";
