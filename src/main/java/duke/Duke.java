5c2063027f1e68dc9b049a35203bb25835a11b49
Koh Jia Xian <39460640+koh-jx@users.noreply.github.com>
2021-08-30 12:33:31 +0800

    Setup JavaFX

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,40 +1,54 @@
 package duke;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 import task.TaskList;
 
 /**
  * Main Duke class
  */
-public class Duke {
-    private final DukeListener listener;
+public class Duke extends Application {
+//    private final DukeListener listener;
+//
+//    /**
+//     * Constructor
+//     * Instantiates a (saved) task.Task List and a duke.DukeListener for duke.Duke
+//     */
+//    private Duke() {
+//        TaskList taskList = Storage.loadList();
+//        DukeParser parser = new DukeParser(taskList);
+//        listener = new DukeListener(parser);
+//    }
+//
+//    /**
+//     * Main function
+//     *
+//     * @param args No arguments need to be passed in
+//     */
+//    public static void main(String[] args) {
+//        Duke duke = new Duke();
+//        duke.begin();
+//    }
+//
+//    /**
+//     * Function that starts listening for the command
+//     */
+//    private void begin() {
+//        Ui.intro();
+//        listener.startListen();
+//        Ui.goodBye();
+//    }
 
-    /**
-     * Constructor
-     * Instantiates a (saved) task.Task List and a duke.DukeListener for duke.Duke
-     */
-    private Duke() {
-        TaskList taskList = Storage.loadList();
-        DukeParser parser = new DukeParser(taskList);
-        listener = new DukeListener(parser);
-    }
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
 
-    /**
-     * Main function
-     *
-     * @param args No arguments need to be passed in
-     */
-    public static void main(String[] args) {
-        Duke duke = new Duke();
-        duke.start();
-    }
-
-    /**
-     * Starts the Duke system
-     */
-    private void start() {
-        Ui.intro();
-        listener.startListen();
-        Ui.goodBye();
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
 }
