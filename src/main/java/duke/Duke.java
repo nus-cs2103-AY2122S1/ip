34e32e746963dead7c90c42b2ea565d75bb85592
Javier <javierphon@gmail.com>
2021-08-30 23:07:01 +0800

    Reorganise Duke class

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -4,22 +4,12 @@ import java.io.IOException;
 
 import duke.command.Command;
 import duke.exception.DukeException;
+import duke.gui.Launcher;
 import duke.util.Parser;
 import duke.util.Storage;
 import duke.util.TaskList;
 import duke.util.Ui;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 /**
  * Represents a chat bot called Duke.
@@ -27,24 +17,19 @@ import javafx.stage.Stage;
  * @author Javier Phon Zhee Kai.
  * @version CS2103T AY21/22 Sem 1.
  */
-public class Duke extends Application {
+public class Duke {
     private static final String FILEPATH = "data/duke.txt";
-    private final Image duke = new Image(this.getClass()
-            .getResourceAsStream("/images/DukeIcon.png"));
-    private final Image user = new Image(this.getClass()
-            .getResourceAsStream("/images/UserIcon.png"));
     private Storage storage;
     private TaskList taskList;
     private final Ui ui;
 
-
     /**
      * Constructor for the Duke class.
      */
-    public Duke() {
+    public Duke(String filepath) {
         this.ui = new Ui();
         try {
-            this.storage = new Storage(FILEPATH);
+            this.storage = new Storage(filepath);
             this.taskList = new TaskList(this.storage.load());
         } catch (IOException exception) {
             ui.showLoadingError();
@@ -71,73 +56,7 @@ public class Duke extends Application {
         }
     }
 
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Formatting the window to look as expected.
-        ScrollPane scrollPane = new ScrollPane();
-        VBox dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        TextField userInput = new TextField();
-        Button sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        Scene scene = new Scene(mainLayout);
-        stage.setScene(scene);
-        stage.show();
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        sendButton.setOnMouseClicked((event) -> handleUserInput(dialogContainer, userInput));
-
-        userInput.setOnAction((event) -> handleUserInput(dialogContainer, userInput));
-    }
-
-    private void handleUserInput(VBox dialogContainer, TextField userInput) {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-    }
-
-    private String getResponse(String input) {
-        return "Duke heard: " + input;
-    }
-
     public static void main(String[] args) {
-        new Duke().run();
+        Application.launch(Launcher.class, args);
     }
 }
