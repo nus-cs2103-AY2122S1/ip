734632f10d05667dadda69b21acbd7214df3ea7b
GnohChengYi <chengyignoh@gmail.com>
2021-08-30 13:46:04 +0800

    Create basic GUI for Blue

--- src/main/java/blue/Blue.java
+++ src/main/java/blue/Blue.java
@@ -13,7 +13,12 @@ import blue.handler.ListHandler;
 import blue.handler.ToDoHandler;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -26,6 +31,12 @@ public class Blue extends Application {
     private TaskList tasks;
     private final Ui ui;
     private HashMap<String, CommandHandler> commandHandlers;
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+    private AnchorPane mainLayout;
 
     /**
      * Default constructor to avoid NoSuchElementExceptions
@@ -121,9 +132,54 @@ public class Blue extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Label helloWorld = new Label("Hello World!");
-        Scene scene = new Scene(helloWorld);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        designAndShowLayout(primaryStage);
+        styleControls(primaryStage);
+    }
+
+    private void designAndShowLayout(Stage stage) {
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void styleControls(Stage stage) {
+        stage.setTitle("Blue");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+        AnchorPane.setLeftAnchor(userInput, 1.0);
     }
 }
