416f6e577c17844c232f2fde66868c37ae798690
darrenhoon <darrenhoon.ad@gmail.com>
2021-08-30 12:58:39 +0800

    Add JavaFX

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,10 @@ package main.java;
 
 import main.java.tasklist.TaskList;
 import main.java.ui.Ui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * The Duke program implements an application that helps to track tasks that the user requires to be tracked.
@@ -9,7 +13,7 @@ import main.java.ui.Ui;
  * @version 1.0
  * @since   2021-08-21
  */
-public class Duke {
+public class Duke extends Application {
 
     /**
      * Initialises app.
@@ -22,5 +26,14 @@ public class Duke {
         messages.welcomeMessage();
         engine.runProgram();
         messages.goodbyeMessage();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
