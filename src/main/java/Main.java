655eb8d0a2e0ad48f8a01da6f4978e1672156ede
hmanhduc2k <hmanhduc2k@gmail.com>
2021-08-30 21:30:29 +0800

    Add checkstyle.xml, config Intellij and other areas so that checkstyle tools can be used

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 import Duke.Main.Duke;
 
 public class Main {
+    /**
+     * Main class
+     * @param args argument for Main class
+     */
     public static void main(String[] args) {
         Duke duke = new Duke("taskFile/taskList.txt");
         duke.run();
