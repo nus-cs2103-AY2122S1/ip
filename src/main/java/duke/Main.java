70c29d6c24c3f9c8448a5087db558ca58a109942
NaokiShum <cxjdwedyk097@gmail.com>
2021-08-31 03:20:07 +0800

    Add GUI

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -19,7 +19,6 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            System.out.println(Main.class.getResource("/view/MainWindow.fxml"));
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
