ecb9526def4d12bedf39fb39e319322a20dd6e35
houtenteo <houtenteo@gmail.com>
2021-08-31 09:33:43 +0800

    Add getUserDialog and getDukeResponse method

--- src/main/java/duke/Controller.java
+++ src/main/java/duke/Controller.java
@@ -2,11 +2,18 @@ package duke;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+import java.io.File;
+import java.net.URL;
 
 public class Controller {
     @FXML
@@ -18,9 +25,15 @@ public class Controller {
     @FXML
     private Button markBtn;
     @FXML
-    private TextField cmdLine;
+    private Button sendBtn;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
     @FXML
     private TextArea display;
+    @FXML
+    private ScrollPane scrollPane;
 
     private String command;
     private MyList list;
@@ -33,20 +46,42 @@ public class Controller {
     }
 
     public void enter(KeyEvent e){
+        System.out.println("HEllo");
         if (e.getCode().equals(KeyCode.ENTER)) {
             System.out.println("ENTER!");
-            this.command = this.cmdLine.getText();
-            System.out.println(this.command);
-            this.cmdLine.setText("");
-
+            handleUserInput(new ActionEvent());
         }
     }
 
-    public void listAll(ActionEvent event) {
-        String s = this.list.listAll();
-        System.out.println(s);
-        this.display.setEditable(false);
-        this.display.setText(s);
+    public void handleUserInput(ActionEvent e) {
+
+        this.command = this.userInput.getText();
+        this.userInput.setText("");
+        if (!this.command.equals("")) {
+            dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+            this.dialogContainer.getChildren().addAll(getUserDialog(), getDukeResponse());
+        }
+
+
+    }
+
+    public Label getUserDialog() {
+        Label textToAdd = new Label(this.command);
+        textToAdd.setFont(new Font(15));
+        textToAdd.setWrapText(true);
+        textToAdd.setPadding(new Insets(10));
+        return textToAdd;
+    }
+
+    public Label getDukeResponse() {
+        Parser p = new Parser(this.list, this.storage);
+        String response = p.dukeResponse(this.command);
+        Label textToAdd = new Label(response);
+        textToAdd.setWrapText(true);
+        textToAdd.setFont(new Font(15));
+        textToAdd.setPadding(new Insets(10));
+        return textToAdd;
+
     }
 
 }
