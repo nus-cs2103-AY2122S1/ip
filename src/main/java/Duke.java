eb9ea2b9d30b50b598c5592050f553318a94238d
James-Kua <james@Jamess-MacBook-Pro.local>
2021-09-01 02:35:04 +0800

    Done JavaFX tutorial 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,7 @@
-import duke.exception.DukeException;
 import duke.parser.Parser;
 import duke.storage.Storage;
 import duke.task.TaskList;
 import duke.ui.Ui;
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-import java.util.Scanner;
 
 /**
  * Duke is a personal assistant chatbot that helps keep track of various tasks.
@@ -25,7 +9,7 @@ import java.util.Scanner;
  * @author: James Kua
  * @version: Duke-Level-8
  */
-public class Duke extends Application {
+public class Duke {
 
     /** Stores location of the stored tasks. */
     private static final String LOCATION = "./data/duke.txt";
@@ -37,14 +21,6 @@ public class Duke extends Application {
     private Parser parser;
     /** Deals with making sense of the user commands. */
     private Ui ui;
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/faker.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/faker-art.png"));
 
     /**
      * Constructor for Duke.
@@ -63,63 +39,7 @@ public class Duke extends Application {
         tasks = new TaskList(storage.loadData());
     }
 
-    /**
-     * Starts the Duke program.
-     */
-    public void run() {
-        ui.greet();
-        ui.intro();
-        boolean isRunning = true;
-        parser = new Parser(tasks, storage);
-        Scanner sc = new Scanner(System.in);
-        String input;
-
-        while (isRunning) {
-            input = sc.nextLine();
-            if (parser.parseCommand(input).equals("Bye!")) {
-                isRunning = false;
-            } else {
-                Ui.printMessage(parser.parseCommand(input));
-            }
-        }
-        Ui.exit();
-    }
-
-    /**
-     * Iteration 1:
-     * Creates a label with the specified text and adds it to the dialog container.
-     *
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
-     */
-    private Label getDialogLabel(String text) {
-        // You will need to import `javafx.scene.control.Label`.
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
-    }
-
-    /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-    }
-
-    /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
-     */
-    private String getResponse(String input) {
+    public String getResponse(String input) {
         parser = new Parser(tasks, storage);
         String response = parser.parseCommand(input);
         if (response == null) {
@@ -127,74 +47,5 @@ public class Duke extends Application {
         } else {
             return response;
         }
-    }
-
-    @Override
-    public void start(Stage stage) {
-
-        //Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Step 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-    }
-
-    public static void main(String[] args) {
-//        new Duke(LOCATION).run();
-        Duke duke = new Duke();
-        duke.run();
     }
 }
