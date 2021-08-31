f60e09526db4d9a732df1a653f5cb0c25f0b0f9e
meerian <myronneozx@gmail.com>
2021-09-01 02:10:06 +0800

    Support for gradle running and testing

--- src/test/java/DateValidatorTest.java
+++ src/test/java/DateValidatorTest.java
@@ -2,10 +2,10 @@ import duke.DateValidator;
 
 import java.time.format.DateTimeFormatter;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
 public class DateValidatorTest {
--- src/test/java/TaskListTest.java
+++ src/test/java/TaskListTest.java
@@ -1,7 +1,7 @@
 import duke.DukeException;
 import duke.Task;
 import duke.TaskList;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
