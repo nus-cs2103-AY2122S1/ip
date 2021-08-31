e8243c3dd253cf22c5bf89cb06c3b7d2b45e6e51
ramapriyan912001 <ramapriyan912001@gmail.com>
2021-08-31 13:52:35 +0800

    Complete Iteration 3

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -105,6 +105,7 @@ public class Duke extends Application {
         userInput.setOnAction((event) -> {
             handleUserInput();
         });
+
     }
 
     /**
@@ -130,8 +131,8 @@ public class Duke extends Application {
         Label userText = new Label(userInput.getText());
         Label dukeText = new Label(getResponse(userInput.getText()));
         dialogContainer.getChildren().addAll(
-                new DialogBox(userText, new ImageView(user)),
-                new DialogBox(dukeText, new ImageView(duke))
+                DialogBox.getUserDialog(userText, new ImageView(user)),
+                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
         );
         userInput.clear();
     }
