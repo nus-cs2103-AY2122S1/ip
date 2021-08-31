2e7a93162f94d0953060cb030212f855fa930279
Jason <jason.ng.zq99@gmail.com>
2021-09-01 01:23:31 +0800

    Remove unnecessary code in Main.java

--- src/main/java/duke/main/Main.java
+++ src/main/java/duke/main/Main.java
@@ -1,25 +1,13 @@
 package duke.main;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.Scanner;
-
-import duke.commands.Deadline;
-import duke.commands.Event;
-import duke.commands.Find;
-import duke.commands.Task;
-import duke.commands.Todo;
 import duke.controllers.MainWindow;
-import duke.data.DukeException;
-import duke.data.TaskList;
-import duke.parser.Parser;
-import duke.storage.Storage;
-import duke.ui.Ui;
+
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * Encapsulates the entire Duke program.
@@ -30,28 +18,6 @@ import javafx.scene.layout.AnchorPane;
  * @version: Duke Level-9
  */
 public class Main extends Application {
-    /** Storage component for Duke */
-    private Storage storage;
-    /** UI component for Duke */
-    private Ui ui;
-    /** TaskList component for Duke */
-    private TaskList taskList;
-    /** Parser component for Duke */
-    private Parser parser;
-    /** Find component for Duke */
-    private Find finder;
-
-    /**
-     * Constructor for a duke.main.Main() class.
-     */
-    public Main() {
-        this.storage = new Storage();
-        this.ui = new Ui();
-        this.taskList = new TaskList(storage.Load());
-        this.parser = new Parser();
-        this.finder = new Find();
-    }
-
     @Override
     public void start(Stage stage) {
         try {
