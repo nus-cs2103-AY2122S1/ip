819bcf376c2f56fe2acf80d8d6970dcda0e76fb3
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-08-31 23:18:49 +0800

    Clean up Duke
    Removing methods that are no longer in use ensures that the code will be
    cleaner.

--- src/main/java/duke/main/Duke.java
+++ src/main/java/duke/main/Duke.java
@@ -3,11 +3,8 @@ package duke.main;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
-import duke.command.Command;
-
 /**
  * The Duke program implements an application that takes in user input and acts as task list manager for users.
- *
  * @author  Nicholas Loo
  * @version 0.1
  * @since   2021-08-25
@@ -19,7 +16,6 @@ public class Duke {
 
     /**
      * Class Constructor specifying the file path where data is stored.
-     *
      * @param filePath The file path giving the location of the file containing user data
      */
     public Duke(String filePath) {
@@ -34,41 +30,25 @@ public class Duke {
     }
 
     /**
-     * Runs the Duke application.
+     * Returns the Storage associated with the Duke application.
+     * @return Storage The class handling the writing/reading of data
      */
-    public void run() {
-        this.ui.greet();
-        boolean isExit = false;
-        while (!isExit) {
-            try {
-                String userCommand = this.ui.readCommand();
-                Command command = Parser.parse(userCommand);
-                command.execute(this.tasks, this.ui, this.storage);
-            } catch (DukeException e) {
-                ui.displayError(e.getMessage());
-            }
-        }
-
-    }
-
-    /**
-     * This is the main method which makes use of the Duke constructor and the run method.
-     *
-     * @param args Unused.
-     */
-    public static void main(String[] args) {
-        Duke duke = new Duke("data/duke.txt");
-        duke.run();
-    }
-
     public Storage getStorage() {
         return storage;
     }
 
+    /**
+     * Returns the TaskList storing all the Task.
+     * @return TaskList The TaskList storing the Task.
+     */
     public TaskList getTasks() {
         return tasks;
     }
 
+    /**
+     * Returns the Ui handling input/output.
+     * @return Ui The Ui handling input/output.
+     */
     public Ui getUi() {
         return ui;
     }
