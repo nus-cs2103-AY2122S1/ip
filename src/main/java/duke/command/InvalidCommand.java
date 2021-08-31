5d2956f8f7c7ec09c2b229e50ae10d78de3df66f
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2021-08-31 23:19:54 +0800

    Truncate Invalid command error message
    Truncating the Invalid command error message ensures that the message
    can fit nicely into the DialogBox

--- src/main/java/duke/command/InvalidCommand.java
+++ src/main/java/duke/command/InvalidCommand.java
@@ -21,14 +21,7 @@ public class InvalidCommand extends Command {
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
         String errorMessage = "\t Sorry I do not understand this command \n";
-        errorMessage += "\t Please use one of the following commands: \n";
-        errorMessage += "\t \t list - To list the added tasks so far\n";
-        errorMessage += "\t \t todo {description} - To add a ToDo task\n";
-        errorMessage += "\t \t deadline {description} /by {time} - To add a Deadline task\n";
-        errorMessage += "\t \t event {description} /at {time} - To add an Event task\n";
-        errorMessage += "\t \t done {number} - To mark the indicated task as done\n";
-        errorMessage += "\t \t delete {number} - To delete the indicated task\n";
-        errorMessage += "\t \t bye (To exit programme)\n";
+        errorMessage += "\t Please enter a valid command \n";
         throw new DukeException(errorMessage);
     }
 }
