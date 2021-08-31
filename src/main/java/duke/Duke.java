5c9fb02d35a4a373ea9e5d53b8762f0a4c0cf50a
ntwbruce <e0544240@u.nus.edu>
2021-08-31 22:17:15 +0800

    Delete unnecessary methods from Duke
    Remove unused methods start (extracted to Main) and showWelcome because
    they are unused.
    
    Let's delete the unnecessary methods mentioned above.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -21,7 +21,7 @@ import javafx.stage.Stage;
 /**
  * Represents the main Duke program. Running this class's main function executes the program.
  */
-public class Duke extends Application {
+public class Duke {
 
     private final Ui ui;
     private final TaskList taskList;
@@ -93,54 +93,6 @@ public class Duke extends Application {
         input.close();
     }
 
-    @Override
-    public void start(Stage stage) {
-        scrollPane = new ScrollPane();
-        VBox dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        Button sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        Scene scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-    }
-
     /**
      * You should have your own function to generate a response to user input.
      * Replace this stub with your completed method.
@@ -159,10 +111,6 @@ public class Duke extends Application {
         } catch (DateTimeParseException e) {
             return ui.showException(new DukeException(DukeExceptionType.INVALID_DATETIME));
         }
-    }
-
-    String showWelcome() {
-        return ui.showWelcome();
     }
 
     public static void main(String[] args) {
