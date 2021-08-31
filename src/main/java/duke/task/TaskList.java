3e2af96d0e13a8391b33b947568aa73e0b877d22
Frederick Pek <ssixthlhm@gmail.com>
2021-09-01 00:03:38 +0800

    Improve coding style: adjust indentations in TaskList.java

--- src/main/java/duke/task/TaskList.java
+++ src/main/java/duke/task/TaskList.java
@@ -10,79 +10,79 @@ import java.util.List;
  * @version CS2103 AY21/22 Sem 1 iP
  */
 public class TaskList {
-     private List<Task> tasks;
+    private List<Task> tasks;
 
-     /**
-     * Creates and initalizes a new TaskList with the given list of tasks.
-     *
-     * @param tasks The list of tasks for the TaskList to be initialised with.
-     * @return A new TaskList object.
-     */
-     public TaskList(List<Task> tasks) {
-          this.tasks = tasks;
-     }
+    /**
+    * Creates and initalizes a new TaskList with the given list of tasks.
+    *
+    * @param tasks The list of tasks for the TaskList to be initialised with.
+    * @return A new TaskList object.
+    */
+    public TaskList(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 
-     /**
-     * Retrieves and returns the Task at the specified index of the tasklist.
-     *
-     * @param index The specified index to retrieve the task from.
-     * @return The task at index.
-     */
-     public Task get(int index) {
-          return this.tasks.get(index);
-     }
+    /**
+    * Retrieves and returns the Task at the specified index of the tasklist.
+    *
+    * @param index The specified index to retrieve the task from.
+    * @return The task at index.
+    */
+    public Task get(int index) {
+        return this.tasks.get(index);
+    }
 
-     /**
-     * Marks the Task at the specified index of the tasklist as done.
-     *
-     * @param index The index of the task to be marked as done.
-     */
-     public void markDone(int index) {
-          this.get(index).markDone();
-     }
+    /**
+    * Marks the Task at the specified index of the tasklist as done.
+    *
+    * @param index The index of the task to be marked as done.
+    */
+    public void markDone(int index) {
+        this.get(index).markDone();
+    }
 
-     /**
-     * Returns the list of tasks held by this object.
-     *
-     * @return The list of tasks.
-     */
-     public List<Task> getTasks() {
-          return this.tasks;
-     }
+    /**
+    * Returns the list of tasks held by this object.
+    *
+    * @return The list of tasks.
+    */
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
 
-     /**
-     * Appends the specified task to the back of the task list.
-     *
-     * @param task The task to be added.
-     */
-     public void add(Task task) {
-          this.tasks.add(task);
-     }
+    /**
+    * Appends the specified task to the back of the task list.
+    *
+    * @param task The task to be added.
+    */
+    public void add(Task task) {
+        this.tasks.add(task);
+    }
 
-     /**
-     * Removes the Task at the specified index of the tasklist.
-     *
-     * @param index The index of the task to be removed.
-     */
-     public void delete(int index) {
-          this.tasks.remove(index);
-     }
+    /**
+    * Removes the Task at the specified index of the tasklist.
+    *
+    * @param index The index of the task to be removed.
+    */
+    public void delete(int index) {
+        this.tasks.remove(index);
+    }
 
-     /**
-     * Returns the numeber of tasks held.
-     *
-     * @return Returns the numeber of tasks held.
-     */
-     public int size() {
-          return this.tasks.size();
-     }
+    /**
+    * Returns the numeber of tasks held.
+    *
+    * @return Returns the numeber of tasks held.
+    */
+    public int size() {
+        return this.tasks.size();
+    }
 
-     /**
-     * Returns true if there are no tasks in the list.
-     *
-     * @return Returns true if there are no tasks in the list.
-     */
-     public boolean isEmpty() {
-          return this.tasks.isEmpty();
-     }
+    /**
+    * Returns true if there are no tasks in the list.
+    *
+    * @return Returns true if there are no tasks in the list.
+    */
+    public boolean isEmpty() {
+        return this.tasks.isEmpty();
+    }
 }
