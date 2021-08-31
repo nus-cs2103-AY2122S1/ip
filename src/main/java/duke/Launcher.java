ad078db7386f78be979d6a9ead4a2d1a92e4e48f
houtenteo <houtenteo@gmail.com>
2021-08-31 09:36:10 +0800

    Remove println statement

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -7,6 +7,5 @@ import javafx.application.Application;
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(Duke.class, args);
-        System.out.println("TESTing");
     }
 }
