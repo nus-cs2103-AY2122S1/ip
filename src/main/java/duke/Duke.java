8d15bfa64e7679ea594cd95f904cfa34e8d66c22
Li-Lehao <e0492462@u.nus.edu>
2021-08-31 20:52:31 +0800

    GUI.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,8 +2,21 @@ package duke;
 
 import command.Command;
 import duke.exception.DukeException;
+import duke.gui.DialogBox;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Duke {
+public class Duke extends Application {
     /** Duke's Storage. Deals with loading tasks from the file and saving tasks in the file. **/
     private final Storage storage;
 
@@ -13,6 +26,27 @@ public class Duke {
     /** Duke's user interface. Deals with interactions with the user. **/
     private final Ui ui;
 
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/lehao.jpg"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/zixuan.jpg"));
+    /**
+     * A public constructor to initialized Duke.
+     */
+    public Duke() {
+        ui = new Ui();
+        storage = new Storage("data/taskList.txt");
+        try {
+            taskList = new TaskList(storage.loadTaskList());
+        } catch (DukeException e) {
+            ui.showDukeException(e);
+            taskList = new TaskList();
+        }
+    }
     /**
      * A public constructor to initialized Duke.
      *
@@ -49,6 +83,105 @@ public class Duke {
     }
 
     public static void main(String[] args) {
+        System.out.println("main");
         new Duke("data/taskList.txt").run();
+    }
+
+
+    /**
+     * Creates a label with the specified text and adds it to the dialog container.
+     *
+     * @param text String containing text to add
+     * @return a label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+        return textToAdd;
+    }
+
+    /**
+     * A private method to handle user input and add dialog box.
+     */
+    private void handleUserInput() {
+        Label userText = new Label(userInput.getText());
+        Label dukeText = new Label(getResponse(userInput.getText()));
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, new ImageView(userImage)),
+                DialogBox.getDukeDialog(dukeText, new ImageView(dukeImage))
+        );
+        userInput.clear();
+    }
+
+    /**
+     * A private method to get Duke response.
+     *
+     * @param input The given user input.
+     * @return A String, Duke's response.
+     */
+    private String getResponse(String input) {
+        return input;
+    }
+
+    /**
+     *
+     * @param stage
+     */
+    @Override
+    public void start(Stage stage) {
+        // Setting up required components.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        // Styling.
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        // Handle user input.
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
     }
 }
---
+++ src/main/java/duke/gui/DialogBox.java
@@ -0,0 +1,66 @@
+package duke.gui;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class DialogBox extends HBox {
+    private Label text;
+    private ImageView displayPicture;
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    /**
+     * A public factory method to get user's dialog.
+     *
+     * @param l The given user's Label.
+     * @param iv The given user's Image.
+     * @return The user's DialogBox.
+     */
+    public static DialogBox getUserDialog(Label l, ImageView iv) {
+        return new DialogBox(l, iv);
+    }
+
+    /**
+     * A public factory method to get Duke's dialog.
+     *
+     * @param l The given Duke's Label.
+     * @param iv The given Duke's Image.
+     * @return The Duke's DialogBox.
+     */
+    public static DialogBox getDukeDialog(Label l, ImageView iv) {
+        var db = new DialogBox(l, iv);
+        db.flip();
+        return db;
+    }
+
+    /**
+     * A public constructor to initialize the DialogBox.
+     *
+     * @param l The given Label.
+     * @param iv The given ImageView.
+     */
+    public DialogBox(Label l, ImageView iv) {
+        this.text = l;
+        this.displayPicture = iv;
+
+        text.setWrapText(true);
+        displayPicture.setFitHeight(100.0);
+        displayPicture.setFitWidth(100.0);
+
+        this.setAlignment(Pos.TOP_RIGHT);
+        this.getChildren().addAll(text, displayPicture);
+    }
+}
