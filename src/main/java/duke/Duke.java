05fa8871d4f229855bf22a692bbd568ecbf0f533
Wee Han <weehanng@yahoo.com>
2021-08-31 20:53:39 +0800

    add GUI to Duke up to tutorial 3

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,22 +1,48 @@
 package duke;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 import java.util.Scanner;
 
 import commands.Command;
-import tasks.*;
-import exceptions.*;
 
-public class Duke {
+public class Duke extends Application {
 
+    /**
+     * These control the GUI for Duke.
+     */
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+    /**
+     * These control the internal logic for Duke
+     */
     private Ui ui;
     private TaskList taskList;
-    private Storage store;
+    private Storage storage;
     private boolean isRunning;
 
-    Duke() {
+    public Duke() {
         this.ui = new Ui();
         this.taskList = new TaskList();
-        this.store = new Storage("data/tasks.txt");
+        this.storage = new Storage("data/tasks.txt");
         this.isRunning = false;
     }
 
@@ -24,10 +50,12 @@ public class Duke {
      * Runs this instance of Duke, which starts a new Scanner object
      * to accept user commands.
      *
+     * This method is not used in the GUI version of Duke.
+     * Use the start method below instead for GUI.
      */
     private void run() {
         this.isRunning = true;
-        this.taskList = new TaskList(this.store.readFile());
+        this.taskList = new TaskList(this.storage.readFile());
 
         Scanner sc = new Scanner(System.in);
 
@@ -37,7 +65,7 @@ public class Duke {
             String input = sc.nextLine();
             Command c = this.ui.parseInput(input);
             this.ui.printSepLine();
-            c.execute(this.ui, this.taskList, this.store);
+            c.execute(this.ui, this.taskList, this.storage);
             this.ui.printSepLine();
             this.isRunning = !c.isQuit();
         }
@@ -52,6 +80,113 @@ public class Duke {
      */
     public int getNumberOfTasks() {
         return this.taskList.numberOfTasks();
+    }
+
+    /**
+     * From the tutorial at https://se-education.org/guides/tutorials/javaFxPart3.html
+     * Iteration 1:
+     * Creates a label with the specified text and adds it to the dialog container.
+     * @param text String containing text to add
+     * @return a label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        // You will need to import `javafx.scene.control.Label`.
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
+    }
+
+    /**
+     * Iteration 3:
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Also flips Duke's dialog so it appears on the left. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        Label userText = new Label(userInput.getText());
+        Label dukeText = new Label(getResponse(userInput.getText()));
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, new ImageView(user)),
+                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
+        );
+        userInput.clear();
+    }
+
+    private String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
+    /**
+     * Provided by the tutorial at https://se-education.org/guides/tutorials/javaFxPart2.html
+     * @param stage The Stage object where all nodes will be displayed on
+     */
+    @Override
+    public void start(Stage stage) {
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+        stage.show();
+
+        sendButton.setOnMouseClicked((event) -> {
+            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+            userInput.clear();
+        });
+
+        userInput.setOnAction((event) -> {
+            dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
+            userInput.clear();
+        });
+
+        //Scroll down to the end every time dialogContainer's height changes.
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+        // more code to be added here later
     }
 
     public static void main(String[] args) {
--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -1,2 +1,13 @@
-package duke;public class Launcher {
+package duke;
+
+import javafx.application.Application;
+
+/**
+ * A launcher class to workaround classpath issues.
+ * Provided by the guide from "https://se-education.org/guides/tutorials/javaFxPart1.html"
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Duke.class, args);
+    }
 }
