f02b1b14360b4b82f0987ecbb3591b2a83a1bc6d
Jason <jason.ng.zq99@gmail.com>
2021-08-31 17:39:13 +0800

    Add images to dialogue box

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.Region;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * Encapsulates the entire Duke program.
@@ -49,6 +50,8 @@ public class Main extends Application {
     private TextField userInput;
     private Button sendButton;
     private Scene scene;
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DSC_0352.png"));
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/Timetable.png"));
 
     /**
      * Constructor for a Main() class.
@@ -215,6 +218,15 @@ public class Main extends Application {
 
         //Scroll down to the end every time dialogContainer's height changes.
         dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+
+        //Part 3. Add functionality to handle user input.
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput(defaultFont);
+        });
+
+        userInput.setOnAction((event) -> {
+            handleUserInput(defaultFont);
+        });
     }
 
     /**
@@ -230,6 +242,31 @@ public class Main extends Application {
         textToAdd.setWrapText(true);
 
         return textToAdd;
+    }
+
+    /**
+     * Iteration 2:
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput(Font font) {
+        Label userText = new Label(userInput.getText());
+        userText.setFont(font);
+        Label dukeText = new Label(getResponse(userInput.getText()));
+        dukeText.setFont(font);
+        dialogContainer.getChildren().addAll(
+                new DialogBox(userText, new ImageView(user)),
+                new DialogBox(dukeText, new ImageView(duke))
+        );
+        userInput.clear();
+    }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    private String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 
     public static void main(String[] args) {
