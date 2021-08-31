f3d3782b1c304ddac7b22578d678a16328f64440
Koh Jia Xian <39460640+koh-jx@users.noreply.github.com>
2021-09-01 01:00:12 +0800

    Implement JavaFX with .fxml

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -15,143 +15,48 @@ import javafx.scene.image.ImageView;
 
 import task.TaskList;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Main Duke class
  */
-public class Duke extends Application {
-//    private final DukeListener listener;
-//
-//    /**
-//     * Constructor
-//     * Instantiates a (saved) task.Task List and a duke.DukeListener for duke.Duke
-//     */
-//    private Duke() {
-//        TaskList taskList = Storage.loadList();
-//        DukeParser parser = new DukeParser(taskList);
-//        listener = new DukeListener(parser);
-//    }
-//
-//    /**
-//     * Main function
-//     *
-//     * @param args No arguments need to be passed in
-//     */
-//    public static void main(String[] args) {
-//        Duke duke = new Duke();
-//        duke.begin();
-//    }
-//
-//    /**
-//     * Function that starts listening for the command
-//     */
-//    private void begin() {
-//        Ui.intro();
-//        listener.startListen();
-//        Ui.goodBye();
-//    }
+public class Duke {
+    private final DukeListener listener;
 
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
-        ScrollPane scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        Button sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        Scene scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        //Part 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> handleUserInput());
-
-        userInput.setOnAction((event) -> handleUserInput());
+    /**
+     * Constructor
+     * Instantiates a (saved) task.Task List and a duke.DukeListener for duke.Duke
+     */
+    protected Duke() {
+        TaskList taskList = Storage.loadList();
+        DukeParser parser = new DukeParser(taskList);
+        listener = new DukeListener(parser);
     }
 
     /**
-     * Iteration 1:
-     * Creates a label with the specified text and adds it to the dialog container.
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
+     * Main function
+     *
+     * @param args No arguments need to be passed in
      */
-    private Label getDialogLabel(String text) {
-        // You will need to import `javafx.scene.control.Label`.
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.begin();
     }
 
     /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
+     * Function that starts listening for the command
      */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
+    private void begin() {
+        Ui.intro();
+        listener.startListen();
+        Ui.goodBye();
     }
 
     /**
      * You should have your own function to generate a response to user input.
      * Replace this stub with your completed method.
      */
-    private String getResponse(String input) {
+    protected String getResponse(String input) {
         return "Duke heard: " + input;
     }
-
-
-
-
-
 }
