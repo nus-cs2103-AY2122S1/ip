922e898814a452fb994038f0f3656f39be840c1c
SkyBlaise <zsc990124@gmail.com>
2021-08-31 15:24:06 +0800

    Change Label to Text to prevent long response from being truncated

--- src/main/java/duke/controller/DialogBox.java
+++ src/main/java/duke/controller/DialogBox.java
@@ -9,11 +9,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
 /**
  * Controller for Dialog Box.
@@ -21,7 +21,7 @@ import javafx.scene.shape.Circle;
  */
 public class DialogBox extends HBox {
     @FXML
-    private Label dialog;
+    private Text dialog;
     @FXML
     private ImageView displayPicture;
 
