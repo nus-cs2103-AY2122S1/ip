a110f31d55cf6d0218217e0bdcc9006e0beed8af
Preshita01 <pandipreshita@gmail.com>
2021-08-31 09:28:55 +0800

    Modify all methods to return strings in Parser.java

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -43,17 +43,19 @@ public class Parser {
      * @throws IOException if there is an error in updated the list of tasks saved in the user's
      * hard disk after marking a task as completed
      */
-    public static void parseDone(String instruction) throws DukeException, IOException {
+    public static String parseDone(String instruction) throws DukeException, IOException {
+        String result = "";
         int taskNum = Integer.parseInt(instruction.substring(5)) - 1;
         if (taskNum >= TaskList.getCounter()) {
-            throw new DukeException("\tHmm, I don't have task " + (taskNum + 1) +
+            throw new DukeException("Hmm, I don't have task " + (taskNum + 1) +
                     " in my list. Please key in 'list' if you'd like to " +
                     "view your list of tasks again!");
         } else {
             TaskList.getTaskList().get(taskNum).markAsDoneAndUpdate();
-            System.out.println("\tGood job! I've marked this task as done:");
-            System.out.println("\t\t" + TaskList.getTaskList().get(taskNum).toString());
+            result += "Good job! I've marked this task as done:\n";
+            result += "\t" + TaskList.getTaskList().get(taskNum).toString() + "\n";
         }
+        return result;
     }
 
     /**
@@ -63,19 +65,21 @@ public class Parser {
      * @throws IOException if there is an error in updated the list of tasks saved in the user's
      * hard disk after marking a task as completed
      */
-    public static void parseDelete(String instruction) throws DukeException, IOException {
+    public static String parseDelete(String instruction) throws DukeException, IOException {
+        String result = "";
         int taskNum = Integer.parseInt(instruction.substring(7)) - 1;
         if (taskNum >= TaskList.getCounter()) {
-            throw new DukeException("\tHmm, I don't have task " + (taskNum + 1) +
+            throw new DukeException("Hmm, I don't have task " + (taskNum + 1) +
                     " in my list. Please key in 'list' if you'd like to " +
                     "view your list of tasks again!");
         } else {
-            System.out.println("\tNoted. I've removed this task from your main list:");
-            System.out.println("\t\t" + TaskList.getTaskList().get(taskNum).toString());
+            result += "Noted. I've removed this task from your main list:\n";
+            result += "\t" + TaskList.getTaskList().get(taskNum).toString() + "\n";
             TaskList.deleteTaskAndUpdate(TaskList.getTaskList().get(taskNum));
-            System.out.println("\tNow you have " + TaskList.getCounter() +
-                    " task(s) in the list.");
+            result += "Now you have " + TaskList.getCounter() +
+                    " task(s) in the list.\n";
         }
+        return result;
     }
 
     /**
@@ -86,19 +90,21 @@ public class Parser {
      * @throws IOException if there is an error in updated the list of tasks saved in the user's
      * hard disk after marking a task as completed
      */
-    public static void parseTodo(String instruction) throws DukeException, IOException {
+    public static String parseTodo(String instruction) throws DukeException, IOException {
+        String result = "";
         if (instruction.length() < 5) {
-            throw new DukeException("\tOOPS!!! The description of a todo " +
+            throw new DukeException("OOPS!!! The description of a todo " +
                     "cannot be empty.");
         } else {
             String taskDescription = instruction.substring(4);
             Todo newTodo = new Todo(taskDescription);
             TaskList.addTaskAndUpdate(newTodo);
-            System.out.println("\tGot it! I've added this task:");
-            System.out.println("\t\t" + newTodo.toString());
-            System.out.println("\tNow you have " + TaskList.getCounter() +
-                    " task(s) in the list.");
+            result += "Got it! I've added this task:\n";
+            result += "\t" + newTodo.toString() + "\n";
+            result += "Now you have " + TaskList.getCounter() +
+                    " task(s) in the list.\n";
         }
+        return result;
     }
 
     /**
@@ -110,9 +116,10 @@ public class Parser {
      * @throws IOException if there is an error in updated the list of tasks saved in the user's
      * hard disk after marking a task as completed
      */
-    public static void parseDeadline(String instruction) throws DukeException, IOException {
+    public static String parseDeadline(String instruction) throws DukeException, IOException {
+        String result = "";
         if (instruction.length() < 10) {
-            throw new DukeException("\tOOPS!!! The description of a deadline " +
+            throw new DukeException("OOPS!!! The description of a deadline " +
                     "cannot be empty.");
         } else {
             String taskDescription = "";
@@ -136,10 +143,11 @@ public class Parser {
                 String by = instruction.substring(currIndex + 5);
                 Task newDeadline = new Deadline(taskDescription, by);
                 TaskList.addTaskAndUpdate(newDeadline);
-                System.out.println("\tGot it! I've added this task:");
-                System.out.println("\t\t" + newDeadline.toString());
-                System.out.println("\tNow you have " + TaskList.getCounter() +
-                        " task(s) in the list.");
+                result += "Got it! I've added this task:\n";
+                result += "\t" + newDeadline.toString() + "\n";
+                result += "Now you have " + TaskList.getCounter() +
+                        " task(s) in the list.\n";
+                return result;
             }
         }
     }
@@ -153,7 +161,8 @@ public class Parser {
      * @throws IOException if there is an error in updated the list of tasks saved in the user's
      * hard disk after marking a task as completed
      */
-    public static void parseEvent(String instruction) throws DukeException, IOException {
+    public static String parseEvent(String instruction) throws DukeException, IOException {
+        String result = "";
         if (instruction.length() < 7) {
             throw new DukeException("\tOOPS!!! The description of a event " +
                     "cannot be empty.");
@@ -167,7 +176,7 @@ public class Parser {
             }
             if (currIndex == instruction.length() ||
                     currIndex + 5 >= instruction.length()) {
-                throw new DukeException("\tI think you forgot to key in your event timing!");
+                throw new DukeException("I think you forgot to key in your event timing!");
             } else if (instruction.charAt(currIndex + 7) != '/' &&
                     instruction.charAt(currIndex + 10) != '/') {
                 throw new DukeException("Please format the date as dd/mm/yyy");
@@ -178,10 +187,11 @@ public class Parser {
                 String by = instruction.substring(currIndex + 5);
                 Task newEvent = new Event(taskDescription, by);
                 TaskList.addTaskAndUpdate(newEvent);
-                System.out.println("\tGot it! I've added this task:");
-                System.out.println("\t\t" + newEvent.toString());
-                System.out.println("\tNow you have " + TaskList.getCounter() +
-                        " task(s) in the list.");
+                result += "Got it! I've added this task:\n";
+                result += "\t" + newEvent.toString() + "\n";
+                result += "Now you have " + TaskList.getCounter() +
+                        " task(s) in the list.\n";
+                return result;
             }
         }
     }
@@ -189,25 +199,26 @@ public class Parser {
     /**
      * Prints out the list of tasks saved by Jarvis
      */
-    public static void parseList() {
+    public static String parseList() {
         int num = 1;
+        String result = "";
         for (int i = 0; i < TaskList.getTaskList().size(); i++) {
-            System.out.println("\t" + num + "." +
-                    TaskList.getTaskList().get(i).toString());
+            result += num + "." + TaskList.getTaskList().get(i).toString() + "\n";
             num++;
         }
+        return result;
     }
 
     /**
      * Prints out the list of tasks set for/due today and all todo tasks
      */
-    public static void parseToday() {
-        System.out.println("\tTasks scheduled for today are: ");
+    public static String parseToday() {
+        String result = "";
+        result += "Tasks scheduled for today are: \n";
         int num = 1;
         for (int i = 0; i < TaskList.getTaskList().size(); i++) {
             if (TaskList.getTaskList().get(i) instanceof Todo) {
-                System.out.println("\t\t" + num + "." +
-                        TaskList.getTaskList().get(i).toString());
+                result += "\t" + num + "." + TaskList.getTaskList().get(i).toString() + "\n";
                 num++;
             } else {
                 int currYear = LocalDateTime.now().getYear();
@@ -219,36 +230,36 @@ public class Parser {
                 if (TaskList.getTaskList().get(i) instanceof Deadline &&
                         (((Deadline) TaskList.getTaskList().get(i)).getDeadline()).isAfter(start) &&
                         (((Deadline) TaskList.getTaskList().get(i)).getDeadline()).isBefore(end)) {
-                    System.out.println("\t\t" + num + "." +
-                            TaskList.getTaskList().get(i).toString());
+                    result += "\t" + num + "." + TaskList.getTaskList().get(i).toString() + "\n";
                     num++;
                 } else if (TaskList.getTaskList().get(i) instanceof Event &&
                         (((Event) TaskList.getTaskList().get(i)).getEventTime()).isAfter(start) &&
                         (((Event) TaskList.getTaskList().get(i)).getEventTime()).isBefore(end)) {
-                    System.out.println("\t\t" + num + "." +
-                            TaskList.getTaskList().get(i).toString());
+                    result += "\t" + num + "." + TaskList.getTaskList().get(i).toString() + "\n";
                     num++;
                 }
             }
         }
         if (num == 1) {
-            System.out.println("\t\tLooks like there is nothing due today!");
+            result += "\tLooks like there is nothing due today!\n";
         }
+        return result;
     }
 
-    public static void parseFind(String search) {
-        System.out.println("\tHere are the matching tasks in your list:");
+    public static String parseFind(String search) {
+        String result = "";
+        result += "Here are the matching tasks in your list:\n";
         int num = 1;
         search = search.substring(5);
         for (int i = 0; i < TaskList.getTaskList().size(); i++) {
             if (TaskList.getTaskList().get(i).getDescription().contains(search)) {
-                System.out.println("\t\t" + num + "." +
-                        TaskList.getTaskList().get(i).toString());
+                result += "\t" + num + "." + TaskList.getTaskList().get(i).toString() + "\n";
                 num++;
             }
         }
         if (num == 1) {
-            System.out.println("\t\tNo matching results found!");
+            result += "\tNo matching results found!\n";
         }
+        return result;
     }
 }
