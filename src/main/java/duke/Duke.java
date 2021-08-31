9dd46364d3479fdf56013600e58d7ccbbd007340
KelvinSoo <e0544272@u.nus.edu.sg>
2021-08-31 20:26:09 +0800

    Add basic javaFX feature

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -7,13 +7,28 @@ import duke.storage.Storage;
 import duke.ui.Ui;
 import duke.task.TaskList;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
  * A chatbot based on Project Duke
  *
  * @author KelvinSoo
  * @version A-MoreOOP
  */
-public class Duke {
+public class Duke extends Application{
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
 
     private final Storage storage;
     private TaskList taskList;
@@ -27,6 +42,17 @@ public class Duke {
     public Duke(String filePath) {
         ui = new Ui();
         storage = new Storage(filePath);
+        try {
+            taskList = new TaskList(storage.load());
+        } catch (DukeException e) {
+            ui.printStringInErrorBox(e.getMessage());
+            taskList = new TaskList();
+        }
+    }
+
+    public Duke() {
+        ui = new Ui();
+        storage = new Storage("./data/duke.txt");
         try {
             taskList = new TaskList(storage.load());
         } catch (DukeException e) {
@@ -51,6 +77,29 @@ public class Duke {
                 ui.printStringInErrorBox(e.getMessage());
             }
         }
+    }
+
+    @Override
+    public void start(Stage stage) {
+        //Step 1. Setting up required components
+
+        //The container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+
+        // more code to be added here later
     }
 
     public static void main(String[] args) {
