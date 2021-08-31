a26da2535b3ca32e562298bdfc150ea7c753802b
Jonathan Tan Xin Jie <e0543805@u.nus.edu>
2021-09-01 00:02:36 +0800

    Small styling fixes

---
+++ src/main/java/addon/Popup.java
@@ -0,0 +1,71 @@
+package addon;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class Popup {
+
+    /**
+     * Creates a popup window with the given message and an acknowledgement button.
+     *
+     * @param message Message to be displayed.
+     */
+    public static void errorPopup(String message) {
+        Stage window = new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Error");
+        window.setMinWidth(250);
+
+        Label errorMessage = new Label();
+        errorMessage.setText(message);
+        Button back = new Button("Ok");
+        back.setOnAction(a -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(errorMessage, back);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.show();
+    }
+
+    /**
+     *
+     * Creates a popup window with the given message and an acknowledgement button.
+     *
+     * @param list List of items to be displayed.
+     * @param str String representation of the terms used for the filtering.
+     */
+    public static void filterPopup(ObservableList<String> list, String str) {
+        Stage window = new Stage();
+
+        ObservableList<String> filteredTasks = FXCollections.observableArrayList();
+        ListView<String> listView = new ListView<>(filteredTasks);
+        listView.getItems().addAll(list);
+
+        window.setTitle("Filter results for " + str);
+        window.setMinWidth(250);
+
+        Label explain = new Label();
+        explain.setText("Here are your filtered results for \"" + str + "\"");
+        explain.setAlignment(Pos.CENTER);
+        BorderPane pain = new BorderPane();
+        pain.setTop(explain);
+        pain.setCenter(listView);
+
+        Scene scene = new Scene(pain);
+        window.setScene(scene);
+        window.show();
+    }
+}
