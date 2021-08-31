9b28c63c869fac5e7385575e0da7b7f34d828407
Tanishq4331 <tanishq43311@gmail.com>
2021-08-31 13:14:25 +0800

    Design Ui Layout; Move Ui setup from Duke.java to Ui.java

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,8 +6,6 @@ import duke.main.Storage;
 import duke.main.Ui;
 import duke.task.TaskList;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 /**
@@ -31,7 +29,6 @@ public class Duke extends Application {
      * @param filePath for storing tasks.
      */
     public Duke(String filePath) {
-        ui = new Ui();
         storage = new Storage(filePath);
         try {
             taskList = storage.load();
@@ -85,10 +82,9 @@ public class Duke extends Application {
      */
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+        ui = new Ui(stage);
+        ui.start();
     }
+
 
 }
--- src/main/java/duke/main/Ui.java
+++ src/main/java/duke/main/Ui.java
@@ -2,19 +2,90 @@ package duke.main;
 
 import duke.task.Task;
 import duke.task.TaskList;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.util.List;
 import java.util.Scanner;
 
 public class Ui {
     private final Scanner inputScanner;
+    private Stage stage;
+    private Scene scene;
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private AnchorPane mainLayout;
+
 
     /**
      * Constructor for Duke UI
      */
-    public Ui() {
+    public Ui(Stage stage) {
+        this.stage = stage;
         this.inputScanner = new Scanner(System.in);
-        greetOnStart();
+    }
+
+    public void start() {
+        //Step 1. Setting up required components
+        this.scrollPane = new ScrollPane();
+        this.dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        this.userInput = new TextField();
+        this.sendButton = new Button("Send");
+
+        this.mainLayout = new AnchorPane();
+
+        this.scene = new Scene(mainLayout);
+
+        formatWindow();
+
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        stage.setScene(scene);
+        stage.show();
+        // more code to be added here later
+    }
+
+    public void formatWindow() {
+        //Step 1. Formatting the window to look as expected
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        // You will need to import `javafx.scene.layout.Region` for this.
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        // more code to be added here later
     }
 
     /**
