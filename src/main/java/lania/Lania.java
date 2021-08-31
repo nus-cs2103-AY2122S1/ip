f2d07e6879c254ceb6b17d16abfc93da09f7d45d
nguyiyang <nguyiyang47@gmail.com>
2021-08-31 21:46:03 +0800

    Refactor Gui

--- src/main/java/lania/Lania.java
+++ src/main/java/lania/Lania.java
@@ -25,15 +25,6 @@ import lania.task.TaskList;
  */
 public class Lania extends Application {
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/User.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/Lania.png"));
-
-
     /** Contains the task list */
     private TaskList tasks;
     /**  Deals with interactions with the user */
@@ -114,82 +105,7 @@ public class Lania extends Application {
 
     @Override
     public void start(Stage stage) {
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-        stage.setTitle("Lania");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        dialogContainer.getChildren().addAll(
-                DialogBox.getLaniaDialog(new Label(ui.showGreetingMessage()), new ImageView(duke)));
-
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-    }
-
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        try {
-            Command c = parser.parse(userInput.getText());
-            Label dukeText = new Label(c.execute(tasks, storage, ui));
-            dialogContainer.getChildren().addAll(
-                    DialogBox.getUserDialog(userText, new ImageView(user)),
-                    DialogBox.getLaniaDialog(dukeText, new ImageView(duke))
-            );
-        } catch (LaniaException e) {
-            dialogContainer.getChildren().addAll(
-                    DialogBox.getUserDialog(userText, new ImageView(user)),
-                    DialogBox.getLaniaDialog(new Label(ui.showLaniaException(e)), new ImageView(duke))
-            );
-        } catch (DateTimeParseException e) {
-            dialogContainer.getChildren().addAll(
-                    DialogBox.getUserDialog(userText, new ImageView(user)),
-                    DialogBox.getLaniaDialog(new Label(ui.showDateTimeException()), new ImageView(duke))
-            );
-        }
-        userInput.clear();
+        Gui gui = new Gui(stage, tasks, ui, storage, parser);
+        gui.runGui();
     }
 }
