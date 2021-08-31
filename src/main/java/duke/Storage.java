df7a10fa710978cd891fce40734bd72422a335f0
germaine <germaine5152seah@gmail.com>
2021-08-31 17:47:36 +0800

    Edit the code to comply with coding standard

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -5,7 +5,11 @@ import duke.task.Event;
 import duke.task.Task;
 import duke.task.Todo;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
