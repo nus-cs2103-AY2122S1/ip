d4868901fd3a624ca1b0b9cc5af2d62afd754231
sivayogasubramanian <sivayogasubramanian@gmail.com>
2021-08-31 23:37:59 +0800

    Use vararg in launcher main method

--- src/main/java/jarvis/Launcher.java
+++ src/main/java/jarvis/Launcher.java
@@ -10,7 +10,7 @@ public class Launcher {
      *
      * @param args Command line arguments.
      */
-    public static void main(String[] args) {
+    public static void main(String ...args) {
         if (args.length > 0) {
             switch (args[0].toLowerCase()) {
             case "-c":
