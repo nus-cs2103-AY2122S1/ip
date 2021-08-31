fca27c22cde9259a3bb5a1250e00150e1f0c13eb
Zenlzb <leezenbeen@u.nus.edu>
2021-08-31 21:56:32 +0800

    Change commandList array into constant

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -4,10 +4,10 @@ import duke.commands.*;
 
 /**
  * Encapsulates a parser class which parses user input
- * and creates a different command object depending on the input
+ * and creates a different command object depending on the input.
  */
 public class Parser {
-    private final Command[] commandList = {
+    private final Command[] COMMAND_LIST = {
             new ListCommand(""),
             new ByeCommand(""),
             new TodoCommand(""),
@@ -22,11 +22,11 @@ public class Parser {
     /**
      * Takes in a string input from the user and returns the invoked command object.
      *
-     * @param input The string input by the user
-     * @return The created command object
+     * @param input The string input by the user.
+     * @return The created command object.
      */
     public Command parse(String input) {
-        for(Command c : commandList) {
+        for(Command c : COMMAND_LIST) {
             if(input.startsWith(c.startsWith())) {
                 if (input.equals(c.startsWith())) {
                     return c.of("");
