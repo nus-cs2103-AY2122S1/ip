a23dbb801a89ddc7efc45214a13583ec14216111
Frederick Pek <ssixthlhm@gmail.com>
2021-08-31 17:30:00 +0800

    Improve GUI aesthetics

--- src/main/java/duke/gui/MainWindow.java
+++ src/main/java/duke/gui/MainWindow.java
@@ -18,10 +18,13 @@ import javafx.scene.layout.VBox;
 public class MainWindow extends AnchorPane {
     @FXML
     private ScrollPane scrollPane;
+
     @FXML
     private VBox dialogContainer;
+    
     @FXML
     private TextField userInput;
+    
     @FXML
     private Button sendButton;
 
