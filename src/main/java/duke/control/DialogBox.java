d69f9e4cb1cfd608998d0f1579977e7cc99b7a98
DesmondTo <e0550619@u.nus.edu>
2021-08-31 11:28:40 +0800

    Remove redundant imports

--- src/main/java/duke/control/DialogBox.java
+++ src/main/java/duke/control/DialogBox.java
@@ -1,8 +1,5 @@
 package duke.control;
 
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
