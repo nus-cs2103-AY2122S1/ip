9704b9ea3775acd8a24987d918e49f939656007e
ntwbruce <e0544240@u.nus.edu>
2021-08-31 20:21:44 +0800

    Add minor formatting changes
    Displaying of messages and lines are formatted wrongly now that the GUI
    is being used to display the inputs and outputs rather than the console.
    
    Let's make small edits to these classes to conform with the current
    state of the Duke program and the GUI.

--- src/test/java/duke/StorageTest.java
+++ src/test/java/duke/StorageTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import task.Task;
 
-import java.io.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Tests the functionality of the Storage class.
  */
 public class StorageTest {
-    Storage testStorage = new Storage("data", "duke.txt");
+    private final Storage testStorage = new Storage("data", "duke.txt");
 
     /**
      * Tests the Storage load function with an empty list.
@@ -34,18 +39,18 @@ public class StorageTest {
         FileWriter writer = new FileWriter(duke, true);
         writer.write("bruh");
         writer.close();
-        
+
         ByteArrayOutputStream newOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(newOut));
-        
+
         testStorage.load();
         FileWriter rewriter = new FileWriter(duke);
-        
-        String expected = "  __________________________________________________________________\n" 
-                + "  Loading Duke...\n" 
-                + "  __________________________________________________________________\n" 
-                + "\n" 
-                + "  OOPS!!! There was a problem with reading tasks in the hard disk!\n" 
+
+        String expected = "  __________________________________________________________________\n"
+                + "  Loading Duke...\n"
+                + "  __________________________________________________________________\n"
+                + "\n"
+                + "  OOPS!!! There was a problem with reading tasks in the hard disk!\n"
                 + "  Check out duke.txt for any erroneous entries or incorrect formatting.\n";
 
         assertEquals(expected, newOut.toString());
