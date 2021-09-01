f8702e8ca91c1d50f99651fb4be0fb2efd945827
wonyeji <75670073+wonyeji@users.noreply.github.com>
2021-09-01 16:23:34 +0800

    Add echo Duke GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -20,4 +20,12 @@ public class Duke {
         parser.initialiseDuke();
         parser.runDuke();
     }
+
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
 }
