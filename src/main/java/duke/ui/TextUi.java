9fde1330257ddb756e063f0c24a48327c32fc46f
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-09-01 19:17:06 +0800

    Add GUI

--- src/main/java/duke/ui/TextUi.java
+++ src/main/java/duke/ui/TextUi.java
@@ -20,8 +20,9 @@ public class TextUi {
      * @param i The index of the task.
      * @param t The task.
      */
-    public static void showTaskNumbered(int i, Task t) {
+    public static String showTaskNumbered(int i, Task t) {
         System.out.printf("%s. %s\n", i + 1, t);
+        return (i + 1) + ". " + t.toString() + "\n";
     }
 
     /**
@@ -29,8 +30,9 @@ public class TextUi {
      *
      * @param tasks The TaskList.
      */
-    public static void showTaskAdded(TaskList tasks) {
+    public static String showTaskAdded(TaskList tasks) {
         System.out.println("added: " + tasks.getTaskList().get(tasks.getLength() - 1));
+        return "added: " + tasks.getTaskList().get(tasks.getLength() - 1);
     }
 
     /**
@@ -38,8 +40,9 @@ public class TextUi {
      *
      * @param t The task.
      */
-    public static void showTaskRemoved(Task t) {
+    public static String showTaskRemoved(Task t) {
         System.out.printf("Noted. I've removed this task:\n%s\n", t);
+        return "Noted. I've removed this task: \n" + t.toString();
     }
 
     /**
@@ -47,8 +50,9 @@ public class TextUi {
      *
      * @param t The task.
      */
-    public static void showTaskDone(Task t) {
+    public static String showTaskDone(Task t) {
         System.out.printf("%s\nThe task is marked as done! Good job :D\n", t);
+        return t.toString() + "\nThe task is marked as done! Good job :D";
     }
 
     /**
@@ -56,12 +60,14 @@ public class TextUi {
      *
      * @param tasks The TaskList.
      */
-    public static void showUpdatedNumberOfTasks(TaskList tasks) {
+    public static String showUpdatedNumberOfTasks(TaskList tasks) {
         int len = tasks.getLength();
         if (len == 1) {
             System.out.printf("Now you have %d task in the list.\n", tasks.getLength());
+            return "Now you have " + tasks.getLength() + " task in the list.";
         } else {
             System.out.printf("Now you have %d tasks in the list.\n", tasks.getLength());
+            return "Now you have " + tasks.getLength() + " tasks in the list.";
         }
     }
 
@@ -70,15 +76,17 @@ public class TextUi {
      *
      * @param s The error message in String.
      */
-    public static void showErrorMessage(String s) {
+    public static String showErrorMessage(String s) {
         System.out.println(s);
+        return s;
     }
 
     /**
      * Prints a goodbye message when the user exits the chat bot.
      */
-    public static void showGoodbyeMessage() {
+    public static String showGoodbyeMessage() {
         System.out.println("See you! :)");
+        return "See you! :)";
     }
 
 }
