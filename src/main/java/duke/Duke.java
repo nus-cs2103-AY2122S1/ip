3b8b96d8a6c140a74fc1f4fa50782926ab7750c4
jodichoo <jodi.choo@gmail.com>
2021-09-01 20:34:01 +0800

    Refactor code into new command classes

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,75 +1,42 @@
 package duke;
 
+import duke.commands.Command;
+
 /**
  * The Duke class that runs the Duke program.
  */
 public class Duke {
     private static TaskList list;
     private static FileManager fm;
-    private Ui ui;
 
     /**
      * Constructor for Duke.
      */
     public Duke() {
-        ui = new Ui();
         fm = new FileManager();
         try {
             list = fm.getListFromFile();
         } catch (DukeException e) {
-            ui.showError(e);
             list = new TaskList();
         }
     }
 
-    private static void addTask(String[] args) throws DukeException {
-        switch (args[0]) {
-        case "todo":
-            list.add(new Todo(args[1]));
-            break;
-        case "deadline":
-            list.add(new Deadline(args[1], args[2]));
-            break;
-        case "event":
-            list.add(new Event(args[1], args[2]));
-            break;
-        default:
-            break;
-        }
-    }
-
     /**
-     * A method that runs the program.
+     * A method that gets the response from Duke to be displayed in the GUI.
+     *
+     * @param input User input to be responded to.
+     * @return Response to be displayed.
      */
-    public void run() {
-        ui.greet();
-        while (true) {
-            String input = ui.getInput();
-            Parser parser = new Parser(input);
-            try {
-                String[] args = parser.parse();
-                String cmd = args[0];
-                if (cmd.equals("bye")) {
-                    fm.writeToFile(list);
-                    ui.stopInput();
-                    break;
-                } else if (cmd.equals("list")) {
-                    ui.displayList(list);
-                } else if (cmd.equals("done") || cmd.equals("delete")) {
-                    list.editTask(args);
-                } else if (cmd.equals("find")) {
-                    TaskList matches = list.find(args[1]);
-                    ui.displayMatching(matches);
-                } else {
-                    addTask(args);
-                }
-            } catch (DukeException e) {
-                ui.showError(e);
-            }
+    public String getResponse(String input) {
+        Parser parser = new Parser(input);
+        try {
+            Command command = parser.parse();
+            String response = command.execute(list);
+            System.out.println(list.toString());
+            fm.writeToFile(list);
+            return response;
+        } catch (DukeException e) {
+            return e.getMessage(); 
         }
-    }
-
-    public static void main(String[] args) {
-        new Duke().run();
     }
 }
