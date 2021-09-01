2c9f41bccd8e1df37be5f5d464ca8387fd2fcb59
HongshuW <hongshu.wang.uni@gmail.com>
2021-09-01 14:50:39 +0800

    Duke class: stop extending Application

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -9,31 +9,17 @@ import duke.data.TaskList;
 import duke.exception.DukeException;
 import duke.ui.Parser;
 import duke.ui.Ui;
-import javafx.application.Application;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 /**
  * Represents the Duke program. Manages tasks based on commands received.
  */
-public class Duke extends Application {
+public class Duke {
     /** Storage that can write to or retrieve data from a file on hard disk */
     private Storage storage;
     /** List of tasks added */
     private TaskList tasks;
     /** UI of the program */
     private Ui ui;
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
 
     /**
      * Constructor of the class 'Duke'.
@@ -78,14 +64,6 @@ public class Duke extends Application {
     public static void main(String[] args) {
         String filePath = System.getProperty("user.dir");
         new Duke(Paths.get(filePath, "data", "duke1.txt")).run();
-    }
-
-    @Override
-    public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
     }
 
     public String getResponse(String input) {
