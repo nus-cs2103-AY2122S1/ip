426c55b1738ffe144e423085a224b1726d15bb29
Gabau <au.gabriel@yahoo.com.sg>
2021-09-01 10:35:56 +0800

    Add some documentation for javafx parts

--- src/main/java/util/windows/DialogBox.java
+++ src/main/java/util/windows/DialogBox.java
@@ -51,6 +51,7 @@ public class DialogBox extends HBox {
         setAlignment(Pos.TOP_LEFT);
     }
 
+
     public static DialogBox getUserDialog(String text, Image img) {
         return new DialogBox(text, img);
     }
