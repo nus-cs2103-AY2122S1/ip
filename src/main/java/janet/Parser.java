72b70e20c3656c03e1aeb2cdad6b58c5f48fcb7c
wpinrui <wongpinrui@gmail.com>
2021-09-01 22:53:20 +0800

    Parser: change strings to a more intuitive name

--- src/main/java/janet/Parser.java
+++ src/main/java/janet/Parser.java
@@ -15,61 +15,61 @@ public class Parser {
      * @throws JanetException If invalid or insufficient arguments are provided
      */
     public static Command parseUserInput(String input) throws JanetException {
-        String[] strings = input.split(" ");
-        String operation = strings[0];
+        String[] inputStringParts = input.split(" ");
+        String operation = inputStringParts[0];
         switch (operation) {
         case "bye":
             // Fallthrough
         case "list":
             return new Command(operation);
         case "done":
-            if (strings.length == 1) {
+            if (inputStringParts.length == 1) {
                 throw new JanetException(Ui.EXPECTED_DONE_INDEX_GOT_NONE);
             }
             try {
-                int doneTaskNum = Integer.parseInt(strings[1]);
-                return new Command(strings[0], doneTaskNum);
+                int doneTaskNum = Integer.parseInt(inputStringParts[1]);
+                return new Command(inputStringParts[0], doneTaskNum);
             } catch (NumberFormatException e) {
                 throw new JanetException(Ui.EXPECTED_DONE_INDEX_GOT_OTHER);
             }
         case "todo":
-            if (strings.length == 1) {
+            if (inputStringParts.length == 1) {
                 throw new JanetException(Ui.EXPECTED_TO_DO_DESCRIPTION);
             }
-            return new Command(strings[0], input.substring(5));
+            return new Command(inputStringParts[0], input.substring(5));
         case "deadline":
-            if (strings.length == 1) {
+            if (inputStringParts.length == 1) {
                 throw new JanetException(Ui.EXPECTED_DEADLINE_DESCRIPTION);
             }
             String[] descriptionAndBy = input.substring(9).split("/by ");
             if (descriptionAndBy.length == 1) {
                 throw new JanetException(Ui.EXPECTED_DEADLINE_BY);
             }
-            return new Command(strings[0], descriptionAndBy[0], descriptionAndBy[1]);
+            return new Command(inputStringParts[0], descriptionAndBy[0], descriptionAndBy[1]);
         case "event":
-            if (strings.length == 1) {
+            if (inputStringParts.length == 1) {
                 throw new JanetException(Ui.EXPECTED_EVENT_DESCRIPTION);
             }
             String[] descriptionAndAt = input.substring(6).split("/at ");
             if (descriptionAndAt.length == 1) {
                 throw new JanetException(Ui.EXPECTED_EVENT_AT);
             }
-            return new Command(strings[0], descriptionAndAt[0], descriptionAndAt[1]);
+            return new Command(inputStringParts[0], descriptionAndAt[0], descriptionAndAt[1]);
         case "delete":
-            if (strings.length == 1) {
+            if (inputStringParts.length == 1) {
                 throw new JanetException(Ui.EXPECTED_DELETED_INDEX_GOT_NONE);
             }
             try {
-                int delTaskNum = Integer.parseInt(strings[1]);
-                return new Command(strings[0], delTaskNum);
+                int delTaskNum = Integer.parseInt(inputStringParts[1]);
+                return new Command(inputStringParts[0], delTaskNum);
             } catch (NumberFormatException e) {
                 throw new JanetException(Ui.EXPECTED_DELETED_INDEX_GOT_OTHER);
             }
         case "find":
-            if (strings.length == 1) {
+            if (inputStringParts.length == 1) {
                 throw new JanetException(Ui.EXPECTED_SEARCH_QUERY);
             }
-            return new Command(strings[0], input.substring(5));
+            return new Command(inputStringParts[0], input.substring(5));
         default:
             throw new JanetException(Ui.UNRECOGNISED_OPERATION);
         }
