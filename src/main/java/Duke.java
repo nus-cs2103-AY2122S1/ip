9e74106f77ec9fd75327e74f22ff65caf619817b
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-09-02 23:47:03 +0800

    Move command parsing functionality from Duke to Parser

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,7 +23,6 @@ public class Duke {
     private DateTimeFormatter dtformatter;
     private Storage storage;
     private TaskList taskList;
-    private Parser parser;
 
     public Duke() {
         ui = new UserInterface();
@@ -32,126 +31,13 @@ public class Duke {
         Path dukePath = Paths.get(home, "Documents", "duke", "data.csv");
         storage = new Storage(dukePath);
         taskList = storage.load(ui);
-        parser = new Parser(this);
     }
 
     public void run() {
         ui.displayGreeting();
+        Command command;
         do {
-            String command = ui.getResponse();
-            if ("bye".equals(command)){
-                storage.save(ui, taskList);
-                break;
-            }
-            else if ("list".equals(command)){
-                System.out.println("Here are the tasks in your list:");
-                for (Task task : tasks){
-                    if (task.isTimed()){
-                        System.out.println(((TimedTask)task).format(dtformatter));
-                    } else {
-                        System.out.println(task.toString());
-                    }
-                }
-            }
-            else if (command.startsWith("done ")) {
-                for (Task task : tasks){
-                    if (command.substring(5).equals(task.getName())){
-                        task.markDone();
-                        System.out.println("Nice! I've marked this task as done:");
-                        System.out.println(task);
-                    }
-                }
-            }
-            else if (command.startsWith("todo ")) {
-                String name = command.substring(5);
-                Task task = new ToDo(name);
-                if (name.equals("")){
-                    System.out.println("Oops! The description of the task cannot be empty :(");
-                }
-                else {
-                    tasks.add(task);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(task);
-                }
-            }
-            else if (command.startsWith("deadline ")){
-                if (command.contains("/by ")) {
-                    String name = command.split("/by ", 2)[0].substring(9);
-                    String time = command.split("/by ", 2)[1];
-                    if (name.equals("") || time.equals("")){
-                        System.out.println("Oops! The name and/or time of a deadline cannot be empty");
-                    }
-                    else {
-                        try {
-                            Task task = new Deadline(name, time);
-                            tasks.add(task);
-                            System.out.println("Got it. I've added this task:");
-                            System.out.println(task);
-                        } catch (DateTimeParseException e) {
-                            System.err.println("Error: Date format not recognised");
-                        }
-                    }
-                }
-                else {
-                    System.out.println("Oops! The deadline must be by a certain time :(");
-                }
-            }
-            else if (command.startsWith("event ")){
-                if (command.contains("/at ")) {
-                    String name = command.split("/at ", 2)[0].substring(6);
-                    String time = command.split("/at ", 2)[1];
-                    if (name.equals("") || time.equals("")){
-                        System.out.println("Oops! The name and/or time of an event cannot be empty");
-                    }
-                    else {
-                        try {
-                            Task task = new Event(name, time);
-                            tasks.add(task);
-                            System.out.println("Got it. I've added this task:");
-                            System.out.println(task);
-                        } catch (DateTimeParseException e) {
-                            System.err.println("Error: Date format not recognised");
-                        }
-                    }
-                }
-                else {
-                    System.out.println("Oops! The event must be at a certain time :(");
-                }
-            }
-            else if (command.startsWith("delete ")){
-                String name = command.substring(7);
-                boolean taskFound = false;
-                for (Task task : tasks){
-                    if (task.getName().equals(name)){
-                        tasks.remove(task);
-                        System.out.println("Noted. I've removed this task:");
-                        System.out.println(task);
-                        taskFound = true;
-                        break;
-                    }
-                }
-                if (!taskFound){
-                    System.out.println("Oops! A task of that name could not be found :(");
-                }
-            }
-            else if (command.startsWith("dateformat ")){
-                try {
-                    String newDtformat = command.substring(11);
-                    dtformatter = DateTimeFormatter.ofPattern(newDtformat);
-                    System.out.println("Gotcha :D Dates are now printed as: " + newDtformat);
-                } catch (DateTimeParseException e) {
-                    System.err.println("Error: Date format not recognised");
-                }
-            }
-            else {
-                List<String> command_list = Arrays.asList("todo", "deadline", "event", "delete");
-                if (command_list.contains(command)){
-                    System.out.println("Oops! The task description cannot be empty L:(");
-                }
-                else {
-                    System.out.println("Oops! I'm sorry, but I don't know what that means :(");
-                }
-            }
+            command = Parser.parse(ui.getResponse());
         } while(command.shouldExecuteNext());
         storage.save(ui, taskList);
     }
