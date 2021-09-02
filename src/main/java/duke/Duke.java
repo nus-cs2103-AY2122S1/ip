0283fe43eeeec9e62e440a0af1fb73848168529d
zhuoyang125 <zhuoyang125@gmail.com>
2021-09-03 01:25:25 +0800

    Add Level-10

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -11,39 +11,33 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Duke {
+    private static TaskList taskList = new TaskList();
+    private static Parser parser = new Parser();
+    private static final String GREETING = "Hi, I'm Duke, your personal assistant!\n";
+    private static final String FAREWELL = "Bye from Duke!";
 
-    public static void main(String[] args) {
-        Ui ui = new Ui();
-        String home = System.getProperty("user.home");
-        Path filePath = Paths.get(home, "tasks.txt");
-        Storage storage = new Storage(filePath);
-        TaskList taskList = new TaskList();
-        ui.greetUser();
+    public static String initialize() {
         try {
-            taskList = storage.loadData();
-            System.out.println("Welcome Back!\n");
-            ui.printTasks(taskList);
-        } catch (IOException e) {
-            System.out.println("Your schedule is empty. What should I add to your schedule?");
-        }
-
-        while (true) {
-            String input = ui.readInput();
-            Parser parser = new Parser();
-            if (input.equals("bye")) {
-                ui.farewellToUser();
-                break;
-            } else {
-                try {
-                    parser.parseInput(taskList, input);
-                    storage.saveData(taskList);
-                } catch (DukeException | IOException e) {
-                    ui.displayError(e);
-                }
-
-            }
-
-
+            taskList.loadFromDisk();
+            return GREETING + taskList.toString();
+        } catch(IOException e) {
+            return GREETING + "Your schedule is empty. What should I add to your schedule?";
         }
     }
+
+    public static String runDuke(String input) {
+        if (input.equals("bye")) {
+            return FAREWELL;
+        } else {
+            try {
+                TaskList updatedTaskList = parser.parseInput(taskList, input);
+                return updatedTaskList.toString();
+            } catch (DukeException e) {
+                return e.getMessage();
+            }
+
+        }
+
+    }
+
 }
