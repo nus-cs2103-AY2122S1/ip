287cb4e58e811e9ff1c220c4e16b356f70a5b38e
Daniel Sim <dsrecompassvale@gmail.com>
2021-09-02 23:32:52 +0800

    Add FXML to project

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,12 +1,12 @@
 import javafx.application.Application;
 
-import duke.Duke;
+import duke.Main;
 
 /**
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Duke.class, args);
+        Application.launch(Main.class, args);
     }
 }
