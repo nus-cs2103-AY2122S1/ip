ec9b8f12c109b26e2bb2f75a181e4136a9116f62
Benjamin Tan <benjamin@dev.ofcr.se>
2021-09-03 01:13:18 +0800

    Allow running duke from command line as well

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,7 +1,5 @@
 package duke;
 
-import java.util.Scanner;
-
 import duke.command.Command;
 import duke.ui.Ui;
 
@@ -12,14 +10,6 @@ public class Duke {
 
     public Duke(String filePath) throws Exception {
         super();
-        this.storage = new Storage(filePath);
-        // TODO: throw error if unable to create file?
-        this.taskList = new TaskList(storage.loadTasks());
-    }
-
-    public Duke(Scanner in, String filePath) throws Exception {
-        super();
-//        this.ui = new ConsoleUi(in);
         this.storage = new Storage(filePath);
         // TODO: throw error if unable to create file?
         this.taskList = new TaskList(storage.loadTasks());
@@ -40,49 +30,4 @@ public class Duke {
             return false;
         }
     }
-
-//    /**
-//     * Runs the instance of {@link Duke}.
-//     */
-//    public void run() {
-//        ui.printGreeting();
-//
-//        boolean shouldExit = false;
-//        while (!shouldExit) {
-//            try {
-//                String fullCommand = ui.readCommand();
-//                Command command = Parser.parse(fullCommand);
-//                command.execute(taskList, ui, storage);
-//                shouldExit = command.shouldExit();
-//            } catch (Exception e) {
-//                // TODO: custom Duke exceptions?
-//                ui.printMessage("Error: " + e.getMessage());
-//            }
-//        }
-//
-//        ui.printGoodbye();
-//    }
-
-//    /**
-//     * Initializes an instance of the {@link Duke} class and runs the program.
-//     *
-//     * @param args The command-line arguments.
-//     */
-//    public static void main(String[] args) {
-//        Scanner inputScanner = new Scanner(System.in);
-//        String filePath = "duke.txt";
-//        Duke duke;
-//
-//        try {
-//            duke = new Duke(inputScanner, filePath);
-//        } catch (Exception e) {
-//            // TODO: figure out static/non-static Ui class
-//            System.out.println("Unable to initialize data file");
-//            inputScanner.close();
-//            return;
-//        }
-//
-//        duke.run();
-//        inputScanner.close();
-//    }
 }
--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -1,12 +1,55 @@
 package duke;
 
+import duke.command.Command;
+import duke.ui.ConsoleUi;
 import javafx.application.Application;
+
+import java.util.Scanner;
 
 /**
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Main.class, args);
+        if (args.length == 1 && args[0].equals("--console")) {
+            runConsole();
+        } else {
+            Application.launch(Main.class, args);
+        }
+    }
+
+    /**
+     * Initializes a instance of {@link Duke} in the console.
+     */
+    public static void runConsole() {
+        String filePath = "duke.txt";
+        Duke duke;
+
+        try {
+            duke = new Duke(filePath);
+        } catch (Exception e) {
+            // TODO: figure out static/non-static Ui class
+            System.out.println("Unable to initialize data file");
+            return;
+        }
+
+        Scanner inputScanner = new Scanner(System.in);
+        ConsoleUi ui = new ConsoleUi(inputScanner);
+        ui.printGreeting();
+        duke.setUi(ui);
+
+        boolean shouldExit = false;
+        while (!shouldExit) {
+            try {
+                String fullCommand = ui.readCommand();
+                shouldExit = duke.executeCommand(fullCommand);
+            } catch (Exception e) {
+                // TODO: custom Duke exceptions?
+                ui.printMessage("Error: " + e.getMessage());
+            }
+        }
+
+        ui.printGoodbye();
+        inputScanner.close();
     }
 }
