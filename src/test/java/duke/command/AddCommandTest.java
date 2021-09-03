c27cf3a72efa2b0526ba378a6629f30d7f9c562d
DesmondTo <e0550619@u.nus.edu>
2021-09-03 11:46:01 +0800

    Resolve coding standard violations

--- src/test/java/duke/command/AddCommandTest.java
+++ src/test/java/duke/command/AddCommandTest.java
@@ -1,35 +1,57 @@
 package duke.command;
-
 import duke.exception.IncompleteDescriptionException;
+import duke.exception.InvalidDateFormatException;
 import duke.util.Storage;
 import duke.util.TaskList;
 import duke.util.Ui;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * A test class which tests if addCommand's methods work as intended.
+ */
 public class AddCommandTest {
-    AddCommand addTodo = new AddCommand("TODO", "borrow book");
+    // Initialize problematic addCommand instances.
     AddCommand emptyEvent = new AddCommand("EVENT", "");
     AddCommand emptyDeadline = new AddCommand("DEADLINE", "");
+    AddCommand invalidEventDate = new AddCommand("EVENT", "sample event /at tomorrow");
+    AddCommand invalidDeadlineDate = new AddCommand("DEADLINE", "sample deadline /by tomorrow");
+
+    // Initialize dummy arguments to put into execute method.
     Ui ui = new Ui();
     Storage storage = new Storage("data/tasks.txt");
     TaskList taskList = new TaskList();
 
+    /**
+     * Passes test if IncompleteDescriptionException is thrown when the event has empty description.
+     */
     @Test
-    public void isExitTest() {
-        assertEquals(false, addTodo.isExit());
-    }
-
-    @Test
-    public void eventExecuteTest() {
+    public void addCommand_emptyEvent_exceptionThrown() {
         assertThrows(IncompleteDescriptionException.class, () -> emptyEvent.execute(taskList, ui, storage));
     }
 
+    /**
+     * Passes test IncompleteDescriptionException is thrown when the deadline has empty description.
+     */
     @Test
-    public void deadlineExecuteTest() {
-        assertThrows(IncompleteDescriptionException.class, () -> emptyEvent.execute(taskList, ui, storage));
+    public void addCommand_emptyDeadline_exceptionThrown() {
+        assertThrows(IncompleteDescriptionException.class, () -> emptyDeadline.execute(taskList, ui, storage));
     }
 
+    /**
+     * Passes test InvalidDateFormatException is thrown when the event has invalid date format.
+     */
+    @Test
+    public void addCommand_invalidEventDate_exceptionThrown() {
+        assertThrows(InvalidDateFormatException.class, () -> invalidEventDate.execute(taskList, ui, storage));
+    }
+
+    /**
+     * Passes test InvalidDateFormatException is thrown when the deadline has invalid date format.
+     */
+    @Test
+    public void addCommand_invalidDeadlineDate_exceptionThrown() {
+        assertThrows(InvalidDateFormatException.class, () -> invalidDeadlineDate.execute(taskList, ui, storage));
+    }
 }
--- src/test/java/util/ParserTest.java
+++ src/test/java/util/ParserTest.java
@@ -1,25 +1,36 @@
 package util;
 
 import duke.exception.EmptyDescriptionException;
-import duke.exception.IncompleteDescriptionException;
+
 import duke.util.Parser;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * A test class which tests if Parser's methods work as intended.
+ */
 public class ParserTest {
 
+    /**
+     * Passes test when EmptyDescriptionException is thrown when the command is empty.
+     */
     @Test
     public void emptyCommandTest() {
         assertThrows(EmptyDescriptionException.class, () -> Parser.parse(""));
     }
 
+    /**
+     * Passes test when EmptyDescriptionException is thrown when the command is incomplete.
+     */
     @Test
     public void incompleteCommandTest() {
         assertThrows(EmptyDescriptionException.class, () -> Parser.parse("todo "));
     }
 
+    /**
+     * Passes test when EmptyDescriptionException is thrown when the command consists of purely whitespace.
+     */
     @Test
     public void allSpaceCommandTest() {
         assertThrows(EmptyDescriptionException.class, () -> Parser.parse("      "));
