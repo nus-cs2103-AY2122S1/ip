c4a7bec6a8b396620fc0901a70d7894a20f336d4
Siew Hui Zhuan <huizhuansam@hotmail.com>
2021-09-03 18:07:42 +0800

    Fix line length

--- src/main/java/duke/command/AddCommand.java
+++ src/main/java/duke/command/AddCommand.java
@@ -70,7 +70,9 @@ public class AddCommand extends Command {
         tasks.addTask(task);
         return formatOutput("Hehe buoi, I've added this task:",
                 task.toString(),
-                String.format("Now you have %d %s in the list.", tasks.getSize(), tasks.getSize() == 1 ? "task" : "tasks"));
+                String.format("Now you have %d %s in the list.",
+                        tasks.getSize(),
+                        tasks.getSize() == 1 ? "task" : "tasks"));
     }
 
     /**
