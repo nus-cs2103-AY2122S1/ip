a4b62091568ffb19bb6d8d3d9fe19fb899f67adc
Patrick <patricktan9955@hotmail.com>
2021-09-03 19:27:53 +0800

    Fix more coding standard violations in test classes

--- src/test/java/DukeCommandsTest.java
+++ src/test/java/DukeCommandsTest.java
@@ -1,31 +1,32 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Test;
 
 import duke.PersistentStorage;
 import duke.Tasklist;
 import duke.UI;
-import duke.commands.*;
-
-import duke.tasks.Task;
-import org.junit.jupiter.api.Test;
-import java.time.LocalDateTime;
-import java.util.Scanner;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import duke.commands.ByeCommand;
+import duke.commands.Command;
+import duke.commands.DeadlineCommand;
 
 public class DukeCommandsTest {
     @Test
-    public void ByeCommandTest() {
+    public void byeCommandTest() {
         Command c = new ByeCommand();
         assertEquals(true, c.isExit());
     }
 
     @Test
-    public void DeadlineCommandTest() {
+    public void deadlineCommandTest() {
         UI ui = new UI(new Scanner(System.in));
         Tasklist taskList = new Tasklist();
         PersistentStorage storage = new PersistentStorage("./test.txt");
 
         DeadlineCommand c = new DeadlineCommand("test description",
-                LocalDateTime.of(2012,2,2,16,30));
+                LocalDateTime.of(2012, 2, 2, 16, 30));
 
         c.executeCommand(taskList, ui, storage);
         String expected = "1. [D][ ] test description (by: Feb 02 2012 16:30)";
--- src/test/java/DukeTasksTest.java
+++ src/test/java/DukeTasksTest.java
@@ -1,14 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
 import duke.tasks.Deadline;
 import duke.tasks.ToDo;
-import org.junit.jupiter.api.Test;
-import java.time.LocalDateTime;
-import java.util.Scanner;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTasksTest {
     @Test
-    public void ToDoTaskTest() {
+    public void toDoTaskTest() {
         ToDo item = new ToDo("test todo description");
         item.completeTask();
         String expected = "T | 1 | test todo description";
@@ -16,9 +17,9 @@ public class DukeTasksTest {
     }
 
     @Test
-    public void DeadlineTaskTest() {
+    public void deadlineTaskTest() {
         Deadline item = new Deadline("some deadline description",
-                LocalDateTime.of(2011,12,2,4,59));
+                LocalDateTime.of(2011, 12, 2, 4, 59));
         String expected = "[D][ ] some deadline description (by: Dec 02 2011 04:59)";
         assertEquals(expected, item.toString());
     }
