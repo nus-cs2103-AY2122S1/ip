335e18d9a0d332eb036289a050fd005c132ac4fa
GabrielWLM <gabrielwlmatienzo@gmail.com>
2021-09-03 23:49:48 +0800

    Level-10 git commit -m Level-10 git commit -m Level-10

--- src/main/java/Dates.java
+++ src/main/java/Dates.java
@@ -10,6 +10,7 @@ public class Event {
     public Event(String input) {
         //create dates from strings
         this.eventDate = LocalDate.parse(input);
-        this.eventDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));        
+        this.eventDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+
     }
 }
