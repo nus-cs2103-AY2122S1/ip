ee5fd96f7eaf7a84da684e906c9234ee24b973b3
qreoct <9080974+qreoct@users.noreply.github.com>
2021-09-04 01:08:03 +0800

    Change tests to not rely on unix/windows differences

---
+++ src/test/java/duke/io/ParserTest.java
@@ -0,0 +1,53 @@
+package duke.io;
+
+import duke.commands.Command;
+import duke.commands.DeleteCommand;
+import duke.commands.ListCommand;
+import duke.exceptions.AuguryException;
+import duke.exceptions.InvalidActionException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParserTest {
+
+    @Test
+    public void getListCommandFromInput() throws AuguryException {
+        Parser p = new Parser();
+        Command c = p.parse("list");
+        assertTrue(c instanceof ListCommand);
+    }
+
+    @Test
+    public void getDeleteCommandFromInput() throws AuguryException {
+        Parser p = new Parser();
+        Command c = p.parse("delete 2,3");
+        assertTrue(c instanceof DeleteCommand);
+    }
+
+    @Test
+    public void invalidInput_nonInteger_exceptionThrown() throws AuguryException {
+        Parser p = new Parser();
+        Exception e = assertThrows(InvalidActionException.class, () -> {
+            Command c = p.parse("delete 1,a,b,c");
+        });
+
+        String expectedMessage = "Please enter a valid task number.";
+        String actualMessage = e.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void invalidInput_negativeInteger_exceptionThrown() throws AuguryException {
+        Parser p = new Parser();
+        Exception e = assertThrows(InvalidActionException.class, () -> {
+            Command c = p.parse("delete 1,-999");
+        });
+
+        String expectedMessage = "Please enter a valid task number.";
+        String actualMessage = e.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+}
