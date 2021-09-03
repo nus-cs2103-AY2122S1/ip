2ed2c0874ce38c5cdb0d79608f544b730f1aa93d
GWphua <phuaguanwei99@gmail.com>
2021-09-03 13:59:23 +0800

    Implement JavaFX Hello World

--- src/main/java/virushade/Virushade.java
+++ src/main/java/virushade/Virushade.java
@@ -1,11 +1,15 @@
 package virushade;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import virushade.tasks.TaskList;
 
 /**
  * The Main class of the application.
  */
-public class Virushade {
+public class Virushade extends Application {
     private Storage storageFile;
     private TaskList taskList;
 
@@ -30,10 +34,15 @@ public class Virushade {
     }
 
     /**
-     * Runs our program under "data/Virushade.txt".
-     * @param args Not used.
+     * Starts the Virushade application.
+     * @param stage The stage.
      */
-    public static void main(String[] args) {
-        new Virushade("data/Virushade.txt").run();
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
