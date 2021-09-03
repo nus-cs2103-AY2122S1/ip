c95cf985dd7009fbbd5ec8bf5084e4f161421cba
Zhouyirui <72332263+Zhouyirui@users.noreply.github.com>
2021-09-03 08:50:30 +0800

    Add testing to Deadline

--- src/test/java/duke/TodoTest.java
+++ src/test/java/duke/TodoTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TodoTest {
     @Test
-    public void dummyTest(){
+    public void printString(){
         assertEquals("[T][ ] this", new Todo("this", false).toString());
     }
 }
