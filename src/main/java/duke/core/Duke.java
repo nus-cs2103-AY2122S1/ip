b3024297958d6874fe35a2b0f1ec32d79808ea3a
BenjaminLHR <benjamin.lee.hr@gmail.com>
2021-09-03 15:53:49 +0800

    Add method getResponseAndExitStatus

--- src/main/java/duke/core/Duke.java
+++ src/main/java/duke/core/Duke.java
@@ -32,6 +32,21 @@ public class Duke {
         }
     }
 
+    /**
+     * Returns a String array with two elements. The first element is Duke's response to a given user input, and the
+     * second element indicates whether Duke should exit, with "1" indicating yes and "0" indicating no.
+     *
+     * @param input The user's input
+     * @return A String array with two elements. The first element is Duke's response to a given user input, and the
+     * second element indicates whether Duke should exit, with "1" indicating yes and "0" indicating no.
+     */
+    public String[] getResponseAndExitStatus(String input) {
+        Command command = Parser.parse(input, taskList);
+        String response = command.execute(taskList, storage);
+        String exitStatus = command.shouldExit() ? "1" : "0";
+        return new String[] {response, exitStatus};
+    }
+
     public static void main(String[] args) {
         new Duke("data/duke.txt").run();
     }
