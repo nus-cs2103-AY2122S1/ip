3b3d6c161954dd4a42de27a6f8d099ab9ae20263
Py0000 <>
2021-09-04 20:19:47 +0800

    Update Javadoc for UpdateCommand Class

--- src/main/java/duke/commands/UpdateCommand.java
+++ src/main/java/duke/commands/UpdateCommand.java
@@ -9,7 +9,19 @@ import duke.gui.Ui;
 import duke.tasks.Task;
 import duke.tasks.TaskList;
 
+/**
+ * Encapsulates the information for the UpdateCommand object.
+ * Format to activate the update command: "update taskIndex tag newUpdate"
+ * There is a total of 3 tags:
+ * 1) title, to update the description of the task
+ * 2) date, to update the due date of the task if it exist
+ * 3) status, to update the completion status of the task
+ */
 public class UpdateCommand extends Command {
+    private final String TITLE_TAG = "title";
+    private final String DATE_TAG = "date";
+    private final String STATUS_TAG = "status";
+
     private final int id;
     private final String tag;
     private final String information;
@@ -39,13 +51,13 @@ public class UpdateCommand extends Command {
 
     private void checkTag(Task task) throws IllegalFormatException, UpdateFailException {
         switch (tag) {
-        case "title":
+        case TITLE_TAG:
             updateTitle(task);
             break;
-        case "status":
+        case STATUS_TAG:
             updateStatus(task);
             break;
-        case "date":
+        case DATE_TAG:
             updateDate(task);
             break;
         default:
@@ -54,14 +66,31 @@ public class UpdateCommand extends Command {
         }
     }
 
+    /**
+     * Updates the description of the task specified.
+     *
+     * @param task The task to be updated.
+     */
     private void updateTitle(Task task) {
         task.setDescription(information);
     }
 
+    /**
+     * Updates the status of the task specified.
+     * The word that follows after "status" in the command indicates:
+     * true indicates that the user wants to update the completion status to completed.
+     * false indicates that the user wants to reset the completion status. (Not completed)
+     *
+     * @param task The task to be updated.
+     * @throws IllegalFormatException Wrong format used by user.
+     */
     private void updateStatus(Task task) throws IllegalFormatException {
-        if (information.equalsIgnoreCase("true")) {
+        final String markAsComplete = "true";
+        final String markAsIncomplete = "false";
+
+        if (information.equalsIgnoreCase(markAsComplete)) {
             task.markAsComplete();
-        } else if (information.equalsIgnoreCase("false")) {
+        } else if (information.equalsIgnoreCase(markAsIncomplete)) {
             task.markAsIncomplete();
         } else {
             String errorMsg = "Please key in true/false only!\n"
@@ -70,15 +99,24 @@ public class UpdateCommand extends Command {
         }
     }
 
+    /**
+     * Updates the date of the task specified, if this field exist.
+     *
+     * @param task The task to be updated.
+     * @throws UpdateFailException User tries to update a task without a date field.
+     * @throws IllegalFormatException Wrong format used by user.
+     */
     private void updateDate(Task task) throws UpdateFailException, IllegalFormatException {
         String taskType = task.getTag();
         boolean isInformationDate = tag.equalsIgnoreCase("date");
         boolean isTaskTodo = taskType.equalsIgnoreCase("T");
         boolean isUnableToUpdate = isInformationDate && isTaskTodo;
 
+        // User tries to update the date of a Todo task.
         if (isUnableToUpdate) {
             throw new UpdateFailException();
         }
+
         try {
             task.setDate(information);
         } catch (DateTimeParseException e) {
@@ -87,6 +125,11 @@ public class UpdateCommand extends Command {
         }
     }
 
+    /**
+     * Throws an IllegalFormatException as user entered a tag that is unrecognised by the chat bot.
+     *
+     * @throws IllegalFormatException Wrong format used by user.
+     */
     private void invalidUpdate() throws IllegalFormatException {
         String errorMsg = "Plase check if you have used the correct format.\n "
                 + "Also, Please note that tag can only be either one of the 3:\n"
