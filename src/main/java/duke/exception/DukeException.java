39f05ca13bb6a0fb1fdf3de696fef00ecef537fc
luminousleek <68138671+luminousleek@users.noreply.github.com>
2021-09-04 15:00:40 +0800

    Add Exception subclasses
    There used to be only one DukeException class to encapsulate all
    the errors in Duke.
    
    This made throwing a new error tedious as a new error message had
    to be passed in every time, and editing error messages would also
    be a pain.
    
    Let's
    * Create new exceptions for common exceptions
    * Move all these new exceptions to the duke.exception package

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -1,11 +1,13 @@
-package duke;
+package duke.exception;
 
 /**
  * The DukeException encapsulates all Exceptions that are thrown when using Duke.
  */
 public class DukeException extends Exception {
 
+    private static final String ERROR_EXCLAMATION = "oh no!";
+
     public DukeException(String msg) {
-        super(msg);
+        super(ERROR_EXCLAMATION + msg);
     }
 }
