d3545d07488db0bc011e3401f90933080bfd8490
houtenteo <houtenteo@gmail.com>
2021-09-05 15:21:00 +0800

    Add distinction in CLI and GUI duke launch methods

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -5,7 +5,12 @@ import javafx.application.Application;
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
+    /**
+     * Main method to launch Duke.
+     * @param args
+     */
     public static void main(String[] args) {
-        Application.launch(Duke.class, args);
+        //new Duke().startCli(); //Method to launch CLI version of duke
+        Application.launch(Duke.class, args); //Method to launch GUI version of duke
     }
 }
