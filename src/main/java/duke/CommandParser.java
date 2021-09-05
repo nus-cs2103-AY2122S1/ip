62c5a2cc09795b034a9d0bf0459645663265d503
pbthang <bathang02@gmail.com>
2021-09-05 11:28:45 +0800

    add assertions

--- src/main/java/duke/CommandParser.java
+++ src/main/java/duke/CommandParser.java
@@ -160,10 +160,10 @@ public class CommandParser {
 
     private static Command parseFindCommand(String commandString) throws DukeException {
         String payload = commandString.substring(FIND_PREFIX.length()).trim();
-       if (payload.length() <= 0) {
-           throw new DukeException("Please indicate a keyword to find tasks!");
-       } else {
-           return new FindCommand(payload);
-       }
+        if (payload.length() <= 0) {
+            throw new DukeException("Please indicate a keyword to find tasks!");
+        } else {
+            return new FindCommand(payload);
+        }
     }
 }
