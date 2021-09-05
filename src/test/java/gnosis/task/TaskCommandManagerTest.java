3c8633bd82316e3ed7b6e988dff8c0b384bfdfcb
Pawandeep Singh <pawandeepse@me.com>
2021-09-05 21:45:02 +0800

    Update Gnosis test cases

--- src/test/java/gnosis/task/TaskCommandManagerTest.java
+++ src/test/java/gnosis/task/TaskCommandManagerTest.java
@@ -1,17 +1,20 @@
 package gnosis.task;
 
-import gnosis.util.GnosisException;
-import gnosis.model.Todo;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import gnosis.model.Todo;
+import gnosis.util.GnosisException;
+
 
 public class TaskCommandManagerTest {
 
     @Test
-    public void getTasks_nullList_NullPointerExceptionThrown() {
+    public void getTasksNullListNullPointerExceptionThrown() {
         TaskCommandManager taskCommandManager = new TaskCommandManager(null);
         assertNull(taskCommandManager.getTasks(), "ERROR: There is tasks");
     }
@@ -20,7 +23,7 @@ public class TaskCommandManagerTest {
     public void addTodoToTaskManager_someValue_success() throws GnosisException {
         TaskCommandManager taskCommandManager = new TaskCommandManager(new ArrayList<>());
         Todo td = taskCommandManager.addTodo("JUNIT Test");
-        assertEquals(td,taskCommandManager.getTasks().get(0));
+        assertEquals(td, taskCommandManager.getTasks().get(0));
     }
 
 
