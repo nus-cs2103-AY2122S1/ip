0bfb02970b7d7c2c14cc26c2de0fad287e8f218b
Chang-CH <e0543479@u.nus.edu>
2021-09-05 14:47:48 +0800

    Fix Parser imports

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -5,13 +5,12 @@ import java.time.LocalDate;
 // Duke Commands
 import duke.command.AddCommand;
 import duke.command.Command;
-import duke.command.Command;
-import duke.command.AddCommand;
 import duke.command.DeleteCommand;
 import duke.command.DoneCommand;
 import duke.command.ExitCommand;
 import duke.command.FindCommand;
 import duke.command.ListCommand;
+import duke.command.HelpCommand;
 // Duke Exceptions
 import duke.exception.DukeException;
 // Duke Tasks
