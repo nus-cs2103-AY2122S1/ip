bb0c92cc9db5a596e71b9d1bbced8c96b86dc4ea
Samay Sagar <manu.sagarg1@gmail.com>
2021-09-06 00:43:14 +0800

    Checkstyle for Test files

--- src/test/java/duke/task/DeadlineTest.java
+++ src/test/java/duke/task/DeadlineTest.java
@@ -1,21 +1,20 @@
 package duke.task;
 
-import org.junit.jupiter.api.Test;
-import duke.task.Deadline;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class DeadlineTest {
     @Test
-    public void createNewDeadlineTest(){
+    public void createNewDeadlineTest() {
         Deadline deadline = new Deadline("Submission", LocalDate.parse("2021-02-11"));
         assertEquals("[D][ ] Submission (by: Feb 11 2021)", deadline.toString());
     }
 
     @Test
-    public void markDeadlineAsDoneTest(){
+    public void markDeadlineAsDoneTest() {
         Deadline deadline = new Deadline("Submission", LocalDate.parse("2021-02-11"));
         deadline.completeTask();
         assertEquals("[D][x] Submission (by: Feb 11 2021)", deadline.toString());
--- src/test/java/duke/task/EventTest.java
+++ src/test/java/duke/task/EventTest.java
@@ -1,21 +1,20 @@
 package duke.task;
 
-import org.junit.jupiter.api.Test;
-import duke.task.Event;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class EventTest {
     @Test
-    public void createNewEventTest(){
+    public void createNewEventTest() {
         Event event = new Event("Project meeting", LocalDate.parse("2021-01-01"));
         assertEquals("[E][ ] Project meeting (at: Jan 1 2021)", event.toString());
     }
 
     @Test
-    public void markEventAsDoneTest(){
+    public void markEventAsDoneTest() {
         Event event = new Event("Project meeting", LocalDate.parse("2021-01-01"));
         event.completeTask();
         assertEquals("[E][x] Project meeting (at: Jan 1 2021)", event.toString());
