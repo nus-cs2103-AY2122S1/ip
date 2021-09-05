70f7986bd40ec6549a2ef16534cda6e2b4ceb65a
Preshita01 <pandipreshita@gmail.com>
2021-09-05 23:42:28 +0800

    Code quality: structure code logically and store string outputs as constants
    The body of most methods is written as one block with no line breaks
    in between to identify the various parts of the body that handle
    different parts of the logic. Specific strings at different parts of the
    code also makes it harder to understand why the string is returned
    there, as well as to modify it.
    
    Grouping method bodies according to the logic they handle and adding
    line breaks to separate the parts improves readability of the code.
    Storing the strings to be returned as aptly-named constants and then
    calling on the constants themselves makes it easier to understand and
    modify the strings.
    
    Let's,
    * group method bodies according to the logic they handle and add line
    breaks to separate the parts
    * add private constants outside of the methods and assign them to the
    various string outputs

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,7 +1,6 @@
 package duke;
 import java.io.IOException;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 /**
  * Class which operates Jarvis the chat-bot
@@ -11,8 +10,13 @@ public class Duke {
     private TaskList tasks;
     private Ui ui;
 
+    private final String NO_TASKS = "There are currently no tasks on your list :)";
+    private final String UNRECOGNISED_COMMAND = "I'm sorry, but I don't know what that means :(";
+    private final String BYE = "Bye! Hope to see you soon :)";
+
     /**
      * Retrieves all the tasks stored by Jarvis in the hard disk upon running the main method.
+     *
      * @param filePath The file in which the tasks are stored
      */
     public Duke(String filePath) {
@@ -27,42 +31,42 @@ public class Duke {
     }
 
     /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
+     * Returns the response to be given by Jarvis to the user upon receiving a command
+     *
+     * @param input the command given by the user
      */
     public String getResponse(String input) {
-        while (!Parser.parseCommand(input).equals("bye")) {
             try {
-                if (Parser.parseCommand(input).equals("list")) {
-                    if (TaskList.getCounter() == 0) {
-                        return "\tThere are currently no tasks on your list :)";
-                    } else {
-                        return Parser.parseList();
-                    }
-                } else if (Parser.parseCommand(input).equals("done")) {
-                    return Parser.parseDone(input);
-                } else if (Parser.parseCommand(input).equals("delete")) {
-                    return Parser.parseDelete(input);
-                } else if (Parser.parseCommand(input).equals("todo")) {
-                    return Parser.parseTodo(input);
-                } else if (Parser.parseCommand(input).equals("deadline")) {
-                    return Parser.parseDeadline(input);
-                } else if (Parser.parseCommand(input).equals("event")) {
-                    return Parser.parseEvent(input);
-                } else if (Parser.parseCommand(input).equals("today")) {
-                    return Parser.parseToday();
-                } else if (Parser.parseCommand(input).equals("find")) {
-                    return Parser.parseFind(input);
-                } else {
-                    throw new DukeException("OOPS!!! I'm sorry, but I don't " +
-                            "know what that means :-(");
+                switch (Parser.parseCommand(input)) {
+                    case "list":
+                        // If there are no tasks in the task list
+                        if (TaskList.getCounter() == 0) {
+                            return "\t" + NO_TASKS;
+                        } else {
+                            return Parser.parseList();
+                        }
+                    case "done":
+                        return Parser.parseDone(input);
+                    case "delete":
+                        return Parser.parseDelete(input);
+                    case "todo":
+                        return Parser.parseTodo(input);
+                    case "deadline":
+                        return Parser.parseDeadline(input);
+                    case "event":
+                        return Parser.parseEvent(input);
+                    case "today":
+                        return Parser.parseToday();
+                    case "find":
+                        return Parser.parseFind(input);
+                    case "bye":
+                        return BYE;
+                    default:
+                        throw new DukeException(UNRECOGNISED_COMMAND);
                 }
             } catch (DukeException | IOException e) {
-                //System.err.println(e);
                 return "" + e;
             }
-        }
-        return "Bye! Hope to see you soon :)";
     }
 }
 
