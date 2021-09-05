cdd22acfa26b18db07e687734a0f878ef92045f4
xlzior <wenjun.lye@gmail.com>
2021-09-05 21:30:59 +0800

    Add assertions to assert that tasks is not null

--- src/main/java/iris/command/ListCommand.java
+++ src/main/java/iris/command/ListCommand.java
@@ -5,6 +5,7 @@ import iris.TaskList;
 public class ListCommand extends Command {
     @Override
     public String run(TaskList tasks) {
+        assert tasks != null;
         StringBuilder response = new StringBuilder();
         for (int i = 0; i < tasks.getCount(); i++) {
             response.append(String.format("%d. %s\n", i + 1, tasks.get(i)));
