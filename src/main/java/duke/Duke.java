fe8bef2747f86b05853cc25ea9477c701d7b37f9
rehad-a <rahaad@hotmail.com>
2021-09-05 13:48:23 +0800

    Remove thrown exception from main header

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -50,7 +50,7 @@ public class Duke {
         }
     }
 
-    public static void main(String[] args) throws DukeException {
+    public static void main(String[] args) {
         new Duke("./data/", "duke.txt").run();
     }
 }
