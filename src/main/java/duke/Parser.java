3751383cb69d62e7f8e52edfe5240a2556b791ec
Gabriel <77230723+gycgabriel@users.noreply.github.com>
2021-09-05 15:22:55 +0800

    Rearrange import statements

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,11 +1,5 @@
 package duke;
 
-import com.zoho.hawking.HawkingTimeParser;
-import com.zoho.hawking.datetimeparser.configuration.HawkingConfiguration;
-import com.zoho.hawking.language.english.model.DatesFound;
-import com.zoho.hawking.language.english.model.ParserOutput;
-import org.joda.time.DateTime;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -15,6 +9,14 @@ import java.time.temporal.ChronoField;
 import java.time.temporal.Temporal;
 import java.util.Date;
 import java.util.Locale;
+
+import org.joda.time.DateTime;
+
+import com.zoho.hawking.HawkingTimeParser;
+import com.zoho.hawking.datetimeparser.configuration.HawkingConfiguration;
+import com.zoho.hawking.language.english.model.DatesFound;
+import com.zoho.hawking.language.english.model.ParserOutput;
+
 
 /**
  * Parser handles parsing of user input.
@@ -30,6 +32,143 @@ public class Parser {
     private Temporal datetime;
     private String searchKey;
     private boolean haveParameters;
+
+    /**
+     * Parses temporal string from load file.
+     *
+     * @param datetime Date-time string
+     * @return Temporal instance
+     * @throws DateTimeFormatException Incorrect date time format given
+     */
+    public static Temporal parseLoadTemporal (String datetime) throws DateTimeFormatException {
+        try {
+            if (datetime.contains("All day")) {
+                return LocalDate.parse(
+                        datetime.replace("All day", "").trim(),
+                        DateTimeFormatter.ofPattern("MMM dd yyyy", Locale.ENGLISH));
+            } else {
+                return LocalDateTime.parse(
+                        datetime,
+                        DateTimeFormatter.ofPattern("MMM dd yyyy h.mma", Locale.ENGLISH));
+            }
+        } catch (DateTimeParseException e) {
+            throw new DateTimeFormatException(datetime);
+        }
+    }
+
+    /**
+     * Parses Date or Date and Time into computer-understandable format.
+     *
+     * @param datetime Input date and time
+     * @return Temporal instance
+     * @throws DateTimeFormatException Invalid date-time format
+     */
+    public static Temporal parseTemporal(String datetime) throws DateTimeFormatException {
+        HawkingTimeParser parser = new HawkingTimeParser();
+        DateTime jodaStart;
+        DateTime jodaEnd;
+        try {
+            DatesFound datesFound = parser.parse(datetime, new Date(), new HawkingConfiguration(), "eng");
+            ParserOutput po = datesFound.getParserOutputs().get(0);
+            jodaStart = po.getDateRange().getStart();
+            jodaEnd = po.getDateRange().getEnd();
+        } catch (Exception e) {
+            throw new DateTimeFormatException(datetime);
+        }
+        assert jodaStart != null && jodaEnd != null;
+        Temporal result;
+        try {
+            LocalDateTime ldtStart = LocalDateTime.of(
+                    jodaStart.getYear(),
+                    jodaStart.getMonthOfYear(),
+                    jodaStart.getDayOfMonth(),
+                    jodaStart.getHourOfDay(),
+                    jodaStart.getMinuteOfHour()
+            );
+            LocalDateTime ldtEnd = LocalDateTime.of(
+                    jodaEnd.getYear(),
+                    jodaEnd.getMonthOfYear(),
+                    jodaEnd.getDayOfMonth(),
+                    jodaEnd.getHourOfDay(),
+                    jodaEnd.getMinuteOfHour()
+            );
+
+            if (ldtStart.plusHours(23).plusMinutes(59).equals(ldtEnd)) {
+                result = ldtEnd.toLocalDate();
+            } else if (ldtEnd.getHour() == 23 && ldtEnd.getMinute() == 59) {
+                result = ldtEnd.toLocalDate();
+            } else {
+                result = ldtEnd;
+            }
+
+        } catch (DateTimeParseException e) {
+            throw new DateTimeFormatException(datetime + "could not convert to local date time!");
+        }
+        assert result != null;
+        return result;
+        /*
+        if (datetime.trim().split("\\s+", 2).length < 2) { // no time given
+            return parseDate(datetime);
+        } else {
+            return parseDateTime(datetime);
+        }
+        */
+    }
+
+    /**
+     * Parses Date and Time into computer-understandable format.
+     *
+     * @param datetime Input date and time
+     * @return LocalDateTime instance
+     * @throws DateTimeFormatException Invalid date-time format
+     */
+    private static LocalDateTime parseDateTime(String datetime) throws DateTimeFormatException {
+        try {
+            return LocalDateTime.parse(datetime,
+                    new DateTimeFormatterBuilder()
+                            .parseCaseInsensitive()
+                            .parseLenient()
+                            .appendOptional(DateTimeFormatter.ofPattern("d/M/yyyy"))
+                            .appendOptional(DateTimeFormatter.ofPattern("yyyy-M-d"))
+                            .appendOptional(DateTimeFormatter.ofPattern("dMMMyyyy"))
+                            .parseDefaulting(ChronoField.YEAR_OF_ERA, 2021)
+                            .appendOptional(DateTimeFormatter.ofPattern("d/M"))
+                            .appendOptional(DateTimeFormatter.ofPattern("dMMM"))
+                            .appendLiteral(" ")
+                            .appendOptional(DateTimeFormatter.ofPattern("HHmm"))
+                            .appendOptional(DateTimeFormatter.ofPattern("h.mma"))
+                            .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
+                            .appendOptional(DateTimeFormatter.ofPattern("ha"))
+                            .toFormatter(Locale.ENGLISH));
+        } catch (DateTimeParseException e) {
+            throw new DateTimeFormatException(datetime);
+        }
+    }
+
+    /**
+     * Parses Date into computer-understandable format.
+     *
+     * @param date Input date
+     * @return LocalDate instance
+     * @throws DateTimeFormatException Invalid date format
+     */
+    private static LocalDate parseDate(String date) throws DateTimeFormatException {
+        try {
+            return LocalDate.parse(date,
+                    new DateTimeFormatterBuilder()
+                            .parseCaseInsensitive()
+                            .parseLenient()
+                            .appendOptional(DateTimeFormatter.ofPattern("d/M/yyyy"))
+                            .appendOptional(DateTimeFormatter.ofPattern("yyyy-M-d"))
+                            .appendOptional(DateTimeFormatter.ofPattern("dMMMyyyy"))
+                            .parseDefaulting(ChronoField.YEAR_OF_ERA, 2021)
+                            .appendOptional(DateTimeFormatter.ofPattern("d/M"))
+                            .appendOptional(DateTimeFormatter.ofPattern("dMMM"))
+                            .toFormatter(Locale.ENGLISH));
+        } catch (DateTimeParseException e) {
+            throw new DateTimeFormatException(date);
+        }
+    }
 
     /**
      * Parse input arguments and assign parser class variables to corresponding input.
@@ -146,7 +285,6 @@ public class Parser {
         return true; // parsed succesfully
     }
 
-
     private ParsedInput getParsedInput() {
         return new ParsedInput(
                 this.command,
@@ -155,144 +293,6 @@ public class Parser {
                 this.index,
                 this.taskType,
                 this.searchKey);
-    }
-
-    /**
-     * Parses temporal string from load file.
-     *
-     * @param datetime Date-time string
-     * @return Temporal instance
-     * @throws DateTimeFormatException Incorrect date time format given
-     */
-    public static Temporal parseLoadTemporal (String datetime) throws DateTimeFormatException {
-        try {
-            if (datetime.contains("All day")) {
-                return LocalDate.parse(
-                        datetime.replace("All day", "").trim(),
-                        DateTimeFormatter.ofPattern("MMM dd yyyy", Locale.ENGLISH));
-            } else {
-                return LocalDateTime.parse(
-                        datetime,
-                        DateTimeFormatter.ofPattern("MMM dd yyyy h.mma", Locale.ENGLISH));
-            }
-        } catch (DateTimeParseException e) {
-            throw new DateTimeFormatException(datetime);
-        }
-    }
-
-    /**
-     * Parses Date or Date and Time into computer-understandable format.
-     *
-     * @param datetime Input date and time
-     * @return Temporal instance
-     * @throws DateTimeFormatException Invalid date-time format
-     */
-    public static Temporal parseTemporal(String datetime) throws DateTimeFormatException {
-        HawkingTimeParser parser = new HawkingTimeParser();
-        DateTime jodaStart = null;
-        DateTime jodaEnd = null;
-        try {
-            DatesFound datesFound = parser.parse(datetime, new Date(), new HawkingConfiguration(), "eng");
-            ParserOutput po = datesFound.getParserOutputs().get(0);
-            jodaStart = po.getDateRange().getStart();
-            jodaEnd = po.getDateRange().getEnd();
-        } catch (Exception e) {
-            throw new DateTimeFormatException(datetime);
-        }
-        assert jodaStart != null && jodaEnd != null;
-        Temporal result = null;
-        try {
-            LocalDateTime ldtStart = LocalDateTime.of(
-                    jodaStart.getYear(),
-                    jodaStart.getMonthOfYear(),
-                    jodaStart.getDayOfMonth(),
-                    jodaStart.getHourOfDay(),
-                    jodaStart.getMinuteOfHour()
-            );
-            LocalDateTime ldtEnd = LocalDateTime.of(
-                    jodaEnd.getYear(),
-                    jodaEnd.getMonthOfYear(),
-                    jodaEnd.getDayOfMonth(),
-                    jodaEnd.getHourOfDay(),
-                    jodaEnd.getMinuteOfHour()
-            );
-
-            if (ldtStart.plusHours(23).plusMinutes(59).equals(ldtEnd)) {
-                result = ldtEnd.toLocalDate();
-            } else if (ldtEnd.getHour() == 23 && ldtEnd.getMinute() == 59) {
-                result = ldtEnd.toLocalDate();
-            } else {
-                result = ldtEnd;
-            }
-
-        } catch (DateTimeParseException e) {
-            throw new DateTimeFormatException(datetime + "could not convert to local date time!");
-        }
-        assert result != null;
-        return result;
-            /*
-        if (datetime.trim().split("\\s+", 2).length < 2) { // no time given
-            return parseDate(datetime);
-        } else {
-            return parseDateTime(datetime);
-        }
-        */
-    }
-
-
-    /**
-     * Parses Date and Time into computer-understandable format.
-     *
-     * @param datetime Input date and time
-     * @return LocalDateTime instance
-     * @throws DateTimeFormatException Invalid date-time format
-     */
-    private static LocalDateTime parseDateTime(String datetime) throws DateTimeFormatException {
-        try {
-            return LocalDateTime.parse(datetime,
-                    new DateTimeFormatterBuilder()
-                            .parseCaseInsensitive()
-                            .parseLenient()
-                            .appendOptional(DateTimeFormatter.ofPattern("d/M/yyyy"))
-                            .appendOptional(DateTimeFormatter.ofPattern("yyyy-M-d"))
-                            .appendOptional(DateTimeFormatter.ofPattern("dMMMyyyy"))
-                            .parseDefaulting(ChronoField.YEAR_OF_ERA, 2021)
-                            .appendOptional(DateTimeFormatter.ofPattern("d/M"))
-                            .appendOptional(DateTimeFormatter.ofPattern("dMMM"))
-                            .appendLiteral(" ")
-                            .appendOptional(DateTimeFormatter.ofPattern("HHmm"))
-                            .appendOptional(DateTimeFormatter.ofPattern("h.mma"))
-                            .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
-                            .appendOptional(DateTimeFormatter.ofPattern("ha"))
-                            .toFormatter(Locale.ENGLISH));
-        } catch (DateTimeParseException e) {
-            throw new DateTimeFormatException(datetime);
-        }
-    }
-
-    /**
-     * Parses Date into computer-understandable format.
-     *
-     * @param date Input date
-     * @return LocalDate instance
-     * @throws DateTimeFormatException Invalid date format
-     */
-    private static LocalDate parseDate(String date) throws DateTimeFormatException {
-        try {
-            return LocalDate.parse(date,
-                    new DateTimeFormatterBuilder()
-                            .parseCaseInsensitive()
-                            .parseLenient()
-                            .appendOptional(DateTimeFormatter.ofPattern("d/M/yyyy"))
-                            .appendOptional(DateTimeFormatter.ofPattern("yyyy-M-d"))
-                            .appendOptional(DateTimeFormatter.ofPattern("dMMMyyyy"))
-                            .parseDefaulting(ChronoField.YEAR_OF_ERA, 2021)
-                            .appendOptional(DateTimeFormatter.ofPattern("d/M"))
-                            .appendOptional(DateTimeFormatter.ofPattern("dMMM"))
-                            .toFormatter(Locale.ENGLISH));
-        } catch (DateTimeParseException e) {
-            throw new DateTimeFormatException(date);
-        }
     }
 
 }
