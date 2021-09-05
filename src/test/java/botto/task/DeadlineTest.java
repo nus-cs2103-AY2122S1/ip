a4f0b18cf348fa3a096765b91002cbaa9e7bb29d
ngchisern <ncsern2000@gmail.com>
2021-09-05 18:07:17 +0800

    Attempt to solve unsuccessful CI workflow run (part 2)

--- src/test/java/botto/task/DeadlineTest.java
+++ src/test/java/botto/task/DeadlineTest.java
@@ -4,15 +4,21 @@ package botto.task;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
 
 public class DeadlineTest {
     @Test
     public void testStringConversion() {
-        Deadline task1 = new Deadline("read book", LocalDateTime.of(2021, 7, 15, 4, 30));
-        assertEquals("[D][ ] read book (by: 15/7/2021 4:30 am)", task1.toString());
-        Deadline task2 = new Deadline("return book", LocalDateTime.of(2021, 12, 3, 15, 30));
-        assertEquals("[D][ ] return book (by: 3/12/2021 3:30 pm)", task2.toString());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy h:mm a", Locale.getDefault());
+        LocalDateTime localDateTime1 = LocalDateTime.of(2021, 7, 15, 4, 30);
+        Deadline task1 = new Deadline("read book", localDateTime1);
+        assertEquals("[D][ ] read book (by: " + formatter.format(localDateTime1) + ")", task1.toString());
+
+        LocalDateTime localDateTime2 = LocalDateTime.of(2021, 12, 3, 15, 30);
+        Deadline task2 = new Deadline("return book", localDateTime2);
+        assertEquals("[D][ ] return book (by: " + formatter.format(localDateTime2) + ")", task2.toString());
     }
 }
--- src/test/java/botto/task/EventTest.java
+++ src/test/java/botto/task/EventTest.java
@@ -3,15 +3,21 @@ package botto.task;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
 
 public class EventTest {
     @Test
     public void testStringConversion() {
-        Event task1 = new Event("read book", LocalDateTime.of(2021, 7, 15, 4, 30));
-        assertEquals("[E][ ] read book (at: 15/7/2021 4:30 am)", task1.toString());
-        Event task2 = new Event("return book", LocalDateTime.of(2021, 12, 3, 15, 30));
-        assertEquals("[E][ ] return book (at: 3/12/2021 3:30 pm)", task2.toString());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy h:mm a", Locale.getDefault());
+        LocalDateTime localDateTime1 = LocalDateTime.of(2021, 7, 15, 4, 30);
+        Event task1 = new Event("read book", localDateTime1);
+        assertEquals("[E][ ] read book (at: " + formatter.format(localDateTime1) + ")", task1.toString());
+
+        LocalDateTime localDateTime2 = LocalDateTime.of(2021, 12, 3, 15, 30);
+        Event task2 = new Event("return book", localDateTime2);
+        assertEquals("[E][ ] return book (at: " + formatter.format(localDateTime2) + ")", task2.toString());
     }
 }
