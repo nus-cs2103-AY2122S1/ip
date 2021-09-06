a960c923fd2cd3adfec1c0d332d101e7f67484c8
ambroseboo <ambroseb9977@gmail.com>
2021-09-06 17:03:24 +0800

    Code Quality improvements
    Add methods in Parser class to make parse() method more concise

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -44,74 +44,40 @@ public class Parser {
         } else if (userInput.equals("list")) {
             return ui.listMessage(tasks);
         } else if (isDoneCall(userInput)) {
-            int index = Integer.parseInt(userInput.substring(5));
-
-            if (tasks.size() >= index) {
-                tasks.getTask(index - 1).markAsDone();
-                storage.markAsDoneData(index - 1);
-                return ui.doneMessage(tasks.getTask(index - 1));
-            } else {
-                return ui.noSuchTaskMessage();
-            }
-
+            return parseDoneCall(userInput);
         } else if (isRemoveCall(userInput)) {
-            int index = Integer.parseInt(userInput.substring(7));
-            if (tasks.size() >= index) {
-                String result = ui.removeMessage(tasks.getTask(index - 1), tasks.size() - 1);
-                storage.removeData(index - 1);
-                tasks.removeTask(index - 1);
-                return result;
-            } else {
-                return ui.noSuchTaskMessage();
-            }
+            return parseRemoveCall(userInput);
         } else if (isFindCall(userInput)) {
-            String keyword = userInput.substring(5);
-            TaskList matchingTasks = tasks.find(keyword);
-            if (matchingTasks.size() == 0) {
-                return "There are no tasks that include your keyword.";
-            } else {
-                assert matchingTasks.size() > 0;
-                String result = "Here are the matching tasks in your list:\n";
-                for (int i = 0; i < matchingTasks.size(); i++) {
-                    result += ((i + 1) + "." + matchingTasks.getTask(i).toString() + "\n");
-                }
-                return result;
-            }
+            return parseFindCall(userInput);
+        } else if (userInput.startsWith("todo ")) {
+            return parseTodoCall(userInput, tasks);
+        } else if (userInput.startsWith("deadline ")) {
+            return parseDeadlineCall(userInput, tasks);
+        } else if (userInput.startsWith("event ")) {
+            return parseEventCall(userInput, tasks);
         } else {
-            try {
-                if (userInput.startsWith("todo")) {
-                    return parseAddTask(userInput, tasks, Duke.Type.TODO);
-                } else if (userInput.startsWith("deadline")) {
-                    return parseAddTask(userInput, tasks, Duke.Type.DEADLINE);
-                } else if (userInput.startsWith("event")) {
-                    return parseAddTask(userInput, tasks, Duke.Type.EVENT);
-                } else {
-                    return " OOPS!!! I'm sorry, but I don't know what that means :-(";
-                }
-            } catch (IllegalArgumentException e) {
-                return e.getMessage();
-            }
+            return "Oops! I don't understand what you mean!";
         }
     }
 
     /**
      * Check whether user input is a valid done call for a task to be marked as done.
-     * @param strNum String input from user.
+     * @param str String input from user.
      * @return True if input is a valid done call, false otherwise.
      */
-    public boolean isDoneCall (String strNum) {
-        if (strNum == null) {
+    public boolean isDoneCall (String str) {
+        if (str == null) {
             return false;
         }
-        if (strNum.length() < 6) {
+        if (str.length() < 6) {
             return false;
         }
         try {
-            int d = Integer.parseInt(strNum.substring(5));
+            int d = Integer.parseInt(str.substring(5));
         } catch (NumberFormatException nfe) {
             return false;
         }
-        if (!strNum.startsWith("done ")) {
+        if (!str.startsWith("done ")) {
             return false;
         }
         return true;
@@ -141,66 +107,78 @@ public class Parser {
     }
 
     /**
-     * Handles the user input when it is a command to add a Task.
-     * @param userInput String input from user.
-     * @param tasks Current list of tasks to be edited.
-     * @param type Type of Task added.
+     * Handles user input when user calls to add a Todo task.
+     * @param userInput String format of user input
+     * @param tasks TaskList object used to store tasks
+     * @return Duke response to the user
      */
-    public String parseAddTask(String userInput, TaskList tasks, Duke.Type type) {
-        if (type == Duke.Type.TODO) {
-            if (isTodoDescEmpty(userInput)) {
-                return " OOPS!!! The description of a todo cannot be empty.";
-            }
-            tasks.addTask(new Todo(userInput.substring(5)));
-            storage.newTaskToData(userInput.substring(5), Duke.Type.TODO, "");
-            return "Got it. I've added this task:\n" + "  " + tasks.getTask(tasks.size() - 1).toString() + "\n"
-                    + "Now you have " + tasks.size() + " tasks in the list.";
-        } else if (type == Duke.Type.DEADLINE) {
-            if (isDeadlineDescEmpty(userInput)) {
-                return " OOPS!!! The description of a deadline cannot be empty.";
-            }
-            int slashIndex;
-            if (isByPresent(userInput)) {
-                slashIndex = userInput.indexOf("/by");
-            } else {
-                return " Please set a deadline by adding /by";
-            }
-            try {
-                tasks.addTask(new Deadline(userInput.substring(9, slashIndex - 1),
-                        LocalDate.parse(userInput.substring(slashIndex + 4, slashIndex + 14)),
-                        LocalTime.parse(userInput.substring(slashIndex + 15))));
-            } catch (DateTimeParseException | StringIndexOutOfBoundsException e) {
-                return " Date and Time must be specified by YYYY-MM-DD HH:MM";
-            }
-            storage.newTaskToData(userInput.substring(9, slashIndex - 1), Duke.Type.DEADLINE,
-                    userInput.substring(slashIndex + 4));
-            return "Got it. I've added this task:\n" + "  " + tasks.getTask(tasks.size() - 1).toString() + "\n"
-                    + "Now you have " + tasks.size() + " tasks in the list.";
-        } else {
-            // At this point, type must be of Event
-            assert type == Duke.Type.EVENT;
-            if (isEventDescEmpty(userInput)) {
-                return " OOPS!!! The description of an event cannot be empty.";
-            }
-            int slashIndex;
-            if (isAtPresent(userInput)) {
-                slashIndex = userInput.indexOf("/at");
-            } else {
-                return " Please set a specified timing by adding /at";
-            }
-            try {
-                tasks.addTask(new Event(userInput.substring(6, slashIndex - 1),
-                        LocalDate.parse(userInput.substring(slashIndex + 4, slashIndex + 14)),
-                        LocalTime.parse(userInput.substring(slashIndex + 15))));
-            } catch (DateTimeParseException | StringIndexOutOfBoundsException e) {
-                return " Date and Time must be specified by YYYY-MM-DD HH:MM";
-            }
-
-            storage.newTaskToData(userInput.substring(6, slashIndex - 1), Duke.Type.EVENT,
-                    userInput.substring(slashIndex + 4));
-            return "Got it. I've added this task:\n" + "  " + tasks.getTask(tasks.size() - 1).toString() + "\n"
-                    + "Now you have " + tasks.size() + " tasks in the list.";
+    private String parseTodoCall(String userInput, TaskList tasks) {
+        if (isTodoDescEmpty(userInput)) {
+            return " OOPS!!! The description of a todo cannot be empty.";
         }
+        tasks.addTask(new Todo(userInput.substring(5)));
+        storage.newTaskToData(userInput.substring(5), Duke.Type.TODO, "");
+        return "Got it. I've added this task:\n" + "  " + tasks.getTask(tasks.size() - 1).toString() + "\n"
+                + "Now you have " + tasks.size() + " tasks in the list.";
+    }
+
+    /**
+     * Handles user input when user calls to add a Deadline task.
+     * @param userInput String format of user input
+     * @param tasks TaskList object used to store tasks
+     * @return Duke response to the user
+     */
+    private String parseDeadlineCall(String userInput, TaskList tasks) {
+        if (isDeadlineDescEmpty(userInput)) {
+            return " OOPS!!! The description of a deadline cannot be empty.";
+        }
+        int slashIndex;
+        if (isByPresent(userInput)) {
+            slashIndex = userInput.indexOf("/by");
+        } else {
+            return " Please set a deadline by adding /by";
+        }
+        try {
+            tasks.addTask(new Deadline(userInput.substring(9, slashIndex - 1),
+                    LocalDate.parse(userInput.substring(slashIndex + 4, slashIndex + 14)),
+                    LocalTime.parse(userInput.substring(slashIndex + 15))));
+        } catch (DateTimeParseException | StringIndexOutOfBoundsException e) {
+            return " Date and Time must be specified by YYYY-MM-DD HH:MM";
+        }
+        storage.newTaskToData(userInput.substring(9, slashIndex - 1), Duke.Type.DEADLINE,
+                userInput.substring(slashIndex + 4));
+        return "Got it. I've added this task:\n" + "  " + tasks.getTask(tasks.size() - 1).toString() + "\n"
+                + "Now you have " + tasks.size() + " tasks in the list.";
+    }
+
+    /**
+     * Handles user input when user calls to add an Event task.
+     * @param userInput String format of user input
+     * @param tasks TaskList object used to store tasks
+     * @return Duke response to the user
+     */
+    private String parseEventCall(String userInput, TaskList tasks) {
+        if (isEventDescEmpty(userInput)) {
+            return " OOPS!!! The description of an event cannot be empty.";
+        }
+        int slashIndex;
+        if (isAtPresent(userInput)) {
+            slashIndex = userInput.indexOf("/at");
+        } else {
+            return " Please set a specified timing by adding /at";
+        }
+        try {
+            tasks.addTask(new Event(userInput.substring(6, slashIndex - 1),
+                    LocalDate.parse(userInput.substring(slashIndex + 4, slashIndex + 14)),
+                    LocalTime.parse(userInput.substring(slashIndex + 15))));
+        } catch (DateTimeParseException | StringIndexOutOfBoundsException e) {
+            return " Date and Time must be specified by YYYY-MM-DD HH:MM";
+        }
+
+        storage.newTaskToData(userInput.substring(6, slashIndex - 1), Duke.Type.EVENT,
+                userInput.substring(slashIndex + 4));
+        return "Got it. I've added this task:\n" + "  " + tasks.getTask(tasks.size() - 1).toString() + "\n"
+                + "Now you have " + tasks.size() + " tasks in the list.";
     }
 
     /**
@@ -271,6 +249,60 @@ public class Parser {
             return false;
         } else {
             return true;
+        }
+    }
+
+    /**
+     * Handles actions to be done when the user inputs a done call.
+     * @param str User input in String format
+     * @return String that Duke will reply the user
+     */
+    private String parseDoneCall(String str) {
+        int index = Integer.parseInt(str.substring(5));
+
+        if (tasks.size() >= index) {
+            tasks.getTask(index - 1).markAsDone();
+            storage.markAsDoneData(index - 1);
+            return ui.doneMessage(tasks.getTask(index - 1));
+        } else {
+            return ui.noSuchTaskMessage();
+        }
+    }
+
+    /**
+     * Handles actions to be done when the user inputs a remove call.
+     * @param str User input in String format
+     * @return String that Duke will reply the user
+     */
+    private String parseRemoveCall(String str) {
+        int index = Integer.parseInt(str.substring(7));
+        if (tasks.size() >= index) {
+            String result = ui.removeMessage(tasks.getTask(index - 1), tasks.size() - 1);
+            storage.removeData(index - 1);
+            tasks.removeTask(index - 1);
+            return result;
+        } else {
+            return ui.noSuchTaskMessage();
+        }
+    }
+
+    /**
+     * Handles actions to be done when the user inputs a find call.
+     * @param str User input in String format
+     * @return String that Duke will reply the user
+     */
+    private String parseFindCall(String str) {
+        String keyword = str.substring(5);
+        TaskList matchingTasks = tasks.find(keyword);
+        if (matchingTasks.size() == 0) {
+            return "There are no tasks that include your keyword.";
+        } else {
+            assert matchingTasks.size() > 0;
+            String result = "Here are the matching tasks in your list:\n";
+            for (int i = 0; i < matchingTasks.size(); i++) {
+                result += ((i + 1) + "." + matchingTasks.getTask(i).toString() + "\n");
+            }
+            return result;
         }
     }
 }
