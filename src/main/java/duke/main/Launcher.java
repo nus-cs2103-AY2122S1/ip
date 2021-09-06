d96128969ef39b568385dc0b47326268e26e12af
Gordon25 <73068242+Gordon25@users.noreply.github.com>
2021-09-06 14:04:16 +0800

    Add skeletal GUI

--- src/main/java/duke/main/Launcher.java
+++ src/main/java/duke/main/Launcher.java
@@ -7,6 +7,6 @@ import javafx.application.Application;
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Duke_Application.class, args);
+        Application.launch(Main.class, args);
     }
 }
