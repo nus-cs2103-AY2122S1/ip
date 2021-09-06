a91cf6e1ca0db85ccc0e59ed7c3ee1b5e4b004d7
jiarong15 <jiarong15@gmail.com>
2021-09-06 16:04:42 +0800

    Add minor edit

--- src/main/java/javafxGUI/MainWindow.java
+++ src/main/java/javafxGUI/MainWindow.java
@@ -8,7 +8,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import javafxGUI.DialogBox;
 
 
 public class MainWindow extends AnchorPane {
