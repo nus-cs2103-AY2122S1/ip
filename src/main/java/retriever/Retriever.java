126909750a38ffa69a6c98241e76ece20af401b7
Jai2501 <jaylulla9@gmail.com>
2021-09-06 21:47:13 +0800

    Update files to improve GUI support
    A GUI was added to the Chatbot, and therefore
    the CLI based U.I. became obsolete. Consequently,
    there is a need to remove the dead code.
    
    Remove various lines of code that help run the
    CLI version of the Chatbot.
    
    This would enhance the code quality.

--- src/main/java/retriever/Retriever.java
+++ src/main/java/retriever/Retriever.java
@@ -62,44 +62,5 @@ public class Retriever {
 
         return retrieverResponse;
     }
-
-    /**
-     * Runs the Chatbot engine.
-     * Main body of the Retriever Chatbot.
-     */
-    public void run() {
-        // To show the welcome message
-        ui.printWelcomeMessage();
-
-        // To store the user input string.
-        String userInput;
-
-        // Main body which is repeated till the "bye" keyword is encountered.
-        do {
-            // Taking input
-            userInput = ui.readCommand();
-
-            try {
-                parser.parseUserInput(userInput);
-            } catch (RetrieverException e) {
-                // Catching various exceptions and alerting the user.
-                ui.printErrorMessage(e.getMessage());
-            }
-            ui.printDashedLine();
-        } while (!parser.isExit());
-
-        // Printing the good-bye message.
-        ui.printGoodByeMessage();
-    }
-
-    /**
-     * Returns an interactive session with the Chatbot. Basically a mean to interact with it.
-     *
-     * @param args Here, usually nothing is passed.
-     */
-    public static void main(String[] args) {
-        // Calling the run() method to start the Chatbot.
-        new Retriever("tasksList.txt").run();
-    }
 }
 
--- src/main/java/retriever/Ui.java
+++ src/main/java/retriever/Ui.java
@@ -41,18 +41,12 @@ public class Ui {
      */
     public void printMessage(String message) {
         retrieverResponse = message;
-        System.out.println(message);
     }
 
     /**
      * Prints the welcome message.
      */
     public void printWelcomeMessage() {
-        System.out.println(dashedLine);
-        System.out.println(welcomeMessage);
-        System.out.println(logo);
-        System.out.println(dashedLine);
-
         retrieverResponse = welcomeMessage;
     }
 
@@ -60,12 +54,6 @@ public class Ui {
      * Prints the goodbye message.
      */
     public void printGoodByeMessage() {
-        // Closing the scanner.
-        scanner.close();
-
-        System.out.println(goodByeMessage);
-        System.out.println(dashedLine);
-
         retrieverResponse = goodBoyMessage;
     }
 
@@ -75,15 +63,7 @@ public class Ui {
      * @param errorMessage The exception message to be printed.
      */
     public void printErrorMessage(String errorMessage) {
-        System.out.println(errorMessage);
         retrieverResponse = errorMessage;
-    }
-
-    /**
-     * Prints a dashed line.
-     */
-    public void printDashedLine() {
-        System.out.println(dashedLine);
     }
 
     /**
@@ -93,9 +73,6 @@ public class Ui {
      * @param taskListLength The length of the task list, after adding the task.
      */
     public void printTaskAdded(Task task, int taskListLength) {
-        System.out.println("-> Where's My Treat? I Added:\n\t" + task);
-        System.out.println("\nYou Owe Me " + taskListLength + " Treat(s), Master!");
-
         retrieverResponse = "Where's My Treat? I Added:\n"
                 + task + "\nYou Owe Me " + taskListLength + " Treat(s), Master!";
     }
@@ -107,11 +84,6 @@ public class Ui {
      * @param taskListLength The length of the task list, after deleting the task.
      */
     public void printTaskDeleted(Task task, int taskListLength) {
-        System.out.println(badBoyMessage + "\n"
-                + "Task Deleted!\n"
-                + "\t" + task
-                + "\nYou Owe Me " + taskListLength + " Treat(s), Master!");
-
         retrieverResponse = badBoyMessage + "\n"
                 + "Task Deleted!\n"
                 + task
@@ -124,10 +96,6 @@ public class Ui {
      * @param task The task that has been done.
      */
     public void printTaskMarkedAsDone(Task task) {
-        System.out.println(goodBoyMessage + "\n"
-                + "Task Done!\n"
-                + "\t" + task);
-
         retrieverResponse = goodBoyMessage + "\n"
                 + "Task Done!\n"
                 + task;
@@ -140,21 +108,17 @@ public class Ui {
      */
     public void printTaskFoundByKeyword(ArrayList<Task> taskList) {
         if (taskList.size() == 0) {
-            System.out.println("Sorry Master, I Couldn't Smell And Find WHat You Asked For!");
             retrieverResponse = "Sorry Master, I Couldn't Smell And Find WHat You Asked For!"
                     + "\n (Task With the Given Keyword Does Not Exist)\n";
             return;
         }
 
         retrieverResponse = "Woof! Look What I Found: /n";
-        System.out.println("Woof! Look What I Found: ");
 
         for (int i = 0; i < taskList.size(); i++) {
-            System.out.println("\t" + (i + 1) + ". " + taskList.get(i));
             retrieverResponse += (i + 1) + ". " + taskList.get(i) + "\n";
         }
 
-        System.out.println("You Owe Me " + taskList.size() + " Treat(s), Master!");
         retrieverResponse += "You Owe Me " + taskList.size() + " Treat(s), Master!";
     }
 
@@ -166,14 +130,11 @@ public class Ui {
     public void printTaskList(ArrayList<Task> taskList) {
         // If the list is empty
         if (taskList.size() == 0) {
-            System.out.println("My Memory Is Empty, Please Feed Items!");
             retrieverResponse = "My Memory Is Empty, Please Feed Items!";
         } else {
             retrieverResponse = "Your Tasks, My Master:\n";
-            System.out.println("-> Your Tasks, My Master:");
             for (int i = 0; i < taskList.size(); i++) {
                 retrieverResponse += ((i + 1) + ". " + taskList.get(i)) + "\n";
-                System.out.println("\t" + ((i + 1) + ". " + taskList.get(i)));
             }
         }
     }
@@ -185,21 +146,6 @@ public class Ui {
      */
     public String getRetrieverResponse() {
         return retrieverResponse;
-    }
-
-    /**
-     * Reads the command entered by the user.
-     *
-     * @return The user input command.
-     */
-    public String readCommand() {
-        // To store the user input string.
-        String userInput;
-
-        // Taking input
-        userInput = scanner.nextLine();
-
-        return userInput;
     }
 }
 
