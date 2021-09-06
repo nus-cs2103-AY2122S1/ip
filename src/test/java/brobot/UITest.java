039f748e8853e3692246470fa4ed0faa9f0ac0e7
Markus Lim <markuslim24@gmail.com>
2021-09-06 18:12:34 +0800

    JUnit Tests: remove unsued test cases and refactor test classes to follow checkStyle
    There are unused JUnit test classes and the ones that are necessary do
    not follow the checkStyle
    
    Resolving these issues will clean up the code and make it more
    reader-friendly
    
    I will remove the unused classes and refactor the test cases to follow
    checkStyle

--- src/test/java/brobot/UITest.java
+++
@@ -1,9 +0,0 @@
-package brobot;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
-public class UITest {
-
-}
--- src/test/java/brobot/task/TaskListTest.java
+++ src/test/java/brobot/task/TaskListTest.java
@@ -1,10 +1,12 @@
 package brobot.task;
 
-import brobot.exception.BroException;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+import brobot.exception.BroException;
+
 
 public class TaskListTest {
 
