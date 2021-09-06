948b9ab8180904b4685921a17c895c9e11ca7136
1waykiat <1waykiat@gmail.com>
2021-09-06 09:42:40 +0800

    add FXML to manage JavaFX

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,4 @@
 import duke.command.Command;
-import duke.control.DialogBox;
 import duke.error.DukeException;
 import duke.general.Parser;
 import duke.general.Storage;
@@ -22,7 +21,7 @@ import javafx.stage.Stage;
  * Chatbot that helps you form a task list
  */
 
-public class Duke extends Application {
+public class Duke {
     private Storage storage;
     private Tasklist tasks;
     private Ui ui;
@@ -78,97 +77,14 @@ public class Duke extends Application {
         new Duke("data/duke.txt").run();
     }
 
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        Label welcome = new Label("Duke: " + ui.showWelcome());
-        dialogContainer.getChildren().add(new DialogBox(welcome, new ImageView(duke)));
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Step 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-    }
-
-    /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        String input = userInput.getText();
+    public String getResponse(String input) {
         String output = "";
-
         try {
             Command c = parser.parse(input);
             output = c.execute(tasks, storage, ui);
-            Label dukeText = new Label(output);
-            dialogContainer.getChildren().addAll(
-                    DialogBox.getUserDialog(userText, new ImageView(user)),
-                    DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-            );
-            userInput.clear();
+            return output;
         } catch (DukeException e) {
-            System.out.println(e.getMessage());
-            Label dukeText = new Label(e.getMessage());
-            dialogContainer.getChildren().addAll(
-                    DialogBox.getUserDialog(userText, new ImageView(user)),
-                    DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-            );
-            userInput.clear();
+            return e.getMessage();
         }
     }
 }
