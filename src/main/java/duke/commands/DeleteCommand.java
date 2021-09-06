8eaa8b6f435535be08b6a5ffa9f01915596f108a
rushilramesh <rushilramesh@gmail.com>
2021-09-07 01:34:02 +0800

    Add multiple delete feature User does not have the option to delete multiple objects in one go.
    User would manually have to delete each task individually in order to
    delete multiple items.
    
    Add multiple delete feature that allows user to input various indexes
    separated by a whitespace to delete the tasks at the given positions in
    the list.
    
    This would streamline the deleting process for the user.

--- src/main/java/duke/commands/DeleteCommand.java
+++ src/main/java/duke/commands/DeleteCommand.java
@@ -1,5 +1,7 @@
 package duke.commands;
 
+import java.util.Arrays;
+
 import duke.DukeException;
 import duke.storage.Storage;
 import duke.task.Task;
@@ -14,16 +16,55 @@ public class DeleteCommand extends Command {
 
     @Override
     public String execute(TaskList tasks, Storage storage) throws DukeException {
-        String response = "";
-        if (commands.matches("\\d+")) {
-            int index = Integer.parseInt(commands) - 1;
-            if (index >= 0 && index < tasks.getLength()) {
-                Task task = tasks.deleteTask(0);
-                response = "Noted. I've removed this task:\n" + task + "\nNow you have " + tasks.getLength() + " tasks in the list.";
-                storage.updateFile(tasks);
+        String response = "Noted. I've removed these tasks:\n";
+        try {
+            int[] indexesOfTasks = getIndexes();
+            for (int i = indexesOfTasks.length - 1; i >= 0; i--) {
+                response += deleteTask(indexesOfTasks[i], tasks, storage);
+            }
+        } catch (DukeException e) {
+            throw new DukeException(e.getMessage());
+        }
+        response += "Now you have " + tasks.getLength() + " tasks in the list.";
+        return response;
+    }
+
+    /**
+     * Parses command inputted by user to return array of indexes to be
+     * deleted in descending order.
+     * @return Array of indexes in descending order.
+     * @throws DukeException when there is invalid index.
+     */
+    private int[] getIndexes() throws DukeException {
+        String[] indexes = commands.split(" ");
+        int[] indexesOfTasks = new int[indexes.length];
+        for (int i = 0; i < indexes.length; i++) {
+            if (indexes[i].matches("\\d+")) {
+                indexesOfTasks[i] = Integer.parseInt(indexes[i]);
             } else {
                 throw new DukeException("☹ OOPS!!! Input a valid index");
             }
+        }
+        Arrays.sort(indexesOfTasks);
+        return indexesOfTasks;
+    }
+
+    /**
+     * Deletes task from tasklist and storage and returns string
+     * format of deleted task.
+     * @param location index to be deleted.
+     * @param tasks TaskList of tasks to be updated.
+     * @param storage Storage object that updates the file in storage.
+     * @return String format of task deleted
+     * @throws DukeException when there is an invalid index.
+     */
+    private String deleteTask(int location, TaskList tasks, Storage storage) throws DukeException {
+        String response = "";
+        int index = location - 1;
+        if (index >= 0 && index < tasks.getLength()) {
+            Task task = tasks.deleteTask(index);
+            storage.updateFile(tasks);
+            response = task + "\n";
         } else {
             throw new DukeException("☹ OOPS!!! Input a valid index");
         }
