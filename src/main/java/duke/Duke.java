ca21f2d14d1adf5a0b6d3de996b4d50defb57a05
Lee Zheng Han <e0544595@u.nus.edu>
2021-09-07 00:58:31 +0800

    Add getScrollPane() method for Duke class
    start() method seems rather long and difficult to read.
    
    Let's abstract out the configurations on the scroll pane as a method
    by itself.
    
    By adding getScrollPane() method and making it private, there will
    be a clearer separtion of concerns.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -11,6 +11,7 @@ import duke.command.ListCommand;
 import duke.command.ToDoCommand;
 import duke.exception.DukeException;
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -55,16 +56,14 @@ public class Duke extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        scrollPane = new ScrollPane();
         dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
+        scrollPane = this.getScrollPane(dialogContainer);
 
         userInput = new TextField();
         sendButton = new Button("Send");
 
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
         scene = new Scene(mainLayout);
 
         primaryStage.setScene(scene);
@@ -75,25 +74,13 @@ public class Duke extends Application {
         primaryStage.setMinWidth(400.0);
 
         mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
         dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
         userInput.setPrefWidth(325.0);
-
         sendButton.setPrefWidth(55.0);
 
         AnchorPane.setTopAnchor(scrollPane, 1.0);
-
         AnchorPane.setBottomAnchor(sendButton, 1.0);
         AnchorPane.setRightAnchor(sendButton, 1.0);
-
         AnchorPane.setLeftAnchor(userInput, 1.0);
         AnchorPane.setBottomAnchor(userInput, 1.0);
 
@@ -128,6 +115,20 @@ public class Duke extends Application {
     public void printResponse(String response) {
         Label dukeText = new Label(response);
         dialogContainer.getChildren().addAll(DialogBox.getDukeDialog(dukeText, new ImageView(duke)));
+    }
+
+    private ScrollPane getScrollPane(Node content) {
+        scrollPane = new ScrollPane();
+        scrollPane.setContent(content);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        return scrollPane;
     }
 
     private Label getDialogLabel(String text) {
