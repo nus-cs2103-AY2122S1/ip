df31be04861ea6eb448c27a5c893d161a952c4a3
RachelCheah <55133167+RachelCheah@users.noreply.github.com>
2021-09-07 00:15:08 +0800

    Fixed Indentation

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -26,61 +26,6 @@ public class Parser {
 
     public static Command parse(String input) throws NoSuchCommandException {
         switch(input.split(" ")[0]) {
-<<<<<<< HEAD
-            case "list":
-                return new ListCommand();
-            case "done":
-                try {
-                    Integer index = Integer.parseInt(input.split(" ")[1]);
-                    return new DoneCommand(index);
-                } catch (NullPointerException ex){
-                    throw new NoSuchCommandException("Invalid Command");
-                }
-            case "delete":
-                try {
-                    Integer index = Integer.parseInt(input.split(" ")[1]);
-                    return new DeleteCommand(index);
-                } catch (NullPointerException ex){
-                    throw new NoSuchCommandException("Invalid Command");
-                }
-
-            case "todo":
-                try {
-                    ToDosTask.isLegitInput(input);
-                    return new AddCommand(new ToDosTask(ToDosTask.getNameInput(input), false));
-                } catch (NoSuchCommandException ex) {
-                    throw new NoSuchCommandException("Invalid Command");
-                }
-
-            case "deadline":
-                try {
-                    DeadlinesTask.isLegitInput(input);
-                    return new AddCommand(new DeadlinesTask(DeadlinesTask.getNameInput(input), false, Task.parseDateTime(DeadlinesTask.getDeadlineInput(input))));
-                } catch (NoSuchCommandException ex) {
-                    throw new NoSuchCommandException("Invalid Command");
-                }
-
-            case "event":
-                try {
-                    EventsTask.isLegitInput(input);
-                    return new AddCommand(new EventsTask(EventsTask.getNameInput(input), false, Task.parseDateTime(EventsTask.getDeadlineInput(input))));
-                } catch (NoSuchCommandException ex) {
-                    throw new NoSuchCommandException("Invalid Command");
-                }
-<<<<<<< HEAD
-            case "find":
-                try {
-                    Task.isLegitFindInput(input);
-                    return new FindCommand(input.split(" ")[1]);
-                } catch (NoSuchCommandException ex) {
-                    throw new NoSuchCommandException("Invalid Command");
-                }
-=======
-            case "bye":
-                return new ByeCommand();
->>>>>>> branch-A-JavaDoc
-            default:
-=======
         case "list":
             return new ListCommand();
         case "done":
@@ -88,7 +33,6 @@ public class Parser {
                 Integer index = Integer.parseInt(input.split(" ")[1]);
                 return new DoneCommand(index);
             } catch (NullPointerException ex){
->>>>>>> branch-A-CodingStandard
                 throw new NoSuchCommandException("Invalid Command");
             }
         case "delete":
@@ -122,9 +66,18 @@ public class Parser {
             } catch (NoSuchCommandException ex) {
                 throw new NoSuchCommandException("Invalid Command");
             }
+        case "find":
+            try {
+                Task.isLegitFindInput(input);
+                return new FindCommand(input.split(" ")[1]);
+            } catch (NoSuchCommandException ex) {
+                throw new NoSuchCommandException("Invalid Command");
+            }
+        case "bye":
+            return new ByeCommand();
         default:
             throw new NoSuchCommandException("Invalid Command");
-        }
+    }
     }
 
 }
