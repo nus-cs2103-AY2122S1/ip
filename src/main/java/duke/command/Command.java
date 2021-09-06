0070719668d3862c6e0b4f0aeb0d8ac9b1c776d9
Cedric Ong <cedricong99@gmail.com>
2021-09-07 00:35:56 +0800

    Change ordering of final and static in constants

--- src/main/java/duke/command/Command.java
+++ src/main/java/duke/command/Command.java
@@ -9,14 +9,14 @@ public abstract class Command {
     protected final String userCommand;
     protected final String userArgument;
 
-    public static final String LIST_COMMAND = "list";
-    public static final String BYE_COMMAND = "bye";
-    public static final String DONE_COMMAND = "done";
-    public static final String TODO_COMMAND = "todo";
-    public static final String DEADLINE_COMMAND = "deadline";
-    public static final String EVENT_COMMAND = "event";
-    public static final String DELETE_COMMAND = "delete";
-    public static final String FIND_COMMAND = "find";
+    public final static String LIST_COMMAND = "list";
+    public final static String BYE_COMMAND = "bye";
+    public final static String DONE_COMMAND = "done";
+    public final static String TODO_COMMAND = "todo";
+    public final static String DEADLINE_COMMAND = "deadline";
+    public final static String EVENT_COMMAND = "event";
+    public final static String DELETE_COMMAND = "delete";
+    public final static String FIND_COMMAND = "find";
 
     public Command(String userCommand, String userArgument) {
         this.userArgument = userArgument;
