decfb696ce5d5518d567ac2d6c72685be4db295d
nataniayp <75572811+nataniayp@users.noreply.github.com>
2021-09-06 20:39:39 +0800

    Use assert feature
    There are not many things to assert, except for some string returns that
    should not be empty. Otherwise, other codes are straightforward and lead
    to direct changes that would make assert redundant to add.

--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -73,6 +73,7 @@ public class Parser {
                         DateTimeFormatter.ofPattern("dd MMM yyyy")).toString();
                 time = date + " " + time.substring(12);
             } else if (time.contains(":")) {
+                // do nothing
             } else {
                 time = LocalDate.parse(time, DateTimeFormatter.ofPattern("dd MMM yyyy")).toString();
             }
