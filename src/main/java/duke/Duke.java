28992d121ab694557cc0c3dbab90ca2ae6f458d7
Allard <AllardQuek@users.noreply.github.com>
2021-09-06 17:40:49 +0800

    Add more Duke specific exceptions
    There is only one Duke Exception to account for all types of Duke exceptions.
    
    In order to change the exception message for one type of exception, edits would be required at multiple locations.
    
    Adding new Classes to encapsulate common Duke Exceptions would allow us to more easily make changes to the exceptions being thrown.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -12,30 +12,39 @@ import duke.util.Ui;
  * various things.
  */
 public class Duke {
-    public static Storage storage;
-    public static TaskList tasks;
-    private final String STORAGE_PATH = "data/duke.txt";
+    private static Storage storage;
+    private static TaskList tasks;
 
     /**
      * Constructor for Duke.
      */
     public Duke() {
-        storage = new Storage(STORAGE_PATH);
+        storage = new Storage("data/duke.txt");
         try {
-            tasks = new TaskList(storage.loadTasks());
+            tasks = new TaskList(getStorage().loadTasks());
         } catch (DukeException e) {
             tasks = new TaskList();
         }
     }
 
+    static Storage getStorage() {
+        return Duke.storage;
+    }
+
+    public static TaskList getTasks() {
+        return Duke.tasks;
+    }
+
     /**
      * Returns the response from Duke given the user input to be shown on the GUI.
+     *
+     * @param input the command the user typed
      */
-    String getResponse(String input) {
+    protected String getResponse(String input) {
         String response;
         try {
             response = Parser.parse(input);
-            return "Duke heard: " + response;
+            return response;
         } catch (DukeException e) {
             return e.getMessage();
         }
@@ -46,15 +55,13 @@ public class Duke {
      */
     public void run() {
         Ui ui = new Ui();
-
         ui.greetUser();
         Scanner sc = new Scanner(System.in);
-        String command;
 
         while (true) {
             try {
                 System.out.println("What's your next command?\n");
-                command = sc.nextLine();
+                String command = sc.nextLine();
                 String response = Parser.parse(command);
                 Ui.printFormattedMessage(response);
                 if (response.contains("Bye")) {
