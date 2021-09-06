1beb5067d1d603942f630cb59fc1ef1f462a6f5f
TohWeiJie01 <tohweijie01@gmail.com>
2021-09-06 20:12:31 +0800

    Add assertions

--- src/main/java/duke/storage/Storage.java
+++ src/main/java/duke/storage/Storage.java
@@ -112,6 +112,8 @@ public class Storage {
                     break;
                 }
 
+                assert task != null : "The task should not be null";
+
                 if (taskBreakdown[1].equals("1")) {
                     task.completeTask();
                 }
