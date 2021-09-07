257fb502b3c47f21fe2d0c77833d5c908abce7de
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2021-09-07 11:08:24 +0800

    Use RegEx to match commands in parse() method in Parser
    Using RegEx reduces the text checking and processing done by us. It also guarantees that the command entered matches the proper format leading to fewer bugs.

--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -33,28 +33,26 @@ public class Parser {
                 return new ByeCommand();
             } else if (cmd.equals("list")) {
                 return new ListCommand();
-            } else if (cmd.startsWith("done ")) {
-                int serialNo = Integer.parseInt(cmd.substring(5).trim());
+            } else if (cmd.matches("^done\\s\\d+$")) {
+                int serialNo = Integer.parseInt(cmd.substring(5));
                 return new DoneCommand(serialNo);
-            } else if (cmd.startsWith("deadline ")) {
+            } else if (cmd.matches("^deadline\\s.*\\s/by\\s\\d{4}-\\d{2}-\\d{2}$")) {
                 String[] splitText = cmd.substring(9).split(" /by ");
-                Task task = new Deadline(splitText[0].trim(), LocalDate.parse(splitText[1].trim()), false);
+                Task task = new Deadline(splitText[0].trim(), LocalDate.parse(splitText[1]), false);
                 return new AddCommand(task);
-            } else if (cmd.startsWith("event ")) {
+            } else if (cmd.matches("^event\\s.*\\s/at\\s\\d{4}-\\d{2}-\\d{2}$")) {
                 String[] splitText = cmd.substring(6).split(" /at ");
-                Task task = new Event(splitText[0].trim(), LocalDate.parse(splitText[1].trim()), false);
+                Task task = new Event(splitText[0].trim(), LocalDate.parse(splitText[1]), false);
                 return new AddCommand(task);
-            } else if (cmd.startsWith("todo ") || cmd.equals("todo")) {
-                String trimmed = cmd.trim();
-                if (trimmed.length() == 4) {
-                    throw new DukeException("Sorry Boss, todo must be followed with a description.");
-                }
-                Task task = new ToDo(trimmed.substring(5).trim(), false);
+            } else if (cmd.matches("^todo\\s*$")) {
+                throw new DukeException("Sorry Boss, todo must be followed with a description.");
+            } else if (cmd.matches("^todo\\s.*$")) {
+                Task task = new ToDo(cmd.substring(5).trim(), false);
                 return new AddCommand(task);
-            } else if (cmd.startsWith("delete ")) {
+            } else if (cmd.matches("^delete\\s\\d+$")) {
                 int serialNum = Integer.parseInt(cmd.split(" ")[1]);
                 return new DeleteCommand(serialNum);
-            } else if (cmd.startsWith("find ")) {
+            } else if (cmd.matches("^find\\s.*$")) {
                 return new FindCommand(cmd.split(" ")[1]);
             } else {
                 throw new DukeException("Sorry Boss, I cannot understand the command.");
