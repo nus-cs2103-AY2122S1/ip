86cbb9334fc1ca473c174ac25c20310090eabb2b
Tan Joe Wel <>
2021-09-07 18:57:42 +0800

    Small update

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -4,6 +4,27 @@ package duke;
  * Represents the UI class that handles user interaction
  */
 public class Ui {
+    private String generateLine() {
+        return "        ----------------";
+    }
+
+    public String byeResponse() {
+        String message = generateLine() + "\n" + "    okay :<, bye!" + "\n" + generateLine();
+        return message;
+    }
+
+    public String listResponse(TaskList tasks) {
+        String message = generateLine() + "\n" + "    " + "Here are the tasks in your list:\n";
+        int i = 0;
+        while (i < tasks.getNumberOfTasks()) {
+            message += "    " + (i + 1) + ". " + tasks.get(i).toString() + "\n";
+            i++;
+        }
+        message += generateLine();
+        return message;
+    }
+
+
 
     /**
      * Outputs the greeting to the user
