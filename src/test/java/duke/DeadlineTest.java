259f0dd02a372001d22ba2ebe94b490c68339980
CheahYan <cheahyan1103@gmail.com>
2021-09-07 13:10:23 +0800

    A-JUnit

--- src/test/java/duke/DeadlineTest.java
+++ src/test/java/duke/DeadlineTest.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DeadlineTest {
 
     @Test
-    void testToString() {
-        Deadline deadline = new Deadline("return book", "02/02/2022 1800");
-        assertEquals("[D] [ ] return book (by: 02 Feb 2022 06:00pm)", deadline.toString());
+    void testStringConversion() {
+        duke.Deadline deadline = new duke.Deadline("return book", "02/02/2022 1800");
+        assertEquals("[D][ ] return book (by: 02 Feb 2022 06:00PM)", deadline.toString());
     }
 }
