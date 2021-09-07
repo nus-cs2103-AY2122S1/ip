6b50ab04d856db23dfa11fa44c522b4d7410c627
limzk26 <dylanlim12698@gmail.com>
2021-09-07 18:50:22 +0800

    Improve task searching flexibility.

--- src/main/java/duke/misc/TaskList.java
+++ src/main/java/duke/misc/TaskList.java
@@ -98,16 +98,39 @@ public class TaskList {
     }
 
     /**
+     * Checks if String b is a subsequence of String a.
+     *
+     * @param a 'Container' String.
+     * @param b 'Containee' String.
+     * @return The boolean value of the subsequence query.
+     */
+    public boolean isSubSequence(String a, String b) {
+        // Convert all characters to lower case and remove all whitespace characters.
+        a = a.toLowerCase().replaceAll("\\s+", "");
+        b = b.toLowerCase().replaceAll("\\s+", "");
+
+        int i = 0;
+        int j = 0;
+        while (i < a.length() && j < b.length()) {
+            if (a.charAt(i) == b.charAt(j)) {
+                j++;
+            }
+            i++;
+        }
+        return j == b.length();
+    }
+
+    /**
      * Finds all tasks with specified keyword.
      *
-     * @param key keyword to search.
+     * @param key Abbreviation or specific keyword.
      * @return String of all tasks found appended in rows.
      */
     public String findTasks(String key) {
         StringBuilder sb = new StringBuilder();
         int idx = 0;
         for (Task task: tasks) {
-            if (task.getDescription().contains(key)) {
+            if (isSubSequence(task.getDescription(), key)) {
                 sb.append(String.format("%d.%s\n", ++idx, task));
             }
         }
