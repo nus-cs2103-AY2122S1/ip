f339117cb9ea62f495790cddcb1008b79cd2239b
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-09-08 00:40:22 +0800

    Add UndoCommand class

---
+++ src/main/java/duke/command/UndoCommand.java
@@ -0,0 +1,34 @@
+package duke.command;
+
+import duke.task.TaskHandler;
+import duke.ui.Ui;
+
+public class UndoCommand extends Command {
+
+    private static final CommandType COMMAND_TYPE = CommandType.ADD_DEADLINE_TASK;
+
+    /**
+     * Creates a Command instance.
+     *
+     * @param command The command represented by the instance.
+     */
+    public UndoCommand(String command) throws DukeInvalidCommandException {
+        super(command);
+    }
+
+    @Override
+    public void execute(TaskHandler taskHandler, Ui ui) throws DukeInvalidCommandException {
+        CommandHandler commandHandler = CommandHandler.getInstance();
+        commandHandler.undo();
+    }
+
+    @Override
+    void parseCommand(String[] tokens) throws DukeInvalidCommandException {
+
+    }
+
+    @Override
+    CommandType getCommandType() {
+        return null;
+    }
+}
