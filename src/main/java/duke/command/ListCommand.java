35449fbf988fdaee27a8235165533409afcfda3b
KelvinSoo <e0544272@u.nus.edu.sg>
2021-09-07 21:10:35 +0800

    Add Assertions for command package
    Code should include assertion to alert programmer errors.
    
    Assertions have been added to the AddCommand, DeleteCommand,
    DoneCommand, FindCommand and ListCommand.
    
    Using assertion for the command package is important as there might
    be instances of programmer error when passing values that are
    incompatible such as null list.

--- src/main/java/duke/command/ListCommand.java
+++ src/main/java/duke/command/ListCommand.java
@@ -29,6 +29,8 @@ public class ListCommand extends Command{
 
     @Override
     public void execute(TaskList taskList, Gui gui, Storage storage) throws DukeException {
+        assert taskList != null : "task list should not be null.";
+        assert gui != null : "gui should not be null.";
         if (taskList.size() == 0) {
             throw new DukeException("It seems that your task list is empty.\n"
                     + "Try adding some task using \"todo\", \"deadline\" or \"event\"");
