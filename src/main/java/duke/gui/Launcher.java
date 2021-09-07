195b13b589c8f94011bf1372ec4029f7aa59e3fa
IrvingHe000 <heoutong@gmail.com>
2021-09-07 18:47:32 +0800

    Add assertions to the program
    There are some places that a certain value or range of value are required.
    Using assertions can help to warn users when their is a bug that those values
    are not like what we expected.
    
    This change improves the accuracy of the program because we can find bugs
    more quickly.
    
    Let's update the code by adding assertions when necessary.

--- src/main/java/duke/gui/Launcher.java
+++ src/main/java/duke/gui/Launcher.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
  */
 public class Launcher {
     public static void main(String[] args) {
+        assert Duke.class != null : "There should be a Duke class inside the project folder";
         Application.launch(Duke.class, args);
     }
 }
