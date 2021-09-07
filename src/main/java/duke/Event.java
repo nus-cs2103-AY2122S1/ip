c853d73f546f5af8f6cd4fffc18385be6aba122f
Wilfredwongkc <Wilfredwongkc@gmail.com>
2021-09-07 21:28:24 +0800

    Refactor code by replacing magic literals

--- src/main/java/duke/Event.java
+++ src/main/java/duke/Event.java
@@ -24,6 +24,8 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (by: " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+        String prefix = "[E]";
+        String suffix = " (by: " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+        return prefix + super.toString() + suffix;
     }
 }
--- src/main/java/duke/Todo.java
+++ src/main/java/duke/Todo.java
@@ -11,6 +11,7 @@ public class Todo extends Task {
 
     @Override
     public String toString() {
-        return "[T]" + super.toString();
+        final String prefix = "[T]";
+        return prefix + super.toString();
     }
 }
