b4671e3a7e17365dc2bfd67ff8540f174e00409a
CheahYan <cheahyan1103@gmail.com>
2021-09-07 14:38:04 +0800

    A-Jar

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,11 +1,6 @@
 package duke;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Duke {
 
