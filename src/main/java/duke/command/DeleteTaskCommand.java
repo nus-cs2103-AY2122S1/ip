24ac8aedcc108d2c814db413b1770cd7f2cebae5
jayasting98 <65202977+jayasting98@users.noreply.github.com>
2021-09-08 01:20:48 +0800

    Make ApplyOnTaskNumberCommand a sub-type of UndoableCommand

--- src/main/java/duke/command/DeleteTaskCommand.java
+++ src/main/java/duke/command/DeleteTaskCommand.java
@@ -8,27 +8,35 @@ public class DeleteTaskCommand extends ApplyOnTaskNumberCommand {
 
     private static final CommandType COMMAND_TYPE = CommandType.DELETE_TASK;
 
+    private Task deletedTask;
+
     public DeleteTaskCommand(String command) {
         super(command);
     }
 
     @Override
     public void execute(TaskHandler taskHandler, Ui ui) {
-        Task task;
         try {
-            task = taskHandler.deleteTask(getTaskIndex());
+            deletedTask = taskHandler.deleteTask(getTaskIndex());
         } catch (IndexOutOfBoundsException e) {
             throw new DukeInvalidCommandException("The task number does not exist.");
         }
         ui.startMessage()
                 .addLine("Noted. I've removed this task:")
-                .addTask(task)
+                .addTask(deletedTask)
                 .addTasksListLength(taskHandler.getNumberOfTasks())
                 .printFormatted();
+        CommandHandler commandHandler = CommandHandler.getInstance();
+        commandHandler.addToUndoableCommands(this);
     }
 
     @Override
     CommandType getCommandType() {
         return COMMAND_TYPE;
+    }
+
+    @Override
+    public void undo(TaskHandler taskHandler) {
+        taskHandler.insertTask(getTaskIndex(), deletedTask);
     }
 }
