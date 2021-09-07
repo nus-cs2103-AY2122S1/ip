edcf3929fbc26389db3e45ecd73f85a88e7dc06e
nguyiyang <nguyiyang47@gmail.com>
2021-09-07 23:48:51 +0800

    Remove I/O redirection tests

--- src/main/java/lania/DialogBox.java
+++ src/main/java/lania/DialogBox.java
@@ -13,7 +13,6 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Paint;
-
 import java.io.IOException;
 
 //@@author nguyiyang-reused
