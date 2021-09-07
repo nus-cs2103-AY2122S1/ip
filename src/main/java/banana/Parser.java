451fa00d06791dcc987ff7619a7362f1c0df2725
Banana3021 <anaravi21@gmail.com>
2021-09-07 23:43:16 +0800

    Parser class: parses the user’s input and provides the according output
    A new command called "undo" helps to reverse the input that was just
    made, but its edge cases need to be tweaked.
    
    Let’s check whether the previous command is null.
    
    This is to prevent an exception from being thrown if
    "undo" is the user's first input, as a result of the previous input
    not yet holding a value.

--- src/main/java/banana/Parser.java
+++ src/main/java/banana/Parser.java
@@ -83,30 +83,31 @@ public class Parser {
      * @return the label.
      */
     public String undoCommand(TaskList tasks) {
-        String undoText = "Sorry, undo cannot be done,  "
-                + "there are currently "
-                + "no tasks in the list.";
-        if (tasks.getSize() >= 1) {
-            if (prevInput.contains("done")) {
-                int index = Integer.parseInt(prevInput.substring(5));
-                tasks.getTask(index - 1).setIsDone(false);
-                undoText = "Undid done on task " + index;
-            } else if (prevInput.contains("delete")) {
-                int index = Integer.parseInt(prevInput.substring(7));
-                TaskList latterTasks = tasks.subList(index - 1, tasks.getSize());
-                tasks.removeTasks(latterTasks);
-                tasks.addTask(deletedTask);
-                tasks.addTasks(latterTasks);
-                undoText = "Undid delete on task " + index;
-            } else if (!prevInput.contains("find")
-                    && !prevInput.equals("list")) {
-                Task temp = tasks.getTask(tasks.getSize() - 1);
-                tasks.removeTask(temp);
-                undoText = "Removed task " + temp.getDescription();
-            } else {
-                undoText = "There is nothing to undo.";
-            }
+        String undoText = "";
+        if (prevInput == null) {
+            undoText = "You have not inputted anything"
+                    + "to undo yet.";
+        } else if (prevInput.contains("done")) {
+            int index = Integer.parseInt(prevInput.substring(5));
+            tasks.getTask(index - 1).setIsDone(false);
+            undoText = "Undid done on task " + index;
+        } else if (prevInput.contains("delete")) {
+            int index = Integer.parseInt(prevInput.substring(7));
+            TaskList latterTasks = tasks.subList(index - 1, tasks.getSize());
+            tasks.removeTasks(latterTasks);
+            tasks.addTask(deletedTask);
+            tasks.addTasks(latterTasks);
+            undoText = "Undid delete on task " + index;
+        } else if (!prevInput.contains("find")
+                && !prevInput.equals("list")) {
+            Task temp = tasks.getTask(tasks.getSize() - 1);
+            tasks.removeTask(temp);
+            undoText = "Removed task " + temp.getDescription();
+        } else {
+            undoText = "This command does not change the tasklist,"
+                    + "so there is nothing to undo.";
         }
+
         return undoText;
     }
 
