6e28bc56c2f04a689e5202c2a1444457fc79ec0c
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2021-09-07 13:02:47 +0800

    Use assert feature
    Currently, there are no safety measures in the functional code.
    
    Add assertion statements to document important assumptions that should hold at various points in the code
    
    Assertions add additional safety measures inside the functional code

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,8 +1,7 @@
 package duke;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Pattern;
 
 
@@ -15,16 +14,16 @@ public class Duke {
     private static final Pattern DATE_PATTERN = Pattern.compile("^((19|2[0-9])[0-9]{2})-(0[1-9]|1[012])-"
             + "(0[1-9]|[12][0-9]|3[01]) ([01]?[0-9]|2[0-3])[0-5][0-9]$");
     private final TaskList tasks;
-    private final DukeUI ui;
+    private final DukeUi ui;
     private final Storage storage;
     private final Parser parser;
     private boolean isRunning;
 
     /**
-     * Returns a <code>Duke</code> object that can reply to command.
+     * Create a <code>Duke</code> object that can reply to commands.
      */
     public Duke() {
-        ui = new DukeUI();
+        ui = new DukeUi();
         storage = new Storage(DATABASE_PATH);
         parser = new Parser();
         isRunning = false;
@@ -32,23 +31,25 @@ public class Duke {
     }
 
     /**
-     * The main method run by Duke, to respond to commands. The functional
+     * Runs Duke, to respond to commands. The functional
      * commands are: bye, done, deadline, todo, event
      * delete, list. Other commands are ignored.
      */
     public void run() {
-        storage.loadTasksFromFile(tasks);
         String command;
+        storage.loadTasksFromFile(tasks);
         isRunning = true;
         Scanner scanner = new Scanner(System.in);
         while (isRunning) {
             command = scanner.nextLine();
+            assert command != null;
             parser.interpretCommand(command);
-            String firstCommand = this.parser.getFirstCommand();
+            String firstCommand = parser.getFirstCommand();
+            assert firstCommand != null;
             try {
                 ui.respondToUser(respondToFirstCommand(firstCommand));
             } catch (DukeException e) {
-                this.ui.showError(e);
+                System.out.println(ui.showError(e));
             }
         }
     }
@@ -64,6 +65,7 @@ public class Duke {
             storage.loadTasksFromFile(tasks);
             parser.interpretCommand(command);
             String firstCommand = parser.getFirstCommand();
+            assert firstCommand != null;
             return respondToFirstCommand(firstCommand);
         } catch (DukeException e) {
             return e.getMessage();
@@ -92,13 +94,14 @@ public class Duke {
             response = tasks.listTasks();
             break;
         case "find":
-            String keyword = this.parser.findKeyword();
-            ArrayList<Task> tasksWithKeyword = this.tasks.findTasksUsingKeyword(keyword);
+            String keyword = parser.findKeyword();
+            ArrayList<Task> tasksWithKeyword = tasks.findTasksUsingKeyword(keyword);
             response = ui.showTasksWithKeyword(tasksWithKeyword);
             break;
         default:
             throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
+        assert !Objects.equals(response, "");
         return response;
     }
 
@@ -110,9 +113,9 @@ public class Duke {
      */
     public String deleteTask() throws DukeException {
         try {
-            this.tasks.deleteTask(parser.findCommandIndex());
-            this.writeDataToDuke();
-            return this.ui.showDeleteTaskMessage(this.tasks.getTasksLength());
+            tasks.deleteTask(parser.findCommandIndex());
+            writeDataToDuke();
+            return ui.showDeleteTaskMessage(tasks.getTasksLength());
         } catch (IndexOutOfBoundsException e) {
             throw new DukeException("☹ OOPS!!! Index out of range!");
         } catch (NumberFormatException e) {
@@ -121,7 +124,7 @@ public class Duke {
     }
 
     private void writeDataToDuke() {
-        this.storage.writeData(this.tasks);
+        storage.writeData(tasks);
     }
 
     /**
@@ -132,32 +135,33 @@ public class Duke {
      * or when the user formats the date wrongly.
      */
     public String addTask() throws DukeException {
-        String firstCommand = this.parser.getFirstCommand();
-        String date = this.parser.findDateInCommand();
-        String taskDesc = this.parser.findTaskDescription();
-        String aOrAn = firstCommand.equals("event") ? "an" : "a";
+        String firstCommand = parser.getFirstCommand();
+        Task.TaskType taskType = convertToTaskType(firstCommand);
+        String date = parser.findDateInCommand();
+        String taskDesc = parser.findTaskDescription();
         if (taskDesc.equals("")) {
-            throw new DukeException("☹ OOPS!!! The description of " + aOrAn + " " + firstCommand + " cannot be empty.");
-        } else if (date.equals("") && convertToTaskType(firstCommand) != Task.TaskType.TODO) {
-            throw new DukeException("☹ OOPS!!! The date of " + aOrAn + " " + firstCommand + " cannot be empty.");
-        } else if (convertToTaskType(firstCommand) == Task.TaskType.DEADLINE
-                || convertToTaskType(firstCommand) == Task.TaskType.EVENT) {
+            throw new DukeException("☹ OOPS!!! The description cannot be empty.");
+        } else if (date.equals("") && taskType != Task.TaskType.TODO) {
+            throw new DukeException("☹ OOPS!!! The date cannot be empty.");
+        } else if (taskType == Task.TaskType.DEADLINE
+                || taskType == Task.TaskType.EVENT) {
             if (DATE_PATTERN.matcher(date).matches()) {
-
                 String[] dateSplit = date.split(" ");
                 String dateString = dateSplit[0];
                 String timeString = dateSplit[1];
                 LocalDate ld = LocalDate.parse(dateString);
-                this.tasks.addTask(taskDesc, convertToTaskType(firstCommand), ld, timeString);
-                this.writeDataToDuke();
-                return this.confirmAdditionOfTask();
+                assert !dateString.equals("");
+                assert !timeString.equals("");
+                tasks.addTask(taskDesc, convertToTaskType(firstCommand), ld, timeString);
+                writeDataToDuke();
+                return confirmAdditionOfTask();
             } else {
                 throw new DukeException("You need to put the date in yyyy-mm-dd hhmm format!");
             }
         } else {
-            this.tasks.addTask(taskDesc);
-            this.writeDataToDuke();
-            return this.confirmAdditionOfTask();
+            tasks.addTask(taskDesc);
+            writeDataToDuke();
+            return confirmAdditionOfTask();
         }
 
     }
@@ -166,8 +170,9 @@ public class Duke {
      * Confirms the addition of a task.
      */
     public String confirmAdditionOfTask() {
-        int tasksLength = this.tasks.getTasksLength();
-        return this.ui.showTaskAddedMessage(tasksLength, this.tasks.getTask(tasksLength).toString());
+        int tasksLength = tasks.getTasksLength();
+        assert tasksLength != 0;
+        return ui.showTaskAddedMessage(tasksLength, tasks.getTask(tasksLength).toString());
     }
 
     /**
@@ -179,10 +184,11 @@ public class Duke {
     private String markDone() throws DukeException {
         try {
             int taskIndex = parser.findCommandIndex();
-            this.tasks.markTaskDone(taskIndex);
-            Task task = this.tasks.getTask(taskIndex);
-            this.writeDataToDuke();
-            return this.ui.markTaskDone(task);
+            assert taskIndex >= 0;
+            tasks.markTaskDone(taskIndex);
+            Task task = tasks.getTask(taskIndex);
+            writeDataToDuke();
+            return ui.markTaskDone(task);
         } catch (IndexOutOfBoundsException e) {
             throw new DukeException("☹ OOPS!!! The number you gave is out of range!");
         } catch (NumberFormatException e) {
