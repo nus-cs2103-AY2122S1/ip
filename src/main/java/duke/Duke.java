e3e7e076a54bce455495617b246775c5bd470f28
Jingwencloud <jingwen098765432@gmail.com>
2021-09-07 14:43:30 +0800

    Remove unused parts of code

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,21 +1,5 @@
 package duke;
 
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.Region;
-import javafx.stage.Stage;
-import javafx.scene.control.Label;
-
-
-
 import duke.command.Command;
 import duke.exception.DukeException;
 
@@ -24,19 +8,12 @@ import duke.exception.DukeException;
  * and keep track of different tasks.
  */
 
-public class Duke extends Application {
+public class Duke {
 
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
     private Parser parser;
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
     /**
      * Constructor for Duke
@@ -63,41 +40,6 @@ public class Duke extends Application {
         this("data/tasks.txt");
     }
 
-    /**
-     * Runs the program.
-     */
-
-    public void run() {
-        //ui.greetUser();
-        boolean isBye = false;
-        while (!isBye) {
-            try {
-                String fullCommand = ui.readCommand();
-                ui.showLine();
-                Command c = parser.parse(fullCommand);
-                c.execute(tasks, ui, storage);
-                isBye = c.isBye();
-            } catch (DukeException e) {
-                ui.showError(e.getMessage());
-            } finally {
-                ui.showLine();
-            }
-        }
-    }
-
-    /**
-     * Creates a label with the specified text and adds it to the dialog container.
-     * @param text String containing text to add
-    * @return a label with the specified text that has word wrap enabled.
-    */
-    public Label getDialogLabel(String text) {
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
-    }
-
-
     public String getResponse(String input) {
         try {
             String fullCommand = input;
@@ -111,18 +53,6 @@ public class Duke extends Application {
     public Ui getUi() {
         return ui;
     }
-    @Override
-    public void start(Stage stage) {
-
-    }
-
-    /**
-     * Creates an instance of Duke and runs the program.
-     *
-     * @param args Command line arguments
-     */
-    public static void main(String[] args) {
-        new Duke("data/tasks.txt").run();
-    }
 
 }
+
