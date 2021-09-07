c8286d0eaf6b546be447aa60a04a0852a6c0ed57
Trushti <tsm20901@gmail.com>
2021-09-07 14:59:55 +0800

    Fix CheckStyleTest for the DukeTest class

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -1,32 +1,32 @@
 package duke;
 
-import jdk.jfr.Category;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 
 public class DukeTest {
     @Test
-    public void dummyTest(){
+    public void dummyTest() {
         assertEquals(2, 2);
     }
 
     @Test
-    public void addTask_todo_addedTodo(){
+    public void addTask_todo_addedTodo() {
         Task ob = new Task("todo read book", Duke.Category.TODO);
-        assertEquals("[T][] read book",ob.toString());
+        assertEquals("[T][] read book", ob.toString());
     }
 
     @Test
-    public void addTask_deadline_addedDeadline(){
+    public void addTask_deadline_addedDeadline() {
         Task ob = new Task("deadline hw /by 2015-09-18", Duke.Category.DEADLINE);
-        assertEquals("[D][] hw  (by: Sep 18 2015)",ob.toString());
+        assertEquals("[D][] hw  (by: Sep 18 2015)", ob.toString());
     }
 
     @Test
-    public void addTask_event_addedEvent(){
+    public void addTask_event_addedEvent() {
         Task ob = new Task("event concert /at 2018-06-18", Duke.Category.EVENT);
-        assertEquals("[E][] concert  (at: Jun 18 2018)",ob.toString());
+        assertEquals("[E][] concert  (at: Jun 18 2018)", ob.toString());
     }
 
 
