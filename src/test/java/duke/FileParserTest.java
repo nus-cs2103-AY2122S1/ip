ad4fe8006395a37d0c7cfda01058db4321b5eb77
ngsengleng <ngsengleng@gmail.com>
2021-09-07 19:39:15 +0800

    Change junit tests to pass checkstyle

--- src/test/java/duke/FileParserTest.java
+++ src/test/java/duke/FileParserTest.java
@@ -1,27 +1,28 @@
 package duke;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class FileParserTest {
     @Test
-    public void ReadLineFromFile_StringFromFile_CorrectEventReturned() {
+    public void readLineFromFile_stringFromFile_correctEventReturned() {
         FileParser f = new FileParser();
         assertEquals("[E]|[]|buy food|2011-01-01", f.parse("[E]|[]|buy food|2011-01-01").toString());
     }
     @Test
-    public void ReadLineFromFile_StringFromFile_CorrectDeadlineReturned() {
+    public void readLineFromFile_stringFromFile_correctDeadlineReturned() {
         FileParser f = new FileParser();
         assertEquals("[D]|[]|buy food|2011-01-01", f.parse("[D]|[]|buy food|2011-01-01").toString());
     }
     @Test
-    public void ReadLineFromFile_StringFromFile_CorrectTodoReturned() {
+    public void readLineFromFile_stringFromFile_correctTodoReturned() {
         FileParser f = new FileParser();
         assertEquals("[T]|[]|buy food", f.parse("[T]|[]|buy food").toString());
     }
 
     @Test
-    public void ReadLineFromFile_IncorrectStringFromFile_ExceptionThrown() {
+    public void readLineFromFile_incorrectStringFromFile_exceptionThrown() {
         FileParser f = new FileParser();
         try {
             assertEquals("[T]|[]|buy food", f.parse("ss|dd"));
