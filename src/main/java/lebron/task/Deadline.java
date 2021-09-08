6b08a399833825aa644a617f3e50df7e2b8bd92b
nniiggeell <e0559733@u.nus.edu>
2021-09-08 22:25:27 +0800

    Update Deadline.java
    To accept time

--- src/main/java/lebron/task/Deadline.java
+++ src/main/java/lebron/task/Deadline.java
@@ -1,7 +1,12 @@
 package lebron.task;
 
+import lebron.exception.InvalidInputException;
+import lebron.exception.LebronException;
+
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * This class represents the Deadline event.
@@ -10,26 +15,35 @@ import java.time.format.DateTimeFormatter;
  */
 public class Deadline extends Task {
 
-    private final LocalDate by;
+    private final LocalDate date;
+    private final LocalTime time;
 
     /**
      * Constructor.
      *
      * @param description the name of the task
-     * @param by the deadline
+     * @param date the date of the deadline in dd/MM/yyyy
+     * @param time the time of the deadline in HHmm
      */
-    public Deadline(String description, String by) {
+    public Deadline(String description, String date, String time) throws LebronException {
         super(description);
-        this.by = LocalDate.parse(by);
+        try {
+            this.date = LocalDate.parse(date);
+            this.time = LocalTime.parse(time, DateTimeFormatter.ofPattern("HHmm"));;
+        } catch (DateTimeParseException e) {
+            throw new LebronException("    :( OOPS! Please check that your date and time is " +
+                    "valid and formatted as 'yyyy-MM-dd' 'HHmm'.");
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("dd MMM yyyy")) + ")";
+        return "[D]" + super.toString() + " (by: " + date.format(DateTimeFormatter.ofPattern("dd MMM yyyy"))
+                + " " + time.format(DateTimeFormatter.ofPattern("HHmm"))+ ")";
     }
 
     @Override
     public String getStringForFile() {
-        return "D | " + super.getDoneValue() + " | " + super.getName() + " | " + this.by;
+        return "D | " + super.getDoneValue() + " | " + super.getName() + " | " + this.date + " " + this.time;
     }
 }
