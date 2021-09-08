954f35509dc81fe850c2dd199e2b03bbb1c4928c
nniiggeell <e0559733@u.nus.edu>
2021-09-08 22:24:24 +0800

    Update Lebron.java

--- src/main/java/lebron/Lebron.java
+++ src/main/java/lebron/Lebron.java
@@ -1,20 +1,9 @@
 package lebron;
 
-import java.util.ArrayList;
-
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.layout.VBox;
-
 import lebron.exception.LebronException;
-import lebron.task.Deadline;
-import lebron.task.Events;
-import lebron.task.Task;
-import lebron.task.TaskList;
-import lebron.task.ToDo;
+import lebron.task.*;
+
+import java.util.ArrayList;
 
 /**
  * This class represents the chat bot.
@@ -30,14 +19,6 @@ public class Lebron {
     private TaskList taskList;
     private final Ui ui;
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
-    private final Image lebron = new Image(this.getClass().getResourceAsStream("/images/lebron.jpg"));
-    private final Image user = new Image(this.getClass().getResourceAsStream("/images/blank.png"));
 
     /**
      * Available commands that the bot can understand.
@@ -50,6 +31,7 @@ public class Lebron {
         EVENT("event"),
         DELETE("delete"),
         FIND("find"),
+        BYE("bye"),
         OTHER(" ");
 
         private final String text;
@@ -93,83 +75,94 @@ public class Lebron {
         Parser parser = new Parser();
         String reply = "";
         try {
-            if (text.equals("bye")) {
-                reply = ui.exit();
-            } else {
-                String commandWord = parser.parseText(text);
-                String[] splitWords = parser.split(text);
-                Command command = Command.fromString(commandWord);
-                switch (command) {
-                case LIST:
-                    reply = ui.replyDisplay(taskList);
-                    break;
-                case DONE:
-                    if (splitWords.length < 2 || splitWords[1].equals("")) {
-                        throw new LebronException("    :( OOPS! Please specify which task you wish "
-                                + "to complete.");
-                    }
-                    int pos = Integer.parseInt(splitWords[1]);
-                    reply = taskList.markDone(pos - 1);
-                    storage.saveToFile(taskList.getLst());
-                    break;
-                case TODO:
-                    if (splitWords.length < 2 || splitWords[1].equals("")) {
-                        throw new LebronException("    :( OOPS! The description of a ToDo "
-                                + "cannot be empty.");
-                    }
-                    reply = taskList.add(new ToDo(splitWords[1]));
-                    storage.saveToFile(taskList.getLst());
-                    break;
-                case DEADLINE:
-                    if (splitWords.length < 2 || splitWords[1].equals("")) {
-                        throw new LebronException("    :( OOPS! The description of a Deadline " +
-                                "cannot be empty.");
-                    }
-                    String[] splitBy = splitWords[1].split("/by ", 2);
-                    if (splitBy.length < 2 || splitBy[1].equals("")) {
-                        throw new LebronException("    :( OOPS! Please check that the '/by' keyword " +
-                                "is used and that a due date is given.");
-                    }
-                    reply = taskList.add(new Deadline(splitBy[0], splitBy[1]));
-                    storage.saveToFile(taskList.getLst());
-                    break;
-                case EVENT:
-                    if (splitWords.length < 2 || splitWords[1].equals("")) {
-                        throw new LebronException("    :( OOPS! The description of an Event " +
-                                "cannot be empty.");
-                    }
-                    String[] splitAt = splitWords[1].split("/at ", 2);
-                    if (splitAt.length < 2 || splitAt[1].equals("")) {
-                        throw new LebronException("    :( OOPS! Please check that the '/at' keyword " +
-                                "is used and that a due date is given.");
-                    }
-                    reply = taskList.add(new Events(splitAt[0], splitAt[1]));
-                    storage.saveToFile(taskList.getLst());
-
-                    break;
-                case DELETE:
-                    if (splitWords.length < 2 || splitWords[1].equals("")) {
-                        throw new LebronException("    :( OOPS! Please specify which task you wish "
-                                + "to delete.");
-                    }
-                    int pos2 = Integer.parseInt(splitWords[1]);
-                    reply = taskList.delete(pos2 - 1);
-                    storage.saveToFile(taskList.getLst());
-                    break;
-                case FIND:
-                    if (splitWords.length < 2 || splitWords[1].equals("")) {
-                        throw new LebronException("    :( OOPS! Please specify some words to search for.");
-                    }
-                    String keyword = splitWords[1];
-                    reply = ui.replyFind(taskList, keyword);
-                    break;
-                case OTHER:
-                    throw new LebronException(" OOPS! I'm sorry, but I don't know what that means.\n");
-                default:
-                    break;
+            String commandWord = parser.parseText(text);
+            String[] splitWords = parser.split(text);
+            Command command = Command.fromString(commandWord);
+            switch (command) {
+            case BYE:
+                reply = ui.replyExit();
+                break;
+            case LIST:
+                reply = ui.replyDisplay(taskList);
+                break;
+            case DONE:
+                if (splitWords.length < 2 || splitWords[1].equals("")) {
+                    throw new LebronException("    :( OOPS! Please specify which task you wish "
+                            + "to complete.");
                 }
+                int pos = Integer.parseInt(splitWords[1]);
+                reply = taskList.markDone(pos - 1);
+                storage.saveToFile(taskList.getLst());
+                break;
+            case TODO:
+                if (splitWords.length < 2 || splitWords[1].equals("")) {
+                    throw new LebronException("    :( OOPS! The description of a ToDo "
+                            + "cannot be empty.");
+                }
+                reply = taskList.add(new ToDo(splitWords[1]));
+                storage.saveToFile(taskList.getLst());
+                break;
+            case DEADLINE:
+                if (splitWords.length < 2 || splitWords[1].equals("")) {
+                    throw new LebronException("    :( OOPS! The description of a Deadline " +
+                            "cannot be empty.");
+                }
+                String[] splitBy = splitWords[1].split("/by ", 2);
+                if (splitBy.length < 2 || splitBy[1].equals("")) {
+                    throw new LebronException("    :( OOPS! Please check that the '/by' keyword " +
+                            "is used and that a due date and time is given.");
+                }
+                String[] dateTimeArrDeadline = splitBy[1].split(" ", 2);
+                if (dateTimeArrDeadline.length < 2 || dateTimeArrDeadline[1].equals("")) {
+                    throw new LebronException("    :( OOPS! Please check that your date and time is " +
+                            "valid and formatted as 'yyyy-MM-dd' 'HHmm'.");
+                }
+                reply = taskList.add(new Deadline(splitBy[0], dateTimeArrDeadline[0]
+                        , dateTimeArrDeadline[1]));
+                storage.saveToFile(taskList.getLst());
+                break;
+            case EVENT:
+                if (splitWords.length < 2 || splitWords[1].equals("")) {
+                    throw new LebronException("    :( OOPS! The description of an Event " +
+                            "cannot be empty.");
+                }
+                String[] splitAt = splitWords[1].split("/at ", 2);
+                if (splitAt.length < 2 || splitAt[1].equals("")) {
+                    throw new LebronException("    :( OOPS! Please check that the '/at' keyword " +
+                            "is used and that a due date is given.");
+                }
+                String[] dateTimeArrEvent = splitAt[1].split(" ", 2);
+                if (dateTimeArrEvent.length < 2 || dateTimeArrEvent[1].equals("")) {
+                    throw new LebronException("    :( OOPS! Please check that your date and time is " +
+                            "valid and formatted as 'yyyy-MM-dd' 'HHmm'.");
+                }
+                reply = taskList.add(new Events(splitAt[0], dateTimeArrEvent[0], dateTimeArrEvent[1]));
+                storage.saveToFile(taskList.getLst());
+
+                break;
+            case DELETE:
+                if (splitWords.length < 2 || splitWords[1].equals("")) {
+                    throw new LebronException("    :( OOPS! Please specify which task you wish "
+                            + "to delete.");
+                }
+                int pos2 = Integer.parseInt(splitWords[1]);
+                reply = taskList.delete(pos2 - 1);
+                storage.saveToFile(taskList.getLst());
+                break;
+            case FIND:
+                if (splitWords.length < 2 || splitWords[1].equals("")) {
+                    throw new LebronException("    :( OOPS! Please specify some words to search for.");
+                }
+                String keyword = splitWords[1];
+                reply = ui.replyFind(taskList, keyword);
+                break;
+            case OTHER:
+                throw new LebronException(" OOPS! I'm sorry, but I don't know what that means.\n");
+            default:
+                break;
             }
             return reply;
+
         } catch (LebronException e) {
             return ui.printException(e.getMessage());
         }
