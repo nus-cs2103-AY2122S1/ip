10effcd6d17f07319f0d8be9d61c366f8d252290
timothywongej <e0559903@u.nus.edu>
2021-09-08 17:33:54 +0800

    Add additional command classes

--- src/main/java/duke/commands/ListCommand.java
+++ src/main/java/duke/commands/ListCommand.java
@@ -1,4 +1,25 @@
 package duke.commands;
 
-public class ListCommand {
+import duke.Storage;
+import duke.TaskList;
+import duke.task.Task;
+
+public class ListCommand extends Command {
+
+    public static final String COMMAND_WORD = "list";
+
+    public ListCommand() {
+    }
+
+    @Override
+    public CommandResult execute() {
+        int count = 1;
+        String response = "";
+
+        for (Task t : tasks.getAll()) {
+            response += (count + ". " + t.toString() + "\n");
+            count += 1;
+        }
+        return new CommandResult(response);
+    }
 }
