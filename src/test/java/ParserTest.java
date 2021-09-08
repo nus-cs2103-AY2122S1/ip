8fb25b7ae695721627f9ada072c299ca9aa811d4
Aishwarya Nair <aishrn1202@gmail.com>
2021-09-08 17:27:26 +0800

    Add improvements to code quality
    Method to parse the inputted user command was rather lengthy and deeply nested.
    
    Very long and deeply methods reduce readability and code quality and indicate the
    need for better abstraction.
    
    TaskList related methods have been moved to the TaskList class while common parts
    of the code in switch case branches and exception handling have been abstracted out.
    Default cases have been included as well.

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -2,10 +2,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import tasks.Deadline;
+
+
 class ParserTest {
+    @Test
+    void parseCommand_invalidCommandEntered_errorMessage() {
+        Blitz b = new Blitz("data/blitz.txt");
+        boolean isEqual = "Sorry, but I don't know what that means :-("
+                .equals(Parser.parseCommand("hello", b.getTasks(), b.getUi()));
+        assertEquals(true, isEqual);
+    }
+    @Test
+    void parseCommand_taskWithEmptyDescription_errorMessage() {
+        Blitz b = new Blitz("data/blitz.txt");
+        String result = Parser.parseCommand("todo", b.getTasks(), b.getUi());
+        boolean isEqual = b.getUi().getTaskDescriptionCannotBeEmptyMessage()
+                .equals(result);
+        assertEquals(true, isEqual);
+    }
+    @Test
+    void parseCommand_taskToAdd_invalidDateTimeFormat() {
+        Blitz b = new Blitz("data/blitz.txt");
+        String result = Parser.parseCommand("event party /on Sunday", b.getTasks(), b.getUi());
+        boolean isEqual = b.getUi().getIncorrectDateTimeFormatMessage().equals(result);
+        assertEquals(true, isEqual);
+    }
 
     @Test
-    void getIndex() throws BlitzException {
-        assertEquals(9, Parser.getIndex("done 10", 11));
+    void parseCommand_taskToAdd_success() {
+        Blitz b = new Blitz("data/blitz.txt");
+
+        String result = Parser.parseCommand("deadline return book /by 12/08/2021 2359", b.getTasks(), b.getUi());
+        String message = "Got it. I've added this task:" + "\n\t" + new Deadline("return book",
+                Parser.parseDateTime("12/08/2021 2359")) + "\n\nNow you have "
+                + b.getTasks().size() + " tasks in the list.";
+        boolean isEqual = message.equals(result);
+        assertEquals(true, isEqual);
     }
 }
+
