b2cab0052188e48c9fa16cbaa1821585726a2fb5
Owen Tan <tanteongyuowen@gmail.com>
2021-09-09 04:18:26 +0800

    Update GUI

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -23,7 +23,6 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
-            scene.getStylesheets().add("../../resources/view/styles.css");
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
