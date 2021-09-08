2baf3519da6e655ecf2ecaaa25c209d13ae93f79
radiankrisno <radiankrisno@gmail.com>
2021-09-08 14:50:29 +0800

    Update test cases for TaskList

--- src/test/java/tasklist/TaskListTest.java
+++ src/test/java/tasklist/TaskListTest.java
@@ -12,20 +12,23 @@ import models.Task;
 
 public class TaskListTest {
 
-    private final TaskList list = new TaskList(new ArrayList<>());
+    private TaskList list = new TaskList(new ArrayList<>());
 
     @Test
     public void addTask_validInput_addSuccessfully() {
-        this.list.addTask(new Task(""));
-        assertEquals(1, this.list.getSize());
+        list.addTask(new Task(""));
+        list = list.getNext();
+        assertEquals(1, list.getSize());
     }
 
     @Test
     public void deleteTask_validInput_deleteSuccessfully() {
         try {
-            this.list.addTask(new Task(""));
-            this.list.deleteTask(0);
-            assertEquals(0, this.list.getSize());
+            list.addTask(new Task(""));
+            list = list.getNext();
+            list.deleteTask(0);
+            list = list.getNext();
+            assertEquals(0, list.getSize());
         } catch (DukeException error) {
             fail("Should not have thrown any exception");
         }
@@ -34,7 +37,7 @@ public class TaskListTest {
     @Test
     public void deleteTask_invalidInput_throwError() {
         try {
-            this.list.deleteTask(0);
+            list.deleteTask(0);
             fail("Should have thrown  DukeException");
         } catch (DukeException error) {
             return;
@@ -44,15 +47,18 @@ public class TaskListTest {
     @Test
     public void getTask_validInput_getTaskSuccessfully() {
         Task task = new Task("");
-        this.list.addTask(task);
-        Task result = this.list.getTask(0);
+        list.addTask(task);
+        list = list.getNext();
+        Task result = list.getTask(0);
         assertEquals(task, result);
     }
 
     @Test
     public void getSize_validInput_returnSizeOfTasklistSuccessfully() {
-        this.list.addTask(new Task(""));
-        this.list.addTask(new Task(""));
+        list.addTask(new Task(""));
+        list = list.getNext();
+        list.addTask(new Task(""));
+        list = list.getNext();
         assertEquals(2, this.list.getSize());
     }
 }
