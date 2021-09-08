d56314dbd7477731a1e642ac582e5d535c565c26
Owen Tan <tanteongyuowen@gmail.com>
2021-09-08 22:44:09 +0800

    Add comments

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -20,26 +20,52 @@ public class Ui {
     public Ui(TaskList tasks) {
         this.tasks = tasks;
     }
+
+    /**
+     * Returns a greeting message.
+     *
+     * @return A String representation of a Greeting message.
+     */
     public static String showGreeting() {
         String message = "MY BEST FRIEND! \nWhat can I do for you?";
         return message;
     }
 
+    /**
+     * Returns a farewell message.
+     *
+     * @return A String representation of a Farewell message.
+     */
     public static String showFarewellMsg() {
         String message = "GoodBye Brother!";
         return message;
     }
 
+    /**
+     * Returns a AddTask Message.
+     *
+     * @return A String representation of a AddTask message.
+     */
     public String showAddTaskMsg(Task text) {
         String message = "Sure thing! I've added this task:\n  " + text + "\n";
         return message;
     }
 
+    /**
+     * Returns a Completed Task message.
+     *
+     * @return A String representation of a Task completed message.
+     */
     public String showCompleteTaskMsg(Task text) {
         String message = "Nice! I've marked this task as done:\n  " + text + "\n";
         return message;
     }
 
+    /**
+     * Returns a Deleted Task message.
+     *
+     * @return A String representation of a Deleted Task message.
+     */
     public String showDeleteTaskMsg(Task text) {
         String message = "Noted! I've removed this task:\n  " + text + "\n";
         return message;
