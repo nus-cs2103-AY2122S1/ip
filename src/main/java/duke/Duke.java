78aae112717fe281a59e58ce60fd2b905e5e55a7
tsy24 <szeying3@gmail.com>
2021-09-08 22:59:51 +0800

    Delete Ui class
    GUI added handles the UI, making the Ui class redundant.
    
    Let's delete the Ui class and remove the Ui object from Duke class.
    
    Deleting the Ui class will result in better code quality as we
    remove dead code.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -3,41 +3,38 @@ package duke;
 import java.util.ArrayList;
 
 import duke.command.Command;
-import javafx.scene.image.Image;
 
 /**
  * Duke class that starts and runs the Duke bot.
  */
 public class Duke {
 
+    static final String START_MESSAGE = "Hello! I'm Duke\n" + "What can I do for you?";
     private TaskList taskList;
-    private Ui ui;
     private Storage storage;
-
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/User.jpg"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/Duke.jpg"));
 
     /**
      * Constructs the Duke object.
      */
     public Duke() {
-        ui = new Ui();
         storage = new Storage("data/duke.txt");
         try {
             taskList = new TaskList(storage.getFile());
         } catch (DukeException e) {
-            ui.printMessage(e.getMessage());
+            e.printStackTrace();
             taskList = new TaskList(new ArrayList<>());
         }
     }
 
     public String start() {
-        return "Hello! I'm Duke\n" + "What can I do for you?";
+        return START_MESSAGE;
     }
 
     /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
+     * Gets the response from the Duke bot for the input.
+     *
+     * @param input String input message.
+     * @return Response string.
      */
     public String getResponse(String input) {
         try {
