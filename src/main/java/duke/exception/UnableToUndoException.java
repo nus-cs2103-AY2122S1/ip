694e063b6aa5fe2da497cde1e09d7d619ba8ce14
HongshuW <hongshu.wang.uni@gmail.com>
2021-09-08 20:47:21 +0800

    Add UnableToUndoException.java

---
+++ src/main/java/duke/exception/UnableToUndoException.java
@@ -0,0 +1,12 @@
+package duke.exception;
+
+import duke.command.Command;
+
+public class UnableToUndoException extends DukeException {
+    /**
+     * Constructor of the class `UnableToUndoException`.
+     */
+    public UnableToUndoException(Command lastCommand) {
+        super(String.format("☹ OOPS!!! I cannot undo the previous command: %s\n", lastCommand.toString()));
+    }
+}
