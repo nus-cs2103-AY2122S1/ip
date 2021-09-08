d957dd5e295eab64172f1f85e72de7443c64e983
adam-ky <69505439+adam-ky@users.noreply.github.com>
2021-09-08 11:49:40 +0800

    Add Java varargs

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -6,19 +6,21 @@ import duke.task.Task;
 import duke.task.TaskList;
 
 public class FindCommand extends Command {
-    private String keyword;
+    private String[] keywords;
 
-    public FindCommand(String keyword) {
-        this.keyword = keyword.toLowerCase();
+    public FindCommand(String ... keywords) {
+        this.keywords = keywords;
     }
 
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) {
         TaskList keywordTasks = new TaskList();
-        for (Task t : tasks.getTaskList()) {
-            String description = t.getDescription().toLowerCase();
-            if (description.contains(keyword)) {
-                keywordTasks.addTask(t);
+        for (String keyword : keywords) {
+            for (Task t : tasks.getTaskList()) {
+                if (!keywordTasks.getTaskList().contains(t)
+                        && t.getDescription().toLowerCase().contains(keyword.toLowerCase())) {
+                    keywordTasks.addTask(t);
+                }
             }
         }
         return ui.showTaskList(keywordTasks);
