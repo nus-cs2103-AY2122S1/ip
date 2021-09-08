43f35f3898a6552fa69dcb17e582065f697bb097
James-Kua <kua.james30@gmail.com>
2021-09-08 12:05:59 +0800

    Fix Checkstyle issues

--- src/test/java/duke/task/DeadlineTest.java
+++ src/test/java/duke/task/DeadlineTest.java
@@ -2,10 +2,10 @@ package duke.task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 
 public class DeadlineTest {
