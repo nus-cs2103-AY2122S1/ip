66ca8b92038ba94441693e488d03d05098145622
adam-ky <69505439+adam-ky@users.noreply.github.com>
2021-09-09 20:49:34 +0800

    Add Streams feature
    Stream is used to filter out the task list when provided with a single
    keyword.

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -1,5 +1,9 @@
 package duke.command;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import duke.Storage;
 import duke.Ui;
 import duke.task.Task;
@@ -14,18 +18,39 @@ public class FindCommand extends Command {
 
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) {
-        TaskList keywordTasks = new TaskList();
+        TaskList filterTaskList = keywords.length == 1
+                ? filterWithSingleKeyword(tasks, keywords[0])
+                : filterWithMultipleKeywords(tasks);
+
+        return ui.showTasksFound(filterTaskList);
+    }
+
+    private TaskList filterWithMultipleKeywords(TaskList tasks) {
+        TaskList filteredTasks = new TaskList();
         for (String keyword : keywords) {
             for (Task t : tasks.getTaskList()) {
-                if (!keywordTasks.getTaskList().contains(t)
+                if (!filteredTasks.getTaskList().contains(t)
                         && t.getDescription().toLowerCase().contains(keyword.toLowerCase())) {
-                    assert (keywordTasks.getListSize() <= tasks.getListSize())
+                    assert (filteredTasks.getListSize() <= tasks.getListSize())
                             : "Task list found by find command should not be greater than original task list.";
-                    keywordTasks.addTask(t);
+                    filteredTasks.addTask(t);
                 }
             }
         }
+        return filteredTasks;
+    }
 
-        return ui.showTasksFound(keywordTasks);
+    public TaskList filterWithSingleKeyword(TaskList tasks, String keyword) {
+        List<Task> taskList = tasks
+                .getTaskList()
+                .stream()
+                .filter((task -> containsKeyword(task, keyword)))
+                .collect(Collectors.toList());
+        ArrayList<Task> keywordTasks = new ArrayList<>(taskList);
+        return new TaskList(keywordTasks);
+    }
+
+    public boolean containsKeyword(Task t, String keyword) {
+        return t.getDescription().toLowerCase().contains(keyword.toLowerCase());
     }
 }
