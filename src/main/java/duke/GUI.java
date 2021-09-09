a423ad08af934d6ef07da2c107b8cb1588a600b9
mingyi456 <mingyi456@hotmail.com>
2021-09-09 10:39:43 +0800

    Integrate JavaFX into application
    A GUI interface is the next intended level for this iP, and it has been
    overdue for quite some time
    
    Currently not fully functional, will be improved on soon

---
+++ src/main/java/duke/GUI.java
@@ -0,0 +1,43 @@
+package duke;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    @Override
+    public void start(Stage stage) {
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public String getResponse(String s) {
+        return "";
+    }
+
+    
+}
