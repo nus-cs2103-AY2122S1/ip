5724ae2a83081b184c3ad91aa831403b7c3381df
Gokul Rajiv <gokul.rajiv99@gmail.com>
2021-09-09 23:59:47 +0800

    Add MassOps for delete

--- src/main/java/commands/DeleteCommand.java
+++ src/main/java/commands/DeleteCommand.java
@@ -1,8 +1,13 @@
 package commands;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
 import bot.Bot;
 import bot.Ui;
-import exceptions.InvalidArgumentsException;
 import exceptions.InvalidTaskException;
 
 /**
@@ -12,21 +17,26 @@ public class DeleteCommand extends Command {
 
     @Override
     public String[] run(Bot bot, String[] args) throws InvalidTaskException {
-        int index = Integer.parseInt(args[0]) - 1;
-        validateArgs(index, bot);
-
-        return bot.getTaskList().removeTask(index);
+        System.out.println(args[0]);
+        Stream<Integer> indices = Arrays.stream(args[0].split(","))
+                                        .map(s -> Integer.parseInt(s.trim()) - 1);
+        List<String> messages = new ArrayList<>();
+        indices.sorted(Collections.reverseOrder()).peek((i) -> validateArgs(i, bot)).forEach(ind -> {
+            messages.addAll(Arrays.asList(bot.getTaskList().removeTask(ind)));
+            messages.add("");
+        });
+        return messages.toArray(new String[0]);
     }
 
     /**
      * Validate command arg
      *
-     * @param arg command argument
+     * @param args command argument
      * @param bot Bot in context
      * @throws InvalidTaskException invalid command task
      */
-    public void validateArgs(int arg, Bot bot) throws InvalidTaskException {
-        if (arg < 0 || arg >= bot.getTaskList().size()) {
+    public void validateArgs(Integer ind, Bot bot) throws InvalidTaskException {
+        if (ind < 0 || ind >= bot.getTaskList().size()) {
             throw new InvalidTaskException(Ui.ERROR_SIGNATURE + "This task does not exist in the task list!");
         }
     }
