7b692850d742f63a922d91d9c8d6f4cccf424eab
mingyi456 <mingyi456@hotmail.com>
2021-09-09 23:21:51 +0800

    Add code quality improvements throughtout the codebase

--- src/main/java/duke/GUI.java
+++ src/main/java/duke/GUI.java
@@ -2,38 +2,28 @@ package duke;
 
 public class GUI {
 
-    private DataFile dataFile;
     private TaskList taskList;
-
-    private final String WELCOME_MESSAGE = " ____            _      \n"
-            + "|  _ \\ _   _  __| | ___ \n"
-            + "| | | | | | |/ _` |/ _ \\\n"
-            + "| |_| | |_| | (_| |  __/\n"
-            + "|____/ \\__,_|\\__,_|\\___|\n";
 
     public static void main(String[] args) {
         Launcher.main(args);
     }
 
     public GUI() {
-        dataFile = new DataFile("./duke_data.txt");
+        DataFile dataFile = new DataFile("./duke_data.txt");
         taskList = new TaskList(dataFile);
     }
 
     public String getResponse(String s) {
-
         return processInput(s);
     }
 
     String processInput(String str) {
-
         Task newTask = Parser.parseInput(str);
 
         if (newTask == null) {
             if (str.equals("bye")) {
                 taskList.save();
-                return "Bye. Hope to see you again soon!";
-
+                return "Tasklist saved; Exit command not implemented yet!";
             } else if (str.equals("list")) {
                 return taskList.list();
             } else if (str.startsWith("done")) {
@@ -55,8 +45,6 @@ public class GUI {
         }
 
     }
-
-
 
     
 }
