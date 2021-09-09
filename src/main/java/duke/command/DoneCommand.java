42c5baf8286b269443fd66b556f2680e635ac476
czhi-bin <iopwslw@gmail.com>
2021-09-09 19:49:47 +0800

    Fix bug where invalid task number will cause duke to crash

--- src/main/java/duke/command/DoneCommand.java
+++ src/main/java/duke/command/DoneCommand.java
@@ -2,6 +2,7 @@ package duke.command;
 
 import duke.exception.NoSuchTaskException;
 import duke.storage.Storage;
+import duke.task.Task;
 import duke.task.TaskList;
 import duke.ui.Ui;
 
@@ -36,7 +37,11 @@ public class DoneCommand extends DukeCommand {
      */
     @Override
     public String execute() throws NoSuchTaskException {
-        storage.save(list.getList());
-        return ui.markDoneMessage(list.mark(taskNumber));
+        try {
+            storage.save(list.getList());
+            return ui.markDoneMessage(list.mark(taskNumber));
+        } catch (IndexOutOfBoundsException e) {
+            throw new NoSuchTaskException();
+        }
     }
 }
