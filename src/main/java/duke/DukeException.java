3a9ebac7137f4419ff8a74652f7c139995644291
MGRL2201 <e0638854@u.nus.edu>
2021-09-09 15:12:10 +0800

    Code quality is not great.
    The code needs to be more readable and abstract.
    
    Further abstracting code has made the code easier to read and follow
    along.
    
    Abstracting shortens method length and good naming of abstracted methods
    allow for more readable code.

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -11,37 +11,37 @@ public class DukeException extends RuntimeException {
         EmptyDelete {
             @Override
             public String getMessage() {
-                return "(ㆆ_ㆆ) OOPS!!! You haven't specified the task you want to delete.";
+                return "(O_O) OOPS!!! You haven't specified the task you want to delete.";
             }
         },
         InvalidTaskNumber {
             @Override
             public String getMessage() {
-                return "(ㆆ_ㆆ) OOPS!!! The task number you have specified is invalid.";
+                return "(O_O) OOPS!!! The task number you have specified is invalid.";
             }
         },
         EmptyTodo {
             @Override
             public String getMessage() {
-                return "(ㆆ_ㆆ) OOPS!!! The description of a todo cannot be empty.";
+                return "(O_O) OOPS!!! The description of a todo cannot be empty.";
             }
         },
         EmptyEvent {
             @Override
             public String getMessage() {
-                return "(ㆆ_ㆆ) OOPS!!! The description of an event cannot be empty.";
+                return "(O_O) OOPS!!! The description of an event cannot be empty.";
             }
         },
         EmptyDeadline {
             @Override
             public String getMessage() {
-                return "(ㆆ_ㆆ) OOPS!!! The description of a deadline cannot be empty.";
+                return "(O_O) OOPS!!! The description of a deadline cannot be empty.";
             }
         },
         InvalidCommand {
             @Override
             public String getMessage() {
-                return "(ㆆ_ㆆ) OOPS!!! I'm sorry, but I don't know what that means :-(";
+                return "(O_O) OOPS!!! I'm sorry, but I don't know what that means :-(";
             }
         };
 
