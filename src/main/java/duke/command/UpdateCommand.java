350cb6726be4ddc5dc9328df050337206e8ec087
Win Lynn <ngaiwinlynn0323@gmail.com>
2021-09-09 22:30:27 +0800

    Task.createTaskFromInput: throw different types of exceptions.
    The method currently throws the same exception for having no
    description and no date /time.
    
    This may cause users to be confused as to what is an accepted input.
    
    Throwing other exceptions with specific error messages will inform the
    user on how to use Duke properly.

--- src/main/java/duke/command/UpdateCommand.java
+++ src/main/java/duke/command/UpdateCommand.java
@@ -23,9 +23,9 @@ public class UpdateCommand extends Command {
         Matcher m = pattern.matcher(input);
 
         if (m.find()) {
-            int index = Integer.parseInt(m.group(1)) - 1;
+            int index = Integer.parseInt(m.group(1));
             Task task = Task.createTaskFromInput(m.group(2));
-            taskList.update(index, task);
+            taskList.update(index - 1, task);
             return ui.getUpdateMessage(index, task);
         } else {
             throw new UpdateException();
