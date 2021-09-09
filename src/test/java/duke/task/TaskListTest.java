8957054261fd04a99d42c7b55f17ed40ea27482a
david-eom <david.eom.hyo.shik@gmail.com>
2021-09-09 10:18:16 +0800

    Fix failed test cases

--- src/test/java/duke/task/TaskListTest.java
+++ src/test/java/duke/task/TaskListTest.java
@@ -13,13 +13,15 @@ public class TaskListTest {
     void testDelete() {
         TaskList taskList = new TaskList();
         Todo test1 = new Todo("test1");
-        Todo test2 = new Todo("test1");
+        Todo test2 = new Todo("test2");
         Todo test3 = new Todo("test3");
         try {
             taskList.add(test1);
             taskList.add(test2);
             taskList.add(test3);
-        } catch (DukeDuplicateTaskException ignored) { }
+        } catch (DukeDuplicateTaskException ignored) {
+            // no handling
+        }
         taskList.delete(2);
         assertEquals(test3, taskList.get(2), "`delete()` should remove the task from the list");
     }
@@ -29,10 +31,10 @@ public class TaskListTest {
         TaskList taskList = new TaskList();
         try {
             for (int i = 0; i < 10; i++) {
-                taskList.add(new Todo("test"));
+                taskList.add(new Todo("test" + i));
             }
         } catch (DukeDuplicateTaskException ignored) {
-
+            // no handling
         }
         assertEquals(10, taskList.getLength(), "`getLength()` should return the length of list.");
     }
@@ -44,7 +46,9 @@ public class TaskListTest {
         Todo todo = new Todo("todo");
         try {
             taskList.add(todo);
-        } catch (DukeDuplicateTaskException ignored) { }
+        } catch (DukeDuplicateTaskException ignored) {
+            // no handling
+        }
         assertEquals(todo, taskList.get(1), "`get()` should return the task inside the list.");
     }
 
