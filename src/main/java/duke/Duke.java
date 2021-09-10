5a4cb47eb83d8c78fbddb9b1284df19d3a3f1013
BenjaminTan99 <benjamin.tan.chin.han@gmail.com>
2021-09-10 17:25:57 +0800

    Minor fixes with regards to long methods

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -97,57 +97,14 @@ public class Duke extends Application {
      */
     @Override
     public void start(Stage stage) {
-        // Setting up required components
+        // Setting up stage and formatting window to look as expected
+        setUpStage(stage);
 
-        // Container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-
-        // Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(true);
-        stage.setMinHeight(700.0);
-        stage.setMinWidth(500.0);
-        stage.show();
-
-        mainLayout.setPrefSize(500.0, 700.0);
-
-        scrollPane.setPrefSize(415, 565);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(375.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
+        // Display welcome message upon starting Duke
         String openingMessage = Ui.welcomeMessage();
         dialogContainer.getChildren().add(DialogBox.getDukeDialog(openingMessage, DUKE));
 
-        // Add functionality to handle user input.
+        // Functionality to handle user input.
         sendButton.setOnMouseClicked((event) -> {
             dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
             userInput.clear();
@@ -158,12 +115,11 @@ public class Duke extends Application {
             userInput.clear();
         });
 
-        // Scroll down to the end every time dialogContainer's height changes.
+        // Allows scrolling to the end every time dialogContainer's height changes.
         dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
 
-        // Add functionality to handle user input.
+        // Functionality for handling user input.
         sendButton.setOnMouseClicked((event) -> handleUserInput());
-
         userInput.setOnAction((event) -> handleUserInput());
     }
 
@@ -191,5 +147,60 @@ public class Duke extends Application {
                 DialogBox.getDukeDialog(dukeText, DUKE)
         );
         userInput.clear();
+    }
+
+    /**
+     * Sets up a stage using JavaFX tools.
+     *
+     * @param stage the stage we are setting up and using for Duke.
+     */
+    private void setUpStage(Stage stage) {
+        // Container for the content of the chat to scroll.
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        scene = new Scene(mainLayout);
+
+        stage.setTitle("Duke");
+        stage.setResizable(true);
+        stage.setMinHeight(700.0);
+        stage.setMinWidth(500.0);
+        stage.show();
+
+        formatStage(mainLayout);
+    }
+
+    /**
+     * Formats the stage for better look and sizing.
+     *
+     * @param mainLayout the AnchorPane from the stage used in Duke.
+     */
+    private void formatStage(AnchorPane mainLayout) {
+        mainLayout.setPrefSize(500.0, 700.0);
+
+        scrollPane.setPrefSize(415, 565);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(375.0);
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
     }
 }
