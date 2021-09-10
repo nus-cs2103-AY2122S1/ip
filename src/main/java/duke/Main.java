bf01f17ba79b78e5f4df08a9926994ed85d19ed3
dt-td <77134471+dt-td@users.noreply.github.com>
2021-09-10 14:20:21 +0800

    Modified Ui

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -6,6 +6,7 @@ import duke.gui.MainWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -23,6 +24,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Duke Bot");
+            stage.getIcons().add(new Image("/images/Icon.png"));
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
