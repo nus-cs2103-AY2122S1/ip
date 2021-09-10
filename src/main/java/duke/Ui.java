2169270715c2e05b4c731415816976a8f84b40b5
weiquu <ztolenstar@hotmail.com>
2021-09-10 13:51:12 +0800

    Add Help function
    Duke has no help function to provide in-App guidance to users should
    they be unfamiliar with the application. This is an issue as there will
    be higher barriers to entry for new users.
    
    Let's add a help function that lets users see all the commands they can
    use within the application. This will significantly reduce the barriers
    of entry for new users while simulataneously giving more experienced
    users the opportunity to remind themselves of the commands and format of
    the commands that they can use.

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -17,4 +17,22 @@ public class Ui {
         stringToPrint += LINE;
         return stringToPrint;
     }
+
+    public static String getHelpMessage() {
+        String stringToPrint = "";
+        stringToPrint += LINE;
+        stringToPrint += (INDENTATION + "Here are a list of commands you can use:\n");
+        stringToPrint += (INDENTATION + "1. list:" + INDENTATION
+                + "shows a list of all the tasks you currently have\n");
+        stringToPrint += (INDENTATION + "2. todo task_name:" + INDENTATION
+                + "creates a new ToDo task with the name given\n");
+        stringToPrint += (INDENTATION + "3. event task_name /at date_time:" + INDENTATION
+                + "creates a new Event task with the name and datetime given\n");
+        stringToPrint += (INDENTATION + "4. deadline task_name /by date_time:" + INDENTATION
+                + "creates a new Deadline task with the name and datetime given\n");
+        stringToPrint += (INDENTATION + "5. bye:" + INDENTATION
+                + "exits the program\n");
+        stringToPrint += LINE;
+        return stringToPrint;
+    }
 }
