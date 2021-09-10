e6d2d69f92235e87afbda0b694f601f4b19bbb50
clementkfj <clement.kfj@gmail.com>
2021-09-10 14:37:55 +0800

    Fix Parser and improve code quality
    Parser currently is buggy with the new introduction of friendlier syntax.
    
    Code needs to be modified so user input will not cause any bugs and will
    be accurate.
    
    Abstracted methods out of main parse method and changed methods that used
    substring to split.
    
    This improves code readability as well as cause it to be less buggy as user
    input is not used separated by a fixed number but by splitting by words.
    
    This commit helps improve SLAP in this program as well as arrow headed style of coding.

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -32,70 +32,176 @@ public class Parser {
         } else if (str.equals("list") || str.equals("l")) {
             return new ListCommand();
         } else if (splitBySpace[0].equals("done")) {
-            if (splitBySpace.length == 1) {
-                throw new DukeException("Sorry please indicate a task number to update!");
-            }
-            assert splitBySpace.length > 1 : "No task indicated.";
-            int doneNumber = Integer.parseInt(str.substring(5));
+            int doneNumber = handleDoneCommand(splitBySpace);
             return new DoneCommand(doneNumber);
         } else if (splitBySpace[0].equals("deadline") || splitBySpace[0].equals("d")) {
-            if (!str.contains("/")) {
-                throw new DukeException("Sorry please indicate your deadline time with a '/by' after your "
-                        + "deadline title!");
-            }
-            int endDescription = str.indexOf("/");
-            if (endDescription <= 9) {
-                throw new DukeException("Oh no! Deadlines cannot be empty! Please try again");
-            }
-            assert str.substring(9).length() > 0 : "No deadlines given";
-            String description = str.substring(9, endDescription - 1);
-            if (description.equals("") || description.equals(" ")) {
-                throw new DukeException("Oh no! Deadlines cannot be empty! Please try again");
-            }
-            String endTime = str.substring(endDescription + 4);
-            return new DeadlineAddCommand(description, endTime);
+            String title = handleTitleChecking(str);
+            String endTime = findTime(str, "D");
+            return new DeadlineAddCommand(title, endTime);
         } else if (splitBySpace[0].equals("event") || splitBySpace[0].equals("e")) {
-            if (!str.contains("/")) {
-                throw new DukeException("Sorry please indicate a time your event begins with a '/on' after"
-                        + " your event title!");
-            }
-            int endDescription = str.indexOf("/");
-            if (endDescription < 6) {
-                throw new DukeException("Oh no! Events cannot be empty! Please try again");
-            }
-            String description = str.substring(6, endDescription - 1);
-            if (description.equals("") || description.equals(" ")) {
-                throw new DukeException("Oh no! Events cannot be empty! Please try again");
-            }
-            String startTime = str.substring(endDescription + 4);
-            return new EventAddCommand(description, startTime);
+            String title = handleTitleChecking(str);
+            String startTime = findTime(str, "E");
+            return new EventAddCommand(title, startTime);
         } else if (splitBySpace[0].equals("todo") || splitBySpace[0].equals("t")) {
-            if (str.length() < 5) {
-                throw new DukeException("Oh no! ToDos cannot be empty! Please try again");
-            }
-            assert splitBySpace.length > 1 : "No todo specified";
-            String description = str.substring(5);
-            if (description.equals("") || description.equals(" ")) {
-                throw new DukeException("Oh no! ToDos cannot be empty! Please try again");
-            }
+            String description = handleToDoAddCommand(str, splitBySpace);
             return new TodoAddCommand(description);
         } else if (splitBySpace[0].equals("delete") || splitBySpace[0].equals("del")) {
-            if (splitBySpace.length == 1) {
-                throw new DukeException("Sorry please indicate a task number to delete!");
-            }
-            assert splitBySpace.length > 1 : "No task specified to delete";
-            int deleteNumber = Integer.parseInt(str.substring(7));
+            int deleteNumber = handleDeleteCommand(splitBySpace);
             return new DeleteCommand(deleteNumber);
         } else if (splitBySpace[0].equals("help") || splitBySpace[0].equals("h")) {
             return new HelpCommand();
         } else if (splitBySpace[0].equals("find") || splitBySpace[0].equals("f")) {
-            if (splitBySpace.length == 1) {
-                throw new DukeException("Sorry please indicate a keyword to find!");
-            }
-            assert str.split("\\s+", 2).length == 2 : "No task specified to find";
-            return new FindCommand(str.split("\\s+", 2)[1]);
+            String keyword = handleFindCommand(str, splitBySpace);
+            return new FindCommand(keyword);
         }
         throw new DukeException("I'm sorry :( I don't quite seem to understand, try again pls!");
+    }
+
+    /**
+     * Finds the string that contains the information of the time of event or deadline depending on the type.
+     *
+     * @param str The String containing the user input.
+     * @param type The type of input they gave(e.g. "D" for deadline and "E" for event).
+     * @return The string containing only the information of time.
+     * @throws DukeException Thrown when there is a wrong user input.
+     */
+    private static String findTime(String str, String type) throws DukeException {
+        try {
+            String time;
+            if (type.equals("E")) {
+                time = str.split("/on ", 2)[1];
+            } else {
+                assert type.equals("D") : "Type error";
+                time = str.split("/by ", 2)[1];
+            }
+            return time;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException("Sorry please indicate your deadline time with a '/by' after your "
+                    + "deadline title or a '/on' after your event title");
+        }
+    }
+
+    /**
+     * Checks whether the title of the task given by the user is correct.
+     *
+     * @param str The String containing the user input, which contains both the title and the time.
+     * @return The String with only the title.
+     * @throws DukeException Thrown when titles are empty.
+     */
+    private static String handleTitleChecking(String str) throws DukeException {
+        try {
+            checkSlashFormatting(str);
+            String title = str.split("/")[0].split("\\s+", 2)[1];
+            checkDescriptionLength(title);
+            return title;
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("Oh no! Titles cannot be empty! Please try again");
+        }
+    }
+
+    /**
+     * Handles the steps taken before returning the FindCommand Command in parse.
+     *
+     * @param str The String containing the user input.
+     * @param splitBySpace The String array that was divided by space.
+     * @return A String containing just the keywords to find.
+     * @throws DukeException Throws An exception when user input is invalid.
+     */
+    private static String handleFindCommand(String str, String[] splitBySpace) throws DukeException {
+        checkTaskLength(splitBySpace);
+        assert str.split("\\s+", 2).length == 2 : "No task specified to find";
+        return str.split("\\s+", 2)[1];
+    }
+
+    /**
+     * Handles the steps taken before DeleteCommand is called in parse.
+     *
+     * @param splitBySpace The String array split by whitespaces from the user input.
+     * @return The task number the user wants to delete.
+     * @throws DukeException Thrown when the user input is invalid.
+     */
+    private static int handleDeleteCommand(String[] splitBySpace) throws DukeException {
+        checkTaskLength(splitBySpace);
+        assert splitBySpace.length > 1 : "No task specified to delete";
+        return Integer.parseInt(splitBySpace[1]);
+    }
+
+    /**
+     * Handles the steps before a DoneCommand Command is called in parse.
+     *
+     * @param splitBySpace The String array split by whitespaces from the user input.
+     * @return The task number the user wants to mark as done.
+     * @throws DukeException Thrown when user input is invalid.
+     */
+    private static int handleDoneCommand(String[] splitBySpace) throws DukeException {
+        checkTaskLength(splitBySpace);
+        assert splitBySpace.length > 1 : "No task indicated.";
+        return Integer.parseInt(splitBySpace[1]);
+    }
+
+    /**
+     * Handles all steps to create a ToDoAddCommand, including checking for violations in user input.
+     *
+     * @param str The string input of the user.
+     * @param splitBySpace The String array that was split by spaces in the original parse().
+     * @return The String that contains the Description to be added to ToDoAddCommand.
+     * @throws DukeException Thrown when there is a user input violation.
+     */
+    private static String handleToDoAddCommand(String str, String[] splitBySpace) throws DukeException {
+        checkTaskLength(splitBySpace);
+        assert splitBySpace.length > 1 : "No todo specified";
+        String description = str.split("\\s+", 2)[1];
+        checkDescriptionEmpty(description);
+        return description;
+    }
+
+    /**
+     * Checks whether the description is empty.
+     *
+     * @param description The description to be checked whether it is empty.
+     * @throws DukeException Thrown when the description is empty.
+     */
+    private static void checkDescriptionEmpty(String description) throws DukeException {
+        if (description.equals("") || description.equals(" ")) {
+            throw new DukeException("Oh no! Titles cannot be empty! Please try again");
+        }
+    }
+
+    /**
+     * Checks whether the description length is too short.
+     *
+     * @param title The title of the item to be added.
+     * @throws DukeException Thrown when titles are seen to be empty.
+     */
+    private static void checkDescriptionLength(String title) throws DukeException {
+        if (title.length() <= 0) {
+            throw new DukeException("Oh no! Titles cannot be empty! Please try again");
+        }
+    }
+
+    /**
+     * Checks for the presence of the '/' in the String.
+     *
+     * @param str The String that contains the user input.
+     * @throws DukeException Thrown when there is no presence of the '/'.
+     */
+    private static void checkSlashFormatting(String str) throws DukeException {
+        if (!str.contains("/")) {
+            throw new DukeException("Sorry please indicate your deadline time with a '/by' after your "
+                    + "deadline title or a '/on' after your event title");
+        }
+    }
+
+    /**
+     * Checks whether the task length is sufficient for certain commands such as delete, done and find.
+     *
+     * @param splitBySpace The String array that was split by spacings.
+     * @throws DukeException Thrown when there is nothing for the command to do.
+     */
+    private static void checkTaskLength(String[] splitBySpace) throws DukeException {
+        if (splitBySpace.length == 1) {
+            throw new DukeException("Sorry please indicate a task number to update!");
+        }
     }
 
     @Override
