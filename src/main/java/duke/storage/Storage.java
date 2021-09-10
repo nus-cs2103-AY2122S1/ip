1ea1dc4af44ae66461bd299baf6401f9ccd900fd
Chen Hsiao Ting <hsiaoting0919@gmail.com>
2021-09-10 20:55:20 +0800

    Using Stream: use Streams feature of Java in code.
    Storage of data is only done after user enter bye and exit the chat bot.
    
    Changes made to update storage file immediately after user enter a valid command.
    This saves the data even if user quit the program without entering 'bye'.
    
    Let's use the streams feature in storage class to store the data with short and concise codes.

--- src/main/java/duke/storage/Storage.java
+++ src/main/java/duke/storage/Storage.java
@@ -11,11 +11,17 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Represents a storage that stores the task data locally.
@@ -119,6 +125,68 @@ public class Storage {
 
             fw.close();
         } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Adds a new line of task into the file.
+     *
+     * @param t Task to be stored.
+     */
+    public void addTask(Task t) {
+        try {
+            File file = new File(PATH);
+            String str = t.getType() + ", " + t.isDone() + ", " + t.getDescription();
+            //@@author hsiaotingluv-reused
+            //Reused from https://stackoverflow.com/a/37674446 with minor modifications
+            Files.write(file.toPath(), str.getBytes(), StandardOpenOption.APPEND);
+            //@@author
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Deletes a line of task from the file.
+     *
+     * @param t Task to be deleted.
+     */
+    public void deleteTask(Task t) {
+        try {
+            File file = new File(PATH);
+            String str = t.getType() + ", " + t.isDone() + ", " + t.getDescription();
+            //@@author hsiaotingluv-reused
+            //Reused from https://stackoverflow.com/a/45784174 with minor modifications
+            List<String> out = Files.lines(file.toPath())
+                    .filter(line -> !line.contains(str))
+                    .collect(Collectors.toList());
+            Files.write(file.toPath(), out, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+            //@@author
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Updates status of a task in the file.
+     *
+     * @param t Task to be updated.
+     */
+    public void updateTask(Task t) {
+        Path path = Paths.get(PATH);
+        String original = t.getType() + ", false, " + t.getDescription();
+        String newLine = t.getType() + ", true, " + t.getDescription();
+        //@@author hsiaotingluv-reused
+        //Reused from https://stackoverflow.com/a/67385356 with minor modifications
+        try (Stream<String> stream = Files.lines(path, StandardCharsets.UTF_8)) {
+            // Do the line replace
+            List<String> list = stream.map(line -> line.equals(original) ? newLine : line)
+                    .collect(Collectors.toList());
+            // Write the content back
+            Files.write(path, list, StandardCharsets.UTF_8);
+            //@@author
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
