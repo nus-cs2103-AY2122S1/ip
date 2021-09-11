2298881e7684404f69dc7a47bac36b220be5a72c
Eunice <eunicesimsingyee@gmail.com>
2021-09-11 21:41:34 +0800

    Clip the Images to have fit in a circular border and display Duke's full response

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 /**
  * An example of a custom control using FXML.
@@ -25,6 +26,11 @@ public class DialogBox extends HBox {
     @FXML
     private ImageView displayPicture;
 
+    // sets the minimum height of Dialog Panel
+    private final int Min_Height;
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+
+
     private DialogBox(String text, Image img) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
@@ -35,8 +41,15 @@ public class DialogBox extends HBox {
             e.printStackTrace();
         }
 
+        // Min_Height of DialogBox is the number of lines that Duke's response will return to the User and
+        // multiplied by the height for each line (18)
+        Min_Height = text.split("\n").length * 18;
+
+        dialog.setMinHeight(Min_Height);
         dialog.setText(text);
+
         displayPicture.setImage(img);
+        displayPicture.setClip(new Circle(49.5, 49.5, 49.5));
     }
 
     /**
