a9091d298cb41067802cee396d8a7e2ee9c2771d
Lemonsr <46766154+Lemonsr@users.noreply.github.com>
2021-09-11 19:11:25 +0800

    Fix parser uncaught exception bug

--- src/main/java/aisu/parser/Parser.java
+++ src/main/java/aisu/parser/Parser.java
@@ -31,41 +31,32 @@ public class Parser {
     public static Command parse(String input) throws AisuException {
         assert input.length() >= 1 : "There needs to be text entered by the user.";
 
-        if (input.equals("bye")) {
-            return new ExitCommand();
-
-        } else if (input.equals("help")) {
-            return new HelpCommand();
-
-        } else if (input.equals("list")) {
-            return new ShowListCommand();
-
-        } else if (input.startsWith("done ")) {
-            return new MarkDoneCommand(Integer.parseInt(input.substring(5)));
-
-        } else if (input.startsWith("todo ")) {
-            return new AddCommand(TaskList.TaskTypes.T, input.substring(5));
-
-        } else if (input.startsWith("deadline ")) {
-            return new AddCommand(TaskList.TaskTypes.D, input.substring(9));
-
-        } else if (input.startsWith("event ")) {
-            return new AddCommand(TaskList.TaskTypes.E, input.substring(6));
-
-        } else if (input.startsWith("delete ")) {
-            return new DeleteCommand(Integer.parseInt(input.substring(7)));
-
-        } else if (input.startsWith("find ")) {
-            return new FindCommand(input.substring(5));
-
-        } else if (input.startsWith("tag ")) {
-            try {
+        try {
+            if (input.equals("bye")) {
+                return new ExitCommand();
+            } else if (input.equals("help")) {
+                return new HelpCommand();
+            } else if (input.equals("list")) {
+                return new ShowListCommand();
+            } else if (input.startsWith("done ")) {
+                return new MarkDoneCommand(Integer.parseInt(input.substring(5)));
+            } else if (input.startsWith("todo ")) {
+                return new AddCommand(TaskList.TaskTypes.T, input.substring(5));
+            } else if (input.startsWith("deadline ")) {
+                return new AddCommand(TaskList.TaskTypes.D, input.substring(9));
+            } else if (input.startsWith("event ")) {
+                return new AddCommand(TaskList.TaskTypes.E, input.substring(6));
+            } else if (input.startsWith("delete ")) {
+                return new DeleteCommand(Integer.parseInt(input.substring(7)));
+            } else if (input.startsWith("find ")) {
+                return new FindCommand(input.substring(5));
+            } else if (input.startsWith("tag ")) {
                 String[] parsedData = input.substring(4).split(" /with ");
                 return new TagCommand(Integer.parseInt(parsedData[0].strip()), parsedData[1].strip());
-            } catch (PatternSyntaxException | ArrayIndexOutOfBoundsException | NumberFormatException e) {
+            } else {
                 throw new AisuException("That's an invalid task format...");
             }
-        } else {
+        } catch (PatternSyntaxException | ArrayIndexOutOfBoundsException | NumberFormatException e) {
             throw new AisuException("That's an invalid task format...");
         }
     }
