bea7892a6cf4eecd2af1802a78122fc3dde897b9
jodichoo <jodi.choo@gmail.com>
2021-09-11 18:17:39 +0800

    Edit style of dialog boxes

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -5,11 +5,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 /**
@@ -22,10 +30,17 @@ public class DialogBox extends HBox {
     private Text dialog;
     @FXML
     private ImageView displayPicture;
+    
+    private static final Background USER_BACKGROUND = new Background(
+            new BackgroundFill(Color.GAINSBORO, CornerRadii.EMPTY, Insets.EMPTY));
+    private static final Font USER_FONT = Font.font("Arial", FontWeight.BOLD, 14);
+    private static final Font DUKE_FONT = new Font("Arial", 14);
+
 
     private DialogBox(String text, Image img) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(
+                    MainWindow.class.getResource("/view/DialogBox.fxml"));
             fxmlLoader.setController(this);
             fxmlLoader.setRoot(this);
             fxmlLoader.load();
@@ -35,6 +50,20 @@ public class DialogBox extends HBox {
 
         dialog.setText(text);
         displayPicture.setImage(img);
+        cropDisplayPicture(img);
+    }
+    
+    private void cropDisplayPicture(Image img) {
+        boolean hasSmallerWidth = img.getWidth() < img.getHeight();
+        double newMeasure = hasSmallerWidth ? img.getWidth() : img.getHeight();
+        double x = (img.getWidth() - newMeasure) / 2;
+        double y = (img.getHeight() - newMeasure) / 2;
+        
+        Rectangle2D rect = new Rectangle2D(x, y, newMeasure, newMeasure);
+        displayPicture.setViewport(rect);
+        displayPicture.setFitWidth(99);
+        displayPicture.setFitHeight(99);
+        displayPicture.setSmooth(true);
     }
 
     /**
@@ -47,6 +76,14 @@ public class DialogBox extends HBox {
         setAlignment(Pos.TOP_LEFT);
     }
 
+    public void setUserStyle() {
+        setBackground(USER_BACKGROUND);
+        dialog.setFont(USER_FONT);
+    }
+    
+    public void setDukeStyle() {
+        dialog.setFont(DUKE_FONT);
+    }
     /**
      * Formats the user input into a dialog box.
      *
@@ -55,7 +92,9 @@ public class DialogBox extends HBox {
      * @return DialogBox containing the user input.
      */
     public static DialogBox getUserDialog(String text, Image img) {
-        return new DialogBox(text, img);
+        var db = new DialogBox(text, img);
+        db.setUserStyle();
+        return db;
     }
 
     /**
@@ -67,6 +106,7 @@ public class DialogBox extends HBox {
      */
     public static DialogBox getDukeDialog(String text, Image img) {
         var db = new DialogBox(text, img);
+        db.setDukeStyle();
         db.flip();
         return db;
     }
