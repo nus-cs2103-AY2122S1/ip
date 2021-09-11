b73d643a8fd12f3c944d7654ff714f72b2467ddd
KengXIII <kengi0813@gmail.com>
2021-09-11 16:14:57 +0800

    Change the task type from String to enum

--- src/main/java/command/AddCommand.java
+++ src/main/java/command/AddCommand.java
@@ -13,25 +13,43 @@ import ui.Ui;
 public class AddCommand extends Command {
 
     private final boolean EXIT = false;
-    private String taskType;
+    private TaskType taskType;
     private String taskInfo;
+    private enum TaskType {
+        TODO, DEADLINE, EVENT
+    }
 
-    private void checkDescription(String description, String type) throws NullDescription {
-        int numOfCharacters =  type.length();
-        if (description.length() <= numOfCharacters || description.split(" ").length < 2) {
-            throw new NullDescription(type);
+    private void checkDescription(String description, TaskType type) throws NullDescription {
+        switch (type) {
+        case TODO:
+            if (description.length() <= 4 || description.split(" ").length < 2) {
+                throw new NullDescription("todo");
+            }
+            break;
+        case DEADLINE:
+            if (description.length() <= 8 || description.split(" ").length < 2) {
+                throw new NullDescription("deadline");
+            }
+            break;
+        case EVENT:
+            if (description.length() <= 5 || description.split(" ").length < 2) {
+                throw new NullDescription("event");
+            }
+            break;
+        default:
+            break;
         }
     }
 
-    private void verifyDate(String dateInfo, String type) throws InvalidDateFormat {
+    private void verifyDate(String dateInfo, TaskType type) throws InvalidDateFormat {
         switch(type) {
-        case "deadline":
+        case DEADLINE:
             if (dateInfo.split(" /by ").length != 2 ||
                     dateInfo.split(" /by ")[1].split(" ").length != 2) {
                 throw new InvalidDateFormat();
             };
             break;
-        case "event":
+        case EVENT:
             if (dateInfo.split(" /at ").length != 2 ||
                     dateInfo.split(" /at ")[1].split(" ").length != 2) {
                 throw new InvalidDateFormat();
@@ -39,7 +57,7 @@ public class AddCommand extends Command {
             break;
         default:
             break;
-    }
+        }
     }
 
     /**
@@ -51,25 +69,25 @@ public class AddCommand extends Command {
      * @throws InvalidDateFormat If the input does not follow the specified format YYYY-MM-DD HH:MM
      */
     public AddCommand(String input) throws NullDescription, InvalidDateFormat {
-        this.taskType = input.split(" ")[0];
-
         switch (input.split(" ")[0]) {
         case "todo":
-            checkDescription(input, "todo");
+            this.taskType = TaskType.TODO;
+            checkDescription(input, TaskType.TODO);
             this.taskInfo = input.substring(5);
             break;
         case "deadline":
-            checkDescription(input, "deadline");
+            this.taskType = TaskType.DEADLINE;
+            checkDescription(input, TaskType.DEADLINE);
             this.taskInfo = input.substring(9);
-            verifyDate(taskInfo, "deadline");
+            verifyDate(taskInfo, TaskType.DEADLINE);
             break;
         case "event":
-            checkDescription(input, "event");
+            this.taskType = TaskType.EVENT;
+            checkDescription(input, TaskType.EVENT);
             this.taskInfo = input.substring(6);
-            verifyDate(taskInfo, "event");
+            verifyDate(taskInfo, TaskType.EVENT);
             break;
         default:
-            System.out.println("Something went wrong...");
             break;
         }
     }
@@ -78,24 +96,18 @@ public class AddCommand extends Command {
         Task newTask = null;
 
         switch(taskType) {
-        case "todo":
-            newTask = new Todo(taskInfo, false);
+        case TODO:
+            newTask = createTask(TaskType.TODO);
             break;
-        case "deadline":
-            String deadlineDescription = taskInfo.split(" /by ")[0];
-            String deadlineDate = taskInfo.split(" /by ")[1];
-            newTask = new Deadline(deadlineDescription, deadlineDate, false);
+        case DEADLINE:
+            newTask = createTask(TaskType.DEADLINE);
             break;
-        case "event":
-            String eventDescription = taskInfo.split(" /at ")[0];
-            String eventDate = taskInfo.split(" /at ")[1];
-            newTask = new Event(eventDescription, eventDate, false);
+        case EVENT:
+            newTask = createTask(TaskType.EVENT);
             break;
         default:
-            System.out.println("Something went wrong...");
+            break;
         }
-
-
         tasks.addTask(newTask);
         storage.write(tasks.getTaskList());
         return String.format("Got it. I've added this task:\n"
@@ -103,7 +115,28 @@ public class AddCommand extends Command {
                         + "Now you have %d tasks in the list.\n", newTask, tasks.size());
     }
 
-
+    private Task createTask(TaskType type) throws InvalidDateFormat {
+        Task outputTask = null;
+        switch (type) {
+        case TODO:
+            outputTask = new Todo(taskInfo, false);
+            break;
+        case DEADLINE:
+            String deadlineDescription = taskInfo.split(" /by ")[0];
+            String deadlineDate = taskInfo.split(" /by ")[1];
+            outputTask = new Deadline(deadlineDescription, deadlineDate, false);
+            break;
+        case EVENT:
+            String eventDescription = taskInfo.split(" /at ")[0];
+            String eventDate = taskInfo.split(" /at ")[1];
+            outputTask = new Event(eventDescription, eventDate, false);
+            break;
+        default:
+            break;
+        }
+        assert outputTask != null : "Task type should have matched";
+        return outputTask;
+    }
     public boolean isExit() {
         return EXIT;
     }
