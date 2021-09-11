266a4509be610d1f9b8484463bb8422cf1c660b6
Eunice <eunicesimsingyee@gmail.com>
2021-09-12 00:27:29 +0800

    Improve layout of Dialog boxes

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -13,7 +13,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import jdk.jshell.Diag;
 
 /**
  * An example of a custom control using FXML.
@@ -42,10 +44,12 @@ public class DialogBox extends HBox {
 
         // Min_Height of DialogBox is the number of lines that Duke's response will return to the User and
         // multiplied by the height for each line (18)
-        Min_Height = text.split("\n").length * 18;
+        Min_Height = (text.split("\n").length + 1) * 20;
 
         dialog.setMinHeight(Min_Height);
         dialog.setText(text);
+        dialog.setStyle("-fx-border-color: #897caa; -fx-border-image-insets: 5; -fx-border-radius: 8; " +
+                "-fx-border-width: 2; -fx-label-padding: 7");
 
         displayPicture.setImage(img);
         displayPicture.setClip(new Circle(49.5, 49.5, 49.5));
--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
-            scene.getRoot().setStyle("-fx-font: 14 serif;");
+            scene.getRoot().setStyle("-fx-font: 16 Tahoma;");
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
