84838c8305b8522590489e7ad0ef1eeba0ab4a53
auhc99 <auhc99@gmail.com>
2021-09-11 21:56:44 +0800

    Add User Guide

--- src/main/java/duke/command/HelpCommand.java
+++ src/main/java/duke/command/HelpCommand.java
@@ -4,15 +4,15 @@ import duke.DukeList;
 
 public class HelpCommand extends DukeCommand {
     private static final String HELP_MESSAGE = "Here are the available commands:\n"
+            + "- Use \"help\" to display the list of commands.\n"
             + "- Use \"list\" to display the list of tasks.\n"
             + "- Use \"todo {description}\" to add a todo with a description.\n"
-            + "- Use \"event {description} /at {dateTime}\" to add an event with description and dateTime.\n"
-            + "- Use \"dateline {description} /by {dateTime}\" to add a deadline with description and dateTime.\n"
-            + "- DateTime must be in the format \"d/M/yyyy HHmm\".\n"
+            + "- Use \"event {description} /at {d/M/yyyy HHmm}\" to add an event with description and dateTime.\n"
+            + "- Use \"dateline {description} /by {d/M/yyyy HHmm}\" to add a deadline with description and dateTime.\n"
             + "- Use \"done {n}\" to mark task number n done.\n"
             + "- Use \"delete {n}\" to delete task number n.\n"
             + "- Use \"find {keyWord}\" to find tasks with matching keywords.\n"
-            + "- Use \"help\" to display the list of commands.\n";
+            + "- Use \"bye\" to exit the program.\n";
 
     public HelpCommand() {
     }
