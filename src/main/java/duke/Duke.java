8777bb9438aab0389531b60a4b05693e322a3115
houtenteo <houtenteo@gmail.com>
2021-09-12 13:04:12 +0800

    Remove unused import statements

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -7,12 +7,8 @@ import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 
