d255b4d2500ddfa659ae9aaae138ffd3871a1575
sj1999-BIT <e0543446@u.nus.edu.sg>
2021-09-12 10:54:46 +0800

    Improve code quality
    Improved code quality
    1. changed multiple if-elseif statement to switch
    2. Added new function comments
    Remove a bug that causes program to print logo instead of parsing the input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,57 @@
+<<<<<<< Updated upstream
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
+=======
+import java.util.Scanner;
+import Storage.*;
+import Task.TaskList;
+import Task.Task;
+import Task.DeadlineException;
+import Task.TodoException;
+import Task.EventsException;
+
+public class Duke{
+    private final Ui userInterface = new Ui();
+
+    public static void main(String[] args) {
+        Duke user = new Duke();
+        boolean end = true;
+        user.userInterface.greet();
+        user.userInterface.getDataInputList();
+        while (end) {
+            end = user.userInterface.echo();
+            System.out.println("__________________________________");
+        }
+    }
+
+    public String getResponse(String input) {
+            return userInterface.choiceOfAction(input);
+    }
+
+    /**
+     * Function returns the logo of the program.
+     * @return string containing the logo.
+     */
+    public final static String greet() {
+        return Ui.greet();
+    }
+}
+
+
+//deals with interactions with the user
+class Ui {
+    private final Storage store = new Storage();
+    private TaskList taskList = new TaskList();
+
+    /**
+     * Function returns the logo of the program.
+     * @return string containing the logo.
+     */
+    public final static String greet() {
+        String logo = "__________________________________\n"
+                +" ____        _        \n"
+>>>>>>> Stashed changes
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
