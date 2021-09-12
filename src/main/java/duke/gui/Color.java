7d0a7fb7bcd00acb00649ae5f10d5f8dccdcf998
Adam Oh Zhi Hong <adamohzhihong@gmail.com>
2021-09-12 16:04:54 +0800

    Add Javadoc for Color.java

--- src/main/java/duke/gui/Color.java
+++ src/main/java/duke/gui/Color.java
@@ -1,5 +1,8 @@
 package duke.gui;
 
+/**
+ * A class to store hex codes for colours used in Duke.
+ */
 public class Color {
     public static final String TEXT_EXCEPTION = "d9004c";
     public static final String TEXT_DEFAULT = "000000";
