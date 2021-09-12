94fa23edf5b21035f92eefe9dd2f5aadb4cbe6bb
limzk26 <dylanlim12698@gmail.com>
2021-09-12 10:41:15 +0800

    Refactor code with Streams

--- src/main/java/duke/misc/Parser.java
+++ src/main/java/duke/misc/Parser.java
@@ -1,6 +1,7 @@
 package duke.misc;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import duke.exception.DukeException;
 import duke.exception.InvalidCommandException;
@@ -31,33 +32,31 @@ public class Parser {
         if (idx >= 0) {
             commandType = input.substring(0, idx).trim();
             args = input.substring(idx + 1).split("/");
-            for (int i = 0; i < args.length; i++) {
-                args[i] = args[i].trim();
-            }
+            Arrays.stream(args).forEach(String::trim);
         }
-        switch(commandType) {
-        case "todo":
-            return new Todo(args[0]);
-        case "event":
-            if (!input.matches("event [\\s\\S]+/[\\s\\S]+")) {
-                throw new InvalidFormatException();
-            }
-            if (DateTime.isInvalidDate(args[1])) {
-                throw new InvalidDateException();
-            }
-            assert args.length == 2;
-            return new Event(args[0], args[1]);
-        case "deadline":
-            if (!input.matches("deadline [\\s\\S]+/[\\s\\S]+")) {
-                throw new InvalidFormatException();
-            }
-            if (DateTime.isInvalidDate(args[1])) {
-                throw new InvalidDateException();
-            }
-            assert args.length == 2;
-            return new Deadline(args[0], args[1]);
-        default:
-            throw new InvalidCommandException();
+        switch (commandType) {
+            case "todo":
+                return new Todo(args[0]);
+            case "event":
+                if (!input.matches("event [\\s\\S]+/[\\s\\S]+")) {
+                    throw new InvalidFormatException();
+                }
+                if (DateTime.isInvalidDate(args[1])) {
+                    throw new InvalidDateException();
+                }
+                assert args.length == 2;
+                return new Event(args[0], args[1]);
+            case "deadline":
+                if (!input.matches("deadline [\\s\\S]+/[\\s\\S]+")) {
+                    throw new InvalidFormatException();
+                }
+                if (DateTime.isInvalidDate(args[1])) {
+                    throw new InvalidDateException();
+                }
+                assert args.length == 2;
+                return new Deadline(args[0], args[1]);
+            default:
+                throw new InvalidCommandException();
         }
     }
 
@@ -79,39 +78,39 @@ public class Parser {
             suffix = input.substring(idx + 1);
         }
         switch (prefix) {
-        case "bye":
-            if (!suffix.isEmpty()) {
-                throw new InvalidCommandException();
-            }
-            tl.saveData();
-            return Ui.GOODBYE_MSG;
-        case "list":
-            if (!suffix.isEmpty()) {
-                throw new InvalidCommandException();
-            }
-            return Ui.LIST_MSG + tl.displayList();
-        case "done":
-            assert !suffix.equals("");
-            try {
-                int taskIdx = Integer.parseInt(suffix);
-                return Ui.DONE_MSG + tl.completeTask(taskIdx);
-            } catch (NumberFormatException e) {
-                throw new InvalidIndexException();
-            }
-        case "delete":
-            assert !suffix.equals("");
-            try {
-                int taskIdx = Integer.parseInt(suffix);
-                return Ui.DELETE_MSG + tl.deleteTask(taskIdx);
-            } catch (NumberFormatException e) {
-                throw new InvalidIndexException();
-            }
-        case "find":
-            assert !suffix.equals("");
-            return Ui.FIND_MSG + tl.findTasks(suffix);
-        default:
-            Task task = makeTask(input);
-            return Ui.ADD_MSG + tl.addTask(task);
+            case "bye":
+                if (!suffix.isEmpty()) {
+                    throw new InvalidCommandException();
+                }
+                tl.saveData();
+                return Ui.GOODBYE_MSG;
+            case "list":
+                if (!suffix.isEmpty()) {
+                    throw new InvalidCommandException();
+                }
+                return Ui.LIST_MSG + tl.displayList();
+            case "done":
+                assert !suffix.equals("");
+                try {
+                    int taskIdx = Integer.parseInt(suffix);
+                    return Ui.DONE_MSG + tl.completeTask(taskIdx);
+                } catch (NumberFormatException e) {
+                    throw new InvalidIndexException();
+                }
+            case "delete":
+                assert !suffix.equals("");
+                try {
+                    int taskIdx = Integer.parseInt(suffix);
+                    return Ui.DELETE_MSG + tl.deleteTask(taskIdx);
+                } catch (NumberFormatException e) {
+                    throw new InvalidIndexException();
+                }
+            case "find":
+                assert !suffix.equals("");
+                return Ui.FIND_MSG + tl.findTasks(suffix);
+            default:
+                Task task = makeTask(input);
+                return Ui.ADD_MSG + tl.addTask(task);
         }
     }
 }
--- src/main/java/duke/misc/Storage.java
+++ src/main/java/duke/misc/Storage.java
@@ -8,6 +8,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import duke.task.Deadline;
 import duke.task.Event;
@@ -41,21 +42,21 @@ public class Storage {
             String[] args = data.split(" // ");
             Boolean isTaskDone = Integer.parseInt(args[1]) != 0;
             switch(args[0]) {
-            case "Todo":
-                assert args.length == 3;
-                al.add(new Todo(args[2], isTaskDone));
-                break;
-            case "Event":
-                assert args.length == 4;
-                al.add(new Event(args[2], args[3], isTaskDone));
-                break;
-            case "Deadline":
-                assert args.length == 4;
-                al.add(new Deadline(args[2], args[3], isTaskDone));
-                break;
-            default:
-                assert false : "Wrote data wrongly";
-                break;
+                case "Todo":
+                    assert args.length == 3;
+                    al.add(new Todo(args[2], isTaskDone));
+                    break;
+                case "Event":
+                    assert args.length == 4;
+                    al.add(new Event(args[2], args[3], isTaskDone));
+                    break;
+                case "Deadline":
+                    assert args.length == 4;
+                    al.add(new Deadline(args[2], args[3], isTaskDone));
+                    break;
+                default:
+                    assert false : "Wrote data wrongly";
+                    break;
             }
         }
         return al;
@@ -74,10 +75,10 @@ public class Storage {
             parentDir.mkdir();
         }
         FileWriter fw = new FileWriter(filePath.toFile());
-        for (Task task : al) {
-            String taskData = task.getData() + "\n";
-            fw.write(taskData);
-        }
+        String data = al.stream()
+                .map(Task::getData)
+                .collect(Collectors.joining("\n"));
+        fw.write(data);
         fw.close();
     }
 }
