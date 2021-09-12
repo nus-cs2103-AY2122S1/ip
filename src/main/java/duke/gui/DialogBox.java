68897ef69473919284799cac16e619d3ea020b9a
Frederick Pek <ssixthlhm@gmail.com>
2021-09-12 14:01:11 +0800

    Change user dialog colour

--- src/main/java/duke/gui/DialogBox.java
+++ src/main/java/duke/gui/DialogBox.java
@@ -56,6 +56,11 @@ public class DialogBox extends HBox {
     public static HBox getUserDialog(String text, Image img) {
         HBox db = DialogBox.getDialog(text, img);
         ObservableList<Node> tmp = FXCollections.observableArrayList(db.getChildren());
+        tmp.forEach(child -> {
+                    if (child instanceof Label) {
+                        ((Label) child).setStyle("-fx-background-color: #90ee90; -fx-background-radius: 5");
+                    }
+                });
         return db;
     }
 
