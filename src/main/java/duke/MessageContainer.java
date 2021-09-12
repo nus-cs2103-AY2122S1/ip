be6b1ed1cac67be3cb87a2dafbafef5b27cddfe8
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2021-09-12 11:45:43 +0800

    Improve GUI to highlight errors with different background colour

--- src/main/java/duke/MessageContainer.java
+++ src/main/java/duke/MessageContainer.java
@@ -21,9 +21,12 @@ import javafx.scene.paint.Color;
 
 public class MessageContainer extends HBox {
     private static final Color userTextMessageColour = Color.rgb(44, 130, 201, 1);
-    private static final Color dukeTextMessageColour = Color.rgb(255, 165, 0, 1);
+    private static final Color dukeTextMessageColour = Color.rgb(181, 179, 179, 1);
+    private static final Color dukeErrorMessageColour = Color.rgb(255, 89, 89, 1);
+    private static final Color blackColour = Color.color(0, 0, 0);
     private static final CornerRadii messageCornerRadius = new CornerRadii(10.0);
     private static final Insets messageInset = new Insets(-5.0);
+    private static final String errorMessagePrefix = "SORRY!";
     @FXML
     private Label textMessage;
     private MessageContainer(String text) {
@@ -48,8 +51,15 @@ public class MessageContainer extends HBox {
     private void reverseMessageDirection() {
         ObservableList<Node> text = FXCollections.observableArrayList(getChildren());
         Label dukeReply = (Label) text.get(0);
-        dukeReply.setBackground(new Background(new BackgroundFill(dukeTextMessageColour, messageCornerRadius,
-                messageInset)));
+        if (textMessage.getText().split(" ")[0].equals(errorMessagePrefix)) {
+            dukeReply.setBackground(new Background(new BackgroundFill(dukeErrorMessageColour, messageCornerRadius,
+                    messageInset)));
+        } else {
+            dukeReply.setBackground(new Background(new BackgroundFill(dukeTextMessageColour, messageCornerRadius,
+                    messageInset)));
+
+        }
+        dukeReply.setTextFill(blackColour);
         Collections.reverse(text);
         getChildren().setAll(text);
         setAlignment(Pos.TOP_LEFT);
