46df1a1942124952a4dd1367b37466106fdcc066
sj1999-BIT <e0543446@u.nus.edu.sg>
2021-09-12 09:38:34 +0800

    improved codequality
    Added a few more assertions to double check the format of the command.
    Changed if-else statement to switch case to improve presentation
    Removed the bug that the first input results in duke greeting message.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,39 @@
 public class Duke {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         String logo = " ____        _        \n"
+=======
+        Duke user = new Duke();
+        boolean end = true;
+        user.userInterface.greet();
+        user.userInterface.getDataInputList();
+        while (end) {
+            end = user.userInterface.echo();
+            System.out.println("__________________________________");
+        }
+    }
+
+    public String getResponse(String input) {
+            return userInterface.choiceOfAction(input);
+    }
+
+    public static final String greet() {
+        return Ui.greet();
+    }
+}
+
+
+//deals with interactions with the user
+class Ui {
+    private final Storage store = new Storage();
+    private TaskList taskList = new TaskList();
+    /**
+     * Generate the initiate message.
+     */
+    public static final String greet() {
+        String logo = "__________________________________\n"
+                +" ____        _        \n"
+>>>>>>> Stashed changes
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
