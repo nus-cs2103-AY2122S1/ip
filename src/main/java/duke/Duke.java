fb403ced0919d8963eb3dedf9b1e5d1755e95f76
Preshita01 <pandipreshita@gmail.com>
2021-09-13 22:45:48 +0800

    Refactor code by shifting any output strings by Jarvis to Ui.java

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -12,11 +12,6 @@ public class Duke {
     private NoteList notes;
     private Ui ui;
 
-    private final String NO_TASKS = "There are currently no tasks on your list :)";
-    private final String NO_NOTES = "There are currently no notes on your list!";
-    private final String UNRECOGNISED_COMMAND = "I'm sorry, but I don't know what that means :(";
-    private final String BYE = "Bye! Hope to see you soon :)";
-
     /**
      * Retrieves all the tasks stored by Jarvis in the hard disk upon running the main method.
      *
@@ -24,8 +19,7 @@ public class Duke {
      * @param noteFilePath The file in which the notes are stored
      */
     public Duke(String taskFilePath, String noteFilePath) {
-        ui = new Ui();
-
+        //ui = new Ui();
         taskStorage = new Storage(taskFilePath); //To load user tasks
         notesStorage = new Storage(noteFilePath); //To load user notes as well
 
@@ -45,49 +39,44 @@ public class Duke {
      *
      * @param input the command given by the user
      */
-    public String getResponse(String input) {
-            try {
-                switch (Parser.parseCommand(input)) {
-                    case "list":
-                        // If there are no tasks in the task list
-                        if (TaskList.getCounter() == 0) {
-                            return "\t" + NO_TASKS;
-                        } else {
-                            return Parser.parseList();
-                        }
-                    case "notes":
-                        // If there are no tasks in the task list
-                        if (NoteList.getCounter() == 0) {
-                            return "\t" + NO_NOTES;
-                        } else {
-                            return Parser.parseNoteList();
-                        }
-                    case "done":
-                        return Parser.parseDone(input);
-                    case "delete":
-                        return Parser.parseDelete(input);
-                    case "todo":
-                        return Parser.parseTodo(input);
-                    case "deadline":
-                        return Parser.parseDeadline(input);
-                    case "event":
-                        return Parser.parseEvent(input);
-                    case "today":
-                        return Parser.parseToday();
-                    case "find":
-                        return Parser.parseFind(input);
-                    case "note":
-                        return Parser.parseNote(input);
-                    case "delete note":
-                        return Parser.parseDeleteNote(input);
-                    case "bye":
-                        return BYE;
-                    default:
-                        throw new DukeException(UNRECOGNISED_COMMAND);
+    public static String getResponse(String input) {
+        try {
+            switch (Parser.parseCommand(input)) {
+            case "list":
+                return Ui.listTasks();
+            case "notes":
+                // If there are no tasks in the task list
+                if (NoteList.getCounter() == 0) {
+                    return "\t" + Ui.NO_NOTES;
+                } else {
+                    return Parser.parseNoteList();
                 }
-            } catch (DukeException | IOException e) {
-                return "" + e;
+            case "done":
+                return Parser.parseDone(input);
+            case "delete":
+                return Parser.parseDelete(input);
+            case "todo":
+                return Parser.parseTodo(input);
+            case "deadline":
+                return Parser.parseDeadline(input);
+            case "event":
+                return Parser.parseEvent(input);
+            case "today":
+                return Ui.listTodayTasks();
+            case "find":
+                return Ui.findTasks(input);
+            case "note":
+                return Parser.parseNote(input);
+            case "delete note":
+                return Parser.parseDeleteNote(input);
+            case "bye":
+                return Ui.BYE;
+            default:
+                throw new DukeException(Ui.UNRECOGNISED_COMMAND);
             }
+        } catch (DukeException | IOException e) {
+            return "" + e;
+        }
     }
 }
 
