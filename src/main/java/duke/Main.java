20a086b883dc367804299c89f428c06993db4e1a
TLChicken <tehlightningchicken@gmail.com>
2021-09-14 02:24:03 +0800

    Add icon and name to window

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -29,6 +30,10 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+
+            stage.setTitle("The Seagull Tasker");
+            Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/ROTS.png"));
+            stage.getIcons().add(dukeImage);
 
             scene.getRoot().setStyle("-fx-font-family: 'Courier New'");
 
