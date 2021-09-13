6b61920775195655630378c8836f5990d59f49f2
HongshuW <hongshu.wang.uni@gmail.com>
2021-09-13 15:09:00 +0800

    Main class: add a title to the stage

--- src/main/java/workdone/Main.java
+++ src/main/java/workdone/Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("WorkDone");
             fxmlLoader.<MainWindow>getController().setWorkDone(workDone);
             stage.show();
             fxmlLoader.<MainWindow>getController().greetTheUser();
