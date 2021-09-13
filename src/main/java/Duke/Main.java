4dcf61a79dbaa3ac4c63ddc8f3f708d90afd78c7
janjanchen <ssp.18s20.janicechen@gmail.com>
2021-09-13 15:30:51 +0800

    Design GUI

--- src/main/java/Duke/Main.java
+++ src/main/java/Duke/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -35,6 +36,8 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/images/Duke.jpg")));
+            stage.setTitle("DukePro");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
