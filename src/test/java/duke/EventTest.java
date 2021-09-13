0187808871b6333ea82ff8d6928561c12778f213
sheehui <tansheehui@gmail.com>
2021-09-13 21:44:12 +0800

    Fix JUnit tests
    Tests failed due to amendment of datetime formats in deadline and event tasks.

--- src/test/java/duke/EventTest.java
+++ src/test/java/duke/EventTest.java
@@ -13,20 +13,20 @@ import duke.tasks.Event;
 public class EventTest {
     @Test
     public void testOutput1() throws UserInputError {
-        Event event = new Event("merge branch", "2021-09-09", true);
-        assertEquals("[E][X] merge branch (at: Sep 9 2021)", event.toString());
+        Event event = new Event("merge branch", "2021-09-09 0900-1200", true);
+        assertEquals("[E][X] merge branch (at: Sep 9 2021, 0900-1200)", event.toString());
     }
 
     @Test
     public void testOutput2() throws UserInputError {
-        Event event = new Event("merge branch", "2021-09-09", false);
-        assertEquals("[E][  ] merge branch (at: Sep 9 2021)", event.toString());
+        Event event = new Event("merge branch", "2021-09-09 0930-2359", false);
+        assertEquals("[E][  ] merge branch (at: Sep 9 2021, 0930-2359)", event.toString());
     }
 
     @Test
     public void testInvalidDate1() {
         assertThrows(UserInputError.class, () ->
-                new Event("merge branch", "2021-90-09", false));
+                new Event("merge branch", "2021-90-09 1299-2359", false));
     }
 
     @Test
@@ -37,7 +37,7 @@ public class EventTest {
 
     @Test
     public void testTaskToString1() throws UserInputError {
-        Event event = new Event("merge master", "2021-09-09", false);
-        assertEquals("E ~#~ 0 ~#~ merge master ~#~ 2021-09-09", event.convertTaskToString());
+        Event event = new Event("merge master", "2021-09-09 0930-1200", false);
+        assertEquals("E ~#~ 0 ~#~ merge master ~#~ 2021-09-09 0930-1200", event.convertTaskToString());
     }
 }
