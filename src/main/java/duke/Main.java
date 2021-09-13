03a60b6aa9101ced356e560603f30e82f15aef82
YuPei <sunyupei1999@gmail.com>
2021-09-13 22:34:37 +0800

    add title to application and Ui picture

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.setTitle("Friendly Duke TaskManager");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
