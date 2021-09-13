3ddd1aafe73418d98959c5126fd7a38cbb257b96
Didymus <e0544195@u.nus.edu>
2021-09-14 00:03:42 +0800

    Fix bugs from FILE PATH

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -28,12 +28,9 @@ public class Duke {
     public Duke() throws Exception {
         this.listOfTasks = TaskList.makeNewTaskList();
         this.storage = new Storage(Duke.FILE_PATH);
-
-        try {
-            this.loadSavedTasks();
-        } catch (IOException | DukeUnableLoadTask e) {
-            throw e;
-        }
+        
+        createFileIfNoFile();
+        this.loadSavedTasks();
 
     }
 
