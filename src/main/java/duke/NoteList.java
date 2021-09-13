d57c812a70aaae8723e6bbd54814cda311f12b02
Preshita01 <pandipreshita@gmail.com>
2021-09-13 22:48:25 +0800

    Refactor code by shifting any output strings by Jarvis to Ui.java and any writings to the note file in user's hard disk to Storage.java

--- src/main/java/duke/NoteList.java
+++ src/main/java/duke/NoteList.java
@@ -22,6 +22,7 @@ public class NoteList {
 
     /**
      * Returns the noteList
+     *
      * @return the noteList
      */
     public static ArrayList<Note> getNoteList() {
@@ -30,6 +31,7 @@ public class NoteList {
 
     /**
      * Returns the number of notes currently save in user's hard disk
+     *
      * @return the number of notes currently save in user's hard disk (counter member)
      */
     public static int getCounter() {
@@ -38,6 +40,7 @@ public class NoteList {
 
     /**
      * Adds a note to the noteList
+     *
      * @param note the note that is to be added to the noteList
      */
     public static void addNote(Note note) {
@@ -47,6 +50,7 @@ public class NoteList {
 
     /**
      * Adds a note to the noteList and update the list of notes in user's hard disk
+     *
      * @param currNote the note that is to be added
      * @throws IOException if there is an error in appending the note to the list of notes
      * in user's hard disk
@@ -54,13 +58,12 @@ public class NoteList {
     public static void addNoteAndUpdate(Note currNote) throws IOException {
         NoteList.noteList.add(currNote);
         NoteList.counter++;
-        appendToFile("data/notes.txt", (counter) + "." +
-                NoteList.getNoteList().get(counter - 1).toPrintToFile()
-                + System.lineSeparator());
+        Storage.appendToNoteFile();
     }
 
     /**
      * Deletes a note to the noteList and update the list of notes in user's hard disk
+     *
      * @param currNote the note that is to be deleted
      * @throws IOException if there is an error in re-writing the list of notes without the
      * deleted note
@@ -68,46 +71,7 @@ public class NoteList {
     public static void deleteNoteAndUpdate(Note currNote) throws IOException {
         NoteList.noteList.remove(currNote);
         counter--;
-        if (NoteList.getNoteList().size() == 0) {
-            writeToFile("data/notes.txt", "");
-        } else {
-            for (int i = 0; i < NoteList.getNoteList().size(); i++) {
-                if (i == 0) {
-                    writeToFile("data/notes.txt", (i + 1) + "." +
-                            NoteList.getNoteList().get(i).toPrintToFile()
-                            + System.lineSeparator());
-                } else {
-                    appendToFile("data/notes.txt", (i + 1) + "." +
-                            NoteList.getNoteList().get(i).toPrintToFile()
-                            + System.lineSeparator());
-                }
-            }
-        }
-    }
-
-    /**
-     * Writes to the file in user's hard disk that stores a list of notes or to overwrite
-     * the contents of this file
-     * @param filePath the relative path to the file
-     * @param textToAdd the content that is to be written
-     * @throws IOException if there is an error in writing to/overwriting the file
-     */
-    private static void writeToFile(String filePath, String textToAdd) throws IOException {
-        FileWriter fw = new FileWriter(filePath);
-        fw.write(textToAdd);
-        fw.close();
-    }
-
-    /**
-     * Appends content to the file in user's hard disk that stores a list of notes
-     * @param filePath the relative path to the file
-     * @param textToAppend the content that is to be appended
-     * @throws IOException if there is an error in appending to existing content of the file
-     */
-    private static void appendToFile(String filePath, String textToAppend) throws IOException {
-        FileWriter fw = new FileWriter(filePath, true); // create a FileWriter in append mode
-        fw.write(textToAppend);
-        fw.close();
+        Storage.rewriteNoteFile();
     }
 
 }
