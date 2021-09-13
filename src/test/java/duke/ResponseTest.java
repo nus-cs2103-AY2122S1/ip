403231126fd5774a7dc8d90e7fa462771bb9bffd
Bryan Loh <lamofacade@gmail.com>
2021-09-14 03:41:18 +0800

    Add a GUI to Duke (#30)
    * Add JavaFX to gradle
    * Refactor static to instance attributes in Duke class
    * Create JavaFX skeleton
    * Create a GUI for Duke application
    * Create interaction with user
    * Add fxml to JavaFX
    * Connect MainWindow GUI to Duke application

--- src/test/java/duke/ResponseTest.java
+++ src/test/java/duke/ResponseTest.java
@@ -1,18 +1,18 @@
 package duke;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 public class ResponseTest {
-    private static String LINE = "    ____________________________________________________________";
-    private static String MESSAGE_INDENT = "     ";
+    private static final String LINE = "    ____________________________________________________________";
+    private static final String MESSAGE_INDENT = "     ";
 
     @Test
     public void toString_success() {
         assertEquals(
                 ResponseTest.LINE + System.lineSeparator()
                 + ResponseTest.MESSAGE_INDENT + "message" + System.lineSeparator()
-                + ResponseTest.LINE
-                , new Response("message").toString());
+                + ResponseTest.LINE, new Response("message").toString());
     }
 }
