3b440f385df63a33d8ab27fb8457e2ba2ef2dac9
huyuxin0429 <e0564960@u.nus.edu>
2021-09-14 22:52:38 +0800

    refactor javafx UI portion to level-4

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,38 +2,16 @@ package duke;
 
 import java.io.IOException;
 
-import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+
 
 /**
  * Duke chat bot main class.
  */
-public class Duke extends Application {
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-
+public class Duke {
     private final Ui ui;
     private TaskList taskList;
     private final Storage storage;
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
     /**
      * Creates a new duke object
      */
@@ -48,20 +26,6 @@ public class Duke extends Application {
             taskList = new TaskList();
         }
     }
-    /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-    }
 
     /**
      * Provides response to user based on given input.
@@ -70,83 +34,6 @@ public class Duke extends Application {
      */
     public String getResponse(String input) {
         return run(input);
-    }
-
-    /**
-     * Initiates the Duke chat bot GUI.
-     * @param stage JavaFX stage to be passed to the chat bot.
-     */
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        dialogContainer.setPadding(new Insets(10, 10, 10, 10));
-        dialogContainer.setSpacing(10);
-        scrollPane.setContent(dialogContainer);
-        userInput = new TextField();
-        sendButton = new Button("Send");
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        // more code to be added here later
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Part 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-    }
-
-    private Label getDialogLabel(String text) {
-        // You will need to import `javafx.scene.control.Label`.
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
     }
 
     private String run(String input) {
@@ -179,9 +66,7 @@ public class Duke extends Application {
                 } else if (!currentParameter.contains(" /at ")) {
                     throw new DukeException("Missing /at command");
                 }
-                System.out.println(currentParameter);
                 descriptionAndTimeParts = Parser.dateParameterParser(Command.EVENT, currentParameter);
-                System.out.println(descriptionAndTimeParts);
                 output = taskList.addTask(Command.EVENT, descriptionAndTimeParts[0], descriptionAndTimeParts[1]);
                 Storage.updateLocalFile(taskList);
                 assert (taskList.getSize() - prevSize == 1);
