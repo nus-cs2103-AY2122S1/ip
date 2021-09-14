11f8f058db1a19e88a20f04624999c82c443fd89
Preshita01 <pandipreshita@gmail.com>
2021-09-14 12:02:59 +0800

    Add end() method to close the application

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -2,11 +2,15 @@ package duke;
 
 import java.io.IOException;
 
+import javafx.animation.PauseTransition;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import javafx.util.Duration;
 
 /**
  * A GUI for Duke using FXML.
@@ -14,6 +18,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     private Duke jarvis = new Duke("data/jarvis.txt", "data/notes.txt"); // Instantiating Jarvis
 
+    private static Stage currStage;
     /**
      * Sets up the GUI for Jarvis
      *
@@ -22,6 +27,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            Main.currStage = stage;
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
@@ -31,5 +37,11 @@ public class Main extends Application {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void end() {
+        PauseTransition durationToWaitBeforeClosing = new PauseTransition(Duration.seconds(1));
+        durationToWaitBeforeClosing.setOnFinished(event -> Main.currStage.close());
+        durationToWaitBeforeClosing.play();
     }
 }
