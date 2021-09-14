646b61f0bae75d4f2b9852fa3a07482a52e2e4d1
timothywongej <e0559903@u.nus.edu>
2021-09-14 17:15:44 +0800

    Remove redundant exception

--- src/main/java/duke/exceptions/InvalidCommandException.java
+++
@@ -1,8 +0,0 @@
-package duke.exceptions;
-
-/** Throws error for invalid commands, e.g. task */
-public class InvalidCommandException extends DukeException {
-    public InvalidCommandException(String message) {
-        super(message);
-    }
-}
