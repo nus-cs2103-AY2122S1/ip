24f13aa63f1c5169bdc42cc8912b6cc7c2252ce1
Hang Zelin <69517383+HangZelin@users.noreply.github.com>
2021-09-14 19:37:38 +0800

    Add icon and title to Duke product

--- src/main/java/duke/uimanager/Main.java
+++ src/main/java/duke/uimanager/Main.java
@@ -6,12 +6,14 @@ import duke.main.Duke;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 /**
  * @@author Hang Zelin
  *
  * A GUI for Duke using FXML.
+ * note: The icon image is free to use. link: https://www.flaticon.com/free-icon/chat_724715
  */
 public class Main extends Application {
 
@@ -29,6 +31,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.getIcons().add(new Image("/images/icon.png"));
+            stage.setTitle("Duke. Your best task manager!");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
