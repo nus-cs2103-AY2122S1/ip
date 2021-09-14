cb59785d8bb8ebab4e3b2becf2f46062f831a706
tchiong <57283702+tchiong@users.noreply.github.com>
2021-09-14 18:49:21 +0800

    A-BetterGui
    Improvements to Gui in the form of a nicer background and duke profile picture

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -6,6 +6,8 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.VBox;
 
 /**
@@ -24,7 +26,7 @@ public class MainWindow extends AnchorPane {
     private DukeGui duke;
 
     private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private final Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private final Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DukeNew.jpg"));
 
     /**
      * Initializes main window
@@ -32,6 +34,9 @@ public class MainWindow extends AnchorPane {
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        Image bgImg = new Image(this.getClass().getResourceAsStream("/images/Galaxy.jpg"));
+        Background bg = new Background(new BackgroundImage(bgImg, null, null, null, null));
+        dialogContainer.setBackground(bg);
         dialogContainer.getChildren().addAll(
                 DialogBox.getDukeDialog("Greetings from Duke!", dukeImage)
         );
