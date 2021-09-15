c1290a65c1f7aaa2f6932cb8d941a47c67dd40ca
Jeff Sieu <jeffsy00@gmail.com>
2021-09-15 22:28:42 +0800

    Fix build.gradle

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -4,10 +4,10 @@ import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length > 0 && args[0].equals("cli")) {
-            new Duke(false).startCli();
-        } else {
+//        if (args.length > 0 && args[0].equals("cli")) {
+//            new Duke(false).startCli();
+//        } else {
             Application.launch(Duke.class, args);
-        }
+//        }
     }
 }
