440a6c0fb9761db37092e62356ede802dbfdb803
peilinye <peilinye18@gmail.com>
2021-09-15 21:13:55 +0800

    Update JUnit test cases

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -15,26 +15,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest {
 
-//    @Test
-//    public void testParseAddCommand() throws DukeException {
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-//        assertEquals(Parser.parse("deadline return book /by 2021-08-25 23:59"),
-//                new AddCommand(new Deadline("return book", false,
-//                        LocalDateTime.parse("2021-08-25 23:59", formatter))));
-//        assertEquals(Parser.parse("event group meeting /at 2021-09-01 15:00"),
-//                new AddCommand(new Event("group meeting", false,
-//                        LocalDateTime.parse("2021-09-01 15:00", formatter))));
-//        assertEquals(Parser.parse("todo read book "),
-//                new AddCommand(new ToDo("read book", false)));
-//    }
-//
-//    @Test
-//    public void testParseDeleteCommand() throws DukeException {
-//        assertEquals(Parser.parse("delete 2"), new DeleteCommand(1));
-//    }
-//
-//    @Test
-//    public void testDoneCommand() throws DukeException {
-//        assertEquals(Parser.parse("done 5"), new DoneCommand(4));
-//    }
+    @Test
+    public void testParseAddCommand() throws DukeException {
+        Parser p = new Parser(new Duke());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        assertEquals(p.parse("deadline return book /by 2021-08-25 23:59"),
+                new AddCommand(new Deadline("return book", false,
+                        LocalDateTime.parse("2021-08-25 23:59", formatter))));
+        assertEquals(p.parse("event group meeting /at 2021-09-01 15:00"),
+                new AddCommand(new Event("group meeting", false,
+                        LocalDateTime.parse("2021-09-01 15:00", formatter))));
+        assertEquals(p.parse("todo read book "),
+                new AddCommand(new ToDo("read book", false)));
+    }
+
+    @Test
+    public void testParseDeleteCommand() throws DukeException {
+        Duke d = new Duke();
+        Parser p = new Parser(d);
+        assertEquals(p.parse("delete 2"), new DeleteCommand(1, d.getTaskList()));
+    }
+
+    @Test
+    public void testDoneCommand() throws DukeException {
+        Parser p = new Parser(new Duke());
+        assertEquals(p.parse("done 5"), new DoneCommand(4));
+    }
 }
--- src/test/java/duke/StorageTest.java
+++ src/test/java/duke/StorageTest.java
@@ -1,7 +1,6 @@
 package duke;
 
 import duke.task.Deadline;
-import duke.task.Task;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
