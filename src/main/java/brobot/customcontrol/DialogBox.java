d128d07a21b93dbc7b1ac91250f75a5e48f5137f
Markus Lim <markuslim24@gmail.com>
2021-09-15 12:48:57 +0800

    wrap reply in text bubble and improve padding

--- src/main/java/brobot/customcontrol/DialogBox.java
+++ src/main/java/brobot/customcontrol/DialogBox.java
@@ -7,7 +7,11 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class DialogBox extends HBox {
@@ -22,12 +26,19 @@ public class DialogBox extends HBox {
      * @param l The label for the dialogBox
      * @param iv The image for the dialogBox
      */
-    public DialogBox(Label l, ImageView iv) {
+    public DialogBox(Label l, ImageView iv, int translateXValue) {
         text = l;
         displayPicture = iv;
 
         text.setWrapText(true);
-        text.setPadding(new Insets(0, 10, 0, 10));
+        text.setPadding(new Insets(5, 10, 5, 10));
+        text.setTranslateX(translateXValue);
+        //Credits: https://www.programcreek.com/java-api-examples/?class=javafx.scene.control.Label&method=setBackground
+        text.setBackground(new Background(
+                new BackgroundFill(
+                        Color.rgb(204, 204, 204, 0.7),
+                        new CornerRadii(5.0),
+                        new Insets(-1.0))));
         displayPicture.setFitWidth(70.0);
         displayPicture.setFitHeight(70.0);
         displayPicture.setClip(clip);
@@ -47,11 +58,11 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(Label l, ImageView iv) {
-        return new DialogBox(l, iv);
+        return new DialogBox(l, iv, -10);
     }
 
     public static DialogBox getDukeDialog(Label l, ImageView iv) {
-        var db = new DialogBox(l, iv);
+        var db = new DialogBox(l, iv, 10);
         db.flip();
         return db;
     }
