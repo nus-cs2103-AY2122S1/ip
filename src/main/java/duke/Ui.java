330cf1da173b13ef7434bb2ff88d13262b072fbe
Poh Hui-En Ruth <iluvcelebi@gmail.com>
2021-09-16 02:43:10 +0800

    Updated Ui to promote the website, need to figure out how to execute jar by double clicking

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -63,7 +63,8 @@ public class Ui {
 
                         return "Commandws supported:\n\n"
                                 + "- bye\n- clearall\n- help\n- list\n- todo\n- event\n- deadline\n- "
-                                + "delete\n- done\n- find\n\nPlease check our website for more details!";
+                                + "delete\n- done\n- find\n\nPlease check our website at https://ruthpohrp" +
+                                ".github.io/ip/ for more details!";
                     } catch (DukeException e) {
                         return this.getErrorMessage(e);
                     }
