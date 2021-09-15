012c3d1959c110fea4e0d8e79034937617ed439f
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-09-15 20:41:19 +0800

    Add getResponse back to Duke

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -57,6 +57,14 @@ public class Duke {
     }
 
     /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
+    /**
      * Runs this Duke program.
      */
     public void run() {
