445a7799d7abdfae606dea46fa0d136660348a45
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-09-15 19:13:46 +0800

    Add padding for DialogBox

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -2,6 +2,7 @@ package duke;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
@@ -36,12 +37,15 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getUserDialog(Label l, ImageView iv) {
-        return new DialogBox(l, iv);
+        DialogBox db = new DialogBox(l, iv);
+        db.setPadding(new Insets(10, 10, 10, 10));
+        return db;
     }
 
     public static DialogBox getDukeDialog(Label l, ImageView iv) {
         var db = new DialogBox(l, iv);
         db.flip();
+        db.setPadding(new Insets(10, 10, 10, 10));
         return db;
     }
 }
