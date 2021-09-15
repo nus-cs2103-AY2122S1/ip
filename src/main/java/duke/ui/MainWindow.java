6e0369b9f009651a313ce5ac530b10089448be58
Chow Rui Yan <chowruiyan@hotmail.co.uk>
2021-09-16 00:17:03 +0800

    Change MainWindow.java such that GUI will auto close when ExitCommand is given.

--- src/main/java/duke/ui/MainWindow.java
+++ src/main/java/duke/ui/MainWindow.java
@@ -9,6 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * Controller for MainWindow. Provides the layout for the other controls.
@@ -62,5 +63,10 @@ public class MainWindow extends AnchorPane {
         }
         dialogContainer.getChildren().addAll(inputDialogBox, responseDialogBox);
         userInput.clear();
+
+        if (input.equalsIgnoreCase("bye")) {
+            Stage stage = (Stage) dialogContainer.getScene().getWindow();
+            stage.close();
+        }
     }
 }
