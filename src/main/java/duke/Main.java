d785695b23561a6e5a0eca8fb9b9966f83b631b2
Tanishq4331 <62829987+Tanishq4331@users.noreply.github.com>
2021-09-15 12:26:39 +0800

    Change GUI font to Courier New to add macOS compatibility.

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
+            scene.getRoot().setStyle("-fx-font-family: 'Courier New'");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
