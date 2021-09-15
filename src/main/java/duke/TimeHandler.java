0fd181729b03d7426894f979fe8423b26b16ab97
bklimey <bklim@comp.nus.edu.sg>
2021-09-15 20:33:21 +0800

    A-CodeQuality

--- src/main/java/duke/TimeHandler.java
+++ src/main/java/duke/TimeHandler.java
@@ -21,22 +21,36 @@ public class TimeHandler {
         String formattedTime = "";
         for (String s2 : s) {
             if (s2.chars().filter(c->c=='/').count() == 2) {
-                String[] date = s2.split("/");
-                try {
-                    LocalDate localDate = LocalDate.of(Integer.parseInt(date[2]),
-                            Integer.parseInt(date[1]), Integer.parseInt(date[0]));
-                    formattedDate = localDate.format(DateTimeFormatter.ofPattern("d MMM yyyy"));
-                } catch (Exception exception) {
-                    System.out.println(exception);
-                }
+                formattedDate = parseDate(s2);
             } else if (s2.length() == 4 && s2.matches("\\d{4}")) {
-                int hour = Integer.parseInt(s2.substring(0,2));
-                String minute = s2.substring(2);
-                String meridiem = (hour > 11) ? "PM" : "AM";
-                hour = (hour - 1) % 12 + 1;
-                formattedTime = " " + hour + ":" + minute + " " + meridiem;
+                formattedDate = parseTime(s2);
             }
         }
         return formattedDate + formattedTime;
     }
+
+    private static String parseDate(String dateString) {
+        // parse date from format from d/m/y to d mmm yyyy
+        String[] date = dateString.split("/");
+        assert date.length == 3;
+        int day = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int year = Integer.parseInt(date[2]);
+        LocalDate localDate = LocalDate.of(year, month, day);
+        return localDate.format(DateTimeFormatter.ofPattern("d MMM yyyy"));
+    }
+
+    private static String parseTime(String timeString) {
+        // parse date from format from 24 hr format to 12 hr format
+        // e.g. 2030 -> 08:30 PM
+        assert timeString.length() == 4;
+        assert timeString.matches("\\d{4}");
+        int hour = Integer.parseInt(timeString.substring(0,2));
+        String minute = timeString.substring(2);
+        String meridiem = (hour > 11) ? "PM" : "AM";
+        hour = (hour - 1) % 12 + 1;
+        return " " + hour + ":" + minute + " " + meridiem;
+    }
+
+
 }
