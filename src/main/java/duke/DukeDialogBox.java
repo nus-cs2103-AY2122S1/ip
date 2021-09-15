02594bb1465d92ff0c6131d406496a253658f4e2
lwj1711 <limweijie1711@yahoo.com.sg>
2021-09-15 22:24:32 +0800

    Rename GUI to Duke

--- src/main/java/duke/DukeDialogBox.java
+++ src/main/java/duke/DukeDialogBox.java
@@ -1,14 +1,9 @@
 package duke;
 
 import java.io.IOException;
-import java.util.Collections;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -37,16 +32,6 @@ public class DukeDialogBox extends HBox {
 
         dialog.setText(text);
         displayPicture.setImage(img);
-    }
-
-    /**
-     * Flips the dialog box such that the ImageView is on the left and text on the right.
-     */
-    private void flip() {
-        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        Collections.reverse(tmp);
-        getChildren().setAll(tmp);
-        setAlignment(Pos.TOP_LEFT);
     }
 
     /**
