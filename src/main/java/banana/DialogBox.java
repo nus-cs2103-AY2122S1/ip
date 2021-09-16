b4e030bef898df4338bba6e20cc64ff39fdaee25
Banana3021 <anaravi21@gmail.com>
2021-09-16 23:23:39 +0800

    Add comments to the DialogBox class

--- src/main/java/banana/DialogBox.java
+++ src/main/java/banana/DialogBox.java
@@ -27,12 +27,20 @@ public class DialogBox extends HBox {
     private Image img;
     private String text;
 
+    /**
+     * Constructor for the DialogBox
+     * class - loads the FXMLLoader
+     * object.
+     *
+     * @param text the user input
+     * @param img  the image to be used
+     */
     private DialogBox(String text, Image img) {
         this.text = text;
         this.img = img;
-
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource(
+                    "/view/DialogBox.fxml"));
             fxmlLoader.setController(this);
             fxmlLoader.setRoot(this);
             fxmlLoader.load();
@@ -41,6 +49,13 @@ public class DialogBox extends HBox {
         }
     }
 
+    /**
+     * Sets the label text and the image
+     * display.
+     *
+     * @param f the font of the text
+     * @param c the color of the text
+     */
     public void setDialogBox(Font f, Color c) {
         dialog.setFont(f);
         dialog.setTextFill(c);
@@ -50,22 +65,45 @@ public class DialogBox extends HBox {
         displayPicture.setImage(img);
     }
 
-
+    /**
+     * Takes the user input and image
+     * and returns a DialogBox object
+     * which prints to the GUI program.
+     *
+     * @param text user input
+     * @param img  the image
+     * @return the DialogBox object
+     */
     public static DialogBox getUserDialog(String text, Image img) {
         var db = new DialogBox(text, img);
-        db.setDialogBox(new Font("American Typewriter",  18),
+        db.setDialogBox(new Font("American Typewriter", 18),
                 Color.SADDLEBROWN);
         return db;
     }
 
+    /**
+     * Takes the parsed output and image
+     * and returns a DialogBox object
+     * which prints to the GUI program.
+     *
+     * @param text parsed output
+     * @param img  the image
+     * @return the DialogBox object
+     */
     public static DialogBox getDukeDialog(String text, Image img) {
         var db = new DialogBox(text, img);
         db.flip();
-        db.setDialogBox(new Font("American Typewriter",  14),
+        db.setDialogBox(new Font("American Typewriter", 14),
                 Color.DARKBLUE);
         return db;
     }
 
+    /**
+     * Pushes the bot image
+     * and text to the left of the screen,
+     * and puts the image to the left
+     * of the text.
+     */
     private void flip() {
         this.setAlignment(Pos.TOP_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
