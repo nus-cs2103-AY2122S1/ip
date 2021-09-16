79425bd6abe657894c14a0c01ed968a9ea206fd4
Yew Meng <yewmengchai@gmail.com>
2021-09-16 13:54:09 +0800

    Improve GUI

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -11,6 +11,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+
 import java.io.IOException;
 import java.util.Collections;
 
--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -55,5 +55,4 @@ public class MainWindow extends AnchorPane {
         );
         userInput.clear();
     }
-
 }
