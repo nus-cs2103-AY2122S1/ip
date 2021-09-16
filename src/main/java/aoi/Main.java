5eb8bb3445067ebae987fa06780ca3bc0bc66c7e
Owen Tan <tanteongyuowen@gmail.com>
2021-09-16 19:50:21 +0800

    Add Ui screenshot

--- src/main/java/aoi/Main.java
+++ src/main/java/aoi/Main.java
@@ -7,9 +7,9 @@ import aoi.controller.MainWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
 
 /**
  * A GUI for aoi.Aoi using FXML.
