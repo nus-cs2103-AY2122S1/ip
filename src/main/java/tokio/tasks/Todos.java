cc11e346125194e4a938299afabf076e4e920c7b
sharmaine <sharmainec@u.nus.edu>
2021-09-16 10:32:44 +0800

    Update check for duplicate tasks to be case insensitive

--- src/main/java/tokio/tasks/Todos.java
+++ src/main/java/tokio/tasks/Todos.java
@@ -2,6 +2,8 @@ package tokio.tasks;
 
 import tokio.commands.Instruction;
 
+import java.util.Locale;
+
 /**
  * Represents a todo task.
  */
@@ -51,7 +53,7 @@ public class Todos extends Task {
             return true;
         } else if (obj instanceof Todos) {
             Todos t = (Todos) obj;
-            return this.description.equals(t.description);
+            return this.description.toLowerCase(Locale.ROOT).equals(t.description);
         } else {
             return false;
         }
