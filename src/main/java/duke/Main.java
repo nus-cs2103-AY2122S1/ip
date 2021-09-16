f6b67708483ce02ce45ec7768e9097c14489175b
YeluriKetan <e0603110@u.nus.edu>
2021-09-16 18:50:06 +0800

    Improve the GUI

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -13,6 +14,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    private static final Image BOT_ICON = new Image(Main.class.getResourceAsStream("/images/BotIcon.png"));
     private Duke duke = new Duke();
 
     @Override
@@ -22,6 +24,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.getIcons().add(BOT_ICON);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
             stage.setResizable(false);
