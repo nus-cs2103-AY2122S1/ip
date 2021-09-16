4a8438ada17b106673e1e4dd793163bbd4c00363
xlzior <wenjun.lye@gmail.com>
2021-09-16 17:17:43 +0800

    Implement closing app on bye command

--- src/main/java/iris/command/ByeCommand.java
+++ src/main/java/iris/command/ByeCommand.java
@@ -1,10 +1,12 @@
 package iris.command;
 
 import iris.TaskList;
+import javafx.application.Platform;
 
 public class ByeCommand extends Command {
     @Override
     public String run(TaskList tasks) {
+        Platform.exit();
         return "Bye. Hope to see you again soon!";
     }
 }
