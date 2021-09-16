fbfe8e3408cb000608c01b5b7c96bbdd1786c257
wlren <weilinwork99@gmail.com>
2021-09-16 21:45:03 +0800

    Reset progress to redo GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,29 +1,15 @@
 package duke;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 /**
  * Contains main() method for Duke chatbot for IP of CS2103 2021
  *
  * @author: Ren Weilin A0216723J
  */
-public class Duke extends Application{
+public class Duke{
 
     private final UI userInterface;
     private final TaskList taskList;
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
 
     /**
      * Constructor which initializes the necessary components for Duke to function.
@@ -45,24 +31,4 @@ public class Duke extends Application{
         dukeInstance.userInterface.start();
     }
 
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-    }
 }
