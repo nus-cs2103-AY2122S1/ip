fe34b031706edaa5905a02298abda6879ddd33f4
nzixuan <nzixuan@gmail.com>
2021-09-16 10:11:33 +0800

    Tweak GUI to be more suitable for usage

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -24,7 +24,7 @@ public class MainWindow extends AnchorPane {
     private Duke duke;
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.jpeg"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/derp.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/derpy.png"));
 
     @FXML
     public void initialize() {
