f64ff59d74e19e6f2848542f389737d2e06174b4
ramapriyan912001 <ramapriyan912001@gmail.com>
2021-09-16 23:31:37 +0800

    Fix font issue on some Macs

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            scene.getRoot().setStyle("-fx-font-family: 'Arial'");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
