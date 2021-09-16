a4f32d119abd6279b31869f96ad279011979b62f
Banana3021 <anaravi21@gmail.com>
2021-09-16 23:27:19 +0800

    Edit Javadoc comments and format code

--- src/main/java/banana/Launcher.java
+++ src/main/java/banana/Launcher.java
@@ -2,12 +2,6 @@ package banana;
 
 import javafx.application.Application;
 
-/**
- * The Duke class runs the GUI
- * program.
- *
- * @author: Ravi Ananya
- **/
 public class Launcher {
 
     public static void main(String[] args) {
