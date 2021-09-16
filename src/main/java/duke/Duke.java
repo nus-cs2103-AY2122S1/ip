e62c45433d0364be6b856e7335695ef5e1e82c88
Wilfredwongkc <Wilfredwongkc@gmail.com>
2021-09-16 15:42:02 +0800

    Refactor code to remove arrow head shape. Place code in Parser class

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -156,62 +156,16 @@ public class Duke extends Application {
      * Replace this stub with your completed method.
      */
     private String getResponse(String input) {
-        String s;
-        int taskNumber;
-
         Storage storage = new Storage("data/duke.txt");
         TaskList list = new TaskList(storage.load());
         Ui ui = new Ui(storage, list);
+        Parser parser = new Parser(ui);
         try {
-            if (input.startsWith("bye")) {
-                return ui.goodbye();
-            } else if (input.equals("list")) {
-                return ui.listTasks();
-            } else {
-                if (input.startsWith("done")) {
-                    taskNumber = Integer.parseInt(input.substring(input.indexOf(" ") + 1)) - 1;
-                    assert taskNumber >= 0 : "Task number should be positive";
-                    return ui.markDone(taskNumber);
-                } else if (input.startsWith("todo")) {
-                    Todo todo = new Todo(input.substring(input.indexOf(" ") + 1));
-                    return ui.add(todo);
-                } else if (input.startsWith("deadline")) {
-                    Deadline deadline = new Deadline(input.substring(input.indexOf(" ") + 1, input.indexOf(" /by")),
-                            input.substring(input.indexOf("/by") + 4));
-                    return ui.add(deadline);
-                } else if (input.startsWith("event")) {
-                    Event event = new Event(input.substring(input.indexOf(" ") + 1, input.indexOf(" /at")),
-                            input.substring(input.indexOf("/at") + 4));
-                    return ui.add(event);
-                } else if (input.startsWith("delete")) {
-                    taskNumber = Integer.parseInt(input.substring(input.indexOf(" ") + 1)) - 1;
-                    assert taskNumber >= 0 : "Task number should be positive";
-                    return ui.delete(taskNumber);
-
-                } else if (input.startsWith("find")) {
-                    int thingToFind = input.indexOf(" ") + 1;
-                    return ui.findTasks(input.substring(thingToFind));
-                } else if (input.startsWith("help")) {
-                    String helpMessage = "Welcome to the quick help page!\n" +
-                            "Here are some examples of what you can type: \n" +
-                            "\t1) help\n" +
-                            "\t2) list\n" +
-                            "\t3) todo do homework\n" +
-                            "\t4) deadline assignment \\by 2021-09-24\n" +
-                            "\t5) event career fair \\at 2021-09-08\n" +
-                            "\t6) find homework\n" +
-                            "\t7) done 3\n" +
-                            "\t8) delete 1\n" +
-                            "\t9) bye\n";
-                    return helpMessage;
-                }
-            }
-            return "I don't understand. Please try again!";
+            return parser.parse(input);
         } catch (StringIndexOutOfBoundsException e) {
             return "You keyed in the command wrongly";
         } catch (IndexOutOfBoundsException e) {
             return "Sorry but the number you keyed in is out of range";
-
         }
     }
 }
