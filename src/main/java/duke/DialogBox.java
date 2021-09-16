6e830cf06196bebc96122063183f78aa76ab2e95
bernardwan <bernardwan99@gmail.com>
2021-09-17 05:29:58 +0800

    Improve GUI

--- src/main/java/duke/DialogBox.java
+++ src/main/java/duke/DialogBox.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 /**
  * Dialog box of chat box. From JavaFX tutorial.
@@ -35,6 +36,8 @@ public class DialogBox extends HBox {
 
         dialog.setText(text);
         displayPicture.setImage(img);
+        displayPicture.setClip(new Circle(displayPicture.getFitWidth() / 2,
+                displayPicture.getFitHeight() / 2.1, 37));
     }
 
     /**
