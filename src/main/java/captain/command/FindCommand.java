bda8cb945d97267f7d8a4596b94b7a729f0729ae
adam-ky <69505439+adam-ky@users.noreply.github.com>
2021-09-16 16:57:23 +0800

    Modify FindCommand logic
    The FindCommand's execute checks for the length of keywords list.
    
    The updated logic does not require this check.

--- src/main/java/captain/command/FindCommand.java
+++ src/main/java/captain/command/FindCommand.java
@@ -18,49 +18,50 @@ public class FindCommand extends Command {
 
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) {
-        TaskList filterTaskList = keywords.length == 1
-                ? filterWithSingleKeyword(tasks, keywords[0])
-                : filterWithMultipleKeywords(tasks);
+        TaskList filterTaskList = filterTaskListWithKeywords(tasks);
         return ui.showTasksFound(filterTaskList);
-    }
-
-    private TaskList filterWithMultipleKeywords(TaskList tasks) {
-        TaskList filteredTasks = new TaskList();
-        for (String keyword : keywords) {
-            filterTasksWithKeyword(tasks, filteredTasks, keyword);
-        }
-        return filteredTasks;
     }
 
     /**
      * Filters the task list according to the keyword provided.
      * @param tasks The list of tasks.
-     * @param keyword The keyword input by the user.
      * @return A TaskList containing only the tasks that contains the keyword.
      */
-    public TaskList filterWithSingleKeyword(TaskList tasks, String keyword) {
+    public TaskList filterTaskListWithKeywords(TaskList tasks) {
         List<Task> taskList = tasks
                 .getTaskList()
                 .stream()
-                .filter((task -> containsKeyword(task, keyword)))
+                .filter((task -> matchesKeywords(task, keywords)))
                 .collect(Collectors.toList());
         ArrayList<Task> keywordTasks = new ArrayList<>(taskList);
         return new TaskList(keywordTasks);
     }
 
-    public boolean containsKeyword(Task t, String keyword) {
-        return t.getDescription().toLowerCase().contains(keyword.toLowerCase());
-    }
-
-    private void filterTasksWithKeyword(TaskList tasks, TaskList keywordTasks, String keyword) {
-        for (Task t : tasks.getTaskList()) {
-            boolean notYetFound = !tasks.containsTask(t);
-            boolean isMatch = containsKeyword(t, keyword);
-            if (notYetFound && isMatch) {
-                assert (keywordTasks.getListSize() <= tasks.getListSize())
-                        : "Filtered list should not be greater than original list.";
-                keywordTasks.addTask(t);
+    /**
+     * Matches the task with the list of keywords.
+     * @param task The identified task to match.
+     * @param keywords The list of keywords input by user.
+     * @return True if the task contains at least one of the keywords. Otherwise, return false.
+     */
+    public boolean matchesKeywords(Task task, String[] keywords) {
+        for (String keyword : keywords) {
+            if (containsKeyword(task, keyword)) {
+                return true;
             }
         }
+        return false;
     }
+
+    /**
+     * Checks if the task contains a keyword. This check is case-insensitive.
+     * @param task The identified task to check.
+     * @param keyword A keyword from the list of keywords input by user.
+     * @return True if the task contains the keyword. Otherwise, return false;
+     */
+    public boolean containsKeyword(Task task, String keyword) {
+        String taskDescription = task.getDescription().toLowerCase();
+        keyword = keyword.toLowerCase();
+        return taskDescription.contains(keyword);
+    }
+
 }
