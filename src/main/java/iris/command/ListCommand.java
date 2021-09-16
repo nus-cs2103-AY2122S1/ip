d44fb3ec9d9ad09d2fa3265b51dd301538ebaab9
xlzior <wenjun.lye@gmail.com>
2021-09-16 17:45:33 +0800

    Update list command to be more informative when task list is empty

--- src/main/java/iris/command/ListCommand.java
+++ src/main/java/iris/command/ListCommand.java
@@ -6,7 +6,13 @@ public class ListCommand extends Command {
     @Override
     public String run(TaskList tasks) {
         assert tasks != null;
+
+        if (tasks.getCount() == 0) {
+            return "You do not have any tasks yet.\nUse todo, event or deadline to create a new task.";
+        }
+
         StringBuilder response = new StringBuilder();
+        response.append("Here's your list of tasks:\n");
         for (int i = 0; i < tasks.getCount(); i++) {
             response.append(String.format("%d. %s\n", i + 1, tasks.get(i)));
         }
