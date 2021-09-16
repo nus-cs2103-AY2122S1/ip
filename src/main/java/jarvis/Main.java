5ce279fdb9230aee8c7c5a481f3c32389a168934
sivayogasubramanian <sivayogasubramanian@gmail.com>
2021-09-16 20:37:58 +0800

    Change images

--- src/main/java/jarvis/Main.java
+++ src/main/java/jarvis/Main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -21,7 +20,6 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             scene.getStylesheets().add("styles/jarvis.css");
 
-            stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/images/logo.jpg")));
             stage.setScene(scene);
             stage.setTitle("Jarvis");
             stage.setResizable(false);
