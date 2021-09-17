229d602aa2f91d3cfa2aa61acd6d9bec528afc2d
Samuel-bit-prog <e0544506@u.nus.edu>
2021-09-17 20:39:32 +0800

    add checkstyle

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,8 +1,8 @@
 package duke;
 
-import java.util.Scanner;
-import java.util.ArrayList;
 import java.time.DateTimeException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Duke is a task manager using CLI.
@@ -40,62 +40,62 @@ public class Duke {
         ui.sayHi();
         Scanner s = new Scanner(System.in);
         String command = s.nextLine();
-        while(!command.equals("bye")) {
+        while (!command.equals("bye")) {
             try {
                 try {
                     switch (Parser.parseCommand(command)) {
-                        case "list":
-                            ui.sayList(list);
-                            command = s.nextLine();
-                            break;
-                        case "done":
-                            int finished = Parser.parseNumber(command);
-                            tasks.done(finished);
-                            ui.sayCompleted(list.get(finished));
-                            command = s.nextLine();
-                            break;
-                        case "todo":
-                            if (command.equals("todo") || command.equals("todo ")) {
-                                throw new DukeException(DukeException.Type.TODO);
-                            }
-                            Todo newTodo = new Todo(Parser.parseTodo(command));
-                            tasks.add(newTodo);
-                            ui.sayUpdates(newTodo, list.size());
-                            command = s.nextLine();
-                            break;
-                        case "deadline":
-                            String[] splitD = Parser.parseDeadline(command);
-                            String description = splitD[0];
-                            String date = splitD[1];
-                            Deadline newDeadline = new Deadline(description, date);
-                            tasks.add(newDeadline);
-                            ui.sayUpdates(newDeadline, list.size());
-                            command = s.nextLine();
-                            break;
-                        case "event":
-                            String[] splitE = Parser.parseEvent(command);
-                            String descriptionOfEvent = splitE[0];
-                            String dateOfEvent = splitE[1];
-                            Event newEvent = new Event(descriptionOfEvent, dateOfEvent);
-                            tasks.add(newEvent);
-                            ui.sayUpdates(newEvent, list.size());
-                            command = s.nextLine();
-                            break;
-                        case "delete":
-                            int del = Parser.parseNumber(command);
-                            Task toDelete = list.get(del);
-                            tasks.delete(del);
-                            ui.sayDeletes(toDelete, list.size());
-                            command = s.nextLine();
-                            break;
-                        case "find":
-                            ArrayList<Task> keyTasks = Parser.parseFind(list, command);
-                            ui.sayFind(keyTasks);
-                            command = s.nextLine();
-                            break;
-                        default:
-                            ui.sayWrongInput();
-                            command = s.nextLine();
+                    case "list":
+                        ui.sayList(list);
+                        command = s.nextLine();
+                        break;
+                    case "done":
+                        int finished = Parser.parseNumber(command);
+                        tasks.done(finished);
+                        ui.sayCompleted(list.get(finished));
+                        command = s.nextLine();
+                        break;
+                    case "todo":
+                        if (command.equals("todo") || command.equals("todo ")) {
+                            throw new DukeException(DukeException.Type.TODO);
+                        }
+                        Todo newTodo = new Todo(Parser.parseTodo(command));
+                        tasks.add(newTodo);
+                        ui.sayUpdates(newTodo, list.size());
+                        command = s.nextLine();
+                        break;
+                    case "deadline":
+                        String[] splitD = Parser.parseDeadline(command);
+                        String description = splitD[0];
+                        String date = splitD[1];
+                        Deadline newDeadline = new Deadline(description, date);
+                        tasks.add(newDeadline);
+                        ui.sayUpdates(newDeadline, list.size());
+                        command = s.nextLine();
+                        break;
+                    case "event":
+                        String[] splitE = Parser.parseEvent(command);
+                        String descriptionOfEvent = splitE[0];
+                        String dateOfEvent = splitE[1];
+                        Event newEvent = new Event(descriptionOfEvent, dateOfEvent);
+                        tasks.add(newEvent);
+                        ui.sayUpdates(newEvent, list.size());
+                        command = s.nextLine();
+                        break;
+                    case "delete":
+                        int del = Parser.parseNumber(command);
+                        Task toDelete = list.get(del);
+                        tasks.delete(del);
+                        ui.sayDeletes(toDelete, list.size());
+                        command = s.nextLine();
+                        break;
+                    case "find":
+                        ArrayList<Task> keyTasks = Parser.parseFind(list, command);
+                        ui.sayFind(keyTasks);
+                        command = s.nextLine();
+                        break;
+                    default:
+                        ui.sayWrongInput();
+                        command = s.nextLine();
                     }
                 } catch (ArrayIndexOutOfBoundsException e) {
                     throw new DukeException(DukeException.Type.INVALID);
@@ -118,6 +118,6 @@ public class Duke {
      * @param args Arguments from the command line.
      */
     public static void main(String[] args) {
-            new Duke("./Data/Duke.txt").run();
+        new Duke("./Data/Duke.txt").run();
     }
 }
