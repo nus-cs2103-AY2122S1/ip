b89a6afd37ecd139870e76ad0bd8e92871c3285f
Chan Yu Cheng <e0560244@u.nus.edu>
2021-09-17 15:50:22 +0800

    Update Ui with new profile pictures

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -22,7 +22,7 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/levi.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/levi.jfif"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/duke.jpg"));
 
     @FXML
