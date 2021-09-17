9053aeab8e0e8f3286dc859f4b353fdb56d618ea
Nicolas Chang Weng Yew <nicolas.chang99@gmail.com>
2021-09-17 22:12:45 +0800

    Change import order

--- src/test/java/kermit/tasks/DeadlineTest.java
+++ src/test/java/kermit/tasks/DeadlineTest.java
@@ -1,18 +1,24 @@
 package kermit.tasks;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class DeadlineTest {
+    /**
+     * Tests to get short form for Deadline.
+     */
     @Test
     public void testGetShortForm() {
         assertEquals("D",
                 new Deadline("Test deadline", LocalDate.parse("2021-04-05")).getShortForm());
     }
 
+    /**
+     * Tests to string representation of deadline.
+     */
     @Test
     public void testToString() {
         Deadline deadline = new Deadline("Test deadline", LocalDate.parse("2021-04-05"));
