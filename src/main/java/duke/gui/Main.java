dd77e3ba4dea5cf558363bc95530dc07a5734003
hanif-kamal <hanif.kamal47@gmail.com>
2021-09-17 22:26:31 +0800

    Add Duke title

--- src/main/java/duke/gui/Main.java
+++ src/main/java/duke/gui/Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             fxmlLoader.<MainWindow>getController().printWelcomeMessage();
+            stage.setTitle("Duke");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
