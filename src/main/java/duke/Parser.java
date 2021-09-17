de9d99c796f4666b4f8c42af83f68ac8fd3b476c
Samuel-bit-prog <e0544506@u.nus.edu>
2021-09-17 17:43:56 +0800

    add javadocs

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,27 +1,73 @@
 package duke;
 
+/**
+ * Class for Parser in Duke which
+ * deals with making sense of the user command.
+ *
+ * @author Samuel Lau
+ */
 public class Parser {
 
+    /**
+     * Parse the user command to find out type of command.
+     *
+     * @param command Command from the CLI.
+     * @return String representation of type of command.
+     */
     public static String parseCommand(String command) {
         return command.split(" ")[0];
     }
 
+    /**
+     * Parse command to find out task number in the list.
+     *
+     * @param command Command from the CLI.
+     * @return Integer representing task location in the ArrayList.
+     */
     public static int parseNumber(String command) {
         return Integer.parseInt(command.split(" ")[1]) - 1;
     }
 
+    /**
+     * Parse command to get description for the to-do task
+     * to be created.
+     *
+     * @param command Command from the CLI.
+     * @return Description of the to-do task.
+     */
     public static String parseTodo(String command) {
         return command.split(" ", 2)[1];
     }
 
+    /**
+     * Parse command to get description and date for the deadline task
+     * to be created.
+     *
+     * @param command Command from the CLI.
+     * @return Array containing description and date of the deadline task.
+     */
     public static String[] parseDeadline(String command) {
         return command.split(" ", 2)[1].split(" /by ", 2);
     }
 
+    /**
+     * Parse command to get description and date for the event task
+     * to be created.
+     *
+     * @param command Command from the CLI.
+     * @return Array containing description and date of the event task.
+     */
     public static String[] parseEvent(String command) {
         return command.split(" ", 2)[1].split(" /at ", 2);
     }
 
+    /**
+     * Returns the task to be added to the TaskList from
+     * a line of text in the loaded text file.
+     *
+     * @param line Text in the file that represents a task.
+     * @return Task to be added.
+     */
     public static Task parseData(String line) {
         String[] split = line.split("\\|");
         switch(split[0]) {
--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -7,13 +7,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Class for Storage in Duke which
+ * deals with loading and saving tasks in the given text file.
+ *
+ * @author Samuel Lau
+ */
 public class Storage {
     protected String filePath;
 
+    /**
+     * Constructor for Storage class.
+     *
+     * @param filePath The path of the given text file.
+     */
     public Storage(String filePath) {
         this.filePath = filePath;
     }
 
+    /**
+     * Returns the ArrayList of tasks obtained from loading
+     * the text file.
+     *
+     * @return ArrayList of tasks.
+     */
     public ArrayList<Task> load() {
         ArrayList<Task> list = new ArrayList<>();
         File file = new File(filePath);
@@ -41,6 +58,12 @@ public class Storage {
         return list;
     }
 
+    /**
+     * Save the tasks in the TaskList to the text file
+     * in the specified written format before exiting Duke.
+     *
+     * @param tasks TaskList to be saved.
+     */
     public void writeAll(TaskList tasks) {
         try {
             FileWriter writer = new FileWriter(filePath);
