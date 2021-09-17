4cf283f593d96fe36bb06980d9215c8cc8c272a6
Jason <jasonang0210@gmail.com>
2021-09-17 15:34:18 +0800

    'changes'

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Jason's Duke Program :-)");
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
         } catch (IOException e) {
