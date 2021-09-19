658fd18dd0bb9cf8af0c4139fef4899698a8c7c9
eltonyeh <e0550632@u.nus.edu>
2021-09-20 01:06:30 +0800

    Fix the unclickable textfield issue

--- src/main/java/duke/javafx/MainWindow.java
+++ src/main/java/duke/javafx/MainWindow.java
@@ -43,6 +43,7 @@ public class MainWindow extends AnchorPane {
         dialogContainer.getChildren().add(
                 DialogBox.getDukeDialog(greet, dukeImage, false)
         );
+        userInput.setPromptText("Message...");
     }
 
     /**
