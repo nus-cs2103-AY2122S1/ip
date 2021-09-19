feb8e14b305f8790d38d7733f29e5725c095f31f
ErnestCuong <hltc11032000@gmail.com>
2021-09-19 22:24:42 +0800

    Add Duke test method

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -3,10 +3,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class DukeTest {
-    private Duke test = new Duke();
+    private Duke dukeTest = new Duke();
 
     @Test
-    public void dummyTest() {
-        assertEquals(2, 2);
+    public void getResponse_validUserInput_showSuccess() {
+        String response = dukeTest.getResponse("todo abc");
+        assertEquals((response.split("\nNow you have"))[0],
+                "Got it. I've added this task:\n  [T][ ] abc");
+
     }
 }
