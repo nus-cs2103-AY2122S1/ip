d7cf635528c15033460e4e18ec5b2166adafef30
luluyousef <lolwa.yousef@outlook.com>
2021-09-19 18:09:44 +0800

    The code does not have any assertions.
    Certain Assumptions in the code needs to be verified.
    
    Let's utilize Java Assertions and use the keyword assert to verify assumptions in the code.

--- src/main/java/duke/DeleteCommand.java
+++ src/main/java/duke/DeleteCommand.java
@@ -17,7 +17,7 @@ public class DeleteCommand extends Command {
      * @param u
      * @param storage
      */
-    public  void execute(TaskList tasks, Ui u, Storage storage) {
+    public void execute(TaskList tasks, Ui u, Storage storage) {
         tasks.deleteTask(this.index);
         u.displayTaskRemoved(tasks.getTask(index), tasks.size());
     }
