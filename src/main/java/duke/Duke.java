11efe4872f4e374f890814e6c01c28319e06681c
Linda124 <lindatomy1@gmail.com>
2021-09-19 21:05:03 +0800

    A-CodeQuality

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,10 +2,15 @@ package duke;
 
 import java.util.Scanner;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 /**
  * Class includes methods required for running this project.
  */
-public class Duke {
+public class Duke extends Application {
     private Scanner in = new Scanner(System.in);
     private CompilationOfFiles storage;
     private ListOfTasks tasks;
@@ -33,6 +38,14 @@ public class Duke {
             isEnd = Parser.handleCommand(command, tasks);
         }
         ui.end();
+    }
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
     /**
