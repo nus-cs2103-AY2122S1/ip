0f3475273977be69ca65b389cbeecc57d19d6fb5
aakanshanarain <aakanshanarain@gmail.com>
2021-09-19 16:44:29 +0800

    Rectify problems with the GUI

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -6,8 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class EventTest {
     @Test
     public void testStringOutput(){
-        assertEquals("[E][ ] Finish iP Tasks (at: 7pm)",
-                new Event("Finish iP Tasks", "7pm").toString());
+        assertEquals("[E][ ] Finish iP Tasks (at: Mar 12 2002 11:59pm)",
+                new Event("Finish iP Tasks", "12-03-2002 2359").toString());
     }
 
 }
