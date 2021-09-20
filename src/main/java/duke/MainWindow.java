47842471ad4d8c2f8574b25b0ed743f306e3c6ec
timiditi <timothy.ng.jw@gmail.com>
2021-09-20 19:39:26 +0800

    Add welcome message to Duke

--- src/main/java/duke/MainWindow.java
+++ src/main/java/duke/MainWindow.java
@@ -25,9 +25,14 @@ public class MainWindow extends AnchorPane {
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
+    private static final String WELCOME_MESSAGE = "Welcome to Duke ver T. What can I do for ya?";
+
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        dialogContainer.getChildren().addAll(
+                DialogBox.getDukeDialog(WELCOME_MESSAGE, dukeImage)
+        );
     }
 
     public void setDuke(Duke d) {
