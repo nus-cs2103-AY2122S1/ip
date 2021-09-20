c92980afe794d750436accdb3edcd87fcf52e501
Gordon25 <73068242+Gordon25@users.noreply.github.com>
2021-09-21 01:21:40 +0800

    Update test methods for DukeException

--- src/test/java/duke/main/DukeExceptionTest.java
+++ src/test/java/duke/main/DukeExceptionTest.java
@@ -12,17 +12,17 @@ public class DukeExceptionTest {
     /**
      * Tests the getMessage method.
      */
+    @SuppressWarnings("checkstyle:MethodName")
     @Test
     public void getMessage_StringOutOfBoundsException_CommandIncompleteMessage() {
-        StringIndexOutOfBoundsException e = new StringIndexOutOfBoundsException();
-        DukeException dukeException = new DukeException(e);
+        DukeException dukeException = new DukeException(DukeException.Exceptions.StringIndexOutOfBoundsException);
         assertEquals(dukeException.getMessage(),
             "☹ The duke.task description or duke.command is incomplete.:$\n");
     }
+    @SuppressWarnings("checkstyle:MethodName")
     @Test
     public void getMessage_IllgealArgumentException_DoNotUnderstandMessage() {
-        IllegalArgumentException e = new IllegalArgumentException();
-        DukeException dukeException = new DukeException(e);
+        DukeException dukeException = new DukeException(DukeException.Exceptions.EXCEPTIONS);
         assertEquals(dukeException.getMessage(), "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n");
     }
     public static void main(String[] args) {
