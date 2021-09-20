8b46f0c1322767d9418b036de2d037fb0107fc18
wlren <weilinwork99@gmail.com>
2021-09-20 16:02:45 +0800

    Refactor Duke logic for GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,155 +1,30 @@
 package duke;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.scene.layout.Region;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import java.util.Objects;
-
 /**
- * Contains main() method for Duke chatbot for IP of CS2103 2021.
- *
- * author: Ren Weilin A0216723J
+ * Runs the initialization process for Duke chatbot for IP of CS2103 2021.
  */
-public class Duke extends Application {
+public class Duke {
 
     private final UI userInterface;
-    private final TaskList taskList;
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/images/spongebob.png")));
-    private Image duke = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/images/patrick.png")));
+    private final Parser parser;
 
     /**
      * Initializes the necessary components for Duke to function.
      */
     public Duke() {
-        Parser parser = new Parser();
-        taskList = new TaskList();
+        parser = new Parser();
+        TaskList taskList = new TaskList();
         Storage storage = new Storage(taskList);
         userInterface = new UI(parser, storage, taskList);
     }
 
-//    /**
-//     * Starts Duke chatbot.
-//     *
-//     * @param args CMD arguments.
-//     */
-//    public static void main(String[] args) {
-//        Duke dukeInstance = new Duke();
-//        try {
-//            dukeInstance.userInterface.start();
-//        } catch (DukeException e) {
-//            System.out.println(e.getMessage());
-//        } finally {
-//            dukeInstance.userInterface.start();
-//        }
-//    }
 
-    /**
-     * Enters GUI start
-     *
-     * @param stage The stage for Duke UI.
-     */
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Setting up required components
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-
-        userInput.setPrefWidth(325.0);
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-//        try {
-//            this.userInterface.start();
-//        } catch (DukeException e) {
-//            dialogContainer.getChildren().addAll(
-//                    DialogBox.getUserDialog(new Label(e.getMessage()), new ImageView(duke)));
-//        } finally {
-//            this.userInterface.start();
-//        }
+    public String getResponse(String input) {
+        CommandType command = this.parser.nextCommand(input);
+        try {
+            return this.userInterface.start(command);
+        } catch (DukeException e) {
+            return e.getMessage();
+        }
     }
-
-    /**
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-    }
-
-    /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
-     */
-    private String getResponse(String input) {
-        return "Duke heard: " + input;
-    }
-
 }
--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -1,28 +1,22 @@
 package duke;
 
 import java.time.LocalDate;
-import java.util.Scanner;
 
 /**
  * Contains logic and methods relating to parsing user input from CLI.
  */
 public class Parser {
-    private final Scanner scanner;
-    private String currentCommand;
+    private String currentInput;
     private CommandType currentType;
-
-    public Parser() {
-        scanner = new Scanner(System.in);
-    }
 
     /**
      * Matches first word of line of input with current available commands.
      *
      * @return CommandType The command type of the next line of input
      */
-    public CommandType nextCommand() {
-        currentCommand = scanner.nextLine();
-        String command = currentCommand.split(" ")[0];
+    public CommandType nextCommand(String input) {
+        this.currentInput = input;
+        String command = currentInput.split(" ")[0];
         switch (command) {
         case "bye":
             return CommandType.Exit;
@@ -49,33 +43,33 @@ public class Parser {
     }
 
     public String getSearchTerm() {
-        return currentCommand.split(" ")[1];
+        return currentInput.split(" ")[1];
     }
 
     /**
      * Uses current command type to decide how to parse the description of task.
      *
      * @return Task Either ToDo/Deadline/Event task based on command.
-     * @throws DukeException
+     * @throws DukeException In case of incorrect instruction format.
      */
     public Task generateTask() throws DukeException {
         if (currentType == CommandType.AddToDo) {
-            String description = currentCommand.split("/by ")[0].substring(5);
+            String description = currentInput.split("/by ")[0].substring(5);
             if (description.isBlank()) {
                 throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
             }
             return new ToDo(description, false);
         } else if (currentType == CommandType.AddDeadline) {
-            String description = currentCommand.split("/by ")[0].substring(9);
-            String by = currentCommand.split("/by ")[1];
+            String description = currentInput.split("/by ")[0].substring(9);
+            String by = currentInput.split("/by ")[1];
             LocalDate deadline  = LocalDate.parse(by);
             if (description.isBlank()) {
                 throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
             }
             return new Deadline(description, deadline, false);
         } else if (currentType == CommandType.AddEvent) {
-            String description = currentCommand.split("/at ")[0].substring(6);
-            String by = currentCommand.split("/at ")[1];
+            String description = currentInput.split("/at ")[0].substring(6);
+            String by = currentInput.split("/at ")[1];
             LocalDate eventDate  = LocalDate.parse(by);
             if (description.isBlank()) {
                 throw new DukeException("OOPS!!! The description of an event cannot be empty.");
@@ -92,6 +86,6 @@ public class Parser {
      * @return int Index of task to be deleted/marked as complete.
      */
     public int getIndex() {
-        return Integer.parseInt(currentCommand.split(" ")[1]) - 1;
+        return Integer.parseInt(currentInput.split(" ")[1]) - 1;
     }
 }
