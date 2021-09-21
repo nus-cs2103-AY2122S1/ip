9e7867378feb139c68bcae03f7332383bc51328e
EssWhyy <swenyitang99@gmail.com>
2021-09-21 13:20:16 +0800

    Implement level-10 of individual project
    Converted the text UI to a graphical UI to make the bot more user friendly and appealing
    -Added JavaFX libraries
    -Created java and fxml files for Dialogs and Window
    -Refactored some code in Parser.java
    
    Deleted Text Ui (Ui.java) and files from A-TextUiTesting as they are no longer necessary

--- src/test/java/duke/DukeTest.java
+++ src/test/java/duke/DukeTest.java
@@ -20,29 +20,29 @@ public class DukeTest {
         LocalTime endTime = LocalTime.of(16,30);
 
         assertEquals("[T] [ ] Sleep",
-                new Duke.ToDo("Sleep",false).toString());
+                new duke.task.ToDo("Sleep",false).toString());
         assertEquals("[D] [ ] Finish Project (by: 2021-04-20 08:30)",
-                new Duke.Deadline("Finish Project", date1, time,false ).toString());
+                new duke.task.Deadline("Finish Project", date1, time,false ).toString());
         assertEquals("[E] [X] CS2103T TP (at: 2021-06-09 13:30 to 16:30)",
-                new Duke.Event("CS2103T TP", date2, startTime, endTime, true).toString());
+                new duke.task.Event("CS2103T TP", date2, startTime, endTime, true).toString());
     }
 
     @Test
     public void load_invalidPath(){
-        ArrayList<Duke.Task> emptyList = new ArrayList<>();
+        ArrayList<duke.task.Task> emptyList = new ArrayList<>();
         String WRONGPATH = "./blah.txt";
-        Duke.Storage storage = new Duke.Storage(WRONGPATH);
+        duke.storage.Storage storage = new duke.storage.Storage(WRONGPATH);
 
         assertEquals(emptyList, storage.load());
     }
 
     @Test
     public void getTask_success(){
-        Duke.TaskList tasklist = new Duke.TaskList();
+        duke.tasklist.TaskList tasklist = new duke.tasklist.TaskList();
 
-        tasklist.add(new Duke.ToDo("task1", false));
-        tasklist.add(new Duke.ToDo("task2", false));
-        tasklist.add(new Duke.ToDo("task3", false));
+        tasklist.add(new duke.task.ToDo("task1", false));
+        tasklist.add(new duke.task.ToDo("task2", false));
+        tasklist.add(new duke.task.ToDo("task3", false));
         tasklist.delete(0);
         tasklist.delete(0);
         assertEquals("[T] [ ] task3", tasklist.getTask(0).toString());
