ddb9b0cb0097f82ac91e31be3450838736d44260
CaspianMike-L <looweesing@gmail.com>
2021-09-22 11:54:15 +0800

    GUI Changes: Make GUI refelct pictures as shown on tuytorial

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,11 +1,2 @@
-import duke.Duke;
-import javafx.application.Application;
-
-/**
- * A launcher class to workaround classpath issues.
- */
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(Duke.class, args);
-    }
+package PACKAGE_NAME;public class Launcher {
 }
