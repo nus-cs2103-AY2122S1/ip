2534be7c44d238fef8e769f33a1e07e491cef5d2
Leeroy Liu <leeroyliu999@gmail.com>
2021-09-22 14:56:35 +0800

    Minor fix for checkstyle

--- src/main/java/ailurus/Main.java
+++ src/main/java/ailurus/Main.java
@@ -1,11 +1,8 @@
 package ailurus;
 
 import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
