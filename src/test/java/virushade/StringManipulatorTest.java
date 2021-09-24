f02f60b8e15b24d67d81aed7d64b5e72c4c78a86
GWphua <phuaguanwei99@gmail.com>
2021-09-24 20:14:45 +0800

    Settle small coding convention adjustments.

--- src/test/java/virushade/StringManipulatorTest.java
+++ src/test/java/virushade/StringManipulatorTest.java
@@ -1,12 +1,14 @@
 package virushade;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 
 public class StringManipulatorTest {
     @Test
     public void slashPartitionTest() {
-        assertEquals("Before",StringManipulator.slashPartition("Before/After")[0]);
+        assertEquals("Before", StringManipulator.slashPartition("Before/After")[0]);
         assertEquals("After", StringManipulator.slashPartition("Before/After")[1]);
     }
 
