7f5526db1fb28a17878e6a4a4551a345991f40b8
Jonathan Tan Xin Jie <e0543805@u.nus.edu>
2021-09-25 01:24:33 +0800

    Fixed gradle errors

--- src/main/java/head/Sados.java
+++ src/main/java/head/Sados.java
@@ -201,6 +201,7 @@ public class Sados extends Application {
         borders.setBottom(bottomPanel);
 
         Scene home = new Scene(borders, 500, 250);
+        home.getRoot().setStyle("-fx-font-family: 'Helvetica'");
         stage.setScene(home);
         stage.show();
     }
@@ -279,5 +280,6 @@ public class Sados extends Application {
             filterString = nameString + " + " + dateString;
         }
         Popup.filterPopup(list, filterString);
+
     }
 }
