bc04ce33685ca14adadd090c0ee6e7a0dd88de9d
GnohChengYi <chengyignoh@gmail.com>
2021-09-24 10:29:27 +0800

    Improve code quality - remove deeply nested code

--- src/main/java/blue/handler/DeleteHandler.java
+++ src/main/java/blue/handler/DeleteHandler.java
@@ -23,22 +23,19 @@ public class DeleteHandler extends CommandHandler {
     @Override
     public String handle(String input) throws BlueException {
         String[] arguments = Parser.getArguments(input);
-        if (arguments.length > 0) {
-            try {
-                int index = Integer.parseInt(arguments[0]);
-                if (0 <= index && index <= taskList.getSize()) {
-                    Task task = taskList.remove(index);
-                    String response = "Noted. I've removed this task:\n" + task + "\n";
-                    response += "Now you have " + taskList.getSize() + " tasks in the list.";
-                    return response;
-                } else {
-                    throw new BlueException("☹ OOPS!!! No task found at index " + index + ".");
-                }
-            } catch (NumberFormatException e) {
-                throw new BlueException("☹ OOPS!!! Index must be a number.");
-            }
-        } else {
+        if (arguments.length == 0) {
             throw new BlueException("☹ OOPS!!! The index of delete cannot be empty.");
+        }
+        try {
+            int index = Integer.parseInt(arguments[0]);
+            Task task = taskList.remove(index);
+            String response = "Noted. I've removed this task:\n" + task + "\n";
+            response += "Now you have " + taskList.getSize() + " tasks in the list.";
+            return response;
+        } catch (NumberFormatException e) {
+            throw new BlueException("☹ OOPS!!! Index must be a number.");
+        } catch (IndexOutOfBoundsException e) {
+            throw new BlueException("☹ OOPS!!! No task found at the specified index.");
         }
     }
 }
--- src/main/java/blue/handler/DoneHandler.java
+++ src/main/java/blue/handler/DoneHandler.java
@@ -23,21 +23,18 @@ public class DoneHandler extends CommandHandler {
     @Override
     public String handle(String input) throws BlueException {
         String[] arguments = Parser.getArguments(input);
-        if (arguments.length > 0) {
-            try {
-                int index = Integer.parseInt(arguments[0]);
-                if (1 <= index && index <= taskList.getSize()) {
-                    Task task = taskList.get(index);
-                    task.markDone();
-                    return "Nice! I've marked this task as done:\n" + task;
-                } else {
-                    throw new BlueException("☹ OOPS!!! No task found at index " + index + ".");
-                }
-            } catch (NumberFormatException e) {
-                throw new BlueException("☹ OOPS!!! Index must be a number.");
-            }
-        } else {
+        if (arguments.length == 0) {
             throw new BlueException("☹ OOPS!!! The index of done cannot be empty.");
+        }
+        try {
+            int index = Integer.parseInt(arguments[0]);
+            Task task = taskList.get(index);
+            task.markDone();
+            return "Nice! I've marked this task as done:\n" + task;
+        } catch (NumberFormatException e) {
+            throw new BlueException("☹ OOPS!!! Index must be a number.");
+        } catch (IndexOutOfBoundsException e) {
+            throw new BlueException("☹ OOPS!!! No task found at the specified index.");
         }
     }
 }
