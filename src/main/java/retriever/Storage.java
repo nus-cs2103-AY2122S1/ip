138e3473edc44f1d65473f3b0fc9d0d4a57e343e
Jai2501 <jaylulla9@gmail.com>
2021-09-25 16:49:04 +0800

    Tweak code to follow code style

--- src/main/java/retriever/Storage.java
+++ src/main/java/retriever/Storage.java
@@ -9,7 +9,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.logging.*;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import retriever.task.Deadline;
 import retriever.task.Event;
