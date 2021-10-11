57072bfb0f7874919cd5efb91e4a3a6cd00488a8
NaokiShum <cxjdwedyk097@gmail.com>
2021-10-12 02:01:37 +0800

    Fix checkstyle

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -1,18 +1,18 @@
 package duke;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class ParserTest {
     @Test
-    public void Test1(){
+    public void test1() {
         String test = "BYE";
         assertEquals("bye", Parser.parseCommand(test));
     }
 
     @Test
-    public void Test2(){
+    public void test2() {
         String test = "LiSt";
         assertEquals("list", Parser.parseCommand(test));
     }
--- src/test/java/duke/TaskListTest.java
+++ src/test/java/duke/TaskListTest.java
@@ -1,20 +1,20 @@
 package duke;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class TaskListTest {
     @Test
-    public void Test1(){
+    public void test() {
         Task todo = new Todo("read book");
-        Task deadline = new Deadline("return book"," 2020-08-12");
+        Task deadline = new Deadline("return book", " 2020-08-12");
         TaskList tasks = new TaskList();
         tasks.add(todo);
         tasks.add(deadline);
-        assertEquals(2,tasks.size());
+        assertEquals(2, tasks.size());
         tasks.remove(1);
-        assertEquals(1,tasks.size());
-        assertEquals(todo,tasks.get(0));
+        assertEquals(1, tasks.size());
+        assertEquals(todo, tasks.get(0));
     }
 }
