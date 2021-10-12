9be3eea7e03e2974a2652a02a82ca87c91acf8f8
hanif-kamal <hanif.kamal47@gmail.com>
2021-10-12 20:48:26 +0800

    Refactor Parser to reduce method length
    Fix incorrect exception message for event

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -39,143 +39,169 @@ public class Parser {
      */
     public String parse(String input) throws DukeException {
         String[] words = input.split(" ", 2);
-        String firstParameter = words[0];
+        String command = words[0];
         if (words.length == 1) {
-            switch (firstParameter) {
-            case ("list"):
-                return list.printTasks();
-            case ("notelist"):
-                return noteList.printNotes();
-            case ("done"):
-                throw new DukeException("Please ensure that there is a number after the command 'done'. Try again.");
-            case ("deadline"):
-                throw new DukeException("Please ensure that there is a task description after the command 'deadline' "
-                        + "and a deadline after '/by'. Try again.");
-            case ("event"):
-                throw new DukeException("Please ensure that there is a task description after the command 'deadline' "
-                        + "with a date and time after '/at'. Try again.");
-            case ("todo"):
-                throw new DukeException("Please ensure that there is a task description after the command 'todo'. "
-                        + "Try again.");
-            case ("delete"):
-                throw new DukeException("Please ensure that there is a number after the command 'delete'. Try again.");
-            case ("notedelete"):
-                throw new DukeException("Please ensure that there is a number after the command 'notedelete'. "
-                        + "Try again.");
-            case ("find"):
-                throw new DukeException("Please ensure that there is a search term after the command 'find'. "
-                        + "Try again.");
-            case ("notefind"):
-                throw new DukeException("Please ensure that there is a search term after the command 'notefind'. "
-                        + "Try again.");
-            case ("note"):
-                throw new DukeException("Please ensure that there is a note description after the command 'note'. "
-                        + "Try again.");
-            default:
-                throw new DukeException("I didn't quite get what you meant. Please enter a valid command.");
-            }
+            return checkCase(command);
         } else {
-            switch (firstParameter) {
-            case ("done"):
-                try {
-                    String listIndexString = words[1];
-                    int listIndex = Integer.parseInt(listIndexString);
-                    if (listIndex <= list.size() && listIndex >= 1) {
-                        return list.completeTask(listIndex);
-                    } else {
-                        throw new DukeException("Please ensure that a valid number follows the command 'done'. "
-                                + "Try again.");
-                    }
-                } catch (NumberFormatException e) {
-                    throw new DukeException("Please ensure only a number follows the command 'done'. Try again.");
+            String commandInfo = words[1];
+            return executeCases(command, commandInfo);
+        }
+    }
+
+    private String checkCase(String command) throws DukeException {
+        switch (command) {
+        case ("list"):
+            return list.printTasks();
+        case ("notelist"):
+            return noteList.printNotes();
+        case ("done"):
+            throw new DukeException("Please ensure that there is a number after the command 'done'. Try again.");
+        case ("deadline"):
+            throw new DukeException("Please ensure that there is a task description after the command 'deadline' "
+                    + "and a deadline after '/by'. Try again.");
+        case ("event"):
+            throw new DukeException("Please ensure that there is a task description after the command 'event' "
+                    + "with a date and time after '/at'. Try again.");
+        case ("todo"):
+            throw new DukeException("Please ensure that there is a task description after the command 'todo'. "
+                    + "Try again.");
+        case ("delete"):
+            throw new DukeException("Please ensure that there is a number after the command 'delete'. Try again.");
+        case ("notedelete"):
+            throw new DukeException("Please ensure that there is a number after the command 'notedelete'. "
+                    + "Try again.");
+        case ("find"):
+            throw new DukeException("Please ensure that there is a search term after the command 'find'. "
+                    + "Try again.");
+        case ("notefind"):
+            throw new DukeException("Please ensure that there is a search term after the command 'notefind'. "
+                    + "Try again.");
+        case ("note"):
+            throw new DukeException("Please ensure that there is a note description after the command 'note'. "
+                    + "Try again.");
+        default:
+            throw new DukeException("I didn't quite get what you meant. Please enter a valid command.");
+        }
+    }
+
+    private String executeCases(String command, String commandInfo) throws DukeException {
+        switch (command) {
+        case ("done"):
+            return handleDone(commandInfo);
+        case ("deadline"):
+            return handleDeadline(commandInfo);
+        case ("event"):
+            return handleEvent(commandInfo);
+        case ("todo"):
+            return handleTodo(commandInfo);
+        case ("note"):
+            return handleNote(commandInfo);
+        case ("delete"):
+            return handleDelete(commandInfo);
+        case ("notedelete"):
+            return handleNoteDelete(commandInfo);
+        case ("find"):
+            return list.printFilteredTasks(commandInfo);
+        case ("notefind"):
+            return noteList.printFilteredNotes(commandInfo);
+        default:
+            throw new DukeException("I didn't quite get what you meant. Please enter a valid command.");
+        }
+    }
+
+    private String handleDone(String listIndexString) throws DukeException {
+        try {
+            int listIndex = Integer.parseInt(listIndexString);
+            if (listIndex <= list.size() && listIndex >= 1) {
+                return list.completeTask(listIndex);
+            } else {
+                throw new DukeException("Please ensure that a valid number follows the command 'done'. "
+                        + "Try again.");
+            }
+        } catch (NumberFormatException e) {
+            throw new DukeException("Please ensure only a number follows the command 'done'. Try again.");
+        }
+    }
+
+    private String handleDeadline(String deadlineInfo) throws DukeException {
+        if (!deadlineInfo.contains("/by")) {
+            throw new DukeException("Please state the deadline for this task with /by! Try again.");
+        } else {
+            try {
+                String[] restOfDeadlineSplit = deadlineInfo.split(" /by ", 2);
+                if (restOfDeadlineSplit.length < 2) {
+                    throw new DukeException("Please ensure that there is a task description and deadline. "
+                            + "Try again");
                 }
-            case ("deadline"):
-                String restOfDeadline = words[1];
-                if (!restOfDeadline.contains("/by")) {
-                    throw new DukeException("Please state the deadline for this task with /by! Try again.");
-                } else {
-                    try {
-                        String[] restOfDeadlineSplit = restOfDeadline.split(" /by ", 2);
-                        if (restOfDeadlineSplit.length < 2) {
-                            throw new DukeException("Please ensure that there is a task description and deadline. "
-                                    + "Try again");
-                        }
-                        assert restOfDeadlineSplit.length == 2 : "Deadline should not have anything other than"
-                                + " exactly 2 parts!";
-                        String deadlineDescription = restOfDeadlineSplit[0];
-                        String deadlineDateString = restOfDeadlineSplit[1];
-                        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yy");
-                        LocalDate deadlineDate = LocalDate.parse(deadlineDateString, dtf);
-                        Deadline createdDeadlineTask = new Deadline(deadlineDescription, false, deadlineDate);
-                        return list.addToList(createdDeadlineTask);
-                    } catch (DateTimeParseException e) {
-                        throw new DukeException("Please ensure that your deadline is formatted in the following "
-                                + "way: DD/MM/YY");
-                    }
-                }
-            case ("event"):
-                String restOfEvent = words[1];
-                if (!restOfEvent.contains("/at")) {
-                    throw new DukeException("Please state the date and time for this task with /at! Try again.");
-                } else {
-                    try {
-                        String[] restOfEventSplit = restOfEvent.split(" /at ", 2);
-                        if (restOfEventSplit.length < 2) {
-                            throw new DukeException("Please ensure that there is a task description with a date and "
-                                    + "time. Try again");
-                        }
-                        assert restOfEventSplit.length == 2 : "Event should not have anything other than "
-                                + "exactly 2 parts!";
-                        String eventDescription = restOfEventSplit[0];
-                        String eventDateTimeString = restOfEventSplit[1];
-                        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yy HHmm");
-                        LocalDateTime eventDateTime = LocalDateTime.parse(eventDateTimeString, dtf);
-                        Event createdEventTask = new Event(eventDescription, false, eventDateTime);
-                        return list.addToList(createdEventTask);
-                    } catch (DateTimeParseException e) {
-                        throw new DukeException("Please ensure that your deadline is formatted in the following "
-                                + "way: DD/MM/YY");
-                    }
-                }
-            case ("todo"):
-                String todoDescription = words[1];
-                Todo createdTodoTask = new Todo(todoDescription, false);
-                return list.addToList(createdTodoTask);
-            case ("note"):
-                String noteDescription = words[1];
-                Note createdNote = new Note(noteDescription, LocalDateTime.now());
-                return noteList.addToList(createdNote);
-            case ("delete"):
-                String toDeleteIndexString = words[1];
-                try {
-                    int toDeleteIndex = Integer.parseInt(toDeleteIndexString);
-                    return list.deleteTask(toDeleteIndex);
-                } catch (NumberFormatException e) {
-                    throw new DukeException("Please make sure only a number follows the command 'delete'. "
-                            + "Try again.");
-                } catch (StringIndexOutOfBoundsException e) {
-                    throw new DukeException("Please add a number after the command 'delete'. Try again.");
-                }
-            case ("notedelete"):
-                String toDeleteNoteIndexString = words[1];
-                try {
-                    int toDeleteNoteIndex = Integer.parseInt(toDeleteNoteIndexString);
-                    return noteList.deleteNote(toDeleteNoteIndex);
-                } catch (NumberFormatException e) {
-                    throw new DukeException("Please make sure only a number follows the command 'notedelete'. "
-                            + "Try again.");
-                } catch (StringIndexOutOfBoundsException e) {
-                    throw new DukeException("Please add a number after the command 'notedelete'. Try again.");
-                }
-            case ("find"):
-                String searchTerm = words[1];
-                return list.printFilteredTasks(searchTerm);
-            case ("notefind"):
-                String noteSearchTerm = words[1];
-                return noteList.printFilteredNotes(noteSearchTerm);
-            default:
-                throw new DukeException("I didn't quite get what you meant. Please enter a valid command.");
+                assert restOfDeadlineSplit.length == 2 : "Deadline should not have anything other than"
+                        + " exactly 2 parts!";
+                String deadlineDescription = restOfDeadlineSplit[0];
+                String deadlineDateString = restOfDeadlineSplit[1];
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yy");
+                LocalDate deadlineDate = LocalDate.parse(deadlineDateString, dtf);
+                Deadline createdDeadlineTask = new Deadline(deadlineDescription, false, deadlineDate);
+                return list.addToList(createdDeadlineTask);
+            } catch (DateTimeParseException e) {
+                throw new DukeException("Please ensure that your deadline is formatted in the following "
+                        + "way: DD/MM/YY");
             }
         }
     }
+
+    private String handleEvent(String eventInfo) throws DukeException {
+        if (!eventInfo.contains("/at")) {
+            throw new DukeException("Please state the date and time for this event with /at! Try again.");
+        } else {
+            try {
+                String[] restOfEventSplit = eventInfo.split(" /at ", 2);
+                if (restOfEventSplit.length < 2) {
+                    throw new DukeException("Please ensure that there is a task description with a date and "
+                            + "time for this event. Try again");
+                }
+                assert restOfEventSplit.length == 2 : "Event should not have anything other than "
+                        + "exactly 2 parts!";
+                LocalDateTime eventDateTime = LocalDateTime.parse(restOfEventSplit[1],
+                        DateTimeFormatter.ofPattern("dd/MM/yy HHmm"));
+                Event createdEventTask = new Event(restOfEventSplit[0], false, eventDateTime);
+                return list.addToList(createdEventTask);
+            } catch (DateTimeParseException e) {
+                throw new DukeException("Please ensure that your deadline is formatted as DD/MM/YY");
+            }
+        }
+    }
+
+    private String handleTodo(String todoInfo) {
+        Todo createdTodoTask = new Todo(todoInfo, false);
+        return list.addToList(createdTodoTask);
+    }
+
+    private String handleNote(String noteInfo) {
+        Note createdNote = new Note(noteInfo, LocalDateTime.now());
+        return noteList.addToList(createdNote);
+    }
+
+    private String handleDelete(String toDeleteIndexString) throws DukeException {
+        try {
+            int toDeleteIndex = Integer.parseInt(toDeleteIndexString);
+            return list.deleteTask(toDeleteIndex);
+        } catch (NumberFormatException e) {
+            throw new DukeException("Please make sure only a number follows the command 'delete'. "
+                    + "Try again.");
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new DukeException("Please add a number after the command 'delete'. Try again.");
+        }
+    }
+
+    private String handleNoteDelete(String toDeleteNoteIndexString) throws DukeException {
+        try {
+            int toDeleteNoteIndex = Integer.parseInt(toDeleteNoteIndexString);
+            return noteList.deleteNote(toDeleteNoteIndex);
+        } catch (NumberFormatException e) {
+            throw new DukeException("Please make sure only a number follows the command 'notedelete'. "
+                    + "Try again.");
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new DukeException("Please add a number after the command 'notedelete'. Try again.");
+        }
+    }
 }
+
