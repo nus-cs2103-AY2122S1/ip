a212449b1c67c49086e6820b926f5d5ef45dce9f
Alfred Koh <alfredkohkj@gmail.com>
2022-01-18 17:14:56 +0800

    Completed Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.sql.SQLOutput;
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,28 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke\r\nWhat can I do for you?");
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            String command = scanner.next(); // Can also convert result to lower-case to handle cases.
+            switch(command) {
+                case "list":
+                case "blah":
+                    System.out.println(command);
+                    break;
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                default:
+                    System.out.println("Command don't exist. To exit, type the command 'bye'");
+            }
+            if(command.equals("bye")) {
+                break;
+            }
+        }
+
+
+
     }
 }
